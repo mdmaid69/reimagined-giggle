e414fa09d8698d252fba2027fbd314c13d3ba0c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:36 +0000

    greeting trousers recommend show nervous

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
