b571764480260bed785a9a90fea2d93fec049dc8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:37:10 +0000

    pity brother various better both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
