498d218625c65a9a6708bf82c9b6a0105ec51fef
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:49 +0000

    beat style pump put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
