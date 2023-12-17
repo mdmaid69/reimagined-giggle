f5fe782454c44efdac9bde7bd4b39079637934c6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:26 +0000

    neither share mainly pull project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-for i in range(5):
-        print(i)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
