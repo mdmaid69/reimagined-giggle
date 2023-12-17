038c0c99abc5fc62e9085732d73996dd0d86bb3e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:45 +0000

    put bet with tip alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
