3ca3f8127f5479434c3208f3272329fcaf91fba7
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    thick university measure say win

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
