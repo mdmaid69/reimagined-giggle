a7af73b47d75ae6e94d71ff915c647911bd19a36
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:53 +0000

    prepare possible high play concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
