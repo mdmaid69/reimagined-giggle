87d7606eb160be21a5a4adbccb1c571fea7cd3ae
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:39:38 +0000

    generous male couple chief noise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
