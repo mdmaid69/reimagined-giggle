77631c4ccc583e8b68edc58b3853721102fade3d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:47 +0000

    born temperature lead although taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
