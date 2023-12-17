b601f25198fb02c3550128c4f1cb8ee5961b41bc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:59 +0000

    secret shine note single none

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(x, y):
+        return x + y
