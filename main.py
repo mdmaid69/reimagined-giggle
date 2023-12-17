fde7d36d25ef5b30bd5abc045555eae848d2082f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:27 +0000

    fix wrong tap goup Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
