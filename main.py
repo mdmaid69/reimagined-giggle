f1f0e61a7ab96f43c15e5a280814339b3dad1fd4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:55 +0000

    certain father game either brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
