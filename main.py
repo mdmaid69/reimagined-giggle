ce155d145ba885e36bbb37d18b768263e05ed104
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:25 +0000

    drive seem clever bin content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_area_rectangle(l, w):
+        return l * w
