df93728f4f503cb23a5668a8bb7be46d3c1ad848
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:57 +0000

    you convenient petrol holiday concerned

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_current_time():
+        return time.ctime()
+for i in range(5):
+        print(i)
