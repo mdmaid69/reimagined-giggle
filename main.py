f709633286121c93ead831a312454f36ed6414dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:49 +0000

    weekend everybody exactly entry memory

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
