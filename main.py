d8f6a7802a861fdca0894ed997616e6614ad98db
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:07:55 +0000

    around theirs fashion comment question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_memoryview(array):
+        return memoryview(array)
