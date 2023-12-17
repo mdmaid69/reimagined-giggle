085650e1927940c07e0cb51f85d66f1db2312f8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:00 +0000

    file respect about doubt dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_work(force, distance):
+        return force * distance
