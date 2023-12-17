3326f80ed347e16a2082c0843744da7cdcad8a8a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:00 +0000

    pay himself opposite prove

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def count_words(sentence):
+        return len(sentence.split())
+def reverse_string(s):
+        return s[::-1]
