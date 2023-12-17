78039ef5831337f2d7ff3803058ffa431fc9d6c5
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:47:19 +0000

    worse journey modern bend curious

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
