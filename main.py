8ca0428bc8ead76013ea0ae95cd2e675e24c5b89
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:37 +0000

    children tonight rather goaway push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
