b313193431446504be01dc609f8728a9022f279d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:51 +0000

    save praise peaceful less round

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
