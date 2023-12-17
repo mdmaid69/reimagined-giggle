00c8d31099032c4166f16a3e1dd81e6698d1d37e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:56:11 +0000

    forgive declare search tape ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def square_number(x):
+        return x**2
