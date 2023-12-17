50e6a92d1f715ade03dd8adfb10535b4ff9e2666
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:59 +0000

    bottom female what settle try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
