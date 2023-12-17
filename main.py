7c9a7d9fb8d5e4a3aa6189c80ce0a548350053a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:46 +0000

    pause smile connection standard greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
