acef6df2729a376c680a6592541689709cd88c65
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:11 +0000

    careful happen not muscle there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
