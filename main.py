a3372d5bc3b00bdba6622c63f5fc6e3c20ce69d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:46 +0000

    somehow glass run detail waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_repr(array):
+        return repr(array)
