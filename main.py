02cd17bc7b5ca2f0e2faa8936ec5dd0ad7083263
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:14:17 +0000

    reason natural perhaps inquire point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_length(array):
+        return len(array)
