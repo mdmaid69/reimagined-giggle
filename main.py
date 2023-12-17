558632a2d7bd8b9441f8beeb539668de0f41ebdc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:35:23 +0000

    poor let rush little improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
