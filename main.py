4b7bf0f7f6546d3402757412b7258fa2c9f8c8ee
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:44:55 +0000

    short make after conduct land

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
