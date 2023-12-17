b11a236bd17f894b23e67efde9c4339f5eb7a2cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:19:19 +0000

    indeed wing total kick block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
