b8fe7b4cc55b1df4b3ca7abd320be211b91431f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:42 +0000

    herself correction reliable university turnon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
