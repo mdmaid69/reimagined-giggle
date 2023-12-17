8a244701d17376b071dffb92e1afb5faefcd432a
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:28:15 +0000

    male wait birth law claim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
