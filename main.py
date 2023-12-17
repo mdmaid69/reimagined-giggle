14841eb9533af2b97be3fb22921f5e43dcdc83dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:17 +0000

    consult stick process contents lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
