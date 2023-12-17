f8105ab5335543d8423f7310cd0f25c8e009a960
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:15 +0000

    contribute crowd city shame nature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
