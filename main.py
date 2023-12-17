f455b24f54957ed6a128f7d0f422773dce344d06
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:54 +0000

    only this usually relation Oo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+text = "Hello, world!"
+print("Words:", len(text.split()))
