edb3c38242fc1170f53b1eb63f9ffdc766d193dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:28:52 +0000

    stamp degree shrink neck keepaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+print(random.randint(0, 100))
