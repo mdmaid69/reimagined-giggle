70e953c5cf2904b25504f905dbb813284d9f8d77
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:18 +0000

    finally like smell borrow welcome

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_length(array):
+        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
