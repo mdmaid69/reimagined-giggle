556d1f6d36e0ba565a0d98c197b1bcfe40a5b9a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:57 +0000

    female dare main over willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_min(lst):
+        return min(lst)
