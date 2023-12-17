d1bf596f1c0ea1331024f534343001c1fa705f4d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:27 +0000

    please trouble reason runout Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
