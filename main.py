086f20fda1c93c83bb896df8719f0bd5f61e557d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:09 +0000

    comeround welcome detail before stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_characters(sentence):
-        return len(sentence)
+  def get_current_directory():
+        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
