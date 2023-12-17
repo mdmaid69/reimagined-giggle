fc552a04aaad3001307192eabd5d86bab512b783
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:50 +0000

    ocean exit public secret market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
