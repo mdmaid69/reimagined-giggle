34f4439b094f64b66b255313ea4ceeeb4d6f12d3
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:50 +0000

    typical public drop name development

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
