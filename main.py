8b5d94667a28385d39d51e81f0c7624e66ede9be
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:16 +0000

    on ought tidy black content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def add_numbers(x, y):
-        return x + y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
