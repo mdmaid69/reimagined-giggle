8ef1908e171f7630c68411cf57b2845ccfc75bc2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:26 +0000

    suit wild compete other on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
