0664d0a12ca7c677255f85feb4440ebe256ac1f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:30:03 +0000

    sleep judge getout reject noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
