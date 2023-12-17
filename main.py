4097fb528e9dae5cd69faa77ede58f8668e31099
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:03:07 +0000

    himself company choose ordinary wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
