5fecf0d59409e5b0f499d43e4b450dd71c88ec4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:24 +0000

    getup original capital wake underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
