afd23b0b990030bc4558bbf2dfffaa2aac3e0723
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:19 +0000

    long safety finally new apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
