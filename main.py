3b4aa038ab0ee02320c4434fe8b29f3943d6b8fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:41 +0000

    arrival in fill normal miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
