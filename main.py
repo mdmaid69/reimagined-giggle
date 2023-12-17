aa3c3d5e23df934757a4463aca18237626064035
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:19 +0000

    practical process takeback bring influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
