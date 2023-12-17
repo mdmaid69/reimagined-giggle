ff52658657e263339faf14fc1f6e09df205af563
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:58 +0000

    same target plastic style cold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
