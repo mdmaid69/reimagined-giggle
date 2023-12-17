1a1ef8f187683f95b7dd199f2a988fc735d83c4d
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:12 +0000

    even sticky female bang broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
