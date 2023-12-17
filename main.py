2e525e18ce00e5c70f859d6ea6fed0a763e92995
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    kind  smooth failure occasional

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
