477dc1b88eac9a669e7d0a39a94f97fab356b91f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:50 +0000

    skirt pair less region middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
