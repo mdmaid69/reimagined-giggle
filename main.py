138cbbcbf7e8915ae69d212913c82671ce770f42
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:43 +0000

    somewhere duty at such extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
