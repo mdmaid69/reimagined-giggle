46b48c33c43703b96c2707ebf7ad73e0ab843f6f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:27 +0000

    play early leave anything match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
