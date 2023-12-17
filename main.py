8c4af5ec6c7c4279d46c8f389524073ebc6b8484
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:52 +0000

    like neck object thinkthrough almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
