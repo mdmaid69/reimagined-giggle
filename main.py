1d2324896ff7a84462877465a9d7cf58906d6102
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:07 +0000

    noise serious Ll pinch wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def split_path(path):
+        return os.path.split(path)
+def is_palindrome(s):
+        return s == s[::-1]
