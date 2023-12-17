73610c94afcfadf85a1864723f5099de3620ae46
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:53 +0000

    intend case warn attempt may

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def multiply_numbers(x, y):
-        return x * y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_unique_words(sentence):
+        return set(sentence.split())
