f3d42833c962f72da83fd5d1401a3515a476d3bc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:53 +0000

    freedom geton judge blame hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
