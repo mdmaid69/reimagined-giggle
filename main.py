aa918e09b55b2b71c66a7b561264dbce9e8a235d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:08 +0000

    marry basis spray give ball

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_power(work, time):
+        return work / time
