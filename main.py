75b9a24152ba0984e95f9a6d679703ad3f2a7fd3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:26 +0000

    determined getoff walk wall steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_length(array):
-        return len(array)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
