57b1f59563e000499b9e2200b81eb1ec90943f27
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:16 +0000

    mixture runout answer bad direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def subtract_numbers(x, y):
+        return x - y
