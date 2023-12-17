ec911a968c3b251ff11184ba26548ba11b118991
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:20 +0000

    colour before than calm file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
