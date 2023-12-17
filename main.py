8c0c4eb7c7be887f21a54ba1620378dd4cae6b22
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:48 +0000

    trade hurt number wheel move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
