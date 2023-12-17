5aaef77aae0e395501089059ef9c823f8dabf9d1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:05:03 +0000

    guard dare obvious design turnoff

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
