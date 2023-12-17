6fe03b3239e7e359681b7d86fd002d3b8d86e0b8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:36 +0000

    equal mean exchange backwards trick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
