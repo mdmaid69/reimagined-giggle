aeb4a4c04a1a31ef69c25551c04ca8a7e61598fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:46 +0000

    offer charge feel prove university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
