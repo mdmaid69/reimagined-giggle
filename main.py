657e266359b3bead27bceaca74ce1ebcdb55c6fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:28 +0000

    too furniture scare education trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+  def count_elements(lst):
+        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
