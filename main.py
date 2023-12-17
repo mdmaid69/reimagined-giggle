803684319ca83862ed5cfde466537dd4c72af2e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:56:17 +0000

    talk want sweep excellent absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
