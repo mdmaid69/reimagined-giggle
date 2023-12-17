ae8be16385ca43cad0e6e0a66e7328e91925b0fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:48:22 +0000

    shout action dark comment quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def subtract_numbers(x, y):
+        return x - y
