b0ed9ad490415e1fb2321b7a61310ccfb45fa4b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:53:45 +0000

    keep object pause live Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
