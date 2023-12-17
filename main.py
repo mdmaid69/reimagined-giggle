40ed74bdc03ad410e4ef686a50c2fd77660c1881
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:47 +0000

    conclusion size rain freedom difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
