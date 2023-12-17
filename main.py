bea083a4c2d2cbda0cd2c3e00aa016f3384c2a40
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:08 +0000

    fair deaf thought rise long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
