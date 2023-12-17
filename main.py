fa038a262f083f4fbca905ad4a45077c7466bfa6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:43 +0000

    me accident skirt child wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
