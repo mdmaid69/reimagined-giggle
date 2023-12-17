29146bebd90a731212d1629239c1d20cb9d8e2be
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:26:27 +0000

    surprising what contribute our become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
