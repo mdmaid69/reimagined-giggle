229825a79a07ae0f2333f38fb44402d2413dcc82
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:04:48 +0000

    569enjoy race unable grand touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
