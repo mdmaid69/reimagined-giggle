d8a2f0cfa5506aec385d4df262ff256271ddf915
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:06:18 +0000

    say precise married completely meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
