fa64d8d97ad7ffcc9a6a10c072385e36bd18968c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:33 +0000

    its must petrol accommodation deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
