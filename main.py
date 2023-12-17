bf0504c2723c7a71a672704586cc4b23b7145743
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:56 +0000

    ring whistle identify better age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_set(array):
+        return set(array)
