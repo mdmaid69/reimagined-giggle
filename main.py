fb618fc87659fa99213ee441e40304cd3b8ad97a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:50 +0000

    exact one bend safety performance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
