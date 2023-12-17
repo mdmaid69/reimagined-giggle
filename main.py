076b0c0bb8d4dc252a43027e841bc4ebaa3f2e4b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:45 +0000

    treatment catch name usual defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
