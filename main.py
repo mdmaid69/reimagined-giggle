4a724e0f43f77f7b82075daa51a5dc44e926a366
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:48 +0000

    absolutely off stare belong earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
