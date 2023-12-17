26fdd4b56a24d0fd4e6f6f2468aeabb6d98c38d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:35:32 +0000

    even really serve lay polite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
