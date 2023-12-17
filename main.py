bbd4c6afb8ba8a9068880d2b740a735a2e6d5711
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:27 +0000

    exact pipe shine mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
