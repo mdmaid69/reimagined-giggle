6147d5e5f1878de0bf2c441fadd4bd7beae15b54
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:34 +0000

    unpleasant something excuse disturbing yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
