1867ca86cee2928089d7e0d87b6eda60a25b034a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:50:28 +0000

    big claim declare owe bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
