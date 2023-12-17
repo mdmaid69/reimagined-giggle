67e1177f2f797307fa4954c06452bc80daddf89e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:29 +0000

    behaviour earn tall freeze fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
