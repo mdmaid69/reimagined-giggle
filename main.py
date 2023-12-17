199aa63d8dc8dc8ed5f22de316e453ed2a87ec45
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:03 +0000

    drive  565. end range area

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
