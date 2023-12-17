f2536402cb3193d8dbfce300485485b3c5de27e3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:33 +0000

    scream close bear bush form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import datetime
+print(datetime.datetime.now())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
