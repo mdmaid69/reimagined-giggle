e0bccbd17529f3f374c560c826a96396b2cc8362
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:04 +0000

    far major patience authority previous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def convert_to_hex(n):
+        return hex(n)
