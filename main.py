a583494a456d78b2a70a1cfab1686addb40d667d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:22 +0000

    train feature keepup statement dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
