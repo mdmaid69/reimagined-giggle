12ecef98964c72fa3f069cc6f1d5fabad28076ff
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:44 +0000

    tap simple from heavy danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
