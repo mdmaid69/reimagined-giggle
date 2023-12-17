0a81798c320be3fb55f05300d4bddb8e4e3f9d5c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:17:36 +0000

    giveaway takeback greeting convince straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
