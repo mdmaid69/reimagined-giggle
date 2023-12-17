6b7bf603c2ebc15812d971ca44e63716d1f32aa9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:03 +0000

    downstairs weather cinema attention slight

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
