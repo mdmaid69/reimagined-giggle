c48fed82fbcb6b1d202686767215af9fe5e6c4ee
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:20 +0000

    571enough gentle nigga steam claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
