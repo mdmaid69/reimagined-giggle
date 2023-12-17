14abea2d278915bc0919d25bd55aebdda5cf9171
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:33 +0000

    steam test mix dress callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
