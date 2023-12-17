087064a4c530ad08c87acc19300f4392812b9711
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:55:59 +0000

    interview sting pupil next nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
