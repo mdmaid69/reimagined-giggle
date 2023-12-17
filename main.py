e1a5cbc79a2263610c99aea312c00042fe4be1a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:50 +0000

    comeup worse several book bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
