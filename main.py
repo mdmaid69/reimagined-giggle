4f5e7df731efaf808a8ba650149accc63f0885be
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:21:22 +0000

    uncertain dream sell float all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_counter():
+        return collections.Counter()
