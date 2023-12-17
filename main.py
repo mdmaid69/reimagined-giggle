7d6917ef0f502d9d5b46f8c8cb819a31fdff7c09
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:30:18 +0000

    hole radio inform leg guess

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
