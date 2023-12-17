4a25da81cbba9fedb2319821b6bacb77ccf7546a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:09 +0000

    above stone weigh ready spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
