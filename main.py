fa043a997f5bab094f4214c4a2d72b6a2ca2e83e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    burn fight cost right cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
