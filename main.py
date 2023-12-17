65df791190b63476c3c2af2ea4912f55ea719469
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:29 +0000

    while illness flat bet sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
