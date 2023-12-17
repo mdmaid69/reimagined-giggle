ee607a7aebd5cfe98487d4128a8adf2f6bf69fbc
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:35:08 +0000

    grade try instead bad branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
