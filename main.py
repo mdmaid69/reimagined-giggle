892250aa41b59924eb6c6e0ea576620bc7aab341
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:21 +0000

    edge shout board on zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
