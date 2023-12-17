b7ed8b2dda839279c5b7c776b1f043cc738463f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:48 +0000

    weigh big draw guide loud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
+def add_numbers(a, b):
+        return a + b
