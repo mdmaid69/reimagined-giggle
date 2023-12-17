1598c0be6254f1384890af53bfaeaddc635e9050
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:51 +0000

    avoid type terrible change charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def convert_array_to_string(array):
+        return array.tostring()
