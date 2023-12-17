0fe2638a0abbc6dcf2ed29ac4068da5b8b892612
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:52 +0000

    spend rush birth what concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
