3c54bfe828535c27c791c6c85b42a63db8072e60
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:34 +0000

    smell rain afford lie manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
