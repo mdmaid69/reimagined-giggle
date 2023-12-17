bf459e900eb5167addcbaf62c3a721e8b9bfc163
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:35:10 +0000

    stupid fire awake sore 569. enjoy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
