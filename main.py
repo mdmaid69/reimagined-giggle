0939fffab3d8ba35d0f1fd308eed030613760929
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:54:00 +0000

    mix back break shame equal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+print([x**2 for x in range(10)])
