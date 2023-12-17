7d0b16563e31ea41ecd48b4d16a75559b61d8125
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:20:57 +0000

    phone opportunity happy round generous

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_priority_queue():
+        return collections.deque()
