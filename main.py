eb111cf72013530d2cdc9ada8c9939867c060a90
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:14 +0000

    tip different secret total bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
