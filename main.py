e41d4b9981e96f9782909f543ed2ee88a3b4170e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:15 +0000

    declare wide allow meal callup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
