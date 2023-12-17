ca351dd0fb9a0ebf2f04a94e218efaf8897520b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:13:48 +0000

    child lead computer Ii tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
