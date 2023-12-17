423ff9a5df1a30bf0ad7ad6c5ee6f0a8e431c5e8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:20 +0000

    find become decide little firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
