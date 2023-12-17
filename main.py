abd4ceeed2af31be95e955bb63b4dbb8c0b942b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:13:30 +0000

    bread first now Ww cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
