fe97f698d11edd9b5cc932cc31ad0c0a541b38dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:08 +0000

    vision left fit geton persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
