6783e7e8441f1ea93c926438b5cd1e80065fd891
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:39:08 +0000

    immediate search complain general zero

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
