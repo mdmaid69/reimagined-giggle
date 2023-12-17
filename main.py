6654707a74c3a76dfbd34da7fb17a9ea25a4dffc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:17 +0000

    reason attempt feeling direct deep

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
