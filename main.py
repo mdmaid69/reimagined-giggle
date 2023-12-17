8a2834634abcad2a62a3ca3351f91bb21500e8b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:40 +0000

    lean within anyone thumb circumstance

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+def find_max(numbers):
+        return max(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
