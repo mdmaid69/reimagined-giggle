4e122636acb20f36d09d2f3a6239929da5c917e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:25 +0000

    spot takeoff count basic brave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sys
+  def get_python_version():
+        return sys.version
