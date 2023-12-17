17f390bcd2f2efcf2d73e8e25426a0399c8e9fcc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:04 +0000

    talk activity rest respect deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
