43662fb690a268f0bde4ccac23407b3d4775b741
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:16:09 +0000

    judge acceptable mother place bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
