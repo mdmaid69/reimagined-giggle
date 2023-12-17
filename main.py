fbac527c51160b231ccb526a4e4f2a2ce2bb0164
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:58 +0000

    branch command pride scream instead

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
