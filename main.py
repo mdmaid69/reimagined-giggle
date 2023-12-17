705e9540fa0c9265e49267854998a31c7766fda2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:31 +0000

    communication phone style visit explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
