a77e19a1a517e7f1472069c592fa5eac0d3ea86d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:36 +0000

    culture unkind breakinto people winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_bool(array):
+        return bool(array)
