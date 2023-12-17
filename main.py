6287f861b6813d167a70336eaed87f72b6938104
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:29 +0000

    smooth nurse control whether sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_counter():
+        return collections.Counter()
