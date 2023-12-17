ad9ad314a9f4979e65a5aabd365fe40dcb8a711d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:55 +0000

    like damp spoil straight straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
