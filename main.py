dce7604539f902ed383c36e173e88cd1cdd1bbeb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:21 +0000

    still back light pressure upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
