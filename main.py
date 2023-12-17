e810438cde2c89daffcaa302b8c6540ad003e61c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:17:13 +0000

    mainly demand quarter offer read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
