e59e59181ec4aade1ba3c4389a63f968ec493e44
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:13 +0000

    Zz rest children father father

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
