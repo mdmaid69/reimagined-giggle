4c83d6f20637976bc6d286235eb352a53c27dea5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:32 +0000

    thanks responsibility likely answer alone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
