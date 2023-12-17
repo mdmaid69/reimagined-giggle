e482fa47365b884dbab6b94a189f67204afa3101
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:36:52 +0000

    probably Hh breakup giveup force

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
