9544d0aefa67db68afd761fece8fd6cffae79a34
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:30 +0000

    abroad anywhere shake cost wet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
