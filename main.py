d6be68887a500c7ffdf8cd8b1d7aee4aa87044b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:54 +0000

    before improve prefer test basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
