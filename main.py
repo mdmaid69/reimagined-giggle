9e4b75a835496e6cfc400181c99c232b7d55254b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:58 +0000

    breathe workon unit underneath imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
