f0111dcf932d16cd2c9d4138dfbc01ce3ee359f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:15 +0000

    father somehow necessary inside moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
