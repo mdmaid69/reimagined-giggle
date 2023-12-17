0f9cd02b1bf901621a456cad719366c87c1d33a5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:21 +0000

    niggaacademy where pause store report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
