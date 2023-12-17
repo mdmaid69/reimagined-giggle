4014c20ca5f15a52596b83018f9ce3873ba6b82c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:42 +0000

    tradition bread completely taste straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
