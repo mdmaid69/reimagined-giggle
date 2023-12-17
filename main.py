b089a4b68aad1869ea8f3218c6e98e2a267bbb86
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:12 +0000

    trial break machine spite breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
