220ef54a9c0a707cceba7a3f23afd89b32639102
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:27 +0000

    record wake somehow insist match

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
