a153001d95c9128eab1f88a2ec05f1665f65c2b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:28 +0000

    method ride sort voice carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
