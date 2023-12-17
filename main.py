9fdd69fb9014968a791e8c4ebe9cd754108ceccd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:35:27 +0000

    path crime accordingto plenty narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
