81a2ea95b115df8b0b179e00c2b0cffce29a0326
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:46 +0000

    identify chase pretty contrast close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
