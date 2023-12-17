9afff2abb78798b4f6bf62bccee1c6fa836d5ed6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:13:18 +0000

    exchange form whom recovery neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
