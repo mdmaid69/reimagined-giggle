db0f7601df3f4d978ee0ade0def8e2cc314461e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:38:42 +0000

    further wherever strike bang refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
