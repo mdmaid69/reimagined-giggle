4f2cde1576e63a48b60dc9e2784e19ae65ca4e70
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:35 +0000

    attract average whisper stretch wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
