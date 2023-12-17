c4854e6a3d87e9bec67bcc01ee0cbdb95f94b510
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:18 +0000

    declaration situation ask aware modern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
