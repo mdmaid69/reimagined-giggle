7ceb319eddc77318bf523ce05aaf7d4eeccfc234
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:18 +0000

    crisis law kind brave arrangement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
