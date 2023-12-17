9c4a0637efb4cecd5bd3ab9ba27b7a6c53be850e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:08 +0000

    season slowly please Tt part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
