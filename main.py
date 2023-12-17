7768ddae2a1f70f12e4fb39c57d048186c31de8e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:14:37 +0000

    press seriously height weigh office

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+print(datetime.datetime.now())
+def find_unique_words(sentence):
+        return set(sentence.split())
