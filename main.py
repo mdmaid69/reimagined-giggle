04ac8827ae0ac5a6d362a07130ba7317b1b3757a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:49 +0000

    interrupt chat pride regularly trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def is_odd(n):
+        return n % 2 != 0
