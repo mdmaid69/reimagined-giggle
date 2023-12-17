6e03ad5d9b0fbfc42c9d3083926a0ce37c5d7329
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:04 +0000

    nerve talkinto find card immediately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
