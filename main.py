6878cd508aa0f86a740cd829e3f817993a0d83d0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:37 +0000

    note change major clear cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
