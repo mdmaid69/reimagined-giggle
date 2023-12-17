b05d750ab9248fb0fc63777fea7da9d32e1d064b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:28 +0000

    major previous holiday silent distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
