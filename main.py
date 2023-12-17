9970fbd4304befe66e38a6193522f829e09b6f66
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:33 +0000

    part show cause fact sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
