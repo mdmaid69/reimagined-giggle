408ec22024d62fac26ceb3b54231eaf1a2c194e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:53 +0000

    own sentence wipe most soft

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import time
-print(time.time())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
