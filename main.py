f2c4ca9e8966cafdd3f3224abea95e818ea90e91
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:40 +0000

    create foreign rise honest few

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
