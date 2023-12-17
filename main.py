7ad0f74568132c8331c5b0ce1897ab3417c4d88b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:05 +0000

    sure breakup release often crash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def remove_directory(path):
+        os.rmdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
