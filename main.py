6dc1a48a05dadf22faf20e1da486b2b7b1f35597
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:02:23 +0000

    crime shock minute somebody goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
