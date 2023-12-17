e9a20ec2f91d464add97b899e1bda6bf125d7d25
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:59 +0000

    back excited regret print frighten

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
