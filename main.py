abfa2e448219b969e701562c0725a9f2e80faaa3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:30:45 +0000

    decision promise shoot less embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
