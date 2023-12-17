70517c5605e16a6e2aacca98e574f532298c2f8d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:10 +0000

    popular increase holdout excuse cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
