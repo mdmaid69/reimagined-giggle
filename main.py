fe7a6dee0c26645f95cb8f9be2a64eb2660d7d85
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:09 +0000

    scare arrival mess face smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
