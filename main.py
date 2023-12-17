1ef5521f6c0d8ada64ea6950d8cdc4319dcc3e73
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:53 +0000

    man crowd water protest along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
