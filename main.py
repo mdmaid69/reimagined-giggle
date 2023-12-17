ac051c34cb450f29ca0f40ea242c2582e08b5467
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:39 +0000

    slip fall to loss thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def remove_directory(path):
+        os.rmdir(path)
