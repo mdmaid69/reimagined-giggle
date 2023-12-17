4e25abaf4e1386145fb92490634166197a8f5142
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:57 +0000

    study waste level marry could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+name = "Python"
+print("Hello,", name)
