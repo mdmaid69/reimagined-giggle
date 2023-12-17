9248a2fec6feb38a819420c4fa7cba21ede2c53b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:49 +0000

    crash distant preparation lookup ahead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_tuple(array):
+        return tuple(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
