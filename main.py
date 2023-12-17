fa75a70c16b57a17883b2e58983f2279371d503e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:13:38 +0000

    form value sensitive water switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
