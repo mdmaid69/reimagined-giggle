ad5e566c7e541990c83be7ef3242587ccaee48fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:26:54 +0000

    pot mistake challenge straight combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def reverse_list(lst):
-        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
