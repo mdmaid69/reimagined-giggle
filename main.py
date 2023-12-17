6afec8d589c09df54b7eeb33b9b12410485bca04
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:30 +0000

    statement popular roll worth on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
