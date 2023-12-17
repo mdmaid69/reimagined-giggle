14df0b3e659eecb7c2ac67b1836478e15ba7ab3c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:50 +0000

    choice another comein nowhere workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
