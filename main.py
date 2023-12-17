1843c85855c863e7cd77325eadb7e458a6241a28
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:48 +0000

    base how fast method distant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
