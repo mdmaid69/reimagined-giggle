aa4a692a2fa91195d5ac3170a9e79bb8783f0839
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:03 +0000

    imagination union spray turnon make

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import tensorflow as tf
+print(tf.__version__)
