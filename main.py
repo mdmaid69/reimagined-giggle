44bcc174ca68479f6658445ffaca9666e9583b6b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:10 +0000

    survive lead cook rich first

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
