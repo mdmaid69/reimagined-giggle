a1009f9decb2f43be8325ef44cfff939508f1fed
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:54 +0000

    dangerous strike main container also

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
