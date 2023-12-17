166fa9a2dfa16136847b61f7784418e5c75e18b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:17:43 +0000

    desperate high sensitive invite otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
