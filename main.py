95f4390857179c8137bb7d85c80af79033a66e55
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:36 +0000

    laugh various time future upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
