7944676a5f38388f02c53ba5fb7ffb41558e6a0c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:08 +0000

    business doctor unfair later comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
