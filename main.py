7678175feb12e5dd57d982fef5dc8019f0bbc76d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:39 +0000

    obvious spring branch habit car

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
