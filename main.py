dc3af2214d1a502eda3100804641af89c4a7bc31
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:49:23 +0000

    night copy back work brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
