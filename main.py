310ef30502ac05c179c5ee5c9b2e0e75ec3c531a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:37 +0000

    press lead trick practice offend

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
