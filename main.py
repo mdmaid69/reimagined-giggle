9f136a0dd2e929f23ead0519b0a08635218ae5f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:11:09 +0000

    except change thinkthrough chat giveup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
