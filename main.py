559fb745f64685ad12fbb46cd325cc9637f2da51
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:28 +0000

    shine hot bank shut imagine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
