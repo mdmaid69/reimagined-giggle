6df01c2f7868033a072f21d93a3976b9dc9802ea
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:34 +0000

    praise undo sit dance personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
