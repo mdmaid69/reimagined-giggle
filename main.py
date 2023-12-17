0aa132108164901faff132759a32042ff482d41f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:43 +0000

    person reply while anywayanyhow illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
