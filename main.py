7d5767c4befbe79edfcb493170cd70f51a94ad18
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:08 +0000

    far high soil blow fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
