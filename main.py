e449d1dc295da23677db01e33006859173916250
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:06 +0000

    exercise cut pair cool raw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+print(os.getcwd())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
