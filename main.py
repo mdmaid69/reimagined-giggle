1bd0d64f1482074b2f566163fbb567194513343a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:00 +0000

    eye under wait future inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
