67bbb1d9bc779891c4d6e04692cf662ff4be18c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:54 +0000

    throughout entrance black habit pair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def reverse_array(array):
-        array.reverse()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
