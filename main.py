ca34f1edf601dfbcf5d9b2eedc035814b1a42d3f
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:48:26 +0000

    they market method meal passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
