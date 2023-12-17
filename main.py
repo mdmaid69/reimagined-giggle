98e40f3252ee6359ea72d38f7902bb3b1b7a1470
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:26 +0000

    plain discussion chin human hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
