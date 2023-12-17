d0a563307dc11173de0cb71f20b68617cf6fea5b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:11 +0000

    charge noone below exaggerate crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 def calculate_pressure(force, area):
         return force / area
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
