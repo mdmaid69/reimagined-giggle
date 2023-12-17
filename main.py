e589c508e91110a9c8959c6331dcf9681bae18bf
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:43:19 +0000

    plastic wing basic private correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
