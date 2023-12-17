34958ac485447ffb9510cbcdea47fcc121969d62
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:15 +0000

    mention petrol should usual crash

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
