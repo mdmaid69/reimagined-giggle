2c2b9e594d49719a77e4d5d95076eb1e77767b51
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:04:49 +0000

    machine tall scream generally respect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+from collections import Counter
+print(Counter("hello world"))
