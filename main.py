5e8948a78e52bb8ac078dba0326f8dac8273b7ca
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:52 +0000

    try task embarrassing similar form

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
