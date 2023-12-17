73d77210ce3269aa9c5087f4ec6a1bb8fdb37a0e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:37 +0000

    print environment Kk defeat beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
