2bd98646ffc6c0361dd13ed042ef21fd4703e17a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:39 +0000

    Kk tear along complete human

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def cube_number(x):
-        return x**3
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
