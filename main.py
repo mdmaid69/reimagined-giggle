2e3b9d62401f4abee1edaa46c407dda81cb84985
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:14 +0000

    path beat listen safety defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
