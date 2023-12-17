c78d2b5d1918d65605030493c02cd22136211e50
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:21:25 +0000

    content about earn close spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import random
+print(random.randint(0, 100))
