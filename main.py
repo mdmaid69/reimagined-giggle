a079f6ea0a43e56d65ff9987f66205cedd7780ea
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:58 +0000

    snow cover check whistle lay

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
