cca972558c47f643d2ca8ef81d94775c834608e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:03:43 +0000

    hate reply certainly praise theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_float(array):
+        return float(array[0])
