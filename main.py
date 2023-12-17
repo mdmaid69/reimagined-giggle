57858471de894b5e72c3ae5e59cf2bed78cd3026
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:28 +0000

    survive creature scared sad change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_elements(lst):
+        return len(lst)
