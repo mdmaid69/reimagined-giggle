2b6acc50c29a7fec70aa80331924d9f9892f8e77
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:31 +0000

    deliberate work dance top payment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
