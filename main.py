32c42b34acaf75009df9381c4d2c0e97c2e4959a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:45 +0000

    minor lead right urgent fit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
