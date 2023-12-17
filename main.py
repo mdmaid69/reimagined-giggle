8e6a228358a9956cedd2811e2bf5e238a34a86d4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:41 +0000

    star appeal takeout black rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
