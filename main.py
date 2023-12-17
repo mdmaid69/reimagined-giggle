18e528838b830ba8e74b5174c28994fa5bb0812e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:53 +0000

    gate splash general cum maybe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
