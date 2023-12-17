2aca111376f0a2ab521664babd15793da4c63169
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:11 +0000

    with pipe godown comeoff disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
