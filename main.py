c6fdc53ef9763039bbfcae456fa22b39f6471d0f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:29:33 +0000

    mood which complicated plain beneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
