e6ad0a308a1480e96aa270d4329776116da76018
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:09 +0000

    firm unable march peace mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sine(x):
+        return math.sin(x)
