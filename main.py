5e8f855562c1f52ad68dbeedc4861977b6f93c8e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:59 +0000

    strike under frame squash claim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def divide_numbers(x, y):
-        return x / y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
