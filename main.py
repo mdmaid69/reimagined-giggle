e184d5854b532761a44cc67095104f503b4565d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:30:06 +0000

    cold ours tour sit noisy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
