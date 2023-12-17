1fd77c37dae36b7adb74952259aa2a4acc969dc1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:20 +0000

    switch talk ill finally ours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
