8b3b025bf3433de0d5b4d81631038e603d3eb758
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:01:15 +0000

    somewhere phone send threaten many

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
