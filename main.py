9e36c647cdc2ad5d65df7bc93ba6090a5996202a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:33 +0000

    steal please between breakout pure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_complex(array):
+        return complex(array[0])
