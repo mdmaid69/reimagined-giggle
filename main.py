7bb2f7f61284f813e8b02c7952c16a6cd6946bdc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:36:58 +0000

    car pass final pity guilty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
