2eaf58f0bb6cdba0c97dd7fdfe654906f3cc6267
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:15 +0000

    strict weigh problem miss everything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
