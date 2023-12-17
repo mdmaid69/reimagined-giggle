750ac8aced8cabc0dbbab2a98891864c54feac01
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:35 +0000

    more quality still guilty catch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_exponential(x):
+        return math.exp(x)
