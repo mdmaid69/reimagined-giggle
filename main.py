4cc1340053812e92e526a19bb24b3c9307c1cdd8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:35 +0000

    somebody visit professional minor sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
