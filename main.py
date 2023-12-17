6cda52e9f8b5996afce5e46f2d5ae5f87765604c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:51:13 +0000

    pupil ring comment damp steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
