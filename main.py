a13782adf832bfd6c9cc72408fa45e8090e5668c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:16 +0000

    keep Pp record slowly square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def divide_numbers(x, y):
+        return x / y
