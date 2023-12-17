03904240ab22fabc418b86061bdce88cd0e9be1f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:28 +0000

    passenger rule improve error sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
