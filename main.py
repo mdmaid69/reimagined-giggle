e89a287155338279281c25ba73d85fee725af927
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:38 +0000

    release evening manner contain gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
