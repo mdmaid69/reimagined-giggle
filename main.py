8802c38f91a6da9234cdb581e4d06b48dd56c236
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:47:00 +0000

    wrap develop fresh knowledge shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
