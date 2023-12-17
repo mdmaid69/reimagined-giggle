680ecb8e0d8a0e584688e857244f58503ac704a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:46 +0000

    pretty welcome movement part finish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
