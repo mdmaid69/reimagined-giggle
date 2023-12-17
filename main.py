9318937c71fe0ec5b420922abd080c01e2e985be
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:29:07 +0000

    rid brush passage Rr negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
