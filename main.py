4b97ca0315ce55b2129a9c1815684c3810d86884
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:49 +0000

    car sweet deaf across still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
