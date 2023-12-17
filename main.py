6fcb3e098d7a9377067e47c00678d379d5dbbcdd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:03 +0000

    interesting funny inform brush communication

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
