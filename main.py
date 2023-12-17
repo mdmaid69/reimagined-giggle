e0b3f1f0706335a1ccbbc39fb4fc29e107ab3ee6
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:08:24 +0000

    promise mind share interest spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
