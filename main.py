76e998b04f6812cf934c20798485aa22ff9b74ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:49 +0000

    lookafter godown yours risk free

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
