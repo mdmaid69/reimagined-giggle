e18732bf410b224ae1e11dabd5d0d336797acef9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:24 +0000

    battery question march weak purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
