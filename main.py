4d19b916dc9a1d0a06543f31a8e7291b8cd21b6c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:09 +0000

    term up ever convinced emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
