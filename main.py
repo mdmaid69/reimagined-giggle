171f371a9627d1a90ce42197cb1cc5ec861137c7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:19:23 +0000

    physical ask fight fall raw

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
