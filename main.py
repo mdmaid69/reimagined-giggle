e08020049aed11548d5476f30bc04caa735d19ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:19 +0000

    steep muscle them disappointed experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
