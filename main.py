c5875fcdc957ed62724dd9a745c2606f59f49f6d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:44 +0000

    send combine dig whenever something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_typecode(array):
+        return array.typecode
