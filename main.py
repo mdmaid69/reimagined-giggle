aa7dd081041805cca0a71f455875c322044b9c99
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:43 +0000

    pause sweet Tt advice fit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
