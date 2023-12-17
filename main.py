d72ac3a28f0d4144f64706fd3720c649f3b41c14
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:45 +0000

    flat turninto bus discuss positive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
