d5b34bb930ec9aee9b489fa0076b2a29feb9d5e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:34 +0000

    prove know impossible crowd miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
