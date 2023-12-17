16b97922414367830a6a5209e95fc72d87f50c8e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:06 +0000

    excited chat alive separate drink

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def reverse_string(s):
+        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
