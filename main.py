7621de804356a50b71f2755b695e821528b122dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:13 +0000

    birthday else start like state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def cube_number(x):
+        return x**3
