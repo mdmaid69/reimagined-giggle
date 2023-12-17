677eac20312ed3a8c7e1dc823609293b0efb98eb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:48 +0000

    whoever giveup exciting argument accept

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
