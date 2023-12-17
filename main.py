a1d0a7bb3c90cc2cf0270b26eac32654196cccf5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:23 +0000

    into goon live decision estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
