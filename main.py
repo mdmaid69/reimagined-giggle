3bc1a2753629c543ea125996e796ea4262e63b50
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:28 +0000

    pair mood possible lift argue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
