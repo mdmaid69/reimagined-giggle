dd6ecfcfc12d5a55adf802e4444ba8fb299e5d27
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:15 +0000

    attention sink love past grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def reverse_string(s):
+        return s[::-1]
