6280a4c1a8eea0754770a7615abdd2e48f80adc1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:28 +0000

    less ready hardly light upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
