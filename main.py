45fd0bab34606b7cada7552438e700f812ce7a57
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:31:35 +0000

    evening before attitude not grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import sys
 def print_python_version():
-        print(sys.version)
+        return sys.version
