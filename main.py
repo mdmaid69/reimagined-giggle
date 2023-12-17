b28ce1cb9eafd79674a0b4c57f44c230431006e4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:34:06 +0000

    effort body uncertain object empty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
