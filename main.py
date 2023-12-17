cbb76c4b0d223886c965c196acd0720b4b7a1452
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:05 +0000

    file could independent wing within

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
