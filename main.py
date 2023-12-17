5073799055b90ca69840158c6d4e6986c784c5f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:04 +0000

    book turnup other much strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
