d556ff2e267c3f393493686e51e3e9e572825754
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:39 +0000

    think suggestion average one moment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_pressure(force, area):
-        return force / area
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def multiply_numbers(x, y):
+        return x * y
