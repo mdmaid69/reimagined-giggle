3292caf039faea42e5c0c42c5cfc21399b6671f2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:11 +0000

    young error stretch interview plastic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
