dc6e584f9b01ae87ec6e7381b15d250e9462e094
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:29 +0000

    instant switch swallow damage listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
