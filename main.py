2f8a47824b2cae0c445d9b16f2afc8fdfb9b049d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:34 +0000

    excuse develop worse central juice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
