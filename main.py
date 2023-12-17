879bbb10b2e8492c5c1732dce4acb2b4da2d409c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:27:52 +0000

    spend space sort disturbing brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
