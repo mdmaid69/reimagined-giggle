7922ff03e4ffe6e231dbbbb1d70a45449fa0be68
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:01 +0000

    ago separate cross culture funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def count_words(sentence):
+        return len(sentence.split())
