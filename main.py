8b93c452b9c1124c036dc5a831a9bd1e7d4c3520
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:24:29 +0000

    complicated after shoulder contact find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
