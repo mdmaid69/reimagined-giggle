5e1cc420ba844035eb77ec8caad8db51cd25c4f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:42 +0000

    guess dry smell opportunity need

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
