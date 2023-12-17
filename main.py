db2ecb7b40a2d27c314fb5a2ec220968f82cb56f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:01 +0000

    regularly stick generous manner post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
