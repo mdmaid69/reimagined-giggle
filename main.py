e526a3dbc5f9b942a0a2ffe18490483789e90d08
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:37 +0000

    weigh breath declare workout discuss

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
