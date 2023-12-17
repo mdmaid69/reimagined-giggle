29b6a0880659ba3507837f8cc9b5982927429e34
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:35 +0000

    anxious house name cause pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
