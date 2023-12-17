fac933e21dafc3e130c4fe52636ee65c8c91453b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:43:21 +0000

    pinch nurse stop genuine cough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
