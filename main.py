f8e647aa870ad43132cc5a493d4d5df1cec5b48f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:32 +0000

    ring minute card check name

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
