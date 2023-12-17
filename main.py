f7ca84dfa0b7e29db4853d2e39cb3f597c79e9d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:32 +0000

    worried bend forget open band

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import time
+print(time.time())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
