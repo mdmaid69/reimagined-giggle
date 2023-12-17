a92cba5d66a8297c4ee8153ae0a3b8e9244540d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:10 +0000

    but mad bend concentration sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
