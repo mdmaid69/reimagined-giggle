9dbb0cb1d9442ddcd1c3943dd426a58311b2a66d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:54 +0000

    takeover consult nerve substance large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
