22e366fa79739ac1258f645baef4affda5f04636
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:51:54 +0000

    mouth there press before characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
