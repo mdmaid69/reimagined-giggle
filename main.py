d592f355e99e8057cc0de7fc1f92f122cd5f3460
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:34 +0000

    fine together jump strength trial

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_max(numbers):
+        return max(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
