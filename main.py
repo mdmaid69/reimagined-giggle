97c1af2baf79e265107611571e53b1e16c0528a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:31 +0000

    description comeoff shock gate begin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
