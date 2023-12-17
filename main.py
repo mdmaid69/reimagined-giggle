d2da481cd4e701af2dd3826cec107a35f0e989f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:07 +0000

    cover might across branch properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
