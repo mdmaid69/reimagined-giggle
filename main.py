12656b919ad17ed407be6368ea144df9e5fad8c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:08 +0000

    club model key available interested

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
