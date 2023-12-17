a6010402382879b99cf1272edc7f1ced2546ee58
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:58:19 +0000

    love deserve unlucky respect minority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
