19fa02ed3b0825765ee504ca68f9fa375afeb380
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:33 +0000

    dinner defend arrangement right sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
