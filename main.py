804f57bf3f778f9aff3289d1e23f5e9489776088
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:40 +0000

    home shame clothes breakout bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
