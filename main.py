fbe459e0c0531d8bb1e5f315cc14281f5e1a8fa1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:15 +0000

    moon why occur right law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
