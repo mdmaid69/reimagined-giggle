473f141228a0441813a0252b1c3d0845ad7a1fa4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:00 +0000

    man number final exit round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
