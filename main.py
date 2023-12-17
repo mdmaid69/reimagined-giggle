75386d0a292a6cee692b47e270aa3d08aa581564
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:06 +0000

    theirs sale Tt exact like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
