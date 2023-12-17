c05a648cd8c84198d8377ccb485582409c651b42
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:32 +0000

    to their care precise lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
