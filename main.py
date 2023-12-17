f66b3a8ecff22603b8d35a7532e2f4e897bf290e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:05:13 +0000

    instrument uncertain manner finish walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def reverse_string(s):
-        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
