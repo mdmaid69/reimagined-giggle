1e987c53b0b3c82c408527c7c842a9fb19a2ac53
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:39:09 +0000

    by apply store blue ago

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
