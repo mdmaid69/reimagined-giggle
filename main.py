7370cd21fb89ad5bc81498106507df1d1f4d4a9c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:15:25 +0000

    group travel plenty deal manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
