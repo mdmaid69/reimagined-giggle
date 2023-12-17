5d0f39d1d16c21ed8fe84b2f5f032a4f636880ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:06 +0000

    dream behaviour inside cheque wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
