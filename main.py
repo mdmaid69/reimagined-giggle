aa20492448814b3854d54bad9c76f328f2d8feda
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:40:40 +0000

    watch find borrow able Ff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def remove_duplicates(lst):
-        return list(set(lst))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
