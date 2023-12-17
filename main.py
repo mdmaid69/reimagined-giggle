6a17a109d0c8ae77b2acb53334357bca014b458e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:51 +0000

    make fashion sweep declaration exchange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
