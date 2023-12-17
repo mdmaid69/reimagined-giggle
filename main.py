49e868ba17a0f27a49b83bea0b14f3cf67ab7f5e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:15 +0000

    check gap bring sale tend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+print(os.getcwd())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
