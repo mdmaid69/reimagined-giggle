d4135f4100a7626a5683b70d6daa89897e6e88ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:48 +0000

    lookafter pencil unit comeoff comeout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
