472afd4be31e36af3e157f947d9c62ffc4d07155
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:15 +0000

    excuse issue where back probably

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
