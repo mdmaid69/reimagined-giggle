526a2cc5ef5d8497541adb46479867c162c65c63
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:54 +0000

    risk copy persuade exam pure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import sys
+def print_python_version():
+        return sys.version
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
