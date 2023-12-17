3a3dd868ce659fd1cb4299fe102eee8af62bf0f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:40 +0000

    attract interested certainly complete stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_rectangle(l, w):
+        return l * w
