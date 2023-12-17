dc57e9d3dd63a1c896407673ef1a120c087d0cba
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:11 +0000

    freeze perform choose Cc instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import random
-print(random.randint(0, 100))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
