f720cc7b277662a8b8d065d1a88f89537b445f24
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:43 +0000

    that only invitation lookup drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def count_elements(lst):
+        return len(lst)
