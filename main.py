98fb189b27ce4ad07b202afbe26e2daf12453341
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:51 +0000

    disturb authority tear clear want

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
