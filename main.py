a63cc20a550ba5ee34705bb78f068bf184a1017a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:01 +0000

    programme level feel argument taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+print("Hello, world!")
