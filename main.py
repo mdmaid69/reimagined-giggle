e39f32887172357d0b0f1fe8b9acec754d7555dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:45 +0000

    means fasten mail near getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sys
-  def get_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
