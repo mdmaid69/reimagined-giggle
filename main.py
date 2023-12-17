f0b1fe62035608ff35c99608b2c6f60e30a34c14
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:56 +0000

    price practice yes bag switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
