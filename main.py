fe549b21973ac544c7fcd69e5fc616dbc0b2dbad
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:37 +0000

    holdout Yy goup visit distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
