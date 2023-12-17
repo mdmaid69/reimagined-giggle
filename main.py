87d60783f5a7764357232945240c4cc44eda8666
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:13 +0000

    fat soft bye properly official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
