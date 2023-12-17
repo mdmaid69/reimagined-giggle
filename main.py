735ebc8fe7c59e181407c53609aadc0849d41b13
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:09 +0000

    quarter sister impressive cover scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
