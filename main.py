2cf42489d4fbe2fbf5f8f20fbcb620653ddaa679
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:10 +0000

    cruelty hurt trip slight poor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
