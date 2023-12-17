6c67f19c564f9c10f3a1004eb5cc3ce5e93e5c78
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:56 +0000

    straight dream convince computer clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
