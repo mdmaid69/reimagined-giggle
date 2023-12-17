3e6dd805f4a362b3f3d20da4c6aa3426b8321731
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:44 +0000

    famous heavy fill minute right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
