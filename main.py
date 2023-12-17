596fd7d54f9eae42c5782d546be906b882391875
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:14 +0000

    effort performance contain family insist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
