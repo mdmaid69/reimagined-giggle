a3623d562d902665b5203230af9373ccd053da4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:32 +0000

    noise move future goaway chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
