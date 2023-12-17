9c3025f0097fe641c2450c3ec6315fc782b9ade9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:10 +0000

    allow suffer bottom any arrangement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
