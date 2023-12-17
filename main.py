0cee20dadaf9447ede9c1c9837b9f53a80e76f35
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:49 +0000

    expect mean damage wake system

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_max(numbers):
+        return max(numbers)
