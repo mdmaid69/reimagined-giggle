dd27439c5480c11721218d2eb18e957cbb4fcfa1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:23 +0000

    second stamp usual OKorokay brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
