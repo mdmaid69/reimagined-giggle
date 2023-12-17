dd517f8dae6f604f5d08c3eb316d1fbd45db41bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:55 +0000

    tear friend duty string steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
