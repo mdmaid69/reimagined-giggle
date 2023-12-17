17f7c04889ac8ce6f02eb2bb2aa3f1a0de69da4f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:31 +0000

    hurt dream compare city wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
