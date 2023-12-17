5c19d029e3d0d6a95967455607eaf1787d7d2476
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:41 +0000

    tidy sort Uu grand evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
