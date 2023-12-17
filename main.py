2f057f542278397425768d56b8f57a94c76cbbe6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:59:23 +0000

    blind mystery main thing coat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
