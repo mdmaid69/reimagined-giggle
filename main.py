ce384deff319c02e5d1405f32d5b8470bd67d6e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:00 +0000

    authority neck fall sweet play

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
