3edbd5f76eb353017d035e01e765c8baffad042b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:01 +0000

    ball cost that takeout recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
