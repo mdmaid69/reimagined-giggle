0e8b994426d43f34cb3ad2fb41e1c65d869928bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:43 +0000

    escape drawing collect outside week

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
