a5f19aa5da42f4ba70796c910bb3b8ab4d84301e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:11 +0000

    unkind walk friend convinced develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
