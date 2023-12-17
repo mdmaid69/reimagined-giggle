fe87f1b05bfbfadcb7e868f5fa3d3831914fde09
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:57 +0000

    appeal win method gap disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def square_number(x):
+        return x**2
