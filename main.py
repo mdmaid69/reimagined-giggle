f8ee80ec6894f36d7cd955565f5e30b5ce26a170
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:14 +0000

    cry back boat entry comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
