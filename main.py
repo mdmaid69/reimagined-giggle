057acd603571af5472e2db1beeb5e02f7bc329eb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:51 +0000

    road perform prepare team unite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
