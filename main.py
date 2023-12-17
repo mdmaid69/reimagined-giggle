985a7eff4eebb5cc7d41d40c5b7d345fe809bee8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:40 +0000

    completely influence pocket but open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
