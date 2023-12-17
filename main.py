bdd484b46b153796d15a699b998415e57aad8246
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:16 +0000

    young some target deal relax

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def find_min(lst):
-        return min(lst)
+def is_even(n):
+        return n % 2 == 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
