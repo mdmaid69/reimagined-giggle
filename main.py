89d382f5bd1e60bee9f0ee68f10a7db49860c262
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:10:22 +0000

    nurse always share square light

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def convert_to_octal(n):
+        return oct(n)
