fedaaf84ced96f6de21a84d6c1e103c757edeba1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:49:56 +0000

    properly visit close smile importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
