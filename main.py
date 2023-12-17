e07c4baf6e4512e8e9e50575d4917c87ebf66510
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:38:09 +0000

    use phone long play ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_pressure(force, area):
+        return force / area
