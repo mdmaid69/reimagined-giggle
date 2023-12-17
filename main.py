1115350e1dcfb003d0fb1ca202397e88cbcf87be
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:49 +0000

    journey quiet usual alive exploration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_int(array):
+        return int(array[0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
