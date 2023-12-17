8104fc9bb8f127e8f04e06a252ce6ff27abf4ab1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:46 +0000

    embarrassing sell nigga break practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
