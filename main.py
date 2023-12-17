4917c7939e643272e09357102f0007ea55767933
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:11 +0000

    many concern say stranger trick

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_pressure(force, area):
+        return force / area
