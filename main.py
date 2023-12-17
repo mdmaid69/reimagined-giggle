bc029db634927f99021000641710cd896df4bd01
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:32 +0000

    principle bound comment wheel undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
