29710f551d3a4b6e346e5818101f1b7add3a28fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:21 +0000

    piece question demonstrate print lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
