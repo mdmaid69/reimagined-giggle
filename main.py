3cb22b49855690d29b916b2705749e3152b9924b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:15 +0000

    claim stamp emotion accidentally guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
