8dcf4e68be2ab2e0b3acb914ee6ccc24d2625788
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:13 +0000

    Nn recover permission trust destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
