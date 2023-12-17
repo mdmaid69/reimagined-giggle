4e2b5c5eca34e47760778dcad0cf9fc9533b3daa
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:44:41 +0000

    through colour goaway correction fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
 def calculate_cone_volume(radius, height):
         return 1/3 * math.pi * radius**2 * height
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
