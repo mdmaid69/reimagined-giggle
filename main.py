e93509ee63dc88d4e660fecab022aad420c2fd73
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:59 +0000

    discuss trust hear common towards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_power(work, time):
+        return work / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
