ac306c7a2ee597265b237bacdf2f483dbf4d8402
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:45:20 +0000

    conclusion total wise prize report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
