a7cade835424a732eb96bbab4642731f3c6f1bee
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:18 +0000

    communicate relative concentration pause tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
