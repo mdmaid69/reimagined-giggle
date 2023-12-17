61a2bdef6fde980ad2ddebd3ccc6e02e22c4bc85
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:53:07 +0000

    cry range exact declaration tip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
