902f1551049dc72503b808077a5ea9b36050c79a
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:49:26 +0000

    attention play pretend plan food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
