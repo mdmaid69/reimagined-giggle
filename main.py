559ad84931fe2fb4ee350f05852be8cc3c505a00
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:40 +0000

    comein fold how manage path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
