a725025ca27fe0b877fd2e67e9e4c1f241f57e0e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:26 +0000

    quality take love delicate direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
