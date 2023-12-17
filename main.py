87bf04d07a54b6b7335d93310796a6aae800b7fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:03 +0000

    disturbing level performance play accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import time
-print(time.time())
+  def square_number(x):
+        return x**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
