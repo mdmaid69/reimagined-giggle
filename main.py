36df7d3362979f9b84871c4ff52c60ee088c1757
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:44 +0000

    peace godown swing refuse transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
