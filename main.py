80439c9b61ec182d6eff26c9bb7c20f491ed34a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:25:22 +0000

    takeover connect please forgive claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
