c3feaa038607c24ff8e8638a8d58530d256ff35e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:51 +0000

    rain mass print turninto live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_exponential(x):
+        return math.exp(x)
