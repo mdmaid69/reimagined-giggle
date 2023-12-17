49c711c2e0eea92f1f55613d86b79c647cd99bcd
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    town past call at involve method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
+        return sorted(lst)
