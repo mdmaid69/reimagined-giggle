5c82bfb1820afef80aa319c8d3222fd87221f380
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:47 +0000

    plant keepup gather escape rush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
