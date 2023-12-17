dd0bbd80a4d384e5174637d48a6d1a4d5c63fb7e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:40 +0000

    group low lay bill only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
