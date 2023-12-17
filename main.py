840866b3f12848c69fc9199751c021998a5807ca
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:41 +0000

    switch no visit scale classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
