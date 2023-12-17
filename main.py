2edc6ed0a33c662b8d19f4bed524e1f7004b99ad
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:08 +0000

    lay without science fence concerned

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
