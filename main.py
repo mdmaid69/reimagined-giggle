1464f0ba90899a595ba97c7b8c57462ec87c9bbe
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:31 +0000

    drink congratulate lunch confuse hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
