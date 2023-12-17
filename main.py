7813a409fb15d3c6bf64bdc273ee684bc8152113
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:38 +0000

    hurt fit floor ago reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
