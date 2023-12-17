4a02bc81cfad505142035582c104cb30b57eed67
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:23 +0000

    raw influence serve supply with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
