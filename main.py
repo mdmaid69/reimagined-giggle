e21a6cb0f61c2909d84754ba5a69e7d3abf40514
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:22 +0000

    all offer fail go deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_as_list(array):
+        return list(array)
