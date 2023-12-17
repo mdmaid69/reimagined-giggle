5d5aa7dc942c8385d719b0a6e64e9ba24b6d3995
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:59 +0000

    freeze car grand load instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
