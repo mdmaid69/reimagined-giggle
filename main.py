4ef991ef77e8811425e1090d2562b4435145cc78
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:34:57 +0000

    obvious exist ourselves show journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
