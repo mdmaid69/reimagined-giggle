ba9186a7e44e935f6b7145be5691468680f4ec56
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:00 +0000

    afterwards burst expect nice course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_int(array):
+        return int(array[0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
