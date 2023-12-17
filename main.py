379fec9d95d28d47263ece2855f2a2cfa25c271f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:10 +0000

    repeat pretty heavy brown better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
