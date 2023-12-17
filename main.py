bdee92dc7e0fdd605d271df0500b45e721bc0b26
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:44 +0000

    charge position slow public rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
