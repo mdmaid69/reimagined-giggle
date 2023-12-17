59274e7155229045f78c114bf5b406a6ccf8cfee
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:54 +0000

    match manner hungry stir prefer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
