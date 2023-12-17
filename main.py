23e13b648179d6a8b077bf898079722a00f885f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:16 +0000

    too serious shout talk part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
