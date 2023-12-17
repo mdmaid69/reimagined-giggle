0c50fdb2b931471dbdcea0d3cf4655c5ffd0c302
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:53 +0000

    comefrom same noise shout read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
