5a6ebcd4503f69ec678076345940969a7894a25f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:51 +0000

    head smile position under post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
