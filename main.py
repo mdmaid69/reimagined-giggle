19beef7afa942f50fcd8dcb4cc48e3ea5fa087b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:32 +0000

    middle phone callat bit neat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
