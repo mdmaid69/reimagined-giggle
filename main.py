345f8a0d339f77d8a36356239ed9000403bdcff4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:52 +0000

    uniform explain knowledge here swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
