2f5ebf4744201a445cd566990398e7aa10569885
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:35 +0000

    profit be form matter accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
