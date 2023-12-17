977321d3abe08b292a846908a03d82fd47be4027
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:06:22 +0000

    unkind dish rub question influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
