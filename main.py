230b47599019217f2e161a5f2cb3dae86ef4da09
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:18 +0000

    pride hurry shoulder broad touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
