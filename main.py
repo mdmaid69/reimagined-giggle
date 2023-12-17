f3d6df6d63c5a4cd3da045c7700707ff84722694
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:51:36 +0000

    step regret nurse beautiful afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def create_user_string():
-        return collections.UserString()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
