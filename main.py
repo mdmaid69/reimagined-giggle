b1955d12a6eedb0d4d5931e80021f57283d119b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:36 +0000

    scared measure and noone possession

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+print(sum(range(10)))
