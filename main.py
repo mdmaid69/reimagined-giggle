3145764698c3c9284c179e6554f0fd2551c87136
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:49 +0000

    quit sometimes build trick loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
