cb0bbd26e881c7ddcbacf0a70d36e23c971bfaf2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:31 +0000

    clever rescue concentrate heavy flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
