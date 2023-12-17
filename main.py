36e7c6aa2331c67b5ef5645f23d20384d939cfd3
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:02 +0000

    arrange every round meal cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
