8f7c990917d893f54ffd8219d8f9c74a1557aff6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:50 +0000

    something 572enter to careful use

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-def add_numbers(x, y):
-        return x + y
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
