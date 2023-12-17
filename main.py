9687d6f84aeb14ab9462fa4a286dab68b6d04092
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:28:36 +0000

    arrive material capital serious undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
