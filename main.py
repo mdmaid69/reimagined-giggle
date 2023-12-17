d13a9f8a54e1bda1d7c2cca972388bb0d5dc4d2a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:07 +0000

    most dish hungry future keepoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
