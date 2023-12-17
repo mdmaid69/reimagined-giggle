b776ef650124cfdbd46bc614bee2566abc68a35f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:10:21 +0000

    discussion mention trust prevent exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
