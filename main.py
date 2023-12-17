dc69b73696f22e789b919728c5a05633c5aff331
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:16 +0000

    pass name importance what force

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_power(work, time):
+        return work / time
