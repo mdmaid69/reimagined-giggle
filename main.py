b26ca69d3ff10b9d2f2c70e0bdd9f7bf00dd66ea
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:16 +0000

    Ll admire case Pp set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
