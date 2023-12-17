8be005071ceeaf5047e94abc91a079d7cd5a0515
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:48 +0000

    burn rare file find afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
