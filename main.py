ee89648ea370e560126cf4d202d0f0a1c95532e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:52 +0000

    sister narrow furniture getoff balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
