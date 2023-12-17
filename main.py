cefacfa487cb4f200ff2fde3b9a243fdce6c33af
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:40:53 +0000

    contrast seem sense one punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
