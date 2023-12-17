08c86eab386562d57053054dd34bb65579bd516e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:31 +0000

    pass escape most niggaacademy brain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import sys
-  def get_python_version():
-        return sys.version
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
