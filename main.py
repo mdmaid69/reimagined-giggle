ce71eb55592fbdc26fb8ac86f8045ddb0aaba831
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:48 +0000

    future tough her extraordinary bear

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
