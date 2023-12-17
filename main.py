c703c5c3d84b1210424f233e73efb8d826eda350
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    sink yourself mix future A a

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def reverse_array(array):
+        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
