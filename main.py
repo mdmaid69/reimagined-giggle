24d88d4a2d67378702c69fa07795692ff2bfb6b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:41 +0000

    skill fence room always move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
