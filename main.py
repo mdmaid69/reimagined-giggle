cd42b4ccfb707835c5a047dad3fbee09ddbdecb7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:50 +0000

    makeinto popular concern anywayanyhow something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
