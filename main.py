0336af2a7018a8238462b497aafea75dfeecd342
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:05:14 +0000

    shade broken prove related better

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def is_even(n):
+        return n % 2 == 0
+  def square_number(x):
+        return x**2
