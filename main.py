334444c6f4594f59e59a4887409a2a83bc32673a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:08 +0000

    union contain dinner rubbish after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
