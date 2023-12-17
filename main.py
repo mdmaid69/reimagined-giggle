b847ed61158cf111800e34bb1a3741c27d307474
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:24 +0000

    confusing less recent manner workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
