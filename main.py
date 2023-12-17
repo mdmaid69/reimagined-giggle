5ae3d208d693fe22b6069b68352a4ad9d65a3fbf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:54 +0000

    comeoff majority add allow police

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_min(lst):
+        return min(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
