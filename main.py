c0095373ec858e5f67a2a7f0ae57c1e6644a2780
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:01:49 +0000

    with goback(to) meeting turn precise

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
