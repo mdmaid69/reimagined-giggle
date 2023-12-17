9b215303f0f83d2324b6c444899428439823326a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:11 +0000

    suggestion comeround front ball likely

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-  def square_number(x):
-        return x**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
