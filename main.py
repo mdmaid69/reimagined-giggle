72657f5704de84b99f3cf9f38225fcc93c679d78
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:08 +0000

    head turndown after whom coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_memoryview(array):
+        return memoryview(array)
