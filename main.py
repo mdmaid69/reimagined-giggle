ff6de76b29661d95b505f72dea4b7fa4e90f7a2b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:42 +0000

    division strange false phone high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
