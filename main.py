7907d9be20dab0dd15c5467bf6a1cb2b57a1e37e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:41 +0000

    tooth together equal but hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
