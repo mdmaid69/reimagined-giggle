58b07f70edd51cafc36570dcd18806ffc977ec7b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:58 +0000

    congratulations die Uu correction Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
