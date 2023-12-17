3c0ea832c1ca2f93200e5b8cf93af03bb7046b89
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:51:22 +0000

    walk related address thirsty suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
