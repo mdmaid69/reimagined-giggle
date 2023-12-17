2410455d8f3e7aaaacf407cd9a5eda4bf014c257
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:29 +0000

    pair fence computer generally pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
