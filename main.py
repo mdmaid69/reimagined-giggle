39c71b44651a5e751d57742ef8f5e6e3ef1120e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:38 +0000

    attempt negative embarrassing difficult swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
