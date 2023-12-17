22fe9f4aa9c2c74364a60397fe8cac6da38d3166
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:28 +0000

    wander answer wide arrangement interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
