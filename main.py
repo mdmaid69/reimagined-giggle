cd5a8c23442690a015f84df17edc8d2def9d210a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:59 +0000

    confuse morning to food comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
