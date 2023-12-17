0db27c943d72264c368695211271966c47dbaba9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:35 +0000

    cheque memory top change to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
