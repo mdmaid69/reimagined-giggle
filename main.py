ff833debe550a49cb38cdf42a798433bb4d98a00
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:25 +0000

    behind figure future mend condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
