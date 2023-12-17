995ba866b43ee0f54aa3305969fffb15c73f53a5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:35 +0000

    apart mix harm rate excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
