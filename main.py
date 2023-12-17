091540967107fae3440a4a65433bc31275c1d72b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:18:23 +0000

    rise rush if family declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def delete_file(file_name):
+        os.remove(file_name)
