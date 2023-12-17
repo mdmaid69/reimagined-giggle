c2695370cce737d11344381f5b59d402f1efe0c2
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:41:40 +0000

    definite 568. energy fortunate chat at

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_rectangle(l, w):
+        return l * w
