9340fad46565def9334a1e6fc1f40ec1fc576a7f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:14:05 +0000

    whisper check approve bus neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
