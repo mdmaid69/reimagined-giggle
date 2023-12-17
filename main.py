10cabb7e0219e12eca45e0ba57973129f2320b23
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:51:21 +0000

    569enjoy opinion add sight whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
