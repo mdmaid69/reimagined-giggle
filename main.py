7ec22fcf82fc368f6502da95a10ebd51f16e2292
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:27 +0000

    your both handle keepaway sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
