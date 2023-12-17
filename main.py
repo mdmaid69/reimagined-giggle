f7551453902d565d04cffe051a14ccc974361319
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:57 +0000

    market public square stupid division

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+name = "Python"
+print("Hello,", name)
+import sys
+print(sys.version)
