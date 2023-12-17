a218cb13e94f163878ab06fde398e3acf4be1edc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:35 +0000

    completely forgive official beyond steep

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
