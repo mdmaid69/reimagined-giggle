03f7df34b03fddee3b9ffd07e543cebbb22d4df0
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:34:28 +0000

    work up to sure gas long train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item(array, i):
-        return array[i]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
