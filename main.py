785137fa6948c14c0bf5252bb970f48261635d96
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:25 +0000

    near comeout disapprove use only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
