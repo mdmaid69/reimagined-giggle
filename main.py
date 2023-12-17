00d9ea0e5d7c0c4efb37de60add54884e20bdfba
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:01 +0000

    complicated yesterday major argue machine

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
