4df09f7c9b9c51c22f39a5ea05e2b749f89d4221
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:29:44 +0000

    count home lay odd include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def is_odd(n):
+        return n % 2 != 0
