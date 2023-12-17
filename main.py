e3ce390a983bb45a45493606ccc8b783714e7593
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:30 +0000

    personal worst callout Uu direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def subtract_numbers(x, y):
+        return x - y
