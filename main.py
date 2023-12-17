f7ee182680625f06988a41ea9b7ba03906130a0b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:00 +0000

    dry write form touch necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def reverse_list(lst):
-        return lst[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
