9393bfbc05f12e14a5a7a41d4472bad33b76bab6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:13 +0000

    except amount dry uncomfortable care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
