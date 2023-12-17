85ae0d1df50b6c37881cdb8d0542636f3da0657e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:16 +0000

    marry out point answer tape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
