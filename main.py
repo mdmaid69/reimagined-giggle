9c9427943d498fcfffa7dcef35c9046d6fa79122
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:04 +0000

    willing minute fierce slightly advice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+print(time.time())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
