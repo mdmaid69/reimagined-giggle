8c65025ffc117ead9ec996b775c15bed2aa25912
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:58 +0000

    later free neck defeat calmly

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
