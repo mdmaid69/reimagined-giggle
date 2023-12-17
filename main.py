691cacd448f8d60a888343a3423412a6a412252c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:36 +0000

    apparently repeat difficult sort raise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def is_even(n):
-        return n % 2 == 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
