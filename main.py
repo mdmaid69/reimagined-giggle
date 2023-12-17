fec69d0f7cd546c110571e203f96c496d9ef479a
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:03:12 +0000

    find violent spread cheat violent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def subtract_numbers(x, y):
-        return x - y
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
