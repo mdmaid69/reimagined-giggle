11a4e39ae5757d28f8acd89fade95f89115b0c3f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:00 +0000

    about trade believe rare belong

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
