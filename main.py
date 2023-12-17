552ec46e277515bb2f2ad0ec0a11b6038e863b0b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:11 +0000

    pen together front blame recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
