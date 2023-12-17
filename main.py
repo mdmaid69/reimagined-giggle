b4aa744f1c2ce1c029518746ff71bfa36f3523ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:09 +0000

    season dream comein should container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
 def calculate_modulus(x, y):
         return math.fmod(x, y)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
