3d8dcca2716a233b883228a8c8616ccb731ebb07
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:38 +0000

    before watch sound previous only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_density(mass, volume):
+        return mass / volume
