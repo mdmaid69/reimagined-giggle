3fadc6a646903754096bd650c44019b5238e76a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:16 +0000

    argue wheel improve uncomfortable mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
