2a96d6f89cdfc946db4a11f645b34e3746358791
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:52 +0000

    business claim study sentence nurse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
