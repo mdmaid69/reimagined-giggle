8c39753ba86c6d840fe0ce2fa45bbea1463c9e8b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:33 +0000

    condition field disagree than lead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
