4164aca0ba5a7c3180075ed9fb3b89ba6d400d53
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:46 +0000

    slowly criticize comeoff tend new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
