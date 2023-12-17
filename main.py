5059266c4387c500400255cf7514aec11d770bd7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:25 +0000

    book getback speed wind objection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
