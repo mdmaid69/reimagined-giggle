66c1f94694d49980bdcedf5ed7b1e54f654d0b21
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:01 +0000

    check trap meet through block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
