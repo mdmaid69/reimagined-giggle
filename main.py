160b113ce191aa329fb056912c42aae0939c3b75
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:06:06 +0000

    skin plain happy shake callat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_itemsize(array):
+        return array.itemsize
