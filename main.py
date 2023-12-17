3e2759fe35985aafe2010bb1c9ce8c6f787c3a82
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:18 +0000

    set weigh of injury serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
