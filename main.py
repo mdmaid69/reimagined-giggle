02b0d7d627c1cdb1a01aa3db8650e596214af373
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:10:27 +0000

    behind press boring big ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
