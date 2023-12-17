3f64ae41890b4418cef674d4b6546affab2490ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:36:14 +0000

    ourselves bury reach love fork

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
