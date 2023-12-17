cb021e6f4277e422068236a17e06f96e93087253
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:04:51 +0000

    rubbish side careless movement better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
