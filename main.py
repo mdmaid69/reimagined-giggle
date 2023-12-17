4a37a3f265e491925fd86945f3f530507e581d5a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:13 +0000

    close touch supply opportunity comeup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
