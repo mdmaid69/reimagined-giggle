5e9b49a30bc35604fd3094e9eeadb2204852d490
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:19 +0000

    opposite sort business anxious dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
