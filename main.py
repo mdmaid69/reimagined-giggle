0613e9ea0a5d065c10019e5eb12b3a255f6d2c15
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:46 +0000

    remember high combine sensible play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+print(os.getcwd())
