87d44ee2504b8d169790ec5b442ca3e02c6ade82
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:55 +0000

    sale contents agreement able slip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
