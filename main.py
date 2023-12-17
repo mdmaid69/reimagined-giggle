523d20d40c0033c2335719efe1de5a069cea2b90
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:28 +0000

    ready 570enjoyable comeup home study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
