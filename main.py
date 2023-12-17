3475b842b975abc4c793c0c4ea08c674341b197e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:16 +0000

    flat undo behind beyond quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
