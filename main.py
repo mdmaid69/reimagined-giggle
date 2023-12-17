eb04fcdeb431f57bc94821346d88ab6c7a2486f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:16 +0000

    exploration taste dark rub beat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def clear_array(array):
+        array *= 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
