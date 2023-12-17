9e42617d99a665bdc1c24df6fbd4b6176a6b574c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:41:41 +0000

    sense suit protect similar discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
