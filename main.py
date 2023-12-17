2c1e974136d861370e6357d86612d232ee322553
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:23 +0000

    boil inform comment shut book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def reverse_list(lst):
+        return lst[::-1]
