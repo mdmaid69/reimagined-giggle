f5e1d1caef6a287bfa1dc97c448acc31f0496639
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:12 +0000

    involve be relationship niggaacademy factory

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def cube_number(x):
-        return x**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
