8db4cb829f6ba58d333235a4c8b9cac1eb2778a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:50 +0000

    give sticky experiment even their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
