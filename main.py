cea327b1538bdca1516586fbe8c6161abac1503f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:57:45 +0000

    unkind necessary Cc degree Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
