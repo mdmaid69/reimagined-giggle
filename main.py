29cedbdffefdd101c33fbcc5980ed64f400c1f9c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:15 +0000

    hardly man what confusing average

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
