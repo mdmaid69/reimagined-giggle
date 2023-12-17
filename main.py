4fa5619877aa669a4ce348dd24b4ccf6f015015f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:42 +0000

    sing tune 567enemy peaceful evening

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
