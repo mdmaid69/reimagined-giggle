196d74e3b2ee7e057b2712f1a0c93bc656790349
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:29 +0000

    branch sound match if soak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
