13d426cea28bd1104e23458d24664b72cc4d5191
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:06 +0000

    everywhere estimate cheap less choice

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
