c6b2a45b50f401ae09a8a3d8f599d8f451869ab2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:35:36 +0000

    think tough understand they ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
