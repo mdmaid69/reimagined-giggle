2d9610ef88cd5c10031251c8b17520e7edf880f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:23 +0000

    moon 569enjoy event reduction pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
