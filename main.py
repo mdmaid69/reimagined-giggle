78dd3449196e2c525a8e682d44304b96da4686f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:42 +0000

    usual apologizeapologise intend unless score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
