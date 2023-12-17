3238cbe25936ac966f013bccff074106733f1d6c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:33 +0000

    chief hard allow familiar worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
