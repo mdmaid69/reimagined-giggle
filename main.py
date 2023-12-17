349de78921956b6db6045d188fc3c03f447cfd4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:35:24 +0000

    nice job discuss sign live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
