09fa6e9478fd07febac26514954fcb422b927506
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:25 +0000

    distance shop ourselves come than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
