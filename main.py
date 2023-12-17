3ec9a06c9ab7d1c0188482d5abb6f8f202263628
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:45 +0000

    also level find belief guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
