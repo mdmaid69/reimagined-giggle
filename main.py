b073af5f96cb0a5a58a529577b63e9faf160157f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:32 +0000

    brown appear bend mark trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
