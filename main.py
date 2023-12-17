d3d3bf0c2b5fff4185c05069a5ad09f051a31aa6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:43 +0000

    obvious sister occur children obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
