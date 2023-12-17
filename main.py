2099d4eaf915d285862f7c244aa5b035e4e7f8e3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:50:02 +0000

    please square question stranger view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
