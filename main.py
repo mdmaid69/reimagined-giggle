342a4878baffb2708ef492ae7e97b65b16324c7c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:05:55 +0000

    every tight land kind return

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
