a64330d3cf1bb93989ee834cadf8ccef9729de1d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:07 +0000

    suddenly turn wear appeal congratulate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def convert_to_binary(n):
+        return bin(n)
