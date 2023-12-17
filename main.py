0af20633631f660ecfe7f9755018b5a8fdfb658b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:03:58 +0000

    extra trial little blame before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
