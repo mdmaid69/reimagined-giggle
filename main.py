0cb98406b73ad3a846644a220ade712e94553afd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:19 +0000

    reply runaway turnup walk arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
