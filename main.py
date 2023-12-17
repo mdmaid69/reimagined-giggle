efd91afb2a51d3aaeba6ef49dcc1ac808fc91db2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:56 +0000

    prefer Rr improvement guess design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
