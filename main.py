67474eff9886de3999ac09fc0a0b37163bcc9242
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:40 +0000

    narrow unlucky tone instant black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
