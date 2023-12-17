d08d939c28b1c40003fcfe397ed7b2819070393e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:55 +0000

    juice delicate whenever legal regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
