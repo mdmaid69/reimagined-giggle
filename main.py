84d0be8639c318297c5ee5f84e7c5c52a48e72f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:24 +0000

    cinema welcome classroom scream tool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
