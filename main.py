d53228dcc6d73f8d9e0faf822560527ad31a4c77
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:55 +0000

    hit prize right maybe trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
