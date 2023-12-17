1e49f155f6d3d2dee6762912f855fb2387087fa2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:41 +0000

    noisy callat season evidence later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
