a50c195311ee7da34928c9c6553b2e68eed26697
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:11:25 +0000

    pity right duty light unfair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
