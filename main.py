b67f4c643266bcce9d219bb2d33e90edc8509b93
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:40:24 +0000

    this quantity heavy broken free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
