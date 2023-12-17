e8d12a4f622cf12084b29f17b0b2015341bc01bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:45:56 +0000

    centre already content accommodation secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
