e8178e015bc241fa96587ce0fee444cae7b92a0b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:39 +0000

    apart gift satisfactory letter cheque

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
