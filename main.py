dd46896913df209111b94ac1d75038573a45af07
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:56 +0000

    holiday settle minute unkind connected

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
