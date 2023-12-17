ab44838c311eedffe7a7ed5fc2cf12c0c0d2d1fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:29 +0000

    carry giveup tip shame lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
