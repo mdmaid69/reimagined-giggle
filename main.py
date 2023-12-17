545b299d93cff5f4ad2f3ccc1cea768367b5f5cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:01 +0000

    love soft several low late

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
