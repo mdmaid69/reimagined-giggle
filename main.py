440acf75c19524c5a2963303a87a8146d36850b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:34:27 +0000

    sweet more poor long contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
