0be40c1bef73c60f7df9c3799d932cfb0f5679e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:11 +0000

    pretty replace early accident live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
