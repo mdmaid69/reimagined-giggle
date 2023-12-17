e6fe7e92cd305e3ded8794b9cecead6394950d6c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:34 +0000

    dangerous average interesting dance reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
