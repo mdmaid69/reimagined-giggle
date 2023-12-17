1efdc4491fd654060f504f7fc22afe2604d8bd46
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:13 +0000

    comfortable expect narrow reserve wear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
