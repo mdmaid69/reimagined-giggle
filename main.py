a5d3d0a53f22be6001dfac5a01d59c47720a775c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:04 +0000

    rubbish he awake nerve ordinary

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
