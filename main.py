b479596eca2b2b359647e6e1b1edd45dd4825680
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:36 +0000

    ago only gradually certain street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
