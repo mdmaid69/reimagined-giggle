e6c162171552f852f6e30e32af1441fd6ef155bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:25 +0000

    loud rare ever improvement post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
