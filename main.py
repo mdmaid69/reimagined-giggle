e88f8ee7d3acf3735a9b58d7b51657a9d1b39703
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:31 +0000

    ashamed slowly skill smile tall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
