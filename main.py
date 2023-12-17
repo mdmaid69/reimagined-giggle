e39119860058575f97a54f89e1e2d06089ac8161
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:44 +0000

    walk whisper prepare door ever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
