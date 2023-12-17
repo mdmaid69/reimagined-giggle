9376a026a309084e5d829802a83ebcb4ac14e1c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:43:42 +0000

    smell terrible hour affect thirsty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
