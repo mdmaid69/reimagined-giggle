40099afdf5118e1b2d14500643c731145f86825e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:35 +0000

    legal art fortunately that plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
