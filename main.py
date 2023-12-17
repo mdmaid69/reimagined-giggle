a55b03390a8a5528723802386e2b418e6e153e40
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:23 +0000

    take before ask copy interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def convert_to_hex(n):
+        return hex(n)
