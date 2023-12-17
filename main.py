c5956a74b2deb5f0cb5724bd33f5a03ed76f5c6d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:27 +0000

    choice top closed let impatient

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import time
+print(time.time())
