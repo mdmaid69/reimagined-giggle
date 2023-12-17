7f70cc943394779db0a1ce25571fc23d60e13f0e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:13 +0000

    goon correct straight hot not

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
