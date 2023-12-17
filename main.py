2fefbe2a1acc1f819ab1220f5c8f9166ca7dfc57
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:47 +0000

    prove away comeround cost post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
