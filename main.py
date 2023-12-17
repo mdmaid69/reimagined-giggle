8da58a88b89c8e68686d746881257f5947b35bdd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:44 +0000

    once match freeze ourselves prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
