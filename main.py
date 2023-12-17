aaa2774afb6b0c122be45295f5813d9e09241b68
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:59:04 +0000

    pause remove shape dig everything

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_area(radius):
+        return 3.14 * radius * radius
