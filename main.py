b27885b51e2a033d163cd45851ca30a4e1e270b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:17 +0000

    private entry tooth complaint noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
