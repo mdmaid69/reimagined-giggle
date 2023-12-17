0c1f2cef35c216ae544e1b91f3558f088dc651e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:51:50 +0000

    market pen doubt call she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
