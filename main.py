cba3cff90db2b757570f0546664329b9c0e80ab6
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:57:41 +0000

    meaning mainly opposite side argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
