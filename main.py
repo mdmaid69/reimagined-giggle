e671bdaf20099fb3361e21a0a0e35514c8a37364
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:45 +0000

    row cook sad price equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_stack():
-        return collections.deque()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
