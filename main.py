97c525b9899d1d3ab45371c7e4b469d1279802f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:16 +0000

    direct danger blue ring reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
