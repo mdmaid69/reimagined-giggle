020268bd6be4a69af3b3e0adccf7985343018c43
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:11 +0000

    your level steer top yourself

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
