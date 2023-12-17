30ffc0e7c372fe331aa1ae8695a4e6a516c26e90
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:11:28 +0000

    ours discuss lot path calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def split_path(path):
+        return os.path.split(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
