effb886768dc4064520104ddceba4833ec320706
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:35 +0000

    produce approve when present Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
