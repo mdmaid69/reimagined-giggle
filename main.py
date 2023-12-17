cf75597e72bd860699c9b5d1400f1cbc85175f45
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:20 +0000

    runaway separate freeze accidentally no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
