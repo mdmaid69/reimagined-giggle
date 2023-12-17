a225efd959fd17b0d40895d77355e7172fc42fb9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:04 +0000

    recently computer pot defeat crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
