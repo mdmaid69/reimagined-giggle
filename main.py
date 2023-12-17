061e730381e7397f63bf4dd24b803a875e6b19cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:10 +0000

    plastic maintain mistake meeting platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import datetime
-print(datetime.datetime.now())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
