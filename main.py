455fcfecb21626bc08826f427116fa9e475621be
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:47 +0000

    in story pattern path strict

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
