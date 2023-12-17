71e540f280922cce9e97bb1c01de562956c6c8e3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:37 +0000

    show equipment force unite Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
