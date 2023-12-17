39a3d35a09255a60beae93e776d14ac29e530f07
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:35 +0000

    sail plenty must another surface

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import platform
+def get_python_version():
+        return platform.python_version()
