d4d27321b0672b465e7c651b8ecbd4c20e5bbefc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:43 +0000

    project lonely board sort hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
