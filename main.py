6fea28000986cf65279f985348aacb8afe27323e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:16 +0000

    organization scare return hers skirt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
