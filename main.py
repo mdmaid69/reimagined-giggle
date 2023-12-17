d17362a649cf5d4ecb9343b1367cabb12675f1ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:49 +0000

    pinch connect radio mean only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
