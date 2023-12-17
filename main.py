53d3f1af098746979a64e2186231d78c6f9288b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:41 +0000

    goon myself me unite protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def square_number(x):
-        return x**2
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
