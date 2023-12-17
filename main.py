cfb1ee3a5a00809dd54504fc1454fdedc2445a9e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:59 +0000

    mysterious snow quickly talk pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def count_elements(lst):
-        return len(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
