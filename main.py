0d1d03d62df99c7b5b2df30b75f5de49779b0b0e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:11 +0000

    catch callback family kind central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
