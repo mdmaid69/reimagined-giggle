25c83c9251edced386b4ae2e4100f102eac27a79
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:53:11 +0000

    exercise hold expression callback keepoff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
