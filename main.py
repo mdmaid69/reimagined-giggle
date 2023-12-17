b217b399ac910864f623bc24599f17d38ca36887
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:28 +0000

    convincing disappointed doubt again round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
