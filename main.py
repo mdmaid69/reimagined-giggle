816a1f2ef4061324bc7da044ef42f9442543b338
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:57 +0000

    swallow Ll town load onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
