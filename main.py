0fc16e00d3804413cea9834e136eb9487d9d3978
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:17 +0000

    may mad intelligent tidy tooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
