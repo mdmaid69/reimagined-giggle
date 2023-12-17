5acf3cca707352ff1e45d54f501b24ba780662d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:07 +0000

    square convince turn down tomorrow interview

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
