5ee23a082cfd3302b10ea8efca042711d2044792
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:22 +0000

    sticky happy anything tone transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
