66a29f5d9c44769d12ad1a4c08b9b6fa86cfc0d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:13 +0000

    coat national father feel possibly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
