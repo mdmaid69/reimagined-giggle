3fc67de4e63ff87d08b4d630032cbb48659e9aac
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:08 +0000

    turnout top several apart smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
