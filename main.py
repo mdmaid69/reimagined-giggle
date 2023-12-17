402721519ccd32c31992dba1dde796aa8ace7843
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:51 +0000

    confused Yy impossible national contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_pressure(force, area):
-        return force / area
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
