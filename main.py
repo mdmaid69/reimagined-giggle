6d1e02209b0371af85fc75062d5e4d9ce7058c4b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:49 +0000

    contribute talk accidentally dry post

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+for i in range(10): print(i)
