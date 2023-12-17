1f7adf3ea9ffb9d37eb1f77358fe7c4a726ec908
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:05:03 +0000

    rub government takeaway develop mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
