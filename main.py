829b4e28753ddd47bd27528890a475b9bb3f0c73
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:36:56 +0000

    still mine cry event thirsty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
