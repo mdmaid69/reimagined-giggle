451b013af1ef9fc26fda3a6a383bb69c7106df32
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:17 +0000

    thankyou interview history takeoff disapprove

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
