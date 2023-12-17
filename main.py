587027601a29c04f8023a1d54f3b5ea603d46b51
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:34:19 +0000

    needle action confusing will expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
