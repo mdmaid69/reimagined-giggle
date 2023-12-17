a6491b2452344d7c86ec0703ad6b0213da29c7a2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:57 +0000

    possible solve tip substance smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+print(os.getcwd())
