8e8b1a65e735f1e9b5cb39f30d50ce65d404fedd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:40 +0000

    while illness report generous back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
