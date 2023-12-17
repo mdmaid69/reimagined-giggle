f93b184958e4d480ca0bcd2c72d15e95e0653082
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:50:17 +0000

    war even now band ability

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
