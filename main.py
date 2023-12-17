c33fd48311324608c9e6220f5b14f568fa40b071
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:46 +0000

    glad bringforward weigh free heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
