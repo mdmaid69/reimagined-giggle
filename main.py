91df2a2a3e638e23c1a3bd9d86ef58b560811cce
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:21 +0000

    century there so ride later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
