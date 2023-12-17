0ec9eaff57706ed0688a363921d849cb84ab36dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:21:55 +0000

    harm father goup introduction forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
