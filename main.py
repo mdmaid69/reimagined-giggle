6ae78d1615a48cc4ac1392b267887f09daaff8a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:53 +0000

    exchange frighten rush comeoff tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
