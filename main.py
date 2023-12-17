b76bca01308d6e7718b1b903addecd653cf19975
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:22 +0000

    disappear everybody rain mark language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
