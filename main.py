eb538adfe76e5bf689dd13381641b6e07d133e69
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:15 +0000

    complicated probably judge visit argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
