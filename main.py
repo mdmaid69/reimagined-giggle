a5b7edcb7bb7b4faec229c27a2d8b8a619832158
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:41:15 +0000

    half slice suit pretty wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def is_odd(n):
+        return n % 2 != 0
 import array
 def get_array_length(array):
         return len(array)
