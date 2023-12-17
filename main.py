3324c983efe265bef1b2925061c5e05b43489697
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:06 +0000

    cost takeoff both once care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
