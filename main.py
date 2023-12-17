dc95ae9661a89d3a8ae221752bb56e1b49eda595
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:26:15 +0000

    split to food depend blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
