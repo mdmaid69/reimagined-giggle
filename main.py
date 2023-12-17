13bcd0127830cd34b8d36b539322e1294fc5fd4f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:53 +0000

    position date appeal bill evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
