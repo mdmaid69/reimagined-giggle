16892b8b106fccf4674bc3128e2dbc33699bbbf8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:55 +0000

    around beginning remain path respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
