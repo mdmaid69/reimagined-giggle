2065c75a7334a5d5c6e5fcf336c8513e48d42ebf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:58 +0000

    right square pinch change treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
