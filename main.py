7c99ddd94ee8a297e7a4034bd45237c9bd2c8b8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:45 +0000

    order flood attach fortunate before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
