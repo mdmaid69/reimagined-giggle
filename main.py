6ea68f62fbb75de6736eb3f6737f4891641de866
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:53 +0000

    regard prevent club your excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-from collections import Counter
-print(Counter("hello world"))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
