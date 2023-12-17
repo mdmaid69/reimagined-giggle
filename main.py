36969a1ec5747edc3804cd3a5b3580dd828774a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:12 +0000

    perfect house stop concentrate somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
