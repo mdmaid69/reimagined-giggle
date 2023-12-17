8a84fdd3b17f3248e1eba1af794c84537e3516b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:11 +0000

    report weigh organize yours part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
