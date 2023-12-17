8640a7ec1e2fcd79a17d28e0f5cfad93c74d5220
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:42 +0000

    will design climb something small

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import time
+def get_current_time():
+        return time.time()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
