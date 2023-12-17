e3428f56968e6d932562fc8ef15680accbdbe09f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:15 +0000

    blow expensive chin even deaf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import sys
+  def get_python_version():
+        return sys.version
