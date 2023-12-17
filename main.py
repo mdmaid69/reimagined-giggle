f486c5e79a9b7f950b6664d2936b0d5b6d744374
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:22:30 +0000

    among effect confusing getout soon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(10): print(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def clear_array(array):
+        array *= 0
