2bf96ea34441e0619e88a936cd8cff8b7a8059ef
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:46 +0000

    takeback spend dry fortunately familiar

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import time
+print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
