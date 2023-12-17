8a178b908fafa25f255759816feeebdb0f247482
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:41:37 +0000

    send otherwise him thorough fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def add_numbers(x, y):
-        return x + y
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
