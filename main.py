3cbc4eb26a173d952e22ac934f01909ad1df7ef6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:46 +0000

    breakout disappear how trousers form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
