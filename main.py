c2e7b6c1a8883911b8302b6c6312e52ace3684ad
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:38 +0000

    brave beat train between legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def square_number(x):
+        return x**2
