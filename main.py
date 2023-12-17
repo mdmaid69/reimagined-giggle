bfd26b3b36450cf0170e4b2bf56720bcca972b80
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:18 +0000

    child as silent law gap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
