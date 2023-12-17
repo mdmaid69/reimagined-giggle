acbc461e5c39cbceefa0b8568609027a5adf22ad
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:06 +0000

    keepoff sort steady live unless

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_stack():
-        return collections.deque()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
