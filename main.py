e64b6c083b87b964738903299a5d39367807bda8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:30:51 +0000

    same almost fit artificial Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
