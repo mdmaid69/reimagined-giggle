8a782b0ea18f55b77c66cad533fb2f161aae4072
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:09 +0000

    refuse forgive church deal steep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
