1c273dacf763e6caeaf7e3a44d0643da72b331fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:35 +0000

    against shame hope apart morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
