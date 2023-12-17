59c90976cbb92310ad9a8b80c57dfffa4bbf6753
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:59 +0000

    face unfortunate takeover strong satisfied

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+print(os.getcwd())
