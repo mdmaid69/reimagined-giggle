55f273644ade15406b390f23dd87a0eadc7223e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:05 +0000

    guard expression progress tape before

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
