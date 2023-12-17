7f35c3ecc1d868bc0be9aeb41cd57ee1aa98133f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:51 +0000

    date fasten laugh unite spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
