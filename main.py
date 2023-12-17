9266e42db1707010ac4ae287106f3d3accd74505
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:41 +0000

    because hers hers thorough dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
