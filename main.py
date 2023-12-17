83d152708d5bcb05a569ec73f16a85e1413e65cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:37 +0000

    lock 570enjoyable dress figure black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
