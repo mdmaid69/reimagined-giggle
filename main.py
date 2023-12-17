ae9855d7d7c17ce03b20c2795a20e571d87584fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:56:30 +0000

    stage drink comment backwards loud

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
