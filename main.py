71dfeb79622c940540cb59912fe906b2c50919c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:56 +0000

    mind proof apply shall soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
