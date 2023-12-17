be1678b9721a2e5e7159ae446bf794d2c1d7a974
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:42:27 +0000

    bye public print fairly may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
