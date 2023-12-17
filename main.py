a3853c22da0e22213ef5cbad38fa8a0704f2967c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:56 +0000

    sense welcome trade ours floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def square_number(x):
+        return x**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
