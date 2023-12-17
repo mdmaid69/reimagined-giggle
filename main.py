a1052ba1195514da310e4bec34fe12bf5c84f3ef
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:45 +0000

    major welcome happy sudden guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
