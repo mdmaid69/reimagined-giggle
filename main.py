abd0667015c4bbc4949014abe6736cedb67f026f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:11 +0000

    sink flood float division deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
