36e516c4a92992d0de3d9f7577703204e77aac6e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:51 +0000

    closed reliable invitation grateful travel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import logging
+def log_message(message):
+        logging.info(message)
+import os
+def get_current_working_directory():
+        return os.getcwd()
