6134051e7be01a27010097eed402359ee5b83e32
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:59:39 +0000

    possible downstairs exam callback bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
