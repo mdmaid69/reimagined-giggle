47f58fb5e1b499de220a7a9a268799b839b4092c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:50 +0000

    giveout boil happy because 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
