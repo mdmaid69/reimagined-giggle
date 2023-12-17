77ef6192897b0f44854aa8d937f209c79c3b55b8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:58:27 +0000

    except furniture slowly question contribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
