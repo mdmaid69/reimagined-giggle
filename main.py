d86f209fcd1b216b940fd1de78df9fbe2bd8c265
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:03 +0000

    bar bridge their love refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
