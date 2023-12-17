48313b08b19737162fc19f6e67af0d118cf209b7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:28 +0000

    sight willing brief wire before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
