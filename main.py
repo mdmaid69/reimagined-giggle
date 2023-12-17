a986b26037ca57a0739e227c905e2c6a3c42f139
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:52 +0000

    fashion personal cause bang replace

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
