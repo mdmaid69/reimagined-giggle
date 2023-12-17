0df47c5353e829a6e3d3eb51abb9a62bbde69903
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:30 +0000

    suggest innocent mix mind present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
