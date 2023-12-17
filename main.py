ee0beba794bf0d7c165341884d47f4993dbab389
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:10 +0000

    loose every whoever contact pure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
