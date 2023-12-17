6d2a5f4719404fae5eba7e13dcce8ab09a242a71
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:23 +0000

    tone clean bite neither fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
