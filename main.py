3cf745efe186eb981c9b2479c71cab3272284027
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:51 +0000

    weekend welcome comeout hold lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
