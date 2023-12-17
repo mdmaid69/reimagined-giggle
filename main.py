359de2107d238fa2d7e14bf60550ddb3b284ea2a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:16 +0000

    draw protest mention subject interfere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
