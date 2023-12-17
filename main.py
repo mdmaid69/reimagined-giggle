2ef4c03cef3fbe8514a07b708db1bced4c77c08a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:42:50 +0000

    still mass straight by moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
