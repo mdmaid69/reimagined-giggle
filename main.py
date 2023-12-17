6450e4b0f9ea4418c83c30e4aaad74b3247b394e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:29 +0000

    takeaway makeup lean 572enter production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
