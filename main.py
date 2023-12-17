460f84e252429fc75d847831acc99f054d42fb67
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:34 +0000

    game patience train the contrast

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
