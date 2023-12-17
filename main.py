366fff634ae07e1fc75546de244ce19a0547472b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:55 +0000

    slightly many official sight general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
