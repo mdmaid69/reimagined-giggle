e81b5c20150acbc9da734c41d709d47377eb1970
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:05 +0000

    familiar sail train whisper colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
