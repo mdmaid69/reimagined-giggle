bfdd96be1d573ee72dcf31439fb2377d0fb34e55
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:47:13 +0000

    noone come hard whom oil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_speed(distance, time):
+        return distance / time
