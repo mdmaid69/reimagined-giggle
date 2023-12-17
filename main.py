a5d450433044b599c71c77f4a2826d8d38416580
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:50 +0000

    various university add increase hers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
