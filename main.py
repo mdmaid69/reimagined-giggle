42d5ee1b77d925eb302e9cf69063ac10804581a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:22 +0000

    spot total concentrate section refer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_max(lst):
-        return max(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+print(re.match("h.*o", "hello world"))
