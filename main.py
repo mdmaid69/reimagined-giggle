454b0ae5ce578aadcf6e17437d74d751ad5654f8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:54 +0000

    flight waste regular ourselves must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
