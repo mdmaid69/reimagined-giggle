995ababb6b677cfbe07584231d4e5b4261f3926b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:35:48 +0000

    character comeoff getback live although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_str(array):
+        return str(array)
