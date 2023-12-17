e18dd623e943c178ea7a35967781a68c6d4d1c1b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:48 +0000

    repeat bored false lie far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
