11c049cd27f1ec77d0bfc32ba8e324d69d5161f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:23 +0000

    content substance awake responsibility left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def cube_number(x):
+        return x**3
