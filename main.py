d99ba9429c35c3b65b047b45cbb23a6511de2fc1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:48:01 +0000

    recommend about judge sore very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
