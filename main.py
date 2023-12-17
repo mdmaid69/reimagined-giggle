d1bc30cd66467e289cf12a7cbe755c5b2deb3d61
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:10 +0000

    getout skill you release 568energy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
