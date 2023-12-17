8790535c755d2a203d351f8a71e5f71e64a91bd3
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:01 +0000

    mark achieve bag claim rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
