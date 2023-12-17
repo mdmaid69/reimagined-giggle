f1146fe1ef868c43c1ea817adc96735b223f82b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:40 +0000

    manage except too right however

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import logging
+def log_message(message):
+        logging.info(message)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
