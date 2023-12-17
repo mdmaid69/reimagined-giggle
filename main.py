06fb40128a4ce7c34772872a15a8197f326e4e8d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:38 +0000

    sore sensible impressive post mainly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
