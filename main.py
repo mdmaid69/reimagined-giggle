94986322d56d959134e0c40f3d981c2aecf4e2f8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:30:20 +0000

    container spill although prepare autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
