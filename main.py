86bf3e6c0294bcbe33932a69128240372b7e4735
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:43:34 +0000

    frightened grateful telephone soil figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_length(array):
+        return len(array)
+def convert_to_binary(n):
+        return bin(n)
