76f8369411bc43b3cd7da08e4ccf557454853ae2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:55 +0000

    load morning powerful increase murder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
