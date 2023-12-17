b848702fa15309ce134b42f7e56beccf9e77bd3b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:18 +0000

    bar damage plenty bag replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
