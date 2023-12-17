281704e2ef0b8616a8a28b545a60b03bd84778d6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:50:25 +0000

    key thorough person wait Cc

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
