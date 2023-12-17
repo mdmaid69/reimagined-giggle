8704db9022c3ebaa41ecb38073816d795d82f838
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:36 +0000

    typical section today authority little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
