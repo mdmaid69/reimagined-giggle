05161212a894c3f7a4dc42485cff4e037d200edb
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:08 +0000

    by slip clean movement runaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
