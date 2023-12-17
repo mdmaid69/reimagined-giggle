e2656ab4e152392c7398d1ebf18b51d8bd473b2c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:00 +0000

    gap smile quick bang centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
