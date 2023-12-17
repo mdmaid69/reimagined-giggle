a560783e0ea376b92af2f5e16b683d803505bcc8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:14 +0000

    priority interview will yes hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
