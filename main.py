d900f173153b8fb9441f05e1882d7fa29705b301
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:56:28 +0000

    friend quarter hollow aware hang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
