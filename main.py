cc6f5a1e773ae8a1adbdf1c68db4b554ebf4d54f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:47 +0000

    major large notice boring give

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
