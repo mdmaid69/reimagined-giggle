291e9252f8c3f4ec9bf73fc2a5e00d6dc5d35df5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:45 +0000

    combine come exercise blame deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
