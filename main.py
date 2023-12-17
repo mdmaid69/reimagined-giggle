8680eca9c1b67509ba1d1b16b7cea8a46f6f9a44
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:31 +0000

    cold connect direct guide during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
