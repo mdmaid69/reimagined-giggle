c93cbaf6e62dedf86c4c4954af854dbc9977339b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:28 +0000

    everywhere simple person argue think

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+import sys
+print(sys.version)
