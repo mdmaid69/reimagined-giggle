00c889c5ce139a665e31926f8d42e443f704b419
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:24:08 +0000

    concerning exactly hair to seem

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
