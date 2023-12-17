d4e57fd0b648fdee82c2b7ee160df4c02a2f93e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:36:02 +0000

    fix sudden then share profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
