80b78d73f09b08fa32d34ed1a4b5c689b57f91b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:24:56 +0000

    line sit point still apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
