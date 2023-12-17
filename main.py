a681e89bad78e155b454a5ee724d9e42c8bedcc6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:59 +0000

    get method satisfied speed powerful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
