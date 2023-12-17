89ea3ae357036c508f7bae6a7a45a6acfdadeda7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:45 +0000

    soon deep approval right best

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def is_odd(n):
+        return n % 2 != 0
