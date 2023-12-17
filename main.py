8c104116ed0255f8b76e719c40f32db2aa36b4d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:41 +0000

    turnback rob air thankyou behaviour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_palindrome(s):
+        return s == s[::-1]
