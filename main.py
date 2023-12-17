415e20ca8f06ded56b6d4ebb82a776f238442a7a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:37 +0000

    respect floor club unite later

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_file(file_name):
+        os.remove(file_name)
