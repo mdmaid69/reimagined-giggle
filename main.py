c5b66ee5b4f56b63e4b19492a9213d192c3e9138
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:59 +0000

    during calmly major activity breath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_current_directory():
+        return os.getcwd()
