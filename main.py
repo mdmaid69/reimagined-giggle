a8005b91b839d99333cd5dfe7185b8a03a8cd7e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:52 +0000

    honest impression thumb safe tap

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
