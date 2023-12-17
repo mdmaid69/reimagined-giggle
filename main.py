d03af6c1c2b019b96535115b056a3b22ed05ff31
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:52 +0000

    machine breathe continue container famous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
