9adad68db4bd8a9a1f1fecf48f96a42554bde76f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:08 +0000

    legal callout squash surprise brief

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
