ebc19c555df64cc19eb47fb2014247a0e14981ea
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:17 +0000

    delight kick fear weigh anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-print(math.pi)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
