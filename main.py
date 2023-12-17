beb882110004592928640fdf9ad7b80e4fb0930f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:47 +0000

    disapprove public wash all bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
+import array
+def reverse_array(array):
+        array.reverse()
