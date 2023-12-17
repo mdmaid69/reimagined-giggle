7dc1ab07a94d96f2028bf06e8727f41dd64b055d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:57 +0000

    figure bag dry fashion turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
