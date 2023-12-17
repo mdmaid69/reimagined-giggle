7f3b2e9e910ae16c534e3f5bf8c21eea1ec9f228
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:10 +0000

    correct store underneath substance thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+for i in range(5):
+        print(i)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
