6d35a0c27f95ec381c324747dcf3c22f34a794af
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:14 +0000

    poor battery unit clear inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_list_from_array(array):
+        return array.tolist()
