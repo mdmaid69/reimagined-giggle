b8d2073a415e0d5aa217e44f1797683e550733d6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:35 +0000

    spite flood necessary mess round

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
