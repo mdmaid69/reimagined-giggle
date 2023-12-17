6d179136be1bfe9bc316824bd8fb4b5839fd551c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:51 +0000

    mention history design funny cold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
