e5456f73b3561c376472b06f7141d3afcfce7a9f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:39 +0000

    frighten coat little advanced tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
