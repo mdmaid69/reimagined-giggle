8df1a57fb662710b934678837c6d5fedd5cd0c42
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:34:21 +0000

    Ee shoulder town stick cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def count_elements(lst):
+        return len(lst)
