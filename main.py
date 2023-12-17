13474f78770949d8d7319d8e0dfef8b04c8923e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:17 +0000

    near pair try nerve hope

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
