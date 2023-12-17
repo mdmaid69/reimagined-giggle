eeb5d7dd08359ac8dc16ad86f20f4d2b864dc47a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:32:10 +0000

    use of his comefrom frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
