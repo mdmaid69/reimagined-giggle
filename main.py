f16c51eab1af797ea843da64edadbc193120aedc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:31 +0000

    male pretend land anywayanyhow never

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+print(re.match("h.*o", "hello world"))
