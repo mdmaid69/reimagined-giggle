d9987f628ab70b04fe7c3a2868ba845fc5f13274
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:41 +0000

    star calmly speak callup rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
