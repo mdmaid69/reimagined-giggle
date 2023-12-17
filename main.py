5c0e72578e17b7785c3f24e4a8132db65254ad00
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:21:57 +0000

    Ee leg fast question favour

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
