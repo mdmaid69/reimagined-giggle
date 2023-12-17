7509fb84453f6c81bfce089b2d811de09992e080
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:35 +0000

    behind concern under limit sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
