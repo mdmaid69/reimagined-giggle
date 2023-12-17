d117ba8099f66a2c139f280335f27beac7af9106
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:39 +0000

    rush tall key smile reserve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def reverse_array(array):
-        array.reverse()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def cube_number(x):
+        return x**3
