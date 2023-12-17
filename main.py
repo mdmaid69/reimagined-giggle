0794ad430ffa2b66a90b26b5fd2186f1a36c348f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:55 +0000

    heat breakfast live basis together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
