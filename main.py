9233e06ae905b82ebb6042f52ca7ed55b68197b7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:44:24 +0000

    period drive sad charge accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import sys
-def print_python_version():
-        return sys.version
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
