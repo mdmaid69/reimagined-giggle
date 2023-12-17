da08e35eb1dead58b75d77f59bb9fdda9487e794
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:12:12 +0000

    change disappear government recognize hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+for i in range(10): print(i)
