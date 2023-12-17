2e38d283c0f33563ca7c61f43a12183c72af3f54
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:47:19 +0000

    band big makeinto importance rid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
