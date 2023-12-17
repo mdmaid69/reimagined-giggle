97e34563b1e4f235f3b4b1fd261be01fffd38c14
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:49 +0000

    dead recognize tie lower considering

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
