b131daa62aa97fd07edfaa440579ba0b35ebe4f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:18 +0000

    stay succeed turnaway neither ear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
