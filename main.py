9754b4d34ce8741b8c09e8307eb9b76e631c4a8d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:37 +0000

    accident classroom cope throw basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_item(array, i):
-        return array[i]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def get_array_as_bytearray(array):
+        return bytearray(array)
