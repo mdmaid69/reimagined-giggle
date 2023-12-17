4a0b325cc04e4938b0005c1712357bcf3aba4a7c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:50 +0000

    hurt emergency beneath borrow ours

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
