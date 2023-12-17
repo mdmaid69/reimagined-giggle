3626f498a77d784b53ca26f30a004b46d73076e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:56:31 +0000

    squeeze strong tend approve nigga

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_speed(distance, time):
+        return distance / time
