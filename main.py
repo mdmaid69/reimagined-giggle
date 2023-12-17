fa1bb1986d53eaf098d4f030b4496eaee188743f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:03:42 +0000

    tooth hollow period discovery wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
