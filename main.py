b08ecf23e89344b7969b4c57218dbef31189e13b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:34:37 +0000

    combination smile mention seriously accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
