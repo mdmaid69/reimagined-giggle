da431c7a238ba378451127f16f8132438b530b0a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:09 +0000

    own friendly greeting successful hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
