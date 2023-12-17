1e5378b5ef41ba1b27225a8da9cb4504977920f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:08 +0000

    ill soft closed history feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def get_time_since_epoch():
-        return time.time()
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
