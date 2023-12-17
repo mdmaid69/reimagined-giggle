2be13621327414af079d2af449f3f7edbcf700ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:52 +0000

    damp willing closely holiday shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
