29d102cf82ec0cc0eee975b5fe0d64535ea79fdd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:50 +0000

    boring practical cruelty climb talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def reverse_list(lst):
+        return lst[::-1]
