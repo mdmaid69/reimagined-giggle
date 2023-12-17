6d5889ffc10cb8eceaeb4b628ecf494250f1e013
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:27 +0000

    believe dare odd boring backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
