a02e4203b57070baad1cfd154a5f0815d1859817
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:49 +0000

    ask reaction minute shop hot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
-with open("some.csv", "w", newline="") as f:
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def reverse_list(lst):
-        return lst[::-1]
+        writer.writerows(data)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
