7ccb04bd1aaf049694fae9d6a883f911c398d052
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:01 +0000

    promise Itself consist content moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
