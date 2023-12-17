751e7d7b02230e0a8b117c424ff5fdb26542fbe3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:59:49 +0000

    the software familiar degree far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
