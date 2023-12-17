5f069fcd6438fbdc8285a7b1fe15187360352ee5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:09 +0000

    preparation instead neat us awake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
