3497da6eb3fba56f8623109d0da047505821ff33
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:36 +0000

    move spray earth boil mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+print(math.pi)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
