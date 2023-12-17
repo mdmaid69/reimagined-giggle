7b5d133a774f5d244f180e3264f77775a63b99a5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:51:28 +0000

    hard instrument speed storm period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_speed(distance, time):
+        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
