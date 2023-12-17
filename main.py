d8a77f525f11cc8ffd22842673070c13bee44d49
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:11 +0000

    closely brain favour care ourselves

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_power(work, time):
+        return work / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
