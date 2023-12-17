be063db7e132e14903ab07af551051450be50c7f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:28:09 +0000

    business doctor leave reach clever

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
