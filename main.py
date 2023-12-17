129b3ee9a37da2bcc74d4c6b745af26d0dc951fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:33:50 +0000

    flavour dangerous learn software bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
