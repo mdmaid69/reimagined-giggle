4153b86d9f0664f5c54167a635700fc94d666d44
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:58 +0000

    model high up long interested

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
