fa8d0a423beaff07927267121361df6840d46292
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:01 +0000

    unlikely S s thank you introduction fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
