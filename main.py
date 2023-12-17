083a3984d12571b91b8f911000ce5dfaecba6426
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:27 +0000

    issue children job community minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
