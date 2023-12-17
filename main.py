3859685b0811cbb70ff39eae1975ad63738efab0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:29:58 +0000

    sound mass will fit correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_power(work, time):
+        return work / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
