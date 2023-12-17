c2346744860824610c9bb73c6e820f8644240f73
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:12 +0000

    pollution peaceful should pinch sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
