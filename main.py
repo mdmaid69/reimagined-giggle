6fd0132d481d8a8eee69fda0767c62ba622b3ae6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:17 +0000

    dream judge luck principle sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
