6a45869b4ef86e65b705db87b37cd45348dbbddf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:33 +0000

    control ring image open grade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_area_circle(r):
+        return 3.14 * r**2
