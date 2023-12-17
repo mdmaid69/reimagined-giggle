90a5610d148335b97c8ca8804c88e999f97c6a94
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:03 +0000

    tie check community town Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import getpass
+def get_username():
+        return getpass.getuser()
