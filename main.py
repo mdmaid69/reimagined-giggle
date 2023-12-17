d865930b3cb36f7a0c59a688ff144342a40c9a3a
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:43 +0000

    list fetch actual condition later

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_perpetuity(payment, rate):
+        return payment / rate
