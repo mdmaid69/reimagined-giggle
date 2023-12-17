81b85b9803b0f68d73de867a85d009973d2f46a6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:35:01 +0000

    opposite out violence interesting meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
