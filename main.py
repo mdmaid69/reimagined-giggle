9e135284dc5c33c6103ba7f2058ef49bab5e57ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:26 +0000

    inquire bringup own further Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
