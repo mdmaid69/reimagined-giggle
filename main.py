8709cef057044962a1e2ba3fc50dbabc1ebd4d4a
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:49 +0000

    worse advantage closely polite understand

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+print(sum(range(10)))
