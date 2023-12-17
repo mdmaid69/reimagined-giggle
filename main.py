e937f8790a81311317659e1684a72f09152f1a6c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:13 +0000

    Yy wish university his before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cosine(x):
+        return math.cos(x)
