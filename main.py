a4de8edd807d9c98d9604f54f54ca378138b8030
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:00 +0000

    production borrow at store nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
