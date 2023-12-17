c5ca10d2d91b91d0f5be78be264bdac07477f128
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:38:22 +0000

    keepup than dare bored future

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import platform
+def get_os_info():
+        return platform.uname()
