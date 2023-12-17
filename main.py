b52ee28097f905a0fc116451aa62de6c0db33abe
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:38 +0000

    thank sweep face store switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
