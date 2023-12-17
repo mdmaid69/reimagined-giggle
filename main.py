eb7a02d0aef9e5ca481fbf8b4edd426bc9cfb247
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:53 +0000

    guess pronounce claim previous memory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
