1ca0c20aac7deef09f7ee95ba30002fca893446d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:25 +0000

    ready jump past meet case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
