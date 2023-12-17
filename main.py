ff99fc5fff61182f5221306f581401cf025be2e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:42 +0000

    event afternoon second transparent lock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
