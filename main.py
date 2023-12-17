c45e7f869f10eb25dda6df7fb5fefb41797155ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:59:02 +0000

    complete actually door sing just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
