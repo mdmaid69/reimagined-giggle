7315e651c86125ab2b375eecd1e3e10cdc73db74
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:20 +0000

    phone division gate dish laugh

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(lst):
+        return max(lst)
