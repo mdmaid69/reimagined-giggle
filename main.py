5bee59e0641f2a061fe3c5534a601b1375adfb30
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:55:58 +0000

    them balance Yy available deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
