a68039af4c1c8a4970d7438dad1feaf31abf38f9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:02 +0000

    them spoil sign opponent interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
