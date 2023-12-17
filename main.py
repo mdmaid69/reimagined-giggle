f54ff5e32bc350fedc79f5725c601dd61b5c26fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:58 +0000

    correct centimetre fast study method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_stack():
+        return collections.deque()
