7e67c7ba3a1bc8484744a542a588c5d0f95378b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:00 +0000

    ear damp charge accident experiment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
