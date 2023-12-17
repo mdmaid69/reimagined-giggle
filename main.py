b2588ff6066062b034f7e9c4dec4d7e4398ba0ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:44 +0000

    recent reject succeed tax ordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def sort_list(lst):
+        return sorted(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
