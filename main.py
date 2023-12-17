3c343a306ce51c34b1cd4b99f064aaa5348d9fcc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:04:54 +0000

    worse lead disgusting who brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
