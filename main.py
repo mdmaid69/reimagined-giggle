5b6abee9edc1d9c6e7e979566fa496535d0cb10b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:39 +0000

    fond before below spare programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
