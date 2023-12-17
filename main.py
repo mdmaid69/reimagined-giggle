f894db11d2cea48f3e0f651f044e7b3730700f65
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:47 +0000

    estimate punishment reduce exploration reliable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import tensorflow as tf
+print(tf.__version__)
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_counter():
+        return collections.Counter()
