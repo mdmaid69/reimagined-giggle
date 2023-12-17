3f1c24cb682153e7156f2c52ac637cf87e75c469
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:46:14 +0000

    wire lend further judge home

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-print(random.randint(0, 100))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
