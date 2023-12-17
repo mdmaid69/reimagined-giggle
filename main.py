82d3974419e26534000d98f65f62304c0b74951e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:38:33 +0000

    tight yes expect shut their

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
