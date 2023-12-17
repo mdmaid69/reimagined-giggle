4f82ad94e6c0f4a58b449b1ede53259c27b5bd17
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:17 +0000

    speak tough cover pinch spare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_user_list():
-        return collections.UserList()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
