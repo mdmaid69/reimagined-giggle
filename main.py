2c31fa647a0aeacdee7fb375916fc4b29397aa44
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:14 +0000

    pretty under crowd responsible feed

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
