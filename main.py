adf36647dab72eb36427867dd78f24e99a9a4be5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:05 +0000

    excited central advice flood quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
