f42624e65983555eb6b091c2d12177c5fe6ddf67
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:34:58 +0000

    up family better interruption clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import tensorflow as tf
+print(tf.__version__)
