32d30a895a1626492d3af016c3fa89ce82869775
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:27 +0000

    week clean throw perform room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
