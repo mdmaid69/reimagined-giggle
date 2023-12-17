e7a5aaaf3b1674fac66ae2e4fddb1b9f11635711
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:43 +0000

    smooth thirsty afraid file Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
