f6506533ed22a0c613d4baddae51dfaa3fc4e863
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:23:41 +0000

    smart block building expression introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+name = "Python"
+print("Hello,", name)
