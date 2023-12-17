4e723b23f7e5c52381be4b37ab734c7d2eeeaa05
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:02 +0000

    tap earth often ahead its

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
