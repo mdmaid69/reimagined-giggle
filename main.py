fb165c86e00c8bad957d27dc39f98d0eec6c58e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    knowledge truth possibility R r lost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
