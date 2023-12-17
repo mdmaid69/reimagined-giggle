6e0fb65651966e71eba2b758308dc3a3c8d53cde
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:22:51 +0000

    spot trousers top pay weather

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
