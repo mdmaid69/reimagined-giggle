fb1fe92ed445de21eadf8bee2e797d6746da375c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:19 +0000

    unit grow season fit stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
