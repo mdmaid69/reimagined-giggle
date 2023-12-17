e3d723a89ed0f0718b58f8c86012e246fecd2376
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:22:36 +0000

    brief recognize fire nervous loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
