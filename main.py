97bc782add20dd2ccbe11bfeccb9a0b260180dd3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:11 +0000

    fix degree decide innocent bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
