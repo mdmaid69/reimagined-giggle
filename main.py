cc69a65f9d70b0eb557d571a5169111253bcb247
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:30 +0000

    pure uncertain plenty argue medium

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+name = "Python"
+print("Hello,", name)
