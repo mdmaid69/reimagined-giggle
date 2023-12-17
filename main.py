1ae39c930e4b7abc8334f4d543a54626eb9cabae
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:21 +0000

    tour leave trick otherwise message

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_force(mass, acceleration):
+        return mass * acceleration
