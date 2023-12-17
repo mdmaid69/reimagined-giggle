0ff3cd7686015de3c74aea955170d5442c1ca9c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:35 +0000

    home prove produce suckme pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def exit_program():
+        sys.exit()
