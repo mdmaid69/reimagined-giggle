119953c553ad5f17ff09144c940a16d72b5603a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:15:31 +0000

    approval dig waste official approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_list_from_array(array):
+        return array.tolist()
