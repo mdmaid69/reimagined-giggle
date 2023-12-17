291ac7d6667d4f3636ed1b2ea59079c9cfda18e4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:22 +0000

    Tt shrink slowly fly listen

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def add_numbers(x, y):
+        return x + y
