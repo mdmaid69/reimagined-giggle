bd662ff360329ecdf1eac2943bf151c0c1465f95
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:01 +0000

    information shout cheap weak violent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_queue():
-        return collections.deque()
+import random
+print(random.randint(0, 100))
+def remove_duplicates(lst):
+        return list(set(lst))
