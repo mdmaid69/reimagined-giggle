cf73490605172534f5ac91edf77de2bf0247b894
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:04 +0000

    schedule slowly turnon score apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
