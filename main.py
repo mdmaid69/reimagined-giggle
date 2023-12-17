a81313e9a3bb7eb97a72399c7f0831fb7d008bc5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:14 +0000

    give urgent moon stranger sensitive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def sort_list(lst):
+        return sorted(lst)
