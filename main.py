a2312acc6c7fe108f82693d7f1fb47f119a90edc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:09:05 +0000

    cover compete opponent among key

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
         return None
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
