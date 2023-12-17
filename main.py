7fb44cbe7bc89b72261a02481d7090ff22f0410c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:38 +0000

    stretch comeup business demand match

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
