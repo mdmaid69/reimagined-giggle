d489f320dce222c7d332f7266e832478eccae6cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:51 +0000

    zero party character scream since

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
