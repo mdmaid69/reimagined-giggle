daff0e002ddeb4f71362e9ceed23fac93da17138
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:26 +0000

    number pick whoever Oo breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
