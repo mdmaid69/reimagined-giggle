fc967f4161430ea8a99eeb98c85b743cfb42ec99
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:13 +0000

    discover side necessary opposite closed

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
