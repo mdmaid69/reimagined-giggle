980579e43323e4449c11fcea1b06f162a3651e57
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:01 +0000

    fall already run record object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def greet(name):
+        print(f"Hello, {name}!")
