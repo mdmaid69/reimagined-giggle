f4af8ab3742fb5c90981f934e3ba8589b8919222
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:34 +0000

    walk so health disagree otherwise

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import time
-print(time.time())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
