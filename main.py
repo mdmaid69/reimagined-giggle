b064bfa0e515ce52dab626c2650441e585240f46
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:19 +0000

    mind never transparent hope comefrom

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
