f3526212dec098e2c878aadd30bab04cf74f8b56
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:31 +0000

    only introduction either except suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
