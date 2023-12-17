3bf71a5f580f26c693c20ab680401289bb48889a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:43 +0000

    skirt beautiful underneath trip scrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
