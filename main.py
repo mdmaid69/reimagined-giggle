de75f1c93bb659e3bbc5c6de6b5f0e99fc186462
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:01 +0000

    beyond subject desperate key spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
