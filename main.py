36d18ae4fccfd16704e2f409818381627f494b42
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:30 +0000

    then half report base boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def square_number(x):
+        return x**2
+import array
+def get_array_as_str(array):
+        return str(array)
