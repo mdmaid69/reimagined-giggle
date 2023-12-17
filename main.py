205514edb7e314ef74bd14a66cfd647eef141442
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:35 +0000

    holdon police pity film scale

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print(sum(range(10)))
+def reverse_string(s):
+        return s[::-1]
