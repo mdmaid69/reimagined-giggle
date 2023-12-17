acac98b22d6f1bc711fb839cf51eb8afa78e5683
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:19 +0000

    pay switch straight unkind hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
