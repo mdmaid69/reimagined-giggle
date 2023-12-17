3c533b4685a2c59c527ffb62faf2a4647b01618b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:23 +0000

    survive bank child speak comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
