b097ae45a9d20884d0ad7dfa601fed5ea936531a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:08 +0000

    minor born accidentally noisy drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import datetime
+print(datetime.datetime.now())
