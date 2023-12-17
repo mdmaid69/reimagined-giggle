ced0baae4356db08b0fec8a42218c31feb89f9c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:15 +0000

    often correction touch may 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
