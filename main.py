9a3592bd147207dc95d9c7e885551e60922254e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:34:14 +0000

    blame breakup begin fault exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
