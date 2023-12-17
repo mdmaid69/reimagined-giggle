34aa21766eb3dd099f32c32c219ed75ac20670f8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:50 +0000

    least port turnaway smile invite

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
