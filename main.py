e795ab37448eaa03e425e76e6efdeaa30004f2d6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:40 +0000

    towards suit uncertain show start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
