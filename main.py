a35c6e4296c83da353aba95ab57d3e2d82c48186
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:08 +0000

    design uncertain distance description possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
