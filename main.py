1c5a1daf844b2d8730a28c8339645a88f04573fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    number muscle rent yes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
