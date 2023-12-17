40ba7f617fe1f83318c52b07e9470bbe7106b557
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:54 +0000

    direct sure best runout worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
