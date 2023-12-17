a7ba08b1b33d97d26d2afa02947086003f665055
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:59 +0000

    trick own first physical example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
