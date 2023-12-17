03fc961ee93a781a719ac46589ee7be3617133cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:15 +0000

    weekend medium circle hurt Dd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_user_list():
+        return collections.UserList()
