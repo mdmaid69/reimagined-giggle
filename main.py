636f875a4e24d4b6fdeef4c3fe426b7ed17952c9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:11 +0000

    trade art ground from funny

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
