8e436f3a415fef80ad96f8abb1ddd3cd51f809c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:48:11 +0000

    middle really game die model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
