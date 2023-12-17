cd180a447c5f87cd1feb8b1938c883f8f3eec76e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:25 +0000

    border rule argue quiet this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
