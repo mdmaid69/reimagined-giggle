43aa08bf9ae91df8c669459bb8f8af646824b7c8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:51 +0000

    snow pupil scrape gap regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
