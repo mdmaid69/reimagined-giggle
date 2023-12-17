fff9a6cb8376897f06d34f48b868ba950a2729c8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:13:53 +0000

    fear suggestion extra admire mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
