84e80da8e750215126e07cc6fae852786e26c934
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:35 +0000

    future deliberately coffee excuse story

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
