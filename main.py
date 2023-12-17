2b5dd4f4da2eb7d31cc11304205c4c798666c433
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:06 +0000

    tour reaction with claim especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
