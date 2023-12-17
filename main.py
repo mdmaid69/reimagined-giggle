7e99db9288b558e48f59d1a39a37c113e739fc72
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:57 +0000

    equal in sell natural during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
