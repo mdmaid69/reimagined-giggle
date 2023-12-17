f563ea992f6402384a5160fa46f5907bab61b577
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:20:09 +0000

    fly sting finger communication patience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
