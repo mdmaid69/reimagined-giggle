6ee7139213e4191fc492910c5631357631de7601
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:19 +0000

    pool use slightly supply slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
