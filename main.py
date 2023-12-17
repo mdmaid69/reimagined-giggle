9d562736df36329b7908b5bf2722ae4d02c97d7c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:04 +0000

    decision offer noone book high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
