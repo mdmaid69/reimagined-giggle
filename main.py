0bd2c53758e1bab6d087a7455bb5a2273e5cbcc7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:54 +0000

    plan change bill turn partner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
