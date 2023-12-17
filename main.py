d6cdac245de585051a36b95bcc04b49a78c998ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:10 +0000

    food safe confused plant healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
