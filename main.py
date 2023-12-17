2990a2e95e9596260671f946f9d45bf8f9549499
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:38 +0000

    look good though turnback tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
