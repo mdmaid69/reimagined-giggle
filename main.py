546117b65e670427d8a6a85a52c3e8d7bbc273a6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:13 +0000

    zero please ours manage print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
