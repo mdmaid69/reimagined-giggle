fbefa460e407121088eea6b63f9aefaa1e41298f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:55 +0000

    crowd attract offer bury shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def add_numbers(x, y):
+        return x + y
