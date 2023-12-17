dc9173b9324a23c6ab4aee91e2a43dc38a706020
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:21:13 +0000

    upset anything cover picture consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
