13fa35b084c4e200f5be847aee4e5118b6f8ebb5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:44 +0000

    something stop gentle repair 568energy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
