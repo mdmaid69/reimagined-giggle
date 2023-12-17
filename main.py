bc665fe8ff114007f003e82e4bebedfeed8a98f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:33 +0000

    answer dance shade vanish weather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
