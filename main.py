39849fab804410a917b5bdce11ce58cfe10eb717
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:37 +0000

    mean week door near law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+def square_number(x):
+        return x**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
