72eca8166b4a26e956efc05a289f6705f92192fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:54 +0000

    succeed fold small welcome stick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
