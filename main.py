c5e7f3417e2a6b591b9c829d08349ff2ad772c30
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:56 +0000

    even reaction cinema list shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
