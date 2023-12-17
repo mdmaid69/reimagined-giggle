92ac128dcc69699be7ff0dc0f26730728097f56e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:08 +0000

    move rain along dear day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import time
+print(time.time())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
