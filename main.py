38200b58f8eb046c5fe4d22476cfc33b5fc36639
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:31 +0000

    cool judge direct carry be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
