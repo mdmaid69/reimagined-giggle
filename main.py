1fdca7d516670052c654c1fc15840f693faabc6d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:01 +0000

    information clean deal hit load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
