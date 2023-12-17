af767814b4fc6a048af5095d368912427dae1503
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:50 +0000

    thing complicated and thank mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
