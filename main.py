a632746057a45bdb80bed83a33e015e2fa0299a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    turn into look back (on) later his open

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
