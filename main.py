0beae20d56ba18b5ce80262fbb1965a214a01df7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:57 +0000

    both bag influence weather modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import time
-print(time.time())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
