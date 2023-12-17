e106ff3a6472fb7a1f799d679a61df4911e027f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:06:00 +0000

    gift bin late more slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
