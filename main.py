fe9ac00143d7c10d2efd13fca022c1dc0bbd4bca
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:12 +0000

    charge loose set argue strike

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
