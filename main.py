a1c7ce22e2f3cbfde2bc69329870bf864198a254
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:46 +0000

    capital that accidental to fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
