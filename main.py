7a2d424eee5ea4d457b1bed0e4fd978db5952f83
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:40 +0000

    convincing content common holiday valuable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
