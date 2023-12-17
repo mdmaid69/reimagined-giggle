44034c441b27f7d5623d5242ddfef44ccdb51fa4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:42 +0000

    expect hurt with geton fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
