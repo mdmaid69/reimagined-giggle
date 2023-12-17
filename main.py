a8f0017156a4685e5eab71dfa4398664d0852c8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:36 +0000

    male surprised talkinto partner do

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_repr(array):
+        return repr(array)
