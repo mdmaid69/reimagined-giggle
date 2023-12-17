5df8906b4a65a8b76fabe3ece7607a2349a9fbf2
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:28:31 +0000

    getback unusual during OKorokay win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
