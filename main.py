5af21abac8e1026fa7c6030837ab863680cbaaf1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:04 +0000

    mistake swing dry compare ocean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
