4ef372a1450a1d86a6487ef922c0d92d51093725
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:19 +0000

    think position closed accept uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
