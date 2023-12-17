361e73e509a82a91d5f05f0145f7111749e70c4b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:16 +0000

    tight reason yesterday everybody pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_max(lst):
+        return max(lst)
