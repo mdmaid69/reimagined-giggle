2aac2f5cc5b613e3134fa7c3453ca2f5515fbebd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:01 +0000

    takeout scrap general when relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
