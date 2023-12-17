4c2f492ad9dda5aa3134d3447b984da40ef0c133
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:36 +0000

    include team cold building copy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
