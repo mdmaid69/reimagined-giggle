a6a58605edc349b9e6518977142e97e1961b4599
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:56:13 +0000

    back secret thinkthrough flow content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
