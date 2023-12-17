b852e959dc2efe6422757e8966ad389689a553ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:21 +0000

    professional instruction talkback involve comment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
