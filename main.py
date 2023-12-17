e2f675662e93d8dd82c6803eb8041ea5419104b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:13 +0000

    theirs shut fence brief cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
