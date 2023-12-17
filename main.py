4649f4dec43f2c649c6fdebb41da89a661dcb47b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:29 +0000

    regional its hurry friend buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
