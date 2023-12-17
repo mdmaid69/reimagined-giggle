6ab15ea1287ea12a23d033387c608de59daadc7f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:35 +0000

    failure worry flight remember teach

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
+print(sum(range(10)))
