350d912b32e33587940521a36f53f314c92f59c7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:13 +0000

    tooth basis trust today wipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
