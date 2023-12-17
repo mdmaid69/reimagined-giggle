9cc3a53d94152981ce385f3beb6f6a1339bb3571
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:41 +0000

    before action neck design live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
