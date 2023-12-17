b11ed51510c9ffef1e3f36e41e5068fc46496779
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:02 +0000

    trick system cold heart beginning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
