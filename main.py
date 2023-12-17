1f39f55d1e1f86746eab0cabf5fb7e5fb09dfa98
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:27 +0000

    know show clever smoke interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
