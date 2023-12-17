0afc32355861824d9a274d69ce4d5eb69db0d20b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:23 +0000

    solve cut result funny mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
