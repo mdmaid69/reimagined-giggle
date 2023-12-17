dacbf9ce71277902afd0926612e33b46c9b38b37
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:54 +0000

    tool trap benefit popular age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
