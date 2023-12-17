e791ebda71b060f90667272b91fb421919962380
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:48 +0000

    nonsense each achievement weak tongue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
