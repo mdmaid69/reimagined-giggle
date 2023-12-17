b0eb2f71b69462a06acc4f551df72b8171ddf057
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:10:55 +0000

    act equipment doubt obtain new

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_list(array):
-        return list(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
