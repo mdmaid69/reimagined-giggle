25c397810d058dde3bd1e1b63a6a3d852d863181
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:24 +0000

    bush well why considering nerve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
