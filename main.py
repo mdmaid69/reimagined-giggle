f2c03d8beef4700d739c92773de7d3ea3a655cfa
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:26 +0000

    turndown type brief little while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-from collections import Counter
-print(Counter("hello world"))
+def get_array_as_frozenset(array):
+        return frozenset(array)
