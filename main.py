be0ba41433d6b3899d1d01b711ef0007974fdb59
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:04 +0000

    thinkover telephone free no vote

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
