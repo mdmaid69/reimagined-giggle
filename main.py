3a4109ffac30d0d739182c4f8dc9c434f317bca8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:31:39 +0000

    announce connection pain dream crazy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
