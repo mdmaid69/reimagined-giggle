9b18c53ba9a42ad3a6c20032b176e62c771e9216
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:45 +0000

    furniture number careful difficult illness

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
