105f0ecb82685057c773d0f2627b3bbb29b5fb74
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:03 +0000

    plan degree wall lift wander

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+print("Hello, world!")
+import os
+def change_working_directory(path):
+        os.chdir(path)
