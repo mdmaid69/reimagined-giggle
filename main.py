a83bbc38d816a4411ea684d26edba53304bc0a28
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:33 +0000

    system turnon autumn then hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
