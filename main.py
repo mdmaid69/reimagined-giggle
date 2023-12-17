58cf7404d7d3ed797c951c89bd2ca8a520d8f927
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:52 +0000

    Cc bury hard entry plastic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def multiply_numbers(x, y):
-        return x * y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
