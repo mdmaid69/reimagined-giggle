19b7a625af33721615d0b707618879057af9936a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:39 +0000

    little wear getout touch demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
