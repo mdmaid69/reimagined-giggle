69f3f5b306d43658bbaee71d26c51791a44295f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:04 +0000

    burst mine really regularly concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import numpy as np
+print(np.array([1, 2, 3]))
