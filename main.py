d72a9663d07efe9cb0269f6f75ca481e6f3fa1e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:25 +0000

    present control speak escape fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
