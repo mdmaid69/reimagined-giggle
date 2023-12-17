175d75f9a5171cad60c7d0435ee8f1e5bf1ca751
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:06:48 +0000

    later environment street clear punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
