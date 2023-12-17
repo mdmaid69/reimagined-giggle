a936dd4db2c69675b2b407af3cd7502a2773f6ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:05 +0000

    somewhere key more miss fear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
