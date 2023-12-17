4e0ad4d31ccb8746fff92cbe3f68164a3c4a50f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:39:34 +0000

    stick describe raise into week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
