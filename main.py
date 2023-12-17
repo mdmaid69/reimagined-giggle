2be52228b2c240b8b82ac58f658e0319e648824f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:07 +0000

    fashion main if often cheat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
