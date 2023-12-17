017d79f2b3ea869e0ab275e7b2e2effdfa6ddc0f
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:38:48 +0000

    hide apologizeapologise hurt little represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
