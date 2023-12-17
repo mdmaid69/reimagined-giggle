f9dd3355e0d90912aa69d666cbbecbbca4807905
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:05 +0000

    substance mistake really swim 571enough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
