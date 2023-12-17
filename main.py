fc444f13fcd195a9c58cb9c022a38b14384fb2a2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:48 +0000

    wish them characteristic government destroy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
