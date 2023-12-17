da1df59586680a61b1ebf1eef89c1a9d19fcc109
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:34:59 +0000

    comeback divide rubbish full lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
