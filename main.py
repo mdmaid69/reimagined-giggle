937f7856d5f78fdecb24cb73be759806b7346db7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:09 +0000

    match challenge tip handle chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_pressure(force, area):
+        return force / area
