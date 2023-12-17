34fd3a007fc9e5064b83c7f6b378c40c69330707
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:32 +0000

    challenge careful possible talkback fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import numpy as np
+print(np.array([1, 2, 3]))
