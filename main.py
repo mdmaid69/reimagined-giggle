8819dc3a8cf8f8570092a2745da34a038d48f217
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:57 +0000

    extraordinary study patience blame luck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
