35be7451cb90f54b743ebc0481a36adb1b028e94
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:39:48 +0000

    doubt shout mix disturb someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
