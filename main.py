6ae5d55b8dc8420d0124a1736ed9c5467872b628
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:59 +0000

    start goup disagree neither outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
