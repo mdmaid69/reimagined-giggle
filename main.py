829287bd96aaffffba340c1d9d2298c5e83b0028
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:51 +0000

    start attractive ability will needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
