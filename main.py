c2a0725d64f2d07299bef1ef353d7b6b49bb9018
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:56:06 +0000

    comeout keepfrom thick lookafter rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
