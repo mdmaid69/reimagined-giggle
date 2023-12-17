7265a2220f8dad0def85aa962f155e0d8ac29ab2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:08:11 +0000

    add introduce quantity grade ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
