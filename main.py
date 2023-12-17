27b5eb977a59563f498ccc5e6762ee3bd4055ca3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:10 +0000

    brain ring connect wide habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
