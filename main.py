d4c136848a6a9d497c63731d18ce1e1c0efb28ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:01 +0000

    flat downstairs professional probably platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
