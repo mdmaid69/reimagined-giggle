418123e5f9a45fabf489ca254899af54a4829660
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:06 +0000

    believe loud talk Vv matter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
