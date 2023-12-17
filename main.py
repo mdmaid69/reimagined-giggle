6b6268766ee9348e2dd439f140eba5172dafa73e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:10 +0000

    cut above boil almost respect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def add_numbers(x, y):
+        return x + y
+  def convert_to_binary(n):
+        return bin(n)
