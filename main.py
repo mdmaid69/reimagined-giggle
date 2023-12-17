63dc8863cc6c8e9d26caad437a2fbfb5e8a74087
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:10 +0000

    edge search authority could care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
