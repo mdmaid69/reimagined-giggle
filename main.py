a2c7a2020337f8c61830e94ea595e9736fff3190
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:52:59 +0000

    nurse play process opponent tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+print(random.randint(0, 100))
