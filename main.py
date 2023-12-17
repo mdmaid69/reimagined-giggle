90e1198dba3d8dd2376fe7fbb611f3da8d1831db
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:42:07 +0000

    support genuine thick put stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
