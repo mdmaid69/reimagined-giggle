41c5bc9a1df8e554b188475f51cbd2f5e53d9532
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:07 +0000

    skin odd claim cost still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_float(array):
+        return float(array[0])
