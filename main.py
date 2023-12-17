5ff708e904e9b47766879ced97e4cb2df0b48c5d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:35 +0000

    touch wall drink worried strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
