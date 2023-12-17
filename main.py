8912878b3f395242a8975288e6ae277a1d5c851a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:05 +0000

    favourite word hurry Kk getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
