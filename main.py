0a3961eadb335903c60c53b3ac0f722b67dd98e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:48 +0000

    affect definite wire path chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
