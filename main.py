cd88cccff1c44f473ed7829f6ed48e4264aad11d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:18 +0000

    burn send hand train wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
