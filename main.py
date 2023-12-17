46d3ecfe99be300bf6e4074c42a519bca721bdc5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:25 +0000

    holdon birthday careful related region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
