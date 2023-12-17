2641d6d6e781432caa17b4a9bfda42cec903772b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:05 +0000

    talkoutof discovery character coffee nearly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
