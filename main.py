1af843ac8059713c1b25ad3b48441ab07163538e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    actual arrive sail laugh different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
