af4e1ca35a669715f7526d05fec970720fbf78d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:57 +0000

    talkback getout trick offer design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
