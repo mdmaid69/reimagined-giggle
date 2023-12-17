fe9819199fb5ab120b846dd2519b22ce68483f07
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:05 +0000

    develop ahead people immediate anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
