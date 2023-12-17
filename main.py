ad2e17c7460a1069b95931c9558aeaf79c3e10f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:24 +0000

    patient pour closed every month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
