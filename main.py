16095df3977cb87e2799d9fb43b379c2aeefe432
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:46 +0000

    person drive modern adjective part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
