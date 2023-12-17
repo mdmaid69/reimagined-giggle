8058fe5044bc21f59041a3b47a867d7a4f3675d0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:30:58 +0000

    sail sensitive progress satisfactory war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
