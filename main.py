59c558bc4841d3237faa2c5816cab1ad5ee04b1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:02 +0000

    group miss divide popular tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
