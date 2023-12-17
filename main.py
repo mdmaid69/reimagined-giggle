1215553cb231a867a201251a8575bc9b6c4b3a9c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:08:15 +0000

    fresh fix present hand off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
