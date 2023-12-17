cb2c32c6c794e739dc9b3633f8dc92c07c4ae474
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:48 +0000

    steam payment entrance correction advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
