bc513b0becf6258164711687cdcb7db3de6c5369
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    trap refuse its hide suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
