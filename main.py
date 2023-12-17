c3f654de901a8cdd51e28a4a9aebfe456771ebc0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:36 +0000

    callat pipe fond smart major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
