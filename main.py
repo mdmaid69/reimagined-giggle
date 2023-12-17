df96b1c81220f0affb6ba029d1e05cb984f63091
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:49 +0000

    apart branch strength high visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
