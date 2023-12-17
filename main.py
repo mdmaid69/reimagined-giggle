9f80031222aae676f979305073265589a77ac2f2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:57 +0000

    until nurse organize risk education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_density(mass, volume):
+        return mass / volume
