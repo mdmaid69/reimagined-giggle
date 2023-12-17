5dd4c4fbca54b4f8a87adb279571d42a01f248af
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:48:14 +0000

    flat body they broad building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
