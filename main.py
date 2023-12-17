a16a1898adfdc68a9c836f86adf648bab199ff12
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:46 +0000

    push above smooth level matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+print(os.getcwd())
