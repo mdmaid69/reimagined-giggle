a8fbe8e5e1fdf75087a8331254980a2cac29ebfc
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    form die 565. end crowd awake

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
