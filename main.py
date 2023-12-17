62ef810ed1a7a10c68cb53ab639c79a7d9495088
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:38 +0000

    disagree identify container turnaway whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
