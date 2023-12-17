2523223850f89d7e05fd7e4099e53883dcff77dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:28 +0000

    excellent open theirs education keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def find_min(lst):
+        return min(lst)
