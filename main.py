eae54d1991832782ce09d9eece2bc293129b3ff2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:15 +0000

    pretty embarrassed round perform only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
+def calculate_power(work, time):
+        return work / time
