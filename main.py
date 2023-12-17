384e3445445afd010739e35083df23fd883999e8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:17 +0000

    once how brown stranger wall

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_bool(array):
+        return bool(array)
