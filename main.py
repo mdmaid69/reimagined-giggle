b1bd3667d8e937460e507e8799cc8992429bbc12
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:39 +0000

    share takeover advanced frightened grow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
