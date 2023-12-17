314054aa2990d10e286c297272ccc7b3f8df95ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:35 +0000

    makeup accidentally relationship cover drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
