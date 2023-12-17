6a1b774dbfa3ccb29029b482cd20f91ba55c7c7a
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:37:32 +0000

    joke reduction takeoff demonstrate imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
