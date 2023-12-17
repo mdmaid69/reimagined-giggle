6cbe51c17249150bbfa493915acc10f568b83531
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:20:19 +0000

    attract quick either fact interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
