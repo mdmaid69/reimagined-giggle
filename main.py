81606ae173cff6d78ebd9fb83640b62deacf746c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:15:11 +0000

    underneath over evidence exaggerate build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
