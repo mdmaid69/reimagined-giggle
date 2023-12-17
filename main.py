1d80074a4943706efafb33fdbafa4126a474977f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:17 +0000

    right mouth hope peaceful likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
