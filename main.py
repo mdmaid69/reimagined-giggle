6616f44b3271414094d829d7a0a0f3abde0c880b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:42 +0000

    fine grand surface side doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
