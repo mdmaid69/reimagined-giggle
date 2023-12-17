3c4c4c55c8f65ae5d17fdcc13a41e96cf76735e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:48 +0000

    soak mention dark spring interest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
