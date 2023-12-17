9474d602bab5fcb3c54ee8ce9baeb8da14005596
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:15:24 +0000

    please snow load out step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
