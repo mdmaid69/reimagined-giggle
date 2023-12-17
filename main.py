4ad275e4794795f9b77f11e8a72564cfb72e15d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:08 +0000

    act advice instruction last discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_factorial(n):
+        return math.factorial(n)
