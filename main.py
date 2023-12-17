e38ddc7e89a4ddd6f7df93cc225b5daef5d4bd73
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:05 +0000

    mistake vanish score spread contact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import time
+print(time.time())
