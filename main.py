7d575337818d8693cc844f7b2f16a1969ac24bf2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:15 +0000

    to past burn Ee doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
