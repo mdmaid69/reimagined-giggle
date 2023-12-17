273df55ffc002ca9413087013e2b3098ed6d3bf5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:55 +0000

    disappear every secret replace cost

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
