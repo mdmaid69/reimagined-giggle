76236462d1699951625353a7d16fbfe6268d11e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:02 +0000

    allow spring post exactly order

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_area_rectangle(l, w):
+        return l * w
