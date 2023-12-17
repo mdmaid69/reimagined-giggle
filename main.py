54c2a9473cb21bbb7305f17477ce158a75d27f70
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:45 +0000

    disgusting hospital empty cheap cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def split_path(path):
+        return os.path.split(path)
