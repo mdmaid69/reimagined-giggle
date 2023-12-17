183c4a198fa1608165e62766cc8e404c57c54ce3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:14 +0000

    godown blind walk reduce yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
