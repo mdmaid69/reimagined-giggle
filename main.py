ecd54bd0838fdbd73e44eeeaf109546b1a69c163
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:05 +0000

    complete disappear Gg bury attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
