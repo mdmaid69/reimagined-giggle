3c16e48c1d2c3253ee7622e5489e41d3ee9c3dd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:14 +0000

    comeback street land emergency joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
