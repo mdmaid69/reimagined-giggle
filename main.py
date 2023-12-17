f58b3a20ac07de02927524d5b21f5b71df3d8f2f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:36:50 +0000

    happen store threaten sign honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
