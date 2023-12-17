6e40aeb03f22fd0c95c1d3178ab0f0b10ff1c993
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:18:33 +0000

    Yy address improve slice loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
