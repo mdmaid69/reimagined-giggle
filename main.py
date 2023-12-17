c160d9459534a78697c5348da96cc9fd13bded34
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:08 +0000

    shut unite neither makeup transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
