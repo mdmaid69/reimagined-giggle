a37e001db0cbb59b4ab2655524cf210ca482be37
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:43 +0000

    fasten world replace office open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
