6bb88bc33e94fce960f91d50e00306d5e1b3b4ca
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:02 +0000

    force congratulate circumstance owe callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
