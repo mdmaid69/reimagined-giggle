178f26f1c4a966e3704545e1692b3fb40a26c1dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:50:15 +0000

    tone Gg tread invite Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
