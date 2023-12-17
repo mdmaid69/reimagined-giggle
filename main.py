162fe6a0388fa9b476c10d106dfa2edfbb0ee167
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:26 +0000

    close piece exit national angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
