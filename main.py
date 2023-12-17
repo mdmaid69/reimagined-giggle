e7b266664730669218786bc42e4645fe0e5f8be9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:42 +0000

    manner our set bar position

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
