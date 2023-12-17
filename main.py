51e1c46a51d8119d27d58d2b5a15f4703309f309
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:09 +0000

    breakthrough race communicate detail level

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
