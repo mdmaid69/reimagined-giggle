49165e62e0164055eaf90e7c77ae67a64b406210
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:47 +0000

    music born food raise them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def cube_number(x):
+        return x**3
