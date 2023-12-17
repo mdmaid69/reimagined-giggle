f97550391190ca7dd8820fac38efa61154d5eb29
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:53 +0000

    stick left realize right open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
