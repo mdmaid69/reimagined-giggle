769f523084616e8f2b898dcf43b8f8688f416da8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:31 +0000

    side set answer effort silent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
