5e5cf3ea9f6235094f97b2b8e8f9e669740af358
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:09 +0000

    young sound slightly ask show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
