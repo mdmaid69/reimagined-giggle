a19929127b7ce59a37f7134f93bafec818617465
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:49:14 +0000

    respect disturbing steep mine concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
