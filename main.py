3ecc92c79714b6faa03c4cad96128679b287906a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:58:22 +0000

    snow sure guilty themselves month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_sine(x):
+        return math.sin(x)
