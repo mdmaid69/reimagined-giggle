a67d1924d60d43e4c9d11046b78bb32887b71ed9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:32 +0000

    otherwise shine destroy seat operate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
