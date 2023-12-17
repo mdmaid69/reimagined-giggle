8b963c3b78e89336d88d491dbb595cd881e6837f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:44 +0000

    wish gooff long star glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
