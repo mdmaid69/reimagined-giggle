43283812091ef128accebb79fc236d4d0d0bef88
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:36 +0000

    poor artificial job degree voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
