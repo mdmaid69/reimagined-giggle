21708a30c82996e9c8663d60e18d655b355b399b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:59 +0000

    urgent soak free sore smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def remove_directory(path):
+        os.rmdir(path)
