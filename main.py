d36bd8be5f573235b1ee8998eb5af6882bf2a329
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:15:09 +0000

    crazy Nn satisfied spoil workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
