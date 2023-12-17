7ba406b74dbd59c8e8c26acf1a915fbaa5152513
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:19:07 +0000

    right terrible watch accept aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def reverse_array(array):
+        array.reverse()
