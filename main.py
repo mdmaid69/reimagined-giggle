f3f94d1abf919c994ba3b592df884f1f3bb57b32
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:28 +0000

    each kick realize back turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
