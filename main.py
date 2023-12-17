823d4585c94254a9c162152b95ba61bcba420d40
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:04:01 +0000

    eager bound tape Pp most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
