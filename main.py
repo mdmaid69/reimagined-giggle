a60c593c3eb7f54d9de7ea1d0a1b58668eacc074
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:59:07 +0000

    that fast scream makeinto equal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
