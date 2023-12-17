87bcdcadcd1fb8067e6e424a2a1dc93100f3879d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:21 +0000

    protest sit difficult best shopping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
