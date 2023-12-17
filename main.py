11cc5618bd049b1f1151fc5fb8185d3abd97a128
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:43:37 +0000

    port relief error block introduction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
