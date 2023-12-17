a53bf5dfb19b0fb20995ac85ba6df7de0ba12f3b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:34:01 +0000

    police reasonable land listen for

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
