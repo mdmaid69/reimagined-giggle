a5b860bff5dabb58363c2b5583c8535b3ff22548
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:35 +0000

    special clever stir chin story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
