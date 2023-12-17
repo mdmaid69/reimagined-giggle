9e054492e7b6729912cc36dd6ccaf1bec8f067cd
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    wave speed appeal collection complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
