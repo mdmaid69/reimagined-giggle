94fe654076c87882e3ba351db9a074fbf0568b8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:53:11 +0000

    beside afford among reply short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+print(math.pi)
