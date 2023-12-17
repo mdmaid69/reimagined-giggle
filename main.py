b88af89c4a43d0b759c57c06204b90e5956faab0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:10 +0000

    shallow sign sensible improve then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
