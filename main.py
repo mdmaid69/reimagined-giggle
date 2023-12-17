093716e6a9529caa8cd1597db6d211de27f34d0b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:07 +0000

    difficult hour  year other

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
