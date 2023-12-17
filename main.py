1e00461abe75c78ff8c57ed71fa605d7be4d87d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:11 +0000

    performance especially hope everywhere old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
