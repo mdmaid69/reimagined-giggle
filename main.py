36be31cde9da3ec5303cefd1ea59b522aa0ead32
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:22 +0000

    instead accordingto sudden extra previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_max(numbers):
-        return max(numbers)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
