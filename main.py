60f877525f2276d63f0998a9e1ce51559c63b470
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:50 +0000

    dead in behind moon giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
