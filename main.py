199585d528ca89f599504a8e6d794e4c9b0f6502
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:41 +0000

    exactly cum condition getaway empty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
