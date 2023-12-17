0533c9c1526ef2751281d1f6c6f1d98bfd5c22f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:28 +0000

    this unite fetch probably snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
