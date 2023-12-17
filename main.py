c6bfb1e75b6618b0c3e62adcec39c5ce345bc116
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:48 +0000

    protest vanish accidental talk hit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
