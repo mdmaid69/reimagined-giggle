d00309013201ac19ca0db369faac68b06305ef46
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:45 +0000

    alone mistake mean lead Hh

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
