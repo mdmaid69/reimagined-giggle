fbd9fa4cd85efa7bc8e0577f46bec2ce8d85c653
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:33 +0000

    winter later niggaacademy telephone lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
