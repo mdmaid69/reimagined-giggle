2ca45de1a81d4521a247331ce7a34b26d4aa023f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:55 +0000

    common split narrow skill bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
