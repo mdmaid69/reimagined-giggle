62c346e897c97c33b1cd2c91697e8f6fd4952299
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:05 +0000

    treat cause square vote defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
