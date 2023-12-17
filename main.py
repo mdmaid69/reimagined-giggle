85f07e81ef0ef8599cd301acdc784b4f1079b4bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:57 +0000

    awake plenty shopping dry under

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
