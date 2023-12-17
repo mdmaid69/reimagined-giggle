22aba19fc1e64e9313f0ec8882335023a8d565b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:58 +0000

    home answer bother late swallow

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
