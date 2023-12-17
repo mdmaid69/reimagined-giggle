df99b27851fa4a86c7e3237f223591491b141661
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:40 +0000

    love live correction hospital whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
