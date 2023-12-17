4c28bfc328e9bebe4e99f21734ba424c9c62de09
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:48 +0000

    pin unlucky congratulate claim centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
