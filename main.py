5a3c7516a3ce9d84e574becfaff78cd302942b19
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:44 +0000

    again honest cross effort top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
