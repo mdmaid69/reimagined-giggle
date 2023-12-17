c32b662e8b9bd08b803baca39bfa9ba09446fc9c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:12 +0000

    close connected coffee angry telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def reverse_string(s):
+        return s[::-1]
