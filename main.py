bba78f1f73a4a6475b84c78ca65c83deca204b91
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:47 +0000

    hour aim interrupt subject probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def convert_to_binary(n):
+        return bin(n)
