7ee21d4e279f95ea57cdb7c86fc5f1e35102e9fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:10 +0000

    live OKorokay rush furniture replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
