3b15f41009da4f3bf5918bfba02fec0b226207be
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:38 +0000

    else face community pour grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def split_path(path):
+        return os.path.split(path)
