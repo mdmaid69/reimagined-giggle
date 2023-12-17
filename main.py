9193b914860f7af0c0ff79abf47b5d29c71a5e13
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:04:16 +0000

    difficulty somehow search skirt name

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
