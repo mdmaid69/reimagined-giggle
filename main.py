8b6fffd5b37c451da09dfcdcc6a14ee31c4d3b1b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:39 +0000

    I cover pause solve holiday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
