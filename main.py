95f2ccba448ca9211570cfaa1dc5c17f7297aa15
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:13 +0000

    566end increase object sleep term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+import array
+def clear_array(array):
+        array *= 0
