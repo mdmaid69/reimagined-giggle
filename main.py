8d5a2e9cbe7ace5adb9cf2ba95fbfc6b91f3d545
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:31 +0000

    major approval improvement delight near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
