a3be05843312231c18315c6f4bf35c533b087bca
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:01 +0000

    debt translate hour would Yy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
