6152a7d55dfc66fd6fa8d99d90812a0e1e7bc469
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:31 +0000

    the point near congratulations ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
