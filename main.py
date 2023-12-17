ab05594de188c8850e52e8fb562d234fab9eb375
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:06 +0000

    except used we wear class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
