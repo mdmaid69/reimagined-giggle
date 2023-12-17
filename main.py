4ac777e01f7660340c8b71f4dfa1660b55d7c35c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:01:51 +0000

    feed soil question Ii company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
