4d65fa2dffa15a5f4934a64aa69506e082a074ea
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:37 +0000

    contents pride shake get impatient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
