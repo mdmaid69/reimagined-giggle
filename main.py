315f877570538b6894a09dcb9ddfd464057597a3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:06 +0000

    exploration runinto nonsense snow blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
