ede7ca05a77ead7a3da011d4531256023503245e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:30 +0000

    moon remark suit cut Zz

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
