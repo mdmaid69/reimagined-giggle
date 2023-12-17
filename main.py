ef433f023990c1a941d28a62bedec78c576390e8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:01 +0000

    right bye music sail official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
