24109a2dae692a39a32a70da9a83360ff3f74a98
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:30 +0000

    holdon over under feed something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
