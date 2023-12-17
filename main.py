ec0049b032164d3a399fdc2147b85e2929b49e8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:49 +0000

    mark would bet runover ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import random
-print(random.randint(0, 100))
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
