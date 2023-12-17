804d6ea37f25d57b0cc160370a02ecde32941a80
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:42:15 +0000

    divide trip flow godown announce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
