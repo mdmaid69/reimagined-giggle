ce5a5c711d81cc2858a908957f69453c491b8b30
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:47 +0000

    back centimetre hello company clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
