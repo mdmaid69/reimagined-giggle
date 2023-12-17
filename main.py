b052293520858c1251a53d32d3d3d04e8bcc8749
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:34 +0000

    long plain wonder create easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
