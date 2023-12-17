833af46b2eafe588891d34b70b722d11b7855110
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:16 +0000

    congratulations earth cover base fly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pressure(force, area):
+        return force / area
