cea5d76f5ae3fccd5b49ba796dfef69899727afa
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:11:15 +0000

    let spot mix glass runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import logging
+def log_message(message):
+        logging.info(message)
