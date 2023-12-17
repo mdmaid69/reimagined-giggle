33c1a610b6563e47cb86c9288b15c5a61a2df6cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:36 +0000

    buy pot survive window anything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
