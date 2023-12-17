d7f702860f04e00c70c39e37f3d99a5c89ad5d8d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:37 +0000

    replace there race roll no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
 import array
-def get_array_as_set(array):
-        return set(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
