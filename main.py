d2b934156e3071f67120d9baac261578df95288e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:00 +0000

    in bunch shoot cheat line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_set(array):
+        return set(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
