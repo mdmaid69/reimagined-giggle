f6b68688bbfce020e91503898a70072f68b3e644
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:01:31 +0000

    twist operate design value content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
