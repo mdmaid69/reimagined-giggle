846ce86fb7ddee73eea9e247ef8a80d65f542204
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:19:37 +0000

    mainly something many bother then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
