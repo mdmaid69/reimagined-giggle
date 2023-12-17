f6b2f77c3e6831bbb9da77796e05aa39c5ad3543
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:05:29 +0000

    dance but attract 567enemy stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
