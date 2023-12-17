0cefcd27135dc8fbbf27dbd1b37e7ff7450a04f8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:25 +0000

    borrow pinch hello average practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+from collections import Counter
+print(Counter("hello world"))
