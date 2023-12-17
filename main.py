9499e8bb7002022600a4c969b9223f6861194893
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:10 +0000

    use whichever share pattern frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
