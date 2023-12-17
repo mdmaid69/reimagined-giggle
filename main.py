ce6a064643075d61d67b2e8b57fcb018636b20a3
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:10 +0000

    central string threaten whichever gate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
