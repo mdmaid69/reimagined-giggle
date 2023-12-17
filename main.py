bcb0b0395ed666c44b1f7caa588ec60a24a662af
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:16 +0000

    willing still shallow general lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
