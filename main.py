b0939bb30491c265d0a899879439cb463bde6a29
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:27:37 +0000

    coffee solve his wish rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
+def add_numbers(a, b):
+        return a + b
