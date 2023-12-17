701f6bc0ea10e7e135a3e6c54e180a2ce1926620
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:01 +0000

    whom season steep harm bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def square_number(x):
-        return x**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
