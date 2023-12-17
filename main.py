8bfa99011714e8f68fae3dc665ed7b5ea91637af
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:40 +0000

    order direct smart example thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def remove_from_array(array, item):
+        array.remove(item)
