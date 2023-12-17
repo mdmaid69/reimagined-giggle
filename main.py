0b0dd434ed761b45710140ccf20478e7d1085e0a
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:16:04 +0000

    brief bringup practice wherever mistake

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
