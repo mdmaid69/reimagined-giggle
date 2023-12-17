f6588e148a38d8f16d89410eca81047d2f83c7e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:49 +0000

    fly spring takeover world unlikely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
