1b3f3e1386d3c5a8e10c19f9ba30105b56c36fb0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:40 +0000

    nigga develop different opposite together

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+def print_python_version():
+        return sys.version
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
