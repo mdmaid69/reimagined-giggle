6c82c3fcf9c17eec0b607d5ae552a962d6e7307c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:41 +0000

    eat improve private none trick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
