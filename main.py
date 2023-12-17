d9524021c69c0d8da129e3a3c535bf5c39c99eb5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:45 +0000

    famous spray communication basic power

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
