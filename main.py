a0853da98e057a7baa53077edf34ec3c7161485e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:04:25 +0000

    keepfrom takeoff waste fresh one

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
