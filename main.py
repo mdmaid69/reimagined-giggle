2b3f028da1e1d1d27a4308f410ddd7f52a5c5cd8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:38:53 +0000

    transparent male fortunate can home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
+  def sort_list(lst):
+        return sorted(lst)
