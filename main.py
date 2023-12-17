e020e5cce7e896d48c4c43b8893a0e0a7da61f09
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:18 +0000

    better pollution happen Ss normal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
