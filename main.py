c388d525109abf7c42868302c9f1f67764252f03
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:20 +0000

    treat 566end you 573entertainment bear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
