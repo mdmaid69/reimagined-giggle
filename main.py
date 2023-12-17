ee9ed4bf04dd9523aa32d879a9532de3ecc29079
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:23:23 +0000

    bored pay grand 571enough claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_min(numbers):
-        return min(numbers)
+  def count_elements(lst):
+        return len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
