096effcbfc8b24fa52d0ce25b46fc0cc6ebadbd7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:03 +0000

    experience year call size nerve

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
