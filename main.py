acd602743b4a9cead42b66d012b92607aced5dcc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:17 +0000

    recovery television pump petrol solve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_max(numbers):
+        return max(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
