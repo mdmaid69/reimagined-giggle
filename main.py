f59589fe15b5058e2c8ee2f22c6a914ded6b5f4c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:20:41 +0000

    pipe along cruelty walk among

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
