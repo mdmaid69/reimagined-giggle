ee3bde81eb6a20757efb0d82bf89b7cbd7d4214d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:50:42 +0000

    no schedule accidentally convinced afford

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
