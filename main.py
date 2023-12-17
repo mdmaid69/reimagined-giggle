8d73bb838816ca2338992dbc811535336cf7e0b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:07 +0000

    share burst beside ring show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def remove_duplicates(lst):
+        return list(set(lst))
