eba252995defe5396b67b152ab24d06f06d9f4ae
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:06:35 +0000

    operate grow hollow home buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
