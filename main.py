134222a11822219028c68f870ba2262e17cfa33b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:16 +0000

    thankyou stupid study visit escape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def remove_from_array(array, item):
+        array.remove(item)
