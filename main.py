a863e53c9484ec33de4a5328409201fd74002dc4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:51:04 +0000

    centimetre history change habit stare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sys
+  def get_python_version():
+        return sys.version
+import json
+print(json.dumps({"name": "John", "age": 30}))
