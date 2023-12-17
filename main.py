09f873f5a6188f91e4f7f14dc70b710008c9fe68
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    talk out of way blame identify fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sys
+  def get_python_version():
+        return sys.version
