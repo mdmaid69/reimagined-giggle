c1334bd8f5c1285cb7a0f1140a458871a6cd5bed
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:51 +0000

    on cross stop find way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
