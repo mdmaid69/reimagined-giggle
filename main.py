c7fb2c18f6dd577ece41730f6d1a3e4b60eeb81d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:10:06 +0000

    although manner at bound first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
