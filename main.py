5090e4bbc4cef152bafd090ea27f75a9967c00b1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:23 +0000

    brave 565end protection mean sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
