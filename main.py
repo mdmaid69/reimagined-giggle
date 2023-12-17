fe85a8bff4ac2d38ee92bf5fc95f01661dbf7325
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:26:17 +0000

    distance forever modern friendly slice

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
