818d3f35521318cd1f544ab974ce40a39766cb7d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:19 +0000

    deep we attempt condition say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
