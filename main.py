c0925116ea867e1846add40e0a3365e035ca0104
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:46:35 +0000

    relation somehow anywhere arrive add

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_even(n):
+        return n % 2 == 0
+def add_numbers(a, b):
+        return a + b
