1f2fc94894f99478dc37df1a1ed33d5d99e22c75
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:57 +0000

    pronunciation uncomfortable pass expensive deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def delete_file(file_name):
+        os.remove(file_name)
