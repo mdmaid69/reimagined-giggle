3c2e386eb7192a7bdf2dab4fc7b761ac3512319c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:58 +0000

    habit talk fold reject reliable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def square_number(x):
+        return x**2
