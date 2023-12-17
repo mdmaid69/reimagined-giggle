2c501630f58b9fdf16127ecb6878b79ab350b66b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:21 +0000

    influence stupid fold film empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
