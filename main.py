f877fc638253b66d82a0b3055c6e41a8ac9f747d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:00 +0000

    test apologizeapologise come train prize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
