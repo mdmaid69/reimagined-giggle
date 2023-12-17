2b860bfbdeab85c18d37ab1e0b68ba487f54977b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:14 +0000

    no eat conversation board answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
