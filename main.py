ca20cb5526360b19b1371a5928bdd2b18d2cb7e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:06 +0000

    new surface that winter responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def get_time_since_epoch():
+        return time.time()
