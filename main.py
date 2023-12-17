4c35f79b4ebaf48165dffe41826de431e68ac36e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:26:46 +0000

    try pay jump duty performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
