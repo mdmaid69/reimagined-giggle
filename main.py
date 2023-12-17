537971aa7855604a2f80e9c08dd099d8a2377849
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:51:39 +0000

    object exaggerate suddenly find present

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_density(mass, volume):
+        return mass / volume
