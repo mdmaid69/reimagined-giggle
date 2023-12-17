1a09d9e7dc3ac552d7867d856753563ea5567aba
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:18:39 +0000

    seat nervous measure convinced soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
