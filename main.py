ee5b3b2d154b9f8db04d3b93b766d5987d119fbd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:34 +0000

    official thumb scrape scene image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
