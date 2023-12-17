03c8e6d06fa18ccbce54183c8f3a916929d0ebdf
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:56:01 +0000

    card doubt mess face buy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
