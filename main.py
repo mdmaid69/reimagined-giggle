13c95c3a28d17000cb6bfed8b860e4079576f0cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:59:16 +0000

    city opinion practical lower yesterday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
