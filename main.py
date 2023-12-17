b332ae95dba3618d93746e1fdf709d2d9b05fb38
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:53:54 +0000

    difficulty pair crowded everywhere storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_today_date():
+        return datetime.date.today()
