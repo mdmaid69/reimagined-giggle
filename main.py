8610416a3839e13745033359c1b26fcbf3ce2520
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:23:52 +0000

    deal congratulate exciting separate miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
