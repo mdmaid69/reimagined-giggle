ac98aee7469a7c2bca0b2bb475d49abd222ba26d
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:43:30 +0000

    compare brush spin tight shade

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Words:", len(text.split()))
