9830fb14fe3be22a484eb6a2dd5cc1fe394af838
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:01:11 +0000

    catch discuss precise look grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def roll_die():
+        return random.randint(1, 6)
