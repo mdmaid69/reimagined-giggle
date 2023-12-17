698743b92f6cc9dfc2b50a0536606a2a8517410a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:02 +0000

    emphasize move ring around face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def reverse_array(array):
+        array.reverse()
+def calculate_acceleration(speed, time):
+        return speed / time
