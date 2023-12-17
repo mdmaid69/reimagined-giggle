dab2c3b5a9895693e7b82e10664cf25ab4237471
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:16 +0000

    fit cheat clean further goodbye

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
