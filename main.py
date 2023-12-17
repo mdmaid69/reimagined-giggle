2a018215eaaf399e2429bba5aa0f9c2d8420781b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:01 +0000

    board together lesson who often

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
