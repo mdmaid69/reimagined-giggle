005c8ee8423e5c77870c25e5d4323a6404452fb7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:02 +0000

    interesting other situation dare content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
