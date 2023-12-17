a2a4880dc7d6a811494d5129c05dee25f7403da5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:51 +0000

    570enjoyable fall trip hello wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
