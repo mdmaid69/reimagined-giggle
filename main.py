4a9f9f23af9cbabb910750811723670f2d7190bf
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:14:48 +0000

    warn invite appeal squeeze earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
