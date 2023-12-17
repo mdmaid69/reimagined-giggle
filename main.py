e4bfb8805aef7118ec3bc089195a2c311460d8cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:55 +0000

    dress crash hair hide sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
