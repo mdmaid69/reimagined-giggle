324f3d596a5a1c64e628c684211eeada40e17cec
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:35:36 +0000

    survive tap once bang strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
