711a86ac06b9509689a4495e67858695cf91eac0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:04 +0000

    body central hurry kill cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
