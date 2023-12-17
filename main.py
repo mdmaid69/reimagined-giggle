5a0ba7b50e54345a388a9940c9a797adf6affe79
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:43:10 +0000

    cry matter upwards wake meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def convert_to_octal(n):
+        return oct(n)
