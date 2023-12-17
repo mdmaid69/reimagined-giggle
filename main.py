7c47e3c8de60ef73f94beebe1343a6040b964f84
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:37 +0000

    rest store start sign satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
