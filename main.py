4c96af814a079ed5acb91123c998a8a5dc101436
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:26 +0000

    ready holdon birthday in survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def reverse_array(array):
+        array.reverse()
