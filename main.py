f432193cfb2d1fef6ba18268f0a60064a956c2db
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:25 +0000

    hers total feature lookup contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
