59c5f2d5142f824659ac26ce5de565f1d68b9b96
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:29:49 +0000

    short quit far heart quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
