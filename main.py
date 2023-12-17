d81eaba743211f7adabfebbe6e4c99ac5537194d
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:12 +0000

    frightened government  direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
