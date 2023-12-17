8b7f291a21e09653d2db539b4b122392f7976df9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:55 +0000

    any dig calmly further medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
