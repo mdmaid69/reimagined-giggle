f6fa174a1ef15cbcc056e5aefad5b7663686b2a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:06 +0000

    post dream shut peace stop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_str(array):
+        return str(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
