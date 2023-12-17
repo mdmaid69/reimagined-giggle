c8d306317b9c62e9ae1f95ef02e49e16e420058f
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:11 +0000

    ordinary flavour but squash 570. enjoyable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
