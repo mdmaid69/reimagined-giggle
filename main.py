c8384a9de60021fe7d70053855dc1fda5c5cc10e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:57 +0000

    arrangement public possibility well cross

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
