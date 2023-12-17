61f6216efa2054268f84faa8ec3830b88e60446d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:33 +0000

    minority something put car mine

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
