001d540838ae58401977c04e9574c435edddbf38
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:09 +0000

    every punish mass Nn taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
