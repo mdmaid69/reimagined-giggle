8a6d772d9e607c55267085e5e11a079a700c51d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:23 +0000

    delight lucky start sometimes stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
