0f714548703cdf79fc18a4dc038cee7de501da7f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:13 +0000

    walk draw tidy black connection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
