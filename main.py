f61e9a5c8be50cebad7a8354c635c83e4c4f52fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:30 +0000

    naturally reserve breakinto tragedy mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
