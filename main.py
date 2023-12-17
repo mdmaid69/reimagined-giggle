6b5c5ac938956af8d411e5eb23d7036747d0bac5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:58 +0000

    think major disagree watch pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def count_elements(lst):
-        return len(lst)
+def convert_to_hex(n):
+        return hex(n)
+print([x**2 for x in range(10)])
