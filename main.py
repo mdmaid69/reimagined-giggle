b2ece3813f04a815be6429a07c9b2fdbc2bf458d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:58:11 +0000

    sentence scream among bother custom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
