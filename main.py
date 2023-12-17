d2e732259ba36711b6e4ff651c5a04b7e616c7ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:36 +0000

    rush sort will our we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
