771ca4d2e01fcf5285adfcec0b5ca2572f21f94c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:11 +0000

    inside example respect slowly rate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
