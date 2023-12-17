2283a8d197086d263beaf1437a3cbb269a5ab4d0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:06 +0000

    spring government file surprising add

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def cube_number(x):
-        return x**3
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
