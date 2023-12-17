7d8e551fee2575517af4a072a0dcea36add33fb7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:17 +0000

    material plain careful not point

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
