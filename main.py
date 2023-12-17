6e8ba28f21ea7beedbb6f78b8e97f421534058f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:28:31 +0000

    herself target travel holdon breakinto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-print([x**2 for x in range(10)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
