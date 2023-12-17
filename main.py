9f60e18f839bf11111f76b0662e9a505a0f894f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:10 +0000

    rely spoil bin usual track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
