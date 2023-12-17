061ee93b77179292f4ed1f88192308f718cd1f2c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:30:46 +0000

    read getoff length likely somehow

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
