6c70b76996535b7bc9460289c2cd8c5aa648bbaf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:57 +0000

    front ticket coat left true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
