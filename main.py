e2ab94871de44e4911049737f1e5b0d2f3f723ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:50 +0000

    lose sudden spread form along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def set_array_item(array, i, item):
+        array[i] = item
