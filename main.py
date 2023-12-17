f5947466395272e70032279a0070e791bbb67198
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:18 +0000

    replace deserve classroom test quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
