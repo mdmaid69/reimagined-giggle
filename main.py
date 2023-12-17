726eec33f9ef9ef46b2c02e7d4dcfc0ad7a648f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:47:58 +0000

    train path fairly dress cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
