59d0d2d7c46b5a2a39eb56921384d846d67bc7c7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:02 +0000

    cover giveback peace wonder set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
