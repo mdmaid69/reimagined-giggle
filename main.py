5b7be31fcd9d7e213f0b8a40381de7a08a273326
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:13:17 +0000

    relax family foreign comedown consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
