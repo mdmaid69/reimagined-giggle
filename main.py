37f44916d5a878c13ead4b059dcf58cb839aba31
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:00 +0000

    opponent raise rate storm plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
