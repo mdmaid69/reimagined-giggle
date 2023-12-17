ae049dffaf454d2999984ae87b325f433f2479d5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:31:12 +0000

    hurry lookback(on) generous towards neat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
 def add_numbers(a, b):
         return a + b
