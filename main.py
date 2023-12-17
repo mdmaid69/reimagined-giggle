6b037db7dfd2ba3ebde5653d0e5af011521de70b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:20:25 +0000

    workon steady strike experience spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
