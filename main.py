ee594a67f0cf4d330e4e001a976530a27a9d6560
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:34 +0000

    wealth bunch nor seriously payment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
