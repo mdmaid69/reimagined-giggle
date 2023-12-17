9e730115e3cb7168fc6c9efb40dd4725e190d197
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:55 +0000

    correct think intelligent movement sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
