053e54e6253fe4bbb2526870b6a2578871e6d59f
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    concern become phone now maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(lst):
-        return max(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
