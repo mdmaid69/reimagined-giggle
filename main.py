b3135a34d11c1dfbe4dff45a969fafcbed21cc4d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:10:20 +0000

    aim visit immediately hurt consider

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
