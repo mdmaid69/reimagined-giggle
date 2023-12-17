b59cf752543a3a574f52b3b0bed942abde5d7bbe
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:29 +0000

    as breakinto oil party soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
