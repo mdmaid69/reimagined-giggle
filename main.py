b08c3456fc999f91c981abf7655529044fb8ea77
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:16 +0000

    away comeover luck flat deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
