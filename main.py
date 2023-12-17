6f1f91504cd0263adc607cec2691377cb79f4cb8
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    strong hurt doctor stop load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import sys
-def exit_program():
-        sys.exit()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
