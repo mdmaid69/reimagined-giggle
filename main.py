df04d351791006a26944807d78216c00f575db5f
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:10:00 +0000

    weak cope fine abroad use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
