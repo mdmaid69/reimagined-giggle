f68c38ceb0e0700550dc2723bda6b9b900e3ed9f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:13 +0000

    bound justice form count floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
