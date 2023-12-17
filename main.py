401ffdc81e83f5f0e3e02234e4df81c3d19c57ef
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    surprise embarrassed their  add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
