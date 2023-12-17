25558d9bc1513739b0e0a3ffa6c9115af76da071
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:31 +0000

    fall partner combination welcome smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
