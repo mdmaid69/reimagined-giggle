def6774c239a612105db4585e733a4eb952e0335
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:34 +0000

    be part over official last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
 def calculate_power(base, exponent):
         return math.pow(base, exponent)
