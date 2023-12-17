26402c92eb093efdafb2f2dc36d039a25e71cb05
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:03 +0000

    nose people steal understand will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def cube_number(x):
+        return x**3
