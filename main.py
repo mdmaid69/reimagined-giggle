3dcf8fb6d9b3579f734f0ac8cb3d90be8cce37ac
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:29 +0000

    completely thumb dig file somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def reverse_list(lst):
+        return lst[::-1]
