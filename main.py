79a4c5993650be33b8ad7e1924f7c1dcdb920ca9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:21:49 +0000

    fresh dinner charge unpleasant sign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
