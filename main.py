2518e56c9472bfda57c896424f491719073e6f71
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:22 +0000

    will decide distance business Qq

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
