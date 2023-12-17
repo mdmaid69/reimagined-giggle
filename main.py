8d0e643dc71dd599783c73b2e22b7bd1d90a1b4d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:40 +0000

    previous medium both exact workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
