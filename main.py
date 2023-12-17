654ce95821704e09d22a972437873b88f008f7f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:55:30 +0000

    improvement turnout respect loud border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
