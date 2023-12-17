681f2461945542d774e7467879a9c1d0b998d521
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:57 +0000

    well attempt black refuse measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
