4c72de30c8404fe977915e6f60e93ccf9c7cb8c8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:07 +0000

    welcome full discuss react its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_priority_queue():
+        return collections.deque()
