2fe8718d3325803bac1ec671291e59ad11d30233
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:31:48 +0000

    command gift rush business trade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
