45ddc564429fe34ad2d569c0da568bebccbbe067
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:36:17 +0000

    organize past walk expensive gas

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
   def get_environment_variable(var_name):
         return os.getenv(var_name)
