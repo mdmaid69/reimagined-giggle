828c2d06e78a79fad7c5fded7be2229098be8ebe
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:10 +0000

    great emphasize when wherever different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
