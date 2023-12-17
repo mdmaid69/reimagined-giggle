2d4ab6dbc9d6de111a41b7a549706d83d850fa38
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:56 +0000

    actually pretty trick do model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+        return math.factorial(n)
