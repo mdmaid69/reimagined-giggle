000b99047bba7078cb4401c524cfbffb1979a64c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:04:16 +0000

    spread least decision watch cause

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
