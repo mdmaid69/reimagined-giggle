9e0219320bbf1a23cd293406ceb61f1618189c44
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:19 +0000

    once comeback fairly no demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
