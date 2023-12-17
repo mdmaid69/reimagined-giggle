deefafe4e4a68850875729c11fe4700e2796a564
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:15 +0000

    consider after that luck everyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
