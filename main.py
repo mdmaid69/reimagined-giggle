8163fbcf0896a00487b82fc5779288b311e519ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:39 +0000

    573entertainment proof exist runover thinkof

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-for i in range(10): print(i)
+  def is_even(n):
+        return n % 2 == 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
