896dad23e17d25764971489ec5d8d5da8d01f859
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:26 +0000

    right city challenge circle condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
