b1d979a73cf98517c0e25259edd373e115aaf8b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:24 +0000

    keepoff present plan whistle shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
