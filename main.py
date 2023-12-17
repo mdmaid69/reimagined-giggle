9209fb424764001195ec1ed77614b9a0703be830
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:39 +0000

    recent within lean eat pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_user_dict():
+        return collections.UserDict()
