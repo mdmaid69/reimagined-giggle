afabcc247526758c09732e77b81099099639a351
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:11 +0000

    steer middle late careful quarter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
