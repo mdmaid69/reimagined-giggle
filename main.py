839ac0bc3d409586ed831c79bb09a3cbaacf8d99
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:08:18 +0000

    arrive strange 573entertainment not position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
