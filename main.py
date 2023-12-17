1b2653d9c3dc28de3eddfa5caee86bce70b2b745
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:20:15 +0000

    tone one effort sick hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
