eb6a4863e09c9aa0ad06dbaef89be56844071f14
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:47 +0000

    mine slice cheek disapprove walk

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def is_odd(n):
-        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
