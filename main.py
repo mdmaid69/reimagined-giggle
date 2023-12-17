bb8afa583c913506e1fd2bbcb26ad217d5f59ee8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:37 +0000

    play tax dinner less right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
