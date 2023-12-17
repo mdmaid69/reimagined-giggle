4bb8a67e459f707affebaca40f89340e7fee7d87
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:18 +0000

    record pen surprise frame phone

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
