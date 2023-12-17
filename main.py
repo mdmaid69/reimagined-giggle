36a8c1414cfc1ae7cc0f2017298fdd8dac72ac9b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:09 +0000

    fasten substance principle sink agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
