f020605e8601c588d21f8b8c3f400cffcb8f9099
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:13 +0000

    wash trap love who refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_repr(array):
+        return repr(array)
