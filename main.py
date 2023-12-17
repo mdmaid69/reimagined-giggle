9c880ef447a57f7ddb9b050c902193baf445cba6
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:36:03 +0000

    flavour strange fence doubt turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
