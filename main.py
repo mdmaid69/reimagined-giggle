e697ec5d12ddd0872ef969dbbc2f0c8e14ef0aea
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:42:21 +0000

    fly fierce whole line offer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
