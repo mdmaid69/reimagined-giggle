5d30f9863109b6a3c9a56b8f182b3b7d3e076576
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:45 +0000

    model drink trap pile environment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
