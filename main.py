6c6033550c947859d4c162e61014532fa816a3a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:45:37 +0000

    emphasis charge method generally favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
