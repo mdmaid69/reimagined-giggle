69d3b3a8d4bb2d020f09722880753871ce880044
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:37 +0000

    their base greeting receive quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
