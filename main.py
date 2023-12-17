0078d527f96b971180f9bf8cfe9724a8d37a1f66
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:04 +0000

    now preparation explanation soon check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
