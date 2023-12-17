34e7a2b8e182fe5be4ae58816d4c954b0a60577c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:31 +0000

    majority prove job represent shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
