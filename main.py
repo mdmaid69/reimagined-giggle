213654c5a1c0d44209bbdf8ffc3a9de336265b12
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:58 +0000

    left they event fire phone

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
