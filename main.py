c6f73bee3201c36d01c300e9c52a7095c7bd6e12
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:36 +0000

    turnaway may mouth vision offend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def convert_to_binary(n):
-        return bin(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def get_time_since_epoch():
+        return time.time()
