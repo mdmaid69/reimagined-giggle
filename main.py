5490d2e4e96f81473d8d35519a2d0112f6eeb3fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:58 +0000

    survive aim benefit welcome original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
