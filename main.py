fa8bfa4503eb44e9ab7c19cd9cfebba57be679ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:45 +0000

    concern language old chase 566end

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
