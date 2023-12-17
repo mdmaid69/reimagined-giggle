c6ce22e99c40281813c5dd6c27307c94e2c34b4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:08 +0000

    benefit practice beat concerned gooff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
