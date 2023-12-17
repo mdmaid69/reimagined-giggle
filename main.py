5a5b9534665c823bb2e47b17f454af10f655b798
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:02 +0000

    odd rain so special careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
