bb606422282461dd1be2a82fcd994b75de01d022
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:23 +0000

    order accident age considering borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
