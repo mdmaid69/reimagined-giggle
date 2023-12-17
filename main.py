e7a107dd301c1eea51cc76fe42813c00d759979d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:24 +0000

    but present cheque measure left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
