9f885e9ce6e34090d248fe8a4218df9691d82c60
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:32 +0000

    accept surprising whatever concern change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
