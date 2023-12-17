607f225663151621dcc27a75ffc406095f5066cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:17:17 +0000

    comeover ago comeup direction care

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def count_elements(lst):
-        return len(lst)
+print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
