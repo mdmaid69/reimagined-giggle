580aaa16d5006d0da8b95810562a99d9003ed2bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:07 +0000

    confuse hand gentle human rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
