c4b7e3e3ddcbc8894d5f7045fdff3af064ac636a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:55 +0000

    answer murder show bit call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
