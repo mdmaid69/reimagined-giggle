f3a12d7b2c982100c799e2b39bef4f6da46e4026
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:25 +0000

    vanish hour cruelty by search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
