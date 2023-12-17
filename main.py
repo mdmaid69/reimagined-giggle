7d55b222e4c18812490c119ea9dab623b82f4a5d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:00 +0000

    turn dream accordingto sell share

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
