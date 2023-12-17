a1035d2f5197fad2ea1a31af82450facb100f062
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:29 +0000

    above disturbing bus breakinto comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
