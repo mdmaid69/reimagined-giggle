7eea0cb1e6565eef23bd276320954e4b34fa9cd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:33 +0000

    pull disapprove possibility central seriously

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
