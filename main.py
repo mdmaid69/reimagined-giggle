cc95c395dcc455c152a73f087bae279226a0ea23
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:53 +0000

    steam watch bad soon club

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
