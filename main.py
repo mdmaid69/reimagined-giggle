0895a3ec48776d8ebfe2ae6e382f7d3c87466af9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:15 +0000

    pain common frighten experiment face

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
