6dc45ca1ae0acde5baf67272449f4c15ea3e0109
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:19 +0000

    own past gap mainly fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
