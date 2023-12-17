84b6a9a005bdc9aa60f48cac09541485263db895
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:24 +0000

    common level talk boil call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
