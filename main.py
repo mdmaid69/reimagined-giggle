2f39edf5544dddfe80a1fe0cfdc84a0917e97cc6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:21 +0000

    influence firm blind behind rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
