138c050efb00849bd01533fb1759de30bb75f049
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:56 +0000

    skin laugh beat might relationship

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
