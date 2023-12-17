77fbec69e11261d1b2d01553a5624902af2e751b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:55 +0000

    book study avoid top takeback

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
