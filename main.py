c554fd4803b681b5c2f12c6725a69902ea8eda8a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:36 +0000

    regular comment himself rent sensible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_power(work, time):
+        return work / time
