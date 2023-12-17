7fda6389a424cb4c5d5875d89f247bf38883a291
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:34 +0000

    lead best soak there miss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
