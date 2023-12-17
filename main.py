8d2f464ce95fed3cd73022478a3fef4f25eeb212
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:48 +0000

    mark open that back geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
