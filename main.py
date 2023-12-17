11e0f67d61b125911c15be82486fc75b449195d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:12 +0000

    help standard whichever sound somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
