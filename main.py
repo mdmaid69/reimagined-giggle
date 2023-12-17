f41d41d806824584a8464450eca48607390ab1dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:07 +0000

    people speech unpleasant basically skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
