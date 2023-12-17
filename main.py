ae69ccad9ee9f7eca0195052db55560cf9fa653c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:09:30 +0000

    police prepare bye doubt how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
