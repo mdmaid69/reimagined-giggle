2fd51c2e384d8d98d1f5572d10ee8b9bcc9d452d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:15 +0000

    autumn trouble accidentally breath transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import numpy as np
+print(np.array([1, 2, 3]))
+def reverse_string(s):
+        return s[::-1]
