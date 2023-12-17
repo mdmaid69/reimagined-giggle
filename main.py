94131b4109cc8f8d4710a9a63b15f8493bc18c12
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:09:28 +0000

    tonight guess chase sensitive worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
