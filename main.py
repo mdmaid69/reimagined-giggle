8435ac780cb06872886683a41803e2fd9aff492f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:58 +0000

    far safety male owe centimetre

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def count_words(sentence):
+        return len(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
