603e9318ee071646bb11dc4f8952656a3d5d0f40
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:16 +0000

    closed idea sharp hurt store

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def find_max(lst):
+        return max(lst)
+  def convert_to_binary(n):
+        return bin(n)
