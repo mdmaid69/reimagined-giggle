48839017ce98008ce082944742f0bd4a9338f2f3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:41 +0000

    talk formal period brave grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
