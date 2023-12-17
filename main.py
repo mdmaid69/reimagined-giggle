934c64794c954ce4b4c00d42503413d4b2283051
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:25 +0000

    brush difficulty port cook bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
