fae973d41f62685bf525432b80b45fb1a58e133c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:43 +0000

    book fill able unit little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def get_array_as_bool(array):
+        return bool(array)
