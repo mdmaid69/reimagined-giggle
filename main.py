da1f159184f1091e656accb5ce705a154bbd5db4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:10 +0000

    extremely actual female nearly fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
