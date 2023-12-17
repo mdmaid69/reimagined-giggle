04a2d26ca6b61a89158814bbf8bc8568ff4387e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:46 +0000

    together whisper purpose change damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
