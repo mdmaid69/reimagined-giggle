796c24d2ddf6e114f25dfcd4376b64a4c52e3fd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:31 +0000

    mention now national steal hard

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
