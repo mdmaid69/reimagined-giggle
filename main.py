2b5833d88d7ab3d4444fbb04888d1b91a5693a15
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:07 +0000

    cruel 572. enter pair respect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
+import sys
+print(sys.version)
