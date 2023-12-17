7bfbb4fca859da751f238a374ff48b660619430d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:11 +0000

    gooff paper track autumn handle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
