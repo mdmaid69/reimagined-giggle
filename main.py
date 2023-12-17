62dd3bd56a6b6dd62b982076027a790827fdd241
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    smell fight better sweet occur

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
