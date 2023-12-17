662455d152c6685e17edf268304c0f651f05c1e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:32:55 +0000

    impression family hit print healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
