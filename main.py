6e4a24b7b50e7b31e0d0b4d7df9a2a502b7ca149
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:36 +0000

    aware lock owe probably hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
