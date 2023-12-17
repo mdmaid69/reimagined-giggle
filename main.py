a3bdbda80bb2764ff83b9416c96fd0bb0c97f596
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:08:09 +0000

    law trade behind no store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
