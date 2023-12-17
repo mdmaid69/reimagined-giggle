39d87ba93ad1e22d9465849a25dbe2c56a7d1a99
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:36 +0000

    sure water influence pupil raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
