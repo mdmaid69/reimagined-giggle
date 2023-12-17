211788922664760ffca1465cb613410f95fec3ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:37 +0000

    brief we crowded reply politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
