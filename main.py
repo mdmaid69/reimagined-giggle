002ffdf841055602cbcbb250ffcfd81053f60402
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:38 +0000

    pencil set division approval down

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
