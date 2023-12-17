b93bd3eb1bec647301fcd306b8da31e02f467ee7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:35 +0000

    surprised wave urgent organization interested

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
