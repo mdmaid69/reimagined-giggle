b132a7e3270ff1871284dc4355100f1852eeb1bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:01 +0000

    scream bury fence twist brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
