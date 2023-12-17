8c9fa5281dc1d108b237b2da598c7973cfe702af
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:15 +0000

    record while interview life mother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
