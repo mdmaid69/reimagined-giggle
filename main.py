42a4a3b5d605c481548dbaba7d0ff0eb5088ec23
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:34 +0000

    advise lucky usually escape advice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
