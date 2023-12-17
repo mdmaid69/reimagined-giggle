24e9523248be6820ada91ccc54bcd7ae3c62b559
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:29 +0000

    clue test gooff exactly row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
