ccabb7c51e8dafb04ed2c321b088b192c394c3bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:00 +0000

    breathe please season spring reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
