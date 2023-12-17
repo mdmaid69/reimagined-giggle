b46e8aeafa840f35c5c69081b22868051079cb0b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:34 +0000

    thumb careful visit equal average

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
