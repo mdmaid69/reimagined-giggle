659578cc677eb0be3eba09b66baa925571b2c740
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:04 +0000

    drop unusual smell many joke

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import collections
+def create_user_string():
+        return collections.UserString()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
