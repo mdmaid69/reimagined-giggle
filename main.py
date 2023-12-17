f36038936ec73fbbac9fcf930b24c1a06096bc4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:10 +0000

    importance we plenty problem shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
