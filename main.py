bb7976a7c7182f234c3449da1628009f99fa8422
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:01 +0000

    cover stranger steady talkback stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
