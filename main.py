787e65e055cdcea185d3f540257446a680cb0f49
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:46 +0000

    pile community confidence visit name

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
