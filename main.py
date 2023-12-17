106285b0ed6452ed10a286f258a02d7731907035
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:22 +0000

    interfere common wake central material

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_max(lst):
-        return max(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
