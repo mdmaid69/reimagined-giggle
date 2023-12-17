5cd2da1a5509f5725f96cb4cd8ce1d25e99c9060
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:04 +0000

    trap beat world comeup fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
