2750a0222ad152074f0ca1eba2765872567991e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:10 +0000

    wide fetch hungry emergency politics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
