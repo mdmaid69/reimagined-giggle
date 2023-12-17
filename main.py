759be4b46680e796ca548add15c1155cbdb87e9e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:42:48 +0000

    store accidentally touch whisper exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
