797765209328ea8db664b2214cfe9d3cd0689277
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:39:15 +0000

    forever loss plan worried shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
