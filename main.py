968b833e83e62e0767545fd0f0bd4beadabd2d97
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:30 +0000

    dinner but visit Ww autumn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def reverse_list(lst):
+        return lst[::-1]
