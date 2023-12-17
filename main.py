7bb7d580463c0b9198fa78786b48b2f9976e4e78
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:17 +0000

    in suggestion collection reply nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
