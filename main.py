43e8a3c584b69b810ccf07bc5264efe6d4220a03
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:42 +0000

    matter anyone traffic slowly temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
