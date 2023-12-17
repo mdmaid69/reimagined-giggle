f37e33fc8d6eb3c37baa649f42a5bf4be95a90c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:05 +0000

    harm miss far just regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
