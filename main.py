4f7a23c4e6ed06ad3339c74a360a1449c93602a6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:34:22 +0000

    contribute neck other shut suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def square_number(x):
-        return x**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
