0ac97316d928cde1fdb0cf8291796c6123bb6ac5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:53 +0000

    ignore apply desperate spot turnover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
