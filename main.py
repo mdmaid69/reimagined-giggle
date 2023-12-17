bb62e030f023db6637f008ea0a4af86496f35f27
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:45:49 +0000

    interest protest influence point interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def count_elements(lst):
+        return len(lst)
+def calculate_work(force, distance):
+        return force * distance
