4bd53b76b71bea8b62573d5922a4a75eb3d28ca4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:44 +0000

    declare sticky war comefrom state

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
