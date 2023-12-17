076f112c15266fd79ef8a6410d1096aa36e44b99
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:57 +0000

    measure discussion patient block ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
