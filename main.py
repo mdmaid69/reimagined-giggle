0ab49cea2ee21f6f87968f46cdc994d9dd0276e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:35 +0000

    dull fast rescue possession base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
