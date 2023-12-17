b570f74f6a7f102826d9e52027503c692474a97d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:45 +0000

    open through please shade so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def remove_duplicates(lst):
+        return list(set(lst))
