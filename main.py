0463eae1d7557f1bd02c3b3569bcf85f148ffd28
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:45 +0000

    spoil hear hold touch no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
