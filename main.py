fcb66e50e2a43dd45c404ee1fc6a05889637add3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:03 +0000

    cold runover level count relation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_npv(rate, cash_flows):
         return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
