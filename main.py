56387af18407311221f5f0b4e0125338ad83e0be
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:05 +0000

    plant middle black worse behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
