285dcde84df77201a203487ae175d19a6214f73e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:00 +0000

    keen still Ii key bunch

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
