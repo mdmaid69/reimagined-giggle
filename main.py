91e3c06d725a9b93c1f0807ef353be377cfcc72e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:10:29 +0000

    neat hold safe reason slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
