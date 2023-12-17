ff4eab7c143cc68d63b1dd0046a465004957680e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    natural catch party ignore deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
