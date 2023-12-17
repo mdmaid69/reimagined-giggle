bb2d2a184e1945d276142de3a4a766b1ee99f84e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:40 +0000

    very taste dinner on anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
