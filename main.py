912a99c23cba0c59e8f6dc9c0d91a0f06c84cdf0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:43 +0000

    copy full measure call improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
