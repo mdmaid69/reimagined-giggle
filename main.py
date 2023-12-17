a75948da8eda03a8797f57a6f50e71b06f2f52a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:38:20 +0000

    complicated independent transparent murder born

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
