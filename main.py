33e835acd52f163eeaa4643c0ff58fe37f9e828b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:20:06 +0000

    main choose concerned roll practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
