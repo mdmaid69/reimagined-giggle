30cdcf651f1784631921a11bfd061777a2008cfb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:57:02 +0000

    own risk ever sudden top

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
