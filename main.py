3a3e5585ea6b190260bacbaa18155f442c46f408
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:36 +0000

    correct invite art anything tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
