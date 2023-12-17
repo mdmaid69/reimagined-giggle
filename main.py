b3d51a44ea8464c0e44ec12b0f654835405cd1e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:27 +0000

    noisy bringup getout land pleasure

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
