0572c17cd1a44898cc3988654db0a0a1e2d599ae
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:03 +0000

    escape order go up cry holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
