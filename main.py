4b8a13fe09f4891a7c2e612a111066a62a7a46a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:24 +0000

    ill moon nowhere regular brush

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
