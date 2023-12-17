e7ef9c4711351ffcaee0ee9405f6d15cfef5136d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:21:14 +0000

    dance survive present damage regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import platform
-def get_os_info():
-        return platform.uname()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
