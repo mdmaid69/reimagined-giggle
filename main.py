e81afade68350ac3d6846438a3f11e9383398081
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:19 +0000

    label but 564encourage show valuable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+print(os.getcwd())
+import array
+def reverse_array(array):
+        array.reverse()
