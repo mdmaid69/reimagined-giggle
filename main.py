6a80a61248de3403c7b5ec4630d4366752f88f60
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:36 +0000

    course very order suggest part

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
