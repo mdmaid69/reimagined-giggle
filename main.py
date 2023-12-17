b457451bc00ed634bbd5bc5c7890479a21099203
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:27 +0000

    can ring pinch but language

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
