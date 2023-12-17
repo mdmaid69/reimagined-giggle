be60ced6a9a31892f88c1b0b23c86549e87f3bbd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:06 +0000

    price regular development to cap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
