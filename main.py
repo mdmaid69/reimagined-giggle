0aa24d01a2d7f06b4e886cb7b5bef29f1263f301
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:05:47 +0000

    surprise although dangerous Qq force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
