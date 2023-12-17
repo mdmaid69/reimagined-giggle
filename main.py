1335d5f6903617b720d4ae00d54d841df3004be8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:43 +0000

    cross plain Ee port watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
