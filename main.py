efe6f02fb5ba1201ffe8e3791f71058de59d2f3e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:44 +0000

    Jj wind stamp act argument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
+for i in range(5):
+        print(i)
