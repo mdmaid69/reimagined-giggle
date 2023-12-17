f1b0556779396e1feef2e7ea7675c7f8b454b1ae
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:48:05 +0000

    she cry cover problem appeal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
