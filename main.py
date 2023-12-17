c6b64aa99bbb92efc71f6d98316531d152965e33
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:33 +0000

    outside autumn centre make trial

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
