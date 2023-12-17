e67c90b302924a790b3132975d8500a8452e0283
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:32 +0000

    public flow middle programme Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+print(math.pi)
+import random
+def roll_die():
+        return random.randint(1, 6)
