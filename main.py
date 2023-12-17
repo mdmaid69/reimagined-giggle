3570804ff5eadc6797d0870bfdfe1ebef70cc86d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:26 +0000

    compare turnon slight sensible soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
