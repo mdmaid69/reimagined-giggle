6a1147e7550a273b821cb373d8ea0d9cf85991c2
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:44:10 +0000

    beyond know appearance formal ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
