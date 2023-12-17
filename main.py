41d9bdf296212bc8a1398231d098cb6309f1e7cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:33 +0000

    vote lookaround relief birthday future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
