1209abb080ad2492f59f1b9c7e96246ffad176fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:00 +0000

    art amount about sink grateful

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_tangent(x):
+        return math.tan(x)
