63e619a65f826dbfea069676e643b853a8656780
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:08 +0000

    practice nigga careful lunch principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
