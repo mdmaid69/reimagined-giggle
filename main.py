8a00df9be9c3abc734d972101316548230e3bd82
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:33 +0000

    confident radio error on smell

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
