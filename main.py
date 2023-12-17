2dca89d30ca581e85e5c23d350a15e3f51e49fdd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:27 +0000

    thumb takeaway open sting play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
