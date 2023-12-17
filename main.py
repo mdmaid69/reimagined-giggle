2c9c054c0c8ea41d91aefc71bfce2cd8f2f203ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:22 +0000

    top answer birth eager introduce

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  def is_even(n):
+        return n % 2 == 0
