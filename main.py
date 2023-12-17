5741ca925d9f76186041e32224878b8370747e1b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    luck pot before exit fast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def square_number(x):
-        return x**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+print(sys.version)
