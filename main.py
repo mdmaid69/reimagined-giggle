9be10082d67dcc23e7e56ce561d1729ad194729c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:48:38 +0000

    bar whom treatment line manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
