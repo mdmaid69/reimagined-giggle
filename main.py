b1ad1fe473e9d019d37239b6e4357502f9520a5f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:04 +0000

    mistake punishment thin connection judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import numpy as np
+print(np.array([1, 2, 3]))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
