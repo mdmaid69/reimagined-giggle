49b0566359a4b4a198ca2e16028b21637b68ccce
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:33:09 +0000

    spoil string fear moon us

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
