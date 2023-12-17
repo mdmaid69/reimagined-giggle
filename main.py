6389f5e095519270d76a3fa0e03afbdc42d98a98
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:14 +0000

    kick hospital history which keepaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
