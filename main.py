776a01e9d81bbe3cd061b039966ee44179024250
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:06 +0000

    total everywhere perform top to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
