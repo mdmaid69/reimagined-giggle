7cb8d84dbadd614811bf07c5fdbbeb41d3b7f00a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:14 +0000

    alive attention loud about success

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_speed(distance, time):
+        return distance / time
