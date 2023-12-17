af90cfdf99df099b8a922bada53fc2631a0b9ffd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:29 +0000

    this tooth than deserve over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
