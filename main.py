b9d5b4445d897ce416e87022de04e05dfe068559
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:55 +0000

    pronounce treat kick hair connected

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import time
 print(time.time())
-  def remove_duplicates(lst):
-        return list(set(lst))
