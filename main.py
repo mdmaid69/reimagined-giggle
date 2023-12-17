cf91f92685f92b30447981f7749e659e6e6c1e93
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:28:46 +0000

    into bit comeup concentrate reduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
