8e39db87b98b7cc7e904f8b141cb0355b5164e1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:11 +0000

    for include tall genuine excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_sign(x):
+        return math.copysign(1, x)
