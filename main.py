b5887c1990ef0d3f024fad700da96a55ba88c594
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:17 +0000

    invite fast suckme sink communicate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_force(mass, acceleration):
+        return mass * acceleration
