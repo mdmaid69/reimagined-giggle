2f0feeb4ef335d5dc190d3d12d07020d58b6b8f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:27 +0000

    return normal permission tune tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
