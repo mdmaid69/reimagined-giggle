64e1b1748b8155c144c9ea3f1b9205dc6532afa5
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:54 +0000

    need see ring reserve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
