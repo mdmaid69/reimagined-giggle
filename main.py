782ec461473cd03c82f64e0a19964f7b232e0a91
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:31:16 +0000

    record takeout move shop indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
