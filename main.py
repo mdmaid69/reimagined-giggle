05be78152bb4c11e8f85ae6eb29431023813a5ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:25 +0000

    pour turn let their weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
