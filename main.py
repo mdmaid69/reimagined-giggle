3cd15f815556a5e3841353770ba9f98554d3b526
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:09 +0000

    suck term finish scream goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
