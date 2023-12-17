724fa72a0992f58011c5dcf8693c8e5079aa592a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:17 +0000

    boat foreign switch charge hungry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
