d39a7b5a5ecb58c943ae50848b275cfc7329dcc3
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:18:38 +0000

    answer price must dance upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def add_numbers(x, y):
-        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
