07ba9a45e589259c27b96e5f4dc21cce9a1b8cfa
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:18 +0000

    heavy health statement drop square

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
