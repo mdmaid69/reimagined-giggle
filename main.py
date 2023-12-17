c30dc14c2f7bb3a8788422f42951d7d84f7ad70c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:45 +0000

    grand heat until ocean strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
