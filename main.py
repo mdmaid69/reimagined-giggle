a6451fdce137dd4f1170709cadb8c70ceda87471
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:12 +0000

    cinema present satisfied over cook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_palindrome(s):
+        return s == s[::-1]
