ec0b9f42b1307b02e74257aa1180ffca741575f3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:35:50 +0000

    press feeling claim Jj instrument

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
