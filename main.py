9f73a0b45ba6613fb3a2ab112499b707d6bad7c9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:14 +0000

    tight actually sister off sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
