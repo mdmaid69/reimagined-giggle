37b84fbbcf0dd99ff5af61ff9378806e2054e703
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:27 +0000

    dark matter recover concern ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
