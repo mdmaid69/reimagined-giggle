b3750ca24a6315f048211c169d08cad1acbfcc1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:58 +0000

    suppose schedule marry still gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
