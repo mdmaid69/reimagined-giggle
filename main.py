266e3a56ba41f699bbc30b14c903afcc11242fee
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:33 +0000

    point occasion never translate height

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
