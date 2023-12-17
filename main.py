1022c2d2acae38593526f4df894f198537afe7a6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:56 +0000

    famous contrast explanation action sting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+for i in range(5):
+        print(i)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
