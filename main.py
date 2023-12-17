dcef94c0d48f77d7a3f2970cf22f8b933c5cda9d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:04:14 +0000

    factory none safety desperate statement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
