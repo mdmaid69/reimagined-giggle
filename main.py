0d8fc554a9ce5c1568d5d65f0d59517d0811b0fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:05 +0000

    entry call everyone pay fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
