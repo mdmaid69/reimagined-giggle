2aed0184de6e31eb59075dd6440915e7471c15f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:15 +0000

    disgusting wire memory which shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
