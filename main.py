453b218c7da3fe67849119b5c7a5ae52f0c382a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:56 +0000

    hole flavour basically but much

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
