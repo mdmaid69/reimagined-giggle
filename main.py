e508582f64a02abe1df69602fc93f42416ac3240
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:24:34 +0000

    Rr advantage worst door similar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
