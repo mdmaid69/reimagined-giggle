7eb9323a09a1be474a898aac42d1c863ea3f13a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:50:34 +0000

    holdon message sensitive children piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import datetime
+print(datetime.datetime.now())
