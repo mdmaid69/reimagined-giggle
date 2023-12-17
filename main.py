61643d1d16af6a6039a83481c790b1b52627259b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:02 +0000

    stamp passage mend pass however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
