f0d3b56ab3dd7b5efac08b24985d064c41ae9aa9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:20 +0000

    pressure cum steep artificial piece

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
