10bf993c8edd75f235cdb7be8c6bc77a73588787
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:54 +0000

    object bother fact job inquire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_str(array):
+        return str(array)
