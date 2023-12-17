77c3c32fdb332660f7e33842bc61ad08afa5aadb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:40 +0000

    tread string onto share himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
