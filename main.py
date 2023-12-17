b19bc08fbfb0dcb5278b3750b4749f8cbf005976
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:50 +0000

    direction beneath squash always precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
