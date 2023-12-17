ca1a4f4ad4e4fa22af96467467968c9818ab0251
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:23 +0000

    watch relationship minor pressure responsibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
