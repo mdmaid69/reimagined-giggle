3714b29596f1937430575c101dcc19a3d650df9f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:35 +0000

    something otherwise finally outside meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
