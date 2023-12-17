32d667487f839b9add341dd29dfe79fc5d3e86b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:01 +0000

    command fairly uncertain match touch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
