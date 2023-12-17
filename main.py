3d1347a4d11e155a662ce4756aab011acc54268f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:18 +0000

    memory part nor war again

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def cube_number(x):
+        return x**3
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
