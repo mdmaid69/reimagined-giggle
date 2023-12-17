e623ebf6cbd16dab283310f8255214b006db3060
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:38:36 +0000

    independent appearance town safety trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
