7fba7174f6117f280da01ea04b73bcd84f5f45bc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:48:55 +0000

    minute change modern depend free

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
