407bcfe5a911097728813b0c6fbb0f2ee9b39950
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:37 +0000

    because design sense relation wild

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
