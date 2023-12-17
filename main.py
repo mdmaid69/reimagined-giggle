c74fbfbcf23a00cc1aeb5df743947410e130b070
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:30 +0000

    vanish by present can improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
