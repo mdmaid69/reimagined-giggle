22488fd0a8437ea8551b9685ed8bf27fa1ae461d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:20:31 +0000

    soil with correction trade wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
