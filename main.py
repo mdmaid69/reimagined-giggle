d3ea28fe1277f548fe64f768b62d2099ada0f395
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:30 +0000

    soil takeout scrap sick less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
