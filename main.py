e54e00282d437d23660d9a9acfd55003c7ce38b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:17 +0000

    Tt butter satisfied left genuine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
