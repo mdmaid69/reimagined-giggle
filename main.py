f7aad3c8fb6abfb37ce01e5e155a7aecfcc3a19f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:23 +0000

    spite ask obtain sink die

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
