daebf5a077fb7d5a0a3fb09c4449ce6ece7f8ce7
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:40:48 +0000

    union brush cap half last

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def append_to_array(array, item):
+        array.append(item)
