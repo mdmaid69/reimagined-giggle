ab559bccf58f3e9ecf0eb9b3c268c5bc5cf65fa4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:53 +0000

    friendly side store steep try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
