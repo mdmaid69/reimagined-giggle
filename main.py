300cf4bfa6f731d927aa27e1ddcb263d825db98e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:26 +0000

    fold run holiday example convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
