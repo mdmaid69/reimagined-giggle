c1143363995dc2121574d5acf17d44e0770535a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:56 +0000

    factory scare test wise flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
+  def is_even(n):
+        return n % 2 == 0
