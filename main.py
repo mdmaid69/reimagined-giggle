d0ab5befb24338808a1abc13dccc31f805106311
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:21 +0000

    rob discovery relationship outside goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
