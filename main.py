5b3faa84cfbe8c19d1ecbd74b9041ad989229a31
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:00 +0000

    bang frame short step rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
