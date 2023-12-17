2feda5a9e510e65abc9f4bd9da6edd404d05ee02
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:48 +0000

    pinch avoid board talkinto perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
