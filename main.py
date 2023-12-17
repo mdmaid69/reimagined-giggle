c975c54ebbbbeed0573aba3933801cd41b5faf93
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:39 +0000

    imagine breakthrough steam so pressure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
