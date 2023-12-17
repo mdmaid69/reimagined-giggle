a0fae54549df59613bd01bc86181571fb2f609d3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:51 +0000

    573entertainment advise evening hospital round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
