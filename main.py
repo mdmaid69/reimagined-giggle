e7d5791ae1625a6b56283968db3b00d76ead863c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:18 +0000

    special deliberate pity protest suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
