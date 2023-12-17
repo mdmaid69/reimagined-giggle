f582220f31ba7370660f3306cc6896b4bb97c965
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:45 +0000

    produce unite combination attempt sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
