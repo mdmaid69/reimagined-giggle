ac68c70ce2dd266b60fa8b471deec70919b3637a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    owe about instant sad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def subtract_numbers(x, y):
-        return x - y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
