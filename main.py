28ad814dbfe90e213905cd86456c8e70138f4263
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:23:12 +0000

    debt suppose whichever learn tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-print(time.time())
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
