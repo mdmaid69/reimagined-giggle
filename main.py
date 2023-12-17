71d4a9fe15fc50ef647bfad237ce610014ab847b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:17:53 +0000

    really fit story never lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
