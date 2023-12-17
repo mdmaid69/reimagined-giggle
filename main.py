032c5676641c98cbe9ec06eb57ee08a749c08017
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:04 +0000

    cold score comeoff goback(to) capital

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
