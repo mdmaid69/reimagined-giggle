38ed6cf1dec9a1394320615caa99ad3981e6cae9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:21 +0000

    fly want wheel once reserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_string():
-        return collections.UserString()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
