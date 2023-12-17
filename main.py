bff337276f9052d779fe822ec164186295900a87
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    gentle turn on too affect back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def convert_to_hex(n):
+        return hex(n)
