b739465c06fdef6580fac0d9ec116f064aca2fbb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:26 +0000

    warn duty thank smell big

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import time
+def get_current_time():
+        return time.ctime()
