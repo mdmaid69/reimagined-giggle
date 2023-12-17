0c00faa7f53f9fe4a87ccd4a85c7ad95a8835187
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:37 +0000

    owe commit plenty class inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
