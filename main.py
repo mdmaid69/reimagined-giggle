45553546df14d101b57f31d8928f9e8db5f5724a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:39 +0000

    male eat seem set continue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
