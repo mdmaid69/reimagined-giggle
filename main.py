156e46ea18c2f6e96bbd665ef7bfccdb9f23211d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:04 +0000

    slight relative bar basically brave

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
