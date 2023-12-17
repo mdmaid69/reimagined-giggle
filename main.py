40811cd32eb39211d49ce1393dbbad5f6c8a7904
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:10 +0000

    worry side thanks tall lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
