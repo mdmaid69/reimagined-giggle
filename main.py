b654c6b9e94be63ac5537ffa41de19e8f765be45
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:10:13 +0000

    I tone control house callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
