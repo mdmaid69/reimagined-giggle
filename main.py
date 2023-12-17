8f980d0d7d27021e350eda75a00796e248f6d075
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:10 +0000

    mess emergency herself delicate suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
