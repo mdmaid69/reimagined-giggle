eeec68e3d09294b0f93780b76f063cebe648a74b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    speak style murder scream downstairs

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import datetime
+print(datetime.datetime.now())
