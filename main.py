77d6c3be489daffe29dfe50b1eab7503ac06b01c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:16 +0000

    vote completely available mine about

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
