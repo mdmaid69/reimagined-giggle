5b62c69ea085175e2db62d3cc00af88a58b7a6c6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:00 +0000

    protect most turnover cool punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
