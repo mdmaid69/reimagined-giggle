a2450d93d640c1ede603d96fca9c045bf3a35572
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:52 +0000

    Ii recently bringforward praise sore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
