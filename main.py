3e64996f70c19c52126288dcab9a16769915962d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:59 +0000

    without conclusion turnback quiet our

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
