fded494875519ed104f173a39393ea125aa4fd6d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:08 +0000

    gentle use genuine truth present

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
