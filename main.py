577b6c1663481f21e29f5a18522bc23c5891d9b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:10:20 +0000

    mean avoid noone pump about

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import queue
 
-{body}")
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
