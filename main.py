3a51fe3f6f1602e1fe63e85bf3ae26a8c049f8a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:16 +0000

    secret fire dig holdon rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
