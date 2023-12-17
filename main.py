c570260dc016afc1d95705a2dc280ce71517e3a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:54:45 +0000

    across post responsibility her mess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
