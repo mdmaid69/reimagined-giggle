81c42e2a06cb4cf1b647065ba5d75e54a5061646
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:45 +0000

    564encourage education score centre willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
