64bb5550c913012f4dd7fbcca9e2052e1caded15
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:24:36 +0000

    formal interested suit same balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
