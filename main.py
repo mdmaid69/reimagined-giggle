8d936468e1d7c6593a2d3baf08eceeb197911859
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:21:18 +0000

    tradition smile influence love search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
