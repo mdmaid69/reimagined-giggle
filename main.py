114f08c6920e729f15ee31b346f11cba22517232
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:32:07 +0000

    free turnback light life violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
