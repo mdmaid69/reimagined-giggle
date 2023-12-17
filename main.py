96574f4019abc28f88fb8c93b2ed262624832cc3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:40 +0000

    file slow Dd 571enough minority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+def square_number(x):
+        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
