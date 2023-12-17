5b22a620a4736c53649e7e4799bd4e45d2845d3c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:19:03 +0000

    public damage breakinto approval tonight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
