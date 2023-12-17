96edb408173928e75d8f110ef29b4a01d7fa3dd6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:43 +0000

    concerning race copy communicate description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
