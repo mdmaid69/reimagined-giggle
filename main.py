854dcc6ff8d8b6a52eca56502ef14168787f07f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    your close voice day anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
