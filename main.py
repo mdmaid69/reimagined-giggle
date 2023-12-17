56754272d14753c090ea958d83bbbb6228c655a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:01 +0000

    confidence take stretch on Kk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
