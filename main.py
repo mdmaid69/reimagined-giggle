19c965b828094511dec45464444e435a64491fbf
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:24 +0000

    then have upstairs side although

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+def remove_directory(path):
+        os.rmdir(path)
