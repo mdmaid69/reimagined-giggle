70774709987039df8b825a7a5c1d53bf5c7e4e13
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:03 +0000

    count stare expression bear bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
