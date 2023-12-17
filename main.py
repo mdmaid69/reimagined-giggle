2ab245fb09c21a82512d9c85e9d48fe6e47c6f7d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:49 +0000

    sink complete blame deep walk

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
