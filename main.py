81ca27f80f0dc3b7c3edd98e2e70acad24ed9cc7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:18 +0000

    trust level environment table authority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def fibonacci(n):
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
