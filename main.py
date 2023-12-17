a4a6c21f4a08f84bcd4be7a96cc13a61990d7f93
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:29 +0000

    people rush star allow possession

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
