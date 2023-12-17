813032eea872c6a0ebce518e0747cef543c024e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:28:38 +0000

    it crowded fire emphasize threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def is_palindrome(s):
+        return s == s[::-1]
