9642b907e307831d062965c86bfc3fce4e8d0755
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:03 +0000

    experiment form coffee lucky young

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def greet(name):
-        print(f"Hello, {name}!")
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
