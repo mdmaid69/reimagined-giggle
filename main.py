510bd9ddca341acb08c1ff8fdb2381018c47d689
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:59 +0000

    Pp long decide properly broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
