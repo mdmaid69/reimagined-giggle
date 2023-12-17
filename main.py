72533d37c0d87aebea02c3c167a5923c8c66667d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:33 +0000

    shout ought fresh standard confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
