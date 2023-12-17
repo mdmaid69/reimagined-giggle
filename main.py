f5664192ac29d6743186f824ee5e70c1fe375744
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:22 +0000

    block burst achieve trip bus

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
