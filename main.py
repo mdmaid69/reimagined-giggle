38199caf2f69d15381eadb347773fa9aa2775b06
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:44 +0000

    stranger muscle doubt about sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
