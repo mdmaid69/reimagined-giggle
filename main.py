b4925921a4bcf1bfec5ffac39186406d054d6428
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:17 +0000

    deep pour return surprised really

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
