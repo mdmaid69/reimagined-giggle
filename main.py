fd7d0b4cc79f2f8e0212399a0ebdc9045f507538
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:54:05 +0000

    race play turnout ability here

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-print([x**2 for x in range(10)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
