07b89d7f283acf1aae4a0af0fc35fa42bf81fbb8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:09 +0000

    wing hot upset finally middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import random
+print(random.randint(0, 100))
