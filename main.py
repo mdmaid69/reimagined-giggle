9a18dfea1b81f38b6b9999c78d98ab9200cec11f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:51:47 +0000

    dangerous scrap unlikely lead design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
