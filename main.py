119bed2e0cfd08a02aa31268a68725fa129c864f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:27 +0000

    exaggerate Jj content deliver general

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
