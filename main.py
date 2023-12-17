d3a53d8c95b57a4fcad3948351deec7117e23be4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:29:15 +0000

    comeover date completely charge destroy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_ceiling(x):
+        return math.ceil(x)
