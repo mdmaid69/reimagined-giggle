c9c277513e6b3da11348b8acf155701418aa9f2b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:53 +0000

    crime wherever maybe admit drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
