ed70ad7671780f145d13362063cca58685d0d3b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:19 +0000

    payment Rr half table runaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
