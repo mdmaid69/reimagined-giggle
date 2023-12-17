b75eef55a3c8de3c70271940532dcbb176675feb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:55 +0000

    giveup but size calmly cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
