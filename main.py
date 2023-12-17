2c8b1f5c8918fb92d5e5c1d82afc4ed247cc212a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:24:40 +0000

    cross plenty remember kind square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
