9c1beb4f35a287b76d75d86885bb5cb6acb724c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:59:48 +0000

    share street hold sail crowd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
