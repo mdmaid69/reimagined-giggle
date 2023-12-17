f70722ca84e4fec533ef10fe4b408740797ada6e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:57 +0000

    transparent pronounce talkoutof nerve turnback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
