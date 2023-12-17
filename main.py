93b36b15b9d83a012a1b886c1fa7e5bd6bfc0b1d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:03 +0000

    hurt usual half negative space

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
