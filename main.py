2eb06e073302b576a86780474f9b618e6e1a66f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:20:33 +0000

    accidentally be job hurt part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
