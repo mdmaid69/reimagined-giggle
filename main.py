3a17959d9992950d71835826a66f65c34c674e5d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:15 +0000

    breakin prevent value authority surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
