ba7350dd6f4c787c1f031445ddfdaf521cd52538
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:10 +0000

    earth memory support unite later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
