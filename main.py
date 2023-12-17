72594094239b8946825bad28d0a5812f54e917cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:39 +0000

    wash comeround increase very extra

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
