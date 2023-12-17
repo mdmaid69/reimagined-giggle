2591602ae113b98603cafaf89f0955c2ba1a86ca
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:47:37 +0000

    share unusual charge speech when

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
