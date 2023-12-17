934ade6dfb5e44109dc87b3b6be5f01c29e773ed
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:37 +0000

    shout answer worth payment worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
