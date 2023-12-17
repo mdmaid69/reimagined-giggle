b917a481a068da1e6e4173363c7d237fa375e804
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:12:39 +0000

    no Itself watch bend herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
