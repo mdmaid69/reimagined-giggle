fc002f6ca43b831369ca990e232bf282fa0b53d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:32:58 +0000

    fold concentration rain what pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
