70eb2b70d6a2373919df0f70caf69c5993c08c08
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:35 +0000

    Dd shut increase schedule live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
