a026aa0d6bc8c92ac783b3020f169f2298daf74a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:26 +0000

    eat excellent mind low sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
