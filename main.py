0343a888d4d622ecc912abb1043c26bd61956689
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:12 +0000

    real consider tooth tonight prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
