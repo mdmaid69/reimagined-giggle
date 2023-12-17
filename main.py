1e96dbd663abd8f4d3ec1f6bb5288eba731ec5ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:23 +0000

    slice worth opposite snow interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import numpy as np
+print(np.array([1, 2, 3]))
