81f0aac37177bc46f08654fdd40b3cca01f10c44
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:10 +0000

    into generous unlucky answer tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
