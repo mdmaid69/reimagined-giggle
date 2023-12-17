ae22b541fa97f0668195395d26d2e50ab6154f8d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:00:06 +0000

    unlikely course tired bang step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
