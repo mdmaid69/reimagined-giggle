bd9ee4ebcdf10cfc2285eff7045961eb1c7d3acb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:46:06 +0000

    apparently meeting smooth full city

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
