04bd864197a2677fe923f5dc3a99b7ccb4301dfb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:03 +0000

    comfortable hate stick earn breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
