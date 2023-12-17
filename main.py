4851ca97da3a3851b390034c4fa2ea97546c96e4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:32 +0000

    commit hello kind scrap decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def create_user_string():
+        return collections.UserString()
