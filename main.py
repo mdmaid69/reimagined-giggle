6226ad9972447afb6ae19ba66cb8b38ef9573b44
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:43 +0000

    never snow tap chin perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_palindrome(s):
-        return s == s[::-1]
+def get_array_as_memoryview(array):
+        return memoryview(array)
