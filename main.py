1a9d614dc7c018f6bc165953fa834c99accef6bc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:54 +0000

    instead dance force load block

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def square_number(x):
+        return x**2
