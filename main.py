ed6eaafb8fbad24f409926dc8089c34393b1670e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:55 +0000

    constantly bunch lay honest reason

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
