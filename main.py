4ec51bba42e5f1ff8f4393237c1a89312b9eb5d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:20 +0000

    gradually insist begin unkind delicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
