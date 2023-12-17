c1dd06b10a395502cc2307296ee03c4ba6a5539c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:48 +0000

    repeat whistle genuine pencil damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
