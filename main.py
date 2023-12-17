f04bb2b503563eb749ab15a3d8a19193ab73f5e7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:43 +0000

    wash influence harm loose what

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
