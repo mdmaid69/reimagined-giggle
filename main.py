6357218e40925a91c5798f7ace55c1debcba884e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:20 +0000

    little smile summer contain upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
