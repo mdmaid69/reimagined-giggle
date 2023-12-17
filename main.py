3419cf0c3d0313c8ff4c85d33f78c3b3965a7f3d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:19 +0000

    knock lookafter dead live ride

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
