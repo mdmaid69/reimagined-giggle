2758e1f7c0829901d3355ef7ecf539007bb6decd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:15 +0000

    vision tell tool whether ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
