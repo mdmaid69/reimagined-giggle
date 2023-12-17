881463aef42c74a580faf03f0a105cf161a67440
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:41:58 +0000

    tidy kind estimate rid task

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_repr(array):
-        return repr(array)
+for i in range(5):
+        print(i)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
