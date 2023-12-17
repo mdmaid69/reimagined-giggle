f7a27bf0da9b87e80f6be998114bc0ff37d4c188
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:13 +0000

    rush yesterday any busy machine

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
