61dff72366ff693cf774db9f67392aae1c6555eb
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:20 +0000

    forward polite suit fail properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
