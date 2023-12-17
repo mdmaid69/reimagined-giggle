e85ab14c7454e7801277d6bf01b6b263954765fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:39 +0000

    everything direction frequent touch pure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+print(sum(range(10)))
