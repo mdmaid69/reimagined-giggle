d5bb2f002c46047977628485a0f1b34e57542212
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:47 +0000

    on mind command swim niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
