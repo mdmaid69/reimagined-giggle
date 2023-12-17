fd5c1bb49659ab6966b31c7cbfb40d38f80a8621
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:21 +0000

    concentrate level discover tax major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
