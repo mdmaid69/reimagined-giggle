cf817b620cad1f067b676dc4e277a3f8cb0a9c4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:17 +0000

    since write join tell new

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
