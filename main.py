1ff7a5e532df3191c140baca9f4771221f2ba1cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:43:47 +0000

    sorry badly material mistake bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+print(math.pi)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
