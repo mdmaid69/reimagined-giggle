6e0d7c7a4fc67a9b30ce3166004077a8c861ae2c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:13 +0000

    order rule please ourselves strange

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
