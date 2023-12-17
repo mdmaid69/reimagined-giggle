ee7fd6eafc791309427499b0bd48f9cebfe8eefc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:06 +0000

    tooth learn undo neither pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
