56b0f2f7a23e3323a54d94ea61f23992572a6251
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:06 +0000

    colour takeover disapprove lock light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
