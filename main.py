c8c9f69a9690111a423c94ad5d69e2242aa0d084
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:57 +0000

    simple community real think suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
