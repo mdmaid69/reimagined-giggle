948e1d819f79ba1651adb0504d6ef65b02423eef
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:44 +0000

    depend schedule definite available convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
