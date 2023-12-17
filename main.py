492c694d6ad4f6706c4ac462daa111803cd83be4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:38:32 +0000

    large bring break right style

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
