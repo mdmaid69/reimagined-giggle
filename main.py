ecf8d58b8cfd8d9a950b52fbc08f4f6fb83e89f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:23 +0000

    address fail tradition gas somehow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_min(numbers):
+        return min(numbers)
