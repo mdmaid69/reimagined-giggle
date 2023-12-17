fe5cef2f16487d749456cebc3bb17f2ba19f4ff4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:27 +0000

    guess either never move clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
