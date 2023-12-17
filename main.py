ea7a54573931bb8be086b4d1827b41c566fb9c8a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:52 +0000

    ticket trouble public trouble scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
