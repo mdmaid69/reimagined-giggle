0dc678568606fc93e570d3b087b79fddb5023a55
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:50:37 +0000

    manage top somehow meaning dream

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
