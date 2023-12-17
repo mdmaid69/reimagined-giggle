5ca410e13414a8f555263d5cb6b7fa1ab0a65810
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:34 +0000

    rest wander beside dress pen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
