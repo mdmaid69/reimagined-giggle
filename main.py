7bf38b31dff727dc7f0b9937e308799c7ab0661e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:50:06 +0000

    business load shopping appeal child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def clear_array(array):
-        array *= 0
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def extend_array(array, iterable):
+        array.extend(iterable)
