56b753bcd353b32a4e026fc55177327f6adc6246
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:21 +0000

    goup strike takeout equal complicated

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
