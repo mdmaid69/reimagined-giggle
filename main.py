47c4375a38bc9547cc4c110a15f7bae77c7640de
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:18 +0000

    mouth deep just quality party

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
