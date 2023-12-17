c88146f7c918007c31f900ed4543267ee40dd490
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:23:45 +0000

    base go question immediately shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
