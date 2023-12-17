29d5cf44b8f9878b3947d778be8edbb6ad101c50
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:47 +0000

    drop money 568energy full rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def reverse_string(s):
+        return s[::-1]
