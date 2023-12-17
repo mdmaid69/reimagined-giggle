a602d0be0921efd34cfc5fda52caf1b2dc13419b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:07 +0000

    stick speed relative scream trade

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def remove_duplicates(lst):
+        return list(set(lst))
