051618e5b6794469757b85dbe019c56061244bcf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:19 +0000

    possibility idea there add she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_current_working_directory():
+        return os.getcwd()
