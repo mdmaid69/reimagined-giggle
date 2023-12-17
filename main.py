13960e5b763f9051d97441ebacbad95bd35666bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:44 +0000

    sleep sure surprising national rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
