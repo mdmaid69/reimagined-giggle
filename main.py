3713f9c29dbfcbbcc3e434d71237f46401f40753
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:49 +0000

    accidental autumn escape cough stick

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-def count_words(sentence):
-        return len(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
