2113cbf12586c0df21de2fcb5cc52b4d380f0603
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:07:29 +0000

    country plastic table weak store

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
