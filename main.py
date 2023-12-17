8b019ed126e9475a051a0b1d6f253148446e77ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:31 +0000

    abroad Ww hate weather clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def reverse_array(array):
-        array.reverse()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
