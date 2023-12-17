a701a944d7f908f638d2f62cfc24c5a169509066
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:21:09 +0000

    fat slide confidence heavy tip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
