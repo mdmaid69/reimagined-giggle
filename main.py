e46b03c18c01246c6bdcaa4d4453244d05354b98
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:17:47 +0000

    container introduction pencil dull manner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_power(work, time):
-        return work / time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
