6b18fe046c0d8c07eb5cd018d6301e7697594940
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:34 +0000

    will whom whichever Dd kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def count_words(sentence):
+        return len(sentence.split())
