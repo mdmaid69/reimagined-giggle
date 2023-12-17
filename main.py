26d4e68a7db6122cf67a52b128e288b8f1d7bf3e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:05 +0000

    measure squeeze pick  Y y hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
