748a6c0477cc0f8f6babdd5c1c159bf0655de207
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:43 +0000

    dream first period same win

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
