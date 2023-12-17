77410ba99a40fc19ab2a9d5386913b84eb45eb41
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:24 +0000

    actually back lunch every none

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def is_odd(n):
+        return n % 2 != 0
+def convert_to_hex(n):
+        return hex(n)
