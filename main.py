db28ec35cec93e469bdf95e25559631dfecc9e81
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:45 +0000

    honest present better by discussion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
