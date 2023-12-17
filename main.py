4a46eed40b1f408f70b0708842a0c0bf9ae9e6b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:55:28 +0000

    contact niggaacademy visit Ee crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
+import array
+def reverse_array(array):
+        array.reverse()
