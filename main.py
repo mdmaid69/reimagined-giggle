bdcb0fd7f68b32dfa33f18ccb88ebd9d17bf1b77
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:40 +0000

    black good final mood delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
