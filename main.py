cb7578b4877368dd4d7d8ccd3bc45c75bb902f3c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:25 +0000

    accordingto choice ahead violent birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
