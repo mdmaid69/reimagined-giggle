ad9493510908fd40ecdb8a5796629a621d9aba42
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:45 +0000

    lookup serve goout warm pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
