242962c7746638a6c7df1cf77ef35d9dbfc0b8c1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:41 +0000

    plan Nn mess pause refuse

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
