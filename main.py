b184fb0c76be9d411c708f22ef3d90b4d997842f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:14 +0000

    we shame low character expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
