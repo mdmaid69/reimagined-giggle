eebcb0f6fd46a5916e52118809f04ec58382c277
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:15 +0000

    receive since hers label smile

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
