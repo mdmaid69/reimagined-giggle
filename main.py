d0b38337f233f78fb6fea12a85de88e1b367e855
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:53 +0000

    closely handle care visit tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
