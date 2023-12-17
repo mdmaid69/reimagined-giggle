64f3bd52bff5003d3afb49384bc735c5feacbf28
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:54 +0000

    terrible compete ask storm before

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
