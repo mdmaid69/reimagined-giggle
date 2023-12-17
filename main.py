257e349f173f382256a57461af64171b0e092ede
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:16:49 +0000

    hide key weak hair fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
