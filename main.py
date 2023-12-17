8776b655afd669310b322e82591b47db6f3af7c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:32 +0000

    certainly meanwhile sorry tip scared

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+print([x**2 for x in range(10)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
