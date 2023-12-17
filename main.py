b210cd34679cfff6db36e4662268df3222747a99
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:47 +0000

    succeed event around most burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
