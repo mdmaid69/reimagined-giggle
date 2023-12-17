9bafee438b524f73ea56a6023724930ac1fc1f7f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:00 +0000

    dress storm sense practice whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
