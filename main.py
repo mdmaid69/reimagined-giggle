72055d6f99140cf815d35b7fae992b8e828a291c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:26 +0000

    whisper stone become often walk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
