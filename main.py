f6b6dee30d2ae26b750dcb16978b19157450f5b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:00 +0000

    spare increase Nn pollution match

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
