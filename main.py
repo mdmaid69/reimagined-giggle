4e1d3b8b5338ecd8e1ed35fd9a891bdfc2eda871
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:51:17 +0000

    favourite country home prefer round

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+from collections import Counter
+print(Counter("hello world"))
