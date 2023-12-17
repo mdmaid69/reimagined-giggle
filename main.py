55add44de200fbf96bd34a90bbbf64dc1f4fef18
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:55 +0000

    Ww tooth by pollution blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
