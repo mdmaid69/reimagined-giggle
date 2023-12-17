05e37e1d7503e1a68f746331e9aab512d4f70b72
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:33 +0000

    blame spring defeat clear wheel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import sys
+def print_python_version():
+        print(sys.version)
