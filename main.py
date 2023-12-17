c28163505845d3e6dedc4c23323fa40e2dd5a1a2
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:10 +0000

    demand guess it early in

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
