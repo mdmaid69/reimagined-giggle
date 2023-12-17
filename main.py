75bdd3e825d6480a8a2d9a7c0223a9d21c4b28fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:38:22 +0000

    hurry prize stand recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
