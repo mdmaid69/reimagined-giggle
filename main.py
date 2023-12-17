c058850e5cdefb687b74dbcb1e945bf466007749
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:01:14 +0000

    shout trade soak truth float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
