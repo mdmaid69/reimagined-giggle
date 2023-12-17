9dfa6b0351531096b2f2f4a6a709335ce4285c17
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:21 +0000

    break necessary guilty sensitive rain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
