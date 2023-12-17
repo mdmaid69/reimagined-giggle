e303d2eb254194312cf453fb125a6b42142b1434
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:51:09 +0000

    lucky borrow doubt natural conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_factorial(n):
+        return math.factorial(n)
