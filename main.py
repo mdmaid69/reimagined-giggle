3860401b7f6006c2f6e63184f479adcd1fd9f6a3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:28 +0000

    pass week content round cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def split_path(path):
+        return os.path.split(path)
