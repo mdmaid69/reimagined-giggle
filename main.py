5c16b2019a11f4bd2ba6789cb28db94610797207
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:09 +0000

    swimming comment gradually heart search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
