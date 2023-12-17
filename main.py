9c615fa892fc7ddcb3bedca4c8ea71baec6455dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:00 +0000

    shut instrument opponent use period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
