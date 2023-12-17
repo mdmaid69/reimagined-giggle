b4179dad2781ccd0da4ff658307192f5e4a561a2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:09 +0000

    peaceful water lot good authority

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_tuple(array):
+        return tuple(array)
