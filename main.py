a93908c7dffe3efc8d129c6fc89b4c4d3f303ead
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:45 +0000

    too private without into congratulations

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
