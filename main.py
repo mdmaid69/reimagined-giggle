9880f9594b479d2d776601373aada8d8a3e96805
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:22 +0000

    bread television wish event bother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
