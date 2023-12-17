2fb6c63b10d8edbdfb66ba580356c951d600f936
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:33 +0000

    correction spray urgent agree front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
