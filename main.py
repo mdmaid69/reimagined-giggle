ac4d3b2f1acc07428ab687089907f22a4ff310f9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:01:01 +0000

    form claim lie suggestion shoulder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def reverse_list(lst):
+        return lst[::-1]
+name = "Python"
+print("Hello,", name)
