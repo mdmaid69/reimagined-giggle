0d9c29817320fb204851c27aa86a35b43cdbab06
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:09:58 +0000

    find search herself authority leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
