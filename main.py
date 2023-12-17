501119388477d7c7d2d31cc2154a266e94ceeb59
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:15 +0000

    pleased prove shrink tax connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
