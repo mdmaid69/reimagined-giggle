75ad9a904764247c1611eba9af7fdde405a24a83
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:39 +0000

    basis miss attempt comfortable workon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
