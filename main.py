bf0f711fdcd4e582b6b393bf7c66212471d55805
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:01:34 +0000

    demand rank will answer certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
