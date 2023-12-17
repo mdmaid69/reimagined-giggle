327ca50d03c1d0dd0da8e18e2917c0dffd46f3bc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:41 +0000

    well guess help recommend black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
