1061a857be174106128eba2da47f7922e822b0a3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:28 +0000

    advice bringback open rob spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
