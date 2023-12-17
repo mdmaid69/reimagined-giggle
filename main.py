caed37c435cb2cc351f249cfe5a896133324ff41
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:04 +0000

    cause embarrassed quality important sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
