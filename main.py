a57f1566156dc3a1ca1d4dbde136db2b82c45399
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:06 +0000

    sort shoot pool sweep wave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
