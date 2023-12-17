046187f2e997ecfa29d2555c884d7a703cb49de8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:31 +0000

    left sting depend worst move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
