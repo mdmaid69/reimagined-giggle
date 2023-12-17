df72296415f01b9bf58c57178f9bad6ab1029c6b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:55 +0000

    conclusion long experiment afternoon niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
