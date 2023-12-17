841744eb1c6357e37fb0a95724545dfd1f94e1b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:05 +0000

    anywhere appearance song sensible schedule

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
