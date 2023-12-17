46d32eda3a0e104dcea95c6834426bec594d9ead
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:14 +0000

    day exchange blue problem over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
