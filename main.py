da82fca6d79cc18e90d3eeb8326664edbd85ad91
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:23 +0000

    sea one loud moon them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_odd(n):
+        return n % 2 != 0
