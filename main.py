cebfc4a250ebe5b917881f7b4dc4fe3e67978a07
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:01 +0000

    practical progress in politics official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
