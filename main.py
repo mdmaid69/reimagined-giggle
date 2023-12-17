b085db2b7642f31de9b6a344eba2ec9dae085906
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:31 +0000

    attempt trick so average tooth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def get_string_from_array(array):
+        return array.tobytes()
