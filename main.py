d65b14049b3a8b29df6b7af3e28e28dddbdf6e8f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:18 +0000

    unless realize success wire pinch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+print(os.getcwd())
