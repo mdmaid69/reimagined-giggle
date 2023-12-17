a31af493a187b70da1741520cb2cea1eaf2e0392
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:33 +0000

    influence agreement load in month

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
