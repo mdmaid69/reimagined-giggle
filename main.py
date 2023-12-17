ce539c96563eae061c6d6ff2b065f295d47d5354
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:17 +0000

    appearance Kk ignore crash first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
