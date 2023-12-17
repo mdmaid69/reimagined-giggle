06fbab10eb3ca30b1ba6b70b2db795122a9f3aea
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:49:48 +0000

    crazy callup cry thought ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
