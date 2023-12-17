7e2d4f2d6fdbee28279c3c6c382531ad5c7af4d3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:29 +0000

    danger wave luck copy push

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
