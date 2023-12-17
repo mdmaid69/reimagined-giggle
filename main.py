9a79c3bd01da88fce2aaf93b29ab0d0fa8d3a62f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:05:59 +0000

    play regularly invitation sure invite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
