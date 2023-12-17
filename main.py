a57b3f115377291e909ee07c1f88050852d59872
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    absolutely lot over roll tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
