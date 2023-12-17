526d8f91db4aa1a8ad852129283b33eb5f2de2d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:01 +0000

    brave string laugh lucky look

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
