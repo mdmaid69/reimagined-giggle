18b65cf1eb3986a410504947ea3cda860cb25694
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:36 +0000

    worst free clean once gate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import tensorflow as tf
+print(tf.__version__)
+name = "Python"
+print("Hello,", name)
