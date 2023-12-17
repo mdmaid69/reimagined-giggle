178ca1757d2f001274402fd33082c5ccd380d178
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:52:47 +0000

    bored reduce about basic press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
