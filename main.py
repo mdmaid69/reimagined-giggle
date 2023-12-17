6df79395711cba4fafe640d6a1bbc2abec4683bf
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:26 +0000

    necessary sad development expensive machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
