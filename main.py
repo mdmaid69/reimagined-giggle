f4f372244f5f21df8bb63bb1d3e8441d43c72919
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    run  future square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def clear_array(array):
-        array *= 0
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
