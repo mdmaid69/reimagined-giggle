2fb77b275609f4735271a5344f00066f8e258828
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:41:55 +0000

    low behind die score mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
