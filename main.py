dd6b2e421677ac73dae86f9e8ef0fb92d2b2b465
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:26 +0000

    godown threaten male really shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_odd(n):
+        return n % 2 != 0
