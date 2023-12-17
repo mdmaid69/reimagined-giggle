4912037836293917c3da197a96b48291dc07e0c6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:36:16 +0000

    loud why exchange human activity

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
