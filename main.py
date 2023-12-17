83344bc3b360293c965ff7a4cbf2e7b89252cd00
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:45 +0000

    gas live goodbye ashamed transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
