22e1abd5448705894f92f897f4a82e0278af3db1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:09:15 +0000

    sometimes wish way innocent push

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
