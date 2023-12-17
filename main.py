f4689500ac177c4e9150661909cc3cad23ad1d8b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:21 +0000

    relation charge fix sink track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_queue():
-        return collections.deque()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
