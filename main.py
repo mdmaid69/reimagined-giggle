de2c24da018a4cf41bc6265587a3faa683a32d8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:17 +0000

    learn corner cheek cry clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
