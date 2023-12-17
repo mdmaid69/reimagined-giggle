010aaf979d55595d521478f0ef654b96c8483470
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:04 +0000

    reliable interview holdup here to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
