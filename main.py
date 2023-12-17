bbf8befb62ab51d91dd82ec29656c9d12ebd8f4b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:45 +0000

    public mark voice content call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
