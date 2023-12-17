99f1d1513b78caee6e3344cb93a65cab040c5102
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:28 +0000

    pollution week forgive outside borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
