6ae240040a4c6b1af45cd2c6b7d89ca27714b3d0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:47 +0000

    land back approval air community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
