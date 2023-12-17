2c48f9274acac0ddc6bd75174baf260f0721fdbe
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:30 +0000

    art excuse lend spray coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tensorflow as tf
+print(tf.__version__)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_int(array):
+        return int(array[0])
