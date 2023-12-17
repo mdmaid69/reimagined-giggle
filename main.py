68140bd1db58a749cc8aff3807a9c41c4da7f5e0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:43 +0000

    every dance hard pick about

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
