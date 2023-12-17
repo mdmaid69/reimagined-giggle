69163e21624e6ce99411ec4335e45762509a1702
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:44:39 +0000

    distance comeover task crash road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-print(time.time())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
