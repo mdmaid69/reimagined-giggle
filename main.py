0092c05697006a96c356ab4998d67ab0c6b90974
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:16 +0000

    smile fly busy before though

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
