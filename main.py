3513f55f5682942518a6f5431403c24057f3e292
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:57:19 +0000

    straight pretty bear deal beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
