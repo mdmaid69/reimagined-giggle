965a9a4688f605388cb74777a96e39cdabad7b2d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:23 +0000

    recommend dead turnback something convincing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
