a9964b43488a9c51936087f4a12d07b7f1272605
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:26 +0000

    national weather lean advice search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def reverse_list(lst):
+        return lst[::-1]
