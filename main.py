420dcf20657571eacc16c3be4df31218708ed59d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:01 +0000

    twist suit stamp bread poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
