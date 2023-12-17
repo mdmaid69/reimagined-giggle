e39f5c7da4d6b86c235f77e79fa48665111a8dcf
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:44:59 +0000

    seem exchange compare difficulty band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
