e71ec15998773b0ed00badb5adf01dcfd38d11fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:12 +0000

    as first emphasis convenient city

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_elements(lst):
+        return len(lst)
