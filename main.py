1a58481e910455577d85ca0d50b14dec6ab3fe60
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:48 +0000

    at transparent accidentally herself matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def find_max(lst):
+        return max(lst)
