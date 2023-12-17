b1a3d5f0de1cdbf0dd3693368aa1363fb041b25d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:29:41 +0000

    comeup modern take always tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
