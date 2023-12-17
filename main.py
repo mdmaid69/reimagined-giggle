7784a4e2a6ac92d2d9e37dc68ba23f2a3d295690
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:38 +0000

    deal pipe so flow print

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
