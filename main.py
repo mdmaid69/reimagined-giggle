4f49e506764996dd42c8d31fc7cab746cce06e0f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:41 +0000

    everybody unusual think exactly still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
