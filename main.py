11c84fc96ddaf6d1e9c434b99acf15a4bcb3cc23
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:09 +0000

    tonight write harm  interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def get_array_as_set(array):
-        return set(array)
+def clear_array(array):
+        array *= 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
