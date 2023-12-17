deb4406bb5bc91142d1f7b1335dc542acc2ce384
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:06:21 +0000

    tend impressive second coffee loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
