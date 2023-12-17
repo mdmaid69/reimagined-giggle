2bbc10c545aa43ee8a1303ea70c886bf550db0c7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:14:25 +0000

    arrangement quit pretty steep exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-name = "Python"
-print("Hello,", name)
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
