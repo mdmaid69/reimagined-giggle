935bb5f2f20b2d318a1bf53e761fda00e1113e4c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:45 +0000

    coat quantity simple inform team

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
