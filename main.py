99a642536281405d4e08cf89cda28c67f3b3f064
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:05:31 +0000

    comeround surface serve somehow tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
