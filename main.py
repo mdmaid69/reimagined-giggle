abcdbd2eaf37cf470757e57ac3603e088a73b9d1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:17 +0000

    familiar thank scared find nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
