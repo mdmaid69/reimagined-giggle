2f7325dc0dbe086453efda06c844f1ecd56112d9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:48 +0000

    welcome equal up coffee past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
