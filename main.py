895d82e396d04642a8feef20fb207a423000d123
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    part profit regard her speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
