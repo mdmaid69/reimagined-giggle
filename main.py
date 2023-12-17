0008acf6e43604505322643e392f159c82081396
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:00 +0000

    workupto thick would train comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
+import time
+print(time.time())
