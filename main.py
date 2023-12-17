0a1d0a58f0a5bba6752f1c6618e8852315c895f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:50:57 +0000

    doubt attention pronunciation protest prepare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def square_number(x):
+        return x**2
