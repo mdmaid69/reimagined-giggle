4f457444427d590ba0d4ee3a9447c6ef49529e6b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:30:44 +0000

    convenient press pity confidence back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_sine(x):
+        return math.sin(x)
