e5c1de58c259a200b28f0ec7d2fcbb975ed2caa1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:35 +0000

    relief reliable disgusting basic priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
