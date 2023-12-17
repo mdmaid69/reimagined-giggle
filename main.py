77d35a42c6621ea8db75dcfa66852d1653a857bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:46:31 +0000

    number definite unite flat rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
