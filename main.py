58aaf73480bfef17b6678e81bc9c72129e9dca83
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:21 +0000

    church extra difference onto discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_min(lst):
+        return min(lst)
