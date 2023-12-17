81d7cd6db2a551f8c5f830eedf4be02215d15ecc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:22:37 +0000

    protest model keepup thorough combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
