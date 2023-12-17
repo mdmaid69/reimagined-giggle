3dd9f273486960f48e685282fc46e3ddb6c9c06f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:10 +0000

    copy until 566end family telephone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def divide_numbers(x, y):
+        return x / y
+def find_min(numbers):
+        return min(numbers)
