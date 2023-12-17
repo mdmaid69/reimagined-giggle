582aa76d9a9a8653b57518a516291ee45ab9fc73
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:33 +0000

    so scrape zero while lookaround

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
