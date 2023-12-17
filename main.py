451fb34c41bc6fa0d26b3b59f10dd5420d83b649
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:37 +0000

    calm worse move rain extremely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
