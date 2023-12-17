7b1447accf6f48e69ccd4b30682da1972ac7555a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:37 +0000

    tongue keepoff woman flow alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
