8441fedc6ffcc6f3b950703e6d8421f830b28d0a
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:19 +0000

    string promise cry Jj serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
