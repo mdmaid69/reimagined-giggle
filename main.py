5c99588ceed8121c3efa7540c34f5e44b6498533
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:27 +0000

    instrument shock inform think object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
