7521727adbf6f6073a1c0eb117663c17eb8f6bad
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:20 +0000

    slip necessary test realize unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
