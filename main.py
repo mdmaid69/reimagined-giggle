7fee48cb4935907618c80e801cebe8ea764f53df
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:22 +0000

    grateful impossible tongue impressive man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
