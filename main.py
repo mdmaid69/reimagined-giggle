126e76e32f6b0579d05bc3eed9092221cca68268
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:18 +0000

    forgive brain admit gate street

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-name = "Python"
-print("Hello,", name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
