1b04b52942b725ec981a7c1c84428b47803cb5b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:38:28 +0000

    best understand waste mix during

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
