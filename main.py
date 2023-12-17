478c0edb1587ad689c7d290e49dc7b6fa93937b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:44 +0000

    Nn also breakout convincing appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
