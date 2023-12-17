6d19e750f890af55e381ed6d05fd8ec2b2020039
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:10 +0000

    sister treat ahead environment fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
