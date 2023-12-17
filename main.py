4ce272229f6f56f3737c17701c49fee9dc28b95c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:13 +0000

    guess level advice edge border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
