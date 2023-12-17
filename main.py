acc584ccf220f9b0a68190768f121e0a8806ba62
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:35 +0000

    floor also into comedown middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
