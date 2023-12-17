584f1bb1d09185adc5df721baadd97914b36a51e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:32 +0000

    purpose display holiday do owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def count_characters(sentence):
+        return len(sentence)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
