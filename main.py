bfe832df42ba3305bedd00740ceff8dcc1b29c66
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:22 +0000

    pupil back male insist invite

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
