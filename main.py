def1d56941650e2d98c98890c077f387a0f6ef92
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:06 +0000

    combine mind makeinto flat precise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def cube_number(x):
-        return x**3
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
