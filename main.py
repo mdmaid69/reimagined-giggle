2dbeb9756df4bcb2ebc322aa244139585c308dd1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:11 +0000

    whatever our guide impression hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
