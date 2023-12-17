3cd5ac608aa11c63015677e8062e13d5838a0cee
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:12 +0000

    definite hold brush catch opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
