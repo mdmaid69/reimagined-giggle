59de26a3c45be7bb6f4c6b015eede27a7d506620
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:08:51 +0000

    until face tour lead influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
