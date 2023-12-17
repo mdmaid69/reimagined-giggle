ded9b8791ae7fa8aa01df940b7379c7a3fa38dc6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:08 +0000

    pattern nurse develop copy subject

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
