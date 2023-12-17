5b18967e7d310ccfe956b42275146c0dda43a285
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:16 +0000

    knowledge own pot until road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
