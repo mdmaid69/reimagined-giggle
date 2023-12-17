0139a95b0f82406938fceae1670639a9bae679db
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:51 +0000

    ability here ilovedicks equipment its

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
