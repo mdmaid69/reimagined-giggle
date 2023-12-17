da72dd71231bd37b580393b963f4da230f3b60fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:36:28 +0000

    past grateful legal continue my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
