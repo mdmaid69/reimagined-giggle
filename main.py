ec131c8ca26d9053185e61326034f136d31fa785
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:45 +0000

    relative hour male increase near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
