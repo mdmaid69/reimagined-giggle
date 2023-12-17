cd034c24380f483a8b25fa0bfca36bfae92f19bc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:23 +0000

    reach interest she cover record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
