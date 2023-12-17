92c7dd076eacd5cf886a355b73637e0a8c9925b7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:39 +0000

    steal cost tight joke keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
