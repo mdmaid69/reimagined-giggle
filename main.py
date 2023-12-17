2d4f18e348a10686e6b537bf7ffb2e1d69bb07b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:47:17 +0000

    card fortunately downstairs show mood

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
