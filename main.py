c1c16195ade061fa3e831c13ee72b2d3e3789aac
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:35 +0000

    at name any earth attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
