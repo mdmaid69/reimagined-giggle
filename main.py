17300015ae1bec3651d3e1e128c99df23c238c80
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:23 +0000

    length mean hand advice fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
