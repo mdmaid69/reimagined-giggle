ed5ba8510e65f2e29e0b40bdfec1421b5e3fa1f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:02:47 +0000

    his make plenty 568energy accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
