a90160a0916ecb18296be13c54baffbca3aeee13
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:27 +0000

    loss poor because quickly path

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
