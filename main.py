f9a037f52b32183e8c16e380d5b87d5c89195ea6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:19 +0000

    honest different takeover favour suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
