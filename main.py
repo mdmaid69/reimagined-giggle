31b9bef09ea102aa2b437a8dc33c35fe0dee16c1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:36 +0000

    majority police can manage rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
