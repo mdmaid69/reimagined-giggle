6b8524bee21134a8d9dbb6a98cc22b7c32e83b3d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:27 +0000

    pattern defeat goback(to) fire without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
