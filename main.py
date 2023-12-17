ac3e42ec5017a5ac1edd0065a4b7a0af59568ae5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:21 +0000

    single help mend project board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
