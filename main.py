0c03d03f2fc8a112dfb956c1fb7baeaf70aa6421
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:29 +0000

    belief letter thankyou minority comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
