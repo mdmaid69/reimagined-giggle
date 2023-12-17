f5786da8ca028d8deb82a97a350238d50b54c664
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:20 +0000

    instrument tone quality become better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
