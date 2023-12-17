049d12c0225972ed0514d7e25e6b96200c43a87d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:13 +0000

    hand rule carry branch substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def sort_list(lst):
+        return sorted(lst)
