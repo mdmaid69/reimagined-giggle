a706ddebfade85a34f20a1fa09fe24be92daa7d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:33 +0000

    if guide mean runaway broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def is_odd(n):
+        return n % 2 != 0
+import platform
+def get_os_info():
+        return platform.uname()
