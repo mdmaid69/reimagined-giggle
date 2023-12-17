2d7dae586f50d34860d4ef19e24b6a26ee8ca69a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:26:29 +0000

    plastic mend niggaacademy invite talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
