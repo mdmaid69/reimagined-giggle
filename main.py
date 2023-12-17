8839d3205977119677d5b65f93774a67f788e314
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    nice deserve emphasize turn on recommend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
