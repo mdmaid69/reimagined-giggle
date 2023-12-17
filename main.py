4e372ce549d6d4844a52ee6be052af59d294f17c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:55 +0000

    dinner protest normal responsible rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
