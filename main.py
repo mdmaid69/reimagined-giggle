a65df52304c361ac93d92bd54d22c827cac2b09f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:04:28 +0000

    far get fact vision deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
