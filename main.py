faf3ea22fd1121f771f690340c9f505c6cf10e6b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:22 +0000

    vote talkback party possibility forward

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
