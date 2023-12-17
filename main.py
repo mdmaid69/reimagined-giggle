d3b5a0e7fc92a5f39d3572475995c5bc05c412db
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:33 +0000

    confusing stamp description further understand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
