4dd991812e8b9755062a3b4faddf515782006558
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:28:42 +0000

    breakout tall share them grade

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_rectangle(l, w):
+        return l * w
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
