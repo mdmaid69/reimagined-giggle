72f0c3589785442097a4aa263146cbbd26bf258d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:39 +0000

    today busy ours guess criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
