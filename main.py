096a0e84f1a48e0da858751bb9f68dc83ad45e4b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:36 +0000

    exhibition computer blame distant ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
