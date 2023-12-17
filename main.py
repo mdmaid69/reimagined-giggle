60529bc818997ba87476dc8b6d118283e99a0e53
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:04 +0000

    formal immediately walk giveback shame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
