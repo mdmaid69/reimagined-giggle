1e6c327306693d093020a91e498faf7f69094b8e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:57:35 +0000

    turninto sad impossible hurt treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
