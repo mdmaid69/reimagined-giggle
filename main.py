aece733f8cfe20fa6784b3b5cb80b4ad4fa61ef2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:08 +0000

    567enemy room conversation trial best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
