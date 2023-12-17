7e06bb68e60afed0d846f4c1c90782aa74fc6813
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:06:23 +0000

    card complete phone minimum drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
