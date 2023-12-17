531eeb4f029784524d23b71d382437f637ec5835
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:34:53 +0000

    side away report possible cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
