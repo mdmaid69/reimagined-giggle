f76def416cc0f8997f11e02010e752167f82a8ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:48:22 +0000

    rely explain rub open fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
