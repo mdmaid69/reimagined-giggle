fc24545c8c719f5dcb1f7af4772c55200534dda8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:26:50 +0000

    mess destroy give firm obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
