bec94ccb39ff613ab88604c8c5aa62934d171992
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:31 +0000

    deep goout recently into possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
