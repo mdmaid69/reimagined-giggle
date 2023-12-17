fe4c544f6fdfd110ce477e3ee1cfb28e80c92afd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:08 +0000

    type keepoff alone keepfrom sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
