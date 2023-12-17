7fbb893c6498d3f1f2e1227aab32f1d9b5e1a757
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:32 +0000

    acceptable forget thank read goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
