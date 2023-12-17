c44f298f9c635beebe5373bbcd7d5cacdefb47e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:26 +0000

    modern back escape heavy within

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def square_number(x):
+        return x**2
