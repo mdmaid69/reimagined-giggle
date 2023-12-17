65953e0947d9e21fc759dc0f9e3742d6520a5553
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:31 +0000

    complete car subject whistle getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
