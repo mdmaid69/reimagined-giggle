9d5422c26a63949ca260634b481564bf5d402beb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:10 +0000

    error consist commit pot cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_list(array):
+        return list(array)
