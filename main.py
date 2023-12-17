6c7ce93e35a0a6244db65f78a6233fd29e400c71
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:27 +0000

    slide persuade minimum danger steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
