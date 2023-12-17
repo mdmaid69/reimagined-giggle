330b3a72621ade700a30872459ddac044d897392
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:46 +0000

    except little body whenever amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
