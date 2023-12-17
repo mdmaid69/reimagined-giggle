8dbcc8432e4cc620865befce13964bccd6979221
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:42 +0000

    every except boring radio over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
