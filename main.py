1118ddc3fea4d26108c944ea903c23b02f36a968
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:12 +0000

    reserve earth stir modern remember

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
