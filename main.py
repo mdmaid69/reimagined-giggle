1c5ab927f8f1b1874892884395f7fd135af6dd21
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:16:10 +0000

    thinkof produce luck thinkover hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print("Hello, world!")
