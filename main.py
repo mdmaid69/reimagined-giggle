af7bbbd2e0c30568e1110e09ae1e078c12298acd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:06:22 +0000

    thinkof goup ride straight family

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
