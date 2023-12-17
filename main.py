4f649f3fd6ab4c4ee4810a11f27e4814b2c077cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:56:15 +0000

    mixture impression dangerous ball office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
