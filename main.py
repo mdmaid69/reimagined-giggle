8f501dd3e7d2990e3d441e85c20788b6f1fcc525
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:55 +0000

    left form when than face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_as_list(array):
+        return list(array)
