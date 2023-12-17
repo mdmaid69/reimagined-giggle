7e5efe55c427eafc9dcdd71d270b8dedfa891af6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:47:44 +0000

    anyone head path instruction might

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
