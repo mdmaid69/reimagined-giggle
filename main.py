bb7c36b94d9c7f507b6a13dbaf7e32124430efc6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:15 +0000

    lunch uncertain settle spin get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
