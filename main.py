1b17e31bb17fdbc757c6ce6e3a2bb528566be6f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:11 +0000

    square movement circumstance meet bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
