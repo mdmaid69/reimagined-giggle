4a6706eac5dd81c1b823ce3e6951ba4eac9df24b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:18 +0000

    pretty neither pump getoff tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import tensorflow as tf
-print(tf.__version__)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
