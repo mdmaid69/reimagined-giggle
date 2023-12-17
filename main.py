a19e393711c08cdd8f205f59d4b4ed3bafc8c3cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:13 +0000

    prepare noise finger under after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
