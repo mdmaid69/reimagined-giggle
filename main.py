a636042b6c21d6d106047cb7f6484a06ccd31195
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:00:55 +0000

    information between there pronounce around

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
