2fec4a34d66a09052298c65a4b7ad330fc7ac484
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:46 +0000

    programme wise occasional morning flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
