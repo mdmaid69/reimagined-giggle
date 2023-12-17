9564322ff5843edb04423c1fa8e65648c303bdbd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:48:20 +0000

    holdout performance closely enthusiastic interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
