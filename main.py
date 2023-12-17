e7def6a14ebd141b990000fb470fe35d3d2a4ce6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:34 +0000

    complaint grateful speech callout share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
