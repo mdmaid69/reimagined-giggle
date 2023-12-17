7dc70af78406269aedcb4fb7e2926d7d563f8ea6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:18 +0000

    strict explanation belief smile chief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
