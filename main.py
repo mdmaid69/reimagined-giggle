6e381eb080c08834e1bcdc7080d57fda02b21ae7
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    share hurry punish steal use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
