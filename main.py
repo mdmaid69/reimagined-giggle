994e65753ef393222b108c77f1a6b9017a21e4f8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:29 +0000

    height in agreement flow row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
