ada233b20ea4c2398fe0982f82e3d78b736dcf23
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:36 +0000

    march hard case bury interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
