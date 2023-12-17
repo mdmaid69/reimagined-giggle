4772a70262fe6d58ade8dcd91b089afcbe1789d4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:10 +0000

    friend which task pressure after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_list(lst):
-        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
