b1e2a37e0aee7ff88c075e7b04cae87445e8ebe1
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:26:55 +0000

    such bad birthday smart square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
