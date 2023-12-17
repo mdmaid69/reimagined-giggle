d4abc9c5a45b31b95e84492d36efcfdf1ff9c0dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:50:14 +0000

    other strange cheat seem compare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
