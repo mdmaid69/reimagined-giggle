661c08bda66e0d7e0af94b940db4a4f653cb9cc0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:06 +0000

    never definite boil very row

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def convert_to_binary(n):
-        return bin(n)
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
