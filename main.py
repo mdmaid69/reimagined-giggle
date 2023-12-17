96e79d6acd16c444bada38475ab095289b19c3e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:06:56 +0000

    clean picture spend start tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
