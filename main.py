7c54a7426d745414c8353bd6c50fa5d17d413db9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:36 +0000

    your bound related kind exist

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
