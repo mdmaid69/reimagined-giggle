dfa2b5b8a550d7496f8dc87f8647a8bb68724e50
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:33:09 +0000

    gradually pay as cry cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
