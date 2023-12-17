286578d4dd31b669bddee49b5ef0dfa526b0fbae
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:11 +0000

    wrap train easy single breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
