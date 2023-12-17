0d667e36559544b42abde9bf0a98e99c9a7a3d11
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:08 +0000

    manage final march bringup fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
