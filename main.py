fe47ff91bb043205a7c2086bc746a24c36328f1f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:05:19 +0000

    go take sight appear kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
