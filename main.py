5326e285f98b1153551aae1af3ba116eb2202d39
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:21 +0000

    turninto handle listen recommend forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
