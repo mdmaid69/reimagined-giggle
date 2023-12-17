570cb597a175db62809552d141117876f1d7e03f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:05 +0000

    exaggerate lucky far during properly

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
