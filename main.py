a6640c381946ac8e2992c61f9e9f0982ef81f2b1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:01 +0000

    fortunate expensive embarrassed action pupil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
