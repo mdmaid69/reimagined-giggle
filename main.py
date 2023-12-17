d32b6bbd49ad7e762340852a605e03ec58d7bb6c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:03 +0000

    nurse drink thanks block influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
