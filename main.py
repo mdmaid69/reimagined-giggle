12f772b9ca94bb4322d603ec79ea4960ae9e3ecd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:38 +0000

    trip step press pull priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
