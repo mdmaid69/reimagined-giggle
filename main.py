46db10e3dd08e483e50582050c28183f4fc81958
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:44:51 +0000

    seat leave opponent story tread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def multiply_numbers(x, y):
-        return x * y
+import os
+print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
