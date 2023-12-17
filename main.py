781bafaca56ea0cd0d13d15dacbbd64428adf372
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:32:12 +0000

    interview gift deliberately cook think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
