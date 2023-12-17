925f9fe9a34484f7185a1b36fb18fbe8105a9e92
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:10 +0000

    no suppose doubt instrument side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
