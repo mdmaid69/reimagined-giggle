dcacb36c973c774d79bc1cee931e311526740b47
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:34:06 +0000

    row illness level invite flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
