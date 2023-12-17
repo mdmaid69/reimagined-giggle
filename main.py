b0db5ba5389d01410fc9ab8348323df469735c80
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:31 +0000

    doubt choose dick fire detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
