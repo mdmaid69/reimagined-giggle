6aea4b8a1eb4648b41fb362b18dac0f2ffc6353f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:49 +0000

    nurse flight walk turnover too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
