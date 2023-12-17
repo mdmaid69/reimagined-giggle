9833b14bfb9c5c9f0ce469ace0ac84001fa05851
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:50 +0000

    awake classroom than would raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
