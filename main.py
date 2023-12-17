976c3b5331d619098082f4ad40f0104da336b624
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:21 +0000

    remove tired characteristic public extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
