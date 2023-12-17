9b6882a3015b6ae32280efbdab233f6c9c0315b9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:04 +0000

    culture pretty this pencil ticket

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def add_numbers(x, y):
+        return x + y
