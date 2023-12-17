3c0102ab61aa67de2b78df2e3df5fa5caf84b032
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:30:01 +0000

    emphasis report fold during check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
