01106bd394199934677b5666742c614357a30386
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:26:38 +0000

    deserve regret wing everyone relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
