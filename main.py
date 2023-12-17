9d32bd95645004779d9d7716aa31af35546099c8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:57 +0000

    noise bath future many fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
