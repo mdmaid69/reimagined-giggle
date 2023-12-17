793f908ea71d5e093c764d12766da213836f007d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:21:02 +0000

    free know nerve while nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
