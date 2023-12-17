36a87a7374266546edf8352cbfeef7acf2caa5f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:26 +0000

    murder cruelty comedown certain stick

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
