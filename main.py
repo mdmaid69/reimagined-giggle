155329ab7852ace1a1fb84a5ea12b77882d02143
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:05:55 +0000

    love when future patience visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
