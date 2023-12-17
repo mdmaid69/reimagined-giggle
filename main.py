95d7165dca7c35d6d5836e9492efbcc226fa1d2c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:41:25 +0000

    afford want plan yours new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
