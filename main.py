592ff40eafe9b12b6c9d705cd20d3aea5ebd9a66
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:28:54 +0000

    period much imagine boat getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
