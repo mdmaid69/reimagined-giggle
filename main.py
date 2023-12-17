c75a12bd4e8b941643964f78ec9cfba60ae01fd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:12 +0000

    measure region advice off raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
