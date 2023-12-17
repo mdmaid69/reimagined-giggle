64219b8b16749aa4ebf301af2bef356b4fb2de5e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:16 +0000

    meeting record nature handle outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
