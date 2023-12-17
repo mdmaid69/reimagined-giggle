5bb9a0e8d963b663d69c6b785de80b7f2c6a16df
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:34 +0000

    interruption nothing practice mark cold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
