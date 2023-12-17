fc64a5cd7f4b2b683f6c405ef03cd5453a9c8a9e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:55 +0000

    range many conversation nigga second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
