c040a601d31441a1f7e9286b8c8d8796716bd56e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:30 +0000

    mind line country custom 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def split_path(path):
+        return os.path.split(path)
