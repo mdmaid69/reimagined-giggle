e0b6aa959e25fcc3132a841c0a0e8915da8d40cd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:43 +0000

    every lose store keen feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_length(array):
-        return len(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
