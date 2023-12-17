f4d388aecbb599871044012574b9b063b89c0906
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:32:22 +0000

    previous buy bet getup moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
