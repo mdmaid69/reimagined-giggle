94adccfcb888dc02e2045efe34d33f40f500848f
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:47:55 +0000

    murder date share or study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
