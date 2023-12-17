d6994773c284bd898aaa544f872852f72f96cac4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:26:48 +0000

    judge in matter central claim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
