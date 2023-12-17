fc52fa2ad9cd6289b83b0e7b316b5e2902b46eba
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:25:42 +0000

    guilty live eat funny excuse

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
