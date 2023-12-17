37b146abbf1106f32c61d453e5e455c21c7523a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    correct later  impossible wander

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def get_array_index(array, item):
+        return array.index(item)
