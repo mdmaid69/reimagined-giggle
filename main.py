9e9f0ae8d8918584c6f77d0ab4d4e55a6a8abf33
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:35 +0000

    nowhere no independent wall shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
