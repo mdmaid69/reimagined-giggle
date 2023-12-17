b889107ad14c09c1026a3b75ef87981d015b9103
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:23:58 +0000

    wide generous goback(to) exchange in

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
