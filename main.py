6d9333d48dcd5d85c48e13263d971d3bf7a3e3e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:30 +0000

    difference lead brave recent no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_factorial(n):
+        return math.factorial(n)
