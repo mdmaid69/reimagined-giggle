c1de63c288d49f875d5ec9d752b9febc4becd3fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:43 +0000

    best claim open discover bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
