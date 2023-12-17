50ed7c3400b6543e8f8a2709e692e5b83f78be83
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:47 +0000

    drawing again noise mean schedule

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
