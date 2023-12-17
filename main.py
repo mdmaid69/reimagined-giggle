ac37e43abe001d641d2b1a1e8c76a891f34e37fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:27 +0000

    serve total pure list home

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
