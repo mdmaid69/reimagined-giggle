46168eb77bf6c6ffac0c3754adb6b0c4e009e868
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:56 +0000

    care long process square instant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
