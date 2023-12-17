81268ef051b6393b644bcc44d0d9f21dc14cbdae
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:45 +0000

    spoil live compare deaf spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def cube_number(x):
-        return x**3
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
