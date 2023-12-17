6219e676a2d3473741af8cb7812f9bec65bb8596
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:41 +0000

    colour woman suddenly consider benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
