87ae5f00abb72b710fe3f131c8bfec03597f435e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:51:26 +0000

    doubt how declaration male compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_list(array):
+        return list(array)
