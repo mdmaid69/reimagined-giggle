550d9e53df4a793f1ae06fb0459ec8c962ba249c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:57 +0000

    rich mix tone shrink most

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
