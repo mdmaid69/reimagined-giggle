629babc449490aeb6e3ae7f7ce8a3a788075091e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:40:14 +0000

    flood short line keepup waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
