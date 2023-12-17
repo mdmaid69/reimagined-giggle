ed24017912bdc92dd85021e9dbe4ce5119374c7a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:54 +0000

    port appear flat inform nonsense

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def count_words(sentence):
+        return len(sentence.split())
