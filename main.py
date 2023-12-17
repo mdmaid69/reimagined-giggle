8fec984910e0a8805638ccb5ef39b8a4faa6b039
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:32 +0000

    goout destroy begin steam best

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
