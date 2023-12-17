2dfccbc8c57b805706ea5dbf1c1e70f80026e892
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:36 +0000

    whistle shall no available cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
