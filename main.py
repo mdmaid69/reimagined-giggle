9d1b645c471e383f50146e48828599781f6d19a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:55 +0000

    recently communicate discovery leg Kk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
