43390549b210752c6ca10a81f9322d947304b1b8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:22 +0000

    impatient dinner admire pause drop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_python_version():
+        return platform.python_version()
