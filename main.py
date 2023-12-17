83dd360e7be7962677a2fc9f19e179da87a43168
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:20:10 +0000

    cool spring unable during contents

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
