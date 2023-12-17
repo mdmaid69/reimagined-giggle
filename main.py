4ddc002c0b2e127056e6341695d54bf06c52e774
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:01 +0000

    object compare immediate clean pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
