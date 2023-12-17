29bd12f6c1e545bcfa2c846802010996d9bd44e3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:05 +0000

    gentle question have row combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
