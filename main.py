20e5743a29596d31ebd5df3ef9bfa7ec179c3fc8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:42 +0000

    comeover over dick wet previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_from_array(array, item):
+        array.remove(item)
