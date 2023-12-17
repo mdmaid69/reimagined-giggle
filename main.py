7db42193829ea1124b3304f5ca17a02de799b2b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:20:55 +0000

    unfortunate rub fair recently fork

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
