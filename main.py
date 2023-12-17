01500caf10c0b5f202b47930bb8c193cbf1d1ebb
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:01:41 +0000

    tip hold free fat fill

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
