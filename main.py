2c5b71b208ffb2e36882724f5453e6ec09461caa
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:56:44 +0000

    measure switch long gate shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_queue():
-        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
