73e0ce2953b5301bc9307960f689bf974a9937e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:37 +0000

    period transparent slip indeed account

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
