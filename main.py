ee2473c7dfb3c0bb2cc3a5955b5ba3288f518293
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:31 +0000

    force fire praise exact sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
