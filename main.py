77dc63b56b12397cbbb7f9eeac88c47f185e5d10
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:59 +0000

    across interest through present leg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def sort_list(lst):
-        return sorted(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
