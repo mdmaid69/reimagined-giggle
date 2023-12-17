04d47e9892908688e81f69dd62ea4ddef4f2b37a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:34:02 +0000

    heart keep up loan method under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
