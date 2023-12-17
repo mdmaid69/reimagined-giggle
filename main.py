af6069ca9f46d279f6caec200f54b913c9510c30
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:45 +0000

    factory accordingto nice decision like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def get_array_as_set(array):
+        return set(array)
