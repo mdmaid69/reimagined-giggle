3c2d09a2ef61e0e75520cb121d84cb490f79e13e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:41 +0000

    present trip real yours fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
