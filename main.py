db036fb31a3f3b665a6225d19e5f1a1401e2e956
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:16 +0000

    stir better very gap immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
