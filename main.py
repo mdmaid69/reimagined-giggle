7c2dcfe6238f72a1118a4420298da4d55f01c78c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:40 +0000

    gift need line year improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import json
+print(json.dumps({"name": "John", "age": 30}))
