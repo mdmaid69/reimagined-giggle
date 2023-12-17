bd8591b09f0f21cde1f532527b7550c20d274768
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:15 +0000

    loud born and performance rise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
