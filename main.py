0d421efcbb0455f26d928ab72b18f843ab3a14f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:02 +0000

    matter correct cope uncertain step

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
