3a8c27f4e7e87fd0a179e81baa29f1e9c8fa7a40
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:34:30 +0000

    wrap drawing rent near view

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_current_directory():
+        return os.getcwd()
