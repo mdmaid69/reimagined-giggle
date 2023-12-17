9b909f3f5d3c22053b7e506c4caac697e8233f02
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:10:51 +0000

    strength his by middle authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
   def get_base_name(path):
         return os.path.basename(path)
