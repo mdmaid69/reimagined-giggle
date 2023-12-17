9c05e8a9ae3a6a4479d61c483afc84466e17cd3f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:00:15 +0000

    watch laugh conversation watch stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
