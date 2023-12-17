5c5f58543944b991467f23d9ee6f4323bef8d3c7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:14:42 +0000

    content miss my low name

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
