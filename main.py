77165f45bf9ab79e2705a9d34e74abe4c55734f3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:41 +0000

    breakfast offer neither university possibly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_speed(distance, time):
+        return distance / time
+def square_number(x):
+        return x**2
