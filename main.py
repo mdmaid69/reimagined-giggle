8e61eaeb6305a9a62434a355399102c96df55946
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:04:28 +0000

    allow target hurry touch part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
