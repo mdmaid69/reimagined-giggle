efb593b83f89567ae5f27f227a01b50d596363de
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:27:20 +0000

    centimetre eager smile material lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
