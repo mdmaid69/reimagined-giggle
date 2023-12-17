ab2b9d096157808a83d94d5ad9d89906c73ab75f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:18 +0000

    sharp breath thing settle admit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
