ab1e1b308632ebd5b941c8d9d9a826a8a2567140
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:11 +0000

    point move far reach busy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area(radius):
+        return 3.14 * radius * radius
