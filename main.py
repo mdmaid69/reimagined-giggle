eb0f79be85898830aacc1fda64c38b2c7505b420
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:30:56 +0000

    fresh thin laugh brain dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
