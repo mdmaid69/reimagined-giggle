694770fa78b1a89993541a488811850b7b1973d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:12 +0000

    study powerful nonsense card can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
