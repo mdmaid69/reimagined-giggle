956a2390327eb6081a4ca6465fc04ba7d9913420
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:36 +0000

    flight evening accommodation house less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
