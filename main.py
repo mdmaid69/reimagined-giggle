519645a34e250c39d5afc935b859003e7352095c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:24 +0000

    important give pump list crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
