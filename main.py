a71595cfe88e94d5859ad9401c44f7bb18731f2c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:33 +0000

    argue as declaration that means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
