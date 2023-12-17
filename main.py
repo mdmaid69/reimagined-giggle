76d1dccdaef22a6f3746f2dbcc4a41f802f1fc0e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:24:11 +0000

    appeal emphasis empty important reduce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def convert_to_octal(n):
+        return oct(n)
