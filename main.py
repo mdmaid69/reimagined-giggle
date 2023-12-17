052b2a8e7eaa7e94e7646f7ad0cdc1c27e208023
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:27:57 +0000

    base giveup still onto address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import numpy as np
-print(np.array([1, 2, 3]))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
