5cd21098acceaa0d6d833984e9e2ccc71e7e2e4f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:41 +0000

    defend colour really consist love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_int(array):
+        return int(array[0])
