77a7f2ebaa2a94c24d229d6bbc9848084b570bcb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:56 +0000

    connect his big up shout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
