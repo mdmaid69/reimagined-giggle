f3f0a24cd2be5b93020e5335ce1e470d7181a564
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:52:32 +0000

    effect tape achievement communication base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
