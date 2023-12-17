17d39ef7368d05a32977e56dd05a1190b7a08c37
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:42 +0000

    statement human run interesting praise

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
