51ef5f6f1a57d33e98523f47beddad91b3710fe5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:47 +0000

    indeed combination land law doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_pressure(force, area):
+        return force / area
