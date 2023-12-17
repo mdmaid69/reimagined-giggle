2ed3c6b282932b4d275b4b712cd5aa0de5a775ee
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:28:37 +0000

    throughout worry community female sort

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+import sys
+def print_python_version():
+        return sys.version
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
