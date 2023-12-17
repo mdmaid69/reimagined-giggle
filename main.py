230e36e110f27af62a57dee748f818268be23d0c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:06 +0000

    off receive land pause positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
