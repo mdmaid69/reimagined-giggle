414e7b59bf5a2d3e2074be135b8cebc3a04b97a3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:23:13 +0000

    compare force handle remark 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_int(array):
+        return int(array[0])
