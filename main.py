dd9e0e7818c63762559d9829169be65e0f8b7a70
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:35:40 +0000

    ring win walk control pay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
