a1298f925a69c0227f29181e626285e1766cf57d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:18:09 +0000

    as hope unlikely experiment interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
