bee86936b5fcc5231376e6e438f117ed6bc16c96
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:13 +0000

    kick test good punishment turnaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
