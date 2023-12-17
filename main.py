90e2726633dfb57605dc1dd425a8298012d9b55d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:19:27 +0000

    only black pinch future string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tensorflow as tf
+print(tf.__version__)
