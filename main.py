899be5671e2be9d17c3ce703687593fa6e31c742
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:22:24 +0000

    suggestion regularly control through runinto

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
