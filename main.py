f263bfd7d75f9d5d6b95584a40d98dea44499d83
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:25 +0000

    difficulty yours powerful generally bunch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
