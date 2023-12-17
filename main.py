17ebb955414e6e4e58a4beeaf7fd4c3b05c1a9dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:59 +0000

    though uncomfortable hang shade capital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-print(time.time())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def multiply_numbers(x, y):
+        return x * y
