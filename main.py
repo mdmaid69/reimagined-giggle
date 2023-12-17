4ede7f24357458086ed9077e6b2162b17c0e4421
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:44 +0000

    illness increase public convenient acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
