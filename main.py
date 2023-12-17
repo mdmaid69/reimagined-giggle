a30db807dd85a3ad22c3ae6ac19449d1ebbc51f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:32 +0000

    pump brief principle house reject

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
