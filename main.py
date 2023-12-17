09f72da0e8ffc9d20318afe15fd0ba6d68104823
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:55 +0000

    fine content machine understand kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
