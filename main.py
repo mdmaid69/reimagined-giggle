b88b751cd2ab1e9e37a35e6948da0d4941ff2ce1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:28 +0000

    ability memory question pipe Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
