ab58de08621840b60db7903612966e66bb2bcbdd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:24 +0000

    coffee beginning pipe fond finger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
