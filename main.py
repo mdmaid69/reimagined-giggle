ef2c561c6d7f5e5cfcf61ad3bb5ee8680eb4eb76
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:28 +0000

    unfair charge far where behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
