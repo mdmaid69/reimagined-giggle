7f627449efdefa6003b591a8cbfae9da7cc25eda
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:34 +0000

    class cause though order confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
