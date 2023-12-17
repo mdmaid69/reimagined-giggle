14e7eaeaf84035cc22e0062a04ad1c87c6bba14c
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:20 +0000

    serious together estimate lock month

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
