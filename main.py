e971967635870e20187879492936c89b72b801f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:03 +0000

    black find day 572enter lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
