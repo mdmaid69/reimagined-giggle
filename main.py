6f23598b6e29f2b8b57ca7a299e5e41a39d41a3f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:41 +0000

    spring we active punish whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
