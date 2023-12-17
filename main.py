757d9ff5afe76b0947006ebcdee871e560098e23
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:55:10 +0000

    partner influence centre ball possible

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
