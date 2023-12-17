c3caee46a9d727d36b1f2821f16028c30201f61d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:57 +0000

    crash unfair squash wave fat

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
