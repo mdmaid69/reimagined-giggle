8a986a9b60245b92f74bee20fbbfd4aae7ef01a2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:45:26 +0000

    deaf own rush difficult later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def append_to_array(array, item):
+        array.append(item)
