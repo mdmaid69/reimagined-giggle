0f5333c8a916da12da208d6e5259080a006cd5cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:51:13 +0000

    Zz speech this plain fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
