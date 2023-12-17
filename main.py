97bee2b05b87e29a1fdd6378dbc0b0afa60766a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:35:20 +0000

    store ability degree prove total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
