a920a6b748bce0673cd04ac68cd3cec677611572
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:56 +0000

    table breathe fire flow love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_min(lst):
-        return min(lst)
+name = "Python"
+print("Hello,", name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
