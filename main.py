48b054f8d301a7016f9809af4dd06c245a503c52
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:51 +0000

    lookup ashamed design project imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
