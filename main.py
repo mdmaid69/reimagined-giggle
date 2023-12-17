23132701ebf5c85864ad7c579b1edf3b47557051
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:10 +0000

    available again first plate example

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
