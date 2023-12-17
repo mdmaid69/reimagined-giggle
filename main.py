09139e407a80a20d462b26491fae7eaa971ceaa8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:10 +0000

    blue discovery law radio up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
