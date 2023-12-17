830c0d7a69fa2a0a1b51782c5fd54f5f3fe7f2d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:29 +0000

    no pencil pain fall find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
