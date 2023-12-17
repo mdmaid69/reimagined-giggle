10b5793076cd239aa7459ba13ed9bab94b9af5b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:38:38 +0000

    full answer first direction keepoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def divide_numbers(x, y):
-        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
