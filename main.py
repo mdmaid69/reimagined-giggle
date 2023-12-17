7899f4f0850c1ef407fdce2ec880f9116168867b
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:18:25 +0000

    get deliver hear kick survive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def subtract_numbers(x, y):
+        return x - y
