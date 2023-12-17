7de621c2d3e9019d2a94c0bc9f99fc416183e3c6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:37 +0000

    make Kk trousers lucky exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
