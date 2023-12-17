508dff7f935242b24c6f4d30424b505d5d4e4fcd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:16 +0000

    cinema trip takeaway lot no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
