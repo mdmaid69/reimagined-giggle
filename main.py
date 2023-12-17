28a5536dfb6c99327cfca555e51dd61c8b9142c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:25 +0000

    note settle many smell pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import time
-print(time.time())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
