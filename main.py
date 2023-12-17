d3478bd9ac9325ceb398c7b3a06a89e65b46496e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:29 +0000

    dance funny somewhere operation drive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_repr(array):
+        return repr(array)
