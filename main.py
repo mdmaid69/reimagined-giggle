8cd461b57147ea9e68e93b40f222365bbd87b3e6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:47 +0000

    comeback bringup safe bear season

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
