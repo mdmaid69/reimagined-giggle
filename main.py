8d1fd00b7d07353f7b52db1a80b9d116970fe777
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:13:27 +0000

    put slice rare back immediate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
