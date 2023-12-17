099218c3c26974c14fb43399e8b34cbce6f22c5e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:00 +0000

    twist breakthrough act door professional

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_size(file_name):
         return os.path.getsize(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
