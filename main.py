d58c833bce91f478474305659430d78ff38d9395
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:39 +0000

    awake nonsense pipe beat lost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
