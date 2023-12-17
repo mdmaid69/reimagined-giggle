c834b9c2571365537cc12b623edc08a771dc0f1e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:39 +0000

    back confusing takeoff centre fire

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
