5d5e1397f1fc1bc1592579fcbec9b39a5da17fd5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:55 +0000

    beautiful listen frighten goback(to) pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
