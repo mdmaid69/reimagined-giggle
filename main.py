33c02a9c2b38d21d3e724bd9f75b0fb60cee152e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:32 +0000

    confusing sink gradually mixture vanish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_min(lst):
-        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
