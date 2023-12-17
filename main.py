e4aef49fc1487ffa42fc32d172863e2e73666d60
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:48 +0000

    shock platform hungry underneath front

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
