71745d44045fd6da140ebaca21fc602012b1d9bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:42 +0000

    listen ours too genuine on

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
