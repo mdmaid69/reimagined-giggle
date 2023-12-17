82f478c7c8869ccdc02acc9b3fc0ce135ac8878c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:52:30 +0000

    bound willing hard possibly thought

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def convert_to_binary(n):
-        return bin(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
