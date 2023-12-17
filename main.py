1a6d29169c725ec39369497d2eb558e6b8044d1d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:55:26 +0000

    within pressure goout little problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
