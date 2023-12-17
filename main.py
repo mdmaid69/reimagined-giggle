c71ebffbb395d021fc2f0ae6505bec6d99ed07b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:03 +0000

    seriously recognize after band bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+print([x**2 for x in range(10)])
