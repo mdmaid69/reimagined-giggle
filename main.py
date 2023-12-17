987811109adbf8a94b1ea650e29624db8a67dbdb
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:13:50 +0000

    buy secret treat plenty neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
