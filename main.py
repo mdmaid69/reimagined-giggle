48ab76dcdd72fbe33965e42e7f9d53c67d484609
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:59 +0000

    workon hurt unless arm situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_density(mass, volume):
+        return mass / volume
