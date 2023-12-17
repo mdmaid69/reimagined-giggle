537679598acc8e89887a524ee6cf2106dac7e200
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:14 +0000

    stiff convinced priority into male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
