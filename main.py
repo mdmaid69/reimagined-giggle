5cde80fd014ec993caded43daafd7612f68194bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:06 +0000

    block lookback(on) admit supply comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_acceleration(speed, time):
+        return speed / time
