fe8dd297f896a01b08667362572c75dbcdb8edc2
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:04:21 +0000

    ring public bunch owe various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
