d4d15cf2e48b839130b535330ea77968d1e32d75
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:17 +0000

    solve butter private strike lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
