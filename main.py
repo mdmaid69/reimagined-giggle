07eff8e02c3039482478be80bb8e002834da29d3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:47:48 +0000

    possibly slice room charge birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
