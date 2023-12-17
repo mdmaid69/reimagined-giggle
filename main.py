0ee67553d53eca4468f0a5ae6bb52e9cab6446b1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:34 +0000

    necessary characteristic somehow protect dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
