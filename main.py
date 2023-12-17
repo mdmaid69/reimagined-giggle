92349c106a1ef6a71a5bec5f9f43d2ef1332f1b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:06 +0000

    suit develop use favourite excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
