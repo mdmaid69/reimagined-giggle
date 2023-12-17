517a0893fb99d55a318b269b7437eda9bce19dba
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:01 +0000

    break bad press use house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
