054f263c6861159e8c32ccbd723936039704f8ed
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:51 +0000

    live workupto advantage Kk introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_directory():
+        return os.getcwd()
