b9c4408658f2ea2cb9f4891ff6e121949173746e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:36 +0000

    left escape escape blow generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
