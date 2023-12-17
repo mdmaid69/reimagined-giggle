4ba996d6431a1c609ba52de249d4d5bfd7952315
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:29 +0000

    paper unit sort public fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
