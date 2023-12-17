14c002e5a3437e5f5ead93b38629e26f9c792f85
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:03:23 +0000

    swimming indeed sometimes walk repeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
