fdb0d1e6ded62532a70de9da9c8500578c471c64
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:27:10 +0000

    since basically burst rain visit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
