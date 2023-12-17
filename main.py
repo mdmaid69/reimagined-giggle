cb7b37ecf0b6c04cb062cabc6157e99760126eb2
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:48 +0000

    affect heavy keep out  typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
