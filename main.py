29449c35c020e87d938e62d4bffe15627d0739ca
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:05 +0000

    spill final lock passenger message

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
