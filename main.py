55a023fde9a5b4e70894d6fdef8d3c17652b9212
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:55 +0000

    trial whether turnup somehow beyond

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
