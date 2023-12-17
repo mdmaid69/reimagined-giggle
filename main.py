3f5bcd8f323138f54a0d9dd9c0b5b53cbaf19fd6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:49 +0000

    private paper turnout say kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
