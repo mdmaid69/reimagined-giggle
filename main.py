97e1f64c3fc6541a8a2d10fa1964337b473a2170
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:13 +0000

    drink mood fierce simple shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
