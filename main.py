3b2a4bd4ccb6e93a3c2993374d34b2b3c8ee77f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:21 +0000

    cheap coffee autumn off giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
