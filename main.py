84521c1900943542aac414606dd3b0a8a2ed6426
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:50:08 +0000

    wire quick allow control ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
