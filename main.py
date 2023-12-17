e67c72ebc87201b1b57a3dff36aefe462e13f5d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:58:15 +0000

    beginning anything contents crash middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def find_max(lst):
+        return max(lst)
