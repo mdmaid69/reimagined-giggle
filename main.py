810c38f824ae7b43b1f1ea29eae78378a16f3a52
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:55:46 +0000

    concerned comeover criticize punish shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
