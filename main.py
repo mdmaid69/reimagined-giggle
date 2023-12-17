ef86a605c6755b779a153d591eecbdf5e7362ba6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:26 +0000

    tax side strong nurse office

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
