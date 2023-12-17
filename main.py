72427bedb7074b2cb4fa04e785cdf6b48c7cd79d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:39 +0000

    sit sentence explanation afternoon whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
