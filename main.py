3d307a566657470ee8aa6cd70e6fdf75ac9368ac
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    police example peaceful straight bring forward

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
