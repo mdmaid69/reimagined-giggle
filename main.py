8f65e46d7c1cad4270088492a243bcc4b4c9b69b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:06 +0000

    from best name part window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def reverse_array(array):
-        array.reverse()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
