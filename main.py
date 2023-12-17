8f6ec882a2a683f57250e113619af0f5722cf4ea
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:11 +0000

    favour discover regret accidental distant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
