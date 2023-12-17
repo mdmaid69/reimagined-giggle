f6ece64883a73b2988297d8974602e0c0e48d83f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:03:16 +0000

    Ee wait chin light learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
