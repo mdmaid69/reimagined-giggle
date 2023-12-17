38ef633a43815552a290fd86975a25d0ea45cced
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:39:11 +0000

    split light answer behind interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
