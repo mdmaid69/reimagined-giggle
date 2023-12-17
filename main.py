9b38deda390e794cf7bdebdaf2d524b00441fec7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:01:34 +0000

    invite equipment finger pencil sing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
