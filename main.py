d1e6ea9bb7229e8f91d76188e33da2f2867b8802
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:52 +0000

    train priority bored otherwise authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
