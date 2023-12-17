25f9cfbf7209713bc3c0a836eb67f02c3ca097ae
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:32:20 +0000

    accept clothes trick material thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
