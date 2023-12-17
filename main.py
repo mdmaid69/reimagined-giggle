43bd797ce585453ad3706b0beaeb4039d7cf9d52
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:43 +0000

    Mm hold duty knowledge change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
