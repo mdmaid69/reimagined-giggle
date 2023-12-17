76cf03be83b8fbb1c993b560b3af5095d5018c22
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:41 +0000

    share doubt area also exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
