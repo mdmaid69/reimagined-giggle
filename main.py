2408264272e94bc619b248e4bca90f531e320507
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:47 +0000

    repair along occur admire dare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_item(array, i):
+        return array[i]
