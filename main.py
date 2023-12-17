b41ae95745ae1a2ac4932944ad9b7d1b97c05f98
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:41 +0000

    edge join produce support transparent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
