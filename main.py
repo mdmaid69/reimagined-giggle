b6ac4833dcde95d0512929c6fd66382dac1d2dd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:11 +0000

    exercise between press yet

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def reverse_list(lst):
+        return lst[::-1]
+print(sum(range(10)))
