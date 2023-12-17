05636025c209f10e6292fd29ef65595002c63050
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:43 +0000

    name hard shake bank remember

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
