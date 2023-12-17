b329f2408d897a6863a9e6a78835ce77318d0ccc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:29 +0000

    connect something gap it plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
