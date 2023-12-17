8f8cce4afdc63fdd629c2e5334cae88ba05df0e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:56 +0000

    exactly contrast callout respect callup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
