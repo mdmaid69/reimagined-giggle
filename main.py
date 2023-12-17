d0fd0f6d23ae919b793a4f3b05c868da34cc18d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:39 +0000

    hang wait second hit fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
