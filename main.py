13e8feded4ff51bc4469a5278eb1e596dacb6441
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:13:52 +0000

    finish stage flight suit within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_power(work, time):
+        return work / time
