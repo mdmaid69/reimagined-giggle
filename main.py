e205a83afddcdbc02a9877a35242d8318727d981
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:18:37 +0000

    getout return luck never part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
