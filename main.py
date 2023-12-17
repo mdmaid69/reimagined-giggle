8537af7d9999897b6a96976676a7f7db1bc1f141
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:09 +0000

    trade beautiful bit believe frequent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def reverse_array(array):
+        array.reverse()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
