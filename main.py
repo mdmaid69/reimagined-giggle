c73392c5df7c5d188d517ff6eee534adf48d9426
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:27 +0000

    stiff inform half arrival culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import time
-def get_current_time():
-        return time.time()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
