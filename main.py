af83bb73859c2d3124cc2715090857e2d1b291cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:28 +0000

    alive touch later friend earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
