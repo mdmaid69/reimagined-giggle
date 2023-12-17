419ae1a33c1848caba8528d65ac3a2c5752907ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:50:52 +0000

    moment 564encourage land natural bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+for i in range(5):
+        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
