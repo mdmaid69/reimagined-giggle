0caef4eea5a29d80263e4e7bc68c84c134218bd7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:21:20 +0000

    mass hard save regard warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
