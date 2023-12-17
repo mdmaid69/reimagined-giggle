5230e1f9e61b234cd19c25e1d1f483a9393a61c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:28 +0000

    plenty cover collect any him

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def square_number(x):
+        return x**2
