cc2771cb302531f04dacbcac356cd0d067d446e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:46 +0000

    claim say respect damage sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
