4b915e96f38ce62d2f7edfa41c23f3d1f045b1b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:03 +0000

    dick wash project heart respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_str(array):
+        return str(array)
