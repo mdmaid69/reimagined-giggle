4696c29dff69eb20d71788180a8d6fe88a12f17f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:44 +0000

    defeat release die recovery preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
