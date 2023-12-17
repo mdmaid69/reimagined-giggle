cd4ef8c83ce0228df32c33d358636b4546f888ac
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:51 +0000

    before by permission open bend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
