f0beee61bf535f6e45439355923bd96eb06c7345
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:30 +0000

    Oo foreign trousers workout check

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import sys
-def exit_program():
-        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
