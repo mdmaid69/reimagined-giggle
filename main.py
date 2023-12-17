60782004ce88b75df8b131272c4416f3b1b6ecb9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:56 +0000

    cap promise fair team Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
