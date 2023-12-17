34709d67426f6232f83b8935985185fcca26e431
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:35 +0000

    authority after Oo corner congratulations

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def square_number(x):
+        return x**2
