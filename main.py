8b138c01b75cfdc08de01e6cd3f532f7314adbf7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:04:52 +0000

    flavour conversation square recovery pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
