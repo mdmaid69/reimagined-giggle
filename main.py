7959738c96f7ecf3146ad85a34079b7f96ad3386
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:49 +0000

    573entertainment accept education complaint occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
