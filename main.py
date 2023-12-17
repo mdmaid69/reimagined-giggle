f1b1974a44382072c8594c1e2ca22d0682b93e26
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    you shake morning book call at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def square_number(x):
+        return x**2
