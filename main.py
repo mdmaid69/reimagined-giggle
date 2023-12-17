6b8652624358b9ddd8927fee234f29f55deb5fa5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:48 +0000

    important apparently public copy speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
