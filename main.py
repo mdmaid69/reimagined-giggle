5adf69a6d280c9aa7d74a25924d657aae70b1773
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:57 +0000

    cause beautiful coat breath every

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
