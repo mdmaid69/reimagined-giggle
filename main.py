71a4a88c47d97477dc1d1c42e6a6865803ff5758
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:18:42 +0000

    middle possession high turnaway lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def reverse_string(s):
+        return s[::-1]
