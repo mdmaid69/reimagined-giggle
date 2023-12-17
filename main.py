c94dab63aed7ae4a67d9b11d841260cd47149e0e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:06 +0000

    address height stand order level

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
