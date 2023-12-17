831a2d209017c0870ef3595091c5e32ea6f110f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:43 +0000

    good plan pay defeat afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
