60a8432279bbf9071e4ba14cf665ce6c1be91b13
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:22 +0000

    wise town repair well history

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def remove_directory(path):
+        os.rmdir(path)
