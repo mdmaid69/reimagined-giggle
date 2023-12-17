5b622624bae0a3a278fd1d88b7aafcf785c329ee
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:54 +0000

    safety disappointed point close pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
