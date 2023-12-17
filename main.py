f2fe947ed897642439656fdf26e2cab3a613e536
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:39 +0000

    separate sleep trust exchange prevent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
