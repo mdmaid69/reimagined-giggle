3ab8f16807e378d469ec948fcc4d00b2dec9a371
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:00 +0000

    line ball never cough strength

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
