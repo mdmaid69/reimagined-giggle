1671b7813cebff96216250224417344406948561
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:14 +0000

    scrap stop proud full fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
