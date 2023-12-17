6937553c449db1c94a09e6fd9c6fe35758538eaa
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:34:45 +0000

    equal search soak his contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
