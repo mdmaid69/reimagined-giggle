c51b02a4775337738b1bc65fc8eae3886109e504
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:53 +0000

    make her often lot shout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
