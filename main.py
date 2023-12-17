bcbff0800bea0959091a42cb21d518283292659d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:33:11 +0000

    anywhere fine need sign pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
