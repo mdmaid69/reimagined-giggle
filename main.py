5655147d3ceddbcad6ecd0bc1f05b997b32c5647
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:02:04 +0000

    confidence direction holdon favourite actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
