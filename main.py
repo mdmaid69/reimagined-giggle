b668dbcaf3c3dc5f16d41049f85b472df836d8b8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:30 +0000

    cook stranger empty regret represent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def count_characters(sentence):
+        return len(sentence)
