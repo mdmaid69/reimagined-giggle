0856c193aee245640e445d5b2d1f606fccb8948e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:44:15 +0000

    drawing protection laugh surprised fast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
