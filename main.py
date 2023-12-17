c3e11de57e9cd0246d7f68995612ec528ef869d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:39:45 +0000

    so something reply few either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import time
+print(time.time())
