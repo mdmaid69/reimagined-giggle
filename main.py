b3e18e724a3c9ba68d70e1de07d48453b325e347
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:17:32 +0000

    cinema brief perfect match wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
