31885f859eb9241c8b76684a118514cfa037d3f9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:26:56 +0000

    note explanation under wave fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
+  def remove_duplicates(lst):
+        return list(set(lst))
