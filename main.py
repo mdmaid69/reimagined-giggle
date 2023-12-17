a9047c574d8e8225757938fe1232790b05c14c47
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:24 +0000

    OKorokay possibly teach once until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
