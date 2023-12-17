080d2ba57a8f5e2011cf4a2f50ca20c0d7a5fcc5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:30 +0000

    though feed consult reduce further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
