71ec8a9149af53fb8f660b59881db26a0bdd95d3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:08 +0000

    average corner block frame water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_string_from_array(array):
+        return array.tobytes()
