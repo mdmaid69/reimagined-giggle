509e830978272b0ade8aff29d657556191ab9b15
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:38 +0000

    laugh matter terrible slightly physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
