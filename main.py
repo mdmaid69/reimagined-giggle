18f5df8ce3d1d2231af6b393eeab9a82bebbffd5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:32 +0000

    otherwise crowd beside plastic cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
