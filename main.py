e64822490bf687644e6502669ad658939eb75654
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:20 +0000

    start part ability lend shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
