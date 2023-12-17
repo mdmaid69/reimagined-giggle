92ab89d7c1fa7b9baacd145e00cd1c7ae299f074
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:09:32 +0000

    remember emotion many dream impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
