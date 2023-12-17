d241bcab75fac5a4a8ad676e002c1bb6e37fe3f2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:34 +0000

    feed clear wave flat break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
