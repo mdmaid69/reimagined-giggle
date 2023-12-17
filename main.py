a9e2ef0e822cb66a0e612e8453df959e119bb256
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:51:27 +0000

    explanation break base recent pure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
