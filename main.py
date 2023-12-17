0b67db7d0637f91133548ce75895c63b737e496e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:56 +0000

    past remain will walk lookafter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
