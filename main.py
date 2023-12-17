2256ff8ef62c35462d520bf74b3760beb50f7a0d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:35 +0000

    create cut indeed goback(to) age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
