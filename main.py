57757929cd22a50633261f75a8f6d45b72bdec61
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:59 +0000

    179bite Ii comment protect own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def square_number(x):
-        return x**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
