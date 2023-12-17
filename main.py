a0328ca6caab911af025ec64a33be3516805ace2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:15 +0000

    confusing past reply tax crisis

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
