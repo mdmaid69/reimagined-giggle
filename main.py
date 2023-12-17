7a423464e1e80d924841ac6d3f269d82bedb8825
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:00:33 +0000

    waste talkoutof declaration release mail

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
