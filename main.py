c40914184b51e545bdbe32cdc19793c53737bb5c
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:03 +0000

    kick closed distant unfair somehow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_area_circle(r):
+        return 3.14 * r**2
