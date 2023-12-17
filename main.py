c504880d5412fca98b14cd85361ad25a5baf23fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:48 +0000

    prize more often soft holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
