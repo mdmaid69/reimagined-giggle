2ff35975166cda2f3412e574bebe6224a60a5a9e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:42 +0000

    distance represent hit wind laugh

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def get_current_time():
+        return time.time()
