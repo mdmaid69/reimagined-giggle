44d3accf7444325aab207726a05bf2a26eba965a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:59:39 +0000

    callat free acceptable himself exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def sort_list(lst):
+        return sorted(lst)
