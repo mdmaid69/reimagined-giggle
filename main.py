a210fa13843982fa22193c3d013ca77e04001682
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:11 +0000

    translate fail anywhere past slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_length(array):
-        return len(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
