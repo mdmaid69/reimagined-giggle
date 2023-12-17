0d813d9501cd7a806fa5b381c09f271f702f891c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:51 +0000

    container other sure to Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
