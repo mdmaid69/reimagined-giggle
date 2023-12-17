47f9b93429f25192cbc94f1de0a99df339c5a84c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:35 +0000

    talk quality communicate behave since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
