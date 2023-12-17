3b26aa3d9bbcf5c93244a0ced9777f37f80c6fe9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:42 +0000

    tradition pool tape goon section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
