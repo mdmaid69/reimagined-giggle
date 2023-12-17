0f29f163cb1bf2679c3b0543c821aab8d2323a3c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:45 +0000

    getback insist inside stare speed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
