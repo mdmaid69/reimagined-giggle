19ae18f5b64588e4cd68727e940daef9be2f4c34
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:49 +0000

    reliable frequent goon open level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
