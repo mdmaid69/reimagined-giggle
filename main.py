80cfb4219518a1824ca1eccd17a4f6418c01d3d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:20 +0000

    violence glass sign through understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
