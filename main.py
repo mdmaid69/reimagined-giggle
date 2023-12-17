3a504a7d096c8f91e309ba9737d26f9bb5f0b658
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:59 +0000

    visit replace interruption by unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
