c98bd8297bcd1ade972dfcc6ca78f8616135b8ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:49 +0000

    sail post interest water to

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
