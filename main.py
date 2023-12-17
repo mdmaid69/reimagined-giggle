26f7fc199b4594be3dfeedc1318fa17182c5aaf0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:55 +0000

    limit plenty interruption tell objection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
