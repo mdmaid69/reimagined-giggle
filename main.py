5b4509814023df93631275187bd400d30b8b2cf4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:53 +0000

    pretty string nothing drop exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def reverse_string(s):
+        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
