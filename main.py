3c075480460c5ecc7823e5da79284240e7cce646
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:57 +0000

    adjective add greeting comeround condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
