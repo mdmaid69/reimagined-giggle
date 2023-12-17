eaa1d62ed482bc5490abf5b7f99388203a46a8b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:30 +0000

    thin generous damp right book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
