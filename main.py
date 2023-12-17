8900e1be83b67e40db6c3fbd38b15b93282befc2
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:05:07 +0000

    right surprised lower check back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
