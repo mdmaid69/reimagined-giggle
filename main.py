efecab4216802bcff0b56ab59aa12a9b79364b22
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:13 +0000

    alone remove last afford church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
