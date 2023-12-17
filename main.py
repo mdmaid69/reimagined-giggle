8e699dd3b3af6b0f6c2fd6fa7cd534719c804aee
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:13 +0000

    stiff authority would neither score

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_rectangle(l, w):
+        return l * w
