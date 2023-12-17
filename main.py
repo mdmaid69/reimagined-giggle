a02a3595269bfb93a2ba26bcc87c40ca1e9b082f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:05:49 +0000

    switch curious light walk should

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
