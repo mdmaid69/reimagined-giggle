1d64869e6826447c15ae104e2e2c456d43f7b9fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:03 +0000

    father remark often after ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
