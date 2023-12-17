96737609ddc1e0644a5a394ef7116b1738566883
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:31 +0000

    ticket lift either happen skirt

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import re
+print(re.match("h.*o", "hello world"))
+print(sum(range(10)))
