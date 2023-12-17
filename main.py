4652b95697976ab8919401044254e3c77cdd39a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:39:23 +0000

    tidy bit pronounce learn turnup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
