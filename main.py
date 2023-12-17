3df0587707a0690e8ae6cce8eebd0effa3de3cdb
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:24 +0000

    order then miss effect hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
