17a89d5d03d8ae931ae7c226fbf413d149223af1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:19:29 +0000

    free bus OKorokay below breakup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
