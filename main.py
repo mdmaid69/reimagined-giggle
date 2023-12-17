70d5d7aac4bef243b03b95e63c4c29e83da9a6f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:06:14 +0000

    service along culture raw Ii

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
