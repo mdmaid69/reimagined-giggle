3bb8bb2b513652a3019bbbba743d692ca7d7dc31
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:45 +0000

    pool blow boat patience getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
