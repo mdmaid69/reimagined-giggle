15303ebd4af95e07ff834c231a3edbd0627e52f3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:00 +0000

    bar trap card involve so

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
