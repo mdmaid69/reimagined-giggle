429de4aa1dd97c8d8ec9bd50f49246b3f6a655b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:55 +0000

    shelter sting mind violent reject

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
