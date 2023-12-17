6dc7bed273a540c1840c148c43b3aa327abb8972
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:25:44 +0000

    classroom ahead minute wide strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
