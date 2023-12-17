4c6f9abcd122718bf44e23779159f029cc52ed08
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:24 +0000

    scale persuade about influence care

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-def divide_numbers(x, y):
-        return x / y
+def reverse_list(lst):
+        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
