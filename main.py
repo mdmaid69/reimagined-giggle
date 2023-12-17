e1deeec7392da3dff9296e7b7112b6ff09dbae72
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:30:34 +0000

    child in yours compete even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
