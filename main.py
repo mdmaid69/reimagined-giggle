d5a5174164d3d1fa5da861a7882204a63db7551c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:26:33 +0000

    them married command language dear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def clear_array(array):
-        array *= 0
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def get_array_length(array):
+        return len(array)
