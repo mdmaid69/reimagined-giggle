c7eed68d23e76bc3b719ed749d4b2ece60daaede
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:22:14 +0000

    children leave distant rubbish final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
