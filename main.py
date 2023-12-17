ea0960bc6584702189fbe42a0582e9b9f3204681
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:58:03 +0000

    touch tongue complaint immediate hair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
