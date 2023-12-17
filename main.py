ae0c0f8dacf6cb3987a36d85cb491874b57829cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:14 +0000

    climb target add lock become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def reverse_string(s):
-        return s[::-1]
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_work(force, distance):
+        return force * distance
