0f62349fb550061f27f0a14751424f7afb555cfb
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:05:26 +0000

    achieve usually sure basic reduce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
