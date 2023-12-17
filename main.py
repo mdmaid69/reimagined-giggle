dc863c0b11ff816511175d9c5e82198a6813d579
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:27:53 +0000

    below drop importance single education

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-def sort_list(lst):
-        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
