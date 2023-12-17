f286fb2c0ba51cee67faa56f29415ba94c615e79
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:54 +0000

    about manage hurry group Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
