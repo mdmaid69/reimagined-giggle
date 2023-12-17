fbaa339588246be54023f53292dd9508aa4f39f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:14 +0000

    load murder invite freedom brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def clear_array(array):
-        array *= 0
+def remove_from_array(array, item):
+        array.remove(item)
