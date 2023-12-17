f618bdf007abef5196d1369cc6c1e97b923dbea6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:01 +0000

    into doubt apparently fire regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
