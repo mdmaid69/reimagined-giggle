6c566e5b3a49c6036d709c5b9c2cfc2b56b95eb4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:31 +0000

    power lot break drop total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
