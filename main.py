1e41e449759775f78a0bd8bcd1858d9b4a418b48
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:16:37 +0000

    play squash imagine easy date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
