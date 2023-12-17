c368bc2d428328509c49daaf038b17036b5ab7ef
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:02 +0000

    free middle Kk food own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
