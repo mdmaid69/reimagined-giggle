85c48eff16a091811c886064b041d2709ab66e69
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:37 +0000

    important nigga present properly always

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
