494ea0f91a24fb2eecab5ca26d67b0335d7d1a19
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:13 +0000

    arrange our sort concerned cry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
