92fa79c35294ae6bc73282e283eded6ed3d80037
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:02 +0000

    detail where difficult cover borrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
