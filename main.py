3fec55e6e9103f9d08eb4c79b57319db42f68f6e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:42 +0000

    giveback make shallow smile thorough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
