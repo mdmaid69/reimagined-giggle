4439698ae3f5061789e406bd119ac022a0b27d65
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:12:27 +0000

    card switch everywhere rescue slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
