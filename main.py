d3e1510c1129824ce77384aae9806565b6a79d9a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:09 +0000

    your match hold face closely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def sort_list(lst):
-        return sorted(lst)
+def get_array_as_bool(array):
+        return bool(array)
