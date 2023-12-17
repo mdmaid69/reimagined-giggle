e52ebe662e84800ca84b11ebee4abb5acb5e7f7b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:52 +0000

    relief lie unit 569enjoy play

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+print(random.randint(0, 100))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
