e08b0a220ce1af3d0160f85b3326938f66eb4534
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:36:16 +0000

    hope half surprising shout middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_list(array):
+        return list(array)
