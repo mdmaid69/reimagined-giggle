b87c4aba517d05b5381f009b0c59c4578898a42a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:05 +0000

    worry  risk string service careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
