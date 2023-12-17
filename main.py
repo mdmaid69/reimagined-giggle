d29d645c35c2be7d00cba9bfd88788d3767cb3dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:15 +0000

    private react nowhere print expensive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
