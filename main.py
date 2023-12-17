4632f55cdac4f2911d7b883e1613382071ff2ab4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:09 +0000

    deliver experiment reject Yy steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
