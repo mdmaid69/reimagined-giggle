9bc5001e0d3a5a9640dc039acea31f54bb5efae8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:14 +0000

    rescue they underneath too live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_speed(distance, time):
+        return distance / time
