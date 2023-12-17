95fcf0f2ae6a5f8a378206672b805edb6c57e848
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:31 +0000

    earn sudden connected whom honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
