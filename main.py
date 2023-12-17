ced27c7ba188f5af09fba409d0f14b515fd0ba58
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:53 +0000

    connected stop afterwards slice front

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
