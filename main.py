0dac9884401bd3ba4388f3f02510d9a443ea0d34
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:51:05 +0000

    operate load shut food get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
