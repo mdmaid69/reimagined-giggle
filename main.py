2c4a739bfeba0457678c93aca6c2c43d9727e20c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:08 +0000

    relative emotion over communicate subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+from collections import Counter
+print(Counter("hello world"))
