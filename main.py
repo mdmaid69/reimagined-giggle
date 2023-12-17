2d81d2423e0f05bb297de0547532dcb686b94cf7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:43 +0000

    curious our positive record rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
