7019ed60a3da189e8526bf010998079fc8e9ac2a
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:32:31 +0000

    seem injury table wish machine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
