c7a104ced18eb467349a588b602434b61b4eda26
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:00:45 +0000

    return straight quiet stop callup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-print(sum(range(10)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
