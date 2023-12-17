c5ecdcdd6e4c437530db26637e0b0fdf0933e35c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:24 +0000

    arrange never limit do convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
