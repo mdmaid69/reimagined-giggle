9e3bde84578543e95d3ed2d5e95672d80201a20e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:30 +0000

    fence last room outside regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
