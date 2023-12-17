7160aa9b7d6aa264c0b661ef10b9688cde25a221
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:33 +0000

    skirt influence play race bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
