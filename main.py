4693187205ac7472a10870dc3e67090f67e5c611
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:53 +0000

    short choice new still improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
