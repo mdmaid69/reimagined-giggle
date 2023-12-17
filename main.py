c9d012f208f67b5092201f01e6b14ebe433ecbd5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:12 +0000

    software taste neat eye straight

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
