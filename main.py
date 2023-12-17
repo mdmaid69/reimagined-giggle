c746b729816cb762fbda915689d5b259b581b7e8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:46 +0000

    blow approve breakfast scream spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
