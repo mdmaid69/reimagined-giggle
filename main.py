ce5ef99ceea048a9219921360f4c86fc81fff734
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:38 +0000

    substance test rush string instruction

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def clear_array(array):
+        array *= 0
