c32a48b4b1814feca4b4d2991eff842862c76433
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:42:11 +0000

    themselves score now colour food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
