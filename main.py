223b8227914fc1093957c632e75b6955a59d2280
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:48 +0000

    always appeal light rate rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
