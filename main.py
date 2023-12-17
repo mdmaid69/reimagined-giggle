f95f94f76e76f775afc80634cc37e06ba2aa193b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:39 +0000

    mainly temperature send sensitive or

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
