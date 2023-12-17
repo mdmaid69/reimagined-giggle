b91c70fa43e22bc7da2e1874fc810020fdc0495b
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    spring creature combine knowledge cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_power(work, time):
+        return work / time
