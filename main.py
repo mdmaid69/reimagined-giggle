643ff873fc466e3e01a1664b1d36721db8a8efe0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:17 +0000

    responsible hair keen fasten Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
