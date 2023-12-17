55ba6795d8fc08c0abd3cb2618b775841dcfb470
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:34 +0000

    bet raise whistle send silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
