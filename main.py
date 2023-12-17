ddd26490f4b5aa1ec4f9da802d36d5375f8ca14b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:56 +0000

    punishment average steady create real

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_error_function(x):
+        return math.erf(x)
