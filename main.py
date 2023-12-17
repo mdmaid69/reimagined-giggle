ed0b8b74d2eaf2ac998cb416cac2387db2f5635a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:51 +0000

    gradually lot stamp ignore attract

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_power(work, time):
+        return work / time
