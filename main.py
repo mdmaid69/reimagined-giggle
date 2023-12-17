c72f3b9f9d2f7394df673f054dd7fda8d588b989
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:45 +0000

    statement turnoff spite stay development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
