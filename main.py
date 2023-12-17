7ce278127c714dc62421135ed8b88e833b3e6d13
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:20:47 +0000

    before exercise cum contain neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_elements(lst):
-        return len(lst)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
