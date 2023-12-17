4e5aabdbadbea758c4db6fd4840ec592697eec6f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:43 +0000

    comefrom joke turnoff rain please

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
