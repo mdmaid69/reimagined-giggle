48273a90e947b95eaf4e77874838094432a22cbd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:00:06 +0000

    smart straight listenfor tight detail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
