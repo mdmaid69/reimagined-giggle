f80ab1f40db318a374befc6d8354f105de87bd05
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:46 +0000

    keep maintain surface idea wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
