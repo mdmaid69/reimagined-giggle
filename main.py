be24a59ae1d3da880a78c9ee8cd4d4c6b2c14b7e
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:21:59 +0000

    serve holdout luck always effort

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+name = "Python"
+print("Hello,", name)
