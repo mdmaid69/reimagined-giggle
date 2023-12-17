321f8a2d228909c1863c1b1f52e77f6edd5027da
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:42:02 +0000

    world exchange half model run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
