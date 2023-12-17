2c3022c76efa3f9b56ecb76cfd96c84b02dc2694
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:48:00 +0000

    dry loud question square dry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
