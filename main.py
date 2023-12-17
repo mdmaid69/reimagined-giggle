859e2e0362344433b49a55e2c2428dfedd1b337b
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:11:39 +0000

    on indeed grand too geton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
