a283218d664ec490ee41578acf452b2581efa05d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:01:53 +0000

    beautiful size relation recently doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
