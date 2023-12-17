1e107c81a0e04ba9733f565ce31fc34c107c3d29
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:59 +0000

    power mine little degree occur

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
