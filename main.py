2253f3d4eaed09dcdd0050b96c925ac9e654c32b
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:09:29 +0000

    theirs sit instead which die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
