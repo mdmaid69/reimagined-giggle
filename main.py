68747b9282d0b5242719c3fe2a4c0d3bd3908a3d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:43 +0000

    cry hit dark control popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
