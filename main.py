2080f969a3768d43365ab13ff107839c183aebf5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:13 +0000

    study favour shall decision birthday

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
