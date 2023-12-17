9f25f0f34f4cb2748fdf3fd7dd83626cefe80375
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:26 +0000

    error comfort slowly middle pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
