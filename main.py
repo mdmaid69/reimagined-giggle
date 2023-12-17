44fb6b54f34a6a7a6ae3901b06126b0459986bd4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:34 +0000

    moral software replace direction everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-print(math.pi)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
