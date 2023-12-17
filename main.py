0a8aaa5e40d5479eb320c099e39992cab9f245a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:51 +0000

    pain breakthrough congratulations figure everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
