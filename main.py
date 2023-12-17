760d27b1043a170ef0aeaa0685daed6c223989ee
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:37 +0000

    wander noise talkoutof feature calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
