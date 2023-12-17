c0aab692c511f87c43ff3c4537edc838d5e3baa5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:04 +0000

    say cry embarrassed condition light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
