30693ad1357b9147800d0dabd67a1dd5eb8fe44f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:47:42 +0000

    affect discussion smell use influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
