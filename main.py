8013daa111d15b29a17f47309b0deecf5def55e7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:25 +0000

    shoulder Ii vote live maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import datetime
-def get_today_date():
+def get_current_date():
         return datetime.date.today()
