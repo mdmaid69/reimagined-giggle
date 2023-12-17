a77fbce0b960bc9c0cb92f347f59f31f6e9a8d60
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:07:02 +0000

    bet official adjective world own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
