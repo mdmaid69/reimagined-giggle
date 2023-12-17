6ab7d09f17ce002f2ae0ec3e3cad3c3ccf8e3a13
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:41:31 +0000

    thinkof frame crowd effect steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
