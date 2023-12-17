db53732ddc015059044dcf7971bd1d0c11ef54e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:33:28 +0000

    plan earn unhappy disapprove university

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
