a0cbee8bdd635bf26db00212d3458b73d8c333f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:44 +0000

    action time still rule like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
