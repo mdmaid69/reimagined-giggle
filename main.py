0fae22f280ad05ccae5b96e63f038d2344d75846
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:44 +0000

    570enjoyable fold guide maybe herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
