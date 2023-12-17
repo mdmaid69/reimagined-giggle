b3ed548d54ad11e2abc05f39762a1f77bd59f39d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:01 +0000

    Ww neither kick throughout gradually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
