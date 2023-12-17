2608cdbee4734372b535998a5f9cc8ee020a3492
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:03:50 +0000

    line mine set issue switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
