6ac925604fba133e7d39780f9bec88c0de888262
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:12 +0000

    education machine provide kill body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def print_python_version():
+        return sys.version
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
