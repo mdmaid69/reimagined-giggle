0b6e7a62487b30a10fa44e4c68d8d4018d7a10cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:17 +0000

    divide round future guide noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
