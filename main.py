3ea20ed0909e9141162b6b5a6ca329b311d87040
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:38:46 +0000

    special build shut defeat challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
