347ad0cdc8a5f0ce4f3ab0dc5ae36e7383cc0223
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:27 +0000

    test maybe level holiday Cc

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def find_min(numbers):
+        return min(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
