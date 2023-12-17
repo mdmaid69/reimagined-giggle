50079c1e946f0272feb77eb94ef721a14466e443
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:01 +0000

    because bite black say getback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
