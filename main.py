8e2bad466e6da22f9fbae0f3b3303dc02e78cc4f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:36 +0000

    praise bush marry cross frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
