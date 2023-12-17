201579cf2b4b0639382b7ebdd2bd4645a6e528c5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:09:01 +0000

    put within face prize mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
