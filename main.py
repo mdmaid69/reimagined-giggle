75b9de76c5945427507e23dea80b5c2e1979492d
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:49:49 +0000

    occasion cruelty picture march opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
