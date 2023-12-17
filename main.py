eec07edb373baab7a1a32f12eabf8be5271396b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:05 +0000

    collect explain argue loan class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
