552d423d2d5cc03ca36a5a3c164483a0c0389815
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:28 +0000

    capital put plant desperate strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
