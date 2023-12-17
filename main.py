5604bb7865eb591408f99d8a20932927aa396176
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:34 +0000

    usually yet trick heat giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
