13899f89b51c58c70534b11bbe697d5e6a410c0f
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:03 +0000

    serve  Y y custom guess post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
