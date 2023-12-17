2a7419d7f7d032699edea4cba9f73a1b2c7ce2fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:31 +0000

    walk trade arrive trick feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
