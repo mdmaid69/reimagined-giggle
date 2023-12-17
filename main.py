45fc8aa0bbd4c7326aeae8b4f0edcf82c6bc27e4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:28 +0000

    next confident taste anything you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+print(json.dumps({"name": "John", "age": 30}))
