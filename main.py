91bfe8f9fa60f64e7feb9b8e34c85f18475faeb3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:09 +0000

    load she be fire suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
