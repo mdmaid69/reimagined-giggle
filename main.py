d9630532a8b0b497ccfe45fa474a95fb465fa99b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:16 +0000

    extra hide wake inquire message

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_speed(distance, time):
-        return distance / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
