a487bb60bfa80be584e65b3b8866a2ce70911f47
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:04 +0000

    proper up turnoff calloff curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
