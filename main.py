f1d4ee59a1c6fb0625ebe15038ec002d6a53fb70
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:20:51 +0000

    taste confusing runover brave alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
