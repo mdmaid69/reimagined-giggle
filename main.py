5677e5c55adc2a6223c27f050ccdb7dc7bfdaa55
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:01:18 +0000

    hold expression interruption notice point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
