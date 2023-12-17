6ecba0520ebcf75aad895ff110ede786e05c9335
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:29:35 +0000

    cry include occur naturally positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
