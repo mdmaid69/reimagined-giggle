3ff570ebcba518a897a8bbc290b16852f64a7ac5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:51:35 +0000

    exploration value and punish before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
