3af24c9410194e32668bd9d6595645babc3d7cee
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:17 +0000

    comment forward large when file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import time
+def get_current_time():
+        return time.ctime()
