c2b9e2c31fe22507fa1aab64c8863ddc67819f74
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:45 +0000

    tend chat interview sentence frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
