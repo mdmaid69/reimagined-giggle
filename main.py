12ecf5f449d2183dc0d2dadd6802c76723b3c516
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:38:08 +0000

    advantage impressive bury comeround put

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
