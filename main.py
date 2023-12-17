5d0b7bfb500612651c87ce2b4f9bfb7fa813f3bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:31 +0000

    accident immediately wake active shame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
