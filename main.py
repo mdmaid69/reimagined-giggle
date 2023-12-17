c87d8d901a595fd15ee54e8d023d641b43692445
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:03 +0000

    round dull pin before loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
