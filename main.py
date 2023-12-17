09462eea6a270c3c10af5ba01bd789bc9a3ec18c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:23 +0000

    suppose but definite private push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
