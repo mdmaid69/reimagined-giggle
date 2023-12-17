d423f02eaef950faab2b7b9a433a9263ab653cc4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:05 +0000

    public Oo feel interview shock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
