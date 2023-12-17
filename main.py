b51404207e5dc7471e3bc864c9c50174a593f6c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:33 +0000

    lie careless whistle worth bank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
