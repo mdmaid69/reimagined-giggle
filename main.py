4d2db771ba4e302328bb27cbf62ad0ebee3effc7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:34 +0000

    skill human legal hope post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
