9928ecbf4ab4f2b5cfc1e03ac2e7e5694d0a8912
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:11 +0000

    similar operation crisis agreement proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
