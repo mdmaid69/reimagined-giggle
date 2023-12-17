c31f11ab34353b6bb382ea0292809437e73b18c1
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:41:20 +0000

    traffic smell plenty exist practical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
