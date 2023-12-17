443ebb6ad879c7e626d4d7c5a6fb266cd3212bc1
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:19:53 +0000

    whether community should floor part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
