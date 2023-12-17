e6b807e9ea4b364d4e1bbfe9ebea71204f091ac7
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    system tape trouble strike anything

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sys
-  def get_python_version():
-        return sys.version
+def find_max(lst):
+        return max(lst)
+import time
+print(time.time())
