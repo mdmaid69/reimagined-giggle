6a2be4e771e224ffcdf2c137617637afce3d0385
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:40 +0000

    section cost success importance foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+print(sum(range(10)))
