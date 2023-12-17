80be6db5b687ddda9903f1cbf69d90b21115e2f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:57:55 +0000

    fit walk miss moral winter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
