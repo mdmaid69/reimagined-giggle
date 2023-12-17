4f20318fd6e0f167798daa728e1d046539fe3712
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:27:34 +0000

    region neat day stiff mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import tensorflow as tf
+print(tf.__version__)
