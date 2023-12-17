00f8b5aea8669457d54374f5c760d502b10f6cff
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:56 +0000

    awake trial learn suit future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
