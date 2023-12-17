f2a7b43ea9cf1805040602c12df3f5d6c69d0c5c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:06 +0000

    advantage 570enjoyable and separate available

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
