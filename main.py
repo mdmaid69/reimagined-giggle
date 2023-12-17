78b36aada3365efafc076e8d3727face6c8488e7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:26:42 +0000

    comeup now regular circle pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
