bdbbe76b2268cd2c94e91d7e78be317abd7ab172
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:08 +0000

    owe illness estimate position tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def get_current_time():
-        return time.time()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
