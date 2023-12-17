5ad4ad38e7ea21ffddd798074f53743bbcebe9dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:59 +0000

    blame movement cost feed exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
