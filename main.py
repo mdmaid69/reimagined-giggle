e719cadc7157910dd96358c8af410b3b39494b94
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:34 +0000

    charge sister trip natural central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
