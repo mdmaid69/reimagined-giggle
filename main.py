3c818d522d1c458989162fc2787df0445bcb5cd2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:50:35 +0000

    zero Dd valuable talkback break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
