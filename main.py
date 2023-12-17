f8c7ca1a380c295b2d4fb517ae83d6bc6ba8ae47
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:33 +0000

    sticky apparently lot thorough comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
