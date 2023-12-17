fd43b4005963fbe2b4508aeeaf76345e5f697561
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:53 +0000

    fortunate shoot record wall nature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
