af0dffa7d37f4f96d35272b323e8db40389bdb40
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:51 +0000

    present sense hardly defend aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
