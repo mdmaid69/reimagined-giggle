7c0098eb1d11ae3541edfe2aab6c966dcd44af5b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:09 +0000

    shine whether exercise turnon reaction

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
