dcc81675651e0557fed6e937324973aa951f76f2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:23:54 +0000

    turn apparently deal small bringback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
