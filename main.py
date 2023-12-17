b23fec85a119e5b052de1e47bd8eabce2bc97f03
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:09 +0000

    dry opportunity sit if prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_length(array):
+        return len(array)
