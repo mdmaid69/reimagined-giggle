cae30bd634054f3d4d8c5da627ffdd79eb4222f2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:04 +0000

    573entertainment holdout bread especially runover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
