1d1cbc93fee4d421ad8c4d26db82eeedc9fd9889
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:12 +0000

    contrast prize uncomfortable rough serve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
