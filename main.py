a9947ee838ff91ccd106034cb663ffc5b137c40a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:22 +0000

    safe trick onto yet view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
