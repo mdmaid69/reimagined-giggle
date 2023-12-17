6e482f5ea7e0393377f45b55d4634890fda7b118
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:21:48 +0000

    connect beat blame dry spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
