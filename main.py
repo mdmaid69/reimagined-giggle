10b4b747b6d522dcb266eeb23a8eb65db040dfb6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:23 +0000

    spin plate leave sure hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
