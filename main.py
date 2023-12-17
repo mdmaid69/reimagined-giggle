5bf0559d6cc9b62ce8e4242558764f3493d674e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:23 +0000

    large even start quit travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
