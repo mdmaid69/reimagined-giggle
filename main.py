097eabbabb27614e2a077462bc5f7e2ce93e10b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:08 +0000

    talkoutof dress makeinto manner fierce

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
