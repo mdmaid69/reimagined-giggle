4053f3b9dd2742a9484c00d24f2ba05ad7790ab5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:02 +0000

    way shadow autumn fasten badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
