565db6e9f26daa8004f5ee11b357b29ec2c765b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:12 +0000

    else born fortunate workon depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
