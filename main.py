b3c3f163c914f19cbed56094327f8c3947b844d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:25 +0000

    tap forget mix borrow round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_typecode(array):
+        return array.typecode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
