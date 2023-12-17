43c946a16439831a887ddc9b2f9c85d74e39ca48
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:32:35 +0000

    big object interested criticize with

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
