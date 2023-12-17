0223b91e36b64358f0338b7ab06b74bc42d60427
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:39 +0000

    music immediately problem clear comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
