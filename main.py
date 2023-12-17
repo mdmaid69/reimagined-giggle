9b1152ef931d3c773f5c44abf7308fa6bde9a5d7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:17 +0000

    autumn myself range serve goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
