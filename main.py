e3a12c26f64b9069ce1269a3a6c0369354a123c2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:48 +0000

    scheme cause good much my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
