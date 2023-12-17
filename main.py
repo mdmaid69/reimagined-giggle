66b5cec00e90afef36fd3a055faa586029b8d95e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:02 +0000

    interview grand receive correct congratulations

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def cube_number(x):
+        return x**3
