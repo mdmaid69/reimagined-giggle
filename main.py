48252e33fc978ad240b451e0c01905aa269c9cd2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:29 +0000

    height afraid remember suffer middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
