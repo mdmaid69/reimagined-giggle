c7245c0ba8e062210ece705afd1e0a4f061c1962
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:36 +0000

    show recognize market ignore country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
