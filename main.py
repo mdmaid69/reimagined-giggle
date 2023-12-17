95e5e0d57e8022f3f64c50b838cab7e420a1f54e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:21:39 +0000

    opposite near sensitive Ff disturb

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
