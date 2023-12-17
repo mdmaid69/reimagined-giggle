804ac67d22236ac3f1dfa3d6654ce69b06edf37e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:48 +0000

    tape any face confusing treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
