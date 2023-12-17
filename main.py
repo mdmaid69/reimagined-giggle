76df6c8678bcf73ab7b495fd64456cf682587fef
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:00 +0000

    hold hide community yes pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
