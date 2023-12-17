44dd52658f9b1ea9f30bd5d4cd71c55aea50540d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:29 +0000

    branch complaint injury development wear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
