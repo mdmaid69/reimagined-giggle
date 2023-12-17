701e88279cae6ee78cd7f00fab7bf8224479767e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:38 +0000

    concern continue agree thinkover fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
