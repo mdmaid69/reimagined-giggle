c08ae6746850b8baa17ae8ef0855f0872c42df1c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:11 +0000

    makeup shade sense autumn clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+print(time.time())
+import math
+def calculate_floor(x):
+        return math.floor(x)
