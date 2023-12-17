4a34ec8e127f01a014bff7adab023f896d9fc8ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:25 +0000

    uncertain detail mouth so considering

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
