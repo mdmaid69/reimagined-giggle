6394d70e99362a4fae1d084f52f9c4ece2452375
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:06 +0000

    shake gentle disappointed snow hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
