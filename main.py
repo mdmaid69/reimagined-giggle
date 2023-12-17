2079f96ea0a40da96f9202e569c40cb3fecfa334
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:30:32 +0000

    kind light pay ring keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
