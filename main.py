c7ee38861739cd50bcf20f6af9d753e010f8d91c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:49:39 +0000

    winter surprised how draw breakout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
