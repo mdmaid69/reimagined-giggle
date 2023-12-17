a4a22966a10faede2f3866b0d910e1c3e78585cd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:22 +0000

    cut rubbish attitude about vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
