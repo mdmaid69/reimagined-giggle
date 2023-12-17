db5bf92f908185e39293c5fd1c1bab76fb4f9588
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:16 +0000

    call lost quit tear real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
