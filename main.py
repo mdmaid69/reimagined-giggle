bbc1e0931aa721f01b071fd8bfecdb094f941336
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:29 +0000

    door love market proud move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
