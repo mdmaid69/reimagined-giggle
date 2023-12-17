6ef1964960a98f35e9842a425d0f6b3b28b21936
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:57 +0000

    pleased proof level mix nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_square_root(x):
+        return math.sqrt(x)
