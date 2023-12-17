f289a7a7166a9e2d5a0153bf3dba902ed18ca4bf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:05 +0000

    finish few right bringforward suggest

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
