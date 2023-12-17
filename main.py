e9512497f7a13fe9494c40f4a8602610e77753a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:14 +0000

    event effort trade conversation immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
