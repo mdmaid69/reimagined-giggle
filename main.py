36bf33e764a31d325a592a97b205ec0e5ceb50e8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:24:28 +0000

    tip path operation disturb manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_list(array):
+        return array.tolist()
