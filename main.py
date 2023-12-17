b922a50f246425883f6c4cc824165a042297e989
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:18:49 +0000

    classroom police failure stranger leg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
