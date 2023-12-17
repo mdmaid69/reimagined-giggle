42e284814da76d0876fddb124c08492eab3b6b30
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:47:09 +0000

    nothing right almost invite unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
