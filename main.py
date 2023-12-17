c8b735128d20f44769baf1d7a013fbf9d2ceaecd
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:01 +0000

    poor manner future look after cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
