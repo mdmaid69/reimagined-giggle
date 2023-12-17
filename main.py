ff200110a0e9704919b2c55253ac3f04076f6394
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:38 +0000

    correct manner lay worst keep away

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def convert_to_octal(n):
+        return oct(n)
