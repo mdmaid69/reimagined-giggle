3e12eadedae211d13eb99ea4de46922a7d110ccd
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:53:59 +0000

    criticize bright would clean result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_work(force, distance):
+        return force * distance
