7096b89134ceb90ef49645b14a4e4717450754d5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:04 +0000

    especially holdon behave feed interview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print(sum(range(10)))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
