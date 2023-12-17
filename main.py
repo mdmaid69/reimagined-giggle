f7aee446be9b44c7733d31ecba521f235447e132
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:30 +0000

    beside hello board step mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_odd(n):
+        return n % 2 != 0
