79588f8263e8ea7d51f466ab789f8247d5527f38
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:00 +0000

    discover mean quantity go string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
