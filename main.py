d772df688853458e40f499095e21d777d62c9800
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:24 +0000

    knock stir fact onto increase

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import tensorflow as tf
-print(tf.__version__)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
