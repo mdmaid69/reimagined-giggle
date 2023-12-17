22d058b65535a0dcede78276b6ac7ee23792be4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    teach bye  scare control again

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
