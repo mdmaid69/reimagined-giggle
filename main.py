2fb5b0e440bd9d4b3f5ae75da28b8d6e341b9331
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:34:43 +0000

    onto sail soak sink mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_max(lst):
+        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
