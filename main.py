34e7a7fbce84c3a393949d7137bb314d95611dd1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:48 +0000

    obvious manage feature so stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
