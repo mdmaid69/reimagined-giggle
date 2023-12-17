d248a3549ff061e1ab141d020cc64e6ea5a8843b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:35 +0000

    reaction getup accommodation bank university

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
