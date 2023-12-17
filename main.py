05c4df381402b3669e903457e07525f3e7c62464
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:14 +0000

    safe keepfrom especially formal ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
