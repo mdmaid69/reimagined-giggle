fb36b946fe454c20e301740889e14223e4e3a155
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:07:40 +0000

    season imagination upwards dinner consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
