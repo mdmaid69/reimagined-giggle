235bbdd60dee072029656a73f14dd43a66909b33
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:23 +0000

    so constantly bit challenge sure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
