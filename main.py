798c69722133fffcc504286d01c1b11b08380c36
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:26 +0000

    separate serve splash way turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
