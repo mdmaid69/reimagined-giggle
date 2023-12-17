adc83217906db6501ba068c580073e29c06aa6ac
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:18 +0000

    storm Jj pleasure doubt flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
