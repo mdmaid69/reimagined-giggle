bdd67ef05485b704346d62da5b8a216585621ef6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:27 +0000

    speed surprised nothing winter bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
