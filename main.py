35c3780da4cc67b4b62fea63f04189bd999dddbb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:09 +0000

    niggaacademy mean escape his blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
