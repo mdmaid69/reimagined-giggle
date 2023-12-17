e17b93ed408e95c71babdc4740feae90bd5c2561
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:33 +0000

    school interruption formal beside avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_set(array):
+        return set(array)
