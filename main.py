cdfdbd8b70ba124d8f8552229514c58d27c02abc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:27 +0000

    rate realize gradually tell dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
