a6a81e71993b3080f9679b7f987a3a6e36c567c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:04 +0000

    lead receive path fault culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
