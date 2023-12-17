93155ed3de8f6eb85b8c8400b97806763f1f0fa8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:11:23 +0000

    passenger right material bath nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
