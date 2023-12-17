0fda62a3c166ec6e92d529a9306a86a0712c3483
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:54 +0000

    recent meeting cap recovery born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
