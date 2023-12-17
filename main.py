2d9189b8f752257451443aeaf0af3fea4603e60e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:07 +0000

    pump hand perfect ordinary exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
