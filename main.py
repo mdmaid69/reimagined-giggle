7442bf60ae9eaefac9e9e09fe0ec97e14f19dbc0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:47 +0000

    swimming hide lay where mess

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
