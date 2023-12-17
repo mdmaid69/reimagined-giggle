84780da10475a184292101bb018be58bd5e25879
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:03 +0000

    innocent pronunciation building comeover everything

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
