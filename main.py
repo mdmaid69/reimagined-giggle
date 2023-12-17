c6de768ccd7c9ccc4d6601746756ff585870f4af
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:04 +0000

    shrink flavour reach instant trust

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
