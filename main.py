c064f11df01355d42a1855266d98b1c72d1aabc9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:22 +0000

    across goup fly pair outside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
