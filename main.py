3e35371e711570b6574bc153c8e20b9bcba2e315
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:34:04 +0000

    guide mine preparation brief increase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import logging
-def log_message(message):
-        logging.info(message)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
