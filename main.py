b5c75504679bfa67329cdfab8f8c58ed2473ff53
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:50 +0000

    escape afford let pollution pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
