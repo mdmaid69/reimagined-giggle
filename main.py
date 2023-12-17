04365171f1c8bb96b545302072d1612fa08584cf
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:57:51 +0000

    moon interruption offence grand middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
