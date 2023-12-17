f7f428a199bcd0b73d2ad4cc8e70d78cebf665b0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:29 +0000

    twist experiment earn term crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
