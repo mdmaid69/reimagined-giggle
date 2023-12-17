5bae94ad18ea2cd65ec109fa394fdfd211139a34
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:59 +0000

    scared introduce surprising space mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
