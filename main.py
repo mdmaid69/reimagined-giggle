4d401f53154e4661aaf7a61ae86fcab3b2652c1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:17:41 +0000

    basically lead uponon himself strike

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
