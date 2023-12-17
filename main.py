1f6c6b392a7daa43ddf812c5a1a1ea44210f903b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:27 +0000

    wear shopping post again rare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
