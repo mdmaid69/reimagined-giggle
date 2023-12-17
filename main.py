90dd393e6c9fbd12250d534c7aa02f5ebf5348f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:43:07 +0000

    cheque prevent colour completely improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
