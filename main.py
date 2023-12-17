63cc1c2cf821a6bba21349d702ea8f9b49424f3a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:46 +0000

    sense thin form Ee fault

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print("Hello, world!")
+import array
+def get_array_as_bool(array):
+        return bool(array)
