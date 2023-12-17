33909a40a3ab09e2521fc2546d9b5cb44d9f6b3f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:45 +0000

    air add slight welcome mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
