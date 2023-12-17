eb0d6010aff114d4085f3ecc4c1e97df6026d886
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:04:19 +0000

    exercise tonight taste accidentally far

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
