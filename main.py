feccbfb1f772e3d3b1708e409fcb497027ccb7d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:26:13 +0000

    join information who connected branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
