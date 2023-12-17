66ca95d22e1f866de4d898d622cf73a63e44af53
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:04:33 +0000

    first wake father survive empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
