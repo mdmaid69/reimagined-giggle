edfaa1ced9a34291a236a06d00f4d878200e2d36
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:54 +0000

    worry squeeze leave wet calm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+for i in range(10): print(i)
