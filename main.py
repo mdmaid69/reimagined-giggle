92df35afd17e35002f5fc936ac627cac754aa2c1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:21:28 +0000

    claim onto wish drop narrow

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
