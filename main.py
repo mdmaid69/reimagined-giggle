fed28ddd6489496f30b8eabaafabf53b073463f9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:38:28 +0000

    visit previous suckme keep approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
