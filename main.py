972341558817f69b76c5887e1105b8e8e6f61ae2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:40:38 +0000

    accordingto different need start interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
