ffba7d5a08ecc577b18b33feba396c78044ebda3
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:46 +0000

    safe contrast confused everybody machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
