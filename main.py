cb2b49714515f06ff7da09af4444c7aea51fc0d6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:49 +0000

    century poor too familiar hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
