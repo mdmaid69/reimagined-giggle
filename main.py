5171f358664ae35901f1843d712bdf0c1d3a77ef
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:54 +0000

    lower ago accidentally finally tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
