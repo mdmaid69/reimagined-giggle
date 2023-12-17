e0de13485c70e4699fce49cdb2bd886e9b70bdd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:39:22 +0000

    free so sink clear nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def exit_program():
+        sys.exit()
