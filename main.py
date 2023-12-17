70b82a8cc771745fef95ac8f7c0834150d07bab9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:24 +0000

    beside Vv gentle burn software

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
