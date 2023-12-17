a03dba2a4ff7fa249d4da42e4fdfd3f8fc3868e8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:24:35 +0000

    tape eat point guide image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
