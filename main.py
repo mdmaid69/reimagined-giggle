2c4f8ac323c733f5bd7907555082109033a18319
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:40 +0000

    live reasonable dick power your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
