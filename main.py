5db5f1e12e51611cca7a39214d798f93157cf651
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:27 +0000

    hungry pretty invitation holdup certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
