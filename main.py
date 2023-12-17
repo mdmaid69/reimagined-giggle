906f775627f465fe8f9b3c272dfbd79c60232e28
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:22:38 +0000

    somehow fence sometimes workon clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
