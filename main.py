fd22e2662ca9dee5e7ec247583208f2af991efaa
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:49 +0000

    display bridge her sleep promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
