4ea31aa0641c3f1f8360a72aae19c620893480a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:04 +0000

    narrow realize before slice cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_work(force, distance):
+        return force * distance
