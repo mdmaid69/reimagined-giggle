96b09fd2d9e5dcd59218aecd2db2ccb3dfd00870
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:42 +0000

    extra mess yourself include relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
