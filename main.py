b2fefc5c7c655957e8d05722943c1bdad1914761
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:20 +0000

    change prefer exciting teach reach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
