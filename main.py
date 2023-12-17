bc57cf69c221fadbed80fdb6d559da7f61d23215
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:54:02 +0000

    Oo ordinary further negative supply

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
