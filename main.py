c6698fa4827ad315fba9269e5182b9bf0628bcd2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:17:31 +0000

    once top rubbish quit everything

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
