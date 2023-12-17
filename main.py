0940704c5e7a6edfaabe0e7b89597e0ead524765
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:33 +0000

    familiar throw boring save trick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
