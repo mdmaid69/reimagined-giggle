1017f5194f10b272207747041cc861993573eae3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:58 +0000

    try drop suckme waste sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
