775c942fc3673d089cb632b538e7516c3f478fcd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:55 +0000

    smile convenient relax fact aware

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
