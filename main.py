1762bc610dc25d7ab3a0d630f5b452a28324aebc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:13:46 +0000

    rubbish date such impressed I

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_unique_words(sentence):
-        return set(sentence.split())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
