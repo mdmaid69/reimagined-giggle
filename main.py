2a9805b10abd08ee9ab76ebefcd3c5467339977f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:47 +0000

    left autumn boring tonight the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_max(lst):
+        return max(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
