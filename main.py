fca844a468c04ca894f8134e35a95f6587172c84
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:29 +0000

    fasten turnoff peaceful slip separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
