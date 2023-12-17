365f4691456efb847022f338194f20a91feaf9ad
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:15:06 +0000

    pencil borrow pump wide regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
