ae171fc695de3cee8816311c3be5428f0fc64e13
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:15 +0000

    getaway attractive level strike off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
