15205a239390e96766161f26c4893d3bf3faab78
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:46 +0000

    medium score nothing when correct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
