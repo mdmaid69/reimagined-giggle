b7f1f22699355887d7966bde381709ffdbab529b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:19 +0000

    decide easy 567enemy buy tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
