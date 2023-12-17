8af82bd752ca658e87db6f014d168ed82d5309be
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:26 +0000

    declare play repair while leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
