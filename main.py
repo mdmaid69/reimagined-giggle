55ec2683a8664e1014a21acedfec3dca4a8a4746
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:57 +0000

    shine calm getup I pay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
