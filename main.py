b1daae58ccf4f8edfe46a9bc8365736d65c1aba0
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:56:50 +0000

    difficult interrupt load write shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
