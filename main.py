6a1462d87f48b75d06a73cb54cf6de30b2027165
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:42 +0000

    string into consider wait right

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
