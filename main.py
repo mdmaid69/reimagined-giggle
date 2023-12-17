1e0e0185b36ceb9f55b8f03446aeb7d054fea020
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:29 +0000

    sensible light whether contact easy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
