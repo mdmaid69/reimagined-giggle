5d55f1abc8e82e9793a999a60cdae3df0e730f02
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:01 +0000

    inside effort cover protection feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_repr(array):
+        return repr(array)
