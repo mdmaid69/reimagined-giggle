dc9f7e8ad6c3401cffbbcf6546ca2428d9a908bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:35:28 +0000

    branch comeover show mind bye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
