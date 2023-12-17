29c5bf7d5f1fa178d2f1dbe48bf0cd8a1a70cd77
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:30 +0000

    attract pot fill soak whistle

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
