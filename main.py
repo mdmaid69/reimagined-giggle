96b9acc7ea6da022cb984d03825f1989aa1f30ff
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:03:28 +0000

    mouth hardly right accommodation beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
