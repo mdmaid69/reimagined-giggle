1fa621f7fb0a7447b8e8408ed0dca30ade440459
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:05 +0000

    fly declare power pin bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
