d9d741f38919184e8b07fd417a5b4ab2bd18e654
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:17:05 +0000

    pool my somebody contribute chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
