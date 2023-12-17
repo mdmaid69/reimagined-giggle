2292f8e7a1a55f8f39943d3f59cea69105ad607b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:05 +0000

    hello broken support shoulder terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
