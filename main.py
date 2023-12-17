83b624a11f92ba63bb1c1a3da3e1633600ad22ef
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:57 +0000

    precise promise contribute everybody neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
