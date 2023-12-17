2977b695fe9d59a8a10ce904da8342598fdec52d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:20:08 +0000

    part excellent cry public rather

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
