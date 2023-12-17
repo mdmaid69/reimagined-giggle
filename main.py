eca3be9b1cf67c9695c4559592a0e10bf6f710fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:15 +0000

    Ww snow Yy communication funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+print(sys.version)
