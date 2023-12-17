ac36aa548cd81e8b70a7daa1ae19a07dbe12c6af
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:43 +0000

    custom terrible quantity threaten happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_density(mass, volume):
+        return mass / volume
