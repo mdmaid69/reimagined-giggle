5fbc13ebb623586ceb7727c12489a460ebc4869e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:58 +0000

    573entertainment shape march rest hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
