c19814f8238376b2e46ef3da805861cffbbe4256
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:13:37 +0000

    rent land suck everyone dance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
