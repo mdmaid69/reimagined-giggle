0646d4329a542c1f8a1026e85b6effd9473fca34
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:38:28 +0000

    me short government also full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def create_queue():
+        return collections.deque()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
