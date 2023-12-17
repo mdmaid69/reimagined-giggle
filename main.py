5979003580036044f2f8a8efed58c12dc70b0deb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:13 +0000

    forgive suddenly runinto of like

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
