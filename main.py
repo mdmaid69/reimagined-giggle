5178afe1070718538d78da707e4fd05a461a5acf
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:09 +0000

    male backwards glass male demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
