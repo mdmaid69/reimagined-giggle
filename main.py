9cfa880991c3d2cb7c49d347c8e7d6d27dd7ce03
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:12 +0000

    thank tip lead travel ability

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
