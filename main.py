376168711dd63d8769de9e83374de6c53c5d8070
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:46 +0000

    lot unit clothes behind brief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
