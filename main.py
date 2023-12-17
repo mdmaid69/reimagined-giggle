37c8cd9fcf66447c57a25e5120778b818896fc1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:05 +0000

    father beneath worry deserve sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
