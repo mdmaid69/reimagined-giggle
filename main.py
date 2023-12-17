171eaf66f2ca6933e869b73c2065c357c2d416ac
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:08 +0000

    report evidence argue by quarter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
