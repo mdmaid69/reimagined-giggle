7eaa174b710c7d61e1f3cfc83f4f7bfa57a315f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:20 +0000

    forward open rescue dull supply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_volume(length, width, height):
+        return length * width * height
