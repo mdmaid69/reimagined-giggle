9552a64b67eb832d6fe231005495ad269a77d406
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:01:57 +0000

    damage dark weather hole note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
