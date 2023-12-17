88342684591b439f98cac735b52d1f628f3c737c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:29:00 +0000

    consult sit getoff point protest

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-print([x**2 for x in range(10)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def reverse_string(s):
+        return s[::-1]
