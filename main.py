d5222de1c417b21946f4445a6f31a2aae3fcee38
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:38 +0000

    vote progress swim organization zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def insert_into_array(array, i, item):
+        array.insert(i, item)
