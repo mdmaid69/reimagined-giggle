2c5442fe30ab91aa764cb4029d1f874f847224f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:12 +0000

    hot stretch brown not dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
