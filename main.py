34bb6216bdb524d0047ffd6ab24aa84de8475aa0
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:17 +0000

    declaration constantly in  act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
