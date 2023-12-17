dbca5957858058c1cae371bfbf6546ca1015d3c9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:31 +0000

    display march correction level someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
