3948016a307b1e8a76020a54d6519c9bf14b5c85
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:43 +0000

    uniform keepup push evening same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
