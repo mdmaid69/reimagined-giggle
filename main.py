bc9a458fb05d37e5edefee719db8f30c2edc61d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:41:59 +0000

    rid thankyou herself expect turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
