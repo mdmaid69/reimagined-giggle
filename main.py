1913703592832472ab70bf28361512bfa2cc833f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:37 +0000

    Tt kind tread with crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_repr(array):
+        return repr(array)
