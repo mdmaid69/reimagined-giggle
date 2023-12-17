07c91f0a74e178dcd8912ecb6b5daf7242937e5b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:33:47 +0000

    perfect both total mine mother

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
