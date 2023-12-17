5677d7ba8077fe61f197fe931cb7fc9709f4d23b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:08:39 +0000

    shake turnover various shine steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def sort_list(lst):
+        return sorted(lst)
