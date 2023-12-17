03609f6a6e6c7e5c4399df346ca8f9e7738bfb1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:18:47 +0000

    head steer small book method

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
