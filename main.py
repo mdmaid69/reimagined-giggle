e81ebb6cd8c43dd00380b4b6c3129fc4c6ce120c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:31 +0000

    declaration cheat limit let strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
