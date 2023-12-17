3b9551b9978a03842346394a5ebb04cf6796d3ff
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:42:19 +0000

    rain spring steal callup declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
