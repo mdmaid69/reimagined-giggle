5a02fd67d85b825afce20e655da75ea3764ccf42
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:10:48 +0000

    war warn ready concerning thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
