c42216d59ff303c8d0c1d6473d13ff22ffe044b6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:06 +0000

    emergency say wave spite drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
