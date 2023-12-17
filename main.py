1530ee93a63e6d36f710323620a2f197ce7b2816
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:03:19 +0000

    sit quickly finger damage store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
