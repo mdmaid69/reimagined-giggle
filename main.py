7e6f13cf8e041f428ece76eec3c085abda093074
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:07:53 +0000

    rob worth improve people stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
