3a18203da3fbd4648d4a7fc0626270a4dcdc4326
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:54:48 +0000

    interview Cc generous me letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
