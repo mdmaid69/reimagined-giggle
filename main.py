937ec8cf26ad321512b416d49d1b7610a0018c6f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:19 +0000

    play please better large correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(5):
+        print(i)
