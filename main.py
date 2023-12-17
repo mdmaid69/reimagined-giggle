2d1357f63d9796d1ea1443944d685e68d1e81f98
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:29:27 +0000

    fail their climb shall tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
