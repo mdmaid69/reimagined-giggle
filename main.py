fc44dd77eab69644e6d3027b8e69a4e9a63f6665
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:05 +0000

    task confusing relief type world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
