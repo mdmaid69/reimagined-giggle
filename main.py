b9fd032ceff3f1c4293c49d2914b1ce5a80afe74
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:53 +0000

    clean about takeback they plate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print("Hello, world!")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
