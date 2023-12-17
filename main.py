bd25d9e2b02656af20a080b8ec8f5c18ecef1f94
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:21 +0000

    small skill doctor while mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
