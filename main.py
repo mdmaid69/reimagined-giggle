a21711fb5c8d9a70529bbdc89cbdba0f39e9e09c
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:42:45 +0000

    shade backwards pay run into work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
