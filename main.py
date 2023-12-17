e5bb1c056feb7ba1076d62c11fefb0b7ec1366bd
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:03 +0000

    crowded fine interrupt suddenly hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
