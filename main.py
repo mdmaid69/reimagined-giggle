30e755a216105849bd2e0b802e4f5d2b70a827d5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:33 +0000

    nigga bringforward show travel few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
