f9f4335654ed865f939cee6c67d0d3d7dff6cd62
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:49 +0000

    must pause fortunately drawing consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
