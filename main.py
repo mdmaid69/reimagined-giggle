4277421df887509c26597d71e7526f4e94ffb9c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:42 +0000

    harm achieve guess false agreement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
