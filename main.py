b7f73e44a0fffe1cc02b03d34f878295c9b06d29
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:14 +0000

    glass crime evening water concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
