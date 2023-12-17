a651d2995be3ab214ba2662f7f48bb96de8ae80d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:45:52 +0000

    public think forget rain everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
