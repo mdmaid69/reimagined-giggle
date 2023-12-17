2f24d28f8fef88447d1af50752079a42af60d1fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:11 +0000

    mainly better see slightly wake

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
