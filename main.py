052ec08701182e606ddafacedd7c54047386c5e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:00 +0000

    could clothes official plan stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
