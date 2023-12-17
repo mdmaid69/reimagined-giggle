4989beca71ff0a0631425ce41e7af85010e703fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:02 +0000

    minute add Cc grand ahead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def multiply_numbers(x, y):
-        return x * y
+def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
+        return x - y
