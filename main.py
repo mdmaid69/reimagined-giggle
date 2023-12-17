b344d84d2ab2bd0ec58eabd0bb129d005d3ce1fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:58 +0000

    number continue smooth plan hang

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
