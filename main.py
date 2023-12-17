8810919cd65c1ed7fa1610db9479565e98e9e0ad
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:06 +0000

    intend search turnout complain fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
