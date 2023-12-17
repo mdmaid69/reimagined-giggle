9ede4ded7beea7db962e9fd047299c75f905f592
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:05:41 +0000

    lose imagine into spring division

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_set(array):
+        return set(array)
+  def convert_to_hex(n):
+        return hex(n)
