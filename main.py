bfcc8048da2f82c397f3e427081b4d9e4c8255a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:50:21 +0000

    cheap refuse month crowded swing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def clear_array(array):
-        array *= 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(numbers):
+        return min(numbers)
