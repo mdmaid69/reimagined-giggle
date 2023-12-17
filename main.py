c58dbadf49ebff7daac163725c0f847479711aef
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:26:52 +0000

    rise godown different excuse should

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
