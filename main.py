2d75d60a4edb0f83e992210df655d01cd9da4136
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:58:44 +0000

    contain nonsense present consist line

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
