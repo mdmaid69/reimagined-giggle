8912358c3c1d076cd8617d22c1844880892590a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:33 +0000

    call stand shop underneath present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import random
 def generate_random_choice(choices):
         return random.choice(choices)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
