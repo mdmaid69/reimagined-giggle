1f973e09f447ecf16346ecee8227c994230a3a74
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:16:26 +0000

    attempt lost yesterday 179bite straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
