d7b1662d27639d7b7d113f259fa01ad974432c49
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:43 +0000

    dress recognize comeback noisy injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
