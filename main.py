bdebcdd596afcb8c1a297accbee1322d45600e1c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:24 +0000

    violent emphasis behind repeat better

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def add_numbers(x, y):
-        return x + y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_length(array):
+        return len(array)
