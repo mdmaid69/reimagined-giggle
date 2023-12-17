c23d6790503cf882ef7a92a0af24ca7ef2989699
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:06:03 +0000

    story rule surprised last equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
