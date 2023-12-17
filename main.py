9def80a22b376c2b1aaf40829e93f518e9177a59
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:58 +0000

    release squeeze basic way method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
