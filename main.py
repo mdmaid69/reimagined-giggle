ef1e4b1a338f1f5b91b0a9223e70e1f457854023
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:11 +0000

    reduction almost put scene emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
