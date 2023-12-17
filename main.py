7e284cbca9cfd3fad5e0c275e390d2bfea015bb3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:29 +0000

    unless region card slice no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def reverse_array(array):
+        array.reverse()
