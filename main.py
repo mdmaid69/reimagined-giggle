1b78c7cb0a9dc0b3222998876efa26fc5a23bd8e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:17 +0000

    different dress goodbye safe release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
