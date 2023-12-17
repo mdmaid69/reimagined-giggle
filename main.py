b260c6e07df08e2770ee08e561a010067f5547fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:45 +0000

    consider performance level interesting edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
