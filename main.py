678036d01f7f371b9352b8fc0bf7aefc942a969c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:33:22 +0000

    turn side fond car mainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
