f58b33f549b3d67fafb3aa42fbc84b93bd5f9d5b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:13 +0000

    equal prize traffic target concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
