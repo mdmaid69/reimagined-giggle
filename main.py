9c62ae11945f8fa7a40db33375aa907c788f2633
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:03 +0000

    region live but same return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
