2b4e9907a00cdc743a1ab0d26be778e3ff50b2e7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:06 +0000

    temper quickly bang comeover few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
