4212612f7b5a13ef53bcd2ec3b9fbf670cfc27cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:22:26 +0000

    operation clear if unfair recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
