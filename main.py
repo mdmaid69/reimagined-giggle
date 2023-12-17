688c6b49a1a2efb4f610f1850f993abc706fdf33
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:00 +0000

    soil if heavy full surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def convert_to_hex(n):
+        return hex(n)
