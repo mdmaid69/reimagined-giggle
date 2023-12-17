a39730bb73f665babb15a5c7150968b99300b6ab
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:08:35 +0000

    comfort achievement whoever private battery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
