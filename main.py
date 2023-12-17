3f666ee015e89e98954e3211e7000da8d8665c25
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:32 +0000

    nothing brother unhappy bet lock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
