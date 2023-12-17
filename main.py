def892ac688756186c5c8d4e35a1aa64a3878f32
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:58:12 +0000

    behaviour brain breath last duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
