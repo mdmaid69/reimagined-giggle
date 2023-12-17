6ebdba109fd63a54739a8c6cfd35b1ee7cc2771d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:34:35 +0000

    challenge draw extra thing mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
