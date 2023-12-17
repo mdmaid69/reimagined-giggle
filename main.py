fd07a866858d41356d23a470c5199f7ec1cd4ae0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:08 +0000

    our turnoff equal pretty bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def clear_array(array):
+        array *= 0
