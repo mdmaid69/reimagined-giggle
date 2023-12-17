6234cfb7f45ce273f43f09286628aeb644187996
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:05:17 +0000

    gap proof brush line upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
