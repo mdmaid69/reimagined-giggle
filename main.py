72d53a3f761777114845d3a2f029d661c171cfd4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:15:20 +0000

    bury health say pull disturb

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
