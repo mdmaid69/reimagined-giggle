23e6aa1a1cc58a2f5e8f2c47c1e9932f05cb3aeb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:35 +0000

    above shock want walk available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
