50666f49ac346cd4abe03aa9fa2edb446900ba48
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:11:01 +0000

    hard fail sort far send

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
