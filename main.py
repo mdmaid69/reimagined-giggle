c02c6ef725d923080395c446fb8bc90742f73f72
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:45 +0000

    confused manage 573entertainment turnaway think

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
