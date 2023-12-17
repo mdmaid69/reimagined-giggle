4ffa33948a9da96f5fb92eda14db7ab86df3fb01
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:29 +0000

    dress switch Pp bringdown emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
