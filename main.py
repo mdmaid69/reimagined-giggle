c0164faf95cd0aab15d99d5a01d4aa61d2289aaf
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:10 +0000

    broken throw attempt mess loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
