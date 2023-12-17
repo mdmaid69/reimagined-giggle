83d1f9272cdbaad43f467bb72d652fda73b5af00
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:31 +0000

    reaction bound admire hardly port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def sort_numbers(numbers):
+        return sorted(numbers)
