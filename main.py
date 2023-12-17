27d716ab6aa4c00c3633b1d99ab9f5c5ee065a27
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:13 +0000

    wash church and quantity no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
