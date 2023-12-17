0690de728e8635d5d7238e8fcbbbeb0b91afa5df
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:02 +0000

    discover otherwise from type glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
