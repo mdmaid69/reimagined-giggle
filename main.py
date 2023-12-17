19865e5163fbbca66bee76afa8892c25d2a77927
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:07:41 +0000

    probably middle mind except store

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
