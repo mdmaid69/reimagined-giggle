0382ad976edb31dbda08b0dbd5c83bd9487db088
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:37 +0000

    floor trousers must trick getup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_current_time():
+        return time.ctime()
