46f26e521ecdc7edbba2061191d5d6b69b00d4aa
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:06:02 +0000

    roll especially original no realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_work(force, distance):
+        return force * distance
