d036f18a18759a82809eedb1a17f972bfa07f6b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:39 +0000

    she 564encourage holiday food error

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_current_directory():
+        return os.getcwd()
