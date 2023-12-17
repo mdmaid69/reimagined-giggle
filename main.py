9a34c949f7c076784e166a08c4321c83202a87fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:50 +0000

    catch goback(to) reasonable leg popular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def multiply_numbers(x, y):
+        return x * y
+def reverse_list(lst):
+        return lst[::-1]
