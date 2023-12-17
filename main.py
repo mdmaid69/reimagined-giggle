f9113ef5aabcca4c7d5058a2ccea042a2165ec09
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:55:36 +0000

    half bad mad surprising flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        return sys.version
