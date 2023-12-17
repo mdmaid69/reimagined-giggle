dbc76586d317cef84bf3611e0e813642ee4618f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:45 +0000

    break last modern trip fit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def multiply_numbers(x, y):
+        return x * y
