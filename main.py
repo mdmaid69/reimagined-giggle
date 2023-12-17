b99a4ba0e4a38e78709fa1ecdbe82e77601e11b1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:02 +0000

    giveup below raise about commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
