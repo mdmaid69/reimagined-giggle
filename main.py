b04674eb51a8e95335a9d3a5d2f0d89c7f6bbe76
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:23 +0000

    display charge few drink way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+print(math.pi)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
