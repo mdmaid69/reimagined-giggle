33d8fbe1600d1527c0f4d6dd3f44e62a7a415fa6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:56 +0000

    speech gentle late hand congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
