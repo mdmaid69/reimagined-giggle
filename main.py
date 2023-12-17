9220ac7e4a464a1eee39f00a6af2bc3083d3498c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:38:03 +0000

    accidental shade belief room fond

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
