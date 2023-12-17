43e52eb1e05bdc94b8f7f947c4521315a7e22dc9
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:54 +0000

    then arrange clean young each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
