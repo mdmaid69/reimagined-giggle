5503788a151a4428c98a056df81c6afd1db967f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:37:05 +0000

    trade notice surprising me keepoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def convert_to_octal(n):
+        return oct(n)
