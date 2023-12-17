63807ac2c04e13aaac14e08d8729add5bc63525a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:44 +0000

    suffer coat impressed chance 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
