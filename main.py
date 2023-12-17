6172520648d4b1b8ded1c637d40ff982832463ba
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:47 +0000

    church part soak agree fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_stack():
+        return collections.deque()
