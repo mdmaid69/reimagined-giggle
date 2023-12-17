16db397ca95cac57c30a62cad01fe3e8e5a98457
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:09 +0000

    round key Yy worse drive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
