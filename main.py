757a57cb08e618f4c226aaad90182e8386bce44f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:07 +0000

    trick knowledge protest both sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def find_min(numbers):
-        return min(numbers)
