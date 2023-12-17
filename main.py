c0bca0bba073856d35f25fdb4d31aaa04c91fc98
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:25 +0000

    party sound whom part ours

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_base_name(path):
+        return os.path.basename(path)
