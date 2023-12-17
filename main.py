ff48ddb07788ee26660a7e9ab343b153dd514b87
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:43:15 +0000

    get on  scare attempt underneath used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
