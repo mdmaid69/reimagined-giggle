3b3da964a072363722ad21e0179ce89ff1a4504f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:08 +0000

    exit standard hungry scared they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
