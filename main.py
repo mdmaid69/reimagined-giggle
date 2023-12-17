c110d4afc7384803daca6b388525ad9188b78923
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:56:52 +0000

    justice succeed upset live few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
