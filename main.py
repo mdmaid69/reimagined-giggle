82ff33657a8d1434ee9eb6a9ed09ec02ec100ce8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:52 +0000

    guilty Dd sit free Kk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
