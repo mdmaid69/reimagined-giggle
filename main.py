fec9d8fca746cd8f8331b69e5fc7473ba6322966
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:16 +0000

    steer complaint go Ss public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def reverse_array(array):
+        array.reverse()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
