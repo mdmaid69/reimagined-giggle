2aad9907c7bbba1235c8f5771c506f35ede73d80
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:05 +0000

    run invite use communication wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
