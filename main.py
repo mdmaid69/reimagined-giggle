37e16556c25ce0dfeea27f6ade7e3a13024922dd
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:05 +0000

    answer further happen bottom by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
