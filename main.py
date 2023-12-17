ec1ffac4b228149468edd2b537396743eb34ebd9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:15 +0000

    clue split feed wind yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
