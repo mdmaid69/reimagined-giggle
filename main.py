53a468981767a348ffeed111a4e4df1c58bbb11f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:29 +0000

    Ee quarter trust actual building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import tensorflow as tf
+print(tf.__version__)
