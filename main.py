c9fdac5b5d52ab928da4e8d044d576d887b18433
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:51 +0000

    fine for task crime doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+from collections import Counter
+print(Counter("hello world"))
