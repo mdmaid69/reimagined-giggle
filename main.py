aef03cd6f3e0796f0f2f728009a4f0eebb24ce65
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:29 +0000

    better back play experiment pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
