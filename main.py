0b4d1a5a8ba6bd7b159754575f573074467095aa
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:35 +0000

    complaint peaceful lesson street breakout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
