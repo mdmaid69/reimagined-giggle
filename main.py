ad06ee8c169319ca12034521b199ffd24cdaf7a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:59:57 +0000

    before declare sale replace threaten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
