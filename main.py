c95c41813f81d45130f92a8dc97114e6f199d180
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:32:16 +0000

    develop success solve cause every

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
