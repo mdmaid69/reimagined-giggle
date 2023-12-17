a926a936fa2b2fe6f13e0f2030ded192c217477f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:11:16 +0000

    hard bright ticket take set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
