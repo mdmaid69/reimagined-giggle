ba8a8790b0794e10d81a04b2c0f76fe9f1585c07
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:16:07 +0000

    listen lower agreement naturally stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
