9971bbf5b43bb667dd6e8d1605a6bb719942b9c4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:47:56 +0000

    them instrument share goodbye about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
