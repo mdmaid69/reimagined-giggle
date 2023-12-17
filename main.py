01b35bfb6f12f12a514d84402287ef72f6c21153
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:31 +0000

    pay mistake crime narrow anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
