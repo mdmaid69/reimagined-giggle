1d91ba0da63892dd396aec76ffac613d8900a608
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:34 +0000

    first typical occasional fall swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
