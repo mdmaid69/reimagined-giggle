dbfadffa90674a24be7b6a86caf669d9a5b9e9ff
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:36:09 +0000

    regularly decide kind somehow less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
