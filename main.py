4cfa0a8127e891cb86096db274bc0f0be3953ed6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:11 +0000

    yet mess bus him lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
