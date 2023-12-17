edab27a81b367deb566f0e424dd68b3db05e3bf3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:40:55 +0000

    effort giveup confident none sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def subtract_numbers(x, y):
+        return x - y
+  import sys
+  def get_python_version():
+        return sys.version
