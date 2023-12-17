0276b304d6d30b810acee5afb9c75214dc726c74
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:32 +0000

    protection fire damage stamp file

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
