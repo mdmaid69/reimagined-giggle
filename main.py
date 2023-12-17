dcfbcee9a3ce51742acfca26dedec9ac20c47b55
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:02 +0000

    light practice pupil hurt unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
