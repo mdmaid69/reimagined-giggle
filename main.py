2f004773e745d3e0c7474b2e7e13ed1dc2e13189
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:22 +0000

    capital pencil row congratulations extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
