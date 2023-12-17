21388e5aa8da4d839f008c6d1a25bcab3b9620bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:16:30 +0000

    come bound dream temperature project

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
