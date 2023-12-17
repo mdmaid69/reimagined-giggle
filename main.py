0e52a5067b1772fcdd56fbd21eabe5788d3ab7f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:31 +0000

    funny suit our agreement especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
