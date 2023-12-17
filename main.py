24634fced99da8b204616f111d6de4b132425b5e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:44 +0000

    lock either side benefit anybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
