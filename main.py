c4a6c80eda74c4f4e173e5e1a2eb8fa304d25817
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:59 +0000

    fix subject measure equal turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_power(work, time):
+        return work / time
