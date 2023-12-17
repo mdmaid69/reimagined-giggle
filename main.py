e2231ea4725572bdfa63dc5319a0ba501a8b8ce4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:11:50 +0000

    black news back slice eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
