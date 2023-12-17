4dc2377ba1be6236f9fb4ef2849b143958b3ab73
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:12 +0000

    exam plenty put dry broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
