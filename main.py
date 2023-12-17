9872c8ed85a2110fd40f79fe7e26ff389a20ac30
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:15 +0000

    real tone turninto cause blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-for i in range(5):
-        print(i)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
