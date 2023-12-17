bf05dbe5e987f35655b57a772e0fb8f63d2f65f8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:59 +0000

    must lend opposite worry disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
