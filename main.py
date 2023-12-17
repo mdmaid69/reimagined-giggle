9fcbab860074d53e481907e4abcb3c58238878ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:56 +0000

    pattern final breakthrough fire average

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_max(numbers):
+        return max(numbers)
+import array
+def clear_array(array):
+        array *= 0
