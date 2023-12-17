a9a30b3902bbdf4c368a7914414b868c7e9fd834
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:01:26 +0000

    night myself store equal tall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
