44959a48b0040095bf540ce783fe417901f0bd56
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:43 +0000

    shut welcome goaway mean manner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
