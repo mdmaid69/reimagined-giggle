cdf05b6ae346a456ab089b1083d5975bd05ee447
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:51:04 +0000

    experience computer visit search within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
