422bee85505551975c5ade79b8f5b4d892138467
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:39 +0000

    hand stay switch free regret

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
