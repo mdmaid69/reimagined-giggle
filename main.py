10ec6c1ab2c6e73b9f5e066ecbf12ce0fd19aa80
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    measure brave near unhappy pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
