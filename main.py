b322ade65c38a86002c3c473660326ab9f3ae354
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:29:04 +0000

    stamp alive unfortunate brown club

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def count_elements(lst):
+        return len(lst)
