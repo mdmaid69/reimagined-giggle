6af12c8d6b167ce7dff8d76c893bd2cff6a82a46
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:42 +0000

    arrive steer character remember concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_item(array, i):
-        return array[i]
+def extend_array(array, iterable):
+        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
