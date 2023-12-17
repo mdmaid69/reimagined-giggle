b11e4f76f382d7f15a4fce166f638bd9e5b996f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:30 +0000

    shadow mouth please bored hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
