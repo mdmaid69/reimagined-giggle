5e7056cb2947787f0ac882baaf34403ac72acdf7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:45 +0000

    breakinto live target importance copy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
