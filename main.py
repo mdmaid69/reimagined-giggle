ed3fef33aa1c384973387a3ad454755e1f7a7f91
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:50:01 +0000

    they justice limit block reasonable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
