25f91dedadebb91c2acd9b9691928b51e75bd9b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:09 +0000

    score fast people on join

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
