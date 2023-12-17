3e6596e5ea6988692fb879e2db69f2bcf745e705
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:55 +0000

    hurt welcome future beside private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def extend_array(array, iterable):
+        array.extend(iterable)
