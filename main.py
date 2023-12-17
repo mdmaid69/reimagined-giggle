601cbe050505473aba54bca0a53e2b13e842f609
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:42 +0000

    return comeover course difficult quantity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
