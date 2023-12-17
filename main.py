235a373206ec143af15b21bd6c35ccb24b0ec628
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:34:31 +0000

    control definite complaint like mixture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
