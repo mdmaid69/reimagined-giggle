43f906a4bd763ec2dc539b28000bb5c6545aae6f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:56 +0000

    gentle reply day telephone everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
