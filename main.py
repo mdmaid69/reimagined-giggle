d7a33fc6b30cefdda66cbd86b8030e1de0a75c8c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:19:12 +0000

    keep cum do makeup pen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
