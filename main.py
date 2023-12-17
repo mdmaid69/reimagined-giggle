fa1d2f1c93809163f6530e24c9efb58d8352442b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:24:27 +0000

    achievement hold plan true subject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(lst):
-        return min(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
