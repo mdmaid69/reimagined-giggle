0d95464cce759d2134dd637315ff7b23cf3d3c5e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:33:56 +0000

    season bottom fight rough prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
