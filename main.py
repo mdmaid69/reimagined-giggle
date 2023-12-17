787dd00144314f74b2727f3b4b2d600c4eb2e798
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:04 +0000

    conclusion report murder pick land

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
