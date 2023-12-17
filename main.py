9f5e9ac8984add1767b1179d078cb63e70a5a20b
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:01:09 +0000

    image cheat joke house dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
