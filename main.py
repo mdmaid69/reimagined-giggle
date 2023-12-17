c6473cd83d6343d8fe4936d1511fcb29aa9f20aa
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:44 +0000

    comment short strict provide shock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area(radius):
+        return 3.14 * radius * radius
