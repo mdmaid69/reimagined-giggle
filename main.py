99ad72f7abbe2ab6182232cd494f765b53e61285
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:25 +0000

    reliable live takeover slice holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
