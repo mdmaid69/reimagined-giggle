0e025236fd899c6e8e250bdd1fa418ee452d47a6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:51 +0000

    cause impressed sorry unit average

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
