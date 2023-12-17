8a74fafdf0c0bd01198764500b4e8279880a6576
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:25 +0000

    treatment scheme funny before callup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_set(array):
+        return set(array)
