bea290a2335770e677e200821690d0a49a50efa1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:46 +0000

    Tt slice once opportunity hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
