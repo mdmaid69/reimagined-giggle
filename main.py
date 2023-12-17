ba8b0c781e0795107aac10940a7d05b61ed52074
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:23 +0000

    rule smile freeze statement material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
