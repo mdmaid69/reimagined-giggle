54598482c52eb6450639bd785decbecc5485f484
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:32:01 +0000

    increase reject move test help

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
