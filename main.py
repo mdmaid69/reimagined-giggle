4026e374f8fa0b09b92c3d94b98789324300304a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:51 +0000

    chin heavy Cc brush church

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_pythagorean_theorem(a, b):
         return math.sqrt(a**2 + b**2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
