7883813ef47f4c19ac120a1fec61c0f4cdd932c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:16 +0000

    year home past protest free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_str(array):
+        return str(array)
