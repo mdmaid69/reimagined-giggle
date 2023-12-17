d3d51c0669c8bd064f548c35d239568e7dc71310
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:50:33 +0000

    fight level getup side throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
