c731c47fee2f01f1851b050a0e77a8b83f697324
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:28 +0000

    everyone wonder contrast relax but

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
