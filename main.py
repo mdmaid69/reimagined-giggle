aabe29d4e4b1b995ba175ba13a99965133f376af
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:56 +0000

    meeting somehow rush backwards squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
