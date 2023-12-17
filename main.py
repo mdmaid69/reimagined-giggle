86afc1ed94cf41530ab40e045b07193a387e22fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:19:48 +0000

    supply force unkind sleep inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
