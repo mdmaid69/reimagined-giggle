2d58e902547927073596406f4a70dafbe5706d24
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:17:32 +0000

    pass whisper beginning ring sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
