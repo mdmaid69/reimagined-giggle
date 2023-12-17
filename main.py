019def88c2a0942d4416bdfb7c44aa0b4b213d15
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:07 +0000

    language cause instruction mass own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
