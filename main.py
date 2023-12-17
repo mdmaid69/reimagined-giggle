9e61968e750d1b97b1b6692f4805e26158ca4321
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:45 +0000

    round none fail blow best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
