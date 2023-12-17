644ba8ed4f94dd37bbb7a5d13e48e5288f8a7f73
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:35:46 +0000

    circle often experience cause appear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def reverse_list(lst):
+        return lst[::-1]
+  def convert_to_binary(n):
+        return bin(n)
