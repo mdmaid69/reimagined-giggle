55d7b1bfcedbe1aef8139b30e1aa695b3c9765f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:59:55 +0000

    climb finish still us top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
