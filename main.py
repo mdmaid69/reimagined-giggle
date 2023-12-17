debfaa496e987382d8a6c4fb3259a2f31fb0333d
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:03 +0000

    somebody ring radio half constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
