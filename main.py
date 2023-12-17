aeb0d8f4fee0dcbff1c649882963e7df0383803e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:09 +0000

    yourself criticism arrange beneath influence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
