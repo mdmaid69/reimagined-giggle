cdcfd20bd42f2b91722a801287ad3a8b2c5f39e7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:33:16 +0000

    effort fight style each way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_list(array):
-        return list(array)
+def convert_to_binary(n):
+        return bin(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
