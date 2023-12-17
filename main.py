9ed4287c12a128a10a5233d0574f665dd8017829
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:04 +0000

    whole communicate age Mm crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
