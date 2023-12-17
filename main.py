80ee4839d9166f7b9912ad894bb909f8841744ed
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:31 +0000

    exploration keen prove hurry disapprove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
