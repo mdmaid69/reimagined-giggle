70dcd2889d4272d7c8210ce32a13625d6f96ab20
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:52 +0000

    way quiet target gooff corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
