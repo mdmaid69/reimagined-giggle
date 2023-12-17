a178c8d5095c2801d0b37973b99bf227d4419dfd
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:25:45 +0000

    immediately occasion different exhibition arrange

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
