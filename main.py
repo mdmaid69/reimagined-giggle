2215a29bc708bb3ca55df9e68a5a91e2947e116d
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:14 +0000

    shadow base weather coat shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def append_to_array(array, item):
+        array.append(item)
+  def is_odd(n):
+        return n % 2 != 0
