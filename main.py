478c2177fdfb4b5e34d7d03dc68ca23e44b97a75
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:22 +0000

    water everybody study ago rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
