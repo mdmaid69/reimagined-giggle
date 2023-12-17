1393ce4a7681685f95903c53920d9972e04178db
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:18 +0000

    scared club fail cruelty safe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_item(array, i):
+        return array[i]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
