d7d98fbe75fe79c6d4cecd0e6d26910b66aa6905
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:11 +0000

    think of dig and thought their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
