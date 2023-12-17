030417bbcb146d3ad0535e29427f942112e0a7e2
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:50:29 +0000

    drop mix central afterwards Ee

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def add_numbers(x, y):
+        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
