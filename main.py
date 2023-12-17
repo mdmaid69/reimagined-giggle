767ac9b070f062795d9d7641f5342fa8157dc555
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:11 +0000

    urgent dark full reach touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
