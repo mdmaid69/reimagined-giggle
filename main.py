7b0c6ee799b55fc40aebd8ff43a7e8b466dc167c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:41 +0000

    connected open undo hard love

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
