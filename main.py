9e8414846bc6e19f6f412994b26f7c57fdb765b7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:22 +0000

    Vv when mistake inform talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
