ee1f0d53fc589fc411ac588d5bca0ce4e5599fed
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:28 +0000

    combination Ff shut expression stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
