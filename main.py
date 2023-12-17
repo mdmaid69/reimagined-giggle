7c99b37de50cb5796d4c8338a208ad8de8930be4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:30:53 +0000

    myself defeat include admit personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
