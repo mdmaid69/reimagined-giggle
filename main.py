8651903e93d3debb592618147f3334b9750223d1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:31 +0000

    through attractive obvious weigh street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
