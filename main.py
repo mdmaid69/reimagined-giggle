f3e48b7f3d748def7230d480c4c8880cf9c73a8d
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:26 +0000

    however brush lock no one under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
