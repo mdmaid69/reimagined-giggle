c2c40fae50a5246d415f278939ae1c6757ddb621
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:38 +0000

    breath forward blame fault related

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
