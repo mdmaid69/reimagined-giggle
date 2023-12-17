5c5c72d90aaa61d7ecf1ed0a1efce62f81c57643
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:23:45 +0000

    town ordinary tongue naturally history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
