028f8f8435ffd442ab45510366df8467f95e14fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:06 +0000

    connected use dry majority themselves

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-from collections import Counter
-print(Counter("hello world"))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
