fe2bfc0b81e85b73ac7a19e6f7e8203e0acff9d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:02:53 +0000

    try me judge neither month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
