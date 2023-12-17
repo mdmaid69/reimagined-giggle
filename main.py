41ceeec9d8ba4be1d0957b0eb1b445373c9e5126
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:00 +0000

    brown comeout attention next whole

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
