46736bdad0d3a096628690c470d0485871c95db5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:40:25 +0000

    challenge rush idea regular Tt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+text = "Hello, world!"
+print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
