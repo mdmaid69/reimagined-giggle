43611c30a9c5c7b5cf1e5b63ae567704ef7c9cbc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:43 +0000

    single point defend suffer politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
