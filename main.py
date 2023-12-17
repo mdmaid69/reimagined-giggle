4a1a009ae0fbf789247c2a757fa0d5c0dd4cc12b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:17:15 +0000

    part over please organization down

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def count_elements(lst):
+        return len(lst)
