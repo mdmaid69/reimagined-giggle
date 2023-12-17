06801a3a740b607a0b1b8671027e5c20a5e29dc7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:49:16 +0000

    shine perform bank there breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
