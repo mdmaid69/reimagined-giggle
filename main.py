d0c4c5c800e5fb731c885610dd8e456cf87bc741
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:17 +0000

    runout 564encourage patient safe communication

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
