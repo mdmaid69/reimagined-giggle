ece7a09de7f06e2b73b1a82fc77272131cf9a37a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:22 +0000

    pity active loud middle organization

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
