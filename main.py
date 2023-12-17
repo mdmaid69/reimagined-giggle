5e65c127a4534046201e46a83eca525d5b92ae82
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:18 +0000

    regularly control motion funny rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_stack():
-        return collections.deque()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
