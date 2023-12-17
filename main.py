12066645eab773570514fb48b8059c9315dc1fe8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:46 +0000

    adjective if platform skill Nn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def split_path(path):
+        return os.path.split(path)
