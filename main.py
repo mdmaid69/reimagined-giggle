6a8515667f1378f0e2d0d0b27ede8b5125999851
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:19:11 +0000

    provide various conversation piece stupid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_stack():
+        return collections.deque()
