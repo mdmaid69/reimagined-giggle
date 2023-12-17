425d7f7bb8ec0044e8d44761a5902164ef50a146
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:17 +0000

    hour hit female arrival hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
