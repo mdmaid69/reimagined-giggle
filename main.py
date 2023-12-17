e4aa381dd6c2d512aca3a97ec2313759a6a606f5
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:59 +0000

    dry mistake opinion 567enemy sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
