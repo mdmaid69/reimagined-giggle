60a37455492bdc00c488932d94f33464a9cfba4f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:44:53 +0000

    transparent prove sail callback provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
