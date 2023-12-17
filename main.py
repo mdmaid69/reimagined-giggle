686a20ae0c4819011784fc6a41a6c0b246bacd15
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:29 +0000

    prize tip unkind nurse use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import random
+print(random.randint(0, 100))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
