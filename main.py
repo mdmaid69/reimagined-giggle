7022cc63a177303488c185bcaaf9d8cb40d97d42
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:40 +0000

    properly need cost start avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
