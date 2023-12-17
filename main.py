f6bba668cbac749b65271d5bd43b3ad37751304c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:19 +0000

    brush value cheap finger mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+print(sum(range(10)))
