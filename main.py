ba1027fe18c4d9b05d5c672d6e207144026bad2f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:37 +0000

    combine tomorrow comeover nice thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
