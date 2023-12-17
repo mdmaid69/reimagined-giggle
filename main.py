12e908022f9dd317fa71b573be0149cdb443be25
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:53:50 +0000

    say cause intend chance duty

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
