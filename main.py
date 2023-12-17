4241872ef750ff445e1ce6c99ca4059377004006
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:56:41 +0000

    direction I run unable impressed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
