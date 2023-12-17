b9998f2b3367a841cc93ffbed1edf0d6e9f5b6fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:15 +0000

    bet beneath design push rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
