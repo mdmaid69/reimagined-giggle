d00d54dc2b9186ea7cdda60f119606dbcd132f0d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:17:51 +0000

    oil knowledge care watch protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+print(math.pi)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
