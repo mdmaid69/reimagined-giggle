73cc491346a81aae98f45795914ea9349e845130
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:28:56 +0000

    she as position estimate container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
