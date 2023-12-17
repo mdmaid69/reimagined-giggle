89d9daa9966e5dcbc5aac8900864a6ade0c71359
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:18 +0000

    fat proper interest awake board

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
