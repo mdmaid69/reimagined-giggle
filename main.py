47343fe661cb9ffa6746a3f16ebfd17a11005619
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:11 +0000

    sweep bringback summer display cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
