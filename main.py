dbe3c13aa4c617f05ede253c157bd0e74b170b92
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:14:25 +0000

    view front count production thought

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
