67a3c96e2c587262666e21ae6232818d47d3f1dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:37:55 +0000

    able determined stick centimetre advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
