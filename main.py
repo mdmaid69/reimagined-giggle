b24bca4cca3b4a0fdfa16780b0399dd74a7837f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:20 +0000

    well accidentally yes yours plant

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_counter():
-        return collections.Counter()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def reverse_list(lst):
+        return lst[::-1]
