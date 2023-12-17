27ac25d85ce1a904b8f12565cc8cf288acc6e0b1
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:05:00 +0000

    promise bit substance contain estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
