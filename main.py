5bad360974be918978153557e0cf900073c93811
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:18 +0000

    fit die it anywhere until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
