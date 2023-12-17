37ac5cabf100c12935171e7e41f779ec684bf403
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:58 +0000

    model leg track catch pleasure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
