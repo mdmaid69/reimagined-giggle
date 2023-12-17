ed4d32cbd37fd1765f0bf924549c5b1e75fefa1e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:09:28 +0000

    satisfied strike cause better slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
