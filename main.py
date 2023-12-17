1adb72e62dc68cc39115b7b21fe9713dd4618651
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:22 +0000

    failure round to record crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
