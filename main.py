ba21c7ca408f953be98b56ae67e6c46c1f77b2f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:39 +0000

    obvious cover determined lost question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
