f5878c084c2d24ae5b588dbdffd63deed3390c7b
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:12 +0000

    fasten fierce scare port road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
