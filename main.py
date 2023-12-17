a1378766d4c6a765d19da7c8c77de61639c71ebb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:56:59 +0000

    179bite scale bread pain Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_min(lst):
+        return min(lst)
