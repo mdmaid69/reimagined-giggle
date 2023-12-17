a495e2fb71787141fde632b698165f5d2eb7a94d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:22:01 +0000

    address doubt write rub anything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def convert_to_octal(n):
+        return oct(n)
