ab7e3a3415bf51f5b1a6637f25b0aaccd0ae0297
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:47 +0000

    sell wait polite danger conclusion

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+name = "Python"
+print("Hello,", name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
