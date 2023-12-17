96e6ee5cfda2f5af85af22a1b042e1e05c9ca470
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:57 +0000

    child popular spread praise measure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
