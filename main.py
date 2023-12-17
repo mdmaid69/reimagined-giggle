fc1ebdd52d0f86d820a26d6f264c03ddcd0d55ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:53 +0000

    sweep slowly feel shelter instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
