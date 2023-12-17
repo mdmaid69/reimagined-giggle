4b2e5b7ac3e489e0b82c1b500272ddc8784db935
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:22:06 +0000

    comment concentrate story ear steal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
