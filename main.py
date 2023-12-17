1697fa1bed287154481d62f9702eeb2aa85db056
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:32 +0000

    niggaacademy fail bank sting comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
