c729d88880830eb4f2a8dde2ff3a355484b528ca
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:48:39 +0000

    television spring sort offend left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
