1651ca1d4298a1b424f62a79fde1ff9ca665a82b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:36 +0000

    song confused free large square

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
