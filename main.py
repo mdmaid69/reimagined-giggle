66d2584a5347c1e1998c7517c5e8d66f8e5a52c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:21 +0000

    powerful ring special smoke pocket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-for i in range(5):
-        print(i)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
