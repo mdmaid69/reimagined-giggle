117f4000976a7ab55a503a4db3dbad210fa576b4
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:10:18 +0000

    dish lucky rise tight 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
