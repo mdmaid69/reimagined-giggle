657f3a7c1c3b03e58b613860983aa865a69bba14
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:13 +0000

    cover rank keep spring mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
