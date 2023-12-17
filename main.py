74009fd61dfad4710666e8d54f2e4bd322abf686
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:53:36 +0000

    relief news connected hope bring

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
