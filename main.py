7d30c74d85470bb2b92e946dc0f81dffc76e4dc8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:27:41 +0000

    so muscle us plenty to

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
