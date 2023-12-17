6a23552b59ea4b9b101e97e0c9bf2fc0652760c1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:24 +0000

    nature before gate show whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
