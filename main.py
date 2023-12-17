b1d1b8239ce9b8bb17757bd07ca642e2854fbd5f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:27:28 +0000

    pay coffee responsibility in cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
