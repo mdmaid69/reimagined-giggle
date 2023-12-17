968972a01b7056a842d54bb9f1f60450f6e4941b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:22:51 +0000

    whether congratulations guess stranger pattern

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def clear_array(array):
-        array *= 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
