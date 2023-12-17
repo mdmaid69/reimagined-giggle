31d20e1a165ad7e46376d880072b5e1853128f46
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:04:16 +0000

    every stir exist naturally change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
