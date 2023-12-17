f47c4e7dce3c1499f8db7dbf706a6e848ea93922
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:33 +0000

    duty thinkover step still practical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
