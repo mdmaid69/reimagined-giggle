0fe13d0a1c06c5c0148252193a9b5f4df3c63811
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:48:56 +0000

    phone goup miss discovery lend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
