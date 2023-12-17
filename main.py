d4fd09d45691266910ea1f793164aff4f0079d63
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:00:36 +0000

    knowledge fat suit ground among

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
