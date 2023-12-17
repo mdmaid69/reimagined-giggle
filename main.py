2aab3b90be8bb8a5987a47a4314e972f5dfd33f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:54:04 +0000

    definite certain spite independent doubt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_work(force, distance):
-        return force * distance
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
