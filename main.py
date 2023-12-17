8cdddb369a38b0b4c95f6b71196f2d95e26018fb
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:58:37 +0000

    afford rob pressure game crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(5):
+        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
