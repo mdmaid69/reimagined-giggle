dc47f29a6e6e4839665ff7058bc2e07c85077083
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:54 +0000

    transparent talkback attitude after lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
