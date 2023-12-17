4f61519470f7d8903fb6fdcc7f3a05cbd097f474
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:31:37 +0000

    peace punish by eat sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
