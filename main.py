9b5a44fd5ad93f2c96584b995cc4c58a4dfcd05f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:01:30 +0000

    classroom learn wait aim event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
