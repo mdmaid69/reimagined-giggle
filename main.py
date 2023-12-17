dd57f15fe37525e7466b139042f2726b98f0223d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:55:12 +0000

    off progress strike mind subject

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
