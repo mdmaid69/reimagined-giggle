106b816b40424846e8529a202fdd75ea996d6894
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:23:17 +0000

    left him famous home smell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
