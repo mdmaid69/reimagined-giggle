823df066a53f3b6f3a8f0c6bc9f5efe935bb7785
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:31:47 +0000

    winter takeback fire leg something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
