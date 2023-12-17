7be37f3218e1c91e5a70cb60532d66089f14c1d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:11 +0000

    exam shout mark return quickly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+for i in range(5):
+        print(i)
+def multiply_numbers(x, y):
+        return x * y
