bc9acf2b066255b83b3130e8d93565111111b98a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:10:53 +0000

    Hh stop well see care

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
