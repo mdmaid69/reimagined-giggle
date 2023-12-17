a1a2c43907dfd100377e536fd5d4d26a70acaea8
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:06 +0000

    second call teach same noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
