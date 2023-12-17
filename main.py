a7c14a23d2dbe206c611f83a30feffffc89f5315
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:31:07 +0000

    wise talkback order runover float

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
