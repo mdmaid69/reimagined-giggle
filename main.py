c4ba5c1569821a95c7c2d11787ff043a371d69a5
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:18:09 +0000

    satisfactory under thick separate stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
