21bb280c0e5d0e2f53c7544a5ae4b982c308e0ed
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:49 +0000

    ring spend principle needle nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
