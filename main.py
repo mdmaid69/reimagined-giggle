bc14673d1316add0bd3338aef4ba7cedc66587d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:22:18 +0000

    fortunate factory company last squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_max(lst):
-        return max(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
