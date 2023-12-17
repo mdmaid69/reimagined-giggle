8a9618c051464145eaa9669c146077921bea4533
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:33 +0000

    child disagree rough fear bag

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
