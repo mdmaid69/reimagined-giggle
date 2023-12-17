814584ce2f753140b420fd0da23c2a1930729a1e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:42:42 +0000

    like lot person press ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
