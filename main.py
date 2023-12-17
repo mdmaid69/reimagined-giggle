ee1f24008a08554b60368eb6e06ce6ca05625153
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:52 +0000

    bend address dangerous damage minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-x = 10
-y = 20
-print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def convert_to_binary(n):
+        return bin(n)
