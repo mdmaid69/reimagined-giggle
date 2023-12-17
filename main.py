0193f1adbeed5d1763591c9b73f394a1df3fd801
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:55 +0000

    shut mind mistake suggest meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
