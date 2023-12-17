d2e0e9f5dc4ce12433d5a47163ad59ef36e20ea7
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:48 +0000

    I accept culture ring system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
