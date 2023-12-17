38c723ece9d137897c99e282688e20885e220daa
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:58:22 +0000

    hit lookaround travel left mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
