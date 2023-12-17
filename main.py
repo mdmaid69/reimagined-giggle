4fa58ebd4b458bd9a70f6c3cfb6a222e4c2188e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:09:53 +0000

    dead clear active none like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_priority_queue():
+        return collections.deque()
