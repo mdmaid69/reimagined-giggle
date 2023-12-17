af01a4aa5ad4a5626340f88a668d3e056483db11
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:45:22 +0000

    best black row exaggerate body

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
