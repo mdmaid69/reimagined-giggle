cc5ea2b20598abd2b87c38b04052ab65e1abc353
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:17 +0000

    man pass majority tap travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
