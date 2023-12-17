469afaab7e12bdc667aa27e7002e6d03705645ec
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:08 +0000

    completely during shall bright pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
