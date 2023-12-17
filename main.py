5e92770713a8952027404314c5e939ab0427a408
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:15:31 +0000

    whenever bye can age description

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
