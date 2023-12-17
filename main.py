5d1043d2d158ea8b7572621b5ef6344a80b180df
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:08 +0000

    switch since dick wave breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def divide_numbers(x, y):
+        return x / y
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_frozenset(array):
+        return frozenset(array)
