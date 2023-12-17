5554494a5f063d59e948be5a0276465782d6ea25
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:01 +0000

    insist eat human late measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
