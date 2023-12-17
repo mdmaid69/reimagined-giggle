87adeb76895653c8923e42815711a49285dccc3c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:38:11 +0000

    point obvious Vv any advantage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
