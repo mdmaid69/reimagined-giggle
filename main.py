1178ed365a6db16294e53f2e49bf8b2538b1c288
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:37 +0000

    always something doubt politics blow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sys
+def exit_program():
+        sys.exit()
