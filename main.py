18aa039a0dbea61f8a745d782451cb92315020a4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:30 +0000

    store likely could nervous life

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import numpy as np
+print(np.array([1, 2, 3]))
