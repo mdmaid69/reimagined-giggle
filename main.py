4f91911d7453bab11f8ad05683c557c504f2b880
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:18 +0000

    bag breakdown embarrassing exhibition illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
