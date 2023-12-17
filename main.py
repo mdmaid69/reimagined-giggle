cea32e9f5c98384218f85d57abe0299a82f4714c
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:42:13 +0000

    doubt complete convenient disappear meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
