e959caa9df55db804451746e51d7b407072139d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:56:44 +0000

    prize match talkinto maybe software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
