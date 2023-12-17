e520bf586f53b1ad05f74a47f139453c6a3d9413
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:54 +0000

    square jump such describe warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
