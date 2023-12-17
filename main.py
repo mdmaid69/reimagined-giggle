45f734214887909aa61ea44c7964f9c55429e4b3
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:08:57 +0000

    day accidental delight march since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_list_from_array(array):
+        return array.tolist()
