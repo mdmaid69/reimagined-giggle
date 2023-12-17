c128510652a7fea13e10fac743538afbbf5b6f4e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:10 +0000

    unpleasant firm weekend open yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
