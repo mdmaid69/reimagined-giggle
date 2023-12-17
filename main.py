5f0ee55580cda5c2d3f0565485383cc04e461fec
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:29:21 +0000

    report maybe block fit wrap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import getpass
-def get_username():
-        return getpass.getuser()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
