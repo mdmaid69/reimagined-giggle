da0e9f1762222c890f284be3998078510fd9ff37
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:54:47 +0000

    badly brief wherever thinkover inside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def multiply_numbers(x, y):
+        return x * y
