31edac5d08c12bebc4113eb9e260b316b0a2bd65
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:27:32 +0000

    curious before more wonder order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  def convert_to_octal(n):
+        return oct(n)
