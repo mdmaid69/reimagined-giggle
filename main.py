dd9a509c625e761c8c2e53a5598e9d0eb6b74972
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:21 +0000

    trial community burn wise stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
