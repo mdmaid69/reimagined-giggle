0b924b87b88d9453bd53141bebf910d35d6cfa8f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:04 +0000

    comeout plan trouble its back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_acceleration(speed, time):
-        return speed / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
