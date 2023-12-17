3a132f7e64c715e15eeec62483907176a8b35b62
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:06 +0000

    goodbye ours error ticket excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
