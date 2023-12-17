f7019a10e51ffe00c647305e132d4fce712fa2db
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:38 +0000

    fall giveback attach measure regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
