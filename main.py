0222a6b1d6ffb48f9ebb59104bd95117788c17b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:55 +0000

    story storm nerve consist solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
