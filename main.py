c72e69333b67f828752ec7fd3d2c18bb6d9d9a66
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:59:31 +0000

    protect big improve question during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
