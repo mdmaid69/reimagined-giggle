d7a2148fe7e559d7e7c95029beb9323694639e5b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:13 +0000

    male majority summer less chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
