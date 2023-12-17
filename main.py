ddd2ae9d5f0782a0fb5e2517c7b6ea0fa7293063
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:49:33 +0000

    beside among suck comfort play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
