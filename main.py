6d8abb3629807ee7b6374e48459096a6de80fb9d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:14:36 +0000

    recently size today think marry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
