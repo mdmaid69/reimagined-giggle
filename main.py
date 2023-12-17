35f74b332fc51eaaedd9514ed11a1ec485878b42
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:21 +0000

    behave clean busy deep boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
