eed5383f090861834c93c0ee9b7793f8ceab08fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:12 +0000

    giveup valuable except patience question

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
