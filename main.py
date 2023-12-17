2f0f24a8f961d6aa83c35e3d30b51b68a70980d1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:50:55 +0000

    567enemy weekend past special tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def append_to_array(array, item):
+        array.append(item)
+def is_odd(n):
+        return n % 2 != 0
