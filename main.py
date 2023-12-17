53c9a47fd523036d7466ea952349925a11d5c9f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:12 +0000

    size force partner ticket friend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
