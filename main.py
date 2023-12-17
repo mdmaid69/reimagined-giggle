55c8e880bef7f9a2b1283af234567e21084d125d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:38 +0000

    walk something basic violent refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
