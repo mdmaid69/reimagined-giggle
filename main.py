6f0108e68295914cdd9c98dcfa9ee13db5943501
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:14:32 +0000

    collect doubt careless accommodation hard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_average(lst):
+        return sum(lst) / len(lst)
