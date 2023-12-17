66731e180db527d942fac04335c58d2d68491822
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:18 +0000

    operation find be please meet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import datetime
+print(datetime.datetime.now())
