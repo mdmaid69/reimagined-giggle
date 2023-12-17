4895939d9e50f33350251f4aa2efb42cb62c77d8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:19:36 +0000

    pull not unlikely square breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
