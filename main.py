853f6d067398fd1258f22d3d714d04eb2b6d0a57
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:00:40 +0000

    morning letter advice too official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
