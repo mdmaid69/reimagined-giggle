7e3a0fb8cf548a8708766aaa845ed0d2796d3739
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:35:22 +0000

    ability 570enjoyable level clever tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def sort_list(lst):
+        return sorted(lst)
