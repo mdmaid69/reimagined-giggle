fdc88caa0fedb670dd300756aa1ee51ccdd58fbb
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:52 +0000

    art admire row shop short

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
