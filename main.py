2c6983444059b50314399cd64a7e56cde35a6806
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:24:14 +0000

    star congratulate less wonder pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import getpass
+def get_username():
+        return getpass.getuser()
