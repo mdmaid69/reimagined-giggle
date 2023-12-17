e471f0115f85f82b9c71f81f5b3f0a37e3530618
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:47:51 +0000

    weigh pair shrink broken ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
