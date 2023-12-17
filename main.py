5c13d7f3661d508880428c330725e721101904a4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:59:42 +0000

    tough first the dear estimate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def multiply_numbers(x, y):
+        return x * y
+import os
+print(os.getcwd())
