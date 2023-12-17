591cd984c0dbe7626e37c36cb8aa35b90f1cf9f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:19 +0000

    comeback responsibility combination Dd century

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
