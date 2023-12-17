12be827824957686651f1f5c614d7bc3def7d31f
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:59:51 +0000

    neither difference come polite relationship

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
