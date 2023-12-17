58696b0b7c47a4fac5e05de55ffb133cdfed8766
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:23:08 +0000

    cut seem unlucky difference wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
