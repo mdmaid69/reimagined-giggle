6c35e7321db81053768a242ad61c25f88e5898a4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:20:48 +0000

    run raw sweep ticket front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
