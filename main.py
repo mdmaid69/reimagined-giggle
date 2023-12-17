990faa71b6bf6ef2186c02f0fac8fc91b6130c51
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:09 +0000

    before exchange race Ii bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
