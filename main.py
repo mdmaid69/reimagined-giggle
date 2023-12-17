6af89cebc26aa0dfb0d9c8fcabb9e95948183427
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:05 +0000

    doctor moral every confuse raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
