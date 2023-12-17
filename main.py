76124dd2135223de7c544c8b42e68c0dc7b2991c
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:03:52 +0000

    themselves neat study seat Yy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+  def count_elements(lst):
+        return len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
