9c526f6b29798ee7bf777fa49c9899e35ebb795f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:09:45 +0000

    else sick lift 568energy will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
