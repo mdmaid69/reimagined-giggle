61bf2a68e92429280bfd780dd304f9d86b6127ed
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:07:20 +0000

    bend thanks each interruption neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
