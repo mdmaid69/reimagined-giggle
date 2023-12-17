1582b0fd08e8b538ff84df2b6c3da461f9e2d4c9
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:53:00 +0000

    greeting treatment ahead arrange gift

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-from collections import Counter
-print(Counter("hello world"))
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
