340e769b796c82a55e00866664c9fa6a49415e3d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:57:47 +0000

    bridge total traffic regret pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
