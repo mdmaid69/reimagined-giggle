1889cacd01a52cb0f1634895a8a9adfb7717d671
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:54 +0000

    disapprove sign female sink reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
