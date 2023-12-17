6b9af34456ae657fc184757eb1428f591a124b1a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:58:34 +0000

    window tie organization through muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
