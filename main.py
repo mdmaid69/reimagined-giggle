6181eda3c80dcedeae635619c45d82c98c83c7a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:09 +0000

    short ago Pp slide child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
