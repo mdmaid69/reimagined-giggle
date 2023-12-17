e05a8c9df4a51848de2ae7ae763edea1961b11ad
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:28:02 +0000

    terrible into trip Jj Gg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
