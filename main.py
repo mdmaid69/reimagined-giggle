621cc3bec57b7a1caf6599268b495ed4f7936af0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:01:47 +0000

    179bite shape account letter thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
