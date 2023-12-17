bac69350fd93678336477ce2a0571dcbf46950c9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    all tear street scare H h

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
