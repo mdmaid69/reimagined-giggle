c38e8b68c2aaef1b3e590779cdc5e61edd4b2ed4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:27 +0000

    later power area illness pay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_hex(n):
-        return hex(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
