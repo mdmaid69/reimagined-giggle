d6a1d7dc8524103d2fb1afe0ebae926ccdb75121
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:08 +0000

    closely  stand unusual certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
