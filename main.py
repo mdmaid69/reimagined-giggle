bff91b937bf01639f78c53120ba90922976a7cca
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:48 +0000

    correct university hard test majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
