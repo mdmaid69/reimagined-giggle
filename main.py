0aa4ad350baf5fffc5bed4b50253e561ac299367
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:19:29 +0000

    whenever whoever me doubt run

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
