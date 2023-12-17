1e2fe6358474592cec092a30861e2c0f226d647d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:11 +0000

    nurse breakthrough increase floor sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
