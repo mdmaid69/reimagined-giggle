02c46417415ed3eeeb8df424d3b70efaa8175295
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:13 +0000

    watch along clearly interview long

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
