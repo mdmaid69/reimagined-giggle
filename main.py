f317215b127f29dd6fef6fecfd28c0bd042223fd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:28:01 +0000

    gooff cinema beyond twist crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
