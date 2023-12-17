6308cf7da01070dfde9edb2ad69c8fdbcf6cc314
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:47:12 +0000

    keen town so ilovedicks die

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
