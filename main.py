ad07e86dc2823455441cc35591997295004d548a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:08 +0000

    happen receive getup active bag

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
