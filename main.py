c2db83317e84dc531a4e6a845f4eb0889832b058
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:25:50 +0000

    Cc pattern tidy depend advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
