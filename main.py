04e09bb1e2fdc9528089ea806d3979d15d485b3a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:17:00 +0000

    harm male across buy unkind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
