9725d8ab0b7918e4e0d93677aff9f21ad8a44f49
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:25:50 +0000

    science artificial wherever stamp exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
