5265bfd5b803ebc5b7f5ee8dc13543e2d7c1a552
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:25 +0000

    project ignore mistake threaten scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
