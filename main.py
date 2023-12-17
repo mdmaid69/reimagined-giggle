dfb4fc605aa32ff618a20c0cab13728f1dc3406f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:48:09 +0000

    hit other whistle teach only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
