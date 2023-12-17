97c8fe2ff4280787628ebf11791c414988974226
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:49:30 +0000

    home rank genuine price bit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
