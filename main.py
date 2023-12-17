ed51bde028815babbea8402ff4ee8117d9832553
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:32 +0000

    extraordinary city regularly likely send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
