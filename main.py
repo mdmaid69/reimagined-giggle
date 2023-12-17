9196a690877f21734e00154cc839c99986d2b5d4
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:36 +0000

    succeed direction substance nose behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
