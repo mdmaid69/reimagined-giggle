af1ff3d8eec7304500297ddda359c26831e55db7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:21:45 +0000

    both squeeze foot convince innocent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
