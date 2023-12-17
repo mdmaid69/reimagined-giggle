11ac3bad5067e6224636bab88d727f4ec4670697
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:04:08 +0000

    point medium art aim towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
