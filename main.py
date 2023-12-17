8fb26824abed9089102a2ad738dd73f3394225b5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:26 +0000

    demand view march obtain find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
