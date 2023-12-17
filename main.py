f87a93a0feb6d272ca24fa57758bad60d0fbd671
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:33 +0000

    combination unfortunate Ee earn that

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
