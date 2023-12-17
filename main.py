a2f5730ced934198ce5d62839ada97d6a5c46bb7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:26:56 +0000

    570enjoyable tool forward contain eat

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
