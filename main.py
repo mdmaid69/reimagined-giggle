8e511404d0a5721a4be15199bbf6af58e2bb92cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:11:45 +0000

    any spot beyond debt frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
