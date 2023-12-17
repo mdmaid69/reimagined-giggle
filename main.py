31a314068dae097731dd6e1ed8423271309180a4
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:19 +0000

    plate bringforward minimum criticism accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
