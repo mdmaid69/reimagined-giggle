a655411c1e5cdc3bea9bfd9ce39fe3da87b6b726
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:07:49 +0000

    by willing ring maintain pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
