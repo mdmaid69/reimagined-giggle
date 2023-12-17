ac6b660bb3620253a6ce3eadb076cc85cdc5d4a5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:41 +0000

    public guide before complain receive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def remove_duplicates(lst):
-        return list(set(lst))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
