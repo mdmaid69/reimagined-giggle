743a955e4b1ef40710c435d653df9b4530c94d94
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:08 +0000

    coat beautiful activity organize sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
