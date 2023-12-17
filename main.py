8db99422025974cefa6a060e568f4e124e52c553
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:38:48 +0000

    strange strike mistake absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
