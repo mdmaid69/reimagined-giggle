fc8d996f9e723ad395ac1998f50640b67f124af6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:21:31 +0000

    middle light available shout spite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
