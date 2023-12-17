8cb9a4098510fd78b425e9fd9b9322787808fa5f
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:21:31 +0000

    drink happy luck them ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import random
-print(random.randint(0, 100))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
