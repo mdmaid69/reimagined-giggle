d5072b2d7c6a82155e755d5fc9652ea0a3d2621c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:06 +0000

    avoid serious effect command wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
