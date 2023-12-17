c3bf524e6694b63efd4cb2dbe85382587e169ab6
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:10:25 +0000

    blow lay dick trap square

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
