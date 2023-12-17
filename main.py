c9e15351891984e31bc35a531ae17cfc43babd5b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:32:33 +0000

    coat belong rely platform patient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
