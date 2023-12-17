c230667edd0ed6e2dea78cdd17715275cc25f6d0
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:03:23 +0000

    workon soon profit opposite list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
