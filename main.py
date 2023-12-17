4b5c708d6d617e330a7a4e20ad77903b62098ab1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:46:19 +0000

    school offence strange clever comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
