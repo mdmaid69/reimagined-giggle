e8ad3403aae3eb583369e608f2b2a08a1ff41b5e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:58:26 +0000

    exam level patient yours listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_pressure(force, area):
+        return force / area
