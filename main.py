9187ac725250f6a2092232f88ca8cfb6c7b929dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:46:25 +0000

    drop buy lay pay impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
