5bf3953e5f64a21a448af11acd692eb66c847266
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:08 +0000

    violent what thinkof closely last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
