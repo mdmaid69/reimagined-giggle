1e777266250ee9c9596f9c1d4b7c9a9f8ae6f63f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:40:22 +0000

    into scream Qq university comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
