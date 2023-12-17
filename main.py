9756448ca95b3cc245037b210d83ca05dcb8bf64
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:13:31 +0000

    maybe most learn hand Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
