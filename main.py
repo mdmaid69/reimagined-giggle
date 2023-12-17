8af62efa1af505aa47630f96b3ca86adef1d8d2f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:56:17 +0000

    commit uniform again flat price

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
