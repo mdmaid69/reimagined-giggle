bf455e58783ba912fc60ed2da243485b467c0bdc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:43:19 +0000

    race visit piece Yy dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
