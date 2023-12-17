dc2fe28c52f4f2cc909420643ef5c4468cd5c5f4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:43 +0000

    weak I quickly oil shopping

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
