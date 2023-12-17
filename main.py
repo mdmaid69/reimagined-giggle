e51de882379b90dc47ad6272cea6baa1a91e5010
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:52:35 +0000

    smooth turn cope tie home

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
