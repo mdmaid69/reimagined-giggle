dbe8e884475b32832614a40c87f546a8bda8bcba
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:27 +0000

    surface stick flat naturally inquire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+for i in range(10): print(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
