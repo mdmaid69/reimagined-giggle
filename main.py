4626b420f5d45d3f9d2d1347cab16009b3e27422
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:32:08 +0000

    concentrate turnoff Ff business exciting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
