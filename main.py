a7f72d05f09bdb820754bdd34f94b6a3d1e00830
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:45:36 +0000

    hungry goon shoot else underneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
