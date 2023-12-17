05640a47815447fbad34ddffd692f7187d9eeedf
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:51 +0000

    positive leave false talkoutof rescue

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
