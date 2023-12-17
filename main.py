c6b8454b926cf1aedb06bc96dc92f3c1d7203cc8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:49 +0000

    visit difference drop negative callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
