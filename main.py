cc0c04b4134af0492695d12c18955b3906cfafb7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:45:25 +0000

    Jj chief gradually commit sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from collections import Counter
-print(Counter("hello world"))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
