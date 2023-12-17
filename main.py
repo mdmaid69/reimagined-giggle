3046cf887a9b825960dd67c084122e062ff04d22
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:45 +0000

    573entertainment callout out run centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
