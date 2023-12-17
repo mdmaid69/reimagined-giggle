8a992a220726d6eba71527e4745ca2968e4bede1
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:43:37 +0000

    search pay lower minor image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
