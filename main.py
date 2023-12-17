d5c519587028b38bfda1fcab7bf87fdffb6a0ed3
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:43:52 +0000

    force separate only rank sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
