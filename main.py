29b3002e9c8f9f54af69fc6394b0fa063983a7ae
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:34:10 +0000

    top depend rely extraordinary sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
