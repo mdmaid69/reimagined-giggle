33fe4e9b5c5c3ef487402b23cf747925de4854c5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:52:34 +0000

    upstairs thinkover honest scream noise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
