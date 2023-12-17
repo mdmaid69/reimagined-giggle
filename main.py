20a3575c809eab4b86137668cabb1c7c86ef4252
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:48:57 +0000

    comment hurry law female tie

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
