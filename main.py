873ec1bba6175bfcf2187990c11b7026d09216e4
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:03 +0000

    score fail smell lookaround score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
