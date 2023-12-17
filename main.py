55e31b64fdd6e3effa03ba8f1cd3fc24352fd67c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:53:08 +0000

    several neither position calmly behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
