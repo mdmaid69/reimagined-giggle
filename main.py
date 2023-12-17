5be1c9738f26023418c089c8f7fcbcc4dd9a013f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:08 +0000

    eager point thanks confused estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
