763c3c13887c28c37955d7555afe8938dc1beccd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:37:43 +0000

    cause add borrow so raw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
