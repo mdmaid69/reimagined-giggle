6b18c4eff02ff1722b4791165ca013f852aa3575
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:20:12 +0000

    lock present various life lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_error_function(x):
+        return math.erf(x)
