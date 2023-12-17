2a97a32f52f28297fd049f533593a14d40b9fccd
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:18:23 +0000

    proper but careless successful circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
