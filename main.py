f0ec9003fc369e68db8b48ccba8695d56b04a0dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:13:04 +0000

    talk wise watch union apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import platform
 def get_os_info():
         return platform.uname()
