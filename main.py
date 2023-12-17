6bfc3541f49d7002cf8a9a51fcdc18b06f513cf8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:42:20 +0000

    rob ill relation complaint downstairs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
