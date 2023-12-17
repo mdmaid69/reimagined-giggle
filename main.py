ea80f95be03bd54334ac7e3b51ae6ba37725a579
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:22 +0000

    fill violent mouth touch look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_min(numbers):
-        return min(numbers)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_density(mass, volume):
+        return mass / volume
