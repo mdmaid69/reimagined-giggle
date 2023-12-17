b713679f1a96f311280f83659d84dc663072d87e
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:48:20 +0000

    stupid study frequent actually male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
