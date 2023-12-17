4f4611bae2447b97fd8a389c7df82098975abd96
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:40 +0000

    flat week matter song risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import time
-def get_current_time():
+def get_time_since_epoch():
         return time.time()
-def square_number(x):
-        return x**2
