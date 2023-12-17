b672aae78959bf9eed629247c1b32bf11a229542
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:38:25 +0000

    work something copy proper say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
