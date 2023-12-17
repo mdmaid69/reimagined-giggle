6c58f3c5e90d0280871cb41790a0a8d519521eb1
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:09:50 +0000

    design repair shock mad look

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_min(lst):
+        return min(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
