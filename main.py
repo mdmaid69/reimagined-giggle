3d06a864ba62a273e7fbcfe8b6d6b9190b648b57
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:43 +0000

    once classroom basis makeinto cinema

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_index(array, item):
+        return array.index(item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
