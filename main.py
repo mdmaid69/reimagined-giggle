64b5b2c7834744b64ddd97fa486c08617d8760e9
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:56:20 +0000

    own pull run lend key

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
