1ce4f6c6974fd74d3fcc201c77775d08b4fe892c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:48 +0000

    whistle stand voice left listen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+def convert_to_hex(n):
+        return hex(n)
