c7be730482703a208e1af8719562ba265e7087cc
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:50 +0000

    benefit weak speak minor body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
