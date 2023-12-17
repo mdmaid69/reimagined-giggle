aad9c847a88b314c4cc1425b61304e8ce8dbcf69
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:50 +0000

    pain sale look wish considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
