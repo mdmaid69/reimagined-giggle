92160e8a58231b97a8761b05204d4f9eb6982bc8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:01 +0000

    national embarrassing let let rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_power(work, time):
-        return work / time
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
