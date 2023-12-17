4ccfbb31894989a09e341ab94c5e38863128df96
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:55 +0000

    anywayanyhow themselves plan swing over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
