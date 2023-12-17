2ed79860b0f54666fda0f6941d7070b707521cd3
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:58:06 +0000

    post person pour out discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
