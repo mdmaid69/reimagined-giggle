be6235f89987c3a9d887fd3a57492921f368756e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:35:29 +0000

    dress welcome introduction side wherever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
