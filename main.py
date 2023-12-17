89697ca77888f7fddb28129d8bd331e6842a94fa
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:17:07 +0000

    scare fork frightened kick strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
