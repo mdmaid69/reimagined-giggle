40c8fa09ad4c43b1d2fe768f7f9be97c373b9a4a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:11:36 +0000

    excellent goodbye free her related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
