2ae7cd2158df5d55d93e1970b7bbeb0e217f0d7e
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:44:57 +0000

    mend whom attractive reject beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
