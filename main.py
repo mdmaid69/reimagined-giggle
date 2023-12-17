186043b422487056f4f0e5a52a6741a49db0aa32
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:04:30 +0000

    save trip chief operate have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
