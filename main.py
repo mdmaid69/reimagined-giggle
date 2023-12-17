79ebfac31020992e92ef5c62b5f7f9b41be614e0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:29:19 +0000

    factory minor agree throughout disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
