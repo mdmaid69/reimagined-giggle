00d2b36ad47145107093e3fd81744f369f047727
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:38 +0000

    long turnon lie steep bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
