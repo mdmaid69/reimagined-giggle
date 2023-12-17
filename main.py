7095d1d92260dce34f589faef7c56959eb8bcc81
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:11:02 +0000

    wrap view inquire breakin heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
