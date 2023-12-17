83dcc1eb4597031ea5921faaaca9334bb36e1bc9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:25:12 +0000

    pause common pupil geton onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_acceleration(speed, time):
+        return speed / time
