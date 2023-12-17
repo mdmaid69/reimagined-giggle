2f6d478c8ddb0e0eb5cca56218fcf1a26700dbf1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:57:21 +0000

    problem ear case permission test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import platform
+def get_os_info():
+        return platform.uname()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
