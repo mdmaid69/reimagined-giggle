75c2c2830eff3a42a1b97abd576388b731f10d0a
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:26 +0000

    rain design call that plan

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
