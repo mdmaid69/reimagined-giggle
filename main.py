f55009a353208689094976f18922e89238982b2f
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:29 +0000

    speech precise answer stand other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
