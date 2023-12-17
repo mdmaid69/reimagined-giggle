cf4d3a0c55c40dfa1fcabab5c1f2f5012b29301d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:28:24 +0000

    play begin turn prevent lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def get_current_working_directory():
+        return os.getcwd()
