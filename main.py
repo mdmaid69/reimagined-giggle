ab3dc571ced83b1d697f97524889ab3cc58d9c6d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:20:49 +0000

    sure friendly not disapprove rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item(array, i):
+        return array[i]
