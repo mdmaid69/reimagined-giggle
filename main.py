d1c84e9c0d501cd6855cd07a7ab74f4fe2cc4e21
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:40 +0000

    love embarrassed expensive cruel many

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def multiply_numbers(x, y):
+        return x * y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
