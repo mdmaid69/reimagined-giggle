20e6e528e1982e7f9cf5d275457d1d3e26fcc33a
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:56:10 +0000

    circle upset inside basis science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+print(os.getcwd())
