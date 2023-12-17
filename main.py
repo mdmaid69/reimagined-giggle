fb4c54f802b41cb2a5ed574d33a019e5675461b2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:50:41 +0000

    forward body OKorokay steady choice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-print(sys.version)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
