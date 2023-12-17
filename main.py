5407dcc951a843f23ac07106637e2941c5e90079
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:05:00 +0000

    until reply spring dance calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
