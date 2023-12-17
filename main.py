10619817f068a34e86493c7559580165fb325941
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:27:20 +0000

    city 571enough murder birthday stamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-print(os.getcwd())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
