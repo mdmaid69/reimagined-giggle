e646c610149796c7c60453be3bc673ebaad1745b
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:13:59 +0000

    uniform strong Kk degree freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
