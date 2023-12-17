11784ff394f244c3a9ef50f4f2142c1f29a70244
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:49:20 +0000

    clothes platform place vision correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
