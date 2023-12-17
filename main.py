8b72be785b1c675329bd42d38ec2042bb76ca17c
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:40 +0000

    impressed frame expression push comein

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
