9300c0c034b715d3f0b81ccc7dc85344ba29ff96
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:16 +0000

    light slight shine that plan

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def is_odd(n):
+        return n % 2 != 0
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
