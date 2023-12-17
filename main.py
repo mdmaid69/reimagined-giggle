cdf1e88a0c7f93104069a0fb4757d32f8fadada7
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:40 +0000

    lesson choose by fresh thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
