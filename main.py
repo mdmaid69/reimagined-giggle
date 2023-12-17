5bb540f88e822478ec5755d13255b8d0f509f6a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:38:10 +0000

    story common race show sweep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
