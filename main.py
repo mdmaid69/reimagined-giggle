547d9240775f3294d903e3b28cac551196c3376a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:12 +0000

    matter breakinto second difficulty properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
