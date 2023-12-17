d097d01c4a0413300fded295d57f6c4c315d3227
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:06:45 +0000

    bush intelligent town affect afternoon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
