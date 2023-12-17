f6d72be0f40900645db5625ee6475dffc0461089
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:23:16 +0000

    pay upset spray Ww close

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
