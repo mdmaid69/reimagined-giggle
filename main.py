85df92f14cf889fb6c657c19fb709f98ad47f406
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:28:35 +0000

    rush welcome perform sink unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
