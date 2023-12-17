06ebe687fc0f7058768be993eb9ac36b539d011b
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:17:53 +0000

    thin sail mine excuse television

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
