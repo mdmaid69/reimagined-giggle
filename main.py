546945f8fdc4cec03f78095d47ec24f581cf47ce
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:33:42 +0000

    give story check temperature born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
