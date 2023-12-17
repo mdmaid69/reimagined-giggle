9d88a883d016f1a43d6355dae74cc14806221a9c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:59:09 +0000

    scare small sensitive field Jj

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(10): print(i)
+import datetime
+print(datetime.datetime.now())
