6d0beb667a03585009e91ec4efe0b612b6431ff8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:38:06 +0000

    free autumn improve gentle meeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
