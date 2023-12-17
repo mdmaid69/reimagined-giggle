fdfc2c61205abb850de51e46cdea74d382859764
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:23 +0000

    after nearly follow keepup bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-print(random.randint(0, 100))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
