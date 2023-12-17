ee7746d5222e6bddc8c94688e0f8ff90dcb5fd5a
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:13:12 +0000

    effect deep loud Ff right

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
