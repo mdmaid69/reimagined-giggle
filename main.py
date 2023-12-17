d9e0d356064858edebd9bec6d6376a656d34b26d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:02:28 +0000

    damage gather form second warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
