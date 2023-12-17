d8e455ca88deb8ad933a51b116a2e397884a0672
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:50 +0000

    alive trial deliver couple everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
