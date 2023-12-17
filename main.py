75cd2d83424d1a511c51164806e84f208e7acb30
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:59 +0000

    star swing hurry underneath rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
