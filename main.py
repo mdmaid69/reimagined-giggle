fd244b024a5d8a185d7117d1d21a10bd9467e66f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:17:07 +0000

    forget something smile uncomfortable 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_work(force, distance):
+        return force * distance
