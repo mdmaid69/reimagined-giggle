86a896c315e1fdfc27293a147fcbb6b1cc44fb85
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:41:42 +0000

    immediately pour loud somebody sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
