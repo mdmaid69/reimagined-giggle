a79545198e57e13d6ea57d1610894e94a1057dce
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:43:49 +0000

    shop holiday pool get necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def get_array_typecode(array):
+        return array.typecode
