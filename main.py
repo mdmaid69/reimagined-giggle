2d753a091d1725e5ddcc304a84017ae7ae147bd8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:34:23 +0000

    divide set unhappy relax bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
