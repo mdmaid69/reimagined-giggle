f7c6f23de04532a7320d3725dd953fbbb731cdb2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:55:02 +0000

    for touch scared maybe gift

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print(sum(range(10)))
