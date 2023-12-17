a419a6cc70584a66846535668474fe22a7724447
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:48:15 +0000

    gooff type smile also left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import time
+print(time.time())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
