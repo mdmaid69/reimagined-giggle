c76653e05a8807653f724370392007db726e0e4c
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:06:34 +0000

    married attract trade design up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
