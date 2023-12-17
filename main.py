2a5e8ef1d4d40e425299f4cc1f1b0debf6e2435e
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:33:28 +0000

    trousers uncomfortable pot for tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
