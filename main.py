580bc1eebb3fc98c7cca4260553e1ea5aefb4d41
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:37:51 +0000

    branch phone plan very cheap

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
