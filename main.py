494933cbcb03e25f9607cf550adb152beda9413e
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:34:58 +0000

    manner ear care risk shut

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
