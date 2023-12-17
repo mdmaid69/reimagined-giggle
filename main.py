530e6fb4d207ea58d120bd5c24aff72e1c9e4e68
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:51:58 +0000

    still strict soon fact middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
