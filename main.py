cb1473c234bb711641ec640aba43a276c19328e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:12:52 +0000

    problem turn thinkthrough extraordinary what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
