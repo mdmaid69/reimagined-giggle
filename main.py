a2d75822e068ca34cf94abc7fc6b44851f8f5bd5
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:50:42 +0000

    partner culture race lie film

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def cube_number(x):
+        return x**3
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
