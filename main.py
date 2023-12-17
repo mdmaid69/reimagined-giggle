c35b3c5164e4a910a6c752ea7682e38d0224fa7a
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:12:05 +0000

    obvious when thing short ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def reverse_list(lst):
+        return lst[::-1]
