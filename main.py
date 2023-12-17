a4f909794af9b427ef5ce1c65000fed3797bb68d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:05:04 +0000

    paper short fresh surprised crime

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
