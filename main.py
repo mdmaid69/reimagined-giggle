03bbc9915e5768cf4a8a44f5573666f823f432bb
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:43:28 +0000

    getoff expensive freeze juice tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_counter():
+        return collections.Counter()
