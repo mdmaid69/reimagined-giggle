eb936f148868a223ecda24ae4c433722e86bc90d
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:15 +0000

    back unfair plan other physical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_array_length(array):
+        return len(array)
