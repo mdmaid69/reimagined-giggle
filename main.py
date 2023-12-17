bf05704ae1cdb1bbc90442ae8efa356a12d9c617
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:17 +0000

    begin concerned star plan against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
