1a52ce2e717bb24bb48f9c5305bfc29f13a46d89
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:52 +0000

    571enough possibly mystery seriously thought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
