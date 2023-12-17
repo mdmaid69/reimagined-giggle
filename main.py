d66b91d743e1b70ec364a4deb7a93150fd767b25
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:41 +0000

    decision defend also bridge compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_list_from_array(array):
+        return array.tolist()
