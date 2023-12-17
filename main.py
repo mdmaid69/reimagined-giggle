dfaf6bf335dac59863e2dd51d482b0f3832ed336
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:13:52 +0000

    cut flight punishment talkoutof hate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
