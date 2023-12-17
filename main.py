64972d2b91bc4b1fff0a610c804fa38323f71158
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:13 +0000

    frighten brave steady degree real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
