a5efbd9905961ca81ca55e662e1f527869fb49c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:49:15 +0000

    very plain only record side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
