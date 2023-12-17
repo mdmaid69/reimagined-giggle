89f62ea52968ff15f889377c5bd03e82077550a9
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:36:04 +0000

    survive whistle wave wild anxious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def subtract_numbers(x, y):
-        return x - y
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
