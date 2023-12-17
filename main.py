b83e28c8ac99e4eb07b67c51b7a94525d1da8047
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:27:29 +0000

    exchange window love inform hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_int(array):
+        return int(array[0])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
