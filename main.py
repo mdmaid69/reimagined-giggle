38a471fe85b1454581594b57c35719d07263c65b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:16:56 +0000

    listenfor comeup health earth copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
