91341258819b0d3c53bb15979cc17594ca21c8f7
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:05 +0000

    formal opponent introduction contrast chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
