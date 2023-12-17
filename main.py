daa1aa6e735cd63d513d5f2c06f979df8e2e5e36
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:57:27 +0000

    wear uncertain care punish row

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-def print_python_version():
-        return sys.version
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
