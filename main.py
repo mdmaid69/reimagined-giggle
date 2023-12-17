f728d5da74d36269ccb2e2cc71e07e1b80f9d2dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:23:23 +0000

    sick honest habit walk contact

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
