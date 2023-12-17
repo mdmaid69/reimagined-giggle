cbe2e0ff7cd3ad158532a028a8a3391c73e8afbe
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:06:36 +0000

    form left wash shock protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
