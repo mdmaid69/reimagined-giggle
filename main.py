ac4f2cef8988718fec193f36e2e3810783f4ace4
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:10:40 +0000

    loan mean connection damp law

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
