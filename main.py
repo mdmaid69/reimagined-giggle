adc0db24adbb14be647b1a7477f454c5edfa44fc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:56 +0000

    remain contact chance better spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
