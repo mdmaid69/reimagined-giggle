90b98b7280cf35d6268a8feb81609bba31c2548a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:48:17 +0000

    contrast find answer maintain unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def convert_to_octal(n):
+        return oct(n)
