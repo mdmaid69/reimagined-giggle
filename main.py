77f94dbb9f1efdf6d35e1e0dc12bdecfc3ed49a2
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:06:39 +0000

    niggaacademy please crisis empty polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
