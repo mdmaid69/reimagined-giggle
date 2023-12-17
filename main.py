f0986bbd7cb21cc645dde95e12a460bccb4d77a5
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:01 +0000

    all year wash generally cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
