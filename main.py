9ad6489420e9f9e11337e90b4f3529bc0f6e8165
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:35:42 +0000

    age pump giveout form comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
