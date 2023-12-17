113202cade96d0aece5444fab9f3e14bf3142d19
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:47:13 +0000

    burn limit child time everything

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
