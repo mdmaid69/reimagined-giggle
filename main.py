78fd5d81c43b28e7137d1f403dab7237e7b53e6f
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:58:30 +0000

    key advice interview comedown happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
