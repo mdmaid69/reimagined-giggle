b21cf0efd977cb0cd54bbb05ce86b1ead0f1e420
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:46:59 +0000

    far that age flight far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
