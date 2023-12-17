c39fc790d6f036875ee00094f21d060674136494
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:54:22 +0000

    treat among straight mysterious way

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
