81ea37c24b95a5ee1ee5339f7181f33ec96257a0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:15 +0000

    furniture Nn must fashion race

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
