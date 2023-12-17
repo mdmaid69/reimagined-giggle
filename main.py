2681005e0e86ab010021170de8642ed1eb43ff0f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:44 +0000

    comeround through emotion somebody whom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
