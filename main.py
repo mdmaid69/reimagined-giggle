179f34a60c1b1d3da222efc44eae4e6451668f2d
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:35 +0000

    meeting extraordinary embarrassing serious tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
