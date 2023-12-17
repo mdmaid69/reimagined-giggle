ec5d632c9105d02c5505f4e6f728efa7e827df9c
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:45 +0000

    base crash mind welcome detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
