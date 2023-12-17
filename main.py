7dc41c63aa36dfc39d8a9993af81a4dbcef397d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:15:01 +0000

    sore closely stand leg Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
