6a761c0f884f8124cfbbbf54a06c5c36b5dc8e64
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:50:13 +0000

    promise model build size build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
