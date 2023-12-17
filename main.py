6a2abc880869c1609370dda2f80e5b66672507eb
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:37 +0000

    trap honest supply report address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
