14302f1bf17ac6e17df27af6d2e9dcfa2653dabc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:13:17 +0000

    sink dish surface street reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
