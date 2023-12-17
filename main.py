262a0cb957aa75876739614c8cf2cbe897cb1112
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:14:02 +0000

    immediate doubt question obtain butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
