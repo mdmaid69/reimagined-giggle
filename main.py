a4c53d1139b0883c08ebe183a66cde6232ae7e1d
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:03:46 +0000

    sweet sense them ready busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
