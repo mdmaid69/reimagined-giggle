3ef6b7c9929c0df3b767e414c0f5cd22277e4a65
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:55:44 +0000

    concerned breakfast drive react surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
