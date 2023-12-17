7f08c3812ffc734d45463153c605eeff09b602a8
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:44:56 +0000

    task all ride activity amount

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
