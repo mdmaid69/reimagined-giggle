fdae44dc6a5f587c61977e9ef093d59988af3246
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:16:20 +0000

    workon equipment choice our freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
