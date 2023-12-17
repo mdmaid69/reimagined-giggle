66c46e9ff0468a7d9b8eb3c0d1a332071525ac95
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:59 +0000

    letter sure on sail union

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
