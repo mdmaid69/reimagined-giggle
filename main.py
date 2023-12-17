b93a47acf36ba9dd382227ffa57a1be466693d38
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:15 +0000

    rescue share though mention polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
