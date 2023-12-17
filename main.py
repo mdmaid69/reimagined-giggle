a9a321dea97388167147be828ca8a365405fb14b
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:36:54 +0000

    play somehow greeting bringdown smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
