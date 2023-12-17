5c99e7d00b83b67eb26e5c14d0c58557d7fec520
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:20:43 +0000

    extra broad watch breakout full

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
