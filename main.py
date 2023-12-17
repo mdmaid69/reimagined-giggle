85161a3cde683a0c4ce0c3b40f0feb0d3c956e86
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:49 +0000

    consist first hard beginning backwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-print("Hello, world!")
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
