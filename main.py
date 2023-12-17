c137307f296c16b9e3a3f1fc755cec2c01dcad77
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:18:26 +0000

    goup smell during appeal peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
