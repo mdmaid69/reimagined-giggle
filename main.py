3938a3a23119602c690c2e0d49340243de65cb43
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:24:11 +0000

    by possibly straight lucky own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
