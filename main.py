bf38f3fe48564b2e024931c00564dc5ce30940fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:39:08 +0000

    tomorrow immediately experiment giveback lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
