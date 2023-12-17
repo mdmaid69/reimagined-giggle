ed4bb640a9e88eb42d42187258bc33e43a806440
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:12 +0000

    stay original N n no excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_min(numbers):
+        return min(numbers)
