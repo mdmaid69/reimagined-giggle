653d7f37b1f5fc63979eba0e3d4375e8bb65eadf
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:00:46 +0000

    high check declaration throw Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
