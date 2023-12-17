7fd4a68a87434ee9f5a89adca13c7e6d6938cfde
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:32:06 +0000

    regularly separate blame more situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
