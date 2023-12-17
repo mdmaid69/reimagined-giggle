b56c55f7dd5791de27c3df6f33eda94eafadd553
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:29:02 +0000

    activity live exchange development trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
