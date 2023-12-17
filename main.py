19c559af27851d1b28cfa212560584c5700102e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:31:29 +0000

    although guess people memory heat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
