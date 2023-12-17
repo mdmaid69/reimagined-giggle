cef8d25dd556f2c51a0eb9449572be05345a6f96
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:51:11 +0000

    awake equal afternoon tight service

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
