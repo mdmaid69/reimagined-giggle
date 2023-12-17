50db71377b9dea59a03bec20956d405b60b31422
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:14:35 +0000

    somebody trap makeup meanwhile suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
