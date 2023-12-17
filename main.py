edc0cb54291a66ca7f787bb9056c7cd92fbd6afc
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:18:04 +0000

    after serious exit raise spill

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
