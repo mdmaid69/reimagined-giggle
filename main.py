4ec5f1517acd909bd944e21f09636ca4300f860b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:31:18 +0000

    road show wide university on

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def find_max(lst):
-        return max(lst)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
