ff92a70c888e7711c77d75952cc5bd73ec38effa
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:35 +0000

    runinto present block rate cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_string(array):
+        return array.tostring()
