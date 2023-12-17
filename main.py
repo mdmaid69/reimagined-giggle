0fef7478ec31fa33403b7f5728fd7deb3f0f1293
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:46 +0000

    statement Uu experiment time blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
