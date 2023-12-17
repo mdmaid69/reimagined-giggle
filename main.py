49b75a28b6fabd4a00a26bd2587f1194384d16bf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:51:52 +0000

    prize steal loud some person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_list(array):
+        return list(array)
