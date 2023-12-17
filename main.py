e5ff4920c68ebe963617e321192072f096b2b5f6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:08 +0000

    delight black steam board mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_typecode(array):
+        return array.typecode
