d4539294dd9f56d1204c3cff104082616525ace8
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:11:43 +0000

    shopping issue floor person edge

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def sort_list(lst):
-        return sorted(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
