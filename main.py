8f2b0889d8bccd914a39fb710b9a6cad4b725015
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:07:52 +0000

    tragedy run trouble pass vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
