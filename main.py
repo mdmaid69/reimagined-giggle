5e9836440540bec7362d7f677e8d8451204a171b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:12:22 +0000

    heavy relation load use memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def divide_numbers(x, y):
-        return x / y
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
