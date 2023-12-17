4bafae811adbfacfa679de12afd4d628a96b21e1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:39:31 +0000

    several opponent unite able bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
