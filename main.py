d190edb13983e2a952a705a668ae40d097361219
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:02 +0000

    switch any free only only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_area(radius):
+        return 3.14 * radius * radius
