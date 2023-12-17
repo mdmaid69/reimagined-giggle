3a3fafeb659b54c4149975b1f61430a2200d2563
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:52:13 +0000

    deaf opinion conduct fix freedom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def convert_to_octal(n):
+        return oct(n)
+def reverse_string(s):
+        return s[::-1]
