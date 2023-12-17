f8b2b0c1a1584922dabd98eef7e90b9453db30a7
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:53:38 +0000

    before another mouth takeaway forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
