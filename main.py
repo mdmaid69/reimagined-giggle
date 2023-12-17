445d957a6e860ec077964c0d7d66efba6e1d8516
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:33:10 +0000

    heavy destroy neither exercise soft

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
+print("Hello, world!")
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
