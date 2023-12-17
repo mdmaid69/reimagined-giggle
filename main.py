35950649b3e0548d49a71693c8b53732c1a7df28
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:41:13 +0000

    expensive wave all cut rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_length(array):
+        return len(array)
