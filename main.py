48cf6434f8086b0047cd6337d7a891a426c3a5f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:46:13 +0000

    expect invitation snow couple blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
