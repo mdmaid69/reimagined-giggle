b283621026d63f55c70e286a09bc6d6a85417694
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:02:40 +0000

    winter face gather dress nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
