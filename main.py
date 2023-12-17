c9884d265b1b093429a155e36e8508844b24a8cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:08:27 +0000

    Uu lose trick detail present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def remove_from_array(array, item):
+        array.remove(item)
