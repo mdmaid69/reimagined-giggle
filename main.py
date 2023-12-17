c2d780ed7660b54ff49b28821ec35ce708dc3268
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:02:38 +0000

    level supply test this flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
