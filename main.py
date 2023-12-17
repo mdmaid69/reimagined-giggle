985a66006939521283002e22a856db82554826dc
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:02:23 +0000

    pattern track kick thinkthrough reply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+print(time.time())
