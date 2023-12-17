8656262ab1ae4d06ef6a7deab24457caa4117931
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:12:00 +0000

    art oil my cause previous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
