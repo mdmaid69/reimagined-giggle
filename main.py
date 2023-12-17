96ece2688f4a83f00397b9b0400f27ba157ad87a
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:07:40 +0000

    danger declare interest stranger wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
