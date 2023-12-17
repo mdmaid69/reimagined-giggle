1305454c67c934d37322bcfac84a8d011079aea1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:08:23 +0000

    top visit foreign maintain lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
