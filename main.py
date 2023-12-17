cf9fc55655f8b879d953612841df7286bb7325c3
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:01 +0000

    very blow pen congratulations fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_speed(distance, time):
+        return distance / time
