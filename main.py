cc5e9426088469d680cb6890cd3c0254b363953a
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:57:41 +0000

    grateful goodbye split holdup average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
