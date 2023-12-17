2195f0516d222ce5917d0e8b6d6e0f4937ac914d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:09:40 +0000

    limit believe throw nor passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
