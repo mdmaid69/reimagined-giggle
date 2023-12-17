8f54960042d03145d90c6fe9811cc5bd757006b1
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:16:55 +0000

    friend excuse well safety last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
