8b23f42664a0c24d501ae5b43b204e936667fc3d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:53:57 +0000

    building issue sudden basis turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
