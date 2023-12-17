9e6378f78f442b44f02f48c26cf1fee08618a6a1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:53 +0000

    live rise responsible grand set

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
