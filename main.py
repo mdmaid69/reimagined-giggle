424b870b86da3486d9bd497b9a6c5fd83f302a7d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:06:52 +0000

    squeeze improvement unfair really hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
