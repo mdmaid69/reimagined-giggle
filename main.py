4e9a49c5a985495c5cb7ef5acbc9a18feb432e21
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:41:51 +0000

    use earth careless quick throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
