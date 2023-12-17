7b1a57159e9eba342acee6fa4bd2c7a5cae03a31
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:22:56 +0000

    week classroom collection holdup close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def divide_numbers(x, y):
+        return x / y
