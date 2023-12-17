86f2a819c625422258ba1bf47448b26f5e090e58
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:59:57 +0000

    OKorokay bright shall hollow discuss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
