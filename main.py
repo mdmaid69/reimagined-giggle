d47e3480090258ce211681f0857b64d82770ee70
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:46:50 +0000

    well characteristic person object land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
