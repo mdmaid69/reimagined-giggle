0846f34c8ae4da453793c6b667b2f839707f422f
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:01:32 +0000

    along authority mend stare breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def convert_to_octal(n):
+        return oct(n)
