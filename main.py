79652e332f11551cea832a2093c61ba5cbdd4e66
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:18:52 +0000

    place sell pleased mistake hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
