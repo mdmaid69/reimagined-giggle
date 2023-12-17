72fcffc1f0421b9975d4c88d0bf51a8d9c49095b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:29:50 +0000

    list mistake second possibility inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def find_max(lst):
-        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
