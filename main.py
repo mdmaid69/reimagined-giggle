f5178f43e20443d2268a2c0f71436467cf1ffff5
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:15:33 +0000

    bringback abroad dear plenty negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
