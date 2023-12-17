010533c925798eb8fedd0376a5789edcf386a395
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:38:02 +0000

    introduce later listen throw kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def cube_number(x):
+        return x**3
