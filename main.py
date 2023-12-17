fe09b877988842f0ffe510b13f97952f9d5365d2
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:56:45 +0000

    dark beyond lost nigga mark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
