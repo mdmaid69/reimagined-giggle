ac7098d48110c745eb06d01086c50a47178305b7
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:21:27 +0000

    draw lie wander wing what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
