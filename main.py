f7cde617cb7013cca9fb842af92dddad3d7b3cbf
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:31:10 +0000

    sensitive body Pp shopping bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+import tensorflow as tf
+print(tf.__version__)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
