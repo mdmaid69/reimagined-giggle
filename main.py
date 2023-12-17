f7cff3e179acee203964658f9de758e89fa55ce8
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:09:24 +0000

    quantity dry suit read in

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
