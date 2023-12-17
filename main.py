b03e0ad19f4e6aa8d2d1f5ec75967d6a71fe8399
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:54:06 +0000

    school project crowded level pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
