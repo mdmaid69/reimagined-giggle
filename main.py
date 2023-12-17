dacb0e0d20f3dbd3c60f6768ce3f5a6e6433e880
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:13:04 +0000

    matter concerned lost suckme ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
