eb3a5699fea32571c95bfe7f6a03b1a32c568b56
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:40:23 +0000

    recommend close position interested recover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
