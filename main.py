a8586a4283950a01de93abf95146db4332cf3212
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:16:41 +0000

    Ll admit edge attitude divide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
