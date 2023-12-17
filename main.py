c96214b9b885768adde4e024e74420f20826bbe2
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:28:37 +0000

    clean greeting dry identify aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
