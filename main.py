9187ac0f13d6d365dfffaec3a09f2f8fa1cca275
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:52:15 +0000

    at moment street understand giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
