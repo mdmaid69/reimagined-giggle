3b53f7073f7a8dde70eaeff5fba0ba62fcc14d9e
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:30:42 +0000

    illness favour design settle meaning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
