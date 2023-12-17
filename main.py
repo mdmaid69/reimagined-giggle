78d5251a4a336c77ea5e9305aef467b0d83f551f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:47:45 +0000

    thinkof real spin pick excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
