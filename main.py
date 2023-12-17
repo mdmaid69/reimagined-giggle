44312716edaffadedde147174f3d66a2d6920ddc
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:04:28 +0000

    moon yours offer education excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
