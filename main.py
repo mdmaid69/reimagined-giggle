050b6cdc037ca3082bb36db5e7ca1aaae41f0e52
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:10:52 +0000

    persuade 571enough probably wave gooff

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
