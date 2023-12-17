8e0a776e4baeb7b86a2426cb84a88c86dc6a25cb
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:30:26 +0000

    neck front increase invitation stick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
