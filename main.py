38353c16305dba3b30e0ef8d55116e7a35c3b8e5
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:30:56 +0000

    basis ill cook communication basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
