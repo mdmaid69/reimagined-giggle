3c04f01420ae09f4130aa22bc208046cc48b2a9b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:52:27 +0000

    organization stamp earn feel float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
