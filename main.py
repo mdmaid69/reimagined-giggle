4f76ebe1be0f7667ec68c6ee04b3bb0bf4005472
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:22 +0000

    really comeup welcome dress face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
