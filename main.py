09a461f2463790eb9c5341f09c10db638205c315
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:12:12 +0000

    further distance eager Oo Cc

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
