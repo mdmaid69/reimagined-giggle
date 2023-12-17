41be674a5eeec9cc8fdb35c6c9c7880464ca8d49
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:43:35 +0000

    born sting please sticky tall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
