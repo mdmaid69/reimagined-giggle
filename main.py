28c62485d9393be138030f52cd2b342e997a4ad9
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:59:27 +0000

    close very cinema scrape top

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
