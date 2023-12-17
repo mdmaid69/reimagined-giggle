274611490fa661341c166ce9ca2470b390203a80
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:44:20 +0000

    fly habit evening burst complicated

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
