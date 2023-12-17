367ab943cf6d73a16240e05556e92161d4557d1c
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:45:53 +0000

    565end scream seem centre create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
