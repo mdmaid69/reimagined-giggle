125c3699190a8448ae0cfed0bf4b23afe2bcefd2
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:22:37 +0000

    fortunate mistake Oo abroad later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
