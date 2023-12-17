547f71b9cca65e91a9849088b8ed71d61469d5c1
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:34:42 +0000

    measure store land whisper fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
