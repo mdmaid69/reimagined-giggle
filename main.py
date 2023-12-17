3fe59b7384bb62485d04827662dcfd35ee6c104a
mdmaid69 <robot@alexsyw.me>
2023-12-17 20:20:08 +0000

    evidence size dress trousers confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
