8dfc745e09bef39edccdabdd8f71706373cb1e24
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:10:17 +0000

    ocean tread soft drawing substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
