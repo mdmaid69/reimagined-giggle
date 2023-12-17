3327fa6cde0e536a50beaf2749359d15c9f4338e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:41:49 +0000

    under suggest wheel goodbye on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
