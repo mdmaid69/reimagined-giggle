b29ddd8c51e8fff3400a278a477f9be9bbbc2a5d
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:04:21 +0000

    new complicated able visit backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
