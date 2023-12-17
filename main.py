1310470420fc6f14f3eb7a81314a764eb8fcade6
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:29:12 +0000

    nerve turndown imagine exaggerate concentrate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import datetime
+print(datetime.datetime.now())
