40cb1fc25edf2550aa37956c79e2732df7196d4f
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:32:54 +0000

    figure action table intend lend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
