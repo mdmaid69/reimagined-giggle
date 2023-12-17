be808ee2adeca179a02db1f049aef5471f8b221d
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:12:33 +0000

    either legal arm against letter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import sys
-  def get_python_version():
-        return sys.version
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
