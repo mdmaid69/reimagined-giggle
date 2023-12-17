b532fb5e6b7b3400005dff8e5058908e54104d6b
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:55:07 +0000

    breakout science second realize immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
