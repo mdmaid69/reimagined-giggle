2625cd09f31f104782183b0c5c2415b708c3d3f0
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:03:55 +0000

    exist welcome transport callat greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import time
+def get_current_time():
+        return time.ctime()
