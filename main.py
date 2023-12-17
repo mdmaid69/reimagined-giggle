51205bc191d1392fca6efa3d373fe882ac3e84f1
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:26:11 +0000

    regret group careful container hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
