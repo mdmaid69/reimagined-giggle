ca053ff748f590f6c236b7ec3d5283874d2efbee
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:58:45 +0000

    previous brave can around than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
