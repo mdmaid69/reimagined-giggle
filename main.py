d9047cf48d3af4c578e1babce0d66eda09c4c2c9
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:49:59 +0000

    have Uu available skirt later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
