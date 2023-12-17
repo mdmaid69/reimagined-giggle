6ad49ae5c84256858f874dd0c48eb672b72845c0
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:03:10 +0000

    apart put keepaway branch fasten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
