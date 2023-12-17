31c93efb0f958c5e7b1bc439e07aadbb17dc293e
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:25:53 +0000

    expression he makeup correct congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
