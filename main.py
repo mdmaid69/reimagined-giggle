8209891fe412c2661c53ada9ab1e9e26b052da32
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:39:54 +0000

    behind divide bush carry that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
