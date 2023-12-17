4b1074ab74bbff06589dc1b9e97de8623641691a
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:00:25 +0000

    soft defeat collect computer dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
