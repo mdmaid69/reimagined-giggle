7410243100721fd2f165342b9ba766a2af096cf4
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:38:17 +0000

    bush foreign appeal exist not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
