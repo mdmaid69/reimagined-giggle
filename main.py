f274903c30263c88903eeee821a5469a9badcce0
mdmaid69 <robot@alexsyw.me>
2023-12-17 19:32:54 +0000

    picture goback(to) warning battery deep

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
