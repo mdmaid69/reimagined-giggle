a685c63d3f7258a61af94929532fef47243ea5e7
mdmaid69 <robot@alexsyw.me>
2023-12-17 18:06:20 +0000

    equal comfortable race but interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
