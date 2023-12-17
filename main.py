6019d07c73f84023531b6f63ab13823deb16761b
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:47:22 +0000

    intend necessary later comedown since

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
