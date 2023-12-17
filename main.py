cead587cb712fa619fce4936e40446f52ec4a7fe
mdmaid69 <robot@alexsyw.me>
2023-12-17 23:37:37 +0000

    official draw muscle authority rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
