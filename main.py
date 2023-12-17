fff152b19d41c5b5be5e72189abd2b511524eb10
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:50:40 +0000

    anything style them government middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
