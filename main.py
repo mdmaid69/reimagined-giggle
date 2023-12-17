2de5d86bfe7bcc66615ce336b0a0d6c9a9eacc73
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:35:19 +0000

    term damage black hurry judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import math
 def calculate_cube_root(x):
         return math.pow(x, 1/3)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
