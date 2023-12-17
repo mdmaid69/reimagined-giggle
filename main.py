9c1961afe68a059e4659ba1e2aa4f1c7988d0f98
mdmaid69 <robot@alexsyw.me>
2023-12-17 17:37:17 +0000

    build relative match complaint lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
