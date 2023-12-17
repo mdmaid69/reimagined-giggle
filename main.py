5faa1383623603f7ef7bf7e62c88d8c4ea955e54
mdmaid69 <robot@alexsyw.me>
2023-12-17 21:09:20 +0000

    quarter he building pity home

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
