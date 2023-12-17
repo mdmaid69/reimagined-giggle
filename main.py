76fa067a05462d48c82e5be839927cdac3b44ee6
mdmaid69 <robot@alexsyw.me>
2023-12-17 22:59:37 +0000

    bye effort over instrument train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
