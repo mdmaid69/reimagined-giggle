43a312c02bcd594399550df25747bdc4b0cdef28
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:02 +0000

    bringback herself improve organization path

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
