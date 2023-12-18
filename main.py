c10fd79725fb336459c92b8695fcd709a662d767
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:07:22 +0000

    deaf brush offer settle takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
