ecb272220fb5086c88ce3329ba2e6acf838b7b54
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:36 +0000

    computer cruel transparent mad injury

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(10): print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
