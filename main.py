0d53b9695dd545e0a5975397541edddb8bf29536
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:13:35 +0000

    typical money convenient wake squeeze

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
