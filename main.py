20f25c28eb11d1d6f75cb658a42735395ca6d25e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:00 +0000

    pin average impression book wrong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
