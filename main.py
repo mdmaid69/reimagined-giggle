ecb0eb37bfff691190fc506f18903961b2cdb008
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:01:05 +0000

    beautiful port of her fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def is_palindrome(s):
+        return s == s[::-1]
