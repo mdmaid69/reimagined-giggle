4e584d041b7ec41bb8d4b7127c0b447e136eb7cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:38 +0000

    than weather surprise big fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
