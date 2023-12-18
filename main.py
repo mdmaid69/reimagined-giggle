747e7d171f10c6ff9348c33d1ec7728fb56c09e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:10 +0000

    stick size loss present this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def square_number(x):
-        return x**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
