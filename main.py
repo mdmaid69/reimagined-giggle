7d2bff817d2b282f58893bfeb20f135ee426c939
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:26 +0000

    telephone exact cap variety excited

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
