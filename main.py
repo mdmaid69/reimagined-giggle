e970bd12aa069a994372220019315d015d5f40c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:17 +0000

    practice takeback later mark claim

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-print(os.getcwd())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
