3d4be8b111bdef3e070ac1c1a631b485064d226d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:16:51 +0000

    complain stop unite what tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def clear_array(array):
+        array *= 0
+import collections
+def create_queue():
+        return collections.deque()
