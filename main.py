f13abac85da56a275440f30ee3593edccb31a609
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:24 +0000

    sick just top favour lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def add_numbers(x, y):
-        return x + y
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
