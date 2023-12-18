266c0cb2f095c2c931e0ce2276cb33d26e49686d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:43 +0000

    country gap programme lost only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
