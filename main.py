89e71d08f76d3efebd68ec5bb1aa42fab119efcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:08:24 +0000

    throughout callup arrival consult 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
