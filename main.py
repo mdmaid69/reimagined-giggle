b494be7ad3b04e5d5ccaf18b8685a1e6d582f25d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:17 +0000

    sing other Rr sharp regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
