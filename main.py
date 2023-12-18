d63f3fd8e8e6bcaf93a0c519bfc3c616fb3b745e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:08 +0000

    appear attach will tired area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
