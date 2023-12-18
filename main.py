1d6b3c262171b7dbcd5de0cf541ba69be55a78b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:21 +0000

    reply meal yourself tough label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
