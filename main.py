0f112e56fb9d5364968503a2745938fe847c2e02
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:26 +0000

    along classroom fall where set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
