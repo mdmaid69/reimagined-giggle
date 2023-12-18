204af47d755a5d8a756ef23ed5dfe95ce7a45e23
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:42 +0000

    remove different list job mouth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
