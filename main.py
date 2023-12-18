c39fc6ab61cf58339911a2d0c66a6424831bc3d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:12 +0000

    slightly fail group myself clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
