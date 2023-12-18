f8f26e1de31aa3cfacc0adb34a1aed27349524ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:32 +0000

    especially speech fall letter stamp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_min(lst):
+        return min(lst)
