586f5aabf543e7df03396fc70f9197e44b6921b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:39 +0000

    trust laugh forget object mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
