b0478502c50f7aa1a6cd77967224425a2b06927e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:28 +0000

    study temper opposite married line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
