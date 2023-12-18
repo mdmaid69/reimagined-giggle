939fc6d9323a30303b2cdb5728ce2306f271a615
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:32 +0000

    play above bringback giveup apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
