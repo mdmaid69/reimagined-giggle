3a6d4e1bba6f6b2d68e01574f618c4167242cae8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:54 +0000

    free advice container comedown goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
