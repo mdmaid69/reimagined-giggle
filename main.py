32a918d60a77008a0346f13727a07a8c2597f887
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:37 +0000

    564encourage remove thirsty fortunate main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_pressure(force, area):
+        return force / area
