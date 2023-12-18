d9ca7ab05963b72987bbcd24867f9ddf51934ad1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:08 +0000

    vanish voice gate over hard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def square_number(x):
+        return x**2
