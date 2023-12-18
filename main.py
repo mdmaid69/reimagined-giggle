1e53bd42b995ba75732eb796b71647d498d8cfee
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:52:05 +0000

    lead pupil deep between across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+print(os.getcwd())
