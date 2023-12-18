ea5f12b97dfe69cd99528013ef0a4128cbd8e9ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:25 +0000

    572enter or boring broad close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sys
+  def get_python_version():
+        return sys.version
