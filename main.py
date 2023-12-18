8b4c74d1451b2783671cc027fdf35d2c7e07d81e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:15:49 +0000

    ear transport dull needle regret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
