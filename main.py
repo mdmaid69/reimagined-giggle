5021e4d5cd2695864f7056b7582844d5bdeda7e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:13 +0000

    dead love connection comeoff race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_float(array):
+        return float(array[0])
