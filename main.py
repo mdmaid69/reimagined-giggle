205cc3ba6019f4767d82d9b450b4079ad0456c85
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:41 +0000

    think doubt wind forever risk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+name = "Python"
+print("Hello,", name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
