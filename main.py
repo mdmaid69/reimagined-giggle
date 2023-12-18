6468be5cd103809d50f75d8f8ea59a38337ae799
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:25 +0000

    watch attach laugh school knock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
