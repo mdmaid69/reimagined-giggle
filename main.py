25ff6d8854197b7992412eb38660c968500204e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:23 +0000

    surface little prize top snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
