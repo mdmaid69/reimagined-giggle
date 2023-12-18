281e366d224c2823e99e4387135d69d9899f9fb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:40:20 +0000

    warning after entry beneath straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
