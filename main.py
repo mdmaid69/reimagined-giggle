e90b53f9bffac6a8e9698d6ba4c0fb8125a1d8a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:56 +0000

    Pp politics advanced either remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def print_python_version():
+        print(sys.version)
