1b262fa927503a50b7cb3c7091152732cbcb8b44
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:01 +0000

    question sort generous squash inquire

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
