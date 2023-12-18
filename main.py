94aa18562d394143452c27b4f2784b0f1701ed9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:42 +0000

    scrap dark godown system oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
