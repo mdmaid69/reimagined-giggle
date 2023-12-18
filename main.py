709af8875bc8b01a29b473d56a824ae8180c72a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:49:57 +0000

    loose town from off Ii

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
