4f0c7680d53ce2e3e9cd8463de41bf7f83b8a376
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:55 +0000

    tradition here avoid suckme warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
