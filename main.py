74ab00ed8222e790f9db07e22c1d4f983dc78999
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:01 +0000

    team mend attempt appearance runout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
