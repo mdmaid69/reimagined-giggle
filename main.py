589f3d262c6bfb3d3e8d2cbeb17ed15852a2f888
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:05 +0000

    responsible exit human confusing escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
