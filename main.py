6b9f81e24554378fc1f0ccb2f792ef375080f242
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:19 +0000

    bottom private crowded well spot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
