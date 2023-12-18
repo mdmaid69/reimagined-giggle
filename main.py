18f12b94c7e44826d1f9f67e0ac42ab576c4bc6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:03 +0000

    same pause more certainly giveup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
