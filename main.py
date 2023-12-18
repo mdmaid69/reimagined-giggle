081980b5d6166dad69eaaf85ff4ea27dc75fac66
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:13 +0000

    passage shallow arrival single thing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
