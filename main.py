b9f24c342432b061559be1848ae58c7dcbc4df47
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:54 +0000

    married progress unpleasant pot old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
