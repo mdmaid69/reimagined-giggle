bcf0446001b35d5035b38e826234b98ccb374726
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:51 +0000

    single warning pronounce count politics

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
