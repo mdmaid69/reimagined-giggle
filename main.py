47b8c3713c4b9182720b803121d2bf0302976939
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:44 +0000

    star treatment file joke public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
