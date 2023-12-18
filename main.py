1f912b58ebbc3c09b871b3a3516618adfc61681a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:36 +0000

    before owe recent familiar demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
