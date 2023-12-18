1bd9091cc58a67485c360fd42bbd196936f5ec96
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:39 +0000

    back sell agreement long rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def sort_list(lst):
-        return sorted(lst)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
