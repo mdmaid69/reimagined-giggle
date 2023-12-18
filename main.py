1a7e31596290119d21fa325d57cf6042a3b19a1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:17 +0000

    mean clothes dare tear doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_user_list():
+        return collections.UserList()
