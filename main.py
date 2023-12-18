f9bd142e03efca974c74a612ae08d3c8b89268f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:17 +0000

    decide belong stranger from state

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
