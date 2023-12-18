f34a100c28cdef2b4fa9ca8b08f6e84c70921af8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:43 +0000

    happy report increase congratulations possession

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
