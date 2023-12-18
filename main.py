dbded7bd6adf7f2ae42bde9390121f5b2412d1a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:46 +0000

    plant care laugh runout everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def sort_list(lst):
-        return sorted(lst)
+def get_array_itemsize(array):
+        return array.itemsize
