630928a7c6070699f73a29e152ddfb984c72b52b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:32 +0000

    such face refer next impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
