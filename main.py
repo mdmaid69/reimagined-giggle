3f40fffc5a27cc0e1ede13be9fae88867befa5df
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:06 +0000

    hate promise defend air whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
