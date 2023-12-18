af52580b955fe3cb6f2d16876d7705345acc8bd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:13 +0000

    runover uncertain quality ready practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
