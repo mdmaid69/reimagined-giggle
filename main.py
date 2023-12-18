fb00c4a7691420f6f7770315742008a28d890521
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:01 +0000

    problem another straight contact funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
