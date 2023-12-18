58b1bf4c26ef668023edbd00a79c90f01ac9778b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:51:26 +0000

    suggestion set charge unpleasant close

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def convert_to_binary(n):
-        return bin(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
