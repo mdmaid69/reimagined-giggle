1e79cbb179b0c16a13311663c58bbe964fccc402
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:14:44 +0000

    shut noisy cheek fashion comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
