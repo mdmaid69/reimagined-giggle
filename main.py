1484c1a3b224f3c35313d5ba88304bae306c7dc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:31 +0000

    recovery object crowded path disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
