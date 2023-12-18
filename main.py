fccaad6f3547ac8a08004b51b2e7bcdde041523d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:45 +0000

    manage opponent holdon what keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+print([x**2 for x in range(10)])
