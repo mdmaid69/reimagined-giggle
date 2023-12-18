fda25c1b3841de9bf659e59ac73f82e9dc626df5
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:02 +0000

    how workout dull general boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
