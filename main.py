cb202ea3ab9c3d53e46ab3e76ee3684bec8eb576
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:35 +0000

    block sink this store thought

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
