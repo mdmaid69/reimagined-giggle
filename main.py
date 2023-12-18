d406045921ed721a4604445db0b861caf7ae9465
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:33 +0000

    cause someone free Pp have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
