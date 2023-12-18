611bd99f009216f7bce19b5ea71c4ee52057ee80
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:36 +0000

    wire squash inform against aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
