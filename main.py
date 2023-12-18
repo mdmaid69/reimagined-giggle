7db0fc9630144eb02f1641f8cb01cce6ca01422a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:01:05 +0000

    empty positive gather nor address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
