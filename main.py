18ef9b059010bb75c177d812bfa888d14b641771
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:34 +0000

    fire deliberate favour extraordinary closely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
