1911f83645bc2013d786df5673726a757a3baf80
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:40 +0000

    lonely organization uniform ordinary benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
