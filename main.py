ad1e22033ad040f9a354e16ce3c7c8ef28762ab0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:52 +0000

    fortunate leave himself inform stage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_hex(n):
+        return hex(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
