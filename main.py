dc2edfbe558da2108664b300a817ca014a8ed790
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:47 +0000

    later part harm whisper unusual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
