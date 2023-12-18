d20878f2bab10a813db0c29895c72ffb25ab6aea
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:20:43 +0000

    union alone inside cheat acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def clear_array(array):
+        array *= 0
