cd31677560e213e9458dc6d8dce70659ef995d4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:26 +0000

    environment model steady send Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
