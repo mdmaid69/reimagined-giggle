7b7e8e5c2b9887887a3fdb289e870cb15885696d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:42 +0000

    music suddenly walk might already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+def exit_program():
+        sys.exit()
