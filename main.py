2ed70d8cd4e4b8707a2c522bc6e4c6fb83e97296
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:42 +0000

    admire rob majority change discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
