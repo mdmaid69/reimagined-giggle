b7a6927db39c918bf5c236951cc0ed27e4591556
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:17 +0000

    exit love dream deep considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
