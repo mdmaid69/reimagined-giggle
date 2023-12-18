402e723c44ff4d84367e2cde1c685f345c9a92b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:39 +0000

    market course news involve string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
