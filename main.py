67025d978bcaeea6d516d5fa89c301e8ff72b8eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:50 +0000

    breakinto exam operation rare jump

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
