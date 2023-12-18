ee1e07291e06bb5b6849e7dc98ce99bb510a2008
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:49 +0000

    left control age theirs write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
