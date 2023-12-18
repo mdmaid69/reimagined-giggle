c096fbca8f8a197db0012b36e099ef02faefba82
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:03 +0000

    ticket attempt disappear callup unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
