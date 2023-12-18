ed795d83bf0bd85f4abc9574ffe0e6af5542e4f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:57 +0000

    pattern balance loud tongue experience

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
