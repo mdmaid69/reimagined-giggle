e5a9fad74343b5a75a124602037a4808ee4271d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:54 +0000

    general goout keepout thirsty town

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
