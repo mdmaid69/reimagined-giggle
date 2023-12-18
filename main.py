f5d6b7791e87b110282cfe98408b976af87a0d1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:34:56 +0000

    responsible bye Ss practical sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_bool(array):
+        return bool(array)
