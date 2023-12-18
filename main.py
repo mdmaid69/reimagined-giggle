6745c3e08c30bc6518dc35a9a1b3d5b271f458ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:42 +0000

    introduce tell ask strike ill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_power(work, time):
-        return work / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
