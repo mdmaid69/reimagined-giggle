3690c2671a4731366402c6a36747b78183958125
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:45 +0000

    contact message precise or boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
