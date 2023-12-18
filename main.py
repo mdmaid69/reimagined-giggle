a6e0d336a29ffe6b527afc62d6b458394b3f3898
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:29 +0000

    listen careful action clean genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
