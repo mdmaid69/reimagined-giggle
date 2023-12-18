8fd6d01120712d04e34b187753fe1fbfe4f9e090
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:59 +0000

    piece sea twist similar low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
