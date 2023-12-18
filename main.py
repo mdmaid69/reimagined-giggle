c6d15e33e5d42dfe3544a8738b479413e68ddaf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:40 +0000

    common use notice group mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
+print("Hello, world!")
