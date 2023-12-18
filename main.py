048c4479d0860d629c7d6984ffa1e722b2816f34
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:19 +0000

    breakin Mm rule rest colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
