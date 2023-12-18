12d45e9f04545b54d9ab6103762e2c2e45334551
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:53 +0000

    treat reject press kill I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
