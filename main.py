337c48b77e70e500cd9e66fe2c5a4a4f336ce82f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:44:26 +0000

    bringdown stop glass best important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
