4d9e9c2a5f7f4149c3cb85a509196acce971588c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:28 +0000

    learn concerning knock professional whichever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_binary(n):
+        return bin(n)
