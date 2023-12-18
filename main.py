1da2a43bd3180309214007e18ba8abfa668f5e4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:53 +0000

    smile list definite impressed seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
