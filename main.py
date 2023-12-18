7908e0c66697b6b90615426cd4e2bcff5bb38b03
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:45:23 +0000

    whistle easy storm minimum degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
