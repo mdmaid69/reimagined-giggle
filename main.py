e71a84bbee9e97184f491e882a3f5ba406395a25
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:09:21 +0000

    form runaway greeting mixture declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
