5345f836937e7247c9c27c22c6264fa1f7d5c432
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:40 +0000

    thanks partner natural provide degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_contains_item(array, item):
+        return item in array
