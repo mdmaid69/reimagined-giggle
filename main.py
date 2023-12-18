4a2a422f66a1fe879cbea7c331fdcdccaa43ffb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:19 +0000

    dance spite Nn out fortunately

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 def calculate_pressure(force, area):
         return force / area
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
