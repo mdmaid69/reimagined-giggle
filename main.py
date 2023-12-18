ae74a39d6529e60342a302075d0e906b75d40320
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:25 +0000

    comein run right type hospital

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
