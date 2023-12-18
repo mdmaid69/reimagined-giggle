a985fc1f5f1de1e4c353a397722ef00763092b1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:52 +0000

    disturbing collect realize low party

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
