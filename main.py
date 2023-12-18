7366806582a13ac2582b17201d2b8b567ae347c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:29 +0000

    leg offer dark grade positive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_power(work, time):
+        return work / time
