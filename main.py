7d6f8b10e6b7eb82a98781adc069a29157dd0301
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:08 +0000

    nature safety proper quickly plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
