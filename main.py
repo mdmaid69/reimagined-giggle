15a77b659718c5ac43e3291c26ea2c2a2a21c637
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:28 +0000

    inform notice love never realize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
 import array
-def get_array_length(array):
-        return len(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
