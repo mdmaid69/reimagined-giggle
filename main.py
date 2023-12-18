e7713d4698af20ec24a3784660ad846729c234c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:53 +0000

    extra sit official emphasis through

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
