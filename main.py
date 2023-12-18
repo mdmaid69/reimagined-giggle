a9154d114b397c3aac92a455d0627e70aaec606d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:10 +0000

    home zero twist runaway home

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def extend_array(array, iterable):
+        array.extend(iterable)
