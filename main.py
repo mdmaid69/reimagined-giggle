258b1ef609b0162f129da70acffbb2fc637f8772
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:38 +0000

    next impressed pick computer boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def subtract_numbers(x, y):
-        return x - y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
