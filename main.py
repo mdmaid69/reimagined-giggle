f5b08d1f82ba4268d23f0eecab0abc8b1f7b0905
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:24:14 +0000

    persuade zero turnoff stand consider

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
