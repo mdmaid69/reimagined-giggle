1eb46d170b77a61e66f227b5a88b57bddd9c9a93
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:07:31 +0000

    change expression lay pleasure artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
