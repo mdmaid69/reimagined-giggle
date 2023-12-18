92f19ad30843211b820e77dcb5387045d15bd23b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:50 +0000

    speak system ear from row

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
