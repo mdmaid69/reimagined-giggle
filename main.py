c4f9e03a0270249375f7758895750ca9a0708b64
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:14:19 +0000

    only yourself news save speak

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
