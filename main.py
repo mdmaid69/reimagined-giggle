5c6f011128d5311aaa364035d92fbe651249ddea
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:48 +0000

    like if expensive water age

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
