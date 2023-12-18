4fa02ebf25905b2f711173fcb1a2616bb43c5ab1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:13 +0000

    shade polite completely 572enter part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_bool(array):
+        return bool(array)
