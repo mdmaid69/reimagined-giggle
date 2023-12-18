76eaebd00d05fcb4ca6bbceec7bcad00e3cc0fd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:51:08 +0000

    hello size action comeround any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
