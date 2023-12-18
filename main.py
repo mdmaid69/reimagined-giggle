1b4438ea4201149b2f998bbe7663db95c626c526
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:13 +0000

    swing precise finger force evidence

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
