8de6d382daef06da85b75ab841845407af5b8de5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:55 +0000

    copy free future now mark

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
