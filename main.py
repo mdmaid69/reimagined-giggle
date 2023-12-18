3edb92b4166c142107509f563827493a9ef3b748
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:38 +0000

    crowd lift relief report nurse

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_area_rectangle(l, w):
+        return l * w
