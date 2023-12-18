ca732f58b47f989e4697773c5c6c830dd1de50b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:46 +0000

    favourite left best choose clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
