fb0687e93d9b6ada79f36403cff58e144773f4bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:36 +0000

    fear brief accordingto convincing room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def convert_to_octal(n):
+        return oct(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
