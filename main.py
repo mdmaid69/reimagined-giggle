abf2dc671f7b27d49a70d329217974561d2875ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:35 +0000

    flood turnoff scare thank theirs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def square_number(x):
-        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
