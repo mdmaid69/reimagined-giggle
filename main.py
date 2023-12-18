c71b9128914d23fddd2c57f0c88aae54f51f4742
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:28 +0000

    in touch bite occur characteristic

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_palindrome(s):
+        return s == s[::-1]
+print("Hello, world!")
