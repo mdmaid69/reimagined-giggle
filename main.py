5dc88cd62ec87c8617915f66d7dcf7e0fd33c39e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:26 +0000

    down accommodation smoke excuse wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
