1d76cc296e3177a5d2d460bf48e4ded94f2b96fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:49 +0000

    hurt little Dd between spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
