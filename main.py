f481e9c9c3ae280d87602d31ed17fdd94519bac3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:56:28 +0000

    Oo example sensitive anyone add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
