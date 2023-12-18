62b71dd00f20e94028ab8e18edab828faffd1ebe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:18 +0000

    claim collection modern deal recently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
