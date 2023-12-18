cef366974c86d74321f0b215efedc706284ac4c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:40 +0000

    work pretty roof relief peaceful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def convert_to_hex(n):
+        return hex(n)
+def convert_to_octal(n):
+        return oct(n)
