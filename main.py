579493fcf89b4def8405d5225edd1dfdb8a4f3eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:25:36 +0000

    fashion emphasize exaggerate sorry brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def append_to_array(array, item):
+        array.append(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
