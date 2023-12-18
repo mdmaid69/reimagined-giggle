a70d7fcdae5fbe67ba71d46611efd8418be81674
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:42 +0000

    joke little low reliable oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
