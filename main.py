eedee8027a746b7b6e380684f2ac70d00f9afbc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:05 +0000

    disgusting immediate second vanish future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
