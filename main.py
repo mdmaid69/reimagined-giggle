5fe834d8ec543975bdbd9fff3b13f86515db3b88
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:39 +0000

    absolutely open Gg cough trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
