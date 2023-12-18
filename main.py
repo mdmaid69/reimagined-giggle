1d64b113d73e09f9f2aa503812bacf5fd5dbc89a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:53:49 +0000

    Rr provide schedule offer interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
