5a975171cc858de62e57b1421d6c8bae85f3e780
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:39 +0000

    content dare pour reduce purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
