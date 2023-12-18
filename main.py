f98b01d15915672e86353dec5de57483e61b95be
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:46 +0000

    put unlucky shoot where case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
