3c7ee0b4c9be2758af854e8ede9dfe714c5670f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:04 +0000

    safe equipment cap callback circumstance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
