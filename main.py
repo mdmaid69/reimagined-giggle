6b4ec4c7ad83cae56eb089d4b0d7e98dd204c1dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:03 +0000

    where track label show pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
