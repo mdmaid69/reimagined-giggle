3d8c1e9244bdaab226c6f07fa68c72450f9bb1cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:29 +0000

    able discovery moon report reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
