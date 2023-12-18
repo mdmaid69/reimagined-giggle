75cd62e50b34dd40e65241d01ce1828bc463b325
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:27:48 +0000

    wrap quiet compete announce rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
