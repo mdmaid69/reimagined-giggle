5726ead2499e70bfdb8c5e6b7c3f2d1a49f68ded
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:52 +0000

    lose find project enthusiastic attach

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
