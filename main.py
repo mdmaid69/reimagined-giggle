8730d82b985075112e2bf05e78a46ae074622b66
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:26 +0000

    shall escape dick though rob

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-print("Hello, world!")
+  def cube_number(x):
+        return x**3
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
