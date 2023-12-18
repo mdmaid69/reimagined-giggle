9fa586d47e55c23c195fad2e525574ec7409b86e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:10 +0000

    law trial joke see comeout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
