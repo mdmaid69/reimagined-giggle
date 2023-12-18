5bdc05d19ecd4cb6cef88e594a4ed0873866eb0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:48 +0000

    friendly week disgusting again freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
