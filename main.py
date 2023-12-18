8fac02e1ff5e1cf94c85383469dbda73403e4af4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:28 +0000

    paper round promise failure may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
