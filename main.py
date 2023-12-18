f54b02a3f5fc03d2b28fda095d5d90004ac714c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:09 +0000

    sign pair interested break trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
