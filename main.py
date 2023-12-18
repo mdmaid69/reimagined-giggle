8903b252fd53a402e5ba4c3691185e04be3682d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:29 +0000

    which claim disturbing our common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
