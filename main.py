cd5c2dd5b19cdeeb77d8452855fea7204be8854f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:25 +0000

    bet dance ignore profit permission

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def sort_numbers(numbers):
+        return sorted(numbers)
