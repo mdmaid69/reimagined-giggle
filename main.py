222812d1afe7cb10df5595ed1863f4ea8052cd1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:40 +0000

    either split reason appeal calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_min(numbers):
-        return min(numbers)
+def find_max(numbers):
+        return max(numbers)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
