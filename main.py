37cf235dfeeae90c044b7486f8e9d535e57411e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:50:14 +0000

    like it expect in slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
