9bbfd23f94890df7c12302182e50053a2e17d016
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:06:31 +0000

    separate off family hope sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
