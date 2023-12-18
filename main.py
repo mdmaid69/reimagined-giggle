ce5ead77bb9264875bf1c98b3f6d31b2c432d814
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:10:47 +0000

    fetch sister bar nigga by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
