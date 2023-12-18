8c168ff691e965211e394f28b7eb1f8333699724
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:26:31 +0000

    equipment wrap whenever sink dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
