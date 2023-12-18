3da49ec2fc0ccb5dcf4132205e9a048b1931b151
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:10:35 +0000

    thankyou pot depend sleep sell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
