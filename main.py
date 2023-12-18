b1258c1899eaae5050c93333e53600d47c1881fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:12 +0000

    absolutely mind real touch hair

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
