6a690371dd5e19e9b40f15910aac3cc47e54f72f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:08:58 +0000

    divide sea the fact accept

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+def is_odd(n):
+        return n % 2 != 0
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
