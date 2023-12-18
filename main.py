8fca688a85c2cf4abcd62c19d00f4d95acbd31c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:16 +0000

    giveback comment workupto wipe lucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sys
+def exit_program():
+        sys.exit()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
