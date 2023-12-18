da5dec30842af38239f718286c43a73599fbf122
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:35 +0000

    over idea surface surprising brown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
