7874276c969eaf37b2c53dc3f308815df26f38e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:22 +0000

    translate finger natural pressure friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
