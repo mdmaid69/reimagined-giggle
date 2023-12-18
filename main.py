6a3f0e98cbd6eb1d24070dad26e68e6cdae18d37
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:03 +0000

    film runinto surprise sorry storm

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
