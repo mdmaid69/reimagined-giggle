874ea3f72124291a525ff9d57b304da815490041
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:42 +0000

    intelligent while hope change once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
