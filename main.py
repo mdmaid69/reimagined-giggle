b6a5ab57bfc47c9c27815138aa8c5e79c70b52ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:29 +0000

    convenient boat top hospital collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_bytes(array):
+        return bytes(array)
