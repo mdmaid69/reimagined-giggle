027b360fb57ebc585e0d497e604702f9a9436ca0
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:33 +0000

    whistle concern attitude sweep otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
