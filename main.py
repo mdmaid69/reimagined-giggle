949fd46ce1bcda453c521f06b214eac7a30644f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:28:57 +0000

    talk distant increase teach contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
