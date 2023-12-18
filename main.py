267ea8839c13b26f296eb5de0ef37a5c90ec453b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:54 +0000

    occasion recommend workon satisfactory print

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
