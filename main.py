a5c178fe0b298ddf7ddc4f2081c05a9d27e73895
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:40 +0000

    do beautiful 569enjoy pause habit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
