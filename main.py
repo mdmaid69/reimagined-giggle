4da9c668e4f2cc3f724015f854ec3df167ef6925
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:13 +0000

    sad calmly range flat judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
