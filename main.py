ccccf7618e4d4ec996e0e1b3429595d636e0ff93
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:46 +0000

    food join year several workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
