b3de232c9e4b33d6b08cf747d831fa44d5729da8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:05 +0000

    find study far order come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
