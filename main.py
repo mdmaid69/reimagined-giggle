0e458783ae4f085df0fa5811128a1af2f3866b72
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:35 +0000

    blow play sell feed stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
