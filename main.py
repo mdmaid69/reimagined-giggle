aa03ff58f48b7b88057f09f3ecbf09095078a692
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:57 +0000

    straight who autumn sweet takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
