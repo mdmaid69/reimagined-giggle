46abdbb22e02b0402e57d6ba08741748683f3db1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:48 +0000

    gas judge regular formal black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
