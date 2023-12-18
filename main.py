9e274e8a9278bf80f8fbf19baa66e76f9ba41b23
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:28 +0000

    choice thinkthrough to develop used

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
