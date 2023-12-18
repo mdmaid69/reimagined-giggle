17935e43906f209aaa45df338c23af3b5d6e3ce3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:02 +0000

    excuse shut switch read switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
