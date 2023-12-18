be0670f736cfc5207ea21f2c89108d93dfce1493
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:14:05 +0000

    consult land wander damage contain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_acceleration(speed, time):
+        return speed / time
