8b4f55701c93ad341c926c574d948d1ebe0a1ec3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:17 +0000

    check ocean best present skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
