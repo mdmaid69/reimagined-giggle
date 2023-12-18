ad1731e47f8da1d74379ad6ad8782930edf4db72
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:57 +0000

    consist ready repeat afraid we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
