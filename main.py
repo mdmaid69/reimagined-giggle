011138c19d928682fb049a62a7c6a1b864f55b8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:07 +0000

    car war general hang its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
