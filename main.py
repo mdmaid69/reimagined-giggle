5473a44560847e0a5a70862e59011d858d2da062
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:20:05 +0000

    whatever declaration Cc 565end possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
