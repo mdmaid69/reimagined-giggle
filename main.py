6b956fd825c0fbd6319f69913afe991c117bbc23
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:17 +0000

    keepaway yet theirs pronounce sometimes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
