d659d066ea2c47c5c16f3c90def62436a7f19087
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:24:02 +0000

    hour dream uponon sound care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
