9b785657b18f8bca690dafd4eda50a61437b36f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:16 +0000

    part ability lookafter myself there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
