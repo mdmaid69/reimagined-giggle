be759d4eee91c5349675064c8d3371d2d1012ac4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:09 +0000

    around curious recognize someone sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
