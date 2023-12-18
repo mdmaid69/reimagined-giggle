9ff1ab2b2b716a7d9714abf7761a5aff1ab2db19
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:24 +0000

    tread excellent lookaround pot education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
