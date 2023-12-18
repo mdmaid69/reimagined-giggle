28315965fc791bb83f95ba84f7e6197489fc82a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:11:51 +0000

    ball truth brain near gate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
