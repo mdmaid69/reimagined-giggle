e38a080799aa8858fe8cd72ae7cd07a6553e7a96
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:50:26 +0000

    566end while inform inside live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
