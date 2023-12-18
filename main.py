7b4d10497189ec500a907e01b49274be884675a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:21:32 +0000

    law underneath instrument trap result

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
