4ae762edff8e71229c34fc0f4ed41702f934d947
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:04 +0000

    flow break stop calloff trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
