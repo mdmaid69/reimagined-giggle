2f9121192d6b2228213920b0aa58a7f76d1077d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:16 +0000

    store anything art shame 572enter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
