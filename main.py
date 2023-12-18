ee5aee0748e9cd50b362907558a6c79503f74a18
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:50 +0000

    female address so cheap cause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
