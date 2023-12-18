3ea41c4e4f365aad76181e8c455f2295527c3e26
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:43 +0000

    breakdown confuse pinch the funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
