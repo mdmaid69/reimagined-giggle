679dd67d33cadc70c473e12f46826d62a2034807
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:06 +0000

    take steer tap boring fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
