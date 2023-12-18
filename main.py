fee07098de958247dbe93124263b9b6ff38d29e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:46 +0000

    food direction ring eat exact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
