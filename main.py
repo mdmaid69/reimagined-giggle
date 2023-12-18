59efe240c140acdc2e3963f7a70cf33f25eb89f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:16 +0000

    mess build maybe have set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
