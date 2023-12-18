245d62f652ee27d07eae9df6866a5694d53a9786
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:14 +0000

    friendly out cut fork attach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
