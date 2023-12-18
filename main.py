8907c6f02348ce2e193607cdfb67cb1743ea1f75
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:42 +0000

    tidy smart circle smile inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
