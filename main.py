22aa13c783db91e1567d9c9b0e3b1ee3cb619bb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:06 +0000

    willing promise approval crazy wide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
