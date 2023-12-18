161dc86b4a1b9cc5dc8ef94d74c203d2e9c97c49
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:35 +0000

    turn flood bang beat disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
