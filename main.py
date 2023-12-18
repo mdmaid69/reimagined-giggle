cdbc925f42bb115d84777e32d9275ec53f77821d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:31 +0000

    comefrom uniform Cc law coat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_user_list():
+        return collections.UserList()
