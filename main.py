c99dfc0cf89b1140cbcf8839f38fd7f1399f4f6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:23 +0000

    swallow guess turnback snow uponon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
