53e00a16d1c054bddec481c057684209afdf7c81
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:05 +0000

    custom day fast early pretty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
