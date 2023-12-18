965842edcf0b7d80c24af0a4b59a48d1679f0e78
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:02 +0000

    tour announce train whenever surface

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import random
-print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
