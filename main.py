97d8557b5469b230f216ce858dc530fc8742b4eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:53:54 +0000

    certainly influence only guard perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def convert_to_binary(n):
+        return bin(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
