36432e4b55b63f566b91551e95b5e6a08b74a135
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:47 +0000

    cover declaration turnon fix exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
 def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
