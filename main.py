3ea567042365f09836cb2f67ac1ed173b3ff9eca
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:15 +0000

    talkback contact nor lot geton

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
