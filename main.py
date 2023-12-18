27e3948e281184ddb3d9b6d85437dfc3a076cc09
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:40 +0000

    whichever case direct minute case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
