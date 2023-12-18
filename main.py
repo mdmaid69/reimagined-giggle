3c700ee64e52f9b5f246665a041a55cf8f2e2b14
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:44:01 +0000

    classroom aware unite division average

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def cube_number(x):
+        return x**3
