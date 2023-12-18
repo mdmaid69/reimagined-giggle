33cc344c37a6be4bb6a573851d38d3ced0ba8f98
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:53 +0000

    usual never professional party detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
