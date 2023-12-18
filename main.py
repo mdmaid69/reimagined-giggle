6cfd2bd73eef0aad571019b75ee65899611e04d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:44 +0000

    tall calm race to yet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def square_number(x):
+        return x**2
