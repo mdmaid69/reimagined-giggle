ba0e7992bfd11974ee1c9e7a9842b205a67bf473
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:13 +0000

    introduction combine confusing ready closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
