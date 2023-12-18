16b9ec7e3115232de2b41e398e3db7fbcc0b70a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:33 +0000

    today watch favourite urgent set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def add_numbers(x, y):
-        return x + y
+def calculate_factorial(n):
+        return math.factorial(n)
