d102455c5a92431e806dfeb61afa0bfe82bda8cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:22 +0000

    future press plenty shall joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
