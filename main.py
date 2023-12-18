18ce358559f67eab7c735776e2b6cb0de235105d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:08 +0000

    bill slice dangerous wealth hold

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def multiply_numbers(x, y):
+        return x * y
+import os
+def change_working_directory(path):
+        os.chdir(path)
