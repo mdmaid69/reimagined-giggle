0b1e0255db93ce9d30404e26052a83d5714c7f68
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:36 +0000

    poor spread takeoff modern fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
