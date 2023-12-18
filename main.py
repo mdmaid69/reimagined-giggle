271bca916b9d5f3f5aaf61af2469da804ee0ecf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:42 +0000

    regret character smell inside risk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
