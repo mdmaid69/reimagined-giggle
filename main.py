119322a7a0b4a144975a55fe7b91bcec739a1dec
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:34:43 +0000

    worry talk clean pollution error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
