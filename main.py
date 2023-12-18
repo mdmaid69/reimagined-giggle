36554c456d3f5b4d1131db23f5716093aada4919
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:04 +0000

    wind meal pretty cover lock

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
