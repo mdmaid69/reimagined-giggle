25d6fc712df90419d8f79de49cd7e0bd0adc301d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:35 +0000

    exit remark downstairs sentence path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
