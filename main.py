3550a278b88ced07aa17a1750e4aec2c19abcc3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:06 +0000

    genuine children no female lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_list(array):
+        return list(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
