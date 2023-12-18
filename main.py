63c753ed9d442ae42a30372cf524eaf4d600dd8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:34:54 +0000

    trial bin frequent lean fall

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+name = "Python"
+print("Hello,", name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
