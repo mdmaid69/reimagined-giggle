f416f5ee64f9e836c364ce33c1590e1dcb16f4d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:57 +0000

    left declare guilty tread rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
