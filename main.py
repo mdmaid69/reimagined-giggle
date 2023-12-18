70ea6a80ffb9adc4b70ac468549abd0350fb0fa2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:31 +0000

    Ww store worst statement begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def cube_number(x):
+        return x**3
