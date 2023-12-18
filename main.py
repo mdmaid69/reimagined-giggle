d08cf2fdf6ee06080e02d6ed83fccdd5afae41f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:32 +0000

    dead extraordinary Dd condition explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
