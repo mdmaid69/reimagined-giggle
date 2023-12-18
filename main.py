0ea3800e04a6f75c974851fa1e16e461f688a86e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:50:26 +0000

    creature phone niggaacademy feel lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
