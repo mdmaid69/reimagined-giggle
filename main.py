a0e9a56ff6c6eaceb40630ae97b9a873d909a364
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:14 +0000

    steer or complete instant help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
