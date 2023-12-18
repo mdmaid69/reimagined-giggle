f5ba0904a499bf715874807beb30b48e8dde3002
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:14 +0000

    mind bath name accidental against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
