b117584ca22fe258e0e7b01610d1e3fefc382e89
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:34 +0000

    reply push outside refuse game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
