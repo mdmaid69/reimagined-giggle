b27d245e633f4cff11ca337df6530077ce6344f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:35 +0000

    example one radio Zz substance

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def subtract_numbers(x, y):
-        return x - y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
