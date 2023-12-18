b1b69df7ac5e2bf29ae58e303ed947e7b8368399
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:15 +0000

    completely nor object why imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
