09d011fa215f0f57d50b09fa74a531c2e52d3389
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:43 +0000

    wish loan recently takeaway drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
