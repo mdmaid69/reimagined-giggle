db726ad4c668905a53081897e03e48e84371a288
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:12:23 +0000

    least school crime flight wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def square_number(x):
+        return x**2
