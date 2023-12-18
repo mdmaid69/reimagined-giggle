49a44d7bf1e8b1fd2b674ac5390997ef79bea940
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:04 +0000

    stare fond tall needle lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
