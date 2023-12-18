82dfed14554a294cff9d93f0166a4bc0d58b0006
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:50 +0000

    hungry smooth except debt obtain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+print(re.match("h.*o", "hello world"))
