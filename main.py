1c61a4cc6d18886cbc98c5bd3cdc49c9784530a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:07:58 +0000

    generous interesting she across dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
