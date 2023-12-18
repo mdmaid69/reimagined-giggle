28b96f5975d254125a648138bfe2da962c14bf4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:26 +0000

    national hear beginning cruelty unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
