5b843f02b0f5520c56934ff1f013804c04434842
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:56:10 +0000

    open class get offence from

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
