638f9c522d8e836d16b1066c91b19525099fb8cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:45 +0000

    trial giveout nose sad building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_as_bool(array):
+        return bool(array)
