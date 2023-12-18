4a84fa3959036aefe8e57fd2c5f3155dc6223a0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:12 +0000

    trial frame beat if suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
