1634226f86fee3a9dced5fdfee4b653b92cc6b65
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:40 +0000

    bit put last runinto getaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
