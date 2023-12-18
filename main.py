876324d0ec1ecf9c56391bdf097f73b64411e6c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:36 +0000

    about community noone bite dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
+def calculate_acceleration(speed, time):
+        return speed / time
