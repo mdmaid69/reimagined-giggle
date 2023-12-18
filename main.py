807f5c2b19ef5b63af1c15b034d68ce0d0c2b1c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:58 +0000

    embarrassed within workupto gift illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
