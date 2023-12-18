e1c4606ca5ec20b57d55187831d22b76d5df64f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:36 +0000

    rush dry branch yet error

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
 def get_array_item_count(array, item):
         return array.count(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
