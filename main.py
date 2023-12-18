4c7995e9a727d737a93bfcddb976bf3531afbb3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:54 +0000

    previous thank noisy high argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
