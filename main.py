d5fa92e1e103e65fe590e555e9824aa9ac2af0eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:24:17 +0000

    exhibition trick window pupil spill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
