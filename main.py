f76de0666745cac11fe6a4fbd774047e85d6fc53
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:01 +0000

    effort concentrate suck boat ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.ctime()
