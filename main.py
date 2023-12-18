7094ee5dd207ae5cce7c610e5ed0fba04ed08f3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:58 +0000

    tonight play language problem Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
