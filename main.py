6a22686f590ae33cf874c71066532639f4050903
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:18:25 +0000

    keepaway like stupid keepout bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
