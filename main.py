b5521a5d45674c97b35d5fed08596a7cae2aac3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:00 +0000

    minimum lie in justice so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
