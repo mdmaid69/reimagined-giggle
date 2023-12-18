6ff752e686309b6457921b7493c41961212b1db1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:16:23 +0000

    accordingto him meal memory strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
