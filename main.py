7a4b0d6fe46a45c8c99933665ed90eb5a6b12714
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:08 +0000

    objection female chief show old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
