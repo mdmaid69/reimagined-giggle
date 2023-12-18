b1906dbbc42bf78e62ab7a609fcc68727e122172
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:17 +0000

    start load day nose spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
   def get_file_mtime_ns(file_name):
         return os.stat(file_name).st_mtime_ns
