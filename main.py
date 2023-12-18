73f8219b7f24156274077443ddfdff659c3e727d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:22 +0000

    sit but rank little disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
