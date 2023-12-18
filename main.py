785abf263cfc70b38f95181897b148936baab664
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:54 +0000

    turndown enthusiastic minor wealth coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
