5ba0a889d6fb75825dd11ac042097f2e764c5b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:36 +0000

    manage arrange difficult perform comeout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
