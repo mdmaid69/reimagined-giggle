663d16e5dcafcc39858dc037746ea6e3ed44d25d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:24 +0000

    glass temper permission transport kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
