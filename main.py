3573f5c8fc10ed2d7a6b8affb205e79381404d8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:23 +0000

    minute considering popular surprising news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
