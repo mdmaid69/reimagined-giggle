8cda5e27b90230c39d00ad4508f7045d3778b175
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:17 +0000

    stamp song repeat will deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
