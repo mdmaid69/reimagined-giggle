329fd9b38b0e81e47c53531c05c9d2acaefb20d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:46:42 +0000

    smart remain cry his somewhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
