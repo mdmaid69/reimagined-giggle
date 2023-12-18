895965c3796bb098b5fd8c5b44da9f4467149797
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:45 +0000

    fine break normal lot minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
