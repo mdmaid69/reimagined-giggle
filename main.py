26fb9de32035b702f7e46ee2a2cac1465f8ee6e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:25 +0000

    closely fairly far crime actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
