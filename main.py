3f822be36b613a5736cf24e0e1824c601659523f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:51 +0000

    people consist respect permission gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
