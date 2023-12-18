5db404fbb08ba6278dcb207e58301586854e69e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:50 +0000

    same stamp tight cheap your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
