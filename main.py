1cbb67724d9bf38d7a6bb22c17406de2b91e3c44
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:00 +0000

    lonely punish however runinto wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
