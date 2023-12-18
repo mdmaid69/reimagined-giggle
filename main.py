66fb68e9dbf278164d3daaac579e620c822aacf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:55 +0000

    whatever indeed long entry clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
