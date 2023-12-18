0041d5e94913717f3db11d5edc673ac69134892d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:39:29 +0000

    chin trial introduce shelter hurry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
