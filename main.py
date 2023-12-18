850f1f7d4b2f48a8ce1a1a2ad0ccd0ab69e2a259
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:17:50 +0000

    mad so of funny argument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_today_date():
+        return datetime.date.today()
