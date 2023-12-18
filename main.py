850545e60eab2ba7c642d9bc05957676e48cca51
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:27 +0000

    walk embarrassed bother flat noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
