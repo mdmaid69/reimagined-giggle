232efa8495bf86ba00f683436c6233eb6d440061
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:38:47 +0000

    funny relationship my replace bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
