50fe052b8e6fba78377c739ad1a2da5d3d03a4b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:59:35 +0000

    succeed off failure join secret

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
