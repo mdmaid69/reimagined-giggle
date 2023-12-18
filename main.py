d2de3e44c47917cdb9cf3ea3e288086e01451b0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:04 +0000

    fill trade almost suck I

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def find_max(numbers):
+        return max(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
