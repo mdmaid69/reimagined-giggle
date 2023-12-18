8e6d1835f0a14354ea0618c864bc764bd92bb3a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:30 +0000

    front thirsty weak unfair answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-print("Hello, world!")
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
