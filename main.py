179df9c5cbe8eccf3ac8992c9ff1f576249e5439
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:20 +0000

    extraordinary television platform ask complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_min(numbers):
-        return min(numbers)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
