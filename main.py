c19f28f0e8df236c777240444a6b15a44fab01f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:59 +0000

    regret art tape love callup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def remove_duplicates(lst):
+        return list(set(lst))
