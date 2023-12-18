dcb783885d796cec33e33da0e492582cb3ea2ca3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:17 +0000

    demand door public thumb belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
+import sys
+def print_python_version():
+        return sys.version
