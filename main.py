00c3be88406cf0647a51682b128991b91f02a14f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:53 +0000

    somewhere brush spill tonight interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
