0547e830cd05bcac8c185ffcf199098fefcff01d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:22 +0000

    ring main begin doubt 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
