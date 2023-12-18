49b7b440f331e00a2b1261fd02dde97c3b1023a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:56 +0000

    steal thirsty ashamed boil duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
