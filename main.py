ac2ea9a9ba454d8c81dbdf5123fbd3a312cae82f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:05 +0000

    equipment convince story getout patient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-name = "Python"
-print("Hello,", name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
