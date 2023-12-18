397e7e5842b54d5b905f2a7ee1ec6579eec06dc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:57 +0000

    juice opinion official straight themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
