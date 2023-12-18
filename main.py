69fb1e76f0496a7440d15cb66f3020b9a0be1423
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:17 +0000

    left earn curious walk design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
