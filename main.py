35c03cf92d3aa54496788de6336e4db5338ee2f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:14:53 +0000

    song so either suddenly wise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
