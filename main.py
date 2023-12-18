67768ac3af7bdb543c29f39ed485dcc6b1480f29
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:38 +0000

    university weak succeed cover purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
