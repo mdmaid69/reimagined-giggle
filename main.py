379d83521520ff772e32ba2f6d56b52583a30f39
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:49 +0000

    big shall choice hit sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_max(numbers):
+        return max(numbers)
