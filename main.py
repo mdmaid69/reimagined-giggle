b25d9ff1515cb6515ecca2a342e062af73177627
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:41 +0000

    again where wish both estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_current_working_directory():
+        return os.getcwd()
