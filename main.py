86cc00aa36f8104edc88b3bee7b211e39e2c6af1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:51 +0000

    name find image case agreement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
