ccc7ea82dc3a096a7e8ca5089f565d0e49e39998
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:10 +0000

    difference punish within keepup refer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
