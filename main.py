092be9a145a32ae6399cb4db4c638d55ba630afe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:29 +0000

    somehow apart attach Qq tragedy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
