4d64cb1c38ee7b77018000a2ab57961eb9edfd7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:47 +0000

    thanks empty raise strength OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
