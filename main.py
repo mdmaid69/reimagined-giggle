383be8171c2352d3ecd16c616d73e20d6e44353d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:18 +0000

    nature appear 568energy pronunciation combine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
