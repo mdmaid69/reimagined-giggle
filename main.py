f12adf8e30b625e34e93d3d83b8e23c2e6eb4511
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:46 +0000

    shrink then responsible close tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
