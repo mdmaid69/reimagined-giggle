c6795ab421288409e55a41895e3c457614b35553
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:32 +0000

    stiff slight pair star truth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
