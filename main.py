52500f36f830840254c410aad523d9e26639ec9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:55 +0000

    various unlucky protect valuable gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
