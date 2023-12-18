d721cce36318cf51bacb6adb0d65a60a83e83cf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:06 +0000

    precise study better slice protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
