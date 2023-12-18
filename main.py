04b7d85a79a4c4df3af327c85e3339db20050e15
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:17:53 +0000

    moment your sound square close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
