1f4ed1ed9e9fc5b7317b1658caa308707b4f67fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:28 +0000

    argument share big train hungry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def convert_to_hex(n):
+        return hex(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
