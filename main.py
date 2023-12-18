30c8bc1f157898a61c6b7c3e808427b77057447b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:07 +0000

    popular attitude wash gentle keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
