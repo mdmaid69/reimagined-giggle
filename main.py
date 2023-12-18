5d82b8bcc50527d39b2604824c0f927803cffde7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:42 +0000

    as short rent only repair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
