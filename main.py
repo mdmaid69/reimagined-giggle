20b4ea816804616f31e95186240a48d2ff7bec6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:45:25 +0000

    so fit blow grateful past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
