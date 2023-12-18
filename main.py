50c5a49627a5cbc43f91609b0a306aa10972c5cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:26 +0000

    doubt sorry stiff argue holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
