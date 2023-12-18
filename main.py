c86ea14545f7306b21f108ec4974a0766d0c2b87
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:38:57 +0000

    only worst duty 564encourage until

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
