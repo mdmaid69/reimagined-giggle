ec3cf63b7ba4bfeddf6bf1ed62aec070f375208e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:31 +0000

    development accommodation book fit immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
