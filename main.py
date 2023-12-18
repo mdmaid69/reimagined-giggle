1e625a16473037ec9f9fbb24e598492256ae025b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:46 +0000

    nerve disappear street position lie

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
