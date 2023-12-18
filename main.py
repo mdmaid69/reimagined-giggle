cb306b4045a166a64935e3d0d2c6d09ddf29a229
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:47 +0000

    range shadow respect behind study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
