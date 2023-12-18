1bc95632f6dde8a94e45e99405e069c4b2fd57ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:47 +0000

    trouble off drawing use welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def convert_to_binary(n):
+        return bin(n)
