e26fb4a13bc68157c4d7f8730b10a69fd47fb462
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:10:31 +0000

    water invitation people Dd unlikely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
