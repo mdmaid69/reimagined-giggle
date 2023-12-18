05f1e17b35c7b4318483d29d3a2df0e4e92375d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:31 +0000

    knock wind actually although breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import time
+def get_current_time():
+        return time.time()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
