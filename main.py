4d105d500796fe97614030220ae8657d8246feb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:59 +0000

    admire compete long interview Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
