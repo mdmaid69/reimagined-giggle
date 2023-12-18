5f0379f477bbd4bb49d61d446e2d2a193af5783c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:46 +0000

    to let chase car by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
