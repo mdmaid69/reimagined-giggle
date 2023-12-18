0c2f4ca4ba97973139b4f8ed9dba552f46d8125f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:12 +0000

    couple phone beyond dull unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
