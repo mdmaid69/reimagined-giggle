85de648342cfd68f47009e7977a9df3799062d97
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:56 +0000

    steady shallow rubbish just evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
