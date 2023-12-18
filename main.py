7cbbd81b0f758a4e69b84f534bd0a7bae475c343
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:28 +0000

    among remark anything suggest anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def count_elements(lst):
+        return len(lst)
