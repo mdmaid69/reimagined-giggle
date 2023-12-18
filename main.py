ce48a1d4c535cafcf6430a522b02f313f4b62f61
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:57 +0000

    fresh copy rarely lookback(on) their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_base_name(path):
+        return os.path.basename(path)
