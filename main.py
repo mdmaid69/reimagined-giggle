5fd71885d527ded15030784d228abe0fe59582ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:47 +0000

    rest off instant attention 571enough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
