f4fa8a479e73c147e6bd2d073d9be2474e5b5f24
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:21 +0000

    Yy arrive regret slight table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
