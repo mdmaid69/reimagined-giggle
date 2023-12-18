b596da592b1f4c9d548766c6b30382e886e63101
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:52 +0000

    hate scrape all edge business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
