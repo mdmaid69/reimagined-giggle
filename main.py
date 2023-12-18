614568e069d06d369a2c260b7efc5471c4e97a0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:21 +0000

    popular miss test uniform chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
