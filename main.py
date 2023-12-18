98b44c2aa0af5228f68b0abed4a781c7d2894629
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:38:26 +0000

    bill opposite form definite dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def split_path(path):
+        return os.path.split(path)
