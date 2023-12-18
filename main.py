8b5eb659cc93d7381b53b6a36b365f34c4c6f667
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:20 +0000

    use handle right wire means

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
