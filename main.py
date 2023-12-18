56dfa0d2615b9a31e575dafa3670cbde3a64d82f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:50 +0000

    far preparation habit tax train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
