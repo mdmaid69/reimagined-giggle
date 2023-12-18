04f348bd0f017e4f45f51371d0dc40d856408161
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:01:19 +0000

    busy raw properly takeback advise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
