32ec63f54499f132a6fcf9b5f4dc39fc9df1b082
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:34 +0000

    worth wash job talk smile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_area_circle(r):
+        return 3.14 * r**2
