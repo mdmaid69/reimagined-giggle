9e0f7d9b78a77721acb0f538a4c28bd173fb9505
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:13 +0000

    care beautiful workupto amount cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
