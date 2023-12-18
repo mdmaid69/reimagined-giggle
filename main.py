361a56cb10ea5823cd7cc0b9c426e8b48f9b735d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:07 +0000

    bring allow repeat protest welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
