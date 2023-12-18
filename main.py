678f59d10c917f886359ec3c578f154e18472618
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:08 +0000

    actual model model condition middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
