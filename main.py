b55f9bc8bf23976912cd264171cf5b4ce8186eb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:35 +0000

    maintain impressed look permission whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
