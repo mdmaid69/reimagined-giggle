1625ea403366cf5e68a72e3f774bb57803438f4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:00 +0000

    recovery disagree part smell beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
