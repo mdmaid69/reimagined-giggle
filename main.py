1f4f303478416c258f9b7a7033a9c0138fa0bea5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:39:13 +0000

    except thank may programme warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import platform
+def get_os_info():
+        return platform.uname()
