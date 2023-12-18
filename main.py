e2bfc028d2443a961c89e1b80df7e87931c90b00
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:36 +0000

    doubt content slightly must close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_arc_sine(x):
+        return math.asin(x)
