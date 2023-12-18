0d4693fa0af7683da28190b70fe550f7569fa165
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:51 +0000

    authority takeoff obvious arrive physical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import numpy as np
+print(np.array([1, 2, 3]))
+import tensorflow as tf
+print(tf.__version__)
