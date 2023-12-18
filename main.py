a72a0696ecb4a5ee70ceecbfc324e8bb95fe6f88
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:10 +0000

    average curious cheap genuine remove

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def convert_to_octal(n):
+        return oct(n)
+for i in range(10): print(i)
