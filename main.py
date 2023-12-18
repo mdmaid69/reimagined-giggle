fcc6da5f79c7515b1de8c4aeff2ace63e7a9d146
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:04 +0000

    classroom whom organize place such

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def reverse_string(s):
+        return s[::-1]
