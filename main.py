8ed4fb3fc79eca145b30f8d59eefbeb42753ce64
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:08 +0000

    practice satisfactory police powerful also

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area(radius):
+        return 3.14 * radius * radius
