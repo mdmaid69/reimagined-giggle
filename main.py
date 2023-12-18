28a2013732f3b44a1e8ed1156c1375aad4a5715a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:35 +0000

    male breakdown getout Qq illness

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
