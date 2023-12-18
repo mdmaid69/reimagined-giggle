88e316f6100189c914677a6f32bbbbefd30c8c08
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:39 +0000

    who partner hungry opposite complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
