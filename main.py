ea1b59c7612f5e1f2c048cf76bb45cdd5352f12c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:42 +0000

    professional narrow commit recently run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
