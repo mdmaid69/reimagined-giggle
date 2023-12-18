e5d6eca47362709890b2762b25de6051e8257b06
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:35 +0000

    level butter thorough vision phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
