a5277c20441618f6f7e23e872ff1181066998dfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:05 +0000

    Bb that temperature past limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
