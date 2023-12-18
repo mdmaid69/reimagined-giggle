60db50bebf039848b81d4b32a87e0e1e94cf7f53
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:24:43 +0000

    luck best branch future field

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
