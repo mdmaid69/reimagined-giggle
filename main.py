2eaa2eb33183280ed82b54179c55ba706226c017
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:34 +0000

    act subject way ours attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
