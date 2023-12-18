ec26667c4468d6ee96a8c92400271ba92497abd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:51 +0000

    easy declaration mail appearance side

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
