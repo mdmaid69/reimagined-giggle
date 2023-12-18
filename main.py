b2bc58209eaebcc0816119d7ddd6ca4ee283bd5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:37 +0000

    taste account very keepaway affect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+print(sys.version)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
