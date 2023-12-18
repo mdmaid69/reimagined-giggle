2939ae83fdb62b3d7eefb2c789a6661b415e4118
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:13:38 +0000

    charge trousers stay pass speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
