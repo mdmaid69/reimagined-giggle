3230f099d2581c83a28153f340f7d5b406a67643
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:39:22 +0000

    pain step turnon information design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+print(os.getcwd())
