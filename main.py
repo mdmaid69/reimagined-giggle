c23d9490bf8d9c73b98991a39b5f7f88cd699283
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:34:11 +0000

    container sort inquire lead payment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
