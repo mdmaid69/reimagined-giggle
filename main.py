1e5950e3dde19ca1aed12e51354f6e507ad9f141
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:18 +0000

    pair increase evidence legal book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-print(os.getcwd())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
