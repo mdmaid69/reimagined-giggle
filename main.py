61bbbe15672125f2b72b0f04c3fa8d829ca46767
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:11 +0000

    responsible steep gas shake least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
