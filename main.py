328f0dbd3d3147e3ffbd629d417fab1cba8f24a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:16 +0000

    my thick period who unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def remove_from_array(array, item):
+        array.remove(item)
