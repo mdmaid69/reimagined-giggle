4a145737db2aeccc1681aff1523e737d91e173af
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:57:48 +0000

    care favourite taste down borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
