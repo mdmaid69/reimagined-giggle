aef91b48e7d1ede6e5aa905c698448a9bfe53dea
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:01:14 +0000

    mysterious follow ever influence science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+print("Hello, world!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
