c764131923400af2311ec67ae081423f22db7710
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:56 +0000

    form undo majority still turnover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
