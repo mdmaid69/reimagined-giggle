a95bab325306efb3426a7921b3e84626350bbd91
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:02 +0000

    many middle along rich comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_float(array):
+        return float(array[0])
