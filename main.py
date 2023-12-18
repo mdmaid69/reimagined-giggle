80b792f19a9adbd621bc02cfa6444cbf69078e82
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:41 +0000

    survive say pronunciation score divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
