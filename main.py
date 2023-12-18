2efe5c3d4930d836b9e5e912bea2741e37ca0229
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:32 +0000

    more cheat sweep this apply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
