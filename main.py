d1d4581498d28deaa771f4ef89cc5bd8e521991d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:41 +0000

    lookup small frequent who giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import re
+print(re.match("h.*o", "hello world"))
