a99cc56c92eb24c0b0fecbb4e03333e74919d08f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:04 +0000

    single instead air play long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
