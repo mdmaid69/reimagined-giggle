6b67bb4025ba2816856fe02f675e307921306356
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:05:31 +0000

    active person of increase quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
