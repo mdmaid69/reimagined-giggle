15ad1567620690a0891bf3452efe451020ae50e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:58 +0000

    motion guilty copy story achievement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
