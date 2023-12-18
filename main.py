0327bfd2afe0a4772c2fa3d4d71abb4636266792
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:08 +0000

    swing challenge hang react attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
