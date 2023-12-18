4c6a5146c5953ef1a9de601f00972af6d7c69cc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:26:18 +0000

    shape confused actual degree dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+def exit_program():
+        sys.exit()
