e83a755a68ec5cb0faa4aa08aef19d1777173e48
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:55 +0000

    nowhere one foot ashamed careful

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
