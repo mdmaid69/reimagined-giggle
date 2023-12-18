a53cc3dc68066133bd01935ff4bb0b26bf25c2d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:39 +0000

    uncomfortable copy broken their inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_as_str(array):
+        return str(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
