ec25b3c119dcc64d186117363af33b3e028e3611
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:45 +0000

    angry narrow off achieve comeback

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
