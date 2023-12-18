fff0b4a74e18a926c854e8f328f2046d1d1db4eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:56 +0000

    talkinto fault describe correct black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def convert_to_binary(n):
+        return bin(n)
