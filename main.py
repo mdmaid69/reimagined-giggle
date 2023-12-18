b7c66fd7182749f346369fb0847f7427975d180a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:09 +0000

    feeling sensible question disappointed finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def multiply_numbers(x, y):
-        return x * y
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_speed(distance, time):
+        return distance / time
