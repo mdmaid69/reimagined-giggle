d4c441e7ea2d6cacc719a24f5c22bc3ddc5f9dad
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:36 +0000

    that until name immediately swimming

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
