1b1c6844ef9ef5220fd0279c3237f13a66a397f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:01 +0000

    town nowhere area forward slice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def print_python_version():
+        print(sys.version)
