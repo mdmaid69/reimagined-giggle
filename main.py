d016e2d54b23eb923b4dd28e22f309ee51041710
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:21 +0000

    ocean pity knock general dance

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
