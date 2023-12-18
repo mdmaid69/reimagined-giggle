a0167ba03e28266d847b746959a763ee987e87a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:07:22 +0000

    favour greeting supply watch big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
