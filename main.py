0d10d521fc45b75b9a44159458f42b4a239ec7a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:27 +0000

    food attention bright mean occasion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
