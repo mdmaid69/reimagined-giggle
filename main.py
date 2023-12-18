8b8e10675e25558a083691c6636f71038c7a0885
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:14:20 +0000

    transparent no behaviour home instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_modulus(x, y):
         return math.fmod(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
