e25a16cbb6860ded66a0ee7021d4adb2765379b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:44:57 +0000

    makeinto cum mood crisis total

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
