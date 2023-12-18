63dab82ae7c26893a748db75091ec792401932e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:29 +0000

    also can question tape behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_min(numbers):
-        return min(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
