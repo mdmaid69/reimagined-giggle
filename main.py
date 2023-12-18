39bfbe834866ad75fe3306da2f9b6a3edb55c6ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:17:12 +0000

    married desperate rob realize Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_mean(arr):
+        return np.mean(arr)
