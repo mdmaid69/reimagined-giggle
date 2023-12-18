30a933acc9a4692afe6bcb5dc08d71ec7f8964b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:46 +0000

    building bite runover get noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
