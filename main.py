ca61a146a2ddb31e2f2fbcdcbcde3ac0efd583f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:30 +0000

    care silent rate control loud

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
