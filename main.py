5c00857cb47532e1a83d6ef7c322a73213a218a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:21:05 +0000

    house generous example search spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
