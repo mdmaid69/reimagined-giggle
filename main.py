7db496c1d101dc8e97ef4d34a8a2a1bcd3da4f50
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:19 +0000

    when bright release calloff total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
