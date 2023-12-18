3f1397c1f8e8ab3887fb9bb84b71c4304fbb4e6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:57 +0000

    team wait pretty comfort man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
