71818e212af687da0a4dc5c12f2fdf0f95cf43ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:48 +0000

    exchange sad remark run shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
