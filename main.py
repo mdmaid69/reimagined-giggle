bc73fcc351db75bcc4665982ea191ac3d3641559
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:06:04 +0000

    roof Hh path card clear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
