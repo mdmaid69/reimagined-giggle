d6c531402087118735c88bdc05e0757173c604f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:19 +0000

    area deliberate hello base raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print("Common elements:", set(list1) & set(list2))
