25867222638d8d505c094f1ac6764e7207a0de0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:58 +0000

    phone before children little brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_area_rectangle(l, w):
+        return l * w
