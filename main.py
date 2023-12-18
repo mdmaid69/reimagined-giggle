18cbef4d75685f19ae428f72b05d4724b8c70715
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:18 +0000

    theirs forever introduction mysterious possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
