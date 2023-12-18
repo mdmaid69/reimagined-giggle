b0fc610d6f79e158040d331f88d982dcc2ec09d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:55:58 +0000

    travel peace damp high opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
