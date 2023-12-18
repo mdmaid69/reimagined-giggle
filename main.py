24991cf69fbd6fb4b0c5ead0370a336df6d45bc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:18 +0000

    coffee achievement him 567enemy variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
