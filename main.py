a370596adf4187eada51d9f9220eabae8cd3a296
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:30 +0000

    further flavour schedule jump takeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import platform
+def get_os_info():
+        return platform.uname()
