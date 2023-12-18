85c244cc62e028c29c0956a5fd15b2033c38a3db
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:26 +0000

    declaration operation skirt transport rush

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import random
+print(random.randint(0, 100))
