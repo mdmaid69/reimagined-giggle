322314fab7a03c1ea65d5dd0c22c425008f271cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:15 +0000

    empty car boring knowledge whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
