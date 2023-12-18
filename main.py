917bd85781b859fe462e3fd65f1ca0976bcfab90
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:37:47 +0000

    press fast pleasure discuss kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
