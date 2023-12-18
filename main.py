614ebc29341aeb90e0dc9095138d9bee4208905f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:52 +0000

    Tt disagree protect me physical

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
