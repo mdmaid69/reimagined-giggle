03e02fe43a84239514b4e239597bb3adee04fbc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:31 +0000

    storm politics world few passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
