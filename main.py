313876f61c2041d2c370341a9e7d8cc847bc1452
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:41 +0000

    task key complaint address port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
