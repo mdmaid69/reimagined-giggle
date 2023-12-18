50e9e10028f133340b0bcc1e40811beb15ab8bf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:45 +0000

    share base centre question collection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
