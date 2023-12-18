6d0f2a4392edab691836015b882b73c555bc5143
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:45:32 +0000

    breakin punish minority free present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
