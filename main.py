cb008c995e6fb34a3d771f59c20cca2f30b4807d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:18 +0000

    score light train entrance round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def cube_number(x):
+        return x**3
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
