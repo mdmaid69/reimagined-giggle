3288b766f1de7afd65fcffabc2e6d36b1eaf19f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:02:21 +0000

    muscle drive charge justice across

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
