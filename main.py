a178224e4e29ed72e22ed97866fe7ada5fc3612a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:04 +0000

    try spite expression mark number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_max(lst):
+        return max(lst)
