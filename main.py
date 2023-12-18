2e701eda866b6de8ffd90725281d405a2a2c5a49
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:17 +0000

    pity beginning science organization balance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def add_numbers(x, y):
+        return x + y
