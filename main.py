9740fb9ae328faba2dc6291330ed02a829999850
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:02 +0000

    yet badly clear appearance cruelty

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
