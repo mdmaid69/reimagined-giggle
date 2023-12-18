991c402732fc2193923de49f4d0387c20dc9c001
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:09 +0000

    bother getaway brief inside though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
