4df05cc60a52f0f566bb29f35ec8d75ab0cf03c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:57 +0000

    extra the familiar tap eye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
