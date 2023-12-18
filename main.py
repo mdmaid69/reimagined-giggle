b8f91b0cf3c6ca8c1370bbf304e98454cf573a20
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:53 +0000

    breakup escape point somewhere damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
