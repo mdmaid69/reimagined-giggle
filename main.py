7958cc2f491d597d0abd848b177082ff0298b0ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:15 +0000

    might dangerous delight unfortunate tense

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
