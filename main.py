55e0de1f206668bf55680ca6a756cc0939126057
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:06 +0000

    deserve wide suddenly fashion unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
