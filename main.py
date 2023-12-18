416e3e30536d31c1374f3993fef39dbeea36ca8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:47 +0000

    attach present dare part closed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def count_elements(lst):
+        return len(lst)
