05ab54f84236bbdac444e81954b470af70f766ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:12 +0000

    store give ordinary pollution equipment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
