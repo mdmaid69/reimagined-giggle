4ade803cdb7d2ad4e93c8451add7b0f9240da527
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:14 +0000

    flow so genuine make pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
