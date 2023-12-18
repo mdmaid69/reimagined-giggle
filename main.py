0309987e07292cdab6cc044640cd33554ca62b30
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:00:30 +0000

    freedom flood place any kind

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
