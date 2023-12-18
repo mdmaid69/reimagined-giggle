02a263aee0d7dc6a9868fa9b512a41097f007642
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:36 +0000

    bag attractive shoot available keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
