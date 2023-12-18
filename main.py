07e11ac1643a7ee3f99c9444fe79eb6be4848365
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:56 +0000

    breakdown dream loud correct wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
