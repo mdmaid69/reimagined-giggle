e9bce9732fda8f49d27e28859a9d6cbbdf5397a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:59:55 +0000

    conclusion morning final clothes introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
