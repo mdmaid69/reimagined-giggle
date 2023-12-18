cdb5136a8d9c779fbb37cac15cf1efb7f689b6b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:33 +0000

    ilovedicks unkind everywhere straight middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
