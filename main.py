5336e81e887e72651bd6c0f54a4adcce1214bac6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:01 +0000

    machine themselves fault factory example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
