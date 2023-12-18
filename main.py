4434460fc4eb7011599f92f1933904dbb0e2abb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:34 +0000

    safe offend interest image control

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
