de5c40619054550739e1c8b5ea3dde835c5b8194
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:38 +0000

    answer power bag trap geton

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_repr(array):
+        return repr(array)
