e9207aa27414893999af373f4ae4fcd866015f18
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:46 +0000

    tongue steady able company guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
