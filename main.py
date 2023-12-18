8f8f309b2cd52a2c8aa8db3dbcdfec2482295b55
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:52 +0000

    late you accident front brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
