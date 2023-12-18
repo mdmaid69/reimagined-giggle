dc378d89ef0b9bede128c33db4ff25870277c4ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:20 +0000

    Oo authority opposite possibly straight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def count_elements(lst):
-        return len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
