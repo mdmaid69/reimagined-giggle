84d6aaac410d5c116db9007918add8e785bb0c8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:14:38 +0000

    full tidy calm exact laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
