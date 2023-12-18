802f0ab70c964664e6531da2c16ef31d8e1e6468
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:03 +0000

    level more load me he

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
