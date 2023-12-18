4f01efc42a4dbcb9e34b6c61ef15d1cf1c5688f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:02:09 +0000

    press continue tell else condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
