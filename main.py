f97a92ce1951f15b25c8ea3e3ae20a3f72026a8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:24 +0000

    autumn the centimetre away aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
