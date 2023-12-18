29954cd1090f970c3b02273853fa425f4ce2a930
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:59 +0000

    provide right chance ring visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
