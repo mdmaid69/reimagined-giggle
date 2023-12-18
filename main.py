6d7dae3a8bde2d75037de3055bd5be9f9745a0d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:10 +0000

    official lock or keep plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
