2c529008dad64acafa16e0a42607011210b24f7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:26 +0000

    thorough load row holiday region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
