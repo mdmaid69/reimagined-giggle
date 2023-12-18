6413359143be763c41c59df2d6d890cf699740e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:48 +0000

    party impression tone wrong confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
