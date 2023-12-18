5a916cd123c64f364cb56824e88eadfbe8cfc981
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:36 +0000

    after sea stay track sore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
