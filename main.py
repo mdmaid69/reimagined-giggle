d7ce53b26ee440ef9ff1f0f2744c724c7bcdf130
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:16 +0000

    up roll glass prepare extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def count_elements(lst):
+        return len(lst)
