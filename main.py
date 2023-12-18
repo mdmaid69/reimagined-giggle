50d50587cf427b41deefe68852acf0046cad3198
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:33 +0000

    cheek people run refer stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import time
-print(time.time())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
