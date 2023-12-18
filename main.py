9bee478d4c653cd8dfa8af111203106ac9969fbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:46:29 +0000

    breakup flight around right ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
