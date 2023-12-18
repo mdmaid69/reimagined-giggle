2d688d3fbacc371dca212bc7946d898d02951c7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:03 +0000

    worth advanced another 570enjoyable hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
