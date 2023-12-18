7f09466a3ad046379f2ebb81c1430bfb93963ff6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:13 +0000

    possibility paper pinch car occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
