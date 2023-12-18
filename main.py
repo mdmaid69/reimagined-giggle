69ce4300112ec6ec5f80c48cc7648be14935b920
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:44 +0000

    interesting holdout opportunity scream real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
