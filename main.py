df33480fb2fb10096cb0645eb1a70621d08ebb96
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:35 +0000

    someone prepare convinced tomorrow birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
