51f8b32f843f1e16c906b9e8ab1823bf41043f9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:24 +0000

    pipe scream match pipe kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
