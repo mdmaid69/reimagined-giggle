ca8f04e5c553cbc3cc8c0abf99c41778babbd401
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:22 +0000

    hair innocent remove underneath pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
