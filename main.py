f027e78ca009f14bdabfb5157bf2918dd9f42340
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:32 +0000

    treat wide traffic disagree study

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
