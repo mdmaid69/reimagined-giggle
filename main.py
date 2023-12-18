183733695644f549ed5cf65914e8febddd2fe2cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:38 +0000

    now base broad friendly design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
