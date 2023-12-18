5b3995dab0dc835bd1cf643627b074c51ebff0fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:39 +0000

    now task steal female memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
