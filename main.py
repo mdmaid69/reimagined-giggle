4e7387acdab00c48b7e816bea59a9ff0fc92ebe8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:52 +0000

    its design software judge happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
