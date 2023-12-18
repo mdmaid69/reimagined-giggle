40a89f6eadf8e09e978602088f1ba4ede670fc23
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:34 +0000

    couple takeover necessary convenient greeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
