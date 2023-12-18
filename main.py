de194b6ec42a59effd96bc49bcaf201709ed9e18
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:23:20 +0000

    bag search sport description building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
