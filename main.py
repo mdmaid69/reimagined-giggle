50dade141c5452507d5d40d11688341bd0d23e3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:25:07 +0000

    arrangement cruelty issue responsibility avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_list_from_array(array):
+        return array.tolist()
