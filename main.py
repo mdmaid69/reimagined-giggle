ec003703c0ee79c24a41815be1ea268c7c2134dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:26:40 +0000

    heat ago broken Ii 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
