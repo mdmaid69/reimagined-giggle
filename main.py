c2097c8bbaff4df53d272cac9889f45feb301afe
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:50 +0000

    sure side turninto Qq communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import getpass
+def get_username():
+        return getpass.getuser()
