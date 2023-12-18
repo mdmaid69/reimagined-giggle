93da043e79dbc24ec407fed871f178f2a590047f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:48 +0000

    age lose borrow decision could

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
