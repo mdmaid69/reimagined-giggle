99b4b0ea0d936cfc780ef18af9785057c20999df
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:45 +0000

    Yy shame post inquire improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
