e4e4b9ba5d371fb27abd69ca28225ba841c6b219
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:52:58 +0000

    behaviour laugh brother tax suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
