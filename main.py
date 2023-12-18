f55f2b95f22d12107fd9e3b422b57b8a34705962
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:07 +0000

    upset opinion bye greeting seem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
