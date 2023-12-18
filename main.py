c08fb5e7a2cd7255d3f32a0951145bf16ac13fe1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:41 +0000

    accommodation lean brain lie set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
