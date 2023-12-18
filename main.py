3dd12aa6d75f32b9a001df4a2111e7a0f29bd53b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:44 +0000

    church far average punish shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
