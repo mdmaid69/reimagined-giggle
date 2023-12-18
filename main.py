5cea4d5a2b2675b7790d09be2e987d5729ffea09
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:36 +0000

    equipment machine splash safe blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
