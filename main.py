7adedc4baf7a525d3b87f645dbf55f3570aeacf7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:53 +0000

    the my unpleasant education sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_min(lst):
-        return min(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
