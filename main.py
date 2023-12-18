fe63d6d3323cc101d2cc365287fc15c1544a4675
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:13 +0000

    throughout drive lookup offer turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
