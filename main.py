8415a67d2d6bce1db73664e8ed49054655db69ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:51:57 +0000

    Yy surprise cruel suffer before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
