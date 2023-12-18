7efb71481a60787df2c3ec24e350200f025f6941
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:22 +0000

    hand second recommend muscle neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
