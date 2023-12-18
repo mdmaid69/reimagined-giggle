67012aa58dc51b2f104daec24d3bd0d597cfb668
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:14 +0000

    themselves repair discussion lost exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
