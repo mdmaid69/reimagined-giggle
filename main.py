e8b344b2ef92e02e8bb986e2d2b8ab712adee610
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:13 +0000

    passenger tonight support stone pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def change_working_directory(path):
+        os.chdir(path)
