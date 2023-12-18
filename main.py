e134d6ed1796c163b30372ed673a075121231a2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:38:02 +0000

    measure confidence remain lot female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_username():
+        return getpass.getuser()
