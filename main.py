0b88b7bcf27e89924c4d992273d4c72e7177d500
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:24 +0000

    aware recognize road pronunciation soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
