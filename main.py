3547bb0107415bcd4118079dc8bfd3ef2ba29172
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:30:53 +0000

    shelter fear trust allow repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
