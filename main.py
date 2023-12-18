f2a0c78102810af6fd05f9fc6f1a88e1481ec3a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:20 +0000

    door gate get seriously suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
