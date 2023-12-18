d336874931a99109e541a508aa080ffbcdf55907
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:05:37 +0000

    interest machine attempt Nn year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_area_circle(r):
+        return 3.14 * r**2
