cf5f913ffd5517c167ab44cfc8182e3eeb4117d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:10 +0000

    future thankyou snow wet disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
