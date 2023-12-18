97a94f68711cdefcdc7757999c690db4ba110e4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:01 +0000

    week stamp divide best protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
