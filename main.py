186d7f819fcbf3ffe798ff22ab5eb69f46b4bf15
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:46 +0000

    reliable Ff variety convincing generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
