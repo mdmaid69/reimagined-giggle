0364052a48fa71088c69982505226603ed420227
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:21:12 +0000

    gooff company clever school foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
