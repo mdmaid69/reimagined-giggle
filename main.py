6934c69d526407035768e8a671aae82eb1d800c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:25 +0000

    gooff strike remark fortunate except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
