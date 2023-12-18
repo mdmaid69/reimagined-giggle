082aa4951cfd964d1c1b4c4af8325b1371d20059
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:29 +0000

    comefrom deserve they surprising medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
