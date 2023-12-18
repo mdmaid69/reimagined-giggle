30c82f32e446a223721164ff3fb94bd32dccf581
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:35:27 +0000

    work depend press tonight work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
