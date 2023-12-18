ccae451a53b0e4ff51fc6163399c2fb4c2b5a08f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:21 +0000

    reduce interview leg vanish wash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
