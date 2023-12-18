cef896189b12a3a0aea3e474484fcde1371a0ea2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:42 +0000

    mark uncomfortable crazy protection makeinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_unique_words(sentence):
+        return set(sentence.split())
+def is_odd(n):
+        return n % 2 != 0
