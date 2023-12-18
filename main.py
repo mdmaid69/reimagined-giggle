cfbf9ecc94bf747d8b0b1f10e97f9283a1e76e10
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:22 +0000

    information search tragedy generous however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
