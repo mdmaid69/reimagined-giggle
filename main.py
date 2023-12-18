28ffc894a62ac4d11eccf4fa6754224bba00fe2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:26 +0000

    sensible imagine tax interesting workupto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
