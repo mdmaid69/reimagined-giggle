fa0c34c899930acac7209de0676c63d1da26e04e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:59 +0000

    zero fall substance to shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
