66cd519d2a2ca02385b460260535a36bb97c0ca7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:15 +0000

    university along tune piece sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
