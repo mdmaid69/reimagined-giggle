2bad3a39b5f1c286546178dd6be7cc721f2c6364
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:44 +0000

    slip development announce realize unfortunate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
