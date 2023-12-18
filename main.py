4c06905ce5684138b9549186d1d36fc2199d62c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:29 +0000

    illness criticize somehow material no

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
