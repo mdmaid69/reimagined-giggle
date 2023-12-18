aa7d3acdf5bd16899de5c7caa469120e1d6cdf52
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:49:02 +0000

    window cause friend tune march

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def add_numbers(x, y):
+        return x + y
