7585ba29ea6989ef82c251081482e44b1bd031fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:41 +0000

    process jump spring married crowded

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_int(array):
+        return int(array[0])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
