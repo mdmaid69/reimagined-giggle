a34a0ee0cf426f5afe9ec3c7078dee597a6a962a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:51 +0000

    nearly flow blow wish business

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
