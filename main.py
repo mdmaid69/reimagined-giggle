f4e951dc5ce148aab8e92b3b221e72c8e4ec9968
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:51 +0000

    typical calmly serious delicate Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
