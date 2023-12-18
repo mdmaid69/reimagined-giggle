bc46abc014aac1e33cf941611ad65e846fd5e154
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:56:40 +0000

    describe keepfrom memory responsible stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
