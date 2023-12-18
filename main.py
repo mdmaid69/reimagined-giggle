6e3354aa223b186123dce17c99f5264c2254ce7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:38 +0000

    onto thinkof meeting uponon same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
