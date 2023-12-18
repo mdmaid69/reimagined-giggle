ee9fe4c8b8521c804c55d06fa199bd3a65bb699e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:10 +0000

    breakfast sight bill similar day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
