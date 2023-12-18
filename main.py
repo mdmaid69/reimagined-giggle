2948f7d299591f4b76f13e5011cc5c72fe23ed85
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:34 +0000

    wind shoulder circumstance burst only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
