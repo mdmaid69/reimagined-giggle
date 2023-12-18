e1cb34413165fbc848665ff6313e436713e0313f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:29 +0000

    undo what occur most regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
