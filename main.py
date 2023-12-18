7dc57574469347da5cd668ee95161f99171e5b34
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:25 +0000

    gift better blue collect recognize

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
