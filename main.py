cbb208fe9ded3259f299a6e043f90660505eb0db
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:17 +0000

    few crash meanwhile line block

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
