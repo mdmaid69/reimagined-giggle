ee2faecdbcda08a2cfcde6a24bef94c33ffdf27e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:34 +0000

    sink circumstance invitation characteristic accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
