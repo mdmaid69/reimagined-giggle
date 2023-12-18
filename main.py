a2c2d9a86294543c4d50fbb976e2c332d4f93b53
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:12:43 +0000

    error pile fierce light plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
