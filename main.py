a16d77c032fa7ddda255be1fb9317928d0f4c248
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:25 +0000

    less pour band land grand

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
