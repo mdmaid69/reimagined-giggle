ce2145ddc21845afc7a43cb242ede8530c75919e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:05:10 +0000

    frighten contain cold public giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
