7e95a3cf94a0b1918ff383e3980105cfb4d97f5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:33 +0000

    pause can hers 567enemy offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
