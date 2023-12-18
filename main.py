bcb47bea71a56fc10821ecc4dba5f70dc2941a34
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:41 +0000

    deaf trip threaten police excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
