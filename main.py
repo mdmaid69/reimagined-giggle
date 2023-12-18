6fa73e39256de3bcd41b6571ac35ca6c0e19c152
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:17:00 +0000

    thinkof quantity trip independent relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
