03d6e98d1239ab83a5437e9318797c0cae0cddb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:53 +0000

    will walk loan fall down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
