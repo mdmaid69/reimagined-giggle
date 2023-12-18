2f68dd4f9e3cf22b2b4d3af73763c41aa39c9cb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:28 +0000

    bury who habit congratulate read

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
