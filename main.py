22917b0b15e73aa0fc26d9c7d0c50eacbb09e3fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:40:38 +0000

    speech wide straight place exchange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
