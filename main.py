5a0712a8ab17798bc4d20721e4be03fff3d955f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:52 +0000

    rank development open design admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
