f95be248b2f06a731a2d1ac3f8383639c7fc3e1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:37 +0000

    study fire better but address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
