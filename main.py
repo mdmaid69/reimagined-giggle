5aa736c2908b75e5bffb47b553c96f7e75cddb59
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:51 +0000

    land show spot aware learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
