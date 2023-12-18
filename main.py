df816b84fc9d6edcefa2c1ef640f691e1b55f932
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:49 +0000

    beside common it window makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
