068cf2740ed0c4226a725a6e187f3517e063be97
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:45 +0000

    recently keepaway comeback wall match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
