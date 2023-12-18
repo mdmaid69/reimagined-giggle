de984a6400d1c085717a6a27dc2e20e05ba975bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:43:40 +0000

    protect pupil float probably trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
