7ec31a252de03338e39d7429a5dcdcb93f913c14
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:24 +0000

    profit popular noise ourselves call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
