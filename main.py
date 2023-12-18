fbaea83c0427bf843c3d93c2c631602d6baa4b40
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:45 +0000

    proper thumb keepaway regular all

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
