5a654e6e2848a751a055a7b9139cb2f3ea586a64
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:03 +0000

    effect crazy blue pick public

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
