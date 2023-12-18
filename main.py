3392877dc8f6fcc012e34e3b5304d7f3289a832c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:20:35 +0000

    back secret he situation inside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import getpass
+def get_username():
+        return getpass.getuser()
