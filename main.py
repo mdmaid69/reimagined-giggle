0d7757c213b7ee233d8594b424a9bb95571f34a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:55:07 +0000

    hand direct window breakinto eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
