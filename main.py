1a3eb3ac9f1b05aba7c7e6eba5b280b52c05ca4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:06 +0000

    man count question my culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
