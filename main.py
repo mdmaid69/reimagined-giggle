10c33a9b3150d898d6d0a115b3cd08553981cb73
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:55 +0000

    anywhere wash sure thin treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
