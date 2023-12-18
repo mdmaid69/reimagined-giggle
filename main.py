e9bd08b4ce5d2fa14c927a8f5aa2cd421df5cf7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:19 +0000

    unless prevent left market tread

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
