2ea92a250ed9009636ab8070c69d1caaef32a75e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:11 +0000

    let pick environment radio stamp

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import re
+print(re.match("h.*o", "hello world"))
+import numpy as np
+print(np.array([1, 2, 3]))
