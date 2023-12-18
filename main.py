cf01db253ed8e0affacd160d5156ed7f8173abdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:46:27 +0000

    interview organization achieve whenever wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
