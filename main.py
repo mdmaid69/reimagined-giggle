cd11baee3870838ead9d6671ac7bbe483f981674
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:51 +0000

    concerning attract tone drop between

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+print("Hello, world!")
