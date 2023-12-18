305908f436f28e96c2e854a1c72ff3a82d02bca6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:18 +0000

    operation poor welcome again sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
