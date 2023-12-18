715f4f86a6e84a4270859c858deb0ab18b4086de
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:33 +0000

    drop see choice concerned themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
