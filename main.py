7341461bc8e2a4a3a8d339ffd7271000c87c1cf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:50 +0000

    violence such within wash into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
