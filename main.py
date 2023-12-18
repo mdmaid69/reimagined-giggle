a8fcc82b030ed8bc2efdede9528464e131a0b019
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:34 +0000

    thin trousers ago sometimes outside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_list(array):
+        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
