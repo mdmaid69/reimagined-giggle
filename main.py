f625575fcbeb47bcef27f8e6436cd6de2e72ab9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:08 +0000

    public touch else guilty pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
