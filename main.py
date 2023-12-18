b8fdcc6c88029df21b79985544dbcbc3413d734f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:29 +0000

    spread address action dress wide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
