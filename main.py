2ac28f2a44f7ee26fe5853d0b0a9eea16c86910e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:41 +0000

    clever long kill pure bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
