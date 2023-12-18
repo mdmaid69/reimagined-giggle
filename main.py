1bb2fa2b3a0c830488fe21cdef372d1b57b2f400
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:06 +0000

    distant hurt yet bunch responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
