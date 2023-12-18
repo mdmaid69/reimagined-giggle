a88459902a9349977a586eba0f3ea39292ee0507
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:26 +0000

    pen false perfect ourselves hello

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
