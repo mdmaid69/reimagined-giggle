5e8ec856f1134dcf07e6e551af018a32a481df68
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:42 +0000

    contrast weigh mother remark feature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
