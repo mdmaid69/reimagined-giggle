683248ad7f6621cfa0fc05e2499bee52466a61ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:07 +0000

    smooth block give beginning unable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
