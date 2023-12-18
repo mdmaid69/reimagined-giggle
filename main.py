07ee467edf0df55f4716eb4852e551644dc09738
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:46 +0000

    there few wish unlikely side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
