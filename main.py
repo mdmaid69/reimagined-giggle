b328d3439f2cf2e26fdbf88e5f5d0358b3a0b02f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:07 +0000

    present far reliable Hh imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
