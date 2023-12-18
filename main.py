1b00ae33ea5c254b6cc6e08dc41a96e37b33c060
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:37 +0000

    sink dress Bb comeover crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
