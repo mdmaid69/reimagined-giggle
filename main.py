e8f3fb39366dd7c271498290ed1a1a347c0a4081
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:38 +0000

    cap anything mean light thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
