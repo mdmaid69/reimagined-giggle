cb86ed9d0f8415d459402cc31bf66516330b81f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:40 +0000

    creature press wish conduct play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
