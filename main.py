d455427da672a72d0bf1b11064b124501bebe623
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:58 +0000

    air mine nervous unable measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
