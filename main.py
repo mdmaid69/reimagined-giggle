372c831a48995b807cd94161ff7682a289913f83
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:05 +0000

    tread gas peace after breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
