cdc50181374645ca2fb3285f6730657f29a9b5a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:08 +0000

    gradually late nervous meanwhile image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-print(sum(range(10)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
