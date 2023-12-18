6c51632221690130f2cb81c22ab8113d86f0007c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:43:24 +0000

    steam ignore regional dark swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
