a6faf97c8e72c21432a80dc2cf8002713addd7fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:04 +0000

    getaway nothing Yy real mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
