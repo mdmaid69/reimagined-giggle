e47c89e979d301ce0f4e2e1be311da21f98e8d89
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:26:16 +0000

    direction form Ii prepare available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def add_numbers(x, y):
+        return x + y
