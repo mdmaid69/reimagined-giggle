12383cbb225d0a0b7bd4309c21b87ca854cce488
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:59 +0000

    official mine makeup bring forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
