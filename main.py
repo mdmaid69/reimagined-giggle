5d0e1e70d472589900b2124b204998424de16830
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:58 +0000

    confuse fence whether past level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
