10f821d6540bba5bf4e2bd3c13744382c1405bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:30:55 +0000

    blame cover sound bush comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
