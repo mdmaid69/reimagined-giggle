9433b15202bd8ed13e60be70eb9177063ad733bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:52 +0000

    spray scare replace connection block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sys
+def print_python_version():
+        print(sys.version)
