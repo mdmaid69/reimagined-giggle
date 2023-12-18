0b0b097e96b6dfd36a7a1cd92e78053e5a8e8135
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:19 +0000

    comfort reply health spend turnup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
