ed096f2ae8a9b505e8acd48f8f8eb5b2b60712ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:44 +0000

    window frame stretch her declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
