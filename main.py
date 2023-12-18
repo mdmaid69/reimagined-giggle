0890513e22bc96b161ba997c01b3fdc5fe6dfba6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:09 +0000

    exit ride represent sensible lock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
