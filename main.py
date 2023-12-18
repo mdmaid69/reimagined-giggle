ea8f19ca37111414834b58a103b07c9ba9839e6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:59 +0000

    history mess inform smell somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_user_string():
-        return collections.UserString()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
