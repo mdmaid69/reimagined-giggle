46adecc3c64315863c4cf7c35851065583e89550
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:21 +0000

    high lean mysterious turnover spray

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
