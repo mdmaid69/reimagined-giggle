31a846d86acdd7860aec3916bcbe2ec28d754166
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:10 +0000

    handle terrible watch meaning tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
