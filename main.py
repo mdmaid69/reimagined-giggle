f7fd969ab814a0b214abd82db6305ff1da404fb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:14 +0000

    decide city breathe like today

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
