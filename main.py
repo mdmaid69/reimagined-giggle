5a284a1ba96b0d561415f76c8272c4a8982869e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:21 +0000

    replace colour just injury standard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
