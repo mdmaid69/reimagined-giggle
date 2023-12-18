085db20bc4ff573189aa8560aba397cae5de9ecd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:19 +0000

    platform care branch together flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
