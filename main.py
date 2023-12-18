329f1a8621a6553575a1fd89c7a119450bcacc0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:29 +0000

    full there pride tell goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
