ad88c009d391ed93fedff6f1add76df9b45490b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:15 +0000

    machine chat avoid kind bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
