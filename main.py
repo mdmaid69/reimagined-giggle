b7cb35f189f22bd8e27ffb29848a990f3d1ebe86
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:03 +0000

    thinkof explanation apply open door

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+print(math.pi)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
