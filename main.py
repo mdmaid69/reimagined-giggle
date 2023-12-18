67ad19eae1b12d6acd0949c7bb924f516435824e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:00:02 +0000

    tax sweet exist street proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_max(lst):
-        return max(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
