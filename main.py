31b9beae6bece8487a0a505b8ee82aca19c3f630
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:05 +0000

    uncomfortable shake course crisis afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
