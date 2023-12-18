dd16fad7ab126fccbf40bb0039c608f7cf099ad1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:23 +0000

    bite yes rush injury makeinto

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
