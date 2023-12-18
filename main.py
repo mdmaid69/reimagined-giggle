fa9ce3c0c8c3ea136025c736c0f4aa435ebd3d2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:03 +0000

    aim that long hot lot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import datetime
+print(datetime.datetime.now())
+import time
+print(time.time())
