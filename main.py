96ace53f82c25aa6d98ec53ec27e8034318378a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:49 +0000

    sorry clean sit past conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+print(random.randint(0, 100))
