503f39474138fb5e2d670152f52b6f2d30bf88b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:56 +0000

    anxious take basic lot concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
