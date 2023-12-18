387376da7db3b0283f17a0a8d8c2440198160a1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:27 +0000

    culture honest issue state Kk

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
