623a16ba4ac7ee115e0a7f36ac0f4c7fbadbd150
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:58 +0000

    closed sure spill block original

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
