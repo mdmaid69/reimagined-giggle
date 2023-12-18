f216f80a8f9f30647587a58f4b776d4fee6f442c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:01:59 +0000

    borrow question exhibition block wherever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
