43611a5a7918b4b85b2d4a904e92ac06a2d3fd2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:24 +0000

    workupto bottom complicated front short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
