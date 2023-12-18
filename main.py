9d3304b925273c3cd5abea74b19c410d72ab9d98
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:51 +0000

    remove blind also traffic means

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import tensorflow as tf
+print(tf.__version__)
