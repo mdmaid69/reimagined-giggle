82432cd630199251402489a8cfe0f7dbf6178463
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:31 +0000

    illness reliable still high tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
