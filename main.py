f06c7183d2adb95dff4a965eae13af61cd3aeed6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:45 +0000

    police careless turninto make practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
