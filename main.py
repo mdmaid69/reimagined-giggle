67e447fa8632a4d4718e87292096e3545b411d77
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:14 +0000

    safe gap I cook soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
