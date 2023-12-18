2e68fa5ee75b183f5f4691e32e018f6a934ede8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:23:18 +0000

    manner measure tradition wall patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
