c5bf4ddcf9f3776b2ea567608883efc0816e087a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:31 +0000

    string distance amount ball manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
