d924fd43dbc61dd39ab0b3f6cf28ade2dbbf8940
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:46 +0000

    however 570enjoyable high name voice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import time
+print(time.time())
