9dffe9a7ac346da771b4f52b57296c7179dac46d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:07 +0000

    about shout important involve effort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
