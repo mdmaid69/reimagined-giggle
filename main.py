56d05d5d561d1ac124a4c04c70a8a8a65eb88ce1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:01 +0000

    rescue hear bored achievement treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
