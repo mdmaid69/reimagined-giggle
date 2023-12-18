837d6ace92d55a0a7f7c0076d780d450520ec144
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:08:57 +0000

    Bb seriously zero along especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
