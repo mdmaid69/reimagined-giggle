70945eaa542d854a177e199d855c41c6d8927b43
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:18 +0000

    cruelty crime piece mean position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
