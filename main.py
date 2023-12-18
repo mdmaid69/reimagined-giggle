3e4698eae71aad00f6f9c2b2df7b855c5517b7c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:24 +0000

    declaration drop supply tough miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
