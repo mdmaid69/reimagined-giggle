238616b78b35eac72c8a4c290ef47466ce6a1377
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:04:36 +0000

    myself cruelty mine impression care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
