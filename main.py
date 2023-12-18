9f5c2e9e1a82d23e741155b3bed9c22b0f09264e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:23 +0000

    use so illness worth various

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
