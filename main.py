0b336a61e19ff6175c15a26da34cc36376373ef8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:36 +0000

    many herself correction seriously reserve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
