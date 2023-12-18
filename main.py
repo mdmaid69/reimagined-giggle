8d24a21b17ac649c4c72607007da1191827483b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:48 +0000

    touch only container that artificial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_power(work, time):
-        return work / time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
