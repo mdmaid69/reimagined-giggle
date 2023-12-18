4297dbba9f6dc58c8e4583c321ed5e70178feac8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:29 +0000

    sing sensitive dry backwards goback(to)

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
