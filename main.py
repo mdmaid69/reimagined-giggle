a959d8b7608adb38780965f3700fe6af844ee728
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:35 +0000

    test pupil whatever attach ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
