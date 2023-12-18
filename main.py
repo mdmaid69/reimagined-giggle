c298748390a318e40b175fce006c9382e4d17346
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:44 +0000

    whenever worse find big interfere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
