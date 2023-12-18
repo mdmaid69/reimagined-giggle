55685e5427cd4acea8ad79f2b1cbeaf3df60c63c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:27 +0000

    treatment alone closely getup measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
