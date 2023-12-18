4ca1c8d83d2ad9df03b919af09b692d78076805e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:54 +0000

    song consult married crisis disappointed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_str(array):
+        return str(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
