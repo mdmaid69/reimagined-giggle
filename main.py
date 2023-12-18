ee5322e38c97c309b4b1b3979a6f06127cc5d771
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:33 +0000

    suppose price convince account cinema

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
