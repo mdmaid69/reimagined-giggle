d2f26bf44a6ef5f44e2002300c39b3d3be0e551e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:54 +0000

    real honest myself previous deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def square_number(x):
+        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
