c64c59c595d6ee6938aefaa19204afde4db2e072
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:51 +0000

    explain turnoff regret relation workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_list(array):
+        return list(array)
