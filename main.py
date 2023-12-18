84f0428e5d22613273dd3c0f6602041033f9c9b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:12 +0000

    yourself weak satisfied wear surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
