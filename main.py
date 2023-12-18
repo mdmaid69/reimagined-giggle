f922df1c23d900a0d9a6860b3396db60fef3cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:37 +0000

    creature front arrangement unless abroad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_bytes_from_array(array):
+        return array.tobytes()
