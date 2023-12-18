2710340ceab4a2230bf759c09654957bd4b9bb54
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:42 +0000

    paper swing question themselves test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
