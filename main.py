9543dd672186cab9c11f98cfa6afd3be1262bf63
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:52 +0000

    interruption choice marry any ignore

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
