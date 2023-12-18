88cac0cd14b60988cc78ae8c86f05ca7b0eeca4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:32 +0000

    model trial phone enthusiastic see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
