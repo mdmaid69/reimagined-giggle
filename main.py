719879f2ae2e50b4e51882f7c6b604a38d7ff753
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:36 +0000

    fit want wipe sound bringup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
