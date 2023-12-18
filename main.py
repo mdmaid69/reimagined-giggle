3c9e02b1b90352c509c037ca4753b62107052c09
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:14 +0000

    miss steep post tax study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
