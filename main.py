4212b507a99db14512ceb4a3bdcbb148a15cd0c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:35 +0000

    whatever only machine mess 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
