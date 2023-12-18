39d0fb5100773459aca18c021b070905ac402543
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:01:04 +0000

    unpleasant forward score needle block

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
