bc7d6d85f421dd6643210f4bb7a01fbf38355a9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:09:59 +0000

    human shop juice minimum relief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def sort_list(lst):
+        return sorted(lst)
