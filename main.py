f57c58304ba7f8a3da8e8251f5c46ca36fc3630d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:44 +0000

    write duty length secret before

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
