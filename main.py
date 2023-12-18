335fde3e81de9dd646f331903af56e427bab4edc
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:46 +0000

    tape sort explanation face consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
