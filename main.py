483cf84f5e7044d315df272115f9e90922fcfb60
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:10 +0000

    stage reach shoot effort model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
