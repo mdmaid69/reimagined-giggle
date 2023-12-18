16be93e5704301b782942412aef1359d3dd70566
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:11:42 +0000

    crowd describe whether pleasure rule

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
