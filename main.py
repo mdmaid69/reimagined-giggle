b5885ed3194833bf0cf133acf6c5790fdba68bd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:40 +0000

    example eager crash keepoff tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
