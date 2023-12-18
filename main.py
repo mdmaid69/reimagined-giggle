b07483ba87ad2fa0ab9fda77d868cfb0487dc94a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:30 +0000

    fit talkinto so way collection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
