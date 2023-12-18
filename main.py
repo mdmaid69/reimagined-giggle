0eee24d3239224d86ec314c1c15f73e06232439f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:22 +0000

    dream at thanks mother problem

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
