9c49a1a391a7335f71ebd3ba0fec23d3c079e77f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:37 +0000

    branch fear backwards finish thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def reverse_string(s):
-        return s[::-1]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
