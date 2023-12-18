ba51c5ba48e4efd6a8b62e47b0f244af60098d7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:48 +0000

    phone name hand trust Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
