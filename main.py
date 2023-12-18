ee8666d8eebb3e9a7c3fe543326d8a5c50625fc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:22 +0000

    pour family silent laugh creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
