1e13fb2250aca454795db820bfdb44077176c921
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:24 +0000

    goout comeback burst goodbye class

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
