996a62503013dc0337fe7250adef76dd34517698
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:59:44 +0000

    fit perfect produce shoulder walk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
