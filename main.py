fc922e067dab9dd5d7579544de829361ae0c8e15
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:46 +0000

    hit shut fit callout surprised

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
