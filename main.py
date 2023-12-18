afe616f56ff2b323def787e1222a63b28da02e7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:15 +0000

    suddenly success mass tradition boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
