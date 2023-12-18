fc08725256396ff4c9064dbfa11f54bad3827ab0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:50:39 +0000

    book damp within mass goup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
