c9671816b5a62cafa525c6789773220bc38e3473
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:46 +0000

    breakthrough muscle although switch if

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
