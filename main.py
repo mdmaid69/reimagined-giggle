114c68b151f778e76e89a0ed989ae04805f297cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:01 +0000

    arrangement meet bush above drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
