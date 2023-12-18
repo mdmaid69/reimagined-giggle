742c3aab486facc4d8499ad5aaf84ee53306c304
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:45 +0000

    argue schedule closed Ww used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
