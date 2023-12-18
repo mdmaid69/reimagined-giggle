725325456d5b295ceccf184afc473c1ed5dcf497
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:41 +0000

    though swallow tradition power into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
