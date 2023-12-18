589317c2df77b01c9ebe9a9564df6b3766f85e32
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:04 +0000

    phone moment help much brief

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_work(force, distance):
+        return force * distance
