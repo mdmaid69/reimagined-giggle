0fbb647638d0805c8db681aa812111bbaedfea93
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:56 +0000

    goon only dangerous godown sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
