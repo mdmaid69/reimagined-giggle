6607bccd8369123f45bd392ec71ff6c105b4012e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:06 +0000

    instruction wire fear fit dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
