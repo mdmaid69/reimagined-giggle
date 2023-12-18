774fc7aebc9ff503f3fa7f51842fa6d121fe7b7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:34 +0000

    explain watch understand damp spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def count_elements(lst):
+        return len(lst)
