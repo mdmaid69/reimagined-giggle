103541dcb68284a29032e8b618ae40724e3dbe62
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:45 +0000

    minor spring clean main storm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import random
-print(random.randint(0, 100))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
