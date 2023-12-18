5ed9bcda046a19458df708582679285a62926f73
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:00 +0000

    corner up furniture practice play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
