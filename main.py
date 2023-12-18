2adc83c29b9301cefa8d6798e964e2da5b3eae12
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:13 +0000

    we string form involve them

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
