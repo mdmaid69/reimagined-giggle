a6eea58a48765a4e0170e7b633f84a3b79cdaaf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:48 +0000

    label post deliberately control visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
