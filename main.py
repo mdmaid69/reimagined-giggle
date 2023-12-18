914381a51fb4f43589dde8b59ad9d94f45345ce1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:13 +0000

    often if sometimes train steal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
