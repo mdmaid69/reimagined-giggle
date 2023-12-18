6c6d725f0a8f9e9dff0e38a273461bbf116870af
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:03 +0000

    empty distant degree fast flow

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+print("Hello, world!")
