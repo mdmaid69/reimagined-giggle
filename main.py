1e4f6af069631209b1251f99dfc6a15d95de6041
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:37 +0000

    get face commit throw lucky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
