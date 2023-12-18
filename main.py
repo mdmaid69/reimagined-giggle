2cd2bdf08b791ca2744b74c061cea733a18473c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:45:39 +0000

    objection come feed steal organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
