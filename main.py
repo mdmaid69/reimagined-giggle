3b40880fce4e84b97fade52ae00c44d4b4f14a58
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:59 +0000

    environment sweet any second affect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
