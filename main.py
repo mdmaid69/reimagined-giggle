461eb4463cd493d913f30fccd49e29a0dbe3e207
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:30:08 +0000

    minor black on money education

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
