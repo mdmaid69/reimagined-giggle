bcc7190d7079d9373fcb45a4a398a5c64995a547
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:40 +0000

    broad take environment afterwards keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
