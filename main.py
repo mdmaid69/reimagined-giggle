81eed947df9ad4bf36d33228ca68dc8c1478f9fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:32:17 +0000

    start worried suffer Nn against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
