ac0d7da6ac7d507d84cedbb9ef8cedd66b5d0163
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:37:04 +0000

    wish switch place space unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
