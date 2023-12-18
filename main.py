f0bb74fa0d0dadc94754743913602be1f4e75539
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:25 +0000

    regard compete possibility perform solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+print(math.pi)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
