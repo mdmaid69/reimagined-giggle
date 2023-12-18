99bc5265f8cd3ed60966469378a5637f1100db6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:59 +0000

    concerned purpose better sure spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
