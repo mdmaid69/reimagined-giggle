92b2bc32f6dedf8b9d35d5b41defd688cf0b2b4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:50:16 +0000

    business fine bringdown undo now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_density(mass, volume):
+        return mass / volume
