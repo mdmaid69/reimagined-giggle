89579e9ca913374785fb0810286f08a2d2d77a06
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:48 +0000

    holdup term perfect person knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
