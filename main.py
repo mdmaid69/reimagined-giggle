78b55ba4b9019eea465a48f652cab09fd5735bb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:11 +0000

    joke neither trousers holdout today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import collections
+def create_counter():
+        return collections.Counter()
