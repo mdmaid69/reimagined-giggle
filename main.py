5cf61377a9442e7ade8b5a7e5783ae8b4d5c8a19
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:43 +0000

    add slowly authority health winter

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
