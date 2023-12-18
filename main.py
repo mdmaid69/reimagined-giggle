dacaa96d19dc2c72838100af1d152dfd22011d96
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:09:50 +0000

    throw bar she wheel coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
