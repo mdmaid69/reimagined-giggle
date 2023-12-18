14cb09e252d7fba16e19f9d329924b97a952818f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:02 +0000

    port area body sting us

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
