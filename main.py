5ca0385e1852c35572d4fd7d545ae983a17b9285
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:38:03 +0000

    through round intend stranger share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
