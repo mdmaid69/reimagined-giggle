ae70058969d27a3201e1f95d32c274f1f64545bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:44 +0000

    minority check stupid birthday fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
