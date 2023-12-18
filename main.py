507c8f2a7ce5c271612873e722d94541a2f93717
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:29:28 +0000

    standard plain race soak game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
