3c9f35272f341ff35de1b77cf6b56fe04a96bcdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:29 +0000

    comeoff breakin callback bite OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
