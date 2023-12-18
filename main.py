a1fc15220c205cc7498bf678f4c78d72b60985b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:14 +0000

    above roof ought against date

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_even(n):
+        return n % 2 == 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
