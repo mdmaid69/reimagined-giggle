da2ee0d44a37af51cd8dd5bb489e287df75778fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:07 +0000

    factory dear she that small

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
