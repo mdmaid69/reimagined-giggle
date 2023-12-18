0f9a31accc912af7d23d007f3b68034168264802
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:23 +0000

    road Kk workon fasten battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
