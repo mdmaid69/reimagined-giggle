ce52194f0f4e2b0257ae6595d155f05459f99851
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:45 +0000

    obvious about set play waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def reverse_list(lst):
+        return lst[::-1]
