670c0fa53f246611ace226aa7330596ec610acea
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:15:02 +0000

    experiment inquire in bright honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
