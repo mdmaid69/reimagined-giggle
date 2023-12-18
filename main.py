5f9fff0fd4b3da8a4ad169401a24f703819a78db
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:41:33 +0000

    stupid past often touch treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
