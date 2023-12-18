9c18e619d670d6b4412107a2b012c69274760386
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:56 +0000

    prevent operate centimetre hospital towards

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
