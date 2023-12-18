90e9db851253264fe9eb67081b03b8bb5200b8a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:12 +0000

    score head wonder dangerous telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
