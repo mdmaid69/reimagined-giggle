141e3450533e26906c8a391dbd46db9e4eefbb5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:26 +0000

    record book though bringforward innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+print(os.getcwd())
