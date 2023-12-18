4bc13b41841859665b847a9bec99309a4c8ac8c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:27:09 +0000

    low development tense little condition

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def remove_duplicates(lst):
+        return list(set(lst))
