054d0c109f58dbb1b71d5a02a23b07d0ea062369
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:50 +0000

    urgent purpose none somehow print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
