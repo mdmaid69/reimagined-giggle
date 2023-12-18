997e2f97a240badef46399b2f8e78ad024312275
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:59 +0000

    instead 571enough cry criticism walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
+def sort_list(lst):
+        return sorted(lst)
