a9f689beecd77ec69069ac5af71db2af8c313647
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:44 +0000

    yours familiar public drop than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
