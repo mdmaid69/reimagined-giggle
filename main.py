e53716a7ed875322560bd22ae01a8c415e023923
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:26 +0000

    threaten simple gift forward miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
