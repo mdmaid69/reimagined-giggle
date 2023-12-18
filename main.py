db8b4bc9e8789a986342ee3450893f5c574f5d0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:43 +0000

    natural rather top later evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
