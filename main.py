a267f8f347f6d1f4b861a057e0c30fe50e58e5f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:48 +0000

    mention exploration stage any expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
