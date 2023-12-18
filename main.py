109bb5e8262c5688d83a997e4b4b645768534ed6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:58 +0000

    trade stupid present lift car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
