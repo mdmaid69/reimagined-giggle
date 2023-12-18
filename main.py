d689acbd8b7923e7a2d2fd81e4628c047b53058a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:50 +0000

    children reply pain deliberately vote

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
