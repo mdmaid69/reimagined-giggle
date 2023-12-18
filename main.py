85120946fb22630e37794260247b376e40dbfd00
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:59 +0000

    feed bye least trial better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_work(force, distance):
+        return force * distance
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
