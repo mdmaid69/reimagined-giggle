d7a37afa15eab1988550cf281df78ca5d1bfbe17
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:11 +0000

    born his task position spill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_min(numbers):
-        return min(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
