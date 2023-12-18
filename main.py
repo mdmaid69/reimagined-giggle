3b7035ff2d5fe87dc69baf2ab46d8612dbdedae1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:55 +0000

    instruction control bite except ours

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+print(sys.version)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
