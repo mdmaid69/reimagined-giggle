f6d2415d103aa38022e43b4dd15a2b87028738f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:09 +0000

    finish close hope lower substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
