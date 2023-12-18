529d6ebb03703a54161219a98d8ec73ad5a4f6e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:35 +0000

    process trust play suggest yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
