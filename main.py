e342134e0c46971d0fa05b54410c20a473f888f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:57 +0000

    common open inside Bb eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
