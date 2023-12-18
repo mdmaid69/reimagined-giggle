137a8d92b8280d0e42e28a737cb05f6443452b4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:57 +0000

    purpose distant wait shoulder turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
