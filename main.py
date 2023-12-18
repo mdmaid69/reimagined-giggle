28c741d060ca007e436d32d3a8b81b0e63429b60
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:25 +0000

    geton throw powerful breakdown begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
