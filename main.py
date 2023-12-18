ef607576d76fe9174c6da4a6186eb8d37d122e6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:50 +0000

    fence reserve recent rarely gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
