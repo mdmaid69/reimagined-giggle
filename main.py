6772969cec623f39fa1d9b85187337bf21d13b0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:26:27 +0000

    base history begin basic plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
