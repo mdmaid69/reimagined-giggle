d70d1e00ee817d62c35fbcba6b29a3432a09b7b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:13 +0000

    level fork mend fill nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
