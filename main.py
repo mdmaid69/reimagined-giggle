6d693e897f46177a82e8aa3063947a91d7fc0881
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:14 +0000

    recommend Zz worse way lift

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import random
-print(random.randint(0, 100))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
