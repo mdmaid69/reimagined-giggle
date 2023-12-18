af256a6058d3c3e782809cead944823d6ed3079b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:48 +0000

    tonight down order introduce skirt

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
