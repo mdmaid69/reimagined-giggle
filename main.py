a22b4b67ef5291318f84aacbf777c4d4b6a5c33a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:14:20 +0000

    mystery winter mouth hurry ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
