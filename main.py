918903686cf828933dc0edee658c96a9b70436cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:12 +0000

    birth increase last unlucky while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
