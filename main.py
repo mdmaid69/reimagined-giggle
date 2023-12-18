7a833714f9c11c49babf34a6095fff8e9dfcfffb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:19 +0000

    listenfor well sit remain reject

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
