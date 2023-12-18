d9ebdd6c719c230b44b15b4996c64e524f631dab
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:15:37 +0000

    male slow thumb hurry on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
