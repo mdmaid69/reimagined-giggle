6f88be796607a61a99b514e3ecadd524c6d2702a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:51 +0000

    afternoon unhappy position satisfactory Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
