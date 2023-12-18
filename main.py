ea9bbc49b81449c10ddc96cfda061a6227a700dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:37 +0000

    uniform remove warn powerful door

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
