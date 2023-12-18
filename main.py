634656c56b3ab7b7d4a869e53df5318ce8e99ddc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:17:13 +0000

    certain value meeting guard swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
