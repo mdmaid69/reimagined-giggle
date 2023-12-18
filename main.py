6ccdca1b240d92f16683cc9af2e6e0c63c0fc8e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:28 +0000

    fast accident suck mad Oo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def sort_numbers(numbers):
-        return sorted(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
