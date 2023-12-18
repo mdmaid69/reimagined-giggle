88c6128a5f3bdf6bb0096f6989e7e8de21f99512
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:23 +0000

    defeat add minimum poison opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
