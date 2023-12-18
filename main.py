5e1eb2732a65f39a9a368b4052fc2b25e6024ef3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:30 +0000

    remember so ordinary belief of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
