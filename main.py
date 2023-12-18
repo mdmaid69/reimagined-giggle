168bc7c5486e5f95b0322f070aa53f8a61e75fd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:51 +0000

    Pp matter pool understand tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def add_numbers(x, y):
-        return x + y
+def get_bytes_from_array(array):
+        return array.tobytes()
