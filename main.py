dc71e64ba08bd1f0bfea721fbfbde5b47db1b5b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:24 +0000

    stretch fill skirt bar while

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_min(lst):
+        return min(lst)
