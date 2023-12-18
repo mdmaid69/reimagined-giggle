c6bbe65d72418174354c9c8c307eae275021f87c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:11 +0000

    clean runinto original wild complain

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
