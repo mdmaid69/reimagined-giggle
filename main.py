33264412d697998d3bff174ab34dd9ef0effff4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:40 +0000

    exit narrow design bread peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def convert_to_binary(n):
+        return bin(n)
