bdea62becf9482d4bc84d8414fec21be85dbc3da
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:31 +0000

    expect peaceful runover comment disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
