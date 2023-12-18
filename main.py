8aa79b5c12abccaeaa305e47cb93f5817f89fcc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:50:05 +0000

    cruelty terrible along goback(to) keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
