4d50baa6c202cd5420637a9d046d488544aa5edd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:58 +0000

    smile money Cc suppose pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
