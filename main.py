2a7eb1c8f7bb806f809733bb86f8e15f8eb5f627
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:26:38 +0000

    persuade favour stranger along base

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
