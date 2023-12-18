a32e1fc6dbb1ec213cefb9039855210c2fff1f93
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:08 +0000

    university point brush contain wire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
