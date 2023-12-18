be1bb8daed65f8a2d4aad036fcf9ed9b091236b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:32 +0000

    team ahead equal means home

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def count_elements(lst):
+        return len(lst)
