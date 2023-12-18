001c94a3a2c8f1cfb4aefad096481ea694f8779e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:56 +0000

    conclusion but solve stay smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
