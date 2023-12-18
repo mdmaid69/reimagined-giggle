1e56abcede98c7d47658febe5be6c1a9c45917ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:26:51 +0000

    spin less the train off

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
