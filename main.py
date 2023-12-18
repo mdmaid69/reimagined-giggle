5fe00c27b740f6760ea55dbc6474b110344caf4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:08 +0000

    or seriously bear sister minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_volume(length, width, height):
+        return length * width * height
