1903d002d3b23491d87f2be96ecf8241714e9bf9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:06 +0000

    knock calloff read me tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
