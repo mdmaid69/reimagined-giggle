bf451294f2fee28545b07eaeb8dd41f9876353f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:56 +0000

    no borrow sweep row mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
