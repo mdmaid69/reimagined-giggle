5d50c5f5469f8e3423d6c39a38ec0aff84593fcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:52 +0000

    evidence negative stranger job wear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
