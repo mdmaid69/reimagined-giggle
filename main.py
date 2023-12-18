36f19fc30e7e47dcd8661f210fb0e460e3d1b780
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:14 +0000

    ear tell runout away cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
