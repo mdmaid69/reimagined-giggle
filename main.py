2774aa503f0534ded2dfe491707bf785ec6f0403
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:23 +0000

    protest dare everyone dress freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
