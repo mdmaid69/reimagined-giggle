e932b2ffaecf6adfb4bb3c2d8ffb6db8ff343567
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:14 +0000

    face chin key evidence exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
+import collections
+def create_user_dict():
+        return collections.UserDict()
