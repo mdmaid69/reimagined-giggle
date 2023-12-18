c026934f39f2a5a702e1dd289745ac005ed41ee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:40:23 +0000

    rub cinema view callat thankyou

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
