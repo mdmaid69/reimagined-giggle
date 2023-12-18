f47b903032636e960aaee6f2d4fd69254f7bcc95
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:53 +0000

    ability familiar confident wild somewhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-for i in range(5):
-        print(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
