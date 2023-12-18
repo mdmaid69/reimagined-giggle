f781b9814361650269cdc41b2513fed8b244912f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:23 +0000

    bound rely passage cry service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
