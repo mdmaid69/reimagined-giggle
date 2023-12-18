ce7b0bf127ae5e3bb026bfc3ca5bb623f9aa7d06
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:24:43 +0000

    forgive repeat suddenly blue tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
