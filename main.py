3e35bc5f7f45c6242132389963bbf2aeee08189a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:43 +0000

    arm murder clever reduce voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
