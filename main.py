726f7f63413cbaba5cfddd882c611f034b9f1a39
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:35 +0000

    hope wash talk breakthrough Nn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
