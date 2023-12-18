3bc511c584352fd77b87208138fd1e77b81ea5c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:37 +0000

    prove since satisfactory onto band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def convert_to_binary(n):
-        return bin(n)
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
