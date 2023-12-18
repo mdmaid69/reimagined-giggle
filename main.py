760ec825df5247efe60c247f8f81d8b35c489fe1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:58 +0000

    produce lead rough platform badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
