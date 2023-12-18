1e180a0a432e6cb1477f90d1c8fc4d8604f3f9dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:48 +0000

    satisfactory declaration bite post file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
