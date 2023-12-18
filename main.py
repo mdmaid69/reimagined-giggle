a8c256dbaea568f05d0f01044b5db3a73ee126ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:02 +0000

    female excellent old black preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
