96c7a51fa5256b5ee0f38079fbc16665c25e65ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:45 +0000

    cinema damage persuade gooff attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
