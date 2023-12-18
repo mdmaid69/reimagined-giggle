48fba3d356d2995387581804618e80d032b8a8c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:40 +0000

    share sleep plain affect family

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
