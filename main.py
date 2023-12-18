29ea94f790ebdd513f6e7d4f40c0868fe379b710
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:19 +0000

    correction receive someone thinkthrough job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
