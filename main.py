dbe616262ac315f77fc16c9535231232fc3d0dd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:10 +0000

    mark fit turn complete where

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.ctime()
+print([x**2 for x in range(10)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
