83840b2b09a000bab35847d454f063dae42f163e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:07:16 +0000

    description delicate stretch notice takeback

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def is_odd(n):
-        return n % 2 != 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
