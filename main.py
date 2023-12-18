a76a06f18e2b7f914db59bb225f7a365ec74a258
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:42 +0000

    whoever health treatment since lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
