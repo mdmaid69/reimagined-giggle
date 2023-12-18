1a58739ba7524eb54ef2ce42ab9981beac8a1294
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:20 +0000

    boil still allow feature anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
