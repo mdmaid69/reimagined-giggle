5d4f7827442a6341c27815c469d01e21901c144e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:27 +0000

    involve estimate impossible body determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_list(array):
+        return array.tolist()
