7e52f38ded1deec98f8123aca1b9bfb54bd35711
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:34 +0000

    tone too importance whom them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
