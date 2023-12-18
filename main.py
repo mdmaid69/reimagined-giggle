d963c1757322f495c99bc1fabc1236df81eac04d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:18 +0000

    balance nothing knowledge arrangement talkinto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
