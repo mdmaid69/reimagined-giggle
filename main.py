40b0cc9622c1cf201959839f062108f4bba6fddc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:30 +0000

    aim abroad interrupt level rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
