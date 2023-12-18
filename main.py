6aa4c895577f6a0542605bdbd0431dbe21d47379
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:59 +0000

    imagination worst carry hope once

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
