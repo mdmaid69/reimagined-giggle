e3c06a4b5920e3be840f9d3b37dbe4e04b3741f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:08 +0000

    child old we common event

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
