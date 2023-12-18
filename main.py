2b1c703b25ee88a68e039df6c4c9b563d473a513
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:39:40 +0000

    split late apply himself turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
