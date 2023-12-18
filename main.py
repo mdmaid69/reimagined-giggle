d304ce9762f21661e45114f92af3989fead389e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:27 +0000

    on change feed closely apart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
