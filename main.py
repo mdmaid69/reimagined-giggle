0195de8c2f3f34f74c261d2a688729f4782d9a70
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:25 +0000

    push taste special worth both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
