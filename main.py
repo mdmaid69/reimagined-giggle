d529741075780dcd7ab57f2978390bdb9eb609ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:48 +0000

    once copy warn slide touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
