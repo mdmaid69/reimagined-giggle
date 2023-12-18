66e6284befee9c9000ea997526131cdd23ed986e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:43 +0000

    cheat crash pen light term

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_speed(distance, time):
+        return distance / time
