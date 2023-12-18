c859e3373c65491f19df4fb47ecef7c53ccaf4f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:22 +0000

    bear Ww national Gg live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
