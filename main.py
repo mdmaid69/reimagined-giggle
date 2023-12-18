342c8982e6141f8b7581ae1f2457b553bb9b3a71
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:32:56 +0000

    steer slightly name nerve sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
