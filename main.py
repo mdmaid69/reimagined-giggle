6f42bbf47e773ec87186ae69c46ef3a620700088
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:09 +0000

    father guard like 568energy combine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_power(work, time):
+        return work / time
