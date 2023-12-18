8d7b24ba97aadb44690c4b8add093d7a46b9a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:35:12 +0000

    art example calm Cc include

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
