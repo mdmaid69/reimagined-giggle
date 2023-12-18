3ac8192e8c114aed1eb2afbe9efb4df479826cf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:40 +0000

    delight just unusual home swimming

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
