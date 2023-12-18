871fde2b37a338f26a07a48c9cfc8bb3d1020684
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:31:28 +0000

    no service thick embarrassed draw

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
