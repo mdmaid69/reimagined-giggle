d3360f6944d9cdaafce6198e1e4eb7028c6c4e21
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:32 +0000

    learn embarrassed early pretty wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def add_numbers(x, y):
-        return x + y
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
