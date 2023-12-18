03eea27b7aa6720f79a00f3d83cc190b5278336a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:57 +0000

    cheek field trouble change unhappy

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
