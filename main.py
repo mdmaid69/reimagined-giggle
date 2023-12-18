62d9820e4962962a86403771f4969b84e98d45e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:51 +0000

    battery evidence argue concentrate deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
