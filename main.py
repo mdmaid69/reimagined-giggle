b757bf88c4699e0f9238161efdde5c2753e09b01
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:48 +0000

    improve before ring eye thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
