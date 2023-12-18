26f5fca7d46988b72b6f7318895ce9aad254c0bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:17:45 +0000

    wheel challenge complete release excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_max(numbers):
-        return max(numbers)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
