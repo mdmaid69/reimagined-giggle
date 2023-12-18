8b890fb7ebced811b0dd6bdcbccf95c1fa2d62c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:38:22 +0000

    smile backwards glass practical comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
