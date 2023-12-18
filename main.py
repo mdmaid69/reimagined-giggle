de013d5d5797034ac23990af18516d9fe0e10eec
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:37:11 +0000

    lesson clean bringdown unkind silent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
