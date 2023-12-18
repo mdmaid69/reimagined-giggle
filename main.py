3d513c7b42227cb79d4b4aaf7bdca8a57c59ea46
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:16 +0000

    disturbing open tight throughout comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
