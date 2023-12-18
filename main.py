d785d51a719795d9602d665fdfd4b20fd0c3ebe9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:25 +0000

    small check wall effect comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
