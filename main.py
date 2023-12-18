d656154ec75378ba1ab7e6ee2451a91fe3f276f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:20:58 +0000

    free crisis range progress opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 def convert_to_binary(n):
         return bin(n)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
