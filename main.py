84886c7902cc376d689b8fdfb764d87c3f2926ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:53 +0000

    wide special thankyou late evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
