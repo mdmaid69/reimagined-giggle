7170eb5f6dc227d78cc544a1974a06e7af0eb081
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:44 +0000

    risk lonely war direction children

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
