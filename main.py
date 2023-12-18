cc58c1012a1f945bdaebc3e8383e2b55172a76ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:37 +0000

    freedom tend high breakinto from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
