3311e6cf3ee5ddd375dfd08f9465bdba54332d41
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:25 +0000

    laugh sea story destroy consult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
