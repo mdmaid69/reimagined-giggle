177c5351268bbc948a9e80ebe3221c9a73b37189
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:47:02 +0000

    through bear whistle dream imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
