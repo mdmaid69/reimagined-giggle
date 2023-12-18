6c834faf5237e9a13842ee6df2d9e32ffa355a3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:04:55 +0000

    fortunately standard weak various side

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-print(math.pi)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def square_number(x):
+        return x**2
