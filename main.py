ec4d63a75c24a3d438d96405bb811cb940a30b71
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:41 +0000

    upstairs holdup autumn shall water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
