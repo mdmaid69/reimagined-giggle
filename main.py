ff491a55a5e43ef8898972d2f9e37e023c06781a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:45 +0000

    match law note mother concerned

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_list(array):
+        return list(array)
