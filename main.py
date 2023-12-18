05fe1c5a2e241450f5361a5259cdd02358607825
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:53 +0000

    can Ee capital listen smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
