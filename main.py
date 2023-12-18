ae5f0c7582c693de30b91ad100103be12c3f8561
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:18 +0000

    lot free pretty male sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
