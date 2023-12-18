cf2123bbeeb78d1fb97f76bbdc40741e19e8a3f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:11 +0000

    water steam pleasant almost wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
