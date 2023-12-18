ae4449d31521713d26336145720ae29c7fb1b278
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:54:23 +0000

    replace become cinema average comeback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
