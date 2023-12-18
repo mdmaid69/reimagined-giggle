14ba58260be6e4225601217395c03f1d8190ad78
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:54:29 +0000

    total price anywhere various idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import numpy as np
-print(np.array([1, 2, 3]))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import tensorflow as tf
+print(tf.__version__)
