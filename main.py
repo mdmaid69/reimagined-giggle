dc269da90e4302b4b0d76de09ee0503d3795554d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:01 +0000

    area challenge practical believe weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def subtract_numbers(x, y):
-        return x - y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
