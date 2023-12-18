476708fbe88e8ca1063cf8b2d5c834c5809c2a8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:00 +0000

    regard combination accommodation breakinto desperate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
