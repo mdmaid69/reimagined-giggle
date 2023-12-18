05db05a9a7fe02157c051e5fcb2fc24f60151993
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:48 +0000

    thank car getaway nose live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
