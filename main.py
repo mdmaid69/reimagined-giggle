c0d43a31ad704fe4a4b4dd6c1f79f36ee92de5b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:50 +0000

    fetch repeat often block stiff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
