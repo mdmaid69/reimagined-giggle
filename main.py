b2f1f160ee0e6f957e65f6f0f3263325cdf52873
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:24 +0000

    confidence disapprove once compare true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def log_message(message):
+        logging.info(message)
