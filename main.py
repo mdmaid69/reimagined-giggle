420f838d867dac08892bee8c825446f2b9561e86
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:35 +0000

    classroom bother know ocean wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
