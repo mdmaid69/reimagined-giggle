ec6ec81a4aca229f3eee6429d579205560d665d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:09 +0000

    take congratulations onto failure big

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def is_prime(n):
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
