21e990ff3eea2d1cb7bcfc44f7d7b4e9dcbf2e73
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:35:46 +0000

    seat issue right control workupto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
+def count_elements(lst):
         return len(lst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
