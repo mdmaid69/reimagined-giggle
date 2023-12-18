58bd1645e043d09c9d02fa98686853514fcf1892
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:56 +0000

    precise forward trust broken month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
