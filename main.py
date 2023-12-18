e8f5786425d529df2c8761820af9c6d98396ae5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:13 +0000

    support go instrument either band

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+import array
+def clear_array(array):
+        array *= 0
+name = "Python"
+print("Hello,", name)
