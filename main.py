7aa42108d4a4a3b184350f03b8fc1df416410dc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:39 +0000

    bet still cheque nearly match

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+for i in range(10): print(i)
+def calculate_area_rectangle(l, w):
+        return l * w
