03bd12cefbe848b4b0563d89532d55b430912dc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:02 +0000

    regard eye little willing complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
