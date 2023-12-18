1fbbc76a0c62b2452d991682b00d4f7f353b253b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:17 +0000

    sort genuine case sort 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def split_path(path):
+        return os.path.split(path)
