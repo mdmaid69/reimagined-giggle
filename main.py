0cf270cb7fd3613d0f240c4baa7abea8648c40f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:43 +0000

    neither birth forward meanwhile invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
