4d75a8ee59c388838c87c449b4009ffdf49a50dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:32:00 +0000

    wall produce without hard regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
