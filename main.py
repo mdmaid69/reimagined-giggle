7373731deaf4aa0e38b1f486e5b17c22fde559d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:06:02 +0000

    lock stupid string information pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def multiply_numbers(x, y):
+        return x * y
