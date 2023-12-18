c713cc90b1a97b973377192f4eb3e99c570eab3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:56 +0000

    explanation clearly control search burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
