65dc18f8bda2c4710afa2721e1f08d5dbd36153c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:35 +0000

    accidental mark exit future smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
