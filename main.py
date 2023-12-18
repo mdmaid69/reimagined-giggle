519ba84c8eee0c903de3bbba848bcf1c45bad55e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:08:49 +0000

    back like eye slightly harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
