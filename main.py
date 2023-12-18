1c980e3e5f06701909363aae7092c194b97adba8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:15 +0000

    recommend Dd attach avoid prepare

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+print(sum(range(10)))
