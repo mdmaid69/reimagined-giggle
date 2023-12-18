19de9c8c75304b81998d2bb7d3d7dc10f20f2812
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:31 +0000

    might anybody measure cost suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
