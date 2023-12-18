33470f77156e83f0edd307df6fca918fca32c3c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:00:42 +0000

    everyone feature strike load able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
