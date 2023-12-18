082e9f666bf5667d9b95e19078fd76d2581e07b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:33 +0000

    receive ready stage thankyou put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
