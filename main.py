023ee75ee113be85cad1496c4137dbf89a227356
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:26 +0000

    surprised train wrong here wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
