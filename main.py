ad6df35f812751221b13b690ea17c88665abe34a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:38:13 +0000

    quiet face action love home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_volume(length, width, height):
-        return length * width * height
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
