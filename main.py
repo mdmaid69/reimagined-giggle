36bb8dfb1a6750b67d70f1be1450566d7535dd15
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:03 +0000

    window relax set whole shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
