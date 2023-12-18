90f01197fafa63e1eebbf3f5fd4dc18840dbbc9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:04 +0000

    lay see talkinto react sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
