19b39996ee987dbc05715820e94a0f4d0d59b55b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:44 +0000

    trip preparation average final favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
