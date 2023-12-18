8af9c2f972aa584a154dca16124d9ea001d5e359
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:40 +0000

    official mean community under takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
