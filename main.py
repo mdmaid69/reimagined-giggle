10757914096c89087544e6200b61def7fd162c17
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:35:23 +0000

    behave apparently surprise of war

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_repr(array):
+        return repr(array)
