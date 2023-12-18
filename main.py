14f54733c19ad84dac454db7d98bb5e318c335f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:27 +0000

    curious of wire flood pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
