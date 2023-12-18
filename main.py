e0f208aee1115e3452d65a42d7e0ce9fbaedca89
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:34 +0000

    move promise spare among inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
