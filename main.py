709ba81e62a734a29d4bfc9d83ae0bcf576966d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:41:41 +0000

    thorough takeoff trip callback dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import os
-print(os.getcwd())
+def remove_directory(path):
+        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
