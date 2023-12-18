01619ac14cbe7005d7c5db289f717879b621e72b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:24 +0000

    reasonable bank practical land marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
