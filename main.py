0fdbbd87308d823416d7357a6002b0a5dabbb8fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:22 +0000

    sound successful neither question everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
