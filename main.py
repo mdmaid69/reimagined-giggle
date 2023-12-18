d288090a2feb3a3b809ed45e3c3b16dbf31b23cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:36 +0000

    nonsense know purpose tonight must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
