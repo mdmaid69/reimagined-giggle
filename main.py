12fab75dc7f4dd9513cb66826589f1681adffeb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:07 +0000

    together example near arrival scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
