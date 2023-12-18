d93525482f29dc2e3fb8fd4245223e0020390ea7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:19 +0000

    introduce production expression sort apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
