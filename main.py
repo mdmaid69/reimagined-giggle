27cd49eb1cf555742d97b0ffc2f2481391a56a53
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:09 +0000

    rid day responsible kind apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
