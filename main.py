85b3c2872fe8636f49806596554061465637ae66
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:44 +0000

    Cc talkoutof excuse perform rule

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
