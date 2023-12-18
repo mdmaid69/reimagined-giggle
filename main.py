7f57ec9b52dbc4bb3a1eacdd4c044304656fc81e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:43 +0000

    passage period arm score sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
