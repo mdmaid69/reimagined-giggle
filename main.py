9dac296deb025ae9645ea6c513d65856bcdd8511
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:30 +0000

    extra top switch clear spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
