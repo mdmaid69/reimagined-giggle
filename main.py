cca4de56a97962eea15749a92444e845e8d0248a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:19 +0000

    behave table frighten laugh height

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+for i in range(10): print(i)
