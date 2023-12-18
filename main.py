506dae1223828981bcb416e7df7f175ff8e22cfc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:45 +0000

    conclusion charge introduce damage on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def reverse_string(s):
-        return s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
