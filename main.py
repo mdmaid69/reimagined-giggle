6fa5a076b4933f8f7bdb6ae4867d61cba891a88d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:15:49 +0000

    generous slide listen none ticket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
