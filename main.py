de6baf3995cb8821327d975722a9367fb5530a9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:22 +0000

    comeout takeaway way regular hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
