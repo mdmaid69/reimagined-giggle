193b5e1d0f42fa48ec73d2ad6bebe8788ce2c7ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:31 +0000

    leave trip important direct building

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
