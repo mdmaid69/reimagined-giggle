b90fc9f33abfa321ccf17612e069356b1beb85bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:19 +0000

    apparently step really ours undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
