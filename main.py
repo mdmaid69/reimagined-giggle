a9fac5afb1ecea5074de40eb7dc3aa5a8fc88866
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:29 +0000

    acceptable coffee notice production shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-print("Hello, world!")
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
