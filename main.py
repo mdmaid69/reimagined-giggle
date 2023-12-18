cc24cf2c1aafa4d9bd17bc4a41dfbaed339f6f81
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:48 +0000

    lonely strange dick round negative

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
