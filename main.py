ca2f4ddb18e286b2dce912f07d398db9ddb2142d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:33:57 +0000

    off peace priority snow active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
