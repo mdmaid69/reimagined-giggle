d9f436dd993348173fa5e2c010cdfed1e59e8007
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:36 +0000

    trick instrument part wake loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
