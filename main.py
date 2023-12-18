77b5617d4cfac5d6e88db81cc0288bf85319add2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:26 +0000

    influence bet future reliable hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
