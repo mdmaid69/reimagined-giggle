1f056fb527a68010f8db86d009ecd216fe336152
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:50:39 +0000

    dare zero basic cheap run

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_max(numbers):
+        return max(numbers)
