f073fa7a06a9d88df86f7ef136907c5af0b6e9cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:33:15 +0000

    protest town list argument insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print(sum(range(10)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
