b707d8261ea7c33cf516c80c8bbd54094d31e43f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:14 +0000

    copy least unfair tomorrow such

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
