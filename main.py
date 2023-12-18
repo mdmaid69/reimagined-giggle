ac62629c659070a7cbd453a0faafe4ad3e8120ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:10:12 +0000

    567enemy while identify respect shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
