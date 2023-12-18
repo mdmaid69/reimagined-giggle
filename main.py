d661b3b89eeefff7ba4fe160719fd5a99d39c78f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:48 +0000

    learn strange clever about heat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
