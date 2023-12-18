73383c14d8df5923bafa5f2d986bd473e97761d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:10 +0000

    now shut few forever force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
