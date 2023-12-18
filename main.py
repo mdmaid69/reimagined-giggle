3b841efd84ba5adcbd3259a6021e81aa5a0063d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:42 +0000

    dinner after deliberately temper OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
