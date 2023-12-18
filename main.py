74c2b13a8d4e8b1e7aad6697a6b8d6f150c734de
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:27:36 +0000

    have among lot brown myself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
