873ce8f0dc2e3812b38b486db5f6489bf226b37c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:01 +0000

    probably hold entry below over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
