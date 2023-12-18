d86e909287bf9554855342c4299fa63db62edeb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:41:56 +0000

    lucky believe ear this grand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
