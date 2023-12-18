423bb08e5d1fad3e6751c72ca74a49fb0cc14aaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:55:07 +0000

    Qq fair operation not report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
