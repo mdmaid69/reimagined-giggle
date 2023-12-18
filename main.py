f5608cb0798c7f0ed36a00af8d7585a900146f48
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:08 +0000

    regional edge big sharp turnaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
