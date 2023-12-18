75043fa250df0271ad374184a305b3786b1d09e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:50 +0000

    praise spin plate confidence behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
