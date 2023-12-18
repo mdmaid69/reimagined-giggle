290f8098efae0b577008596000b3ebdb990b9e9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:22 +0000

    summer with regret quiet ahead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
