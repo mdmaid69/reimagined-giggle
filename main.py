34bfd6a034dede26774410b037a645011b5a1402
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:59 +0000

    have not pull lay neck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-print(math.pi)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
