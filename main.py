79bbd2f3f3be85d7ab7005c437a38bef00d80cd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:54 +0000

    survive shout instruction compete school

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
