b2a331a425485c8c1442ab8ca8a43d1926ff1982
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:00 +0000

    set during suddenly popular tired

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
