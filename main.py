d06992f05da9aa2482d033d21500a648dd238dff
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:17 +0000

    performance same release dead better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
