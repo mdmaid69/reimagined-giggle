551fdd84a3330482ee7c66a770f7dc862d15f8dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:34 +0000

    strong subject confidence pause string

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
