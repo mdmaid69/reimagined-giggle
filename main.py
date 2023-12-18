fec017e2d1a71a27804369c1e4181093ad25855b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:08 +0000

    test bottom its book bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
