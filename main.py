afed5fc34809b0712358762c819cce117bed665f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:37 +0000

    by goout it man mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
