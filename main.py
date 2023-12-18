4a4bab40e763ef0fd83f2fa9a9abbd6c5da6551d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:22 +0000

    deaf imagination plate part flavour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
