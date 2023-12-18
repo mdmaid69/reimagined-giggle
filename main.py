6d79054e6bee7c8d6e66e789c01a79a2ed0dd22b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:19 +0000

    determined rescue live how find

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
