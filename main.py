babe3434b183229c1cfa4bb647dd14965b19c44a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:16 +0000

    ear letter callback laugh development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
