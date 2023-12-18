4a9cfa1827fffc1793a5c89d58c09f3c4e58b7a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:18:29 +0000

    murder beginning scream store main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
