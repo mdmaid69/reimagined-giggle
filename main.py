4713feb584b46c64860db5dcecfd6111addd3f67
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:46 +0000

    because Qq likely collect all

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
