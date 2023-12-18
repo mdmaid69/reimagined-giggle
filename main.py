944b9214aa8575c367817266e149e1ae0ef46532
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:49 +0000

    winter middle quantity everything expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
