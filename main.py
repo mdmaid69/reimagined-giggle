0973e97ed5ffa60de274640d47a4f7d726bb2bfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:29 +0000

    blow suck month degree travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
