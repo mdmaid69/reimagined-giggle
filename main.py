339ecc07d351a33c21628afbd8499fc7a1f1df50
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:00:55 +0000

    scream original chat produce square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
