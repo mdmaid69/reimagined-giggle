5ed0aba7e003075181eca73292890ec55d3790cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:33 +0000

    moon exchange cut touch busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
