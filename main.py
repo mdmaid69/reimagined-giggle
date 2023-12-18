36b5c3d17d9208ee044afc2568039efe842ecb7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:51 +0000

    breakdown least walk feature safe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def find_max(numbers):
-        return max(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
