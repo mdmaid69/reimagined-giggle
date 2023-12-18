557defd50e715a547d695017e72d7c4d95bdfa69
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:26 +0000

    Bb deliberately miss fortunately 571enough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
