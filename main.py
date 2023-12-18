a911dc30420cc76ec6e2154fdc151fe6094e03ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:17 +0000

    our could belong reason doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
