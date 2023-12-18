8448b7729a09fd8c10071da7c15063057aab5c7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:50 +0000

    insist frighten while bringback boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
