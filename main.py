945bf10678de2dacade4e3e323122d761208c54d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:34 +0000

    fix or under interesting 566end

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def subtract_numbers(x, y):
+        return x - y
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
