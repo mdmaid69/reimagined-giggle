e71a9dbc0ffcff2a4299a4d5e6001fd69dc32a57
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:45 +0000

    later lock maintain suit rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
