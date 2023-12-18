5392179bd22a465b9b1edcce49dbb6bd965bbfc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:19:03 +0000

    his quantity good practical flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
