eec2114e81aceae1112503fc76087226c849a3f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:07:50 +0000

    bread normal mistake refuse familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_repr(array):
+        return repr(array)
