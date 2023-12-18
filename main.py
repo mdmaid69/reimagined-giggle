a1f5406eb3c9509c8971ec2be73071d14fe32006
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:16 +0000

    ask shake male deliberate stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
