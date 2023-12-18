4f5ce317003b9e64dd1b9f2567e7cbb278a545e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:30 +0000

    niggaacademy fortunately market cross permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
