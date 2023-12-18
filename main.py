dce71f1240528919476fe9db619a22758b0850ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:41:25 +0000

    pick dish stamp anybody Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
