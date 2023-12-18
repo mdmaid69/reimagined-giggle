3824a3be4dadc9b8d506d81fc332cb8f6e30f604
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:20 +0000

    runaway bottom hurry funny fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-print(math.pi)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
