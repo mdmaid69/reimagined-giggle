592e2d4d8bb783b2a48b68f8e25787a39dca37a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:28 +0000

    street region long himself comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
