9cd26250d268d56c22a8d06b3dbffe891bd0af05
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:43 +0000

    by together unpleasant promise could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
