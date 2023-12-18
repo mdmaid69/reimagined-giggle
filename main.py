293f687abfbd4fb8e6cf8db22e47613554664a52
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:52 +0000

    job rule what burst personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
