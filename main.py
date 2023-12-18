72ac34ff030b479b9e8981f6ef89bc212a969161
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:07 +0000

    lesson degree autumn platform wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
