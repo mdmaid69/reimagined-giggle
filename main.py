347d4bd2edbafb62d39dcde57b188273c0e099d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:08 +0000

    pride complicated takeback attempt himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
