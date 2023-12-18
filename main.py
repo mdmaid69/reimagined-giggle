4ebaf1f0f3a20a4c4d9708194e0d7fcc53bd3baa
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:45 +0000

    present cruelty opponent uncertain miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import sys
+  def get_python_version():
+        return sys.version
