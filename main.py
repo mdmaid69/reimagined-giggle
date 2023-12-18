4ab60a6dd75c8488ce58dae630a6d4dfd40afcbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:16:58 +0000

    finger standard clean extraordinary fork

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
