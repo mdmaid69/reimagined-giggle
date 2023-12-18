dce03a99e87ef16df156ca0fc78dec62a3f48f53
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:22:59 +0000

    possible notice draw wise upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
