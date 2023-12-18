59b4b34e7c52b8e4ce970242cbc77661f767b19b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:57 +0000

    pause glass post contact sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
