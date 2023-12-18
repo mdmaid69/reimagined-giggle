3975264ee4e7ee1200f95bcc6872b49fe9ca30ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:51 +0000

    home comment everything street rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
