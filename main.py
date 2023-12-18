5ba8ade370a9871a843b2eed2941b9bcbd11e777
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:25:39 +0000

    imagination meaning so cry exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_min(numbers):
+        return min(numbers)
