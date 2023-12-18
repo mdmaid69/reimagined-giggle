952446f3344aa573df0903066ac68f4a1e6226c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:01 +0000

    tell band birthday rise disapprove

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
