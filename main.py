8d5d1961a3c59117feec28f0ec56d0e4b15828ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:50:22 +0000

    refuse bottom wash position squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
