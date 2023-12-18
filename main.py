c875cd5546f58d1eebdf098d27c4a3275429966b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:53 +0000

    eager excited squash completely anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def create_queue():
-        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
