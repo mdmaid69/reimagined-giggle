d860d2b33e48aba1986abdefc741ce4cecdc706a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:43 +0000

    congratulate fine low beyond convincing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
