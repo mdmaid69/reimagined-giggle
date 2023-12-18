ad631850f2c1ce7c05600395916064b9fd824b18
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:14 +0000

    profit disturbing gate upwards instrument

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-print(sum(range(10)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
