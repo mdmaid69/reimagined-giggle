1052ebda3ab1779a920bb0be358e66ace0af6d8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:06 +0000

    nonsense by motion detail share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
