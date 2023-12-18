4449277440987fcf788438434bad6a4d29fe2977
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:29:31 +0000

    themselves bin for record typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
