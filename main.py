fbecde9829a076a3b475a815204851c6c910d4e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:40 +0000

    culture way radio large takeover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
