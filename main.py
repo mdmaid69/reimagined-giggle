1d127645f9009d33f1690eb54e083d5c974d1094
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:18:40 +0000

    none responsibility proper trade splash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
