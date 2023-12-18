e3396e3607c757c9ca2b96e737f962e22f3361b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:33 +0000

    tidy tell buy down pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
