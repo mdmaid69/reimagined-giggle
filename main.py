7143cfe33f1904d4632a5a9dc195e7342a36869f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:18:07 +0000

    children dare declare accordingto valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
