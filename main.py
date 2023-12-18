3eeb3a06aea05a4c4a84f5a1f8b84857bc1e0fb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:33 +0000

    age trouble moment yes drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 def calculate_density(mass, volume):
         return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
