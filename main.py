474e132c14f463f50b68bec55502f9530ecb8d78
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:21:49 +0000

    improvement several now attitude rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
