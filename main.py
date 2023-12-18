559eacedc5980050e6b19eed52661316a1cbacc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:07 +0000

    hear under young plain high

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def split_path(path):
+        return os.path.split(path)
