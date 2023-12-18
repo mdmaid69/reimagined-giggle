b70f1b6abef6286f03c2aa540be4deab7ac7cf94
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:58 +0000

    record strike bringback print unit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
