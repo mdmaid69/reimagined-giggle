6b07843326db3bb9a443424cb4c8f86c916f9411
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:38:23 +0000

    autumn centre fall thinkof develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
