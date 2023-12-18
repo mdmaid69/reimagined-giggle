cb105f42f874ff4b7328dec4e8efb9d7b12fee72
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:56 +0000

    straight find free makeup well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
