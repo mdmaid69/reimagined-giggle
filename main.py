1981ff67af463660383fcc9b68c47c2e9fd79b4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:27 +0000

    interview heavy human spot battery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
