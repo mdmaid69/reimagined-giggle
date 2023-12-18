ba93b111055dae1f48a1eccd370329537958459b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:15 +0000

    inform different they plastic decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
