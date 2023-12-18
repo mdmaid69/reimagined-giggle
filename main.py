4ed8e8ed2b4bb337ff613d95e31c8390954fe174
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:34 +0000

    goout hold suck uncertain criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
