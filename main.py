ac2eb0c2777b9f84853dbf59000467c03b85af88
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:20:55 +0000

    create that bit trust deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_int(array):
+        return int(array[0])
