99d9f6be80b4e14493764a1143a5c3289278c145
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:32 +0000

    regularly cruelty full exam joke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
