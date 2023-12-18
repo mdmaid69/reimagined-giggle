05958ee72b28597b3b42624346dd76061312f6a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:33 +0000

    suffer speech letter silent worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
