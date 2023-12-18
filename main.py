b8823a100607c746cf0f8d2bc1d8ac84d313b993
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:47 +0000

    worry absolutely die afternoon possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
