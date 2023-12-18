ea8c3348c17748c44bebe775191159842d5c7e57
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:26 +0000

    contact kill hope let callback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
