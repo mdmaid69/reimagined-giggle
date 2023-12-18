57ee1b724980499ec1ec9c2714e6a6680c6cd38c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:09 +0000

    because love return cold important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def reverse_string(s):
+        return s[::-1]
