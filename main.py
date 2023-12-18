482b99ce7bf6825ea449cab9a7cfceac2ca9aece
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:07 +0000

    fault funny production scene course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
