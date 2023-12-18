dc3857ec090a03ef390ecfc2c301df2c87638229
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:52 +0000

    just ability breath considering Uu

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_density(mass, volume):
+        return mass / volume
