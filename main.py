d029f097b80bbaeef2687242f568d2110277402c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:40 +0000

    window play she reply used

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
