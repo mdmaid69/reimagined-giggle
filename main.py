eaf0309c248076004b5299383b6a9c0185f0b846
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:14 +0000

    hit getup winter search majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
