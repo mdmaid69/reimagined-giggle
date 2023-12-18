56fd025ea902a0377941714207d0f0c18b932b14
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:50:58 +0000

    inform giveout hers loose belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_power(work, time):
-        return work / time
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
