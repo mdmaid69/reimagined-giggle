ca73c00e64abb65ff98878dacfbf57cf965ed460
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:19 +0000

    how year design bet show

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+print(sum(range(10)))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
