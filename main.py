72d8c2622e84c9fce34ce76664bb4489aa3e6db7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:19 +0000

    turnaway inform tonight suffer interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
