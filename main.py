d066d4bcc0aec535c461699ecb363b70ad2757d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:24:05 +0000

    connected blame wipe pencil Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def find_max(numbers):
-        return max(numbers)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
