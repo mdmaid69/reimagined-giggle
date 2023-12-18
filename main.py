933588de92c3c1c2330a9cbee03fee847052651c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:59 +0000

    copy sign care law guess

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
