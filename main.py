775039393a6e957f8272f049cbc44944ce2eecd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:00 +0000

    miss trap both glad them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def count_elements(lst):
+        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
