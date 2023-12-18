5bc9aa3fd7a71cd4b113728961a7e36f5b3688ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:52 +0000

    rough giveback woman in bread

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
