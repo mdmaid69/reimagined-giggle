7c3e22fdad27b8a3ea0d539d4ae43b1d34206bde
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:28 +0000

    period brown right settle around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
