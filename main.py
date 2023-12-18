31ba9fde91c4389ead72cbc87c8b50ac6723e47a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:05 +0000

    lesson minimum damage marry either

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
