ae191fe170e759c14fbcd866393c93b43441bb22
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:57 +0000

    brown arm until he here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
