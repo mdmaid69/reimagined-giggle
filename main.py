8592362e243782427784d7878b7950b6d2d06d0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:31:03 +0000

    repeat world simple accidentally guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
