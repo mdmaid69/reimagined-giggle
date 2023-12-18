b45b141fbbac4c518c39ec234c259bf1b1b1af01
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:54 +0000

    across invitation away pencil need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_length(array):
+        return len(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
