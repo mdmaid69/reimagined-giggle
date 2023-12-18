765dfa9dd59c31d27d5bf350fbb1675295fa0eff
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:57 +0000

    beyond eat copy she emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
