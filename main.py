4236d82457b7adef17ed1baefa2e556a8cf70ba6
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:34 +0000

    false step blame nature card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
