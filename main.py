9646910800fd53d2d012dd6052c3c58fc3e3c3c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:44:21 +0000

    fall list join practise experience

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def is_even(n):
-        return n % 2 == 0
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
