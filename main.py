d9b998f687ca8e9d0cd2f9819f70af8a8847ab3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:20 +0000

    own allow immediately swimming talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
