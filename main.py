2a6d0203a4481854af93e2f417e06470bed49ecd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:26 +0000

    storm arrival drop calloff interview

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
