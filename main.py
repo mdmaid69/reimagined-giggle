1ca1e3c861e53b2fb332ff2dd39831a2e17d462d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:34 +0000

    lower trust remove another regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
