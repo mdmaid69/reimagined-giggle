e400af9162705967cece5ee1837d2a28d999197b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:21:42 +0000

    manner pride children under house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
