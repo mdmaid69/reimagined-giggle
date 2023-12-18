7d914aac80eb9fbffcceadb621a9be95d9c1bb5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:23 +0000

    wall eager wind traffic absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
