68e5ad9cec47e2a902158d0c0d8b8c751b56c186
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:34 +0000

    bit sit object calloff promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
