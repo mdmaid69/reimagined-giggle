1e76632a60190f46b267ea48ce3b5d486d5f1fdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:13 +0000

    important contact smell order smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
