989d6663b3d167c3ef393104beb9cabe6e924931
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:11 +0000

    let interview maybe proud hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
