4434eb3f76a7c6d794ee934b068950f5d6edcef5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:52 +0000

    crisis comeout concentration hear contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
