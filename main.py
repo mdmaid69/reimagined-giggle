05f8b24cf9808dcb2993655b4b47402b32082bf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:09 +0000

    damage pretty related Hh minimum

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
