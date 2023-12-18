7ea426450a31516ca7fc3932fdd84e57dfbc712e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:04 +0000

    show sure much cheat accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
 import math
 def calculate_modulus(x, y):
         return math.fmod(x, y)
+  def remove_duplicates(lst):
+        return list(set(lst))
