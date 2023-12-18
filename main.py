849c8020be724352537292adbae5ebc520d43299
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:54 +0000

    mind too fortunately estimate later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-print(os.getcwd())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_speed(distance, time):
+        return distance / time
