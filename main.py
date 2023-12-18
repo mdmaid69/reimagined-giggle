724990e2f1294a173aaf894b159eb3fc67e8d0de
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:08 +0000

    search birthday deaf money occur

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
