ff0acadc9d670935408be508233da040d4200457
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:35 +0000

    judge concerned surprising comeout consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
