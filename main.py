758a53849f2785c0f5fcd8175891717b9dcc2df1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:14 +0000

    hate avoid laugh their Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
