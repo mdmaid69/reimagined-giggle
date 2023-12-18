42ba416376b032a167335b7bd5c157656889d377
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:50 +0000

    giveout finally workout only uniform

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def reverse_list(lst):
-        return lst[::-1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
