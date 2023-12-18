7abe86cb8ea2e0a038c0d76a846cf263824912cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:38 +0000

    politics anywayanyhow goout high trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
