26bab97c062bad5e9bc44a1269a4c81a3f54120c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:02 +0000

    find shopping forgive below rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
