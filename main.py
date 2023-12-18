a19d0fb19fc80511259c148af597feef9dfb8283
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:48 +0000

    my compare switch impressive everyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
