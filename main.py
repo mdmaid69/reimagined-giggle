68b2ea6d453125683573129fe5d2ebd1cdc9de1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:01 +0000

    home blame turnon fill aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
