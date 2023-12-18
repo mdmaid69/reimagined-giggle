8b9634574aaf4e6b977b32d7bd047b7cd4ec931b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:22 +0000

    wander Itself remember fast concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
