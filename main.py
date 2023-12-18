61e96d6bddd09e335e2bce2b64c494d0e213f891
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:56 +0000

    human pencil activity phone imagination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+print(re.match("h.*o", "hello world"))
