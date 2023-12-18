c2f549b7e25f8a5f48c4ea9b1a29632f177ef480
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:35 +0000

    ball apologizeapologise slice situation fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
