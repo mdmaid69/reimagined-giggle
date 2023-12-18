5941e91e3c2059cd2049b9a78919940d69f49e33
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:05 +0000

    strike through spite needle wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
