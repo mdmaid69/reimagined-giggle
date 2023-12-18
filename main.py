562e4093c95db1a0557de006972d796799d8ae01
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:12 +0000

    couple side care middle basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_typecode(array):
+        return array.typecode
