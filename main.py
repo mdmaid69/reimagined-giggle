687963e3e44f3be429eaf2e79791088ec61bad4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:52:11 +0000

    moment closed first getout let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def convert_to_octal(n):
-        return oct(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
