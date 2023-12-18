72715d5fb570bf3acf49d76ea796f975b3c5fe76
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:03:59 +0000

    attempt worry university in polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
