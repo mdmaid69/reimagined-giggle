fe12feca6168844e255151cd2987aa2395e2b0e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:15 +0000

    few duty Qq whoever doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
