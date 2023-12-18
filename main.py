092369d6f0c73c9647e0309997bb051410f4de9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:00 +0000

    pronounce frighten television mistake appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
