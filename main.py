2757e9fc2f6f895aca5f816471fa642d08b7e145
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:51 +0000

    square heat underneath transport past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
