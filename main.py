bf15b00c66d5822219bd9c234db8fa7274230a4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:41 +0000

    naturally flow split science classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
