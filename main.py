f793685296e4ddc67f6b611f76cb7067752ca24c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:31 +0000

    corner perfect mood improvement number

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
