cbac3478e17bb2b7824e935b738cf6f0cf191181
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:08:35 +0000

    smooth distance ask easy note

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
