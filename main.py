9c5997d9fa4bb60be4901c3a0f2181ddd85222cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:15 +0000

    name message surface light gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
