f7c4cb14f650722f4cf98ee3d7569ef1639c1bce
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:14:23 +0000

    break scrape kill try tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
