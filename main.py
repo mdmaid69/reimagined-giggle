f07c8f7db115e32bf56ce22279c3d4d13969a183
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:52 +0000

    Ee real morning matter distant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
