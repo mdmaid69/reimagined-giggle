a787dbd6f602f46d3ae77737bb42b69540c1439a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:24 +0000

    extra middle ilovedicks roll among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
