4a02961e8a655af7fcd9ad3cae00e53b723f7ce2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:18:56 +0000

    wire defeat burst guide convince

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def convert_to_binary(n):
+        return bin(n)
