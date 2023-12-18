f4bd54a47985f75a73b8a05025ef97c4cf88377d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:57 +0000

    famous suit terrible nowhere afford

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_max(numbers):
+        return max(numbers)
