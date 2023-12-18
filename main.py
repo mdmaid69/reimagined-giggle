29029a663b1451ca1d1ae0fbdc216410937180e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:35:25 +0000

    definite turnback risk exchange land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def count_words(sentence):
+        return len(sentence.split())
