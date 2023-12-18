67602ac2cc06f55f2cb1bb7a15d39d8b0934c46d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:57 +0000

    square order attractive comment reason

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(a, b):
+        return a + b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
