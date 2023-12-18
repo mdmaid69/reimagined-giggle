e3619fc3ffb7b12dd9371da5d8fd266fc9ebc958
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:46:33 +0000

    winter keep see cost whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
