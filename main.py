34dab4db1c054425dd3ce91ccf7db567e7f898c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:04 +0000

    laugh level think workupto settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
