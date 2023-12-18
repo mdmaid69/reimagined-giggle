03910a0c10da71d47f5bcc746fb98d3259f5459f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:47 +0000

    born goodbye rent temper support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
