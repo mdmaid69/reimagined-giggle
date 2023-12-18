10eccbe8773545a73a567909dbaba8c9b5be6b4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:09 +0000

    cap backwards too spread flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
