78bfe16ba36262984849c66963d17ee1bea645e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:10 +0000

    miss report type anybody miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
