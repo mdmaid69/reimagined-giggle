fd78c1ad9b6e2fc93b0eb32b7e8a8b1eb76b5386
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:07:18 +0000

    length operate storm ill thanks

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
