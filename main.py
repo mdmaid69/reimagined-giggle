c679e734eab23c8c908ec8d3018b869fc58b6423
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:45 +0000

    breakup demonstrate square guess fear

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-print(random.randint(0, 100))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
