0fe9a2ecad44dee131fade693cca8a4432c82cd5
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:35:39 +0000

    little bottom could complaint realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def sort_list(lst):
+        return sorted(lst)
