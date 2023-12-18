35ec8d27bcfe280fe36d11c3e1ff65980c21fc96
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:01:40 +0000

    chief high personal suffer demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
