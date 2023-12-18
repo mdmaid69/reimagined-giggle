7fdbc20cf9d16f78337d5f814b5a0b5866aa17a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:07 +0000

    pupil waste professional success impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
