32c69498b02db03aec1fcf7aa14bcd05e1423bd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:55 +0000

    your any correct mine backwards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import collections
+def create_user_dict():
+        return collections.UserDict()
