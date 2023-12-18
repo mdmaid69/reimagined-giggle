51885a6b9bebb229b6680604edf44e07ef5750fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:17 +0000

    real emphasis book regular allow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+import array
+def get_array_length(array):
+        return len(array)
