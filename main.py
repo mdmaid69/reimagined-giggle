dcc7fb804ceee07c7715a2c8e12acb4224ea722a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:03 +0000

    571enough help label below similar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+text = "Hello, world!"
+print("Characters:", len(text))
