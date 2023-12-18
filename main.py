fb11faa7f17824eae2442b59169a52810a7639d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:11 +0000

    anywayanyhow birthday bag high roll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
