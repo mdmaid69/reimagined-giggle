2dbc0568452ce122b66f9a4d239141d8617d915f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:39 +0000

    damage turnover forward waste height

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
