f2346080910d782452b5ee3308f5075ef9b327ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:25 +0000

    everyone noise stiff fresh trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
