1944591a397f9695b4ac2e7ce832a2630c9893a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:09 +0000

    everyone arrangement godown work neat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
