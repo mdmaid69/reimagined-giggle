a3a4be3437b60fb71bf2f5176fbd7a8ab38c900a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:20 +0000

    offence colour label gas love

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
