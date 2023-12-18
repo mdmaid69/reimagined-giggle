5efbb04bb8446cf0c5733d8f47a3b56a711376ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:26 +0000

    swallow important underneath rent correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
