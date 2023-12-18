d887041336558474f36480855d50c0e005c45396
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:41 +0000

    throw bet holdon union pride

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
