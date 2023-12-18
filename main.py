e12b24d47ad8986afb7b5155427b421ba62e505a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:54:27 +0000

    would comment unfair vote combine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import numpy as np
+print(np.array([1, 2, 3]))
