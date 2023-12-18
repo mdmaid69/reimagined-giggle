892fed981b6da138ebb3dc624d2223fc9d49c628
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:48 +0000

    noone sight zero piece love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
