3196b4be077119d598bc4e42e64e72e1747841db
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:12 +0000

    clean clear popular general completely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
