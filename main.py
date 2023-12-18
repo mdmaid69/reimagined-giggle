ab5d3dee21e21e3c34d13abfe5d82256dd44d1d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:57 +0000

    collect pause along ahead within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
