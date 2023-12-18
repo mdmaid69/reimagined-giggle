6545920436437ba07fe1febee3ab1d05ae33b227
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:49 +0000

    peace middle hurry trick not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
