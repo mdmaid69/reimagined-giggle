639020c24a95b36d221e036d3d343f45885d85fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:49 +0000

    factory port turndown confuse shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-print(time.time())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
