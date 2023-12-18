3cdbad84839137a5c1c96c3590928c4ec39cc45a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:53 +0000

    married necessary excellent takeout ourselves

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
