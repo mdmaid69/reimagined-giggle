a895cf585d9e5ea3ef132ccb220ad4ae14c29c43
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:56 +0000

    work copy label vote could

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
