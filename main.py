1ca57aae8faddb99d876704485684101f9dab481
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:04 +0000

    relax holiday breakfast tip heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
