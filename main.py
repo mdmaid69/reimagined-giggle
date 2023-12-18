f50470153bc385c289ab4b953142b73f7b9b2340
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:24 +0000

    pair conduct second expression goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
