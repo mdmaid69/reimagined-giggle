e9a607ae4309d01b7da2f9075cdfd5ccd958a3f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:30 +0000

    doubt gift though boring first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
