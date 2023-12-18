14b425d9d98317c2200291d7c031b3e8d5ddb6e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:30:05 +0000

    estimate seriously event after rob

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
