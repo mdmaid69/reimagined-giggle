c94442881e63542e726bd4e7342e9ce0cfda23e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:50:07 +0000

    teach right door unable always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
