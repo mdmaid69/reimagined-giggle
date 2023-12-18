601f23e40f37efb8ac62dde2ab39b846b4e8a561
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:58 +0000

    bet discover either only however

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
