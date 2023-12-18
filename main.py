8c2daa69f0573cc7051f32d7f5a32d62c9e38cf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:17 +0000

    tax demonstrate weather dear present

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
