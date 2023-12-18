0529adc230d20069698f0ef6600c8e3d5a3a4455
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:04 +0000

    flat prove tidy after over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
