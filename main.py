5c2bbd4e6c6585cea7a400802ebd8186957548f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:37 +0000

    share real copy bringdown hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
