20a3caed668188089ae92ff6521bb984f6ac23ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:28:40 +0000

    embarrassing win comment better sport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+for i in range(10): print(i)
