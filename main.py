7b369d99f2099a52c59886e140882a4290385ae1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:38 +0000

    design last shape lot bus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import tensorflow as tf
+print(tf.__version__)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
