f25180cb3e3b95d3ae49ce88d359e337dff8dc01
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:27 +0000

    morning Dd statement cold middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_current_working_directory():
+        return os.getcwd()
