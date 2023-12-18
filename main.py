974e52ffbec548fdce8dbdfc019cf80661b7c300
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:49:31 +0000

    experience out lift typical car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
