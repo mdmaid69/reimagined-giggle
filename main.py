773d9adae3c851e7c523e7bfcbb852f91e28c37c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:31 +0000

    to standard takeback floor number

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
