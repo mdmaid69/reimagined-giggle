3a483ecaa86d5fe5c052b7c5681c96e77fa7e357
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:45 +0000

    land impossible right reliable guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
