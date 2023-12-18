d27d3932322c77097f3ec762db4de6ca3c472d1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:44 +0000

    error stand appearance general once

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
