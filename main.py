198b7f637a2fcd1005f031e3f05fd30aaf41eef7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:38 +0000

    foot flow lead port round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
