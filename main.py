4678a9f593800d3a07e9ebc824fcf03e16151cf7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:17 +0000

    easy criticize modern table real

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
