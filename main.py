972758831911aea8f7a82d94a45e4072999b2f0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:33 +0000

    petrol we factory suppose thin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
