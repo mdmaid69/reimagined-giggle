9edf1ea2db993b8483681a34942af918adca12f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:57 +0000

    weather whatever write choose jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
