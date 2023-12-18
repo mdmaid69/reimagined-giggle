27f421cee0014b0cdb3235150819ef722c029510
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:50 +0000

    beautiful degree relation division rain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_max(numbers):
+        return max(numbers)
