18e6607d68175935b678737e916e1b03d99a92ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:21 +0000

    seat getaway polite drive flight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
