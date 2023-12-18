d7273c91f3e3d26ffb850af9907f6949b959a10e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:02:29 +0000

    plenty tooth date passage from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def print_python_version():
+        print(sys.version)
