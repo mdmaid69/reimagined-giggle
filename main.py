6d9ae64ed4e650ccafb3c67afeec457147141d3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:05 +0000

    float ask progress thank process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
