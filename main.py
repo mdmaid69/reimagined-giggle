26514ffe5ba8520b119dabea9af301d838c458d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:13 +0000

    busy hold otherwise run equal

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
