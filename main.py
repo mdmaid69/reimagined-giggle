5556924dfa1f4afaa6f12a517d1f7fb3723dd031
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:07:20 +0000

    reply entrance responsible callup forgive

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+print([x**2 for x in range(10)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
