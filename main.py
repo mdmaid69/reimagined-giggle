eb76a751a65d52e328446870fc079bc913bbeb2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:45 +0000

    closely mark street before undo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
