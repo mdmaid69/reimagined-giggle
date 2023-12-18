09feb94ecb0cff888763222477f88a7a23d8b99c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:37 +0000

    drawing level step small stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
