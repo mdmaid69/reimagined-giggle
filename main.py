6219049d16e1daa4c213c6eb1084a250694ff726
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:09 +0000

    invite custom attach mysterious uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
