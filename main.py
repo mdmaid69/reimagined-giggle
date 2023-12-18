6eda4c9e562e0eacc6ffe7ed75a191fe2abbdb9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:00 +0000

    Ee fat 568energy exercise pause

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
