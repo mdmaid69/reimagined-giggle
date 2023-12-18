c887010961dcac6587a1cab2edc078364a9e15b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:44 +0000

    judge only reaction like eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
