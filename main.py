064c10262796c44ead1d363f4c90826df9193a4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:14:23 +0000

    vision and praise fix slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
