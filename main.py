3b5b9f0fd02095aa187f09ce6f2dedd31005beb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:37 +0000

    spare try plenty expensive goaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_hex(n):
+        return hex(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
