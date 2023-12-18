01204fd775eee49f3a6e193ab1ac54d3a5ae5110
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:21 +0000

    neither properly pick without mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
