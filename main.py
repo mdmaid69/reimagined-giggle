c3b6e3185a7b2f18daf198f27c4c7d7f4d9dbba3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:32 +0000

    why backwards home night whichever

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
