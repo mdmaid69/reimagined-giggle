ea9bcadd3e77d33a13dace11407b8d16f7ae15ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:21:57 +0000

    fairly head danger class personal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def subtract_numbers(x, y):
-        return x - y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
