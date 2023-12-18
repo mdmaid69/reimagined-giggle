8db3dfe88ee4a4ce1edb01223a626e9f4818d16a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:00 +0000

    eye quick mistake some coat

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_unique_words(sentence):
-        return set(sentence.split())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
