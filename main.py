aaa00e52dba8ba97ef959afa92a943f4eec52e2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:01 +0000

    square if concentrate before pin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
