d34aa7b43a0ded83cdaaa4da3a7b1b2c0d4ccbc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:15 +0000

    drive father concerned passenger negative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
