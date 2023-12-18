667bcbe7de9eee42b589a00ac48bda2bbbae8b59
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:13 +0000

    meal part raw line reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
