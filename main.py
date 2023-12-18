d59040948cfd1a9555b1b6490f7415f57f33b0e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:53 +0000

    less middle offer straight anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
