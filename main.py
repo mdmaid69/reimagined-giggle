b1a6568aa5845c541366999769db6b64e16cd8a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:36 +0000

    slip happy positive medium cry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_error_function(x):
+        return math.erf(x)
