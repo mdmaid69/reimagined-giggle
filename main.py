59ff8defc9db899050ff12fe63c2da675a402170
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:52 +0000

    task Jj dare interview search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
