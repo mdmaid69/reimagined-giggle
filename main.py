4edfbddd85d1bf4983f6b1a61269a6e48d7e71cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:38:43 +0000

    silent confident watch front remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
