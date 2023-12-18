5bc2d9b600117e7b83196903fb49d0e224bc50ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:35:40 +0000

    Zz hand open go match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
