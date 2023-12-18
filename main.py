bc25940b2ce90a5cbf41ad6c9ff5edefbd512d64
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:29 +0000

    sit correct check gas book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
