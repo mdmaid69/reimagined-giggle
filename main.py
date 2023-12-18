61806d04c1e297784f64da25b7b25e10c29b1a8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:44 +0000

    fear tradition front repeat post

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
