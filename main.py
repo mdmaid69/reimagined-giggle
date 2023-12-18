56aa62ab82958b90304b5872a1d9e793c19f0b3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:04 +0000

    base they describe custom solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
