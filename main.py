a750ff8a0a658333002e2a49ba278c1075d54b97
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:41 +0000

    delicate university cause powerful claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import time
-print(time.time())
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
