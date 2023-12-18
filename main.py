7a153c572fa7db091efebb9e55fa23337affc24f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:24 +0000

    full be rich forgive since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
