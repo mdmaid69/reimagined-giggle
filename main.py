b6ed5f83120f78aeb9b94c3cddff8a8dd0126fcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:20 +0000

    bunch roof deliberately have replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
