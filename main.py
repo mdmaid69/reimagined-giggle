c990494d59dfe8ea1f238fce38133c55dcf9093f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:29 +0000

    slice their fairly prevent university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
