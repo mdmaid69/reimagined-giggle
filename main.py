ac16904712de336827361cec0fac532e51525033
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:58 +0000

    illness familiar scene suggest science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
