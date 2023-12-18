6ced89de6b8acfd9da7b0165d565550710ae243a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:54 +0000

    deaf field wrong quantity human

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_speed(distance, time):
+        return distance / time
