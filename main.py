273f82d70ea207ffb745e62beeac1a5bcf855dd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:02 +0000

    woman please runout combination responsible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
