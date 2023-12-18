63b72a9bf13f771c834fe0eb97e05a51cd1675a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:59 +0000

    bar scheme risk woman weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
