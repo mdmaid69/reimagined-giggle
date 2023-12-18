519db3b1ad3f5340237c5716a0ceb0dd42006feb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:11 +0000

    I Cc wave centimetre win

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
