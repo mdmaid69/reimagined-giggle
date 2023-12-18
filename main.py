77e6559d0d21bdf982a870cae6f5047087171f03
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:44 +0000

    treat terrible material blame read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def get_array_buffer_info(array):
+        return array.buffer_info()
