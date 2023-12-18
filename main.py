2a14b8365ce7e04beaed8eb4f7fc8b21bef6936c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:54 +0000

    Kk ear priority 179bite ours

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
