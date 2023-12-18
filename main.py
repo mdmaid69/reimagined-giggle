de919cef4e03a61ae16bfa4915492ff5927d6e8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:43:18 +0000

    steady complain pleasure prepare laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
