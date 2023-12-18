7642bcf9f4614837bb0dc0a2cecfbd8c0a0ba2b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:32 +0000

    fold 573entertainment weigh keepout note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
