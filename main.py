d8799d5e3662412663b57f438c360c323c5bc95d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:46 +0000

    cinema extraordinary phone branch she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
