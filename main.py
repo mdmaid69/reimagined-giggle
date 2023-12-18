c3353b2bac5854da660c3eef8da6444855058243
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:40 +0000

    take remove star thorough wave

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
