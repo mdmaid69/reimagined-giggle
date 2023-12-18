71c0fe5511b92eb47cf2b6d4fabb43ec7d15121e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:50 +0000

    loss way report lot emotion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
