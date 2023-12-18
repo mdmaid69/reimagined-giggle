eee75f9ec28f71c755a74247f6a1196bcd8f83e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:20 +0000

    impossible file judge course holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def get_array_item(array, i):
+        return array[i]
