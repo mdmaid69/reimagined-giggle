b11a21b80f6a385afb050094a71f629f1fc9c80a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:13 +0000

    before battery sort nonsense top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def exit_program():
-        sys.exit()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
