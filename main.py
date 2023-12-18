51997a73aeb653ff9282584e4013907727627c94
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:44 +0000

    sting perfect comeback wealth disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
