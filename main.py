4d97a89a0ebbc3d93fe00fda92e91b9f188ebb85
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:53:43 +0000

    neck lift sort satisfied under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def multiply_numbers(x, y):
+        return x * y
