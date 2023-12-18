b25a55bf7d35bf7d039fb2f23e0a7a565c0192bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:55 +0000

    destroy scream react consider over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
