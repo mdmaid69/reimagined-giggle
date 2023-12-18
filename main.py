9f0a1e463f1b81e4140fff675600484deb4dba87
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:59 +0000

    movement rate mouth operate fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
