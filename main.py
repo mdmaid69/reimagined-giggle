39ab8c73594ec800a71bceef7a06b485df4670ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:51 +0000

    proud duty interesting winter afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
