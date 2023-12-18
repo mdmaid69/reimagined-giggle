2e51b1be8a8f1e34b85d9e2ed7b0d7faab19f544
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:48:48 +0000

    law hospital schedule move offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
