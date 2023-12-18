1ca82b0a156d6448e4379f3c20954e689f307ca8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:01 +0000

    blue argument equal speed fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
