7ffa54d51ef63885d3d531deb15f7bfa748c4bcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:30:51 +0000

    vanish past apart comment turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def find_min(lst):
+        return min(lst)
