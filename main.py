9c29d0d96a6784a7b75a8e6544c052a2b8e0e8fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:25 +0000

    anybody stop divide pleased comfort

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
