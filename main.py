b2a41a8fbefe44dfeedf73b4e314a6a5d0e51066
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:24 +0000

    warning much right breakdown hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
