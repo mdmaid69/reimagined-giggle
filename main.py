4bbe62714a83bc585df826a8316e588ddbabd07d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:53 +0000

    179bite programme bringforward wire process

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
 def calculate_sphere_volume(radius):
         return 4/3 * math.pi * radius**3
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
