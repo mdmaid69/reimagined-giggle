00a8f110ae11c251065485a47acb4c9047386b49
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:52 +0000

    fall argument desperate among lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
