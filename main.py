43308af230931e854f8a09abd9d664c5ea8a5bac
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:40 +0000

    turndown she talkoutof convinced disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
