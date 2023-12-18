f6e0233f368a7ceaa57d387ef45876074466de90
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:20:52 +0000

    boat stamp post contrast rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
