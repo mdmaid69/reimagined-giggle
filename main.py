a0e1c945c86cb85cf2a197a04368b15dac024331
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:44 +0000

    worth thick protest flavour skin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_as_set(array):
-        return set(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
