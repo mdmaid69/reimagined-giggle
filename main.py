c3750228f1e83ec1bbcec43018cd3c53c9272dac
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:35 +0000

    plan train comeup mine concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
