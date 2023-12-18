171baa0ac9fc6243a44cdd397505aefcbd14a539
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:54 +0000

    shut admit classroom welcome ocean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
