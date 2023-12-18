ee0e9554687453ef01eb22aaac1a081dafd5afad
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:17 +0000

    bear shoulder bear wear realize

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def convert_to_hex(n):
-        return hex(n)
+import re
+print(re.match("h.*o", "hello world"))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
