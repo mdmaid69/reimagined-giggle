52524958827be7d9090fa5778114560f594f67b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:53 +0000

    push relax you memory indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
