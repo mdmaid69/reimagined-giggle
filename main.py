924d6a402a246b0956b4cf56b53ac941a1a844c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:38 +0000

    cry sport illness want better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-print(math.pi)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
