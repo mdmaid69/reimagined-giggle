79f25e8b55c68ca91508f402897ae3934bbd26ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:38 +0000

    breakinto replace Ff available lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
