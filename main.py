465220fba9eef199ee5c82a2ad6ed91d5faa48a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:52 +0000

    turnback appeal love mess weekend

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+print(time.time())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
