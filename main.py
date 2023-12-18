ddb7ea75252b3d3ac174dffef9110802175e1e09
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:54 +0000

    negative discover unlikely size lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
