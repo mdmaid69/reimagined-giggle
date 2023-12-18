9f4da53c74084a06cd434dec6437a4b82abe61c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:21 +0000

    sister properly callout account call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+name = "Python"
+print("Hello,", name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
