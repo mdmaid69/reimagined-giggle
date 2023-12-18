6a482b4ca3c70d1ad55dee09da9fada751ddf85d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:42 +0000

    able duty little apart stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
