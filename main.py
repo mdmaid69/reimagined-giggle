f56703d9c42449c8a53946aa570c05e963ad6451
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:57 +0000

    wish turnoff could greeting main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def convert_to_hex(n):
+        return hex(n)
