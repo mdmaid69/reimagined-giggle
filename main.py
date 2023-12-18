4ad2208f755494dc80e5f211a0e6a314d7406e67
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:28 +0000

    little passage expect suckme tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import getpass
+def get_username():
+        return getpass.getuser()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
