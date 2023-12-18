2f52979d4857505fe4c7b9981f3feef649e0eb75
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:46 +0000

    content holdup sink mad ground

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
