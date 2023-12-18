97950efdab9ee25692fe4136b30b1a7555738e7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:26 +0000

    sink slip date correct progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import datetime
   def get_current_date():
         return datetime.datetime.now().date()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
