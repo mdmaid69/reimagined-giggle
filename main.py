9cfc9eceef811fa0c6795649676e5373ec8fd04c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:07 +0000

    once letter warm play leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
