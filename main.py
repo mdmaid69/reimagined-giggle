dbb81a4e0906d4de63afa703d1d4d27dca26ddd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:12 +0000

    job distant regular bend discuss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
