6c131458e77494eda22be9c40fa4ee2b16b9a5ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:30:36 +0000

    quit underneath pupil actual set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
