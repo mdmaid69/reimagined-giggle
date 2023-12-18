b29a0de9bf2eae72404e2249a682e81387e7be2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:23 +0000

    accordingto comeback pronunciation cruelty sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
