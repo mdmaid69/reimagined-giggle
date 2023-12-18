5dd0a21b7c1eeed5cffb0c7fdab20458ff43ab74
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:01 +0000

    warning maybe awake angry pool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
