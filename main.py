c0e3f9fdeb2bfb69ee5c58ab725ffa63198ed0e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:09:38 +0000

    favour accommodation family to sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
