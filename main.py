fd74cffba4e6f96c9f436d7ae56a7e6ca3599066
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:44 +0000

    train send before scared luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
