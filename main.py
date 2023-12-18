84675edd28da14e692f3c66325b5b30816181e40
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:59 +0000

    organization sure careless equipment record

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_power(work, time):
-        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
