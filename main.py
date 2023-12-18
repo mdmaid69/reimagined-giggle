131a0522b6a5bde7513f6f5d8d574c2fa6c946ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:26:41 +0000

    prove over material war hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
