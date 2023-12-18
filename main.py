3f06315292bef9bac98fe7d8d53033fbb81500ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:01 +0000

    take regret sore kill though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_arc_tangent(x):
+        return math.atan(x)
