0b3dd4da60d85cbb0561a09cf789aea534066b92
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:49 +0000

    muscle report copy shake people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
