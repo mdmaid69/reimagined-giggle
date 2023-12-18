197b6d6af8ffc218b852030d5326347e49fd1d2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:28 +0000

    truth target advanced just past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
