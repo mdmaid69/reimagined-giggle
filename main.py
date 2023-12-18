c7b2b3dac04cd4eedb3ea90f05b7fa3d96e97100
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:13 +0000

    official bright flow before inquire

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
