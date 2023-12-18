a478cf3ad41e5d064262cb88f5c8a48da0663079
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:25 +0000

    little frame bottom comment union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
