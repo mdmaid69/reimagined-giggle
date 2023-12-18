b728072f841a230673f8398990f855e253b5ebaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:23 +0000

    formal temperature bit luck big

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
