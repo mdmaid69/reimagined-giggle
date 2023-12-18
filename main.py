dda7a2dfd962eef61296ba8665938e78433d99d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:42 +0000

    reject fall move example teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
