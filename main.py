4f8a07bd48d557b89b6c7ac4d0f7e73b6500da44
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:46:19 +0000

    buy school no Yy meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def reverse_string(s):
+        return s[::-1]
