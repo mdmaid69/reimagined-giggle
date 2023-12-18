85cfbdffb3b1ff2a05d4957b76c7e656c8f65903
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:19 +0000

    book hurry argue tidy arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
