fb7f633a2fe677f6c2115b4aca830195003686c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:24:56 +0000

    when match bread pause too

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_item(array, i):
+        return array[i]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
