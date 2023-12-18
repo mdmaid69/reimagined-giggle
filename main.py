760bb7bfad32c2a25f3a940ca80632486abb716d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:06 +0000

    design wave attach hurt thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
