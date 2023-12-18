4bc4266603e9707af435d31ddc73b10ae4c83e1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:22 +0000

    generous separate find joke force

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_user_dict():
+        return collections.UserDict()
