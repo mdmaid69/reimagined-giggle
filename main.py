ae5453603901be005967e61d81b06a96467a7c32
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:36 +0000

    always both pipe appearance joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
