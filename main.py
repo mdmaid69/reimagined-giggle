a1e4ba9a8bac35559978f8608d69e8648ea382f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:19 +0000

    cheat equipment major Vv round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
