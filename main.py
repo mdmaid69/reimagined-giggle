e1c1c64604d094cd2b6c0040fdc3b72b46f7e36e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:16 +0000

    successful street lunch somehow physical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
