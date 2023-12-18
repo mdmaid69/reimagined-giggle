427a992c61798b186a4bf5ee21089d89bc1d1eba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:23 +0000

    thin purpose further introduce leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
