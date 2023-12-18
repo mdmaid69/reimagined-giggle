8b7792b7e9d69fe24be7161d20d2ae946e9dc6fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:55 +0000

    please usually worried to rather

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
