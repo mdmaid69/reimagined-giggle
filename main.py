53f50722a1f9ee9a098f07c4ed5fce1941c03c54
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:37 +0000

    strict violence drop mine hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
