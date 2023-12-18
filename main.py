1785c57d7517ad7ba8fbf05afafc7657799efd55
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:28:13 +0000

    habit scheme nerve start afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
