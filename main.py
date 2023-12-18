9b9e2bef37ca6ea9831b876f8df83e02c92e7771
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:31 +0000

    human course thankyou trap my

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
