071c918359e03c4ec9d27fb52992d34e361ab374
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:35 +0000

    takeaway telephone tax force flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
