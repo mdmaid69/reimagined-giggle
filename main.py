3f5d06eb256ff656f9aff1a97072a5e5052eb738
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:44 +0000

    difficulty lookafter sad tear block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
