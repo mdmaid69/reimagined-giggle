2d9691232731e28e5774777fbe1236fa01c794a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:25:52 +0000

    holdout care condition along embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
