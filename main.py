fe73c48ab0e8a1f9cc7de43d58ee773d4634632f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:36:39 +0000

    fault pool write test around

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
