1219c8feaa041c50d896fefe5dfb987b5b1143f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:48:29 +0000

    original myself friendly pain taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
