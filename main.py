7f9bcd4ad7f9fb7cf446032a29ea70d9610b9bc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:55 +0000

    brain reduce remove Dd shrink

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
+print(sum(range(10)))
