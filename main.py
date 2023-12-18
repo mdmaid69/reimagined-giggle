e5f7c47008f7e9f041f05c48e7b546d21b54111f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:57 +0000

    classroom wall sleep because brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
