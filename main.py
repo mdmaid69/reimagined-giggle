8f8520cd5e2f67ceb3984b664f124f70d33f79a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:00 +0000

    politics unfortunate man arrange wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sys
+def exit_program():
+        sys.exit()
