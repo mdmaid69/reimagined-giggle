0367e34a0b81bbc1c965044fe5f603ff49c6f242
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:30 +0000

    shock though left hurry contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
