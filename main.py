ae8795d3663a599850d9048738c3cd829205dac1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:44 +0000

    community draw kind agreement birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
