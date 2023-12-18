8553e345be031b60dc7c73deb0d8a55657901e2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:20 +0000

    real advise sign holiday think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
