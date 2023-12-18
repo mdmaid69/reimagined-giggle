9d4decd2c96d390e8ace00302db92b340cef44da
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:14 +0000

    guess tomorrow tend curious workon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
