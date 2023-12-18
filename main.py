3ddeb2ddf93eddc6c201b0464732286e1d0381f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:55 +0000

    dull afford contents only confidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
