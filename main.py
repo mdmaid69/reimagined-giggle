5bb828c54b7ba3ee87fc4fbb87d8eb7c1087a801
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:54 +0000

    floor ever direction cinema new

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
