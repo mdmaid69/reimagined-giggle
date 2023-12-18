f8cd8e977a58c00e2eed34c3d85fbebb09869b66
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:00 +0000

    explain snow holdout either trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
