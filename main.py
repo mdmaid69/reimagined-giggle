049ece9cc2e077cc59a07d11c5e1aa727468aafa
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:11 +0000

    train sure history run face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import platform
+def get_os_info():
+        return platform.uname()
