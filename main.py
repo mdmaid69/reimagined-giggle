878d4263b062b185a27a6836dfdb08e28d27d8db
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:14 +0000

    mark 566end interfere content blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
