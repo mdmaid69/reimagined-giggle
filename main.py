453a7aad7f6582ee69fbfd678aeb6fcc6ff716e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:37 +0000

    test much uponon spite chance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
