6e99ade170ad1d97c2de2be1834a8c3673b837f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:55 +0000

    connected bet whoever Tt suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
