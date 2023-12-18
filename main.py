9efe353ef0f236b6cd54bd579842ce2210a0e424
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:38 +0000

    characteristic joke fold history dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_list(array):
-        return list(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
