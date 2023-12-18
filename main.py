bacf92fb36445957e8569bf2be7865195b991f1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:24 +0000

    command 568energy other square half

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
