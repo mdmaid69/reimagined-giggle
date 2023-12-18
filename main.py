bdb100ddd80b3e9c993fa2a722eaa79c0df6c673
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:35:54 +0000

    else cheque Hh contents pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
