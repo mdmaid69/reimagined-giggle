02dbbb1f08793d11d5622ded6bba8bd76f91bc34
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:32 +0000

    art instrument card switch rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
