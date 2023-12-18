4cd7dd2e18fd73acf872d988c2a3798ff17eb2fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:25:29 +0000

    false female load off turnup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_pressure(force, area):
+        return force / area
