635587d290ec1729481f406447edb75c59104a40
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:49:50 +0000

    burn replace abroad perfect chat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
