1d0f3a3a835f030c32b10a0ca76592b906ccf3ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:32 +0000

    fire prepare Dd fork press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
