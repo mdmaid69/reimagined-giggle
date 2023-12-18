2c3e7f9b67dc6e2d47eaadc5529d530c14e3e102
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:07 +0000

    bet left money tool trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
