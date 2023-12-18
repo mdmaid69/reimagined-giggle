9d18205ada83b1ce1935db8c479313cefcbe87b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:23 +0000

    try say do polite laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
