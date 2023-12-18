c63aa3bdeaf79c766b52fd62b04779ad2c68fe8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:30 +0000

    equipment style nor become person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_speed(distance, time):
+        return distance / time
