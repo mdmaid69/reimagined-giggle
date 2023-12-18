bc81af2cffb70adaac04935314a1d473ea0ec16c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:16 +0000

    exhibition impatient sick challenge dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def convert_to_binary(n):
+        return bin(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
