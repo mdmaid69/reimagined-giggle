c3a2a4ed5f55d3ef0c9859169142dff86ae05736
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:39:49 +0000

    report material trip fall reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
