59d889c0c8d435153519bb3feb783b0b0c9f1e70
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:34 +0000

    whistle constantly shout dangerous shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
