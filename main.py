400f203715af8c7f486eed849e0cdf7195e9a67b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:53 +0000

    completely none understand label here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
