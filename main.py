b4895d8151eb5c975c80597db4d6aa8942cad726
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:45 +0000

    loud mean way eat book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def sort_list(lst):
-        return sorted(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
