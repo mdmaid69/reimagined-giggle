ea9d091f00b3bcc8225550023541e1fead326299
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:32 +0000

    naturally on mistake floor drink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
