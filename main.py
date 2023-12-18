ab530a3307c8516017237c0e294b997bf5cc30af
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:38 +0000

    weekend morning should shout pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
