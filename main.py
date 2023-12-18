e8b52879215e6b44ff2dbb42681fd8c3b604401c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:20 +0000

    Ss sit bringdown holiday cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
