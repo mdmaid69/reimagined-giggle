ffebd6f43af7e00ecec4a862cbda5c9a3422aafe
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:32 +0000

    interested nerve bill switch sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
