6882ff1540854a550b67776a67962bf9b667d28d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:24 +0000

    Jj glad by government depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
