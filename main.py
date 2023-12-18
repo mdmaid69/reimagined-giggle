3872f4bf225b2cd6917b37467ed1b0eaabd1c4de
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:57 +0000

    little tense exaggerate frame regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
