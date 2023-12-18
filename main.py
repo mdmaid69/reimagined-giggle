f77c61c6fbfbdc4cb2b7ffad75d0de85099f96af
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:15 +0000

    physical mess meeting stir might

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
