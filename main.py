88334a0d13324b8b05a3118a02c5cd8303d2bb3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:57 +0000

    anything soil able never subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
