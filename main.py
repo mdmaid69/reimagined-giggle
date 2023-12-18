0537a21cb6d1a45b46889387807e96fd47b3f643
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:11:53 +0000

    achieve turnback skirt goaway dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
