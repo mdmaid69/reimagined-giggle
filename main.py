28d01bc7f16923f3ad5382d2ccd0c97733dcf3fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:03 +0000

    surprising powerful breakthrough legal decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
