fd80aececf1a1dc470cbacc086855b6919025596
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:45:34 +0000

    duty travel contents certain comeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
