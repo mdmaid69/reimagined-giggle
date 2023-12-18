64dd3820ca5c19890ad792cbfb09da52d24b03a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:09 +0000

    concentrate safety area profit prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
