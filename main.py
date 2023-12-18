44b64fbf41e36a7ce3963563faa31d4fe9fa3f03
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:24 +0000

    hot operate confident activity blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
