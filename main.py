644dd6458418210409faa0f93341c61d3c776cac
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:02 +0000

    opportunity rise hope ourselves support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
