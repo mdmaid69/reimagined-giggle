62137ad8d39491d5d0880fad2606e0e6d7da4888
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:57 +0000

    successful fierce behind go shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
