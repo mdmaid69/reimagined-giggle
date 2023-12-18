c2ad4fd12d7c911f5519d17fafc033f7f3d87b64
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:05 +0000

    success contact kick rub different

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
