4c15e3137971eabba534d40eec0963ea67f2ff8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:05:26 +0000

    disagree factory hungry wrap drawing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
