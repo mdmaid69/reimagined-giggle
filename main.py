4026999564eadf3313e46e0d4a4333b53204adec
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:25 +0000

    aim strong curious passage matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
