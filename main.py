bc1e3cfb6a4c3d05486ac29c0809e9eeae9e7c7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:05 +0000

    relief less shoulder objection towards

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def convert_to_hex(n):
-        return hex(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
