8ec39cd9ae88395fd6ffd0177ac5254f97dbbdfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:31 +0000

    address bye course subject busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
