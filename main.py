d09e5a79c20f926ab21c7cfa416c9b900f857f6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:07 +0000

    miss effect roll comeout plastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
