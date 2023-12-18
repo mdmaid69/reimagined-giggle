b360b075c10f378bc4b7f6493c8ed362838ea9aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:21 +0000

    operation Ll active relax snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
