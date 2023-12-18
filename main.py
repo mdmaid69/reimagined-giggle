ad24133aad86bc84673d90077b5e35d23efa9f6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:50 +0000

    neither event mysterious pinch bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
