faeaa22eeb679c9b973dffc92dbf3153c23679b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:40 +0000

    show rush mistake no modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
