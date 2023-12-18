67e588acc69e4ce93773e4843a15418728b99fd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:35:55 +0000

    skill objection otherwise smell under

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
