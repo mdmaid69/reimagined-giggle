d46399c4c15ccb7d4cbff6c4195a0bdb74995c8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:16 +0000

    nose basically throughout deal straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
