5aea160af37932bc2bafed574fc21b62def9b79d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:25 +0000

    herself check bath praise genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
