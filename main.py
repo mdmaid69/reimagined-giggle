d2dd99233d2c7736055ddd9a66f11f1c7ed9461d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:03 +0000

    burn comfort brown suffer state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
