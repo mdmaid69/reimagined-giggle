5785abfd57f925a6e77c99ada9bbac14fa9a322b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:40 +0000

    indeed natural welcome makeup kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
