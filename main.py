2dd43b0d72bfbe73595d3cc16832cb4181d0194f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:55 +0000

    mend rarely prefer love interfere

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
