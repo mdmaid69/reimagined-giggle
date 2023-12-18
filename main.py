cb418f418118682dbb2a6aa0ce8e963d9c218e7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:20:45 +0000

    fall enthusiastic to lookup past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
