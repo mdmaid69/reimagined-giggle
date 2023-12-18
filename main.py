eac7f733f77a250084add85e4390ee1ac429d865
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:34:06 +0000

    mark sign keepfrom charge wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
