c1227df0b41ed3c8b2730f63a4a71d2214af40ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:08 +0000

    mine include search trip cause

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
