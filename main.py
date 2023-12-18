c4d51e3245f8b9da9660d9a9dab9dbfb774587c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:36 +0000

    noone house flavour little weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
