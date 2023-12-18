09f837a30d4045c25729455f31dac5deb4e87a15
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:34 +0000

    appearance centimetre poor opportunity dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
