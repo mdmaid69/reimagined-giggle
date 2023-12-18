5d0ab106daa3d31ada6e65a7ad0596d905e7eb86
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:46 +0000

    teach feel combination bet illness

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+print([x**2 for x in range(10)])
