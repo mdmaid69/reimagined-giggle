2153b9355e55c6f7d7a20fb7bceab7d10b8094c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:18 +0000

    write draw recently class firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
