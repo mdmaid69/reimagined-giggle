8f7cd80ee7edd238e408aa2c36b3c3f29e160a7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:47 +0000

    boat bag sink difference all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
