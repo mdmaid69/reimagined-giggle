9331709563a9c1d0a4c52fb0537c4e69b90b7386
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:18 +0000

    thorough themselves able play them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
