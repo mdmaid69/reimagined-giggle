a703588eb97c9ceca30ecc3983d98968e6bbf82f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:46 +0000

    normal bite dangerous wrap count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
