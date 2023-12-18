b7963a0c53b107100c5c4b2b126d025a6c0ceb42
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:01 +0000

    proof modern issue laugh voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
