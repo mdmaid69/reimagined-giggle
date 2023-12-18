b9697b3b6fb22fe9997547331e257a75617bc3d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:29 +0000

    date profit whom switch clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
