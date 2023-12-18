0cc296ae05b66c76e1feae67211d67a46f883b93
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:25:09 +0000

    attention fire order exam repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_item(array, i):
-        return array[i]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
