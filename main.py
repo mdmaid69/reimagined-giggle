e43126f2745593ef6ba55b0795a7a320cb0be19a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:53 +0000

    approve trick tone performance hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
