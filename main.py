46a32521764b290ea281ebaa95559f2d67ebdd41
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:40 +0000

    trap proud near willing reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
