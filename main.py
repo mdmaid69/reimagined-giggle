5853cddf1c5902328854611decb9ef3269dd63c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:45 +0000

    maybe apparently illness sink shape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-import array
-def append_to_array(array, item):
-        array.append(item)
