e9b5757fa3b3c518535f9177b52bb28466cc5820
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:02 +0000

    thought burn project narrow board

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_power(work, time):
+        return work / time
