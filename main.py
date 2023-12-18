07b8f206ed5359adf34420d2ea3ef5a2a376b7dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:07 +0000

    lead save breakout proper form

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
