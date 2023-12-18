cff76029498ea78ad23cbd15bfccd1160a11e269
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:56 +0000

    divide translate charge certainly perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import time
+def get_current_time():
+        return time.time()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
