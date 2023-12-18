8e999e6123b7ea4cb0b7350559a4f1603c059196
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:01 +0000

    getout lead fair callback sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
