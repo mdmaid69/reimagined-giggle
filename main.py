f0ded38d628f4b7232748fb20f0b43fd4e6670ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:02 +0000

    land pay everybody switch period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
