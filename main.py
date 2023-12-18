ca9fb55201a8393c36e64b4e0ee3e849bf00ecae
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:00 +0000

    Ii beneath temper once live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
