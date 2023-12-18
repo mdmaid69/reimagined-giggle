b1460bbfd3b99cf43fe56c65db477db5bfd31d33
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:05 +0000

    deserve poison recognize pin tragedy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
