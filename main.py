5955cc70b8f9e0a58b77b60ff9525ebc518c5558
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:41 +0000

    apologizeapologise pupil us its bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
