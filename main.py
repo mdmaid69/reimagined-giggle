4c02408fa92a36408c9c24e67e273adcb1c3f499
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:39 +0000

    always closed worse rise mark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
