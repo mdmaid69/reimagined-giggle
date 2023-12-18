3dbdb3086a05c07dce777b2820e1fdcb6b449be0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:26 +0000

    relationship neck before turnup middle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
