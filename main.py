2c50cce9a7a13c7fefabd7a16bc797bc293ba218
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:37:23 +0000

    man godown young push rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
