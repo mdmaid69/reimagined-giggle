33f4a1e26384f1a35124229625d74a4c61fad2ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:24:08 +0000

    small excuse life kick everywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
