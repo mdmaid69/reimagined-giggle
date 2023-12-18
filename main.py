7b4fa307018e56a4ee09f7d00ec5efed8e9c19b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:07 +0000

    whether guess reasonable trust sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
