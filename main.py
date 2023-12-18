72a39898b5df1a3c0a770419110de1546e9ea1b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:31 +0000

    yourself turn bear pair kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
