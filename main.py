a6a07cf46a9712cb9f3bd14b629410bcd7ad94da
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:31 +0000

    week his television like scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
