160ced80116842f9affda92d5e04407bb2acd1ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:43 +0000

    free one year any pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import time
+print(time.time())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
