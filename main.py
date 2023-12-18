29f15495efb7e424a9423ff724a87dfe8adcf060
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:28 +0000

    eye sign just face everything

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
