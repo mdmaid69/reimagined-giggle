40fd83196e39b812636bd22172ef0a3fb36d466b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:11 +0000

    write unite fence next Ll

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sine(x):
+        return math.sin(x)
