0f9a8eb91af689e1c68e33b45103249c87cae523
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:57 +0000

    guess please order stretch reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def remove_duplicates(lst):
+        return list(set(lst))
