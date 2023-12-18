7460847d67535c9aca61e7f5340bcea58335ceef
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:40 +0000

    cry turnaway talk sport for

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
