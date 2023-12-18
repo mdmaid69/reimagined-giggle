0105f21234b7dc0e91b9c80a653d390e21bde3b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:04 +0000

    string someone already admit knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_user_string():
+        return collections.UserString()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
