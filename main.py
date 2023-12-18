15a1191ed314b4bec10c2bffab43f384f98ea8b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:21 +0000

    injury runout gooff herself rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_stack():
+        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
