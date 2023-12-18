7f20302342554b9e07335c38f3d025acead5087a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:32 +0000

    closely 567enemy round either closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
