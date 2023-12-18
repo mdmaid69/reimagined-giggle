3406eed856c06d21cfbbf57adb6bd8ae308bb793
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:37 +0000

    save bottom sure fast join

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
