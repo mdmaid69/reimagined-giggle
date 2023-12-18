cb55a6249a894e6e3f568f23086628d120d5fe9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:36 +0000

    comfort memory mark his destroy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
