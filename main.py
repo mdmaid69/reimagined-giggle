c93377722935bfe1f370c01c1b981a87201be4c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:23:09 +0000

    comment steer visit press remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_bool(array):
+        return bool(array)
