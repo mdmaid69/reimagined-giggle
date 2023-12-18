e699c15b01759a470e51bb5f0c1b267dbaffb8ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:40 +0000

    land runinto this throughout display

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
