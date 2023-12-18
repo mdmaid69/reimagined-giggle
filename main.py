510af1342671723a0efe1a894f1af366a0f24892
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:10 +0000

    disappear control moon behaviour fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def remove_directory(path):
+        os.rmdir(path)
