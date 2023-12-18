3f5ac57c01e28598998d7f67b962d7e420361ff0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:25 +0000

    clear surprise callat oil whether

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
