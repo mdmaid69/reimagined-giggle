6bce6c46a7ba7af1fd8b5f6f93ee39ca2356fddd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:43 +0000

    science regard makeup act film

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
