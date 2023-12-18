3a334af851843e64741c55a62b8ed091f67b4726
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:02:50 +0000

    quit detail turnout what Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
