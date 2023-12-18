fd0cc98017a2f0c6c098754632dab39817ba273b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:48 +0000

    treatment height own evidence delight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
+  def add_numbers(x, y):
+        return x + y
