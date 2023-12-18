6dc4ebdaca46258675acc1838edd550590b763b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:42 +0000

    slide exploration original eye software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
+  def calculate_area_triangle(b, h):
         return 0.5 * b * h
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
