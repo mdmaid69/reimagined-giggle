2d44a28ebdc3774d75ddca809673701c8d9c8b61
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:36 +0000

    design answer development moment help

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
