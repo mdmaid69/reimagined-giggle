db92b10fdc6d8827f62218917def1f0383f05fca
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:19 +0000

    upset office surprising lean manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
