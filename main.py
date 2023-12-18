06f0d821bbd55a96b9f1f150f1f9a197dc9ddbaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:44 +0000

    approval attitude imagination natural callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
