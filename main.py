7562c4638293cfd74505783a8a1f9075921f911a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:50 +0000

    shoot hungry mass other so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
