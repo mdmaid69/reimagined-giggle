d64bab86d2bad21ee818924fe6b417a6798251f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:36 +0000

    snow Uu country question job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
