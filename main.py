c819613ae863b451d124a6cd0a6af7cf48689bbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:01 +0000

    run comeover hurry ever beginning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def cube_number(x):
-        return x**3
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
