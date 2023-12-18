ca117669f191f0d5f1b7e7c16b05f792663e7fee
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:01 +0000

    care spoil wish interest high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
