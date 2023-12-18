e72be6acca776c997865fcd748536504bcf25aed
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:10 +0000

    earn advise upwards world use

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
