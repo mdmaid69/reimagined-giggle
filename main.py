83158fea81f69f9c4cba92728d057083284c659a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:30 +0000

    prepare past fortunate onto Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import random
 def roll_die():
         return random.randint(1, 6)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
