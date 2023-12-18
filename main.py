9b0d0b5d91802583e4792a8921f33ecb29df9611
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:13 +0000

    excited cause spring hold with

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def subtract_numbers(x, y):
+        return x - y
+  def subtract_numbers(x, y):
+        return x - y
