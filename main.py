80dd841cf9109464096a4692e150582c030920ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:46 +0000

    bottom cook classroom tip nigga

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import math
-print(math.pi)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
