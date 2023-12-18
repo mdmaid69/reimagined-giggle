d29d1269fcf3a2c54ca4fd9e2e70f710e79120b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:10 +0000

    window rain sound everybody genuine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
