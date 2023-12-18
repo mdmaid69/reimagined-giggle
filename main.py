46352e6e5c8a2bb9279149e446560efd263b0299
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:22 +0000

    defend tool least bother cover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_area_rectangle(l, w):
+        return l * w
