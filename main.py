2dcb1a6e1564102ef4593bb07ee290b53fae57ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:13 +0000

    guide black aim behind test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+print("Hello, world!")
