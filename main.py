523a3f7d2e5e3eb118eadeaca0bf831f9e9a54b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:21 +0000

    that precise appearance then positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
