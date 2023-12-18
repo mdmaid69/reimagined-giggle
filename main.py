9ebeb4df875fe3df3a1dcb185b27f14e1a42d821
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:47 +0000

    think repair damage either pollution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
