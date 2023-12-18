bb53ef89c0cf0bf79464475c0cc210b4a0b6d40a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:00 +0000

    safe second sister anywhere draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
