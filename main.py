ecfeaa21f46fa4e358c1cb5927c37bcda75502f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:53 +0000

    original search pain forget order

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+print(random.randint(0, 100))
