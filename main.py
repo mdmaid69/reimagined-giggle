81279eb0bb0b72459d1eaa5647460d2b7b01c5d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:25 +0000

    forever innocent whom stamp worry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
