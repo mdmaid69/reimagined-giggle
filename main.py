6a0b54d94ac257106295b597e6893b053718d6bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:03 +0000

    join tip pressure class test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
