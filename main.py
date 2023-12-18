55c9407887ccbfce7283f15766f46ec168323959
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:42 +0000

    without invite disturbing rather score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+import platform
+def get_python_version():
+        return platform.python_version()
+import collections
+def create_user_list():
+        return collections.UserList()
