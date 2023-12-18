39e5d85a18d193afd860570b979125f022a5bb84
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:43:31 +0000

    what off recent scream teach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
