8371fec23b6dd8a28aea32759d0a7e0cc1eb0593
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:59 +0000

    flavour read left team recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def subtract_numbers(x, y):
+        return x - y
