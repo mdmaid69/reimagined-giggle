e8f1347ccaee5fdffbc4abbf5a355686be53a93e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:15 +0000

    connect tongue punish almost die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+for i in range(10): print(i)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
