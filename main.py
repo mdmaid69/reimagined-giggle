a58f1dc753ba0eb41e3b8e65f59720449b758ef3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:40:14 +0000

    influence top jump impression model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
