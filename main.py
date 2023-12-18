1834127eee3f723d7e18ab7aa01c4033fd234a4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:40 +0000

    pronounce guess total insist proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
