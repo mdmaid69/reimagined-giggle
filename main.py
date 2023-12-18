66ea39e5ac378f17f9a46e623f56096de7a8f7b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:29 +0000

    nerve proud geton cheap scrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
