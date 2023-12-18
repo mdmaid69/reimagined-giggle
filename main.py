b8824d8f1ceea8589662a44bc76bf9f52d34b7d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:51 +0000

    finish worth reach fast thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
