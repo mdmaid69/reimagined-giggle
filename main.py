15060d084a90edb13eaeca9a7b4bea3beaecd140
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:50:27 +0000

    beside gentle advise turn goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
