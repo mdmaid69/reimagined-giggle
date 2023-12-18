dccc5c66e4c208b069492bbfec93795612b00118
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:14:32 +0000

    own between play spread feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
   def get_parent_directory(dir_name):
         return os.path.dirname(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
