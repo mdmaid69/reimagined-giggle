d001e62a4b323538ea726a022f347a66f6be17d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:22 +0000

    along owe miss order now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
