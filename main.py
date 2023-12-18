c733558b1dd9728fd0bc7001165f4b48efe7cab3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:39 +0000

    hold shoot visit unless hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+  def is_even(n):
+        return n % 2 == 0
