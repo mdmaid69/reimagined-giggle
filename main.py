dcf6ed23ed18d6b8c85d307c0c82e175b6b5540f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:30 +0000

    able kind cold arm keepup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
