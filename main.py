8c6745386824d2a52acc8ea471ff00a19c1b4215
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:48 +0000

    admit worried balance contact if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
