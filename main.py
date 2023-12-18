d9b61481f99ce738fdf23963535e52afa54ae7ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:05 +0000

    excellent relationship flood pay nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
