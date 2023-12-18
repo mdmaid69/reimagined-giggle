0a56cccb4f13b11ef04562045d498b790482da52
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:10 +0000

    familiar why unit once exchange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
