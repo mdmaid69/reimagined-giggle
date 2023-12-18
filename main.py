f03c0016442d64abc5060b349624c32cd446544e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:47 +0000

    kick disappear answer down change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area(radius):
+        return 3.14 * radius * radius
