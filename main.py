4bc52ece55e283df39c117eecf7afe1aede40820
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:13:08 +0000

    play major takeaway borrow understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
