041d57e5555b4871e876d84abbae1c7af99fee52
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:07 +0000

    attractive goback(to) invite afterwards wild

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
