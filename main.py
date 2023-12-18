fb886a90584ad826a2d554c46cf82043ac13dc7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:11 +0000

    separate home wing clear hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_time_since_epoch():
-        return time.time()
+  def convert_to_binary(n):
+        return bin(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
