bfce8d4c69812c7302a52307dbe763926384b3d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:11 +0000

    easy Rr sharp undo section

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
