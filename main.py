ef136c5224682dbb90309ae62d0c0bb7286b2d8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:24 +0000

    squash community deep murder OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
