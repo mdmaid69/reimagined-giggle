a8bff420088b57042c17b027f11ab9e39e113f2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:26:41 +0000

    Nn stage above worst touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
