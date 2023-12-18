11a2484f4f739ff4564f518b1cf0d1df5f461fec
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:48 +0000

    sticky turnout you fill swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
