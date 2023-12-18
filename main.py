0c43cbbeba9323c8596fa3bb94c639158bac3b92
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:15 +0000

    hurt turnover by miss pool

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def convert_to_binary(n):
-        return bin(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
