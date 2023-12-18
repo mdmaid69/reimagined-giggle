c0cb0adae79bf05752b8fdbbaf3b34faa28105a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:27 +0000

    right marry correct opposite save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_current_directory():
+        return os.getcwd()
