3f45cbc687c75047aad12c93c247e498b3bf1072
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:31 +0000

    lean obtain desperate basis from

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
