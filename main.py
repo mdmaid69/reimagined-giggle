85c4ae813e699eb969d957c952c1309fa0b2838b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:24 +0000

    bite laugh meanwhile some circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
