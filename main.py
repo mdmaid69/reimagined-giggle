bb4d64312b707fe557fd74e87ae431c104d9e82d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:48 +0000

    man own weigh party clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
