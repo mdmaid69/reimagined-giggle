24ddcbfae80b7142c5a38fa1fe457ca4dfec6c90
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:43 +0000

    never characteristic hollow major train

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
