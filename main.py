6e6854e36886eeb3fe7f0b8268da4e8809462f8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:03:54 +0000

    taste valuable dull personal grade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
