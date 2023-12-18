e450c1d38d3d3e5b1e8d0bdee4b8d4fd2b8be1bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:12 +0000

    programme in foot remark calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
