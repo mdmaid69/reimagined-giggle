d103aaa400d9cb520494f6f9ec436f5dac054fdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:22 +0000

    really active bill friendly country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
