3fc6bb10c218e7d5abbdb433716a3bc663c6595e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:49 +0000

    within reasonable getup contain breath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
