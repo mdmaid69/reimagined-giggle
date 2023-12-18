a4177ddf5d7b1705911693e9aa4713de293a63ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:04 +0000

    flight ride judge attitude ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
