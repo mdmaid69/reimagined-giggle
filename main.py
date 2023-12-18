4a9b17cef7839b0166663721e76625b841e38c15
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:20 +0000

    front underneath switch recovery happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
