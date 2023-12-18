4967e91596bc9e0fe3f8c4ec99969ad2d6fbb73d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:02:43 +0000

    already means either comeoff emphasis

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import unittest
 
-{body}")
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
