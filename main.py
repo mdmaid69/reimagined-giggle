b51e7e2de932ff55842711d5762cf11b98b348dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:35 +0000

    among lost tense finish develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
 def get_array_item(array, i):
         return array[i]
