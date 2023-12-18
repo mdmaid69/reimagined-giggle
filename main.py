7fba27a0c8c4f0e9a28bde1be0b05914ca306655
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:54 +0000

    573entertainment sort maximum height heavy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
