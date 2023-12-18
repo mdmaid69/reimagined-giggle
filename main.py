924073ee5532a9e673c7203304889b65b9dcc822
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:11 +0000

    finish opportunity front disgusting better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
