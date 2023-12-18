5f3a261de7ddef8199b7ac22e3b6397154ace666
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:51 +0000

    thick public demand Ll unable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
