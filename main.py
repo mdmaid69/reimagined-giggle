4cb68573e8c2e15018693b285cb5f08179bbd74e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:10 +0000

    breathe pay intend deserve rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
