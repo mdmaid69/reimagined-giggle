259bccb5cc8acf21460d2207b82a9fc81f0b9f96
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:54 +0000

    somebody bunch refer average over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
