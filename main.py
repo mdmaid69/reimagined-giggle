de8a88cca7ead4bcf7c8cb620625d2fe33eccf4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:06:50 +0000

    back joke thorough solve one

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
