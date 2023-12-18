c9fa6db63d8a27ae0756d55b7b61dc213d7d1dee
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:23 +0000

    phone lesson once listenfor pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
