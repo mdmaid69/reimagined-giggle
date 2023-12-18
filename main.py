3cc6660251c0e83c187c4016d4b8fe1a82b9533e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:36 +0000

    swing find ground quantity how

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
