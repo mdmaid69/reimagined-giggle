9c697e9fdbd31180a9297f8af43cdd369f2542fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:11 +0000

    fire negative clean moon still

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
