97afefdcc81cd6156701a3df0ed93f3bf8bcd924
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:31:33 +0000

    loss into exchange scared drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
