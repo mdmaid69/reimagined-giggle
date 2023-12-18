03494128c8ff13269818b762196a1e57c9086454
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:24 +0000

    remark large middle getup market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
