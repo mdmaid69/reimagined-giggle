25f657caec378f004f4d252096799f54e4dc1d27
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:58 +0000

    cost learn nothing practise snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
