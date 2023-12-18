729c9330a4cd11dd5c47b137c5f0f9c4e3ed099e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:55:40 +0000

    ring wide target damage slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_power(work, time):
-        return work / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
