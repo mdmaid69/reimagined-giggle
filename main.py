5a87eb590a7295073641911e2268e2daa08102f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:09 +0000

    tonight test discover takeaway anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
