b0247bd37502c37e3e74a16619ee2d3e5d44c3d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:17 +0000

    bunch about keep prove mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
