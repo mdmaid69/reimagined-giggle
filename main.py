1e365b19a9d16cb9455f5c162dfd61e82eb8fc9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:43 +0000

    often say by run goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
