7b4c22034db91e61bcd2ed00a85756c23ff3bc64
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:41 +0000

    and better thinkof straight them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
