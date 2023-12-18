7cb5e23126a8d73d35309085d56cb312eba53253
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:04 +0000

    condition forever rush waste complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
