ea0b78fb309172d799a147b803ae528b89a15f7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:13 +0000

    mine wander comeback interesting anyone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
