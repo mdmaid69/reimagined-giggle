331be47a29582d0a0844a267aa9b1186ff749cbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:45:51 +0000

    imagination surprised warn label holdout

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
