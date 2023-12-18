8fd3437842b578ca83d571e5177c47ec370f9fb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:33 +0000

    detail will that professional punish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-name = "Python"
-print("Hello,", name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
