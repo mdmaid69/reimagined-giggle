b361a6334e5d8c568f176e676211b5436134290b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:49:53 +0000

    like rob sound rush fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+print(os.getcwd())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
