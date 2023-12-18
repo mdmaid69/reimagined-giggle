b540386ec91c4e79eac7de1e0271d219d584dc24
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:29:16 +0000

    through delicate niggaacademy lie happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
