518925a7964140d3fcadb415903af79a94fe8ea7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:02 +0000

    natural hungry warm convenient prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
