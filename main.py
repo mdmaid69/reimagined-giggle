a4df15ddf4e9437177ee9f55d42dd0beb3a2885e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:01 +0000

    fence workout news pipe feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
