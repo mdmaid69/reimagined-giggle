9df56c6e9a3fd483ad37aa5f2b40088471b91a16
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:09:47 +0000

    track tread little connect murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
