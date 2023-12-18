598b507b98348a162bb6498f5db8d5a8ab3d767c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:24:13 +0000

    release big towards next distance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
