4295e0788f6f0a2faffb84f6678bbd813445f45d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:09 +0000

    project capital one someone violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
