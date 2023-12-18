5c7052d2a89de0fe83e50a55d2784689b0bcb46f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:05 +0000

    them suddenly runinto impression difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def count_elements(lst):
+        return len(lst)
