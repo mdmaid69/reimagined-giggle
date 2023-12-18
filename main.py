22306f818aeab099ca1d1847097b04623fcf0729
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:31 +0000

    rich cut experiment it bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
