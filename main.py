5fc6e7c26ce4f7f14d7d3a9f8e1a0bc39f016bb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:39 +0000

    comment stage scrape reduction estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
