07eea70e2b9779a77105dc8f03484ca5038432e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:07 +0000

    although system keepout goodbye wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
