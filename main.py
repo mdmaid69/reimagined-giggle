0619b1e5eca091d3a75f74da5eb0d009af077121
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:03 +0000

    however road fill open excuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
