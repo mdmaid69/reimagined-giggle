dde843e9a3e717e8b806c5d9b003064fd2cc4f62
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:17 +0000

    bunch judge Gg shade squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
