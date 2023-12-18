015a33d003f53a4b6fc6f6f7391b6d54cb52fb21
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:41 +0000

    burst freedom cause trade belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
