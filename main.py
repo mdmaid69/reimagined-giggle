b503627fc011d0c78c5f591084c3e439ab0ea8ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:15 +0000

    store can police use wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
