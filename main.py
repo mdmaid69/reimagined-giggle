a6222da2214c84175b5a5b0fd431b73efe2fbd94
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:41:50 +0000

    round false argue until memory

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
