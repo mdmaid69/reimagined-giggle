995dd273d410b6a6e67ccf7f512021fb02748335
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:01 +0000

    whistle cheap movement comeout come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_str(array):
+        return str(array)
