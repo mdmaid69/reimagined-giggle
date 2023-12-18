35c5a3c566c1a3123a462827db8de2dbd428dd8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:26 +0000

    time less runaway breakinto thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
