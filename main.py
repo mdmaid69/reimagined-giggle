39613f108a528202514d9f8e7f1a105c7a385a18
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:20 +0000

    port return Pp allow blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
