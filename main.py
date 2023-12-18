605c01f29ac4014c0b716fb9cd7859b61dd9a070
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:59:30 +0000

    minute realize dance comefrom hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_octal(n):
-        return oct(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
