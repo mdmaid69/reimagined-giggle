316a4a4876badce3705cca7b7054b85831180082
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:33 +0000

    base regret geton loan offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
