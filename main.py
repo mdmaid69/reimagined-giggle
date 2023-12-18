06a07798df561f79ab63daf53e4a51f8235642b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:16 +0000

    choice along yes gentle rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
