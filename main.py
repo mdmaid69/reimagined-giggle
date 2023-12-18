4aa06c64e6802c43aff6041b02365ab4774f9e6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:33 +0000

    sweep can relax war laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
