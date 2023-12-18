f23e30da81f97c47251538e5ed9d2aa8a7f10dab
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:02 +0000

    partner of fold yourself nigga

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
