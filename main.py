f9e316c2f8fa5adda3a9ca0fe18a6938be5eae71
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:10 +0000

    fire language extra already fat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
