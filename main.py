2c1a1dda349795f522f961c078de203e0884f3e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:52 +0000

    Mm clothes guard afford note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_acceleration(speed, time):
+        return speed / time
