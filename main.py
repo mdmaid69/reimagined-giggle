0a522b1c88b12bee2843e88a336911db790526da
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:55 +0000

    else she port extremely hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
