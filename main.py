5474735130c67934830b47d4c5904ebdfe61d60e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:03 +0000

    rise common worth attempt reserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+print("Hello, world!")
