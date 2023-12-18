5f5ce34e6fcae7f93ed02153514814ac851c7b1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:58 +0000

    false bear press study plain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
