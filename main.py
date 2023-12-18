b4f751dec29cc10221c1df5179e8f04917d41c64
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:09 +0000

    food matter certain purpose born

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_work(force, distance):
-        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
+for i in range(10): print(i)
