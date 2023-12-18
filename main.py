d540784869db4d3f2b7a28da66045fd7b13b813b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:45 +0000

    tap drink niggaacademy whenever friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def add_numbers(x, y):
+        return x + y
