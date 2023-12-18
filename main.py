1dc32ec320785204991f8426b6f09d0ad476ae31
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:37 +0000

    march disturbing swallow recently cool

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
