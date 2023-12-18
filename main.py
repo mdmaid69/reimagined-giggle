50a2a53b6ffb6a0497158e993a868d8b680461fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:12 +0000

    software load release alive come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
