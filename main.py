3b2ec702487a48ff9e1a0cbbffb73a63ca42a8bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:44 +0000

    under defend so tall comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
