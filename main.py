448d7a0d36e820f568c28074ae694eb6f459a731
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:23 +0000

    clean smile cause damage otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
