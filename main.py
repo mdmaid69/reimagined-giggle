c302da0780d3851a8400a9b0d55e2aa44c63f0c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:12 +0000

    taste accordingto book scheme promise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def extend_array(array, iterable):
+        array.extend(iterable)
+def cube_number(x):
+        return x**3
