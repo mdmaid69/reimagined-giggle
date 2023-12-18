f0fb5b6eb9bcc96efd005de02d0c00ac0764b06e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:47:14 +0000

    cold mad pretty active naturally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
