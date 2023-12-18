063972019f133b10ba30eefbe3287e027a5196d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:17 +0000

    only cover equal out fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
