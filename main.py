05946da533838fe0a486496e6731287ae671299e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:32 +0000

    rest hit temper light lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
