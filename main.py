15dc120b97e70c1766747d1c7ad8a38f14b8fa8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:07 +0000

    stretch unusual spring bottom cope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_binary(n):
+        return bin(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
