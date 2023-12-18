6949add6becf89dcc3ad0521cebd0609a648ef68
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:33 +0000

    note probably lookup hurt getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
