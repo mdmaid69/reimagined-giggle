f7e8f277b8bb69afc74e26e8fb194e1b827fc81c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:21:16 +0000

    concerned moon off would offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
