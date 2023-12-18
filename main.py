c22111312be8e31244ba3b54206ec89a84c86ec7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:14 +0000

    direct switch obtain tongue board

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
