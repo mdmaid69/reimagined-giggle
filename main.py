d0df06bc03aa595b9fd25019e519d80cb48f478c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:09:03 +0000

    game attach speed same full

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def sort_list(lst):
-        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
