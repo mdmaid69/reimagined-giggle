c26d330c8f0ca33d2b19b47a6f74a5fbe2e0ea1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:51 +0000

    vision friend load store question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area(radius):
+        return 3.14 * radius * radius
