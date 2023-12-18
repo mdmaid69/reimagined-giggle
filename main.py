284dbde0ad15096f8b45321b385a3cf525f7e0d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:28 +0000

    funny neck brush last file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
