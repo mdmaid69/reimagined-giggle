36a7b411b54235437c4c2ac964d395bfa8a1aeea
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:48 +0000

    reduction serve borrow offer shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
