cfa6e99348cfe14104767b44c6aa5ecf1d07f8ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:15 +0000

    right content evidence common dig

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
