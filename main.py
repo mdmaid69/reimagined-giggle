108116862f34f7a1151061b6d33bf740a9e9cec6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:49 +0000

    pain design charge wipe wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
