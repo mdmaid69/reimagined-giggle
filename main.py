e8d3e9f2f0eb175a6fd58a680d9b7742bcdcc0a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:47 +0000

    ashamed evening cause choice spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
