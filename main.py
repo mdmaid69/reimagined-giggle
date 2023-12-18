3855fe2b63ea1b21c8a7979294a477cb143ecaa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:39 +0000

    national nose hello splash post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
