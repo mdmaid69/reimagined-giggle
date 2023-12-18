e035928d718434068381235ca77b828dcb080b09
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:41 +0000

    most company pretty back any

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def cube_number(x):
+        return x**3
