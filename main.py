8a72ce3db5a40dc3c70fb5a751a8ac2b980f9224
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:36 +0000

    sign message store considering bad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
