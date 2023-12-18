9b9ae6db6dd8f0b23968051d025dee6bd18f01da
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:30 +0000

    hurry steer whoever computer set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
