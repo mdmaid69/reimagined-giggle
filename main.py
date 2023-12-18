b7eb97ead5ac58d126676c0dbfaa26f2b6ead734
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:15 +0000

    about path die love other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
