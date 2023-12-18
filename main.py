c53cc9c03406cc7ab9d8bc4c9cff5efc0c8d7230
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:41:50 +0000

    myself through alive attempt makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
