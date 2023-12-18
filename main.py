9635142a37e95cd88bc8771cda0134cccee10679
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:22:53 +0000

    stranger along upwards course suggestion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
