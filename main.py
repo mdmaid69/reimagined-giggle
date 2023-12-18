25fab33d30a1f3d7eeef09d8186822a5a2aa177f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:26 +0000

    contain old keen bath organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
