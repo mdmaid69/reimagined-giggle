4aa608ff55bf400d75df0e40a3dbb78e6b3dfd1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:05:40 +0000

    challenge their aim fear welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
