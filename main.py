7ec4a11e3b4bd63075eef84747a801c5758d2bf5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:58:38 +0000

    behaviour kill release regularly provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
