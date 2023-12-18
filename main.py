7e67c29d3caf05846d089b522d36b10557e59bd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:31 +0000

    only study treat picture sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bool(array):
+        return bool(array)
