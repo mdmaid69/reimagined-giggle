8f10e4956fdd508bb324a355996380edc5bdaad7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:38 +0000

    previous talkback circle rely have

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
