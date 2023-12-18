2fb3092e82413da072290fca4ba23a28357c541b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:11 +0000

    definite keepoff feel scream before

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def multiply_numbers(x, y):
-        return x * y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
