c31e246454712a1961cf2ea64d9d6a15c0ae0911
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:21 +0000

    border importance punishment exit mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
