dc6d3a14bd5504abade3947d89feb20e0dcf4d5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:44 +0000

    treat quiet takeback warn pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
