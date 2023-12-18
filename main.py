11c3cef4b57554e09e791d67c32debf04611a2bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:25 +0000

    concentrate tomorrow person hear hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
