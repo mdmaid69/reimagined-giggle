b1321324868679ac1b170dd08b6266e698501101
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:25 +0000

    shock open connected man empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
