98ddeed5ff15df6cad8e21c42bba683fcef7a7e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:18 +0000

    now wake pocket quick successful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def count_elements(lst):
+        return len(lst)
