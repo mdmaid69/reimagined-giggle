92fac0a6d7110658e7789a8b3baaf7045c564add
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:45 +0000

    dig yourself club early centre

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
