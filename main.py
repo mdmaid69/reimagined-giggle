70678eb58b00f39ed1b13401e5b8e11b4eee9b17
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:19 +0000

    like blow plastic swimming your

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
