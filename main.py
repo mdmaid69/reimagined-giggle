7833e97fb37f96daa7619c0eb6ad08f7dee58549
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:36 +0000

    pass during blame cheap left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
