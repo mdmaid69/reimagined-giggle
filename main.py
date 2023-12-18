e4a0425d0844308d6f87de457a74c192c283c853
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:22 +0000

    because workupto will favour plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
