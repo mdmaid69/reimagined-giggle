b1a07071c90a69d3da36ed3cea31c999dec88bae
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:23 +0000

    everything ability instrument telephone hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
