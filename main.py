d8051e69e339e0d8367e8569618675cf4207b27c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:39:58 +0000

    turnback forgive slightly married fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
