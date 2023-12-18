68e10e5e6de656f82bc0121ced8234bd63a5cb03
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:17 +0000

    make tidy only free worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
