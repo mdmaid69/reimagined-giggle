8cccbec5da9da60bbd99fa1adc477e19a0641d78
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:53 +0000

    squash relation lie boring home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
