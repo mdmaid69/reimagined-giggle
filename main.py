a193b8ed39d38c3b08af7fffe75f6df754f8e3f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:02:12 +0000

    dig high law emphasize check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
