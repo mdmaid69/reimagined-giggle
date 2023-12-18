37d74bfac41c871e7abc65a4fcb5212f3142efae
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:46 +0000

    both turninto best except disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
