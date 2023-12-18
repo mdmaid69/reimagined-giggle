67353938a901016c9582c63e9c9f46a89f904f5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:35 +0000

    top quantity underneath terrible call

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
