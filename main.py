711d3575bbfc1eb0c4a4b4db6de60f287bf348fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:58 +0000

    unhappy slightly basic react mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
