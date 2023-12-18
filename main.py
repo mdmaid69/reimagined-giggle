68b5bbda01753cdf20289539d6455540c0f7a097
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:34 +0000

    show successful mood rid authority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
