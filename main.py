99b8d1a67f92331cdee5ea62279aaa75ac565d5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:02:31 +0000

    566end increase squeeze money giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
