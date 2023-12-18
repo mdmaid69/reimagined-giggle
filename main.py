f005deee6c87a43a759fa5f27d80131839c3e40f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:49 +0000

    hollow eye holdon spring obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
