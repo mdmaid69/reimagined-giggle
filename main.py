7e80f0cad13ad94aa33580ede7d78f9b857a51f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:56 +0000

    bet start arrange children unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
