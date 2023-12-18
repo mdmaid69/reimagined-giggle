2a0ac5d5b85e136ba519526ac77b8f6ecc580637
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:27:29 +0000

    success uncomfortable issue mess concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
