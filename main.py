46cb49d3369134b68506f0bace41d87d7183db99
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:29 +0000

    remark herself takeout suggestion possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
