d5ef5cc2ea8ad87623f97fb6f7ae0b1fb3c9dc1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:29 +0000

    unable grade born everybody else

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
