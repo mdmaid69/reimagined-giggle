ec1883e7f0c95bc0646a40482466a9af6b97cd33
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:46 +0000

    announce minute shock concentrate benefit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def greet(name):
-        print(f"Hello, {name}!")
+def remove_duplicates(lst):
+        return list(set(lst))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
