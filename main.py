27ae9ca53a139daa074d630e03e83b1d74e8fe05
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:49 +0000

    content talkoutof break payment perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+for i in range(10): print(i)
