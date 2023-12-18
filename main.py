1ff771f92f0535926d206ca20edc615bfa4464ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:37 +0000

    trip tool bottom sweet over

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_rectangle(l, w):
-        return l * w
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
