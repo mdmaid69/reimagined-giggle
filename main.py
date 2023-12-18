5430ff6397adfaa5ac1f0e02d59806a7ae59b586
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:49:55 +0000

    even along mind wander deal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-print(os.getcwd())
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
