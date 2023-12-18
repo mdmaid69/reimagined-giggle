7d2b50fdc43fe7f71e1c8f110309e0a2c9d98945
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:51 +0000

    office shopping guess snow know

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def count_characters(sentence):
-        return len(sentence)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
