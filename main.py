427e407a5bd57c5c211439bc751888f7322bd1bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:25 +0000

    list blue create either door

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
