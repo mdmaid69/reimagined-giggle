c846c82d0b9c8ac09992f42a6c736d5c19ad8d20
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:11 +0000

    separate suddenly trade wrap attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
