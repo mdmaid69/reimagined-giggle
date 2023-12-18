c88de73c7be3f587d28e285ed7b99527fb72799d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:51 +0000

    raw breakfast drawing build behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
