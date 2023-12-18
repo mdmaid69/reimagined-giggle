0d15f26289b74bd655d40bb6393b33a31642d210
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:37:20 +0000

    length put respect base circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_bool(array):
+        return bool(array)
