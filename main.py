0bd2f9a5ee86a57df5191a414cab4ea75ee76155
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:39 +0000

    winter cheek wide consist realize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
