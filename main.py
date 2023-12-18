cf80c731ed502d07be06e90e2f22bd140df00ed4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:56 +0000

    soon fast aim themselves near

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
