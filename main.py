1bb81245456b6b89b7498346c58c9f288d4e6d2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:23 +0000

    fair check part convenient worst

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def clear_array(array):
+        array *= 0
+def find_unique_words(sentence):
+        return set(sentence.split())
