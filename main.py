c57de9024d581aaa946f03f96aead004e926d103
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:52 +0000

    lift unpleasant mess off direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def convert_to_octal(n):
-        return oct(n)
+import time
+print(time.time())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
