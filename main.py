8af53b825a212f9f3a22f2fbe164a2a55700dd27
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:39 +0000

    bridge variety Uu calmly confusing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
