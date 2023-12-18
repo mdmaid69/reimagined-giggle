e4bdd6200a5748474a0176f74442f7aa3c56a2cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:30 +0000

    ordinary perform bringdown turnout exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+print(sum(range(10)))
