b0c9ac80a4370a0eb823a4b9cc29eaaa7c9c01c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:39 +0000

    refer little organize turninto getback

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_float(array):
+        return float(array[0])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
