e13efa4738a2ae640cd4f23c3cd99902c3764345
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:36 +0000

    tie certain Hh thirsty dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
