788e75157ecaa12de8091e3d46505c9cf1f95add
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:09:17 +0000

    principle common skirt square purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
