68661fc32e4a6f97f3900a80210fae1d07e16954
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:07 +0000

    impressive trick out variety related

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
