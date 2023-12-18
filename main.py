5f61a3afbec23eeb88f7dab6037902ccda69ac0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:31 +0000

    term male remain book once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
