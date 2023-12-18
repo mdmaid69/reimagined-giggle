5d558ed1dcfdc5fbe1a7b021cf4de5d1b2a9ca54
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:20 +0000

    rent error joke march squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
