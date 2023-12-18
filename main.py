4497b4a20b6528c9b21aea5ff6a1665f3d8bfc24
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:02 +0000

    freeze interesting sick child before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
