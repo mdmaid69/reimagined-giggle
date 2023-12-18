ccc1a6d45000e6fbbe8bc67df9b434d8eeeacc71
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:51 +0000

    chief improve determined contribute side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
