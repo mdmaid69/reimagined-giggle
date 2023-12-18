cfb3882f5c1c2bc7adf3878691331b2fea642aa3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:28 +0000

    class gather standard young satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
