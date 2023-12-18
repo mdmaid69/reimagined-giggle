c86b94cc17ecd0f30db79ee0ee23061ff7bb8c6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:59 +0000

    card holdout organize attitude sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
