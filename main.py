54ad7a72878f5039a61d05399f071378d41afd57
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:28:08 +0000

    curious spend base path rescue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
