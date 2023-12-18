a45eacd02f701873d75cdb57af90e6403a3c7643
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:57 +0000

    plain cut border instead calmly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
