28bcf4718c571434321c0cc4a8b114156284e692
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:07 +0000

    exciting traffic question sing exactly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
