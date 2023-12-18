9093df7d21157359d4bf48d83315064376fc1c99
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:04 +0000

    exaggerate nigga translate possibility otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
