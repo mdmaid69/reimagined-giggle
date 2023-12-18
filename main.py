a873b3f3db97078fbdf2a9f8ac304d16359ee670
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:39 +0000

    official degree mistake hello story

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
