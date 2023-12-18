4ebf29e8285e4e12638e28c26c5277c20502bab0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:37 +0000

    my spread joke authority only

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_binary(n):
-        return bin(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
