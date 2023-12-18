1270b6fbc575958f8305c2cb11d15add9313319d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:54:38 +0000

    exact light will demand sore

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
