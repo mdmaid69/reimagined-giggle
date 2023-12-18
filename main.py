427a0eeb4e2b4daa2451e36078a6117f8f9298ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:51 +0000

    excited colour read container inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
