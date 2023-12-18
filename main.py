9c8031f9689ca509e13b772a86f296dae50f832f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:22 +0000

    true shape deliberately round keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_stack():
+        return collections.deque()
