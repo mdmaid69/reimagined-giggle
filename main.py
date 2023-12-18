ea205c3140dc1309ea750192b4f95d8818c9e462
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:24 +0000

    slow trousers crisis by build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
