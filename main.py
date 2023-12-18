134cac37b401cf2d8d45ecb821a7e6c0459b919f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:39 +0000

    exercise address last careful journey

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def calculate_area_rectangle(l, w):
+        return l * w
