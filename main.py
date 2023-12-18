83ce3db71bfdd5b3ef4fac8ab0c5304c9a4de9e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:16 +0000

    protest now complaint sweep interested

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print([x**2 for x in range(10)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
