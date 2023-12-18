81edfae26c337b316abb867a221683f3a49c4760
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:01 +0000

    sentence after neck star spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
