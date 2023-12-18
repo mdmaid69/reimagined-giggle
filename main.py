f11934d6ddcac2fdb5d54318c729baef83678619
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:50 +0000

    present my concentration alive takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
