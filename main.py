5d11c335929a80923af023499aaa77f7ac624cdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:44 +0000

    empty software talkback drawing bit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
