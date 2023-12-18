3035d38ba97a598ea2a1e08c2d437501b4df9a39
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:44 +0000

    possible satisfied confuse wonder moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
