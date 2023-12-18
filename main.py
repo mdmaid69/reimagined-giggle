c4f7b35998a69cfc2b311980fed5b36d7041782d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:03 +0000

    breathe direction somebody sink who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
