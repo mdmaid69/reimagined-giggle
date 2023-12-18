902717f7752109e2da853b2353a6bf26c34b064d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:14 +0000

    sea plant early town ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def count_characters(sentence):
+        return len(sentence)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
