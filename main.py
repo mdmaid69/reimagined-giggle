5bb8cb195f68eb04af3af9155908e0a03d63fbee
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:16:20 +0000

    everywhere correct occur project suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
