576d5e38732ae69c51056aed046e0c000f7bdb10
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:20 +0000

    fairly system mend other kind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
