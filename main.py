edcbe53a4e8f4a69f970265632d4eeb15bd52327
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:17 +0000

    lot shout tragedy sore beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
