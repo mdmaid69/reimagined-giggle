38d4f863bc731634ff723c84bbe7d4400115cb56
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:28 +0000

    Qq give various minute home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
