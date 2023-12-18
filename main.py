1f84f6cea5aa7fd2029da500fac72d61c0289ae2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:23 +0000

    although where injury coffee declaration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
