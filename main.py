168089605df7bee6d2e704a515aec5df62fe394a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:57:54 +0000

    capital handle part bath frequent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
