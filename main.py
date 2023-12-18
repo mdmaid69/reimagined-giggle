09eff04edc0e6d43676d06c21895e6c1d7d2f62b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:20 +0000

    march part clear from middle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-print(time.time())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
