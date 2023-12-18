a3af61c4bac6af915147e9e79bed741bb3e3f055
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:07 +0000

    impressive smell sensible responsibility holdup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
+  def is_odd(n):
+        return n % 2 != 0
