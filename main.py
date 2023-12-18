05cd00c34fcee8bd46a6affe48664ac336a168e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:17 +0000

    circle anywayanyhow close water copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
