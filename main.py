3a229544a9e41a162181573167376d5acd78ee62
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:48 +0000

    neat enthusiastic trousers pollution regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
