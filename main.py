dc94cc2aa6a4485817bd349167112b213af5f585
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:45:47 +0000

    than extra round collect reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_list(array):
+        return list(array)
