3f1cb7cc6d87a09ccfea793fa945a396873ccf5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:15 +0000

    trousers study permission love unite

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+print([x**2 for x in range(10)])
+import math
+print(math.pi)
