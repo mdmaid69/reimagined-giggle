6c3284fa76bb28122aa6d247aa8a520e75d1c89d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:31 +0000

    debt achieve language kind reduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
