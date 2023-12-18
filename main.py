acecb3716fe0b1c034172f6ad30824cdde85fffa
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:15:55 +0000

    low thing funny borrow justice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def count_characters(sentence):
-        return len(sentence)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
