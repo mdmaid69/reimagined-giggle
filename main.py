7611129e0bddc593905c71359fcc5051362a621d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:22 +0000

    offence another worst deep somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
