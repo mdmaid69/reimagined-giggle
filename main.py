2eba31c86270473f6707d24d48e78ef67b926164
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:47 +0000

    friend report march complain deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import time
+print(time.time())
