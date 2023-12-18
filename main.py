694e5f568b1b4b42fa466b3c1dd25e7d339a1724
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:19 +0000

    nurse bush Ff wire tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_memoryview(array):
+        return memoryview(array)
