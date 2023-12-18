2e587da82f0488923fe0a229ba6221d372459043
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:11 +0000

    there lonely record party punishment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
