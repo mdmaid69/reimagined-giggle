986946df4bf5757c9b5012c9fc85b1f7d32a8b9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:45:22 +0000

    borrow none side plan outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import platform
+def get_os_info():
+        return platform.uname()
