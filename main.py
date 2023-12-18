3d0b2fc3b20de95f83469022d11223ac0af24068
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:30 +0000

    on stone note sad fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def convert_to_octal(n):
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def convert_to_octal(n):
         return oct(n)
