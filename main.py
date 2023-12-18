9db3187b28ff36c08ec8d1ecac1e2b27d8212271
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:37 +0000

    both unlucky drive reply poor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def exit_program():
-        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
