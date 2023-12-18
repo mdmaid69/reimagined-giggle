9ac81d91ec947c83d407656dbda855679f14ed03
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:17 +0000

    no comeround himself worry understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print(sum(range(10)))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
