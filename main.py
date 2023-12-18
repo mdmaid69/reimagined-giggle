580071d541359cd49c86a7ea5047200b9b6b787d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:30 +0000

    realize ordinary obvious promise minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
