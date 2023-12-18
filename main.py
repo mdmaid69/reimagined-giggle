199da8ac92c01969f0ec0ebfa6b79ad537bbe949
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:42 +0000

    thankyou there object clearly minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
