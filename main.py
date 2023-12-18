b8b8ee08be70fdc751c877d0e46e9bc3670be119
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:33 +0000

    book shut cause fit cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
