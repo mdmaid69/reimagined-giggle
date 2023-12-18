ac6c5362c8547d895d858cfc0ff459ed0e18acc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:19 +0000

    cinema lie trip mention tooth

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-from collections import Counter
-print(Counter("hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
