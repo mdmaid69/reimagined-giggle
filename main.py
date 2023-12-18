dc8b005a0d4ac64950fc8f5a5deda38f31ab2587
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:10 +0000

    throw sound count quick below

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
