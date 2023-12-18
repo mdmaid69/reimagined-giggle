5343b15778f38a8835afeff20d3e4d27f546ce49
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:50 +0000

    murder above walk best team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+print(math.pi)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
