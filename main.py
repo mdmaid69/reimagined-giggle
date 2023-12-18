fc7844a6ebf94ecc73d33385608b2a2a7a452d7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:00:52 +0000

    temperature subject anxious idea wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
