07b0625238a50a4aae7af9f605a97699a95120ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:46 +0000

    mistake pressure beside stamp her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
