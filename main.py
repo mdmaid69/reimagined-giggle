7d32302075fd57fa321d9b1aaa0bfc0589f017cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:04 +0000

    plan square young slice claim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
