4dc50386bff80ea0cbc2ae9df32b7abd6f0208d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:41:28 +0000

    basic relationship second gate book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
