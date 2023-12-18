ea33d0af05bbed09df20682b4ebefe77a3dabd86
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:26 +0000

    care to abroad maintain challenge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
