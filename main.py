ca2e3d389c4fa810314d90841c5c74c46483f11f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:02:13 +0000

    invitation Nn lead band increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def cube_number(x):
+        return x**3
