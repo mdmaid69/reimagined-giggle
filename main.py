6c9883f344353697f07e0cd716e56e72d19d4c9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:14:21 +0000

    climb reaction chin without draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
