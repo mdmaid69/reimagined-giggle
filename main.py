966d1280e77ace1e1991566894f3026ff8b6be2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:02 +0000

    common on earth transparent Dd

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
