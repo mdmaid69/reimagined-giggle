5f4bc771d2e017fe5a60103c298bbaada04fe44e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:49:50 +0000

    opponent brain common test giveback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-import sys
-print(sys.version)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
