b607ecbcab840b0536d18719c33585bdd3262c81
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:36:54 +0000

    pain Uu recent heart discussion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
