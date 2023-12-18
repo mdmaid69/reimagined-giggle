2058bd5953c4dcc3f78e1264f2c6b1cc6684f4bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:44:03 +0000

    interview exist kick handle track

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def cube_number(x):
-        return x**3
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
