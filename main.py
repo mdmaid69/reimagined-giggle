f67cb28279bc87ed656c24e97275159656cba018
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:11 +0000

    authority ability about detail account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
