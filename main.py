c3f5455c9b36ddd450e5ed441990bd38289f00be
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:58 +0000

    impression after turndown road search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
