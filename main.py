5dc3a4815467ece0bcf0143e17c9f2597f9c717e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:09 +0000

    quantity far spread off there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
