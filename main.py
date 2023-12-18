1d6a78f90cb2fa4bbc9fb9333cf4a137ea6b5504
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:56 +0000

    look hope deaf hit beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
