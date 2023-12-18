991fb99c7f753d7d858ef47f33862d9b5d0cfd4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:50 +0000

    jump practical run section achievement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
