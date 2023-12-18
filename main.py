8cde3d3f3d7d1520743dd178fa4c192edb346995
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:28 +0000

    circle small boring phone basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
