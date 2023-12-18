ffcef1019266d388c7a699a77e827cae85c6aaa4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:11 +0000

    shine risk set future born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def square_number(x):
+        return x**2
