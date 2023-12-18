bda90690de41eb8a91dc27d9fa111a9714d3ac18
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:02 +0000

    might concentrate typical ago used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
