1c665d56adfd4378f3fbf225c32c6147963a68c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:27 +0000

    smile whether copy university thick

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+def square_number(x):
+        return x**2
