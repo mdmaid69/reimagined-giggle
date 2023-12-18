585e584aa733acb893ff97b2b4a221005f03ac1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:39 +0000

    Hh independent mad certainly generous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+def get_string_from_array(array):
+        return array.tobytes()
