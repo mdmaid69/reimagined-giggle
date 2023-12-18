dcdcc7f262e942ef6c4378b44dc94c6143efb92e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:44 +0000

    holdon about lucky quarter why

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
