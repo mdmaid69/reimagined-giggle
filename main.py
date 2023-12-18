e7d18fc1709ea4f783c3bbdf9fbf7693ad4ef6c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:16:10 +0000

    runover study without new often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
