0cada92db08d6fe4af8a36fcf90332a4f9e76cd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:06 +0000

    mail able tooth fetch personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
