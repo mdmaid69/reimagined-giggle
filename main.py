8043936349a13509d5d43b58896e8cee0fc956c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:20 +0000

    place you suggestion image Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def create_counter():
+        return collections.Counter()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
