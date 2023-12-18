3101b8c21be9bd50fb2775205d5a15b6c7c07840
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:27 +0000

    private wrap call fly slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
