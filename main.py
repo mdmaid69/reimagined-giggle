3559c73568f3be2f0153e8b7b5a9490019fa8776
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:08 +0000

    have lookup behaviour Gg medium

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
+def add_numbers(x, y):
+        return x + y
