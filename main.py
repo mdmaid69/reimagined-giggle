9ae3e38e4d7f537e45ffd7028d462204f42d7916
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:49 +0000

    satisfied direction upwards herself Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
