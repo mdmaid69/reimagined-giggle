0c9cbf6844fd52f2e9e9e5dda785e12c75e7bdfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:21 +0000

    relative touch knowledge language combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def add_numbers(a, b):
+        return a + b
