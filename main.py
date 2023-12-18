e2ae024aeef73d65a9785ada6b7466c4461ed761
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:49 +0000

    war worth turnaway make confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_counter():
-        return collections.Counter()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
