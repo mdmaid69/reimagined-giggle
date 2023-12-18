8f8401fdc16424bb376b4b09007fe0ffbf86e850
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:53 +0000

    later fall by die interesting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
