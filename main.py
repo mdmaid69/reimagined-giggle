a1c91b3ceed3e7f42b2b71773ba7db13d0cbb129
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:56 +0000

    ability success splash soak murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_length(array):
-        return len(array)
+  def split_path(path):
+        return os.path.split(path)
