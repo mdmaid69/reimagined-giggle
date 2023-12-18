875c2f87a2d20fa86a510f81a97ca449409cfa12
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:15 +0000

    language wish sensible system tour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
