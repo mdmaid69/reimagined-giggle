8f2d2de7bf12eabaeb59ea8920f60b2125d1cc06
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:52 +0000

    warn closely guard dead clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
+import array
+def get_list_from_array(array):
+        return array.tolist()
