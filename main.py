d685108ab5bfc588be85f113a25ae3540e250cc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:25 +0000

    target something squeeze space flight

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
