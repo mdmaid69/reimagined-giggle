96a6425d7f87a5f5dc8cb408b6cf7a5a791ead90
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:14 +0000

    doubt breakthrough win whom bringback

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
