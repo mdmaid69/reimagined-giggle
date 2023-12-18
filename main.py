dac04a59ed2e412fa358d324299b2ef0a045fae3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:48:52 +0000

    butter snow safe mend fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
