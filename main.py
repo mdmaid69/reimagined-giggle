7419ee2644ecd8c6d3b741cf8b4683099455651e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:27 +0000

    spill substance sky receive shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
