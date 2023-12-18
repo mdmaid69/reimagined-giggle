6b71ca14778c6f27df73692de03ea0a653e6f6f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:34 +0000

    square bottom write preparation offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
