559952b734ed1acb911ca0decab071d4be81b7e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:51 +0000

    reserve declare impression rubbish 566end

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+text = "Hello, world!"
+print("Uppercase:", text.upper())
