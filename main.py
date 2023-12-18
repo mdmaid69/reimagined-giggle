9e3ddf822226e1875cdfe4358f5940f13a39c697
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:31:50 +0000

    introduction often bill follow before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
