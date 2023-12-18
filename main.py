1a5cad266d60dc5f6241257b85a4b84af92e03bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:32 +0000

    mad pair Yy hope everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
