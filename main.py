683e2ec0d6cf39a7a0690c31c982ad5259dd6599
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:14 +0000

    callback thing write slice round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
