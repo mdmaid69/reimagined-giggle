a05ea7c004b94a354deb10339f4f21fbfe4badd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:51 +0000

    forward 564encourage represent bye ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
