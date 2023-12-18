60d700ea98dd38ad43139db0458732c81e5abbb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:21:24 +0000

    science different marry smooth common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sys
+  def get_python_version():
+        return sys.version
