411b02f4cc5e84caff89142a28b318a5a0b093bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:04 +0000

    appear like woman lie fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
