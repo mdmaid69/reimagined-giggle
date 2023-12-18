e83837bd686820a6cbef38f870b808b93ff3b619
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:19 +0000

    shame blind gap simple turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
