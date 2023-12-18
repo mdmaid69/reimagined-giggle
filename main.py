c9f4cde42955cdae17f978bdc4a9e0d3014b07f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:06 +0000

    bother floor different slice lesson

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+def sort_list(lst):
+        return sorted(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
