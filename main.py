bb443764d0cffdcad88d8e2ee35be71c569ab7ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:23 +0000

    book ring exercise charge sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
