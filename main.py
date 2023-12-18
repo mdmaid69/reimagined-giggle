15c2d335e374fa32717df4c2775728dd5e07e4ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:24 +0000

    Zz loose town information ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
