6e7caa0dbec43fafaefe8a3ad23b562de46296df
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:35:13 +0000

    dick spin stop style recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def remove_duplicates(lst):
+        return list(set(lst))
