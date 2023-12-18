6b76fb8b9a0b790b035fdf9f67203f0b773163e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:54 +0000

    tough describe callup open instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def delete_file(file_name):
+        os.remove(file_name)
