843715264528b0f61389e51902517f9ff6e88441
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:57:23 +0000

    contact regard habit pump deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def reverse_array(array):
+        array.reverse()
