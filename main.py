21849e11ffabb1598a9f693984e2509f6410ca86
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:30 +0000

    control practical giveup wrap exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def convert_to_binary(n):
+        return bin(n)
