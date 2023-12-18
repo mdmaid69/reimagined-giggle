d46d4b12c07ddc12e4d106a3a86dd48a896fd691
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:34:29 +0000

    secret turn supply wipe write

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
