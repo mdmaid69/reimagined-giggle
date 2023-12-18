8f826d018f89988d9a619155b7d2e8fcccde8161
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:08 +0000

    confident each bury fasten remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
