676ec1c9b37aaaa06fab4497a37d5aeae17c1db5
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:26 +0000

    thumb perhaps use how hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
