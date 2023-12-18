a1d1ffc4c40af9bde4bb71caf5edaa9e5525d0f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:39 +0000

    hurry neat note opinion early

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+print([x**2 for x in range(10)])
