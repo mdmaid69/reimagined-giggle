b0220f2d14779bf18f736fc24aa1c4117f7f23f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:30:02 +0000

    keen fast study threaten uniform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
