c21dcfd2fd1f1ae5bc9c3b669bf181965100d9a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:38 +0000

    single question boring new in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
