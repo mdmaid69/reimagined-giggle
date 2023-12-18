9df00f084d223070b62e1dba23390e985420dff2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:22 +0000

    where along climb limit year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
