c0fbf028cb6801e873bdfbfbc48fc007e9ad30d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:44 +0000

    match takeout message in plant

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
