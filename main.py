826964b2f6f06b886acd081a4e7ad392e74a9fe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:35 +0000

    interested exploration pay my crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def remove_from_array(array, item):
+        array.remove(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
