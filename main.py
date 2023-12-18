f73fcfb9843955623a32aa04857861f357c259fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:11 +0000

    badly spend earth almost kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
