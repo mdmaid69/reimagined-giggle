93bd7a1d6056b7220add0d54c4aa7da29dbed5b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:16 +0000

    advice could present passenger record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
