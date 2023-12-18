f38b5cc0d7845231daf8a426aff709eb6316bffc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:05 +0000

    impressed disapprove fail suit my

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def reverse_list(lst):
+        return lst[::-1]
+import random
+print(random.randint(0, 100))
