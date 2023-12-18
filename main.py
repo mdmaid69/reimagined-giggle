dca6c09f437424482617aeca6a4e948cc473555f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:59 +0000

    refuse possible pencil scale respect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
