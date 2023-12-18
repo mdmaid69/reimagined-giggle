5ccb98573e9de2bc37bec42221498b5d6b62d638
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:44 +0000

    least voice treatment reply finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def remove_from_array(array, item):
+        array.remove(item)
