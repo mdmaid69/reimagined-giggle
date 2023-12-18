ac41e28181541588d581de61add1531742c2cef2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:41 +0000

    preparation century ticket reason show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
