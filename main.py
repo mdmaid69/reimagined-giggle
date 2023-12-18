b39755b05d575ed601e2ec4d17891ff571b57e0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:44 +0000

    loose upset nature comeround definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
