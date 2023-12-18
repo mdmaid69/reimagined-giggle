58bb48d9aad54753dbcb34d557b5dc60a4518efb
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:28 +0000

    mystery position apart rule back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def subtract_numbers(x, y):
+        return x - y
+def is_even(n):
+        return n % 2 == 0
