c2fbe12cc7fe21a95977937c58b060440b307ab1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:26 +0000

    degree partner part long will

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
