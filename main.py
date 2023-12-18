070a4c5774b07e0eab32782ad505640bbc0fc546
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:19:40 +0000

    Nn vision house punishment start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_stack():
+        return collections.deque()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
