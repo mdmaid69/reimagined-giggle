dae16eec9fcdf22b4246e8efee5c0c7f9bec62c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:02:11 +0000

    national below determined type test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
