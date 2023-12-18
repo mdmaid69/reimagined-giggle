72e90854740fdd327fd6c58c6f0c1b5db7a33d8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:05:11 +0000

    shut thankyou behind forward speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
