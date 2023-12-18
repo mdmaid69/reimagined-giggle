b7de2f36d13559134c5295553e1e85bcbf08bbe8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:31 +0000

    basically pleasant row complete attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
