643adfbaebcbf4ae24e1ac95360e46dcf80c8f28
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:36 +0000

    system our tool measure this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
