40001661f34dbf6bfc4d4b4bc762f22115b2aa6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:40 +0000

    rub speech wire any face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+import array
+def clear_array(array):
+        array *= 0
