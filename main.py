4a7cf1c0d20eaea80646a351572f4686a75a98c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:38 +0000

    jump board break union shade

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
