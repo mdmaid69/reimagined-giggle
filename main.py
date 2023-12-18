7eb32372131db80d5616daa49a07294c6e92b258
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:52:54 +0000

    makeup basic accommodation thankyou opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_queue():
+        return collections.deque()
