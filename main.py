8519c27640df1669c70c7607b85c0a74e982ebf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:32 +0000

    respect exactly demonstrate cheque or

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
