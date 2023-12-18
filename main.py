afbada453e6a5ae27a4de168fed643ae0bc83e43
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:47:57 +0000

    nothing area body brush reason

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
