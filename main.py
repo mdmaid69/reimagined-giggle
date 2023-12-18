f5e827ffb795ee8c298f18bfea1ad3f396d8217e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:21 +0000

    autumn second worry takeaway terrible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
