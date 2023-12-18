5071bd67392be7c244b3d777c7f0f4c43071302a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:18 +0000

    dark telephone Ll breakout quickly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_palindrome(s):
+        return s == s[::-1]
+text = "Hello, world!"
+print("Characters:", len(text))
