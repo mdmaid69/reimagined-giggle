d8e7e22fb590fad8f6b21eb43ebfba91649da27d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:09 +0000

    lot quit which by permission

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tensorflow as tf
+print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
