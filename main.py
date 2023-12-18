b970a4ea49ce43da096ae1c22d7cbd448fc8f686
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:28 +0000

    sleep brain worst sharp cheque

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
