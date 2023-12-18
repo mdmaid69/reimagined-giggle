2226fc074942024c2f2ec4bf15ff47a4681f1b1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:48 +0000

    perform tooth band straight talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+print(re.match("h.*o", "hello world"))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
