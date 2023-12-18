d1a945f07410bb0869b06af7e003b03f414c5110
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:06 +0000

    low car tough happen switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
