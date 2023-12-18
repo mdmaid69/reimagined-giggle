3fe6adbc861f763cc2404e121b911d1065b42f06
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:57 +0000

    place state stamp must interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import time
-print(time.time())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
