1902a7870ae49ef4908fcaf70e0e57ea6ce2f6d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:27 +0000

    hang crazy glad building tone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
