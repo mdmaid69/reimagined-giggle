ae64f3ea2c04ef3689b2244eb52ed42829ffd907
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:18:12 +0000

    lie thought gate short relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def count_elements(lst):
+        return len(lst)
