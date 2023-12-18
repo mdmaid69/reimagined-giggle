72558f0ad15cac58164c5de1f2e940324c0939ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:52 +0000

    lead holdon objection anxious break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
