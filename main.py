424998542874e59c1529b5037646a792effb7258
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:19 +0000

    week tap provide bringup protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
