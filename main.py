4d3787c0e5b825d4fd786144c803071343e24b74
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:23 +0000

    organize national wave throw convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
