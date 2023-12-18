4efcaa4c7be80fd922267a0bd3c8ae84e4353f6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:01 +0000

    trip order temper prevent cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_slice(array, i, j):
+        return array[i:j]
