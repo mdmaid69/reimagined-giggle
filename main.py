218aed1d83254067bd7ce201ba0c75eb3f87da87
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:58 +0000

    any shop left autumn race

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
