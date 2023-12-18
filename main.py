d7da9e79ad00f1bbe3b323827b88e815b77aee38
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:49:37 +0000

    ought boring minimum runinto fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
