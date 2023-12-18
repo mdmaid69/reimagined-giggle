fa6497470b32f7f504d7fce9ae19c922fd831cae
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:48 +0000

    since takeaway pain comeup punishment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
