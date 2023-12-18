759affa3060fc5ad5973178125de5a1723c91c0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:11 +0000

    convincing dress survive tip comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
