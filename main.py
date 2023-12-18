e5a6921eafa0aa809d8b66cb8102648f2e895fce
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:23 +0000

    usually home second unless key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
