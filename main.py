ba413ac3be9475268d972ccdceda80f267fecf32
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:00:54 +0000

    awake depend level tread juice

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def get_current_time():
+        return time.ctime()
