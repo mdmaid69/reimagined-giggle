a7e01764f274da12371a1687c8db5c88d5f057a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:36 +0000

    tall sting whenever attach hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
