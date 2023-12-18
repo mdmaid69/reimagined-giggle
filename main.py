bdbf31e61ed73a34bc261a52ebc2d82aec6433ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:26 +0000

    at matter drawing strong miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
