854cb3f4784874141d522633f6bfa5b08a875950
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:35 +0000

    takeaway classroom cold guilty breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import sys
+  def get_python_version():
+        return sys.version
