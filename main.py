b5d851570f197d26bb5362f50385ab43662556e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:22:26 +0000

    wild ring squash exaggerate cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_user_list():
+        return collections.UserList()
