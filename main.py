de553b2d5be5f79c5cb3de1457626ee14d0c6f41
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:10 +0000

    notice bar adjective ring cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
