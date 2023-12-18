bccf3c2c45c019b84513129cf4799d6e8ffb93e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:22 +0000

    regret string knock rank chance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
