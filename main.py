75ccaca43f9dbdde88949d9cec1ad7ca32b98c0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:06 +0000

    sing ticket punishment fit innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def convert_to_json(data):
+        return json.dumps(data)
