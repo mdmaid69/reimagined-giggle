dc85c1948b3a4e833cf6272b4fe31de0398b34fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:58 +0000

    turnback attach no prefer will

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
