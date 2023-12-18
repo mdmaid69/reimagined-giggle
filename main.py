a8c3ba405e6e9df07935c7a16f6e8b4751af8946
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:27 +0000

    nice snow forget bag bet

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def cube_number(x):
+        return x**3
