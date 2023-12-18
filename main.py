e586199d205c60c0a08b01a18703c24749a885a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:46 +0000

    doubt want soak shade trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
