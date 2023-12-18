e500c3b415b8dcc961978b7e987d2a3fbd73698d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:33 +0000

    justice tradition disappear past there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
