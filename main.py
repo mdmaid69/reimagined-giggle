1c968250c959f5323783752b526b79282ee5d171
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:25 +0000

    himself suddenly sky suck special

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
