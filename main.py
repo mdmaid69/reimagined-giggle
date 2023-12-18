3eaff28a56a6ae6a7ea343f24427576bbeeb2cb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:38 +0000

    everything post manner description few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
