d511890fc0ffb0c74e6398ce6c221574181af706
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:22 +0000

    cost serve shout joke lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
