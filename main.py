4bafdbd6f85025e3d3a986ea8c042bdb1e64fb66
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:57:17 +0000

    cover new yours anybody by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def convert_to_binary(n):
+        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
