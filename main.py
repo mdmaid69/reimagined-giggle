92f30fdab1ce1b859653575bfd310253c8e65da1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:14 +0000

    nothing cope no Cc flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def cube_number(x):
-        return x**3
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
