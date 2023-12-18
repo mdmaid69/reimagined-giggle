e0d47368d0238a54608eca47d16363714ba2b965
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:33:11 +0000

    explain swallow settle connection nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
