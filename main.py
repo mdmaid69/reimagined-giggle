9b8a9e03129d8e9c68bbf6ba60f8f67798e67b5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:13 +0000

    secret public impatient need goaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
