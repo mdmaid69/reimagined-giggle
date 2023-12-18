e8c7638e9254b0b493ed90354da9c1cd1e81fe68
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:05 +0000

    destroy subject travel light clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import collections
-def create_queue():
-        return collections.deque()
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
