db9c0debfc046eb8c8783945e1fb9bd15d39bca5
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:59 +0000

    lead Tt ours fall office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
