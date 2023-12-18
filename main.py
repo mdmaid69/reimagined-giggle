2e9d327882b387486cf42741cd487ab473c0eab3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:29 +0000

    between carry circumstance down uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
