bda9c895cebe66c470f963f2e28c6abbbbbc6c84
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:50 +0000

    hope exaggerate confusing tired waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
