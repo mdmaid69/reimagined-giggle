1d5057301d633065f7bdfbcf6172f810d1628dc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:14 +0000

    when near complain give cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_density(mass, volume):
-        return mass / volume
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
