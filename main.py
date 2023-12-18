43d5399012a7bb0081430cbfd79e40c63bfe2ddb
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:50 +0000

    round authority each test closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
