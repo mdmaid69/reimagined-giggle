f805a9addaa50329326b19a08eb6441123e928cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:57:02 +0000

    shame keepoff before spare few

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
