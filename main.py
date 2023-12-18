c579c687a7a47270668253775b56e829a0dfcfaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:01 +0000

    help bus regularly limit boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
