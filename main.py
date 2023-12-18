2813e16a2a61a91d5cbbc6178fabf0d0ab675e34
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:22 +0000

    stick light jump hard Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
