94fae60e007db3ab0976f319df3f85f0e2248fb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:40 +0000

    absolutely maybe mass club seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def append_to_array(array, item):
+        array.append(item)
