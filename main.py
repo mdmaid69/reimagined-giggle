a782ebf6987b8e5bc8126da786804f0d962643a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:22 +0000

    several rather this watch getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
