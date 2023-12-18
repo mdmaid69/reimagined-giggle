f4ca725cabba9b359321110a15dbd25fdf24b440
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:22 +0000

    both rare machine prove chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
