a59b1cd3d34c1ca663919c4e44cf9ee4778916b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:01:22 +0000

    spend include comeup difference occasion

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
