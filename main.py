3771718547913f6318c3bd173eeaca17c09cf49e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:44 +0000

    smile unlikely sign dream regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
