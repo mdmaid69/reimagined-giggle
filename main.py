5c705ac876c95ab73d9e722827319cb82f3281cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:56 +0000

    fall report prefer murder force

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
