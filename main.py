d65fc0a46572a42eee86ffac63c89147c948fded
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:56:39 +0000

    genuine unable priority until rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print([x**2 for x in range(10)])
+import math
+print(math.pi)
+import array
+def reverse_array(array):
+        array.reverse()
