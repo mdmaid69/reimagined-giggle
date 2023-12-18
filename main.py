5d56dd200222bb5dc4071d60e66ffecc5073ee9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:24:11 +0000

    congratulate opinion stupid boat definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
+import sys
+def exit_program():
+        sys.exit()
