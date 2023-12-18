d7bc124744bf5515c6f2a7c49ce99acd5e20123d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:39 +0000

    pin spring geton arrange impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def cube_number(x):
-        return x**3
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
