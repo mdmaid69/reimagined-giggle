48ff0337cac21c6ee337961ce7668719e8c08917
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:29 +0000

    suddenly ignore opportunity convinced foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def count_elements(lst):
+        return len(lst)
