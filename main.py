3de9dafdd0669a030cce5b4875acfc6dccc5269d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:26 +0000

    partner direct bye consist most

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
