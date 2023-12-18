fad65ff3f315b62ece4b4b88cfa15a9dfa7a6144
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:15 +0000

    bath shall university type past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-for i in range(5):
-        print(i)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
