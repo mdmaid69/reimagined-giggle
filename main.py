82644de7499657ddd5a32218c335e17b76ac8a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:19 +0000

    must value guard wall use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
