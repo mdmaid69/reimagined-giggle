43ec716f425c8a799a65fcfa223e2c82a10214ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:39 +0000

    government visit right sister bit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
