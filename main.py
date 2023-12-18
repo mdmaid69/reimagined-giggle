3ac4c989470d54da167d08a858d7c2dfc9120c9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:07 +0000

    callat apologizeapologise food make need

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
