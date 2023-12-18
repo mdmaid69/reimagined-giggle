a244550b39b8db0f871783b970a7b6094d0f09f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:39 +0000

    quiet smoke slowly present clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
