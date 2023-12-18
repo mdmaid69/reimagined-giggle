703959e5b8ee67176b14f43d95b02bcd3f142fa7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:45 +0000

    for still name command terrible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
