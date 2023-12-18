22188e5eb296ecfe2ae9195ace300469e330384a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:03:02 +0000

    regret male walk duty crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def cube_number(x):
+        return x**3
