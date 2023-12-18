f73132da0804fd99b7d1c0ef2e6e4bfd98c627b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:48 +0000

    field child fair warm produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
