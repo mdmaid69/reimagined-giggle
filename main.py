911244b60a0c03f136f22f952b5536e8099d1348
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:47 +0000

    unkind scheme clean suit supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
