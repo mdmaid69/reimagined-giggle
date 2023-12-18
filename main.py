0675ddc9b1892b899eb531322b40b7ff0e874b72
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:25 +0000

    bother swallow modern slide voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
