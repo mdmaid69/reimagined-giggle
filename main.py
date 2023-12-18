e37e38263cce5d67e5421758c188d0115a5251d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:25 +0000

    benefit refer fence hole successful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
