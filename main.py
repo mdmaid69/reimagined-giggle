be5545c06a2fb6de14b929f975c412df848435b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:29 +0000

    test roof although cause software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_current_working_directory():
+        return os.getcwd()
