ea78741a8e163eb7f9000d5f77843c64257b9ae8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:44 +0000

    visit cheat stretch extraordinary name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
