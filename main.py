2485e7632f02f6da59e9f843a88078fa9de7f407
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:10 +0000

    reduce false sign wave quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
