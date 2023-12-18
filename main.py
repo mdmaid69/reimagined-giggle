2fbc3b226efdc80b0c9b990a66cabacd10efa10c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:57 +0000

    cruel standard wide file hard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def is_odd(n):
-        return n % 2 != 0
+def add_numbers(x, y):
+        return x + y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
