dfbc3ccf8b1d11ec59f25afb7404b14725c8189d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:12:40 +0000

    common road contact behind rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
