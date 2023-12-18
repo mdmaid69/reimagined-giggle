be0b07e8621b61a47df2c89f6a862e9f5d8f8546
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:19 +0000

    occasional serious turnout water risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
