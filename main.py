81d3cc55e10e62140f75df0b92bf81f540345e62
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:18 +0000

    thorough approve trust trust scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
