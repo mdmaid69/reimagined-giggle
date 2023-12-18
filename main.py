64a3a2af628ac98ce22a655804bf91ba939be5dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:10 +0000

    deep face place past future

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
