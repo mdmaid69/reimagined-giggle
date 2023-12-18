e893c34b4f5af72a08942122333e406a820a6dff
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:50 +0000

    couple frequent emphasize either evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
