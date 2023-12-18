dc621247275c3900a9e6296e7114f7be270dec49
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:42 +0000

    class level bin interrupt total

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def set_array_item(array, i, item):
+        array[i] = item
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
