9af35c0cd84d04e5f821aceaa6685c7c56db14ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:03 +0000

    agreement prevent now bridge set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
+def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+        return json.load(f)
