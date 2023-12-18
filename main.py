8c54638cbafbb58e9c373b8c604aec3b62016fe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:14 +0000

    wash grade spend guilty achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
