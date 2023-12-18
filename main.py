512ccb7164b82534f97706ad17385cccf8adbb6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:13 +0000

    wall method image string kill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
