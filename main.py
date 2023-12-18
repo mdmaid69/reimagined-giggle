39f64317663fbe13aee480d2c22d7ae34a345dd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:20:46 +0000

    serve giveout mess bye pattern

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_typecode(array):
+        return array.typecode
