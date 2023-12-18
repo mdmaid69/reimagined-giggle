2098db97ea7b22f8f5ae01cf006a417f0222f8cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:54 +0000

    favourite woman top interest see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
