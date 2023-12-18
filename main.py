cd4e55e02aa0cf6f973853c35138ab67448f5a48
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:26 +0000

    even limit fact operate mood

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
