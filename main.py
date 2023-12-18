d02490b5f45cf368dfed78af7cdc0f2f187b81b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:53 +0000

    show guard welcome reject disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
