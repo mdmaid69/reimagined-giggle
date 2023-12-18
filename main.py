1a4bd9dd8b1c8ed46722e47bb06960f494ca4155
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:17 +0000

    fun flat generally back flow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def add_numbers(x, y):
-        return x + y
+def calculate_exponential(x):
+        return math.exp(x)
