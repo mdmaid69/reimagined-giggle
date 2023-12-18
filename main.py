c6182b7333fa64a872fc9ba30823b59654f24eb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:23:56 +0000

    row deaf here trip closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
