1e5682a4118a0b8cd3528cab28ced0de16454ca3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:30 +0000

    away improvement already some stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
