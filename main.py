ddd90ab62b79834a4f06d766f1eb523ec3901ad6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:50 +0000

    common develop Qq calloff hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
