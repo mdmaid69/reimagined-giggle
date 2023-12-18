dc021a791d0733b8ffd6937f18fc66c8973bf003
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:15 +0000

    closed odd happy form solve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-print("Hello, world!")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def split_path(path):
+        return os.path.split(path)
