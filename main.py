27e3cee07792fb26d039e9568ccaa085c9e38409
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:08:03 +0000

    different operation exploration wealth regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
