08d6594661a43bc3d48a4c149c3a47bd169a1206
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:31 +0000

    allow occasional opinion visit bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
