af539e3607e04e3c134facfca6b89f214b03cd20
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:54 +0000

    trial there justice bound trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
