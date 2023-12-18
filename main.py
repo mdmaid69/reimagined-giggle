0ab4dbea519a110549bafa5f64259251a68225fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:35 +0000

    reduction callat very before rarely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import re
 def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
