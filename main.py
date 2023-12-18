0f16736a43e9bd71d92e5444f24b0063087cb3db
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:38 +0000

    add partner hate shadow rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
