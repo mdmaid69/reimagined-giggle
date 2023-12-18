ec0716e8b6401bf03f9b193f841ba0bf5c8be04c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:31 +0000

    physical Oo comeup often blow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+for i in range(10): print(i)
