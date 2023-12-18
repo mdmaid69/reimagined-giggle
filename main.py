dacd82d53b1aaaf8088ce2ac70977066357db952
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:45:13 +0000

    objection pay deserve radio squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
