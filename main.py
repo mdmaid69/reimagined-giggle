a40b81dee9b8419104d89c4b6f2f0d90b41d58b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:32 +0000

    wild shout neither trial shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
