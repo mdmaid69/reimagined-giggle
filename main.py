900699a1377d83a07e858ee76bb9de5704fe20d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:35 +0000

    roof look rest threaten many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
