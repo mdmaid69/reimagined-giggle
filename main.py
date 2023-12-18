4ab8b81f079721f561bc5f698ffa12f7d45d0205
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:35:25 +0000

    mistake light meanwhile importance trousers

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
