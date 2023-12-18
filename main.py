e6a26dbec0e95c311ca8124b33028130861a98b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:27:23 +0000

    total meeting fail split shoulder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
