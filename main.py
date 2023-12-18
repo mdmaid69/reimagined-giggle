0b5e5d0f3991624bf26d48a946e3d4f4639fdc02
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:43 +0000

    mouth right answer during exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
