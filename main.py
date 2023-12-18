438f34becda4bc274273b46c2ab7e77ea8c32f48
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:13 +0000

    through that use flood style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
