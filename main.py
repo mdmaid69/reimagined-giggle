7b969540911f5b1fd3d9d0d2a1adad50fe3fbc63
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:57 +0000

    invite float getoff I matter

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_os_info():
+        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
