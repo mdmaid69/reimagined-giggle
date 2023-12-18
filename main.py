b0ab63e3e4faa875373e79ab6478e001e7d34a3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:16 +0000

    famous artificial rely post certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
