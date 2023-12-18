f11a2efc0c094aded0e0b948667c5cebd64c3301
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:00 +0000

    what turnup listenfor attract hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
