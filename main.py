d9a04974a90031a6e1d0b7d962949e47923a66a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:08 +0000

    after extra lunch warning wish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
