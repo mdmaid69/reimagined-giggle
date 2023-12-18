cf9242fac5e7641012e159a76790ec09533a5d23
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:59 +0000

    check really the shop considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
