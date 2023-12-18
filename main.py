109e67215b8c7d6437f1bb9a2fd0c99fbf467ccf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:42 +0000

    protection uncomfortable disturbing tidy she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
