8be57def3468b8efe72475b4d8f3d01918f92a6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:35 +0000

    test petrol Oo sport floor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
