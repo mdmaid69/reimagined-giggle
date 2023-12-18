3bc0ad30631cf0e241880baf4b5e0155b0a63a63
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:22 +0000

    square dead to ride worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
