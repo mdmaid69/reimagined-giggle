ee554822b0ecad4aad254953d56a5c3030a252e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:23 +0000

    us sort direct figure wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
