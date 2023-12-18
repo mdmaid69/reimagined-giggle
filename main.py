951de53db2fa883c0125d49d8421ec2fb38dd905
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:57 +0000

    knock suit shake father against

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def add_numbers(a, b):
+        return a + b
