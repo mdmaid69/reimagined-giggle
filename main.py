125acfbbd9d13f37b6bfceb3177acc61c01bcb0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:14 +0000

    foot guess breakthrough slide pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
