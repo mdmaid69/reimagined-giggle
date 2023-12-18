b7cbf97a6b8615c820cb5bcbcdf90082cd72393a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:46:25 +0000

    strange hour practise before tradition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
