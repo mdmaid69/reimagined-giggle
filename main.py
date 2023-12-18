70a02aafb9299461896007a0792f4f835249be25
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:21:07 +0000

    attract store nowhere correct side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
