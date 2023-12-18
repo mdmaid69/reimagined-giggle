e5a3d3c3081a4f0a47e04bcf1640b9eb6f60e32c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:37 +0000

    react injury pencil wheel suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
