edc12b52fde5ab6619a433036bb590f1ebb32483
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:54 +0000

    little trial keepout suit sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def remove_duplicates(lst):
+        return list(set(lst))
