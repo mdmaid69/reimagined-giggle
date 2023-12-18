a152b270eef45ad0803be61737b8794f0508188b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:40 +0000

    target perform climb ride concern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
