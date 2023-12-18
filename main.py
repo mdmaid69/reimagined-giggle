6843d3fa1d9acec8606cdab745d04360cb9d3550
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:11 +0000

    city press brown solve various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
