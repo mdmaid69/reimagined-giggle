25e070b1b6017904267c0438db9d1fec4d5475de
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:55 +0000

    fight team preparation building until

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
