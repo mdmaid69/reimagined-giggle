47428ac146e854c362d6d62ae598bc364ba06be7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:56 +0000

    total any coffee inside wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
