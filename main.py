758e786e9b9e2c3b6384362af9e5708b2f1f411e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:58 +0000

    fair bringup badly pin runover

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
