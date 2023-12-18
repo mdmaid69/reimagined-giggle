b315a03bd0a1c6fa6607678e431e025158b11e01
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:03 +0000

    prize company search little why

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
