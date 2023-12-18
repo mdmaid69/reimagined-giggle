62f6eacf9472546d0565adb49f5eb1da9b13bb3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:32:33 +0000

    in there already hide police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
