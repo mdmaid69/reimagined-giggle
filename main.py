e9b4af2451199db524c06361e2ad383317426491
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:24:02 +0000

    getaway drop obvious single Ll

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
