b317b9dc004516f87ecb80c8cb108efa7289bae4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:02 +0000

    keepaway worried section curious gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
