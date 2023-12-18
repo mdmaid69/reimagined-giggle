f5b0838dd84cc442f49bb7b4075ca2e07793a8ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:15 +0000

    move discuss develop ready shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
