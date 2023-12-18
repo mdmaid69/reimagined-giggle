332571adc0d83ef6ab1d8643281700ce5c465ec6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:03 +0000

    general recovery hurry damp during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
