1ff8e6e68ea1a37ab146828c23d416a3d8bd244f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:44 +0000

    kind above lead relax principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
