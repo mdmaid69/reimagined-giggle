df6c3d8b9ea94bded11fe3034f4d48e73d88d034
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:02 +0000

    needle history apparently guilty switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
