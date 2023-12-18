6cf94b385884c0395841a4493d5c95ef802c4baa
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:18 +0000

    stamp let turnaway cough laugh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_acceleration(speed, time):
+        return speed / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
