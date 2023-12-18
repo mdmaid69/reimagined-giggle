8ec800616dc4e5221571c4687823d433c25e6375
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:39:47 +0000

    keepoff word stage serve threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
