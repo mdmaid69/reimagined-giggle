667a02b4ce067d2efba24357de654024e004cfe8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:22 +0000

    comeover depend dull value odd

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_min(lst):
+        return min(lst)
