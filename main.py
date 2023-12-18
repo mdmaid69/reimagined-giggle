38abfe30744e8dde89b081762f9d9295b0bc2eee
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:33 +0000

    complicated refuse friendly allow store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
