41dee3901d5239539f4daa0e6a623018108e80f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:59 +0000

    honest smile danger demonstrate medium

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
