2483ba6f679de9998998567d945ab5bb3529df5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:31 +0000

    forward mind spread moment case

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
