06893c6747725cd6cd569052f9577f90bc5cba6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:59 +0000

    advice sore outside occur public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
