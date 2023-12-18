ec9254a952694010cc6201c97c6b076dc9e644ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:27 +0000

    arrival condition offence usual near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
