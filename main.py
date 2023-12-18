ddd779f8cb1d89f94a5b4b93e10bfea9adb75574
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:04:37 +0000

    grateful criticism why doubt content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
