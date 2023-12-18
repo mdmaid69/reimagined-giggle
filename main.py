1e445a7aec49a9bc06ac95f6b583ee24b63cb3fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:09 +0000

    pay accept platform comeback note

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
