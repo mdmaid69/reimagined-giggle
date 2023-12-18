013a60787ba6327235fabbe61d540e01c707e50f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:06 +0000

    read put sport goup depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
