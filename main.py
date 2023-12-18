60b3bdccd35c554b417f86f34b01ffedc8ba1e4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:00 +0000

    good callup advise pollution grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
