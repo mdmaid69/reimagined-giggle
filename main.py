e24c9195541eec737181e2cac46166a9ff937d04
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:45:57 +0000

    car mark occur set dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
