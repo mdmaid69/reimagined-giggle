04cec90efcf5bbee83358902b2a41f646e76a03f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:06 +0000

    careful thank heavy successful when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
