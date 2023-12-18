94c06c6c67ef769af5456f928f7287a5de243768
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:30:51 +0000

    favourite ground mysterious please come

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
