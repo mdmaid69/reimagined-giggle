5bbff053460e7364d2a9c017ba5032eb6444a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:39 +0000

    intend legal school exist test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def print_python_version():
+        return sys.version
