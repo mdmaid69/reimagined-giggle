29af8cce27c1b45979a51953c6ab150e41e03cc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:17 +0000

    suppose hang opposite better cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def add_numbers(a, b):
+        return a + b
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
