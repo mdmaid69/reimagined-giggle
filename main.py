20149492fc181c47fbbee6d6141a84b4151596d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:20 +0000

    straight 567enemy still wall careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
