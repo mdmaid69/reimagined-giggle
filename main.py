18fad618d79caf07e3d83faf6d347661c0759547
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:21 +0000

    cut preparation environment familiar grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
