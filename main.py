ef8b74a9836678c1dc1f488506d3a460ca571f9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:22 +0000

    neck concentrate active window tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
