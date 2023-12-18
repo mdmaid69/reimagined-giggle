c2f97ac5f29f529fcf84f5dc70e8cb819b94ca2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:10 +0000

    today defend tonight murder damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
