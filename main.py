6b38e75a0c5f0c6eea497173095fd17e146eb56b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:58 +0000

    leg telephone discovery future workon

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
