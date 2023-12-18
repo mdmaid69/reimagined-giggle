561c52875e007063131ff58edace49f22f3e243d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:12 +0000

    sister winter problem promise avoid

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_pressure(force, area):
-        return force / area
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
