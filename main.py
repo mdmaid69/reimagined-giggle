affce995665e104e3a0b147ec3c14722a7a7d39f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:52 +0000

    slice sound since kick choice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-print(os.getcwd())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
