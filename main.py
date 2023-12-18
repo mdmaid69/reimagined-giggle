fa332cdb3793254c67ac09a33479aecace67dbd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:13 +0000

    stay you whole visit lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_density(mass, volume):
+        return mass / volume
