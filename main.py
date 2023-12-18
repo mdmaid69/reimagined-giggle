ba43f1d04858317bf1e901dd61abaae97ccf0023
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:05:33 +0000

    cook cope sad child watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
