ac94b46401bb04fb52a3252998f69f3b3ac61e58
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:24 +0000

    grateful hold tape forgive Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
