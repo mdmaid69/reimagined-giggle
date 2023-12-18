4f0a0ecd7a7d1d5ccfec591dfe5a20f7080a0c2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:43 +0000

    trap include upwards switch discussion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
