e508e2610ac105a4400af19a935630ef6bc410b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:51 +0000

    judge press dinner direction less

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
