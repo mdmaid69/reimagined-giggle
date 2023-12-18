5c3a397ed28d846c0c39f151af78955ed02a5320
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:49:08 +0000

    surprise journey congratulate anywhere want

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
