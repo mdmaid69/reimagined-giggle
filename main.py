52b3fe6196a709dc4422cd7307a95b6412d7ccf9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:07 +0000

    flow concerning appeal upwards love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
