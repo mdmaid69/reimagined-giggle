e3065a7ae7fc1cea76e9d6ca8593f5be214a62ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:33 +0000

    proud string approve conclusion himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def square_number(x):
+        return x**2
