597c13af263fa3d97ec8f5f3831fb5cb6760f839
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:39 +0000

    flood always quality this curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
