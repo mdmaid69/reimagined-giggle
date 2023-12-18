1d6dddc70db0506efaa2bf5a5af04b15e8f84f8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:56 +0000

    smile smoke recovery thorough brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
