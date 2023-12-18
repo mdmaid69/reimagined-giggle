c39b27a925b4a02b671dce4b7e89b9d15aa9035b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:59 +0000

    goout responsibility wander suddenly callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
