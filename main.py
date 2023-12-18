86b89b829cdf3fb25186e779ffc0a7b428250500
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:13 +0000

    turn possibility impatient godown bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
