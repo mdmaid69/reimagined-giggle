c2a5afdab46f83cb6dac81c75016cd0dca33f227
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:55 +0000

    bored himself release scrap 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
