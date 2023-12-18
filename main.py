ba36f81b30ded03aa215a9aab0c7d68c7b6506b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:38:41 +0000

    shopping crisis rain pick no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
