6a5e17a6a83f55dcb3b1e67ae0254da353397472
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:37 +0000

    waste release no independent plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_python_version():
+        return platform.python_version()
