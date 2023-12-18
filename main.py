9f53d22d2b6a161fab85dcd8718e84b1d5957e14
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:41 +0000

    instead drive ready principle national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
