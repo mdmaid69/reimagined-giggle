04977f007c5c9b33e35b40bfc22e6b6035ed25c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:01:03 +0000

    nor search Itself where break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
