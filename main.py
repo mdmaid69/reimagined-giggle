24aa57255321316c2e92b229eb15f08abd49d5fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:04 +0000

    everybody price loose possession section

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
