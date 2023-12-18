c8ffdf0d6818fd6d524474470f7077c533856582
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:06 +0000

    indeed plain him dangerous makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
