519993638b18692161cf17b74fc308bf711d98f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:26 +0000

    neither kind even peace fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
