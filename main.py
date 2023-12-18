dfadc7e37fe98a52f9a5422b6844a38c58b93249
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:09 +0000

    568energy gradually house respect price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_repr(array):
+        return repr(array)
