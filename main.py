7541f1cc4109460b361f6d077dac1804656fe1b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:15:07 +0000

    spill glad ashamed occasional tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
