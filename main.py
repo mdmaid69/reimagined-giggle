4193516f23d67e8c1085101d5ffb24968b346f7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:14 +0000

    memory concentration hardly knowledge final

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
