05a28e1d4359b6497d46aa8466e90525e6f73ae8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:20 +0000

    better opponent you city Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
