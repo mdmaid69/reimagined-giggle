79a5ab5ec3ef5e3922b85de384135346b8884610
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:23 +0000

    maybe actual stretch gooff tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
