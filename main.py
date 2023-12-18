5dd096dd8f10af9007f08a11878da8b1437dde71
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:42 +0000

    low main approve whole involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
