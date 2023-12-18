dfd4446d00356b19de24654691e25d559d8aa525
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:10 +0000

    port walk way half gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
