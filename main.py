d61c7182f0a4c2c79853c5a68a4e704428049b53
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:27 +0000

    pause chat concern preparation force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
