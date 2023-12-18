f0b7cae720b868659a28620a7eff90cbe10bd79a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:09 +0000

    glad smile so Zz dick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(5):
+        print(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
