5a1605806ce242d051d9402b61ac7334f5e0f386
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:14 +0000

    sad far fear rich protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
