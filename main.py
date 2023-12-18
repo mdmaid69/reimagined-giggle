9d25e6fcb267ef2a9f1892ffcd41e3377cb1c577
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:28 +0000

    wave hit scream responsibility follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
