4e1c0cf3045561fbdf74c100be570c8198fde137
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:58 +0000

    sense but healthy impossible march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
