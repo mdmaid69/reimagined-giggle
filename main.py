4d8efb0fc7208e6c7afdbfd600871366bc371b01
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:58 +0000

    train mother exit sell care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
