b6e7cff3668f059ab2ec561087a1f2dc05dec60b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:44 +0000

    large study bush much pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
