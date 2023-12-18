62c1c42937e55f600273077a682e94bf77918a87
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:09:13 +0000

    machine board declare rub advise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
