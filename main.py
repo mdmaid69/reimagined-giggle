dbf6e5508d38b10a3c725b006b0900285fa31f1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:24:59 +0000

    Cc organize produce apologizeapologise exchange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_length(array):
+        return len(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
