ec633f60758aaf2c5f13758897a309b19722ddb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:13 +0000

    feel room storm noisy possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
