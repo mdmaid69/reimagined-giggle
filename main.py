7237a4293bbebf9f83e33d15d83db9990cd00b0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:59 +0000

    pollution wind Oo attach tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
