bdfd966e2a7c5455c32a974e24f6793457c847f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:42 +0000

    passenger opportunity week Ee spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
