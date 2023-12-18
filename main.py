57382794b112376bb301aaeb28735e978c828c3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:33 +0000

    begin there rest uniform pity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
