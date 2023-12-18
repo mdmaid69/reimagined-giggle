5605f9e409593ce8008262dddc069e8eceaffa11
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:39 +0000

    pull point quality dark ashamed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+print(time.time())
+  def is_even(n):
+        return n % 2 == 0
