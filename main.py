5ba8c10e9b3e899a562f4454329aef8c16cf27de
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:12 +0000

    fear introduce smart failure receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
