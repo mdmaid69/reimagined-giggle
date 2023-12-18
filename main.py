9d17626c8373d1a4829f1fa6894a5dcdba385caa
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:11 +0000

    Bb treat boring either formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
