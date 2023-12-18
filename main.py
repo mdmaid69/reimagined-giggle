874abd9c0a10f5cd64c6b52b5dd3070a420744cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:46 +0000

    break hurry sign house include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
