c94eb2be8b67ff9d0c752acad567a0c5c2b50675
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:04 +0000

    accidentally card wonder hard see

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
