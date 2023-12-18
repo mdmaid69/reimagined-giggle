1ab462774c6eee90eb212ef1a0a647432a90b7f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:04 +0000

    recognize evening raw after storm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
