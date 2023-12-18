a18d385a4d525182c01297790556221ed5b88cdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:00:57 +0000

    adjective until noone may blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
