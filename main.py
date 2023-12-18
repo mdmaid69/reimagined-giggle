e09ef9fd379a7840b31f6a922e65afe4ae4f11be
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:45 +0000

    cry tight history direction exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
