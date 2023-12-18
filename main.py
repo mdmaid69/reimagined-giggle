d10f85a7d7e1e27acdc62ad3afb0d09328f9f866
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:17 +0000

    pure risk such 179bite hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
