4630f4817b788f728969fe3561a486b484b17735
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:12 +0000

    giveup close maintain dance open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
