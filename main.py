d2b5363ce1962fd18ed307d3e82bb28e083a7581
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:03 +0000

    eager lookafter the silent me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
