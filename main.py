7497040ed6a333af636b6c1e800d54a6db9e6050
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:46:50 +0000

    accordingto desperate will and whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
