891b0b8bdd943dca1c47b0664cb68890d5b1801a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:15 +0000

    takeback fold talkinto disagree straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
