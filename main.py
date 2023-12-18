a6e5d22d1d8c44954b9c3f4708287cc012638b74
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:39 +0000

    dick middle fast crash idea

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def append_to_array(array, item):
+        array.append(item)
