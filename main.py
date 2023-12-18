8688fe8ff75ec3bb9b8f88391f9b7515aaa8be1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:10 +0000

    dance lookback(on) inside coffee refuse

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_min(lst):
-        return min(lst)
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
