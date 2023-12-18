5b7bad51f3e7fb5459f0ed9c68e0fbb528e9e738
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:32 +0000

    even hope error main strength

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
