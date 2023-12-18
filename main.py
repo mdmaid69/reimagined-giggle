81e394b093f078db52cdeda0f92f660fb2e90bfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:29:37 +0000

    strike sick standard guess pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
