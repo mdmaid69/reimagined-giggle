2ae7d0057a6328a7284c3d6554d529ee92f329ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:04 +0000

    poor organization communicate mass fork

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import math
-print(math.pi)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
