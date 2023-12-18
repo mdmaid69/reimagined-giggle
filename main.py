b5695f10b09dd0ef39eb29414909f8da533ba7e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:52:25 +0000

    fly suit far live underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
