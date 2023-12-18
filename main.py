5361088bcfb08b0da8bbcb0d77a25287d6725bc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:52 +0000

    soak area custom squash late

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
