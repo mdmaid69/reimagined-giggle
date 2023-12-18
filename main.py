93c072df482e5a9e93db497aa23b85201b0c0b80
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:07 +0000

    sick value criticize approve polite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
