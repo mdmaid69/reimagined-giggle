1543c62c689bbcfad8fba35c398a81fb7b6997c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:54 +0000

    stone immediately beat middle boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
