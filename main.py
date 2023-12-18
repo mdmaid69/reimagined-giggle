5b234fc4b73b94dc5ec3b6f2011ee87b9b0ef516
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:07 +0000

    letter gentle bet proof press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
