398f3cf147266302aaebbc1d43240e342e266cca
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:46 +0000

    Yy hospital character brother measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
