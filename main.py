2c0b4f5243c503daa3f73f7a98dc9d14dbc2d60e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:19 +0000

    clue today please wash born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_buffer_info(array):
+        return array.buffer_info()
