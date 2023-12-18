c9a6f7f6fa4f4af3893a189ec799d0eb1962c104
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:34 +0000

    group wrong doubt save ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
