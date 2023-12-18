d64897f6357fb3ac2161b34a08a811b2a0c0b358
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:45:15 +0000

    road part stir story wealth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
