451f1e6110fc791065319b3df303e821d7d7400c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:17 +0000

    spin increase crime among myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
