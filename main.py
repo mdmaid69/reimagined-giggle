4a0824e54478ac1b2adc387ffc5912e58be6d943
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:21 +0000

    about shelter worry movement repeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_acceleration(speed, time):
+        return speed / time
