ec58a75630a92c2f87d7f51c5f5e204c1913a198
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:25 +0000

    speech prove much you plan

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_bool(array):
+        return bool(array)
