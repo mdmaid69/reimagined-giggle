3391d4699d156a38a41089d0fd1fe72bb3f1cc35
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:14 +0000

    once break bad practical hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def convert_to_hex(n):
+        return hex(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
