bb7cf8bd3ea8a3a24ddc79923ad7d441191e11ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:49 +0000

    mail proof upstairs fierce upstairs

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def count_words(sentence):
+        return len(sentence.split())
+def find_unique_words(sentence):
+        return set(sentence.split())
