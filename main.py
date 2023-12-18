adedb9debd2b6480041741859d17bc45a5ae9ee8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:52 +0000

    dick danger suppose shopping job

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
