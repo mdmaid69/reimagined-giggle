36e4035b1842425b51ec046bdba3ba369b29e373
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:15 +0000

    once doubt gate happy deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
