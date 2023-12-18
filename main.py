322040463cc8a7ed7daf3faa2fb8b8a131dabf3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:19 +0000

    silent run quick earn lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
