5017cad97fb64fd31bc9388e64f92893cef4aa31
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:34 +0000

    refuse conclusion life damage effect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
