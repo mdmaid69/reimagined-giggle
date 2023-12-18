f97d0fe210d84069586dead5ce6bc867809bdc40
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:52 +0000

    rough with negative occasion afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
