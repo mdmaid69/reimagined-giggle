ea710f74a1620f680953169286d96a456ffe4369
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:44 +0000

    safe regret bin set off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
