78fc8db2e27f0659c52b9660d95f3672783e865d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:22 +0000

    whisper foot peace defend attitude

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
