abb5c1cb0a9b4b08068bfd163b0b01182e341614
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:47 +0000

    turninto suck handle sorry stone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-print(math.pi)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
