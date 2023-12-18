82c1b3d04a81f6e9550dfbcfcc45e4c083c70039
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:21 +0000

    injury workupto violence them each

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print(sum(range(10)))
