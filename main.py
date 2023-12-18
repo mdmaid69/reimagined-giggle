8d31ddcd1194d9b8e04c31599bd3667d844e29ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:57:50 +0000

    badly aim firm drive stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
