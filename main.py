eb28c377b059bc7811b21fd2d4666d6e9590ab11
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:31 +0000

    negative we shop beat inquire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
