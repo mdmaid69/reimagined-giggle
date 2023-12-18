c53341adcc54e6abc904ffcb247c1a5b520d4cce
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:59 +0000

    advise wish who gift pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
