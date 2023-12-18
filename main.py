666a99b9faf293b25b45ab718c32cd7c48c86b92
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:28 +0000

    increase concerning begin hurt keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
