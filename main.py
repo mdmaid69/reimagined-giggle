7ac10bad43ef262484ceb9fe05ae6b0c7ac612c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:18 +0000

    ask friend official drop control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
