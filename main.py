2e10672d26e796fe03a99d5cf3a403fa6e9d08ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:58 +0000

    pure guess thanks earth tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
