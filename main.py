8972b20c24e14dd0b56ec3ade27de43176059b40
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:21 +0000

    vision confidence avoid calmly rarely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
