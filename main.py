cdd45934e7bb8849e81b8e41cdd98d48ef98d588
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:14 +0000

    poor impressive so official your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
