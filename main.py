326bc3f08c6913c0eefa0b31002b0f55c5764240
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:26 +0000

    blame several plenty love disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_power(work, time):
-        return work / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import collections
+def create_user_list():
+        return collections.UserList()
