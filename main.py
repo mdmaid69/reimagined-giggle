ad7034c856e11ff90bb8750b8b7ece6bff8fffaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:11:11 +0000

    communicate mark copy voice spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
