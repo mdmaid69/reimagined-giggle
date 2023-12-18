d52365466a1041f984b94cd3d49bfe22b89f2489
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:02:01 +0000

    habit cum tape over demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
