24ef60dfb2be0df9bc6fbc48be0c4b5fa2c58921
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:28 +0000

    that equal typical urgent clever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
