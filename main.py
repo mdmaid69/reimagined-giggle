90660e5a5b2c18693c72a878aa53c2e426d56ca4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:27 +0000

    home correction watch section bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
