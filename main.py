be554e0969a2a43a7ac802a48f0037bd21e24a86
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:43 +0000

    giveout force recent operate I

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_octal(n):
+        return oct(n)
