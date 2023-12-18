f33429fc832dc3cb588291e4a9dc1dea3c769cea
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:11 +0000

    bound clear convenient discovery find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
