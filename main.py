f37ba9f46017e194f56e77f21381cb0c60ce3f2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:33 +0000

    book actual importance responsible task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def reverse_array(array):
+        array.reverse()
