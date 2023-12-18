8a4477e78421f2ff1cdff309fdb7e56e564f6cd5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:07:32 +0000

    birthday quiet soil pull activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_even(n):
+        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
