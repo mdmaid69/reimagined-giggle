dbcf5ad283bda6d0ff9ff4cc1a9485df1546053d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:36 +0000

    cause please protection purpose run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_array(array):
+        array.reverse()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
