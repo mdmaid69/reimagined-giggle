2b73f190904ba9244bbd7051d4385edbfdd81032
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:01 +0000

    someone admire example finally alive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
