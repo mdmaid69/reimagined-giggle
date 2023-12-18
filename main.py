ddc0c403cee4dbc47d1d3bed3633824a467513be
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:23:29 +0000

    notice appear afraid talk combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
