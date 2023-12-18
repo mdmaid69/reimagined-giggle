8223f7d09ff838c1770b79bbd218e96f7ed74f0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:42 +0000

    know message centre himself equal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_floor(x):
+        return math.floor(x)
