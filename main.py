33d7d21ed7c35f3723253e11da7ab12d7f70fc59
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:06 +0000

    dick rather off when variety

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
