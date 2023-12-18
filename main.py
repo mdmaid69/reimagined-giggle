63f25f30fbda1094551ab097c81b0771a5d82675
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:50 +0000

    common person patience snow bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
