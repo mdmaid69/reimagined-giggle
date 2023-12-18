f1c242c51c8e20a260ea7625ba9bb34e813399d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:25 +0000

    likely confused sorry feel mad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
