0f7798fe4f84eb2a430d299e646dfa54373589f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:13 +0000

    opposite juice persuade spite new

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
