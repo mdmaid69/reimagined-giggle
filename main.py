a488794ced8fd6f960ae9ed92a9a3b4e6b42be76
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:28 +0000

    separate some style represent tall

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
