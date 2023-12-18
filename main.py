cdf6362307e7b30e3d548026553677465a878d77
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:35:35 +0000

    recognize giveaway forever pinch exchange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
