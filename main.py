8fb18f8e4b44ada32e5cc07cd3912817c4736223
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:09 +0000

    nose pair matter collection chin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
