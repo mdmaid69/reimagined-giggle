bda518abca81385d8f17843055b07744228d4bdd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:40 +0000

    content 569enjoy history autumn on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
