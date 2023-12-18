df987349b0b6ca554d6494d5464456485a852e77
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:31 +0000

    pay spring murder tragedy cause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
