c3d659f9d987449e59af781e3ff1ab6383f0ed7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:05:04 +0000

    constantly professional life zero drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
