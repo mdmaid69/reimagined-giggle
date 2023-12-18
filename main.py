76c1747cd1014e4fcd586cacb6539bc5bb7ab23a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:56:12 +0000

    pocket thanks contents shoot fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
