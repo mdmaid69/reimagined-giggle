72bbe6a0c45f45dec90eafc373fa0108ea3c9b52
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:40 +0000

    back cover steep reduce goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
