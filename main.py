5c6a8f7f2a8d234d52740738c5b5b5ec1a5972ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:29 +0000

    colour as introduction crash herself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
