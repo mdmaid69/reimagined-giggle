da56501a9be63ae4acdcf4b8aec39c3216160881
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:46 +0000

    intend company neat question touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
