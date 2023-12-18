548fa11b4ad03917a35109b2b53654e17534b723
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:59:42 +0000

    belong understand our squeeze sing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def cube_number(x):
-        return x**3
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
