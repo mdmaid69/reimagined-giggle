dc8750c32687ca10c3f1f5ce4b62c01b49b2ec02
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:25 +0000

    treat hard part care thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
