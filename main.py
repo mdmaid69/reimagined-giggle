860ca3e30fa2a1b394b28a143bf9b22cea75440f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:04:50 +0000

    stage label nonsense total factory

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_work(force, distance):
-        return force * distance
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
