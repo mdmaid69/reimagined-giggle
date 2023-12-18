01307f36df8c0e40f3c5fc5191ccaea2c26f3fbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:19 +0000

    far with lucky continue operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
