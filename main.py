eab07ce232d29077a6775e3ff946fdd3a77b38db
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:14 +0000

    none love onto kind sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def square_number(x):
+        return x**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
