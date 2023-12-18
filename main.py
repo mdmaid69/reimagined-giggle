0ff669f9fe75a6bb8713086dea5abdbc16656a9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:26:02 +0000

    across level speak test stupid

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
