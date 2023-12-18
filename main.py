720bc42a497a441b11e71727be0db36df547b9c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:51 +0000

    female second remain conduct school

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def reverse_array(array):
+        array.reverse()
