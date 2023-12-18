3faf6d4667c53b10c3b160593ff937a0a57e0bc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:44 +0000

    own method uncertain extraordinary far

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
