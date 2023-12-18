9dabdf0e77f5706f405b5a5b772b8e62dd887f87
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:03 +0000

    detail price pair mine comfortable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
