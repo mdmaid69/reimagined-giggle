2afa2f6b61f4fb27eafaef723e9cbb12ba12218a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:53:54 +0000

    commit push once environment character

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
