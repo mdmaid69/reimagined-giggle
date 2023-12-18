98ee1630f1e0794cb9449ed636fd7431f1058cd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:45 +0000

    shine get opponent about day

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
