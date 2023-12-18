5c51212e68d94e5137641c7b1463fd6582cd9226
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:12 +0000

    meanwhile table central main turnback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
