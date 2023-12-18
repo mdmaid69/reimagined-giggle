ee472ea50bbda614853542fb798326f133accebd
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:25 +0000

    tape before fit type travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
