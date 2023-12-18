7fece5e24c88e60d6e48d57faaeae938a33d8bf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:54:28 +0000

    him window pleasure nothing closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
