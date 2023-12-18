9ff06592ef89ef5ce8ca53340e93c1cf2a260874
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:40:56 +0000

    release love fine pretend form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
