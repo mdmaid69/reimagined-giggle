7e5564ef9ed2d2a83c286372bbc5bb34969ab181
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:48 +0000

    exact rid picture central advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
