cc807ed7cb40e47335382f7cae95071d3a1a49ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:57 +0000

    fail repeat preparation correct goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
