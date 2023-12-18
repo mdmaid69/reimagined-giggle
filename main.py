b0ebea0711c4514e735f10a5fe953adb5c36a12b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:41 +0000

    goout demand fault pen interested

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def convert_to_octal(n):
+        return oct(n)
