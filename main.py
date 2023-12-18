505afc6e7f7d129c7490426552fe7be38db19c32
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:11 +0000

    action bill stamp further idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
