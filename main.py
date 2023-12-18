c19eb63eb72c4b8390a40a2ad9d07c39e385f26f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:31 +0000

    this rid mysterious justice worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
