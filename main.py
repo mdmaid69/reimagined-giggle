e9bca6e646e1f962a7a04db2d1714928258b9180
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:43:30 +0000

    settle connected bet enthusiastic himself

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def cube_number(x):
+        return x**3
