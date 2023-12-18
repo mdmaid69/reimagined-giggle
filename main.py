5cd844e9c983a25d210cd4a150638bb6005734cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:07 +0000

    brief insist physical file match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
