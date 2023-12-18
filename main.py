80db6fce2b1716bc0c1d118ea8381734e56fc295
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:13 +0000

    turnback post little extraordinary harm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
