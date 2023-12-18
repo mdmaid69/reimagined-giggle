d0ab1c85d1a71463150f41c0037a7c0167b6c667
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:50 +0000

    forget mess will healthy far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
