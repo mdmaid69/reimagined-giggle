dd8198c2e1703cc80e4a2532aecdd3bdb7179ef7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:30:22 +0000

    goodbye separate actual disturb position

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-print([x**2 for x in range(10)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
