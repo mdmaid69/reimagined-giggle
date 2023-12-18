d45357be716048c596e3c1a3292a0ebe17236d47
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:41 +0000

    pain human objection declaration crisis

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
