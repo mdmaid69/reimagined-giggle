7204e7fdf78cdc61d7300623eb98ede859d8189d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:49 +0000

    shelter edge duty separate influence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
