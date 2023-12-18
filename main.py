f4cf43ea09dea220744139a49eb7b534b49ae012
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:38 +0000

    thanks shrink change charge shop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
