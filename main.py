2c7d9e57e5a6bd86addeadd99452590728999c7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:36 +0000

    opposite commit trick need you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+def calculate_power(work, time):
+        return work / time
