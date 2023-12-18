f1b3297b479f958e2580069654c533d1069a20cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:33 +0000

    form speed mystery danger content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
