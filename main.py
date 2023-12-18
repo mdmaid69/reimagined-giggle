9be8ec6f23a76a40f59ae45178a6f8e55dc4bc0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:27 +0000

    might tomorrow fun opponent expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
