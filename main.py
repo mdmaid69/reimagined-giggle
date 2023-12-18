0c40248f47e0071ebe57ab85164d79a8b30c6ec4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:15 +0000

    table life Dd wind her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
