42af870d49127a1f47d3c37d16da87831e93c537
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:17 +0000

    contrast harm possibility interested level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+from collections import Counter
+print(Counter("hello world"))
