20632511e024151a6977467c89afb698351b2b5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:00:54 +0000

    false emergency bite exhibition mouth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
