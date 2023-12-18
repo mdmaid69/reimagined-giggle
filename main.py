197839b01cff4c654061b60104082e0e174f0bcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:35 +0000

    file I Ss guilty cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
