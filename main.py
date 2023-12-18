95f0cfc053d7518b8078970f211af56f56985fd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:17 +0000

    disagree field possible through knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
