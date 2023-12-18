fbebbfad9640da5abc205fcd2af266b7a1482160
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:05 +0000

    achievement mean world thinkof keepoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
