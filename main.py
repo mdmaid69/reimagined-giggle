1866042330b321105a1c2569ab2f67c021386ab8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:33 +0000

    factory lost remain shelter until

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_pressure(force, area):
+        return force / area
