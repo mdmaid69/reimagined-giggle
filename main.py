1f564f43e97935c4f9834ad9385e6c003f096152
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:58 +0000

    murder kick record underneath difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
