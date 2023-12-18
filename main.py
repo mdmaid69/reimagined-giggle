48dc47ea02bc0d2c01cf0d66b33fe3cb15ffc75c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:20:48 +0000

    rush friendly stage music fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
