b746924509427c20bbaa8b7c180f396fc535d983
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:48 +0000

    reach beyond mean delight cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
