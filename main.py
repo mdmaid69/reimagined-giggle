0f5fcde58860b7a6bc22d0580ad835f19e515bf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:58 +0000

    invite conclusion 567enemy repeat punish

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
