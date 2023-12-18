eaaa0fbf5d9fb18e6da82bc37930fede65d9d296
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:25 +0000

    correction free well sing constantly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
