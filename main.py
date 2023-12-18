87d9651370aad9be46d3638fe275527cfce5bea3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:50 +0000

    especially power head involve sensible

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_palindrome(s):
-        return s == s[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
