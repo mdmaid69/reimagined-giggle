323c4e10c8b542d5780f6f0b8054a17587481238
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:19 +0000

    roll considering reserve advantage comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
