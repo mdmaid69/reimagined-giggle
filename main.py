d3c28b38d66380944a5e65f7e07b28785c469e98
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:07 +0000

    motion bend female behind praise

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_density(mass, volume):
+        return mass / volume
