d1675ce364b3f3b8308dfb778c0a1be3b5c97e1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:52:33 +0000

    568energy exhibition sticky border wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
