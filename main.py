96ebe4828594624479ffc11e4bd33f9d7885434c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:33:22 +0000

    organize jump please sore base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
