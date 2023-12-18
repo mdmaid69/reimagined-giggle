cb89be8a274c6948dfc964f7c729490b18ac9660
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:20 +0000

    spread broken name reason trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
+import logging
+def log_message(message):
+        logging.info(message)
