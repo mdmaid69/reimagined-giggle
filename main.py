1105b9e00beb4684faa69c84048deb04a24319ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:23 +0000

    embarrassing comedown public ignore rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import sys
+def exit_program():
+        sys.exit()
