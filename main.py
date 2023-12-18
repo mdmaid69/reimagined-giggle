0aa1076c53e0a8ec03f8a638466ea2ef2f270cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:35 +0000

    worse past authority temperature flat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
