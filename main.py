8660f038ce6ca0f5d7b1a0442cdc63ccd5fc5a4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:36 +0000

    suckme left better goon accident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
