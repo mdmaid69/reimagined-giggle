8eec03566bbb00d6349383393f5e431c829b8192
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:22 +0000

    hard far cost friend pretty

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
