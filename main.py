60549500639b1a6873d59fdfcf94a0b4a01e4402
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:26 +0000

    height big principle fast emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
