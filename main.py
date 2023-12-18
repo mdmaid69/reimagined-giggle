094bc91c0aa20e096f18eca6a6fa01f6006deb49
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:03 +0000

    custom crash edge feed lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_min(lst):
+        return min(lst)
