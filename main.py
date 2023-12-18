bff0291b9fb24170877e1ca8f329e2e0727e02df
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:57 +0000

    attractive call face description turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
