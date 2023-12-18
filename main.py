42b2fd588b4398a92f92ea4f6a3b5cbb20224a2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:25:48 +0000

    upstairs image place rubbish turn

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
