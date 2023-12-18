f6227f0243c6c6c588f4ac6454c8542d766b1260
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:16:56 +0000

    sound either holdup rush safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
