c593f4582891cb66115e158047ab09343ed55953
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:44 +0000

    worth rough even still report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
