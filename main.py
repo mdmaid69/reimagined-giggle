e45f8cdf0e0dd2418df133dc13a94af32d46c2de
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:07 +0000

    true reply by trust by

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
