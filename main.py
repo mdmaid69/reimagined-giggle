343b285dda0a6db4eba6b0d90239b871e93114f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:29 +0000

    sort error position pride whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
