1c21bde989aadfb12a23caa4a1749f0b63788d91
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:03 +0000

    could shrink train strike beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
