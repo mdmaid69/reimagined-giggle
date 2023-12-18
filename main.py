a49aa9af00e11c566b26d51eef0bb9cab48492ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:10 +0000

    somebody anxious priority community plain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def greet(name):
+        print(f"Hello, {name}!")
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
