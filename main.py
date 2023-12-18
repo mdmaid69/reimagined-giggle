c21bcd7be3e13ce7975c203452e0930d02b99541
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:46 +0000

    finish comment entry practical famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
