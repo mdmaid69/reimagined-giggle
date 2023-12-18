71f5760b45c115a38259b420584fff0288fa116e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:48:57 +0000

    violence total its first quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
