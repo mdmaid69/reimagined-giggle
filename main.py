f724b2b944778178fed5e42c13f03d207ef39a9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:11 +0000

    badly ring dance behind replace

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-print(time.time())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
