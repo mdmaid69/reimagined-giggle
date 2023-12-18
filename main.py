4406727b9f88d4a6b9d6bba4e14482a2e685297c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:39 +0000

    admire below study first difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
