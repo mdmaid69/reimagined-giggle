94a5f2e7a1d9e445e9e8242c340620333b954f74
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:47 +0000

    fast edge anyone turninto communication

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
