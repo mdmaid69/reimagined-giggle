a4252f58e752f36bfcae69763341433a26402c85
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:52 +0000

    up company pride strong achievement

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-for i in range(5):
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
         print(i)
+        i += 1
