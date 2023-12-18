ae46952dc491ac014a54c45e9cb766303803f5ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:30 +0000

    everyone space chief book memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
