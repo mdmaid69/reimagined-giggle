da2355e81f0d07317a0e217bde95678ee1790ebe
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:28 +0000

    drop fashion fat right soft

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
