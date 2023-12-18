32d25c196e421693238cdbd5ee4fcb70c1cfb9b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:43 +0000

    radio goup different advice people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+print(time.time())
