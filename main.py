1a4ae47e0915e5860cd67f4467916744bad66a8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:39 +0000

    lonely become word journey national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
