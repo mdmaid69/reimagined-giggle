dc8fc9f895f4a74235cbe2d0ad65753d865fedb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:06 +0000

    strength comeout low hello switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
