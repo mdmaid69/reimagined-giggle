d816865a67c1eae6b4058357284a6e8326cdf4bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:04 +0000

    used ago fault identify real

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
