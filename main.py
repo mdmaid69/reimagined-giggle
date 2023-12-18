ac4f9493d1f2c55b45f984981b67ffbec2d3f339
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:52 +0000

    continue close tell trick behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
