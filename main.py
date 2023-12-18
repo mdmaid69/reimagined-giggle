78c6ff6bc453756df09b5642bca3058568f67eb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:49 +0000

    her stir cheat press number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
