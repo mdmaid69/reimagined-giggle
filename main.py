250536884a15f22a09580bf495e4ce30398aa021
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:15 +0000

    reserve standard quit straight except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
