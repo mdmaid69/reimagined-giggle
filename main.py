80c09e82594eb3f691f7e4168c6e0d0e985bf648
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:52 +0000

    564encourage motion stretch threaten underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
