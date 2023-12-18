d052915ccce1d7138aeedcd3b86c3794b6cb1db2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:33:34 +0000

    keepaway damp getout yourself dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
