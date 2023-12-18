cfc0dff8458a6dbd1b5d43584d6c4453a6a7eba9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:42 +0000

    call patient know in open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
