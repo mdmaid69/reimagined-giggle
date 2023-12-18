a26273f7cf8ab90818059913788f8399c9fadf61
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:57:36 +0000

    consist pour about pump breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
