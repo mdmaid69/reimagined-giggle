c1d528ca054d18d78fa70ac3fd040aef92c07e6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:14 +0000

    other Bb risk drive benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
