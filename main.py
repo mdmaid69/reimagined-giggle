ba84d247555196bddb87085e84ae017f548d1577
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:58 +0000

    shut similar rent centre high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
