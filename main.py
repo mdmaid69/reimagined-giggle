913c498ecafbbd9c38f5b789f7eefce5e5d16431
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:48 +0000

    cross relation unable careful runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
