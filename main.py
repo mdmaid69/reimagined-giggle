adfe34e3d8efcc5d98222fd040952fe03d59ab35
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:11 +0000

    track cope century room hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
