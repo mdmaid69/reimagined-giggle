bb8b1f9109c192d11e54d8ed506f9919f2f5d558
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:40:30 +0000

    talkback hurry often dance protest

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
