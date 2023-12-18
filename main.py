3b417b54baf89c5143245b9167df1b84f8238e45
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:30 +0000

    loss equal transport judge rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
