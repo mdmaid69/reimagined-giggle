595d8afa2cfca0d1e32239d6ef0687f15fd7747c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:18 +0000

    doubt thinkof smart concentrate basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
