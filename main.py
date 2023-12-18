dfacdef5acf2e6be00dae7a401909540271eaa76
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:57 +0000

    field oil shock flow flat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
