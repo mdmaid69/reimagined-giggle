de4918adc938b13d8cb7c28d26c1b28e5e12caf9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:33 +0000

    price transport dare comeround each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def count_words(sentence):
-        return len(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+print([x**2 for x in range(10)])
