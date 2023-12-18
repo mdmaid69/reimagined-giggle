a1c25e236214a732dfbd907a9b804a398293b637
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:21 +0000

    wise spoil comfortable keepaway rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def subtract_numbers(x, y):
+        return x - y
