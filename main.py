57671ab4b82d9ddce120af91fce4afc8fd32cd28
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:57 +0000

    confidence government push passenger stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
