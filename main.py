1890114a92e814ed5d068ef18b81b4dc0636ab29
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:50 +0000

    alive fairly day cheek consider

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_power(work, time):
+        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
