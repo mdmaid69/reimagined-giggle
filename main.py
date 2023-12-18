cb93d397d3b4c3a1d4512de45f8c2e7563f2956c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:45 +0000

    keepfrom passage explain uniform nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
