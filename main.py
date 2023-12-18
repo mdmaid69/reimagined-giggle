c1ea7d4c7d54d2b90a238927152070dd4f8208b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:32 +0000

    ground runinto demand during necessary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
