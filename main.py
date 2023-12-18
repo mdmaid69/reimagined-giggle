24f580b6a44de694fe0b4930250e3432697c845f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:14 +0000

    bridge earth trouble prize tie

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
