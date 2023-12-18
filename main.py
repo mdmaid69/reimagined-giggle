b654461160177e18513f8c2e948f2bb8a6836e67
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:42 +0000

    swing tip line top face

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
