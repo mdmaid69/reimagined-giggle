f1d017e9a17a5b0359116591fcc69dc15d4e4e48
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:17 +0000

    turn feature miss track concern

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
