c45cbce1bf5c778d1b3e61e6296bf372c11f4f1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:55 +0000

    neither best building town scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
