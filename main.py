a7a6443fa4d50db8542d51a4f37c9b1e29b89ae7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:49 +0000

    towards proof storm equal interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
