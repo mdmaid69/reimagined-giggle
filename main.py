cc30dbb30027b8dd531af008c99ca59b0e04ff92
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:07 +0000

    operation surprise concerning run chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
