2b2c3fbedcdb9b1932cce3764d3fd17021128d48
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:44 +0000

    ready report bin narrow winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
