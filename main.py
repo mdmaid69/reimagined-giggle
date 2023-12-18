7cc196986a5dc27d1d1eb832f285c7b87006049a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:14:40 +0000

    pass 571enough forward collect central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
