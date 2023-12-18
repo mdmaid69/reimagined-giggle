3c9f8eb1400690c2c822a12deccb7faa67b9ac6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:33 +0000

    peaceful press lose disagree warning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
