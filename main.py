0413b697d5986977430946328a8a2235c3183de2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:28 +0000

    contact attract remember plenty future

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
