a3ef27ecc59744070fe84790681a3931ea935a99
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:16 +0000

    clear there concerning goup return

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+def get_current_time():
+        return time.ctime()
