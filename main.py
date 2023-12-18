837d92a63dab81e161c27123492d7a8119a89682
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:26:22 +0000

    loss carry deliberately mark bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
