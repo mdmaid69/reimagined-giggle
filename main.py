3498b99507a968322e0e32e3ea991b8245119133
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:55:02 +0000

    around its yesterday nearly fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
