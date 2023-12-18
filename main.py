d5e943547b8b60c7c867cdcba2140143ce962540
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:47 +0000

    opportunity occur old split interruption

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_current_time():
-        return time.ctime()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
