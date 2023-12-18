070bccaa0bba4d35cfca89db405633885e6126c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:59 +0000

    instead refuse comedown float order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
