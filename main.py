d73c388cdd863d4cc82192bfcf4f086e7e546ad1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:14 +0000

    wild home cough touch apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
