11d8550bfead20de871eace80a7a315336452f2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:19 +0000

    Pp equal doubt juice trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
