6422f74f30bdd10471c2c6327be0a4526eebfbcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:13 +0000

    by equal damp build offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def convert_to_octal(n):
+        return oct(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
