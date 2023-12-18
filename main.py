b39daaaf82b48421e5aba32e0738d49e137dca4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:17:33 +0000

    remain stand main match slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
