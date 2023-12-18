5e0404a3cf59778ff66fc128cafa5b6c06bdd48c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:01 +0000

    him positive advantage excuse 571enough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_even(n):
+        return n % 2 == 0
