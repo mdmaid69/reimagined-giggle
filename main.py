e16ff8e90c047f379d2043a74803f3f199e2404b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:11 +0000

    large lead basis form welcome

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
