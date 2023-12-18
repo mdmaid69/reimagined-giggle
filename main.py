5181ddbf35d0347bd4e0c56bcf346f3d9a326639
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:20 +0000

    order switch cross flood turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
