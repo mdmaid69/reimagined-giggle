7f259d515e3652acb88114deed3da4319848f290
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:44:44 +0000

    leg vote left form nurse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
