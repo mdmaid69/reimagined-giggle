ab429e72eb66c9c008ab24beec698c32acf0d2b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:03:20 +0000

    quiet home pain bye priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def square_number(x):
+        return x**2
