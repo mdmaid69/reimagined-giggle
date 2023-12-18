715b962e795d1f826cc06d2059e6b7d815acbdb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:40 +0000

    the first upstairs sweet house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
