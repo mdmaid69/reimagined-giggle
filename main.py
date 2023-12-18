5ee775872fa44b9098992406a5c3195bfc4d4bdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:44 +0000

    thanks also noise light worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def print_python_version():
-        print(sys.version)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
