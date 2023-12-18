87226c15a332c8a56e17765ca4c61680a4ad8fcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:31 +0000

    contents unusual piece able dinner

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import numpy as np
-print(np.array([1, 2, 3]))
+def convert_to_binary(n):
+        return bin(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
