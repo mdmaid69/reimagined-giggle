3256e0f2a27984c4c729c9d0795af28874beb760
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:52 +0000

    transport entry start through name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-name = "Python"
-print("Hello,", name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
