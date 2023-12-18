ba0eea900900b6aa17b4abb1deecdcdcce81ce8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:11 +0000

    description comeout only odd suddenly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
