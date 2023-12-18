ea2ef094c470fb2e08751c9c028e7ed41aea3f86
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:49 +0000

    eager train tend mixture union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
