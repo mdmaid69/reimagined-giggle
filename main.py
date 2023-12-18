1d4d773631db067566c7aaa9f8cdca6f9cb90aaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:12 +0000

    some fat getout write may

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 def calculate_speed(distance, time):
         return distance / time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
