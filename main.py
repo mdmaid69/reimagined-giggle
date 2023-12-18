bb1efb3d2c1932a96cfc9f57bab39731b14f2853
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:17 +0000

    benefit prefer plenty 572enter remember

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import random
+print(random.randint(0, 100))
+  def reverse_list(lst):
+        return lst[::-1]
