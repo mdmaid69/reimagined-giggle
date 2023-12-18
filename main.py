3fd2c02cfb8d4f82a594630b15579a19885c48d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:38 +0000

    steady relationship apparently release powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
