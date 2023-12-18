6b2ea2898cc7282788bc711601fa62a69226b58d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:43 +0000

    mass happy watch splash range

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
