7caf24f1cd061638b36a943115eb81f1ca378249
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:26 +0000

    later considering experiment guess opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
