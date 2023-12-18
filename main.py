d70cff8a7ba01de2659da0559dd2371566f2b57b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:59 +0000

    there back prefer squeeze blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
