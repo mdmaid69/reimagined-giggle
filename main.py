ae6937fd278ec44fb110c439963c97b894ac7842
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:47:53 +0000

    breakfast independent business story expect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
