f602514cbbe15ae0f27cc3a9c0275e2e63df17fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:23 +0000

    exam peace chance brown trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def cube_number(x):
-        return x**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
