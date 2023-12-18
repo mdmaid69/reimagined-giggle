37ac3bae00aca5820471ad9db9dd7b542d06ddc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:42:21 +0000

    top few cut otherwise about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_odd(n):
+        return n % 2 != 0
