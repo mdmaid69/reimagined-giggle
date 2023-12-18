03f657ea03c11d77f2e972f517762472d56d5be7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:48 +0000

    slow grateful drop may murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
