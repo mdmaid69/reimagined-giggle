5dd24f04589d741ad7777230d3ced629a5fa3b2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:01 +0000

    creature claim hole order refer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
