8be54bdb186b5942c4a1552d54a307b708486d8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:57 +0000

    pretty right under deal right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
