6c09d9571e84df59bf3e0456a11fc3bffc8903a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:45 +0000

    tip neither escape treat delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
