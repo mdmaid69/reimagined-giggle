6ae35976f09f6d2fc9c8c4f54386c787521148fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:09 +0000

    table holdout track pour quarter

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
