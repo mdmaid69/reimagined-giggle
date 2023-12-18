d2752857f1baf760feaac38503b7042eaee25bff
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:38 +0000

    keepoff short discovery judge disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
