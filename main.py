f5b6e6088ae0d2b934d60ed7fffe03e7ce486d5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:35 +0000

    black once left exploration sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
