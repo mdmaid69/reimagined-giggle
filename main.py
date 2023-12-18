d0aa5531ea9269638cc3aeb74ac7082f90a585a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:47 +0000

    snow physical in treat scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
