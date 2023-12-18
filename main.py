f6386754174c5900761fa947790b6d51f4c51e7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:19 +0000

    immediately light exam dance argument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_directory():
+        return os.getcwd()
