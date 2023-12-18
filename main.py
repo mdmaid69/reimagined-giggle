b50e051f54770e7736a1b42901adad9ec09676d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:44 +0000

    city relationship wire here us

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
