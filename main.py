dbcec7510b7abb5ef6cbdb03b0f0d6ae1e62739c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:12 +0000

    wall reject distance nigga juice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-for i in range(5):
-        print(i)
+print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
