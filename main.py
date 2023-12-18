ba6146ab13440c585f165353ae334801056dfb05
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:47 +0000

    lookafter smell book law contents

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
