dcd8f4ff5dd129306484d2fecdf1a29bc978f106
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:54 +0000

    question estimate dance move breakthrough

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
