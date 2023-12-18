fbd5579e8461808f5740dacc40bdce8e93c1eac9
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:06 +0000

    agree sing drive involve guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
