d462d568de472951a39ddd6c484f2daf5a31d32e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:25 +0000

    result involve describe talk goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
