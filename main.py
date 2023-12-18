13b50942d6a8850216794742ffcf8c8a4975a0e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:35:46 +0000

    whichever tape contrast pretty foot

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
