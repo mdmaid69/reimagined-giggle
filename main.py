31477137da0b5b18e332d0699ec6224151dd4b6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:22 +0000

    belong 564encourage properly move mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_current_directory():
+        return os.getcwd()
+def find_min(numbers):
+        return min(numbers)
