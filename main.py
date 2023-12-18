cc4c88913c7e00a348ec138d02a1f83523401a2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:23 +0000

    door tonight rank history full

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
