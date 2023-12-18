04e2859a0864341ceea2513e65597656785fa175
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:07 +0000

    thankyou share close hard pollution

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
