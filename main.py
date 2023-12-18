86755c5b6e6263cd29eef92701802a1a206309fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:43 +0000

    Hh content further Yy point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
