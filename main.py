9682014528c2ef6857d2a5fe05d41312fed59a9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:20 +0000

    pretty physical doubt swim maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
