7d921889ebbd1bb835d29e70528166620302d630
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:04 +0000

    turn interview centre power actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
