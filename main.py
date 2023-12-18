25636a86f8c16ae6eb891ccf2a018ef76ebba91f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:02 +0000

    along satisfied married ask extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
