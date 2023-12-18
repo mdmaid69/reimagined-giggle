cb1dee73b74a59bcb764089fde99805e819cf3b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:27 +0000

    country safe quick limit pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
