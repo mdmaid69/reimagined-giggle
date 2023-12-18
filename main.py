0d4162a688464f5dc47145687557563060326f8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:21 +0000

    fetch earn record cry cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
