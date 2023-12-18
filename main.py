3518927a5a5ce1c39dfd7a3e9597cef3f7dd0e1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:50:46 +0000

    sky giveout us error anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
