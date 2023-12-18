da514655b3f51cc2ae0b5db6e0e76414f47e163f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:39:29 +0000

    police report there sign flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
