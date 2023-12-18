c562a59a8a7120cba3b8a10889849ef7c491b80a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:47 +0000

    dress against beside culture broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
