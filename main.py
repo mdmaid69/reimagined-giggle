004e920499bbe36457cb2c71082a0360776858ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:39 +0000

    Itself comment declare ourselves mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
