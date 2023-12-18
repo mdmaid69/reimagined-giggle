03636cd6b72c06c49189a2ad749d6f19181c4c51
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:37 +0000

    Yy keepout central hair coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
