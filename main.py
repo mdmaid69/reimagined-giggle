59cb26208eee120bc5dd5a06b2707c1ea0d7c238
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:02:56 +0000

    many equipment set natural line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def get_array_typecode(array):
+        return array.typecode
