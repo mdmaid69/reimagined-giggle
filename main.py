809dff286448e04cde164bdf39651493db55eda7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:46 +0000

    wonder regret twist expression weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
