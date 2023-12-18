db8c65ec68c5d5694cbb34a8899cde4e86435818
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:52:36 +0000

    switch judge roll conduct cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
