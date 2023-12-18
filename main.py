20f779f5ed689793c1b31239b8e5b092e2350627
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:20 +0000

    fat extraordinary mess study mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def cube_number(x):
-        return x**3
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
