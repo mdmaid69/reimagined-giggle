eee48a966ee6b8ded55f92779231257b8af5bcf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:15 +0000

    pour turnback and wide beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
