484ee39fa250740844efc0cfc3db6c036e96fe4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:38 +0000

    fun neck finish split piece

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
