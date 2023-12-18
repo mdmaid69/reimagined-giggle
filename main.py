5b257fd6405c3cd0c7ef567bda9f134917abf432
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:21 +0000

    similar loan afterwards quit rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_density(mass, volume):
+        return mass / volume
