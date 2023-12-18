240d4155fe127b7d9cc219284cac7245cf4626eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:25 +0000

    tie fit comfort arrangement 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
