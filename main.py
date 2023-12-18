df0adf8f247e4c1fd8828576713c93bb5a232edd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:43:37 +0000

    market unite exercise over nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
