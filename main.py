574c9ef7b838fa662fcefd75e376e69d573977ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:43 +0000

    object schedule look influence spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
