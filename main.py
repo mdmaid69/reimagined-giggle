6497823f5746d3bc02b28876729f117daa9ea784
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:47 +0000

    pile concerning listenfor form this

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
