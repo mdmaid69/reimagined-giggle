a110bea01c06dc3f965e619027d715162751244d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:25 +0000

    apologizeapologise active its arrive show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def subtract_numbers(x, y):
+        return x - y
