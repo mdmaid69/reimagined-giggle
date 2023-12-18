82294c9aa53bfd6c418d9fa0d3f65391d9a0fed7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:23 +0000

    person slowly most operate season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import datetime
+print(datetime.datetime.now())
