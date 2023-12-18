460acace2b14c974739485412d8eefff61b5cb82
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:17 +0000

    heavy mood reason schedule hot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_max(numbers):
+        return max(numbers)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
