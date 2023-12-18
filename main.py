f0d144ed42635409689616d8fd67421648fffd2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:10 +0000

    order bye spoil famous while

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
