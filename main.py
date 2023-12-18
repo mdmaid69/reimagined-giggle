0274691851ebaf9a9ff816c823a0ccece2686c4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:42 +0000

    teach rely enthusiastic record apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
