59b61fe952f78fbf91ff829e44fae3ac9791e1fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:34 +0000

    frightened catch goodbye cheap wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
