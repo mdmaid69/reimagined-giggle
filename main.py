38500327eb99cd28cc44d6287d107d2b27a65203
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:27 +0000

    play forget chase swim thirsty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-print([x**2 for x in range(10)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
