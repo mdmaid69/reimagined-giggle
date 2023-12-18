6df7fd008daca6088958997802caade5bf738046
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:32 +0000

    depend wear stamp car apparently

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
