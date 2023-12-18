27a99f70eba2b6b873095d728a66e4d84f377bab
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:13 +0000

    declare glass print music she

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-print("Hello, world!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
