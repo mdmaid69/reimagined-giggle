6610f617c86184a32966884d85b9c191e9c39b5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:07 +0000

    way outside contain even legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
