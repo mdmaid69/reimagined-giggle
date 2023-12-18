bf7d39131161ca5555a63c905804755c2ab54893
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:47 +0000

    fit supply ahead thinkthrough slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
