4bfa9c5c9c7fa343a614c283d6cd82a01563b67d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:14 +0000

    least grow Rr declare perform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
