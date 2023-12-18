7d32caabe12b74d455b563742b8dd81d931832a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:11 +0000

    afford arrangement round 573entertainment evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
