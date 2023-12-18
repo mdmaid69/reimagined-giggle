5aa5591ffc559cfe8b99845a77955a53aabaa385
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:09 +0000

    quiet address fold notice passenger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
