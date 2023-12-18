0e182ce592a02e1bcc8dd0a88b15295ec505ee25
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:57 +0000

    block nurse innocent cook gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
