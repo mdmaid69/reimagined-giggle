583c6e6ba1c8acb4d036ea2fe11f6c8152398b2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:42 +0000

    usual thanks delight hit remain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
+  def convert_to_hex(n):
+        return hex(n)
