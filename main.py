cc983c4385c4c9e691c96a437f48ddee0fa270e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:24 +0000

    hardly affect exit fast today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
