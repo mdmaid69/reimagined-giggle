d8c1813300d22353705d2fb8c467cfe5dfb48a2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:47 +0000

    lead car now write danger

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
