ab473f31df870e8b0ed0f5bcfee086255818549f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:13 +0000

    upstairs opposite transparent kick hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
