2796bdc59a447fceabdeacfdda6dc071dc92f855
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:51 +0000

    curious stamp callup complaint comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+print(os.getcwd())
