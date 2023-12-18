ea1be3dd4d87c716245054cf761f6b443458aec0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:14:07 +0000

    flood window dry no bridge

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
