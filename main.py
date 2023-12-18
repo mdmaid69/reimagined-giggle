02b380eb88267a3c3ab423d5f4c8cfcc01ac314f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:38 +0000

    murder fortunate tidy climb attitude

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
