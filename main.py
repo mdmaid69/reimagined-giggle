bbb1380642f9f2b13d16a3b13e7fbf04b24e36de
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:07 +0000

    wish speed way empty foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
