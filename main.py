d393d8c177fbfcc39ad5fa10d6a505aeb6ac9f73
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:21 +0000

    nor able behind forget contents

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
