df8becea639b003763641dfce7dfa4cf73b84412
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:38 +0000

    round message their idea crime

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_density(mass, volume):
+        return mass / volume
