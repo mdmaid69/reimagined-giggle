8f31e1e126e84a7e381c8a659e2b6a67505756f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:00:37 +0000

    preparation worry impossible occasional next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
