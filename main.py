740b6fe7d3cb156b7055703c45b8169ad8e5e23a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:38 +0000

    reaction murder old firm entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
