6bd2b65be1c678c657eceed720640ba7ef33ecc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:22:49 +0000

    Tt callup opposite broad Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
