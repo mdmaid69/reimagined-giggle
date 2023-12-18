afa760fb8fd6ae0e00730acef4a0941620e98856
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:21 +0000

    noone angry bridge holdout cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
