6c3208948ddce9f578629ea8bcd5ddb9018f37e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:52:41 +0000

    doctor expect actually disgusting nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_elements(lst):
+        return len(lst)
