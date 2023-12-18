84d24439ab60637228eda413b271a774e1d4494d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:11 +0000

    second consult get sight glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
