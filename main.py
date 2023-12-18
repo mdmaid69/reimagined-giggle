96d9455b5a5748447f93477f0ae9d51375672f35
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:56 +0000

    situation week offer holdout trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
