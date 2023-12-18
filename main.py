3a3c96765423237a7417b3bd1baf62530d3f4293
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:39 +0000

    hospital free judge tooth in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
