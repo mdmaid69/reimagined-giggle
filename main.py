c6dc60f3d8cd28b373d6a7e7e4b235931d694e22
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:03 +0000

    bottom card many open scare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
