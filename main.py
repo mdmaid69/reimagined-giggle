4c62c1e0a8686f47d204276ab9670f54f3f3daa0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:42 +0000

    part stare search welcome note

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
