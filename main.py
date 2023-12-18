17eabd584aba46c260e02a777ce7cc57b6cf2f91
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:35 +0000

    holdon plate eat although busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_area_rectangle(l, w):
+        return l * w
