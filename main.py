43b28ade638a169869c497b8ffbdb08c7638be32
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:10:53 +0000

    conduct quit report mistake getoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+print(os.getcwd())
