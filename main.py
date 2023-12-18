3707b8f18be1e747fc8c594b3ceb9d31f159e7e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:21 +0000

    letter threaten translate Ff his

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Characters:", len(text))
