3bfb7609fc8be528ff88b6546bcc8adf1b9c7f2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:02 +0000

    pupil surprise finger trap makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
