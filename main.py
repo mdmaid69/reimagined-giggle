1a93b8bd84020d9ac1549159b9761620f9d14bb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:33 +0000

    violent tall work sense win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
