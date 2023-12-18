73a97ff3cb5812720b5c4f4eebd21208a33c0f97
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:18 +0000

    stop careful strict noise plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
