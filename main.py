4471dd649cd99b90b3d2470347d3fc67f1975e7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:55 +0000

    crazy control shop why reply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
