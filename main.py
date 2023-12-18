a946f9d457741fa8d1fac4d6360642bd21c17be5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:52 +0000

    children damp centimetre deep conclusion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
