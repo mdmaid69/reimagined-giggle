fc81632500e799cd296d6b128d1b1249e3720a26
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:19 +0000

    conclusion switch clue market seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
