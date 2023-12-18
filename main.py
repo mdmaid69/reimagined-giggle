ad844c74b3508d24a1924454f00c8a4c29a07cca
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:05 +0000

    few round that deserve approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
