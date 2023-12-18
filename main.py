09a7d73db2744d0782069e692f2f524b5d24e7ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:04 +0000

    nowhere similar maintain mean become

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_power(work, time):
+        return work / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
