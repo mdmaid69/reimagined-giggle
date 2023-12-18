46168d42e213ab9aeb50b6f93ba5b7c949ec8490
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:31 +0000

    second row recommend beautiful smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
