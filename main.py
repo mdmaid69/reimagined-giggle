ce3516c76094858d6990fb8bd61258941f1112dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:50 +0000

    cough warn how birthday phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
