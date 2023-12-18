94c3a18d1344b2f9059a311b36b966341cc6bc0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:15 +0000

    report whoever appear 564encourage there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_power(work, time):
-        return work / time
+print(math.pi)
