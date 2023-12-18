cec67309a5edc8117e1ce57e83ad7f252bcc5deb
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:00 +0000

    present under refuse their equipment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
