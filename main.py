85ff65895bf6d382096c2e345900f183bec8baff
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:22 +0000

    week back keepfrom software constantly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
