7bf7083d29fd7e460c4d9cf25f487f152b59aad4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:25 +0000

    Tt in but division important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
