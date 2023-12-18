04da79133c48893f9e70159e36102a83cc86438f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:32 +0000

    regional kind mistake you example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
