02e7cddcbb27604dff77114d481bf41c2a086546
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:04 +0000

    emotion ask respect brief after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
