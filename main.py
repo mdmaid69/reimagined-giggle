d51c0c398b92376173a00247e96977cd0b7b90ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:24 +0000

    disagree cook with coffee unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
