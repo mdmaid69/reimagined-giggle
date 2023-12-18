c8b05e8aeff9a203391f2a23d2be5cc6a8d4bea5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:22 +0000

    safe birthday unit bite branch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
