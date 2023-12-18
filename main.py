f8b5a574abf355414f85fa875943a22391900e25
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:00:35 +0000

    grow clear platform means treatment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
