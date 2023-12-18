e89776ce3e9ca262b2da2f9385d7b3ff36e2ae2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:56 +0000

    quality alone sometimes report may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
