2a0106ea85d88cf876a46f31ccb58c2211f66277
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:55 +0000

    comfort fortunate fresh sail travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
