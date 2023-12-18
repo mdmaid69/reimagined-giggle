74a4e9da821b97d37101a08fa6cda63574881366
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:23 +0000

    catch file pity introduce doctor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
