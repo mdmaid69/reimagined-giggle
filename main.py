3a59ca3ba5dd2c7fffd81f329f3da1562a9fcb95
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:41:47 +0000

    bridge there error wish Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
