480e4e407fa8f94696317d6723af133da2768223
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:23:07 +0000

    politics climb figure pretty lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
