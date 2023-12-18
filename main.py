a9fe3cc8e6e4a54d9631dae2341a814c2c4e83e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:38 +0000

    prevent service bound harm free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
