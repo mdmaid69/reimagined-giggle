13ba3ffe8dfb89b9739609fc7c0feaca2023f906
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:02 +0000

    clever group mend favourite sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
