7baea485b78dc44f5e105f80087b3e41bcb74db0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:31 +0000

    admit normal step thinkof promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
