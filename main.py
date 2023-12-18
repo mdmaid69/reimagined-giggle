73bcce6239e554341a0abb891fd815cccf5a4b89
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:44 +0000

    obtain organize holdon chance film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
