4d7d4f013200c0c533561af83bb56b4550180a3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:28 +0000

    maximum attempt share turn cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
