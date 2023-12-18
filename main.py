4e5fa4f0509ef7cc4757202f4309587c198a70a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:32 +0000

    answer recovery violent boring passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def find_min(numbers):
-        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
