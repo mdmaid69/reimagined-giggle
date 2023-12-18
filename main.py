3348c5572132fdd751a4d56640663053f841b736
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:22 +0000

    hold acceptable break cheat especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import platform
+def get_os_info():
+        return platform.uname()
