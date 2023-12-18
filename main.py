bba9a91e91f5e913a67732df8a1968b60691ee56
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:58 +0000

    attempt bored soil team space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
