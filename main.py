98a1005611851e614ddbd0947f6fe0a682ca2667
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:59 +0000

    song left 179bite set follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
