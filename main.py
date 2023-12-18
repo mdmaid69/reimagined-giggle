0a6291e6d7f686d9d65f63d47e9d0a94f286f886
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:31:52 +0000

    behave new unkind reply excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
