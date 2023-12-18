66926a16bae917498c3f8fdfde946366ff53f8c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:01 +0000

    spite far container combination 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
