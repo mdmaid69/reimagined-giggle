7dae6f9eb07e35fe523e54928dd1b7beddbf8cb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:06:28 +0000

    bad correction behind probably excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
