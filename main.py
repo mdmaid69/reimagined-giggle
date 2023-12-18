dee865431173c411bc952de0cf5c94ccf6c74b6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:10:00 +0000

    once takeaway possibility splash improvement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_max(lst):
+        return max(lst)
+def count_elements(lst):
+        return len(lst)
