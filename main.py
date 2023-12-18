b8f968454fc8b8c33e8ddd1bfa754169e846948f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:11 +0000

    rent grow first already waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
