7dcd0224c39afbf37188b77d8f92dff1f88568cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:04 +0000

    Rr high friend responsible once

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
