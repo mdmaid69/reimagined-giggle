c14f7cf9489257de63229786ec0efeba5fed41fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:56:35 +0000

    unfortunate idea suckme curious cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
