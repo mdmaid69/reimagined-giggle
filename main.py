1ab82d32b9150ca96a52d163a77d40fd3cac784f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:42 +0000

    storm long mind tear discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
