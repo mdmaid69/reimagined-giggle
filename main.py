3062f1a331ccbefecee3fcd8998f7d390554bb8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:50 +0000

    ashamed last stamp promise slightly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
