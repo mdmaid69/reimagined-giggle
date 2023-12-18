d7e2e1201c840ac4b7c796375aa691f80698ec71
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:37 +0000

    risk properly necessary his step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
