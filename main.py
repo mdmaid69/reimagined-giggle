71dbc87278c6a7d9e7e419251e1b8901ee627e67
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:05:29 +0000

    dead combine substance immediate its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
