c43afe0bc4a9c7000372634f5b572ba1a6b53c23
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:29 +0000

    quiet keepup story honest violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
