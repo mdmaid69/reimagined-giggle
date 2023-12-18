583685db89d5f01a8a105a91bcb2f9b7f39ca134
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:38 +0000

    instant congratulations giveaway accidental regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
