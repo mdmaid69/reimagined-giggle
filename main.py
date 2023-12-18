9da4577be572804bb4cae7f37d20751d34c81749
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:53 +0000

    work discussion position excuse professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_factorial(n):
+        return math.factorial(n)
