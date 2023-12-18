2dbbc5d2e12d3d44a2686ab7b7ca823d37dc977e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:29 +0000

    completely runover suggest balance nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
