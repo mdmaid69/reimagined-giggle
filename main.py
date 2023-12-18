5c3d72058c501b52e674dd6d1fc848e7808440fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:32 +0000

    agree normal command art roll

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
