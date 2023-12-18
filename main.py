5bda89588c236156eb8a226fa3b75c0f516dfd1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:45:42 +0000

    opposite succeed Rr wrong who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
