9b044ad3fa1cc9bd94f78a9a16d012220c856e44
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:14 +0000

    shut success neither even luck

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def greet(name):
+        print(f"Hello, {name}!")
