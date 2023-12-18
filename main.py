8af79487c66ff1a57fb39cdf7eceb95202eeb625
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:14 +0000

    goout lean turnup search satisfied

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
