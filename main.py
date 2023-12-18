5679ab2a79054898ed1803e3c7660be74759ab68
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:32:41 +0000

    miss service when get hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
