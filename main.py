27eb6f9ec952d7734baa4c14f689c483ef4e4856
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:35 +0000

    generally still tired pleased price

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_array_as_float(array):
+        return float(array[0])
