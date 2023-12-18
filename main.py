7579281393b87cae856c814a1fc6b6e185a73cce
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:10 +0000

    official back sensible try rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def reverse_array(array):
+        array.reverse()
+  def multiply_numbers(x, y):
+        return x * y
