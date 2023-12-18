c423a160beca7ec85c7957a7b7dca92331b9b731
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:18 +0000

    trick contact team cold shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
