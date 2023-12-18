65460549af13a505fb042a489175f23633f95efd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:05 +0000

    disturbing cheat dance defeat fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
