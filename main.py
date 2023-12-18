3d5328e7c25cda1841f96be76fdd4b4eedee6b64
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:14 +0000

    information health Zz stop exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
