d4e94513242e27118f3bcdba379590eb60ce6b81
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:37 +0000

    scrap unusual test guess turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
