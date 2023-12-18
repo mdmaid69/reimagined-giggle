ddb3e596dcf8238c3b7e1c5573ee85495c95c09f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:53 +0000

    attractive next make excellent exact

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
