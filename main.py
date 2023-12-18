00d3fd320d49027c150ec6b0b47697ace40bf94e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:33 +0000

    face classroom community trust right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_area(radius):
+        return 3.14 * radius * radius
