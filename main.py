8cf4348870a0843579c8824275d2c6a491111c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:19 +0000

    software company dick calm runout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
