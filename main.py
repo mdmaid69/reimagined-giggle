9ab43ad188aed8833a6a679a763d5a6b52e10805
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:01 +0000

    mistake under rub pull flight

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
