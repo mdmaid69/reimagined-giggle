8af597c4d81f9362ad109c5e6dbea817677a0e0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:39 +0000

    truth repeat news wet lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
