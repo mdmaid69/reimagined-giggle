8a30a7c62afd6d211f4f67711618fb96d01a5962
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:29 +0000

    order blind surprising week recover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
