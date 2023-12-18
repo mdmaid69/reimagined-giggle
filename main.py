5d759d4370bbb18ed0a3cb090da1db838067e389
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:21 +0000

    perfect unfortunate content giveback moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
