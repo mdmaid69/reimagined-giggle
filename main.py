f56794a9893675a515114bfd6d0caa9bf612967a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:08 +0000

    calmly wrap shoot form somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_user_string():
-        return collections.UserString()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
