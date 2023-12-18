9f7bf338fbebed805b0a7439ab7f4046fc2387c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:16:25 +0000

    plan second apologizeapologise thing region

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+print(time.time())
+  def add_numbers(x, y):
+        return x + y
