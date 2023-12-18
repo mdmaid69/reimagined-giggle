a17eb1ebf5b15f3ab94ee46be8900d30ed2936aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:33 +0000

    similar radio slice advise blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
