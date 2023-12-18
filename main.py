cc9cd90fecf6ca89e3516203bea3795e94622a26
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:52:52 +0000

    likely trousers bye mine check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
