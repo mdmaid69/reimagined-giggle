4fd9c84a2356200ed9deac66c2b340d5736ce3ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:50:30 +0000

    Ee down then deep Cc

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
