e931cd0ec99c50dd2385e9d7eb833ca787eb187b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:36 +0000

    material record I whistle tooth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
