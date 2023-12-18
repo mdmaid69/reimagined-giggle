cb47fa52da4a9840e35a13414c6dd2e664169534
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:02 +0000

    on enthusiastic operate moon recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
