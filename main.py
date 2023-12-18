f2292b4325487c7c6286671d2b406230444dc474
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:15 +0000

    wipe open occasion even shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-print(math.pi)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
