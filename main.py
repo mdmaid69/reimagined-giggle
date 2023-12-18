9264ac906513c88eb92960c90c1c64f3dfbeac83
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:58 +0000

    voice look extremely communication performance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
