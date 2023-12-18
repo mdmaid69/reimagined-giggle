d196235efdfe30e99584f1156436126b62d727ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:08 +0000

    same sensitive of move word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
