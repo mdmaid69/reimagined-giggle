42cbb4cca94ca83b10d2578cf39ef2f76937192f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:46 +0000

    container sleep pretty respect drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
