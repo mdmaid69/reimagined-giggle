5fccd650634e51c50e9f5830cb2e689d05a391e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:51:59 +0000

    evening collection determined freedom passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
