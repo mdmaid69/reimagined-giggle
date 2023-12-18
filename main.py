b43d770696c6f7a280a60b0b8c4ca627e4883e7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:07:57 +0000

    shrink figure actual command join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
