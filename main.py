eaa7384dda39db1f2fd7e3cf0e5c9aa7b412d648
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:51 +0000

    whisper angry compare share pleasant

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
