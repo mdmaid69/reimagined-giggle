efade7fcdce2c922e5fdf0d18cd67a7fa3ecdfd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:26 +0000

    blind left tune fetch sit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+print([x**2 for x in range(10)])
