e145955d13a4a9c642be71405a7581e2d0471d92
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:56 +0000

    warning conduct giveback worth happen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
