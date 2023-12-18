45da1c22424bac0c51480e1d14c2ef92aa58c24f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:38:58 +0000

    announce classroom suppose 570enjoyable yesterday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
