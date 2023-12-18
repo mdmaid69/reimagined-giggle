849a6c6456e2a3cbae591a2eeea6cf3f3711e654
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:00:33 +0000

    pretty physical protect politics round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
