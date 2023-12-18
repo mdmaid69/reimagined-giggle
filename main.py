6c74246732ebecc6ae4f6f6e8a5c53fe95259500
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:22 +0000

    since debt statement convenient long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_odd(n):
-        return n % 2 != 0
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
