a60d12ed0581eba5bcd942f94820877a2375bc27
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:56 +0000

    its skin fair bin plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import time
+print(time.time())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
