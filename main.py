6ea7358e094b77c38887e840c3463a38f0d61c6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:14 +0000

    turn her respect zero colour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
