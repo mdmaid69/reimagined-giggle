954df404fc5ffa0fee3453c23b641996a08dfcef
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:07 +0000

    live protest chat scream exit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
