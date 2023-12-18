e6ee41140f77fc6e21e7c19e322f277a32578ba0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:38 +0000

    character check bin post before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
