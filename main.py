cb7caecff15082dceb8682c69ad9be9d8fb15940
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:40 +0000

    Yy unfortunate minor fact care

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
