3a844ae9a70221d4bde4fc9fe8a60ba51fe478f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:14:01 +0000

    violent borrow successful valuable everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
