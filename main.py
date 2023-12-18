d9bd50d2560d7a75294f6be967214b3e0a0f1a3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:59 +0000

    direct walk great thin hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
