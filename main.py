5fd93c7f79ba8abf89e548a44659183524e37d00
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:51:17 +0000

    place listen pour fast row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_area(radius):
+        return 3.14 * radius * radius
