8d02fc34b9a9fad59226984bbeab0099f431bf9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:02:39 +0000

    comeover forward stamp flood strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
