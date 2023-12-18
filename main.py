0eeae7764dc1447cc688a7592e4383fb8076cd83
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:27:46 +0000

    show breakthrough wrong day possibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
