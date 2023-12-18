004fd5842720c62048c257271b20aff61362602f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:13 +0000

    push patience soon hit maybe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
