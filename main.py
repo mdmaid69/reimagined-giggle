6d0cb73599bdc0404761e0d318f12478a8e62771
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:40 +0000

    sure lift money friendly love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def set_array_item(array, i, item):
+        array[i] = item
