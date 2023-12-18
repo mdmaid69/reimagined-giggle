d82537f9a976f664c998241ae2833fd87f633919
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:17 +0000

    pinch speak declaration difficulty impressed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
