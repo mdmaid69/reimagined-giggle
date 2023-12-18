bd37ae8f5eb27ead7a35ea72671be2ad7e9fbe8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:48 +0000

    by goback(to) out genuine genuine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
