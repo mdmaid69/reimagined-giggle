640140f44a23bac5008b3ff930714686362ece19
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:15 +0000

    scream high bin murder comeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
