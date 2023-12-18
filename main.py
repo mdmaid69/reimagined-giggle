e5b58d2eb99156d198bea38394589dfdef821407
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:32 +0000

    discovery quarter through nose strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
