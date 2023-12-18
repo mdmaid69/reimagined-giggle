e925333ab58cf2fbdcba5160c2213480d8e1da1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:40 +0000

    Ee stare month address power

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
