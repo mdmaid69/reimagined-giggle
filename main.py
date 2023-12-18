9212b60968a078fc8dba9fb56d1282a9512e1947
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:33 +0000

    generally level distant giveup run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def count_elements(lst):
+        return len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
