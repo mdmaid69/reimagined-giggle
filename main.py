4f4aa427b58c29f3877f3f6937aad48c7bfbe6c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:53 +0000

    special pencil scheme human suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
