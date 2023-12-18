642af7fe7d0feed44d576e553956992ffdf5c7de
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:05 +0000

    estimate fortunately ability sink goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
