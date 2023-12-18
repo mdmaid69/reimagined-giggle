ebd9565ae6aebb39278f56bf7f09b69f4d736983
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:28 +0000

    official skin major perform goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
