72cf6453111815a4fea9438e9a765a5b635330d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:38 +0000

    determined wall sensitive criticism long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
