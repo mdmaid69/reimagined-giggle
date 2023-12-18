08e946467034e3078f24c0b4b24e80f53f452663
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:04:42 +0000

    frighten objection refer means temperature

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
