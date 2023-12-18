7e042895dc498fd36bbcfd0a3608092a1561d48b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:57:09 +0000

    bringforward convinced float tap against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def add_numbers(a, b):
-        return a + b
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
