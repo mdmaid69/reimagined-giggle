a51e53664e9badba4fdc504b221533054ab6efeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:18 +0000

    wave favour party ground thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
