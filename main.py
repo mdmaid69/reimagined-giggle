5237e561091b6edfdfb7197dc23e97beb6d9d26b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:39:43 +0000

    scared their name lookafter minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
