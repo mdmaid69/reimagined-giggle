01d00da8a688c486947cbe77aaaf2e4b96352c65
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:04 +0000

    564encourage dinner takeover trick born

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
