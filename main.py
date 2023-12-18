517be61f9c3d7a6ac482532368138b197df169ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:55 +0000

    head regret unkind pollution steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
