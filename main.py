77c9d31b7275b9c97926a3e3a04e60b574f43c37
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:40 +0000

    stand big realize road typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
