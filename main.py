ac45189cf0880538bdc1ba3f3d9e42829756c1f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:49 +0000

    tidy arrangement tie objection sale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
