b801d016945a825a99811c2e7402bbb71f1dfe7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:04 +0000

    correction rest fall vote dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
