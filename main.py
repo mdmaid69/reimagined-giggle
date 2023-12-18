0b9fbcbc2b6cea7cfbc8941ca5ff59a9b8988c64
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:00 +0000

    it half trick strange turnout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
