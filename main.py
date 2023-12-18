2fd9eb4b3064fa8b1f1791f5f7f8d7327a9f075e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:08:51 +0000

    run accordingto hurt wide scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
