62ad6c9b363edd7a941de9124ad33b270ce639c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:02:03 +0000

    flavour money quality hour sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
