5a1483be9ffef366e1c1c1c04dd36edab31bdeac
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:33:13 +0000

    today behind mood towards between

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
