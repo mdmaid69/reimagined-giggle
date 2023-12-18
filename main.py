55c8b10b084275e137158825c7a5ce5cadda0c72
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:13 +0000

    fierce demand maintain upset switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
