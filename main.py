16361dcbd7cb7ebe029ffd229a505487f46051e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:07:46 +0000

    turnback part still small among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
