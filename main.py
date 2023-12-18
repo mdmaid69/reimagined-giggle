33fce2644edcc65f797e499f901f031df3b6fe7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:23 +0000

    roll confusing nervous house family

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def clear_array(array):
-        array *= 0
+def get_array_as_bytearray(array):
+        return bytearray(array)
