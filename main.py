8e16bfa80d51e88136e1d54bd5a4514145f0b7ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:27 +0000

    turn image speech narrow deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
