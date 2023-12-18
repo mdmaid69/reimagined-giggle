bba6e1ed3f88bf1b46d75c777fcea2c6b39a1f9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:47 +0000

    easy gas story journey schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
