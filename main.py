5d109fc8d034579ebc40f3de7972878c557d4333
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:52 +0000

    suffer declare hand unlikely mess

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-print(random.randint(0, 100))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
