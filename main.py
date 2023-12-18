b73376c3347df4d35a94ddde24ec72286bc0e980
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:26 +0000

    mood may suit away peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
