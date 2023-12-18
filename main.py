329772225e91b5518cb05385c894b510e6768ba0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:12 +0000

    hang classroom bag cool dear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
