e64dd4c3c3202e97fb1c936f7f061bd6f0f7b199
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:03 +0000

    quarter thinkof war regularly available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
