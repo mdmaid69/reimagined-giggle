c2646db0abd0f97c16a29cc8589ee2798703e161
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:43 +0000

    fall fairly discover hard depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
