0f83a543c1d6620ca65be633f69a571ecb496310
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:19 +0000

    manage minimum deliberate anxious plenty

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
