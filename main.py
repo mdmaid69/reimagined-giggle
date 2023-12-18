8a801911020ea00aa5f3a1b24547b72797655d19
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:34:55 +0000

    few wheel fortunate week relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
