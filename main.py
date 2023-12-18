2103216c5f999b4acc7ab948b0612d2c5d822bd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:49 +0000

    detail frequent see anywhere week

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
