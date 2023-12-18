c20cecffda09b4289591ce8e2ecfdfafaeb8a26f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:47 +0000

    deserve taste machine software average

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
