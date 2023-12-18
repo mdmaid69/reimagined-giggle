a27a03c7dab9f1aa50da3445836ef75abdddb1e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:27:59 +0000

    similar dish communication wave rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import time
+def get_current_time():
+        return time.ctime()
