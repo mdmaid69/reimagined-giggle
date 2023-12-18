acec16df3b97ad2428ca2ead90abb1d642f7365e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:23 +0000

    progress church meaning however large

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(5):
-        print(i)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
