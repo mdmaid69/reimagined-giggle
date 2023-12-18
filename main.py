b3d0390bb9180f3afcef27441495a72185793ee8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:31 +0000

    motion aim instant slowly Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_even(n):
+        return n % 2 == 0
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
