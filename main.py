d11cd578212203d25a4a3e2f98955aa4ec7b25ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:33:51 +0000

    thinkover benefit storm shade sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
