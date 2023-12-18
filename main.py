3c6f9a186cedc1c9721666ad02c2390be67fe3a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:55 +0000

    store buy teach after comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-name = "Python"
-print("Hello,", name)
+def find_max(lst):
+        return max(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
