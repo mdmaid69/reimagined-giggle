1ac8f3560647c1069446479315f5700814f0c434
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:53:18 +0000

    most crash name tour run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
