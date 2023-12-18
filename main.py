9467eb16ec56edba435c895e169c99fc514b6978
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:49 +0000

    in lookup confidence commit straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
