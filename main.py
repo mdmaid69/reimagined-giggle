4b984889ca1171838727c6d6a46a33f6831aedc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:41:37 +0000

    few think glad where no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
