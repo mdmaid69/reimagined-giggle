c86a3d4fb1e66fad4527207cccd1d6ff06583731
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:17 +0000

    sorry match art description die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
