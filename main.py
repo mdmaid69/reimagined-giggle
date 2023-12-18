b93ae6a982653108b1a2bc84cda8af64cf80658d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:21 +0000

    nurse not mood none upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
