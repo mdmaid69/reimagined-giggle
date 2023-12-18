e5d2d903da83b1238cd63bc47004db380081dea1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:34 +0000

    fill thick circumstance pour record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
