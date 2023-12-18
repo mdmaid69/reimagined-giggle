f9390a0f847322227d6f3d325bf2fc6bce7cdd29
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:10 +0000

    bag church pupil pay Ww

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
