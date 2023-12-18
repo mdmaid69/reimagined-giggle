56a7e1318e25591e033be8993b00d99f76b3e981
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:59 +0000

    niggaacademy giveout hurry several them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
