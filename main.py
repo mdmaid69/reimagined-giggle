12f53cff18c6289fe6d3916b570de1a795bb8b67
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:52:50 +0000

    giveout natural address copy invite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_acceleration(speed, time):
+        return speed / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
