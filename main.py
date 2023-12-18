27b5219664f6bc2b2d85d8602bbfe4fdfaff2d1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:06 +0000

    extremely hair use makeinto immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
+import sys
+def exit_program():
+        sys.exit()
