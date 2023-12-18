dfd4ddabffd4f112e2f7abe8740c3469a7960e7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:01 +0000

    healthy regard everyone takeoff ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
