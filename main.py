56ac80fc5dcca2846e31b1b468feb7db400a2028
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:15 +0000

    blind cut cry sport yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
