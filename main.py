10d69de2bef4eaf85842a2c6f7bbe036ff16d3b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:58 +0000

    considering mix risk comeover Tt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
