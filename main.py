ca646340bbc23a3a1d89cb280bbc6c2769a8b5de
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:36:16 +0000

    most deliberate price burst bite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
