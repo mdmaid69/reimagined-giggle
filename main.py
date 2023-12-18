a9544700ae2ac492fecd6128c7af98f4858a08b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:04 +0000

    call consider evidence add tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
