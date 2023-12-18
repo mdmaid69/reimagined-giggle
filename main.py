993bdf96ced972cc50d37ab8a01f37ab54c3e28f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:40 +0000

    away too maybe mind independent

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def cube_number(x):
+        return x**3
+print([x**2 for x in range(10)])
