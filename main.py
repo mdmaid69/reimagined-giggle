a17ea93096183500b683ca1f8e351eb8bbd9a6aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:51 +0000

    heart appeal eye ill illness

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
