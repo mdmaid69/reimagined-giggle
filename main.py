a8719fc52edc90ed2d7cd32c7332157a4ccacbe4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:41:16 +0000

    bunch connected square ability whom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
