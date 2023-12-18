2a625e23a9c2b4e9042c09079cece96a6a74fb68
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:51 +0000

    glass for juice quickly ride

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import collections
+def create_user_list():
+        return collections.UserList()
