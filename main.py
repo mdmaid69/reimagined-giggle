ea686d2a650855e7f1043e80ef8673a00dacd7e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:24 +0000

    steep wide fierce unpleasant television

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
