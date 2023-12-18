010eceb7d79ffd352a8031811449d6a04559d02f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:25 +0000

    medium bury ability accidental now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
