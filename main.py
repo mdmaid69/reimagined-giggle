198c9e16c64d509390e3ed978b4aeb3b267b4b87
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:17 +0000

    power bright absolutely level grow

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+  def add_numbers(x, y):
+        return x + y
