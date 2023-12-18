8b9c8006694a8b5ce582b87422ab1a2c7bd2bf1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:55 +0000

    wheel below needle book turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
