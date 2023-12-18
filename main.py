0763e2e9925e84a940da1fd7e650bd3152456979
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:56 +0000

    replace narrow lead suggestion with

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_power(work, time):
+        return work / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
