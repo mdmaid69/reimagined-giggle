a42b281a500a7ec3e17763369824abb37e4c5f83
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:04 +0000

    out pin sting thankyou afternoon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
