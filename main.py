22ce6f44fda8bdb06db3eb9c36a07b6f5639cd89
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:05 +0000

    university plastic turnover such operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
