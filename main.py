067cedafc2ea9e6d6c16c734e3b1b50ca9792bae
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:00 +0000

    scheme else piece around sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
