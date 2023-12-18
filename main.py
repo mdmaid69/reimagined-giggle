c9886415a9049b7e1104764725834c2d1a8d5955
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:19 +0000

    certain poison against possible myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_octal(n):
-        return oct(n)
+  def split_path(path):
+        return os.path.split(path)
