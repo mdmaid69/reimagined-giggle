cead1dddbb2f8bf689b6b86e7be0272157228090
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:11 +0000

    bet second comfort part then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
