a710a0f486b511c072c55581a9dec2c84bd69a7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:46 +0000

    hear off everyone free interrupt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
