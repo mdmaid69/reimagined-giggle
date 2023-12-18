2131d09d7cdb4988eda3c8aad9d431bb24bbbd4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:24:33 +0000

    organize refer prove flavour control

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
