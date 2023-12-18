5133b1e108786cd8c20cbc74506afbc71a778d34
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:48:42 +0000

    insist tip damp back ago

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
