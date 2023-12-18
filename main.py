270ffbadfefc69f9a8b0a0406dc5b05a5a65892e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:50 +0000

    dry twist fetch burn him

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
