20e5149de59034bac2af1c658871e7a21c1bb80d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:15 +0000

    admire comefrom advanced peaceful second

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import time
+print(time.time())
