f2bdc960a0d7763be8475dfabd42afd6dc6f74c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:29 +0000

    surprising car street closed connected

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
