13e6d08802f3110b92af13c37f1f74709b2feb7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:54 +0000

    law that lean confidence increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
