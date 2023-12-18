507f67a011c3108ff838eb68b2a62d168dab7daa
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:51 +0000

    responsible dry high break full

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_current_date():
+        return datetime.date.today()
