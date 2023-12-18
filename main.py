c3ca05019c8dce1a206c7200fb0d8e4c0018f3ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:04:59 +0000

    stop immediate free OKorokay bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def reverse_list(lst):
+        return lst[::-1]
