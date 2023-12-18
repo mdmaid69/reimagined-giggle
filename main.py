ad45b8d13b7c61c38901e8c9edc1f10144e1a88b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:41 +0000

    record right calmly concern head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
