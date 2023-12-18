4a6dd88f319ae32f17a166ec3f525077f54b48b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:48:32 +0000

    moment across such company opposite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
