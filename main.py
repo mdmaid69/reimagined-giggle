77fcc6800aa1d5f21c6e45393a999fc6ee73413f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:56 +0000

    excellent tear wire petrol occur

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
