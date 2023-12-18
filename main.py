55a19b251f4d6acbc15d7fccf4533dff61c3248f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:07 +0000

    genuine share war high push

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
