b70a4eff0287705363ccacfb745400b3e2733370
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:30 +0000

    unlikely breakup water cut completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
