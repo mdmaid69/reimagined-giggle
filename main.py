7fa4e241d273fc0db92299216f4910541a80a75a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:26 +0000

    between church hair concentrate over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
