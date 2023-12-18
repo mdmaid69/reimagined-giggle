554ace291a1e42989e5967c01904932d552dd79b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:18 +0000

    accidentally reduce pity post mark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
