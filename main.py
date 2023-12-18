6004c84a6a32c0247f6d0368a1f0ac03502f6f89
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:33 +0000

    figure stop afternoon proper limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
