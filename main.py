8deecb16d214f648747af46ae43a0d0deac07eea
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:12 +0000

    familiar knowledge stretch official recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
