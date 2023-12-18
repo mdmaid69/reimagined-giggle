53289c5c8c5f757344a341222abb80eece245fca
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:24 +0000

    getback forward shut command cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def convert_to_octal(n):
-        return oct(n)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
