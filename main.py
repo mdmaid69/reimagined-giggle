a5ae9ca8890eb8bbdec40999a2cc5d5d29498d38
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:55:34 +0000

    will bound accommodation offer find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import platform
-def get_os_info():
-        return platform.uname()
+import getpass
+def get_username():
+        return getpass.getuser()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
