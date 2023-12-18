53bdad6ad2e5bd1f2592a08d78bc0a797d461c35
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:50 +0000

    feed dance one regret scrap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
