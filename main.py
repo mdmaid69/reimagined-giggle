78bdda9182a45162209bd224a50c5e21daa3a63a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:59:41 +0000

    doubt estimate pile present loose

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def cube_number(x):
+        return x**3
