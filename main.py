b9b5c7cfaa5cfd770ab7f367e64ce581c5b457ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:10:32 +0000

    will valuable communication confuse please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
