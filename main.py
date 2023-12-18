9c8ada443595e331c9e409e926b3365d2b11b8ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:51 +0000

    use yourself gooff needle wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
