bf73065e84d132f04b2b88ae6a10df9bdf874f17
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:24:06 +0000

    confused performance concerning perform combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def count_elements(lst):
+        return len(lst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
