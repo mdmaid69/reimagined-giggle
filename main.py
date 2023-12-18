ac94c9bd3560e06e985f66eab1198c9893d746d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:45 +0000

    majority afterwards Tt mind him

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
