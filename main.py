5e9a9be848dda4e0d08bd5c7a4346e9d3351a1b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:38 +0000

    claim wish circumstance design constantly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def find_min(numbers):
+        return min(numbers)
