a34d115a83026b3a8bdfe1c0e9acab27e1957e83
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:53 +0000

    object decide physical estimate left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
