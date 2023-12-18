e80dd7de83f267fccf4366eba94b06321913cbaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:54 +0000

    reaction job match office normal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
