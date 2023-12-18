0f068238fc472d8f88452be26611fa319bc431d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:56 +0000

    speech stamp that fine sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
