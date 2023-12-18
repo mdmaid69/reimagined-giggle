654b70c29869d5d0581f65f81d39912d117fca3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:46 +0000

    class delicate major drop cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
