9aa4fa9055e3eac902324fc1855d2b7d12e27734
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:48 +0000

    mystery limit worried bang wise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def count_characters(sentence):
+        return len(sentence)
