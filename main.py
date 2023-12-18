1d1b7305c862e31d3063468ba22bda128cb4d112
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:21 +0000

    team bound chief comfort separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
