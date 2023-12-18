8235d40301c00059f7d8d02e9aa2de9bca79f849
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:13 +0000

    apparently consider why comeoff get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+print(json.dumps({"name": "John", "age": 30}))
