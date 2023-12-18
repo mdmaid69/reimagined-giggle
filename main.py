998ccb4002a349c7a6e1962f71512093e1a6ebd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:53 +0000

    compare coat although decision provide

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
