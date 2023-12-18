79241af6d1154f13cd9664e96bd2164e2952cb15
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:48 +0000

    trap risk full vote steep

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
