48690501b5c4837fc468cf3a1fe9e3b92b14019c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:59:59 +0000

    because hold warm buy pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
