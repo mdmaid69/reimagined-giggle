ca463fe1f39daa0d3d11b1a68c7a8253473b0580
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:17 +0000

    skirt also turnback sound play

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+def calculate_power(work, time):
+        return work / time
