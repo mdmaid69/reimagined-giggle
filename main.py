66e50cda966cd762266d95a35ef6d285e21909ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:47 +0000

    union best then far dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
