bccfb40bf3c87d704b35c118e7603ad553ef4bcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:50 +0000

    suggestion heat paper most most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
