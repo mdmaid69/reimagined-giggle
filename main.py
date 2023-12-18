86085d39b970c3edf1254e49bb31938e7398e944
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:44:56 +0000

    separate collection plan real scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
