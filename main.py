1c62f852a8070f78ebd4435784251547390598c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:29:48 +0000

    again thin formal comefrom moon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
