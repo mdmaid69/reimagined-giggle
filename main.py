833d4832ce77e2cc226f462140279c271a9c4cf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:18 +0000

    before child happen broad split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
