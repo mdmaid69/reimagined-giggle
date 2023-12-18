c061a57c33fed2cb6d68d35dfa4c72267fac3c36
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:26:04 +0000

    responsible tall exact word keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
