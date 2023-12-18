4a823a21b6a129c13411a8768df1855f085ca74b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:02 +0000

    become cruelty same influence unfair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def cube_number(x):
-        return x**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
