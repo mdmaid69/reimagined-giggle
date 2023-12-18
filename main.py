3eaa27f42456e53151794f06844c0f6243081353
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:48 +0000

    few pleasant country scale bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def divide_numbers(x, y):
+        return x / y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
