31e077e9918fb4d061e4d4a5a9710f0f46a13c17
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:22:56 +0000

    workupto giveup meal in rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
