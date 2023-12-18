6f9ac2fa132146976aefef6bb3bea3ae1504cf64
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:08 +0000

    system persuade ring fly tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
