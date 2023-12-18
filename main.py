d981e18b056918d758eddcc213f5e4aeb4accb63
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:14 +0000

    relative ill sticky seriously unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
