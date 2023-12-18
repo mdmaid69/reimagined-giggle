45c8816c6be01443ecf070634545e8ebe2745746
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:11 +0000

    communicate edge rest hour embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
