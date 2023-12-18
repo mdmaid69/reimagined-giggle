c398af6433b71fcb84dc9fb27acfa4c9d510e712
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:52 +0000

    search again scrape simple hold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_item_count(array, item):
+        return array.count(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
