73e147c203b96cb3d8316037145c39ebce8e27f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:47 +0000

    wind notice exist objection flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
