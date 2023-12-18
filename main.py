3184a78523f4de30d89f80cb0cd46c8603b9f4a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:21 +0000

    guide advise battery and attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
