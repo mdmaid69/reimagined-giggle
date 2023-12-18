f178ccdd9ad5a182bb716dc40d9f05a699270b95
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:10 +0000

    under score movement offend event

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def cube_number(x):
-        return x**3
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def convert_to_binary(n):
+        return bin(n)
