f7887108012887a0f08421d41457ca79ae55d684
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:24 +0000

    room breath nothing somehow practical

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
