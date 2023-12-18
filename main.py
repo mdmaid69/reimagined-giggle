1cf194ed5a8beeadbad984148ff7d8650b8dbcdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:12 +0000

    comfortable unless treatment exchange muscle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
