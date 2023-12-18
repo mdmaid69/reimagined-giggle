93f4b67a088e500d1a829c1c4e2579f3d7094290
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:49:03 +0000

    wake main trap cover sensible

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
