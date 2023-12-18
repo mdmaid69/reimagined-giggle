f791fdd38e95a32bb2d61a3255ae569bb68d152f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:30 +0000

    opposite more juice light prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
