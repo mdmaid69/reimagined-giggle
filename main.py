e967805c798057287e1f594fb57c30b14621a4fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:47 +0000

    sleep describe train block trap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
