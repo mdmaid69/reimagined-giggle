c03849dc18386eb1576769f0c522906d0fc0347b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:31 +0000

    scare laugh truth giveback behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
