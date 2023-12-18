7dcff5651fcba8b4bc8984f37c8045f29bf9f779
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:06 +0000

    sort describe better open cheap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+name = "Python"
+print("Hello,", name)
