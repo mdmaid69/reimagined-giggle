5e9119574a26e436005b0f928485caa1f1a7920c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:22:12 +0000

    concerned drink comeoff gift talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
+def count_elements(lst):
+        return len(lst)
