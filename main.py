bda5e7c1e3328f7ddfcc1a0cd6ecdb35a40455c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:44 +0000

    woman beside pile which she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
