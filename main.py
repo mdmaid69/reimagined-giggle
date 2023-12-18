afe10fa33c6329b6ef22c33ce26f247912293203
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:01:30 +0000

    guard post reply tape reserve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
