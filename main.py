cd84c97f6aeb966da21bfcd066298d8dbfb32b74
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:56 +0000

    borrow wash stone reasonable down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_index(array, item):
+        return array.index(item)
