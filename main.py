af574b267ab91b74dfa0b9ee065e07d19145cd44
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:19 +0000

    hot concern organization guide line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
