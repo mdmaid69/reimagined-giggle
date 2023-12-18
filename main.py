47fe36d6b259364a9eb40feb0057a035a6bb288e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:38 +0000

    become touch fine year yourself

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
