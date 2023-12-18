a4cf065bc2474bad85dd4933a59c00589403e932
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:23:49 +0000

    respect sting along turndown down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
