01711281c25544c224285a1473b7f774e49fa2b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:55:30 +0000

    burst once Hh light nerve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
