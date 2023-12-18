1424c179d65e65275876478ed23ce4bc1c1615b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:27 +0000

    sensitive control difference tape payment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_max(numbers):
-        return max(numbers)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_power(work, time):
+        return work / time
