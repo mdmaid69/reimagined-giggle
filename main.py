6bba03cb70fbe9b675de2b8c0f80a16305228bb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:20 +0000

    unless fetch accidental tip exchange

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
