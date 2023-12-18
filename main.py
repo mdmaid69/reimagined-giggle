0e6946c9562c5a051250a73dd3c15fa1ba29bddb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:38 +0000

    familiar glass Oo grand whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_factorial(n):
+        return math.factorial(n)
