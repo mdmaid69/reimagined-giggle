0ca1ab436c68797399f52fa1baab01834ae6f602
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:54 +0000

    sore feeling hurry impossible peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
