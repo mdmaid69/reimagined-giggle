22ac7c7b256a8cd8928ae3edf62284bb60804bee
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:23 +0000

    refuse mystery spend with for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
