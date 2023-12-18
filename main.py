50cfa45fea10ea41389a248cabe4aa424c13ca89
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:31 +0000

    slip service rush reach corner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def divide_numbers(x, y):
-        return x / y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
