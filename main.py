9265d93a399c51c02039e4f4101758ef5987fc57
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:51 +0000

    crime mention settle difficulty scheme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
