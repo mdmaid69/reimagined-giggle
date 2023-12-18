446b92fdab97aacabefa726641f313f25195bb1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:14 +0000

    skill spite copy position muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_current_directory():
+        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
