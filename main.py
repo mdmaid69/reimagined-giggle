6a0319d2a8468662a77e1dcf71fdd18a2bc3df92
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:14 +0000

    start course determined curious harm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
