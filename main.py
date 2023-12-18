fe1d10e7a6b904a2517e27c6e85266e9fb399290
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:25 +0000

    description beside all uniform ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def count_characters(sentence):
+        return len(sentence)
