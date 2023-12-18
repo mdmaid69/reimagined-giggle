7bcb2396f850dde7c3f9811fc5a0e52f598e00c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:30 +0000

    identify change gradually feeling else

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def convert_to_hex(n):
-        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_bool(array):
+        return bool(array)
