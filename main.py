4b4ea313cd29fadaf4b3e6a35b6971aee28b7b54
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:16 +0000

    belief love wash under tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def cube_number(x):
+        return x**3
