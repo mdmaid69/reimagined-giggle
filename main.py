e639a4a0ffd55c237d176d9f5299bc14f70b0c61
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:37 +0000

    lock frame light blame address

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
