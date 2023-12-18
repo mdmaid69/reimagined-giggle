b3bfae005d4a74be16d53347c4815a6c24fdca43
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:00 +0000

    narrow contact speed almost although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
