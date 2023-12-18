6e8686496d33f0506b71782fac8d2a17e1b86137
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:02 +0000

    callat add trust present order

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
