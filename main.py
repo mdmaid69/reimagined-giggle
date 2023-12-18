4b79894bfb49c870118dc4fdcda683dd9808d41e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:39 +0000

    platform disagree enthusiastic over steep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
