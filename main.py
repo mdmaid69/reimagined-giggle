74ba99dd9a634d08b3e00b532795589d078a5d9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:28 +0000

    sign commit weekend thinkover history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
