60194bbd56425738b44bf35bdfdfab87d91b5d6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:26 +0000

    active doubt special bright view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
