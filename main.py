192e06a4e61a7dc5379bed96950b2d32db902674
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:32 +0000

    deliberately film market tune male

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
