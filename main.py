fe841b909cad44e330e1679d2449b78b17f56382
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:20 +0000

    tense provide tough especially development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_length(array):
+        return len(array)
