7b57f6daa3f10a2dfd9c03feb1d4df5c4de81a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:29 +0000

    come market save already wind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+import collections
+def create_queue():
+        return collections.deque()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
