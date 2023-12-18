f6bc348fd9eb0a99991a1a6ec04ac0373f4281eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:21 +0000

    rush lesson wire later difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
