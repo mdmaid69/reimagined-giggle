1a01ca949523d728d6dc210c7ed044df5a4853a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:38 +0000

    possibility now maintain quarter argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
