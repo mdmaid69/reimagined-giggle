b161612cafd49d910e53392b90cceb10bdc4b766
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:07 +0000

    comein innocent minority pause perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
