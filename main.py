f2cbd53ca88a0cdadf00d4eb9d717893f377c8b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:08 +0000

    exactly know gentle check price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
