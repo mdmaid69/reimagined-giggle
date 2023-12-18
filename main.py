0a510b7074b5daf8871ea4eb7f24bb26235afef0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:11 +0000

    confidence fit wide obtain petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
