76dff53251cfc1ce67ab2bfbf23e1457ff93ef5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:25 +0000

    future me contents protection dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
