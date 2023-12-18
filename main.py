d4279ff0adb73fbc00fdfcd85faea79d00e68831
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:56:26 +0000

    innocent truth negative plenty fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
