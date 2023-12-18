9a6ed2b75d09b793055b143fb0be51b2f02aa22d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:35:20 +0000

    relationship force born development silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
