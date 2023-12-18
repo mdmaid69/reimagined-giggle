aae8579c9b78ed1d154da1e16c8197dfd9ea863b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:05 +0000

    introduction married reduce react sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-print(sys.version)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
