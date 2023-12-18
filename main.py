0ff8c8b44640c1f57db8992340d3f306d0277e5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:11 +0000

    stone straight nurse surprising squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
