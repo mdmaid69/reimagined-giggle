6c3ba092b68fa57e42737cc1961b586ff0b86891
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:07 +0000

    thinkthrough accommodation easy lock absolutely

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+from collections import Counter
+print(Counter("hello world"))
