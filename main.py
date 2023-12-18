dae909be530b37ef86ea227e50b22f9bee76d706
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:06 +0000

    cause sign peaceful comment shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
