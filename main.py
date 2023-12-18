5ba315ccf5316ab7c29581d675ddb8ab57962f6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:32 +0000

    drop few married support discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_current_working_directory():
+        return os.getcwd()
