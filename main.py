0b1ce0f2b74d16e02a8516a597ac83accae00666
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:00 +0000

    truth run goodbye lookup careful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
