63d01cdf93690a09a34e83b8ab15a322b9ccd42a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:46:11 +0000

    list quick relation borrow fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-print(datetime.datetime.now())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
