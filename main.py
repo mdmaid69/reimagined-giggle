d2adf13fc9d6366643e390ef5ba2d5a90af7428a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:57:21 +0000

    laugh will offend ear wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
