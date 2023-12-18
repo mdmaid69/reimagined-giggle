ce5728a83832ec3b874da334ccad9637ef206e70
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:15:31 +0000

    the scared introduction rank minority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
