5ab01df28a242a204c40b63034a9f129107c72ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:23 +0000

    each pour bang face clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
