be5e1f7d90d536ce2c1b5f2018c5210759098cc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:59:11 +0000

    unkind wander godown turnout talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
