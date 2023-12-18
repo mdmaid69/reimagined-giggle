a1408a6d278ce404238f85ba7574bca8480a3dd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:24:41 +0000

    yourself objection snow be ask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
