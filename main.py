1d140de56c85f417ae3e46d6fa2f866cb75f2d4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:29 +0000

    sleep building often way tall

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-for i in range(10): print(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
