3035c22e06215cfaa894c93a7f8cf2a7bae5f9a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:52 +0000

    loss free odd dull chance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
