e9781c8c0e98f5621cb720271205e96073d5c989
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:24 +0000

    free delight treat claim action

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
