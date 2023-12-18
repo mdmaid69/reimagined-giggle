6bc71058ab093c4f733763186e3ccc61dc99378c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:14 +0000

    stay meanwhile exact tidy bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
