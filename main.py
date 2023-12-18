3ad87ab8d78e4c61a846d98b4c0d519c975eeb5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:19:33 +0000

    surprised shape beneath grand attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
