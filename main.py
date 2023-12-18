6baf05bcf74f99ca0a2d9d09309c45d0634360cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:21 +0000

    hospital hurt trust angry recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
