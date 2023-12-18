019f6f52c9fd72e4e495c25d7fe35c114d4f48f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:13 +0000

    bin better statement store now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
