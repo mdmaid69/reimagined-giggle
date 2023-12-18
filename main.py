40556470b9818e6bf0c26cbc352b9ae1e33f0f63
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:25 +0000

    nothing definite desperate female declaration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def subtract_numbers(x, y):
-        return x - y
+def get_array_as_bytes(array):
+        return bytes(array)
