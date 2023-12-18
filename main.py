6b1310de5eea32700bc4df86b9393d89daab8398
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:15 +0000

    neither hand disappointed listenfor field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-print(os.getcwd())
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
