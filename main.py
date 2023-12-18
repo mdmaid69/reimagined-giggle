70ae99f70a56c96a786f30b1309430ace00598e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:47 +0000

    indeed operate summer thought unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
