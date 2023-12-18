6c1b2dbc99ff7cf199019f460a2e2580c0db1361
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:34 +0000

    build too creature save pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
