2c1d2ba1c681ba4767205feb40bd9c753517ba68
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:42 +0000

    suit convenient willing dance sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
