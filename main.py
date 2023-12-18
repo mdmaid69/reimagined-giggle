57b649cc8cbd570c22c827eac3d15d51dc5500ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:08:21 +0000

    from fine off backwards cough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+print(sum(range(10)))
