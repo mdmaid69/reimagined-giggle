3c4853ce371e6171662f917de399c4027e08c3a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:47 +0000

    set protest angry smoke mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
