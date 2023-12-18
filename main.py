e86e334524c6074f92c03f1beb8e7f336c09c3ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:23 +0000

    consider something regard sound muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
