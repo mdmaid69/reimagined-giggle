5e5673b1fb34808ec56f6fe77060667eb55901b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:36 +0000

    physical wheel divide uniform correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
