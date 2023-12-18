5343419399f35b3b98890c1bfcb7989e1eb5f5e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:27 +0000

    rely flood bear talkoutof enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
