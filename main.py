8bcdf2018ea76a1210ea7cf4ea97a04b08109794
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:12 +0000

    buy exist pick exchange below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_str(array):
+        return str(array)
