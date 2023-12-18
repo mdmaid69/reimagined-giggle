825dc53c224ca59c4b373ffd446b057c08f68ae1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:04 +0000

    order congratulations love noone burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
