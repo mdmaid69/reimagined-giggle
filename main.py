f4e9d7382ea4d6eced3e89a529173146ff9a0267
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:45 +0000

    carry seem estimate mainly unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
