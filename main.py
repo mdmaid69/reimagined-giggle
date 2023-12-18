e3d341c906d07cb3b1f70d07bdae3d003e53604a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:50:32 +0000

    uncomfortable sick practice 179bite back

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def convert_to_hex(n):
+        return hex(n)
