6e973beca37ee75beef829ad27f4683a580ef3a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:23 +0000

    trap weather concerned unable class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
