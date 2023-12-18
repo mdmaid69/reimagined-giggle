9c217085cc6a5ffa34ad1aba343836b78538b9c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:10:03 +0000

    crowd Ww complaint wet pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
