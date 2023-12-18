29ace70c1a8c7fecb38bb1829be01153f5ea80f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:04 +0000

    bye report heavy spend kick

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_str(array):
+        return str(array)
