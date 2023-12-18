244f13be73bccb58d997a17fe38310035979ff8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:41:20 +0000

    spring warning only possession pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
