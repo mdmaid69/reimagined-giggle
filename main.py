7ec0dd29019777034b17cd08dadffbf24dc92768
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:57 +0000

    occur exactly strong night science

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_length(array):
+        return len(array)
