68aa1f4694bc181cadcfe4a113a07e923408b78b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:29 +0000

    pick limit like exercise law

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_min(numbers):
+        return min(numbers)
