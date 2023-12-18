1c99e57ce14637c1274c33a6b98522f0d33e6261
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:51 +0000

    left thorough value pull act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
