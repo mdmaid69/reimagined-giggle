997ea68529e9243fdf47b5dbce27da22abfbd522
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:26 +0000

    comment poison bang especially yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
