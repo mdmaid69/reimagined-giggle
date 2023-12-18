0decdf31bb2c04a15c22fed7b47e6c30ae9b8d44
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:11 +0000

    estimate community strong mean fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
