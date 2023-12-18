8a4aeb5509f601e9637bfe1a7ee3a8bb7f9dc741
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:55 +0000

    Zz boring confuse beyond suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
