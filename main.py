40ddac683a9fb8bd1885ae7f4ea18b0970d8bd3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:42 +0000

    silent grade sharp shop wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import random
+print(random.randint(0, 100))
