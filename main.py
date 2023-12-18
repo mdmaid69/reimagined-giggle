9970f61bddc96716724ba506a62c614594fa98d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:18 +0000

    smile lower line 568energy calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
