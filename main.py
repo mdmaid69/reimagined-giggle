aa0cca08812e4014d746d6992a5e0a9709041a3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:54 +0000

    pleasant order lock lonely quiet

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
