8de4c91401b73be9902973140cd6517a5c745cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:06:06 +0000

    disagree apparently translate sort approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
