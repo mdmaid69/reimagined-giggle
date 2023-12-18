eb3253737af6f3b6bb3f9514f39744309b4a0f06
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:50 +0000

    branch myself crowd club breath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
