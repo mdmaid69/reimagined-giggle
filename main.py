5bdc19e8e69d15db9113bf43771a3993dfe60eea
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:29:30 +0000

    possession long health wise still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+print([x**2 for x in range(10)])
