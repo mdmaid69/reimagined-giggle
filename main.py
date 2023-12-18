a90c762a01f52f4c6511af33df1e8e427b5d010a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:26:27 +0000

    impossible world arrange brother sensitive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def square_number(x):
+        return x**2
+import sys
+def print_python_version():
+        print(sys.version)
