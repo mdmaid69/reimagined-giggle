5fa010aedb38acbe1dd46bd12cc95f4ba719ca05
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:55 +0000

    call complaint relationship will block

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
