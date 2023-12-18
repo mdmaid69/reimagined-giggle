b8bd8ebc20e4c9ec3bebb3899d52ee67528e95cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:18 +0000

    bother common clearly take far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def remove_duplicates(lst):
+        return list(set(lst))
