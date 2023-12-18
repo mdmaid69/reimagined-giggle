fea256a42ca3dc70b209812cb39905c8c21b3ab6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:03 +0000

    smell chief education attach almost

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def remove_duplicates(lst):
+        return list(set(lst))
