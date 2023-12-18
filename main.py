d22ef5a5ee67c85669bb1e15237c09f20f2dc8fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:24:06 +0000

    emotion wave sink organize blame

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
