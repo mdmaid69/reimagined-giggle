1c1a763217e058b1091d7d9ff9020b2ed36b7af5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:41 +0000

    total fierce regular space mainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def print_python_version():
-        return sys.version
+def convert_to_octal(n):
+        return oct(n)
+def sort_list(lst):
+        return sorted(lst)
