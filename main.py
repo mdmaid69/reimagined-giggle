a5d5d5ccefd0362f8be47cca085d80ee6cad3f41
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:09 +0000

    operate noisy rest take through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
