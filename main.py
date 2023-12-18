5e5aa593627ad8ae61fe97045a062bcaa9ea2fff
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:33 +0000

    best occasional Qq approval unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
