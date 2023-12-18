dde6e3a626414cfce634a01adef41418479edacf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:24:31 +0000

    characteristic surprised pour add callup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
