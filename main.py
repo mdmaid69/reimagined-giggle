1f74c2efc641aa019dc5116896a4cd6816a6d111
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:35 +0000

    workout favourite afraid issue transparent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
