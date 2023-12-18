c64188e41eb6458e0b46d8a64173a3deb8c337e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:51 +0000

    cruelty material off pause blind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
