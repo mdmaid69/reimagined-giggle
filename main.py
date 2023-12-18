26102c065a3e14674ce33359029160a49448cc84
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:32 +0000

    teach unkind makeup tidy course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
