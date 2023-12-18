21f2722fb8775e34b1f5642068a5836bf80f1298
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:58 +0000

    like murder each effect niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
