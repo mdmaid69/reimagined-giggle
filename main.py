e027fb2cb79d5e05ebc5bed69fa9bbb28cc47e0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:51 +0000

    check next man window city

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
