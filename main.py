90aebb2fe4d4eedef256ca909238f749ae8a3341
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:25 +0000

    explanation special partner rub positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
