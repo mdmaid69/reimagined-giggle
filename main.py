5492415935d371e3d16f546d17476c57887c941d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:17 +0000

    entry rubbish stand crowd strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
