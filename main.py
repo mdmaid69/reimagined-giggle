ad7a3d856fd34ea20dfdfc6db344056a8cc92ecc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:11 +0000

    kick insist holdon makeup far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
