d439b83aaeb9369a4901e3eec77b3c2f2c480998
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:14:50 +0000

    arrive argument laugh big himself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
