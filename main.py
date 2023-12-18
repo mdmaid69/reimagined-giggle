1ed359d191cd2dab573109648eca157303ad7a2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:44 +0000

    opinion clear crazy hospital criticize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
