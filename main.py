e7bff1a8b15062e991b2f1612487738cf0dfd9ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:24 +0000

    as cross generous heat shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
