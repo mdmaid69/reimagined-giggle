03203ef8ccd6a2a6715de562fb6ec9f2b48b61f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:05:56 +0000

    traffic reach slight brain kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
