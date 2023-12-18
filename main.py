e67dae6c7c370c4098654e9a19480ec2efd7783d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:58 +0000

    tidy stretch tap their he

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
