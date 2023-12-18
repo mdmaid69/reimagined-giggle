d7870f1616f6d33c7ac5c641b7c4b77dbb1a3757
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:15 +0000

    especially fork various physical drop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
