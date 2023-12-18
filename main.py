8693341d38c789dc45dd60ea8681a1dba131867e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:27 +0000

    small maintain miss belief interruption

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
