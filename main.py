93b769d4a79c04721bcfdd7204d65a49df2f7d3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:33 +0000

    flood surface goout collect afterwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
