087aa3339e35ae4f8af88bab1370185cf7abbef5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:14:52 +0000

    target forever peaceful detail pool

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
