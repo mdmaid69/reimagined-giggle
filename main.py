7b532c1ef7a93f11f4489ebccb222d47535f42e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:39:16 +0000

    winter dangerous handle exercise blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_pressure(force, area):
+        return force / area
