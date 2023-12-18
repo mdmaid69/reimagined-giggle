0fadab46732c4f4d91510b20d1ca948824aa4f58
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:21 +0000

    exchange pay content duty dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
