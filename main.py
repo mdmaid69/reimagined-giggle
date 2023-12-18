83e2a788d86e540168bf730c37b7dcb542b4b622
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:25 +0000

    several stand church doubt honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
