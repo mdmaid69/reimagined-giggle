a4a5cfd91ca9b9836e284ad950f81393265af661
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:37 +0000

    order zero waste especially wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def sort_numbers(numbers):
+        return sorted(numbers)
+import random
+print(random.randint(0, 100))
