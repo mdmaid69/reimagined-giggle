ab91c5c7c68d8bcc9c6802c6cb9edd801517a19a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:50 +0000

    certain operate knowledge swimming advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
