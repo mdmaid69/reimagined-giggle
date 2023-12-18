b8b1f9df80c7bcd18f41d23059831a804838ed33
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:28 +0000

    talkinto phone snow base wheel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+text = "Hello, world!"
+print("Characters:", len(text))
