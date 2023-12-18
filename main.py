d20f8868dac42b15f0e244e9f73f8817fffe57a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:21 +0000

    finish exploration set score 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
