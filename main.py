e04a6070f8255f07521cb801bc7560fa006140a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:10:35 +0000

    book hide mind keepoff their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
