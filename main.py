adce29c2076dc5132123ef1b2877e5d8ce9039f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:00 +0000

    crime down answer dream drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_max(numbers):
+        return max(numbers)
