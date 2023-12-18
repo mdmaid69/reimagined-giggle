0e784592ebd088a7b93407188416aaf1fdc8fc2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:13 +0000

    unite destroy your hers common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
