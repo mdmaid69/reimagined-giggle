875afe94b2c73ecd1aa793d25f65c306dbb9ab28
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:46:05 +0000

    will achievement smile law past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def convert_to_octal(n):
+        return oct(n)
