fd8f07c60c40a27b7021ac3aa57904fd3a4bc596
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:35 +0000

    date plastic shout carry major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sys
+def exit_program():
+        sys.exit()
