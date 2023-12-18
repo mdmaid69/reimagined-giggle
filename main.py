ac69e455348c157c735d24f68ac78f72d06c14a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:23:53 +0000

    side tired port centimetre settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
