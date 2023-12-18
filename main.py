f3b24b310a8d91884d2d4c383c69f9ad64e0e93f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:24:58 +0000

    equipment smile thirsty occur bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
