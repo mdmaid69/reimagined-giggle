30b21de837da3427f3c38647b270058863068a50
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:10 +0000

    period they mine bus empty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_exponential(x):
+        return math.exp(x)
