cf8faaf91c8cf1621368b15f7509d9a450e1b5ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:25 +0000

    claim official deep can afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
