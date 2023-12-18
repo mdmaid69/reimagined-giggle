acf4a7ad8f9031510a12739e5b634ac135ac89ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:42 +0000

    new yourself cause first fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
