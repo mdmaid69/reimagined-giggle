952906d08bc6d9b343ac244aa953c252e2bf8b6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:47 +0000

    after contain window rescue room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
