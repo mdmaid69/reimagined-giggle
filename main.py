5acca8ecec8df3c81afcd05bbd32016a82623ed8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:02 +0000

    somehow own cover church trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
