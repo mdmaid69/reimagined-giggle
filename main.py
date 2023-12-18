244910e99a5d9a710333bee6915b672aacd81c09
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:11 +0000

    crash statement before thirsty damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
