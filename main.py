10bdaef5f49b0c0505e2015e939e1e7e0a0c2059
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:02:13 +0000

    pain already last lookup whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
