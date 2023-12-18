2bbf07e1a65a2dcc41a3d1141aef3e33b5b1f1b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:51:57 +0000

    keepout describe sound put owe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
