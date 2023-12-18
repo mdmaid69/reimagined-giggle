d68e5634ad495dc84c327d6ee7f63cdb4d3ecc22
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:34 +0000

    our dead emergency possible ocean

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
