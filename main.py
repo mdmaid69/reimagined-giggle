d4aa194d517ffd261d8a0079e6980de7f78e30b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:02 +0000

    content available score convince event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
   def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
