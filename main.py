ad5f0793002bcb1e7603f1e0e7e23d9fc9b44605
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:19:16 +0000

    drive place soil insist trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
