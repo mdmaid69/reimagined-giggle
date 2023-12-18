0f51ba5083e09674f0994fc247cf7472d8fa13ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:20 +0000

    strike unite afraid fair plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
