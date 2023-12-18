f15534a02419da4e4444cd2551bb0741df0a39c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:46 +0000

    back fair switch sky before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
