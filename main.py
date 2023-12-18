79e97a209827ccb600920d786248a61fc452fda7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:22:46 +0000

    sort friend throw understand area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
