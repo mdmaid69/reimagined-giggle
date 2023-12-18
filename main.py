bf123bcb1234504f9215364e71a43bb87c269d26
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:34 +0000

    mind public scheme mistake nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
