5f01840711c69297f8c9cdfcf2ef426e26ff437c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:40 +0000

    holdup fast better help every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
