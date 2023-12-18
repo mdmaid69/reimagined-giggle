d06db35b9ba958907dc80524b503ea7f2ed2a4b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:09 +0000

    neither trip squash myself scene

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
