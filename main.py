257f8f912ca0b081defda33910e9755237ab6359
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:28 +0000

    bridge walk term shop shall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
