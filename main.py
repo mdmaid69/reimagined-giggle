8c5233f4ccf971f772982bf65b2312169a8d6c3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:54 +0000

    public somewhere lie clever track

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
