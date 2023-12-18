61a15c51eecc2fa0169824f282386a5efd412493
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:06 +0000

    corner spill come cheat search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
