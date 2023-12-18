21d5280c25bdfb620d48fa0a70d77250a035acfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:55:08 +0000

    defeat basically after gift can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
