a08ce25a6724ac9d0f838e11c7c73629ddd79756
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:30 +0000

    official practise than content somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
