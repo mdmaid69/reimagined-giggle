1be94d204ac52b8259b2410857c20ea2bfdf0a21
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:27 +0000

    priority attract it phone cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
