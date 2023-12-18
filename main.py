83d2a27da087d03b3b2b44119a3bc6241e191b05
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:05 +0000

    stupid cum phone hand usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
