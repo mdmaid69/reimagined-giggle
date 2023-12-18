8c4f7dad7e1e696f50f5f00a031acbc8992659e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:47 +0000

    eager price share pin bringup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
