d2ab24d680da9c2c769e70d7f539bade348f1356
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:33:46 +0000

    sail disappear appeal interesting gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
