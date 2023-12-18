b9c3450863f4b2779ca800acfdfcde868e9963d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:53 +0000

    lower smile keepaway soak consult

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
