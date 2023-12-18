d5f65b20abf76358d9c3e13bcfe61cb297439158
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:51 +0000

    stupid talkinto that exist attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
