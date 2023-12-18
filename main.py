f93f08aa2bed1c18af382649ba342c50d3b8a64b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:46 +0000

    neat short think exhibition along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
