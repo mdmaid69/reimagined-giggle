1999b03d57238c6f3777aca0252bd0bc466e2577
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:23 +0000

    cruel worth further again considering

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
