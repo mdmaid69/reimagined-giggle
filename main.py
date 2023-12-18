af2bcfceed0154ee163ecfc32d98dd7e80a08cae
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:46:12 +0000

    tend before exhibition extremely inquire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
