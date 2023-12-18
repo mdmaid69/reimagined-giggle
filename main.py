d3ddfbe5ef28f796a42ff7899667bf0dcd6f9f98
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:57 +0000

    pass perfect object reaction sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
