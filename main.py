042482d6706c300a750ce3c48bbf1e934bbd46c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:26 +0000

    children bit forever still open

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
