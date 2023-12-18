01e593b8fafc993165c166b3b829bab569ff56ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:47 +0000

    interrupt Yy weather flat temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
