be3b7bf8f0739a4154286bab3d64aa624f7010f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:51 +0000

    bottom book disgusting agreement stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
 def calculate_polar_to_cartesian_coordinates(r, theta):
         return math.polar((r, theta))
