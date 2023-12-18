58eedb37d14f64abe4fa2721cdb05809daf66ed9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:09 +0000

    keepup easy forever power cough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
