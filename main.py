2dda88b824d1315231d082a91ec87b22c7338b2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:23 +0000

    way exchange like floor unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
