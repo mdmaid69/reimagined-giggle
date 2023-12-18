447d376a15ce3fccc1d8c2679769b06d89642ad6
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:31 +0000

    listenfor all catch confidence go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
