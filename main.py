033b5ef0a8a5b1277d7839c9cc400ceaf9efb575
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:41 +0000

    deliberate dick thinkof work sorry

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-print([x**2 for x in range(10)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
