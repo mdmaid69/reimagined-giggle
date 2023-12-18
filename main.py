a10f2b19e917bbac5a195c0d058457002e95fbba
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:45 +0000

    claim drive direct steady history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
