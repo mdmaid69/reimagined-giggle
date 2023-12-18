590a6b22e2816c3d25b8493762af9058617b76de
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:53 +0000

    neither price swim along deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
