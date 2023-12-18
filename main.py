fbd77e416f3fa21eb49ff440b3a3df8028a061a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:11 +0000

    keep warn tragedy ride minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
