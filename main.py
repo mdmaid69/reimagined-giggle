09f6770bb28a242f39fcbb697ec76f31095a8234
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:36 +0000

    terrible recovery moral cry city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
