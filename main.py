5a7e383536ad829870b3ad20b29185fc44915580
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:43 +0000

    attractive round chin callup tonight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
