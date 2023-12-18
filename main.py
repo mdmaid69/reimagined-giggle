73f517757cac81c75b76b49c5ef85d565bd09e6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:17 +0000

    clearly room begin Yy joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
