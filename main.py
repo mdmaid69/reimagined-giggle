2c6435e965fecaa8f90aa987bd615435ea17a6f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:01:33 +0000

    else know leave forward only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def list_files_in_directory(path):
+        return os.listdir(path)
