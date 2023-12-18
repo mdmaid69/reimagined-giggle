147ff00766b232e79c8b186da8270f3cf2499017
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:41:14 +0000

    whistle thirsty opportunity pleasant disagree

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
