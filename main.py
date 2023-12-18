ef8e75e56cfec837ee586969a673a759feaf7455
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:38 +0000

    practical kind means upstairs scream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
