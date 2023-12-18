bc32c573afdda3819b847cb81d2814ec4ca653f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:10 +0000

    education destroy front concern beneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
