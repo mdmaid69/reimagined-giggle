51abd7d6bb82961e1d252d599d8038121e18644a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:28 +0000

    remain possession 573entertainment interview difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
