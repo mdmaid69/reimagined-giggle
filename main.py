9ce5b4b7cf9d7606840539db8ae55334fdfda120
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:45 +0000

    increase rate generally guard meaning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
