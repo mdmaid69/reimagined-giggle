28a2376b99dfacfdf5b2f7bc336ae5d738618625
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:53:20 +0000

    tie favourite interfere principle declaration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
