7cace050db76dba1352b3f259645795a235285f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:37 +0000

    power even connection limit nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
