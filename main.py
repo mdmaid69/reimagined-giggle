e00e3a25b6b407c1b5c6ae03335ec5a6f3cbf3d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:44 +0000

    buy 567enemy determined strength discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
