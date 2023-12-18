593703093ebb6411d7ec53efab0c91b83d96b5fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:56 +0000

    Bb sensible immediately obvious journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
