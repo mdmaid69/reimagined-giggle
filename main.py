d61a3c88d0d1006284775c468bbdb51b6f27442d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:56:17 +0000

    Ww explain own violence boring

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_str(array):
-        return str(array)
-print("Hello, world!")
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
