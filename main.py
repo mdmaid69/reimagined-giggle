e3e328e2b48b7a7a0bfa6ff459e73929554d5d62
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:49 +0000

    tip hurt matter base best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
