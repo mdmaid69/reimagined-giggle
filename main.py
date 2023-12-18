eba691b773e4b00789ff61be21b71404faa702b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:34 +0000

    comefrom total tidy duty 566end

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
