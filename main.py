d59ebd755a2468bce76d89475bdd17a59bef1e0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:10 +0000

    turnback develop factory compete thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
