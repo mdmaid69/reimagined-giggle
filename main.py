ba3ec48e2c164f56b07d9fe34177ac423456fce3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:10 +0000

    another order get cut runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
