7d47caa23bcba7230bfd130782e02088d86eff21
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:30 +0000

    hour rare section organization information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def append_to_array(array, item):
+        array.append(item)
