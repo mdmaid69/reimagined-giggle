ba6ebfecc31cb46fa84cdee3f285c93d9155c8ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:45 +0000

    move whether injury instead mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.time()
