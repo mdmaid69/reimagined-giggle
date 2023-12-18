3eaf9e06b73c2683ee450ae5d7767f0e715fc72b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:39 +0000

    unable convince completely debt woman

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def reverse_array(array):
+        array.reverse()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
