0c8b94e69bfb7aba7fd702d4c863f031a5ab3465
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:35 +0000

    true conclusion communicate space hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
