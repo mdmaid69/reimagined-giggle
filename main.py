ec5fdd9aaef21cedda786874f98d515035772f76
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:21 +0000

    pair his scream permission Cc

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
