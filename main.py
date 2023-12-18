2eedaed94ee6d415ff257899a30b1000e515cff3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:12 +0000

    566end either course teach stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
