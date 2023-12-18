035d16d5361fe0d95e75e98180a4d96808e25f20
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:25 +0000

    plant giveup blame address branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
