d7651351a94e615cea05cea0bd0982b48508a52d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:38 +0000

    remain terrible giveout uncomfortable talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
