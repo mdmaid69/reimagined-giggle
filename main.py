94c2ee051524e4d374f100731bb2cf52e5acfaf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:12 +0000

    terrible further note lean other

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
