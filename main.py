967a7341a5872eec3f9cbef08b4cd360fc6674a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:15 +0000

    count hospital shut hello talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
