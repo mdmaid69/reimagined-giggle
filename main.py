7f1bbd43b148f48caff9bde46507531f0717d407
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:16:47 +0000

    month begin abroad bye foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
