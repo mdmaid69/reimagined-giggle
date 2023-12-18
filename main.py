9a60ad9792f0ffd3938495033f9f30833dd4e86a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:15:26 +0000

    present fire normal wealth side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
