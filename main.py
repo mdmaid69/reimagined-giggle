6e515c6e121831b875553f91b44a9dde2eb32da4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:06 +0000

    control keepoff upset immediate clothes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
