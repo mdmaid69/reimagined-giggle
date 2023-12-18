b6d052c641b0ecbb7835e11e21f91c4326d4a6e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:03 +0000

    keepup late order belief start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
