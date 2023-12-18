8382bdd482e1be9556df4c738ae3f25a84d22cd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:56 +0000

    big out anywhere platform taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
