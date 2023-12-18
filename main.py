1aeb7ddef9253acd2cf33ad11c51f973a9b4bf07
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:22 +0000

    agree win see seat dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
