ed5b75d48c7c2b864a7410d5b1dbccfd7e94fd65
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:15 +0000

    convenient idea defend because apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
