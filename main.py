49b3eadab9e99852aeed65bba8832f5e17763372
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:25 +0000

    runout something whichever less watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
