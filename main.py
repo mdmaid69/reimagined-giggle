a9576da2dcb599014fa0964ecaca5df53ea6befa
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:57 +0000

    plastic search in head official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
