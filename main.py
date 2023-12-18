768c6d4d2d63552258e16e40822e61029454ed19
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:03 +0000

    flavour improve expensive bite properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
