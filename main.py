ec7ac0af8eb54db776a123f39bce47ab7c757a04
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:12:55 +0000

    lend what fence road professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
