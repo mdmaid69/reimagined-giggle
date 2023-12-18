41b7ec8c009ce86a070f1a2b22f077c87f1dfeec
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:45 +0000

    information prepare wind various float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import math
-print(math.pi)
+def calculate_arc_tangent(x):
+        return math.atan(x)
