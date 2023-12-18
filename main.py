74b1041b3e5b3b48f343e2ff0d09c2300bce66ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:55 +0000

    second tonight general strength yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
