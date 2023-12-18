80e0e9391cb5440c0fe90ae664907a4e93a5468f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:47 +0000

    birth still comeup complain familiar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_max(lst):
+        return max(lst)
