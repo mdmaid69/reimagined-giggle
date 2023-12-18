209a296ab331edee5fd68808186fc2659e48fcbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:26 +0000

    slow material callout side already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
