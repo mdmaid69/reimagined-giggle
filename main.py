5a14214bb5d0cb4b05184c4dec30337efc1e3e56
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:08 +0000

    ear hair proof involve mark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
