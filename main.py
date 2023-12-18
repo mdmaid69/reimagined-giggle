d25f49b0eb13d5db341f5349c808de59bf73bf35
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:07 +0000

    tight dark church depend calm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
