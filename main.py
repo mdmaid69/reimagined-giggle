1ccb9085406d247d7f4c59d8f47f2cd944da16c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:15:02 +0000

    nurse easy cheque tax field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
