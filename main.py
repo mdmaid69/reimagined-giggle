4b0f73ecc6fdd2f1ee0234102dca2b62c5b44124
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:05 +0000

    typical discover hide classroom scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
