a9ac11afdb58c200f28f9e379d521b7ea71a6101
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:36 +0000

    hers eat rest danger soil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
