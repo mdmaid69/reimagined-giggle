55fde45da78ba72b9e541c2777438dde211140b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:19 +0000

    exchange idea through carry government

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_to_hex(n):
+        return hex(n)
+  def convert_to_octal(n):
+        return oct(n)
