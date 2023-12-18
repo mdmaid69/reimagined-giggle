cf50191eceeebc4bc4cb0fd3aba0921aff03c766
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:15 +0000

    fond onto vanish escape getaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
