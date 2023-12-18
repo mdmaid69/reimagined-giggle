2bc5dca4e35d9c11382b3d2817e071e47d0e7dea
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:56 +0000

    turnaway talk band minor shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
