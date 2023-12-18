0c432abb3d7501d4ddbbbf35acfb9bf6056d5869
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:46 +0000

    such suppose regional question soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
