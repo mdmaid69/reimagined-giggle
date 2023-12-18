fa76ce10acd626f9813265d3d9e81f181fd9a1d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:23 +0000

    blame on wear comment themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
