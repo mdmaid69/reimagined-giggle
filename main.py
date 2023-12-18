a417ba62a1901feff02cd295dc04dc40a3631dad
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:53 +0000

    pinch confuse reply run sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
