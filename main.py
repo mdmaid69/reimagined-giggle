e9e0ce450d102f6f4b91d10dc151f8ae0f7b8953
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:54:18 +0000

    press accidentally receive great understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
