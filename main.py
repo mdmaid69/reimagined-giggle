5b8f966396f8bf920b5fc10331b2f2b294a1cc9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:25 +0000

    more cross generous ride interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
