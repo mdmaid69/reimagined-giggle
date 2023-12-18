5d35555f238921be0d479e74e15c7b743afc1f2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:01 +0000

    change muscle coffee terrible part

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
