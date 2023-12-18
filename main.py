35cec8e307cac80fad942b68629d0d399cc39800
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:26:58 +0000

    sign clearly tune which formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
