418aa5298e4261330f5b1f75c7ee44ab989724bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:14 +0000

    blame behaviour check where surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
