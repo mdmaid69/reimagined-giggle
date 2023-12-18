eee73992d71558220650db8fa3e99e38f936a887
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:01:15 +0000

    slice music prize design fly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
