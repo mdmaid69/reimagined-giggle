4e884b2ee3763b1f7edc788122e300608690a5a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:14:47 +0000

    flood pride Ff plan prove

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
