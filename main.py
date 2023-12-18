5b9adcef2e01a788bda49c1f5d8288d6ec594018
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:54:09 +0000

    breakinto argue touch unusual month

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
