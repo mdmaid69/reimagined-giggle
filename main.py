09e19d5f70e2ab6033ca20306b4a82db8aa40c78
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:18 +0000

    everybody no warn picture must

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
