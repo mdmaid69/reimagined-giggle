0773b64588d0f8b9c8be656a73f68bdfb4cc173a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:09 +0000

    second proud instead very finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
