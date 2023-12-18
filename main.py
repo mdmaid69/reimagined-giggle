3540479b1ac16a21105d2f38cd31df0d156c6798
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:20 +0000

    give spray basis demonstrate cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
