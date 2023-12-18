00e1bc3cb63bb3b23031e4f1352d09194098b92c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:08 +0000

    wide love fault spread flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def is_odd(n):
+        return n % 2 != 0
