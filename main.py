5231504525359e7087aff638428fd0f037084b76
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:06 +0000

    straight picture light wake confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
   def get_file_group(file_name):
         return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
