15843cabaaa3b921e1f84ec2e27e1db4fa4ece44
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:08:36 +0000

    extremely ability bottom good Kk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import sys
+def exit_program():
+        sys.exit()
