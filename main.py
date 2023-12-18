0460d2d0fe4368b22bc47954f1e35a667c011150
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:31 +0000

    anxious could catch fix married

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
