c0a4db285a73c7faedeff0d1699793ef18696bbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:53 +0000

    there claim hurry talk house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
