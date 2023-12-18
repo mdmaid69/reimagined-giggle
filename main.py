4adfc1acee20348fd70140bf1c1ecbe29bf6d1ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:29:04 +0000

    public beneath down field bother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def convert_to_hex(n):
-        return hex(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
