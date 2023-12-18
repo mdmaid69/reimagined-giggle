76f4e846f2a2654e685d08d656ff299ffba3eede
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:49 +0000

    turninto remove store outside accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sine(x):
+        return math.sin(x)
