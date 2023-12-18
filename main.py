8fb139bffd91ce5a41aa4edddff228be3eccb347
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:46 +0000

    low full scrape pretty left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
