32f1ebe0bfd897931c2e428ae8820afdbbf9c479
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:54:26 +0000

    mistake confidence tie narrow mean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
