469de9221f3767be3e1b6ccb5964c61365973784
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:50 +0000

    discussion close fold reply bar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def reverse_list(lst):
+        return lst[::-1]
