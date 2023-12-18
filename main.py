c5a6f6b3b2a0e6b3575ebccee562be93273f18ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:32:29 +0000

    neither breakout shut lonely explanation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
