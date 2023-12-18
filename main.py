1c87b83a6243258942015347d806b2226a8399ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:27 +0000

    clear soft pronunciation actual problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
