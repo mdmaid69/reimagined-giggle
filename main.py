f6ee29ef3d6b9b6a9a838a3102454c515aa46f92
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:19 +0000

    after lookaround shame joke and

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
