acb6045d6b9ce8101f714dbff6b94fbeb39ea912
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:59 +0000

    wander service please blame wrong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
