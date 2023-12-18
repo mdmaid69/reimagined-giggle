76ea946db63bdf95d8100dda65395c65f328ed1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:24 +0000

    wrong uniform concerning tape basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
