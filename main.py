9c876477dc70df0bdb9b4564ef4d894f78e8c2a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:07:50 +0000

    pupil clear pay minimum serve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def reverse_array(array):
+        array.reverse()
