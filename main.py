b880e3b1d840793bd88a19eb5fe4d5cb7bc378ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:01:59 +0000

    whistle bringup bye inquire again

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
