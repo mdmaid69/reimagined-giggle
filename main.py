db98b4eed2e38dc9ce6d9da461180a30b9fe719a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:16 +0000

    joke holdout march calloff television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
