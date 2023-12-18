d47023453ae0e5bedae5c5ae7dad14fe826fd15e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:51:03 +0000

    basis bend zero female defend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_min(lst):
-        return min(lst)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
