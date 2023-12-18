9c5983b2aabf48004e9f20ea8dc250d3844b698e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:04:05 +0000

    hurt corner experiment tape address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
