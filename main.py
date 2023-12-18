1891dd3261916e97024c39a9f88c397283a7a8f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:10 +0000

    birthday concerning willing card improve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-def reverse_list(lst):
-        return lst[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
