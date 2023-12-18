7453dd312ae06fd0ed761d099267b9955f1afc1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:00 +0000

    smoke sign basis easy thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
