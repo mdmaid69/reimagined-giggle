e0b2aefd64e0bac88706c98c6b89b49e8bed853e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:42 +0000

    field care delicate crowd her

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def multiply_numbers(x, y):
-        return x * y
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
