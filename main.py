d7263f15cd70ffaadb6c32e2a2a381181279b760
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:08 +0000

    who increase discussion stop direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
