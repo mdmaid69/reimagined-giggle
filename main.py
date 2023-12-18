bfdb46bf9d33f20387c984d0dbb020ffb1daad20
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:14 +0000

    accept clearly quit concentrate expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
