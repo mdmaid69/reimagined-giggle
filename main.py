83ed3ee67c45b0734e7d1d9ab876c65e4eddd6e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:51:11 +0000

    congratulate definite switch result guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
