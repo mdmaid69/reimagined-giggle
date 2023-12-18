b1c9d0f3e09ca2f3d8dd77959310cdc65c329e25
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:25 +0000

    dinner there which unhappy need

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
