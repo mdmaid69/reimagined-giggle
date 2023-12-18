0b1e2f0a4da4b7f7455404bd3ca57045657a14cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:14 +0000

    expect cheek snow only shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
