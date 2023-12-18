176a58323ebafef9e609705b84fc663ee3c49053
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:26:09 +0000

    colour slow study carry usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
