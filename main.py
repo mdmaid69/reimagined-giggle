fec55581b2164023b96ddae9c09cd186dca52572
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:07:47 +0000

    advantage quality takeoff speed second

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
