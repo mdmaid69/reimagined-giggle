5a35c14cbe605bfd703b36503ce970ecda1d5fc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:59:05 +0000

    ourselves soft disturbing underneath gate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
