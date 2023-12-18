53c91b7e2b9c12c0dd68aa468b16ecc2adf12353
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:30 +0000

    official imagine impressed cover ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
