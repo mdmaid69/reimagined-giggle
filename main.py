cb8dbd59ab889d115af6776200c62e5c42008a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:23 +0000

    hardly majority dead measure count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
