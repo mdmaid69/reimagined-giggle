da05de66ab6b99b1582d0ac093b3314e397da944
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:09 +0000

    runinto independent whether wash fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
