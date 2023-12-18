2bd580014907c81a22c69e881fc3786c3894ca5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:54 +0000

    independent swim steer pure worth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
