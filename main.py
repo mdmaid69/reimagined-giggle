cc3272cbeebe088d349aae7a68a3604727d08530
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:25 +0000

    offer indeed thing mean motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
