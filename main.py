ebd772ac8d837f54ccbdf8fe34b3a30e7326bf4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:00 +0000

    ball job that borrow professional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_set(array):
-        return set(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
