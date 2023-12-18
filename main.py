6c2a9055730e66007162e82ce5bba2614803b198
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:49 +0000

    experiment news lock division open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import time
+print(time.time())
