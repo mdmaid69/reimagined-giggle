c127b90ca989df2b51a20f138ced19da01fa1b0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:09 +0000

    means comment branch disappear crime

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
