924b113bd2309b405069e4801e7a279909fa0a3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:03 +0000

    situation various shrink beginning demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
