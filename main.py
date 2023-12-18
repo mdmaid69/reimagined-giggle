111c6229b2f9739f4ce0060c142c0be07381f1de
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:56 +0000

    open low politics convincing man

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+def find_max(numbers):
+        return max(numbers)
