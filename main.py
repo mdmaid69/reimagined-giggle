70624054f5c5c4b0e15cdda927644365b5f1ca49
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:36:46 +0000

    sorry angry complicated getoff marry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_max(lst):
+        return max(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
