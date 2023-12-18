b01a998af1e70ef567e79f3b4726f767b8ef683f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:57 +0000

    afterwards familiar handle almost balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
