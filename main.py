d089d41e44ad4ca688b2f49296751513eeeb7dc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:15 +0000

    wheel exchange steam introduce everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
