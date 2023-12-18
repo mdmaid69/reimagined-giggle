00625df5ecf49ec31f1e8c923f76f6134e8ab660
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:16 +0000

    still cause remember heavy train

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def reverse_string(s):
-        return s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
