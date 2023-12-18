2f89dce5cbb9e5a7593fcacb50016f7c2e551b55
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:58 +0000

    especially no owe open common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
