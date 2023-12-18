40ab71d7831cdb2d96f942735a29fff203567d43
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:13 +0000

    already heart confused recover need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
