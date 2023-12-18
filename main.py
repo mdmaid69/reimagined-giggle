e44c3202fa8a0affd2439dfda7575476e17f5d72
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:44 +0000

    interested blame prize child where

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
