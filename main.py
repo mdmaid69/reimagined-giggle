c333f9cc87974d24f1957a891377bbebe8ba7dff
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:06 +0000

    roof noisy arrange keep generous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
