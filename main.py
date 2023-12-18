a8a5ebe236353ea8abdecb0d57321ef40d234906
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:59 +0000

    bye jump spring low neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
