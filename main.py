abfb96db6d802bb30127081d64a474eda2e09da9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:36 +0000

    night plant off how wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
