8d2a3f540e69d151fc9224af8ede8b64ffd5c466
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:45 +0000

    translate reject Uu centimetre environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
