be6a644c12e5d07e0d05cabbeb463a8d487275ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:30 +0000

    murder contact make kick broken

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
