05ac6de2e9005d6d58c5d75a003635a100a08a92
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:36 +0000

    runinto comment surprised present permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
