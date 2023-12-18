1fc2a551f5ea50956d5f80f967601b7594fb5d13
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:05 +0000

    talkinto see change most ignore

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
