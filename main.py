95475695d97c03d062c54db9c8d5398cd1366be0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:12 +0000

    chief flat before pattern stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
