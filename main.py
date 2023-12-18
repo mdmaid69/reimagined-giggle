f22349bc9e5ca834659891521b5a10f2fd455419
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:11:32 +0000

    combine smell too affect throughout

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def add_numbers(x, y):
-        return x + y
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
