fe4645d383e9a1fe5ea35f04f54c409f792e1595
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:31 +0000

    go food sale exercise approve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
