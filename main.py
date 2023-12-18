1f6d1bde7d04fbe5c7a27371947ab1666164348b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:04 +0000

    again activity consult used repeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
