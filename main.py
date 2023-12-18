279a062969f36d796e8987e495ffa0a5f1062a18
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:03 +0000

    mark snow kill hole general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
