6f3082e98d49b684f2ac7efe6c4cb3b5ce358778
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:08 +0000

    police study total either breathe

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
