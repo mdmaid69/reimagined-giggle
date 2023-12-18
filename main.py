6a70d1e911b3535d8c575d81fb60137a97ffc247
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:58 +0000

    mark conduct tense glass inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def divide_numbers(x, y):
+        return x / y
