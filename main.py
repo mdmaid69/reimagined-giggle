e842267bf4fdadf7d76742076c89e038fe170ff2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:46 +0000

    sticky destroy storm country tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def reverse_array(array):
+        array.reverse()
