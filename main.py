fc3bc8161f1a86c0381220b8228f91e025dbde96
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:53 +0000

    run boring inquire skill balance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
