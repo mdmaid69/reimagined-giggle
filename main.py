dfa55bc03db0132b94b5a1da8ed79b1a8cc6aebe
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:16 +0000

    supply turn use might invitation

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
