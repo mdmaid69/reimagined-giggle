c72d124a9570e4efa5a1a9838a3c86722373bf76
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:48:15 +0000

    sing punish target like sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
