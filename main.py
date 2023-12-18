7573d10729a9bbf19d07b02c46792b002bb856d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:45 +0000

    heart hospital then mail able

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print("Hello, world!")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
