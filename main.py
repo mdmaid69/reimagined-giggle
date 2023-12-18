cf0cf34fd32c60a889349f78d3c5560132a29554
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:48 +0000

    bound use tidy neither twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def convert_to_binary(n):
-        return bin(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
