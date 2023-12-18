6e15a3d93102dcc56bd2a2f7e3323685a9024cb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:40 +0000

    suddenly Ee takeaway past intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
