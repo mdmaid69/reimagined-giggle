0d90735efe3d3e925960922f27517955eb4275cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:07:01 +0000

    sleep family base easy use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+print(time.time())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
