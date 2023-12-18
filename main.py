0a5d68c8561b2b0cf4c3801f9332c2886ebde7f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:52 +0000

    bath suddenly during vote experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
