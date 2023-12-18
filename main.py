11a7139bcfeb518cd11c602c3818f7988602e930
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:57 +0000

    raise war anything reach where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
