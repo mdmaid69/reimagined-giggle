c49375e21249b31ad2e728381373dc783268e130
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:48 +0000

    wide risk brush situation original

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+print(json.dumps({"name": "John", "age": 30}))
