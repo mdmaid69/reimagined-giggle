fe6a92b3c65233daefff3b1e32886a3fb2bc34ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:33:03 +0000

    list between pour vote world

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def subtract_numbers(x, y):
+        return x - y
+  def subtract_numbers(x, y):
+        return x - y
