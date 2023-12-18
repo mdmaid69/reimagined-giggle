bef0c011bd04a23a26e630b5473a0c28cbf8ebfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:51 +0000

    properly polite wear sink ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
