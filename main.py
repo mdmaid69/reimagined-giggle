2d7cbb1d85e9b3cb0e277e248e8a6d59ea4e450a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:19 +0000

    field contribute dick complete mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
