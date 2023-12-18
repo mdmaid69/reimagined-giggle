173d86a6f3458ec0d52cbfcd77fd8a1019dcfa0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:07 +0000

    tie post comedown reaction dangerous

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
