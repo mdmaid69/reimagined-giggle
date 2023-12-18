96ec7c4a6cb2d85129aa506d2e73d446405095ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:11 +0000

    sing ours bad uniform effect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
