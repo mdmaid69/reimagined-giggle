2449b56b91f0254c81802f490232515f472a4efc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:33 +0000

    programme callup keepout angry usual

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
