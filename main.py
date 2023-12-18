29b8b585b8869ac8f1c92df1dcd89dfbd5c046f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:26 +0000

    appear arrive high birthday risk

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
