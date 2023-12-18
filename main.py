643c60422e318c884145edc55900f4dc04bd84f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:58 +0000

    firm learn miss choose count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
