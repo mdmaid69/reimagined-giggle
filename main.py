1cc291cd7b4b762fc3999b6a33146967d7cc6c0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:38 +0000

    abroad meal patient beneath hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
