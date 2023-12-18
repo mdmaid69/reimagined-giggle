d0ab2f3de3cce6e0db782fc26c77f612e17db275
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:23 +0000

    relative exist error scream ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
