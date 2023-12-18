cc30b395a8b7190f8cc83c328de8889c406156d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:54 +0000

    number inform minimum sign admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
