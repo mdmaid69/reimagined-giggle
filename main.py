e3e05e3639b81ccee79013832133bbccaff24a85
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:56 +0000

    mad basically object mess early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import re
+print(re.match("h.*o", "hello world"))
