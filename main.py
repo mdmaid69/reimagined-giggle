8af2c982d41b608a66aff63ad5ff9bd64cdd92ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:02:16 +0000

    target future clean order inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
