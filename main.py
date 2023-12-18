a50a57cbff24c5c2801a65aa461921876b6357f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:00 +0000

    snow boil ordinary calmly condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+  def count_elements(lst):
+        return len(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
