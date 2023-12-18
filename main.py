88b7aded9c302a014b5f5512984fec31e5ce04ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:38 +0000

    collect want taste ride frequent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
