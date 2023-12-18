112b19268608022d5d60169f7d2d7bb8db60b657
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:46:31 +0000

    anything date threaten dig bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_binary(n):
+        return bin(n)
