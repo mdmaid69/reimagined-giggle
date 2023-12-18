23e785af8f8ada85b7ad4a7338ea1fbb8004c061
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:47 +0000

    refuse fasten let chat amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
