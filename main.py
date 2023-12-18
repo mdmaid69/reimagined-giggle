b70982379be2d462550e36f0a3147a4441dcd7ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:18 +0000

    building she equal we base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_median(arr):
+        return np.median(arr)
