f75bd989b3be32948c5b37b8a91a04b443d8e7cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:01 +0000

    play wealth centimetre both lower

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
