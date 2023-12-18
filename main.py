15e5c7b3fe7334ab2a6f991896377456ff08294a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:05 +0000

    the swallow ahead married display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
