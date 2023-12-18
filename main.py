ace44c20616fcdc3ff54ffc39e2b87ef48d3b408
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:07 +0000

    record nor guess 571enough left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
