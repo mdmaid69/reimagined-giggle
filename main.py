0c52a2aa4de6f914af79729796ddf84924aa776f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:28 +0000

    poison finger trap face play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+print(math.pi)
