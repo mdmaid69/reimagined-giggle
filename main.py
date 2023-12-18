c9ed486d8b0fb827f333cfbf062c3b656bce02d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:24 +0000

    angry safety wealth match tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
