2539f0cad989c09cb57cf8be14c77035e45f2652
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:01:38 +0000

    laugh hear debt punish wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
