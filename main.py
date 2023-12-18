ded73cd6679453c2692dac9ddcd762155470ec75
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:12 +0000

    talkinto makeup birthday but as

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def convert_to_hex(n):
-        return hex(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_repr(array):
+        return repr(array)
