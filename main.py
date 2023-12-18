ccb43f725045bebd53659b145226ad847d6f8075
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:10 +0000

    behaviour relief around borrow mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
