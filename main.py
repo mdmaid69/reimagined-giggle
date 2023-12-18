773695e85e9f4bf5c04fb8a7fcc6be1658020747
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:06:42 +0000

    immediate keepoff blame that little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_area_rectangle(l, w):
+        return l * w
