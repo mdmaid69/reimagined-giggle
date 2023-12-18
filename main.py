a5568df316c7640c32ee00388bb41838440d856e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:16 +0000

    drink fear building trust congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
