82a783ea15c4bf6615e3480a747d2955228da03b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:32 +0000

    mood prevent somewhere laugh task

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
