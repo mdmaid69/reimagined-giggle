96079b324e2d79abd10587ec6e1849448fe3f96b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:09:06 +0000

    slice coffee pinch weather approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
