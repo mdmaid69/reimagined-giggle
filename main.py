51953dc41b5940579e694a139d42024765e345c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:29:27 +0000

    cool turnoff pour especially colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def subtract_numbers(x, y):
         return x - y
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
