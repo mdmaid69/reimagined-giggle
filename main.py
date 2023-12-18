f9f2997f6865a8dad1e02ba967c39b5fecda9943
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:48 +0000

    sting house lookup mysterious bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
