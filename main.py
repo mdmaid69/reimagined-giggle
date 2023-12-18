1a3e266fe2bb36b0705bb6586519ca569053bfbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:42 +0000

    trouble ordinary vote limit Ff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
