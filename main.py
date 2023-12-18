99a18ed26d749adb0e3083d46517a85b7cd9b39a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:55 +0000

    trap quiet worst comeback nurse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
