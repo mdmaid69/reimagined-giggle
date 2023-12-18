fd5ba21640a2f864c645dfc17a2511bc17d447fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:24 +0000

    idea use marry fit sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
