6770846642a61e24b384e0998a20433a30d67f5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:54 +0000

    tone undo flow marry tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
