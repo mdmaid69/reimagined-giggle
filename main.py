b2e23cbceaf7531d8297524cc11facf210f93908
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:10 +0000

    exam allow pollution plate interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
