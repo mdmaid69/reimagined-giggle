29784cf64bcdd71cfd908418bcdb5a4432a598b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:47 +0000

    advice doctor to scream later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
