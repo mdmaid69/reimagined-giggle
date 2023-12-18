c7bbe8e06a47a760e7df1d0eadd9f626c46ac580
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:01 +0000

    holdout frightened wish actual man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
