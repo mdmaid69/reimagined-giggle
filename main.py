610a400fcd07d0ba5e3872d97c3a5ecf6339dcd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:27 +0000

    themselves process side advice getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
