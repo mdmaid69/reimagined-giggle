b4e81c63d9b68644c5ab1af74cab935ab547e99e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:44:23 +0000

    operate pretty smell fall watch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
