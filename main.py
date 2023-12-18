1ea5bb21ff2b708fe022cc3235facfa87249afcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:00:29 +0000

    argument takeoff straight entrance check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
