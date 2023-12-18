b4bf74aaa9a57c928d4d47ad23323993e3a36d81
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:42 +0000

    Oo frequent public actually space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
