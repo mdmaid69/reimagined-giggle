aed48180a2f996dc3ecbb7648a617ff680ebe527
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:01 +0000

    maximum inform owe minimum mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
