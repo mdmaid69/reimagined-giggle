3237bb4f27fa89d270919b23ec6bf5f25520881d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:38 +0000

    his hold clue try out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
