bff0ddb4045552e681ab32a80ddd0bfc6166dbde
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:21 +0000

    scene or win bag patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
