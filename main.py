daeaf0965e409abec1a1aff9e12febc8349a0808
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:59:50 +0000

    yet illness sound different off

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
