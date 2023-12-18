46f36c55caac8c35681f0748f42b600977a1f81e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:55 +0000

    flat the rest wash lay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
