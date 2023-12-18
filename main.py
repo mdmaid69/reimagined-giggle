bce83c5de787c6b30938b1ca6a16c1770e234cfc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:56:48 +0000

    fun reserve contain meet television

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
