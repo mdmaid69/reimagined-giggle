5d426dc8c8f838d82576dd2ff96ab3592cbe14c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:44 +0000

    sit love precise platform approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
