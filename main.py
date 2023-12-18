0cd507b402c97785a0d02c617f981f209ca90ac1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:55 +0000

    keepoff sense shoulder hear development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
