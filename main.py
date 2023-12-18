92deb2e9beaf611c7825fc0f5c7223d33ccab5d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:08 +0000

    consider modern rely argument best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def square_number(x):
+        return x**2
