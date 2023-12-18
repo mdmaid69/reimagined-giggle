c1be3e071dde144c9d06bcbfbf7597bc62531d4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:51 +0000

    married favour insist last support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def is_odd(n):
+        return n % 2 != 0
