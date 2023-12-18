38fc4e615d2e254ae12260a0fb7ba9846e1dae88
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:00 +0000

    happy born pleased content violent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
