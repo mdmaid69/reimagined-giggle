0d986c6e338847db4179cfc2e8a395f74ee64da3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:51:22 +0000

    frequent question bet dress pattern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
