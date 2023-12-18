e8ae78ae7137f5e5425b98ed99a0c88a103d8974
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:08:28 +0000

    he happy division reach fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
