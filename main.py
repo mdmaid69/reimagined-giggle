06e5625eb988c6910517312b76089c9b220fb552
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:22:05 +0000

    hers call bar area soon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
