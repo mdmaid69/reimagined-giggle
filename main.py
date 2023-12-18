35cfb3b82cb62238bce9a9ff5689a2631d4d2788
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:53 +0000

    bad month impressed experiment conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def greet(name):
+        print(f"Hello, {name}!")
