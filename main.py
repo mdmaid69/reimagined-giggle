e7404725946665147ea6e10eb0df8f39f8e5e562
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:21 +0000

    clear century dream contact away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
