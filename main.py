6897e0ddc3ed1e8ff543d745762570a64626b382
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:56 +0000

    Yy expect cover trip beyond

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def subtract_numbers(x, y):
+        return x - y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
