ae2435b925b7585a83319a56353743599c45a0e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:20 +0000

    change information matter the wide

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def reverse_list(lst):
-        return lst[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
