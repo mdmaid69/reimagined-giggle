ee86cac9f69cbbf7b8d242d07f3fd9d8e6576956
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:33:54 +0000

    very succeed fine that closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
