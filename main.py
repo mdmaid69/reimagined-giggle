7cdc755cd87ca9ccaf3b1b01af3765c85852d72e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:40 +0000

    avoid fond deaf tradition post

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
