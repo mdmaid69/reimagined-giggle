8c9f908d188d3e24c5401fd3ecef697e56f48a08
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:18 +0000

    whatever nothing loss runover have

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-print([x**2 for x in range(10)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_unique_words(sentence):
+        return set(sentence.split())
