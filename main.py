7aabe7ce92f1f34cf58cffdd86ca330ccf2720b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:39 +0000

    sink heart much gradually challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
