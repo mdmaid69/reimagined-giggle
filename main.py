0b05f21cc078d264b8f36b0e1615a9680f52b0a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:45 +0000

    afterwards only pot people convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
