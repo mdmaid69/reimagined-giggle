e0fa2093b85848878aba5e567eb3cb9708fdcee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:45 +0000

    like behaviour tap runout her

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
