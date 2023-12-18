f3658f560b4feaa4d48023c162d90930a0d64863
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:55 +0000

    back range fear several affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
