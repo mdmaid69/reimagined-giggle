305c3cf2deb04c838498ce56e978bd66e91bc8ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:04 +0000

    burn breakup worry drink personal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import time
-print(time.time())
+def greet(name):
+        print(f"Hello, {name}!")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
