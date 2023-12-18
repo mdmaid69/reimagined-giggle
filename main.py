a9b30d375655a1a053c17475209b66761bef41e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:56 +0000

    tape avoid swallow exaggerate system

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
