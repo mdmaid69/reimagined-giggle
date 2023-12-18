d7cfc02c4b0f5bdbe18f36543bdc50e5765d0543
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:44:52 +0000

    between fly cum method yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
