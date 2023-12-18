20fdc384f974d8498b65c2304c95b6a4e92135c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:03 +0000

    front label close state valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
