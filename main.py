172ee180831bf3cf9504b5ccd10c88ae2e2bee17
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:14 +0000

    necessary loan male direction comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
