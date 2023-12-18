3b2efe7fbca27d859e4545404b9bf1355108853d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:54 +0000

    wear children together sad sit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_list(array):
+        return list(array)
