83b44e36ae9d1c386dd961edaf9235f1f4e8845f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:51 +0000

    slight plenty performance live holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
