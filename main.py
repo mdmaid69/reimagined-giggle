c4b838cf698a96c47959f912695d2373dd3cb31c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:50 +0000

    consist right match square destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
