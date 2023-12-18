3ac514277fe6a264b15ad85bb36eacf3ad7e29f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:03 +0000

    stamp wash regular or close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
