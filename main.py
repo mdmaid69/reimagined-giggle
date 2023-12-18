b6c8781ae97f94d1815cb061bfa5c3737a0c59c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:50 +0000

    range watch stretch shock anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
