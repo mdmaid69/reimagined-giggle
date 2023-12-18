a4584c8596282fd6179a1ef1eca8319f9bf9bc96
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:26 +0000

    holdup aware wind should official

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
