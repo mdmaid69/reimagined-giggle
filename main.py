932bd928d372172aff1e360430a4cd26c9f08ff5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:33:16 +0000

    letter whatever blue below communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
