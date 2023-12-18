d803987c37a694666cc9a65a9eab5ba8848456be
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:29 +0000

    up obvious top collect throughout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_odd(n):
-        return n % 2 != 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
