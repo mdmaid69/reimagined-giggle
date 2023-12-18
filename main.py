d4d3df2a0bf3e77665f88d7bae089d2890cdf815
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:50:43 +0000

    crowded 571enough several part amount

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def count_elements(lst):
-        return len(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
