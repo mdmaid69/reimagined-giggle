e1d93e262b6ced775d01a4f6375fed1e2e1f6a16
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:28 +0000

    concerned follow keepout talkinto rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_user_list():
+        return collections.UserList()
