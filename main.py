f7bf2acc968eaaac27c48690aeaa10f092f1e1ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:30 +0000

    funny than drink quarter similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
