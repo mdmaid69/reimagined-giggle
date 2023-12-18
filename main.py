85029bdbdebe2ccdebc621788a511c0bcc2d9b5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:12 +0000

    maximum capital male doubt independent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
