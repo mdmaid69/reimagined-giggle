05075ece763801d516a689e679659136af1ef4f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:38 +0000

    hungry together suck could pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
