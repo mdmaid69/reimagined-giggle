43cbe814f8281e30493467d76a3428dbcb397a3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:52 +0000

    before top beautiful drop person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
