a91521b98337a55e3de69a214a94948c8f63ac5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:21 +0000

    call style short everyone talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
