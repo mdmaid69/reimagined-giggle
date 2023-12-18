0af7a204e2da56e07bb3d32f16d478718df40de0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:02 +0000

    health guess thinkover guard extra

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
