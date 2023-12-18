4dca5b7f0e2ddc21377b2e1da616b8a31efde5b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:58:31 +0000

    curious fault repair container sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
