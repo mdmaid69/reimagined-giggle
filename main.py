7eef0e47e8781497319481d36185698de7f95f35
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:13 +0000

    idea bring process trial attitude

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
