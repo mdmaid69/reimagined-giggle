32835ce3b8b5d54778a27c69083bcdb22b7eb875
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:21 +0000

    that style sure shake cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
