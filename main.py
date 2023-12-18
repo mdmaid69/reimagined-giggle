5360f5faf595943a80992d503ab579003f411746
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:39:12 +0000

    justice would game importance hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
