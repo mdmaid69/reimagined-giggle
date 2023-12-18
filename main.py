6b5436a2c6e857ff94c04e965018075783a26e0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:04 +0000

    drive correction worried quickly feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
