9072b77a2f4a71b9126f7ecc045511f1edd1cfbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:50 +0000

    truth compete scheme offend hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
