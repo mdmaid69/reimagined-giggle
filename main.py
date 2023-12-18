b9ddb6a748429e526157864cd5890a9ac76aed27
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:30 +0000

    well silent disturbing pronunciation rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
