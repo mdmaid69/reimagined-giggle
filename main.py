8c730e52d024a98f7c904758ac5cb31976c5441f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:44 +0000

    change lock considering software fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
