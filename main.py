9913bfc55dea509164990ad4801838050c2782fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:37 +0000

    system reply middle Pp explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
