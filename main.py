e098b15d44475137fdb0ee74b7d94e9ec296a2c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:11 +0000

    condition unit back stranger intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
