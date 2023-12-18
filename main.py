d737b14daee596de21146a2798bc20841fb8360a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:15:53 +0000

    dare breath Bb negative young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
