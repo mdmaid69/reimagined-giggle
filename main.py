eb49fed32b6fbf12022588fabe50255aa0f4fc75
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:51 +0000

    ought apply computer model track

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def change_working_directory(path):
+        os.chdir(path)
