c296402beceab400ed22ce8d878c694de10836b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:09 +0000

    form tear soil circumstance sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
