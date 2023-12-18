c79cf868161a8fc1b48b0cf4deaab99a3e1f9bc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:44 +0000

    complicated love start better violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
