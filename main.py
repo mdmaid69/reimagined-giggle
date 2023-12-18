f86a5aa1f9a3dae958c058766ca1b30ffd188290
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:00 +0000

    giveaway tour relationship trip cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
