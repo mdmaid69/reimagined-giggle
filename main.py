3610d89f4577988fdbdd75f9f760209a8158841d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:26:56 +0000

    snow loose injury powerful demand

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
