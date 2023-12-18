dd48e451b85a5b15e0851da8b586c865b8549bac
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:20 +0000

    whoever scheme action sensible busy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def count_elements(lst):
+        return len(lst)
