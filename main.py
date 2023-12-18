cd730839e0f923983bab100fe11d2afe40a76a89
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:04 +0000

    slight wing price present spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
