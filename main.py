d577e3a24d2b97a59ca856363eda700d19cc6892
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:07:16 +0000

    bet sad sound conclusion disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
