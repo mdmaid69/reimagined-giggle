720046e373985cb8a564556f55addd3be7cf0540
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:09 +0000

    talkoutof partner ear eager reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
