a95b84b52b1c77cbb1e3b05b2044032268f6dff8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:36:27 +0000

    whether declaration answer glass centre

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sys
+def exit_program():
+        sys.exit()
