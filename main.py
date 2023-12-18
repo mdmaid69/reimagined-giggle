287ef6c5142b82c0ee1968f619a95e8e48722d92
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:30:08 +0000

    study square record divide around

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
