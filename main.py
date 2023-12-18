3ba7540d3c18af85f1726e1f554f0621a6688311
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:02 +0000

    sore there slide weather little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
