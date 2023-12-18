9bcf2a507a3dc735303f9e430ff8ef57ff98f0d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:28 +0000

    add goup welcome pipe tonight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
