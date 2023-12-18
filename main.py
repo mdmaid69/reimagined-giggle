287f5385ee0f8537767b3b3c1c0e77f07d46f5b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:16:04 +0000

    suddenly possession advantage unlucky invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
