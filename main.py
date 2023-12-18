8064ed502bf04e0c18760954746ea76f69a02eef
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:04:04 +0000

    translate that giveback interrupt side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def multiply_numbers(x, y):
+        return x * y
