2dd5d6b26b647842052784d51ae74b085bf8ed48
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:28 +0000

    prefer destroy know favourite occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_list(lst):
+        return lst[::-1]
