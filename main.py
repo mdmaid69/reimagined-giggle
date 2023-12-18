2366637ffdd5e5f5bf9993babf0cc732fdac2635
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:57 +0000

    one fight general creature fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
