96b3bf2047464d3cca3ece01e8d1800127dbf3b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:22 +0000

    degree your sticky commit manage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_max(lst):
+        return max(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
