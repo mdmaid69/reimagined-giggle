84fe2993d9bfa8c91e9dc0229c92a8cc4285f3cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:08 +0000

    muscle conversation theirs comeround workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
