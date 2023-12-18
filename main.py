51607b97fde60640015e721eefb45e41eafd90a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:55 +0000

    detail meal read partner culture

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
