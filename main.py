c1da68b7cf72f638c3d2f10f5088e7d013746588
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:30 +0000

    care people flow empty brave

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
