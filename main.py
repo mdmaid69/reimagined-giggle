d0270cd82f8a1c04b1d2d85af1b0fed5094907cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:34 +0000

    fix ought long set sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-print([x**2 for x in range(10)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
