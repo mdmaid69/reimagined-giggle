e7551d34596ade198897dee2db213f0f7ec3ac47
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:36 +0000

    guilty rely share family seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
