0ff6e4350d30332d883f7de992c2819e4e2b2ca0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:13 +0000

    take active closed comment 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
