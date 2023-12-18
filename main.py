851e9ac836cdb336b7148be24811994c78028887
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:29 +0000

    strike statement chase though accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
