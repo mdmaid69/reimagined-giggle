29af37c63556de517c85589700455ba9ba063f65
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:54:45 +0000

    hear light minimum them bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
