75c5bb0898ba7528d88bd54b44e237dac10d4430
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:21:09 +0000

    check speed surprise shame curious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
