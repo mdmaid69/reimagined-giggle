032b71c2cc6519a5c239d7a390bc8aac16249a35
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:40 +0000

    still believe care unit plan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
