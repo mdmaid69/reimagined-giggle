246106ebc7c2acbf1ae7f205db273058cfff7119
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:33 +0000

    sometimes scene conclusion pressure call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
