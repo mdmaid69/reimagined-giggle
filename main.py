bf130e94c1cd0749716b21cd4160be60bf6635ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:04 +0000

    return pay brush lock space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
