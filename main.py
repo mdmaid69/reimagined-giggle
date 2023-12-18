b1290a35e651b2312fe9062a727f9e556c83d381
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:58 +0000

    cover relation wet lucky card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
