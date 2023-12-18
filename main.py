275d071e2c5f8ff7ac86e190461810971a846bbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:33 +0000

    lead many search dangerous dance

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import tensorflow as tf
+print(tf.__version__)
+def calculate_speed(distance, time):
+        return distance / time
