798987207abd60b7387fb6576ba388ec3fa1d64a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:38 +0000

    little burn house silent war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_error_function(x):
+        return math.erf(x)
