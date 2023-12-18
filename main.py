631b77ca59cafac8666f7ca7c95076a770485857
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:03 +0000

    steer save stir care correction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_counter():
-        return collections.Counter()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_max(lst):
+        return max(lst)
