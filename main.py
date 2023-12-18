1870b0c0bff721e3abc24987434172653a080449
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:08:11 +0000

    centimetre judge sky average grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
