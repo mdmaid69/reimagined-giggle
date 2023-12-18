9c50f8f3b5d5bb8045a716ec9b70bf48a7365573
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:43 +0000

    fond fear slice die type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
