1c123f844ec023102e1a2a0c496b0d6eb1b09f99
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:47 +0000

    why splash least body impossible

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def square_number(x):
+        return x**2
