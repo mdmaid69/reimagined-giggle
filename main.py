3f39139b88601b47e0ba5b789c0eed781f8e3ae9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:30 +0000

    wish wash medium estimate mind

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+for i in range(10): print(i)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
