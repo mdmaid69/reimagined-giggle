bba7d9db892622575657b457a72125ffc12a1b58
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:57 +0000

    instead deal no word forever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
