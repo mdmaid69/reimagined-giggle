9c044209fe5ec6c37245fba90f7ae9dbab5f096c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:36 +0000

    pleased mail floor sight gather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
