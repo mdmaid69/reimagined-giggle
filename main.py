1b2ce7fab643f67a6a7ac2fff53d0cc38c9ce54c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:13 +0000

    hit Oo responsible reject effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
