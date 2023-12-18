9d9a1871134b321c21f3433dd8c47f481530a6dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:50 +0000

    hold consult strong eager stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_array(array):
+        array.reverse()
