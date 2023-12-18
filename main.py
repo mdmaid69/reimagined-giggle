48119e5318ec02ea04aee150c6f63ae5fb2005be
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:58 +0000

    scream opposite destroy important certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
