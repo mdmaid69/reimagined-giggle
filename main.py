de7738df3f8d3365064dafdc671ade1cd8a7da1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:08 +0000

    collection hang call freedom repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_error_function(x):
+        return math.erf(x)
