006c7347f97a28498d52be55aefbd4078e486344
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:26 +0000

    stamp recent each reserve bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def convert_to_binary(n):
-        return bin(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
