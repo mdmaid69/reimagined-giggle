02f8b2208dd1be6ee36a6652557c36feccc9710e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:57 +0000

    recovery anybody precise unfortunate combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
