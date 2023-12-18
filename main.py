b66187661da2dbe14233ca8ad42afa492183f51c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:13 +0000

    pure Itself computer sort lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
