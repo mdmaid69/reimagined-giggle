e339319641950e5df5254b053feb5b67edbb58c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:21 +0000

    raise guess division workout injury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
