e02cbbd0179e71a20ff3ba12b8b84e399c1aa82e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:44:42 +0000

    private in minimum confident giveaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
+from collections import Counter
+print(Counter("hello world"))
