0e3f81d5598533d55d939bb7ebd0f199f72fe76e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:14 +0000

    follow worth record welcome thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
