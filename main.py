d702be2e630acfccf6985ffac3f37844dc10417b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:30 +0000

    stamp count either take whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
