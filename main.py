68a915814f71d6f398a757b34fb66244f303f7bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:01:10 +0000

    relationship far tidy cap tip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
