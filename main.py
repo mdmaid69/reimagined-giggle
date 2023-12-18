63bc395dc59615aa6c8acc83c69e366a097facf9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:44 +0000

    fat 568energy unit cheque flight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
