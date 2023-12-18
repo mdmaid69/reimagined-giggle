c04126f97ac888811faefb48a6d44bc3d1c5f3f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:02 +0000

    proper take loud train thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
