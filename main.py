e0fc1a12e2e63c4dca2e9a2db1eaa7dfa6e49728
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:24 +0000

    determined favourite quiet know protection

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import random
+print(random.randint(0, 100))
+def calculate_pressure(force, area):
+        return force / area
