9cdf50eed8cdb9047d1241b8705bbddee0e81ccb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:45 +0000

    total represent claim back explain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
