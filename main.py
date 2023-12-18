8c465c8ac49e5584c19ce46e1d2620e92900e8ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:38:06 +0000

    influence score flow family name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
