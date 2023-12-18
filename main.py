04e57867a02a62396a23dbd8033c22e6dd3f8b67
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:04 +0000

    sentence slowly regard increase female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+text = "Hello, world!"
+print("Characters:", len(text))
