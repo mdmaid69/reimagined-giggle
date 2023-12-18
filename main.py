bebdf4a65f1aef1e7747357e1564a07e6f8eb34a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:51 +0000

    view high number ocean about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+def count_words(sentence):
+        return len(sentence.split())
