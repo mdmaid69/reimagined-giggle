a1d77b2842b2f8a34328ba6dd26f6df8af5f394a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:44:54 +0000

    control grow reaction account beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
