da8d402a65106edc4d678466066c9183d4b2350c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:19 +0000

    round move feel Yy figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
