d10b5777bb331103276cf11ebaffe2b230826963
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:42 +0000

    turnover dare convinced split television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
