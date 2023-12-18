c55c2294c514d1e4c694decc2da960c6ac94da1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:04 +0000

    572enter enthusiastic lunch because level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
