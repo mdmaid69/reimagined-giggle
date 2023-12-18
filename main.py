d52dcae47270db42f5c61135d2137b84b05f2e69
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:21 +0000

    pay cheque claim it delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
