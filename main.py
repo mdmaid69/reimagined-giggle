083c4584eb64f1ce15080f5515d7315bc10be91c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:46:04 +0000

    conversation edge tool need string

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
