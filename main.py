38c3791ac79c725d32adf0b7825904fbceae0863
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:26 +0000

    spare university general content pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
