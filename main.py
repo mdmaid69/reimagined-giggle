d8cc1e86eb39bb7731cd5ee692920aea04f3d7cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:17:43 +0000

    some lunch congratulate tradition bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(numbers):
+        return max(numbers)
