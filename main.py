0fad30c7ce1836cb9866fd0dbb85827ea9e6f6a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:52 +0000

    increase sleep health heat female

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
