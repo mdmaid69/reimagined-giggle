326344c53b02818f72118c76f1c5d9bc911c44e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:34 +0000

    weather goon weather by computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
