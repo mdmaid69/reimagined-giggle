3145ee4ea416bb69c4037f5ddce0039007bb9507
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:18 +0000

    holdup grand compete hope pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
