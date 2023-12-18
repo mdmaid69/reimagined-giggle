3920a75dfe97c232cba1ef66ffb9af43c21b0620
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:59:26 +0000

    rob battery murder anywayanyhow legal

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def clear_array(array):
+        array *= 0
