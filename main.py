48b078b94ac93db7970cbc851c1c6c677f732039
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:26:17 +0000

    lookup importance only insist speed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
