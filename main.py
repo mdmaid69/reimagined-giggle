4d9fd3a699a0a99b27ee90f96e4da3acadf07495
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:36 +0000

    theirs sore 570enjoyable from roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
