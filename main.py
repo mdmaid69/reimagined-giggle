a5ccbb1c9d36463239e0416bcddd67b85a25f688
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:09 +0000

    steep heavy trip admire degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
