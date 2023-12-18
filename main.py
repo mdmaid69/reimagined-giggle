1fbe4fa505a3c40e565dddce947f8c8d018ef6e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:24:24 +0000

    common race type wrong comeup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_item(array, i):
-        return array[i]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
