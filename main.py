886e8f89669d3b04adc7bc8b3cc91317fcea5058
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:57:29 +0000

    Rr somehow shadow experiment always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
