62914b5c9286b05845226606bc9631c40e7cbbb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:32:05 +0000

    idea lookafter hurt priority hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_environment_variable(var_name):
         return os.getenv(var_name)
