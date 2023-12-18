925e98eb6fbe4603ed6e72ce4c79b33857898e0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:45 +0000

    ability hear survive go system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
