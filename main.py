f65f59b67b27377bc1051bdc1794d9faf66b051b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:24 +0000

    repair cut difficult try house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
