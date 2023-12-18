5f5f5e0ec78fb9b6d5f7f95fb6faec6c40568a86
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:07 +0000

    activity live congratulate neck shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
