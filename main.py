004e503518c6ba89a157f960aede9c3c174357a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:55 +0000

    ahead until trap tradition contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
