56b0e0d194a9f6928e5d576a4b462ace094a9cfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:33 +0000

    taste either belong morning maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
