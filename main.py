4651e6c1f15ee95810464363dee9b6ce0d368c1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:54 +0000

    foot break remain like pocket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
