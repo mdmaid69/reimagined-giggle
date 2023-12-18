a46607c161dd36849e2973463c0ebcdf5888bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:14:21 +0000

    live hour breakdown remember job

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
