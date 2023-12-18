fac3979694bebcf8363ba8a5ac87e69852e3691e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:06 +0000

    lay consist smell after regret

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
