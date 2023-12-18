4324f7aa763b64d89ae0faf5da11f65cb5e1cd9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:22 +0000

    cut scare precise shop imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def is_odd(n):
+        return n % 2 != 0
