2b4eba831f1e91899504ab7ff1453a9c8e625409
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:48 +0000

    offer nature gather Yy communicate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
