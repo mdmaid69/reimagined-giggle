987b9faf1566bbf22b87587e562ea7e05089b0f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:45 +0000

    sleep minor cross visit petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
