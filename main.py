9c4fe42c258b12738d8d70a1123ea06e5e402f02
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:02 +0000

    region rarely whichever bad 572enter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
