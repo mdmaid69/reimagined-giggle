163f4e4fb712d4eafdfa2b5afe8d104c6a1400e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:38 +0000

    figure car circle design border

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
