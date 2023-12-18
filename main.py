f278f0de5e1ebfe042e1ce4021efd478ddaaea8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:23 +0000

    blame authority turnout happen reaction

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
