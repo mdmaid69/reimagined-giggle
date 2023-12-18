65363d17a29cb920c81ee732b851cbdb1dc8fdcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:33 +0000

    about sight opposite able watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
