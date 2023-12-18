56a5583b94f05d731de04e7d2d80a8c620150ef1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:55 +0000

    experiment town soak niggaacademy any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
