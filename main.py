682f392d2b4282e6ade93598d835cc8721680dbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:59 +0000

    within on regard scared improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
