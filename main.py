56b14fc530fc30899d6e9606629532d2dca87cc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:41 +0000

    cover practical slow disappointed small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
