c2baec2126cad83bbc3cdc70c92efd6cacd17023
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:20 +0000

    picture design present male face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
