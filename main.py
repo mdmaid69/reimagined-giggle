bc2df85eacb879dc6504d1110bb5218560de581f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:05 +0000

    authority sting police uncertain relation

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def convert_to_binary(n):
-        return bin(n)
+def add_numbers(x, y):
+        return x + y
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
