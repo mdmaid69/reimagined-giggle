0fa4ac57a01039e2a2611ea87541dba61f159bbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:49:09 +0000

    argue chance respect post female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
