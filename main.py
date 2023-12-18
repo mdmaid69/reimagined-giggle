fc8c9f99a45bb62232ddfb226d6a11b3b8fc62e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:35 +0000

    one study thinkover more recognize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
