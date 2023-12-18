70c49bc8301d039453e6f6d87d4c1478cc6fcb20
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:18 +0000

    innocent change through ago summer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
