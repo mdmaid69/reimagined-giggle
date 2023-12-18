4352564cd1f91fb9ecbf37846262f53f760b634b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:55 +0000

    however left practise Ff disturbing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
