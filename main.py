e830ef929ec258fe7db37b9b4f0ddd52522b94a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:51 +0000

    danger snow crowd takeout perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def count_characters(sentence):
-        return len(sentence)
+def find_min(lst):
+        return min(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
