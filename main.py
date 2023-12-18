6972e8fdb86ad1d96febd02b50a7ea88281cc6f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:48 +0000

    important keen fault never accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-print(math.pi)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
