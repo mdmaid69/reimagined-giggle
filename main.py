322f4db03af7275b29bd168edf93fca50f4aa202
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:37 +0000

    solve like proud cost remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
