ffae112e66a595d0463d14e4bd5a7c82766ba3cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:28 +0000

    quiet window figure side factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
