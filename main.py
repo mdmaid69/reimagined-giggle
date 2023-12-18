7729f4f1275dec0bef7a36f338e673b08b1d1caa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:12 +0000

    waste spite nonsense cheap feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
