3aa589855e718917b92ce78918b91428945d4ad3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:25 +0000

    front practice over march breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
