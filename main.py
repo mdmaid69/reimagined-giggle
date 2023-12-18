8effb7a459c7ac0eaa29897fc4c4381e43e1a1d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:33 +0000

    string wonder stay slight style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
