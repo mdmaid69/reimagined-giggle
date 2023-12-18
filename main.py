2848a9516942b5259f1cac618d62d628591fbdaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:40 +0000

    bring block breakinto call admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
