82e4fddc5b77f2e81b4a23c2d947bfb3b0f28495
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:14:49 +0000

    late represent dance push remain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
