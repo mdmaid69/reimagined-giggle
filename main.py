0235acc2d8637fcd2924d7dfca52b76f487ed8ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:42 +0000

    rate priority feeling region tune

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
