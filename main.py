5e401dbaaa266e8e31a592536c58e9c44fd07abb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:19 +0000

    clean total own side extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
