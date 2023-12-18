80736cd6f3f3d2f4f165b2507d9d161c6eba166f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:38:16 +0000

    whom out experiment recognize suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
