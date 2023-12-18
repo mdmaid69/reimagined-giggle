bfc367d8f70e69e79d5ffb5d59cd598866a9ef6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:54 +0000

    else nurse study mixture rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
