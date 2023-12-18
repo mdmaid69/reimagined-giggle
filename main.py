b4385698e611dc6523856550767a923bc73e70b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:28 +0000

    fine study size wild woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
