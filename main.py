5e230ed4ca13953209ebbd634fb86fd112ada2c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:59 +0000

    sad confusing pour very storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
