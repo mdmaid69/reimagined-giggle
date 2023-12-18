3e27faec2ce758642594ea69739d10c8551c8efe
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:59 +0000

    teach pull keepup car progress

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
