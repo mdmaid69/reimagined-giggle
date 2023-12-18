1068fae1f43e0cca3f8cf52c59a3623ee0145962
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:54 +0000

    dear square from loss genuine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_length(array):
+        return len(array)
