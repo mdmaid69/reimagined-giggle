10fbed8db900e7c79d0b3a5878ad74f1624711c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:09 +0000

    accommodation step discovery strict operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
