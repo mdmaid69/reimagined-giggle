6a659c2f14a5b848bba7f1217bdee31a63fd3dc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:34 +0000

    tongue search listenfor relax load

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
