521f73487f1fcadf2b8ddd5318db95764bb7a05f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:59 +0000

    involve expression round thick word

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
