24910262dee1768b2d5a1e24c156e532e0a51b05
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:56 +0000

    risk dull whoever 565end mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import sys
-print(sys.version)
+def print_python_version():
+        print(sys.version)
