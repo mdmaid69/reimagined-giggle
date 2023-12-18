6df39ea3cf2a03b042bc7d4a62a544fe9e075b6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:33:34 +0000

    previous state frighten mess by

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(lst):
-        return min(lst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
