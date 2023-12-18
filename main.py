af7073e7a6d510f8542001b02c8310185be3ccd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:26 +0000

    Nn shut turnon actual quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
