ca6348ea0e6cb6ba415882eea7921bc0ec2dfe5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:25 +0000

    might bus present circle thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
