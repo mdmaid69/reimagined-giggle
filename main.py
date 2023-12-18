477a95139acd4e0ce448029bedd8490faa450fdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:08 +0000

    thing search window about middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import time
+print(time.time())
