16f9b8f8bbf69c7f837160faf1e662987ba10769
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:20 +0000

    already towards next period produce

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
