6a9daf167c601a42cd06b60ed030e2815ff092ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:32 +0000

    bored minimum callout street fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_str(array):
-        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
