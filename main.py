5256a980d19467ad3f79553e0350f35748519cd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:54 +0000

    better band arrange cheap force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
