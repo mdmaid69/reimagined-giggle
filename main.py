a35767a4d928f0828028e04bd3e269f7f5c10838
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:24:57 +0000

    no blind thin record rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
