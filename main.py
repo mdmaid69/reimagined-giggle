dcd204196fa3d4189d608c5a91673e2b408b6470
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:38:32 +0000

    pleased Qq party send little

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def multiply_numbers(x, y):
+        return x * y
