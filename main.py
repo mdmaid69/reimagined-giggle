1ac7e987f79381c2bf8166445768f69b9cec7ce3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:15 +0000

    office contents many shelter interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
