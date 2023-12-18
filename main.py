26e4d6322fd2fbd8bf1dd4dffe1f67093afefbad
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:28 +0000

    available translate minute birthday past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
