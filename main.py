7703effe8cd947400c3d91d87f4d323ceba2910f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:10:39 +0000

    flood black circumstance determined sell

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
