65d72dcd44c86e582c56ce7b4c6828dad0966b75
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:00 +0000

    trade last loan laugh guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
