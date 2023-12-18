6571e43c6c1fdd65f29f1190ff760163f9c91895
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:49:44 +0000

    year confused lunch switch key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
