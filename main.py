8fdde723ae6840d8c7aefadbd6ed81014299c136
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:05 +0000

    Pp upset break full himself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
