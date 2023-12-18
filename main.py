0ee151999e023e61f348c81de199b0bb345522c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:47 +0000

    lesson closely affect frame realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
