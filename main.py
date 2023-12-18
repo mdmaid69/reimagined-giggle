aa93f5455181bbe40f42985207cfa6fe69c5fb0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:33:41 +0000

    sight rich play our tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def get_array_as_int(array):
+        return int(array[0])
+import collections
+def create_user_dict():
+        return collections.UserDict()
