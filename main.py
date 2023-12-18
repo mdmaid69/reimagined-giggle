a13d97e93ad82ac090634c382c3a876d84a3b6cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:15:49 +0000

    gentle correct suggestion except statement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+print("Hello, world!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
