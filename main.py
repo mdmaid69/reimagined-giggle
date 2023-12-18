8cb0fab0de455b1bf519673065564be8d2b7465e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:14 +0000

    sore left week clothes believe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
