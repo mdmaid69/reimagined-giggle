7c144a93b4790c4559bded35197e7d619e6882e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:48 +0000

    problem proof basic instrument smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
