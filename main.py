a2d0210eaee87c919fe75648efeb35029de1a5b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:23 +0000

    community stamp home example lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
