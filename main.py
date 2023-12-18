c45526b81c13278b3347beeb8a28d78772887cc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:26 +0000

    shopping available statement practice autumn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-print([x**2 for x in range(10)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
