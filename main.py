68c3468a61b67fc34333e831878f9e6dc6ba72c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:36 +0000

    study warm drink healthy morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(lst):
-        return max(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
