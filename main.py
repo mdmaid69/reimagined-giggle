1e8e929ac7c75ac8ce080841d7dcf5a6d56f07e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:37:10 +0000

    even lookup early between downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
