49304390fd39e2628ed4536b2a85a2f4efa9b928
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:05 +0000

    match pattern kind imagine around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_tangent(x):
+        return math.tan(x)
