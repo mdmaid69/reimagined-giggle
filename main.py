aa338548b2c7c1af54c38f4e4375e19414c2305d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:41:27 +0000

    throughout tape several Itself choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
