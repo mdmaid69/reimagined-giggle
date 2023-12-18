4223bfc14da5bd66fd0d92d48ec10bfc901e77b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:28 +0000

    punishment before takeout tour should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
