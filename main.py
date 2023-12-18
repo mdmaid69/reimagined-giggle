6ae18bb0e7e582c34bdd17e1c6145045c91dfb34
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:08 +0000

    approval refer giveback equal circumstance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
