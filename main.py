63118bf37c799a5a84480c923e68af9e7d8a3415
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:16 +0000

    angry evidence system worth answer

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
