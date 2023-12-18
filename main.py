ad3a45d3d853b75e12a0fd4149dfce8d1b8bffab
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:51 +0000

    usual lend hurt nigga mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
