7af67e2769deaa889bf8187225e7df042cd87120
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:39:56 +0000

    object dance runaway both since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
