b803be7eefb8b89adc5860b6f327bed50a61da94
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:40 +0000

    such desperate measure yourself original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import sys
-def print_python_version():
-        return sys.version
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
