529e833e967dd391687bcf916914133bece4531f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:18 +0000

    range store shut exact inform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
