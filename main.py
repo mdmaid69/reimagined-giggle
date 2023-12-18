0bf18b95f595750eb83109cf009fedd944d0a339
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:57 +0000

    warning trick interesting year give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
