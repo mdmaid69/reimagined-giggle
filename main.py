d543a3ef34c8b2c8c8412fcecedfac090b1430ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:18 +0000

    work dead constantly while main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def reverse_list(lst):
+        return lst[::-1]
