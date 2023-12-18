d1aebdd3c2b289e45a8abbbf47aadad7e308a422
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:48 +0000

    public sell ashamed whichever cheque

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
