5e305dc7524e722e108b36b8583a6d4d7f2419cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:22 +0000

    world who 567enemy now popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
