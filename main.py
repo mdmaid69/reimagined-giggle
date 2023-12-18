49f039404fc6ad42de583ed3e48f3218cb4d3451
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:48 +0000

    minimum shoot mend form themselves

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
