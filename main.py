c84c4d09ab1459f20495a23f32bf488825c6e0a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:50 +0000

    Dd throughout ashamed line sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
