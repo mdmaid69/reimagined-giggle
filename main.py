39b1ae08214c5deb634e96cdf457af61f83e345e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:10 +0000

    react close drop wear common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
