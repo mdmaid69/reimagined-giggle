c36f0148a54440c5711bbbbcebff51f5841b35eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:59 +0000

    need become perhaps Gg weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def square_number(x):
+        return x**2
