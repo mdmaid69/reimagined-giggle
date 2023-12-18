6b3bec0568ecaec499e89eb9f9f4620f62cdd244
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:05 +0000

    swallow honest respect help or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
