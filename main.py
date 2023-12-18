12a3f1fb49fc5c3df9b59c54b8f083e00b97caab
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:12 +0000

    print clue recent crowd simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
