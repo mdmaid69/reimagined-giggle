15cee65037a469eabda00a8c0d628ac3327afd4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:29:17 +0000

    pretty hello swim concentration backwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
