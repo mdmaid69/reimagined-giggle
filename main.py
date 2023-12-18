30fd598bf83096ce70e2fae15c8249abc28112bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:13:02 +0000

    568energy duty attention police size

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
