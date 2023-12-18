ababa9446bd5a6ea05b0a21228a43d74d82b23eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:13 +0000

    put scale order violent happy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
