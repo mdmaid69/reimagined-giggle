1415f11296b938c27f090843b1f9cb368428c30b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:19 +0000

    split report goup degree hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
