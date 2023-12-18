49cf77a269651fe62743e650e40c8a87d2c4c0e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:32:45 +0000

    this over discuss television lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
