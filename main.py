3d136b0ba15919cabde6802ad8781bb78f548c20
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:26:31 +0000

    flight service cheek ahead soil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
