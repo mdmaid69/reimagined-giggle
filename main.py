05f9dda176795c900e6c49ea36b9d5e384c04548
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:04 +0000

    effect same field exam colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
