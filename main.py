b9532148a4060f7bd2c0fd2457c5088ab84ba3a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:58 +0000

    cut field by fact age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
