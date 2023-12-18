804cd88a69f120a530b157392cc0c32f6a39df8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:29 +0000

    tonight spin limit sensible yours

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(lst):
+        return max(lst)
+def reverse_string(s):
+        return s[::-1]
