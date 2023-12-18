a9c500ec9c6db0ce1defdc1dc5b9fc75712378ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:35:56 +0000

    conclusion firm side say deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
