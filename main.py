588d428e78fe54527630019279c663f829a24223
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:46 +0000

    brother pass special yet repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
