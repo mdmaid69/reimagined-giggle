b07c8f88b4ef11fc7812cd8ba6bafc19d7f0a493
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:46 +0000

    breakdown performance mark either head

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
