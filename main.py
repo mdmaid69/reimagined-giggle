552b79407ed1babd98ecbd2a2ec0d4095e0b402b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:31 +0000

    frighten shout term confusing open

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
