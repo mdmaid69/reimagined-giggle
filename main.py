0cb25b50a91e48fef26002fee1f7e4a8cae6e397
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:52:15 +0000

    declare design set moon shelter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
