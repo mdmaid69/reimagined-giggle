9fab9f7622d0c2a38d0780e7d97b66d46dd8af6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:55:41 +0000

    kick fill example reply cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_base_name(path):
+        return os.path.basename(path)
