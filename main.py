3b33e4e1c88f4c9ee353ece2c5979f210fa04b25
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:31 +0000

    tread breath remain wide colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_odd(n):
+        return n % 2 != 0
