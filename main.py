c8093b5ae78f7800bdcf390a7952f926854fa5e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:40 +0000

    geton base bringup damage fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
