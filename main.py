b6c39176a1d2914ec2db50766125489d026a9d1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:11:47 +0000

    hope thing turnoff hear lose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
