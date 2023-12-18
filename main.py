a163f8426d08e98888db2f07503168f82dfbb23b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:41 +0000

    568energy out takeout boring window

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
