f1169e14e4c86d71243da5ae5d1e96dd584554e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:23 +0000

    past regret glad squeeze debt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_priority_queue():
+        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
