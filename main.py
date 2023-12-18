d4609df984aec28cf16702238fa16bea6b99c678
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:05 +0000

    environment control free uniform seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
