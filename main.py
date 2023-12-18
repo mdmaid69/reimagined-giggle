f6063fb3b5e5c19e06622557a97aa7d3d83da453
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:52 +0000

    until country port attract great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
