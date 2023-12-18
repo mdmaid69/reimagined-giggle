78b86965cadd75a8c034f9a69a3139029c955eba
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:35 +0000

    touch cinema aware difficult giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
