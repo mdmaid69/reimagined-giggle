27c03384afa87fdf76d4800408fb43830c4977fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:43:54 +0000

    perfect used goout moon poison

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
