d6dfcf7801a5ae9c21dc615b85ece9fc41bb26cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:34 +0000

    advantage bus translate obvious mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
