0ffb25613b52fc0f3992d235aa7ab3e628f8e22b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:39:20 +0000

    tough strike combination fun gradually

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
