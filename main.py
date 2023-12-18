a50d4b0177b6a22ae2434ba371007f1bda045d07
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:04:14 +0000

    something regular storm keepup wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-print(sys.version)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
