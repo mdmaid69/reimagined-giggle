054d2dfa4cb38a4f1774d9704b70007a8ab102d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:07 +0000

    everybody like figure criticism top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
