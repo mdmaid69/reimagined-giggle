9631955f30bd15aa526924fbbae45453d3be3c2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:04 +0000

    provide stiff close direction now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
