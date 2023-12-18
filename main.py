38fa177d501f60e2b3c066fd7fd0e9cdac332790
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:08 +0000

    slice purpose quick club conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+  def cube_number(x):
+        return x**3
