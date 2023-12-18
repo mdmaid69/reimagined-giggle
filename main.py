3ccaa9455a8f191195c873b2c6895452fdb5f065
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:51 +0000

    confidence approve dream satisfied exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
