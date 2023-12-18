9aee842410720f3cc1b73040abdce2c46ef3c1bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:32 +0000

    dry meaning read borrow come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
