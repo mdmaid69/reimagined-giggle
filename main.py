13110924ddc992b3cc31a0b701ce3410e40ae4ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:44 +0000

    estimate punish refuse unlikely talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
