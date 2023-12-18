a66f8eebd5b299ff2762fe56dadbff5a315bf083
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:17:15 +0000

    mine cost spoil record land

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import re
-print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
