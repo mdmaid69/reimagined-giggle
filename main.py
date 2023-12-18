d479809e6e077507dce9f93910b7cf9f70676374
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:53 +0000

    able example worried female little

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def square_number(x):
-        return x**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
