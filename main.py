64567d2e7d16b5141b6997182f7dccd624c4eb7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:05 +0000

    rid tread heart exchange risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+print([x**2 for x in range(10)])
