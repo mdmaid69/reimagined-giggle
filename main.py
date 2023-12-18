4b47343038ca17b87fc573ece17d9db38a23ab6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:15:04 +0000

    bar wave exciting lend government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
