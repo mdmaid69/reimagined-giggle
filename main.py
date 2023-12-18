b1232d8cdcba0786b85a26c53ea25ec447c9946e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:02 +0000

    enthusiastic proud 565end Gg behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
