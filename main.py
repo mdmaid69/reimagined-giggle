1ee37bef1cfd0885253d55f1d1d23333914516ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:38 +0000

    pain would bottom mine spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def reverse_list(lst):
+        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
