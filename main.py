34f349a1d7c146dec99fc34658832bec0e67a6ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:25 +0000

    Jj later length record because

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
