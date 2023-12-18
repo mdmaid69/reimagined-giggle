fa89013b9cd480f1f9701d022e8040f6202dd0d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:26:19 +0000

    search less agreement until concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
