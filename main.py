0cc431ae984f6599c56115e4d07143f39528087c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:44:18 +0000

    practice stiff talk rescue occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
