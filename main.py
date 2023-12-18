4987f60cca5ef9bdd5fd9621efe001c698941abf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:30 +0000

    ilovedicks spoil also negative very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
