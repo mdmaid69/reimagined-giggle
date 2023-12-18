f32931ee697751c5ae4d9dbb844809f2d3130ba2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:55 +0000

    boil likely spare understand play

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def reverse_list(lst):
+        return lst[::-1]
+def find_min(numbers):
+        return min(numbers)
