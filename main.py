c37ee59f4388a9492ac8093a427ae327e8e51707
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:07 +0000

    many cost freeze pinch enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
+  def reverse_list(lst):
+        return lst[::-1]
