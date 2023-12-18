e593e350f65048fa0570abb9dd1bc3a9d364d6cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:33 +0000

    behind month talkback intend entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
