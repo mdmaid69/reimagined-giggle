05b7142eb5a34aaf81503488f1e06314900b3818
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:45 +0000

    passage recent equal truth report

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
