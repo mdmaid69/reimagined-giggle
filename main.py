e873362d503f0b60be14a6362cad1be7ed7db745
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:29 +0000

    declare spend terrible track twist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
