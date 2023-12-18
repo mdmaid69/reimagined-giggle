9c6d2dbf719cc02a91898a544f1a466f343c287e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:44:08 +0000

    central blame practical beside getaway

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
