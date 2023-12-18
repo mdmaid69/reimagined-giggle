bb19c5e79036ee9b0415415ea0938f90158b38da
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:06:30 +0000

    increase live holiday lend against

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_sine(x):
+        return math.sin(x)
