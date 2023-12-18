971aada610be8b2eef73f7c96361373ab058290b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:14 +0000

    trust related else not punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
