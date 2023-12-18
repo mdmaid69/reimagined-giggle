d48cdb91ecf205c1f7f5cebeab43dc6c77ce5e30
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:21 +0000

    if grade shame Ff original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def cube_number(x):
+        return x**3
