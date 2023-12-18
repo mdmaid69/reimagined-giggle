2ac6588be6f735260baadfa767986147cc9c270a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:38 +0000

    godown plan feature furniture trust

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_perpetuity(payment, rate):
+        return payment / rate
