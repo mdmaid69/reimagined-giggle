298064193145f27cbb9489e4c03025e54a984acb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:22 +0000

    stop reaction telephone entrance size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
