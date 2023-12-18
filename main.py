131002ac29e0f06bb8dfc3f0443e061080aa43f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:01 +0000

    bank hardly watch principle work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
