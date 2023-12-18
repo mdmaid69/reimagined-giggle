7997587c158f1fd1a2c6cbb450cb7ab5566321a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:38:57 +0000

    dress themselves cut married poison

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Squared:", [n**2 for n in numbers])
