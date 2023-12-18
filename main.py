8d6f080c3dcb5e7bf632987099e8c5258827058b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:46 +0000

    smell crash half prefer exit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_typecode(array):
+        return array.typecode
