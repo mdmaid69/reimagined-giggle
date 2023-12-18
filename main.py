a8390b3eb3152301b136e185cb1085ee7d29dc33
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:14 +0000

    first song scared last disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
