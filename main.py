32ce8b63ee1378c3c09a97d72be83f4d4f6d7e32
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:13 +0000

    brother like improve Vv violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
