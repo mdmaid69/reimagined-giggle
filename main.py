c1c6ea941850e08cb2da004c230927e65de80e06
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:54 +0000

    room cough uncertain scare drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 n = 10
 print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
