3f8ce64026ae744fc1d9df1822be6a948233141e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:30 +0000

    also hers advantage call me

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sine(x):
+        return math.sin(x)
