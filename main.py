53187c1550e7ff41b614bf7042a5d7800ad3fc34
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:22 +0000

    narrow bye behaviour kick wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
