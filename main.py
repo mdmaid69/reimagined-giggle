27f09f447da540621788918e248ff0e196af4cae
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:56 +0000

    politics model keep hot tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-print(os.getcwd())
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
