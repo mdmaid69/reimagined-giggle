a608f7ecbe8c1e5032ca65bdd2cd822f49c0d799
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:46 +0000

    mind train flat tap exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
