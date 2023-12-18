250268fc98c235f4c8f5f12a8db60d27eeeb2987
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:50 +0000

    birthday habit breakin instant friend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
