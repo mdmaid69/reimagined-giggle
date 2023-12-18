7d16bc300fa8201c39eec868b373b1631bf76f07
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:44 +0000

    nice crisis wipe tomorrow visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
