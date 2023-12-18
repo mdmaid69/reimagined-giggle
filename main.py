7d47d116f6ce8854ea36d07866df5fc3d850f278
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:59 +0000

    crowded Itself finally front discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
