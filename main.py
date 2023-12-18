13a554bb91ee7f5a97011fd0440f04f5e39c2878
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:33 +0000

    recent unfair punish welcome fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
