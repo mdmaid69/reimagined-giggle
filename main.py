4a029d7977904bf997eb4247279e646a29b34665
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:25 +0000

    ordinary ball agreement health chin

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
