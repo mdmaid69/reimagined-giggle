95c198a1799a0ecad6cacb4ad7b0f7fd3a36fec6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:36:18 +0000

    later house boil she confusing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
