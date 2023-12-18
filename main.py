ebdbc9ab733413d4257eddedc274d6eb1226f79e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:47 +0000

    present deep morning better hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
