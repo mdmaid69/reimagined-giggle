64f88e621f2b61258a77d45dfce96f92a8302b37
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:35 +0000

    service breakthrough only work win

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def set_array_item(array, i, item):
+        array[i] = item
