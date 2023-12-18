15dee47b9dbd291c5657bdb7e7277d3b37dd5b0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:46 +0000

    total holiday would basic tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
