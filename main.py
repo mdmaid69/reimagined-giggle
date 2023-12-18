0116e2e4e4ea82f2be5fdd661507ee954a4b5d06
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:03 +0000

    distance water suggestion life frequent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
