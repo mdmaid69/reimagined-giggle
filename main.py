7aaf02501d41c7e5e045ae0db48eb41b4256336a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:34:07 +0000

    closed floor today concern drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
