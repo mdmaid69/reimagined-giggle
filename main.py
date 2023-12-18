85d5988476c5b4baf56f07e57ca0efb50d0036a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:01 +0000

    attract down crash look project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
