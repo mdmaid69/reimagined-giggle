e12cd0c09ff69c9a7dda6688a025d0f9ed91d2bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:38 +0000

    exchange weigh grateful scream store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
