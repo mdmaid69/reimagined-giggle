f75c23c1bc9af8d9b7a53f7b524e4980f632e74f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:22:30 +0000

    office bringforward Vv forward identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_list_from_array(array):
+        return array.tolist()
