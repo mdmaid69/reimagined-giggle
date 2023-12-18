485da7d7e5ab853eb288995b88d63c92d29eee45
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:42 +0000

    workon project subject consult use

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
+def square_number(x):
+        return x**2
