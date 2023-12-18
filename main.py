2e3307197956339f6766b1d6e73394cb6f11783d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:29 +0000

    trust legal even track sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
