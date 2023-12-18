e81ca7f750de960bce68b34a36b5046288ba2ae9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:19:12 +0000

    city escape physical tool snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_working_directory():
+        return os.getcwd()
