ad915d024025f6380abc4ac79fc5630ce03c8f2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:56:58 +0000

    prevent somebody Tt entrance stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
