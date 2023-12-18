4f7e4331d8793930ea4e1b04b433b82a045151a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:41 +0000

    preparation record transparent apply onto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def square_number(x):
+        return x**2
 import time
 def get_current_time():
         return time.ctime()
