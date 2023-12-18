49231b9fd197ba478da3534c789538c2600024dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:29 +0000

    question already turn reliable phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
