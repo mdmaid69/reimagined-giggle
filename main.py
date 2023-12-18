47e41014a015b162f65d3672c664f03972aeb4b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:40 +0000

    trial tune transparent moral inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def log_message(message):
+        logging.info(message)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
