e040921665efd03d17296e8b80ea5e146ac98a9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:00 +0000

    forget organization strength unfortunate breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
