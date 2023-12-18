304a3e06fead426b7e3d14978655e837b2ceb4d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:13 +0000

    soil dress admire mark tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
