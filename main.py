4ce76242094e29d5ba2d28c88c618cb6c0118108
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:17:39 +0000

    union strong battery supply breakdown

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
