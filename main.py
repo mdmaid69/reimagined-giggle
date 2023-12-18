dc1f4296de6a691d784732ff6b3de8539c2084d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:41:16 +0000

    back amount admire name fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 text = "Hello, world!"
-print("Characters:", len(text))
+print("Uppercase:", text.upper())
