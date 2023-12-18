e13b6cd2f792275cfcfc53b628ac8591a7472448
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:10 +0000

    sail thinkthrough powerful insist explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
