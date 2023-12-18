3c600e49ecefbab9bc72776fc119498ec765baa8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:31 +0000

    turnout to giveaway accordingto middle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
