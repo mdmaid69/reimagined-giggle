616005b847d1d0467c549b8bf09eaa120d9d04ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:03 +0000

    exchange image Ss lift nervous

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
