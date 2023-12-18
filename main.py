3cc05fe6ab9c5328ed514515deffd5b189ba8822
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:24 +0000

    danger compete here his future

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import collections
+def create_counter():
+        return collections.Counter()
