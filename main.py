a64d3a0b8c7e84abc99504fe870e6a003acfa986
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:55 +0000

    after disgusting rush necessary regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
