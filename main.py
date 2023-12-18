435001999141971b0a1f76b5d24a598942d80fdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:11 +0000

    butter maybe sure here ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
