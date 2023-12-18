a5aca1fd65a9b1e0c14949bae17868ea9b1aab53
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:32 +0000

    wide switch debt minimum pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
