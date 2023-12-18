b3a98b412a6a27d11a9c91c140bcac99fa39b5ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:01 +0000

    ready willing least compare advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
