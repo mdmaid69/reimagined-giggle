18a410646171ce96a74a486225014352c54a8ffe
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:29 +0000

    certain 179bite increase life kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
