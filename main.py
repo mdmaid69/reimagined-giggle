8743d3976cc83cb532cbdbb5e0eb27659e919e43
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:13:10 +0000

    street dangerous Rr match respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
