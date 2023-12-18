5fd77a2f294989406ecdb3d73238e54aa31d9748
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:44 +0000

    guard eye pile moral control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
