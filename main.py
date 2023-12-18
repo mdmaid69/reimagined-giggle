534eeebba6857549547985ec51cfce121c57561f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:15 +0000

    possibility neat birth decision guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
