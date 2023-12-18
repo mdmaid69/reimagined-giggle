ba220b9842a2b0ffc0546174315e45167ced00ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:06 +0000

    away meal cross burn not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
