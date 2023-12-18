ea8bd5f154ec78da470d52c87b736afd59a9fd55
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:26 +0000

    frame never little takeaway favour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def reverse_array(array):
+        array.reverse()
