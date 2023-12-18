241dfe9a7131921095bedb58cd263e1b00863215
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:24 +0000

    comeup tread tense drive naturally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
