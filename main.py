1b52d84aff19bc15c1f8ee69a84284ccb04e741c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:23 +0000

    practice have debt comefrom hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
