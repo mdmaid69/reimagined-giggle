97480453e94f324731d58f04f874567dc76e3dd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:43 +0000

    guess come give knock 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
