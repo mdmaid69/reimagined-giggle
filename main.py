eb24490486ee78f5324e37c9d212ded28e567019
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:03 +0000

    welcome method release become cook

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def convert_to_octal(n):
+        return oct(n)
+  def reverse_list(lst):
+        return lst[::-1]
