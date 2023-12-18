b00e107bdc867e830c6a02afa65c9c0851f5b344
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:55 +0000

    control fill bridge home rare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
