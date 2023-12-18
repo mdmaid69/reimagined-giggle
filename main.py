2f18e8c8fe7e0540ac656883aa1f79a29bbbffda
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:09 +0000

    education dance mass discovery cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
