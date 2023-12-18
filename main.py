0371b22ab161f7cbb6e04fa35c542c31b0bed3a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:06:15 +0000

    commit Vv confident command connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_queue():
-        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
