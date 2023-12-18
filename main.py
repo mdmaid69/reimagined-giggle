3ef57821960e0af633485155d54f7ecf6dc1209e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:20 +0000

    pass burn hang defeat object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
