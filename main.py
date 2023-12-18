32853ad11018cc8494c0381a1f9b106d91e150d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:33 +0000

    yes talkback movement question profit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
