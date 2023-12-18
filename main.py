9bfae29271c5508acb457eff0710c84ca79f603b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:21 +0000

    tell rough whom lend comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
