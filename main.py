c05bf7373c0cd360badc728bc35b5cba6fcddf74
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:55 +0000

    lucky needle goaway cover less

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
