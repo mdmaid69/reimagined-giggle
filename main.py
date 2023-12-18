95a745bf87c32d8385dd86ee00fbd43e81b2e1aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:14 +0000

    one mix hang divide letter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
