f7a6fa59a62d3eb35f6ed5656f82b75e3ec8fafe
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:44 +0000

    bringforward body from congratulations ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
