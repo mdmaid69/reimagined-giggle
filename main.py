dd9875182f73e7299672a8d673ca513a19042856
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:50 +0000

    use successful pretty fat story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
