3027bd4cebb372002055c11ad7cc8491865c6622
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:11 +0000

    brush left weekend ride cool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
