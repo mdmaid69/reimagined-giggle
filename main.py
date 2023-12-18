a762f3f451d187eb79be09c518623a65699293ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:04:47 +0000

    soon concerned tear challenge avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
