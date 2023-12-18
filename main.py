3303535a21bba5a25e8924973fa8dec337027760
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:11 +0000

    at contribute sit mass compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_density(mass, volume):
+        return mass / volume
