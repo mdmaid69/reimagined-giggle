e5c47a67b3f2e5d594bdd0ba61da5a9f0d6b42d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:23 +0000

    trouble smoke wealth exaggerate pronounce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
