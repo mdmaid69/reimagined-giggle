99a566cf271e24baa9213ed668f24ea51156d3b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:44:58 +0000

    keepaway child strict string private

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
