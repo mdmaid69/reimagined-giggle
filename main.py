8c8b923164ad031fa70f358d9a3086baa4de7f03
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:58 +0000

    Dd big concern only temperature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
