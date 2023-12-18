89047eb147a43cebf91e105264a1ff9aaf91f8bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:40 +0000

    birth have usual secret workupto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
