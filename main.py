4224cb3a341667bfe87f604c603e43be22b1501c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:19 +0000

    dance whoever father translate container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
