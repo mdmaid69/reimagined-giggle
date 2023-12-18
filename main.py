45e4b20eff8eaddaee8a77509a3d184ceec95eeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:47 +0000

    similar Cc bin bored failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
