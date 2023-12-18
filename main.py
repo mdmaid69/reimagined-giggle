65cf1a8f590164f279273eda23940b2fc93b584c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:42:17 +0000

    recover thank mistake light pump

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
