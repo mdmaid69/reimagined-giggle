b4c1f74f8c45d7470c8dd507d95464a5e3b50435
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:54:19 +0000

    satisfactory sell nearly of partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
