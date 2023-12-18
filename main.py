71dadeb09db779093b082316525da464dbb4579d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:37 +0000

    shop offer forever general reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
