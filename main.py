55456206850594f6cb4aecd30958f68552236bdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:33:58 +0000

    tend argument roll instead present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_rectangle(l, w):
+        return l * w
