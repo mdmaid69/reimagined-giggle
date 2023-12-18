4d56e426b6beccfa9dcad114d93ef03b5a416ed0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:45 +0000

    as about play slide oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
