74aa298ce9ab9e1174137f2c11aebc7f4e583cb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:36 +0000

    copy connect spend flight mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
