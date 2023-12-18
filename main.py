9b95c6a24ccf456357b1be4c26956411446d86f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:35 +0000

    match change obvious smell takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
