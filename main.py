deeb5a86fe3af41ef2c1b9088c58d4f6fb0b5175
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:10:07 +0000

    fashion uncomfortable movement company cover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
