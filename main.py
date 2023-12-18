577d161cfb2e4d84404367d1a357a33f235cff36
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:49:56 +0000

    thumb work old aware along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
