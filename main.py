f5ca6e3833bfafc2746327fe48f366f1f3e651d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:40 +0000

    everywhere impatient fault intelligent behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
