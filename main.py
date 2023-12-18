51167e1b73474a8287c0610cb3d8684a24c1c77d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:40 +0000

    announce age minor point kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def sort_list(lst):
+        return sorted(lst)
