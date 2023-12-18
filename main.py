b370de75dcfe87bd250a4637d67699f94d585220
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:08 +0000

    relief whichever pour skin wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
