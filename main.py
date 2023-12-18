3d35cc691a0e79e969b81c1ef56b47f32b22cb98
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:47 +0000

    definite precise frighten mix concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
