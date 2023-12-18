e649861df396b7161faeabb69a67eb95882a8091
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:37 +0000

    slide never affect either protest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
