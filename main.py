f3283548b8a92849f9304cb2c3892392afbdc2c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:21:12 +0000

    thinkover familiar gap blame pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
