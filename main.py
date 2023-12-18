63360ea842a9dbbbd8dc98cb37d84f2078f5aaf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:46:01 +0000

    go disgusting square each match

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import collections
+def create_counter():
+        return collections.Counter()
