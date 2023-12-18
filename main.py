4b7f6f46e5365b9cc1f78802ec8c901fdb1631dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:24:06 +0000

    path matter your her now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def convert_to_octal(n):
+        return oct(n)
