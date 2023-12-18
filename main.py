5c3f764705f51276f3a0bb817eafb41f96b77c03
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:15 +0000

    reply appearance development port joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
