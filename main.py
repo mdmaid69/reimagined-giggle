4313527cbee87b36dd1c84d9c00962b5b6d4599f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:56 +0000

    meaning popular succeed fun blue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
