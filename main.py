cc6fd260b9038e858eb17435c7957fc850838848
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:32:23 +0000

    black brother stupid spot copy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
