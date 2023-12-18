927506f10654807ef79bfe0b5b0ee0590a0629a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:52 +0000

    glad life gas safety wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_today_date():
+        return datetime.date.today()
