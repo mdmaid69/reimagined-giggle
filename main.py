b5927c9eab96dc2eb7e206cceb69db934b5b33a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:30 +0000

    journey event detail ready common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import time
-def get_time_since_epoch():
-        return time.time()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
