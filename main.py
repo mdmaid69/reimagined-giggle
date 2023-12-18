dfff1bbcb0ec3a0573a5f5ffd459130a4c61c08c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:33 +0000

    dinner top rush apologizeapologise 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
