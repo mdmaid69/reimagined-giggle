ac38f062f749d8147819cc9c52e464f5d13c2bc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:29:48 +0000

    team mean range else genuine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
