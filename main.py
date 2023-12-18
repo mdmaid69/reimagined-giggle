a095faf135e2b45264a8df74d8c75c8746c96cd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:07 +0000

    space film later across 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
