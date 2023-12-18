394e18bfcb27e291718ea5e6a5106f836fc44fa4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:17 +0000

    live pronunciation probably noise lean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def remove_duplicates(lst):
+        return list(set(lst))
