f208f1776639e8cc9fb186ce55b84ead6a1c82ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:59:54 +0000

    back beneath today performance no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
