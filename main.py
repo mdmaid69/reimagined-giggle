2a8b7dcccff4f51d373ba15573db6d10fdb38cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:03 +0000

    aim best lock 179bite city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
