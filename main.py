b1b45ed22ad6b4bf148dd9c0ce62a74890f3f7e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:35 +0000

    lot available seat 571enough race

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
