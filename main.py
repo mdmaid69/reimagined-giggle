ef1b1e9a1399e8aeee5b26b080b738c564865cb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:24 +0000

    politics challenge approve face awake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
