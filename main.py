5cbb592ad466038c99d7ce4dd138dbe821fe8651
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:35:46 +0000

    yes mark movement cross excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
