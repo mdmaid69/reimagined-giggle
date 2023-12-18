f52b039ea18584eab2abc4911d5369735a92992f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:41 +0000

    oil yesterday corner cover against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
