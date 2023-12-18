041e6aba3ab7423cfba30554c70f4bfa1f91cc58
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:50 +0000

    refuse funny below once go

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
