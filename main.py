0ee9ae1eff9e8eeef0aa2dc96222e9bc3e914b5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:31:48 +0000

    pull whistle tie plate unless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_characters(sentence):
+        return len(sentence)
+def greet(name):
+        print(f"Hello, {name}!")
