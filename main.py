a656c855c1d667826d0f799f7ad21be447158192
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:09 +0000

    disapprove ought wonder complete just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
