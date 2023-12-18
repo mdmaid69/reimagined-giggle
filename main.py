5fad5dcc546bcd53a5695ca7a7d7596e8495abb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:41 +0000

    run powerful mystery so front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
