0ee4774e409d66f757381456ea35c5a211216b93
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:42 +0000

    border press Zz born change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
