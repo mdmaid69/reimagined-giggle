4d4391bc4b63698aeed1cff9420f5c84a35dd72a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:07 +0000

    down fold keen absolutely on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+for i in range(5):
+        print(i)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
