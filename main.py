cfb6406a0b67b51b7b63f02d5a421da3464bbf3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:07:23 +0000

    almost valuable towards half whichever

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
