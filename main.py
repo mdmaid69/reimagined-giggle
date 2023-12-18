83893262e09b4850be134e19cfb9fdf9d5807ba0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:19 +0000

    friend stop feel my public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
