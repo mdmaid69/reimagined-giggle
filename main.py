84ed1785aebd767fd43c29e7496b4bab760b7844
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:18 +0000

    shadow child brother breakdown ordinary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
