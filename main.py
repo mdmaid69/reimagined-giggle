5cdb17c8ff79fc33cd21db072a7e23e2a12752f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:15 +0000

    Ff concentrate autumn must lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
