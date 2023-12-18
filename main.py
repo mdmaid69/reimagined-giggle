13d4180ad9d229e85488ccc33be413991054f986
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:08 +0000

    loan nowhere stamp which 573entertainment

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
