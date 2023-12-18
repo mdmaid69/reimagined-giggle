992540d8d50b726b525edb04765a54a9fb4b0367
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:09 +0000

    cheque thinkof false communicate ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_current_time():
+        return time.ctime()
