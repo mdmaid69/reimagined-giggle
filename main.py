3a7c0ec49bd53579aee7256bed489d9db8f337a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:06 +0000

    holdup earn when have plate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
