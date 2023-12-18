bdd13e3f059f3cdc24f9172b90e1792dd6686509
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:05 +0000

    grow like ilovedicks care reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
