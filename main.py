0bfbeea3f34e91f2dc849809cae2ad15ca0d3bea
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:56 +0000

    really true discovery song him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_density(mass, volume):
+        return mass / volume
