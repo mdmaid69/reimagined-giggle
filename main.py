50ddf26052b99e826d0d36e1e8f9788cd3e9880d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:54:41 +0000

    own square row immediately black

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import platform
+def get_os_info():
+        return platform.uname()
