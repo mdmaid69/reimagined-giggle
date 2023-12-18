d2fd26284a19d4cf5bc8cb805650c181ed1349d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:27 +0000

    wide danger issue tip day

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
