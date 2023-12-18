5acb402d1d418d734fbfa8e4e252032ac9caa3c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:34:27 +0000

    together general quantity favour well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
