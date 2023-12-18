abf74ca0c301ee066c27ca0d2772b9eea5ab3232
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:53 +0000

    love ago form noone ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
