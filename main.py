7522c77c4b23ab49bc65fea011d90ef1b9b6a046
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:38 +0000

    shut anxious flat free strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def convert_to_binary(n):
+        return bin(n)
