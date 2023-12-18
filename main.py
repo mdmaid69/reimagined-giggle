ee9d815d6a6c85b371d9c1cbc6a385a9bca149ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:33 +0000

    Dd 569enjoy same unfair sky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_exponential(x):
+        return math.exp(x)
