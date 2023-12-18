65b17ae154adbbe6440c46494ccb5c08f57bd0a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:01 +0000

    target job choice order slightly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def print_python_version():
-        return sys.version
+def get_string_from_array(array):
+        return array.tobytes()
