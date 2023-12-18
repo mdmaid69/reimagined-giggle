223040cab2054416ecf4777d300e0a6aa455dfac
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:51 +0000

    whistle over keen scared justice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+for i in range(5):
+        print(i)
