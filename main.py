857e28fd4dd1f4cb6b26a1c647639cc5017d5fdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:41:45 +0000

    conversation damp smoke travel suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
