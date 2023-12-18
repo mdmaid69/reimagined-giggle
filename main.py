aca41c1331354ee2412eedadaadfe3f20e3a19ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:46 +0000

    damp run fight steer close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
