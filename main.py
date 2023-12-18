57aed9e39892051f1e8ddcf7fd213c115d5dc598
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:23 +0000

    when early silent shrink here

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_error_function(x):
+        return math.erf(x)
