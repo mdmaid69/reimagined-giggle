906e8cb1700f6889346a73689ec01c450cb567fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:06:19 +0000

    weigh circle hide since top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
