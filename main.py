401632f77c2ec8ebfea6b8e7c367623967888527
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:26 +0000

    lead wrap distance dangerous spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
