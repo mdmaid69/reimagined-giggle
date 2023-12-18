b04e5a6b25d193d32e2e4afe91770d110a19de9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:01 +0000

    office top him pinch OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
