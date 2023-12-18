e6198850af3ffeda9975d3cbc00ed210d65226dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:57 +0000

    mass flight congratulations well left

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sys
-print(sys.version)
-print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
