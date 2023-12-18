9ef9bdff4fcfb649efd3e2de0f39be9afd7679a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:57 +0000

    collect spin breakup rank sick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
