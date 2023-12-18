a8640bf8104a31fec71ceb2e7ec8e82dafbec1d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:57:43 +0000

    approval before their fetch ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
