97979035a98833e4a0efb4863c00e195f03295f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:29 +0000

    now break minority considering expensive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
