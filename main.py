ad7dbd647836d228bece13fbbb1797fe0bd4c5fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:04 +0000

    blow injury above not oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
