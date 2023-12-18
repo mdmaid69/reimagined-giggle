5bd6cc3ef492d7f85e7f9ae4cceef9937a7cde33
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:54 +0000

    tip come almost whatever recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
