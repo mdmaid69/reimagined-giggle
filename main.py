0c4e63a9bcf54278e720534b4b5ffdd7ba05225b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:30 +0000

    breakin kill strict material clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
