5523f292866b85d268a2bd2709fef3addc2b505d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:36:55 +0000

    key at special brief arm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_pressure(force, area):
+        return force / area
