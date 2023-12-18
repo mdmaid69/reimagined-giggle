b8a0292b2e36a7a4d09a55e5404345ba58d1b371
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:30 +0000

    sign mend noone rely bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
