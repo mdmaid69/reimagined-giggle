1e05948aaa595e2210bc54c5f6f5c4163d72fb56
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:31 +0000

    small state slowly mysterious wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.time()
