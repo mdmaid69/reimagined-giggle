ab70c327ebf00df818ff26605ec95c792d269129
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:02:28 +0000

    roof average Hh because snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
