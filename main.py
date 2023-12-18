779acad772f00fac6cd68fb8fccb615aa29351d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:43 +0000

    problem breakthrough proud whenever defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_work(force, distance):
-        return force * distance
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
