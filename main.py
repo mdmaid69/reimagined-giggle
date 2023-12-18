39e0bbab15b6ccaab3005cb0931ee69c966d1ca0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:46:04 +0000

    nothing such limit list inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+print(math.pi)
