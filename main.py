4d780e649fc78d98f7ffb1b6f5f6ecfd49cc9caa
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:35 +0000

    risk though pay offer deliver

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+print("Hello, world!")
