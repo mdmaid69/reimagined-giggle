8e4b14b0ad01e9d013fc365ff85fc4ca465a870d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:24 +0000

    type before shoot motion workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
