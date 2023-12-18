646ead5af0b0c72b47a5edc8e36715980243aaa6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:11 +0000

    story needle form disagree whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
