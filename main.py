b286927c1187b547e3f66fb3543c27181e19b7a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:58 +0000

    effort reply 571enough touch properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
