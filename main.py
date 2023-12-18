ea4b54c7e9a28fec69bed57b5fe5eb73a231ff50
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:48 +0000

    like straight confusing doctor manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
