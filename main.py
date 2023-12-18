bce47cd4b79b3f1bca135c7aad31f55209721a75
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:05 +0000

    love on grow present face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def square_number(x):
+        return x**2
