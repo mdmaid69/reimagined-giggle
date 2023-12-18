893ee6c558a832499fe66cf9354ab924b5f131cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:56 +0000

    cut real section copy steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def convert_to_octal(n):
+        return oct(n)
