eae8d44a95f9949f82352eb8ea8aa7621a23ddba
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:28:28 +0000

    anywhere wish ours along need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
