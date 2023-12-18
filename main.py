83bc30c6c27c730a17bb62b9e0f8fa94bf664827
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:11:23 +0000

    hear nonsense disapprove pipe join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
