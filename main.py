71dad84a6acc2d47f99e7835484ef28c4cba6778
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:50 +0000

    Ll bad flow middle impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
