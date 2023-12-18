5288b2b7807641bc30f0998f55c3ff0c8bd94f58
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:14:38 +0000

    art roll direct watch look

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
