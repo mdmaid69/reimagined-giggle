e76a08909b0122a7b35d7fe325b783974ddc74a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:52 +0000

    rate child game transport range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
