267c413b877b5d193879787ef59a1a93339866a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:45 +0000

    sometimes geton main rely mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
