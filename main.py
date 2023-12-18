e1fce916309a8912c62027ff9f33a0bbdfa1b96c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:36:15 +0000

    comefrom thankyou here weigh extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
