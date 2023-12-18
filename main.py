536afeb0c018af1633d86048d8d8abe6dd81fe2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:30 +0000

    have discussion fierce excited least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
