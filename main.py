eb3b2ced0572658625303e57ae623c4576dc271b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:24:27 +0000

    production note sensitive consist experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
