283396c34ea927d91559ce0939270d301d5eac80
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:22 +0000

    window patience holdon substance letter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
