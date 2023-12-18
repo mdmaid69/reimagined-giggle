5b6eb69afc1d1fb3dafb73e5a5100fd9276adaab
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:17 +0000

    case hungry practical dark release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
