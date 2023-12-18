21edd483ecfd507637a7f085316de774d4669fd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:22:37 +0000

    lean top flat nice return

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
