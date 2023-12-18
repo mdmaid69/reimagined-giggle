499a72b5f7558a84a68ba42ed72ff8a03bee1715
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:17 +0000

    nothing Pp meaning do statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
