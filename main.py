9bd5ab5fd923a1b9d9c4f0448dc7198354453f66
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:22:16 +0000

    anything sort convince worry worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
