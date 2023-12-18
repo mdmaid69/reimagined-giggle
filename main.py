f8031f93f9cd07a78f9f5bb0b8f90bfe06628e34
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:11 +0000

    burst tragedy autumn trade spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
