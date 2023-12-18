1175acc560e7abfb8fcf6d47cc98dba6a5933cbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:49 +0000

    number not develop correct age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
