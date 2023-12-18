682b82515e79029d9fc0cd55f9b3ad9da2be6438
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:07 +0000

    skill juice emergency stretch set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def square_number(x):
+        return x**2
