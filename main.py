6e0dbfc064ddb5ec3321095aec8c22b08a9bb6f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:33 +0000

    prevent design vanish supply legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
