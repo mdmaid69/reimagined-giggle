16064de23c58160a097b69edf9e326be162ba448
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:31:48 +0000

    include swimming unfortunate boat recently

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
