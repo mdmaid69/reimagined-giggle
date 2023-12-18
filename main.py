a4401c1dbdc6c4ae4bf9d9c7b6ad143637c7376a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:24 +0000

    future school safety port slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
