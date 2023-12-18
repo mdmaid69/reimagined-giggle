237b86a957293f88e1f9d94a544328b2c6f5153f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:59 +0000

    turnon one belong prepare damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
