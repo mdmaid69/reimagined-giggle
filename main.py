dd2f4c1822075bd1827a44dd37270b5cb2ad76b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:38 +0000

    different unusual impossible dance example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
