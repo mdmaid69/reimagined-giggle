e02994b7ef3334e9150f383b18158cfeb89874a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:31 +0000

    discovery sink geton influence wild

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
