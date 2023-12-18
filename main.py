b527ddb8bf75fea7449594a45db4e4032a0e5993
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:42 +0000

    future fight bottom include plastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
