9c79a694e536c10c7beb9f8bdb5fa8ac8d8ff4cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:56 +0000

    weak belong lonely blame price

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
