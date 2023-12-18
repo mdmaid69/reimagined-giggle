fe0421226519248ec1f1bcce8309b6c510e27158
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:37 +0000

    reaction split deliver mess crash

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import datetime
-print(datetime.datetime.now())
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
