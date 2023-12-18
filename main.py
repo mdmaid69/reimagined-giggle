c7ef2c67cc5cbc52c2cb8a0eb97737e89ff85a8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:35 +0000

    themselves bright badly tired receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
