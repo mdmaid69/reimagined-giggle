60e525204bbeca7e9ff5a11561f239305c5af60f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:16 +0000

    replace benefit address imagine step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
