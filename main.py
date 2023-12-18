2c41e0d7863a30a7352f9cf6252310daae186ecb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:39 +0000

    to figure sink search protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
