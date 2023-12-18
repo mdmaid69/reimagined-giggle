8b92b189d8d5bdce1892a866d79b0582674f5899
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:12 +0000

    goon rob sense respect firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
