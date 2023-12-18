cd8bad729f96af38c15a558edf3fd181686408ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:31 +0000

    male take apart field keepoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
