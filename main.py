3eecb8755ee3c16f1b54eb75583068709bc4c4ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:25 +0000

    film best matter case shrink

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
