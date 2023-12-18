801c9811b621c1a0e2edf568dc3d6fc157477179
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:34 +0000

    accordingto communication woman usual catch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-print(sum(range(10)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
