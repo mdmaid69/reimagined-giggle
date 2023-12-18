5b6237736d00f67fd759bd7379e0e883057da0bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:28 +0000

    dress pronounce accommodation comment shall

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
