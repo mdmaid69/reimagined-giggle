db6fa35762bdde1976c55f381e9f563e85242a10
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:24 +0000

    rule challenge majority Ii getoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
