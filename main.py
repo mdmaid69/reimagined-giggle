b3335d3dcc14155595ba1f56c01ef373535d85b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:55 +0000

    opponent practise central regional they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
