fa1627b0782ede9b4eb03e0ee1c998ec0eafc28a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:57 +0000

    keen part pick cut brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
