b6f87833cb39bc03573532ef4167344c87f62477
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:54 +0000

    set lock building include reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
