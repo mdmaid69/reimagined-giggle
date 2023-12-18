227e3cba24e40f46772c2f5639584efd35f58a4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:08 +0000

    yes step similar shoulder warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
