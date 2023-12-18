042742370e76387156f79c38af52972a81c9271f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:26 +0000

    embarrassed shut exploration godown proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
