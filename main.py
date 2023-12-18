3a5dca15f7641116acab131c875c77fe8a32de5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:00 +0000

    base cum think week scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
