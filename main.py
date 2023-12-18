c778e41a6768d84348abdcdbd65cf1b7369e6fa0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:50 +0000

    post Hh pass weekend embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def sort_list(lst):
+        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
