335f9ce9ecbdbb4be21558cb613829775851e51d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:54 +0000

    issue build cap wash breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
