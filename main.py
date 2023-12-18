28759186d3d8238d6b57e60dee9c0d96ab11f52c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:30:56 +0000

    centre runinto quick accept news

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
