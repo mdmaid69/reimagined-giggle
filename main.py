4045cee54e2c2fd427d6f22002652e462297a26b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:59 +0000

    side live blame keepup action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
