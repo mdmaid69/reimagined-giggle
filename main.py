1942395fce0dfd9f1167bded7ed4abd1e3ec8941
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:57:50 +0000

    talk sister cold well adjective

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
