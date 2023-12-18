4cf45f4a348b5d5063ca852409611654b093f9da
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:07 +0000

    later left die health laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
