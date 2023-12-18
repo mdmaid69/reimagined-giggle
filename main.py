c6b861220908a804076c2a4134b40c5034bd960f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:31 +0000

    blame success us embarrassed coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
