2d20f5b6031639a48c430d39264427da607cfa52
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:44 +0000

    plate medium stop underneath work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
