1ae69d033523aac3a79d513f22c37c913ba8d4e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:22 +0000

    contrast guilty lesson that any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def print_python_version():
+        return sys.version
