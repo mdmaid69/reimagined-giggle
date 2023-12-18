e4ce5544910988200fbf89b5302505378b6b48d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:44 +0000

    relative sweep comein surprise Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sys
+def exit_program():
+        sys.exit()
