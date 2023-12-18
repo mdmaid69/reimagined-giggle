49b6bcc167cb148a5c779889f354c718e2ac732a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:06:31 +0000

    detail abroad whole keepoff moral

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
