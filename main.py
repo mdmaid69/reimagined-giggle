a33baf3872bb06569982478e0a50a6de781eb714
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:35:15 +0000

    after season wish by anything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
