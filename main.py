fc619a41f9f333d9a1b56b2ac8ddf3e1ff1f52fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:24 +0000

    desperate rather firm calm rank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import platform
-def get_os_info():
-        return platform.uname()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
