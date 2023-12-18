91111b47e49936106cd5af698a647a7d34f1e538
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:13 +0000

    later I about successful well

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
