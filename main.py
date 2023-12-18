85eea315fd9524feff50071523f11c7fd5bd5198
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:20 +0000

    actually ready part permission chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
