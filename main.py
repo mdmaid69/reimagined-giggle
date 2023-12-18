b675b130eb1c6ac34cb6d32369b56c9b79439c93
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:46 +0000

    scrap party work advice hide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
