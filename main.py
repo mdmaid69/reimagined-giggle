7e89d085a57ef9519df5f5fb0ca9668184f24152
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:42 +0000

    forget stiff excellent police different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import array
-def append_to_array(array, item):
-        array.append(item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def extend_array(array, iterable):
+        array.extend(iterable)
