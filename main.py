a4a02a8621d49ff1afc6edbf38e30710bde299bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:06 +0000

    plastic possibility down comment themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
