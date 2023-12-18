e7b7452fca1a00ccdd2e74f7886519218d187a6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:44:32 +0000

    exist independent various why wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
