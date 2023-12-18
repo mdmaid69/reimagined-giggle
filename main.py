b477d00a8a14bf592e10e8f1e148da36f6939cec
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:32 +0000

    attempt involve without side further

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
