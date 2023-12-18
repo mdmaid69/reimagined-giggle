553724f36891f200bbc5c2c40d0711e62f17bf26
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:49 +0000

    impatient prefer bring mistake leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
