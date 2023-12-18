12a4311374f1e2e3a67023ab5f8be982ef80204a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:20 +0000

    sister gooff coffee winter window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
