8c5fac0ddbbc1c4c1def6f95b5865a7947c0ac84
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:51 +0000

    mean grateful object reaction emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
