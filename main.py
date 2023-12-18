7f5ac5507fc739a881bd273de9b3d1f81ae29e28
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:04:06 +0000

    because out means ashamed splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
