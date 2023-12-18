2c54067c0b735581f95069e99d51f591648e73ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:04 +0000

    pretend scheme whistle worth choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
