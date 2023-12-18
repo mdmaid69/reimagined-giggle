780b47ecffc04213728d6ede499c7a73e3bdc069
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:09 +0000

    very cross face hard yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
