705b0d4155f08c701c4ad4362311c8ac5f3947ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:21 +0000

    aim dance object future father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
