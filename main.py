ef89242b6776e9c2a422fe7b77b1fe4c81d9836c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:52 +0000

    failure continue message unpleasant care

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
