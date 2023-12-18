d6f3e3d88687809ed43af3879547bb90fa5d745c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:22 +0000

    foreign search Zz Jj mixture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
