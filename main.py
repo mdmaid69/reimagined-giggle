9405f2017674ca6f7e978ff3f20c3a377673414a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:19 +0000

    style whenever able anywhere recent

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
