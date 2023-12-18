c90583e0c9a8d7e70730d8d3038e2cd5b6a7832e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:54 +0000

    part decide handle star hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
