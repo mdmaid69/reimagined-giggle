8ae13c0d57423c3aa92565f2dad89d01c7ce1039
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:40 +0000

    usual game born fortunate never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
