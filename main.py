9e93264d65bbaf500fa23579c1ba26e7ab7a2787
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:59:09 +0000

    shallow speak cost Yy split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
