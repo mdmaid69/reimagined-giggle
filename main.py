356f55b8aaa09c300c0ea0831ac9252291f688ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:28 +0000

    shut agreement discover safety fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
