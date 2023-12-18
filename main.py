2a446a33b579e24da3de0be3e4171df14a80924a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:42 +0000

    introduction shoulder consider holdon total

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
