7ba1cf7cb37acd4df6e55c081697665d22df25d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:38 +0000

    sight be hard slice office

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
