236cd9e8d85745dc925716ba4ce68977b5698f60
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:17 +0000

    whole worried friendly steam wind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
