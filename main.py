63ea9e0c81b3d549eee5067b0719487c52dfd990
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:01 +0000

    issue just you instrument murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
