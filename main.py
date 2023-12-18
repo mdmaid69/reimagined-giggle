eaf008cf445ab54a6879073f00458fe82aca337d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:44 +0000

    suffer wish glass purpose birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
