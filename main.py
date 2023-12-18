e21392abff83a8f64367c17ac0d6da1823d8efba
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:25 +0000

    breath heart herself not beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
