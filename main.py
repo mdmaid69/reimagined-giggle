98fb77bd4a6070cad98bd9380140feba1f2776eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:37 +0000

    hang be invite walk level

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
