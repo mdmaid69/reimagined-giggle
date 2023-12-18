45f3dd66169c1fd6b7508442505a78521e64c612
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:36 +0000

    hold giveback empty duty include

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(numbers):
+        return max(numbers)
