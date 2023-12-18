8ecb1f040f80a2ea72bbc9eb754ed65762a7ec6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:20 +0000

    house mess protest accidentally switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
