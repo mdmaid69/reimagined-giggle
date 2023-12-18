fcd3276128112adea2905940d70213498d89c8d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:06 +0000

    anyone effect image attempt scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_item(array, i):
+        return array[i]
