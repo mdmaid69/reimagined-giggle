9e455267295a835e23fddc75a5041be427522038
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:49 +0000

    service fasten turndown close method

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+print(math.pi)
