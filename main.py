0fc2dbef2c8fac5d6ce4c310fb3acb4d8b215b15
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:34 +0000

    open purpose neither improvement polite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
