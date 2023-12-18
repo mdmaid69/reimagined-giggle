ddb14e3a54123028b9ba545da650be7ee21458b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:12 +0000

    satisfied century bag scare clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
