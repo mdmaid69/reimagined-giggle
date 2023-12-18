2f2e87f3a4883371e6941d41fcdc29724fc4c23c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:05 +0000

    hit organization practical anxious hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
