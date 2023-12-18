4c4c524d3e9e6c74d8e3229584b008a49efa5695
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:58 +0000

    roll middle concentration luck bringforward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+from collections import Counter
+print(Counter("hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
