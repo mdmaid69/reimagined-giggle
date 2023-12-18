92bea35a1df59a7f076418ff9565283a74faabb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:09 +0000

    poor hurt whisper something extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
