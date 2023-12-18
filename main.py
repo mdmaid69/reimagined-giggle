6145fc9c0b81024ac5742e655d1b1446fba19b70
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:19 +0000

    confuse lead trade worse afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_set(array):
+        return set(array)
