95a1df457743c20ab9203716a5c7c32b4542bf47
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:16 +0000

    safe substance like disappointed earth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
