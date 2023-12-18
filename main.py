1027928694d4e3719ea3af4e4fbbdf9f0fa37efd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:09 +0000

    necessary Oo small artificial Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def sort_list(lst):
-        return sorted(lst)
+  def cube_number(x):
+        return x**3
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
