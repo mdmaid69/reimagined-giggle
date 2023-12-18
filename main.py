6d4050aa8521f42f4c62a8f66311ea207d4d4f75
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:34 +0000

    view giveback makeup catch plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
