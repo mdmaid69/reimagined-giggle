1193e00826623c5efd2910745f784cbdcbb417bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:17 +0000

    object equipment drink dress talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
