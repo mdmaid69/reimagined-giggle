3d253198f2dd1dd6aaea61c4974e89c6c03ef7b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:46 +0000

    might worth habit street 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import numpy as np
-print(np.array([1, 2, 3]))
+def greet(name):
+        print(f"Hello, {name}!")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
