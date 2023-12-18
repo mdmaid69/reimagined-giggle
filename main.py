0439a31c739bf9cd34d5e52067b05c6ba7b210d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:30 +0000

    for smooth shoot limit right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
