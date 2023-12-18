430b38446e32a4e8289737b51e73f16352ad1a0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:34 +0000

    pleased fierce furniture yours recent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
