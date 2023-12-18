e9b43c90a40c35cf73fac910e998159b44fb4579
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:58:08 +0000

    question boring with far 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
