7d859bb20676cdab2a08f89d4fd8f2fca2269a7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:40 +0000

    suckme his drawing breakfast unit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
