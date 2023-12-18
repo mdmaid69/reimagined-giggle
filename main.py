d4bfcb2655c29043b9cbb33862df9fc20f532cd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:20 +0000

    guess record crazy shine emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
