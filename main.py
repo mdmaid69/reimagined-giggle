ad45277a0bd1d5c48240570b462cf80d8fb09eeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:37 +0000

    shoot pair poor deep also

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
