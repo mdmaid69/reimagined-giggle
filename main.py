f0b41c781d5c753dbee0fad5d09bbea8fe192ae2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:52 +0000

    satisfactory remain but name last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
