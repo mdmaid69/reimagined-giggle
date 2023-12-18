05e6cada5882ff681400c6c147f6a8b25180430d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:36 +0000

    disappointed dry best cook mad

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
