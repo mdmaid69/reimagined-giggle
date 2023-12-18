4937acc1960861a8e799edb7fdac3913354aea91
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:52 +0000

    Pp mother after love turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
