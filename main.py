24b25a58435b549803eed62574f74bf35ec790ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:45 +0000

    your keepoff satisfactory culture change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
