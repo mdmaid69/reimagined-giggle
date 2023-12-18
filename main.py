3a7aa4c7eb14b1c44a52b063cc8116b780b48b60
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:44 +0000

    breakthrough any takeout maybe correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
