84adac5a08a37e6c683227c0f20fb961521d5a33
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:15 +0000

    disturbing than anybody scrap private

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
