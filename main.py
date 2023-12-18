29aad781f6a6a031f1372a819f914ca079b6bcb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:28 +0000

    stone example break pronunciation actually

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
