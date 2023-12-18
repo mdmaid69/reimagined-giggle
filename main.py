0e163d4a4dc1fd6890df9bcb806e4eca19f3d1f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:59 +0000

    wish entrance complete relax past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
