17f89b7241b852b9cdad0694af87fae862c797d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:26 +0000

    check hope water feel class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def reverse_array(array):
-        array.reverse()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_itemsize(array):
+        return array.itemsize
