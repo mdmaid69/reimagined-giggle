ac9d96a8b659993f13fb4715bdb686409b9d15a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:02:52 +0000

    second difficult cheap play downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
