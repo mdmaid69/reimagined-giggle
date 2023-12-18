f9cc853822f425daf015dcbc5a0ec401398e0247
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:53 +0000

    swim sometimes bill hate reserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
