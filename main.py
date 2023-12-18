9c959f8bb9d3761774c2e617218642dff6bae3c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:58:41 +0000

    side develop relax include story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
