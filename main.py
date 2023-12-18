5cd5febb107a5e4caca159e6fe62be82ff1f8108
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:59 +0000

    connection serve best vote mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
