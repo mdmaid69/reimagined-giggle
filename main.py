7508ac46abd9db94cff6edd1def9195c9816646f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:25 +0000

    boring soak any well continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
