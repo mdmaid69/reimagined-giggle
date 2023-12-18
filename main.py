bb637f6e5dc510c93c82b2065459aae15bf026d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:18 +0000

    rare within somebody choose size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
