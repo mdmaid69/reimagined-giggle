387ec7816a0f7730e56094922832cbff799c15ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:49 +0000

    town between stone complaint consider

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
