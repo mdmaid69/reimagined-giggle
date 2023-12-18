f4efb707ae93876adce6ce4b0ba32789b92f25e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:00 +0000

    emphasize basic top brief OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
