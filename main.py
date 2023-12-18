4f7567f323aaa1d8093cf950f1fa6b2f94cfd34e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:13 +0000

    mess reliable fit drive question

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
