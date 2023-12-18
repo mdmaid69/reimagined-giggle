ebff2ec883d80991b7b22c21c0b4f66e8c143068
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:28 +0000

    section full face tool sense

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
