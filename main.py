deb94ec33a83553d0083ca95f7cd0bdc0c429b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:13 +0000

    general slight copy wait beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
