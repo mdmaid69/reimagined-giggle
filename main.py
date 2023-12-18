b031eb9b8fdc2e8a946fa79febc0e8ddd3989ef5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:04 +0000

    future reduction honest duty obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
