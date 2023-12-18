c88126d018df1f58ca9c062afa6b8adc952d99a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:56:04 +0000

    wipe trip door school tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
