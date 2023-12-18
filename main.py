6f7b1e3e461abc4453f0767cd8367de963ae4394
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:42 +0000

    period familiar middle disappointed future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
