321d5b32f26b5643793b6bab4caa180f89648d12
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:44 +0000

    himself fork lot government to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
