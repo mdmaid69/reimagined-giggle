91f1cb4b26342ef34dee27c0bf88169d62a9468b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:10:15 +0000

    fetch wish consult coffee interview

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
