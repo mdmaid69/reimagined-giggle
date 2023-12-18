0a173d4ffb728c497d41a0357effb99628019285
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:10 +0000

    breakthrough really failure Jj certainly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
