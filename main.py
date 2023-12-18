22d4e5faf00c3c483ef491874079c11d4da8b7f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:13 +0000

    high deserve about neither protest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
