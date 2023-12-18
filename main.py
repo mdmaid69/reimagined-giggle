e0701909932f3aa6917008f1740452fb440bd3ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:25 +0000

    base wide help bin thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import shutil
 def delete_directory(path):
         shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
