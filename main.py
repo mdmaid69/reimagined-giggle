8e86ba9ad9640989ee14bb1580f68ab9e06ae458
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:30 +0000

    further 179bite finger persuade smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
