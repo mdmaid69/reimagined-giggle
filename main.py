19e0af9e63f5523bb71da79efffd15fa1d5faebc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:17 +0000

    its together throughout involve move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
