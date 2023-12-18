c76e057c66826dc6b3f56cc9970c6795dee18a2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:30 +0000

    listenfor well mine so recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
