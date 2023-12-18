b42f81095e6c9a45c400b3164abaf829e4ab8ae0
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:15 +0000

    heat settle instruction soft unkind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
