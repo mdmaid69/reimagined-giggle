96e1fffb2908eb2489d4f92b0a9146cc6c797975
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:46 +0000

    trip hold term mark opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
