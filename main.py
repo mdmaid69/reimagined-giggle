3516b32743778562024f63b4d6ddf61ca9379ed1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:04 +0000

    shape valuable organize dream difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
