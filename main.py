dcd063c87f3caedc869d3669b2629bb2f40d1ebf
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:06 +0000

    like neither pick interrupt vote

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_item(array, i):
+        return array[i]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
