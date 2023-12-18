6a437f1ba05ae10b717fdd9c28a730311217997b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:34 +0000

    lift petrol below add distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
