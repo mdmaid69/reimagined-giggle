44da3a8f5799ebd4905f4628d427a2d8260450a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:43 +0000

    straight add start experiment develop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+print([x**2 for x in range(10)])
