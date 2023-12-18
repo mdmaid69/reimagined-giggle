db73889bfb58e6ec9b2eb39b267c204fef3b938b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:54:16 +0000

    better unfair decide sail this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
