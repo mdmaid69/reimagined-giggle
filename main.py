65f3930b3e5be9b51ccaec73c3900d37b4092ddc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:04:11 +0000

    professional flat score reject now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
