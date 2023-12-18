2c014fdd789a302abdc1023002f06c37b0280039
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:14 +0000

    lucky listenfor geton bank spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
