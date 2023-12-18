1d13dd7e5123695f4a1c41aa8ad3f70e11f91443
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:46 +0000

    comeround carry storm exam split

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
