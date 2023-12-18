6cc65813467ff31a729199631f9810ef73766b0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:40:44 +0000

    noone probably pay send admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def multiply_numbers(x, y):
+        return x * y
