f8354c20bd3553ba60db30ccbd167f89515b9051
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:27:47 +0000

    add tragedy letter deep tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
