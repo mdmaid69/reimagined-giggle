5b5a76e30a06653afaab23477ab54979b5327bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:14 +0000

    kind careless letter often Zz

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
