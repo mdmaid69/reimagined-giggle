6de5b64b1f2f79ffa4e64edfc18ee3e5a5c6e2a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:31 +0000

    punish risk passenger pin capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
