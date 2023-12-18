31870dcb80208607d9480f51be2daa7f4a4a24c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:23 +0000

    check pinch person ordinary immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
