c6efcc22e46bf17b7e20a97329ca5367b2f09d7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:00 +0000

    beside light fashion boil duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
