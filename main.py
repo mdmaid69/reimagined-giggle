c94b0b4fc54f68a44d9034e83a8a26ddef205c26
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:07 +0000

    children grow trap whole official

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
