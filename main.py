b964294f5d84574071c053cff4511231d5096df4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:50:41 +0000

    raw valuable part surprised no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
