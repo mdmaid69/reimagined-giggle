1540bb9c4d670e4953ffcb143fe29f8491d3fc25
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:17 +0000

    well beneath mass movement quiet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-print(sum(range(10)))
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
