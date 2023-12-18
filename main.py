5812ee01b501bd147093adfe0b416dc1b9838241
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:49 +0000

    realize answer such carry gap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_list():
+        return collections.UserList()
