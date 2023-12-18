820bf32e26c9d5e53915941eb02a0b51143e51e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:37 +0000

    fire ring advice actual sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
