fa14bd0579cc3f23e8701f5def7769f9dc4731ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:56 +0000

    remark sink unless fight while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
 def calculate_hyperbolic_arc_sine(x):
         return math.asinh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
