2ffe66fd4e99fcd3925a76bdb480893775568211
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:43 +0000

    backwards force temperature notice accommodation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
