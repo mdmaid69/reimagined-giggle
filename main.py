d176c921d31982db5e8661d2891991fd323452cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:48 +0000

    thanks lead cause part principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
