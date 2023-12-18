a2113b6e95338649229ede6ac9d594a962d35a16
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:42 +0000

    address steer towards likely until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
