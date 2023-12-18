44f6bc24fce594092959833eee01b96885739730
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:11:03 +0000

    chase 568energy waste exit breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
