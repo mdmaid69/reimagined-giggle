476ecb5b047c4496b0c853e879ae382e18d07238
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:09:00 +0000

    Vv gas breakin pump anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
