f514cbc6dc5d19321b8eb977ac1215008bcd6991
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:15 +0000

    past window trap common provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
