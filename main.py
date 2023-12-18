fbeef5d20fa0fdb8c15101d44b1654419a876c80
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:27 +0000

    concentration suck pay light anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
