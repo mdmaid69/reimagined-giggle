6975a43d8df2662dcd679501a4140a934c8f919a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:06:59 +0000

    under famous brush for tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
