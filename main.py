186ca1cc74c029eb6e95bc89eaf51955d0d36377
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:35 +0000

    somehow trust here produce term

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
