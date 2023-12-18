c2c14175f04203b58379b4748032d81f9eeedbb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:55 +0000

    benefit supply upwards estimate stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
