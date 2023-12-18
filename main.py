3c7ece1dd442214cb0c0a068096e5cb67aea723f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:30 +0000

    Rr herself off dry anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
