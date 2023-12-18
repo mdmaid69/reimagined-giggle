40015715d90c5c70d21682f27a32f6e7741de751
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:45:41 +0000

    mean tip hang ask breakdown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
