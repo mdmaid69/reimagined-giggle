49038744d37f5d422f11357d1df976fb05ba4770
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:26:16 +0000

    weather bridge some flavour size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
