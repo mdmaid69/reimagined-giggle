97e53c88a4514c3e79712f4273b58c6c7a0463a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:07 +0000

    repair history punish race discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import json
+print(json.dumps({"name": "John", "age": 30}))
