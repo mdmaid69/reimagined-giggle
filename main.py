d645cdd7ff300f4a2f09bb24ebc677ca38a4e249
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:00 +0000

    body concentration pot pain whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
