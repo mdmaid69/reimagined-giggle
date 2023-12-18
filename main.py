f83576a01e402afb7958425bd3f2c7a14f609f70
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:05 +0000

    pipe dry scare gift disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
