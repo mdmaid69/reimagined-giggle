5ec391c43789c5f2723a63b6137e26b9f5e07ef1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:09 +0000

    dull read breakthrough television unkind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def sort_numbers(numbers):
+        return sorted(numbers)
