16455b5bc507876f5b1b77f27e0968f55bfea82d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:00:23 +0000

    hurt swallow love picture wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
