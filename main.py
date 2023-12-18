8b43d9052fa37dca504a30a768a1efdab57d048e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:54 +0000

    spray favourite tough punish along

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
