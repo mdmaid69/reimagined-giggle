95e9f4cadb25032ebec6e96a0ed9063631ab456b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:53 +0000

    school shout shout minimum people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-for i in range(5):
-        print(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
