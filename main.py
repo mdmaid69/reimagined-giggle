6b4363d2fd42d6a96306d4a5c689b95b9d8817e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:33:37 +0000

    Rr only people power speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sys
+  def get_python_version():
+        return sys.version
