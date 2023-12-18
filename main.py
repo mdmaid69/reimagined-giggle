8d548975ea20973c4b9dd32ceb7f44471108fb21
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:33:27 +0000

    evening ready accommodation whisper debt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
