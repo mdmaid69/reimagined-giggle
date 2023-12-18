55c75a1d2caca3e2cfffd00b972ba0dacc62941e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:30 +0000

    much either difference development ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
