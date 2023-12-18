d6c9d8a613fc997dc03d3ac18442062f65fff532
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:39 +0000

    mention thinkover they getoff forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
