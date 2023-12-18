491bf4832b079f6964b5dacf0653aa716fc3d538
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:21 +0000

    private reason level say medium

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
