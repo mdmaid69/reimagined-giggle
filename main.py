c996187283a9499e7d243ef56190291f57d74272
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:00 +0000

    what branch early hit tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
