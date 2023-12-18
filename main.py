cbd4b3565c5ff98eecc05b53e0d5e4520285da3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:54 +0000

    Tt deliberately pump deep care

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
