b2f7a8d8659e6ed7a8bf9d6c6462578d54ff7673
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:47 +0000

    cry check comeover along breakin

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
