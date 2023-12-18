66a57cc964d4a58c2862d503101d147edff627b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:13 +0000

    interrupt juice rough pool regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
