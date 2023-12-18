85d2e4fa86f09ae8f45dd84998c68b6057098bf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:47:14 +0000

    considering limit relief unfair slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
