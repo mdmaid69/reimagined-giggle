9fd4974e9776e6f5b90099860437d58b7387ab20
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:24:56 +0000

    chin Zz thinkover closed letter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
