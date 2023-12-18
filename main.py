1d320a435a79631178d1232905edb7b6c7556783
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:18 +0000

    shallow common afford broad proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
