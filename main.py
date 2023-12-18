bb5418b64432358c11d5cd880993e8726c89c2cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:28 +0000

    organization burst freedom brave life

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
