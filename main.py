f80b8619a61f1e53b9da984ee4edfc3f5930482f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:10:43 +0000

    breakup race brush noisy radio

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
