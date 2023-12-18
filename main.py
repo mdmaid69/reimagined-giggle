9d6a502a0e5b7b49839ee5dca76cd9db5327251b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:55 +0000

    myself easy oil them suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
