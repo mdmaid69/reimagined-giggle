7577b97fe19249ff6e129fae9dade08c67fcaab6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:23 +0000

    discussion measure punish tight part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
