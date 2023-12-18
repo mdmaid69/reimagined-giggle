d712623ee19220ba224398f9501c383b3cf17e2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:33 +0000

    entry stop march tune intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
