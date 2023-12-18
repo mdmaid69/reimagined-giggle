4285caae66bf4d5f6489033db923fab8ad7171f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:36 +0000

    Ll negative before relationship lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
