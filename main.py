076ccd30bb037cfdfe4668554c6d1e23dc642224
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:20 +0000

    consult emergency actually tidy heat

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+print(sys.version)
