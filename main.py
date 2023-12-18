d430504c4fdc4d32ad1a85d907187ec240835f12
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:53:58 +0000

    occasional everywhere beat flat possibility

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
