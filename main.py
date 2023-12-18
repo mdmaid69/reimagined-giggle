0504747d649deece5e52472e49ef61e52fcf33dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:19:11 +0000

    worried instrument after related use

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
