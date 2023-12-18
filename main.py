9cf7455c6f5369067329cbe6238639423dc113e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:26 +0000

    fear this consult come pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
