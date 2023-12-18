0ea5d20cda2bc5dfc3898ebf0bfe61d115a74c65
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:11 +0000

    such side dry load clothes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_speed(distance, time):
+        return distance / time
