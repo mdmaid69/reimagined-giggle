43326a7651a192e8ac4c4d534a11801cf69a945a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:25 +0000

    shoulder reduce damage pleased after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+from collections import Counter
+print(Counter("hello world"))
