aed5259fb794c17d2917e4dbeb6c5a6aad9acc08
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:49:52 +0000

    instrument place race there file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
