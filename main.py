df6d830c042e0dff9af3f6c6b7753a1a6b22930e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:57 +0000

    Cc record during prepare ours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
