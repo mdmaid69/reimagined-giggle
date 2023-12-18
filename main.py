09f0d8573e2a099786e3c70987d5cbd669ad6607
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:25 +0000

    round purpose plastic control sad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
