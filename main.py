8cb5c4ba684622b72eae29cb02736e7f5f2f8555
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:28 +0000

    record calmly size sentence contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
