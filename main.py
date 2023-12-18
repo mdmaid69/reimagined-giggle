6d3e9be4cdd8610dff7d8ae5f6b327434160cd2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:36 +0000

    clue rare reply prove conversation

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
