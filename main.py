db16fb8ee31cd842da95b52c4ae5eadd13ab2f5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:41 +0000

    aware peaceful ignore stupid scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
