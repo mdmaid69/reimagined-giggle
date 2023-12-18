1f69d6c59b39ecac569bfbb5ca67f069d1caab31
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:21 +0000

    store estimate knowledge average pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
