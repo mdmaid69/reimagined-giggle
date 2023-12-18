c53a0523e375a5f44d145d995fb717db32854f5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:57 +0000

    event remark practice mine male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
