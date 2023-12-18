a889249ee4f2971729579d0292c680f71296d48d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:42 +0000

    not clear advise save convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
