e781696e1c533a8108891699cd8e68cd0f457b98
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:37 +0000

    used habit tired discussion comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
