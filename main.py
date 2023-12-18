795e4cce080fd19b53ef6540a64262b8a7e873d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:10 +0000

    Qq objection related another getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
