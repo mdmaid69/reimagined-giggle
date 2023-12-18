0872389e051530ee62240abd394cbb4e961f6f54
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:09 +0000

    rain wrong calloff choose so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
