002b6f92e8df7067a3a99a189c6774500814826f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:24:47 +0000

    ever lie phone duty walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
