fab5ae57b3b5f0f4e837bba6e74b1c8419947c7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:36 +0000

    something admire quiet fortunately share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
