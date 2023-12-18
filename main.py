c6df2b68a72b65a46419273a453bb471c7d8eb8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:39 +0000

    either our usual consult part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
