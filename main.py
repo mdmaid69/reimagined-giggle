989ae9cfe9ea5287a2c0ef238d26f6ae6d8f5543
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:14 +0000

    month mouth will limit provide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
