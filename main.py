47624e76978bdbf3a76df2919443fab355356255
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:25 +0000

    beneath cause temperature because true

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def find_max(numbers):
+        return max(numbers)
