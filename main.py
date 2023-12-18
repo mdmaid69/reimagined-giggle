03a5345349fc71850252c0576f42ef13671789b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:35 +0000

    borrow rare hers lunch petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
