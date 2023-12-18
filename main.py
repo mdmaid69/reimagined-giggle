341330daeba59910541500326648a529bc4fa4d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:24 +0000

    Ff admit trip custom as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
