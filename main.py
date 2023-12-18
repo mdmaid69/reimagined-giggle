7553c68d170fde143d88d72e4ee99a6450b4566a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:30 +0000

    unusual burst fire desperate complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
