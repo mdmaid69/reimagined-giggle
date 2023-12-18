8dc6e7e2e2cc4862a3a1b604c67c23ef240f54d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:07:18 +0000

    patient frightened tooth sing possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
