5ad78dd94fc91652e746722b38250d4d7e538940
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:57 +0000

    hold fork recently design unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
