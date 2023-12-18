e52d166953ea513df5c5c9b5271b1b869c83dabd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:13 +0000

    learn move principle foot unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
