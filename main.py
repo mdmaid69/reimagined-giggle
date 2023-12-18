774ce67038f4cdffeae926711febae06489536bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:05 +0000

    space previous visit better hand

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def convert_to_hex(n):
-        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
