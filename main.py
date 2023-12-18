1c26822eb7378f29abf36b6a33d1bba27f6f7d9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:37 +0000

    progress bunch murder Ll mysterious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_odd(n):
+        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
