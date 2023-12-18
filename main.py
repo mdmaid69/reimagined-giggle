0a21229fdb4048a1384d0340d217314b8177be04
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:51:16 +0000

    maybe callat design ahead section

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_index(array, item):
+        return array.index(item)
