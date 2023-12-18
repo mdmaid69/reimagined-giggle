7745d5556e2c6570572a2e0aa31d5e7cf57cfc82
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:01 +0000

    work drive quickly taste tough

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
