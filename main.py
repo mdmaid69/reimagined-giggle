4909389a234f8d05d7a61f662b81ef5f1f71fde5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:03:12 +0000

    flow terrible bread flood especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
