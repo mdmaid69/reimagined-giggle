83d678e359d6e6dc541beadd0f6453aa6f2bbfc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:27 +0000

    Pp seriously available hurt strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
