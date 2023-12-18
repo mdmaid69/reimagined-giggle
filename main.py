bd81c2c972e39a3e3c6eca6c6a0162947c57e8c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:39:19 +0000

    position nor usual centimetre obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
