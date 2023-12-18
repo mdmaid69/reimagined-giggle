cf327cf588b2a48df75ae7dddf88a40bdb81481f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:09 +0000

    ill brave grand mark strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
