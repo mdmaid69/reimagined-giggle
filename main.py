c23f6485ef5db462bca56f915ec45f373d0adb1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:24:04 +0000

    rent stamp comefrom dance production

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
