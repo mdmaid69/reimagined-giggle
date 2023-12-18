0591905341bc6f9dc88af12227ebc09129c0f135
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:10 +0000

    guess know police brown bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-print(math.pi)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
