976da9bb94d92eeb4c86c8db9269e6a63d746e51
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:24 +0000

    weak show increase odd blow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def reverse_string(s):
+        return s[::-1]
