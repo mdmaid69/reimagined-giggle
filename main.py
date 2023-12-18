a942fef78c4da6126ab5765e732cb9c60f116ed3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:10:27 +0000

    belief flat wave entry deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
