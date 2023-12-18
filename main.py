5aaf77613e297c1f8da271ec7bf9074599c54afb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:02 +0000

    mail quality phone Uu scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
