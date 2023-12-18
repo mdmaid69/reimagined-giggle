7b298da9315a9f00a20c5f56daa3bb7cfefadfa4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:12 +0000

    calm lead vision loud progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+print(math.pi)
