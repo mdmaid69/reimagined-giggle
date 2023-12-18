47bad284210911f3d942661a519599d83585a52d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:00 +0000

    art pronounce 564encourage wall contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
