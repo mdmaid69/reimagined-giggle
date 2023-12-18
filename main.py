04337d984cd71bb638ff41421d4123c327455e81
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:05 +0000

    consist long space pity lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+  def convert_to_binary(n):
+        return bin(n)
