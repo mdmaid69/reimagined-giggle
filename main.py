13a71c335a12ff7108dfe45ffde230c130fc5497
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:12 +0000

    immediately thinkover important exact piece

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
