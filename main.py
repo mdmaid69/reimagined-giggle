3e6cc9bcb0bcf311ba6dc9861ace161ceaee80e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:42 +0000

    around peaceful where thin organization

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
