441b65cd8802bef9d17e68f26dd8ca26e724de7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:55 +0000

    trick draw turn simple turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
