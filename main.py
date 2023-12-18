a888d1e91f7ed74506b23b7facdc3a9d15c3ae7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:14 +0000

    doubt left size gas hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Reversed:", text[::-1])
