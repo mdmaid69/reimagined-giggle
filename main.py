58c81b5d9226be865931bd0dda2c86387103fe7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:35 +0000

    him bottom correct unusual always

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def greet(name):
+        print(f"Hello, {name}!")
