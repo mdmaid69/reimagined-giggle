6f18d67f2a8c575b8d5408c169d3abecce5c70ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:52 +0000

    lift acceptable bottom below final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
