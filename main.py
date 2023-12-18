c75501b3fa3daddbad6d2b91aec72a5ffa88cbc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:30 +0000

    relax getaway opposite case true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_work(force, distance):
+        return force * distance
