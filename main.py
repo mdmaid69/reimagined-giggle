915d3d43496ee0918f809115c1de7b7df429281f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:07 +0000

    breakup invitation public in experiment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
