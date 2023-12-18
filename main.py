b6211f12c859381bf2544a57c3a3b7261b270df7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:24 +0000

    Vv inquire story sticky that

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
