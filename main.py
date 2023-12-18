a5fd35ebfa6ab38d7a8c177ab3f94f439bacc43e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:58:08 +0000

    mean sensitive play strange figure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
