25a9b6cd72d43d6b131525521a618e4ce26151d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:13 +0000

    on damage hollow shade leave

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
   def sort_list(lst):
         return sorted(lst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
