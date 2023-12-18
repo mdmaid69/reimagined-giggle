0202f03a10304bea3f21927ef761efcf58ef9d6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:10 +0000

    early still offence stand cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
