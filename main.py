446936c478c36ed652503ed6626c72215663cc90
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:57 +0000

    drink stir fall translate include

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
