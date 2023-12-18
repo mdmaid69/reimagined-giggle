692f98586043f0e66d824d24339d26be5d0f1f44
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:00 +0000

    rank treat favour dull rubbish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Characters:", len(text))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
