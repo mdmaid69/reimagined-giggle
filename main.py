f61f9c9b223cdd921a571369f358813c1d41cd84
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:58 +0000

    shallow ordinary move tap once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
