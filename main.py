3e9284882803d0834d6bd9fac082278eaf4b86e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:59:54 +0000

    official great ours change case

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_counter():
+        return collections.Counter()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
