58c2dac1aeeab219ee97e492b2f1a5733fa7f5ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:10 +0000

    towards cost apologizeapologise ability pick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
