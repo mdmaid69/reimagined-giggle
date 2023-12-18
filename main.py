eb7252a5d72ada57053a8ece4a39800d17ee5539
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:03:58 +0000

    satisfactory spread will nor boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_density(mass, volume):
+        return mass / volume
