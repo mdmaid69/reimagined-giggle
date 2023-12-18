01ad74466b6b764387db3e0147d580ce90398e6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:55 +0000

    wide prepare continue often pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
