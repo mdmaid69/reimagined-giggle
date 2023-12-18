0a6d0a4fbda21f1259384b1a2eb60620a0399c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:00 +0000

    tap field home worried hungry

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+print([x**2 for x in range(10)])
+import collections
+def create_counter():
+        return collections.Counter()
