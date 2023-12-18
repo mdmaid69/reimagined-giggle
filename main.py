c98d1f75f279106a8d3743f26dfa9e6ac5126424
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:59 +0000

    danger tell sign doubt start

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
