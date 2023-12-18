f0f1cd554be697aa65e83429dd72383eed0f05cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:48 +0000

    emergency be priority gas mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
