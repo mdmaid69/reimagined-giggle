85a4011e102f62818e56abd8ea2b2e93d174e82d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:08 +0000

    concentration judge excellent exaggerate above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
