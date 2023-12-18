65d5b71143349eb63c1dd15ab778a2b36dd3c994
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:20 +0000

    can collect offer comment mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
