4a6704606f84ed1774ad0e5baf99a5e3e10e92f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:37 +0000

    prove show respect protest suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_density(mass, volume):
-        return mass / volume
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
