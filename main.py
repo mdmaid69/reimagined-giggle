a0b675317a24d1aa8c6cfb202195abc5c15406ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:04 +0000

    embarrassed break permission steer godown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
