856ec06a14be1de10f9d646d1529e582349f7393
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:34 +0000

    freeze tread gather detail slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def convert_to_octal(n):
+        return oct(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
