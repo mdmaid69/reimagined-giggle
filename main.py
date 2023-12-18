3f2ad1b5b04493f60ab1bbde6ff341b78331af10
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:18 +0000

    cheek sharp contents since honest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
