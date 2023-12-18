8b676b7911f94a901f436b1a813b9756508b069e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:26 +0000

    shine move getout shopping crisis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
