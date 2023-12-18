20e4ec847336419c81cd0fd6067073962fa46d5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:14 +0000

    that after steady generous few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
