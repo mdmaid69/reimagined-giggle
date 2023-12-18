76b2023bcbb5d2c3b3a502694f89e5e22d13dd61
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:18:45 +0000

    group available concentration play tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
