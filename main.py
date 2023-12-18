280c2470fe19a2faa58bb7e93705a0b8015c6a15
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:27:14 +0000

    this push warning company information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-print(os.getcwd())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
