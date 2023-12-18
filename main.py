dc31abecf2b68afbb3b760bdd037e2714d6e4d60
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:23 +0000

    what break circle design later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
