49ff1942c0203e28111f331fc8f33be75f4c0dca
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:24 +0000

    advice mail stir fast active

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
