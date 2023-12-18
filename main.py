1f94ba8c8095474b6698f2702426137ba758b185
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:08 +0000

    downstairs ring level spoil stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
