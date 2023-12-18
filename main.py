10ac1e31f806fb038e684a3bb5cffc9fe6cd0544
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:41 +0000

    impression thin train thanks stretch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
