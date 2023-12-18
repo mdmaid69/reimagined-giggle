6d6e28b7535e1cffd387466c1641efe0f2ef7790
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:31 +0000

    impossible idea provide movement cut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_as_float(array):
+        return float(array[0])
