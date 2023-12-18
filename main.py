409382c24e35ea89e173f50158be43647e69c1df
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:20 +0000

    cheque keepfrom bringup fetch careless

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
