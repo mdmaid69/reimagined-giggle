858b9a660ecef82da2174f53e34b0442cb5db639
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:13 +0000

    discussion middle ought worry bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
