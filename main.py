3fec40583360e592ab29bc231438fb8748dcbe5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:31 +0000

    grateful word after copy connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
