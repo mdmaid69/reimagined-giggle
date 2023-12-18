9670e4e1017e904e7eda6ef141ca7eace229172d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:15 +0000

    behind difficult like treat workon

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def is_odd(n):
-        return n % 2 != 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_pressure(force, area):
+        return force / area
