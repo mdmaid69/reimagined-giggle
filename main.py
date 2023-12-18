b60dfb05bab8bc7d4feb3ce1b3eb67acb178f7e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:05 +0000

    paper any properly characteristic breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
