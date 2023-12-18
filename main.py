25ccbbc60fc44e9fd7166028f9b777c13246413a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:37 +0000

    disappear grateful coffee dress string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_max(numbers):
+        return max(numbers)
