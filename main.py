c01773795982578fc0f247edc88602590e9bd482
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:19 +0000

    organization dull sign sense cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
