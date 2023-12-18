9910212b5ba4edb29cedeb7bd378a9d182fdd975
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:46 +0000

    exercise announce whoever radio suit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
