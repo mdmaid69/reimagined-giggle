647ab7163906d59edc8d538d39069701afc5943c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:25 +0000

    Ww strange learn yet deal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
