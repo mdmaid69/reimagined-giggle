feb858ae9485f3e106e96aefccd72eeb0c18d1b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:32 +0000

    clean breakup book recommend scrap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
