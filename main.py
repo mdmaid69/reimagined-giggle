e3208c4a7e3328c457411292e9b1fa28a7926120
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:37:36 +0000

    table serious when crisis top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-print("Hello, world!")
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
