fdf1e3c32dda26bdf275ed988dc4e9a8559c1bdd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:27:46 +0000

    base importance listen reliable switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
