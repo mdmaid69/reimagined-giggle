2c1b73dad8ca3719c522ef5c2e531df8e413c3c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:57 +0000

    dance dare rain authority rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
