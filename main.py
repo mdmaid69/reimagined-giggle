e17e73150479d5e3d0a2d09ed2c9c606706bb6ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:03 +0000

    scheme likely tomorrow top uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
