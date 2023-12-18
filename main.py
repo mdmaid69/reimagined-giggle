a1c33495a076ff1e36b3721126f40a150e5dfe69
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:17 +0000

    content last miss tidy worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_index(array, item):
+        return array.index(item)
