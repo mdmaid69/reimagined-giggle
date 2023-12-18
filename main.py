c30c7e1d88c8da471add2be39094385ee542b04a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:06 +0000

    whole touch bet actual moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+print(time.time())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
