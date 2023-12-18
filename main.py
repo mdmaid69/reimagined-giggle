52354a4b150b8fff081be67589f94eaa902142d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:37 +0000

    apparently gentle future doubt software

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
