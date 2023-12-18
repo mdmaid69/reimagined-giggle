22bf303008da21a956bb61d58ffcb7e20cbeb27d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:05 +0000

    school damage wrong way party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
