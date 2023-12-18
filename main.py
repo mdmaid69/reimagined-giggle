28df35995251ef91cdde043659eaf85435e34f79
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:17 +0000

    life century mysterious total could

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
