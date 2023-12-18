203912a86c95789e9b3e0dcd6bc3647b596d7df3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:15 +0000

    peaceful drink company dry stiff

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
