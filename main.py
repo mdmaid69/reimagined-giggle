70c2ab706bce8ec3cddf28ef82a9ecf0be4d1747
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:12 +0000

    last main give second yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
