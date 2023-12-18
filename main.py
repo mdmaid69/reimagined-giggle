15daabeba0c9519220043d241f993505a46ed021
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:47:44 +0000

    remember impatient tip consist attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
