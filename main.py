83d132745ea87518bac625cacb11720d76984075
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:05 +0000

    side description pay effect force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-for i in range(5):
-        print(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
