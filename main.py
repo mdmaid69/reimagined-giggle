ca30526bc239a6d1b722d0c99b0e1713d004eb30
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:43 +0000

    my experience strike set release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_ceiling(x):
+        return math.ceil(x)
