bb980454361e0d23b408e89459b3b5818860da3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:48 +0000

    promise consult so stand doubt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
