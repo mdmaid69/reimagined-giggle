1d2f45d061ab3d600c4ff7b45ecb2e94205efe6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:48 +0000

    happen purpose downstairs head keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
+import sys
+def exit_program():
+        sys.exit()
