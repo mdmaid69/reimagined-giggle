cddbf94e3ca9356359fc9fa82700c2c56e07b8f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:49 +0000

    by everybody meeting label goodbye

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-print("Hello, world!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_user_list():
+        return collections.UserList()
