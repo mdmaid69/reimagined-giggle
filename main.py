63f1cd1cf2b8c286d10953b0ec7c03474f62c2c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:22 +0000

    join everywhere surprise escape position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
