9a7f41b75ca092332edd80673dfe47aae0b23037
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:54 +0000

    creature how Ii modern giveout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
