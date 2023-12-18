d5a46025b7a0c2297812a49d3b3ffabb53e8c910
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:38 +0000

    deaf who leg guilty shut

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
