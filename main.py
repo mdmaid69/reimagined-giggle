7549983c5afebddc28841e576d76b0f4d5b935dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:19 +0000

    risk poor top obvious confusing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
