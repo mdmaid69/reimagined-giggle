8626a03cec2b0d64242620ce92ed9e8e2fc695ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:19 +0000

    spoil do polite male somewhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-print(os.getcwd())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
