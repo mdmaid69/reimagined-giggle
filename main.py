8029478f5652851d6e5fec8ede85a3dadeb560c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:15:28 +0000

    judge unfair everything majority shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
