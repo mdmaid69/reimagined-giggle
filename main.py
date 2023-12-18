2b10e40017b0d2b2540ee1ab82a0167199276b9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:51 +0000

    far principle firm person value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
