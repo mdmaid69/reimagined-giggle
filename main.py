95f288463bcf44cb9126346da8523530fa947a80
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:32 +0000

    lucky payment react send present

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
