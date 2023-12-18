3fd52400f25fdb10f503ab2d1b3d6b522c38c36c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:14:35 +0000

    spring uponon back success ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
