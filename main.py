eb506f0c830035fcdb0bf31926d6b5c4931ced8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:18:15 +0000

    right answer goback(to) physical small

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import platform
+def get_python_version():
+        return platform.python_version()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
