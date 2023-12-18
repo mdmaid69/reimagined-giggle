7213d6cb1fa9b1cbe9a5bacdd704b3f44560c1f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:54 +0000

    scare place wait strength adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
