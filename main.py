53f356864357f382d92ffa52d37619a490ed82c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:38:25 +0000

    turndown contact accident medium new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
