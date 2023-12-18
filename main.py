5100e44dcb14ed3387b7e1ae82350819b1fd3b70
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:54 +0000

    even swing cross back also

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
