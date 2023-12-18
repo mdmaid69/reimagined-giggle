88647d0ea4d6fb8be2cd25cb5f177501c68eae60
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:04 +0000

    sure regard injury talk born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
