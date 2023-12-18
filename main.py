a447b629c4737346a5b707951858139ac84f467a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:12 +0000

    hold wake either before declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
