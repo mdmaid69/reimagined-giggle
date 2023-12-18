2eb566bbbe3264b5383bf59ec96e840138c50f48
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:43 +0000

    keepfrom prefer view letter government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  def subtract_numbers(x, y):
-        return x - y
+def list_files_in_directory(path):
+        return os.listdir(path)
