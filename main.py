d2c59cd68f57b752258aaab9635d2e2bd7c8f9a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:58 +0000

    search dull in blame pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
