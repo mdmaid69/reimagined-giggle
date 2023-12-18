14810e3e7918ab8edf6243c20f635315db8a0da8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:54 +0000

    promise square chief formal fun

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
