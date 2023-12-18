68dd583fb959ce924cbeee89f8a68fa0dbb759a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:01 +0000

    squeeze air need receive increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_stack():
+        return collections.deque()
