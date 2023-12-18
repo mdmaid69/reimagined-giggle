527656a78bf432a5d08d797fcfa83f933d4738bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:08:58 +0000

    move meal comeround contact have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
