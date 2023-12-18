a3be455d3edf22252f0f480ee105e52447e45ad7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:26:11 +0000

    bag minority move approval contrast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
