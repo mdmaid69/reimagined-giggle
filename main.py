b3e7112cf4fdeeebe6e8a5f0c60256523aad851e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:35 +0000

    sleep accidentally Zz store pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def log_message(message):
+        logging.info(message)
