aa4d273174be93d1cda4982a34cb807d9fb4bffb
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:17 +0000

    correct stiff breakdown moral comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
