eb58b65f22a0f1aacf1a2572e0bea13368af8d62
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:36 +0000

    secret chase teach repair smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
