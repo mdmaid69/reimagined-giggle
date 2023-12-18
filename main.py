55257a5e260a8bee1e3071be9b1687467264da04
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:04 +0000

    turnback holdon finger force achievement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import random
-print(random.randint(0, 100))
+for i in range(10): print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
