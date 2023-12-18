d73ae13b9357d9e46508603d2fe143bda5603db3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:28 +0000

    bet favourite pride unlikely excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
