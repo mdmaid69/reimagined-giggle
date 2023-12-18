e3d479621db4242e1f87e745b44546e91e672abd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:30 +0000

    general walk business good build

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_power(work, time):
-        return work / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
