5b08d8528c157aa3f5fada60ce3dc40fe1038e23
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:23 +0000

    179bite 570enjoyable century positive sentence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
