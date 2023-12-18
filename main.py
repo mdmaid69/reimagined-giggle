8f98bc537fa468a6b7e8f9ae5b0008bc4f807463
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:17:23 +0000

    remember next voice instrument frequent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
