46906d719868c01675456596ccc195711a3e3cee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:30 +0000

    Qq paper exercise exist act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
