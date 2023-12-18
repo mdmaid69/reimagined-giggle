75e13bd7d74923103d18c96153dd698b6a5f1e0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:25 +0000

    copy shock needle none meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
