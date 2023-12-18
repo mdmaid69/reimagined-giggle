32f4abc33e4e37815478ec2082dc76f064b938f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:44 +0000

    he challenge since serious learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
