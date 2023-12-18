0c2b92c837fd4df9b36b88899f31810666db3056
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:22 +0000

    tear kill giveup connect that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def sort_list(lst):
+        return sorted(lst)
