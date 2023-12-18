cc66dbe10ebbd887f620e17c1fdb13f7218ba9f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:28 +0000

    single pretty dead drink bus

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+print(sum(range(10)))
