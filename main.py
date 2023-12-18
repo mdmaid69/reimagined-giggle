2e0c2747d8bd60721cdc11148e97f1295e38da2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:22 +0000

    doubt last bang practical convincing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
