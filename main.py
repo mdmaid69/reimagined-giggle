8317e089c92a28d59205ac05bb90e7d38394a0de
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:19 +0000

    score air weather Zz kind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
