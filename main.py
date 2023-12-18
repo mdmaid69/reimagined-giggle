5d51e1d5a48004423ed63a6ecf54b0fb123c11fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:59:03 +0000

    otherwise evening sad copy important

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def square_number(x):
+        return x**2
