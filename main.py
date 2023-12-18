fd01d1487094d6ff6d8871ef92848fec43adff8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:21:23 +0000

    father responsibility male around occasional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def get_string_from_array(array):
+        return array.tobytes()
