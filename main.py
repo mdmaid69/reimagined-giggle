81823e51e8adba4c80fe98dffca942d52644e55f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:08 +0000

    concerned charge fear eye reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
