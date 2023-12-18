3f435d2019f410c107c0a40f77c31bf42fec7b46
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:07:32 +0000

    the yes fond argument avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
