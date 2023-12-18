f434865fdaef5a6afd626793ba419747efded412
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:00:28 +0000

    indeed today produce lookback(on) spare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
