35b20628b6b8eb8b8f96051b5ac15029c461e5f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:48:43 +0000

    refuse special like secret rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
