764158db5416b749f575c7b6a43bf87b42ca4570
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:54:26 +0000

    happen foreign bread child same

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
