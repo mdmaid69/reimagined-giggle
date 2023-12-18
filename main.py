e9778c8e7cc2d72c708f4d34bd5cfc4fd410e796
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:55 +0000

    steam beyond same left once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
