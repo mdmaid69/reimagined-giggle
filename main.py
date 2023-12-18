35cb99c8e843eef8e6f5447d371bc6094548a310
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:29:08 +0000

    general mean sudden workout reply

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
