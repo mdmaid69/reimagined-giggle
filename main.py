1fcc5f5b668f1994cf8a62456ab11cc8065564d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:36 +0000

    satisfied Uu relative treat earth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
