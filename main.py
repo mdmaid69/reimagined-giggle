463253a5ac35bed726d03e04f37c344d8fbeff76
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:03 +0000

    believe door date design sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
