f6bb6e8afe98585cdacf2672defb5400f2f6bb80
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:57 +0000

    light thin method reaction fresh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
