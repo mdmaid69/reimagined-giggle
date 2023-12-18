6106cf8c8c27fac6f91f8d31d22df469342255a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:50 +0000

    dance runover provide invite 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
