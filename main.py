d7abe639c8bfac09bbf8909fb8f0e99456dc7d8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:14 +0000

    loose night breakinto none rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
