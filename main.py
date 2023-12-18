88b1468b34ff956099ecea6dfb6b0267b032bf1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:20 +0000

    mark the know process well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
