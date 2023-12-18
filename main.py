0ecc7b0030c879bb3c97280afe2ab0f979ce1b10
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:51 +0000

    recent next know organization before

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
 def calculate_power(work, time):
         return work / time
+def calculate_speed(distance, time):
+        return distance / time
