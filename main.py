337b7cf7c611affc61042caacbc6b8cf466802a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:32:16 +0000

    unless sorry crowded either impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
