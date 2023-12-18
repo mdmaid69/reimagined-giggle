b08ca9bd595a4e0024f966a7d89d628ec76caf25
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:33:45 +0000

    write pain trip disagree although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_item(array, i):
+        return array[i]
