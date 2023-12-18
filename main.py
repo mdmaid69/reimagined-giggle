f32c03c767a223f0b1fb4e213a4a65c224a55427
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:04 +0000

    rule trouble question error total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
