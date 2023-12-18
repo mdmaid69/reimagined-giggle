f9aeae4ff4cf04c3189bfdd22d9e43849909419e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:37 +0000

    accordingto task breakinto Tt pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
