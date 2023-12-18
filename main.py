04405c7d08206996ab8e989fe236bb108e2e37aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:55:22 +0000

    suggest undo important him seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
