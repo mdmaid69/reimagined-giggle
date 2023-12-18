4ceee57b82ede4b8bae9d02cb4bfa16579d98328
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:50:52 +0000

    patience bit protect 567enemy move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
