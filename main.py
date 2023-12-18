e59ade1dbeacc020b2f7910174bf998260f29898
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:01 +0000

    silent mouth interest whistle description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
