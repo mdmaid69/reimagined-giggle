a27d3fad55eebc822cb50211dfde2744a1e61927
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:27 +0000

    your test friendly phone environment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
