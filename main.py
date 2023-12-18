5638e3bc1a77369a54d31760299ca98f9152de9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:04:36 +0000

    exchange high butter conclusion shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
