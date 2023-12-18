4351a921d677e13eb36f0f785852ef206fd11f37
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:12 +0000

    accommodation plastic listenfor good air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
