edb53d7f7ec9dc7ed59ae573b8e56758dee4c515
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:08:00 +0000

    embarrassing but bet interrupt foreign

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
