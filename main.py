1fe965ad07daa067b2c6b389a4ee6514df44b86d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:30 +0000

    news naturally whether begin discover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
+  def square_number(x):
+        return x**2
