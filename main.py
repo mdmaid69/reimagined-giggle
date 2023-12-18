c87c93aa2b5fe58908aef4abb43a7e46d4d24d33
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:31 +0000

    main rule relative increase fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
