3705c3e31c6ada9ef8349c4519d670423b880bb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:42 +0000

    forward arm popular bill turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
