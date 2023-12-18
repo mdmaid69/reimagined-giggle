e667fefc3031b3287fff22585d0409dddaf72edd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:47 +0000

    pleasure his whenever fork sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
