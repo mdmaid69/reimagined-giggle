cf2742dd678a03a0d27cde74029985d0f0e54a1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:01 +0000

    average somebody past pain bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
