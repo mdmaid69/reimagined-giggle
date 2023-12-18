60840c9008415afab5440af63b93941da83c51f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:34 +0000

    separate team their boat slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
