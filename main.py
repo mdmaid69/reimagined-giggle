02e3dc93f598c398450e7804e7a14a6dbe8a0b9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:36 +0000

    tip scrap sight uniform chat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+print([x**2 for x in range(10)])
