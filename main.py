44bfccf891460a6b0eaad9ab86d12093c74a3bb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:34 +0000

    flow difficult emergency eat press

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
