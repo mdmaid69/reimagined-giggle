3873c9e568b7edb6675c06f2af597213828c0f7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:24 +0000

    open backwards slice doubt seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
