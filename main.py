c0318a4d911a1d666471ddf6dc3b1b0ca9ad109c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:04 +0000

    mix here sentence start people

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
