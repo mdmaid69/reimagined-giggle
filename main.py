a61255720914e188faad1c3f47783af9cf2e50e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:21 +0000

    now modern line watch steep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
