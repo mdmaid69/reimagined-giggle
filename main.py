51489f27e595f57054c29ef0f71d44b5fdb3819c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:28 +0000

    cum sick dream until undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def reverse_string(s):
+        return s[::-1]
