eb5daaea060f653d142dc82d7c06214a136e7b06
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:38 +0000

    exit available skin organization acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
