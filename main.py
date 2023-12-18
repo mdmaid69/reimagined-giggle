c6b24c41048f896c7723df0d634b1d513698745b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:59 +0000

    error start fortunately fall turnback

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
