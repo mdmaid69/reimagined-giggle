1ef51517d974e500aea22cc9eaec02dfd0cd2a6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:11 +0000

    no high dress write label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
