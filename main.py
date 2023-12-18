e13d2b5e95f45d20eef8df65c020521ea5723ec9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:51:49 +0000

    flavour lead unlikely listen machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
