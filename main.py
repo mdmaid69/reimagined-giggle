bf591582c1d06ebcf127d5079025abc9e9e25056
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:27 +0000

    surface glass lend frequent noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
