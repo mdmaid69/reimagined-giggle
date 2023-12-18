251a3e6b3266acaf295b6a8527bf59e2fbef026c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:04 +0000

    collect unhappy unhappy post now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_average(lst):
+        return sum(lst) / len(lst)
