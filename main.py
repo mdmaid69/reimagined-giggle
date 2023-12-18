cad8d137ba452c1c632a692be3d23754e6ebf39d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:50:36 +0000

    meal into light thumb dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
