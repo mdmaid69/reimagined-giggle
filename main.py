adfc22ad23ea9bde7ba8e5644a072bac0a85e5c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:59 +0000

    proper inform tap file off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
