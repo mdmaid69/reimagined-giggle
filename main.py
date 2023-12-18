e6d31ad3ad21acb3beee3013affcf3931c75e516
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:34:27 +0000

    slide approval some moon picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
