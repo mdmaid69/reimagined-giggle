9f41045bffb448ff07f874e30fe217b9f8a266c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:28 +0000

    thought cross result holdon height

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
