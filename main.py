ac042a9f668e87e0f1e063cd7e03ef38fdc57d97
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:41 +0000

    plenty wish stop characteristic cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
+def remove_duplicates(lst):
+        return list(set(lst))
