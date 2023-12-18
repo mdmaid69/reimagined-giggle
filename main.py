be38e4661b53cf4ae5591b54e36e046c2f9bc235
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:14 +0000

    challenge wander birthday pretend count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import datetime
+def get_current_date():
+        return datetime.date.today()
