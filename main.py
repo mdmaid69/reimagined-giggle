a045fa6c4661bcf7de24da7bf919c76e6e179eb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:30 +0000

    test error rare 573entertainment anybody

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
