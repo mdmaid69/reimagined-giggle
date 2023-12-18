a48dc9ca8c95dc21090c42e6f2ab711626586e64
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:16 +0000

    comeout notice be correct never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
