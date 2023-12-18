54c9ce53b147cd6aa76e1daa75b699321727b9c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:30 +0000

    one save welcome swimming concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
