1e0179fb6c35f448663f0b4dae6731805517b89b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:45 +0000

    charge crowd correct object sail

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
