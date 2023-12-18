5313a2053bd5db0d97233306486b7c8bf6b5074b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:53 +0000

    male winter earn comeover rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
