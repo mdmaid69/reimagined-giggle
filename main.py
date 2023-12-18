b7eaddd4b34d4b2394c490ba9a6a3826fdd9f024
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:29:15 +0000

    careless experiment commit listen turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
