12e238ede83a9c30b0478a749f44e42987cf584e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:16 +0000

    after clue display suit proof

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
