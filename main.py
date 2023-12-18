2c291877bc74593d0295164ae6568323da6d9e43
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:25 +0000

    never rescue listen cut through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
