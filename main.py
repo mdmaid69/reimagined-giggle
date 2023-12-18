4ed2be08397635adc4855a6e26902f1930fcb9c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:38:24 +0000

    teach very deal thinkof release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_directory_name(path):
+        return os.path.dirname(path)
