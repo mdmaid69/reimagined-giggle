bfdaa0cce5457bc729dd118e9667bd4294284cd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:47 +0000

    why group broken mix hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
