292aabf8a098db860edbf9a26251571bbe7f2fb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:13 +0000

    dream central print search field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
