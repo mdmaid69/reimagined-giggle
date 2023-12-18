6d456495bfb4b8839abce897278342c8522036ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:13 +0000

    experiment move cause seat thirsty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def get_array_slice(array, i, j):
+        return array[i:j]
