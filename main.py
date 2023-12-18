7a6737d2ff0bc97b6286f78ad8f88c8a9729201f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:06 +0000

    179bite underneath tomorrow embarrassed hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
