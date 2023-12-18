1132228b50c7e15aa2fdd04439699e8e1c4e3832
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:45 +0000

    simple hers convince thing cost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
