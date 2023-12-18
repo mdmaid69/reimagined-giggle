029630188321b9cb7fb9c68e7346ce46a8ea7c1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:06 +0000

    rare lookback(on) like bin shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
