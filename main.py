ad4d0dc30cbce37f060603c1e419fe9c609135f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:42 +0000

    everybody whenever pretty lie people

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
