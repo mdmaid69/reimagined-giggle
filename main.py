c1fee653bc6a85f65085554c9d6fd0fca6f88e1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:27 +0000

    workout government teach hope beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
