87691fb8aa90b7a0ad01bf6226ef65271366ac4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:13 +0000

    tense emphasize rent squeeze hand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
