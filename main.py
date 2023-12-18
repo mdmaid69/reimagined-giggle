7f487cfe961d4ab3798eb80010cf47c8f7722d4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:15 +0000

    people turnover type goodbye threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-name = "Python"
-print("Hello,", name)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
