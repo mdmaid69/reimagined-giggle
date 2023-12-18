28ff160a0cfe848275ef33f6dea7d0ecc9281b22
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:58 +0000

    burn fly exact winter butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
