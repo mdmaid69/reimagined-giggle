435bd10da872ad7997d997f52070fb8411391144
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:48 +0000

    steep welcome foreign Itself giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
