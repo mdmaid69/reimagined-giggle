a9f3e171663c440684e5af6f3ec35fe328b9b0d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:04 +0000

    rest thank goodbye believe behind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
