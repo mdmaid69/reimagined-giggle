aa1c4efb28d3e4537213d488219ad51afac6228f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:58 +0000

    female outside result serve protect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
