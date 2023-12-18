de6b10b26bc9de53def15f8490fc2ae246b2bcc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:01 +0000

    advantage thanks experiment like claim

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
