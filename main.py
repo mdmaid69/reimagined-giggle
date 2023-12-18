201e9d1895abf7bf52058c5aacbe4bb2c78003ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:41 +0000

    failure ought possibility post poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import numpy as np
+print(np.array([1, 2, 3]))
