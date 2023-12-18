febae570886267c1b3a6cea8383c4be89105ccf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:45 +0000

    tongue turnoff arrive mood comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
