e022f355e2b294cb90572914bf331db4827a2485
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:23 +0000

    allow number fold goon rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def convert_to_binary(n):
-        return bin(n)
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
