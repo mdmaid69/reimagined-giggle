d124870dd49bdb9d30c817b8795e23e7c2f9eb50
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:25:50 +0000

    even stone concern present independent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
