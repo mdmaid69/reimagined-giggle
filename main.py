00f20cfa76571a467d62d22a89c9543b263ab900
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:31 +0000

    complain pour man clean last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
