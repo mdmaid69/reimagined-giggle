faf389ff2f6eb9cc88e0e3a69c4812c331a681f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:49 +0000

    contain it long reject original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
