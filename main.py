c932ffadba8035aa4ea9b6271a791c129d5ef26a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:33:08 +0000

    succeed gentle correction scheme film

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
