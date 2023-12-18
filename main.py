6b339efb74f9064e1ad02a57513703ad0774f944
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:53:56 +0000

    plant unpleasant settle advice recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
