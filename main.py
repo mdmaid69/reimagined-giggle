871184f30ef3984fe6f180902635698b8e0b2d11
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:20 +0000

    lead children interrupt taste Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
