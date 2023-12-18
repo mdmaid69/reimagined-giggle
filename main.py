466233069d91a8dbb335ca0b52535dc841405999
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:54 +0000

    demand next excellent object popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
