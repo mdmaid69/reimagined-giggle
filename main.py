097b4cd58f74f3cab76d4428798cee64db2349a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:06 +0000

    sensitive kill child again available

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
