785be578baa67c34e0586d8b11f4ef600ae05fc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:00 +0000

    next peace treat demand artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
