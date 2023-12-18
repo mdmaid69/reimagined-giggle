b1165cf27f5bdb1275ea6e3c71c7974070eae501
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:34:19 +0000

    judge Qq challenge goback(to) push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
