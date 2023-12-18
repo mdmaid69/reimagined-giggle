f41ec82354394c84bf002233220ee804d6ac5804
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:16 +0000

    people practise Kk round detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
