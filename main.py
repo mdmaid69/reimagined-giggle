e0ff128f1ad224d44f4c5a31dc06ad3a5c70e45f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:21:09 +0000

    to combine able related cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
