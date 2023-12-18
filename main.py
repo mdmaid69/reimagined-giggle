3acc4e4a5cc4701f0597d89407da278ae1b86400
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:37 +0000

    trust accidentally circle damp fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
