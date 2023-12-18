c094417c31f0de3f3ad03eb883917d9121a5eec4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:23 +0000

    traffic yourself occur row usual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
