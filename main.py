405aa3ff8d70b0288562ca806d0145094e65a26c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:00:03 +0000

    slip most add noisy greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def check_if_array_contains_item(array, item):
+        return item in array
+import json
+print(json.dumps({"name": "John", "age": 30}))
