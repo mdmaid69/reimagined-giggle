274ff9a1b2e882ec04b37401566a1b646cd7a0be
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:23:43 +0000

    behind home exam around crazy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def is_even(n):
+        return n % 2 == 0
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
