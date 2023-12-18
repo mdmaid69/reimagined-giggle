8657f2b5766788f2b65f2153521efe722540d9fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:45 +0000

    promise afford unfortunate base every

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_acceleration(speed, time):
+        return speed / time
