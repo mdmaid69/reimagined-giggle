9e10872dab19890e47da8bb73eef067d7151b8a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:45 +0000

    experiment answer breakfast forever complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
