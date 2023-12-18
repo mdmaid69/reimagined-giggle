1d739c11f4ea45b69c37c05e2c20c79e7901e5fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:04 +0000

    bringdown bringforward today jump market

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
