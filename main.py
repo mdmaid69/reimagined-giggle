a8f77d4621b1cd5f0ea3ebb9c12dd72dec950b62
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:42 +0000

    listen attempt Nn pass contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
