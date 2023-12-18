e144ac3b69945a3f85e9906ca41e1289fd2c70a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:46 +0000

    tough process development tool float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
