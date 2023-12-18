02960cdd919f9f03c620d058366ccc98b2ca3f63
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:11 +0000

    rush smell argue idea female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
