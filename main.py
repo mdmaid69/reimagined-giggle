0311abb78d10851019ecf9e3313eb66111ff6a88
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:45 +0000

    public month trick ask extra

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
