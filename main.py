849ce8c3ba15e1786cefc72e7edd329820568ae3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:52 +0000

    confuse makeup sea opposite sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
