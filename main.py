425823bf56d33219992ee503470c76310c7a218f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:34 +0000

    pride wealth loud dangerous play

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
