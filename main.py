aa70299a86f0d69d42042335443b972d0fcb8c3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:11:44 +0000

    makeinto plenty quiet rob payment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
