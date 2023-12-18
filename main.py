abf859709b07342ff50bbd9430a1bb71a0ba46d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:30 +0000

    far even stick process violent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_binary(n):
+        return bin(n)
