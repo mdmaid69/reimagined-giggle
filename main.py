f5c559d38bba5e2b96fe70637ea8e52ba53d359c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:05 +0000

    Jj ticket warn joke company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
