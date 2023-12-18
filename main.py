d97dda389187b1c10b0a2d254c4fd48e2a1f9abe
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:34:13 +0000

    final doubt unlikely complete possibility

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
