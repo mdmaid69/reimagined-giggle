1c60e0e2db068467c66b0418e26d5d08f422aed3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:51 +0000

    unless politics declare major present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(x, y):
-        return x + y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
