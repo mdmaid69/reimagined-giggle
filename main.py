3c651c225059ecb3d9fc9f632ce94cbb764bc1ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:46 +0000

    lie computer set duty fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
