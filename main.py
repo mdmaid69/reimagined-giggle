9e8b1ee2729ca95f4aa57f20f673dbe41d2e6168
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:35 +0000

    collection manage great wind lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
