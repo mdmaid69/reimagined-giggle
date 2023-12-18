faa017a4a5926b9bbc1059261cd254f98af8f761
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:48 +0000

    quantity other shop attention strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
