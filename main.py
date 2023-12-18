53f79d5792e16f281861bd10fa58b9d6c2e502fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:13 +0000

    past commit unhappy bye other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
