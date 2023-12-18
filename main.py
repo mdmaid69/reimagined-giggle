79b9d3516a74f0dd894feedaf7e978b57b47307a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:38:57 +0000

    everybody dead break flood exciting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
