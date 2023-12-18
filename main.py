42c1ffa747ab6bafb8583d53294cad52c91db2ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:51 +0000

    keepaway task they apply spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
