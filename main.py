9ae4c4020eecc13860b2921a193117e845c47f05
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:41 +0000

    dinner few demand practise rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
