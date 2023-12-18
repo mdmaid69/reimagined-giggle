5e52d31e9f884db6787df5c90277697ec7c8fcf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:00 +0000

    when up comedown exploration future

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_itemsize(array):
+        return array.itemsize
