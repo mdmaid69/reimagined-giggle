1041563b136eb51f7b1506cca92840f90ac8bbf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:14 +0000

    hello quiet whistle loan somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
