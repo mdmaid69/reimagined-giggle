ae2e9a1019945a8b0034ce7f6af6d0eb09b4a678
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:16 +0000

    together holdup pronunciation appear estimate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import time
-print(time.time())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
