c8ebc5d48779741c2e7e354683b825c43091f944
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:33 +0000

    bank weak dark mean habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
