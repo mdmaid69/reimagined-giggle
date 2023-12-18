596b623a40ca5a887a290bfc91a5e614e3b56439
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:17 +0000

    night also offer slide almost

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
