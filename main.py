5fdc67b4384a0991e2a27c0b17e27f4142870aa4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:05 +0000

    cross occasional occasional break subject

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
