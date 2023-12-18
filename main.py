1900187fcf35042aebe04b398e0548f03437d80a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:11 +0000

    middle mail sting provide copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
