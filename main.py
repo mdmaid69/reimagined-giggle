482ec8723c0bffc9c515e2fca27d0d5dbe1b9a86
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:51 +0000

    man appearance exchange on fun

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
