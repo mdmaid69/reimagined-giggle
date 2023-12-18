ba1f002e2d9df8c835bef1cd93548ad230383c35
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:55 +0000

    price hers free load clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
