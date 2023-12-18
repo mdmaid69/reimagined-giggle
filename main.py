28e2b6368a260d426d62f1d8e2a339cfebca6fb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:50 +0000

    base reach pen bill bother

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
