55873e166d26d2a9b09653ea550fc75fc03de8dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:06 +0000

    debt same spring yourself national

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_max(lst):
-        return max(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
