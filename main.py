e5b5a83c878a298afc9a2d37a73f7cc8f37d4cfc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:56 +0000

    hospital rare cough ball little

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
