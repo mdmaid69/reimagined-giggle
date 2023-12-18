3e1d65a2fd2caba2d833940f13522da7a9ea1480
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:08 +0000

    characteristic wall shrink weekend frequent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
