bf71a47959320eb522c9e525b2df9cc824515121
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:46:18 +0000

    exhibition now favourite about separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_power(work, time):
-        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
