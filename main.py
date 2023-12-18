18b4f0865971207a4f9842d11477a896cf704f18
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:04 +0000

    gift simple frightened foot before

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
