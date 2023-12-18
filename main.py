d1abdcbb4fb75addc4cd6e592bdf255ca8bef55a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:24 +0000

    plan plastic ask tell software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
