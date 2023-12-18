dbd5a9bc8610d003dd1690e15431134a9ba48f16
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:20 +0000

    takeover winter swallow waste damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
