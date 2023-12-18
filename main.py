194d142254a8a41036b4324cddbbe049fa5ee2b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:37 +0000

    behave impatient recovery treat so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
