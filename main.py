0dcdaf5a80bd72326cbb8f346047f938a95db87c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:55 +0000

    vanish Qq lock day without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
