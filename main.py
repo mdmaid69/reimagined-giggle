5874f533b1d2c0e533dca64503456cbde1347b92
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:29 +0000

    admit plastic shake might male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
