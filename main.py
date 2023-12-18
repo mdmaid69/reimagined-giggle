99dba1b5c6ccead4b66af0d1146394a2d82359e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:49 +0000

    entrance address entrance like mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
