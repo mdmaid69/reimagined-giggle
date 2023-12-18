87501314d7ae48bf6ad89167494693408f88c373
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:43 +0000

    meaning taste nurse since sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
