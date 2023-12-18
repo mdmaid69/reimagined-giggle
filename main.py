7d16e9337a0d4fd7b33899c383d0ba4ea6e98b2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:23 +0000

    free shame pure similar late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+import time
+def get_time_since_epoch():
+        return time.time()
