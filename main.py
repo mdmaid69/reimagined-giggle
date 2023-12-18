2d3c719d66d54d9c98db1ce5adcaba8abef0421e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:56:26 +0000

    branch throw previous thing pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
