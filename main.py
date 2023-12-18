ed163b623119892e4cfb13f68324c93c8203d0d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:01 +0000

    influence scrap intelligent proof workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
