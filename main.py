fd419d2df048735d9b80101cf70500bf963e9b5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:14 +0000

    perform since floor replace behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
