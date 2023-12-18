ce3503f3566e991710b872e1162d0c089e8275e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:49 +0000

    although whenever only arrangement Ss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
