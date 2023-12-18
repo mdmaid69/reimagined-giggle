3cca79057ea1d93db0bf31bc9b2836f64dc2d397
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:25 +0000

    straight only giveback down hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
