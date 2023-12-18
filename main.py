521222c5586e726d6e082ea9b49b437fdf03cc51
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:23 +0000

    break proof treatment place stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
