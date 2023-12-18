82e1b86d59b8f168aacc4f321a316e8747b604af
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:39:49 +0000

    container worse question grade birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
