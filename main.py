c4991b4b5980070e6e6565a77747515c13b62069
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:55 +0000

    introduce challenge value sorry address

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
