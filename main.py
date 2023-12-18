8aec7a889c54e7afe27998c1ee13cd7a78e6e9f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:34 +0000

    valuable lesson calloff accept any

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
