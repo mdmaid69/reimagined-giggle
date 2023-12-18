bc2a89e2dea2ab240aaee4489c9fcb09a051a1f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:56:30 +0000

    true enthusiastic lay flow same

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
