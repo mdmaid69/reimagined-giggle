a49b4abfe502ab723eaf0a32fd6ae4a141e9114e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:17 +0000

    care back connected completely mixture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
