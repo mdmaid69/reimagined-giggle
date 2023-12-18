9e464a375074818511d4d3e8dd3cb5ae2657b438
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:30 +0000

    risk goon negative then soft

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
