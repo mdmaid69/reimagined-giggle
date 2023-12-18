db367e2aaacf1242f9990c76e5c885231bb3f21c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:51 +0000

    bringup border plain dish unhappy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
