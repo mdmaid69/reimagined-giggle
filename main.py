69b74a893ed6b1ab5a371809ee7dbd15d707ed7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:56:07 +0000

    achieve send relief unite union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
