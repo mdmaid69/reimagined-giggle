571fd7bec250c1fa53e8b3d522fb5bb0011723a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:55 +0000

    battery keen forward human comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_power(work, time):
+        return work / time
