897b28ec5aa47bd123bd4da6b24266ad280f944d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:55 +0000

    tired loose height plan it

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
