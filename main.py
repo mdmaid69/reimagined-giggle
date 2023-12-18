584ebbae6ab4e0565df34fd37101cf9cc3bc4f8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:11 +0000

    study regular talkback flow miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def add_numbers(x, y):
+        return x + y
