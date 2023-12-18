de0a69b17f9dd9c835725ae3bb15279ce354ec83
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:59 +0000

    charge hospital complete connected destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
