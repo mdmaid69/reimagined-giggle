7d775df7d1569d37bf4fc7b1af44784c33b3d076
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:58 +0000

    realize nature desperate lookaround away

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
