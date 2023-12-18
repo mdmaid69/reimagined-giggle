56b1ff2e54265eceeed00afb266cb3cd6514c936
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:24 +0000

    back fall about attempt load

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def check_if_array_contains_item(array, item):
+        return item in array
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
