8b437eb089090f84dfce40fddbb08a260f794780
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:56 +0000

    congratulations comefrom sudden undo describe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def greet(name):
+        print(f"Hello, {name}!")
