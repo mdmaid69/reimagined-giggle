73af46862f216335b942a338519520931a697786
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:04 +0000

    greeting heart dull class level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-x = 10
-y = 20
-print("Sum:", x + y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
