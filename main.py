7ed64df616dc8b2ac4536e6b281b465b36d37e5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:47 +0000

    shut bath kick secret indeed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
