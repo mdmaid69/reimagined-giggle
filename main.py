de3bfa4ff926ba3d2f4ce9742d5dd0dd599602ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:52 +0000

    mess pretend switch opposite table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
