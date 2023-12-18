e43a30a44b2e174bc657d7bfaa3a4481adf57961
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:35 +0000

    official catch left field no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
