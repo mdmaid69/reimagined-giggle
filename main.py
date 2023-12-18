01ad3dec5e10e3c8135f86581d65ebf7d8fee959
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:34:10 +0000

    introduce last keep afterwards let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
