c67b94d9ac3710d21005be78688287dddc41151a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:43 +0000

    immediate form sale odd fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
