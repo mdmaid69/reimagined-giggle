d7299b07de06a60c89130d2438964c87dd573395
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:06 +0000

    phone neither band bringback account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
