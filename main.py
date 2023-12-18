8e6c440ff69df5ae82901fc4cac0bcae0c922bb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:23 +0000

    plan right rid order workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
