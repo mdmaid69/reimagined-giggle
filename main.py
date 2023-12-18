85cfeb57ab0422659c49d3fdf5df83947113a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:05:33 +0000

    father church first offend absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
