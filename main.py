f4bd47b5326752035a52f2a697d90f96cbd29cc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:38 +0000

    wall ring loan grateful bit

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
