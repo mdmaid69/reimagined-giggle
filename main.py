cc2962e6a94a776e9a6449ce2bca8ed14aa75240
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:24 +0000

    impossible sweet praise spare comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
