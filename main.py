b9bb5a71990111ce1d868d892a6248356ae3cfac
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:30 +0000

    free rarely behind duty reasonable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
