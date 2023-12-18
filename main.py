bdfa9f2eb968f6eb097fb875cbba6ad39595dba4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:27 +0000

    share account sensitive post ask

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
