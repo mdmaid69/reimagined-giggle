a2ad02391efe42b7cee648cc02ce9a5d966e52bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:00 +0000

    tidy success reply wash report

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
