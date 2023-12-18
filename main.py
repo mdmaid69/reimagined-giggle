4729214f23a55696fbde872a54c8f2f40e45c554
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:34 +0000

    juice tend dead smooth ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_queue():
-        return collections.deque()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def multiply_numbers(x, y):
+        return x * y
