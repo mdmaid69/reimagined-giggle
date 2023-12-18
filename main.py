9a9134f7b9ed335e94dbc866d2a3be91a68f0da4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:19 +0000

    shame ago runout programme sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
