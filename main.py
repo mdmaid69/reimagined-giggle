0917cd6bcf5836dfc615a374b92c3f098201fc35
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:11 +0000

    climb ashamed bus crowded middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
