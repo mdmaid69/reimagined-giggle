e63935978b18e796369ad357a261073ce9a3f1cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:38 +0000

    goup suddenly boat rarely exam

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(10): print(i)
