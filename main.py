39e64ad7268dadcf8e686bf6385234bcb2492398
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:54 +0000

    substance wide dead search tired

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
