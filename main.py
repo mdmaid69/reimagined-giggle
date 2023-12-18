2826555a57e65222ec32d20e0fbfed039d6d6b71
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:48 +0000

    cut design reaction talk minimum

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
