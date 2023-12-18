cc21d3ec31153f3e59296322e651057a195fd2df
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:05 +0000

    believe lock operation second produce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
