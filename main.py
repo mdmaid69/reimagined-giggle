3aefbf741832d23fd16b60cf3c0f62fd0d26661c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:52 +0000

    region target body rise brain

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_volume(length, width, height):
+        return length * width * height
