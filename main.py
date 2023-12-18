c1fa91c1e9829df4035681b0e1d2d4673d2037e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:46 +0000

    decide sure healthy myself bank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
