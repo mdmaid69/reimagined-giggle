746d2a757eb3d7ebfb3e35250750270f06790046
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:03 +0000

    bread cover change knock concern

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
