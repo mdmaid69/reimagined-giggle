17a02fb11ae39cfab157ee9da247ebced7f455f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:02 +0000

    meeting card must back control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
