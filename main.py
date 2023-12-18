03b841ed57bce45d6fd27994e2f4014c4df76c7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:56 +0000

    oil crisis today slight taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
