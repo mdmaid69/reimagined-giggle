bab5f08e3320f87b372767425c5dfba10a6a48f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:18 +0000

    do honest impressed better cruel

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
