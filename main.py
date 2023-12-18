465a431864057be1ffea52ab7f35f3fb6ed6ed7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:28 +0000

    event repeat write good shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
