b8bb37d81241d7069a2ad9eb59b5bd0b3a1e1e79
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:37:41 +0000

    finger to deal lookafter basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
