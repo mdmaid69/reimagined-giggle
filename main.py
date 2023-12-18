7c3647bae0e084a512793ee7e5baa6e1b7e89439
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:08 +0000

    oil official feeling tread strike

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(lst):
+        return min(lst)
