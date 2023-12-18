7f8a136ee224710904055d0052b809fef5dd5f5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:32 +0000

    duty 565end event remain spare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
