054690e606d2753857bbb8fb4b215d5491fff3f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:31 +0000

    round fence quick on know

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
