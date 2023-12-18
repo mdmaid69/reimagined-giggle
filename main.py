c571eacd958608bf56d59fa07bf7e26b88c59840
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:07:08 +0000

    trick powerful she uniform runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
