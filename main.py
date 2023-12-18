4160a0735da1403dcd47a2762631cc7b5ec0764a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:58 +0000

    challenge stay their size suckme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def square_number(x):
+        return x**2
