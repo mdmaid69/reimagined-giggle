6181b5f3bcaf9fc5cc0fa420a460d61c0c5cf502
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:25 +0000

    paper takeoff think visit trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
