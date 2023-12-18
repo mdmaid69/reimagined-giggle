231272a7200bf1cae05ed76e1455500bdd1c00e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:33 +0000

    face thing honest turnoff usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def cube_number(x):
-        return x**3
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
