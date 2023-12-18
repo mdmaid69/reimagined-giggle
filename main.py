d0b68032f4e36670fa74b92726b931bc74a9097e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:16 +0000

    say gate skirt imagination himself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
