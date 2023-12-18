62428b7b9bd99b2d880c00c9ece7db20659ec0a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:39:51 +0000

    guard injury typical describe satisfied

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+print([x**2 for x in range(10)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
