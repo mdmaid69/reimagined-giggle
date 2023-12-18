16feeccdd406cfd9c85ec6aa5f276ce8a841590f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:04 +0000

    embarrassing guess round most wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def clear_array(array):
+        array *= 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
