7083e111894528017cb1bf65f58ae8b3afd8d216
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:58 +0000

    race acceptable familiar comein view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
