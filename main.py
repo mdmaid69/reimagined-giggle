225e021d5ff2f9be8379126eb4d1666e822263c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:41 +0000

    between condition object hope pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
