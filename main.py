6fdece5171cd255c94a15f581d462a72581a101b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:05 +0000

    compare instrument Dd spray basically

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
