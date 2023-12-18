739edf9cb229432e15e4246426485502db3cc9fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:11 +0000

    catch connection scene land story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
