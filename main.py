e298f850ef559b60b6044e994242de2b336adc28
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:21 +0000

    to load final develop to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
