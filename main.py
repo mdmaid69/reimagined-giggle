c0457d5cc835a00fc038177ceb3290f203d71135
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:41 +0000

    evidence ashamed strike develop agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
