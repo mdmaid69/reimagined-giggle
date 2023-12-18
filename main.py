491f1535e19032dcc8211439b37b6c8670fc0465
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:34 +0000

    post better there company bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
