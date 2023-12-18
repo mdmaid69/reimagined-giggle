1cb337c50f05c78f3a560cb4e4ea01162b6efb21
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:30:03 +0000

    activity straight science wing city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def pop_from_array(array, i=-1):
+        return array.pop(i)
