45a83482939e7227af7bc3fdea16077946f4e40c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:30:04 +0000

    narrow receive family help winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
