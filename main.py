b27739a95ab0205371eaec9a7647fe0a1b665c25
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:38:08 +0000

    basis loss would press send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
