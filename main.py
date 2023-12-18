3f113891a4e407f478aac49341646def46bf42d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:08:26 +0000

    complain he deliberately imagination ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
