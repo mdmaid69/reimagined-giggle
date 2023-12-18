e0115202a396ddb42fb8d2144540d57c2ba58aa3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:29 +0000

    smart crime machine occasion sell

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
