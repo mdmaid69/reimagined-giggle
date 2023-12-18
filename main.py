23b2723bc52e79a42ae15cd2fc20b1c020bae6db
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:02:22 +0000

    this wish drawing comeout else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
