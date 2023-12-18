37812dc8c3d3331eab8286098c38cb04103e32e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:21:14 +0000

    myself better thorough freeze task

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
