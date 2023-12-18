1f05c2b3aabfcc870b8eb10f1d775d2c28a1c559
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:50:23 +0000

    trousers meanwhile direct past disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
