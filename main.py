157fd3783c2e8bb754bd91407c4d72bd02309c11
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:49 +0000

    cheap correct car stand giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
