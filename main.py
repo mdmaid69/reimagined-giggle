53f74ace129bb672a7c8abd882675be41bfb6e68
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:12 +0000

    Dd brother weak thought base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+print(time.time())
