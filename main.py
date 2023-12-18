8bb991bf89a6fe0c39074c2a5fd1ade0336d8b98
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:11 +0000

    pull bet control unable successful

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
