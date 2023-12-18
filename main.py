08d719775b454c2e34c4909f05c8283b7d4b1798
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:59 +0000

    someone out race grade reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
