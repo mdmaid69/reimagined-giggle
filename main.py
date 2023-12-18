86c4d5e92e443869e89265badc187653e5408f92
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:36 +0000

    foreign pressure ball communication sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
