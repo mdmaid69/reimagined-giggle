2d81e9fe00760b9e4cb65514603104b3dbf4e8e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:28 +0000

    work back muscle solve below

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+from collections import Counter
+print(Counter("hello world"))
