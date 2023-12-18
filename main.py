716c0546ac2ed4fc1bed6174f086d37631f90e56
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:22 +0000

    straight new challenge patience somewhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
