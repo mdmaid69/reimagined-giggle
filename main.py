e1a0fcb3db390c90bb90b6be431523a32329d1eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:38:00 +0000

    comfort before duty afraid low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
