1e6b3c0dfa1daf45d5ee92ddd8b6349edda73731
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:43 +0000

    pinch danger wrap cough drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
