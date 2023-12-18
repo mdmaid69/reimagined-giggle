cec13c72792c0d64531944ad27ff73fa2c2e09e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:34 +0000

    bringup wild between complain human

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
