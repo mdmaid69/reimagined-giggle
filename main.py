b8dcf7e3868db1b85b22e762c6d9f96410d466cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:50 +0000

    we illness roof fence body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
