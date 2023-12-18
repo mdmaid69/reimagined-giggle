9cbfb40cca5eb7b7569db298baf42d59d9b3b0ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:02 +0000

    separate all would passenger exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
