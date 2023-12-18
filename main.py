66680f2ce914e89e5f1ef7844378b2cb4df1e27a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:16 +0000

    call put pool principle patience

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
