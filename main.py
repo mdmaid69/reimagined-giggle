748f8f6901aa360da67aaf6c505e757dcb21155a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:30 +0000

    that necessary pressure mess open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+def count_characters(sentence):
+        return len(sentence)
