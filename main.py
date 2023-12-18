2b99e28ac7685b535d4c037864e5193b4ab9a574
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:53 +0000

    everyone but boring information actually

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
