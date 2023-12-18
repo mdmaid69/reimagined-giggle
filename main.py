196ea059ac2e36ef668ff6b2903a122c38507f20
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:43 +0000

    compete gas keep bar interruption

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
