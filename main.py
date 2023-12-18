7fa400cb98a5617ac9d099f25675a261c8331b20
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:49:59 +0000

    exercise pile affect result both

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
