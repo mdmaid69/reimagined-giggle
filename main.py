8495867df7407a05f4352585cd5ba51f6c3494ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:05 +0000

    professional dish pause medium give

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
