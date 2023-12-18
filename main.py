ee592c22b4682a6aa661f510fa88369b1b7dacbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:10 +0000

    beat smile slight bear partner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+x = 10
+y = 20
+print("Sum:", x + y)
