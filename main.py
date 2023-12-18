72f6376e8e43e164f1280917a982b2c2526efcb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:18 +0000

    damage impatient instrument building reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
