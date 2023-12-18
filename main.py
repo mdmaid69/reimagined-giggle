7385595e59b01579466cf0a9edea25a660e690cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:55 +0000

    centre lookback(on) surprise blame under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
