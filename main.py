8b3d8e915e46d888631eca35903b0db04ec27ed1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:29 +0000

    lean treatment late rank guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
