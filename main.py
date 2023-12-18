b9e91c1d57f1f0196be480776de952d6e239091c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:07 +0000

    hair whichever Yy drop godown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
