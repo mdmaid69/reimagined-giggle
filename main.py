537ca65a399869b0637c542859a6bd67bb955fe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:20 +0000

    post star neither rescue conclusion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
