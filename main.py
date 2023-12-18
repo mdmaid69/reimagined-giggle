6a0eb7f36960ec4be0124e465d588cd2d8b23a3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:35 +0000

    relative sore pretend answer constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
