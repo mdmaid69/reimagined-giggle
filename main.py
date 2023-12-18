939a19e635ed91ce52bb85ed4009187c2d9cba7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:45 +0000

    mass along bread listen minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
