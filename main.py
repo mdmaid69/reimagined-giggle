1b662d5493faf1bc24181a9cbb61ef8da5c52186
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:19 +0000

    tight scared wish final getoff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
