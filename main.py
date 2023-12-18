d60d6f2123a44f91efd2cbd900dfe71cfc551fe4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:05:50 +0000

    hard fold minimum prepare pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
