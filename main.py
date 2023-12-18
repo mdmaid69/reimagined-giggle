1cef37f5bdf9fefee017c2a98ddc9e9399ad1af7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:54 +0000

    adjective pupil present naturally plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
