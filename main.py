b74cd9b914a197f2f9d15ffff248a8098b2074e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:03 +0000

    wake clue kill welcome protect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
