2406874fe30d5d2c21e55c81cf99661b79fa0846
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:38 +0000

    police narrow battery point crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
