470a63bb0941581df94552864d5258f1ee04ca39
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:28 +0000

    interruption run grand impatient test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
