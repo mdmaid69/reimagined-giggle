d3cb8fc92945e30b2a32ba9dc73728ef962236e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:20 +0000

    ability control variety keepoff refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Characters:", len(text))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
