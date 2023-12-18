82e61e656d8d14c3259b6948b8f6c3f41e3d6c24
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:57 +0000

    fast make blame post forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
