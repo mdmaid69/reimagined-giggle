b0b5b2b6e3f705a782d32e757cbe0ecfe4340319
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:13 +0000

    convince discussion ignore custom boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
