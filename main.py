52e0779a0f1a47d22ecefd335f6bd00d517bb003
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:16 +0000

    one between trip stretch warm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
