0b368ab11d198fb901e6ade71c685282e9acda0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:18 +0000

    survive female message represent confused

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
