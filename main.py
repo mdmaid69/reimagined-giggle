52eaefba16f11b85fc2230a6a6571fd1e9851e59
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:24 +0000

    disapprove swing teach seem finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-print(datetime.datetime.now())
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
