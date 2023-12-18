cfa75ac7949a7338fbb8c540a22bd2f92d59cf65
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:47 +0000

    learn relax admire telephone this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def is_even(n):
+        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
