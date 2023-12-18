ecb9072a4265fca40e0e0ff902597bbcb25ae4c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:45 +0000

    telephone model crowded address judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
