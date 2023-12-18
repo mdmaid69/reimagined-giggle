00d4c0cc2ac68b242a5257b90ae79e056dac4ddf
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:06 +0000

    Kk lunch exhibition tread ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
