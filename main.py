b282dffe75b011e40e6e076611a5a9c2a568a535
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:20 +0000

    left keepfrom arrangement cold occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
