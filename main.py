88ab74f92580abecd3c4399dd16af28fa97a4213
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:44 +0000

    special drawing day afford effect

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
