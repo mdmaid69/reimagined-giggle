3dd1ddcfd99144d275e7c6bf9c472af0bc01fea5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:23 +0000

    short display would any story

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
