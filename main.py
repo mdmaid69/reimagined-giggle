0a6902d64bcda667cd1b898258caa588bc733da8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:32 +0000

    Hh extraordinary test tomorrow dish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
