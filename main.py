dc08f4968c39b0875121dee8bcb56c13cf17110d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:26:33 +0000

    fear hurt answer present project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
