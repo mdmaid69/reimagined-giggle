b2e9dd039812a04d36c6ba622da43f806aa00ed4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:10 +0000

    female sticky breakinto should bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
