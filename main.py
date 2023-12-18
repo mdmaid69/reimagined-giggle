42bbed97c2477f6683c0b4fcac427a9600c12257
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:07:25 +0000

    every news joke only shrink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
