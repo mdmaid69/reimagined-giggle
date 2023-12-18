b6d7f01eea1de2ba5e5239d78a11e80cd8dd3bbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:04 +0000

    spend birth high past classroom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
