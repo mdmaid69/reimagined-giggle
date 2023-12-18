e686fd8f072604f02678637a22a89d0e6825c223
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:17 +0000

    boat offend furniture thankyou cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
