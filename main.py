93200c9b2b3f7b1f7f19981020615501d0e594c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:24:29 +0000

    shoot broad spoil talkback increase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
