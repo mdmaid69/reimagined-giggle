018b33d97ced74de1cc5e8da12c611d1c8257e94
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:43 +0000

    dangerous trip spoil risk force

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
