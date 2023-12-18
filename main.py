7be4ba3d782ac0ea3b322039dae49c29436f6fa4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:26:32 +0000

    excellent cough kick connection familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
