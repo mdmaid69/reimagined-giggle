50868bfcfd16657e06a8e42f00256ef0e7a92f51
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:36 +0000

    duty return glad hide lost

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
