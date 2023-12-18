ce514f56cc75e71e054e1913154d3e3e39eafc23
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:05 +0000

    quality introduction expect ticket strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import time
   def wait_for_seconds(seconds):
         time.sleep(seconds)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
