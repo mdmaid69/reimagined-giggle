092f84b73349eed42461cf884191d3b2287e2f36
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:33 +0000

    belong opportunity themselves stand thought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
