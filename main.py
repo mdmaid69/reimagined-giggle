da0a086bdd9afc1c989494957e8d7a4fe9f73f7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:44 +0000

    appear almost attempt pupil delicate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_stack():
-        return collections.deque()
+import time
+print(time.time())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
