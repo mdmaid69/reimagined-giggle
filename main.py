ba1aa7ca4cde8f8797d1e5bc521fa23b816a57cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:17 +0000

    study illness slowly low hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
