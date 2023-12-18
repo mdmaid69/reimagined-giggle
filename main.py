32959b64cc931eb28ce63209c258258980132bd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:24 +0000

    clear wet talk dead than

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
