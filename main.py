99c51013b79db8f8a6db0ababfc1687ae57c37fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:16 +0000

    trust content although proud whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
