99e8713c64ae3acd4caa4ea8c65f74125eebdbbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:30 +0000

    quantity hurt search consider straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
