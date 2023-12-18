a06a308d902eed564d7398cb286ccf4c37d88429
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:22 +0000

    excuse bar together block address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
