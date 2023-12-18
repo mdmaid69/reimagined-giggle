9f0c13bd49db069bfa0df96dc955a40e0b564280
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:19:06 +0000

    instrument scrap tragedy return onto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
