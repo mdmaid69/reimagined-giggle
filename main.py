c94a585d03a06daf239e771c86131c0112293fd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:36 +0000

    mainly doubt impression everyone attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def convert_to_binary(n):
+        return bin(n)
