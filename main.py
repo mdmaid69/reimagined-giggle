0a9a71b993cb96189d47cc02327a67eb16f2630a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:45 +0000

    familiar meanwhile breathe ourselves uncertain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
