55053cbff2262f78192cbb18ce20d03fc446f54a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:22 +0000

    after finger be danger circle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
