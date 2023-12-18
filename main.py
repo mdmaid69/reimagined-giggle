fe8fbe9e1c1e60c3c94b3f21529fdb22c4480ca1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:18 +0000

    comefrom what strange main stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+  def cube_number(x):
+        return x**3
