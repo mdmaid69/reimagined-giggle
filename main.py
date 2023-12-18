5e39881b25a91379a375bcc4e48b4c00f9dce1d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:39 +0000

    he far bury meal below

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def is_even(n):
+        return n % 2 == 0
