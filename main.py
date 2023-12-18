33512ed28d0555db8e5729c3a1c48911ba7eba85
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:07 +0000

    which land hardly ground left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
