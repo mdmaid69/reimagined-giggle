e04edcfd62c0834faa6c615abcc8fbd5b0d67ead
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:47 +0000

    burn wash press air walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
