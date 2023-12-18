e20a04dfda32d0a0f2b100a9cb5762887454d06a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:09 +0000

    march close container climb influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
