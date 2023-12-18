714c799f0b4066a02302b69555222b1da6e512a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:05:47 +0000

    himself early effort early scrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def count_words(sentence):
-        return len(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_priority_queue():
+        return collections.deque()
