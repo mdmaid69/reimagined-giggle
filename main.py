9d3ba705570a4f5b5f152d4113a95bf0b944b95c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:00 +0000

    to accident medium apart anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
