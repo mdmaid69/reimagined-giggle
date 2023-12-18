00dc669139c22dd97a6a2c8594e3fce8f1aea270
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:36 +0000

    appear model train float innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
