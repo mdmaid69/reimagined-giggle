72c6845620824ccdd6c53e6241c51382f653d600
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:59:48 +0000

    piece level runinto realize act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
