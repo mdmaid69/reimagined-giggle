654d87a4803b088c4e4f8bf3d1f376fb514064c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:44 +0000

    someone opposite scream do side

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def square_number(x):
+        return x**2
