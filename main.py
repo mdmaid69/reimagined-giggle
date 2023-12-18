3b1c72b1d010bd123deffbb3c84c143ac9d059e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:32 +0000

    unkind calloff promise otherwise interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
