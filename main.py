08be7b62d5ca4b2a927649b434fe5dccba492bd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:37 +0000

    nerve tool water flood impression

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
