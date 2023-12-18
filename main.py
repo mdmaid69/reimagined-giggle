96c1ed5eac1a07bddf994d0ed5e49c0e0f5b39f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:41 +0000

    kind nervous watch live another

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
