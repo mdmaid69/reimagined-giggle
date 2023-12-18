be86be3577469072009f14c6fd24b3ff23bad0c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:44 +0000

    far mysterious about show getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
