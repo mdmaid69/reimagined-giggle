c0774c85ee19b588067102ac0dd89e796e1bd037
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:11 +0000

    may building pupil spill unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
