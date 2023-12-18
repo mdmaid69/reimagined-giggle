5bfaeb0acd9b4956b631aa80c6148b65e6f91f99
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:49 +0000

    deep mistake wave official glad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def greet(name):
+        print(f"Hello, {name}!")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
