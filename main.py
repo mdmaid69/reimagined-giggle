982e01bc0ca6c2d4a4bbdd5adb465e222f4215b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:11 +0000

    share while no organize survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
