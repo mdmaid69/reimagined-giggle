91e089bfb83a64acbe5f46b3facdc6ce47de82c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:07 +0000

    manage square completely couple earn

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def clear_array(array):
-        array *= 0
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
