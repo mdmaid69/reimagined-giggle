d84669e72dea2dbf68ab3fb5e1670ce54680b392
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:54 +0000

    course wave system reserve pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
