f5f7289fc736385c31b9d3502c0605ee37e0d797
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:04 +0000

    capital cost goback(to) boil yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
