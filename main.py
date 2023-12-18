aeee1527f2ab9e53814fefe45222d93648156fee
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:38 +0000

    month polite drop service edge

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
