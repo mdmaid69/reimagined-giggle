cc68c6224a6ef2e6eefdbcc74fca49d0b6d457ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:11 +0000

    black strike imagine culture maintain

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
