e8904e8a0cbcc51899e2a6a6f35f0dd00eceaccd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:26 +0000

    copy force too drawing press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
