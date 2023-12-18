f649f92fd121adc8ae78cbea56ef4bac96ee8dad
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:36 +0000

    left breakin poor callout tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
