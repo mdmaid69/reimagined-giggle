b9edaf5ec40d950e7722142424a9d5640d4d5c3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:44:15 +0000

    571enough behind takeout involve kind

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def count_elements(lst):
+        return len(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
