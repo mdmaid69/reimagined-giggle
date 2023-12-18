db629d6a6a525cdab7bdefae77a9ba284f286bba
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:00:48 +0000

    side spare post start used

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
