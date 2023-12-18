bf3bd390d7a3fdee2bee1a1a4a3e01c99c18a881
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:00:26 +0000

    beat half sore tip takeaway

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
