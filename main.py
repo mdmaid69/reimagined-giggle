6b2eaa82685ee373e7937371d2e43ae3dd9c89c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:08:27 +0000

    foreign yes demand getback repair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
