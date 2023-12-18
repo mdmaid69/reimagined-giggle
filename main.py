c972063a5e0fbbb59cc5efc91bbd78740347f4af
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:09 +0000

    fall spill nose pride protect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def sort_list(lst):
+        return sorted(lst)
