4c4469dfdef7017e19b52b6cd8520620e8f471f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:55 +0000

    early show lost goaway fresh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
