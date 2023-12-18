027e00d4c50d301816b52cb67697804fca194753
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:48 +0000

    beginning break close welcome world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
