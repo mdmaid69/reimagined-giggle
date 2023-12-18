65452aef357f774415e8b7bf991a088a3b0c9913
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:06:49 +0000

    manner onto combination my confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
