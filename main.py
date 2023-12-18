e325bef1025cf5ee83f2c88405bf0eda698120e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:52 +0000

    date read clue arrival wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
+def cube_number(x):
+        return x**3
