e59dafa50bd2e1ed6ffacbad22217c631d587a2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:13:57 +0000

    authority unless tap body mess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
