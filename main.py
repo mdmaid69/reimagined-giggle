cf95fab9e0ceb6e5b80ae04207024a2b91e754c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:00 +0000

    mainly lookback(on) science learn occur

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
