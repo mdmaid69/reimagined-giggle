9bf96e412ecb24cc62776a09f086fef2025d73d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:01 +0000

    science mouth runinto understand port

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
