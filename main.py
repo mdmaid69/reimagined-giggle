e7cacda56ff0e60a078ff6845ac99cce1381c216
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:17:53 +0000

    plan wall problem date opposite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
