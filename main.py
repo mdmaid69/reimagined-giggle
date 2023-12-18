90f48f492a782105cc5bb88a66bbebb7da1fa91c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:44 +0000

    age Nn best report pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
