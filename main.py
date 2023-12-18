c415ebe66f5ebfafc69da90453ac221e5a3b4d87
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:23:13 +0000

    muscle hear less tie cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
