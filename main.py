f29579cbc96c218e1a232cf4542661a778115ec9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:33 +0000

    sight badly drive female bringdown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
