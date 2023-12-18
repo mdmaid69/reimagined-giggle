ca8018d1ee67328cb8d62fb6f0c259a8773666e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:28 +0000

    only home stand Bb large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
