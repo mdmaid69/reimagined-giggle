caac7fed1ae36e8360ccddd2cc75c52ded869128
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:13 +0000

    onto notice serve it intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
