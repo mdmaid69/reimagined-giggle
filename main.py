48f3b86881852cde8c4d397cf3fd0b511f3d2608
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:39 +0000

    treatment once 566end danger contain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
