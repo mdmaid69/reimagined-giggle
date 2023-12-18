7a7c5d4a1e3e4710be9c3acc8234a1a9c09147d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:35:58 +0000

    lean deaf everyone little principle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
