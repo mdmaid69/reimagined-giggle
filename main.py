828c64b9ccd36ea371277f2962c0f8a6bbec2b4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:52 +0000

    motion condition wish mainly well

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
