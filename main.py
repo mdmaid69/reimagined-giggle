2fa715774df0bd472aaa57eb0bb6f81383ae6e66
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:39:37 +0000

    noise forever experience noise proud

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
