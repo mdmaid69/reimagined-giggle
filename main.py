137cabdd72b95abcdf42d31b1b91b1e43a5cd99f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:31 +0000

    height book sign problem harm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
