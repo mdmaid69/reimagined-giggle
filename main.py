82c3549461d17127c35737fa2107aae2db340009
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:22 +0000

    doubt personal gate side thorough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import sys
+def exit_program():
+        sys.exit()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
