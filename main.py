e1aeb311cd561a6fd98f89e89164947b46b0187e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:50 +0000

    transparent direction land move season

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
