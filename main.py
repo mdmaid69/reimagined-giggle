de45c72f4469ec8e217c0afbb5731a654c086e65
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:17 +0000

    fat rush process read beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import platform
-def get_os_info():
-        return platform.uname()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
