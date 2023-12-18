8c4b3c1eb987a092e01c61b45783a4ca8956dee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:37 +0000

    normal successful back market sink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
