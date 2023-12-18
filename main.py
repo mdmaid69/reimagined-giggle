f3bec11a62a310b076ba709e7993d73f975b145b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:51 +0000

    regularly come quantity level public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+print([x**2 for x in range(10)])
