276b9213977e6b448c195e7be6934c168d4fbc3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:38 +0000

    debt right middle exciting goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
