bdc89f4d452f8065890829d5382a9b3644b09893
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:48 +0000

    want uncomfortable niggaacademy alive edge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
