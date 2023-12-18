45c551199b962f5335d288642e92ba68a92a5c20
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:50 +0000

    city uniform choose spray clue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
