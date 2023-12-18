b07000e9bd2085347d4b36007a0616d5f48bf6fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:32 +0000

    aim production confident total hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
