97ce14aa3bec6803c20be6038caf4ac0c1ac049d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:23:51 +0000

    guide related once workon sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
