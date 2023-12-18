515280e0b1d95496d9142f16ee57663ecf3fc9a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:31 +0000

    approve beautiful necessary tone geton

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
