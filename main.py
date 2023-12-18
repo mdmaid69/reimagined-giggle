2a2c61248cda5d6ac9ad8819a75706ab1d796fea
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:22 +0000

    bet tomorrow weak team try

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
