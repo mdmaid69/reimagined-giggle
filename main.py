283409703181ea5e246f850cb35f9928495fcd18
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:43:09 +0000

    black succeed time band whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def count_characters(sentence):
+        return len(sentence)
