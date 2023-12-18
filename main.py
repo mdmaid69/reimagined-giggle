be7e4b4718e08b68b2e60b26a1acdd4967e58dea
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:45:36 +0000

    zero draw vote worth 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
