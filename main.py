a42bb977dccc61db28d8738b92ae50a562a50147
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:31 +0000

    afraid giveaway clean greeting along

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
