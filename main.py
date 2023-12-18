e69557f072094427fb65cfa8ea0db988276c5efb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:25:00 +0000

    practice include never over nose

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
