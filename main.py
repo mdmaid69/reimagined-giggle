90197b29e0700ea8fb24b1b0170117719cbaf139
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:57 +0000

    together Kk start fortunately face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
