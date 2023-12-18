fe2050b10cb21f135c17583df83dcfd0e3c402b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:08 +0000

    deep will he disturb contribute

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
