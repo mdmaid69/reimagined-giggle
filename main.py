3e7c02134e59a965edd5f2279aa15e2faa5b2fa1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:37:19 +0000

    except plastic getout already opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
