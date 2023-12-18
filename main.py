c7de7b2546a3630ca399c7496cb6c239114768bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:21 +0000

    fold criticism turnout travel house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
