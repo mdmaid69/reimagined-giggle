dc0111776fab996a9baa9fd8f34a22427813c92f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:01 +0000

    star mind damp serious declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
