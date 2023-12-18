e73248efbbc51c7d2f370f0e08c1697be0bf3141
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:30 +0000

    wave from accident among beautiful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
