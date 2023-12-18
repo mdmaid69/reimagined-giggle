7713506747ae9a5b936c23152b88b6ef82618aa5
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:25:31 +0000

    strict people turnout waste immediately

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_itemsize(array):
+        return array.itemsize
