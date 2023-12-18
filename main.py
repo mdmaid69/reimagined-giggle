c502e3b1dcdaf88a3ac70f4464e6e508616dc86b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:33 +0000

    brush minimum bye total pin

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
