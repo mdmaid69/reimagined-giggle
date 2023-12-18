4ba519f8eddf764d00b85c4614a1197e70093244
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:03 +0000

    club food would absolutely money

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
