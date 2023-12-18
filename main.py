a57113dd62d6d0ab603c32603731d25cee413c63
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:09 +0000

    forever cover suit wear gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
