3e8aef190e6ec9df36023048c3505f84d10a32f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:02 +0000

    reaction swallow print actual turnoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
