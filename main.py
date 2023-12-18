ea3d8d75ed5ede1aedd6124d7d39ec33ecf2afaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:54 +0000

    admire shake lucky breakup steal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
