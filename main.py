65ab81274cf1dad935809c748307a319b71b3a73
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:23:37 +0000

    pay bringup answer disappear room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
