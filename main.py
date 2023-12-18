4b90ba434cebf152ce7b00f21ee3f58e53541eac
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:56 +0000

    dress target war nigga whoever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
