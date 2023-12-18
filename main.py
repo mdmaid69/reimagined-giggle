a90c3b8e2478c4cb733b33112cdc8c5c1285ee8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:59 +0000

    butter ask scrap sensitive cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
