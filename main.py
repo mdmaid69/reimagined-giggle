6d923751a07c20a180c6ded78fd6a22627fbe4e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:37 +0000

    crisis train stone probably father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
