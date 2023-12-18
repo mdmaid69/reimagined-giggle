1eaf8b03000e960105a4e858dd7047ee8f823840
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:37 +0000

    generous early reach fit getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
