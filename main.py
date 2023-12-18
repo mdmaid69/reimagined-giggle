9a491c950082e9e2d288687c5cb23ff919dc9633
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:02 +0000

    crime independent as next convinced

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
