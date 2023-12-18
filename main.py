624d0f9a1012feb46b32da0dc892d2aad52a5147
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:08 +0000

    offend government that wall true

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-print(sys.version)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
