53cbe88a60fa963309e046c0a2f9177b37038ae5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:31 +0000

    warn open middle comeround thing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
