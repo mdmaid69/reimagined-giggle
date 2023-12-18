c4197247caed393fed26e9f8785d5e9fde7260bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:47 +0000

    recommend pattern or sorry central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
