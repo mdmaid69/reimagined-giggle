69e08aff62f9f12de70a894fcc63715e1b138bdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:05 +0000

    danger wild talk sound accidentally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+name = "Python"
+print("Hello,", name)
