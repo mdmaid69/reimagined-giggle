75a1e2cf4a44b3fb1d18e77d912489e640257e3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:50:49 +0000

    middle damage recent middle beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
