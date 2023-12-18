06e717e0e7e3199b369b835c09107458389265e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:19 +0000

    foot spring grand week pretty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
