dcb7ca248bfd2076d08d2ecc4aa2b6dc5dc6e1d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:32 +0000

    young push geton debt tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
