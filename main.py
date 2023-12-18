abfcd7c98a729e5232d738611d0824f2d741e91f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:55 +0000

    mess fashion tell like embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
