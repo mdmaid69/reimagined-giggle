97dfd9e08627b2be4119b01362afe77ae832d149
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:18 +0000

    pleased drink brain holiday tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
