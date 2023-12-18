4c34d5e6bafc812cee7ea8b5cfc5ce05f12a55fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:48:56 +0000

    clearly distance ring peace complain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
