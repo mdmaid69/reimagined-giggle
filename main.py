d8618ab09fc61c9124c31842e6388aeaec9d2f5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:33 +0000

    accordingto arrive conclusion her delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def subtract_numbers(x, y):
+        return x - y
