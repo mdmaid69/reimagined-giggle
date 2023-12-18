d0441ffb564497a05c97001c1d3f401d662a45ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:42 +0000

    defeat bath match half concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
