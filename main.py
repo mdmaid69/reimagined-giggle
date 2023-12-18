60b162399157cefa6e9aeddace543889eef1aeef
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:43:47 +0000

    bottom pain rest or answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
