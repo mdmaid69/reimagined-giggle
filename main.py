153c8c4126a804d6b9d282b8cce7b9b20383a1bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:01 +0000

    opposite whether vote sure watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def square_number(x):
+        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
