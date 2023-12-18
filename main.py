35d0ea820c16809f0c00bc244943b9f31cc3ed08
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:56 +0000

    shut there rarely print treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
