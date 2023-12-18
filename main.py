3d113b2580b332e0c8a985587ad173d11d0a769f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:38:32 +0000

    tired reserve block trousers apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
