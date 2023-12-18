c67ea09f98895caac7358d31302be871e02b4357
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:05 +0000

    reject early low stare even

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
