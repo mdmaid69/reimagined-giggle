325d92a82dac2af48109e104350de03da15b2d7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:30 +0000

    cheat female inquire mix fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
