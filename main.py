197f3238ef9655805dd91f7d880098f42f2d6059
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:13:00 +0000

    warm Kk giveup moment imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
