572925c4b74064c9cfaba0dd7a3434e9c763a303
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:12 +0000

    see everywhere throw sale dear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def is_even(n):
+        return n % 2 == 0
