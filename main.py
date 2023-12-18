238cb58df5c0248667632aba6654eb574acd418e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:05 +0000

    fork science goup joke pool

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
