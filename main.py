2c5d2613fd01fd0d54fd7694e0150d3a7b8aae94
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:05 +0000

    gradually well sweep violence so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
