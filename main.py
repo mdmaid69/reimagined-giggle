8ed8a709dd7afebd6ab97e62db877defde1458c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:28 +0000

    water edge you practical worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def reverse_list(lst):
-        return lst[::-1]
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
