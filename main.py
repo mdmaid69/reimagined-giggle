c2be16ddf7c49b4e75694b86bfc05e52c3fd4422
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:40:16 +0000

    wish spare comeback unite nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def square_number(x):
-        return x**2
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
