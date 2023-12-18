b76d9e58a01cd447070b30e457f4e5607ded9cf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:32:27 +0000

    scrape disagree rush excuse kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+print(datetime.datetime.now())
