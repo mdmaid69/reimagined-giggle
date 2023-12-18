9364bfb9d174f0687fbcd7ffd8311c571671ddcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:35 +0000

    wealth defeat deliberate obtain common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
