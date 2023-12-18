10af6a223ff5127adc6dca90740008e76ba2acaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:11 +0000

    burn state generous rain street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
