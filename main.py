2988fb1ebb12b4f690af8a439c987bc76c69c63d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:07 +0000

    danger physical occasion large target

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
