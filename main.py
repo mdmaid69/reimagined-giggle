5be154732dd8ed54ad7dcf5af7d206a73bea2a69
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:32 +0000

    comment history unlucky little wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
