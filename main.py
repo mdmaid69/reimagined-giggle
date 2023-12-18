4d3e28273f0d94a8134d2067928c7ed91b94b956
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:50 +0000

    visit embarrassed home maybe to

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
