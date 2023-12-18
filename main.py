b02debfa071fb0521728c55695c33b1cde696ff7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:03 +0000

    miss dangerous consult exercise aware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
