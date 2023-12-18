7584b4d40dc1f541bcf5c355f40641cbabecca60
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:34 +0000

    damp nigga little promise part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
+  def convert_to_hex(n):
+        return hex(n)
