7615eaac1dfd0b286e9cfa3fa38b250ecd89faf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:45 +0000

    favour conduct already react definite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def add_numbers(a, b):
+        return a + b
