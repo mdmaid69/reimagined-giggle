b29bd971c67cd84539e303e408690e3ce51f0d05
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:49 +0000

    funny responsibility wear book apart

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
