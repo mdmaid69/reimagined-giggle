e83d06a977cb8858790e52a3ea844972f3ecf073
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:12 +0000

    how my pool price hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
