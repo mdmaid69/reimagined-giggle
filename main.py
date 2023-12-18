a4554a509a90b9188905757111ae1facf999c25a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:40 +0000

    remember smart ready loose drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_list_from_array(array):
+        return array.tolist()
