50cff36f778a8341957429036ed6e1b6d1b91db9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:11 +0000

    fun regional point maybe print

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def square_number(x):
+        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
