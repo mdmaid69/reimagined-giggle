cb6ba4bb914ce9606ec3344c53661e34a764caff
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:26 +0000

    fire company tool window picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
