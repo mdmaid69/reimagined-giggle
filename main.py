1d5ebb48135d463824ccc668e9dd4a6d36499331
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:37 +0000

    wander hot small excellent plain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_max(lst):
+        return max(lst)
