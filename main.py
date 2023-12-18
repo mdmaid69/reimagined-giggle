23695a536465fe16adfe79ff88bc6e1c7011de4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:37 +0000

    breakinto space judge blame country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def remove_duplicates(lst):
+        return list(set(lst))
