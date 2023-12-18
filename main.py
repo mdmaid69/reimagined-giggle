602facfa63911f68f49a982f9baaed3e3bf35f01
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:33:12 +0000

    past born whistle surprised maintain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import numpy as np
+print(np.array([1, 2, 3]))
