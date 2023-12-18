d379291c57be65f1636b1869249cb4f989f9aca3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:53 +0000

    holiday excited hit used real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
