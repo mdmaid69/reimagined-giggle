7c49f2f10c522031c329714e1691f69167a620b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:31 +0000

    imagine holdout welcome interview meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
