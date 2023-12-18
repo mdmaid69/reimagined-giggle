56b54dd12ee463fc38b30b28cb0575c989c54ff1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:47 +0000

    notice present defeat persuade telephone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
