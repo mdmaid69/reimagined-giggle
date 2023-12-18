a7af15e7abda2153fb2f56655bbb1432aa917b1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:00 +0000

    form protest little bridge claim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
