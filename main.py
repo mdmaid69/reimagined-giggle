c60ceacc435de19fd249213eafef6fc034b23666
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:53 +0000

    history with comeover mainly type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sys
+print(sys.version)
