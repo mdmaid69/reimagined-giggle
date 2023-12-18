6c2081d9b24e11822fd3893be2003e48008bc10d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:35:42 +0000

    careful basic towards organize turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
