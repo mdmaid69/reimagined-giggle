859ff081ca7ae58754f3d367338ec6a5b220717c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:07 +0000

    everyone scream moral anywhere necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
