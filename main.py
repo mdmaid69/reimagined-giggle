d02285e9248b5555fbbd9eeb8a56e4b5816dbb1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:38 +0000

    ill familiar immediately fence ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
