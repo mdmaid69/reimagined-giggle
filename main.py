9c44c06f097d8d58c1f7d8fe17992fc482554571
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:25 +0000

    realize hurt justice keepout formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
