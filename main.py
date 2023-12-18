c566c9989102ff72b8305cef3af0f05e4d140486
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:05:31 +0000

    murder dry normal shout whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
