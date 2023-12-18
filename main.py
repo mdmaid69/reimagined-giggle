6b7185b1ca48289e28b18504ca037cefd2bf8249
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:10 +0000

    quantity workupto base callup once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
