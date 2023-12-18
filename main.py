dfaf20281098d497af69af41955f20730ddedac4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:16 +0000

    mistake dangerous system suckme another

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
