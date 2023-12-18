7232ff1bf9f313f3efe484259120649b8b158d87
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:40 +0000

    too leg certainly choose operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def count_elements(lst):
-        return len(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
