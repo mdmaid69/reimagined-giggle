53da596016a267af1a2ff82bfd77d3e309aa202e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:06 +0000

    theirs main freeze butter turndown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
