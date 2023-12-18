ec2bc59f1c6105e0855c93b2baf579429c5a9de7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:37:39 +0000

    fill bit chat scrap look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
