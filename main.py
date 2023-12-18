b5a22dd20649c287f1ab245ccaece9fb7f789f59
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:15 +0000

    place century active worst possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import time
+print(time.time())
