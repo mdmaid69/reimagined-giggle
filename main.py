8ccd0c7b98f309e44fd91721570b9f33b6d281a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:59 +0000

    fall entry dress beneath able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
