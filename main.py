7988f6f9cd1f139ca837ea17f96ef2e24e68b79d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:25:33 +0000

    record steady runaway round ignore

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
