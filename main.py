f456678f9bf581d1e0f1b981add6d80256d5cc0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:29:33 +0000

    light week message total protect

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
