9b6da36a7d36ed47cf2c364303cd9bfd311c3b82
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:35 +0000

    lock nerve 572enter allow range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
