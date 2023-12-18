fe9229eb19f53a3dd0eff2995fe32e68edff378a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:36 +0000

    pleased giveback opponent slip underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
