2b1c26a22551a70fc046e18f5f46e9ebab1ec975
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:55:51 +0000

    tough picture so sticky thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
