b562cb46feef6e2f9772c16027ebdbf07c953063
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:32:01 +0000

    cough break object first commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
