793a7fe756935c2ac6dbd7aec876b502887e6e1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:35 +0000

    worst slice sit fly young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
