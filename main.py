617b9f16ecac5fac59826ca1736d07f1476995f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:04 +0000

    promise sound refuse spread holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def divide_numbers(x, y):
-        return x / y
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
