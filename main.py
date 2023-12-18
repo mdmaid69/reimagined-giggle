a902433e239eb84be81083b0feaed1833bd2fa5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:21 +0000

    tidy vision happy later even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
