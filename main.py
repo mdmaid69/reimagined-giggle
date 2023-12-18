8b7695a25f0b16e8f625f0a4cbb96705ea6ec0e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:21 +0000

    seat downstairs callout damage breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_bytes_from_array(array):
+        return array.tobytes()
