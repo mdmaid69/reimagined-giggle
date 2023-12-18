2f534a1d8cafe9fb06cb499084c489518928eb8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:24 +0000

    straight strong content fairly keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
