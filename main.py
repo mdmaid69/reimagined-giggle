9998d6b4fdb965a6486f2ae4f5544c3cd1c3ad32
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:57 +0000

    memory target runover trouble begin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
