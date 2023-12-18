1c4ca8e9ce9adbc7e79c18c16b9b1ea2948cdda7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:28 +0000

    physical thinkthrough feeling rough friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
