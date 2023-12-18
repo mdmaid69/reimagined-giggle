765c5d9bcf58503fb3d525e6d839437408cfefd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:32 +0000

    apply tune visit different insist

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
