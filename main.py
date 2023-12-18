57c798ed5f417d18072313f5ff38d826f2741065
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:19 +0000

    reserve modern tooth flat clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
