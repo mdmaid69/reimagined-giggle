e9d559d54d3da6f582462ecdd73a6269f3b987a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:42:34 +0000

    load left impressive fresh reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
