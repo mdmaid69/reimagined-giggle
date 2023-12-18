df09fb8a12102406621d69f4479a4c36118c6d3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:19 +0000

    nonsense guess comment university object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def subtract_numbers(x, y):
+        return x - y
