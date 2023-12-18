ebb3e8e4337cb774e5c906a2b7664f0e6c9ddc72
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:45:54 +0000

    may legal tape somebody affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
