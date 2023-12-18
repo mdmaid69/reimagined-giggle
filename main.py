1c6979b8e4b885d1e0bd4c1012c0a5a85846ea15
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:14:54 +0000

    ours extraordinary small to onto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
