c98281571a7e777cbf076e68e6f5ec95e4b1fcb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:17 +0000

    peace judge healthy play completely

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
