8aeef8f6962a32104eaa7b14c117aeea539f49f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:03 +0000

    after hot coat far fill

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
