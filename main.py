0f403bdfb8f8614e3562d5562664d9a44ac1971a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:59 +0000

    guard Ww shrink rest waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
