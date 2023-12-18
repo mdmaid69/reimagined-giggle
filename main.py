5b6ce7f6ccdf1ef44dbfcd69bc1f80ba00d5b2d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:29 +0000

    Ff stretch getoff wave sister

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
