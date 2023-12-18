740373d3c99b193219a5eaf5bc886763a0dd6bad
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:02:43 +0000

    rise far apply outside workupto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
