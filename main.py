01d88409a1c349c76b7a6846997c3a0678816adc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:16:10 +0000

    fun smell travel inside stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-name = "Python"
-print("Hello,", name)
+def calculate_density(mass, volume):
+        return mass / volume
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
