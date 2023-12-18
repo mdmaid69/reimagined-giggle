5390e5200a798d1f2e310820233dbb48e17b90a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:23:56 +0000

    giveout foot most bit storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
