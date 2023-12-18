0f12e490d1b5c5e3afe484171662b6f3617a049e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:15 +0000

    music much lookafter shape seriously

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import platform
+def get_os_info():
+        return platform.uname()
