396fcb46c87edc4dc5e787b985535e42732afa83
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:18 +0000

    tread side act shine time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
