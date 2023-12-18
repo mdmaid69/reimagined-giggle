b761d0bacd0e483a2ecc938cd75ab91c236ca3f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:16 +0000

    half passage attempt bother man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_set(array):
-        return set(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
