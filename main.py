f77b52a881633106027e02e31f703483fdf4db25
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:50 +0000

    serious either job further today

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_current_time():
+        return time.ctime()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
