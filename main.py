dd03e1f51b3fe31b43d236cc8df82c230bf486f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:10:29 +0000

    nothing build last surprise partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
