200b816f350343f957c9f8077a6ed475b8e6cef3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:10:55 +0000

    either either order effect bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
