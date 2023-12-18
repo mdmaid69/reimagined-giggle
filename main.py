f4a1a8cc91044d1c1d450e0bb2c02ddb99236f59
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:16:08 +0000

    share situation corner family scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
 import array
 def append_to_array(array, item):
         array.append(item)
