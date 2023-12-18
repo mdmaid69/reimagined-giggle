c4835d83b673b793b030351a6c152f6f74f879ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:18:58 +0000

    scrape Itself speech fetch underneath

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_user_string():
+        return collections.UserString()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
