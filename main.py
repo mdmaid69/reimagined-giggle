b81eb51290c161da8ccf5f628dbcfbdfe253ac22
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:55 +0000

    holdup neither correct load listen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
