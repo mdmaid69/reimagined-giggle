ed377c1e2a565022c170e2a1df0e3f795344f473
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:52 +0000

    drawing write care reply considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
