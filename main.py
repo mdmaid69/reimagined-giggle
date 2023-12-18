23b810d0d4db0d2f8468e5792922660e23d6f4e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:55 +0000

    unfortunate contrast method couple course

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
