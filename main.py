c57a7119692440d51535e272b01e57f51d1e41c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:12 +0000

    call busy giveback truth keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
