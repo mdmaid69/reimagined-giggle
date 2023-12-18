09f195f7d10ff4168a8caa4fb3966da1b4e54dd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:06 +0000

    play nature crash busy past

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_pressure(force, area):
+        return force / area
