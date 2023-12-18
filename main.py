87f4fe61c2320ffabe9276af32a5ee541bd30905
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:06 +0000

    violent shopping disappointed knowledge row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
