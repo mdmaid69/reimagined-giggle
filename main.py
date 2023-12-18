3dcd53fbdb4933dad95da896b244a4aa20dcb153
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:51 +0000

    accommodation hungry practise brush cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
