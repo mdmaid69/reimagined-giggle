b17630abb4927dc725cbce9ae53fab1c68ff3d59
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:43 +0000

    most proper platform lookafter weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
