e88a293088c0a71fbe7221489327f2c84c206eb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:48 +0000

    unless convince pride distant regret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
