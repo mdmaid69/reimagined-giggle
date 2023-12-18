720f44511ffa2584a8044963d44689415926d1a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:04:09 +0000

    stop yesterday care treat means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
