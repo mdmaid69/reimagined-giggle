04660ebb65bb4ddf2ed303b085d9ac23deb290fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:40:51 +0000

    correction trap own among error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
