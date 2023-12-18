6cfb0458ef84103ddf13aee2a8da186d0d545c76
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:52 +0000

    undo foot row handle perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
