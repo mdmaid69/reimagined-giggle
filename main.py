b52ace6df1e7528f40cb13135cc09408ae11e7f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:23 +0000

    married much genuine last acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
