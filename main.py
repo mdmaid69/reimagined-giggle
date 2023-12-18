497df20a6f1c05ff7dc9e5355c11497a4c47f2f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:26:54 +0000

    division pretend push new fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
