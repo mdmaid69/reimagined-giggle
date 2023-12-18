8dbd2a96833c6b6ecfa30ddcdd1361e83ca6b713
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:07 +0000

    method Uu direct chase interested

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
