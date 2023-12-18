0c3576634d299f08c78c85d0c1273462182bb240
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:26:12 +0000

    demonstrate shop offer traffic house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+print(sum(range(10)))
