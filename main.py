1b46f61aa1788f4df1553483409328b35b506bb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:10 +0000

    meaning direct after moral land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
