1c83eb35cdea0916c18ed4b4c49077841d9ca759
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:03 +0000

    inside direct neither mark active

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
