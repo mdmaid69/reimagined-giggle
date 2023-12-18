e97fdc257a4112eac0bb2072bc54ce4f8867d82c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:37 +0000

    measure degree getback discovery 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def create_user_list():
-        return collections.UserList()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
