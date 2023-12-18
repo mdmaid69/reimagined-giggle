a7859f92274ef59ee0212e4bf319fe5087c1b604
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:46 +0000

    ear term neck hour set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
