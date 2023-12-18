efab4d9de217b5130a803354daf9154bebc6e5e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:35:08 +0000

    again value big cost set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def clear_array(array):
-        array *= 0
+def get_bytes_from_array(array):
+        return array.tobytes()
