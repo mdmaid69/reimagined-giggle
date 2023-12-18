beac7734a87d35160ca11670cb2e20abc655e2cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:13 +0000

    any freedom confidence waste correction

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
