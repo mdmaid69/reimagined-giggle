50a957bbae3dcbd13addfdbb806220d5f331fcdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:13 +0000

    situation maintain tongue its seat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def count_elements(lst):
+        return len(lst)
