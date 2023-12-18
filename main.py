80cddad17cceb5199062b51ca936696ae82a2eda
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:24 +0000

    dig cum holiday force waste

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-print("Hello, world!")
+text = "Hello, world!"
+print("Characters:", len(text))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
