3f516f2d1b5f62979009132d1e66c0a90fdc7d1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:34 +0000

    second accidental do break likely

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
