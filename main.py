87c8f84fad4fb737f253969aa72eb713f7340ed9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:59 +0000

    dance miss doubt probably wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
