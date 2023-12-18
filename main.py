90f5ba386a437b4552aa6367f4fcc8b462cdcce7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:32 +0000

    bread fork office behind part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def square_number(x):
+        return x**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
