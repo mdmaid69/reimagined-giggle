6c412013382093e7ab5ef14815e851ad8ecab499
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:23 +0000

    positive formal Hh drop shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
