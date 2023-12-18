3333cc08df1015d080cd0ca7db20a7d610aae880
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:54:08 +0000

    deserve unable murder argument open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+print(sys.version)
