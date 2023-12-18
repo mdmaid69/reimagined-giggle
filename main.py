042a8d04c36a43c5f4e59b30d259733fe4235a51
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:43 +0000

    sink switch right spin hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
