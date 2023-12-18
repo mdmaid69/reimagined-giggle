bbfac8edd131996eb10059629a3da735c44e1999
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:24 +0000

    clothes narrow police thinkover flat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_from_file(filename, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.frombytes(bytes)
         return a
-def calculate_area_rectangle(l, w):
-        return l * w
