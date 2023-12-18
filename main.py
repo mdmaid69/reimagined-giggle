9fd5fd21f10f001a26036b1426146b2681e6f760
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:00 +0000

    read careless street lot press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
