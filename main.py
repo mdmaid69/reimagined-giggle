415d7bf3679aeffcda017557fe1d02dba2112b6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:51:55 +0000

    disgusting other rid ticket cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
