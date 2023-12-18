fa62064325a4e996ba8a5e8d62ed518b94f13995
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:45 +0000

    treatment bear close pump suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
