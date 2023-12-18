7f3880f36a33483273de00ea09d75459e87fb48f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:02 +0000

    like wing guide whether reduce

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
