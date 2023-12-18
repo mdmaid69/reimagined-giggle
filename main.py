1fceee480f2a2e883cb951ee2ae77460589af34f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:53 +0000

    lost neat respect die reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_odd(n):
-        return n % 2 != 0
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
