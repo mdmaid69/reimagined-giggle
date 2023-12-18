259c7677b38a6962c4ed9bae965e82ea5fd3820a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:24:12 +0000

    top somewhere morning 564encourage slightly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
