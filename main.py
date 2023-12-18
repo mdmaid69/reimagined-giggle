e5f9821fb84642ec2ea6674bbe4298bf8f303223
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:09 +0000

    almost recovery embarrassed 565end change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
