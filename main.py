f13a328c8d0bfc04ea25d7e755fd2a4858ea5301
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:09 +0000

    finger underneath table runover brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
