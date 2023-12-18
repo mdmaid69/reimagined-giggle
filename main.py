e7431411726c67d01d221fbe8b5f2eb4167aa5b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:20 +0000

    oil base dark common business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
