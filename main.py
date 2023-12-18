f1a4a761c8ed2fd3ee846bb6b70c6acd613dec5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:52:03 +0000

    bang object spring necessary OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
