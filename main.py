60287bb7ee05d24ea78aca83b1ce135d0c0e46fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:36 +0000

    interview free once attempt intelligent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def reverse_list(lst):
+        return lst[::-1]
+for i in range(5):
+        print(i)
