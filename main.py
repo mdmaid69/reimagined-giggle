1785036a6ba21e7d4e55e19d123419dba6d62b22
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:39 +0000

    old moral declare its shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
