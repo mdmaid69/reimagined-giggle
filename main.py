20eca48ed956f9f3442a911c748f0d482c0e0485
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:06 +0000

    ordinary Hh save film glad

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
