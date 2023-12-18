0ac1ef8bd0e68f087b2666499ce8fba0c4b35777
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:23 +0000

    exhibition finger dress set physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
