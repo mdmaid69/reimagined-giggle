a10656c835617ee34f7c97204c8973010e6143ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:51 +0000

    degree work congratulate brown smart

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
