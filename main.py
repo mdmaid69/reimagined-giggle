f78e00e66017117eef367ceb99c98558269f5e61
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:41:09 +0000

    weak innocent finish health really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
