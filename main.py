d9761f3632afbc9506db062feec93649922fb050
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:57 +0000

    better course discover favourite tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
