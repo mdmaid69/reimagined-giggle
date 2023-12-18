e461f6ab6aaf219bafb4b7f103a35256c958e419
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:10 +0000

    object comment food opponent hello

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
