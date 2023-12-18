45ee2f58212b986a051009fbb8df404da5bb5e98
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:12 +0000

    smell nose inside wave join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
