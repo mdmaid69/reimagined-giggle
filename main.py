eafd9d920660200514df9376ba4ffb01de759b0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:32 +0000

    single mind couple good programme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
