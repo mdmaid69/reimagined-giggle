1a240166ad11d8d1304d962ea7c997fc7235e315
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:52 +0000

    block provide solve themselves gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_list(array):
+        return list(array)
+def is_palindrome(s):
+        return s == s[::-1]
