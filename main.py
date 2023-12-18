f84f0f8edcf145caccd4ac619e7e624d85e3df02
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:35:42 +0000

    pipe realize forever consider capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
