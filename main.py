49537cb4e42089c3c55e6c4b099f1a7056a312c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:55 +0000

    result weak cope challenge snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
