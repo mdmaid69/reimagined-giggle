3ba17439a44690f43a0ad2f1268487776e888ae3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:18:29 +0000

    embarrassing accident available open smoke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
