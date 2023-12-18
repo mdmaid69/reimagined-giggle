7705306ee4730ee4abf1cbe672d55c26243741cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:01:58 +0000

    lookafter bush whole above holdup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
