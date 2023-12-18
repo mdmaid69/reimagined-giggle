3aca5866091a2dd2f0703b3f70bf84216836c34f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:41 +0000

    line Bb unlikely dead mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
