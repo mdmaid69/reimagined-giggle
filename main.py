24b04c4b54f0354dc5b9f8f8ae991042cb60b9dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:14 +0000

    typical port either active reasonable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
