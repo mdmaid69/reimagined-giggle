c6c74c79bf6a3331a9f883611238880a41be754c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:04 +0000

    genuine able likely add leg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
