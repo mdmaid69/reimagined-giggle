0556104d85bf8fa0e34b63fb915a55a6764d353b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:41 +0000

    thought error shadow related bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
