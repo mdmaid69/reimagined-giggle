7b0806ffef56ee94ba010bc8b96d5e7ece9a7d6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:21 +0000

    post brush congratulations brave must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
