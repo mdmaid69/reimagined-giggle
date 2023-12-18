efb8b5892e7a7cae7aed35ba4490e13e3d17b6c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:54 +0000

    environment giveout judge distant confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def is_palindrome(s):
+        return s == s[::-1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
