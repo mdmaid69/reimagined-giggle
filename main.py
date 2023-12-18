c694994e31af787f82078291bb19a21d61acd1ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:18 +0000

    command good shop accident regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
