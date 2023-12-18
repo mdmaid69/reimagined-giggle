e0a4d69e2a933cd91a318fa8d65a567effac0628
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:25 +0000

    rare though bush appeal relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
