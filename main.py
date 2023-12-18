10bb908fe6428c50b1acfb09c452ccea8fc4ff66
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:35 +0000

    exam weekend practice sensible deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
+        return l * w
