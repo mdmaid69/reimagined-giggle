57d9bebee56328d9e6d6b7c57c64e5a01fb01f70
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:50:35 +0000

    goback(to) shopping depend bang priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
