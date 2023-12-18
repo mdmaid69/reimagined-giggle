0f68fee9d93e24159961af96323c039693d93245
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:23 +0000

    forgive drink finally she breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
