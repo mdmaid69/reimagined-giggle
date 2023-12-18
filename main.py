e16350544dec01fa2d0d9e852a3c91ec23f06143
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:34 +0000

    morning music false tax determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
