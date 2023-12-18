4a02282896d4b56c06e40017b6e3aa98f2fb1bab
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:30 +0000

    careless serve nose sort performance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+def find_min(lst):
+        return min(lst)
