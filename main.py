96c9e0ad94f84280e6cd7b0d7fca61537e8d6926
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:35 +0000

    turn impressed plain club worry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
