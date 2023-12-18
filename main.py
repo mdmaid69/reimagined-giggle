0a63fba4beeb1fd5bc48c5682bcf21a9ce0c7269
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:35:07 +0000

    ilovedicks fit protect tell achievement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
