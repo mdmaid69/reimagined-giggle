ce06ab87d3700921752116fddd416ee83f83e78f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:32 +0000

    pretend fix spoil female admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
