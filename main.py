935d2e0c3d16dba943451d55a0c8f5c17639e412
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:55 +0000

    face catch hot wide polite

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
