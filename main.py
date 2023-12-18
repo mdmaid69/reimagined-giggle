e67c28f3308b4ce4861766d87e369db2b8a940da
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:33:05 +0000

    ours several know immediately careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
