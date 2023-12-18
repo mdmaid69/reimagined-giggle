8b1bd35574b0ee1685b1f011dba484fdd75e971c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:19 +0000

    right criticize scene yet shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
