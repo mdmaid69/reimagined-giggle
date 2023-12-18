bf977b2b6952fa7549b5bb4becffcd260d056a4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:36:41 +0000

    under long quiet regional opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
