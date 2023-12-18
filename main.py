bd1999dd3574b454486268b50d44f51fc9201f8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:55 +0000

    tune smile formal here point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
