fcb51b895e26693e7c2e12995a5a609db1675c57
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:57 +0000

    zero sweep last block laugh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
