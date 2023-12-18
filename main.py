31401aa329938244614c7667b27e358917e6d081
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:52 +0000

    month concerning frequent figure group

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
