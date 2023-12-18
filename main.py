b2ad5919ac75d225fadfaf8c5ee320232b951a9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:14 +0000

    last person touch star whenever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   def multiply_numbers(x, y):
         return x * y
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
