3b9528420ef5d42ec3bb9e55a4b4973dd12da9ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:13 +0000

    considering exit unite beat moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
