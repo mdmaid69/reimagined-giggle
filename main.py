4a0e5a9fdc1f7f42ac7b1f2acc8e86d8f0848e20
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:21 +0000

    lead prevent close contrast moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
