2c2d765275729b710380bca825e6dfd162dff1a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:25 +0000

    pollution admit alive persuade punish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+print(math.pi)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
