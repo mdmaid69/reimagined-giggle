7b3ed577972521bfe3cd0d393e753b03a7f74542
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:30:59 +0000

    backwards offend willing our vision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_speed(distance, time):
+        return distance / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
