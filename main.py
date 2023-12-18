9a9927204f61997b43ea2fa4f651a8ba9f0838c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:13 +0000

    hello Bb ashamed finish dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+for i in range(5):
+        print(i)
