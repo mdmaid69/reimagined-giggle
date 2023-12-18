43e406f56eb15f5d3c938e868b991d58bbdb66f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:06 +0000

    confusing game contain chief money

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
