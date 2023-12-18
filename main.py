6bf612e03616464d92920dd0f42ae70b77b9b174
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:19 +0000

    report turnon exciting morning measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
