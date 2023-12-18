bafc9c19c71587759aac8a7811eaf846ee633474
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:55 +0000

    Ss human unhappy scale family

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def square_number(x):
+        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
