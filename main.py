d7515af007fe3435b3ebe1794f0c40dde3e83663
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:44 +0000

    question high himself reject fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
