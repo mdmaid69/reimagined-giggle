d6715e4293453e9355ab1d829aa771201eac1515
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:23 +0000

    turndown forget wing big guide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+print(math.pi)
