7570447ab56f0835223f22f6430033a5c65caac2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:05 +0000

    needle rain especially public anything

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+def find_max(numbers):
+        return max(numbers)
