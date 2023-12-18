ee30695a9251c0e413ba8afe171cff1e82b4b3f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:29 +0000

    afterwards office few pressure really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def divide_numbers(x, y):
+        return x / y
