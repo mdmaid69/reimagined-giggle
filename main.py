67f2a3eb6b416af1b4aa77b705e6141e3a58574f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:26 +0000

    pass stage scare test knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
