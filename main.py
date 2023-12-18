fcba2cd05d42a06495b0eb3be47f87f6f84cabbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:41 +0000

    further store upwards intend turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
