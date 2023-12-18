b879998a40225011f390efe8077575c59cb6f785
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:17 +0000

    wherever report refuse so consult

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
