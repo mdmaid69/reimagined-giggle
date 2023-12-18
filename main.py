06fdb6b2a0b52617e28536e59c104f7a6ca09e93
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:29 +0000

    event bottom sea give contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
