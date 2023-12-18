b1f541038df1f8c5940a5fa130392ca1ae24e3b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:30:43 +0000

    raw fix learn generally shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def square_number(x):
+        return x**2
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
