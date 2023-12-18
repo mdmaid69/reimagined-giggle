0d977c131ef714512be00cf9ba13d33e56acd4f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:35:00 +0000

    letter present to odd who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
