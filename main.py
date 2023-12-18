b7998909492c65af493397c01fe7f80ac284a0f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:51 +0000

    nerve frame secret bath report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def is_even(n):
-        return n % 2 == 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
