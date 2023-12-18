67e010bec6d410eb2e801873ef27b1f2cd7425dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:24 +0000

    hurt so comein callup reserve

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
