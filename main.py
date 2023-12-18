5ff1d585686cb7b051e916dbf6928234044b2b2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:58 +0000

    getup card underneath Kk tie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
