90f049b8b554bf47900a296ac79dd814dc94edc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:31 +0000

    spare try rather smell order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-for i in range(10): print(i)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
