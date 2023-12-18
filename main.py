1bc7e5ace2205ed9c83aef4b85d6d5d21e4d4c3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:37 +0000

    number certainly slowly keep aim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
