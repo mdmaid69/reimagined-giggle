acd67236f79713ad28adda8b2cb568aeceed8af5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:44 +0000

    ashamed actual final waste right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
