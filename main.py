4da5c97563154f9e6e037ae7b6fdf6f03078048f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:59 +0000

    problem justice vote rent rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
