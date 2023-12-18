eb4cf19ac582299226aa3b4d89ad655d64756222
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:35 +0000

    familiar sport tidy spot needle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
