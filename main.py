b3b20424fd7a60710fa6d68e2f352f013da0b0bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:44 +0000

    past whatever contact recover personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
