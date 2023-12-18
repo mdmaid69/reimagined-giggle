3bd156042701affde7e542ceec75f6c4d801a15e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:31 +0000

    storm flavour thorough happen tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
