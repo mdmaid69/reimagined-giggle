764595b84391ff9f3655e42732dbe64c1ff0d3dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:57 +0000

    danger post will greeting wrong

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def subtract_numbers(x, y):
+        return x - y
+x = 10
+y = 20
+print("Sum:", x + y)
