0fede3ae237258b2f68e84fee0cbc03272cfd92e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:12 +0000

    future lunch tired fact hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_max(lst):
-        return max(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
