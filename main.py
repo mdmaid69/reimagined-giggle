e3ab7e5001fca4a337b6a5e75ce78176fad42a50
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:46 +0000

    lucky fine about ahead band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_max(numbers):
+        return max(numbers)
