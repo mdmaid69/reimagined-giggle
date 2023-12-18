0116e519e7547fd635bbf316dc0d08609e2da1d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:28:22 +0000

    bringup either include far plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def add_numbers(a, b):
+        return a + b
