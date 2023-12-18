995604560e3196cf08ee53cac315a6134c489618
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:35:13 +0000

    address bridge sick bread sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
