cc8b6a008027aff1f08e862681e1c9a91ccf317b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:23 +0000

    responsible again remember bet Rr

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
