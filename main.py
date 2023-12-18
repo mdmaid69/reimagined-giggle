3496ef55273e7f7c69e6a2ac46d8c49eff1af659
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:12 +0000

    ever runover standard Ll acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
