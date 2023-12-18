18e1fd9c744f2372c823025e255bec927eba1403
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:11 +0000

    small rise dead street note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
