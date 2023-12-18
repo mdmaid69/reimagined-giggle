b1e85c780f3a8bef9f68f7d2353d4ce01f5870f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:18 +0000

    cheek his brother view politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
