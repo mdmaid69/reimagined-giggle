729e84a236982f90576d12d16de3edee2ff4c390
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:32:12 +0000

    discussion embarrassing along thinkthrough university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
