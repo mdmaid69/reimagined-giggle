eeb18aa100094023fb02caa285ef176049e117a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:40 +0000

    please home style upstairs obtain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
