4b1ed458f5b4388dc78e979995b06c3e8f473c3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:35 +0000

    outside lonely instant tap substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import re
 print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
