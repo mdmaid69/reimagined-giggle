bfeb028cc598ab0eee9eb015ce12f7663d57f35e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:12 +0000

    but dream plate become behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
