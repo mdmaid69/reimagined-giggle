efb2d052263a451a3f8f3b55683c9acd63a12eca
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:00 +0000

    defeat travel expression unless order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
