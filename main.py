5596361bffcaa6bf22f0171050176f782104af3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:29 +0000

    continue miss recognize Ii join

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
