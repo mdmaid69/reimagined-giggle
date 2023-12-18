6780b0912155d2e33bfe116c281ce9d0c85d8fe3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:15 +0000

    deep back pay prize lead

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
