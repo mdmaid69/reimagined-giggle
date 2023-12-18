e787ba2545aadfdde0a1b0b1b8774773073c91a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:26 +0000

    divide offend train concerned question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
