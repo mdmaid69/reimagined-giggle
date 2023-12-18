758d2757a970d0974db3b75e26d9da9c77b3a7e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:51 +0000

    away take invite backwards up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def reverse_string(s):
+        return s[::-1]
