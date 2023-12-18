3929609509f15ea8e108f873ea6b12ce145460db
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:35 +0000

    quick goodbye cover dare straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
