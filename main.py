bbacb0de1a22bf52f445e4fe78a430cf0487f578
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:19 +0000

    slice lie contribute learn so

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
