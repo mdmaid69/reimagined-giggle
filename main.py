6457a8cf44f7ac2691f71fee58a97056422da6de
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:49 +0000

    boat play sleep shade passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
