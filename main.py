2b0e591a318e9d56b87e94de2209974a4d140f6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:34 +0000

    otherwise address squeeze manage argue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
