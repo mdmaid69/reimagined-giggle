3cc8197116a397c00fb56bef5e51c58fc10dc1cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:40 +0000

    565end brush nor weekend poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def sort_list(lst):
+        return sorted(lst)
