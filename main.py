f386d910a264d9ac56574118daad85ee543e86c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:42 +0000

    generally punishment arrange should second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
