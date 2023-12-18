736244f78c259ac51b955704d7f3b83cc2628010
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:35 +0000

    name because so float unusual

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_pressure(force, area):
-        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
