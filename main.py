4ebde6e44a49d288d9ccf3ed85bb2e0d4d76febd
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:08 +0000

    successful disappear squash drive willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
