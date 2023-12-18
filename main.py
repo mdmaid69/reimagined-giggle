74e0a2a967a77bc09c8e8a4b987bad84edb4c441
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:54:43 +0000

    level keepoff second wild shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
