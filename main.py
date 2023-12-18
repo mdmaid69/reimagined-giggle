c3b775fc479f42861e63ee9406c94b9adbde96fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:25 +0000

    close environment content waste distant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
