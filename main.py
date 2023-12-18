e0d85627fb2933e88d84b41b7848a61be682712a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:34 +0000

    discovery stamp action temperature estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
