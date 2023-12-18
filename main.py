05689e983957b049a63cba5db77d9f2ff6cd585d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:18:17 +0000

    friendly goon identify illness stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
