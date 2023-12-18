7c810e76f7bfd0a2c896fc3d90e168e337d64539
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:01 +0000

    wonder brain fence belong class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
