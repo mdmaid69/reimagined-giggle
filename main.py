d60b26ccd7269f5e682d1051aeea246daec063a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:44 +0000

    agreement calm plenty shopping take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
