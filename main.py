e366d45337dc51c9e80d8507a47205ecbe96f051
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:42 +0000

    arrive wing doubt share visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+text = "Hello, world!"
+print("Words:", len(text.split()))
