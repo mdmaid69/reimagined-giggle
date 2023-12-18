c66a0a469c92d574a327cb2705cebda6ee3c3907
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:40 +0000

    from secret makeinto behaviour bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
