68c7dafb9c855d6636db0f5f019786d845e5a25c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:32 +0000

    full improve fit willing keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
