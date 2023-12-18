308f3195e44b87b11575290bca94025f2e92a1fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:23 +0000

    flavour attitude Ii glad proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_power(work, time):
+        return work / time
