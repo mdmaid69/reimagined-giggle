4dfc5937ba8b4a75dd06ac16bb7aab87f15d56c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:52 +0000

    straight juice bend pass exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_density(mass, volume):
+        return mass / volume
