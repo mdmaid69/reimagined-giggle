e4a3f4f52ec97a168a78b7395276402ac3ce2633
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:39 +0000

    satisfied issue pronunciation catch impressive

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
