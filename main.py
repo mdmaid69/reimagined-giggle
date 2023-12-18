1e4e46fad7665be0136fe3cef080016fdfde5343
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:28 +0000

    later survive contact teach can

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_min(numbers):
+        return min(numbers)
