69503e4c2998bd094fca91e337064caf46ad38ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:54 +0000

    play cool plastic about agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
