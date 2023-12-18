8acfb09a2b644c21dca569c0c64c1b8315f25f51
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:22 +0000

    neither ring settle Itself crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
