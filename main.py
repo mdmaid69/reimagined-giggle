dae38fd06b272e9a6877a2ccbbe496c61d7c4d5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:08 +0000

    funny brief accept joke frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
