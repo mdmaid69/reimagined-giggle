ec0876bb63237bb52500e7cc4c2b3cded3641e2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:11:26 +0000

    turnover error measure scare thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
