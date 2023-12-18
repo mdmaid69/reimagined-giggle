ee6569ac33e29f26e88c879ab171e8d92da2b3f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:01:21 +0000

    bound accidental interested stretch search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
