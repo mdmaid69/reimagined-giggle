8fbe6a2b3f1143701b7a7e4b47db104abdf7f13d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:03 +0000

    evening can during silent behaviour

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import time
+def get_time_since_epoch():
+        return time.time()
