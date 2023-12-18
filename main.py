4b5185c7739d601867fab93769a837ad20014e65
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:10 +0000

    other stick fall furniture him

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
