678d8888fb30580954f0190af9752d5706d940e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:05 +0000

    bring simple 565end mark deliver

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def append_to_array(array, item):
-        array.append(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
