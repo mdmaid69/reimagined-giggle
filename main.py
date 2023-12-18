689c7f357f0d7e471fd188db6aa2b39d7765a013
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:31 +0000

    worst care theirs surprise people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
 def calculate_circle_area(radius):
         return math.pi * radius**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
