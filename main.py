b0b1f3e47c96692f8d14c9215bb6ba6325555a9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:09 +0000

    involve line race dangerous flat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
