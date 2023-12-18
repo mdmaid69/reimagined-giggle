ed1fc0622c565345d9d7abbc0ce454a3818ff9b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:45 +0000

    holdup blow dry injury object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_os_info():
+        return platform.uname()
