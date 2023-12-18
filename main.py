b83b14963c24d398e779609f0ba4ef7e0ad11edf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:12 +0000

    amount surprised over production permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
