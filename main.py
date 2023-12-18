0c4d3cb4b82207fb0734f5031e1ed5e6d13d39c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:04 +0000

    shelter adjective edge strange knock

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+{body}")
