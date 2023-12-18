bf62af21c606ee42060d5d349d11513e693dd76b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:35 +0000

    burst the return unable compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
