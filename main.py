aa0d6f3eb5398346d539f94228180ff7ac3dd34d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:39 +0000

    flat busy act table underneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import time
+def get_time_since_epoch():
+        return time.time()
