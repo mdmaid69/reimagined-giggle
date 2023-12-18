bcd16e861bca72726b0fa07a56230a972b207d91
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:54 +0000

    vanish list sweep leg leave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
