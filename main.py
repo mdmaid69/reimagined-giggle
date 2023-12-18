d6c53ced3c0393fed0da40f0d311927bdd65e59f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:13 +0000

    somebody who hide store hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
