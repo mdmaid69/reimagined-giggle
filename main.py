28fa54cafaa6521d6fd0c672e6fd4245bf97b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:45 +0000

    result search building return sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
