e6feef039d09b9881f56d8e44834f065a95cd6c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:23 +0000

    move brown sign natural grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
