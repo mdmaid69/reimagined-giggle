ac02d5bb76b77b2b89a8b300da3ef51edbf9998a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:38:22 +0000

    length thank time wide 568energy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
