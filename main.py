c6eae5a79b6e95388c8f2582dcec3c70955e767f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:19 +0000

    impressive reserve considering yet pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+import collections
+def create_user_list():
+        return collections.UserList()
