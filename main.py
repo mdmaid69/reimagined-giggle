d2a4c3f80e1637c94e56e11de7081bfa54deb624
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:22 +0000

    wrong shelter play thorough hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
