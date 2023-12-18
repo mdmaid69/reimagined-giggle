c84c8194ea7d37ea7e7756112f6982df5010bf95
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:43 +0000

    noise gap answer miss correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
