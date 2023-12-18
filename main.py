a27e03eb0b1cc96f1d36d4ccbd4e3272731de45c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:00 +0000

    office have friendly shelter hate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
