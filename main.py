ecb0e5755307360c39a38e2699dc3d79a4b36542
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:28 +0000

    bet powerful zero notice child

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
