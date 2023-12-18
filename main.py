4c7b9a49a82f83e681316cd2738a551bdfcf82be
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:15 +0000

    receive need anything unite belief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
