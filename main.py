e99f56f588633fc34c0df908cc371952f70e0a00
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:41 +0000

    ignore clean its chin under

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_binary(n):
+        return bin(n)
