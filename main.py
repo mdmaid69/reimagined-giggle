45dc7b23c8c3adbf959dd1902fb22a278b403058
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:47:06 +0000

    board precise goodbye light afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import platform
+def get_python_version():
+        return platform.python_version()
 import random
 def shuffle_list(my_list):
         random.shuffle(my_list)
