943db385394af42f456ef6e47ab948e9572df7da
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:14:57 +0000

    whoever before file order remember

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
