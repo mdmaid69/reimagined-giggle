22da9edeb8370ef32a190ee2502cab45bebdffdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:49:55 +0000

    comeup themselves smart tongue see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
