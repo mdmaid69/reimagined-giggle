64f5b0dd6546457e88fc5ab02e73d31a651fac5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:50 +0000

    law rule block culture married

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
