2619a11d5226896adedc50f4bcac84f1e85c7396
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:53:58 +0000

    television somebody left value extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import tensorflow as tf
+print(tf.__version__)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
