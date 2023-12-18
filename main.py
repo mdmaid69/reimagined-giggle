9bee8b371303a738969d5153d1d9ace6fdefde9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:17 +0000

    stretch roll much could refer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
