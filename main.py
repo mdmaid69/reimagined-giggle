d0a45b5c4c87955a7e989d2963dfad4b9cb88db0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:34:36 +0000

    star ask suddenly may Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
