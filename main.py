cc3523f3a85d186cfb91ef0ce013a9cfa9eaf507
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:35 +0000

    fierce beside light rob row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
