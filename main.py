c0ad50676a8e710e315ed2d34f58e50ad64ba221
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:18 +0000

    fill bye phone branch passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
