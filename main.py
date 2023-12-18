1f1c74cb3766b01de7f94fc92bc10e8be27f7123
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:05 +0000

    apart name listenfor bother set

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
