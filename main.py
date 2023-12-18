cde19124fa29f6dc0f73a2f3beee83dacbf05ccd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:28:30 +0000

    space regional shout keen smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
