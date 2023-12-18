7689e6831b36df1baca07ab2533da5fcfa05c0db
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:08 +0000

    shut Ll any runover worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
