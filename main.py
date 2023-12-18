877570c10e7a5f78f2c81e97addb541cfe35e351
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:53 +0000

    fortunate sleep tidy speak relief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
