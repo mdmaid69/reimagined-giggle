3da9b208c5638bc8021322247c789388c59c03c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:15:30 +0000

    strange attention vision cause fit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
