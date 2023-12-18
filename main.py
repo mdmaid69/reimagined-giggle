3758d0c73445a317afb64660c1edbd98c85f6811
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:11:24 +0000

    trick news attitude everywhere pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
