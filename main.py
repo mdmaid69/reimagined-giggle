5dbcee943f7cc56e5cd18aa6ae492c11f1cc90bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:40:13 +0000

    touch make insist use left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def add_numbers(a, b):
+        return a + b
+import array
+def convert_array_to_list(array):
+        return array.tolist()
