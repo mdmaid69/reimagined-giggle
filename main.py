6a53fef1b3ad53740214c7db74fa66d102b0fac9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:57:15 +0000

    protest issue lookaround style push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
