04e4db71abd1211929f0b4985a1871c86eec17c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:35 +0000

    reserve stare precise print say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
