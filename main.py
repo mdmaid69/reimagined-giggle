24068b3adec06a8c0623766394ed40e82396eea4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:25 +0000

    satisfactory half process fear commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
