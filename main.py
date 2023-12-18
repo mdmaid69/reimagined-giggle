e10dc7735bb6e3a90e02fc7b386874033881e232
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:10 +0000

    suggestion bang circle size change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
