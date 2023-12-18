1941423f7a90a1097b3de09d3a3375622de53d71
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:48 +0000

    compete 572enter shut minor influence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import collections
+def create_stack():
+        return collections.deque()
