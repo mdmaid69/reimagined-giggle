48b868510818bdf7c364c48c745ec5579de7fd80
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:21 +0000

    complain anyone long sound flood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def convert_to_binary(n):
+        return bin(n)
+  def square_number(x):
+        return x**2
