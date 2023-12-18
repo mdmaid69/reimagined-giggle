cd902255eff25ffaff23e40335faed04ce1e55d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:09 +0000

    excited scream less careless question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
