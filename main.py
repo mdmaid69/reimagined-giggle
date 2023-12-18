ce18cf89dc159dd00341b466e1faeb31a6ecf3f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:08:17 +0000

    everybody defeat list else drive

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_user_string():
+        return collections.UserString()
