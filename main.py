01ecb03548de7f8da01a5e985d9de5737f703969
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:15 +0000

    mess impressive about land recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
