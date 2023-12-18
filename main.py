0632f5ac006360770758eb909fef1c92bdd0f256
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:24 +0000

    image keepfrom exchange check either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
