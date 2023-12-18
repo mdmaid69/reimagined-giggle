248d0737f37eb2f8d1c16c4ba5328832ac0888cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:01 +0000

    deserve situation whoever quantity wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
