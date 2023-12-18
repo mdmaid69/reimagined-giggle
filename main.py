b0ddebf9a07d52e3f03b61598d9b207079d25b36
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:06 +0000

    otherwise arm risk generous second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
