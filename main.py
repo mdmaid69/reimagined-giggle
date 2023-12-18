d42a778780cc43e542f533aff8590830a5bb9cbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:13 +0000

    claim owe best rich outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
