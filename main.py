8672c0414b75b20ffb1f3d3ec44b94f908d79c08
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:09 +0000

    try takeback trap turnup deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
