2e418ac788e9dbec8fdc6e3fbcedeecb3c0ea661
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:50 +0000

    week display eager matter bread

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
