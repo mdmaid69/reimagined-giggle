5a9bbd6599ad3918713b5f66476bda9abcdfac50
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:02 +0000

    track experiment trap by emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
