eb692899219d7c66aea0e2cb0c0c1d7a4eb03af6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:59 +0000

    interruption waste sense draw flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
