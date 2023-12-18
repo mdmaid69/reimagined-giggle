4cff4eda20954470379194e527af03b14d1fb756
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:22 +0000

    exam exhibition expensive every keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
