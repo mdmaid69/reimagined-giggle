df76cc643a689a814a39dae4787f0884c59bade3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:57 +0000

    moral no sight refer square

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
