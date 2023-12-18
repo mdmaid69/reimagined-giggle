f25cdaa2bf963c46051dfbafd7cb92af7f31e4ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:03 +0000

    Ee turn write ask certain

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
