843d49b988f7587260503b588e266adcd9aa63bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:37 +0000

    fortunate copy invitation ilovedicks worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
