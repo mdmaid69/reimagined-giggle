089d8a92f7a86e279283aec3e018ad0379678744
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:30 +0000

    care string class thorough soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import sys
+print(sys.version)
