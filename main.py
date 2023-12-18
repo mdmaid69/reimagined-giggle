e81857add921cce01a3305d891b65bd7fce6192c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:43 +0000

    health shoulder pronunciation occasion offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
