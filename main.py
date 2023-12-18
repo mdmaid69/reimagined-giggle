374d292465014665450e6b1b56e5b5df5b01fc87
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:27 +0000

    result 570enjoyable help post giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_priority_queue():
+        return collections.deque()
