d67c016d1581ff614194d0bcd90bfc4b4c5bf560
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:34 +0000

    win persuade guilty characteristic impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
