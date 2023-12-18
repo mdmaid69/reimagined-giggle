c1cf842e7c289220f9e9285c21c944096f5f6a7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:14 +0000

    study object spare phone room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+print(re.match("h.*o", "hello world"))
