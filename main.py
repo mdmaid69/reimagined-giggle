51f885bf34d20916fb1272955f6e324dc54ba005
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:44 +0000

    allow case thing minimum act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_acceleration(speed, time):
+        return speed / time
