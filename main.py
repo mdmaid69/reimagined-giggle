23238471f6160684be3cb79db43e65097c7d2c6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:55 +0000

    against figure pass interesting tragedy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
