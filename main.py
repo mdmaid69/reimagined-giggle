926a08f4651c4a84fdbdba7a69efbe620bbcfc10
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:28 +0000

    furniture declaration shame away satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
