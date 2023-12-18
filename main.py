108c81dc5cea211e22430de63e9600fcfad493d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:36 +0000

    suit band agreement better strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
