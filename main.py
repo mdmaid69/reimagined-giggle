ff44d463a5e8e5c339629e187539b2cf7279b5dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:09:55 +0000

    bottom rather accept scale still

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import csv
-def load_csv(filename):
+def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
+def divide_numbers(x, y):
+        return x / y
