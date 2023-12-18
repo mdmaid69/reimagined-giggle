abd091af491e6fe6ed23b22549f5ebcf9a7600cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:27 +0000

    division fall Pp luck name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
