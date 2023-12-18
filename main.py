fa40a904d025b21b0b67e3b8b2bd1fe7b8a26a41
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:37:00 +0000

    quick ill ever advanced simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
