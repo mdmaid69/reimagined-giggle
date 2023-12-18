e4623e8ffb70acbc7674fefd52b7c7a22e2fadd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:17 +0000

    snow black partner black soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sum(range(10)))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
