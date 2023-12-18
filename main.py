1c1df4cd3ca8165c19dd77aad3f62b628406c306
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:29 +0000

    lie might nurse shut thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_density(mass, volume):
-        return mass / volume
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
