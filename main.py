5f619271a2c69ec38efa6a8e21c5d61effd29cf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:57 +0000

    clear free lower load distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
