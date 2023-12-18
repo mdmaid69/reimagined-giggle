1121fa8110a3898389f00823fd51d9564bb10710
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:26 +0000

    spare less add when lonely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
