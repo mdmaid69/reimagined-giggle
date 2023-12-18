376424ac2c758157968b3c363383e662b4b89b8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:20:36 +0000

    plenty have front strike worth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
