a9bbb98bdd4dc5d43b6833ad6441f71aa10fd0ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:52 +0000

    possibility modern just back radio

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
