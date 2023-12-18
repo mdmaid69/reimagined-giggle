696f5004af63bb669aa111fa9ec3bba20a3e0bf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:37 +0000

    what wire company snow flat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
