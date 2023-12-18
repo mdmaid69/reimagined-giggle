e3ba56924ed5d89319fffea00dc0aa13edff69fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:25 +0000

    right lookback(on) level handle complain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
