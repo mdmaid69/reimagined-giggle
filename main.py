e4f8ebded6bfe6f6985a7058210cc177f9e9f89a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:16 +0000

    Ii blame image special behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
