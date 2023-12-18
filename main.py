4d8ecb53d3916e1297d0eef503b4337a2682d812
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:58 +0000

    heavy excellent himself past occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
