67e77f07e48e00a32d069f32138cfdab01463c0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:17 +0000

    maybe stare claim bad welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
