f2ae8ad3397641e910181edfb8364970eecfaad2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:50 +0000

    float certainly public meet embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
