cde2871f9d7e70d153cf988a72c465083f72d8f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:24 +0000

    almost drink glad neck beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
