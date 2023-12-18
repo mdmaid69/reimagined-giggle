bfbf562ba3713b2658325efeb7bb85aa2729488d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:06:32 +0000

    doubt circle bank around date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
