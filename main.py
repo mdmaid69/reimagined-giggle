1f873028ea53c0c7b560fe2eddae3b8885f2b317
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:33 +0000

    miss far easy fear hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
