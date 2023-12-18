ce6b974994ca4c9c3c161a79f1c32e2761c3f6ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:58:26 +0000

    land finger stand disapprove upstairs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
