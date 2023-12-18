60aa2fb184396b28c38f15e0f74e4d61fa0a6ae7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:00 +0000

    club gift evidence mix right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
