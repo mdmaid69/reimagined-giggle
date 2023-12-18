8d9db5c99cdedf73937578d8849a2f08aad3e856
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:17 +0000

    excellent swim yet shout neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
