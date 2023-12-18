1650fb5679b621cc0a5d775f5dc9cbc97662ddb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:42 +0000

    your possibly honest regret exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_stack():
+        return collections.deque()
