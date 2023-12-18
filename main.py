41e8fa7dbad9a46d8517b70d95bae6a09cf0d5cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:29 +0000

    food believe top law anything

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
