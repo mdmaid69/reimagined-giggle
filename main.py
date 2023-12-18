f98fda477ab3da2dc94d5d99ec25cb9f4669015f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:17 +0000

    between include welcome home pleasure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import time
+print(time.time())
