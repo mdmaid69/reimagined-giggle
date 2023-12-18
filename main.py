f5eb1ff5f2701aea397c16a8b763e77b22dc2fd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:54 +0000

    coffee furniture fond at room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import collections
 def create_queue():
         return collections.deque()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
