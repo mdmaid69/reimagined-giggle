3781956c4a3e61331075340e26ab175a8fa28101
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:03:57 +0000

    bet strict turnout nonsense breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
