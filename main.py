4ab4e6a6a736f40bf53e9e83838152f55f879844
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:53:02 +0000

    possibility smoke space shade who

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
