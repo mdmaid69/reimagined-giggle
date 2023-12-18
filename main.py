7ac6a7218bad6846bc02da9bf7ecaf8d0c1ee9da
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:16 +0000

    print limit practise secret describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
