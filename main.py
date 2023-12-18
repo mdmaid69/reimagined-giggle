701e80e00510546c3ef51824f006aa5718eec6eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:17 +0000

    nigga pretend recovery excited keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
