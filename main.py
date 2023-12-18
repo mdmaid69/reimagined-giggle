d2eb4514b0dd9885e2893e72c7b6e911cb25296b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:03 +0000

    science shop make ear talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_acceleration(speed, time):
+        return speed / time
+def convert_to_hex(n):
+        return hex(n)
