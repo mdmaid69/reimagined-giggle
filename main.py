4f99ac01d2112ea3e5f182f43cf25e5fda52ed29
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:56 +0000

    aim congratulate which weak prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
