021d87011a515a026630d1a1abbc5f5c4280eee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:23:20 +0000

    finish claim anywhere important lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
