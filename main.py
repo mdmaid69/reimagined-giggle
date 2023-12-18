05247ca9599ee1c4cb39b11c78c2cd848f0de15d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:19 +0000

    impressive fail him upstairs getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_max(numbers):
+        return max(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
