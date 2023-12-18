830508b0735aa8d4bc3f108f792c6f7d9178d9b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:12:32 +0000

    too call workout none point

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
