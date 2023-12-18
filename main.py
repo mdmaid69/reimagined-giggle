27f8a25b3542657372e3ef2d5564b2fe6d21cc49
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:52 +0000

    rate breakup quiet throw empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_speed(distance, time):
+        return distance / time
