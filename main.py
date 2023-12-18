7e5f523d7e58d91c63fbeea18c315ddd3c659944
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:56 +0000

    pair any joke wonder risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
