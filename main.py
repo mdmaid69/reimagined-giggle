d0b0b0273d91787dbc919c30bd60a68b6ece0e3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:00 +0000

    bread except cheap teach prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
