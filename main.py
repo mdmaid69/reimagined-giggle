5ea7232147ee5f5f2e3c20c28dd95025df03cb08
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:06 +0000

    comefrom summer bring they meeting

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
