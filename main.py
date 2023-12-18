69619f679e0151fc2ff4c057f243e2fd67d7e6d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:34 +0000

    separate sort head damage level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
