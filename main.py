1a07e582e5d8106e6d630622147c447ddfc835af
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:09 +0000

    earn attention profit fit mention

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
