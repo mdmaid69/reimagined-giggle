894445341ece1cf0641da29d9ce8e8130df2b58d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:38:07 +0000

    sit want hair big vote

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+print(math.pi)
+def find_min(numbers):
+        return min(numbers)
