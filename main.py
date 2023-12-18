84ac8a8c6ba498b95f7f6f96c79914101120b5c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:40 +0000

    formal ordinary describe programme able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
