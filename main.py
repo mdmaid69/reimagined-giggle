22cb5162b30e9ad71ae5d806129c4b18830f39f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:07:26 +0000

    mine style run human few

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
