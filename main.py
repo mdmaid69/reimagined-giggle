9190786feeec40152b98619e048b2ea70e5e21cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:35:39 +0000

    woman tie wait promise interfere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
