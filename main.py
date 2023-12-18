72f2dae631f644bd4f361560492bf11f64ba1e04
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:37 +0000

    swimming save innocent shape load

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
