013e5e00216b634bc37438b1306227171f2b7512
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:33 +0000

    interview hide pocket pool cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
