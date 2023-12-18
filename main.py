812fa23c6512d2544b0e41a9f911fc5a92806ba7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:18:30 +0000

    die stop open terrible skin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import random
+print(random.randint(0, 100))
