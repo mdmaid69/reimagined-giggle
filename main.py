31a142144c4bc3b8a9ae97c05fee715337b68be7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:22 +0000

    talk test priority laugh need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
