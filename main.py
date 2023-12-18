3fb970271ab2770d69637cbc101a4ad8ee987b05
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:44 +0000

    blind means dream lend balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
