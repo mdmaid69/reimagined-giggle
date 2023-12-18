22605fc21f31e330b5c34f7cb3dc1ff86d83a8df
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:46 +0000

    swimming preparation distant experiment fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
