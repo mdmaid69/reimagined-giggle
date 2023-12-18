525708c9de18d2419ed060502eb863096a31eeac
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:47 +0000

    error surprised move win Dd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import sys
-  def get_python_version():
-        return sys.version
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
