8a5e36038d39c9fd78e1ef5cc28de87cbcaad093
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:16 +0000

    clue could operation advantage near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
