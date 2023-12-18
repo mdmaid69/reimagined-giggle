7e9ded1650574149f09cc705a57c0cee00805b7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:34 +0000

    use squash so first common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
