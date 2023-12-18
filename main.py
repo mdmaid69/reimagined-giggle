0c209c40420b5904faadcd1307fe4e12ba05bd08
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:47 +0000

    home land cinema tool used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
