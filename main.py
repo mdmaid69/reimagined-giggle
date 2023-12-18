ab9c1ea49a3517db2f5c1968aa6bfc6b94eb11c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:10 +0000

    will mouth connect mainly excited

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_power(work, time):
+        return work / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
