e4e8eb702e57144bae558532a27b98533f524820
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:56 +0000

    callat word uncomfortable tone recommend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-print(math.pi)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
