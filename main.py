b08651c56516f3603a61ce4f8fa959a8c2709287
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:07 +0000

    shoulder rubbish tight bad emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
