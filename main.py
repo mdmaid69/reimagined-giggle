18ae19d12aa0d4da0c4ae754ac42b3f41e24d2eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:33 +0000

    attitude deserve contrast shopping pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def add_numbers(a, b):
+        return a + b
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
