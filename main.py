405aeaf558ed179a379510c9d4ace9c016daa5da
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:24:08 +0000

    drink type concentrate aim accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_bytes(array):
+def get_bytes_from_array(array):
         return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
