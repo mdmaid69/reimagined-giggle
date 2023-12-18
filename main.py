7afb865903d8929750f82dc55376ab1e3baa6c70
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:57:54 +0000

    insist tight scheme store wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
