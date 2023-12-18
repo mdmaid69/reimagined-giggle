52ec0c84e72f3be63128f4f962f0a8afb2eae864
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:51 +0000

    wherever hope total bound like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
