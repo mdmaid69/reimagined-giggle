74d50df3a8511ea8801d3082929b5fe7e0cecd7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:26 +0000

    silent worth order valuable punish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
