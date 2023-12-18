c07af7aa6d4585adccb8172621d69a00ffe88c62
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:47 +0000

    tidy push drop hurt loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
