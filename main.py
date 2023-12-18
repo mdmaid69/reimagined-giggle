5706f6270e7446d468533c7f6b6aa401e98b901b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:45 +0000

    maybe comeup introduction typical cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import os
+def get_current_working_directory():
+        return os.getcwd()
