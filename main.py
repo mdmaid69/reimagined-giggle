344111a064fa73d288a5f2421cf142ecfbed30c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:43 +0000

    politics vanish comment cruelty answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
