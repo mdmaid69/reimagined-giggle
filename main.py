afdda87edaa9d440ae93ed40f8a5b6ecb67680c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:08 +0000

    pick dress slip move fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
