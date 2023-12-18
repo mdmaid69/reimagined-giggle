5757164b0786ce33f4dd61fec1e0b0f7c519bcc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:35:10 +0000

    black hang listenfor part about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
