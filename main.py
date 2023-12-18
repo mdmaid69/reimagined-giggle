28ab03c633d50314e492efd5978199fa793ca7d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:43 +0000

    opportunity dick poison worse begin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
