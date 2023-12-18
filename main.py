b3df2c777660f064f0fae7b6f365c9f5ab90253d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:58 +0000

    exactly operate weigh confident mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
