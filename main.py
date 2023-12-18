eab31b7cbc01c16c83f2ef782a5a6b27d00b6547
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:15 +0000

    mistake morning basically throw terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
