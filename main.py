91b383a78820115b929969f195f786ec6e549dae
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:10 +0000

    action count happen limit heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_max(numbers):
-        return max(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_palindrome(s):
+        return s == s[::-1]
