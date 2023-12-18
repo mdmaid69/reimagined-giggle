0d0c2c754e9f3893e97710dac6d95c7669e0b774
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:05 +0000

    burst sorry especially unless right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
