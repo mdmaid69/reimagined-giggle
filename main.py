eaeb2a6236de0d0c6660daaaba4d3f5ad78969be
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:24 +0000

    upset with normal building introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
