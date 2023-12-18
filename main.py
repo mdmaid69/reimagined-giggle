d60b81a0214da4919767d3b705f308d5a6ef9332
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:20 +0000

    die pen instant before official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
