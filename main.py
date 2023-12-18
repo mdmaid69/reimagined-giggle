8026fcfdde09328a6169bba949de09b930dc13fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:30 +0000

    day joke tight switch left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def divide_numbers(x, y):
-        return x / y
+def is_palindrome(s):
+        return s == s[::-1]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
