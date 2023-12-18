d9ffc6c8afa0a61bbeee81146e0f3178546a05bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:25:35 +0000

    steady negative study century serve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import time
+def get_current_time():
+        return time.ctime()
