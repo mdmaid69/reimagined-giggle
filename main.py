e292b908381cff432e1736e6dec33f35b826459d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:24 +0000

    goaway goaway crowd artificial rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+print(math.pi)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
