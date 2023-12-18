466c6bcac423daae064a85b5fe32a787d913880e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:58 +0000

    turn necessary design getout turnout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
