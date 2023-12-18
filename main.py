3c69182b1721fd61fbba2fcb9ffd5ca750b94532
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:20 +0000

    until seat sign interview beginning

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
