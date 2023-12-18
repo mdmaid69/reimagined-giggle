75fcc4e8fd1d908867c738a93abf8a797a34d393
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:06 +0000

    giveup chief arrange Mm inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_sine(x):
+        return math.sin(x)
