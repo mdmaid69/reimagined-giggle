659fa64a4ca85ecb5f74fc353cdbceb7037335a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:42 +0000

    Mm too cheap protest confuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
