d1aa120ddbc97acca16683fc68ebc075daa588a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:46 +0000

    wet bring thanks balance present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
