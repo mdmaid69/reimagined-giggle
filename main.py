5ace0bdec39cd158e75d55206579da78ab8dbd8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:22 +0000

    light excuse embarrassed punish why

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def get_array_as_tuple(array):
+        return tuple(array)
