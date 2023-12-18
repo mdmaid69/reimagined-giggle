e8c09a872655df94d571ef364853996f98ff231a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:20 +0000

    send holdup generous limit safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
