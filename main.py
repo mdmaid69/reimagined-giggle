5ea14f3efbfdc1fd1631b2b1bf6fe584db0e4b08
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:28 +0000

    pronunciation afternoon blind harm gradually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_min(lst):
+        return min(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
