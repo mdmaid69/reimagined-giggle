c061854e2c87232eac69b262b37ed4cddddfba7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:25 +0000

    shock exact science important myself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
