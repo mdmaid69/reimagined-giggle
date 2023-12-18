19d7556e1d822a84bd1960b7c541dc05e019423b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:11:30 +0000

    century afraid little same fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import re
+print(re.match("h.*o", "hello world"))
+import sys
+def exit_program():
+        sys.exit()
