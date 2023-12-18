3d843e3d3e0078871b6fedf02177dd66e484c8f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:20 +0000

    Rr could 179bite Yy demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
