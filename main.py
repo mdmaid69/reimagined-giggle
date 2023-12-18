618387edba1c5a2be49670c7b9ff0581ec118628
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:59 +0000

    work contribute present opponent sweep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_acceleration(speed, time):
+        return speed / time
