0566c628007d0f06450eba892446b835117ae926
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:49 +0000

    system play fashion permission Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
