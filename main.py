3433b7177013f5b7ddb94241422dc7fbb8cca57f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:20 +0000

    away agreement along swimming always

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
