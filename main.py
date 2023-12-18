e7573ebda8bd498f97b5cb68782f6111cdeab01a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:26 +0000

    shake cinema point want fly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_int(array):
+        return int(array[0])
