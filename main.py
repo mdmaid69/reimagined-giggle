c374e7784aa93996edb1b10cd5db9589a8c4ee14
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:28 +0000

    concerned talkoutof ear regret many

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
