e8ac4a72165a1dc54c75f554699325fc0146ec74
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:32 +0000

    proud large probably law town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
