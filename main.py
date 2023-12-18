79c41d98fbe619a38f8c67b16c1215c4e71a90e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:43:41 +0000

    address culture before process callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
