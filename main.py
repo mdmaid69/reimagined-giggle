84a8a0ab8f70732187faab718a1e9ea5438b7362
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:27 +0000

    key appear mainly push themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
