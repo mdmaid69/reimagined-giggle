4d609a37fd66bf3037b98d3d82b003e415d2fa5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:46 +0000

    around switch maximum add comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
