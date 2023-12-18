771363f19d371b542636ad32964270d597dcfcf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:29 +0000

    support slightly hide attach goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
