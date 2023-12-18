7a34442fbde17c485ba7f767811763e2881003f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:56 +0000

    sail deal Pp roll suffer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
