4a88e5c7a7e4933a56dc56bf73773c0cba153fee
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:25:17 +0000

    central through workout tear above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def subtract_numbers(x, y):
-        return x - y
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
