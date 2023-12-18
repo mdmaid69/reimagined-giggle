cc86a66702457d88346d44a942a580465af527e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:13 +0000

    suppose remove refer ticket breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
