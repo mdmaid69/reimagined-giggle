a4da11b2640d82d6bf2cf3b132097cb4abf81758
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:38:40 +0000

    art angry lend accidental relief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
