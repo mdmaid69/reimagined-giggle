fa420d31243e7c03279ee9c034c3acd9cbd8bbdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:56 +0000

    few destroy exam beautiful close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-print(math.pi)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
