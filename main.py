0683ede6c4cb535c9c36e4ec052189c5615a2270
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:10 +0000

    except value free Ff complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
