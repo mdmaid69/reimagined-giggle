e74167561d26e67b3966635fe2be8c45fe61d02b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:07 +0000

    down sensitive exist strike slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
