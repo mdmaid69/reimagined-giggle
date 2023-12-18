6b31724cf9f32eed4fd1320685aa98767368a9ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:25 +0000

    light punish agree suggestion 570enjoyable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
