811779c3cff65147556d4f34f248f314f7f64696
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:31 +0000

    unfortunate smile arrive message our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
