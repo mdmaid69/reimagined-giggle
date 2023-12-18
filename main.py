44422c579917d7dc924be49647dd6d36522d3fe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:19 +0000

    plan skin unless damage single

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
