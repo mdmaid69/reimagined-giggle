87cc75fadcbb103637002a60793facd61ac71900
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:16:48 +0000

    detail bank period everything behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
