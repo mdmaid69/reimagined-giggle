5ec1a421e25af6fce6d4c0e4470839c5d2a3bd33
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:24 +0000

    completely sting bar now more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
