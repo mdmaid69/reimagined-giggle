378a5b006020bfc6b03782c2926873e706043810
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:16:50 +0000

    opportunity damage much exciting contrast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_speed(distance, time):
+        return distance / time
