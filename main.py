b38daf2dcc7c31ed907c5fd1154041e85641d3e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:24 +0000

    famous union know prove become

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
