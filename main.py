081641cdd51fccd9a3c931f1da80760ea85e60fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:37 +0000

    disapprove hate consist inside collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
