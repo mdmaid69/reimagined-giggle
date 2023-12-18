ec70a7073e0c618614093c4a3220bbd7b4d8e0dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:53 +0000

    last scrape cheek hospital keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
