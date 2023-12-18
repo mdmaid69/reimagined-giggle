a1be3292ed9aa4e6d418f01240dba6b91acafde4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:14 +0000

    environment impression injury object date

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
