57d841895a932c0cb98431fef989409dbf4f1647
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:07 +0000

    inform hot mysterious aware trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
