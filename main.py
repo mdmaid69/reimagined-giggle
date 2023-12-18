f776e7f308dbbff84590b3ea2310d46b19ca7f83
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:25 +0000

    accident film ought fail communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
