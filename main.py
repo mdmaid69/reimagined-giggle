b979cb64761f87ca5313f94d25d7ebdfd7cb324e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:25 +0000

    doubt preparation convenient badly next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
