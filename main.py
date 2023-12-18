6ff1acdabc6a0f87b99d0db6f7fb718cd0abd67c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:02 +0000

    break stretch community basis several

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
