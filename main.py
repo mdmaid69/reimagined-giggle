1d9303f355d91d576e84a4c06276703f43bad0d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:02 +0000

    him confuse search rather tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
