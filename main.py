9774c35bb9bb243b2e2b6609093fb9243ba77a99
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:58 +0000

    reduction store open never finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
