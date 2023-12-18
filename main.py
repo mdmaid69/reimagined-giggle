8fced932357aba5e870421fa0e4009fbd0ad2aa3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:43 +0000

    solve exit afraid arrival angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
