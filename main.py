a77292bd4a56e68859d80757e3e5fa07f703a617
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:05 +0000

    confused why direct mass design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import sys
+  def get_python_version():
+        return sys.version
