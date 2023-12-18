50b1d19c5103a10536378dddf053050e8f3d3ebd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:37:56 +0000

    Zz spread cap claim platform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
