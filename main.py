2ad06e567ba38766c19e3804c9959ad0dba4b8ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:17 +0000

    authority wonder train bang message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_list_from_array(array):
         return array.tolist()
-def calculate_density(mass, volume):
-        return mass / volume
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
