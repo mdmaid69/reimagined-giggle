97641af2529b59ea93ce4b734811997832814f54
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:29 +0000

    rob introduction stranger dead number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
