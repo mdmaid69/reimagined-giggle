33e4cf74f61561add5763777582f3fa1a4e536b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:51 +0000

    afford scream dress window regret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
