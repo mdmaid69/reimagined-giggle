3551d855428918b53e44261f90560cf3dadffe4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:21 +0000

    centre table blame still bar

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
