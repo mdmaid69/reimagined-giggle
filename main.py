aec54bd19c2b79f9eeb894f272e7a3caea2cbdf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:14 +0000

    soak smile idea year offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
