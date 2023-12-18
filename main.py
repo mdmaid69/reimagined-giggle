eeae3c3b5a225cf0fd903dbd8d9ce1f134044b2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:54:01 +0000

    study positive run hello call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
