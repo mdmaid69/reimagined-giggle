b6a0449c8134818e735ba7fcd0305c547515c227
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:06 +0000

    it where telephone giveaway together

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
