b9a9dfd94a977571e4227402162a85dcbc32ba5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:29 +0000

    recent dry new reach main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
