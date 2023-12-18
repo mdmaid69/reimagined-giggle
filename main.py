e83e33595af8d10e7a492978af15b9c275f2f1d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:33:04 +0000

    run radio marry hit about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
