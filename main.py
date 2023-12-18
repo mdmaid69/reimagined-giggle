0172786eb76fd02b7568f01ff0493c9aa923ecdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:32:33 +0000

    necessary finish frequent declare neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Characters:", len(text))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_item(array, i):
+        return array[i]
