5448a9b9fa37fcc9614393398f415d3d9740fbca
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:24 +0000

    goback(to) regular achievement compare near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
