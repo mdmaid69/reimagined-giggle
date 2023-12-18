4142e7d9cfc8a014cfd49db5e2b211acd94444a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:01 +0000

    emphasize difficulty jump tragedy scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
