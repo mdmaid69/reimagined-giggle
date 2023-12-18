6fd351e3c4a3dbc06b3ec522d25e0e55a231cc9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:18 +0000

    circle feed punish Cc suck

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
