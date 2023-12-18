7f3198db580e374555275a365a63da2074dbb7fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:56:06 +0000

    foreign scale almost shop to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
