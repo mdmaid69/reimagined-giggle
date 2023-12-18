49b14a93ceb0b62b715f9548e498221fd942b9e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:05 +0000

    block call personal steer pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
