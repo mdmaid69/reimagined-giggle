c549a0e6bbe6bf7bdf03aa91cad492c489f8e23f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:32 +0000

    goon trap callout equal tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
