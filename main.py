dc1a076c9e76c0eba695ba57e94d3159af96c0bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:47:25 +0000

    priority since tone snow range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_odd(n):
+        return n % 2 != 0
