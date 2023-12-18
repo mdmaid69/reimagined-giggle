7d26be14f2d7ca520b628daafc0403b9ba10242e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:48 +0000

    there sleep bank somebody store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
