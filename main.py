8095725d9726ecf50319b2e7ba829432aedec1e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:22 +0000

    mistake top human best pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
