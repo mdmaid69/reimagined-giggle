8f6cfa5f50613bd1cd0daf986f5fe5ea105f00cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:16 +0000

    central everybody feature range arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
