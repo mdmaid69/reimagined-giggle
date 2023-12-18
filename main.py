abdc1cf302621930c120c9a2a6f1d8241e42d82b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:59 +0000

    congratulate funny feel fat communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
