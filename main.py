39d452933762eaee0bdfb01723e18b5ff1cb601b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:02 +0000

    space common face invite friendly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
