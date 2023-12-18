761fb6e13a3adb89a6fd5c48efce3352e7d5898d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:12 +0000

    opportunity point trousers tend before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
