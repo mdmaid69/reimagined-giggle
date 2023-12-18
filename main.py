f70751c69deb18e98e28228d16954a5da841c13b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:20 +0000

    health us sport store shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
