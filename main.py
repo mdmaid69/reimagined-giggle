09715d6a3de221993f1ae6619a2135cc3ad65f04
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:00 +0000

    divide glad frequent young convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
