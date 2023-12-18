e2b57a600eceed661d62bb27f8452ae602e56e5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:55 +0000

    active afraid party rain move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
