f86163be3a32f28a1781a2674fb07fb6885592d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:49 +0000

    basic preparation allow about formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
