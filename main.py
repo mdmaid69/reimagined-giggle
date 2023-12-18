bfc4b541a06b3100647fcd56378314b2b091c418
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:37 +0000

    ought about he type rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
