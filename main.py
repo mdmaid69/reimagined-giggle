4dba9e1a7c1d439764c99d039c9cba8c3e7e1e32
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:52:48 +0000

    wheel practise slow pen judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
