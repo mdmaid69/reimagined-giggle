5ff24dd52c3cd051d96888f4e8de5b0a0fe9a8f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:26 +0000

    voice under bite exactly work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
