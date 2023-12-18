e059ce7fc05c41ab9fd25476b3bdbf665def2a94
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:39 +0000

    catch excuse comfort ahead reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
