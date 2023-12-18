974abe44e27b501f6b5d91ce2c9ca95584806773
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:15 +0000

    off pretend communication computer clearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
