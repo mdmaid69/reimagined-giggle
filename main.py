8688e2fd5e8cb38b2dff109e6b889f32b75185ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:28 +0000

    fairly far advanced today touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
