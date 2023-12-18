db0e4340f1f5f5648621ba3992a03ada75a42823
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:59 +0000

    here step way train praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
