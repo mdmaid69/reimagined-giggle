ba2811df9ebe13eabb497c958c00e9234516c124
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:39:56 +0000

    preparation complete 573entertainment quality lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def is_odd(n):
+        return n % 2 != 0
