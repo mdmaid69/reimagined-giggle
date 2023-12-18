1369fa81043ab705750201ae698adddd67525a40
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:22 +0000

    apply type tip tax bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
