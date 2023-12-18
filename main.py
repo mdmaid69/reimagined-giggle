e88c22d9432af45dd7775e91847c390a693fe4b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:29 +0000

    neck operation waste scream us

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
