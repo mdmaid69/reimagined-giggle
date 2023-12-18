7bdc6254671944ed817a1a8d38601c2ac3bbddd5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:21 +0000

    street as future bang game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
