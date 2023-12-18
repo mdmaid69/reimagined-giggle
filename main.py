a9d6fb92b2813e9b98122b8092f4c2cc2bc99fa5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:01:22 +0000

    compare stop deal Nn rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(5):
+        print(i)
