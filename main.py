897efa27380bba4b2b1980f9597ff6e241971743
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:00:38 +0000

    much appeal culture discussion target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   def sort_list(lst):
         return sorted(lst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
