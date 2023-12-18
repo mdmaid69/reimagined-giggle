59b49da8d4438129382c0a0c5868420c84213823
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:27:56 +0000

    onto bored takeout sentence opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_list_from_array(array):
+        return array.tolist()
