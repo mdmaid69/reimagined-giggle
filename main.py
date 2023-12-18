856eee1c2a9067343f8cfb6293e9b99747a89f4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:15 +0000

    middle callat roll responsible instant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
