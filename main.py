6a2c90b3b6910fbf9530907d693c4e3f713ed50d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:54 +0000

    can just talkoutof rough long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
