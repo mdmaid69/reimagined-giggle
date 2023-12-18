3c7a3e44db0418960f5b7304a2151cadb52f48e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:45:57 +0000

    delicate support Zz pollution serve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
