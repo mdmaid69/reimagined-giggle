ba516f573eea47c30ce46dbb9504f6cb6349671b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:05 +0000

    over politics joke reason fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
