828fb3e4ca700968235fe4c503126bafd419f8c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:12 +0000

    by however arrangement official stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
