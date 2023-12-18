8203d0508ae4213e6731e71f91ec53c41de4952e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:59:32 +0000

    lead fasten callat promise apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
