b4dd09d95939b439fc5658f589513852392db190
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:56 +0000

    bring nowhere herself treatment combine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
