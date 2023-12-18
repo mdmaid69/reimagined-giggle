b060f7eab85894a6239db2ac098c27650708b3d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:17 +0000

    accordingto pinch happy cry thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
