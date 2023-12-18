da3550e2b6e2454604ed93420c8df9182513258d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:34 +0000

    mainly string welcome want own

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
