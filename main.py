b51b768d31823f65dfd72d2d87d5e5485b1aa19a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:07:26 +0000

    reduction bright except drive commit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
