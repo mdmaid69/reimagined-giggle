f06653ba0271650713564786b533e49917cf3fb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:15 +0000

    whatever matter what check succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
