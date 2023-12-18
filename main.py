4b477679094cc6e168d48c5a6e6364d6bb3695d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:11:16 +0000

    yours smile that sport record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
