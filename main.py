9ec47baa47f1b7f1e495473304924f0d5555101d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:36 +0000

    zero worst plan ocean turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
