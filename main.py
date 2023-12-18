84402426ab8fc097b37e4483fe8c321ceab5c391
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:38:41 +0000

    rich trap would past deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
