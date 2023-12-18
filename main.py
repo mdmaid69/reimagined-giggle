e2810ae1033f41e97947e1d4b95a0bc808a217de
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:17 +0000

    theirs will theirs stupid load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
