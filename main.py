eab94e9ae864e64865282d713ae32370ecc2a17c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:43 +0000

    takeover excuse arrive busy across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
