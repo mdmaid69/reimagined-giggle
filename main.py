e8fdedd648c28059c7ac0e268d09f58e99ac1e35
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:35:04 +0000

    tough sting reserve support place

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 n = 5
 result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
