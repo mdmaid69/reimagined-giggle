b98a5e963caa03c9f771b12f61d758ac00224e72
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:33 +0000

    must maintain loose steal muscle

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
