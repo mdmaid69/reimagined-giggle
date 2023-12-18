9a36db27dd3b4b86ad136a5f77ff06138e563fcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:30 +0000

    fasten lonely object whenever colour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
