894f976c5e49e67c0013abf830681aa47bbfbabb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:55 +0000

    several design original giveback everyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
