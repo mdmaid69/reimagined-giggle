20c0062d3032c37a8a37c4ebdaa84f6b92b72675
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:10 +0000

    especially to song smart fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
