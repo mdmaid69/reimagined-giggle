c25bb91113603cc8a6eb6e3af301d8a1411769b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:38 +0000

    human regularly line distance keepout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
