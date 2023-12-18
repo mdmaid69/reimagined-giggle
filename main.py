67d9c82a1ef33a0ea05242ab6927517c943bfa3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:10 +0000

    by ahead sticky model base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def sort_list(lst):
+        return sorted(lst)
+import time
+print(time.time())
