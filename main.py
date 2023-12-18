5b54557074fa1a510669ed7eceeaf38d257737f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:01 +0000

    holdup exciting tool like whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
