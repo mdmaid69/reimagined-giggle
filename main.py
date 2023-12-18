8329003e7b0335df54411c553bde70190ff6c2f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:55 +0000

    power shadow shake separate interfere

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def clear_array(array):
-        array *= 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
