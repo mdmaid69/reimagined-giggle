1c137f8bff52b994ca2caa3b2cec68102d30714b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:35 +0000

    fence cross with situation innocent

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_str(array):
+        return str(array)
