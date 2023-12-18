8e009d64d9f9745c2dbeed5366a6278794344319
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:32 +0000

    pretty way one remain machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
