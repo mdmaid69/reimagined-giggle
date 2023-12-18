a40921e93de725b4ce3d6d6956b3ac162c0fd4fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:06 +0000

    person miss excuse design although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
