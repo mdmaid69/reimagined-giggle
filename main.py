cf8bd1efe2b3ac0468fb337589b445f64ffef56d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:22 +0000

    put score loud excellent bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
