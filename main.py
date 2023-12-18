f0903b151cf812bec588c12a579e9655b579a53d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:04 +0000

    pass round moment tip brother

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
