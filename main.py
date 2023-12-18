09e694de5105bd5debafb502baabefcddd9825b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:56 +0000

    only gap affect please wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
