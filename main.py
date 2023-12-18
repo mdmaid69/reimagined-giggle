4e7d367bea9957831b0327632d9a91a953203202
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:04 +0000

    much under boring platform intend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_string(s):
+        return s[::-1]
