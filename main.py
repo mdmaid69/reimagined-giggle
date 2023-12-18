3c3b3a98c5490da391d90309229bbad41354dff1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:42 +0000

    damp bye style dance clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
