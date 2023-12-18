47dc951313ed6f629309aeac20daa2612ce8b16d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:04 +0000

    sea meeting crazy long change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
