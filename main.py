8ce5aaf88b42005690d1fb48c76444c0cc9fa955
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:26 +0000

    wave lead dream concern hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
