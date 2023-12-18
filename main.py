0d2022123f01f3a564e4ecd50a161364e043e379
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:14:24 +0000

    lookaround instead sore where congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
