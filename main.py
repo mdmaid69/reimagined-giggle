a302adf39cbaaaa5a356a5de49e6fb0bf57596dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:43:13 +0000

    rubbish burst concerned lead cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
