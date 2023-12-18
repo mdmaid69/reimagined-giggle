2a270871fb1dd43e5aeff9b74413aedb98416751
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:52 +0000

    this dark slow several needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_typecode(array):
+        return array.typecode
