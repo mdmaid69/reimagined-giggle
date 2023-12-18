6381b19b57777e7524265082d66b51ddb36d7284
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:32 +0000

    hide pity than impatient disagree

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
