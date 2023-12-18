5e307d4bd56ba9342781768f30102c09d54c79a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:30 +0000

    related spread normal speak 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
