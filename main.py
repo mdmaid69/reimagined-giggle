87308ac57a776d89534497dd19ae9c15efebfc55
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:44:48 +0000

    anxious play face later rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
