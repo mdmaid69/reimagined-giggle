d9a072b7c9b0ea8be858ca0cd35c8f3284850e08
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:26 +0000

    win priority floor tap scale

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
