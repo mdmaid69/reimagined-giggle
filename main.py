20b68a07ea9bf029c1e2f959bb343a85effb0e0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:03 +0000

    achievement communicate inquire instruction write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
