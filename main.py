33e72b255ed2de8ee93a19be568bd6c067f606e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:09 +0000

    eager produce what treat effect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
