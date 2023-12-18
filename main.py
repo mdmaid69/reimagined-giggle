8d3ca8dd1a8ea5adc4a57231e8ad3d230ea23949
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:48 +0000

    plate above high runaway callat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
