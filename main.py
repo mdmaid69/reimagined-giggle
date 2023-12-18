44ea66311871355f888ff8a8f47f1d190ac159ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:21 +0000

    stay Jj however expression cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
