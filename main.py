cfe4f10f8462757b55c09b57c4a6c3fc78a7b3e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:15:32 +0000

    pronounce Ll workon so ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
