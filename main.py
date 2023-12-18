8de31e1cda5ce5c6f668493aef81ae2f4bc3728b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:15:55 +0000

    cut surprise pollution keepoff shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
