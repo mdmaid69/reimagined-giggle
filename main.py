95f3bb660381b4b6a40239ab271f8c010ef82df9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:58:59 +0000

    sharp law phone wide there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import time
-print(time.time())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
