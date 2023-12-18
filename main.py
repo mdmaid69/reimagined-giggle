f322449722e7c7b1f7b207ea8068b70b6b141743
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:38 +0000

    appeal powerful cause government empty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
