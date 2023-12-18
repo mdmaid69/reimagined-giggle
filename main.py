4070a3af03f5e45cce597b86bacf8bbe32c1f31a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:24 +0000

    guess object letter either new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
