5cac261201670c1cb979781b6f933d4b94cd0c3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:49 +0000

    hospital each content back moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
