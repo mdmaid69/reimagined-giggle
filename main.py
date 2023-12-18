a7013a512e22d7d4785806f8c9be927378b968cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:53 +0000

    find welcome by honest yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_area_circle(r):
+        return 3.14 * r**2
