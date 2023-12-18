1bd064e175baa5b383b23d172078b2d6188b9689
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:15:13 +0000

    failure simple callback complicated rely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
