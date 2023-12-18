4cfc9cd95a7d6afd6138458c4b6e96d9812ba1b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:58 +0000

    normal rough list he come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
