630985d323649b157eae98e53d0e51d117a84fd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:58:07 +0000

    miss exercise slice wide traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
