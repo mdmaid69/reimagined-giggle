5dabab34d2a138c21bbaee1ff30de8d749f7fb76
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:35:14 +0000

    Vv trial respect complete damage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
