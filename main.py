481e44aa552059556c0845e99fb898296b3dbdf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:41 +0000

    before appear raise shine block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import time
+print(time.time())
