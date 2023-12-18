36963cc9c3b2b95d5d6231259386825404b0813f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:50:05 +0000

    after practice card price rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
