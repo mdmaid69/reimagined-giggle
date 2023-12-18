a0b5835b968ab512638baca5e115eae2086e8328
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:40 +0000

    cry spare interested getback there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
