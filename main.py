13ca9552a136c29cc2b16c3d5dd655b93e49c705
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:53 +0000

    brief Kk emotion refer base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
