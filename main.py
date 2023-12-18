3da2789c11ea6031960f8d57d7b627c2988fbb01
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:28 +0000

    pronunciation trip create ignore react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
