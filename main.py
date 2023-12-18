b4802c9f5482aec8a656052ebb83f3401bd7fc0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:01 +0000

    square mend major rush getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
