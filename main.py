380a6bfbf148bef9fbd24bb0865870d106ca7031
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:05 +0000

    image several comedown duty neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
+import os
+print(os.getcwd())
