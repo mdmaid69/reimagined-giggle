7ec8d5358fb710390f29ae66553cb1db25776b5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:13 +0000

    size move communicate climb raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def convert_to_binary(n):
+        return bin(n)
