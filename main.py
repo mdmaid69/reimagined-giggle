2b451ddcb37339008cf1f0245c5a08f0551ec767
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:40 +0000

    escape beneath generally vision grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
