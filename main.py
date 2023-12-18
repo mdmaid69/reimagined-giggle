e2d8be139a0f5750d5ceba40e76431bae3a34591
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:08 +0000

    well brush appeal joke turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def reverse_array(array):
+        array.reverse()
