795871d02ce5968ac8ce889195977bf81d644f1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:08 +0000

    by especially shoulder target minor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
