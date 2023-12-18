53844955fb00af9e599141e5fe0433d57925e4b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:35 +0000

    consist how receive fall immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
