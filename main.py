05378b904404ff252de5d90dc4e43123279efffc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:02 +0000

    card that question file computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
