03e6dc2cd2c91f342ed0285cc9c6866a2644631e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:28 +0000

    keep angry lead bet confusing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
