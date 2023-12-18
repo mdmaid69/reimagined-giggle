c05ed139d71b27e6ae221290c8b5ca575841d9fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:12 +0000

    peaceful fond speak thanks comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
