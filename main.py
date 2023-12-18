26765f2ce2bf0a46cc9e09ff7bb83a8be2389c1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:26 +0000

    model nervous expect 571enough clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
