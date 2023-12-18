603c323fbcb675d5a84b3d496142359252ae00c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:47:39 +0000

    spin test treat mad claim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
