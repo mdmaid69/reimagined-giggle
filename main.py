4218e231f52a0235fd01eb81814dc220695b6374
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:35 +0000

    thankyou abroad recover read inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
