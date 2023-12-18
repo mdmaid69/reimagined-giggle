8b16616b11f7d10d6fc7713d35591e4a331e4889
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:14 +0000

    nose sure partner someone heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
