cca7968aed6a68204da95de112517b50dce6d3ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:55 +0000

    opponent rather some guess fire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_tangent(x):
+        return math.tan(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
