8c45affa8f6dffa71771432732d86b388cdde413
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:21 +0000

    pay middle backwards miss maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
