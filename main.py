38712c55f1811c46f3cb72f6f8c69166fa3ba405
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:45 +0000

    wide them dark holdon downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
