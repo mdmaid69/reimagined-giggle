f6d6d215885f95958e2a1db89c54f937381c4b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:55 +0000

    clue Itself unable spring company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
