b80ec982b8ad176ce9370e32c70ed305cebcbfe2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:16:52 +0000

    turnup convince quickly transport wide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
