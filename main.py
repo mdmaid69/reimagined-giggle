4c67bc0f61d9d57976ca0c5600efbb6b6e94f71c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:32 +0000

    world convince smooth cheek consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def remove_duplicates(lst):
-        return list(set(lst))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
