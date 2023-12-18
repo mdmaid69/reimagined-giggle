10acccb635626f1304799516421f77ca110e9f1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:17 +0000

    split sound surprising acceptable freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
