3cd5864b4bc6e68d42ee999ae2d9230ad990ab0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:10 +0000

    most usually conversation extra stone

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sys
+def exit_program():
+        sys.exit()
