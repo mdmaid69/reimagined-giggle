3d4648d1845166ecfe1245b19b9154d80b60c7de
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:50 +0000

    mistake scream repair makeinto tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
