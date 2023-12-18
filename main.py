1f9c4a06ddc1e91fbfc7a0c85a438d04d9350354
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:47 +0000

    wrong pause back responsibility everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_list(array):
+        return list(array)
