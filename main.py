523bc7db6da2bbcf99784c315aee06cdd73b63e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:13 +0000

    deaf calloff fun risk search

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def square_number(x):
-        return x**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
