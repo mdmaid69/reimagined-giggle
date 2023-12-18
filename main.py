0d9ee79d9d0a611ea2fc4183a546a406c84db0f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:30 +0000

    old holdup giveup offer which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
