9c02f1c48c73498c9f53ef202cf9919f06a42ab1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:40 +0000

    down beat flow careless pause

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
