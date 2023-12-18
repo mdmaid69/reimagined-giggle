a9905e4918655863f4dd46bf6f5db4b271793380
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:00 +0000

    respect off admit definite life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
