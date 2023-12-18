2e50a23135bfee3e6d3aefb64af71fefed447a5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:50 +0000

    glass need here acceptable appear

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
