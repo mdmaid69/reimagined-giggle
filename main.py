9888e299857aa303cb6a093c37db60e6febf17c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:00 +0000

    collect moment achieve action reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
