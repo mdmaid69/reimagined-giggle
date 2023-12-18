d6f75b500b2904c8dfe4627c7b5bdef5354605ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:21 +0000

    niggaacademy head Ii demand tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
