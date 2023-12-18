4bd0b437aae0ff521dc1c0fd24d7add9f5f06339
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:00:28 +0000

    start activity body tooth stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def sort_list(lst):
+        return sorted(lst)
