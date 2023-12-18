fe2bbd21e67eb63e83f6d9c6be8ba086ef9e7518
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:16 +0000

    tie partner draw knock place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import os
+def change_working_directory(path):
+        os.chdir(path)
