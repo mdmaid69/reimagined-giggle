ee0ef938dba8f20d7937b594eb5d1cdf86daa6a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:29:26 +0000

    tool test string zero towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
