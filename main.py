35429e515a4d74341da0f36760fc4f4665717d48
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:29 +0000

    last neither scheme possibility address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
