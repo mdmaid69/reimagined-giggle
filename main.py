8794b65efba8cbde4014cbef30ff44c784940031
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:10 +0000

    build likely even watch any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
