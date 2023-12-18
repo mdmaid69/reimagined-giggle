99df561d99064f36201e980ddd8cff81b10ad052
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:30 +0000

    platform ours switch sensible rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def print_python_version():
+        return sys.version
