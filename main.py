7986db937221597fde5a50d08017aaaaf55b978b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:48 +0000

    official belong concerning comeround Dd

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
