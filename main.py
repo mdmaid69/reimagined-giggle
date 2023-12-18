e2ecd7eb99586c77f601db96b43877cedc555950
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:13 +0000

    soon agreement holdout basis sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
