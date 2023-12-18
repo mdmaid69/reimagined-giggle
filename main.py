fcb96378e5099f6c0492e3c34b299fc136ba81cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:31 +0000

    really sort themselves trip difficulty

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
