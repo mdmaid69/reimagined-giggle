c03fc185aff533f1e1285ffceb386f360c614efb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:20 +0000

    opportunity independent probably fortunately relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
+def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
