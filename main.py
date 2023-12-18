16b4119936973402fead60a95bced829f18f880b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:59 +0000

    poor luck everywhere refer certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
