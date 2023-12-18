bd01762adb626c249926135df4a137f3786bedc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:55:21 +0000

    smell wide switch culture edge

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-name = "Python"
-print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
