78ed1f32d68a5d31d67a71136c87b1e2e81405e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:31 +0000

    least battery join private moment

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_max(numbers):
-        return max(numbers)
+def check_if_array_contains_item(array, item):
+        return item in array
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
