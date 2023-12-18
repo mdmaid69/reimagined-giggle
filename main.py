4ead62c39eca8489d95565e19143f6f2365bb95c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:15 +0000

    fall method knock do attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
         return hex(n)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
