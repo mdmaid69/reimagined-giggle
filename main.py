30c7d321d35b9cc92d583810bba8ced4586b9f71
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:50 +0000

    wait chase cheque worth connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
