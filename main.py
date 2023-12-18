c8de49122cd83f4f80090658dded9a0124e04a75
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:10 +0000

    born necessary no sister her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
