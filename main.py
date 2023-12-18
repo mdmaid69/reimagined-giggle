46f92745cdb40bf51f5f3429cf3107203d3b9c38
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:06 +0000

    inquire bank ought fierce bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
