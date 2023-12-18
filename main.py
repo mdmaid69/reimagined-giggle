3a67eea48b6d3549d59bc55cdbc1de2c8ad95c0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:43 +0000

    copy period cold post afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
