06b81bd0445356649eeb3c0e487912ea7413ab75
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:12 +0000

    roll achieve event total year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_pressure(force, area):
+        return force / area
