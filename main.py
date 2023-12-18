3ee101af0c6467564faff3eefed50c1d7dd7092f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:27 +0000

    broken full whole complain even

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
