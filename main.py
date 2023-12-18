0c8c8a2f865cebeb0e6d2e3a1f01bb578986c1ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:38 +0000

    shop let goout variety his

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
