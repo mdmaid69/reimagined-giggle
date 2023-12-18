7a6765134989c7af8b726da9f24fa8ea55017cf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:56 +0000

    some expression brown smart border

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
