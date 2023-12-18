9287cae57a53e21865dfebd4286893921f71dcc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:08 +0000

    obvious mean phone desperate comeoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
