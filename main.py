541c4ce8a7691120f0e518d9cde1e33dc52e1231
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:30:15 +0000

    correction everyone lock advise passage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
