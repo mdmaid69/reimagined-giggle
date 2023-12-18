f32e07476f9a6a7cd7f9e0364505571a3d66057b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:35 +0000

    suit remove desperate tomorrow smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
