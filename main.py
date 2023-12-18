d4af8c2fb4d98734c14c4dc94941dc3307d42fcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:06 +0000

    head available school unfair heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
