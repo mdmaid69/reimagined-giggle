ea1e64a6747c901c7af65bb528d6d69c24aeca14
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:24 +0000

    scrape underneath sea prefer rare

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
