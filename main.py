ad63ea7c8913a3f1bf7d774246bebc6c80c86fc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:52 +0000

    attention roll combine help and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
