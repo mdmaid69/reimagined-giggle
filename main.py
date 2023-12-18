8196a8eadef134522f893332fd20d6060f654e62
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:39 +0000

    important trick praise relation interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
