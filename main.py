ea1ffc082d1148f324b2e4c35de3c446bd0dcb61
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:09 +0000

    profit afternoon telephone tape say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
