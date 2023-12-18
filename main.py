a30b9e095e841206873cdbcb4555297392f78f17
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:29 +0000

    discuss noise comeup exploration comeback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
