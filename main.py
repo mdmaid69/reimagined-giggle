fd629f818846b7a052b5a6b1a0c203ba82067f94
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:29 +0000

    cross convenient deaf feeling pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
