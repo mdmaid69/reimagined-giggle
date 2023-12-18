b943a4cdfaa214eff491f0b375111afa80b3d2d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:42 +0000

    train rest exactly hate nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
