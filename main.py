e5c320717a7b8df5316ce01f5c03b100700a6597
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:16 +0000

    country score here holdup lucky

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
