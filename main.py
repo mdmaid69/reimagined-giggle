a4eaedbb10e27fa1112741a39196234539adbfef
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:40 +0000

    other sort task likely suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
