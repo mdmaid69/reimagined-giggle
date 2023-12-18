9e52198a4930bcc74a2599955e0c82ae569661b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:31 +0000

    under into fact tape my

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def count_elements(lst):
-        return len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
