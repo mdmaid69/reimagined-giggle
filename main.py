c48c7c1bc43759503216ae454b460a03cb177a19
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:25:17 +0000

    point shock belong turndown chance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def reverse_string(s):
-        return s[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
