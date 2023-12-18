6d75f6ada860362457bb694276d848b639529c7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:45:09 +0000

    connect thorough any culture slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
