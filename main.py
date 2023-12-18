1c2de790d8225c2df015bff7a9dcae926e7e0ec3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:19 +0000

    confidence ago pleasant same night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
