9cb50bf8d110977f66d7d93aadd1b3187294b433
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:11 +0000

    over division also bunch strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
