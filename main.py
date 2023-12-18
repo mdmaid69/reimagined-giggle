b82766601b5d71818441f8dc922199fbc0941b47
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:34 +0000

    book downstairs software real me

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def add_numbers(a, b):
+        return a + b
