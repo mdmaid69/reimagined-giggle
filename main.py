86eb34ec5de427b67b3f4f9cb4e3e5ca99f609c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:09 +0000

    wave have scheme breakinto everyone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_work(force, distance):
+        return force * distance
