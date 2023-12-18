001e4ddd242aa635c421985ff3ab699c48dd08f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:34 +0000

    characteristic rob fire cheek confuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
