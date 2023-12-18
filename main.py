e924f484187f158326524f4f07389d71f30ea69c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:08 +0000

    watch second wish attempt and

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_min(numbers):
-        return min(numbers)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
