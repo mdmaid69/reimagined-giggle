aef1c689958660d7f87368076d341a422abb84f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:02 +0000

    break brief however pass cover

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_string_from_array(array):
+        return array.tobytes()
