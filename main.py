2f4bd9436aeb391fb2a63eb8fe4eb8646754ac25
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:56 +0000

    begin embarrassing turnup put charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
