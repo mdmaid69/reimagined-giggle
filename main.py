583e526129295a7e4c0cdb11da84dbf876669314
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:59 +0000

    various schedule squash concern have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
