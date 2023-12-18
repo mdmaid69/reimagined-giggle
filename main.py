18c8a1b946ac39c3f14b441f5f6a8db69c62affa
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:51 +0000

    patience goout influence distant exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
