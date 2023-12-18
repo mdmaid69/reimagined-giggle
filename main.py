5e8b01bc055b34aac15e8c7a245342cce2dcaa13
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:52 +0000

    tie criticism yes typical suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
