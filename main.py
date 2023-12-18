a535f598dd729090a2b27503b792d14db5681cfc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:40 +0000

    pretty from height try evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
