fec174c03a2dd8a0adaf7d821f6fd3240302826d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:45 +0000

    ashamed unpleasant any tooth share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
