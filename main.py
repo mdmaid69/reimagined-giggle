5edf66c0cb6951532c2af3497397fe4d7038c4ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:48 +0000

    snow recognize interfere terrible little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_itemsize(array):
+        return array.itemsize
