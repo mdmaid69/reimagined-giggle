2c97dfffc8a6d3e196f9a51fea05229b415ca58b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:01 +0000

    strange complete public steal turn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-for i in range(10): print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
