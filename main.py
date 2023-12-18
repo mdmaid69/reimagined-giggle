dd9b6ad651ea92e96ba91055faa0762833796e15
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:36 +0000

    design price character shade quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
