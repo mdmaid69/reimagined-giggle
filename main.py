e42b8f97cdc347d3e01a99102bf14a64a10433e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:07 +0000

    condition lay operation stay considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
