6794967f530a52810e565c96121000efae44dc09
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:44 +0000

    pretty confident bin later luck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_even(n):
+        return n % 2 == 0
