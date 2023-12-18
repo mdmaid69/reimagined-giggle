b20fda1c851611664665f922a0c9ab71790bc260
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:44:40 +0000

    stranger yourself charge paper exercise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_min(lst):
-        return min(lst)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_int(array):
+        return int(array[0])
