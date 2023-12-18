d6b5bbf394693b0d324b59c99141ecf5a99f8314
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:31 +0000

    too telephone prevent short plant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
