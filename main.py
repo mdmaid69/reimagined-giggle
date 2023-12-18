c12f71b8bd46040cf82fb491a937a49f9e792cf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:32:11 +0000

    why consider themselves grade to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
