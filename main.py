6599e0792bb3c009b7e57297a40f45d3fd90f95b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:07 +0000

    sport can complete godown vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
