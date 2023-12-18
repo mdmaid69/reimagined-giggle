19f9cb9c79c872a38ea9198bfebbf9930d182f68
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:07 +0000

    scare round constantly other sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import random
+print(random.randint(0, 100))
