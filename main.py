ab4a808f7c3e750182b00c45186e9155482a6460
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:15:57 +0000

    spread somehow prepare thick too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
