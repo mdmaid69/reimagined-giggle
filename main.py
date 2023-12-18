c7aa119fc59f2df3732d0abaa67cdf453ea96760
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:41 +0000

    calm crowd whatever get importance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
