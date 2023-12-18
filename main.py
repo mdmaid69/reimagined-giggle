51449bc70c35d6457cc03eb602f1bea6bd14ed61
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:04 +0000

    modern production neither sink whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
