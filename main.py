cb6b17150f3d95fa0ccc96efc496c3866390d53f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:48 +0000

    desperate pocket guilty cause serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
