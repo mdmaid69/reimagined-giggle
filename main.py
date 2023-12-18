581a32a60e94227b8e176b8d56231d18257733ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:51:39 +0000

    breakup cost not payment middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
