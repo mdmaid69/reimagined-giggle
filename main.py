45a1a7d4a1aa72daafa287a53ca3d002b7500b59
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:37:17 +0000

    passenger anywhere sting behind suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def cube_number(x):
-        return x**3
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_density(mass, volume):
+        return mass / volume
