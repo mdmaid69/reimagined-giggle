5d5e854e5b5715a48d0c720c24361e70f13163e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:53 +0000

    burst instead love battery may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
