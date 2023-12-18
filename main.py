ea64639952c1d298229a972cd0987e7bc2fa822b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:05:26 +0000

    major professional situation fat winter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
