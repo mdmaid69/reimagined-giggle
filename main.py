248619cd2a25975674b3735fb8396dc213f85afa
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:43 +0000

    surface 564encourage fortunate much turnout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def reverse_string(s):
+        return s[::-1]
