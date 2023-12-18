9963e205c3328854969f90f8415cc93532f0c981
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:01:49 +0000

    crash cough responsible ilovedicks radio

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
