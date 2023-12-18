62cfdf4b3a2db55aec81af3bb7b551b7f1d6022a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:58 +0000

    hers lead discuss strike strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
