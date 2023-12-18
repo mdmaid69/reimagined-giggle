4f12c0744e319af84cf826651a6df9789afd9fc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:42 +0000

    accommodation go sit scared frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
