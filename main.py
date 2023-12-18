4ec4646a26153886ca610374b68943bcd9433132
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:28 +0000

    account string past temperature become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
