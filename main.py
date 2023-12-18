76fca7af99815398b9a574ef6468674eedec8b29
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:32 +0000

    ring together perform hold onto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
