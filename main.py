58473889c8a730ed4b6ed44799b814d503d55469
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:12 +0000

    apply plain approval there geton

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def square_number(x):
-        return x**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
