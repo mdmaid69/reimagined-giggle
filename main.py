8baa1312a1ddb0073ec430d98e9915a590e1aa89
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:39 +0000

    fetch simple thinkthrough loose brush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
