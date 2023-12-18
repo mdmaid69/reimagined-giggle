16329b757e3b9994ce84b95d453fbf2a18089608
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:14 +0000

    bringback tool generally somebody thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
