d3be575942a99e1800b84970c2e3184e5e6872f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:35:58 +0000

    concerned information hurt stage promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
