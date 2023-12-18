5a757d31846068f9a3cac3eb0410f2aed0c1c90b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:36 +0000

    intend backwards estimate workon hard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
