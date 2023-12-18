2780bb06d0404dbdb7908a5d76620c10a3c31ea2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:33 +0000

    approval proof attempt shade who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
