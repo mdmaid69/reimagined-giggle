6d4df94a7d106fae6b17a82ab32a0065c928ff3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:22 +0000

    real little recovery office healthy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-for i in range(5):
-        print(i)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
