46eba07e00cd08312d52db0dbc443e8367c77cc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:40:57 +0000

    tour you period like comeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
