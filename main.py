82f79f04b765045023f4bfecc50f2636ffdd72ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:27:31 +0000

    bunch concentration nor sky walk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
