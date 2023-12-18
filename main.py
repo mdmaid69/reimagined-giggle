23fa6f7f837290cd35e03309eba0632fac42634e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:45 +0000

    production about friendly present sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def sort_list(lst):
+        return sorted(lst)
