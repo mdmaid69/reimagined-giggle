9c7bd69eae854a59a3f04d2018bc1b2287f03cbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:29:32 +0000

    up miss train smell goout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_set(array):
+        return set(array)
