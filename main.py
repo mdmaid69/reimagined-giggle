f844e9e2704884314c42bff0eb40efb35123d53e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:54 +0000

    emotion shut suffer much fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def greet(name):
+        print(f"Hello, {name}!")
