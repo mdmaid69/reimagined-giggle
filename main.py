027233e3b75e108b7082c0f7624c695671ac1de3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:08 +0000

    keepaway pass culture pass mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
