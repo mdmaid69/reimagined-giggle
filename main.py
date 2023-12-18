7c59710e2aa2d3841702665e783299ecd0096cb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:45 +0000

    them comeup what pick wrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+print(time.time())
