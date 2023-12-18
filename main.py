21ba6624e07d5558050f860825a2457a90011843
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:42 +0000

    promise difficult holdon understand slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
