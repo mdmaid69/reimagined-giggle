a6eedd3b62d78802fdf3c6ad8113b50ab67fa12c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:00 +0000

    water cry sensible able maybe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
