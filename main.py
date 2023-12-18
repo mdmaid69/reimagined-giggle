6642744951b9c1274dc0533ad313ae7817cac5aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:49 +0000

    original paper skill loan matter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
