43be1e486d0d76339a56bf11432bdaf9e1143f40
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:52 +0000

    upset introduction spoil choose custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
