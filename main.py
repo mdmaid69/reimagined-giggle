4cdd061dfde0312af67abe3bfb9029c4de86f0a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:09 +0000

    real interrupt tragedy imagine later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
