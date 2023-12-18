183c0e4813c2dd07852e8193b2abe367dc5b4cfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:22 +0000

    kick deep breakinto information Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
