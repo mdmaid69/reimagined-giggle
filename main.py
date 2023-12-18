2bd29bbd0537123a3f35afed829d9d221862126b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:22 +0000

    keepup remain change interested keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def cube_number(x):
+        return x**3
+  def cube_number(x):
+        return x**3
