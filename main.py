d056ecf0a3889334151aede38c4336a22c9448e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:15 +0000

    mend use giveout cross appeal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
