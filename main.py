1a697c6090886e3793d52e99681dd71ae4e1785d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:08 +0000

    afternoon fairly great bus kick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
