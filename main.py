cbcf193ab729c3f0f4430d3bf5bd552a27d57d20
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:59 +0000

    storm party theirs border float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
