18ae58a3bd0301af2c252290e695b3acf37ab921
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:05:56 +0000

    relax means team callup consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_sine(x):
+        return math.sin(x)
