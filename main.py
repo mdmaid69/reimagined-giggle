31d6609ac6dfa7953c918c5c753bd508327df03d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:53 +0000

    gradually away affect callup trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
