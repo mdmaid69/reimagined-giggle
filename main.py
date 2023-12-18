c9f5a8e3c62149a38961431ac3a9209f060df1be
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:31 +0000

    risk attractive over nose least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
