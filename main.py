adf5cb065d24f698542c984f893f64b64b1748d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:57 +0000

    coat gap loan surprised thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
