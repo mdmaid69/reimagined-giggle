d55a7b1e8d1895dd4a2899a35a98e2f700c7ca1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:24 +0000

    street against disappear step her

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
