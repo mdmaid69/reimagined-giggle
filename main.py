bd311175d7dd12a3a958b080424a3045f5bc1bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:54 +0000

    hot machine direct morning sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
