fb3f855bee87ff526e9bbc97c6177c7b787945eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:50 +0000

    occasion fetch any crime game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
