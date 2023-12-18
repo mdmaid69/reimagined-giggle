29b52d2963135e9d8b8a242c6ce4141f24b16202
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:14:12 +0000

    gift burn capital knowledge now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
