bf7b61542caae3102347ce395ff45f4735b67d29
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:05 +0000

    question worse draw dream sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_float(array):
+        return float(array[0])
