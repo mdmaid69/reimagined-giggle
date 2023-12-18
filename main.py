1252280b9b40318c79635d3f5162f5a754533494
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:36 +0000

    bath polite foot dinner impression

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
