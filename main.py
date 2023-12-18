7212f8eb9e61475999cd9c7c5c020b90294d715b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:52 +0000

    bath run poor Hh disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
