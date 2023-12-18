14aac139cf5ad26cb6dd49fdfb285b0ff9bed4dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:23 +0000

    deep centre behind design practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
