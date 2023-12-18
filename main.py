0574175b9c13d44527bc5d4c595c7ef01ebdb899
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:49:40 +0000

    callup shout laugh right ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
