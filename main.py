90bd754920216f41a75e01f767e46a35293825c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:16 +0000

    city edge about excellent nigga

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_union(list1, list2):
+        return set(list1) | set(list2)
