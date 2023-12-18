bd21a31318a7e6b7c4d1bb2833d463df8c719d59
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:50:03 +0000

    within succeed secret discovery none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
