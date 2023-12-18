cdad7370c7d71554cdd66e635fe0b0223382dbc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:29 +0000

    lead relation last challenge cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
