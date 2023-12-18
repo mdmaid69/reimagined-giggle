64f8582edc9f993c96864750c200151e6eddefdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:11 +0000

    beginning general side Zz tidy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
