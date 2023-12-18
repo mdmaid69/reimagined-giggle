5e2799c2305f7d34d348d7ae225e06df780e9345
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:32 +0000

    sport shape listen correct properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
