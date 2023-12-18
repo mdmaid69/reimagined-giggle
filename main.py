6ada39ad073bf82aa992529b1f921d8202edb15c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:34:29 +0000

    method as drive regret air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_odd(n):
+        return n % 2 != 0
