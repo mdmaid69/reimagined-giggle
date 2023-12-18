0545488740383f7bad02b7fcfc5d3a2581d3f68e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:36 +0000

    action fault voice contrast accidentally

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
