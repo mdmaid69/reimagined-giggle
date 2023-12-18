6986cb972e9ac1f4740453f21e99c3d8e5a2d371
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:03 +0000

    pronounce search satisfied mail could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
