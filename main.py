13e046aeea14152d198d8b6ccbbd4cb4ccc8e96e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:14 +0000

    trade petrol store completely lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
