9bd54d34c400b3fb19848cb728cb9f8408398e6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:59:22 +0000

    unless tense ours thin fair

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
