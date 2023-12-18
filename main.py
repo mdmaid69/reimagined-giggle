fbccf63c6fbfd7905942c3693bff808e270fdc4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:34 +0000

    declaration guess at absolutely Gg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
