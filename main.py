6ca6752c275fb2d75d5ff213c9d157cc931115fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:55 +0000

    considering rain false impressed confused

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
