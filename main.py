17463a077ea4f97870ecaf49069ca36a35229dfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:23 +0000

    port alone thinkof shade straight

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print([x**2 for x in range(10)])
