8daa38c26a8d61cfc220cc706161e872bfe49445
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:03 +0000

    calloff suggestion hit sharp report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
