77f838a43b40ba394a46cb6020bf8ee6ca58bf92
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:40 +0000

    proof bringup none suddenly brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def reverse_list(lst):
-        return lst[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+print([x**2 for x in range(10)])
