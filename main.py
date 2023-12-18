370362d57203a52c542fda7cccf0cf9dfb663382
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:19 +0000

    crash example argue university hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
