8f42530e28f09b057658b9ea7f3cbf0458d969a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:21:40 +0000

    comfort then improvement theirs once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def clear_array(array):
+        array *= 0
