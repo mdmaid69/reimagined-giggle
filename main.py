f8c57118b256e147fcf6988bae1abadf81415e90
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:43 +0000

    distant unusual range material call

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
