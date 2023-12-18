3abb8f1c9f8ef6a6cd8fbac58beea09d0e6691ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:05 +0000

    sleep afford load turndown official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-def get_time_since_epoch():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
