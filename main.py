7bfb6871a9c569cb44aad22e6c769842620fe091
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:11 +0000

    transport future guide accommodation still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def sort_list(lst):
+        return sorted(lst)
+def subtract_numbers(x, y):
+        return x - y
