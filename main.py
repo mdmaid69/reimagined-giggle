95c0031652d49b19daef15878d1ba39d18252f45
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:41 +0000

    hospital harm above shallow constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
