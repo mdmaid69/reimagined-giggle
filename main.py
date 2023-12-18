06eb40c5fc4e7e4dba3fe5cea151d66e4cf87716
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:17 +0000

    mouth face model practise point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
