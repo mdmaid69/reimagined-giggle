4621aa97a88a01613c2ca860291805d544a1449d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:27 +0000

    shallow small practise refuse pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def square_number(x):
+        return x**2
