473a262826911c78f0810b0e7eb68aa52b554845
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:57 +0000

    unlikely prepare return position turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_octal(n):
+        return oct(n)
