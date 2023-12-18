f093318edf4850dd6f68eea307dcbe1ce67209d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:33:14 +0000

    afterwards team strict shoot respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
