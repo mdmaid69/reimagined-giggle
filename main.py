53a04c8adc423b78e487d20944c443647cfff69c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:34 +0000

    confuse custom post certain awake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
