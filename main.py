2b2ff0b0fa60b982000a30577173d569cf2d77b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:52:36 +0000

    back importance difference curious until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
