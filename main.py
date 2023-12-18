7be96f9b43288777a8ff7844964290d03b28a5bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:29 +0000

    suppose claim juice comment sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
