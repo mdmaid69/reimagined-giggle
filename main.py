0446596ccefc1f7c50089e65b9eec45c36dc1639
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:25 +0000

    praise new label you turninto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
