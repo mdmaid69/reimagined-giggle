8b4b2d766728714efa73143ced39a010e6e02a39
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:42 +0000

    interruption pin forget heart clue

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
