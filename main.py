1830aedb8be8a1ef12498f1afe3b541691667df4
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:42 +0000

    report light kind command extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
