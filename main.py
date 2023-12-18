de40d38b54e589316600dc07f46dcb4bc9a4aa39
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:38 +0000

    him exhibition middle principle exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
