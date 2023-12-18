28a84b5e32d705a28861b82268a8d86d7de4465d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:48 +0000

    line face turnon guard honest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def convert_array_to_list(array):
+        return array.tolist()
