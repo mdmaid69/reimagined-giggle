fd99bbacd12868e1446723a82b814db32b6c7444
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:22 +0000

    refuse previous store cheat drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
