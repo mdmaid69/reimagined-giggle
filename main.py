aa89bde0ca10806fca7d9ae31906182adf88b2e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:54:57 +0000

    permission priority situation degree substance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
