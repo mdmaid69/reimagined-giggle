161b25f923982d178b92e6f0778e8245ec584682
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:38 +0000

    practical tour frame tidy pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
