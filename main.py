0a515bdc02ce79c9b6a05e08a5c47743e646ad99
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:47 +0000

    science excited ring contact history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
