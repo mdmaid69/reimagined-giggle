e038c6974b4a0f7451c2c22a6872794cdde4a2d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:52 +0000

    tour funny fire personal pity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
+  def count_elements(lst):
+        return len(lst)
