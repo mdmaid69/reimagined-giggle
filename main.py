b473193c3a603057c2f4b8f13db3a329f5fff77c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:43 +0000

    coat crowd hot split fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
