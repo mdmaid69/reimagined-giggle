dd288db350d7ad04795c587206c2c8b00104bc3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:32 +0000

    form free as office favourite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
