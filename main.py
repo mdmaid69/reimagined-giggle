41a311f39830aba3ec105e2ddee66707fe939090
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:59 +0000

    course lay visit turnover under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
