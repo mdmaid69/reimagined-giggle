c4b1178b93a053a2a0b6977f107f4e3fa2d0e4c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:31 +0000

    professional event thin independent unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def square_number(x):
+        return x**2
