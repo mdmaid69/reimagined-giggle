6afdf54abe443790484287d6ea38cff5d4e6c7f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:29 +0000

    breakinto help deliver happy precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
