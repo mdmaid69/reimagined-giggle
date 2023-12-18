752455499342798b6d0d4590fc064be804f4d2bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:53 +0000

    stranger size pinch to part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
