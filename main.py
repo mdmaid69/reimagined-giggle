aa6ec3e889552c65901b8d529a094d7d34ec6226
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:54 +0000

    and comefrom Kk spill odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
