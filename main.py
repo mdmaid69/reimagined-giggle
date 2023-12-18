1054f9238d982969ae789ef7028b3e0490ba1ae0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:01 +0000

    call stay dinner method exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import time
-def get_current_time():
-        return time.time()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
