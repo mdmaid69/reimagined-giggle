6fbb61fd1584735acfb396fd152d225962142165
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:01 +0000

    before month actual copy calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
