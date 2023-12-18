5c2970dd247773cb3280309341b0419a5e0c167f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:59 +0000

    than steep apart drive include

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
