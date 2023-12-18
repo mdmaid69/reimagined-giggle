8dbdc086803deb4a00da36e47306f54aefe6b14b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:26 +0000

    impressive appeal clean owe motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
