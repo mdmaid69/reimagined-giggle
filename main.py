324f305e55370c7a24eea7d08c56e43b8d81a5e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:58 +0000

    fairly prize influence from free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
