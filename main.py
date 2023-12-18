5c8e0a95902ca7fbf58f244f3753d83ea78357ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:48 +0000

    legal height book better music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
