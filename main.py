ffe1c1666c252e6216cb9fc1315629ebfa3ee595
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:11 +0000

    concentration unless method popular field

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
