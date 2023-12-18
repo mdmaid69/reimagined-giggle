715df94f5b47beb5d1c050bb715c050ec5619179
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:26 +0000

    comeback breakin use unlucky discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
