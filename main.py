3959bfd63c452af5b274b52b9bc64bc12250d552
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:55 +0000

    hard declare union unpleasant gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
