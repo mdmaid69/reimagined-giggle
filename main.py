2e6bc5d8065a191d3ff780d5fad8869fad010f94
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:13 +0000

    exactly guess certainly steady hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def split_path(path):
+        return os.path.split(path)
