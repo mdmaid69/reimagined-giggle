54a38a57a7bbfc5258de90cd4a739147012c468f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:32 +0000

    back image dress push boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
