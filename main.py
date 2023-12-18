0b8afa9ca5869d481f0857c5c15ba380203b3788
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:51 +0000

    dark ring born love frequent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
