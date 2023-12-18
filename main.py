d6a457cc02a0fb48cc679a5cfb77beae79ea0505
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:45:32 +0000

    shape present custom spring ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
