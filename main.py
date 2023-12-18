d80c16e8695224ff2369b3a58b9dee64bd617344
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:43 +0000

    organization rest uniform flow occasion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
