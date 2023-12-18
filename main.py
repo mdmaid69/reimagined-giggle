f71748b17b712981735fdc7a0676bf211d46fb3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:07 +0000

    plain nice nervous thank such

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
