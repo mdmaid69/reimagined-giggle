25c41ba1178382cd7251a57f9aa0c4b4373d6823
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:24 +0000

    wear doctor attempt cost weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
