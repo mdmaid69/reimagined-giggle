faa402b260d3d61f3be87113b5f86a038c96e831
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:17 +0000

    mood base principle complete substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
