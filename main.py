ec9e622da0c8c2b01444b28241015a719f32f58e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:54 +0000

    bring board bet list comeback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
