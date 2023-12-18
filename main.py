05ce6e8d37e280ad5eb820f0b2449b3eb0a8ad63
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:11 +0000

    last tongue state extraordinary what

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
