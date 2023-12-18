6f57413519af12d7a5a6eb9f68d1615115d326a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:41 +0000

    heat apart must like base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
