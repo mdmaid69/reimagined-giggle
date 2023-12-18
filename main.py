a544e101c201de702ffe0711663a1eb6193a33ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:03:20 +0000

    respect pick slice probably separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+print([x**2 for x in range(10)])
