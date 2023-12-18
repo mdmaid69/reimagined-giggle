ecf17f0405b4dfdfa5ab1afe3b2391dc67c10ee7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:42 +0000

    last agree godown office medium

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
