f759b24e89ba3d873df049f791b58944216b2bd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:38 +0000

    degree determined 573entertainment turndown challenge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
