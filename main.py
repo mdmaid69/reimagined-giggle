90ab87be778adb7908d3395337c19c181bbade75
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:27 +0000

    unhappy rather search greeting correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
 import array
 def get_array_itemsize(array):
         return array.itemsize
