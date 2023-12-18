159818f18073ec06ffe6bb8bda9518733a40a544
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:43 +0000

    important push fail port convenient

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sys
+def print_python_version():
+        return sys.version
