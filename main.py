e890dd5fd8fb3b0cf54a6f7588adcee018774ef5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:37 +0000

    us travel everything immediately thought

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
