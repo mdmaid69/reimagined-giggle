621e51cb03a5020529e8861730077400cace9b0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:11 +0000

    guess mystery movement cheat cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
