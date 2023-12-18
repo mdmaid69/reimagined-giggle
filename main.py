2991765cb0e31349726590c7ada3041d66186592
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:15 +0000

    mean march cap smell disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
