eb717bc5f2accdcfac3c0eed7e67bff32c6a50a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:36 +0000

    reduction peaceful talk tour apparently

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_average(lst):
+        return sum(lst) / len(lst)
