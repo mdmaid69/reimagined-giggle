356aaffaad120c38b3a9667c5faaaf6e2d151bee
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:59 +0000

    yourself range turn way ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_power(work, time):
+        return work / time
