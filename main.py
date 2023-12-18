8cbf6f3e8fb0b8f6753ca6bd65833bce764c29e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:31:33 +0000

    area pencil apologizeapologise great classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
