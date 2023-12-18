d433a4ca3bdde42746e6df1ed1489817c5e1b4ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:46 +0000

    he approval bye war exactly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
