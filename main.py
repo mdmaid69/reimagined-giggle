4b5856517eb35ba42a4fdfca205d84d529995c14
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:25 +0000

    swimming close environment head seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
