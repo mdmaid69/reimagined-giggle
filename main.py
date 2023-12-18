2600d3d85dc9478b09d959771ffba580a218ade1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:43 +0000

    right learn collect answer getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
