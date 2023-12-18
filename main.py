a9080b5719ba9417347ac84dc84a1bb5b5c78b3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:21 +0000

    accept hair ourselves probably like

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
