22c882725f0085be2d8be5b73542132034c4a771
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:13 +0000

    can bag zero holdout back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
