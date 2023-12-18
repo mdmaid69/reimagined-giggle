43c68e5d9a44781ed768a818b094ff584bc0271f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:30 +0000

    important passenger anywayanyhow into common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+print(sys.version)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
