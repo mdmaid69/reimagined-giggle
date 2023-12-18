6575cf4a5302996e643b87e6b2991e9684dae3c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:22 +0000

    process while dick lookback(on) travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
