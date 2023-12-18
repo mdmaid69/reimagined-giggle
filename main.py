2551e869d7c1389c813d7790bffc0798ecc68c98
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:41 +0000

    point noisy minority neither defend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
