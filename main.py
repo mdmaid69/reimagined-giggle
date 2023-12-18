b3979424cea7dfec02603cfc8e590963ba48b1fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:49:35 +0000

    measure underneath strength state lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
