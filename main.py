ab12f2e9a5ce34c799229d2c878da20ab74ed073
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:58 +0000

    introduce disappear smart goodbye treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
