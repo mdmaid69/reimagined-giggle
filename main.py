cc4a0aec1a02dbf27228b7e7fe80ab2a9444a341
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:33 +0000

    standard tie total side conversation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
