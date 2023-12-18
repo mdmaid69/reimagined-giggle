f44ea042b2366757da3929d8d55613a42a2ecab7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:50 +0000

    ability trip back speed hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
