69c12fff01e810a11ba116c56f0ef647dc20c1d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:42 +0000

    slice boat calloff autumn exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
