efdd677856474a57e65f624fa1f025c09338ba38
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:54 +0000

    drive better proud actual judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
