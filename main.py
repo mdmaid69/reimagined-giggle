ebe3bdc19a71c03481028125c20b0a6ed660f04f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:52:09 +0000

    radio crowded straight settle priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
