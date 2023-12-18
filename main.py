dd083a91b9bea61c2d8fa9327571515a3fb64c58
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:34 +0000

    discussion friendly present down sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
