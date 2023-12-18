d1b5e9710f1f55a2448febb1db1646861dbd79e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:28:49 +0000

    leave care moon makeup bread

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
