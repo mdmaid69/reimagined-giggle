7c8d9fa97f8afb6d3211bc0e0d4da6a4ddab7005
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:04 +0000

    quickly completely takeoff original wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
