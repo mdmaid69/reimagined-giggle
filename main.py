512691afd949e17bd7cc10c37f7932590907ccc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:53 +0000

    shock behind pen round appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
