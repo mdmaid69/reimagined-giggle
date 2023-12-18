0f46bf47ed447fc35bc6da90531b666165614186
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:16 +0000

    someone tidy secret fault equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
