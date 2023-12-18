04bbdea00a59347c7670c3132144984f446909a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:59:28 +0000

    honest surprised practical interview concentrate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
