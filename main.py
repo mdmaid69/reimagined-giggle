ec52ce89f302d70e2b9d139d58f0297a34939e0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:16:18 +0000

    contain produce scheme breakinto level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_list_from_array(array):
+        return array.tolist()
