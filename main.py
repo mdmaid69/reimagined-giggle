61362a119f9a7bda233845fee690b4a5caf83718
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:34:46 +0000

    talk wind plan gift run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
