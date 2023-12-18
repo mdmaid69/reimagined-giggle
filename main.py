72d7bb2b99bcf3a1361f8202b7e4c1924f33b891
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:09 +0000

    thanks represent free strict bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
