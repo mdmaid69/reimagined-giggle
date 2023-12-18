3765a9dee1f5cbecb0c157798e82abdbef3d7c65
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:26 +0000

    wealth production tense journey attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
