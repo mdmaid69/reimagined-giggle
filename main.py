6ae2b5cb81367b71c478b21f382b0346af016469
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:30 +0000

    early work negative own breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
