0cdb41cde1db2f294859a4cc0cc0b96f3a57c61e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:58 +0000

    getback herself target loose cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
