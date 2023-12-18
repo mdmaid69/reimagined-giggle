10ab0cd66df53234834db211df52a11a0d3b7583
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:18 +0000

    practical earth separate wander sudden

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
