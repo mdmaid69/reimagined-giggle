2b3b5848302cc4e8eaf419aef9d7c80a951bc88f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:16 +0000

    breakinto no threaten holdout organization

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
