996d59dada2476e980932bd23e2739c3f133c360
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:54 +0000

    unable amount clear style cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
