44df954b4a483d79c5c2320fc74cd6b95a0f9c77
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:01:47 +0000

    used crash confuse close wherever

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
