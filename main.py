04bd59372b6331bb580b178c2f18507ce474e6f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:42 +0000

    only tool story already slice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_max(numbers):
+        return max(numbers)
+def calculate_speed(distance, time):
+        return distance / time
