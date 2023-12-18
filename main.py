be4c1564bedb87290aad8d3213e72f14df61d581
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:21 +0000

    bush vote excuse remark anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
