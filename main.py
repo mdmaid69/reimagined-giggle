b4ba34ee34caa78e0ccc6e9a7a7fb1f29c4888fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:51 +0000

    disapprove concentrate callback defend begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
