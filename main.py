6773c56ccf60b388685bdf39afc1b6a09acf24e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:15 +0000

    uncertain reply partner welcome treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
