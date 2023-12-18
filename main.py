4398f43218e63fac8d2069daf3a9f07e79deb33a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:21 +0000

    afternoon business sick commit mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
