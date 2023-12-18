5705cb8a7083af1734a3ae0f0b4a780c16289559
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:01 +0000

    confuse simple emphasis hit friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+for i in range(5):
+        print(i)
