77fce48e3301b38a84a9d19eac3709c91d276434
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:46 +0000

    comment Mm medium sit tend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
