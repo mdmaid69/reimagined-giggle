58d2ed43e1917a4cec4988e87bec2f7b1933d1c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:19 +0000

    generous reasonable wake contribute time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
