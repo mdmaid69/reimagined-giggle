a2b30ef22620e8b7dbc19a955e1d001c35a2755d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:57 +0000

    mad tooth complete rough flood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
