9b3bdbdc3826d66fd6b15aedd7b8a357643a10c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:03 +0000

    smile side break feeling bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
