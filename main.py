e74235a6459867b7e99414a33f9af80e32d89264
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:09 +0000

    represent funny label land legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def remove_duplicates(lst):
+        return list(set(lst))
