00504225b76e2537406d5c8795a0e322856f8b61
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:14 +0000

    bound like copy guess tidy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
