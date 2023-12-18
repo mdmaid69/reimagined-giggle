95d1a815ef30ceb3ffe2b392484a6e2efaf94f3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:30 +0000

    rubbish jump wash 564encourage ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+name = "Python"
+print("Hello,", name)
