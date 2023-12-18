b1c2645ac998a2fac1af1f7a141676f0e09266c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:44 +0000

    wish traffic hair party like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
