b05eb7e92bfda9467f3cf98af589b4c04b23c6c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:48 +0000

    start train trap mother blue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
