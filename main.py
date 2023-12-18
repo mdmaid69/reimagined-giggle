a8233b79f66d2709b9053da00077e155fbd8b1a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:23 +0000

    once mouth illness corner importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
