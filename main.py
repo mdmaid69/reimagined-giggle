9c7ad2a9a15c8a2fdcf0bc86226fef984a20f61b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:16 +0000

    relief allow charge communication pass

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
