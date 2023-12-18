c453c4ca26b5e4fd570fdf7beae3ab702d3da40b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:59 +0000

    land market comein downstairs snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def add_numbers(x, y):
+        return x + y
