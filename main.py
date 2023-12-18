7ceb46442f9e909270783e18a1757d268fa5a15b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:40 +0000

    spring grade burst original list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import getpass
 def get_password(prompt):
         return getpass.getpass(prompt)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
