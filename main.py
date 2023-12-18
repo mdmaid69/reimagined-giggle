9cf6a7962412dedf7d54139133fa52a85e7eae0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:01 +0000

    freedom allow bang reliable behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_str(array):
+        return str(array)
