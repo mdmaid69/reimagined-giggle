59cf4b0a82209d05a12664353b4de922c129fa24
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:12 +0000

    bunch decide needle realize straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
