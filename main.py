e498ef2adce40ac52bd4056efe0010614ab05974
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:53 +0000

    control respect talkinto weekend price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
