82d97623f43ee2a579ae441da50a66d2badf430b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:21 +0000

    earth gap dish instruction we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
