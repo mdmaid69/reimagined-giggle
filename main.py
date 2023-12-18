ffc058616bba0f4ff23e8d9e42f5441035d16249
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:22 +0000

    reasonable church lookup thinkof safety

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+print(random.randint(0, 100))
