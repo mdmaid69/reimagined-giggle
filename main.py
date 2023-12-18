334e8a747df626346e07b5f99db9a7068bb8771d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:46 +0000

    rather summer software winter work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
