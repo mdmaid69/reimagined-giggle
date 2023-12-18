e280c08c8d713c1e13e96784c9c48077ce7234d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:22 +0000

    other holdup steer once physical

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
