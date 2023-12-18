980ecedff8fabb4b4dc117272b500fe4d762a0bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:31 +0000

    service rest story everyone brief

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-for i in range(5):
-        print(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
