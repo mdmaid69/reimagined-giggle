bee1b42fbf39c3f33c6a7a7a463975c2653a6938
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:11:31 +0000

    in window however suppose chat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
