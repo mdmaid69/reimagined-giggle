ef3e0dc4d161a81bb9149226fe40976eb2d43830
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:11 +0000

    swing OKorokay keepout we please

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+print(os.getcwd())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
