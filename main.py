349ed4614508dd226b8a76da766a9e0aba30e5bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:25 +0000

    stage estimate edge curious cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
