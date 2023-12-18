8b0e2d5338bd615ea2fbbb73ae7edd9f9c4ab1bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:19 +0000

    regret opposite goaway cross improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
