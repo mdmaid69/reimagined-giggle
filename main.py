6ec060834ab22eab9224bef413257608a0a343da
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:13 +0000

    usually wander most little splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
