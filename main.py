c017c23253c522a08cc1d5739309d116399e0ccb
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:26 +0000

    company Ii police deal gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 def factorial(n):
         if n == 0:
         return 1
