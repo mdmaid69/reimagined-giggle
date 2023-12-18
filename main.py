fc72702eac3c97448fa086982159ee6110704d92
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:08 +0000

    live busy nice thorough rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
