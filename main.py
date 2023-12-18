3ed01fd1d93cfb5f427867d2d09f58dc1bbbfe61
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:42 +0000

    shop compete spoil congratulate wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
