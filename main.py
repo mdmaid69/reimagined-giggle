ee158d69a881511ac2852a1c7ab96a5d2758aa1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:26:20 +0000

    pay clear air they peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
