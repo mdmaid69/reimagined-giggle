9784e6c2564b07a5901fa5e22f6d21074ba0a808
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:50 +0000

    lie ready seem run free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
