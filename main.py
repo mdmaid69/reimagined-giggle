42e8b44007061fc1d4adb6e4c86ac4e2bbd0d94c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:53 +0000

    temper corner drawing criticism arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
