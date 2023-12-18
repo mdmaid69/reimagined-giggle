970e42d182a0ad489e21f5f2f4e80bc9c09d7f24
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:22 +0000

    skin medium comeover name cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
