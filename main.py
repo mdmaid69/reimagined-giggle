a345f9cd5ff400652cf5a4637a95c9eae84e16f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:25:52 +0000

    high hope ask opposite brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def cube_number(x):
+        return x**3
