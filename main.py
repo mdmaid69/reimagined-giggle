be85a779862f15c325781ada7348ab46e52c0c1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:31 +0000

    bush treat pin why easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
