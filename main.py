0dde56fc9ccf541d16ffb4f28e5110aedc39a069
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:28 +0000

    love stop weekend visit scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
