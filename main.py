4f0dadeea902cc5405473b3a920235c5b02a162f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:45 +0000

    awake total breakfast forever treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
