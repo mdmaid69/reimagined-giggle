6760a015879451eae1f38bb974c61cb5f9326d73
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:08 +0000

    feel splash dance like ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-print(random.randint(0, 100))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
