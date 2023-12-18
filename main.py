a55a51db5b1c09c9c1f3f49695908eb96b2658f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:12 +0000

    myself wheel press thank embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_today_date():
+        return datetime.date.today()
