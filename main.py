ba2540c6dcc161fc806adb70dedca0b722a9cbcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:53 +0000

    among relax terrible change give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
