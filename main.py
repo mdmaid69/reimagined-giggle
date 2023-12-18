525f8bcc479a5bacd760bdcec55a475fb240a96b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:46 +0000

    under ago easy post effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
