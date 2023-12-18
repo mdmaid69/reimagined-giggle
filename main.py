da617f9e18aaaa42281bc657e711133c27cf4af1
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:16 +0000

    shop description everywhere week table

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
