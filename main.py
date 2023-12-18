36bb121b5c8bc347e4bbb3052cc89741447ca740
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:39 +0000

    burst workout upwards exhibition weather

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
