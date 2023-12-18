75f2c5a64d189275cfe3c3dbe0b8cdb84c35942e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:23 +0000

    goup rare character key tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
