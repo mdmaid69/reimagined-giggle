f374eae52ea9cec81b4a3dbb96a7a41e4bde943a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:25 +0000

    fast callback sign instant wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
