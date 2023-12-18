6105be318b37e8770947f068a4e2e440c8395547
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:48 +0000

    fall gradually respect available official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
