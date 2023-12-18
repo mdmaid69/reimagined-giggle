d22c6a81de771e6456b2d51ba81ec2ad16f80ef5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:40 +0000

    burn there art night feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
