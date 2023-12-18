68d44087836328dec4cf07740a89309c4bcd0f90
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:50 +0000

    translate tread relationship achieve deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   def divide_numbers(x, y):
         return x / y if y != 0 else "Cannot divide by zero"
