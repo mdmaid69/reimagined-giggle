a62d5c84073d1a789fcd0dcb8f1f27c7aa2a92c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:38:49 +0000

    Bb record pity lie they

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+print(sys.version)
+def count_characters(sentence):
+        return len(sentence)
