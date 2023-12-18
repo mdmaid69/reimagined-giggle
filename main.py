b914952a8c860e09f8b68f7c01ff035c77fd6d98
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:55 +0000

    legal trick cry wonder regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
