e0e8d00fc0a704dbd133ea2cc9b59347bff10540
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:31 +0000

    character official tie wish scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import time
-print(time.time())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
