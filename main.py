69051f3bfdd9721e8ad43090a05b3545e15146a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:02:19 +0000

    behind complete across argue their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
