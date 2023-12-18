6065e2e33ac772892ce2104d3664e678e9fb473f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:40:14 +0000

    joke perform surface average middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
