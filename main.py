b18f0497a50a1dd6000476004c06f36478d529fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:39 +0000

    furniture cheek load shopping bottom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def multiply_numbers(x, y):
-        return x * y
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
