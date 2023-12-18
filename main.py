1e0dc61db50eaf5b7672d54e1bacc9bd2d9ed2f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:38 +0000

    begin deaf compete know content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
