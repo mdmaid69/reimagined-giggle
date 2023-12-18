35c43af62412803f3d7b35ea3643b1b83b2fbdc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:18 +0000

    love store sea year comein

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+for i in range(5):
+        print(i)
