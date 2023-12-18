6d01dbb28fcb71bb1b8434c79b82df9ad167b631
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:47 +0000

    length what naturally off switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
