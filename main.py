efa5a305bb69e587cf0eb18fd859c777585ad3ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:40 +0000

    sleep giveup production insist weekend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
