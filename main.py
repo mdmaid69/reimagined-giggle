ce017631c1301d5d9afa9565c19b21ad413dbf47
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:37 +0000

    town comment performance sister goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
