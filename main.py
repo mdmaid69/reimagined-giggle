38ee14427270c2bf766755bdb4704a92a424ec2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:24 +0000

    when means light surprised expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_speed(distance, time):
+        return distance / time
