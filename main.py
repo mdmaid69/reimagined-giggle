2d537eec2d3a6781af77f604cbc6819bfc0479e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:59 +0000

    blue spread interested confident pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
