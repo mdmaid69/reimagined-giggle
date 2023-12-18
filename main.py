1f3ccb9c6903db8fb407eb70746f1e5e9cd647ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:02 +0000

    news tired form ocean 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
