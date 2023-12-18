ad891e10113ef934ea4c7f17331eb070be1e9837
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:05 +0000

    pass brave bye steady responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def count_characters(sentence):
+        return len(sentence)
