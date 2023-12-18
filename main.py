6665cf4c6c76f33c08129cd25cc0f469ab1c0e21
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:49 +0000

    stir guard experiment ill stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
