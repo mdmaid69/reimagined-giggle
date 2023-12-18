f82266cb42c251d410de727669af7221769674e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:56 +0000

    shine rubbish official black swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
