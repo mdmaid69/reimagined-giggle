ccc7bbfebb43ca65a66055735fbdbe057b2d463d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:07:42 +0000

    instrument variety worried their needle

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def is_even(n):
-        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
