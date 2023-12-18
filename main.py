1a8cd20534fb2dee72b36e34b6f1869240f6f9ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:50 +0000

    schedule rough sell familiar quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
