8edbf4473222472b44e6dfb3d037e3bb521c8d68
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:44:47 +0000

    between scene Hh occasional class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
