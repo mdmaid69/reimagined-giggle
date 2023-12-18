c11b8f1062dabaea9050acb29dfd24086398d249
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:57 +0000

    genuine blame bringforward dead declare

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
