51cb1f3a95655dc2af0bb5afce9a17ac2edbc3bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:35 +0000

    risk describe port out well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
