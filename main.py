09f6490a6b3e981f7eb1df12c0ffa1409c641758
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:28 +0000

    neck holdout arrange pronunciation callback

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
