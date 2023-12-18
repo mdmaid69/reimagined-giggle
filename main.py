306c597b1e199006db2d0a187b92137dfa41220a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:11 +0000

    home remain tall tight sing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tensorflow as tf
-print(tf.__version__)
+import math
+print(math.pi)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
