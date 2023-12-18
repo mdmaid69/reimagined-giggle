511f70deda4c6368d61497c461a7dfa7ab477cbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:33 +0000

    mixture regional squeeze flood effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_contains_item(array, item):
+        return item in array
