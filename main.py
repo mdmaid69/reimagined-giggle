6f0dc15b26519313f55d7cb21633d704e053dc52
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:06 +0000

    pause arrange subject clearly careless

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import array
-def get_array_length(array):
-        return len(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
