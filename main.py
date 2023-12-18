dc8b89d3cfc641522c712ae8fec518e26dcc04b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:29 +0000

    serious set different yourself especially

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
