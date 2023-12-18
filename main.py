b7e51d657c0ac67fdf0e4734b71aa65171f3b7f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:18 +0000

    less vision nonsense glass seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
