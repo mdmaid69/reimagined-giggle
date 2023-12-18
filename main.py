8d48bfa2c77cb9ff4944fde93a6e6cda60399678
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:47 +0000

    new mood only field mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
