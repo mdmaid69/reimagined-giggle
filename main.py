0c458e6c13b38fd5315adf1d36047e3ccd12b16e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:37 +0000

    beyond Ll circumstance whisper smile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_even(n):
+        return n % 2 == 0
