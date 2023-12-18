617ade77b4ec5aa7f0fe76b9c82cbad0657518c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:06 +0000

    shut still wash belong bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
