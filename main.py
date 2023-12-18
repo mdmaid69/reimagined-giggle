2d9df22544cddd4d2f02d86e3c113adf3e3a708f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:45 +0000

    bringforward hard move tight mystery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
