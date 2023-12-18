423fea8f45aa003bd68190860462387e581bb455
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:18 +0000

    what ticket spare only weekend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
