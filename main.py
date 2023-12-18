536e4b5a78e3aaf60eb32cdddae3648a455135e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:48 +0000

    dress yourself shrink equipment up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
