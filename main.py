84c66271c7ac8d83d6aa6da17309872652a4f08d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:29 +0000

    negative follow goon help very

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
