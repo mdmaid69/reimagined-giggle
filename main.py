9bdeb2cd05d78ed2942080250284f2eb2ec53462
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:36 +0000

    goon trade characteristic join feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
