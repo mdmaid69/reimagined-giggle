1bdc2d2bda730a53d0b2efd18841c3d44a4a4f3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:42 +0000

    bank gas belong suffer complete

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
