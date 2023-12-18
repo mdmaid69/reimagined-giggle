bb7b7578a04423734e3ee908294bb33b7e672930
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:26 +0000

    would describe flight talk 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
