14139ed681571e3a9657de9d719ed4e18fa828ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:13 +0000

    twist promise bother spread confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
