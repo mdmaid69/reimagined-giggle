f06e9ce1e69a2dbe2cbb2eaded7034c993be6eee
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:29 +0000

    noone call set what stay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
