2c46d778fde5f66442ef2e322b391cbb61dbee50
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:46 +0000

    forward fresh happy string television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
