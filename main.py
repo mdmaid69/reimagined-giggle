77cd3f0374dd75ebd1a855b043f34fed8d56362b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:37 +0000

    rest mysterious thinkthrough tax left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-print("Hello, world!")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
