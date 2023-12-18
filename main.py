3f4b38f4b18122931a408b91c5dc5cedc7691ce6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:43:28 +0000

    order acceptable actually comment Cc

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
