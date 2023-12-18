48df686a3d7c719668591a5191177c82c5a96e2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:17 +0000

    unkind extremely Dd whenever limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
