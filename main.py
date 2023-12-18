469b36d98ff67f13c5e5e08e397b08b199124cb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:50 +0000

    spend water bet silent model

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.frombytes(bytes)
         return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
