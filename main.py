acec3d0013b569174c04988862abef2522f1916d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:28 +0000

    beneath improvement book threaten with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
