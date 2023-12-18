85cb1351d182593f447e2f31cc083d634d421009
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:19 +0000

    whistle turnout unfortunate shock copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def is_even(n):
-        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_current_directory():
+        return os.getcwd()
