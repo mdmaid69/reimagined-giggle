b7c320d4b77856b0121ad4eb758c198c33c065c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:27 +0000

    temper confusing situation section need

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
