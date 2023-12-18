99c514eae7e318aac84f4004a8cb45035294b52f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:40 +0000

    able hair insist poor glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
