243c2c7e4d16f17860b061cb8dde07abe52b5d70
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:22 +0000

    moral rush account lie finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def cube_number(x):
-        return x**3
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
