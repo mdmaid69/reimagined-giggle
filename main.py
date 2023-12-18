50934f7399524f96b1edbaea118b3f9a472069e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:43 +0000

    hospital runout line step smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
