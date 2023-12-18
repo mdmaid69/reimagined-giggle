a451b9ebd4ee929ea981b42d56b89ed78196ec13
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:16 +0000

    the important mixture grade name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
