52ed65a5bf81c1fc351d86b2531e925c7a31a0c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:12 +0000

    rest quick game impression aim

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_volume(length, width, height):
+        return length * width * height
