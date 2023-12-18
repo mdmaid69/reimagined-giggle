afbc15e7b286e43361e7f28f6a53a092f24fe6f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:32 +0000

    worry describe workon account sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+x = 10
+y = 20
+print("Sum:", x + y)
+def greet(name):
+        print(f"Hello, {name}!")
