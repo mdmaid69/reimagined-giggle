6681cf7bd59391714efa343bdc8a8604463877db
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:18 +0000

    traffic attention estimate tool serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
