e2ae0745875307c7c2814c9a2d726522af7c6998
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:30 +0000

    black tense issue system tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
