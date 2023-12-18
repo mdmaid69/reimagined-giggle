a7be8508b60949830ef96e110d1282dbef4c44d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:13:24 +0000

    advantage off in out phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
