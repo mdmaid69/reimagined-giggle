79d7456f9d1452921e4794080a846e1d9c0ffbd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:24 +0000

    neat downstairs touch anyone entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
