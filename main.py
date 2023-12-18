24216be1104c1ad6499488456368e947961ba267
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:23 +0000

    actual either delight back paper

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
+name = "Python"
+print("Hello,", name)
