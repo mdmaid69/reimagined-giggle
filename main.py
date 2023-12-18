7464af4c3f95ac21534ce53e39decb53046fee30
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:13 +0000

    listenfor sign note promise straight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
