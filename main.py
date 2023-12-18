d061c812ce6b4937b42cbf6e38cea8a33c4be702
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:03:27 +0000

    solve mistake bit chief talkback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
