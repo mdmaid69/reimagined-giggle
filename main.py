725dcd5e1984a6f3972c810ff6dd72de276dbe28
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:51 +0000

    because importance house press distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
