1707a59b874daaac1c0f2aee69700c4058823c68
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:57 +0000

    limit loan on trap crime

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_string():
+        return collections.UserString()
