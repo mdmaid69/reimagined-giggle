19fed949733db408291f8fadbcbd3a6c724f81a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:48 +0000

    without influence how familiar work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
