dca24b896864bbec97564d40d8056ed4c1fca461
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:23 +0000

    flight effect related close that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_item(array, i):
+        return array[i]
