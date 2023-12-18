dee26b1ec4207f056516df3791bd1ba1d5cd16a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:17 +0000

    when takeback scale turnon and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
