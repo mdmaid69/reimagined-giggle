bc55578b70269abf25eb18335059b85941884d88
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:29:23 +0000

    order noise ear goout afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def count_words(sentence):
+        return len(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
