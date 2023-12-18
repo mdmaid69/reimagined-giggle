1bceb3fb61fdf8b34559a10013450028d6ccfe67
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:29:41 +0000

    trial workout phone sink knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
