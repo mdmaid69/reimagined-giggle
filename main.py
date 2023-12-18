37786aa1c29b7f89b8982f3871e068259df1b1a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:34 +0000

    trick odd have until attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
