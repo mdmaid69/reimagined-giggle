e828046c1ae1d6cddc168fe2c69d2bb86c9ff9a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:15 +0000

    walk paper freeze instruction natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
