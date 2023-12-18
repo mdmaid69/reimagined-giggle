fa7d13685784a183f549653b3a9976e4129a9cd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:36 +0000

    state escape deserve safe squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
