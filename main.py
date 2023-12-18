dad98e8248489b03bdfbc4f69dad969a202b9c83
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:17 +0000

    fit challenge food yes turndown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
