7ee466f5f8c3367c000b3cb4f4e18dfb2815114e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:46 +0000

    criticism pinch firm opposite risk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_float(array):
+        return float(array[0])
