3ad68fe4779f3b4c9870f2781159ebf94a37e6c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:21 +0000

    getaway official slightly very lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
