35aa54c2c27685323ab92fc05a42ef16e0f5021d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:32 +0000

    include grand walk big avoid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_max(numbers):
+        return max(numbers)
