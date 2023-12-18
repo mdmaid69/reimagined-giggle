d6bb1b1c91ecbbf81c50fc50ead6a0ed82016e97
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:10 +0000

    matter marry sleep ability deliberately

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
