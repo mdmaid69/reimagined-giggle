6803d801d8952e2068d7caeb19d65f49d8264fa1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:43 +0000

    knowledge join immediately cover repair

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
