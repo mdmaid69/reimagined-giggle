70635dc4e584f200e3e9990b93652eb4228b11c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:28 +0000

    cough demonstrate beside noise straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
