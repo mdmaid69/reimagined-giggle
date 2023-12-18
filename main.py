98eb8b0f817955074100268d2e6d4cfdd1c41289
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:40 +0000

    fast press callout waste wash

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_logarithm(base, x):
+        return math.log(x, base)
