fd7a448d3554239afc5e1517cc7ab8902cd95e10
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:24 +0000

    accept hot compete trick Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
