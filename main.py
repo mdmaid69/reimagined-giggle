ff277e2f53da297c1302a0deea112d4cc355d1dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:06 +0000

    still mainly typical damp over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
