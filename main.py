b85ad110e228178455a978acacb78e77d59d18fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:52:16 +0000

    strong smell 572enter consist ordinary

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
