32ebe3ebf0030ac985f1c90be44c644f7afd8860
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:45:45 +0000

    tip class bang sea party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
