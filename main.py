572e440b8b7b4a58638b8964c551a91768701dfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:43:24 +0000

    about poor weak yesterday keep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def reverse_array(array):
+        array.reverse()
