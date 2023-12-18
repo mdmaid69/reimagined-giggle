cee15b72dd8e895167019a85065165abb60fd337
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:49 +0000

    fashion treat correct right delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_current_working_directory():
+        return os.getcwd()
