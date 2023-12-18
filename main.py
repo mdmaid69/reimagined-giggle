ae7fb9aa46fa839cf8d626d3599c8afdfc398bbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:46 +0000

    Ee 564encourage realize smooth arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
