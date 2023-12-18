0e64afdfd26be8a657fc11b2c5a3ebb2d7aa3b34
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:34:51 +0000

    lay past normal breath any

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+print(re.match("h.*o", "hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
