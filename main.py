39b85320b3b08878bb7ad088a7e3ae6996f504ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:52 +0000

    carry across dry outside class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
