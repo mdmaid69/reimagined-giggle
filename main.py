971ed57981230f04cc90a1e6d8bf1a71ece143c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:23 +0000

    he gradually order circle crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
