f61d062440c99939d87ab025a799a50f701ad224
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:29 +0000

    why machine possession decision relation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
