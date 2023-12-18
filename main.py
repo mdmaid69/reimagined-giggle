f310f155ccdb2ef14fb387f1681396845f00b6be
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:37:55 +0000

    funny change connect exercise since

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def remove_duplicates(lst):
+        return list(set(lst))
