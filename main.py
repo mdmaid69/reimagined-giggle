aebfc09d757f86076df74ee0602c9c95ff00478f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:56 +0000

    mess tax obtain tune left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_power(work, time):
-        return work / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
