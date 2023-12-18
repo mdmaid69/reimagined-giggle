4a35eb94621c7c5c054edf978ffaf3bcab847665
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:35 +0000

    runinto person learn book sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
