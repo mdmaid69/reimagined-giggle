4893e904947b244a1c906258c9bb29d5d0f834ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:04 +0000

    steal pupil belong slow satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
