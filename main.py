3db212b0071d479de28067bf196ca5ce3584752f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:40 +0000

    unusual near flat bag object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
