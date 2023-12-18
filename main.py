9a734b6807191fa2b0b054399935dc49362d4a8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:22 +0000

    snow hear suit tip turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import re
+print(re.match("h.*o", "hello world"))
