345e63170b0dd7c8fddcb2b955c4bcddb199761d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:46:24 +0000

    equal tour spray confident petrol

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
