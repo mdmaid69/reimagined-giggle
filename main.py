4309c21c155a9292488c76f905e7b4bf5d17de87
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:45 +0000

    temper floor original bad certainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
