6ef38fb8784668989b08d432ea3c3506a5f0dc57
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:35 +0000

    movement pronounce interest pencil Jj

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
