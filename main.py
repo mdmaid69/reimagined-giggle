778757b9d1e6ce3e5ff15951d8d59f7047134d6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:26 +0000

    illness bring dick cheek kind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
