fe5c602e94b32e259747b2711a9ff1b05ff1fc9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:35 +0000

    attitude display hello game loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
