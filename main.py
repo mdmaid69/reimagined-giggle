3a14847134068d962af0a5d6f20592638a5eca69
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:10 +0000

    whatever notice how statement low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
