e8f7456b0e89d09139561e970d2270554e1aafec
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:07 +0000

    pleasure interruption group weekend unkind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def clear_array(array):
-        array *= 0
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
