c6d8eb7ce3e8901831fdeecfad0b7577a7a0622e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:49 +0000

    near offer survive remain error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
