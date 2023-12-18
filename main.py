99aab9d524e7fb237ae96999b01abbfce06569e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:01 +0000

    bread tall hurt building fear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
