e39f6b0d8067d70a29e282f24206a5edd03c7509
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:03 +0000

    tooth estimate anxious band own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
