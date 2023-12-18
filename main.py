b9d0a92d8a732b48dfc3532e28d45c9d65ab00f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:28 +0000

    believe 571enough anywayanyhow field shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area(radius):
+        return 3.14 * radius * radius
