afa869a2e43d8e1d47719bf4c99b39549a64958a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:28 +0000

    fall friendly slip computer callat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def convert_to_binary(n):
+        return bin(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
