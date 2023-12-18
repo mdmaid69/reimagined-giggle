6a5c170cb994423c0a1fa19266dd84ffc3025579
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:48 +0000

    desperate divide flat serious wide

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def add_numbers(x, y):
+        return x + y
