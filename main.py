cb41396bd655ab2699c6666070599cc840e9689a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:04 +0000

    make hardly possible rank describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
