6fddf120bc2d1d023c791b9710d8e66bbcd56b5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:26 +0000

    back wind team back combine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
