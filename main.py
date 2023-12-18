637b8bdead77cdd6dbf32f9495ab4424d4ee88db
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:08:29 +0000

    progress attention loss sort expect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
