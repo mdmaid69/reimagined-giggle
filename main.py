546142163c6841dc2e76a9d63081cd232746a135
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:53 +0000

    clue wide describe life test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def find_min(lst):
+        return min(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
