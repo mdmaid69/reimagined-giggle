70c2c309c474d36655d04262baeb9879481a6ad9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:32:29 +0000

    mind suggestion grade sing branch

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
