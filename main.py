4ccb28fa9109e6926a21a68b2d3b949941f6fd10
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:09 +0000

    soil before trust mystery fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
