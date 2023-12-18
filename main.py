f7627ce67b0ae65ab8aa7bb5eed28eaa7df3b674
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:20 +0000

    cope cross Rr price operate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
