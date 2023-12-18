f89f50591a008d05a5ab7a8acfc19b1c5ad6611a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:55:10 +0000

    slide set concerning bread past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
