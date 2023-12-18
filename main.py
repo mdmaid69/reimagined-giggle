844b7d28d06b96c46ce2fc51bb277230f85d597e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:26:14 +0000

    OKorokay holdout big worth far

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def print_python_version():
+        return sys.version
