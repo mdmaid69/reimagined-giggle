13a75164cdae8417c156734c0a5e74113f07a0f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:31 +0000

    exercise should confident confused act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
