9c6a2b653319154d006be609d87127e977752b18
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:28 +0000

    front middle complete scrap operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
