5f9c8839bf99e0a29c7e2d419f72b70be35cd4ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:01 +0000

    moral hear mother wise except

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
