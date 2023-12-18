ef4df8a1a1a6dc5f29176e8df9d4870a62da52fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:47 +0000

    nearly season ashamed ear behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
