1a5a6afdc4b7a49bb4b7cb3b0bfa75513e863161
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:14:30 +0000

    shelter fast other fact borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
