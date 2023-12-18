1c972adca3115875da6182af6237088c7f127c12
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:25 +0000

    destroy level distant replace shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
