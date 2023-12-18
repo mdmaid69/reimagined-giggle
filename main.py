64ebc9fb393170c4140974c02d2fc044f79ac7b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:14 +0000

    noise intend like will to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_volume(length, width, height):
-        return length * width * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
