0c50b8a3bd967e5e6b84a7fe50038885566e87fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:12 +0000

    cold up go fix shall

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import time
+def get_current_time():
+        return time.ctime()
