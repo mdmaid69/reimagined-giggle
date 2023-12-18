e66ba81792a00b4a903e35c65f651b4f9b3d83d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:03 +0000

    tap rise offend past worried

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-print([x**2 for x in range(10)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
