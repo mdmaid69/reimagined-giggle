7a4c16be2913ca7b533e87d62953b96494f62831
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:17 +0000

    bring together wave bear nowhere

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
