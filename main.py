d559751546ac5b28c11cdcf3578b2e6352b7a91a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:13:22 +0000

    leave amount meeting doubt left

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
