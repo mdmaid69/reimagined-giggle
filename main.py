1bb83ac930f823ab56d691d3709480c9d44f6a60
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:33 +0000

    present fortunately run contrast loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_min(numbers):
+        return min(numbers)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
