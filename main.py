16e941804302924d1153a67ba8976b9bbadb272f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:56 +0000

    cheap straight whistle goon head

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
