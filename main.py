10dd840bd7064a9dfa59ed8838a39977596fbf90
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:15:46 +0000

    bus class sensible demand final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_bool(array):
+        return bool(array)
