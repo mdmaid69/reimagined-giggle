1bf5a41f66fa8b2a8f11aae28ccf2eb7f739b0b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:45 +0000

    positive occasion suffer belief look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
