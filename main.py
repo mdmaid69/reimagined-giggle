f25da26e963ea708cd85405b1f2f08978ccd8197
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:12 +0000

    there apparently trick greeting backwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def count_elements(lst):
+        return len(lst)
+def greet(name):
+        print(f"Hello, {name}!")
