541644e3d0d8acc66befbf198f073b74b8672c54
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:49 +0000

    plan earn pollution pick all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def multiply_numbers(x, y):
+        return x * y
