6e419be487b78ac139c346c0a3516d15d0668a9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:57 +0000

    trip Qq communication form objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
