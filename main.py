e2280928c99db5bcbd86509ae10c13000faec93a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:54 +0000

    undo confidence noise write name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
