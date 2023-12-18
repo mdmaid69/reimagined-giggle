3891fc293ba981071f6697274027a95c0f892c23
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:37 +0000

    possibility cut heat eat judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
