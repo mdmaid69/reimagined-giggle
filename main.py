c97459ba5cc76401d021b07bc81f5004c5d4e1c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:50 +0000

    last picture promise tense phone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
