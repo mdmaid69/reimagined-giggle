87d60cac7ae245ae902b3ace846481b548d4e564
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:48 +0000

    not comeover sweet harm shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_string():
+        return collections.UserString()
