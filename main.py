7db8661e49ee2e4a2cf3db902a2ef00305557ed7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:12 +0000

    good compare fat excuse influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
