4e5ca19785119eb76e6e5ef261cb6150c4f84097
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:25 +0000

    instead season holdup immediate new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
