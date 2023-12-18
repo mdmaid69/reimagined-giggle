ab1b958824076e5146acee9166c265ba1f8701f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:09 +0000

    me little let turn deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
