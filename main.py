476c3d54c4861efce8763cdfe670d16dcd0abc14
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:38 +0000

    decision reliable deaf brief week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
