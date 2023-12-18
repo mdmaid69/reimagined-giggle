d65032219b7a20ebc3a1e61600fb2cc94169b04d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:12:10 +0000

    above otherwise use lucky trust

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
