4b8c7c2493f538f50898900dd63c52633ff12a38
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:22 +0000

    match needle float care container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-print(math.pi)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
