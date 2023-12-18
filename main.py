312ca1d6540b7511afa7d652734bc8b99e922713
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:14:59 +0000

    detail need child provide major

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
