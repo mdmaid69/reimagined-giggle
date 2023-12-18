334984616400fc9932bcbfc6986b80d5c974921f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:34:39 +0000

    control exaggerate minute movement so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
