b6bd26fc7170ee3d789babe9f84a2dc1cc238509
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:55 +0000

    spare ball dare comeout importance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
