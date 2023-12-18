de29f636ca8cd65db2e791053c6c47732d446f34
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:45 +0000

    bend treat noise declaration full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
