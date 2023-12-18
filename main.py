5d60f28d3c5ab74f8a186de31807059d84229b36
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:41 +0000

    please immediate payment badly command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
