03c12b1e58cd31a97202d8ff081474ebf6e45de2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:29 +0000

    minor remove that rise arrangement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
