a9eea6f0e287af31efe90096f6fdc35801be5a8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:11 +0000

    copy cover string cry lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
