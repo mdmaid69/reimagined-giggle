bb8a63fe7a624163ff6442850f2055d077f801ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:08:01 +0000

    world capital runaway pot your

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
