7886cd89d0c3ce4a8cd8566fc4003fa7e9f5bfd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:30 +0000

    far excellent track correct mass

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
