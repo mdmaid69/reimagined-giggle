6135714af388fb93f4813f7c55791786606b9fd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:44 +0000

    strange moon least mean 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
