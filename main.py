e6fbf08b9721222b776f56c665f659836b212bf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:55 +0000

    genuine there rarely meanwhile bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
