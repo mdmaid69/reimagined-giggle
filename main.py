753fe35e12bce10a8ef8b430a389d84ddcd07179
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:54:59 +0000

    something discover who trick check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def count_elements(lst):
-        return len(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
