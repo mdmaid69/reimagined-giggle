e79bf9fac4e38590e28928936c19daf4f32e1bee
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:47 +0000

    hear breakout illness habit introduction

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+for i in range(10): print(i)
+def remove_duplicates(lst):
+        return list(set(lst))
