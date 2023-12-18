30a7d46a08454578870532d50f957be54b2fd335
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:31:08 +0000

    punishment decide climb turnover film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
