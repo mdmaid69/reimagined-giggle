30e86c35ce772b053cb1be00fa87a01480b10c1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:50:34 +0000

    secret this path character over

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
