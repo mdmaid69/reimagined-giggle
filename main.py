23468015fba8c0622deb1d819469c42224dfb03d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:20 +0000

    say geton excuse bunch you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
