cbad76b1d8ff821134c8c11caec1c5fe79e02b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:46 +0000

    blame gradually gap whatever wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
