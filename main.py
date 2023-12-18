9552802ddb0498c82f95fc1445afc5ca654088b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:58 +0000

    exaggerate its majority usually disappointed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
