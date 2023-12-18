0536e120440f73ef6a1b1d2f0ee6ee2e712c5f41
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:48:11 +0000

    or principle gift generally strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
