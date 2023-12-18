ba2a33f221cc4713ce10ea95d24831b9057b365d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:26 +0000

    goodbye about similar several number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
