c1fcb5ed32eee11a42bf56acb20f12728fb9bd84
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:29:28 +0000

    as strict really reject of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
