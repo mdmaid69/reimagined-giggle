7c47be5ec3efbe938933d6ef22d3fa7f046e11dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:01:54 +0000

    cheap sale full actual press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
