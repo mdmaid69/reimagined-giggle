fd2c8495ed6bf75e3dbeb7a2a9b36186e2183d95
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:11 +0000

    protest skin surprise common skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
