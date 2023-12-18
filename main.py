4ed7aa7af13c32c807407141368e164e81c9afe2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:32 +0000

    offence difficult send introduction throw

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
