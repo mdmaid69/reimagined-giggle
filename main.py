fed7f74743a6ac6a459d0790366dcb3fd9084436
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:10 +0000

    sting hospital address pleasure detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
