250ad58e9b00cfd1242d500ae7d516b517c2c7e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:40:54 +0000

    refer tidy opinion preparation thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import re
-print(re.match("h.*o", "hello world"))
+print("Hello, world!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
