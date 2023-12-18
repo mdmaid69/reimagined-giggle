a882c798b71c3f32632e224070ddcf6f47287aa0
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:19 +0000

    admire interview aware summer offend

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
