f37f3aa1cc91e8a65bfa895479a65feeb8e70941
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:58:39 +0000

    process swallow recovery sister modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
