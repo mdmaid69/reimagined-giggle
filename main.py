6d2d8fc9092f68962b3108bdce45e7e2f833e325
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:21:00 +0000

    always guard trick example wear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
