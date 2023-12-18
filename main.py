5cc6c362523fe19858f9ec1a636362c6b1ed7d72
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:09 +0000

    recent practice roof far nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
