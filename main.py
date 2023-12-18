2275598e72bab3f705a73ca9ea7d357a5c3e049e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:41 +0000

    common mystery ready something contain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
