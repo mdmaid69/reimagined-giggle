679957146c60e35bcc15188623ba16a110b79d0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:17 +0000

    shake guard warn hard slowly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
