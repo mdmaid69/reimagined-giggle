f525fa02e9f7d318a38aa1d9be0568cef40ac084
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:25 +0000

    ours thirsty below giveback blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
