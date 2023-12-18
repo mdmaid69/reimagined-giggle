cd85eb8ad618547eef014acdfe4f9e1e6199c739
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:08 +0000

    not exhibition feel season talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
