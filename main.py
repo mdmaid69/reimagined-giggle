a1fe44fb32a37210ffc1435050ade295953cbf12
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:35 +0000

    who careless comein balance injury

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
