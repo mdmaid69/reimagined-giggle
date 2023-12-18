2281d4ade7b6f232bffb4f1f11bdafa3b7467f34
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:48 +0000

    replace immediate walk aim anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
