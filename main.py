54390bb42ff9e1e20aeff96dc9dfe34894df5ae5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:18:41 +0000

    use swallow later price flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
