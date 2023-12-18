de3fa28139507e047eccd84b4d1b14b217459ac8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:38 +0000

    suddenly sort wind declaration shut

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+print([x**2 for x in range(10)])
