89ccac71d41155339a75626cba8d99e892d7db9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:37 +0000

    train moon chance kick ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
