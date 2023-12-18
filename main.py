7ef0e126b952984b416d07bfd23319b83d426c60
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:29 +0000

    slice give past hungry mood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-print(time.time())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
