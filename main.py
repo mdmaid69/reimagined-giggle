00c4d409576d87c3b63fbd77f7d530c88e7cd65f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:17 +0000

    accidental the plan wire whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
