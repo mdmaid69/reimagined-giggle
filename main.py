477257cfff302a3db0b894e4717c507e44f5bc2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:27 +0000

    worry seat correct arrive 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
