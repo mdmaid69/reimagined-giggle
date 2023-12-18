d5ffcd3866c43b6e5b9cfbcc1aa6aa92d688ef52
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:45 +0000

    software even smell objection definite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
