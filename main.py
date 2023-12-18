9ed7e7d0ba8778bc749470fbefb46057b8d15fb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:05 +0000

    concentrate smile sink himself kind

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
