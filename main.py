a03d9fc9297c3973c76384dc0554e5018723da96
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:38 +0000

    fresh around pay exactly dream

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
