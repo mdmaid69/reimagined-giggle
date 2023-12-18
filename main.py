9bbfd2909210522b677b60961ddd05e658b98114
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:29 +0000

    warm rush train survive air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
