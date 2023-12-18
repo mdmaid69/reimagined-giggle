bf8b694a5f2cfec7bed522534387077f2867766d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:44 +0000

    continue ourselves interested pain cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
