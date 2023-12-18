04c022be9a5bc81ba74c1f2085c471f6b5137f4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:59 +0000

    human sort unfortunate disappointed mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
