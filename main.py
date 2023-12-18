c9bf784b9c61b1d4db94e0f0f2f48c80b5919d52
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:56 +0000

    common perfect pin concerning niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
