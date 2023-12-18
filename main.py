9e81726cc89a07cef02474eefe9e3608d62f056f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:16 +0000

    note workupto I deserve cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
