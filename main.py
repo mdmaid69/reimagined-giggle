6b8df15fd31a475fa61029d4ad2967d8d61e6a11
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:31 +0000

    front mix various vision man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
