66fb1b0c74e1cc38d7a15e76d38e5c27345c4ef4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:02:40 +0000

    worried regard common absolutely set

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_list(lst):
+        return sorted(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
