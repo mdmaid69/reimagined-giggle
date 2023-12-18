3dccb272905016c5dbb40a113e71cc62e172f6bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:42 +0000

    computer blame obtain clue nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
