58fbcab881e59082c90f823a6c3409e8d31e4ce0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:20:36 +0000

    eye expression relief century blame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
