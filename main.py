a2379f6cc28fde8e79dff592d2cfdb7196c8594e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:29 +0000

    makeinto neither all comeup flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_str(array):
+        return str(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
