8e6e5c822594335cfccf8595fc226028da1b9e39
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:58 +0000

    chance remember draw bite bottom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
