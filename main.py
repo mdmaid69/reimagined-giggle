a71a978c041d825a9cfdfbcc802a4b2fd1d4b03d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:18 +0000

    decide twist go production prize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
