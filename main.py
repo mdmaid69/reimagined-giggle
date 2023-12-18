886eec35ce3465f8765e439a48d05841ccc32699
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:24 +0000

    this because quick couple nature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
