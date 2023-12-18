b95bcee902207fbe6fd1b315ccea3a3b4bec30a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:21 +0000

    contain do curious argument shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
