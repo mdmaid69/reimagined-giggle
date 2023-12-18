2e5431d3534fd4d225db0aa5776338885ebd2d91
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:08:47 +0000

    charge many share sensitive rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_directory_name(path):
+        return os.path.dirname(path)
