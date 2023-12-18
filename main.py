a9002916cbcb8af0e60cf0cb383a1d74cb4a7fc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:24 +0000

    build realize consult suggestion hungry

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
