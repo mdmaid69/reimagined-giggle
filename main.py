3453bbff1abf89911d62a17f554d79b767f10f28
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:21 +0000

    flight steal splash mine promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def is_odd(n):
-        return n % 2 != 0
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
