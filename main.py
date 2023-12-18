4c07a424b142ca17831855896cf0ee2b6bb9546a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:31 +0000

    close for satisfactory sick possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_string():
+        return collections.UserString()
