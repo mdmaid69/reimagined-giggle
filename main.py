e8015b5037641d93f4d70c793bb35093bdeca989
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:03:24 +0000

    proper regular swallow bound project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
