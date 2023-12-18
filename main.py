884c2a4516d08758ce199291e0d7b83ac692bb93
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:01 +0000

    minority tongue afford fold substance

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+print(sum(range(10)))
