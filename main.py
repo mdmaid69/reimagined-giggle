64975eb88b3369db933edbdbbe3774a551c5f28f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:36 +0000

    thirsty crisis move quit know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
