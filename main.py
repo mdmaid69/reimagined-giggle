8870cc021b8a229ebd44e1391d923eaf9ac5e99e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:40:18 +0000

    branch relationship open turnover recent

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import platform
+def get_os_info():
+        return platform.uname()
+def add_numbers(a, b):
+        return a + b
