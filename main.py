e7e3f6b5c8b2c8fb4b5b77b2395368a63932178d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:27 +0000

    comedown need born century steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
