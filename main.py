838b8ed66667c2e69c532f7cc3c49d803b28c607
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:31 +0000

    climb share art temper press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_density(mass, volume):
-        return mass / volume
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
