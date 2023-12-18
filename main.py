e2c4074d05ce867bd2b9c8715f2adce0b40fb6fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:33 +0000

    violence manage probably peace day

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
