d08568f2ca1cbb9517666741286a18550354d08c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:22:07 +0000

    section tonight best keepaway thick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
