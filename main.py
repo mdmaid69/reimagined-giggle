9a34d4a47bf3e6683bde02cd9e99dd5e261eb52a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:21 +0000

    film way move capital dream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def multiply_numbers(x, y):
-        return x * y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def convert_to_hex(n):
+        return hex(n)
