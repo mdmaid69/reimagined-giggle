127fde279a63eb5581ea967dffacfec078060193
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:47 +0000

    matter have wrap lift present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
