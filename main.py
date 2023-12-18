2e7609ad12d0a36d7a9c56d7eddbd141aab9a3a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:38:18 +0000

    design niggaacademy suffer anybody display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
