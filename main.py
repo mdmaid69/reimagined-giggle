0cd6eb87376ef5168ec57689666c4deaf7885bf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:00:50 +0000

    use tend complaint rare divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
