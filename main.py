75204d8cb83f82081c6e4c6a623eebf581718e75
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:59 +0000

    wing smile workupto foreign pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
