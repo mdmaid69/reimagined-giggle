0e16ec6a804d2d0a3299c2ed413aa71b1dfcb63d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:58 +0000

    occur any shake appear family

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import random
-print(random.randint(0, 100))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
