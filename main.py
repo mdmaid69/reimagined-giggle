9860d8b90723ba6ff4507d80b650b0972a90b561
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:23:13 +0000

    emphasize evening less development waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+x = 10
+y = 20
+print("Sum:", x + y)
