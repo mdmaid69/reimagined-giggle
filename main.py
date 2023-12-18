6654c7c7be5d958728fc15e3447f84592e88c8fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:06 +0000

    rescue his food quality dish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def reverse_list(lst):
+        return lst[::-1]
