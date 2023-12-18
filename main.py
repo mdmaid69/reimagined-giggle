2af737355373106ab62c8938f4cc7c8da95c5fc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:31 +0000

    urgent respect hate recognize their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
