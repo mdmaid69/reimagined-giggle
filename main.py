6e6ce0becfa82685050aa6a5d1520e40309792b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:52 +0000

    at blame bread middle return

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
