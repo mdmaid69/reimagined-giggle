d75675aa56ada9751fa619f8ca85a0a9c68c9104
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:13 +0000

    polite at apologizeapologise example callout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
