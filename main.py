ca5fb0cc4f62df8eded9b47f813e27edc4c1588b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:28 +0000

    pain mine boring 566end prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
