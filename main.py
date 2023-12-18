88e15f99d716610318d251f90b84808424d2627d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:00 +0000

    cause valuable climb back near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
