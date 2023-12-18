d2e49009f3270ac92ac8f3a73243f7b8c0c6bb5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:15 +0000

    telephone poor jump record machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
