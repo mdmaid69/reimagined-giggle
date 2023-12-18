b74601bd4338eccfe69f6248c4d392ca608047fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:45:01 +0000

    stage slice influence tongue platform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def is_even(n):
-        return n % 2 == 0
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
