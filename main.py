5aaccbb7b33f80449cb4e2fd5215c84deec79d67
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:52 +0000

    importance react notice makeup rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_queue():
+        return collections.deque()
