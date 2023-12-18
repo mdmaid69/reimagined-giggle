23cbdcf55eb6447e3ecd0ec120fad9c8fa93bbf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:40 +0000

    church usual target bored sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-def get_current_time():
-        return time.time()
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
