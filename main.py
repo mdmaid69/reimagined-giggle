cac1b31b89bb1b0ec569a71283dbebdda992db53
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:14 +0000

    point town voice cap already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
