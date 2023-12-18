3eb2f1c2837aaaa617d4c1b75b3e73963bba96e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:34:20 +0000

    ago target amount original keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
