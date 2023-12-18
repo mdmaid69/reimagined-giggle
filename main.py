64b466dbc60ffbb445510bedd0b7f3604f3fe78b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:30 +0000

    base Nn Zz shall better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
