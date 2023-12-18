fc2454b2905a70d54d01ec2f138af66b70781d85
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:40 +0000

    keepfrom weak spoil turnaway comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
