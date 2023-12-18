bc3fd93b07ce8d117472b8c2274c1f627e88f3b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:41 +0000

    comeoff mean complete illness first

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
