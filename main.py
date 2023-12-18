481e1eb8c5ec4d904963e98731a4e80c33c60ce3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:04 +0000

    keep glad dance software average

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
