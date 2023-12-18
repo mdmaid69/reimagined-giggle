ef2d30a8fa084914862bad5e6878bd8696f130db
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:04 +0000

    now block workupto operation only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
