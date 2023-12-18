5422104b0db5423471497e083ae4594d498fa705
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:05 +0000

    care offence moral flow genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
