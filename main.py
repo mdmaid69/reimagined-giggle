6f79ef9c7f6943c74faec5316989824a3110e08e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:30 +0000

    face total will wheel minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
