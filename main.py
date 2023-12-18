078d2276f00ff75e38ba1401990e03e93d8351b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:02 +0000

    moment world chase later modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bool(array):
+        return bool(array)
+def sort_list(lst):
+        return sorted(lst)
