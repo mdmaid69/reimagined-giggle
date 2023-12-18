febeb2006bfda238cd5800f3f1e906c3dd824061
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:47 +0000

    failure previous flow march willing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
