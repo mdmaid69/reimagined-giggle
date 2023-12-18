58a448dd2f5dfaf9f51d33508531abb9ef3df561
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:19 +0000

    detail mark disappointed chat onto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
