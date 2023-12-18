00d62e89b84688603dca94c6aa28689a4427d20a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:27 +0000

    accommodation rain ordinary tip occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
