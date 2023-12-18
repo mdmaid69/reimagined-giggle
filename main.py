610be8e61798f586d7afa345e234ee3818678fe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:56 +0000

    defeat path bend pronunciation recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
