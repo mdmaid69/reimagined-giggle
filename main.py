0fa171f4e0e6ebc76bbee44d489b319da27965f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:56 +0000

    land duty measure room obtain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
