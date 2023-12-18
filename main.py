8e58ac4a59e3df128b08dbffa73727e59e556b33
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:27 +0000

    trousers on unit tired whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
