bf2ca7cb6e94aa46029f8c04f7a65402b5890697
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:50 +0000

    dance case rush measure confuse

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+name = "Python"
+print("Hello,", name)
