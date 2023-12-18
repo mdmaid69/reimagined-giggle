660cc825f30fa9395926d435784daab98cdd51ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:58 +0000

    fence think lean extremely pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
