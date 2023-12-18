5c4222243dff682bdb1ed77ece8c190fbd783766
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:41 +0000

    stranger top Ww room carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
