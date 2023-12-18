51af5268df18664e6df5df9f3f432fe862966b10
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:44:10 +0000

    warning tight noone closed answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
