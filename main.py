7aefe6d81c1f9412d0510aa6df2ef8b8f9d74778
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:09 +0000

    runinto regular getup lookup safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
