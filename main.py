abb6dd5256dce36569ea6c7b88b058966205aace
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:47 +0000

    food normal where child do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
