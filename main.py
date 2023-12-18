81336602819713d99d035743561b1adfb129a7ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:47 +0000

    world roll soil from wait

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import re
+print(re.match("h.*o", "hello world"))
