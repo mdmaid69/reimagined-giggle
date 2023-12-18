afdf468330336607cda56ce2ea8079ebf066bc26
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:17:55 +0000

    order with belief fault their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
