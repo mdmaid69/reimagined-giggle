6fcf92f5c8379c602fe8129eb830357a859dfd46
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:31:02 +0000

    flat make force tour danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def cube_number(x):
+        return x**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
