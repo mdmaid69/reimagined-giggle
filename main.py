ba3c1853a9d1fb7235ab697a04c05f853680f06f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:13 +0000

    reduce this declaration roof judge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_base_name(path):
+        return os.path.basename(path)
