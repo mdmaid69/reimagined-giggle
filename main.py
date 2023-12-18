e420593ac40c4c11e1ad0f222e4bb8e54a5426b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:15 +0000

    target leave path stop unlucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_directory_name(path):
         return os.path.dirname(path)
-print("Hello, world!")
