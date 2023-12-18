c3c7d6f9ff2b2f6704c5c2a6548b62304bb11307
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:16 +0000

    where pleasure comein best light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
