ee6415a06aa32cda4867ae2420edb51c426a2b43
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:20 +0000

    especially sleep wild interruption worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
