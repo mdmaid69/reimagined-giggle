f0d03cbd9f3eccd82713de1ff79d155e280ad677
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:59 +0000

    in workout organize lookaround wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
