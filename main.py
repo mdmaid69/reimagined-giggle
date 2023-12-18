cc949e4a6f78fc3db896bbcfd6514ab4d8e6996b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:52 +0000

    pool pipe criticism pain make

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
