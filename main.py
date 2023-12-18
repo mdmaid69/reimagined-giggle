905b48b0b4f587a1335846a802e3b5be48fe7210
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:28:05 +0000

    willing ourselves character slight acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def change_working_directory(path):
+        os.chdir(path)
