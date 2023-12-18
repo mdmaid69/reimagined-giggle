57cf838cdae8994411ea0810294335924acefb08
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:57 +0000

    lend open stretch indeed entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
