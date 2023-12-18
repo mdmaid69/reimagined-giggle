105604e1678c01dedbfcfc73a20a96bdf58c9fe1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:40:51 +0000

    somewhere better season complicated lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
