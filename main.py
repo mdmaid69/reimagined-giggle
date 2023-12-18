dab8fdc1a7e57967553328d60f699a1f4ffc57d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:47 +0000

    handle man school desperate please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
