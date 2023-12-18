0de65048f39613f7e637c0f56addd91e842498ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:49:53 +0000

    surface question lot press partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
