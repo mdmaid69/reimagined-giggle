ce2d05708b6d658eb6d4d33e9eb944833f2d07f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:18 +0000

    space fairly breakfast argument pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
