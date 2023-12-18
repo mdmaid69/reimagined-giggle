24214cd4b6be7523dd8ad2bb53d5553235c82551
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:46 +0000

    meal goodbye present discover tired

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
