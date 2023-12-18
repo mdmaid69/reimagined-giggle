0e2bc46be5356993e9961921248692faf1458f34
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:27:29 +0000

    charge exercise conversation failure juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
