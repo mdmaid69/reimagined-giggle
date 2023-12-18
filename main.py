68290bfc237c93fbbd0ad796edb91be1a48c9344
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:10 +0000

    letter immediately limit play window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
