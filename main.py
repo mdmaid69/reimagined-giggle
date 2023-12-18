c571c419cc035a6d00ebf5a34295e6993f4d65fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:19 +0000

    hang company train tip beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_item(array, i):
+        return array[i]
