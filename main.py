d3c179105e37c026f405efd933f87b0c7ed3d3cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:26 +0000

    soil entrance manage speech heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
