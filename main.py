bd0fde4a927a02adc93aae4fbecfc60b43d162a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:57 +0000

    freedom tense luck air search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
