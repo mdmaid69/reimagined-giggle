0dd381a5fe0a6030cb8a61b0eebac6052ab531a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:01:57 +0000

    split opponent care right soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
