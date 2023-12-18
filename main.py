f9f6633d13d097d950649239bc62603c586aa485
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:10 +0000

    real compete occasion support gather

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
