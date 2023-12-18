fddac86c29f187b0f84a2442f92ad2ea5db49b2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:29:03 +0000

    number attention beneath ready late

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
