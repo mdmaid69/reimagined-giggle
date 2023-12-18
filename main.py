50231a58d2016b80f279e7ea3242d605e92a440b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:49 +0000

    comeout deaf honest recently space

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
