1055c798ebe869ec0432d01eeb84b1f69cd03921
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:12 +0000

    birthday block glass card hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
