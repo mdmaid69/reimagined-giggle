a012ec651d0b654a8f1cb678cc7fd54455962e12
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:41 +0000

    music my raise tough favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
