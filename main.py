8f61318d62e3dfa8479ade33eeee29f59218c43a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:02 +0000

    advantage hungry everyone tell ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
