7a9bbb715abb200b4d959e7649a9de19ff121b87
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:21 +0000

    big thought talkoutof interested light

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
