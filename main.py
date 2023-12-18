e40ae8e7fa7dcd4554144b75159682ba44475b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:54:29 +0000

    thought ever of successful speed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
