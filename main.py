68259a83e079c2afc4aabae9d9f42232c5974963
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:10 +0000

    change drink train game owe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_power(work, time):
-        return work / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def reverse_list(lst):
+        return lst[::-1]
