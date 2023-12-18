966fe8ec3d111ac3e028cbc6679647fda91795f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:42 +0000

    close formal question discuss beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
