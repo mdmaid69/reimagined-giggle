82997ea0754ae4378e71a057ffe883e053e7b149
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:16 +0000

    poor limit hold keepfrom delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
