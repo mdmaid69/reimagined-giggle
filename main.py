e062f64588f0cfbe0f30d7d36f72c8fc87286648
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:05 +0000

    right range receive recovery whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
