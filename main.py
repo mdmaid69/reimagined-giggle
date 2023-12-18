963faa1435671cb673917eec5f5902376e7ff2ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:26 +0000

    slowly confident him blame Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
