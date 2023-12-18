c9315be40404f54dbae80b800f4b6fb08975253d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:42 +0000

    colour for minute borrow compare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
