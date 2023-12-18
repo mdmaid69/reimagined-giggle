1b2b57e55f639b76925f2e6b0250837411c010e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:03 +0000

    date miss produce lie communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
