33323ffe22a31c22df24c901a7390be0a7bb3d54
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:03 +0000

    unite choice choice kill exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
