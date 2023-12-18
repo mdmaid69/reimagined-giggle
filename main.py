d41bfe6a8b7dc22158e75f2ebc6fb0f57abb7125
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:28 +0000

    fortunate again person along learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
