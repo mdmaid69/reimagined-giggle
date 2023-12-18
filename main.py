7c76fe4ed7becfc9c4d3f15eb5d3cc3cb1afad4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:55 +0000

    fault force considering basic history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
