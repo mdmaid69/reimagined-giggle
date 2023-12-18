7142f746908fdd4c5884c395c04711fceeea3362
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:06:48 +0000

    sea big 573entertainment circumstance present

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
