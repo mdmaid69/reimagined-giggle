f0820a14469497b24cd0be915c9a68e6f01573ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:56 +0000

    yours slice plant ocean danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
