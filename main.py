b58aa9c773965939768b1e2e34b031cddf98f1f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:49 +0000

    flat keepoff tidy crazy design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_max(numbers):
+        return max(numbers)
