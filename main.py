ffff035202d90b5b9458ecd961f7e6d555e6b514
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:47 +0000

    duty all terrible truth truth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
