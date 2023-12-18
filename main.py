1d2168f0cd4335ce440b440db1b8342b7658a031
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:01 +0000

    trouble next introduction score favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
