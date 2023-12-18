6bf73e526398b3cd2306191299aaac93c39e9cf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:16:52 +0000

    moment closed description nearly limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
