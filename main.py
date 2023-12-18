513220d33d2dfac912e545d11dfc222aa8affec2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:25 +0000

    slight present extremely fix rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
