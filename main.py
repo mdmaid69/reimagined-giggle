30fa05790bc256b03c68ba2aee9e0f7d89fead5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:01 +0000

    Bb convince gooff tune hospital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
