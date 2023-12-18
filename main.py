2bbb5747ae7bc132a35e2f2de542a37a18184e83
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:44 +0000

    both swing even equal cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
