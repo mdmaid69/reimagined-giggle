8760f99a7b9ef25b5c92f7ce9a0b7a7e0ddb529a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:13 +0000

    minimum steer disapprove form plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
