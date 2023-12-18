c82d656b90f9863ffbf5cb8a078b64ab6eafa042
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:42 +0000

    flow keen keepup imagination lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
