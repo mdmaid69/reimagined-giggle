c6f3ab8075c907849c69464e41a626cc3c0d8c98
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:49:00 +0000

    it extraordinary skin already giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import getpass
+def get_username():
+        return getpass.getuser()
