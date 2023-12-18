ff38a1aae55583aa935ac5fe6a5e99be76a5f503
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:00:12 +0000

    contact high lunch raw should

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-print(math.pi)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
