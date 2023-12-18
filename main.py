09c3c47a1cfa238cf8a861dfa342ba6c143a6aaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:08 +0000

    marry dress before flight taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
