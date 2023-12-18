353e844373bc6ceb560741927020e7b09df6802e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:26 +0000

    joke unkind mind tip hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
