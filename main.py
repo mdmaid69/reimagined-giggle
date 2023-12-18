0f2e15b006c007b781f5fe2e17c2b40a2e04cca4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:30 +0000

    machine custom borrow contrast judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def remove_from_array(array, item):
+        array.remove(item)
+import sys
+def print_python_version():
+        return sys.version
