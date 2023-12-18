3206ff8a474991cace9850ebf54e16f5db636dc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:04 +0000

    lucky official petrol agreement guess

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sys
+print(sys.version)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
