77e1aa9b9de6c7fef8cf51806b571f5aca76561f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:23 +0000

    undo runout deal though touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
