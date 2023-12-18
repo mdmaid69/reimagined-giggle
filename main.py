28ec3b00969b2b1dcab61e5c2c1ff9907bce12d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:48 +0000

    attach record mood left perhaps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def remove_duplicates(lst):
+        return list(set(lst))
