6ffd7874ee080e35c5a6bc6bb4d4fa00664475c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:39 +0000

    cold cover frighten please bright

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
