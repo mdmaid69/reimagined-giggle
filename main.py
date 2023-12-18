f5a8098b9963801be46588b523b8757e03d9407d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:39 +0000

    communication always criticism harm supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import datetime
-print(datetime.datetime.now())
+def get_current_date():
+        return datetime.date.today()
