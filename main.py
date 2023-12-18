1390296325c8855de60f765943c4fd757d244cc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:07:38 +0000

    side strike interview refer proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_memoryview(array):
+        return memoryview(array)
