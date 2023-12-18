3bf3231db7adfe915058dc02d87c8827f6ab8cba
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:43 +0000

    row tired spray swing mark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_square_root(x):
+        return math.sqrt(x)
