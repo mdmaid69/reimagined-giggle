8d8c6452fee1ee36cd7862cffc0737bb1ceb2566
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:43 +0000

    fix regularly next most earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
