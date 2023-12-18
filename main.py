a22a3339fe042dc5f1d9a8b900e88c0a59dde9e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:31 +0000

    appeal wait shine tense space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
