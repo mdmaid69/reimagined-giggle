fd70500209bd35f64c9232a878fd46534c03a4d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:45 +0000

    right succeed love change brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
