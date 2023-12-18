0a8b9d24fb0bd419789dfe16b765336d37776c99
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:43 +0000

    personal office complete defeat attempt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
