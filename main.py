9f3dea13591e04e850373ca32f942cc6e5df8f79
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:16 +0000

    advice urgent relationship use obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
