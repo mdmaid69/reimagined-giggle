7da59166f48cab2a0a63cb6fdb92c50e2fd8059c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:51:47 +0000

    inside exam wave love first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
