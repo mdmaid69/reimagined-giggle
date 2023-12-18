979e13bee43d8381077a26fb19934b8c9e7c3235
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:38 +0000

    stop shut bag 570enjoyable anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
