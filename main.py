d7a107cf9cc583aee800019f512375eb8533561b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:13 +0000

    praise unable back schedule happy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def reverse_list(lst):
-        return lst[::-1]
+def find_max(lst):
+        return max(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
