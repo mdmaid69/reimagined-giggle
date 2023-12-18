3271d9f9537e4226b5f80ad571115fe17cb57255
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:47 +0000

    violent violent freedom whom back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import collections
+def create_user_string():
+        return collections.UserString()
