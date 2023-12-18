d00674da38206a81b84fd02d9f382144ef8b4894
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:12 +0000

    circle path radio hurt reply

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+for i in range(10): print(i)
