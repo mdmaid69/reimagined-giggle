6664c27dd0eee668e5e9269e8639efb158f75c91
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:54 +0000

    smart 566end this need proper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
