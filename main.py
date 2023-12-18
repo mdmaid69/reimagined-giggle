1d18a11367df2fa57ad8054513ae18ddf9f1aef2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:34 +0000

    wake spill now bet surprise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def count_words(sentence):
+        return len(sentence.split())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
