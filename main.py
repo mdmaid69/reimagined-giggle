9eff4d546e35d86b3a02eca24770b5ea142e3744
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:21:23 +0000

    shout case creature owe thinkover

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def cube_number(x):
+        return x**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
