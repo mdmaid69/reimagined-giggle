beec3f36cf6e64994d989d1867bcf9e200945ba7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:03 +0000

    beat recent let no so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
