5c00fc90d1a57bba0031813ce7d7b39b13a92fac
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:17 +0000

    tap special takeaway freeze disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
