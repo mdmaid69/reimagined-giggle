d37e33e5eb872b3550e57cd44bb7e9332399c680
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:19 +0000

    without generally suggestion bath lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
