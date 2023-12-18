5aaea7328fd60dba411192d2c4337856f100f02c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:24 +0000

    battery journey recently basically corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
