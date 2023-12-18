c547b2a4d3fb837c9ceb8ee14f8c8944e906c4a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:58 +0000

    or clever class certain collection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_density(mass, volume):
+        return mass / volume
