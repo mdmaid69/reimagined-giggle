f4fbc03f0170b00e29de5a585f503c3483b9133c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:07:51 +0000

    minimum measure instead petrol stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
