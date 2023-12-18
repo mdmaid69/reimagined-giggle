7027685c08b0b21dd51d998661150555637ee195
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:02 +0000

    set afterwards ring exploration partner

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
