59037a6b0dfc0fe3a7d84e6ce318ae24a733a22b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:47 +0000

    size suck contain downstairs yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
