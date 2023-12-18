4d88f02c8861852cd6b80b53201bc3f8854d1a1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:38 +0000

    lookback(on) sweep should uponon except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
