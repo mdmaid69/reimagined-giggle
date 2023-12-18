4a6750ba126e7601f05b13829747fcc852c0497b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:29 +0000

    material stop burn exit disagree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import collections
+def create_user_list():
+        return collections.UserList()
