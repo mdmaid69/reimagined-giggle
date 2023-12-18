4c64d30c6fdd7c44562c060d67b887a814edce4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:01:13 +0000

    thinkthrough minimum 564encourage breakinto tired

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
