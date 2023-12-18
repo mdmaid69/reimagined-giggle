8a41a38712f558896fe25bb345bdbb8aa44b82c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:42 +0000

    summer pool middle willing remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+def remove_directory(path):
+        os.rmdir(path)
