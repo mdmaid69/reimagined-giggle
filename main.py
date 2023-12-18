df4d687ae058a46311713fee0f2999ad49d96309
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:29 +0000

    hers extra below kick order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
