ef0dc8786222bc07d2d0425963830a826ad9ab5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:27 +0000

    upstairs pronunciation mail bread count

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_min(numbers):
+        return min(numbers)
