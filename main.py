c6b219588e06bc874c13773b81078756acaceafb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:46 +0000

    underneath collection future trap bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
