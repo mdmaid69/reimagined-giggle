bfaa1b57266af12b6dfc5d5e05100d2ee367d4c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:12:15 +0000

    noise path pair variety hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
