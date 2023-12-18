d165894a7b672859ddd2cf9dc44e382613218a21
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:43:22 +0000

    join which powerful consist pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
