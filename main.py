e24710e6a93ff6074861774cc94b2d09a8d19c43
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:27 +0000

    funny sea cross destroy exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
