1fabbdba7fb2f1fb84ef230241dea1bef574fde7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:20:54 +0000

    statement flood her Ww operate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def divide_numbers(x, y):
+        return x / y
