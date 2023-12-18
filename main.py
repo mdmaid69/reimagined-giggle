60adb15bd957ec212edd40cb88dd79188d6f9661
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:23 +0000

    lay order pure brush accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
