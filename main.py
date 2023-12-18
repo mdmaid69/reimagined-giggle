4d15993bc7482a16511f5f9b2be296d235a22e97
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:24 +0000

    strange ourselves service command skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
