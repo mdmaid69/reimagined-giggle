69e37e569ab5d1c6ad8f9ae1bf39209224205827
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:04 +0000

    accommodation estimate talkinto goout surprised

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
