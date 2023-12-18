4bdaea82b6dd0940a135926805075d1f1ee65ec0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:55 +0000

    tradition exaggerate aware count copy

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import os
-print(os.getcwd())
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
