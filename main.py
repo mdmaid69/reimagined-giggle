9ad0cf07efbc8a5627fa2a08d65e58321a4868bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:59:11 +0000

    programme illness word open smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
