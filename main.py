801ce6d5b5f3a0288ccb4fec864cb0a52ab80a1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:45 +0000

    meanwhile zero upstairs oil honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
