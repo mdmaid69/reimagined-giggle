d397b95da036bcff728a39b0593e9b99c02b9669
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:46 +0000

    fun breakinto appeal full himself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
