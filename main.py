937365f8f2f471dc7a35b2f74c0e38311521a140
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:34:05 +0000

    interruption autumn platform straight at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def remove_duplicates(lst):
+        return list(set(lst))
