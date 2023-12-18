8fbc178eb1c656e4ae14cda1e8689bb8df2f490b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:25 +0000

    board either estimate treat official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
