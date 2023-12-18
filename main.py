9370da5225d6b15c90d9cca3d4523a94ea5aa45a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:36 +0000

    suit relax most spring sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_tangent(x):
+        return math.tan(x)
