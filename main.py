445b21d74e0226cc2aefa2d92dd60ffbe34cf0ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:41 +0000

    thick understand quickly star comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
