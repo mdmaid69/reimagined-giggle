07c27e3a2d40280eb39dcd154277cd287f04088f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:06:54 +0000

    again mind finger city building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def cube_number(x):
+        return x**3
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
