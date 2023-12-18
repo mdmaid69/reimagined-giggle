caf6e81c427bcb4cec1201416c223b82564d5c0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:27 +0000

    chat suppose 566end emotion strike

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+print(math.pi)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
