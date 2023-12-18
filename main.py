02ad3151cdd7b4d4fb27ff54443fa943c10a8bf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:30 +0000

    only comedown watch gate offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def is_odd(n):
+        return n % 2 != 0
