61c42cf4e96d74a10eff4e0116f35b0a3fefe03f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:02 +0000

    meanwhile decide wander pride either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
