c2f208944e69ef34695c7a595e2dfcfeb16e5485
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:22 +0000

    prevent book zero weigh taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
