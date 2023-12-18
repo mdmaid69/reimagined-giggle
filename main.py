4c89546ea7ded0ca084e4404a725a47db1afd14f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:56 +0000

    keepfrom imagine rubbish sentence still

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_min(lst):
-        return min(lst)
-print("Hello, world!")
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
