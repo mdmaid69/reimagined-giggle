3685df1cb0f289d5c31b2425f3d35bee875abad2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:40 +0000

    borrow across touch report none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
