aa57685d9f079c5ce3a4973caffdd59aac5cb56e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:32 +0000

    holdup art lookback(on) recently health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_index(array, item):
+        return array.index(item)
