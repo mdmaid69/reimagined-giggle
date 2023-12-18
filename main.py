f962072b0ffe8b9ab335254196dccc96ebc0f9e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:39:02 +0000

    forget someone knowledge tend doubt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
