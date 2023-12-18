72b9a26c25d8dc05e23cd6ac7f56c66b7fb19797
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:06 +0000

    valuable talk noise need city

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
