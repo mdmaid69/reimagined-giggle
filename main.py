0ce0017b34a8a814bbf299333971dab6fa0f85ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:44:15 +0000

    extraordinary form basic twist muscle

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
