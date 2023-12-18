c1fabbc2f58735f4abf0839bfe2ac81967b7aaeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:19 +0000

    long cause high special word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
