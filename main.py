14e15a008ea4614eefe7e225fc21dd2847012870
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:43 +0000

    suckme something splash cover swing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def cube_number(x):
-        return x**3
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
