3a3871a80675a80dd56882a272d5cf5ca79a63f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:59 +0000

    shape keepoff most prepare male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-x = 10
-y = 20
-print("Sum:", x + y)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
