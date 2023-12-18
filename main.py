30e79a037498d8930b6ae8dc1111eb6ddb5cee25
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:26 +0000

    Kk near meeting film remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
