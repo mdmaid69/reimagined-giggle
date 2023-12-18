33b49da07f35dd4c98358defde7ff1d539f698f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:47 +0000

    crime exit realize trust small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
