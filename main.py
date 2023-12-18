628784004a6a389283c7c582147903157469d4d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:36 +0000

    size track protection obvious somehow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
