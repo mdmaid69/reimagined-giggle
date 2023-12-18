63b934ef1cd5f1c8c419c4412157ee09770c1157
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:09 +0000

    ocean wipe attitude Cc thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
