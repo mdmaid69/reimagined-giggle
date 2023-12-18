3f986f560552e00dd16a6352536243d5cef4d55f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:35 +0000

    buy mainly bin legal swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
