d4c20547e7ee5f792600d70f366b765aceb7d77c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:52 +0000

    responsibility raw excuse allow disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def divide_numbers(x, y):
+        return x / y
