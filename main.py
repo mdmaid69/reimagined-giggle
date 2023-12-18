01c8479c78671b0e20cc0f47651506acde8c800e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:55:14 +0000

    meaning expression story period quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
