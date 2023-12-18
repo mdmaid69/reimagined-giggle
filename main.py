1c584c11cecd4b09d6bc309708977caa911170d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:50 +0000

    marry prepare scream think exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def is_odd(n):
-        return n % 2 != 0
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
