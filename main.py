c7f45599e2fc38b6415aaf7cc43736875f132941
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:29 +0000

    powerful abroad holiday provide paper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+print(math.pi)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
