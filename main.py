117c6e779ad0e36bf0063e066ae8c956f3afccc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:19 +0000

    through wet turn read but

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_max(lst):
-        return max(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
