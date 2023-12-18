c8b2c8866be849260795e5f2659d58fc63b4fd82
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:18 +0000

    fence funny careless wide stay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
