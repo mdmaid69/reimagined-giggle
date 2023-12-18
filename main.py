0233dd91c46f2dbb2ee6eb6bbf2ec2a84be184d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:11:55 +0000

    set damp aware tough family

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
