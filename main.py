36e5e90d21fd56b1b1dfcd5ab9ec6cf19cfb2866
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:55:38 +0000

    sensitive indeed since Gg because

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
