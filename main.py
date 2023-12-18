7afd95f50eea244a8544172b80ee3ae50b3a5f51
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:51 +0000

    cheque tidy stranger voice goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
