032ba24a7b6453be1f066ad4ea5434edd30ea65c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:06 +0000

    uncertain power pair bang mad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
