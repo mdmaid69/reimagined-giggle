039f6905bec4ae92325b41443c4af3698b9f083d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:15:20 +0000

    previous point waste let group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def multiply_numbers(x, y):
+        return x * y
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
