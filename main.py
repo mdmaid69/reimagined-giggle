9a1aede513bdb6e302f3a1721a2a9618482a6815
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:15 +0000

    bus serious stamp positive roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
