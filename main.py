6a9dbaa56468fdbfea8e3cbcd74d13b064dd4450
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:50 +0000

    cut black big late comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
