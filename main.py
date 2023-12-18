80bb2864ff3d84daf18b32bb9b401faa3f649e6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:57 +0000

    card treat crime science spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
