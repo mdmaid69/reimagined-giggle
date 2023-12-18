71cb5c652add3943750f8af3cc4d58c55ba0cb05
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:28 +0000

    transparent wonder round port pipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
