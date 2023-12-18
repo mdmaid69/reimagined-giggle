5e1b90daed1e6e1d903a54e83a89548771d9e17f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:04:39 +0000

    sign great circumstance trousers watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
