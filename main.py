2ad8351ac5f2ae059f6effd96c7e9ba40a2a7650
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:42 +0000

    address yourself city finally repeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
