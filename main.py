13b537056102959b699a2f2685371a216ceea5d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:27 +0000

    convincing holiday disapprove far secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_current_working_directory():
+        return os.getcwd()
