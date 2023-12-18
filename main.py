b109fcb3ea9bc3fb3024f59108da250423746923
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:46 +0000

    memory freeze mainly choice freedom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def add_numbers(x, y):
-        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_binary(n):
+        return bin(n)
