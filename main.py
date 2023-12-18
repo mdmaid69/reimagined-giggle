ccf9af878e9405b9d1610cfcd33d923b09a3c870
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:10 +0000

    go lean still quick progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
