b4fd396f0dab83edb14a11edbe08a1338966e52b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:34:39 +0000

    concern become education treat seriously

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+for i in range(10): print(i)
