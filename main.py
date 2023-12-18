01fef8b4c2a1a22b142259198be4e175dee9b483
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:17 +0000

    way give speed naturally protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
