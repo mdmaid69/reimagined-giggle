61a0c0d4bc087dbc38d1e74ac8365fdb73a34732
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:07 +0000

    show quick issue who fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
