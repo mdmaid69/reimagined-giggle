a7b2999e4389f93038c32d08545f1f8b9d33ced7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:35:50 +0000

    bear back remain takeback that

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
