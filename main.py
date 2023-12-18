fefacbae5879713c835e2ef4826491f6498c39d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:04:00 +0000

    square coffee morning there workupto

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
