c42760434710041fa73fa2d8e3c393d64452ba15
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:38 +0000

    nothing touch holiday freedom what

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_elements(lst):
+        return len(lst)
