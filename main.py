ac19851b28a388e58b614ed1040f6a30f10ab350
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:27 +0000

    vision wrap train fall peace

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
