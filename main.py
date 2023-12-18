e7b82aebf47165421d393e05435b9f8b0072c274
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:57 +0000

    sign oil eager godown easy

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
