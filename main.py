18eac02924af16cc9328621bdb789de187039188
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:48:27 +0000

    pocket admit themselves hard ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def get_array_buffer_info(array):
         return array.buffer_info()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def square_number(x):
+        return x**2
