4a27081d00262b22612fff90e09d52cc2cb1ed5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:30 +0000

    sister allow disapprove bet space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
