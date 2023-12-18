46c330fb2293aedbd333b923c557b4581a6a6459
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:58 +0000

    single schedule down fair cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
