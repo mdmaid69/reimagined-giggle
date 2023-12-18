8e046e3dc0b19f617f28053c67c4816022c909a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:08 +0000

    otherwise breakdown him everything television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
