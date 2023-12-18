871b3c9614f55c3d222ab29fa102ac982b701b8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:53 +0000

    sweep rough harm date instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
