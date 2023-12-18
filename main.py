ae95365fced02930bc6312cc1ad7d39ad2e40ac2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:48 +0000

    would cry account flat mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
