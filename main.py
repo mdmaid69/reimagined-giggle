344e3654fe9ba110f3e926c2685f8ceac30a7743
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:01 +0000

    welcome but add stranger imagine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
