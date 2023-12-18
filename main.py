67910a09396f924e0ecb4c8e41314f0e43e44c82
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:18 +0000

    this themselves tie wash exercise

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
