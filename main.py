e7e6322b12b204402f0782327b47a7fe5de65c63
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:37 +0000

    guess satisfied drive taste cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_binary(n):
+        return bin(n)
