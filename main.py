867d2aa67bd6531826df8502a7c2fd46209d3ecf
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:02 +0000

    alone number spend motion fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
