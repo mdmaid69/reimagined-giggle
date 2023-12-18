648ebe107e3a55ba19a85d25f1c2c731f5632c53
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:02 +0000

    fairly contact bit like check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
