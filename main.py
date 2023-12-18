d1b24035c18448a1c96dfd127c26a5c207a6d20c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:56 +0000

    centimetre complete touch write sting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
