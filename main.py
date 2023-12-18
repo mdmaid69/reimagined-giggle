474d49a52426f77caa9aed01d5943cc710b2b947
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:26 +0000

    adjective trap range bang either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
