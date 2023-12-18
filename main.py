c0e944cba66fd2011e6056c3eb4ae36d9e7c5444
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:37 +0000

    switch insist exercise during upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+print("Hello, world!")
+import os
+def change_working_directory(path):
+        os.chdir(path)
