5adb12f576a60b8767e4516804af2aceaa8f3558
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:56 +0000

    offend damp least join get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
