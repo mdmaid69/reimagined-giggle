46248cb0b29b189ef7e4d48ca99fc62bbf3747ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:39 +0000

    rare centre downstairs you buy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
