986e2912a1e8d6523c23c64b1739c61e52294306
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:38:04 +0000

    open Ww exchange position below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+name = "Python"
+print("Hello,", name)
