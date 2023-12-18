912ce7d1d3867a8325d5e7d45f946f7afbf66ee9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:19 +0000

    search defeat ball goup comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def count_elements(lst):
+        return len(lst)
