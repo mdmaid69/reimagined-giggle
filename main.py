466d6b76ef347a8f7fd4a87baa629c76005a4af8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:10:18 +0000

    gate at meeting software understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
