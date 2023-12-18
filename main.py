ba14e93a1d35084de72699fbd9b989c74713434c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:01 +0000

    development rubbish share length such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
