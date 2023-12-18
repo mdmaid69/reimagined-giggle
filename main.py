5322ba8e72d1ece17a966f1f887f37592306ba4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:10:08 +0000

    except true petrol head trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
+def find_max(lst):
+        return max(lst)
