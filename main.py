fea73bafdc8e363e4bcc9e291f8ac3a01af59d14
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:26:45 +0000

    display undo scrape punishment find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+from collections import Counter
+print(Counter("hello world"))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
