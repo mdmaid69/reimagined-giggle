15520d8edae7e31eb840f96d114b41080cd9c846
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:46 +0000

    complicated Ww as trust sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
