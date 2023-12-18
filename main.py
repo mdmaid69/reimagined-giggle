7c4886cf485ed2f93fa3df15033fd0e0925901b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:32 +0000

    certainly reduction familiar test attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
