7f20c6ba8e120101b4e1ef64d9d6e3217cb7af62
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:18 +0000

    child remove make short thing

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+from collections import Counter
+print(Counter("hello world"))
