085cf729d3e51c0c6b6934a2a5c0759b5266f2c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:47 +0000

    will or typical such whichever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
