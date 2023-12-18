b597d0ffd89c3c89228880bbf2457e51f0ed99db
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:23 +0000

    Bb store brief fault normal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
