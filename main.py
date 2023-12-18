ce651cb10bed707169f48d1190d9cb772431321e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:35 +0000

    office tired strength fast settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
