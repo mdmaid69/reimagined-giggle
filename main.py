6ed1270127b67d07817308630cea3731c7b5b5f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:37:12 +0000

    sound ashamed pipe slice getback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-print(sys.version)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
