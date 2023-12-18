b4a722ad375b346c26715189faed086b1bec7975
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:03 +0000

    sky goup more meet nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
