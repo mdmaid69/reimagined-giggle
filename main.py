af9098f8bc2ac76fca7d02097a63c5aa395dced8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:37 +0000

    raw discover no feature perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import platform
+def get_os_info():
+        return platform.uname()
