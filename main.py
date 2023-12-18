fd7a19b57bf498a3c05395d1f3b27aa42003d575
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:17 +0000

    mark prepare rubbish on this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
