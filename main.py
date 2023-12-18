0864ca8caada9a675a8368b48859d2e4e5fcff13
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:00:23 +0000

    inside interview correction this remark

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_density(mass, volume):
+        return mass / volume
