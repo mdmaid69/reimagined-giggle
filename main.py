ec05478d23691f20ad789ca3884c58b3525987fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:17 +0000

    surprising bank pay sore brother

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_exponential(x):
+        return math.exp(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
