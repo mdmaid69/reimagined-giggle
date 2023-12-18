a56fdc9cae57c594580666aab49d51e3444ffb12
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:01 +0000

    under now combine shine spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
