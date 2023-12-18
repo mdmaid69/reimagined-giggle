4d66a2fc4850717099556734cd13cf7e6a05668f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:52 +0000

    dare beside crash soak hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
