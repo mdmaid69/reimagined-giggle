66e036a97cfec487e64bca89247f15371bb62899
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:34:23 +0000

    bottom example imagine clear boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
