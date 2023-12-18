a1eefeaf06b86c0ca653b384ab7b89b75a4527d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:58 +0000

    everybody yours post bad fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
