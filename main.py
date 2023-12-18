f5743a9a12a42a4d93821c1818868c7904802031
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:10 +0000

    steal either feed rarely grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+print(math.pi)
+import os
+def get_current_working_directory():
+        return os.getcwd()
