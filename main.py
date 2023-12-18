b70a9a3de98856edfb402cb2819c1b6328537d7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:54:22 +0000

    around downstairs common ball secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
