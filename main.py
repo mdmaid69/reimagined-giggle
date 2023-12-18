4e85c803caf3d4088f06033b7c6f3aef381d1e4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:21 +0000

    characteristic breakinto development cap splash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_palindrome(s):
+        return s == s[::-1]
