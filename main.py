987163940a0d101e35182755f279750af4c8ae0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:16 +0000

    breakdown fence compete Ww used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
