e5f86305c89bcc3c13164510f9f3020cdf93ab90
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:58:59 +0000

    repair danger male even seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
