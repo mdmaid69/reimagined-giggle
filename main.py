1e2edc588d2c563d8a7f36d55729fb0570bb209c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:51 +0000

    run either wave image bridge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def reverse_string(s):
+        return s[::-1]
