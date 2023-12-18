b4b47f18af9ab23d11a0d770ab994161fa632c78
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:09 +0000

    real fat open tie immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-print(os.getcwd())
+def insert_into_array(array, i, item):
+        array.insert(i, item)
