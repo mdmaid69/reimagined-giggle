9d947b538d106de1d81780116b9f7905df6ebed4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:32:23 +0000

    unable comment national return number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
