ecbd164578bd948fdf0c0594ece2f563dde2013b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:50:18 +0000

    rest design base mess education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
