894ae5502487a0acacee2ea700b1257798ce6c3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:28 +0000

    regard grand wide whom second

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
