029c0d095b68c313e2fd4b427d5e20099b55ec5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:08:24 +0000

    result spring interview shock Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
