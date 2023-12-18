e81d2a1e1eb98814ff545a1d447481fbd084fca5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:35 +0000

    blind put deal clue shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
