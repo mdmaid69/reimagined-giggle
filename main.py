1e92e15f463697a8657c2e55e54502dcfb15ea0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:39:23 +0000

    neither negative by same before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
