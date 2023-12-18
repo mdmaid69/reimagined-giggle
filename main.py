1d9ebde81d88311951f871a378cd8100606eae95
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:55 +0000

    port keep waste comeover embarrassing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
