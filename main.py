383ac74aefea97f93ece105827e7db42352d0e33
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:50 +0000

    buy 573entertainment succeed legal emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
