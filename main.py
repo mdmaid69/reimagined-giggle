92ff2745e56e7ad1fdcd71442023492e4e4c0c3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:14 +0000

    dress invite turnaway organize shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
