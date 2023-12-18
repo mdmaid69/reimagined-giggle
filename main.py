36f8db9aa2dad1b10edfaae1baf4a294ed3e0927
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:22 +0000

    sign entrance somewhere rescue confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
