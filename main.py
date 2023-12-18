8a3a5ead96c63bd7a8a60b8911172eb5a07a9285
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:58 +0000

    up cry search arrangement surprising

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
