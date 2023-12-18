7920467c30e9a4362d86bc0b9ad8d334245d456e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:34 +0000

    characteristic medium broken official broad

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
