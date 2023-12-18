4a150dedbf5f4e09b48ea9a1fbbb3c9c72a63a73
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:37:14 +0000

    everything exist simple time getout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_priority_queue():
+        return collections.deque()
