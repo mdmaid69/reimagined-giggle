f6932e4ae18b3a2191d7a2079e7080a44b48c29b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:43 +0000

    child flavour bend wherever suffer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
