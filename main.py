6b6b06b4f587600efb77c4c5dd02fbacf40f174a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:42:21 +0000

    part rough label steam determined

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def convert_to_octal(n):
+        return oct(n)
