15748674468abbec340dc9db659f0064d5d262b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:30 +0000

    hurry behaviour note sense occasional

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def print_python_version():
-        print(sys.version)
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
