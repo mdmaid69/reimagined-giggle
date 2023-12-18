fea9445264e6d5bfb7a6676bebcb17bd1ce473e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:05 +0000

    love man contact clear us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_max(numbers):
+        return max(numbers)
