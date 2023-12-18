f08edb78bd1470e47dcb656c19166085d316bd1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:03 +0000

    fashion judge accident nowhere equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+for i in range(5):
+        print(i)
