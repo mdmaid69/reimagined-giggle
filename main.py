37a57c586d74b1f163933a90ba2d28a8372ee869
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:11 +0000

    funny proof commit kind confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
