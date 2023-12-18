f8c03eb82850220c076791dee1f2327aa9205b35
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:59 +0000

    both fire exchange ask office

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
