3a83446f76b0d02f23a29088c44394cc598b7ad2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:08 +0000

    off too contrast cause sink

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
