debbb3753eded88270b0c4a633c5e846731bd10f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:46:06 +0000

    sudden meet start interested middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
