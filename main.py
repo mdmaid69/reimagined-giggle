b4042b5bacf3b4a16e4360ef38d2bbaac3a9a208
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:11:28 +0000

    false zero shoot strike describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
