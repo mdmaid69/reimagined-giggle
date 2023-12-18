b6f8a433a41e1527818cf80cef6dad4aea64af06
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:00 +0000

    behaviour introduction sick succeed situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
