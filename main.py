02c016e99b315cda6abc7b6bea1faffe31864e05
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:14:03 +0000

    squeeze both deliberately better contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
