ebf5999cc08b508d84a6ff59f23ee4f1149ba426
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:36 +0000

    scream race software sea high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def change_working_directory(path):
+        os.chdir(path)
