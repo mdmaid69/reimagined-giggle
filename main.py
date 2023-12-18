d0034b07e74ec3aa42ded4a5ce85856009b268b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:37 +0000

    remove stop warm unpleasant much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
