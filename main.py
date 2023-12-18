58ae84f2b0fd9eba3626bac8d92f10c290016dd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:52 +0000

    deep justice square present size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
