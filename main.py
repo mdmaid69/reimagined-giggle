5244dd0bcb70c2e98bd5689e473776ebaa9d2767
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:19:04 +0000

    workout responsible listenfor fall port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
