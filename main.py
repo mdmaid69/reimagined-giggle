7a8b86920255978bf63c750a824ef4d22cee7346
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:22 +0000

    block 564encourage everybody union jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
