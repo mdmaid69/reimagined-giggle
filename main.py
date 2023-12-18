5aa9f44f1cdfa638aa8497f9f9ad528959fdc529
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:45 +0000

    quit sure appeal fit children

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
