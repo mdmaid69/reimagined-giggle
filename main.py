a092616e404a7afbf33da7280b6de8c82632877b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:03 +0000

    rescue quarter normal lose makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
