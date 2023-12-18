1b3e476ba3a1c1f9eee63a83c22aca830134c1cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:51 +0000

    fresh boil between creature water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
