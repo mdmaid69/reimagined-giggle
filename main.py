a33d0bd97d74274843e9821c06b966c96a16115b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:11 +0000

    thin plenty brown ago straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
