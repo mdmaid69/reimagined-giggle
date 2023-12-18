10c2c54f4c879a4cc386ae8cb5cc6ccccce1e66f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:44 +0000

    awake fat content bang shock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
