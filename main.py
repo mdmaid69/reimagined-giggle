47be91b99ff3335b527dce5834f4a4e8e0d310cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:31:31 +0000

    warn theirs steer Ll noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
