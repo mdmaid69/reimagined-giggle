8df6d1272f4c0326ca9bc55fb9d7b783030b0471
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:34 +0000

    telephone mistake callat turnup than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
