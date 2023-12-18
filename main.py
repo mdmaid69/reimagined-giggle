b75695ff9433f8a2c127a90a8dc5628940da9a74
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:53 +0000

    taste your unkind swimming not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
