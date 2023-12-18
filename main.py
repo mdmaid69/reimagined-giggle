402147d89df42947d20c3b4b5d52b7affa318e9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:38:16 +0000

    pronounce hole rob discussion have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
