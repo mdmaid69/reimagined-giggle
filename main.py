a288d4e1cce7da052b82bffb5793bc67a6ca287a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:45 +0000

    anywhere complicated admit obtain company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import array
+def clear_array(array):
+        array *= 0
