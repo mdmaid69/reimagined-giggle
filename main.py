e1c5d39526a078bb024ab9c376209b5d807dc968
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:49 +0000

    aim respect in tense shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_priority_queue():
+        return collections.deque()
