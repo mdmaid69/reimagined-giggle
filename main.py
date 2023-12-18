c80b2ee9ff2f784776ce070834952e58155c4015
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:42 +0000

    correct office drive attach pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def sort_list(lst):
-        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
