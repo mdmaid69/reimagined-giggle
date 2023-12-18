f9e0558f431934b257ee03607fed018a9eb898db
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:16:01 +0000

    repair brush count separate sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
