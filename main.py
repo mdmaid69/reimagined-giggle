50b4c5928ceaf6a077bb1e1b17ef2bd69e1cd81e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:53 +0000

    fit please walk experience but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def convert_to_binary(n):
+        return bin(n)
