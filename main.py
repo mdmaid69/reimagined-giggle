97111c5517a4637cfec928936f97e85dee978a9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:27:15 +0000

    who lock cook high floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
