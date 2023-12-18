fd7639cc31c144e7593cb550ef46a48c22a3d8be
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:56 +0000

    our continue fairly offer wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
