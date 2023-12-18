cfa28a754300aa23d0b607a851293ebcf16f450e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:23 +0000

    party strike mean proper would

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
