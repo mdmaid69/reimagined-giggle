cdc1276087527b50d9b9b375756d9a34ac65c095
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:35:02 +0000

    hurry hair practise practice game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
