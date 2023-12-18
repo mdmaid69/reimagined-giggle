cf72871de15b8c13456a942d434ff188e87321fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:05 +0000

    block interesting expression step present

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_power(work, time):
+        return work / time
