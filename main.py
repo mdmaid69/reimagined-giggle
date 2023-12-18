8bb8b2433bdc3e60f04b4f8acbdb074169230fd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:26 +0000

    transparent this vanish meaning except

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
