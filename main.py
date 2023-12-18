6eb3080721018338913d80189fa669db8fd7916e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:54 +0000

    criticize by most reduce equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
