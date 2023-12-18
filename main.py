643a0485df81139aad1c11d94f3a002644911f0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:59:32 +0000

    settle lookafter behave scared then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
