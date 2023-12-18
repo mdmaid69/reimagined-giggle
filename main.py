e129f6bd175bd8bcac29a022fc9a9aa3db8a0682
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:53:02 +0000

    urgent hit Dd bush adjective

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
