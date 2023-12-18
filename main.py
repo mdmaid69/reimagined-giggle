df9bcc2c90f3cbc81300331dcf830842ac393dc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:20 +0000

    cap fond solve play more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def extend_array(array, iterable):
+        array.extend(iterable)
