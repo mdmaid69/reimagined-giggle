057071cf1b5cb536428743bbe47123f441d6290e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:53 +0000

    clear phone frightened drink forward

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+print(sum(range(10)))
