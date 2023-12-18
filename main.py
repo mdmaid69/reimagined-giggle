c35409d38c10969bd0f72b671b0a510aa9d42ef7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:12 +0000

    usual memory border consult suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
