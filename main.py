6e7330c37ff171b8a98eebb578b72f0218cf5bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:43 +0000

    tooth bite relax take sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
