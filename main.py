06f7617ddd92beb3f13ecbe19d0cb840a3394c6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:22 +0000

    low instrument plastic period make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
