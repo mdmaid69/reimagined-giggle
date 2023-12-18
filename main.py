5c96e96aefd476fd8ce42698aab1b4f57ef55d4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:47 +0000

    regularly close extra 567enemy often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
