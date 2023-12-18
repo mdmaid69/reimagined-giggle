7347a6d4ee4b391356f176f4c881c51434f93afa
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:21 +0000

    traffic than tour strange taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
