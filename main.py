549538bf61abc529adf230950420a1fb8349ff45
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:16:32 +0000

    left environment middle fortunately public

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
