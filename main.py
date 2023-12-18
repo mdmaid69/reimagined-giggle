bbeebecddb9006bf23d503b238954d5d9029846f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:30 +0000

    broad turndown lonely nowhere trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
