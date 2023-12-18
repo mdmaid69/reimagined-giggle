7ef96dc4a75e2a6a5c2b531516e4e862fac557bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:28:03 +0000

    politics claim bush determined ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def reverse_string(s):
+        return s[::-1]
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_floor(x):
+        return math.floor(x)
