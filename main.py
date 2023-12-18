cdef073ad6ae8d916dfd94952f65edbc815841bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:35 +0000

    accommodation once journey replace play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
