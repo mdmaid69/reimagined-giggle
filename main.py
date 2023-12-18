93b3fd9835b6a58570e8e561b057127d33b279ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:24 +0000

    spring plate tomorrow argument work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
