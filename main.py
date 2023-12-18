4f0ac1dbbd6e713bf0d2d6471673b9e004fae119
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:07 +0000

    away raise explanation grateful moon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import time
+print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
