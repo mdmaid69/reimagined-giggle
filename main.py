14b0e84ca825b41a5b20b77147a9190ec276ebe2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:25 +0000

    choose yourself steal which represent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import platform
+def get_python_version():
+        return platform.python_version()
