dacfe1976777e8cf659c52c43d1d068f9fd8613f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:46 +0000

    eager should experience involve behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
