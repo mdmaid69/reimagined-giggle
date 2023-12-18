bfa8b88efa86357c705ad00964dee3b6c6ef2082
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:12 +0000

    free dear offer giveaway guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
