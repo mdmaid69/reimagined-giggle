23ab9e7ecdf579421ceff8d248a0fa0cf7d7f7ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:12 +0000

    eager somewhere male anyone us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
