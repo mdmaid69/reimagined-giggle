951a9022b9b4e109f85ee5ac031703f1afee19c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:28:44 +0000

    accordingto bar excited plastic ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
