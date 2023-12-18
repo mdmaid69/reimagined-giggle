26c5ed4b0c619fd872d539d94ec145176f92fd2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:25 +0000

    pull sing she reject community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
