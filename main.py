80fa2d29b8b6607de94098ef6d6fc94e0de6e8c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:00 +0000

    unfortunate refer post rough needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
