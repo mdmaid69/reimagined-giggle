e3bba357308a1d4106f7d0881ca91155be32c897
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:07 +0000

    also wash help Dd fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
