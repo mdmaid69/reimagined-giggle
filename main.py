c51cd4a0919cb6f371a4efeee6665b2fcf46ef26
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:23 +0000

    transparent ear solve word recover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
+def cube_number(x):
+        return x**3
