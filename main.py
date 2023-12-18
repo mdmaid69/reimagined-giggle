ec3869e822c88a1ef357ac1c5fcfdf74a17af087
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:40 +0000

    entry design press image Itself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def subtract_numbers(x, y):
+        return x - y
