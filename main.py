cbaa3f485e706a851dd6e3516a0f04a40363595e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:21:34 +0000

    related terrible would giveout measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_set(array):
+        return set(array)
+def add_numbers(a, b):
+        return a + b
