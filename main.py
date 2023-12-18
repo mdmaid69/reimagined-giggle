0a07aa37a6333d043ba070377f69156580528e6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:34 +0000

    disappear less frequent fall sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
