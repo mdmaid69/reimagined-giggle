29398b456d910efe36e9972658e8ba6b741ed4a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:53 +0000

    last snow jump file gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
