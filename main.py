319089312a4bb27f3a70ba38988250a760c5ebf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:15 +0000

    obtain nigga raw pipe worse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import datetime
+def get_today_date():
+        return datetime.date.today()
