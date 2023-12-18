5a056f11e4ef47d501d3834b1921736d583c2c28
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:36 +0000

    attitude temper nonsense organization successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
