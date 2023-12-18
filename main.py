17605ed208e2ea4ed895d4397a442fdcc6844405
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:42 +0000

    immediately meeting knowledge ride all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
