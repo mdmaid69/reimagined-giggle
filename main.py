bc79c7aa483c39639d089af4234bbd7220aec2b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:12 +0000

    play slice wave normal approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
