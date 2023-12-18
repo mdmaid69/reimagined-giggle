658498c3708772aa99d112591682b8446ad077ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:25 +0000

    past notice reduce punish impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
