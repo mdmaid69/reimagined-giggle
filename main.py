ca55195e8bd2b17c8c30164142edb1679a585d36
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:04 +0000

    offer think play blame turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
