33e15585c827eda0594d95bab4bb32de88b0533e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:25 +0000

    claim belong bunch tip have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
