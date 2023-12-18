6806d943ee33877122da1c3f937239a3a5aa5c8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:50 +0000

    luck opposite turnaway drop have

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
