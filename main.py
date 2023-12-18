989c3fc5ce1a34de6287fd20d66d92070770b693
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:13 +0000

    die against load trap fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
