6e18c0ef65dc17269975214de1814cf6dab8aedc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:28 +0000

    successful smoke there cum willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
