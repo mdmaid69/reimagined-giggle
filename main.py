3e21fe6ebc3449449a0bd4ed977987d6e39c26cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:10 +0000

    Itself feel rare 564encourage beginning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 def calculate_acceleration(speed, time):
         return speed / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
