b5b7bfe1ae711e3ce8cc7e2cb3a0ef0eb30b0f99
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:00 +0000

    poor cost until possession slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
