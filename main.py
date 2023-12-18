1682be2255a4fef2395b6a2fa47c5a8371a19396
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:04 +0000

    worry anywhere concern underneath wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
