1b4f5da73d203aa11167d4bc0207eb82877025b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:08 +0000

    defeat reach class free share

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_pressure(force, area):
-        return force / area
+print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
