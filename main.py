2cf5b4bc8b0e521a0893ee3dfc5eed900a31cdc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:39 +0000

    cover either follow your cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
