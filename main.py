8ea69bf16c15f6091004fa923a1c480affc3af1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:03 +0000

    judge clean where emphasize naturally

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def multiply_numbers(x, y):
-        return x * y
+x = 10
+y = 20
+print("Sum:", x + y)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
