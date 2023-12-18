8bae5a8251820bfde876da053ff3e107e3fb1720
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:59 +0000

    other date down kill central

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_work(force, distance):
-        return force * distance
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
