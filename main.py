e4877f1a8f1cda816dab033331677e3311cc3277
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:36 +0000

    rule typical traffic eager space

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
