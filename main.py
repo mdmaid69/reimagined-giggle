aa87765c1a110690101073ae65c127a425beebb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:05 +0000

    suit short worst copy complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def reverse_string(s):
+        return s[::-1]
