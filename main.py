9ba3e37b35b1a643dbbea6b95cac03ff8d6a9e59
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:04:45 +0000

    always sort importance standard keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
