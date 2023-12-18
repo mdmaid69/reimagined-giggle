5e77ba53a72273d7df1d459d252e79df272716a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:56 +0000

    body burst pollution ought catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_sine(x):
+        return math.sin(x)
