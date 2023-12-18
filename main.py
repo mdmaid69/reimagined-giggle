a0dc4f47486f46b51c8dbd75a06833affeca0d3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:47:52 +0000

    what broken turndown content blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
