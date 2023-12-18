42b7b77822886355c4e32c347fe6780c65c6f04e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:43 +0000

    butter now desperate bottom bound

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_current_working_directory():
+        return os.getcwd()
