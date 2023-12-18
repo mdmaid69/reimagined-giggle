f225b0893508f1905b2f54c0d7f4c9be74b6bb6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:45 +0000

    lose forget regional friendly perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
