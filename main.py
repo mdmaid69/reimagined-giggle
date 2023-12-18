ad6fbef2ce8ad9e35a008f9ad9f04bf4781d9d15
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:56 +0000

    trust become remember train form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def reverse_list(lst):
+        return lst[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
