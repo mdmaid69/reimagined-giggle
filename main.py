b100c5d57a13fa6ef035aa44db242d6f5b3d9551
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:23:55 +0000

    most possibility trust Mm circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
