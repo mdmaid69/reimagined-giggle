7b8a4018c6f21d216232586fa704852fa32c60aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:44 +0000

    sick fortunate nor no separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
