7a357e3ccb4d259197cd1310a8d786e82700be0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:00 +0000

    rich machine Ll sick future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def convert_to_binary(n):
+        return bin(n)
