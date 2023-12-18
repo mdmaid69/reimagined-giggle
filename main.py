2e3bb85ce98db88be463e9fe493404222f309c93
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:14 +0000

    ought material sensitive cost block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
