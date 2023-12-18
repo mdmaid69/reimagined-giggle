1ce60cf5031cb5b304aca92098ba06cba1f35c53
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:56 +0000

    indeed behaviour married tap runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
