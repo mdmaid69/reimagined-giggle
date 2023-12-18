f4a510d19717ad88d8b50f1cd592f06dfb987750
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:55 +0000

    body equal touch from naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
