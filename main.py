744e512fd9fe5193ac40e3f3766bbe10161058b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:32 +0000

    movement giveup tool sentence cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
