031774534905fb897e1d26a78abe239c76e82632
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:10 +0000

    come fold holdon crowded interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
