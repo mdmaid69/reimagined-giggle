4e0f7a0008b1f47bb93db7d12ba913723bceb5e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:38 +0000

    patient finish doubt work anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
