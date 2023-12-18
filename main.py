d2e756fff237c8d0c2c88e5cc6d13f7ec275da72
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:49:16 +0000

    below godown survive splash twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
