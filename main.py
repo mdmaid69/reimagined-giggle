f298122057225d482bf106a239e795714ab73b20
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:18 +0000

    close announce Ii fair nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
