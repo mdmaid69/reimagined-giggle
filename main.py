62871738fc8f48a1c93458bce1317c3c3c1c5bb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:46 +0000

    odd base aware welcome development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
