e34c2d132e7a26b0b9258330afa796b930b74e6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:56:15 +0000

    answer shop nonsense ourselves yesterday

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_floor(x):
+        return math.floor(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
