749db43bc42e22a9ba101346e226883b4e2cb145
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:12 +0000

    discussion exaggerate always large performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
