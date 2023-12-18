d61c7edc05f382fd05e32b6fff0eef22d559e131
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:16 +0000

    no bringforward be different back

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
