8d6496e03211d1732ef55a3ec1117bb5d1a63f54
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:35 +0000

    simple relief shout frighten set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
