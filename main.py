85e0d91b82fb1132b4e9348de154e492d7e08719
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:28 +0000

    emphasis soak machine basis successful

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def is_even(n):
+        return n % 2 == 0
+  def calculate_area_circle(r):
+        return 3.14 * r**2
