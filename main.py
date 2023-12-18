4a51b6ebbf99f1e637d38a84f0cac3ce44209b98
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:41 +0000

    risk contribute low safety available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 def calculate_force(mass, acceleration):
         return mass * acceleration
