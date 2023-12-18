ace52ad0a223a70c428a0d3cff7bbe5e40525582
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:41 +0000

    means long square trouble available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def cube_number(x):
-        return x**3
+def convert_to_hex(n):
+        return hex(n)
+import sys
+print(sys.version)
