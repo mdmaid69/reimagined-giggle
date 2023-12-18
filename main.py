c6f569b4ee6b52ad95aa2b48469ddc354968cadd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:57 +0000

    clearly right stamp content walk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+print(time.time())
