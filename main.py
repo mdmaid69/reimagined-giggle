9eeddebad9e8005ca2277c2ed6d0a8d7c8a5a86e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:28:01 +0000

    friendly empty terrible proper week

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_words(sentence):
+        return len(sentence.split())
