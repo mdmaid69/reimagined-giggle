1f007ae636e8204f877b92fb72efbbf35365462e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:30 +0000

    help reply further land contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
