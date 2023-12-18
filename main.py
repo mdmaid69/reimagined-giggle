d2174400ceee703baa50190ee52680f6dac627f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:25 +0000

    hospital bin identify most platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
