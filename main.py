fc96f56e83470b74c39557f35b4a535a276add81
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:32:35 +0000

    later direct yes create over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def convert_to_octal(n):
+        return oct(n)
