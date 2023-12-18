0a9a0e568c93e7fcff4382940f68c7554348a77d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:14 +0000

    increase base behind family part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
