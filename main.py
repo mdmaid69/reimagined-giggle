f703c0ba183a3d8803691d810b0437e17b218adc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:13 +0000

    goon brush occur introduction without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
