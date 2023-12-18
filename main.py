f552713d004173b85373c355405a91e386749a8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:42 +0000

    about recovery circumstance well pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
