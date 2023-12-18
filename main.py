a46e5f88b114d093e2384a4577886b2d5f475386
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:27 +0000

    shame stranger right visit Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
