ac83c9bd4828a63624181bf664a97be38dddf18a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:09 +0000

    slow complete everywhere sale cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
