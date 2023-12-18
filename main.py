57582b17eacb9469854645e955383680996c7587
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:01 +0000

    smile dinner delicate little town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
