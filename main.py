d047358053424b0b344bd8cae490dbcc88074891
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:52 +0000

    until we course offer cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
