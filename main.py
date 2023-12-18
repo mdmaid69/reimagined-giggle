c0a6298b2df57786838db4ab241828aab4c5d66c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:34 +0000

    imagine increase straight reserve clean

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_str(array):
+        return str(array)
