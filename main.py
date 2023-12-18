b7c0eb2571fa09bfd8fd02be1152d60c3e85ebf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:15 +0000

    occasional mass miss unable mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
