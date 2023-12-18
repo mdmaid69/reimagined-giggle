134a8c64e525b2ff42f0ea9c2d5e1e3b3af9a9e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:37 +0000

    record needle relax basically score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
