7a2fe7933dddc12c16d9a1169d2ec26a9a53e590
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:20 +0000

    half bridge heart block length

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+name = "Python"
+print("Hello,", name)
