ddfc9e538c18910951f552dd15c4f87ee85e8c60
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:28 +0000

    herself considering bear advise instead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
