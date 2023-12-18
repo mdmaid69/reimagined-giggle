f38b692d443fbdc5381a40c846d7f57f5a0a9fc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:01 +0000

    wait breakup apologizeapologise neat keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
