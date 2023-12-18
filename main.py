fc0d1bfd805f648d5c5517715ae48300e30a6ead
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:33 +0000

    spite name watch sensible room

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
