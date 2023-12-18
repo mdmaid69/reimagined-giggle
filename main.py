d378e68fbc7dab09938cce304d43a1e8b5eb5f8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:14:11 +0000

    ear fail shock long shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
