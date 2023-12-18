291539e8f146eabf61ae6c25049716554f3a2aae
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:17 +0000

    bath branch glass busy distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
