84bf05d0f3a0a17644a3915d0b0803766cdd8a8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:24 +0000

    undo meet bringup building personal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
