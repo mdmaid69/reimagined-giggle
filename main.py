1b8cbd325eb04922708e05fe811dd0c1722045c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:58:37 +0000

    get hide getup against interest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
