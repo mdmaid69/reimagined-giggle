b2e3da353992793db018403db65f1b3ba4a1e0f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:15:34 +0000

    workout out design prefer usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
