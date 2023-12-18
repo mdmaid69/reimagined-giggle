d964d4d70cf391ffc5f6cba03d5574efa5f9efb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:21 +0000

    final too name climb breakdown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
