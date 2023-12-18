834b125d7b9fbeaa498b70327aa8cf33259264ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:03 +0000

    design charge possible distance trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
