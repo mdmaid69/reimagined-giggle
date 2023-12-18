33a5821c1fc3efb3b739ad8c3b441740c8bfcffc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:57 +0000

    exist must wing fire keep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
