28ab0a39e05438b386a3723c4b687558930782bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:21:08 +0000

    thorough complaint heavy rush defend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
