e57e9cc8938cd9eaa53c28050198e696a98d5f1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:27 +0000

    absolutely grateful around copy plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
