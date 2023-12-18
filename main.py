2eae37deaff1f98958231f74f89710543f030e86
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:26 +0000

    hair suddenly convinced keepoff depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+print(os.getcwd())
