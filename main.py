60d4158fbd9cca3989517c4a0fe608089ff69d2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:26 +0000

    flat trip money port brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
