03add9ea67c895253100f1e22ab9a5d860dcc0dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:27 +0000

    simple community certainly discovery innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
