3bcb80e825271857bd93426f5970bc14b85c6633
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:28 +0000

    practical sad hospital promise transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
