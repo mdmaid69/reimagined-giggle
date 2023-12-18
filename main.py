4eb7100875b4d0bd0fef15c258c0c4717e73ca67
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:30 +0000

    corner wrap they large match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_even(n):
+        return n % 2 == 0
