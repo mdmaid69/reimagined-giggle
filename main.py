f642c6d9c1f472dc11ab28a177a6ec3bb3f6f365
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:43 +0000

    lose like few fill workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
