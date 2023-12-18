307bc82a3cfe5caa37bc876c0574ef642a8f642c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:11 +0000

    discover nurse badly young sure

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
