4df299ab476810f485844a9cb84434da3c917818
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:43 +0000

    her calloff dick ignore unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
