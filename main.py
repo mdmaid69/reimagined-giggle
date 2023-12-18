c9659a9efb57497efedeaab4f14fe857604039be
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:57 +0000

    open inquire problem wind ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+print(re.match("h.*o", "hello world"))
