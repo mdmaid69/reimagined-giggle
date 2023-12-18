7d9616256ed56626266bcbfd4f18b22f429dd05b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:39 +0000

    both square underneath exit before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_tangent(x):
+        return math.tan(x)
