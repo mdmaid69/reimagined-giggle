98ade61f289fa7b60258a72dd13dd606ae371028
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:54 +0000

    arrange disturb basis from join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
