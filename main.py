e5fa5b736d9f374fa370ee682154462841ea65c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:59 +0000

    keepup run unite issue age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
