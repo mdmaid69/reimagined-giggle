c7959060e75e215a27b428d84e43b62f5f2972c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:43 +0000

    recover moral wait central wait

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
