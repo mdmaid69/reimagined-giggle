fcd00d9d949ab69d4dff93470f028d3f24600a2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:14:45 +0000

    unable swallow tool advanced age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
