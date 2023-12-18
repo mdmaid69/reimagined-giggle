e3c6bba1683236a94905d8d7ad353af8ff8422d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:05 +0000

    person train beyond heavy promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
