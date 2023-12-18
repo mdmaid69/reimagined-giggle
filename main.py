b8ed8dfddbe09ee6cbedab3c262ce470f476740e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:43 +0000

    club lonely fortunate inform light

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_words(sentence):
+        return len(sentence.split())
