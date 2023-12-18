e9d863ec039f8320e25e740d1f532d76cc6fa8a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:09 +0000

    group draw minute offer tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
