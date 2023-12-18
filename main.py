1a1b57a6125ec8e07ebf6119bc60aea3fe4c1d2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:15 +0000

    basically breakin your entry too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import platform
-def get_python_version():
-        return platform.python_version()
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
