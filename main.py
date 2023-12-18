5da76e597da2cb8cc5da1fa1ccf54a6156864d38
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:11 +0000

    noone common interfere edge belong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
