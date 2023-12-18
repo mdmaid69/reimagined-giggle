769e4e9634e0fca719951ba2cdcd17228ed788c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:44 +0000

    schedule across born thinkthrough bringback

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
