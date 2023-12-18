9cf94fa70d1452d275ecec194bf8f239d8fba030
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:20 +0000

    possible listen loose deliberate bury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
