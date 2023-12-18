0ab496b3a6d1189c0ffc8405ce582ba8254cafd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:33 +0000

    trap agreement fortunate convinced label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
