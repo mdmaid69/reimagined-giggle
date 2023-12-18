c9a8234ae7a51c8258dd7bd83b36e0ef6418265f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:59 +0000

    road shake trade interesting shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
