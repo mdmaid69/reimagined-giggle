61a3cb379a04b0120ceded919cfc0f8649309ecf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:14 +0000

    type contrast genuine name bright

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_priority_queue():
+        return collections.deque()
