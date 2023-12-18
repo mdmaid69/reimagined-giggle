6ceeb17053a4a093a67c606df831cd1fa1e7b619
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:48 +0000

    corner music thumb risk present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
