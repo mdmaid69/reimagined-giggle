977518fc185d3b679d1e089a9dd4553ca6e7b6b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:19 +0000

    base describe door vanish awake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
