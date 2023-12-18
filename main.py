3299ee5cdac665ccfc4899622122cfbd3506b326
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:40 +0000

    continue whisper take straight Ff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def is_even(n):
+        return n % 2 == 0
 def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
