e8cfad0afece89fb6d033faee9e92d2491b7b474
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:59:01 +0000

    sleep better far godown Qq

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
