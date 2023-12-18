61095935a9fd88482e0e7a171ce17b73d75445ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:59 +0000

    achievement getout pair careless keepout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
