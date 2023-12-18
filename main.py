09c0520c1671e0bfe6b50dfb75f48eb715b48526
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:11 +0000

    fresh stiff freedom frighten ago

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
