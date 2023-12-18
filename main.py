67102fe2a1626dc99ff75ce7b12895223e25f982
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:26:40 +0000

    rest Jj organization within accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
