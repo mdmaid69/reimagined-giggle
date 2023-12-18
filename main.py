81fd099c7eb0a8784e6695958c20ea8dc6612cdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:55 +0000

    Qq badly pupil ear ordinary

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
