fa7f2ce9509bd6ac65ee8ab0f24059343262ce7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:01 +0000

    organize among partner look wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def multiply_numbers(x, y):
-        return x * y
+for i in range(5):
+        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
