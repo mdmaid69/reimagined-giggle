1b68bd3aeba5294960b0032c283e278e7d20e4d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:54 +0000

    middle stamp Qq immediately stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
