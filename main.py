c38dceb5e5bf0a3b668623a2647e36bc4025f9b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:44:26 +0000

    board criticize damp patient impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
