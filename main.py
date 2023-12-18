82d1a48e186e2d665b6c56e4d70d78abfcd8babc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:59 +0000

    swing score bin mistake 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
