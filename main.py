6e73f62f864f34cd47bf0a85b8e958bf297114d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:07 +0000

    apart march enthusiastic waste Ff

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
