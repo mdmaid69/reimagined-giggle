8553b741f9841da0386263fc242f45de917f83eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:14 +0000

    concern pick inquire guilty chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
