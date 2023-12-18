54416b81ee03c1f8c572fff394e6fa2335668bf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:01 +0000

    star area culture fun guilty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
