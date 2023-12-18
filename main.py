3283813487856216738926d08e65cb4e5213aef7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:27 +0000

    path operate free on below

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
