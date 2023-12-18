06de3938b82b3a8ebe693e00f90faff2f83139df
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:29 +0000

    attract middle up anything welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
