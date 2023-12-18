39219a49ae76c50df8c3b0d408c9fcef9fb75f46
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:08 +0000

    confidence think breakthrough worry along

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
