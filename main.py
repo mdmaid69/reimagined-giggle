0d971bdc987d5cd295c163503867c62de3aa0d44
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:54 +0000

    tend man handle pretty reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
