f6d7017a9d13833d1152f047b4c6b75a788f0a95
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:33:39 +0000

    general crash squash unfortunate party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
