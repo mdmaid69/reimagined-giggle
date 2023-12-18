12513e04a9de92f6eb3429891c37519eaf7d1f96
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:59 +0000

    meaning sleep point stay welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
