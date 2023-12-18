4c55f6b2f2b60de39061450e13c475874b20da47
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:53 +0000

    hurt mood by common throw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
