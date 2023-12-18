99050a1f00d3cfd5fdad31aa3b505da60373f0e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:29 +0000

    behave write comment grade passenger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(x, y):
+        return x + y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
