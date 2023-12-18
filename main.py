56899ed0a69e62c2c1dca8e9e2eedc2b30dae174
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:59:53 +0000

    duty mine anybody mark as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
