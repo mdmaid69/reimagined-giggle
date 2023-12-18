72f443b8fb64f2da3a44fe4914aa67a3f55ff0f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:00:03 +0000

    road event brown wide rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
