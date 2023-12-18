c09a95ae832d3da5dcd4e7e811f852953666e1b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:28 +0000

    wash ilovedicks uncomfortable stay responsibility

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
