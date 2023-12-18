0834e5a8c553221ab94766b0aea80a7f46e91b84
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:41 +0000

    swallow cost meaning young her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
