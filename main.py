df8b163700bbd3ebe273d867f5a99a12e04116f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:32 +0000

    needle sorry Tt nose transport

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
