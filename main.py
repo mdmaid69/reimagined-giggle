bccb360407652ca2b88d145a19e020199918df4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:15:40 +0000

    arrangement range can cry charge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
