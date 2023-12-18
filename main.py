3e7c7c877e29a2506e53f45596b4ec1f24dd3d4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:08 +0000

    market sentence married sorry dinner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
