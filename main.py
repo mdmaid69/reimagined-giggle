79ccbe83c561152dc31215e373e19ed21db58c62
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:25 +0000

    best declaration freedom once consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
