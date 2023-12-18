6af1b2683dfe806b573fdb0274a749532500e0d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:14 +0000

    bin smile sink argue depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def remove_duplicates(lst):
+        return list(set(lst))
