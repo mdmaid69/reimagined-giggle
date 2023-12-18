fb701756674b6dbe3e50ceb8b1960377f4c07a82
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:05:02 +0000

    bang Dd decide swimming rank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
