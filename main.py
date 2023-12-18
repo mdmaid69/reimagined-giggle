6efeafaadd7a3a8f5172f4ddbc0076cb58ca3458
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:26 +0000

    himself clear getback cum come

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
