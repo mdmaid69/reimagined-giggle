0049e0ab2393cb7d0f05ed7472d5783b83346a4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:21:34 +0000

    in cruelty cause lost late

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
