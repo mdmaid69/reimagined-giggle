53ebadf9ca0488dff43f81d5f324816ab4d43833
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:33:05 +0000

    switch giveup shop desperate race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
