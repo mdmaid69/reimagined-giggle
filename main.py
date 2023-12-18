43ee46e38732c52bc75f646ed97a5a950b4023b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:12:49 +0000

    catch float ride success whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_max(numbers):
+        return max(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
