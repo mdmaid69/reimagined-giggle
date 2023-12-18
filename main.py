c20eedb8558a8fa6f4954d7c77ec7a124af58ba5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:34 +0000

    advice clean standard clear test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
