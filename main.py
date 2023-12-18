45fe2009286739284e209dfdce11caee033d7ee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:03 +0000

    unit matter fortunate field mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
