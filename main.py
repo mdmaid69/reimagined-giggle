6405e0686ec01f02f1cc57425c46dede8a77324c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:47 +0000

    564encourage shrink successful please crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
