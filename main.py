50c6b2055d6a6ebb09bf934f3b706e868692d645
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:40 +0000

    entry yours better to attitude

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_work(force, distance):
+        return force * distance
+def subtract_numbers(x, y):
+        return x - y
