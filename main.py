7c8ce4dd2ee8842f29c0df896dca850e740c41b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:27 +0000

    admire keepup human minority slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
