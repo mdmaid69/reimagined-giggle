f485dba6da43c707d4601909daa8f1d269a34844
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:32 +0000

    land better smile arm achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
