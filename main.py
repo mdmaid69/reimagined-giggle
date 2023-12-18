4f889074bab7fc2bdade8ee7ed4a58ccc85412b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:21:14 +0000

    neither wide OKorokay to scheme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
