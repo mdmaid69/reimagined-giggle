af3f913ca5c6f59c4e669f97f7209ef4cf1ca95d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:14 +0000

    break cost convince basically raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-print(time.time())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
