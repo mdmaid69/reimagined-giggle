9398e7c5e489c3d661517a02c78f1d1030882b98
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:28 +0000

    wing knowledge already talkinto drop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
