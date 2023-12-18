5dc4027b53beba8805c58828580869676940bff4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:16:41 +0000

    about nor keepup centimetre real

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def convert_to_hex(n):
-        return hex(n)
+print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
