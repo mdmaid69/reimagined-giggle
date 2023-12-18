3bac157575fee54ff877794c71d40c158f4b0d4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:15 +0000

    face once somehow once appear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
