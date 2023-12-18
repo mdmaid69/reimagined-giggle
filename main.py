e3d5127ea1cbf1519f76ce07f8a6ff8c063a26f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:18 +0000

    obtain comedown cinema takeoff exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
