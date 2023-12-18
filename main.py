d0df2364b7ef2abafc27f5ffd1bbcad70fe9646e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:13 +0000

    hard meaning talkback march deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
