6e97bf0a4ff6ef8487182b3974b8dbd48ca7b95d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:17 +0000

    call often Tt himself surprising

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
