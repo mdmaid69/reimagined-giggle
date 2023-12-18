26133ed3e49a6d182444cb2a3b27ce6400f8ede1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:31 +0000

    often low finally appeal by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def get_string_from_array(array):
+        return array.tobytes()
