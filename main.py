6dd611e97639aa517a7597ce5be1fc5a9ad70ca6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:20 +0000

    trade introduction exchange greeting pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_work(force, distance):
+        return force * distance
