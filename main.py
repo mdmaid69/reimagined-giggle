3a51d00f7640b578f1256dba73de61980da965c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:30:49 +0000

    Ss mainly whoever cook walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def find_min(numbers):
+        return min(numbers)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_item(array, i):
+        return array[i]
