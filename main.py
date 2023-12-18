1976f517c4049cf524dedb547c11d8d6e6407cde
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:17 +0000

    to mine list group within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
