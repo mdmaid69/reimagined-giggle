bb53b97e0c42460a92af723df841f5340b85a701
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:58 +0000

    hello 179bite influence love which

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
