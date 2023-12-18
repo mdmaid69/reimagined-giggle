4f13fef32a2f84d28b600073e6f0a34453aaea2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:59 +0000

    possibility intelligent surface set undo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
