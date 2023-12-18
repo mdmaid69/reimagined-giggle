2fb5f6d3a9e0ffbf7cafc351f51a35013001c8df
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:28 +0000

    spin dear sticky wave persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
