6fb51111579425be75ef8a42675a9feee5b4c9b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:24 +0000

    prepare sale breakfast stop come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
