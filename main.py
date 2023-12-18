2d3d56c440b9979c0ab9dd80c20b2cc1e4413f36
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:52 +0000

    everybody dangerous it think bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
