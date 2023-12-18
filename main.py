612c63e58ce8ec7770e9bca705ab42b12e7752b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:13:22 +0000

    as cost rest whistle opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
