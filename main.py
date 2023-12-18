d0b215a40f9f1e8f2c349c7686a4d4619fb11503
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:06:38 +0000

    balance formal front famous become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
