1f67b9457d28ab8fabe1b310117e72df5d7ba25b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:18 +0000

    result any second us shelter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 def calculate_work(force, distance):
         return force * distance
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
