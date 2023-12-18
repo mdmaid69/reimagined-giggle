1ca9f265a67f8e73885d604026e31b3163d9aaa5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:20 +0000

    thorough number takeback also limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
