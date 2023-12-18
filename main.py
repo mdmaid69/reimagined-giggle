1a8457f1cece479527522f6e912d6acf11b718eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:58:01 +0000

    fail discover copy wall importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
