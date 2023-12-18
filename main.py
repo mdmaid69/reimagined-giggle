d0e2be63f02d68d3638abe21b7d0ee5a1e62d3f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:43 +0000

    book subject wild earn whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
