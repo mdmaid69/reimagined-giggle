d41c549c097f13107b4e017fcc5127ec702b1b69
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:07 +0000

    lock press pause once black

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
