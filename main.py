ffbfc2ca147595bd02032ea32df52096344d9035
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:20 +0000

    calm subject market anxious arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
