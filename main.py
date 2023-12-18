2cc0e19ad9fb8c981c10fc0517483d6542d03ee1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:09:49 +0000

    freeze around ignore during body

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def count_elements(lst):
+        return len(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
