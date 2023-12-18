750abbf33ede890ea29f2d4ea733d9e10e70eff5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:48 +0000

    belong strength breakfast appeal refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
