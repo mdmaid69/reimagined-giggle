49533ce664a0ffc503f8012adcee14f0ac5d34b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:48 +0000

    effort type taste health drink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-print(random.randint(0, 100))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
