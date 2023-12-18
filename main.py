0960e5f8e4d3e166b1eb4d441276efa6e61acc0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:43 +0000

    stop Vv pick independent quality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
