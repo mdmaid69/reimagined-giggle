09c583030a7a07f1062bdc617fcd1a4fa5f3c2e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:48:38 +0000

    pattern cut aim special story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
