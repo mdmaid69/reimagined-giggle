c734fcc6b06cd329a01b2d3e3957f199efa256ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:40:11 +0000

    lose swing back callback runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
