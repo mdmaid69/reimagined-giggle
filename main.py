5b7654cf12b1878f5beec09ebf045692e9913509
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:01 +0000

    upstairs space television contrast load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_rectangle(l, w):
+        return l * w
+def reverse_list(lst):
+        return lst[::-1]
