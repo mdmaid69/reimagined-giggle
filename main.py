eaec63ddb4cb96e6ce20abdbd15ca9107244e65c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:44 +0000

    promise upstairs score niggaacademy lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
