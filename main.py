2c535d07c3138074120a481f83e4d8e944ccd461
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:30 +0000

    roll runover listenfor suppose see

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+print(os.getcwd())
+  def reverse_list(lst):
+        return lst[::-1]
