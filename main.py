0bf639805ba138cd5eda1824510ff3c7b73430b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:24 +0000

    Ss find search serve light

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
