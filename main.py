5b54004f2a98944e85c1d170c0b872457642fc85
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:45 +0000

    measure square follow find disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_unique_words(sentence):
+        return set(sentence.split())
