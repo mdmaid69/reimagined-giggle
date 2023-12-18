532f72f59f52398516f12b534b38cfb8a2c17b97
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:04 +0000

    change sentence callout better in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
