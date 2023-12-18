a77479f1611791db0f38c5465cb8dc9c21717395
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:11:58 +0000

    foreign now punishment steep union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
