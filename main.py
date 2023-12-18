de0465be9877cf37c7b5b1c0333fbcd38ad9afcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:20 +0000

    control warm pretty turn sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
