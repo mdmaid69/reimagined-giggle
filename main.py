faa54bc9e8dcc14725f9c015170ce0851d2260dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:57 +0000

    float hit heat no proud

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
