ca5efbd7237b0fe7f515c392767f2652e60bb335
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:26:05 +0000

    lesson example getoff impression weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import time
+def get_current_time():
+        return time.ctime()
