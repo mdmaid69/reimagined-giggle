fd36dee747abd76a083942f5dc5129c5af093294
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:27 +0000

    somewhere recover region wave complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
