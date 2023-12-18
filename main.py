b29e485c5b57bf5db50b95f1a771d64d40e58463
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:07 +0000

    silent sight combination favourite his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
