d3957945fffff9edbdb40b719478fc06cde5e524
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:38 +0000

    happy declare miss pay paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
