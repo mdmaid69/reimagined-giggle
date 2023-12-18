e9422cef1bc9ad35822a08febfcdb599229252d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:30:41 +0000

    hit hard room floor strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
+def find_unique_words(sentence):
+        return set(sentence.split())
