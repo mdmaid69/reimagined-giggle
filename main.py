2372430847af824d559a1f6d766226310a7ac17e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:49 +0000

    size careless behaviour willing use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
