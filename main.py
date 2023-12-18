dd00fd25e63384d2ab9fca3cf7eefabc41e1b557
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:51 +0000

    danger free odd operation happy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
