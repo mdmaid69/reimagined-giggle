54627351cd901ff42f189bea044e8eb56b60fefd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:56 +0000

    scale indeed relation refuse cinema

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def count_elements(lst):
+        return len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
