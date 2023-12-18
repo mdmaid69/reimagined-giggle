7ab0aa60c0b0646c5452615ad75b76d3da76f015
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:56 +0000

    somewhere reason crowded apologizeapologise treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
