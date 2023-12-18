cece0383e32ed095452c632afaa9350309e205f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:51 +0000

    cheek though its pipe quarter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
