d3104b45fc6e46465d5915b6989a784e283d48e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:06 +0000

    warning safe understand breakinto interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
