f2ef0b851c13a4a5d266f81b58a375d9d6e3198d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:42 +0000

    authority by during strike flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
