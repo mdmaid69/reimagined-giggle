b4c04a67cf7afd4e03dbade2f412e356c62e4c04
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:42 +0000

    pin married bank reserve minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
