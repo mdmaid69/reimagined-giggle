5acaba66a9c21dc83d0410f58d785d6c568f6923
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:49 +0000

    brother cough Hh frighten pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_area_circle(r):
+        return 3.14 * r**2
