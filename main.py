3678da767b4422e922e147f7e515cbad5ca34bf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:13 +0000

    speed amount there visit rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
