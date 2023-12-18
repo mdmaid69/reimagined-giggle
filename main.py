b0e4e19f8feada1a7235b5ca7ec3a20a9d3b89e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:56 +0000

    steam easy brush keep left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
