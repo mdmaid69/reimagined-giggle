d5c71f1928980b78effe2c6ec8e846cc019b5836
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:29:00 +0000

    getaway pupil positive giveback nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
