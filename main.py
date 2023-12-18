12fbf08f4ef5ba0b2913128fde46fa8fb9eed357
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:01:08 +0000

    flow confused appear normal mainly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def reverse_list(lst):
+        return lst[::-1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
