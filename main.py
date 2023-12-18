6b89e11b5a09be3e3d97ce5a96710b480cb55291
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:57 +0000

    sensitive remain kind environment journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
