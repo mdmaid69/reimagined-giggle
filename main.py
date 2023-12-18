2e2e2d9b2817452f55656fab27c1809b2d716f7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:47 +0000

    lay suddenly wall comment along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_stack():
+        return collections.deque()
