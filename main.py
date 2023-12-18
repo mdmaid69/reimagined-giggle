977b1f554d5ad8e7702088e8cc57118c51193df4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:06:52 +0000

    apparently expect formal trust mark

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
