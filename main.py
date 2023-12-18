98075e5197fb3d838fd07956fc42a35d4db759e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:02 +0000

    allow wrong Yy underneath history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
