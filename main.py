d71ff63c04a0937baee4f2fb7482dc322b05cf96
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:39:22 +0000

    comeoff protection experiment generally welcome

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
