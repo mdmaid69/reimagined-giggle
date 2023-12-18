4127dbbbbad390e1fd61f468a3a5016e63f0b869
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:57 +0000

    major drawing week remove die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_density(mass, volume):
-        return mass / volume
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_area(radius):
+        return 3.14 * radius * radius
