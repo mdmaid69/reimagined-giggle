582f0602e66fadf8a0957776995d6895f0090bac
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:56 +0000

    uponon himself stone class whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
