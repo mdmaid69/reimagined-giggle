6f24b4b5d3fcb68d50da5cc2ab67bc6dbe3605b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:07 +0000

    dark since fast power solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
