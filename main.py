8bfd2189ed08f870726b37abd417dcafc0277236
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:53 +0000

    original definite stage pipe whether

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
