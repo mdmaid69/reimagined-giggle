6fdbf7a8f542a13270683010085de0ee14114d14
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:07:00 +0000

    previous butter doubt violent innocent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
