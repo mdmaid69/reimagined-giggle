719573c0905a2cb834adfec3afc3318c42089127
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:05 +0000

    fine spread responsible people operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
