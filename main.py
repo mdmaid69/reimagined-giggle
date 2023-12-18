99117f0041c9f8d70aea7f83fa47be0c7f7b6e01
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:57:22 +0000

    practise spring bring contact throw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
