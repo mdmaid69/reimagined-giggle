c1b6ad98846dc31ac10b5d847e0447533ddf8789
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:21 +0000

    work one clear talkback let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
