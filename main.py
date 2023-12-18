a4f3fdc11dc671d4b98f58b6618290c4701f4519
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:04 +0000

    content expression even cheque among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
