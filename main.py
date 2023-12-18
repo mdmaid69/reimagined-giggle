f49dd6d96b72e3980b747ad148a575ff62c7f934
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:18:57 +0000

    mess arrangement healthy tour software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
