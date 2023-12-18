a5e3adeb56d0b10942560725237c7ddde2a53801
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:24 +0000

    smell no demonstrate contribute taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
