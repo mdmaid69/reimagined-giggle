bad0c0f9708083b94ffa3631851b1d2fedf113d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:34 +0000

    water key neither mine fill

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def add_numbers(a, b):
-        return a + b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_power(work, time):
+        return work / time
