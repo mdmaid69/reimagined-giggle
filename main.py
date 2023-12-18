b6ec808dd8bb17fa3afd8b03a5553b5cce6f4e1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:50 +0000

    underneath turnback 572enter prefer importance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
