011f99d957dd84ef679b4a30d2a68b854036bd44
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:09 +0000

    lonely future post sensitive shallow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
