390820a2b1dc0e73ac90d1f02899d9e4368db721
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:05 +0000

    organize basis confidence into cover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
