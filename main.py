da246a674a056cb44f0fcbfbe7c585fbadfa34c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:06 +0000

    television door determined control crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
