4c30c98a38c1610eb16ffaa6bce27d00749fc5aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:49 +0000

    art dress twist event wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
