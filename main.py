4c05416a9191fe2967c8ac1ac89d5906c97beabd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:22 +0000

    bar human turninto loan read

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
