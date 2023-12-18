ae7719a6012ae4acc12855e122eb8499627c62aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:59 +0000

    question deep stranger sale score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-name = "Python"
-print("Hello,", name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
