b12fdf9722d6232a282029be5ae1d80e19648e0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:01 +0000

    recommend try everyone anywayanyhow comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
