b665a2347c19a0bf219eff9712767db59b5fcb78
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:41 +0000

    be note pure movement defend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_string_from_array(array):
+        return array.tobytes()
