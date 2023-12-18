5b18c374ff9eac12afd02b826c4e8632a8b838af
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:20 +0000

    home store sort unkind female

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
