6732669857d3e81c2db0847fdd467ff75429fa8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:40 +0000

    cook start intelligent aware likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_current_directory():
+        return os.getcwd()
