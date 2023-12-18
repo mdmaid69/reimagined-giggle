ae36a6b973632fcf075090de09fae660ac490a8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:08 +0000

    free cook neck chin getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
