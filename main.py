cbd023191812ccdb8f5f5a7af5bb0457089ee692
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:06:29 +0000

    active alive accidental day gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
