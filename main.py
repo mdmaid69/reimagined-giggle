61760d1e1116594cbe56aca5832202d5779b6d95
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:07 +0000

    judge power listenfor hollow perfect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
