f0a4bb5ac4058fadee07de0dc5f359893f14b6d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:23 +0000

    past or similar comeover advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
