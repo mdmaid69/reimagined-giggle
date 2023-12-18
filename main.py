2b5884699877f33e7d4f70a5e66b5cbcd2d21d2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:28:57 +0000

    low consider wish together serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
