fd3c2abe5cde42866e779873b09b6800a14826a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:03 +0000

    question bound exam importance whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
