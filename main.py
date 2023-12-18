89110518498e122c80082083d87ad75dbfb37b5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:16 +0000

    appeal achieve independent relationship Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.time()
