1add19b065d11df37f205f61bf9730036c9ae5e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:19 +0000

    getup getaway serve takeback onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
