2bfc53280edd6f0d89c161fd04eff31abb74403f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:25 +0000

    sign discovery dig wish concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
