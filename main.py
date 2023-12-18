78b5366b717ae4c78fec47572e78e8b7de5aa0e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:46 +0000

    evidence obtain hide matter pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
