9ce2e4154e381e61beba18baebc0acece237994f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:26 +0000

    entrance university fond concentration instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def convert_to_octal(n):
+        return oct(n)
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
-import array
-def get_string_from_array(array):
-        return array.tobytes()
