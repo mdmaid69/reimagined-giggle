00d1141fa8df9c80559ce7b211fa9c6086016998
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:57 +0000

    science care help this side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
