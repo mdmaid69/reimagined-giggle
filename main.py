bd91deafd755ef1689c50031d73283cd451c0c06
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:14:27 +0000

    miss length depend principle 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
