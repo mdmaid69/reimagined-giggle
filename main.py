120d4f5f2b08b65ec574c4fbd69574271391a133
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:32 +0000

    bath maybe fine final paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
