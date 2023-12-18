4082b9158090b20b73b8b8f7d391ad3c04253131
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:36 +0000

    heavy hurt splash trouble shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
