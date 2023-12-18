964f7d8ed7d925c56bf9012a6fd6726c02b9b543
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:10:07 +0000

    supply go letter cut bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def append_to_array(array, item):
+        array.append(item)
