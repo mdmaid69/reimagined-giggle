4d3ff29896960b0544976a93bfc91d1427252c32
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:41 +0000

    nearly defend sensitive off even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
