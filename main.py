30ec04b461b1dd3d673c36f5de6bf96406b77b4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:01 +0000

    as software put lose closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_item(array, i, item):
+        array[i] = item
