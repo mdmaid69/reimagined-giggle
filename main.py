d21dd3a2c7f26db327b217545112c19513bfb71b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:17 +0000

    steal system material argue where

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-print(sys.version)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
