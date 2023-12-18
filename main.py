c9a9d68719f1cfda110364372ecf3def440af2ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:03 +0000

    bang match factory slide Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
