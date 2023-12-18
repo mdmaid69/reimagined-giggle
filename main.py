eb511599bb677df125b21735e95926b15be4a77b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:25 +0000

    come peaceful loan run mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
