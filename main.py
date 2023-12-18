9840de4b2042827136e98583df1100d67e0d1dc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:38 +0000

    she thinkover tip face guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
