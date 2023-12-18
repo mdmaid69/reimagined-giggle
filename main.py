271b389fa38f9360851e6362f0f7a05480e8b732
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:00:44 +0000

    open notice mention insist summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+import random
+print(random.randint(0, 100))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
