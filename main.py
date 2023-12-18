872b253635017f0fa342e0b832348e9c3359c15e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:18 +0000

    another any order character admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
