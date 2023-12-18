005fe4905bb587a6d493c182efa45d83af4f00e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:05 +0000

    so press turnback exercise birthday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
