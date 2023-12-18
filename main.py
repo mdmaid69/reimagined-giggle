e3d50021af56970742c84d058df1b7cb9933dc4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:30 +0000

    gather narrow doubt confuse Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
