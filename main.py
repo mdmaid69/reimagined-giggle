f4049821096f5a78445c1cd35fd1319106e179d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:46 +0000

    finally kind present laugh on

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-for i in range(10): print(i)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
