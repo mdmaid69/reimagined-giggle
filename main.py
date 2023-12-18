20928072e02af84c9b640d8d4025b86ea201bcbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:44 +0000

    hand cold typical die way

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
