20e2c3d1b19d2bea5d3eaf7948a702c118309ef8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:17 +0000

    tap attempt offence occasion across

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_even(n):
-        return n % 2 == 0
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
