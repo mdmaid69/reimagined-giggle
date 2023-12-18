e873f403e9dda738529c90a986ced1abfef0a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:44 +0000

    special himself handle smile equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def convert_to_hex(n):
+        return hex(n)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print("Difference:", set(list1) - set(list2))
