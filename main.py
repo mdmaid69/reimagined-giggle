4cac13c797429eb31b6750b94b8628979431f2f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:09 +0000

    cum anywhere form for plain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
