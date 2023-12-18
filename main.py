97e09fb1a6ebc92dbb3e947f574d8052e7cbb55c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:01:20 +0000

    ocean shall break opportunity child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
