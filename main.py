deaf28723582e9fb5f889a1e9760507533be8e68
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:37 +0000

    absolutely difficult him beyond stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
