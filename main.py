2341f6958ce1e59a1f5eb1b22a034a03908ad23b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:08 +0000

    satisfied waste complete into ask

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
