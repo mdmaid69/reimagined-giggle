46587bae8c49ff61d967c8b0fb13dab07c173c84
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:43 +0000

    play trouble awake male late

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
