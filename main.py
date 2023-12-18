d4001e276d8a70f9bd9a29e24755b23f042eeb46
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:42 +0000

    offer warn niggaacademy exploration ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
