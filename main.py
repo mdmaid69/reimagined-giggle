4523645ca997cc4bddda69bc41920f7bcb4a91e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:57 +0000

    thank much fork outside agree

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
