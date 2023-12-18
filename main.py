a254413fb110af5c72d2bc434096983cde13e237
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:56 +0000

    heart afterwards squash guess pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
