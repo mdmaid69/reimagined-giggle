d72bca28fcb53333e447ab798a8dfb2237f9b235
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:03 +0000

    enthusiastic about kind opposite noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
