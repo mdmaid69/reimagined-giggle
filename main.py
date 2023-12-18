3952719e7597d7d3778a1ff8e64032072be86c88
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:39 +0000

    line exchange dream character sharp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
