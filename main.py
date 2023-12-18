0c28fd5a37de62c901a756e5d4807d1d0ad64b79
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:12:19 +0000

    health certain beginning obvious all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_max(numbers):
+        return max(numbers)
