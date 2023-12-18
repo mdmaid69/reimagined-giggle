fb831441560caf6bf633c700c342c7b6887ccb93
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:12 +0000

    road approve general forgive skirt

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def reverse_array(array):
+        array.reverse()
