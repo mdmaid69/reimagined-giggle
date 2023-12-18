f110d50af4aa936c6f567f922384e9f461e8e8af
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:13 +0000

    soft actually fortunately takeover straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
