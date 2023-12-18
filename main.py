32e53790e6e4ff42cb952b3d80b45124f99e1be8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:00:33 +0000

    spoil town trip delight fairly

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def add_numbers(x, y):
-        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
