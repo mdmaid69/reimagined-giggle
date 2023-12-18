f103bdc17f3f294096016cbcd34bb1dcb9ee115e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:16 +0000

    rise confuse slightly bend ago

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_error_function(x):
+        return math.erf(x)
