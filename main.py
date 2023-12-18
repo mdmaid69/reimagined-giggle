1bd1144ef95ac74dbc810f98340ad7c0d499f715
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:00:08 +0000

    572enter owe promise tend crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
