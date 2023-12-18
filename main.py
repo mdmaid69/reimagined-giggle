357d7ada6b189ca783cef9c9273e11928362d4c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:03 +0000

    warm history store complete Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
