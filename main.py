30313140aa9ac01255356f2f28adaab9d0f166d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:12 +0000

    brush frightened allow regional drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
