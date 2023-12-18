f76ff173e0cf97f00b8bb1e87869d89a86592156
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:28 +0000

    tap language 573entertainment home until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
