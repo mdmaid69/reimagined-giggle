216d2ed37b4d6601751f078c19909f2d771b3b44
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:27 +0000

    computer time buy along wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
