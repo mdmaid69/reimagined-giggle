c16a13fc5456eaf8f38063aa50f8a8d458263178
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:58 +0000

    round comfortable beyond advantage shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_counter():
-        return collections.Counter()
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
