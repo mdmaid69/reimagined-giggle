5af4a3967b9d14891ccfff037108cd7aeda0e84c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:05:39 +0000

    chance hurt walk we over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
