6a64c0ae00e5d4071dfd115fa6b7b733049492fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:53 +0000

    flavour prepare wide concern post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
