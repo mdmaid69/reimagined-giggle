8c563286063d43f1b6e4439cdb594b3f56ec66bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:05:25 +0000

    vote other careful rescue if

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
