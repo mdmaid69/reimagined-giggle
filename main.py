2a395fd049c3cf8d3162f3bd7970bb04f6ffa505
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:19 +0000

    division moon case country yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+name = "Python"
+print("Hello,", name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
