538dd8211906b4066c1fc30bf794f6b6880041aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:15 +0000

    day fun takeoff tip chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
