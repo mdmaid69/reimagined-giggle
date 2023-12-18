b6b74fbe9819c58a42e66fe9fae35fac0f2cd541
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:47 +0000

    little callback take back interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_typecode(array):
-        return array.typecode
+print("Hello, world!")
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
