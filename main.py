38f4e3e7b1dc582ab09eadb48598a1f1bbcdde98
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:24:20 +0000

    mother occasion wind mend pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
