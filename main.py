35b011c7feca55d70fff5153aafa7a9392d9739d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:52:54 +0000

    temperature test permission surprised pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
