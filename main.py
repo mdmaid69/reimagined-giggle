0c11c30fe54900bad399e52a0bcff9d15ce0451f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:17 +0000

    rely anxious upset correct quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
