cfafcf3e98f62cbce1a078aebcaad782c60c594b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:23 +0000

    public stand hit takeaway course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
