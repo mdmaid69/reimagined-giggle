e9dce5ef5a026786cb159280a7c0b301d46ac148
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:55 +0000

    demonstrate computer shame as secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_power(work, time):
+        return work / time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
