52a8a3e06ed94689c96f6dbd5598c4b0a7d2fb39
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:03 +0000

    store concentrate bang possession influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
