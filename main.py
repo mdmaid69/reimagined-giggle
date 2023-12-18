68081cf2aa323a173103bb32243341e301e55937
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:35:52 +0000

    callat moment breakdown regular speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
