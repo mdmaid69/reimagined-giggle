37183afedff0a549d7fe69110a9eee3a0f693906
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:32 +0000

    plastic like keep means dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+print(sys.version)
