8824e0be649f464d907af1723975b2eea01fa4d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:14 +0000

    any question concentration view kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
