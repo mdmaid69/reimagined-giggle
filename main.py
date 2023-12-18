4cad9e17863e1d8c7630e1f0b91cb17c1e0bc446
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:56:21 +0000

    soft period half mystery turnaway

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
