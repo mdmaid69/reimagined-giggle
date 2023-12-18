c8c164a9847f1bb79c4e97776d2770917326ce79
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:47 +0000

    skirt pressure danger clothes limit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
