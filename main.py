6ff69870ccdb5b3dc20e74c4105aa07e4920e352
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:54:24 +0000

    stop drop instruction manner come

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
