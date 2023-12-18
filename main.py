1baf9b0e135812e0edd9d0212e82cf74f457696d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:39 +0000

    mouth exciting even complain involve

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
