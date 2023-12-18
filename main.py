b7a86257c117ae5fb185068c57217816b7d94842
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:22 +0000

    satisfactory keen yes less tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
