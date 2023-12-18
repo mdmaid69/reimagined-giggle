5cd3f098351f9ebf9eea0d87b77e02e0f1dc3187
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:40 +0000

    bang any owe call government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
