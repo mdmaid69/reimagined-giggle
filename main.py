38d978781eebfa00e64d5b9d2beea151f8f3e34e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:46 +0000

    slip sick distance miss recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
