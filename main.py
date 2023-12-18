0e25831e6550adfc65e2f82646f97bee51e82c9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:33 +0000

    company square shut violent direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def is_even(n):
+        return n % 2 == 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
