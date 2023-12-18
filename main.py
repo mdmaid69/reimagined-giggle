3bbd803bb7cbddf57f785bb72bebd866ae611e2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:41 +0000

    exchange idea she contrast but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
