4ee23bb54a2c8e53fd3e60541e9f4770afd58fc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:39 +0000

    dinner study surprised light getaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
 def subtract_numbers(x, y):
         return x - y
+def calculate_force(mass, acceleration):
+        return mass * acceleration
