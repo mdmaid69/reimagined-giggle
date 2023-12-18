09db7236078b1e2ad11440cda216864670980164
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:16 +0000

    feeling rule lock bring along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-from collections import Counter
-print(Counter("hello world"))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
