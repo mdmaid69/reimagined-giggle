57717afa02376292fe33305fe61bca2e62eda516
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:05:35 +0000

    contact direction stir justice cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
