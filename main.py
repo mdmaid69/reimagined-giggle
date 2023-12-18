55020b35df2bdf6cb73e7e9caf0874af81da8cdd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:19 +0000

    comeround cheat birth bound benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
