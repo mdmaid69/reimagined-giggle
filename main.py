445b85759426e39ca8be9060bf2a208c69478015
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:23:47 +0000

    block letter sleep soak memory

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import time
+def get_current_time():
+        return time.time()
