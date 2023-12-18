a3c72b584a74e8d74e3d33c501cbb42d9e841c16
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:37:42 +0000

    left deserve discussion contrast centimetre

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
 import csv
-with open("some.csv", "w", newline="") as f:
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
