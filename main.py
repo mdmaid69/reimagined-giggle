9f245791254d55cfe3de64537d9405a1b8c440d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:45 +0000

    burst exaggerate border other pure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
