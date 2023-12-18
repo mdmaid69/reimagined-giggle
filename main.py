06bb9cf64d00abeb41193e9faafe73bf3fa6d8e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:46 +0000

    570enjoyable attract name store coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
