8db50517416056987bd3bb31fd3a56019f54d214
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:20 +0000

    generally whistle for shock long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_list(array):
+        return array.tolist()
