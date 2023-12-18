b65aba4974df9197c758d804e89300fbb617686f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:20 +0000

    goout skill help would course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
