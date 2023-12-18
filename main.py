7de0955ebc9b1653afa0857a021e5fa7fc2c7ab1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:32:53 +0000

    tone comeover although announce how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
