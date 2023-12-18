1823396e9ed20459fde29f20bc23f82eadc7889e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:10 +0000

    disagree quantity point fun cause

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
