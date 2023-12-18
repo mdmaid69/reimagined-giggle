3e4e0180ab21039b92a272932757b15b7e1da09e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:04 +0000

    on accommodation maximum crash trade

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
