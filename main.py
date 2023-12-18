554c3ae18e59208b36e3b7c08f77fc7523846481
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:33 +0000

    tool sticky hit leg brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
