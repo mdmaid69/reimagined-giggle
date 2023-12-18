4cdf20b57a1ba02d018359932f13924d5371e096
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:19 +0000

    turnoff Ss male pipe godown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
