13c770da9f23782178da9c4c8a6971f2c55325cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:20:14 +0000

    tour account weekend write used

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
