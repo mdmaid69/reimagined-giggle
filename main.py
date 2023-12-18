6db97a39cd5d0fefed4689fb16f58f874a675f61
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:30 +0000

    wet disappear wet welcome least

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import platform
+def get_os_info():
+        return platform.uname()
