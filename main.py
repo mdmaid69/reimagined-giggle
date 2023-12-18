6ebb61f2bc6c12024572140b6c1b893f4fd024f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:08 +0000

    plenty objection poison bottom instant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
