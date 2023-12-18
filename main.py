c70e2c55b7a42bbd7b997809c86212646ff28a2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:39 +0000

    peaceful total attach communicate hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
