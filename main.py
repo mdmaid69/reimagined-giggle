88f1dc6eabc6b30d5091bc3da9896e5b1afd434b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:36:23 +0000

    far constantly burst politics fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
