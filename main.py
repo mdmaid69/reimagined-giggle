9fb9da97069a80fd4a5832afa7d77d105d6fc03a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:11 +0000

    discussion steady aware damage tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
