6afb89158636f0596ad839dc546b9637f214ab60
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:31 +0000

    apparently communication available except accident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_power(work, time):
+        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
