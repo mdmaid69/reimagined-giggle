5b46497e0cc499bfbefc6b83ba22a47d36368d07
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:47 +0000

    bunch forward union risk lookup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
