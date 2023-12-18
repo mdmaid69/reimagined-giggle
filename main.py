80bf544cd490574c167ca67c839b5b53a46719ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:25 +0000

    pronunciation guilty guess cheap whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
