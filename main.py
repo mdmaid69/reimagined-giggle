2dd8d50fe201b3a3c7aaeae428169c3ab52b24e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:59 +0000

    work plan exhibition terrible information

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
