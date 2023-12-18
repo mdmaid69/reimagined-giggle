37614fa718069fb3156bcf766efb598f9c562308
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:39 +0000

    action gradually happen funny perfect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
