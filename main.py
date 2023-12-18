335dc8b8a0e4fc3cf2057dc231bab0fbe4a4cb7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:32:55 +0000

    mouth friendly see building subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_base_name(path):
+        return os.path.basename(path)
