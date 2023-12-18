9f3ab55d3813bb7dea2ae73fcb4cca2b55232fd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:55:40 +0000

    present sink yet model do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+  def square_number(x):
+        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
