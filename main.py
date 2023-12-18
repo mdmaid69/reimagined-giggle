38fed2eff866ae69e5f80d085e8b541483354da0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:25:58 +0000

    soft although themselves fat around

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-from collections import Counter
-print(Counter("hello world"))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
