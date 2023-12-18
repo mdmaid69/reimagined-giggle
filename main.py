1677f19199ba4e77d0b9f72db0c6eab19121a8a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:26 +0000

    how straight run form confident

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_queue():
-        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
