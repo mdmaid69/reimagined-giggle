351bdf9623513a0b18704002296230e661c8ae01
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:32:20 +0000

    takeback upstairs peace service possible

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
