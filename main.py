75b5a6373c8e717e3b31b79d786abe77da024c28
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:15:00 +0000

    solve some cap sit rubbish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def count_elements(lst):
+        return len(lst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
