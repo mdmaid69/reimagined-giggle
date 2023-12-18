8505645c93a524c157c112b4e9ad22c887070238
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:27 +0000

    joke quiet there definite will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
