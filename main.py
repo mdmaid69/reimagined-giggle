54dbb920de30ae285b0a29539fe54c52581be247
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:39 +0000

    then affect Pp below hers

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
