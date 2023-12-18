bdc62bc74b69d9f13635b3daacf16aae064cacaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:42 +0000

    by swimming truth take themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
