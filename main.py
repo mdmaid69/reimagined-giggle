291da40c10e186b87be57a4aa2c995fffbf4049c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:41:07 +0000

    explanation interruption unable way set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
