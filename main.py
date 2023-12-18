07e631ed41226e2f41003114a5f45a98623c68c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:38 +0000

    repair bet hair interview patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
