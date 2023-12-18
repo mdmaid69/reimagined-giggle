086ec3d9b005095c7215a5863a64d51024160692
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:08 +0000

    like 572enter stage relax constantly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_unique_words(sentence):
-        return set(sentence.split())
+def count_words(sentence):
+        return len(sentence.split())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
