ca0c16f8634bdc72a24eca06aeee6192e643517f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:06:06 +0000

    ago listen wind relief pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
