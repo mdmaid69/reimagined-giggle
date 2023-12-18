a45d1f4dc7e888df7e5f39b1dbf18141c3ed2fd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:10 +0000

    public step you offend include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
