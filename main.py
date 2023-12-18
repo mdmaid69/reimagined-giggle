234390edb910d9af6de6300e177216233e1d1f3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:20 +0000

    preparation happen juice trouble shine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
