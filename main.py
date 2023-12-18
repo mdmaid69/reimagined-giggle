94237645cb0c0a04b5f04c5a570211e5f5c7b477
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:40 +0000

    pain year married downstairs attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_set(array):
+        return set(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
