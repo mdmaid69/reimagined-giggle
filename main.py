a3cdfc96d15d215858f96fa9bdbd3bb8b8f2dc75
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:45 +0000

    accidentally agreement tough class emergency

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
