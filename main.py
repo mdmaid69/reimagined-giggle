5e28ad3ad72ec54f0ae9eb3f89b5a97058ac46a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:51 +0000

    trial radio turnaway rain boil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def is_odd(n):
+        return n % 2 != 0
