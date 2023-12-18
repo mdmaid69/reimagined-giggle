ff0f50b836f0c63aa6d10d3c17f333cbcfd4afec
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:56 +0000

    dish human angry during foreign

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+print("Square numbers:", [x**2 for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
