36b09994f837a7309e6d9cdcb955e0c6bc88da2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:26:00 +0000

    patience stiff runout silent shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
