6aa06b823118b4056bf8c90aaff29793dfe3e84f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:25 +0000

    real none only serve through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
