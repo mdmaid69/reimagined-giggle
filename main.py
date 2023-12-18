39ae17875ff4c7ef9d8f110d01d99f6f38db884d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:55 +0000

    giveout beautiful along wrap lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
