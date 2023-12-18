40a894d811f5bc8bc2a775547fe5cabfc3a1fd13
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:03 +0000

    stamp mad correct rain century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
