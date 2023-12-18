2944c1c99a554cea8d68ec22b675962d00ca3803
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:37 +0000

    not original prepare effort count

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
