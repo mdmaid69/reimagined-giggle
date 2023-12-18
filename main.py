c3c7242c168f72063b67c52d4aa2b652baae82d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:08 +0000

    equal connection past available then

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
