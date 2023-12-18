5cc67d97e3082fd03c8a8921bfb98d76d550958f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:26 +0000

    her break stranger pass price

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
