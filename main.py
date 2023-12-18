917af506669b50e2f5e5ce211acef4e4f4ef93fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:16 +0000

    provide holiday law reduction desperate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
