98dc453c2e98ea12eb621723e863547ac24ca1ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:25 +0000

    Hh description ability teach organization

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
