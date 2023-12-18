2d7c5c1db5519263a6fdd4100c77479443f1da24
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:32 +0000

    print getup priority for brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
