836571bba9f904d0cf90270a7ba4e0d474be080d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:14:41 +0000

    splash trust smile Gg boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_area_rectangle(l, w):
+        return l * w
