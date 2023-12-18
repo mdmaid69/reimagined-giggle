ba3edae6975e21ee46aedccecdb25d3f6a094aa6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:22 +0000

    discuss mail fall high winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
