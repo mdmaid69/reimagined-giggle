ca518d6b45967ae024f6ac3ceaa82b79db9e5281
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:11 +0000

    happen interview tall swing attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
