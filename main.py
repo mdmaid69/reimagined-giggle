6e0e267b285f24385861c231fd65eeffbc600b7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:29 +0000

    giveout closely medium bath division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
