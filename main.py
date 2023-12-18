5886f2747cfe4364646f9f81f29539e49276f042
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:01 +0000

    takeoff wish general plant drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
