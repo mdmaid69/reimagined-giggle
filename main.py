1238cd8698f362ecfd29bcb704ef75199770335e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:45 +0000

    pencil precise tongue treat rich

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
