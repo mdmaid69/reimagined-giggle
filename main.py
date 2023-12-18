ef07f8a3ace63babe926b42ad872533cc2d1b6f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:30 +0000

    exist over influence blind like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
