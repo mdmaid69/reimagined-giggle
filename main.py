d0b668f334eee46090c24f3c1f0d59b9f4a9097e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:23 +0000

    sudden theirs settle keen many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
