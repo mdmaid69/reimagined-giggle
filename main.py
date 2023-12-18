b522fa2e5d3c725b169836beddd80f48fb83c9d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:46:23 +0000

    suddenly throughout release duty lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
