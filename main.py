088b9cbdeab7e0cd2901ac460d2c58998e9b8b38
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:46 +0000

    sail cut gradually live bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
