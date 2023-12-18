4959bbcef67d6d99051619e0fce04978b51cef9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:39 +0000

    dark tool church female other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
