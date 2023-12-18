ced2f9c00e7323171b1aaf70fa28375626a8dea5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:50 +0000

    noise book instrument tradition use

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
