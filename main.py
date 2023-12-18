7d0ed10801998922809f243373539e14e89a7ad5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:06:50 +0000

    mine anybody pair like beat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
