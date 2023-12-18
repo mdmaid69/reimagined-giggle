3a38a7d59493f923ac63462e2c7e0f29e7a59dad
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:38 +0000

    turnaway himself majority ticket expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
