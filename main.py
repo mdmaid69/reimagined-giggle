fdcc547a50abb7f598e006addefcaa208e34d62e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:39 +0000

    miss space sure bin trouble

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+print([x**2 for x in range(10)])
