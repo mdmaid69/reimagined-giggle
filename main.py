04cecb56d9e7489eefd9f0d9b06c939f17a82706
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:25 +0000

    broken die club dry fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
