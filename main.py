b21f96c5bd0a92f1b65ac75c44e3cc53991eaf34
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:08 +0000

    nerve fortunately some estimate fly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
