8d52ae3e5c8a27d4ba66ae19671bece5d65940cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:09 +0000

    this maintain separate mistake actual

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
