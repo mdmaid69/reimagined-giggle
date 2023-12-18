3721fb497dd73cff89d71b11ed64f3fb6ee16634
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:15 +0000

    conclusion thirsty never deep upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
