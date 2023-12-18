3368d3f12581a2fae688ce39d9ae004ed9154e9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:28 +0000

    blame taste sleep supply unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def square_number(x):
-        return x**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
