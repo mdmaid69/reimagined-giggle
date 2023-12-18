e3e53a65c5eb5780f7f3832b786a65b2c3736cc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:57:32 +0000

    Rr account while question brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
