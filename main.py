7058e293ccd39da2663776b5bdde05665a5b81bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:27 +0000

    bound your real admire niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
