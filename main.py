a0581e54bb9dc03a9ef061e288200f40e3631c47
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:54 +0000

    own react takeaway Jj watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def count_elements(lst):
-        return len(lst)
+  def split_path(path):
+        return os.path.split(path)
