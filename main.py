5949bf01badadc3ca3833831e876f2334d165786
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:46 +0000

    breakfast niggaacademy address any lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
