26b2509dc73d4b598e131ada883e219e5e579fca
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:42 +0000

    now classroom contribute discuss complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_power(work, time):
+        return work / time
