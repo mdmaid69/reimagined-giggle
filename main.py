ee7190528bb6a67f135edd9bba61aaec940acc5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:25 +0000

    test stupid everybody herself transport

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
