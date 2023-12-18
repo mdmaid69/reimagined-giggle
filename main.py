3b8fc0a3d61c85babab0c9488c9ab9a587ad5b0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:32 +0000

    too paper decision climb protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
