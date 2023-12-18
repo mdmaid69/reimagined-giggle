2efed478c96bc2ccdc4e6f70e556cc0d454a63ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:24 +0000

    recent letter sight off official

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
