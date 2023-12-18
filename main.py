dd2c23fd5936c1b0948ff27eb1558a726bdbbc0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:58 +0000

    wherever move amount little cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
