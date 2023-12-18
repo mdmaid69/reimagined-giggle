cc858c30e2448c27e70d08f387302c3642d117e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:43 +0000

    first yours Kk dark task

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
