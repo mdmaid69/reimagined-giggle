be874583b0586e77d6ef83c14840f9e593a0c852
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:31 +0000

    drive noisy too comfort apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
