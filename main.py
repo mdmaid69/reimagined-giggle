c093914fcb9ec2ed97d2514977c8955216959604
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:40 +0000

    holdup let explanation again workout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_max(lst):
+        return max(lst)
