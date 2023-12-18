6c2e9295997060cf81146c8a83d18e2c3df7fd0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:00:36 +0000

    succeed that estimate breath stare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
