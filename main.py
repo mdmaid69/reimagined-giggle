8203d8f58bf49e8c819ae6d36a36bb81beeb074e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:36 +0000

    turnup through ring everything arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
