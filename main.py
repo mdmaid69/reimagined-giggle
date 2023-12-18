8b9c143cd7aa7584951feeee16fc88e950fb72c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:18 +0000

    trick obvious lend record whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
