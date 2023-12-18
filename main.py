dc78aa473668a1c985527b11250705408d79b1c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:30 +0000

    fly die gas warn borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
