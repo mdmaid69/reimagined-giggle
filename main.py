3ffe962d5c89bbd5610bd15a37ac81a76e2d870c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:52 +0000

    scrap fire grateful her touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
