8dd028495fcc9045e72c575ea6cb709dbadd9bf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:05 +0000

    entrance board sensitive experience pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
