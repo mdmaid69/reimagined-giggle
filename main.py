963d5ff34d2576ec7811fd87610be92a6930aadd
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:29 +0000

    spite pour vote lock wish

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
