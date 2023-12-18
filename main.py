534e5b1fa32e69fac03fd074138ae7af0547ffa8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:33 +0000

    come chat foot defeat freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
