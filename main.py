4474f1864d249cd0b6b67e7b8bbc7e698cb8a258
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:12 +0000

    undo comeover hit section rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+for i in range(5):
+        print(i)
