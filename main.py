85da04aac8db3213bbb3f9d28a75b11bfb22c63e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:11 +0000

    violent zero top relief 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
