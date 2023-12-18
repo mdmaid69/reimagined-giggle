cf316ba57aba0f3d9e521c2093aa812c5df3439a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:55 +0000

    keep attach perfect somehow friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
