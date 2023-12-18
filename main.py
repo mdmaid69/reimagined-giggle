b07c020cf2cb8053e7562dc4cef8b4eb7e1c3d83
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:55 +0000

    bar keep read sell kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
