d379a1cf82a7b0c158d9a1589a71879062eb4d43
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:12 +0000

    bringup regret estimate forever exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
