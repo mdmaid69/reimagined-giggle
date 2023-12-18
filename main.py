52101408c7eabd8c78e03bda6ee5f5660d198df0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:58 +0000

    badly group lock doctor call

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
