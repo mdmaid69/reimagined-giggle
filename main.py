4d4c1e8021eff91659716ec97b0eedd44f3fd0d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:41 +0000

    narrow definite protest violent effect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def convert_to_binary(n):
+        return bin(n)
