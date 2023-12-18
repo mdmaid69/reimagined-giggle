84d4de7ddcfd06df772450cfeeede8cd40d5dc4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:47 +0000

    measure proper contrast suggestion sink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
