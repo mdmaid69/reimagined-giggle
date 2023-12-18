7f875059e875d91a1ce6f213e0f11e3851370617
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:26 +0000

    quickly cause doctor cope deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
