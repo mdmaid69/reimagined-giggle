51eeea01ec97e33204464c2cc167e1f5b13c4d36
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:03 +0000

    steam work change never beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import tensorflow as tf
+print(tf.__version__)
