6c09e2ca64a72ed1500be7e053cb3617f0a48f63
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:54 +0000

    style gas motion couple smell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
