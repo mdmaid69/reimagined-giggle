5f2fbb5a373a627df572627850200d7086ef879d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:53 +0000

    permission inside tear record gooff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
