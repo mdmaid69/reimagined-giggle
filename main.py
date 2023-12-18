58e3060224c31a61e80968c3f5c959c72958f041
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:07 +0000

    shut 565end damp walk old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
