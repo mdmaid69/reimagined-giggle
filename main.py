9eda6b4a30dadfbe63f56b494d11a285c61babbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:38 +0000

    cruel wind guide cross course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
