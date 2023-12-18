dda577ce2ac0d4482c680c288f79ac6155943633
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:11 +0000

    one clean lot nice unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
