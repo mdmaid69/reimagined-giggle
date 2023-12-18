930068a353286251dc4468de717e9f84904579ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:42 +0000

    theirs measure race cruel skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
