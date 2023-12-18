124117c3cf9f71b1f2d0fc673f20ed44db38be00
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:14 +0000

    this thought car fire clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_item(array, i):
+        return array[i]
