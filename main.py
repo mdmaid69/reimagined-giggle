d32ee18842fb95859d72c4415fb733cb11e90289
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:29 +0000

    walk walk shop question while

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
