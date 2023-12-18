965eba4df79dd95780deadbd8f3b2427fc62990d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:56 +0000

    test coat winter disapprove wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+for i in range(5):
+        print(i)
+def square_number(x):
+        return x**2
