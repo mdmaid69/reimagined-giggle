6a0e314daaa64d6ca4cd354dcadd49b7e5d2bd45
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:52 +0000

    story film getoff description march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
