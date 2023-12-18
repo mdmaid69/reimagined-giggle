f8483fc92538a558be08667ae3911418088d8b72
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:20 +0000

    hang wealth break party load

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
