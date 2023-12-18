abd9e891292b45df6d3ce4d34684b8e0ccc5dc02
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:51 +0000

    stick distance comment question many

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
