91c1464dfd9f81287fc688d6fe704a9a2644f032
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:49 +0000

    afraid command estimate soon sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
