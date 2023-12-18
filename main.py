e2fc2651e53f31a501fdb3ad858261a829651b4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:10:49 +0000

    theirs target miss goaway obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
