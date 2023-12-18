3136bfa9c15cc260ad33269c5c4bd4022be9dbbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:04 +0000

    sting any cut ride die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
