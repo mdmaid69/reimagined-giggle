04c5b8a075ec5e82f8551224892a04cbfef03b2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:23 +0000

    grow awake deliberately mistake reduce

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
