8d305522b51cb1c5181573b24c19f3160ca7ed3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:39:53 +0000

    goon side subject steal bank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
