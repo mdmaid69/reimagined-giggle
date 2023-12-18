5882e36c27131903a6ffcb9d302fc379a0839d9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:37:21 +0000

    pretend means minimum reply experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_typecode(array):
+        return array.typecode
