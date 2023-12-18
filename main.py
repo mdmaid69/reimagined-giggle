4d2a1fa04140e594eddc431c4361e90738ae717b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:46 +0000

    OKorokay easy care how hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
