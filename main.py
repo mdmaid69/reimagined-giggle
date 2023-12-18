497c5ed589c62fa2b300bc96f7018cf0d1060042
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:59 +0000

    computer time hate neither fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_pressure(force, area):
+        return force / area
