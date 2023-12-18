0469d3fc5eb74499ff2f010e29c367593b330aae
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:30 +0000

    nervous dish far pipe pipe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
