9162ec0cb6304efc12d0c45b2a33fcd3d1e4800b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:14 +0000

    defeat strength sky this sea

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
