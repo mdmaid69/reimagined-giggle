8c1ddce7eb01ccf382c9d2de5616e3f2b25ac566
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:43 +0000

    plan workon breakout along professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
