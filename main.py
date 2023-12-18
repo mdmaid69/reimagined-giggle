22394e67dc688309931c9695d06d0fe7ac96140f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:58 +0000

    this breakout listenfor after offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
