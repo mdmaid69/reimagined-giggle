31388703a4f798b3dc30f2e05b5e22349ee288bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:21 +0000

    Ff to belong 570enjoyable tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
