49f4fdd03bdba3e40e8d5ea1c0ba355d7c0b1dcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:50 +0000

    shall child total lift objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
