3ad0cf76c86251c46f191d9f84f8abd052b6e2ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:26 +0000

    correction stamp comment emergency tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
