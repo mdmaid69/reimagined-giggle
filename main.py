49d331cb3bed619ebe40367d5bb4fa4ae1557a6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:11:57 +0000

    possibility cook image pen round

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(lst):
-        return min(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
