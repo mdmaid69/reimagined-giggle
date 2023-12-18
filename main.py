a2c77ff9369cdeec9747074879a4747b67b51ebf
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:32 +0000

    cold another pretend genuine the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
