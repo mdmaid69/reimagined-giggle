fab59cbf81fec17e85f98e35137d544dd9cc5cf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:09 +0000

    wave after fortunately generous say

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
