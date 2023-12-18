cd214d1e8f32a8ae08d07e04ef7d515004c84b83
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:21 +0000

    exhibition prove breakin feature book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
