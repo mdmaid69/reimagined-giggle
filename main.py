e500339def25800c3f77696b7e14210de686e66a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:03 +0000

    you refer 566end conduct mind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
