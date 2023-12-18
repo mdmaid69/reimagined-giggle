ae828d42fa9dcef94095a5c2352ed65d9434e17a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:38 +0000

    soak niggaacademy form ignore threaten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
