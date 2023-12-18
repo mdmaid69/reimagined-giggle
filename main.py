c15ea50856b7c400094a4c3687cd7c0fadc653de
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:12:03 +0000

    thinkover the relax married fortunately

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def convert_array_to_list(array):
+        return array.tolist()
