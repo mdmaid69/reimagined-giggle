176419cf8b4475456f537e5319552125be3a4e44
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:23 +0000

    lunch hollow back risk night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
