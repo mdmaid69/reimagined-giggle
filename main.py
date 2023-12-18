ecda21b7d1bad7b14f76f18b9a7631f6b1a48fd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:06 +0000

    sport circle afterwards proof suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
