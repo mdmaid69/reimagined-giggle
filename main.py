f20658ef6a8fb4939f74a47b56ca6b0991b5aed1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:59:18 +0000

    add rubbish steady air spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import time
-def get_current_time():
-        return time.ctime()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
