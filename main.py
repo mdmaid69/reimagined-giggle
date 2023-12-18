976c7eafe5ca9ee4bf57e5a2445d62c1ddbde65a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:06 +0000

    star passage move join confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
