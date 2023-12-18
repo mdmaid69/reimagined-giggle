aeda1431a68183f6c238ad21a198a2fcedf227fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:12:35 +0000

    dress calmly shade comeback amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
