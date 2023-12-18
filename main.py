f3aa3aa781a703f53f3bb673c946a93d00085ef1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:23 +0000

    design original smooth tour view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
