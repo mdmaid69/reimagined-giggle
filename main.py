ef1b5e42277fdd6a0045b9d954bdc0b14818ccc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:43 +0000

    male plenty lot disapprove home

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
