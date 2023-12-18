4329305cd477d5bcae75cc851a142b5dd6a9ff0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:51 +0000

    think tune town nice about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def convert_to_octal(n):
+        return oct(n)
