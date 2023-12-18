893df49030f9607d90ed035be7b36132a8404be3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:05 +0000

    substance Ii stupid start comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_work(force, distance):
+        return force * distance
