199e95260f2a5d948e65a4db8dc36ea1821f7e79
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:27 +0000

    tomorrow Qq thick roll tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def sort_list(lst):
+        return sorted(lst)
