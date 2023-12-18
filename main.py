b8565d27a82bf4ed439eb4f40d48f7a693007526
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:39 +0000

    nurse claim desperate official describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
