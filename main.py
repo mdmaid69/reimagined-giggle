2a473ee9ce3b7a5a771d71540481c5572a33f6f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:49 +0000

    defend married rise profit influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sys
-def print_python_version():
-        return sys.version
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
