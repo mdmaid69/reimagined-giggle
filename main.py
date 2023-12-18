3a8444e7aa9dc1fd333790ebd1599ad199a9d894
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:59 +0000

    confused commit offend tough there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
