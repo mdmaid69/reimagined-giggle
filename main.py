5619f89e195473627e8f0006f50a5d3e7a02621f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:04 +0000

    address generally contact within glad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+print(math.pi)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
