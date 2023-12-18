720de1804538e25a197bd7708241be7f15658c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:45:40 +0000

    bound thick account popular fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
