4331c793a083e0659d8441f21d0e8a8710c817ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:53 +0000

    whoever clear such petrol clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
