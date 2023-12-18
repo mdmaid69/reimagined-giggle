334440c91092b8c7c117ef383ea111eb1e7e8937
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:14 +0000

    naturally scare Ll birthday hers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
