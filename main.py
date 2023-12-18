fcf4717971923cf6b27452e519dfdd5b244935e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:16 +0000

    positive angry intelligent politics frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
