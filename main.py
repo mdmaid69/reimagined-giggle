0a7e83b7b1f09c8c55e3dc665f14ce530899ca39
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:16 +0000

    bring early march black report

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def print_python_version():
+        print(sys.version)
