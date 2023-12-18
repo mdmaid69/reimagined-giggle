43f5335ded13a108abb8c2d330eea9243d6c5271
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:10 +0000

    born brush exhibition split sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import random
+def roll_die():
+        return random.randint(1, 6)
+def convert_to_binary(n):
+        return bin(n)
