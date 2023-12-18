d6f308cab716ccc383aa7421b0ee1ce889e91eb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:08 +0000

    realize generous us what try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
