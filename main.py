c53a7a2ff6fb7b1cffd9bf2bf74c2295a6e6a254
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:45 +0000

    them impression bag attractive anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
