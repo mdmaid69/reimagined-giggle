ae653553ea7228daa6caea75e6c3d8f46143b9ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:13 +0000

    only conclusion very black can

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
