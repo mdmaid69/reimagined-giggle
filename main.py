6b659d1183c4b8d703043bd9572197e8f1e542f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:05:21 +0000

    truth release giveback container weekend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
