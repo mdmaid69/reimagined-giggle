f84d369b01fe1b884c619c003c68528b5c9ee1cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:38 +0000

    know quickly mind pay sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def exit_program():
+        sys.exit()
