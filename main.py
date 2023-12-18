6b881bc860fc414dc8cf356da5129e4e3b548fad
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:59:10 +0000

    together tragedy convince myself sound

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
