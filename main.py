b3d219331a31b83c830705035ee2d9edc30f06b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:20 +0000

    minute callat stand hers yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
