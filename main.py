acaeed8a6f22c8183782945e709975d0cea7db00
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:40 +0000

    trap communicate meeting another purpose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_string():
+        return collections.UserString()
