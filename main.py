ec0a6ca44bf9b1665673e062abaec6643a6f929b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:50:26 +0000

    near anywayanyhow flavour solve alive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
