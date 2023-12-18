6724df08e9de0dad5158cf6655cb8d48a7600b1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:09:57 +0000

    over offer before interview collect

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
