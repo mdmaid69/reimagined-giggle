112eb12add72682ccbd0a921f429546c35b33171
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:02 +0000

    entry deal majority law on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def setup_logging(level):
+        logging.basicConfig(level=level)
