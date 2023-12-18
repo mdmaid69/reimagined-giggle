3071a2722aa316a63e10ab925ac4838eefb28db6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:58 +0000

    cost question fashion corner anxious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
