a61b3dea9b0a35b06996003746cd5249704ae81a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:39 +0000

    part reserve criticize dress keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_power(work, time):
-        return work / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
