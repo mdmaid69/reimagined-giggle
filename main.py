602513670991f4bd56bfdb7a9a9f863499a4aff4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:55:39 +0000

    butter probably escape different mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
