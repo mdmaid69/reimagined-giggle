3f9e2703fd978e054e58bbf4e694758126d7fd58
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:52:46 +0000

    mother compete stiff different inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
