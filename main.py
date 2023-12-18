48fd0f17fe37e72146bdd7585dca8b6bcb6a064d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:51 +0000

    stop strong stare dick dry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_work(force, distance):
-        return force * distance
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
