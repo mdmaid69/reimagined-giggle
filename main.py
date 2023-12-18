61b779d27cc35850e4adcbb7ab78f35bccf117ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:24 +0000

    turninto relation its lead expect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
