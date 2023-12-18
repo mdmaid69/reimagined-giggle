46cab8873769689af149c43f31b8a4487e36b876
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:32:43 +0000

    pretty clean seriously traffic mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def count_characters(sentence):
-        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
