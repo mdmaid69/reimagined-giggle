5fbcf2015499ed4009f0f51976e853c183d42024
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:08 +0000

    proud extraordinary wealth choice frighten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
