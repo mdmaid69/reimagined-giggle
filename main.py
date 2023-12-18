842660df4f3a6507947034a4916e2478e94d62c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:06 +0000

    convenient embarrassed fortunate criticism produce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
