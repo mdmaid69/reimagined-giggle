65ec196542fe2693ded57915f09be82603388a68
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:42 +0000

    by wise shock tune mistake

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
