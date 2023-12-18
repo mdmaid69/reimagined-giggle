fc6b86aa42c5ee3e0e07bc6a34575d53081b1a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:19 +0000

    rate road last friend much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
