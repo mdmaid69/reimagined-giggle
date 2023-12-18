9075ad7c63bc4afd654e6bb5f78794787f93cd6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:01 +0000

    brain yesterday zero Ii never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
