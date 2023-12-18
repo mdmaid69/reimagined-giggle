dbf1ccb6224c599654755a84c433851b24e9c809
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:33 +0000

    goup study cope treat near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
