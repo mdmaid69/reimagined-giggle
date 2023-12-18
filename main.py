f78af5ebf79d7ef82c9c969a1d10358d6b0f198e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:12 +0000

    favourite risk better big leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
