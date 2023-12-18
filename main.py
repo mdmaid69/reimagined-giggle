4a275ba6f45ae67b776c56cb05e813cd2f2a194d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:02:11 +0000

    above undo so anything part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
