d7b89a7f48f235ca4ff4d8c4812cf4e5ddacb5cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:39 +0000

    bad yet apart check once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
