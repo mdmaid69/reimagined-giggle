d27a47ea19faa5d54387f05dfb8bf50677d2cf55
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:44 +0000

    takeoff failure like punishment care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
