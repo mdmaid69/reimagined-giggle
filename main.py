8196864f6b1a2682c2690df057ee97d1803a7c87
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:08:19 +0000

    fly noisy stop sleep man

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
