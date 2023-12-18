708097ad5d0cedd174d28da5023fd04545a1aa54
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:45:45 +0000

    real only lunch afford glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
