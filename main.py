73786a47cfe0b19807b1bcfa233b85a17a95946f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:29:30 +0000

    race somebody repeat fit feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
