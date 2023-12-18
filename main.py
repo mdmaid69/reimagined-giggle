db96f726f11b20c83dfaff015af41c26bff953d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:40 +0000

    dress wish surface rain check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
