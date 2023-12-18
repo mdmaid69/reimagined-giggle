5cd8b22f6a114c59b3b053fe2ac032ae05a7d742
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:45 +0000

    calm terrible touch attempt pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
