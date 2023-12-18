6bff41d7672e87bffac9a490c7216a066aeb8a4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:59 +0000

    late independent precise steal order

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
