b3f1e075b72ec5109059c0358a2543e9118dfcca
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:03 +0000

    blow evening replace help send

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_force(mass, acceleration):
+        return mass * acceleration
