19b52326b0444cca5d04a4ca2cc46bcfd637caa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:37:56 +0000

    settle name burst comeback float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_characters(sentence):
+        return len(sentence)
