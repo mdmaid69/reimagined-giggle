0eeca3f3cf006104c6f396a2531c1e8ad3edba9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:22 +0000

    still government basic poor hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
