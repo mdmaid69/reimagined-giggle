b4a50be7589003d16d73fcf87774dda77f2788ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:35 +0000

    keep relation satisfied hurry escape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
