4655f8da15d64d77ead27220e1162812b1871ada
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:28 +0000

    fine store film he judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
