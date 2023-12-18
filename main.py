07667eb41f1edbd80d0f2200b20cab24a349a220
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:40 +0000

    strong keepaway abroad tape guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
