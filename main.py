1039d20dbfae371ab6e4adc941f91d93e0098731
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:43 +0000

    damage possibly push environment arrive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
