86400ab35d2737641241e0eb945793f1bb00703e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:05 +0000

    dance study her worth bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_item(array, i):
-        return array[i]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
