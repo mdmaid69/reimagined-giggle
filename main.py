30d50f8718c05ce525e22b04aae1ab877f6fe734
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:45 +0000

    lunch image single authority popular

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import requests
   def get_web_page(url):
         response = requests.get(url)
