ff82a0ec6c324c09ab28e5223e83e675fdb008be
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:23 +0000

    573entertainment possibly hole estimate yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
