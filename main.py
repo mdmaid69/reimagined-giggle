8942eaaf7093f4bc11895e2d026d2dd85a091939
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:36 +0000

    tune effect call 570enjoyable time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
