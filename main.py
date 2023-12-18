aa94823bed9194da7159c51d67dabf1d642cf3b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:30 +0000

    statement top join strange by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
