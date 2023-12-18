402fe0bc9de85dd272691d2390cc99f950d03f4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:40 +0000

    arrive skirt failure taste slip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
