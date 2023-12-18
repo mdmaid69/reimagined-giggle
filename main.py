7c52ee4032f70c9e5654819ab42e9a94edb03f86
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:20:56 +0000

    boat war invite bin phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
