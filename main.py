7eaea7cc5f2148f48c9e9c29fe5ea5e200516698
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:22:01 +0000

    determined round follow history check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
