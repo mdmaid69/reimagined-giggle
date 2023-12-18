682158dbfc76d7127084b7f14ff21948e7231fee
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:57 +0000

    powerful chase similar ride clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
