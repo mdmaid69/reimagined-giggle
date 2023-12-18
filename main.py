e625ac007fc1b7784f930089ee63ce21752573fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:15 +0000

    suggest original own course very

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
