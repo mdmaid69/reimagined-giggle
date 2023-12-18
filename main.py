ef102313d2ac2b948035644e9374197a00c93501
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:08 +0000

    close book dangerous somebody anything

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
