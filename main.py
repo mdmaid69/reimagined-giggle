951a6f006e77cbce5f130033df56b0a09f509d5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:37 +0000

    connected bunch sensible accommodation welcome

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+for i in range(10): print(i)
+  def is_odd(n):
+        return n % 2 != 0
