be018f0a09eb7ae52482c3e48680a5612de648e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:34 +0000

    Uu road past them laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def add_numbers(a, b):
-        return a + b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
