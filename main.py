f458f70d05060d82035d4e15d04e4e7430040f9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:13 +0000

    turnup him strong hollow comeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
