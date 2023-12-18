2afd39cf964f50794de1788b99e2717945b23a2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:46 +0000

    cook accept hour watch phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
