9fbdb2af19d3a13d2ab601d1bb5eac552ccea066
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:50:55 +0000

    ball talkinto press allow refer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_power(work, time):
+        return work / time
