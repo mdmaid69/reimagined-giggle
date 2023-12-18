3deb9f4c03650a89580b60982bed5cc4f69be514
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:07 +0000

    raise fairly season at scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
