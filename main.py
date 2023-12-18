a4ca5b884696fe719eaacf7d5e9c5f4283441508
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:24 +0000

    nervous top fortunately will tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
