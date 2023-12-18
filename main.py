fdd6a99c95605bb758d517aa9cdd21f87226130c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:33 +0000

    possible both suppose poison wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def add_numbers(x, y):
+        return x + y
