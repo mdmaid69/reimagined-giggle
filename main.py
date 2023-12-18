23e94035896a6a34d9313e942130c249d6ed3722
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:38 +0000

    bush 564encourage rubbish aware murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
