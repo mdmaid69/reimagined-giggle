a62ff968a9c7a05b9fef04fb7c6db5a8dd6fe342
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:05 +0000

    indeed 567enemy act broken variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return math.factorial(n)
