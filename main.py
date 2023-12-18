892ede4282760eafe94973fa497dbf8d4663ae43
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:22 +0000

    accept basically what rough giveback

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
