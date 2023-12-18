13fdbda7b501441badb9921ce419ae7860679df6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:46 +0000

    disgusting fashion whether unpleasant in

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
