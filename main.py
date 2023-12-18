0aae521cc37d5cc213b6ca35d17900acd707503d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:42 +0000

    material sound noise quit married

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-print(math.pi)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
