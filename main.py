7979fd896b46cca6e5a473b116df175da9a022f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:49 +0000

    family waste very combination ordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
