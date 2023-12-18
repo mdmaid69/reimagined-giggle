22c68af6bf78c493387de66e607bb011e1bdfeaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:14:57 +0000

    drop mix affect emotion chase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
