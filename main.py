eb3c0124c9c45b0d7641b7628320c4b4e42a94ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:26 +0000

    exactly father expect guess press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
