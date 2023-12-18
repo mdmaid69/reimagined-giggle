be63cf57cb60ce60a771d3a3be107eec4c379f6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:10 +0000

    freeze certain either concerning neck

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
