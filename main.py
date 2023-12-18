2428c127ac08eb1ac3007c63f7e0d465e8da234d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:58 +0000

    rescue direct gentle worse pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
