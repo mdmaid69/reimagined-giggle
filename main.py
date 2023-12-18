2d905f13212fb3a03a0f610e74e48766111c67bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:44 +0000

    hold scrape niggaacademy least block

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-for i in range(10): print(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
