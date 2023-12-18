20c770e1823a3497010c11ab16148904effdaf18
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:29:32 +0000

    approval mystery me call kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
