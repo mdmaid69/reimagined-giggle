d07b351f4ecaab63aff27a344ab5f58760ea0a6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:58:22 +0000

    professional care variety though fat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def get_current_working_directory():
+        return os.getcwd()
