0dd2478b5b447b07c02f5570afee7a512d10e379
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:40 +0000

    taste forgive back taste copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
