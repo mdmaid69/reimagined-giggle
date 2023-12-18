70de8547469c2ce9ce605dcadc18f87d79f08e92
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:00 +0000

    holiday hear mine plastic extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
