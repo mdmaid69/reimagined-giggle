94b8230c10104afa90c15ebde34f3e649499a947
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:18 +0000

    attach quickly lost mine cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
