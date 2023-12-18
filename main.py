7ce939e229d19155cdfaa88d9316b9583abc5a43
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:49:46 +0000

    scrap accept sudden doubt exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
