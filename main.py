1a4a66c6e78470f0d1edaf7667e8a1420a938fd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:23:09 +0000

    hair choice 565end rarely past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
