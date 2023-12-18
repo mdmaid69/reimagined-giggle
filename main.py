88d6ae12d72fcf322c2eb72448d590e58f7ae41c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:38 +0000

    large buy 565end insist pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
