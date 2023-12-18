c205eb2101737d3f3eec0390e2e7115e865ed601
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:14:18 +0000

    move force stretch manner have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
