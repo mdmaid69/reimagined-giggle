3b2ebc1281e95527f4ddc7981f35ebf322e12581
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:23 +0000

    I actual declaration run rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_work(force, distance):
+        return force * distance
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
