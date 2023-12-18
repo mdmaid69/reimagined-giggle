32279f084905955612859087ef569d1f32260321
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:19 +0000

    score tap surface drop on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
