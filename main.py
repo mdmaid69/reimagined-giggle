9e3a5a13c4d6e33fd6eb7e81da231589cda3ae21
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:04 +0000

    goodbye principle pot move getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
