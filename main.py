554043971b2c2e719a10d2445a634d7961866e1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:44 +0000

    support at deep thought among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def reverse_array(array):
-        array.reverse()
+def append_to_array(array, item):
+        array.append(item)
+import os
+print(os.getcwd())
