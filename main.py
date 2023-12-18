6aa4d03af1a59fb81a7867cd978a9b33d902755c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:00 +0000

    talk soon your reduce advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def square_number(x):
+        return x**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
