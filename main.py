81cd803174365b80edd34ade4fd7fd87a11f6c83
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:35 +0000

    cum kill protect temperature exhibition

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
