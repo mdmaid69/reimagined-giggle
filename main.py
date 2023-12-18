b1a3b0016d6a6698d684f79e2bec037f21f59e81
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:42 +0000

    radio excuse tradition national interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+print(time.time())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
