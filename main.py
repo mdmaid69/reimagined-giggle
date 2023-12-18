81057ed5d95d5c43a0f1dd353cfd8612021be5ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:11 +0000

    bar here factory precise pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
