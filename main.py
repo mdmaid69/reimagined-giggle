acf4a7506f9d327e0623ed0087356247f5ed962c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:01 +0000

    believe exploration interesting post improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
