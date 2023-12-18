8e68c7fec794e7308f0aca56f78438c2fa766f1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:21:16 +0000

    bury runout genuine fun Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
