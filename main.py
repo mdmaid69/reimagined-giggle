fd07beb1e28b0e7bc9a086917e12c680e25fcc3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:25 +0000

    base Pp difficult emotion bye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
