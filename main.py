4810f40006c81454c00505ef256e0e9e304b1a10
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:23 +0000

    up difficulty treat get compare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
