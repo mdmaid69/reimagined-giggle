d6e29401f3cbb636ffccd24943b482e3ff96ee2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:58 +0000

    likely street telephone scared successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
