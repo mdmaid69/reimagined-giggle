a1b92ffb7c0ad37b6e5a56bfbda430f168b1891c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:08 +0000

    suggestion joke survive check match

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_acceleration(speed, time):
-        return speed / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
