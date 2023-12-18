ef15698cb4b559bdbb35ccb1770e30cde0e357f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:18 +0000

    use new personal clean wait

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
