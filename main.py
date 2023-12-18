a5d11658849d665d6ef926d9bcbade9238582892
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:23 +0000

    comeback game long workon whichever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
