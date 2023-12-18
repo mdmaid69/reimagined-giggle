f1cffe0e9fcb5f8000d141613dc1e5723248f8f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:22 +0000

    break waste last century similar

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import sys
+def exit_program():
+        sys.exit()
