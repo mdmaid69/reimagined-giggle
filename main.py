99e9c7e11d448e192a3b391d0fb50a8336ef74ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:23 +0000

    remove wind subject education plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
