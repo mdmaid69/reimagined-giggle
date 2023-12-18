cf3177eeb44fbb32a14ba576e8c231ce129d7c57
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:32 +0000

    within intelligent smell suddenly inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
