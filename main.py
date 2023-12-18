16d846f50b15752bf44772d0338025768b701310
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:28 +0000

    spoil language cover someone lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
