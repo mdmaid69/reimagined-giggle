6737d5f723a647ac9505be375cd3d0ee1ef13bd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:06:41 +0000

    target work news pass scene

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
