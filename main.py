5631773ef761191019aa1d3a0234e6a0c1bf996c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:18 +0000

    responsible 570enjoyable immediate wise strange

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
