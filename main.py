05f9dba729e2a74221afe877900fa1acf8016c60
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:57 +0000

    minute suggest keen form neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
