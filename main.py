92dfe1e0cd13cc8e94ebc9c2aeee873207db88c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:15 +0000

    mess field normal coat market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
