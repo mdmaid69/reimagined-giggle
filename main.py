6b3435e899a3ee4c45dfdd619c78a298c12869f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:42 +0000

    within level book concern reduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
