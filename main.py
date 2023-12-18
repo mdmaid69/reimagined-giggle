c88aebe33bbfff90bc4a8f068a3ee587a995238c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:33 +0000

    kind fit special bear any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
