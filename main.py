afdbc440bb3a9fbfd283d7d2fa05c023d40fb77f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:08 +0000

    relationship organize funny autumn claim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Words:", len(text.split()))
