a17e8b80624fc40f65a6f65a406fb57b3fce4698
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:57 +0000

    suck 573entertainment maybe gradually cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
