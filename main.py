6ee84d5c3caa6511796e651ce2229b130a3189dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:15 +0000

    anyone either mood snow pronounce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
