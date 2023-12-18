6070c4493eab14835c3dcf828a540b49441cdf92
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:55 +0000

    image speed pocket our runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
