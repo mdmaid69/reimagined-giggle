f8a099374d36e854c1e595c7d70eba5ce028b731
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:41 +0000

    middle concentration thankyou sit process

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
