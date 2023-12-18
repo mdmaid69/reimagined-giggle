4a33eb2125a7b02d5bcd0e7d6d05b55034878a8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:05:20 +0000

    otherwise modern bear no talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
