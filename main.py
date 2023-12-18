e6112fb97d192883338b115d1c549f4b9108cad5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:35 +0000

    declaration from motion false ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
