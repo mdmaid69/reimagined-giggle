415181ebc9b8f493e451aacc05fba36ca83b2a62
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:41 +0000

    project track across file accept

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import csv
 def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
