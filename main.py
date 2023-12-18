3f7fb73e969a3848c06fabc66f849a40c80c16e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:12:29 +0000

    company market succeed before fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
