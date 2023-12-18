56fdc5b74f101d8f33ad858a8ec62b73001e6d67
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:24:35 +0000

    land peaceful offence send answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
