5993fcc4abfbd8525fbb0c4eeedc62a94a71c6d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:59 +0000

    suck suppose Itself uncertain guide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
