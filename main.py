b2b366edd1e167b8c7f7213c8f001b23adf0b70f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:23 +0000

    nurse defeat defend treat goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_max(numbers):
+        return max(numbers)
