5949f6b39a9ab0e6e15556ad34c177482882d0b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:24:00 +0000

    arm simple warning cap desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
