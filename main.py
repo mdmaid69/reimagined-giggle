73a334888f34115efc49eaf0d925ca0f3eace0ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:14 +0000

    whichever danger talkoutof previous beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_odd(n):
+        return n % 2 != 0
+import platform
+def get_python_version():
+        return platform.python_version()
