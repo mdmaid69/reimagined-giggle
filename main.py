849b840457bb3e8ee7a60457d8745969c487938f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:25 +0000

    flat damage eye habit result

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_max(numbers):
+        return max(numbers)
