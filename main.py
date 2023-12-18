dd4976db3354f660169fc160459f962956536f94
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:24 +0000

    damage into empty bad surprising

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
