6c532e44d143287f3500504b2f438f5662873e44
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:42 +0000

    appeal sort other accept circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
