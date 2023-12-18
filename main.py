3c26047b02513c0dc3f54aaa3ccb5ef4d537c0b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:40:43 +0000

    invitation delight murder forward confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
