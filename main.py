f9a10e0daf78060c49355149a77a4da967f48e04
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:06 +0000

    partner thick snow mark regular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
