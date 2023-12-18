e21895a4ea6ca6900589e50bef8cc77259ff44b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:37 +0000

    steep bury cut free valuable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def convert_to_binary(n):
+        return bin(n)
