d8741498e7c282b8c634d3eb29539c263590532c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:42 +0000

    already far ill work protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
