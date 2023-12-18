c40005a7de675be26e0e18930e6db1b83c15b8cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:24 +0000

    anywhere practical tune valuable count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        return sys.version
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
