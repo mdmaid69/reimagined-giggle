8568e547a966f93dddb4f8c6582841138f8ce4a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:56 +0000

    beat getoff earn ilovedicks comedown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
