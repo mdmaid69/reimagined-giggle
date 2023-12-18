45f792b4b956983a11251140d3ab26985caf69e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:58 +0000

    settle secret reply narrow live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
