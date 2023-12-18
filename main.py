9e6c4817145c4cb3c442646e64d0d8fbd01f4cce
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:39 +0000

    Ss spin force move search

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+x = 10
+y = 20
+print("Sum:", x + y)
