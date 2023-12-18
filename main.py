e2bf59cbdc49bae5a28938118a69f08a4ff7eab0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:41 +0000

    without run reason it project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
