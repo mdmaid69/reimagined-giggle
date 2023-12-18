d44642c007407c9318351a9f0ade9b843cfe7f12
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:16 +0000

    car discuss winter fond excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
