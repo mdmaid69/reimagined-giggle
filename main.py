4fbe958b93462a830802b77788a84880a2cbe280
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:13 +0000

    score goon land find treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
