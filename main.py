695aa7dcc83124d7e1c52f36983fee89629d8c9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:05:55 +0000

    introduction brown custom holdon method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
