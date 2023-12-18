4df97e13cddcada33f1d95c4eded201ef53c2da7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:24 +0000

    offer band unusual cut by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
