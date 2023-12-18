e2e19635dd92960cb59cdc1971dc8a396cb9785d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:38 +0000

    need bang firm mine play

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def is_even(n):
+        return n % 2 == 0
