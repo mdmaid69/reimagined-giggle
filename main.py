802b94b39a2702a085eeaa91b4cf07dedbedbc88
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:58:04 +0000

    window kind lucky over whichever

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def square_number(x):
-        return x**2
-  def is_even(n):
-        return n % 2 == 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
