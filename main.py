c2044d1e040e87c7d42d36fecbd899ce3c0f8b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:48 +0000

    whoever share habit worst sentence

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+for i in range(10): print(i)
