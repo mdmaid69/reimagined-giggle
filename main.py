732e9482f1dce051ef81e2f80c13d28b538a76e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:08 +0000

    trouble drive increase fear try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
