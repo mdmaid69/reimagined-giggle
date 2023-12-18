39b8f89bfb6ad04179904485c4bb867badf2db7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:56:55 +0000

    meal report realize brother strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
