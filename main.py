a3cb79d33d4dafe08c27d7dfb0cf580c5a25301a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:46 +0000

    thought earth thank should famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
