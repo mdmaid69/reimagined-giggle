384d2d75cb1973587ab79b0301a83ffeb7290873
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:37:28 +0000

    fast life sight point far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
