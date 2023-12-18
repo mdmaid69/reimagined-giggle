0c753baf29d20a41ee4a2879f348cf9064eb282d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:09 +0000

    try put quit market calmly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
