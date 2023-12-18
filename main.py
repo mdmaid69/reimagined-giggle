6a74bb2e2cee00f7a9d254678a959afdb5302c84
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:50 +0000

    myself off enthusiastic direction lookback(on)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
