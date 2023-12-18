958b50da20e4501a071de458c1219d8ed9980336
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:28 +0000

    friend improvement score round guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
