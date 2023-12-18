403fd455b891d61d79d172c85aebf51cd8909ff8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:03 +0000

    convincing still improve breakout fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
