fbdce218a0e9aed622c3091a72b4cbf6024b84a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:00 +0000

    future field band recover truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
