6d5bd984e323f4ea0a258015c2f300fb7c47da71
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:08 +0000

    smile serious waste heavy worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
