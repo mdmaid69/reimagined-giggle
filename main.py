5a692ae8d1fef91cc9ce7e7d8b0c9b9468eef703
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:19 +0000

    impression year issue fix foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
