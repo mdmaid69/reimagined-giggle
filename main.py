dfbe2b5570bc5dcc4932b84abdc60b9e6dc21e9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:00:24 +0000

    shout description keen behind through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
