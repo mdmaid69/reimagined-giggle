6f659714d1612269482cbbc4e05db65170971b00
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:01:42 +0000

    them dry during bother music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
