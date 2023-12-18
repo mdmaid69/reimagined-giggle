c0672e45835160f59862f725ce60052fa1d3ae92
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:23:23 +0000

    stand slice wild sell serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
