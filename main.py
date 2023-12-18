8cefa53dda8cf379fa4b354319f97b2185f37def
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:02:37 +0000

    murder obvious still demonstrate contain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
