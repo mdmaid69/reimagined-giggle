6f123a95194c70bab3beed23623a568d54608ec7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:31 +0000

    slightly standard clever minimum glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_power(work, time):
-        return work / time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
