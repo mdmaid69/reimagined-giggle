5410d8ea30f27b137077f4b51ab825f912410485
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:29 +0000

    last final development stone cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
