d7544feeb33260d151c7b0c9e1cfad6074f4a8c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:17 +0000

    frequent winter kind report because

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
