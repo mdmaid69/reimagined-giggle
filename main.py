72fb8fa140d5be50d12bdda1d2cdc5618a47092b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:50:55 +0000

    Yy ability fall eat appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
