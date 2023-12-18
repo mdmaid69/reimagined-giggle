8a74fcb31cd4d91299b9958897827991e8c253d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:50:48 +0000

    unfortunate involve apart couple slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import time
+print(time.time())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
