d52fbf1f3f93f76239247d03a323b64a1cf7469e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:29 +0000

    ready brief spin programme dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
