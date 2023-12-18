827ff942d5e0fd72432f0aae69e80cc045c0d609
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:09:19 +0000

    Qq open favour Yy fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import re
+print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
