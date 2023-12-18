7589370ddd1f3d4d4c3e29001656573bb8d7948d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:47 +0000

    complete ought receive centre necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_exponential(x):
+        return math.exp(x)
