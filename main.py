b8515ac9501293fa1030ac0d68a066f707649046
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:45 +0000

    attach have than gentle nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def count_words(sentence):
+        return len(sentence.split())
