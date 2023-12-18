130649e5f3e2376460302153494c8bae7dc3d098
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:11 +0000

    mine best lonely correct split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
