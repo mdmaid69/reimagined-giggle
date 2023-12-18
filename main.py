6ed936ad96c0c5e46bc796f414a723296bdf7987
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:46:42 +0000

    Bb follow powerful dark owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
