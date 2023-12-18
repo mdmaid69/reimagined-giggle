22cea3f4231a3d5ae73a10f9a3ecdde3033b3fef
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:39:42 +0000

    send smoke recover stranger several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
