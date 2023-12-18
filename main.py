dc28923c3781121b9ab51776db80535106cc0bb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:15 +0000

    exercise low easy married appeal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-name = "Python"
-print("Hello,", name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
