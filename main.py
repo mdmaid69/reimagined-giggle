05cc517d48ddb83d83da3b9c400543e21a301f79
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:00 +0000

    pupil accidental possession trade one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
