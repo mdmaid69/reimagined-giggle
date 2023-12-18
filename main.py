265cdb95cb544dc5bda93e3bac5a2c82f8da5fa3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:44 +0000

    bring dangerous throw near physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
