5aa55e98a69197f5fbe9b9454c66e744b2456e20
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:19 +0000

    game shelter matter schedule between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_min(lst):
+        return min(lst)
