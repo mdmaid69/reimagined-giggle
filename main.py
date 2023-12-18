9190ada0fc9181a5d155190b226f9b5e63b75564
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:04:46 +0000

    smoke appearance shout fit guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
