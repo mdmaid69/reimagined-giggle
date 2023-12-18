f8720c291b35a0f600b942fc08fd165c555a2149
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:20 +0000

    condition accordingto year punish Nn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
