0d480fa9f7b929d89653966cd2066eed07dbf7cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:37 +0000

    moment report open fashion turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def count_words(sentence):
+        return len(sentence.split())
