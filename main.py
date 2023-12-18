fddd4b7069b4cb4e200f13c690c733d7d0d2eb69
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:34:43 +0000

    bag depend long sore tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
