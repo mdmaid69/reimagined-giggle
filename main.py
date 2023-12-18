090446af1d7ef34a22a59b128d095e373db098ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:10 +0000

    spot pattern black people perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
