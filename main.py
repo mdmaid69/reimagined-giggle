2d4e0f7a07b2ee56e5412f942121e93ab3523472
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:49 +0000

    patience punishment stranger flat stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
