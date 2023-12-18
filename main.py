6bfbc3bfd16c0a574fc8232e572c1c04604f1d3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:00 +0000

    gap laugh theirs positive how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
