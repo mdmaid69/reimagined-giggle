c2bf4c168595fcbea35faf310cea4a16d751a5f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:30 +0000

    turnover turnaway contain reply crowded

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+def calculate_volume(length, width, height):
+        return length * width * height
