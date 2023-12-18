c9b41de106e3c65902b984e05befcab6b9830b7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:20 +0000

    field surface Zz basic number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
