c9a720064f1bd869a44c2329d1db07ea3b2ebeca
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:19 +0000

    cook warning still key slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
