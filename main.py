fa8f86ba7f122dc830904bd53cbefb096d92f780
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:25 +0000

    steep father to earth confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
