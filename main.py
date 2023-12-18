a384fef8d69d5295495c2c50f969e268c7b0d2ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:04 +0000

    less complicated police run punishment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
