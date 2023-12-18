130e921f344b0b0bee505c008dd96f6c917e1dc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:57 +0000

    best cover human well cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
