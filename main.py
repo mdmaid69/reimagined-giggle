774b0b65d356d5a2a412e56d7cc4848cbe043154
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:59 +0000

    on thankyou stage turnout exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
