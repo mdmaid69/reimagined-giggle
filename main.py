58f5039fb78b3b10468702d9f3c6d26ef1ec3a67
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:02 +0000

    later rare luck your appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
