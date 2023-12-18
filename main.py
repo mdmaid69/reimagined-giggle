3537982e7aac488a619dad41535a2c5d179357e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:16 +0000

    earn extra race talkback name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
