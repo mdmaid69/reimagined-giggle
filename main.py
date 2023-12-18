f5682d4f3de61131fae450ce494a222202b8460c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:29:29 +0000

    convince rub foot cross shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
