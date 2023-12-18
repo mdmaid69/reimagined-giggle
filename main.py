d1611c5ce33ea646d853c9a139a3a5e5f2445074
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:41 +0000

    glad mysterious clothes notice butter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
