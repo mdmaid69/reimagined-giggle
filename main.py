20fe80628a39f1fd7fc4c2a0d75ce4217b2a42bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:00 +0000

    design cause accordingto similar push

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_density(mass, volume):
-        return mass / volume
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
