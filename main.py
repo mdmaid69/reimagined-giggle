9b7a315e85f43da846c039836d717ed868eaf26e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:17 +0000

    mix 567enemy look actual market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
