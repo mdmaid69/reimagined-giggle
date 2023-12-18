1e9f55114a07d4125c3eab184fedd0321e5da337
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:08:34 +0000

    mail mysterious often shut rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
