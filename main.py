a30cc613ccd65b0cd7f6be4065eed5bf39a1cce0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:58 +0000

    characteristic call subject address before

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
