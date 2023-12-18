7ce5cbb88f4c46b7ff644e314d9c0f8b0d143722
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:02:43 +0000

    minimum trip disturb free weekend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def count_elements(lst):
+        return len(lst)
