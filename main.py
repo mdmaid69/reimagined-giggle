afd77d995e847c3608ccb4f30b274a56fa66ad77
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:26 +0000

    win unpleasant warning among last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
