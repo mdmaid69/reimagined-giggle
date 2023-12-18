48ede54997ccc5f5a965f8497bb871e3bb07a059
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:44:30 +0000

    shade sing pleasure care card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import re
+print(re.match("h.*o", "hello world"))
