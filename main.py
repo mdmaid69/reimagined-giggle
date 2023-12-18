28a1f07a6bf8e827ed252ac225159c95af04a8f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:58 +0000

    this legal swimming break some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
