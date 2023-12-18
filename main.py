9bd083b761088112b069610d043eee8731a08d33
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:48 +0000

    colour embarrassed choose build pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
