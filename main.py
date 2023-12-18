997aa8ae17de1d19e537261370837f4d7ffaecc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:16:02 +0000

    turn behave sport difficulty violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
