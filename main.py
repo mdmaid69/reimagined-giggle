68e8561e2c5bd7c97df04da341301640e14e85ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:24:46 +0000

    track suit duty declaration section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
