9ba4e9960bde018be1873a9b01521f03b19a153a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:50 +0000

    delicate word same importance niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
