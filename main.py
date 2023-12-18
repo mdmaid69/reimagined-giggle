178cb2b2c97ccf7140c82189d221fc9f3d52be5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:02 +0000

    female wish lead fire greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
