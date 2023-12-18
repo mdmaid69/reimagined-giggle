98c112b83f1fd17036e4a5c116cede0dd41f4752
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:34 +0000

    concern creature strange dark step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
