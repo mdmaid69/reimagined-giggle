ee8b2150b979a7f01705e80b421f6b7ba1c837a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:08 +0000

    rule point plain command exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
