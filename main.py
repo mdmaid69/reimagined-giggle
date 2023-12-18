7e63aa3194a17f845827cd1c924662b5512615d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:27:28 +0000

    say demonstrate rich peace children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
