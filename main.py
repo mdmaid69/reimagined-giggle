4a1a964583fff8b9f9566a07235967fd1a1d4a45
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:49 +0000

    dance lean excuse around special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
