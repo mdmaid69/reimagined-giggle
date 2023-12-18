b73b7bda47472030a820d494590f16cef4a55d02
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:57 +0000

    compare Gg share stir backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
