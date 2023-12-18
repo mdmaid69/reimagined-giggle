ba6381e7778642513b46dbb6f8ef38f5c9fab0ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:17 +0000

    brush move mistake dinner spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
