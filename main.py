21d091dcd3a3a43cc382bcb68b04029dfa787eab
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:27 +0000

    proper official goback(to) mean necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
