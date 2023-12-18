57f8927cf3b2d39cc250115e7c754416c1e43dcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:05 +0000

    everything breakfast joke talkoutof certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
