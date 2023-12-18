58a1d7d810a2a8ffe6cfe8ae4b5b3653c75c81f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:41 +0000

    dream with breakdown turnon sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
