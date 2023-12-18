52a1c739d27f2f277a3780a465093565032ec111
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:31 +0000

    shame mistake strange pause circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
