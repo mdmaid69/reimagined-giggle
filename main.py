c6d790c172c43764a5206e6076cf67d67483ebb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:36:10 +0000

    schedule tragedy frighten debt watch

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
