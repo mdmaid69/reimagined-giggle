da76e874ade0d39fd546e194a8e2847745c58fdd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:35 +0000

    married please announce weigh conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+def remove_from_array(array, item):
+        array.remove(item)
