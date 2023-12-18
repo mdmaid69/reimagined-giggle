4e25290aa3770555f11712abf90b040b937e16f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:01 +0000

    software petrol note claim poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
