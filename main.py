c384f4e49d6efd050fb6d331db5166f88751b3ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:48:07 +0000

    turnaway sure fashion past keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
