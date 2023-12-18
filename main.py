5ca6477fa210eaa4d49ce22394f0fbcd921eb9a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:23:50 +0000

    early dress classroom fight cheat

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
