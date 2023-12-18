285d5b5d93aefa177af7ca8f6568a3ccea61ea72
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:20:43 +0000

    wheel keepup crisis listen grand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_unique_words(sentence):
+        return set(sentence.split())
