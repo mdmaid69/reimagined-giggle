62eeefdf0455e46a4da8b02a6706f66ff0919884
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:19 +0000

    walk nurse type eat above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
