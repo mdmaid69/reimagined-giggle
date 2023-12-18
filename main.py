2f1159e2bbab9b0328b64cd64f6af9f9c1666e6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:59 +0000

    area chin spring allow weekend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
