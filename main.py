6a4444517e3274c962128d06194515ee2387f472
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:18:08 +0000

    instrument emphasize impossible favour this

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
