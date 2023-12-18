485bccaedb4fc0192a383ef39bbb2e5936066a43
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:28:09 +0000

    herself talk trip onto increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def split_path(path):
+        return os.path.split(path)
