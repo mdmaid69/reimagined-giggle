b52205c67ccc22036c7759babc3a6b3b82fc9f52
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:39 +0000

    rent noone worse level provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
