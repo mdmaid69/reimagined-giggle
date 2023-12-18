af1b8805df1356b2c089f358e5a97c5d3d958542
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:02 +0000

    protect mark use room runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
