aff5fad4c7212b083ede3f06e5dd7c733477cb3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:13 +0000

    treat unlikely late formal clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
