f5a58254d979ace6865319cbe6cbc946e99f7152
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:05:48 +0000

    active knock jump thin eye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
