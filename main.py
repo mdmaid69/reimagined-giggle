737bfa2a2329b3db07040f86076a432bb0dc25d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:26 +0000

    soak secret present order expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def sort_numbers(numbers):
+        return sorted(numbers)
