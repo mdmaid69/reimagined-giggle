2a705075d2484e39ca8d389c53dabc0416478e5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:02 +0000

    circle nowhere afford equipment health

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
