c91662c793d60138cdc8bcb808dc77b1b06871e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:17:07 +0000

    unhappy ocean influence beat demonstrate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+name = "Python"
+print("Hello,", name)
+def find_min(lst):
+        return min(lst)
