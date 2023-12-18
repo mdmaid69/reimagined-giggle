6239da9a436aa148fc80d8cc133c27cb87c70e2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:51 +0000

    freedom sleep petrol freeze score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
