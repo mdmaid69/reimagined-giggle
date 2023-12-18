d3097d732ba7baf0abbd3d7dd103e62935d967a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:51 +0000

    wall neck breakdown defend stop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+name = "Python"
+print("Hello,", name)
