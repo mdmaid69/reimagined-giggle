614d43fcb81df8a17c23467c4eb3dff129fd923f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:51 +0000

    ground smell illness holdup vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
