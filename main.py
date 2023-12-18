aebedde55626a032be245b2cb3fe9350bd88c2ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:09 +0000

    nice backwards face behind considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
