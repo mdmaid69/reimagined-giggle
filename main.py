d8229595e7bbee3637664575f59fbe42938fbcc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:24:50 +0000

    hello consult dance hear Hh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import time
+print(time.time())
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
