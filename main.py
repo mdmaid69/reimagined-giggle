1501cd3a8357d672b99d773057840642593c9b0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:39 +0000

    mass regular thought hand Ii

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
