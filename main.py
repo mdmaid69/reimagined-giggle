3bdba9dc0ca8b32ebbda49488c6e56dbf6cc2d19
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:44 +0000

    cook along report advantage scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
