7d96a0186c2fa8e2cfb8c2f38bb870c4dde8197e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:47:29 +0000

    giveout emphasis home proud exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
