a294992671e65775a981244847b3fb574546f12a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:41 +0000

    partner finish valuable length stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
