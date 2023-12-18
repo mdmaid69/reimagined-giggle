8e33320867d87a4bc6f435c698acd44e21fd8ebc
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:51 +0000

    draw world 567enemy everyone impressive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def multiply_numbers(x, y):
+        return x * y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
