c4c645861bf7dbdbb9b29b3e1f1f1dbc7cfd125c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:50 +0000

    before suit concentrate settle attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def convert_to_octal(n):
+        return oct(n)
