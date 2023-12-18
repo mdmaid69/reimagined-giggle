056536fb2dc4cd245ab681db1927c944bb831dd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:34 +0000

    ask only find themselves time

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
