44929a6288931ae69d4155aa86dc6116cfaf7d25
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:54 +0000

    another watch yours difficulty rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
