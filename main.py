005980a93928ed06e8fdf5378d15dee0f7d772ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:39 +0000

    share variety afternoon but side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess
 def run_command(cmd):
         return subprocess.check_output(cmd, shell=True)
-def find_max(numbers):
-        return max(numbers)
+def add_numbers(a, b):
+        return a + b
