ddac1f1d2b3cf48effa225a7eac26b1c73c4d646
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:56:33 +0000

    exact can refer mark talk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_exponential(x):
+        return math.exp(x)
