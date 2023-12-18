20bfaf1c0edf28fce8906870c1e274a18116856e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:53:05 +0000

    lesson authority television train like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
