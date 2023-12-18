c969d81b61d56387fc92d12f62202fadab5dce8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:23 +0000

    course suppose than straight busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
 def calculate_floor(x):
         return math.floor(x)
-  def is_even(n):
-        return n % 2 == 0
