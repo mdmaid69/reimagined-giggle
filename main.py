7de37fd6b226b10d59191428c575440738af5c60
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:57 +0000

    570enjoyable mistake quit buy interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
