de5b819609af530ce9683fbdb71e94099225e1a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:47 +0000

    prepare thankyou settle breathe condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
