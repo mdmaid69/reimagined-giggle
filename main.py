d8712cede29a6333931de076a7961264a1c93e88
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:12:20 +0000

    manage swing busy classroom information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
