15fff629c523fdc5d20d32e598705e878769393d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:41 +0000

    case height check spot talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import platform
+def get_os_info():
+        return platform.uname()
