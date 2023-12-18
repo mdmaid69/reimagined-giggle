33974871cf938c19ed10a6aa026bd43cfd35fb7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:34 +0000

    search offer mail strict successful

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
