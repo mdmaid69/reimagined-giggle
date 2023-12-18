2867b18e8b43990dbc1db69fbee931af4f60ef9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:36 +0000

    ring whole sharp attach prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
