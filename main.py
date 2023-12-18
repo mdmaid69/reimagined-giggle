126ae20685e41b77116c314c1aa27dbc60d5f689
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:35:06 +0000

    grade divide home congratulate preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
