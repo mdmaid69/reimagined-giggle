455526875a424446ea3408b5873a48b24865850b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:47 +0000

    turnaway sick invite various unite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
