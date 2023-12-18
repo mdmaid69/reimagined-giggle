8008fcea89c09678db00c322b002ebaee5e79e2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:53:42 +0000

    getaway write breakthrough goout alone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
