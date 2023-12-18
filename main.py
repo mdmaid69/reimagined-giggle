73db08acd45c25ae111618b04adaee201b170bec
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:34:50 +0000

    flow family model emphasis getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
