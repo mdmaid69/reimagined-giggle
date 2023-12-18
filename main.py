7deabeb6a5c2280dec5c81803f4929d7762b5569
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:30 +0000

    between discuss object ask waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
