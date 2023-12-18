60cb889a684ac2b94c54943e167e2e02357ad167
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:02 +0000

    door tool trick challenge contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
