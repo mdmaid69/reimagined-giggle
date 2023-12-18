8795d74d066fa72facc5b5e1be47e49a65bb420e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:58 +0000

    harm duty Hh minor against

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_exponential(x):
+        return math.exp(x)
