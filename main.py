59cee4c0c06c576e706d34d8a5440a0146f9d2f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:56 +0000

    very please authority relationship Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
