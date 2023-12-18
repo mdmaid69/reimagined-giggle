8c2064f0921b32475005760ecbf234fd1533a309
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:43 +0000

    knock break description to shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
