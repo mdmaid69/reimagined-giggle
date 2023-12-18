e08a96eb9d3602fe10f155f6408abfbee5960c4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:51 +0000

    near compare up out future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def clear_array(array):
+        array *= 0
