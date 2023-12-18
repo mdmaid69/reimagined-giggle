9f2384f4677968ec98065cf33c4489a599e995a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:24 +0000

    neither real appear comfort considering

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
