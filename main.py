565d59c4b8a5ff4b7f6a84468b6ade9fd84ce286
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:25 +0000

    any watch compare direction question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
