3d46ac77516b1a592efee97bd38ca5c0ece405a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:57 +0000

    thinkover there people inside embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
