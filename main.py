acc37eab3a918ee59d96e5280345a5cf8e0549c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:29 +0000

    whisper must roll swimming back

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
