517ffbdb21525425ca4784ca6d176ec0de502211
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:03 +0000

    exist definite scare top put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
