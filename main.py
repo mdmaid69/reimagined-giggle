05690e3f28936192e5460a8611fcef555f40a9b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:58 +0000

    comedown stir since Nn begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
