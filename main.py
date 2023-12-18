fb40d54b340237fdd765de342dffff10733b8465
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:14 +0000

    glass goodbye issue platform task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+  def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
