82da34a9fe0f2f4ba60658950b084b421ba56b34
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:45:40 +0000

    again contact pronounce person display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
