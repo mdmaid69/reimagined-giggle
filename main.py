cf19040ba3df9801382a1873f47f0f290347b497
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:03 +0000

    attempt bar or under hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
