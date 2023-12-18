605451a262b5e9f78ae32fbb157fd961022ff067
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:03 +0000

    hole mood round kind hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import sys
+def exit_program():
+        sys.exit()
+  def remove_duplicates(lst):
+        return list(set(lst))
