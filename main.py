dba7810d2e3cc1d3f715884322f49338b2f65064
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:53:29 +0000

    anywayanyhow cut eat vanish rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+for i in range(5):
+        print(i)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
