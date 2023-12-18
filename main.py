0f8432513224401dc83957faa410d71420dc4777
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:41 +0000

    wrap interesting better out ago

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
