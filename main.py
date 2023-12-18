703d715b5b3efcde3af97e22427e6bd72af48fe2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:49 +0000

    indeed degree force age how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
