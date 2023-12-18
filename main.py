517fef69debab3dffd245a5f329a95367668e373
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:53:02 +0000

    expression mark picture across tap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
