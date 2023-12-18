33d935e8e6cf7b815ba0892083bf2b54027e214a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:02 +0000

    very thirsty effect school luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
