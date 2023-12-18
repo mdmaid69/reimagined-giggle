4cc06baaca7441c415f60de42ae75ed33b837a73
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:16 +0000

    press though score actual cheap

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+print(math.pi)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
