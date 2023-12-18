8aec2e88baa133af74699f81e1a19a3179dae592
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:55 +0000

    whoever frightened never tidy afternoon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def greet(name):
+        print(f"Hello, {name}!")
