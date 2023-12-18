9d085775535b25d9b6d0d122f72030786059cb50
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:21 +0000

    bottom offence prepare likely window

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
