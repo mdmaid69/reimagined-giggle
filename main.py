bca9737b117c07fd0a3e5531181466740869cfb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:04 +0000

    callback rest doubt between challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
