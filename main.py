744522046302117fae47bec2f78146062d13f18b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:28 +0000

    minor course vote smooth separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def cube_number(x):
+        return x**3
