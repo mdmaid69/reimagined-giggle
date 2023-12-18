65a54f77286f17ba065532f8c6634f1357df2af4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:28 +0000

    crime giveback return blue scale

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
