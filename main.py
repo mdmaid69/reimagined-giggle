7c7203b9ed1a11abc0c2b16fa957354dff88599f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:56 +0000

    bus goback(to) union display worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_queue():
-        return collections.deque()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
