f8db8873abfe84c8254c226868a6dae3cb46a31d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:57:14 +0000

    ought bring immediate back number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
