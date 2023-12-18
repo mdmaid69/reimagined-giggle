6062d581d3965c11c72ebfffc465b7ebb47e6eec
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:06 +0000

    advise tell only evidence kill

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
