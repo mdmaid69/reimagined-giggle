5848a70982bb9930efabaeeb67c974ae4290ac26
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:35 +0000

    brown undo flow reasonable manage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def cube_number(x):
+        return x**3
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
