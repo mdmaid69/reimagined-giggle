350ec36ddf95d87d5eac67806fb6247db6694cc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:14:08 +0000

    guide bend passage definite callout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
