9f75ffcf6554bc9a786beb7604b7d8a97e13b37f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:17 +0000

    makeinto shop fight now dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
