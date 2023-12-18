63c2ca438485a105dfe738ac0f284258b6d29ed5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:12 +0000

    better happy scale hand problem

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
