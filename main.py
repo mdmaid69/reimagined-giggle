d3428526905031fb6cb935c22963accace47eb6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:20:25 +0000

    turndown ocean regular unless professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
