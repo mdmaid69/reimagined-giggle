2c61e3cef10e285420b781add6360840e3a40dba
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:26 +0000

    spray wrong takeaway expensive lucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
