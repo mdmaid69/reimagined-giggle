46e37b735316b3d96e5cc1a848ffecbdb3e0d84d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:09 +0000

    exaggerate keepaway what wind pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
