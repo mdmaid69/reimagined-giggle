2305cae681e27b30cce26c105f64545fb5ef8e72
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:58 +0000

    comefrom 566end own reliable copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
