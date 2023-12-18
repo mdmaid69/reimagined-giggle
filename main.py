0654fd665258376cd8e73028e7473d010c1e7ebd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:00 +0000

    towards tip both maintain early

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
