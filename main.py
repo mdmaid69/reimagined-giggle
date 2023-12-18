0039ac4a6781289307668faef8048ca5dc54d489
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:00:01 +0000

    bye wake formal discussion listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
