2d732b666b1db1e0f82640bc5edf03b297fa2d4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:38 +0000

    improve feeling basic trick watch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
