29bbd3f858a90b306a21d4ab7233ef7f43505d18
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:51 +0000

    inside crowd dry apologizeapologise early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
