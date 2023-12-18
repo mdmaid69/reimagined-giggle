a81ecc529d7f0c03991d8d225c2b1fc6b9793ebe
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:58 +0000

    display familiar breakdown aim forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
