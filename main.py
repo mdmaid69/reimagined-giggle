348e6ecc1357f8e230dd7dcbdf00d533e7a8c574
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:47 +0000

    impression guess offer reason smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
