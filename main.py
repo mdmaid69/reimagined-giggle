fedcc77213ca058f76458ad9e288be9e8e4c398f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:18 +0000

    respect uncertain report rich theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
