ebba51781440a57e68ace698fc1998b9b9265ec4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:58 +0000

    be left far order warm

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
