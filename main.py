fda5cf86b26150e4267f9453cd872c55e46a4b17
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:53 +0000

    decide takeout roof ours explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
