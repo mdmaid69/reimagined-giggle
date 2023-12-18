b474e07fe3122fca592c5c5c79e33a342337a7f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:50 +0000

    guide actually worth sign pool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
