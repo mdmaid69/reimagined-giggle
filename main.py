902986e611a66661d26a90b0843ac1a6be1b7119
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:02 +0000

    fasten tough takeback road truth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
