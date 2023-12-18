feaa5be0bb0b3a735c89095ff41cdee363dee0d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:21 +0000

    able home 565end dangerous divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def count_elements(lst):
-        return len(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
