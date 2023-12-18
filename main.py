5f1a0659076b4f646dc68317ce47b6831fcd2ef9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:17 +0000

    confidence declaration goon yesterday trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
