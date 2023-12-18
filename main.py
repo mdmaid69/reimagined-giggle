8e6d3e158bfefea30a7ae2ec0375929212c94a24
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:41 +0000

    rush Uu secret because air

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
