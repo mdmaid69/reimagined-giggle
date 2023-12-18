1aecaef526fb1f9efbaf76c650b17474aa7e7c6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:51 +0000

    survive support sky poison remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
