71493d3b1b634068f45ee9113f27c95da303ec4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:26 +0000

    name little combination scared skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
