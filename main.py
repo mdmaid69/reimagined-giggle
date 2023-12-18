b046f9370da68bbc3d2e6b7ac8317f4100e5fd10
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:53 +0000

    season circle onto disappear strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
