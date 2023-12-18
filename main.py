b145b20e89c6acd22c56ea6539f606d901df2bd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:59 +0000

    block moon bringdown concentrate bringdown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
