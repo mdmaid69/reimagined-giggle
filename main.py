19b10f9f829aca2c0878b251864d91b4ab3d87c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:58 +0000

    within climb comment congratulations means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
