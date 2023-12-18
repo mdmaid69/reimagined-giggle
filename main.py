75c8e2401ac8276f8de685cc3f5a81786ea7ad17
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:14 +0000

    improve whistle target reduction intend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
