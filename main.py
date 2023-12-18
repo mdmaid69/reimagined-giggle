dfc0702dbfe6972720bf17c36d7eaa2dfbb773af
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:41 +0000

    heat before base card mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def reverse_string(s):
+        return s[::-1]
