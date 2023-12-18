5859e54b6a90654ebe947896e699e709a30d9e1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:10 +0000

    wind on rubbish they judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
