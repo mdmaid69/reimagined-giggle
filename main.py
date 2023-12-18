67b542a25624626dd71403c921a930d9d5f41453
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:28:17 +0000

    special fortunately smooth downstairs reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import time
+print(time.time())
