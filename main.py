f7ba5b1f68381bd0ae5e0161ccc79bf231e408a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:09 +0000

    sound bear region load regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
