422292aa1d75e6d81ec956b4c5d551dcf27cc4a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:01 +0000

    row lean continue gradually noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
