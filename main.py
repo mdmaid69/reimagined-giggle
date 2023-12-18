e08c18fdfdb1c4b19d412f235a9c21c8e0a16c5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:29 +0000

    dream miss hard soak form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
