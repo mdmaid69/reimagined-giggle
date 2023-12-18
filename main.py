30ed8e64dac70705682240adbad25c6d61563bd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:11:36 +0000

    grow deliberate form see offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
