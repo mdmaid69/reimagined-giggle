9deb2a7a5f134888b0fab5f16060fded8254565d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:12 +0000

    traffic increase he unpleasant dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
