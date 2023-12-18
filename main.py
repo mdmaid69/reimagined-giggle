cedf4f4d2f8fd86e846688328bc9f928a74ecee6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:50 +0000

    choice free rent exact colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
