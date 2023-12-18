5ad90394c8dae62eb8891e241576a01cf90340b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:42 +0000

    legal direction gate disappointed who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
