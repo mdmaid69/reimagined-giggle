396b3c8dc58e71d0be4e1e530e08edda13f64f5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:49:25 +0000

    apart sink embarrassing collect hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
