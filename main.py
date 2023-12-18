ea44ed97120e92be90d1a54c9888ebff4d307cab
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:27:09 +0000

    calm learn they perfect once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
