259fb05ebceaf4b391fa863a14ba96f9416215c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:30 +0000

    consist cost appear 573entertainment reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
