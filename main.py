9565cb33f94e259eff0f62483982ce4f3f0296bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:13 +0000

    guess back trick progress please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
