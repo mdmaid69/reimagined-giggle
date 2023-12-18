6076dc1f0cc542ff9bab00dbcebe59c4d9cb72f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:14 +0000

    declaration flat force dish rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
