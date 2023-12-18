03a94768c0e04728f1fddd0dea3b503ab35ee3a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:49 +0000

    top offer sing this butter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
