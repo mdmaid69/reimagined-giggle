1bec396482253b2f0bb012468998755d972203cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:40 +0000

    pretty rob shall creature considering

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
