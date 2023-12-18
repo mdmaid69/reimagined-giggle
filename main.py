97018eeeb60324f129f0539416b0eb11138074db
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:39 +0000

    advanced serve basic sure uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return math.factorial(n)
