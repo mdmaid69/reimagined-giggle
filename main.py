f322db5ca938adc08778d772dd107effc62b1e98
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:49 +0000

    offer callat Pp shout trap

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
