d308ac4eaaeaeb09ccc263bf45dc474babbcf789
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:44:27 +0000

    sight Cc turn mine to

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-print(sum(range(10)))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_density(mass, volume):
+        return mass / volume
