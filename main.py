5174480913ffce81800497441ba78aa77a255437
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:56:24 +0000

    demonstrate Pp character play skirt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_density(mass, volume):
-        return mass / volume
+def sort_numbers(numbers):
+        return sorted(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
