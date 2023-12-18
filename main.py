65dd920027706d1c8bdda62808d503666c7cab88
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:08 +0000

    brush sit guess print emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
