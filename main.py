5746b6c0ffc8ec621ebd7e95634c81318c564d17
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:31:56 +0000

    gentle relief protest spare once

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def print_python_version():
+        print(sys.version)
