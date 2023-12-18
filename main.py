c998b31580edac6ad42cc26f50bea3e06a7b525a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:59 +0000

    bringup get blame freedom whole

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
