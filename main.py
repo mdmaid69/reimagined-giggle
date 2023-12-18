57adc64ceee8c1a4817202f42f443622537f884d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:07:25 +0000

    us handle complete before advice

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
