67aeff7f626f6f86b423e824b51504bc94efbc1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:00:22 +0000

    health unfortunate school fire up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
