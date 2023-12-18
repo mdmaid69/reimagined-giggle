3260e1b8a1d09050fc295f58873486d5be501b1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:21 +0000

    trade Oo thing accidental runout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_list_from_array(array):
+        return array.tolist()
