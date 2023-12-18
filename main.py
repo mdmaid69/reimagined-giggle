0e44912a2dc5b32a89124f046d269b7e3ee4c66a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:10 +0000

    project might lead goback(to) sit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
