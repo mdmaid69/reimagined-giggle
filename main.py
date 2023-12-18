db710a1efd868f040af35a464545fe60769f04f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:25 +0000

    seem nerve shop female doubt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
