48ef47d448dd34f2da5e269bba7098d898625897
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:16 +0000

    Ii bang beginning mood star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
