b78d29b6ad6f842b12d192d972af1bac863518ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:58 +0000

    reliable recommend cover opposite situation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
