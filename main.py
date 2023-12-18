90404e76be972d4ecb119bf6cd983e02b82dc976
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:17 +0000

    about thumb announce boring pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
