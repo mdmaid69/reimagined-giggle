315dd5aa854f5004c3728e27e1536619fafad42c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:18 +0000

    pride off mystery yourself object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
