0e60fb98ccb32ef419592c4c9318d9941f1d3273
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:44:32 +0000

    actually dull brother believe discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import re
-print(re.match("h.*o", "hello world"))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
