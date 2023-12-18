74744d786303874ec632c15b27915a6a9e5f7571
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:21 +0000

    flavour shut smile talkoutof replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
