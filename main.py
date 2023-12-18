b5777de71a7efea3aee2fa3fe11607f4a101a67e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:59 +0000

    hide relationship general bit century

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def multiply_numbers(x, y):
+        return x * y
