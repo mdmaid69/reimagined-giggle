a1ffd6a18e707a3dfbb5ffc6bfb83159f0451114
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:05 +0000

    giveout against fall high report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_item(array, i):
-        return array[i]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
