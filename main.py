be3bea7cbe04385881135c8b8cad4eedcc87e041
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:51 +0000

    to lie there swimming be

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
