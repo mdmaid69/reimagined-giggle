fc8d87413fef3c344f1f0d7e827cfdd7e8e03a11
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:30 +0000

    accidental before considering mad spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
