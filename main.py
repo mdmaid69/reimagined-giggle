be0269f37584965deb712073c378bddeab873eb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:36 +0000

    burst himself degree name government

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-from collections import Counter
-print(Counter("hello world"))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
