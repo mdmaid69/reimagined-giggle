dbc6365e1ac76a94e6c4aee08bed2accebaabfa5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:35:42 +0000

    further regular curious emotion undo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_os_info():
+        return platform.uname()
