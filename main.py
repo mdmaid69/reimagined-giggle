5c30ad155536f0d07e983cf36955108683b81be0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:01 +0000

    sit sink scheme stone ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
