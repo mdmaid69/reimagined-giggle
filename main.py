bce06af6abda0d226ac85db954044383325b0203
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:30:18 +0000

    badly instrument glass necessary increase

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def is_even(n):
-        return n % 2 == 0
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
