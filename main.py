ac92fac471f11e47de1cbad85613477a875dbfab
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:57 +0000

    well door give congratulate meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
