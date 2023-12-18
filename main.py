2bffacb80480c2852c78e9b394a4871e1232f532
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:37 +0000

    clever noone lesson entry sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
