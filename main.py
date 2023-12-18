d38ad5a5e30e498dca69f90665fe77c642546b02
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:19 +0000

    discover slip steam takeout yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
