28ec93db8ff47bbec75b45f89f8cd6c19c2c5695
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:15 +0000

    relation short experience exist emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
