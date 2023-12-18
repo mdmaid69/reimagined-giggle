f3b509a67c350d25584133f662f92bdb6361208e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:39 +0000

    prepare niggaacademy match chief effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
