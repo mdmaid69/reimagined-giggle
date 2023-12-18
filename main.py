7e7ca9b65d74f1bdfe8e83ca5268f733c05bdc7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:33 +0000

    dress hot slide rain flight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+print("Hello, world!")
