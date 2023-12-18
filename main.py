fe113b4e3e5aac480a92b879d7320059eb456fcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:13 +0000

    keepoff down show check Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
