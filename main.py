ec4bfbd7f681f518a1730caf1b6da84801116730
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:18:36 +0000

    extremely principle sort fear niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import time
+def get_current_time():
+        return time.time()
