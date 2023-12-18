d5caf93a9d49a7de39fd9e0643dcd1dbcf510759
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:31 +0000

    holdup talk content letter destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
