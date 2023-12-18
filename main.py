e5e5ef08dc1535ef573e54dcc8b474acada223cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:05 +0000

    hold judge recognize seem choice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
