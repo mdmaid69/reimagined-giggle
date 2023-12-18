550e3632d5a9ea7526753b0f478ca2e7634fa1f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:38:27 +0000

    sign anywayanyhow quantity kill my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
