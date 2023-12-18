ecb98dc97b4393038b9cbb54ca87aaa440819479
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:07:30 +0000

    and real lose team all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def count_elements(lst):
+        return len(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
