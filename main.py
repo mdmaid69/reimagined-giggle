5f2da99e5db77aa5c3dcc65d599fcb5f92ee3b9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:45 +0000

    shut confusing proper promise hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_factorial(n):
+        return math.factorial(n)
