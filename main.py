4a5da1f00a94d85defd692db9bf79cf1e53e0874
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:34:42 +0000

    easy poison our star next

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
