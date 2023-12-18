a6cdb0de1952a2dc4e096b6ada9d1cf04d1fa63a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:05:21 +0000

    material more minimum communicate produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
