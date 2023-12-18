a6235df17129bd0014e7288b978e4609236b6bb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:18 +0000

    sting disagree money spring afford

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
