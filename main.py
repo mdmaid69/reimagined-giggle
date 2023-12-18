b12df383e5b8a0b6f8e694373aac5b8e7873f3af
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:19 +0000

    scrap future rescue say disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
