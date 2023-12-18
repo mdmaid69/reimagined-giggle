5b96db35f3a427074e683d6c6aa54471eb87f92f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:03 +0000

    schedule justice fill no century

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
