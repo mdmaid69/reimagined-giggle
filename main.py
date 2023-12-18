8b6972f37ce55cf7d55385395ee1322de141c520
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:55 +0000

    reply strange secret culture usually

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+from collections import Counter
+print(Counter("hello world"))
