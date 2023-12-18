5b2b16d779730ed2e1ecc88987f9c515084f390c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:35 +0000

    well him terrible somehow sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
