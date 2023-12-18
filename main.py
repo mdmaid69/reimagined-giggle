2365990b7e1a0f6deee7480054f8137922794858
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:46 +0000

    respect study drawing thank bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
