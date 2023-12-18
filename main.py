f07ef084e2ea6eb56f19e79310f62e435eab2712
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:39 +0000

    computer drop common influence correct

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
