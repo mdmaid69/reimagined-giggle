32d03dfa4d198c37bb6855c159bc1c3eb8a94f90
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:54 +0000

    importance recover water convinced pencil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+print(sum(range(10)))
