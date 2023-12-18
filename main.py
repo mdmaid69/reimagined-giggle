3678fe32f21a54682c75a6c812aeb24c0478aaca
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:40 +0000

    Ff thinkof swallow hard beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_binary(n):
+def convert_to_binary(n):
         return bin(n)
+import array
+def get_array_as_set(array):
+        return set(array)
