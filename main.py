d350aed7b4c629b00dafc8c4cf8b506c963fe944
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:21 +0000

    command ought mess squash lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
