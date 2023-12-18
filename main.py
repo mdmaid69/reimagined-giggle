7d2ecd28c0bcb5094937dfe82ecb4b5cb7b56562
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:39 +0000

    size appeal dig 571enough wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
