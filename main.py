62d52adde2b8c054e466e1d7f13cf1f76abbdce3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:55 +0000

    during consist well eat loud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
