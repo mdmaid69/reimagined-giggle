65a3bb3befa1fc53f89f51c98321189efb327e8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:23 +0000

    deliver identify abroad sing suit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
