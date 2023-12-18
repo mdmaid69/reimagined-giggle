c41722ce039fb7f3487a63de334600887891e885
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:51 +0000

    stone ball poor rather sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
