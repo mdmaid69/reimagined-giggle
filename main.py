dcaa92f92768871278f71bbff9e84e7e56bf15fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:57:29 +0000

    typical please easy oil joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
