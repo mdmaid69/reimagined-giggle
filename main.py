f00bdca50e55ce864e0198bdd5bc31c67d72ff7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:33 +0000

    argument any none control achievement

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def get_array_as_bytearray(array):
+        return bytearray(array)
+x = 10
+y = 20
+print("Sum:", x + y)
