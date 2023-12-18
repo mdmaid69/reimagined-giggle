36a39d6e86aaa51a50d7eb8c42e1bc8874d6f83f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:00 +0000

    goon replace bill exit thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
