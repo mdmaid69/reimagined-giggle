31986406132e7882eac076f9e461d621bd752c30
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:51 +0000

    ready sad think fight mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
