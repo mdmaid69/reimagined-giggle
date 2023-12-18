a2463d00609e6caa915bd9a6fa801b3e6f32a00f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:23 +0000

    raise week pay try close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import time
-def get_time_since_epoch():
-        return time.time()
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
