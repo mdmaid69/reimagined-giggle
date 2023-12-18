6e5e6cce57f7843fdfe70cf40f11de3d1c8e589e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:19 +0000

    maximum all occasion case name

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+print(sum(range(10)))
