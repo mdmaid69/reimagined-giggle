b3c3bec3f167b985d5fc6d47650c622dff2a3726
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:48 +0000

    step juice sorry desperate scare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def reverse_list(lst):
+        return lst[::-1]
