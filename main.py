1e1fd60833d3c556413441672a31ee40c7912677
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:09:10 +0000

    interested badly pile learn moon

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area_circle(r):
+        return 3.14 * r**2
