6e5128a404725e111d48f4fb8126ed57d087a732
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:43:00 +0000

    remark description sense violent moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-print(os.getcwd())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+print(sum(range(10)))
