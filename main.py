ff7d4f52f5567f6fd75d7a9a793825bbd2567ce1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:37:23 +0000

    rather taste joke since us

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+for i in range(10): print(i)
