2b779d32fecc7ca9faba8ef3bac411263fcc4d36
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:04 +0000

    intend determined band past slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
