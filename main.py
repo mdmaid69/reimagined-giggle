581828d9dab23e40b7acf92c6a04f128e5836bdd
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:27 +0000

    would furniture strict whoever shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
