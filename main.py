a1107586480312bf0798b9d3654b8bff0c471394
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:17 +0000

    science pain because cruelty violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
