77a1f515e3ad1f097104e6030fcb3ba4521a57db
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:42 +0000

    bright smile blind prepare none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
