555ce4518b62b5118a5ed2c8960eb2a08be98acf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:29 +0000

    after disagree experiment ring alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
