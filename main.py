a666aea5411e7cc42f00003745ff4a2b6cb919ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:14:52 +0000

    Mm loss my become suffer

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
