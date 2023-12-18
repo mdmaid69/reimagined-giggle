5a37b847a4c18ec10b060fc981d4802bec66941b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:48 +0000

    card movement negative 564encourage certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
