f8b8de111fc9ac673a581631433c65f425030583
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:50:28 +0000

    tie cinema over copy means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
