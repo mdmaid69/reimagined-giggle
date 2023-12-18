93d17244ebd3b07abb19fbd7481b598da85f4130
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:56 +0000

    Dd discover ordinary rather real

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def count_elements(lst):
+        return len(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
