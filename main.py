065ca890764cd1c52456a9781ca968852de1fe77
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:26:59 +0000

    perhaps suffer nonsense callout worried

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def convert_to_octal(n):
+        return oct(n)
