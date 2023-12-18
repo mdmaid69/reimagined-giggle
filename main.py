2a04e8d140713d6a810f145a02c50ee12377f1a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:56 +0000

    closely thank pollution wealth Vv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
