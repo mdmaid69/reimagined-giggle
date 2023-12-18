c6e48d1d68c39970d80c168a7073d1bc8a690fba
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:06 +0000

    talk survive 564encourage to sleep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
