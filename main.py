92b27d077e718d0ec98786ccbeeddf8882899ca1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:15 +0000

    juice decide activity arrange once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
