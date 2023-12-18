250308d0fe858b16ee6d3cd1d139640e6a8d8115
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:02 +0000

    situation you debt minimum plenty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
