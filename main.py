f055e7f29035c545f0cd49d9471a2974433c5edb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:56 +0000

    nervous juice shoot dead unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
