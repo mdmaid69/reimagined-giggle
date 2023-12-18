58ed729bcd7c5cb1d0ef60d3ead0c66e8124ee57
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:47:41 +0000

    slow several announce move second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
