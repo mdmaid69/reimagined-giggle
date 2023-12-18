e4eb176a97069fd08345ed2409e6edc7a708b7d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:25:05 +0000

    book old generous depend command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
