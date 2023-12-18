b0c09ec1694a9379d5ac92f0316c72a66d5c242d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:11:21 +0000

    confused indeed serious active boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
