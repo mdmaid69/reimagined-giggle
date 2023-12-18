fb6782bc820a1e8e904287d58551063c4c636f55
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:42 +0000

    quickly permission different word tone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import numpy as np
+print(np.array([1, 2, 3]))
