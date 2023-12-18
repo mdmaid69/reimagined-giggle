d801f79e71f1bb4b729c131bf254b3f8001c374f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:41 +0000

    range lonely catch left wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
