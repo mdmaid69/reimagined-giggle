ed734f143268bb05fb987bd10165e14d83dc1b89
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:08 +0000

    until cut afraid means shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-name = "Python"
-print("Hello,", name)
+for i in range(10): print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
