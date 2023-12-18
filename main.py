840171d2305ba9a30c401db22efc0c22d61561ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:36:12 +0000

    all disappear nice hold limit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_speed(distance, time):
+        return distance / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
