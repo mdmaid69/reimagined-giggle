5f00605b62e8aed9cb1fe0d758a936f54f330fef
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:11 +0000

    worse exploration come beneath deliberate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def change_working_directory(path):
+        os.chdir(path)
