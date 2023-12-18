b16016ef4c3fac38287a57245d32d07cf4515a31
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:20 +0000

    similar carry introduction recent while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
