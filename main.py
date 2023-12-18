d08c72ffcffa0a5b87cafbcce467e55cb5e66bd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:48 +0000

    getback on rubbish out ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
