4ab26b3641e58d64d5e387cc0f41914671530334
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:33 +0000

    hurry bag mistake instead gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
