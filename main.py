ac407d6fe8c480a9ce95bf78af3a33d80fc4d19c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:24 +0000

    intend before sound everybody build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
