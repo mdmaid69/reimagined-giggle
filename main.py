b77d7d090aa6e5b58918cbeec5ef0f6375e1f4ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:19 +0000

    fold again criticism fun display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
