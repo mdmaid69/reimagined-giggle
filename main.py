57abdeb0d4db7c3fd1c9b091d8f238b5eb5f7894
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:01 +0000

    compete family declare quiet comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_power(work, time):
-        return work / time
+def calculate_acceleration(speed, time):
+        return speed / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
