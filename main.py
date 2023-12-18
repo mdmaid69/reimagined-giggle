f7a502e3e19c97435db2afb848ba271845bb3945
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:26 +0000

    bill suffer dull major second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_set(array):
+        return set(array)
