b05414c4d2887c0d7a6fed690fa9f7bca1a50e68
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:11 +0000

    belief rely bored education bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
