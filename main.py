340cbcd81ac2204b10cb897d97013fcf36004a8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:47 +0000

    declaration forget rubbish runaway become

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
