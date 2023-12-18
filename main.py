ad8f9da10dd2df8c276bb5e44d5b1c5441beb395
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:27 +0000

    conduct listen turnon beside congratulations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
