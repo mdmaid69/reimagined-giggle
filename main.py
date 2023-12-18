f880e13d70f6a3f1bb3bae4735f8d64dfc89dff4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:34 +0000

    wish combine watch private one

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
