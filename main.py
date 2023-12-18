1146cbecd308e54105270a122717ba38fe4c9a43
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:47 +0000

    flow less minimum rise considering

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
