9ba050332837b0f7e517625340c453809b77683e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:11 +0000

    inside do declare last drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
