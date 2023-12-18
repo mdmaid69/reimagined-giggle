cd46a789c4ab75458c0e09d0b9cf3f497f66363b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:15 +0000

    object truth fault will intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
