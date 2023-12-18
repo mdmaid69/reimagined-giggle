db547498b00f22be16fc0fbae6fb688ae906bee4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:16:58 +0000

    know certain visit callout frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import re
+print(re.match("h.*o", "hello world"))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
