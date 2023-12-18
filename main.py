05ea09a499a5024101ca1309aa8292a475cafb85
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:56 +0000

    slow out pass forward purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
