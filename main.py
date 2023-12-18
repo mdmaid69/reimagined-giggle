d68c040eac40c27a5e1c51049d1f46ddad70072e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:09 +0000

    spot valuable exercise earn bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
