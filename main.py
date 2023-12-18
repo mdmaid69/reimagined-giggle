ec396ca488850c9552fb0a360173151fd0ae3bf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:37:01 +0000

    general major suck giveout Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def subtract_numbers(x, y):
+        return x - y
