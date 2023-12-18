8fa3fecfd25b1b609d2c86eeef292d4207570468
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:19 +0000

    anyone sweet getback press failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_tuple(array):
+        return tuple(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
