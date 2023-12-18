11e27f8eb61164ae37c68c7fcaf72315188a04b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:11 +0000

    argue creature smooth admit sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def square_number(x):
+        return x**2
