e769bd909bb05ccfaa91adbde1688c8f820c7f1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:32 +0000

    apart hate machine relief 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
