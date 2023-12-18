3f321b119e0b6294a0cdf107a2228d7eea09f1a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:28 +0000

    butter damage mess slowly light

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
