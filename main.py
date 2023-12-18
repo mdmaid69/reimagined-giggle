19ade2daad08401d1862772717f48da5180a6771
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:37 +0000

    one letter section workout hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
