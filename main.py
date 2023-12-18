920381b78a118d9cadb38565503287a349ebee35
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:00 +0000

    weekend backwards hour bunch give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
