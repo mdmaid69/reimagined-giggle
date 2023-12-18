18041ce21d35a3d703a4190d2b5e1604a6258ce1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:25 +0000

    instead low successful pleasure scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
