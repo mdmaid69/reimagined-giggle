2ea324f4cf83fb3662c5c2eb68f42b2a9009d9c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:56 +0000

    government tax suffer light manage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
