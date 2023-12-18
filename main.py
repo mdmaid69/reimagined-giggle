4dbc6819cc8592bff95c539bf6395c2871b166ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:53 +0000

    list deep even theirs union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
