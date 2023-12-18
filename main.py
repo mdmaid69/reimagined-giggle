f630020b2b0ba0aa6e708f0f433347b32d0ef90f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:06 +0000

    drive hungry extremely previous commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
