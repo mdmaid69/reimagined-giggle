ff94a51064bb38bb21bcac091198bb66c1c558d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:03 +0000

    goout stupid cope catch cook

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print(sum(range(10)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def subtract_numbers(x, y):
+        return x - y
