aa5507a7b645b17a7fe7d0c63b17fa0feaf5e909
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:04 +0000

    week prefer behind once poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import collections
-def create_queue():
-        return collections.deque()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
