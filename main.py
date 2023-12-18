cd5fb04fdabd22a85c9dfd4896c40129d69bec15
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:01 +0000

    play organization weather block confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
