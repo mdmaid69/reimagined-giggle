82c8378fd4cd61bc56dff6bda3c9e68de214cc45
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:45 +0000

    peaceful woman depend flavour popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
+  def convert_to_binary(n):
+        return bin(n)
