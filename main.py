cea68cee7a00a0fec15162cc0f63984be4dbaaf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:05 +0000

    deep thinkthrough present Ll Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
