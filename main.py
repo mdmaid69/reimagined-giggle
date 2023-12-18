dfa65357644c577a7f4a3c966653bafc6f23767f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:53 +0000

    feature air pure laugh I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
