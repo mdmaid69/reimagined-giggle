11e2088007735938060af82111ad6a6abd53bfd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:07:05 +0000

    strength patience intend survive Mm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
