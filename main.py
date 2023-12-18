9d251fb5c3273dd6172b211127a817d9aff11467
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:56 +0000

    disappear payment break fetch drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(os.getcwd())
+def sort_numbers(numbers):
+        return sorted(numbers)
