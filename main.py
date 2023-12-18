7ac6b5929cba1ec31d3b26a61a76d331bed5f7d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:34 +0000

    the weak trust achieve memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
