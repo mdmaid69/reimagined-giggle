0e04679d4a22852b028e5ffdf9f80d1df632f9ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:50 +0000

    butter fierce wait detail offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_to_octal(n):
+        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
