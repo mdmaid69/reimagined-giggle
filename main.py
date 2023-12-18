cd447807e983d94ff22479c4ad21077f1eb82121
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:43 +0000

    sure should stamp put frighten

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
