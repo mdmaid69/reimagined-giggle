27d4c64d8b69039a6621a501565b1aefc7144db8
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:31 +0000

    storm soak wise smell shape

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
