78b80e0dae90e9c2846e53c0d3df3ab15231b3c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:52 +0000

    press manage by post desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
