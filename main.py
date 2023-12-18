28f0d0fbdbce6516ddf9e4bc9b9f6a5ac217556b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:58 +0000

    relative enthusiastic related blind lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
