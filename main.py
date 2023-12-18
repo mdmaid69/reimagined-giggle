9c5f86343e63fcb152f9659692ba6feec3249a0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:27:35 +0000

    weekend Yy edge few whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
