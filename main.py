0bff5cd690196300515b2ccce9139a1f76ec2771
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:48 +0000

    help part that excuse dance

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
