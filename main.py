33509086aeb0c026055bef2e61d819c1cbcb88b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:07:35 +0000

    flight careless pupil lucky backwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
