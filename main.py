25d494e74afa33cd0c1672bd23536b6bb7cfe24f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:47 +0000

    the sport that cost centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
