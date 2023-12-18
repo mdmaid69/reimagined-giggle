555d125c504494befe245318aeb61ade9c5b985c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:54 +0000

    why form shopping who cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
