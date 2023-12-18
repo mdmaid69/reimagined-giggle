6c090bfd8c1f251d092b90e5b1b1611516497aaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:50 +0000

    brave contents comment improve test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
