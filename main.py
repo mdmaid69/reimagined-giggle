7d1a30b9e6c0d50b5509330d41f4a324e79055b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:00:24 +0000

    wave steep holdout whom beat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
