f2dd47c184f9cfec80f54509b93b4933a70164e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:49 +0000

    distance hurt shout chase luck

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(x, y):
+        return x + y
