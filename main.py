82c2b4e061c108e16632c70d3a4751911f0ecb29
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:20 +0000

    further crash prize 571enough precise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
