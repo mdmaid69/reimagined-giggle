0dee86c8d18d0e5122fb50aa8e4f0ee17dbaa473
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:47:16 +0000

    letter flat unhappy never lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
