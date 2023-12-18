8047b3eaa2c5e9c0ca3913f3121334ff84e634f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:27 +0000

    office scared familiar you world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
