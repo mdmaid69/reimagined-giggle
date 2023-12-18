b221232d4e9bf444c6d65a97700b951a91583001
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:46 +0000

    crowd mention interfere comeup attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
