a1dc5b7113f172da67415e48e79e501924b79781
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:40 +0000

    confident good high force present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
