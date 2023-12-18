11f77f43005026349ac86626ef49057dcbd2c332
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:19 +0000

    plastic exist release stone build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
