c8eb1941f919a47b62cf87dc85b48e34d1deab81
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:44 +0000

    combination claim discovery good clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
