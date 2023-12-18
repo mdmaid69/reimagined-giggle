a8fde518da0a4c8f33810abe58ac51f87faf2579
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:52 +0000

    yet pleased mix bang point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
