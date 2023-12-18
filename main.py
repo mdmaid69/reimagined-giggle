73673cba6072ecbc866fb475fcb275c40c014e84
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:01 +0000

    borrow should dare excuse workon

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
