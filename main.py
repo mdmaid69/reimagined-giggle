4a3f89ec9c03155c58c216f79c1a708e84b46e43
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:43 +0000

    minority art dish train ability

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
