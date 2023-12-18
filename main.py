427fdbab1a191e5c148e61d06c60f2589754e0c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:53 +0000

    report occur mass breakout half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
