06dcd0176e75822e0b46240296648c2cdb0c00a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:04 +0000

    plastic opportunity divide complete pride

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_unique_words(sentence):
-        return set(sentence.split())
+{body}")
