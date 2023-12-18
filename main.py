4ebaf1c7a7bd7190d4a8854f4d7569928af3cdf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:34 +0000

    forgive for pay successful programme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
