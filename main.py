c8f9e7a0ad742114760ac7bea556372f0749b0a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:16 +0000

    practice sensible far possession so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-for i in range(10): print(i)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
