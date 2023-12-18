6357f66721f9e8af667112c899644137cef7fb7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:34 +0000

    dry chase book pause fine

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
