2a233f467446268f4bd53546c9282cce8f919928
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:14 +0000

    lend height pour workupto grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def convert_to_octal(n):
-        return oct(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
