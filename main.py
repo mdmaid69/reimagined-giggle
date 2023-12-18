ffa39796f87a59ceafcd94d1e07eee849d9ae128
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:36 +0000

    official arm busy patient little

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_power(work, time):
-        return work / time
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_work(force, distance):
+        return force * distance
