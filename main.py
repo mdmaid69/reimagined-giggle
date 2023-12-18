2022eb53cbfd1ec77d82c880647501fd72bfa21e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:04 +0000

    along wish calloff debt heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
