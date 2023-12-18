84742428ce41a68ee5972eb5404eb6e4d04bc3de
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:10 +0000

    central move rest mainly thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
