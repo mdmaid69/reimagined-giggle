a0084c105865dbe96362be1d02bfb68e63da0ec7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:42 +0000

    criticism improvement level genuine sit

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
+for i in range(5):
+        print(i)
+import socket
 
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
