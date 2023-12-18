b2fd35080d1af14365704f2aef3f8a3b26361784
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:52 +0000

    effect Nn search sore fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
