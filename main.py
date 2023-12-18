6beae16144dc2fb1aec62e81e8e07ba97ddd7089
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:24 +0000

    middle set straight strike cheque

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
