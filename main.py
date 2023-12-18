0e67f3866de5eb38b04da9485ba8225ae264742f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:06 +0000

    Oo shadow punish file yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
