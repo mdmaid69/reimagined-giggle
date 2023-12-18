831c1f30171fbeecf627d28645945692defce9ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:40:37 +0000

    tip young shout back sight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def multiply_numbers(x, y):
+        return x * y
