fe3de62fb70177c4fbf7e87b55f67660347a6d91
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:48 +0000

    when sister cost name edge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area_circle(r):
+        return 3.14 * r**2
