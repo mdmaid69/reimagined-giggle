0308af30c7a0b9098b37b67a6677c1e97d7b49b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:19 +0000

    comeup job sometimes Ss thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
