627708b64fb361c99537fcd03d9505c5442bc03c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:57 +0000

    inquire terrible large fetch uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
