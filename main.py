375ccd623f6bb2a00c95bb9f41d83c96f8c79622
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:57 +0000

    season unable foot star treat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def find_max(lst):
-        return max(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
