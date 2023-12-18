828781161a593197c21e196ad2b7de406b275b93
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:54:55 +0000

    often powerful every basis that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def square_number(x):
-        return x**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
