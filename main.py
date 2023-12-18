b2a64827bad1a88b0f6ee7702f72d91cfa18e3f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:56 +0000

    main nigga coffee remove join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
