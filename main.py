3063b64ad7fb6982202d0cf032d945b24fab0fd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:36 +0000

    large meanwhile communicate satisfied clue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
