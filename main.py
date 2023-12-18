a90d2ed427d3b6b6cc69067dd636d04d4581e976
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:29 +0000

    story show sound bath brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
