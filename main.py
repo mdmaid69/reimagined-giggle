318de73fbe85d57a05f4b3f56297ad46bc491247
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:20 +0000

    force normal party pain issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
