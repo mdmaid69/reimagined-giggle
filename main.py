95c6688cdcb7f9c0b7765a2953ebdb43b0de32f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:50 +0000

    university distant cum whom plan

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
