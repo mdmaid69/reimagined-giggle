abd2f10fa87418475f33bd6f6d4b186ee8b0f4a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:32 +0000

    limit maintain brief confusing forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
