d8be85251b06da57c30f32c99f32ec4b12390959
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:12 +0000

    shopping translate amount store marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
