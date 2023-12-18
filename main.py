3e0303f0da392f81e561f85ce613e0f58b7eb238
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:44 +0000

    recently part even report cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
