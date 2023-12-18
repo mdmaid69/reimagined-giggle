6cf5a248478ce62b26c9737b4a5ac1a33b712359
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:29 +0000

    recovery never absolutely the trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
