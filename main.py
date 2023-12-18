38573c3cb65e79713c7dfcb949726080cec84b9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:40 +0000

    edge upwards trick practise personal

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
