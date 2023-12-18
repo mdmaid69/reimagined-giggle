dc46617d35d163c21ec6c928a65cbda8a56fbd5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:51 +0000

    tend forget sometimes read responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
