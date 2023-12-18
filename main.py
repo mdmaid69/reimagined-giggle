457980595b5ac8e7fcb312aae5bbfb7869971ae8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:45 +0000

    limit sensitive check apologizeapologise judge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def square_number(x):
+        return x**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
