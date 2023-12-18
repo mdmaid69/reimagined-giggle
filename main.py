6771472c6bbd1f3925c44776e7e39b9f08324021
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:19 +0000

    together in wing laugh poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
