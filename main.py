e06e82ed9b0a0b17f708bca49fb13d9f62d708c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:26 +0000

    succeed place popular roll could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
