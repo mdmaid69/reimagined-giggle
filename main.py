f29dd3f73dc5878406fb664ef73a3ca2ea58b8d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:40 +0000

    obvious file calloff regard generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
