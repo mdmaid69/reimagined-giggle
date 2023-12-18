23829ad97eb202495dff69da7b86c2de1357cb22
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:00 +0000

    ground admit often natural scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
