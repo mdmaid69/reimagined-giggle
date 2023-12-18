e19304c7a5069cfac1b1bfcf0c588609c21e909e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:46 +0000

    thumb quick sorry work suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
