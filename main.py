894c4f34a1d5018b9e33bf6084424c3b56912236
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:27 +0000

    bright match count sign bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
