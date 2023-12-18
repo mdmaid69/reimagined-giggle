93696ead3f035bca91a30421f759214d166ae9d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:24 +0000

    contribute fit matter thanks in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
