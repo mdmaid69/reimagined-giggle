fca066ec09253d95042bb0517b6ca198151ef72f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:17 +0000

    shame quiet size turnover I

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
