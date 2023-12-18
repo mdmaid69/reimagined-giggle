1144a8c4de0171cd0ee773f0c9de6ccae8af27f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:52:07 +0000

    spite great charge undo now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
