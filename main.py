410541c6db84eb77800317123154a65a6f98b300
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:08:46 +0000

    length exercise delight Mm sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
