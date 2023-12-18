6001309097eb474db74f7090ede5675ffd93d6fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:33 +0000

    precise tooth fortunately mess spin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
