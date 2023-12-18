5657e1c1b6187df50eab4f2f20a0b082b970007b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:53 +0000

    hurt although system exactly accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
