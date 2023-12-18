7fa58733f0451502c45b25e98ef10b3e829d93d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:07 +0000

    country worth somebody exchange idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def greet(name):
-        print(f"Hello, {name}!")
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
