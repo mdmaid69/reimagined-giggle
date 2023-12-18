b58427acd02425cae880b4972078e9069c8b7089
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:12 +0000

    modern forward good completely contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
