b3f89b7c276cc5bd7b0b7b1b5fb8cb7728474b73
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:58 +0000

    measure question vision feature decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
