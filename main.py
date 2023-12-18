68f1dab83d5004a8872b390b28b498f3e3d5b591
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:10 +0000

    keepup list search risk danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import time
+print(time.time())
