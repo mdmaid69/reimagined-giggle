19cdb23af105b0f33141fd0f4fa3503f3dbc9c0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:49 +0000

    charge never leave some practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
