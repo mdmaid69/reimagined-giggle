5bd23e840ffe55a5bf5bf02a3064a34d042fac0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:02 +0000

    manner pollution uncertain contact her

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
