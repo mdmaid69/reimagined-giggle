91d616e1abf2bf8c7b4f1cf5ed2ed19becde361f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:30:10 +0000

    apart high fairly dick run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
