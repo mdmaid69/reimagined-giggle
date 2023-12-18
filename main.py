7be6cb26314e0961b9d4b2f12d8bb1ce090a3f8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:05 +0000

    weekend entry pocket succeed disapprove

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
