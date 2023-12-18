fc3509f9d62067f6b27041717b5dafdf593c311c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:40 +0000

    result quarter wide hope comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_base_name(path):
+        return os.path.basename(path)
