46fd268a91599bb10ace5f19af614bf282d7c206
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:04 +0000

    thick passage office flow she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
