c34b960a2a612ef37dcad9c19c40f5fc9d30f061
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:10 +0000

    unpleasant later cope bet effort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
