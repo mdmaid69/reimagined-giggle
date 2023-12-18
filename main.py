8702f349e7a731e2a89dfdbb28bd7130f27ced47
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:44 +0000

    skirt classroom cause familiar serve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
