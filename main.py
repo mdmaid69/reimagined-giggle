00bc3cee106efb54e879a4bb71c2fa8c7f99a21f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:04 +0000

    tidy live buy paper trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
