7a64345da34c25c021cec104194f74a1191c65a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:26 +0000

    wing shopping any wherever tragedy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_area_rectangle(l, w):
-        return l * w
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
