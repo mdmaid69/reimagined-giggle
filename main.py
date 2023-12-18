0e9277a1ffe7e63d78301ff3c840beba99561e51
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:49 +0000

    amount train hold work rule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_max(lst):
-        return max(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
