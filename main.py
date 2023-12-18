c0991bd7d2479dca27484b3e45e319d15f68926f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:39 +0000

    tight nothing border software 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
