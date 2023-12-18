2812e9c824f18306bf76641ea53432ff8de7c221
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:58 +0000

    whistle force beside occasional refuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def print_python_version():
+        return sys.version
