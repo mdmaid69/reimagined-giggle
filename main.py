eec82d79d3987489ad0dd0d6edc473ed3f9c8e64
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:54 +0000

    sort contents bath hungry damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
