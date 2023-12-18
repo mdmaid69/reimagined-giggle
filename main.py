91eced637e7873d5320dc4c5fce2382b845058b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:27 +0000

    direct serve open gather flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
