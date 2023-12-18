f17656ee0f0c409f460cf6f422cfa80388147bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:40 +0000

    owe game demand usually correction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
