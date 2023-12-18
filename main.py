6c0641defcecb517935098deae918bf886ec45c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:21 +0000

    field statement bringforward door big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def square_number(x):
-        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
