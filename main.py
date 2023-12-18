8d5f40a93f7dd50f7eefc4cad527c0014ec60baa
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:17 +0000

    wealth circumstance within formal arrive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
