5b58d22f72e5a900275a9b69373ed40c16216643
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:15 +0000

    die display certainly long wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
