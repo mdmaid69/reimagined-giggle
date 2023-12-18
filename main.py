51cf2a935f87dcd3d264c6c1d906f7ddb7006ea5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:46 +0000

    Ll unite deaf off prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
