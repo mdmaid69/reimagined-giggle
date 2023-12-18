a2fc04e1261a10cd65d6cd7287db3852f737164c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:48 +0000

    better gooff comeback wherever education

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
