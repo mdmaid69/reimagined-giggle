ec230d681a70504e491062ba1aff95aabe7f781c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:14:22 +0000

    do leave mix suggest best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
