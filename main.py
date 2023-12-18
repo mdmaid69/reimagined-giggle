436936ba77133a31d4960cbc777eb70dc0b1010d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:46 +0000

    we phone flow time happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def sort_list(lst):
-        return sorted(lst)
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
