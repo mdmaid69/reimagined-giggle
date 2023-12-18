f02dfe36c81ddd4214b0028efca55c12f392225b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:33 +0000

    cheek attempt lunch chance suck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
