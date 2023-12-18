6737770abae7e174b66185968bc8dd6733e7e5be
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:11 +0000

    round every godown wash refer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
