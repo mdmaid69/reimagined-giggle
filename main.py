2d117106913e436a3aa3852cccf6e51f57d63f07
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:03 +0000

    arrive grand pride relief break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
