49442715305bf7b0fa5110c091f3862c0ff4a91c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:52 +0000

    fix could connect pause work

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+print(datetime.datetime.now())
+print(sum(range(10)))
