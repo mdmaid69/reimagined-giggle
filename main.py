d9853acaac0467f78a916531fc4d668df51aeca2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:18 +0000

    slowly spot people demand pattern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
