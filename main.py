7da669bf8846d930ec5a32e8d089597d7ae58e48
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:18 +0000

    else well collection thinkover exciting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_odd(n):
+        return n % 2 != 0
