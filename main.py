79f471280269fc18706ed43d7693bc5977657730
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:06:18 +0000

    ticket press fear getout right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
