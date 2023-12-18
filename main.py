94fd88cf23202094260e26a65c46b2bd71fd1383
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:09:39 +0000

    early however appear sell Yy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def clear_array(array):
+        array *= 0
