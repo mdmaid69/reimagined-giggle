cb60e00cb18cc54fd1a31b6ce3788b0077fc80e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:40 +0000

    event Tt somebody any forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
