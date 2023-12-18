d2eb84c5ab5474fdb3519bf832084b8618b0c050
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:03:17 +0000

    cool inquire difficulty secret mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
