49db6b30bd76370de8e9a79010c9805a1b8cb6e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:52 +0000

    rain lock rain owe take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
