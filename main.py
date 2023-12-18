b4b0e21f8708a07c70130f3986e69cc8b6ad9b29
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:54 +0000

    although control little getup strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
