8ed539078b6a056e2431190f33315a30608a3821
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:46 +0000

    guide manner closed fork birth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
