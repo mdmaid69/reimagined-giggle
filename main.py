f94dc4120eb37f1d14ad8150cf6be360da2e513c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:25 +0000

    quality police show workon answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
