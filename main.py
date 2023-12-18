1421f8dab9635946a66119c91d0568c6b9b8b0dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:29:16 +0000

    basic tragedy will journey next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   def multiply_numbers(x, y):
         return x * y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
