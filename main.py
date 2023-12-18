0b9535bc92690cd91f47e6ffbe0b68cba7f9fd4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:34:17 +0000

    spill treat along cry when

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sys
+print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
