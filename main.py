0bfd39199aeeb843d40610de6b5db91184b47533
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:52 +0000

    shape promise area connected onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
