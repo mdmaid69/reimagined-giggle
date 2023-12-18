f4d86d0fb29cd9cbc1f520db3514c0859dffa2cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:04 +0000

    mine well downstairs offer exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
