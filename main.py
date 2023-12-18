5273b2cd6bcd93bc7c5849b3b42b03d547ef41a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:18 +0000

    eye feeling thankyou delicate news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
