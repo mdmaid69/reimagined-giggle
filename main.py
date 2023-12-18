6fbc1d1da2989a26373c94d9ecb0beda1ffbaaf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:32 +0000

    distance careful praise there learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
