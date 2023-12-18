b2b1a647885a6973e869b3b9703cc1ce2e97b4d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:31 +0000

    fierce total grand stop me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
