a9077aa09bb701f13549b012f4397557539a379c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:01 +0000

    geton and lock positive funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
