fd1e76e0f637d6b52adcb2bdd7b45070498fa887
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:57 +0000

    fire find backwards bag sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
