87647592a41ede43e1f4ef35297a94b49bb0bbf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:46 +0000

    smart feeling experience Gg rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
