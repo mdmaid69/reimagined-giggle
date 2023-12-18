3b152251d244b6b866880f43e747ac927c3cbe08
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:13 +0000

    good translate stare discuss treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
