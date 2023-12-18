8a74990160c58ff49bbb0e4809d3dd2232ae22cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:32 +0000

    attempt lookback(on) high Pp use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
