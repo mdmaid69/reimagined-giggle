0efbd7b1e193df749aaadcf2981a2a4c18255e10
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:09 +0000

    lay past describe advanced search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
