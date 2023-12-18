9c199a55a2ff4963e0ae072c9ffb2f67c05e6fbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:58 +0000

    advice programme criticism sign arrival

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
