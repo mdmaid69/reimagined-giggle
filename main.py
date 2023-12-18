c640d7239da84a7d7c5f656bc43ef497e17573f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:55 +0000

    rush 571enough even suggest around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+print(os.getcwd())
+import os
+def remove_directory(path):
+        os.rmdir(path)
