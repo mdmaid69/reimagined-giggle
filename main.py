e0e473417be067e3337ba3bca76ff7da1e825e13
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:27 +0000

    face mean whom foot delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import re
-print(re.match("h.*o", "hello world"))
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
