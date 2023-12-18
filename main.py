816a599bc4e0e41f041f0e0b16c2b9ad42d5f9d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:07 +0000

    pile mistake operate somewhere Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
