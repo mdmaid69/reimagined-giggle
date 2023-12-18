bbf31d78997f18c539aeee946eb2814dfcc6f740
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:36 +0000

    takeaway spot account roll goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
