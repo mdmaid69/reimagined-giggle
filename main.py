12fa1ea566c37ea706d96de36676a15d3e1dc852
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:23:37 +0000

    rich finally tense noisy situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
