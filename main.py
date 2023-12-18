18f86e13bf619a52b5b2a25998615c39cbd88745
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:18 +0000

    eager battery confused live brown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
