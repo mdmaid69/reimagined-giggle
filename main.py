64746f4385e5bc8bb1b4620c4eb82cf855197daa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:09 +0000

    scheme brave face still thought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
