a06cdd1e95707b6c9caeb3ededd7d908af21d40f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:04 +0000

    minority takeover have goaway apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
