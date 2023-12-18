33a504f9a44ace723341ea3c7781af1db4c1db21
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:39 +0000

    upstairs suggest thick appear because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
