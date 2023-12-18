411085449f9b008599deb6194abca84f3c677900
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:26:05 +0000

    callat better crash first worst

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pressure(force, area):
-        return force / area
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
