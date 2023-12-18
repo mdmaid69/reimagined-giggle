b2ac22c15c25905f0e3da591b7a91eecc555d04f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:32 +0000

    fight section reject actual open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_elements(lst):
+        return len(lst)
