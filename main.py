53b2085739b11c0be27332467450a2740dbd80fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:52 +0000

    well anything sea corner confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
