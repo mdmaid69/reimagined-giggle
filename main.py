6ece6ac9ee1ecddad7c7c544e75cfef82cb746ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:11 +0000

    only long measure hers outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_list(array):
-        return list(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
