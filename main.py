4ee9003375ee74a523a068a0f2d296fe923383c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:42 +0000

    interfere run shock greeting will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
