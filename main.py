b4c8a2c67fc27b123f1421a6060b74236c1cda38
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:09 +0000

    kick foot meanwhile emergency whisper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
