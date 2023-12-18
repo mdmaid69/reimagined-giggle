d60810771cfe34d82c745fc518b7b3f2dfb0cf3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:02:23 +0000

    education choose blind left anyone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_typecode(array):
+        return array.typecode
