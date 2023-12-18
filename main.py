f06a08cc3bd0372d8313f741da9ab87711d58d3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:17 +0000

    issue pure whatever throw OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
