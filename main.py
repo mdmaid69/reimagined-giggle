bab525b0e2ad3c5948633696774eb51957e3b4c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:11 +0000

    may estimate makeup watch goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
