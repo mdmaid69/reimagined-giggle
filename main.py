43c0c9e3aa184c7ad2d096eef89d87d8868b2166
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:15 +0000

    transparent sink total wave impatient

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
