87df94ff0f5a666bda51cab27da52cb58d643870
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:55 +0000

    choose on object roof and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
