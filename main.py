9ba2cbd0c8f26d331af270e6155b73963807ac84
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:19 +0000

    snow kick office be treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
