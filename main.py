9453443e3eb814ec799255b129f2a3d8661f07dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:57 +0000

    wind treat attention break face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
