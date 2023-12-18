0b02bd870e4f26a477cd9f7516fcf9d0c6b88ee6
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:55:33 +0000

    loose sometimes upwards find man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def reverse_array(array):
+        array.reverse()
