7d6750cb68e66938ef1524f465d3fd5ed0cf2d4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:41 +0000

    manner across grateful rent carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
