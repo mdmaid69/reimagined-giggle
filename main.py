3b5fa201b8bab461e460e02fd6929b328b5294bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:53 +0000

    remark wide skill heart squash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
