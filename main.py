e2ac173377a4947c2b8871a37b7dba8a36d72a8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:05 +0000

    light card design ours matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
