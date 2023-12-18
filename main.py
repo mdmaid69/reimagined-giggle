6120325fac37f36829d56105520997e55a075ec4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:20 +0000

    communicate cook influence sorry ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
