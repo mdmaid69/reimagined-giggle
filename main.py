b2cc598ed077a5bf86179dfc54f80900077d96bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:19 +0000

    deliberately recently well how forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
