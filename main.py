e868bd45b87d7b30c0632689f40a6424fb5dc3f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:58 +0000

    view nowhere exchange 569enjoy patience

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
