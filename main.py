f3c8f54118c29f60b10ea789a6ece13597e2d1db
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:44:52 +0000

    hit cool lesson crowd swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
