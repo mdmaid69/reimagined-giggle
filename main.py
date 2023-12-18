2e7b30b5981143745085395e9f544208e1ddbd40
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:50 +0000

    by pretty believe switch steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def convert_to_binary(n):
+        return bin(n)
