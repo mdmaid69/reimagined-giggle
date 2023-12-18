79e47c59ba8be31951016226e93479b72520711d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:05 +0000

    think thank deliberately accidentally bringback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
