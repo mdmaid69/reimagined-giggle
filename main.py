7e0835aa53bc08b7967ff29ab5441bd6d29baa11
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:27 +0000

    question mother slightly would violence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
