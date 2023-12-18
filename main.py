60500c77ad8d68afa629b676baafc61183900044
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:30 +0000

    ready inquire comeout repair similar

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def exit_program():
+        sys.exit()
