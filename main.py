7936e77932ec95649f0144aaa377db6dd7ab1ea6
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:06:23 +0000

    glass claim tough them uniform

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
