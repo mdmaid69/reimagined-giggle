c1d4284cd5c6ff5e6528f0cf2294dd578241cda7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:29:40 +0000

    hungry cost very dress cruelty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
