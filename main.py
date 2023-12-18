df2d753c5861fe317a2e5b396c9200279ee76a59
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:44 +0000

    lot machine moral bill clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def divide_numbers(x, y):
+        return x / y
