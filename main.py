31febe20f2ec8a706e23a432917f17eed4484e68
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:00:50 +0000

    can capital careless ashamed permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
