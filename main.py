7d3e7fe93910995d8d52a6ff03fd1a19a1e034f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:17 +0000

    appear design 565end drink everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
