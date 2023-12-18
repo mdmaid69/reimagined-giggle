20d1157ee570b3dee799933ea70258f48abf257c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:24 +0000

    politics means autumn lend fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
