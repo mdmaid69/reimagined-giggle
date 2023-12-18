8f2aa2673fa2c55ddef38464811378c65cfbb431
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:18 +0000

    rest second mine characteristic debt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+def remove_directory(path):
+        os.rmdir(path)
