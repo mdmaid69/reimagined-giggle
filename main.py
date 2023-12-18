4527c61b6c50ec19522d2112013495fbea154456
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:17 +0000

    today operation boil wish serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
