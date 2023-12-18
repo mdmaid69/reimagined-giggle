a762e4d174b1481f22d0b37b354d118b62c8fb2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:37:26 +0000

    comeback grateful tense snow time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
