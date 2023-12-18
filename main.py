b1cbefccdb0a8f608165e8f81a50b8f02e8ec7ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:28 +0000

    home talkback listenfor meaning recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
