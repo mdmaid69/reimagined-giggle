db4b1bc103a09f6412cc886ecd860152256dca40
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:00 +0000

    joke crash warn extra street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
