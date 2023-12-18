50d4a5e731ae38a249557ff2573d0102df0e43aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:26 +0000

    wear interfere interrupt interview branch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
