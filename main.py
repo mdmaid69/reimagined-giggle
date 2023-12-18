bf36cda23d7f4368105aa8e210b58fc7b42b85df
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:07:04 +0000

    rest dress beautiful valuable beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
