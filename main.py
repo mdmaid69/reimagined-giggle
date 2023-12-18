e3f71811f247a4f4e58eff6eaeb96e48625b3e90
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:40 +0000

    want soil achievement amount practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
