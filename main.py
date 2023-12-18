d0883313542d69d610f0544fa6f0a00d37f8f854
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:54 +0000

    build deserve instead programme top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
