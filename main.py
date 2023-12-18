8f64bd99ef61d2a4a433706f9274bcc65b93174a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:52 +0000

    beautiful old real common right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
