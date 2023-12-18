9041e29ff1e97c6e33eec1cb33d715ae330b9b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:20 +0000

    fast rare fine satisfactory suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
