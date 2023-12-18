42d62d2b94b049ebc742f6ed11b8457d4d1f7d8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:07:44 +0000

    possibly further I wash search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
