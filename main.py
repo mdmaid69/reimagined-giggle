049ab531c963fc420fdd94b3ddcfb42917c2e836
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:00 +0000

    protect same label course one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
