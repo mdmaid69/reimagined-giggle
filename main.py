68ffb8e30b8c2ae2e2a749cb6764162755a62854
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:11:12 +0000

    top thirsty war desperate crowded

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
