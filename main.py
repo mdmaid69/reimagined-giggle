24e8f7aabb31afac37c62a9f5b7df8977f45a836
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:48 +0000

    rather work side bet exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
