96cdb2d1b5c92e4a2a393e2cfb2522f88ad0f8bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:50:23 +0000

    occur cruelty correct excellent responsible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
