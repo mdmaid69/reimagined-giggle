719866dd4a631cf805dd8cb7010e7ab9c055e272
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:45 +0000

    badly it steady far pride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
