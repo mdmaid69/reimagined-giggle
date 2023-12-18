99bbfa8ca0906ee2ea410251c60c2d3cf7097efc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:33 +0000

    today brave giveup scale plain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
