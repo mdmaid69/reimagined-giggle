4d3be0f16e1768b37b7fa451630498775e60ce2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:40 +0000

    string reply nigga interest wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
