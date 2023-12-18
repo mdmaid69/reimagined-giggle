7b205886c41d19c218bd0668ebabdb0ae436059d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:57 +0000

    quit contrast hour computer play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def is_odd(n):
+        return n % 2 != 0
