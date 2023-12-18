6bbec795418170fdb55d2e60ea0dee89c1cfccce
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:51 +0000

    judge comeround duty yesterday collection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
