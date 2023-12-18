b3a40c0630d5be4be2586d223b134a71d8111930
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:24:28 +0000

    pocket only above century agreement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
