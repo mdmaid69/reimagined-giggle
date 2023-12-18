b30bd451a29259b9282d216497f1dde0211724b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:23 +0000

    good note children whom guide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
