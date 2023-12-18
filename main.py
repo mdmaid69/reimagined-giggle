1e0c39ec4f70be0cfe10fc8fcc566f832e467018
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:40:49 +0000

    equal birthday pin certainly light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
