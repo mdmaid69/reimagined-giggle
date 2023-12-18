97a58077ceb787e2a26947f60e848e1ae9480bb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:16:24 +0000

    switch shoot date invitation just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import time
-print(time.time())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
