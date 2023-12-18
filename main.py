71a886b33c525082ebbc8cb2281bdf3c8874c3e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:08 +0000

    dull could concentration workon ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
