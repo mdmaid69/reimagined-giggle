f88d8e6b216a12fd3dc21e9837c2badf0ead1b87
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:56:14 +0000

    safety lift share sign pay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
