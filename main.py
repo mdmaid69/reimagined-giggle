18551b1a127cbd568e3796cc73308a992ebac555
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:13 +0000

    proud happy ready loan original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
