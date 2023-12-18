4db5df420b33338d7d418e967244746da95f276b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:41 +0000

    view introduce importance continue murder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
