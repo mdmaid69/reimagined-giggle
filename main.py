3ce48219b521c0150ea0c8e5e9c52437c9950b0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:38:12 +0000

    voice introduce there medium giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
