7eebe330c072b60f372238d0ac8b662ac972ba39
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:58 +0000

    lookback(on) regular form trip touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+for i in range(5):
+        print(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
