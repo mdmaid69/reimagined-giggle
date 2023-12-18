f1565b6176cede09d1e2e22dbb6dfb5952e2f8f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:11 +0000

    surprised inside joke apparently fashion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def add_numbers(a, b):
+        return a + b
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
