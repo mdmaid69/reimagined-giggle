d032109dfc5dee353b52a58f280014b19dd94420
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:33 +0000

    practical 564encourage Kk side fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
