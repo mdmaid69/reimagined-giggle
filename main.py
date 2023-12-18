c0abea726ffe4311b8e10cbd597dd9039302ce12
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:41 +0000

    better brave react sail excellent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(lst):
+        return min(lst)
