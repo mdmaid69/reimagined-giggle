ef72dcdeab3ca0efc8e10b105ef1097e353e5431
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:02 +0000

    measure go experiment butter produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
