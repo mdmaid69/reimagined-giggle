fff6b72d3e36644ef60e46caafa560885fde8a41
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:37 +0000

    thick judge report holdout rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def count_words(sentence):
+        return len(sentence.split())
