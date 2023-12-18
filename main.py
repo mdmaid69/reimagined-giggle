bc7b9e0e24c28364347f255a2a35944994ca4c76
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:37 +0000

    regularly obvious any regular soak

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_current_directory():
+        return os.getcwd()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
