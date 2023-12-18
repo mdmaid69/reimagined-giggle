618f944503d5abc9503d323d31a2549ac9b7e311
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:27 +0000

    waste foot constantly shame public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
+import numpy as np
+print(np.array([1, 2, 3]))
