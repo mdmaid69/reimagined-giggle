3de50aa7677d45f23dde4eb8a5ffa728d24990d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:29 +0000

    time enthusiastic less Uu correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
