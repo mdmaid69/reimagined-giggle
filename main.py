d938440ac539a0a7f5c87a7bde33e55cf4bc06b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:06 +0000

    prize pressure most 179bite already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
