0ac89495256982745e5536808c2a65f6b64c9980
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:42:25 +0000

    minute unite joke foot my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def convert_to_binary(n):
+        return bin(n)
