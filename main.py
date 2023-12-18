d2c23490e1bcd66a1d73ef9cb85a603426a72863
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:53 +0000

    famous charge smell takeaway trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
