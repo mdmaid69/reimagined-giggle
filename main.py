f39909b4ec6800402799a16b303b05510f279440
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:07 +0000

    storm although aware month people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
