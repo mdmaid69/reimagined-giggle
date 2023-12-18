9e9c819c17a3863c4750e760ec066738e2cacb37
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:35 +0000

    sink mistake error copy complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
