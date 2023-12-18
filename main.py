fc5b16d8cb5448c1b52d87a8056aa52f58d3c3d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:09 +0000

    left instead settle accidental clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
