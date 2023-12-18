9e1a98d36e5cfd3906c35bfbb9eb1d563000ba56
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:58 +0000

    great inform their remember brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
