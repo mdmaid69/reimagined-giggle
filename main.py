dfcbf9af1ef848cfa2579ad3e585425ae1ec00da
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:21 +0000

    Cc cross father crisis reach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
