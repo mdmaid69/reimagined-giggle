6d16ae3042afa554247f0abbd8e572ac281aa521
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:02 +0000

    never maximum lock target where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
