b803ddf687aa119ce3d3ecc1e35ffe8a28dcacc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:56 +0000

    exchange serious permission muscle song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+print([x**2 for x in range(10)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
