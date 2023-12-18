de93e88713c994baa618e23f11eca30ca651cb62
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:24:39 +0000

    soak realize trust behind plant

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_user_string():
-        return collections.UserString()
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
