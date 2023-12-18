4d49a47dcad00efa7de8a755b359bb905849fc5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:43:40 +0000

    good visit thinkthrough science plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
