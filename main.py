db6d781c4bb512a66aef4ec1466ff46fc0389d9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:43 +0000

    prepare workout fit why organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
