86cafa7b583176ff2be109c570b372e08ec6045e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:49 +0000

    increase more generally offer complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def convert_to_binary(n):
-        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
