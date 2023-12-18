4ca1e68073ae0ba71f59e93fad416680e6f32d08
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:24:45 +0000

    scare behind tough computer long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
