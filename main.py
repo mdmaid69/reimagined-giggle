e162f600c747c02a15aa625651973b7be0eff8a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:58 +0000

    hate squash frequent breakup form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
