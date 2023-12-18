eaf8608d0a95715e9a80e4fad16110ac00216b04
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:20 +0000

    colour such advice involve slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
