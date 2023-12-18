a69b7ed984d5eb3149fab8d02b59c5b3d1891780
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:38 +0000

    reduction brave offence private touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_octal(n):
+        return oct(n)
