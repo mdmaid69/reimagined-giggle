9134e5592e1d5ef95c41f3fba1ff6eb54ceb1c74
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:26 +0000

    bit responsibility complain finger weather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
