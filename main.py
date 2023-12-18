a91b24878bfd2eec5c11407f58ca2c64599fd515
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:20 +0000

    high whatever command ill joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
