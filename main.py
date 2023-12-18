9d35c4e4269312f5d6f604288a0ed0512687f2a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:35 +0000

    sentence favour surprised small blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def split_path(path):
+        return os.path.split(path)
 def calculate_return_on_equity(net_income, total_equity):
         return net_income / total_equity
