b04d08405e42422e6a8f94719ad0b77941402a0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:24 +0000

    lost perfect scrape wide future

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
