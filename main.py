90dc7077b9180631cfa838448d521e8a135c0fb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:09 +0000

    strike problem circumstance rush slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
