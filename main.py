2c5d6907b37f21d8447267f58cec5c4a00969b9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:18 +0000

    she treat explain tread nurse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+x = 10
+y = 20
+print("Sum:", x + y)
+print(sum(range(10)))
