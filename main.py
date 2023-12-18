18920932b4522e49cf711552b61fc2dfd23af130
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:25 +0000

    trial praise head thinkthrough train

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
