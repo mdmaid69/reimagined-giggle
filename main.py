828f11b45b2ab47a065e3f91f7d5c77ff06150dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:09 +0000

    plan public off sport welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
