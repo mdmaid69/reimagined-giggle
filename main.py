6e0fd4d33632a54384a69ad700a1d0f506c878f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:14:51 +0000

    almost cool recently very exciting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def calculate_area_rectangle(l, w):
+        return l * w
