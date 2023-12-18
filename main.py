5b9de369ede28c1fd74b8176dde2815fada972d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:12 +0000

    plenty makeinto remove window convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
