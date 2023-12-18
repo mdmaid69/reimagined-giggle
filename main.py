d2471b044dd2930bfd0b54dcd3212cc4d9a4f361
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:11 +0000

    one our nose dear steep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def count_elements(lst):
-        return len(lst)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
