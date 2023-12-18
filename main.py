6df5fb34ff2c405851fdaeb399f31e6c44d7dfa3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:16 +0000

    570enjoyable display space connect comfortable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
