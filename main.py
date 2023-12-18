86803c67ad106865063831b2151258d7e1eb2d7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:40 +0000

    crisis class hand owe force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def convert_to_binary(n):
-        return bin(n)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
