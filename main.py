79e3165ab270f7be88b26ebcf7ff57a228b0f69d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:45 +0000

    weekend happen roof brother project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
