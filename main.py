91ed067da082ad7cc735153c2175ca7370d0e637
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:39 +0000

    official young issue buy stand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
