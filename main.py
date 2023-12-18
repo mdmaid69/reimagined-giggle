e39525962438afb8b620b8ebfef51848e44d63e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:57 +0000

    bunch admire suppose border hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
