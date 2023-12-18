caed532d9ad5ff78a0390361b1517dcf404dc9de
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:09 +0000

    order shut muscle middle late

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
