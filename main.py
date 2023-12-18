9e497cd03d7baf268867d31e5f1d83465c1a6a81
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:14 +0000

    suggest deserve reply smell total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
