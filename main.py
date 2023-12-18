62056fd25b300cb9583b2f3b7c757a1345bb8089
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:20 +0000

    rare lot near whole ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
