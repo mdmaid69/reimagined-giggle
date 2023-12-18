084c82a0db30e6c167b64ff5d93c7d9ab7691a74
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:06 +0000

    comeoff intend would rise keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
