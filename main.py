e7b30165d62472a7a2c4291312325961bd976543
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:03 +0000

    share underneath will front 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_itemsize(array):
+        return array.itemsize
