c69f1088e21b273701b7e0769ee7209e30f93b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:19 +0000

    cheat single unit suffer pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
