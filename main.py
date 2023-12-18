47f90f4b7a8df845943e1d1c976d002de23a625f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:02 +0000

    dry forever spring workout worst

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
