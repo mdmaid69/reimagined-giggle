6ae56ee1eacfa008f4d4f6050d42d5025b389c10
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:45 +0000

    also contribute sleep near male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
