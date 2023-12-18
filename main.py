158c113c240c71b064365338b9a3cd4501bd3cc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:57 +0000

    Vv split sister prevent office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
