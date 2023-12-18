8d890c414c3ab288e0140f83b4ec8f49f12a2f73
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:39:15 +0000

    extra doubt level shine but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
