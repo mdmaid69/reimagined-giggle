5be501bb3e58da66bb71cd4016a55a222c833bd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:14 +0000

    feature brave regularly when equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
