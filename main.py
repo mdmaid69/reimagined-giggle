ba467e28539c80be9ac93bcc30cccb54d57e2a68
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:36 +0000

    pleasure away reach conduct statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
