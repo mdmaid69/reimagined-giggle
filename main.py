76cc3e70b76f7a1f6a0216427daf31a0b35cb332
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:11 +0000

    tense thank balance deserve hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
