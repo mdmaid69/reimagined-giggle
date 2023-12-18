981b9ce7ef6f32978dea68fb83e7d15c1a8d91ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:42:55 +0000

    less second different opportunity recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_density(mass, volume):
+        return mass / volume
