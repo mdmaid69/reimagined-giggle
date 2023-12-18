76ba05f67cd03eab488cdb21bf03e2e800e4a785
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:25 +0000

    connection great lower snow spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
