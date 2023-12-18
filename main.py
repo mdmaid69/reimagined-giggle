c4f778c89bae6618bf90c06c61c47ce9a587689b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:36 +0000

    right trap check alive announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
+import sys
+print(sys.version)
