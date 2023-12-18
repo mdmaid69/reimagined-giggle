4aee5a49e5093b749d57f2959915931916768bb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:59 +0000

    Jj national push answer reduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
