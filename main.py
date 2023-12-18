c4e7e249700ddb38c3e7628649e11780cd00a72d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:59 +0000

    central polite fasten hide alive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
