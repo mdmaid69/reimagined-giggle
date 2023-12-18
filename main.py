f62e104ef8f16074ddea56d0b13e30cab8cf247b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:04:21 +0000

    spread speech problem talk twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
