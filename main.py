b8bb6a2745b980e68f0993cbec73f1e172e88bb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:11:33 +0000

    meaning hand advanced plenty correction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
