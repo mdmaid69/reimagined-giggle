36b62d5acbcc6b8ca6edc3564ea2d4c326d79543
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:59:01 +0000

    no season pay young entry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
