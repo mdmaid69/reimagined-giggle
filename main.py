459e1470251cd2b0ed9a207708e8c29a70ddc714
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:03 +0000

    noone image university pen report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
