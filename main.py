26f8e3e11df42bdd287eda64587ca2f5d71bf75b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:01:01 +0000

    traffic OKorokay gift approve control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
