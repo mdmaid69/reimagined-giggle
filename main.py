a1f16411c078e55af89a4fc41223845507f57bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:17:23 +0000

    side that cruelty shout bringforward

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
