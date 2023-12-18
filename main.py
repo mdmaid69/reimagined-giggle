b72d78378d587a14b9721fe2b0054542ac3aeb17
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:30:56 +0000

    hard concentrate move beginning rid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
