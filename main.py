977c9c526380d75b153b3229d4022a99994fb749
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:16 +0000

    soon round butter practise interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
