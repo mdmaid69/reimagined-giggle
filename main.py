2e99476f2a372fa87bb72be4eba804d62db6e2dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:48 +0000

    apart trade lucky during sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
