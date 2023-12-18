e633caf8b0f194d9eb6555489e799b9ed974aedf
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:35 +0000

    my under sort health dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import datetime
+print(datetime.datetime.now())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
