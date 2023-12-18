95002a978d9c476c9d27f84ed2f2b6c88225be4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:26 +0000

    takeover mine forgive 179bite harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
