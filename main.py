4b2b146937817b8f385d46ce9af67288bdcdc8af
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:36:45 +0000

    somehow adjective whoever letter board

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
