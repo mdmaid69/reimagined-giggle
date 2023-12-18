836ca197bf68d4298656ebbb7ce5903f9ce47fe4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:43 +0000

    bend power bear upstairs example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
