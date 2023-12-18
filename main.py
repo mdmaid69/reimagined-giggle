1193fcd7b04ede0190713e927e59100f01f4ef5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:30 +0000

    impressive wish active star trade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
