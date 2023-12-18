4da8e1fed5549d04b38e64bda8211187b85584a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:04 +0000

    company bye bring would ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
