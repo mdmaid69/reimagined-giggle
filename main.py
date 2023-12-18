0d39e0eb4dcce017c3df902af1882accf8849b86
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:53:24 +0000

    mistake tear runout talkoutof as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
