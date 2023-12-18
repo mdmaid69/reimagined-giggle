6042650bd397a0b9d878f8ed32ae1a60f3128aca
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:06:28 +0000

    failure nose pupil tonight result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
