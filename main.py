e328051797a66951fe0b83811892b2daff7f38a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:02 +0000

    actual bother unhappy Ww last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
