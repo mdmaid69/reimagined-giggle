2c1aaaf5ab6d2c9eaaaf558b52a927f1faa066e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:27:37 +0000

    scrape slide bright concentrate makeinto

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def cube_number(x):
+        return x**3
