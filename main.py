6fcad07f38690437cffc984a7b89de345b8dc243
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:27 +0000

    balance meal introduce pattern whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+print(math.pi)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
