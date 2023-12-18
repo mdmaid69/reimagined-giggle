00fa0d30b8b0ffdc59697b80af9c1e9a680d9151
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:27 +0000

    female same who tough furniture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
