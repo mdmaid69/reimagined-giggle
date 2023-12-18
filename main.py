bc2656b4bca94a2b3435c61889843e11787db493
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:44 +0000

    refuse expect correct tax believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def subtract_numbers(x, y):
+        return x - y
