bdc8f23e722d3dc5fc127c93ac9b7689f8a67589
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:04 +0000

    nice address sleep world temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
