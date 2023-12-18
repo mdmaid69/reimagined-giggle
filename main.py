5c3d3e46a95b9cc644b19061efed154f4b51a967
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:35 +0000

    probably either fetch protect tread

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def create_user_list():
+        return collections.UserList()
+def convert_to_binary(n):
+        return bin(n)
