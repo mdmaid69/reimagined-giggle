2381f85625d7e9431ecb977e9b25c130216ed133
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:50:46 +0000

    uniform joke weak gift uniform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
