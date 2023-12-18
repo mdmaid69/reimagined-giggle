54ee965a5c898c767a4756b57ed8b3d6f67ea1e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:07 +0000

    hard before support listenfor warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
