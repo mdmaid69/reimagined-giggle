f2c52e158b77645b020fec95651165b7e8dacf4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:25 +0000

    peaceful likely neat take lonely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
