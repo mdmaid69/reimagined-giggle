592f95fd125f5ade84a3aa3efd3a76ac54dd508f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:54:30 +0000

    apologizeapologise negative accommodation holiday raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
