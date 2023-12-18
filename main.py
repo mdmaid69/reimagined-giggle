9f7c79d930125606b95cd3d7f1f24245d8d5dc7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:28 +0000

    completely calmly fierce song unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
