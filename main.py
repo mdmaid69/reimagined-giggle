99ea55cc99e7b21d587d6200215535fadd7bcbec
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:53 +0000

    key rush window dig one

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
