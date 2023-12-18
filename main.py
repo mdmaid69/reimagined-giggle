61707a78853ec22b8ce82332d910c01b529b0d46
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:33 +0000

    indeed straight callat bet runinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def convert_to_hex(n):
-        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def roll_die():
+        return random.randint(1, 6)
