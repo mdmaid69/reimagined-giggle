7ac710cf76daee572d5ecfb554364cd07e578f88
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:02:28 +0000

    major sure light reserve wait

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
