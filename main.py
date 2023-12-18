cb107d42501a08d7ef6c72b4f1f36683bfe2bd4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:30:24 +0000

    sudden politics tip approve cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
