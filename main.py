0da6d4b5dd54030e628399a92014fc2231534bcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:19:35 +0000

    get she exit height frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
