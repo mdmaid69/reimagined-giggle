7eb591a3d30104eab2debc4475aec7734e2a7f10
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:47:47 +0000

    report trial information thing bend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_set(array):
+        return set(array)
