b98c0d866c30b7be27bf41def8a1f3fc0ac222d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:56:13 +0000

    face judge name smoke remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
