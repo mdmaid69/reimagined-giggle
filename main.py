c9e648353f4f6968b875a8f4735992cd38b19dbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:15 +0000

    plant eat back within fence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_work(force, distance):
+        return force * distance
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
