855ee0d2eb32c09326f90d0b821499295d2717a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:28:42 +0000

    prize lean dry term treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def count_words(sentence):
-        return len(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
