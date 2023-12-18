b0592be602f83a1b568807792cad396bb47bef72
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:03 +0000

    some day trap way talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
