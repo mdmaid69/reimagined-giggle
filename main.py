b63fe28e43cd5fba353ed0caf3a22e1fb7c78b4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:40 +0000

    pump fresh makeinto estimate correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
