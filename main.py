571d14b49ee8c50cf8cff3c418b4bbafd5da0350
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:55 +0000

    argument charge hide soak send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
