84a6d4af654173b17b8175d3b38dc3c89e0b4fe5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:57 +0000

    hit after minor lose waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_volume(length, width, height):
+        return length * width * height
