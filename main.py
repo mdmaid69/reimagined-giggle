926604b79347d790e6c241ae5df1fbb52edbed82
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:12 +0000

    activity radio damage why lend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def remove_duplicates(lst):
+        return list(set(lst))
