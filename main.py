99c3772fda7e72c54605ded75713b00638fb9cb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:49 +0000

    frighten persuade own feed far

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_circumference_circle(r):
         return 2 * 3.14 * r
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
