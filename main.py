89401c9f12c0770001fc415c16b29ea1225855fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:23 +0000

    art cook trouble callup physical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
