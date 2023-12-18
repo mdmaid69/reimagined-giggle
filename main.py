f6d1526067790be3aa3e9b2642f0f215d5377a09
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:04 +0000

    father out spread hot scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
