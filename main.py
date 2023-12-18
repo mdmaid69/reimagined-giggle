6708d322fe25ef9118f009c970752096865a745e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:21 +0000

    shop bright storm field hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
