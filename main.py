9b2383b0abd13b4e11449453c751e216a96b4cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:09 +0000

    hope advantage habit describe admit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
