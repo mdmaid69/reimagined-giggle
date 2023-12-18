b668aa9ddc88235d00b48fb8fb2305be8ab06ba1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:34 +0000

    within shade leave turndown real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
