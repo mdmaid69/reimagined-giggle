9d2f74c72d00a796f7db5a0319a35c3b3bfbb403
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:57 +0000

    carry afraid level upstairs translate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
