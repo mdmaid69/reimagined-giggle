0a41af7efa84c21e2a80bc2bcb83fc491a73f078
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:16 +0000

    sensitive phone I spot contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def is_odd(n):
+        return n % 2 != 0
