2bb21f209e0436d90211664d198b9e5eed4b9bf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:21:54 +0000

    only every move estimate fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
