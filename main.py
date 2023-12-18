2d75a6df91b0bb17cc0124ec6ae5382137820f93
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:11 +0000

    bye pleasure claim must relative

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
