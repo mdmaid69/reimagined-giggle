88c39df5c03a6595ff9f0fba7103d592ce5619b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:38 +0000

    frighten comeout cough rent between

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def multiply_numbers(x, y):
+        return x * y
