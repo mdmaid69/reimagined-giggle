0a9116b044cc54945e9399eab5d33177e447deff
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:02:34 +0000

    sting develop tread pull steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
