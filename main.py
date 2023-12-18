8a29b82701deb91d93cf14c41c5519eedfd05a6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:58:08 +0000

    television holdout measure practical promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
