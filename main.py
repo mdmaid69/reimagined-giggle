4db28ff43dbb53021c1ded02ed7941b7613210e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:43 +0000

    cinema welcome regret brush neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
