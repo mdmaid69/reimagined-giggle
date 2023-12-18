7d52643a36055c62c7f24bf19c070a4a9e73e44a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:22 +0000

    rubbish lie compare increase thumb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
