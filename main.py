78e3eb9a41e30e2407a8fdb1f449ae26bbc63f00
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:27 +0000

    male mess pure look above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
