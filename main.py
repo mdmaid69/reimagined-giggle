6fe7f55a5bed24abb298a61049cbf2392cc966b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:51:04 +0000

    level 567enemy borrow personal treatment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
