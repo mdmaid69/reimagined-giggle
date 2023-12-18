9b00cec1bfca7c92dfabaead4a42012a83c3ffac
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:35:30 +0000

    summer tip split lock narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
