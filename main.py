df9814eded7bffa20042be3dbdb60e839a6ff36e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:02 +0000

    report search 179bite able by

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
+import random
+print(random.randint(0, 100))
