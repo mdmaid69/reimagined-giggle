d11aad2108e0c84c6dfff983511487b532297ccd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:05 +0000

    interruption pin cook upstairs region

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_work(force, distance):
+        return force * distance
