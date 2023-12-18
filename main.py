e32845e1c6cb61e124e8bd7d297e8145b000c7a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:37 +0000

    maximum custom wear punish urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
