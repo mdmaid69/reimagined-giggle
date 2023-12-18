1672496bb41377f6f2a2068d0597c2bfaedad5a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:42 +0000

    fire proud aim way false

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
