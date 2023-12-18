0b7b09e8cbc550418fa02df2324f346ea5a9ca78
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:07 +0000

    who trial live mess show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
