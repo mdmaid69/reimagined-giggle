e9616e8c1821f645738daff59ccb6a991a72caad
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:04 +0000

    powerful makeup flight occur tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
