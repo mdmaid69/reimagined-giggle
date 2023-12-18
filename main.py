0c3c947a8ea91fa4fa395d7b272b67cacdaf65bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:24 +0000

    fierce hurry warn shame go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
