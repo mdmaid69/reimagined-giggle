feaf29978e8985a00e9dc859140403a2ea950d85
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:28 +0000

    feeling reply shake birthday pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
