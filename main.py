1ba34ac9f66fb0fac6166763e18119e16eacbc11
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:48 +0000

    feeling worse comeoff forget stand

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
