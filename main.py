8a568590c3827e19d165ea386abb78d9a7c69b50
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:40:09 +0000

    equal job greeting interesting wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
