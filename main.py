4de1320c299583edb97cf4fa70f8c5d74895d9a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:37 +0000

    talkoutof nearly help task appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def cube_number(x):
+        return x**3
