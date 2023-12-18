350e223a7b6971efdf2e5b28519b3d0a59aea657
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:30:45 +0000

    design near waste score tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
