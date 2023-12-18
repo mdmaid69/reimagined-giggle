4923719c4a7ee76c456e996d1d20125dd97e6dd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:51 +0000

    public near morning bus score

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
