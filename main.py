6896558bfa850c20c73ee55933dd4462d3548f67
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:37 +0000

    expect announce base live your

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
