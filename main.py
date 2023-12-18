83b5109030dbeef5ae0890548bb73d95a2f42f57
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:34 +0000

    goout drop makeup affect behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
