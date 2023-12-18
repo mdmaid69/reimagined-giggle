44e23392e3c3f4824239559085014f8651a92aa5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:31 +0000

    make tonight than off somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
