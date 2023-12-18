5536bb532354316c40f5a7be938ca3331aec0bf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:05 +0000

    sight sign whether true lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_list(array):
-        return list(array)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
