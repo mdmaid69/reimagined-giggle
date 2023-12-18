be855c76c761edc0ea5fff7dc5003ea58724fa0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:38:18 +0000

    exploration example she workout slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
