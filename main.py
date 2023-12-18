3806d099d5e91f644b2f5f0d13618db25ee4faf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:42 +0000

    cheek thinkover call exaggerate friend

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
