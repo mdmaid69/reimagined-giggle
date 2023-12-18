fc41c01138190fe5ef7a527cd744462801961669
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:20 +0000

    succeed tread cost basis Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
