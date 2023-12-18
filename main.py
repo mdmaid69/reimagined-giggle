dee24021d49060f6e258c03f1108f6f649631b7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:16:01 +0000

    admire tap material who burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
