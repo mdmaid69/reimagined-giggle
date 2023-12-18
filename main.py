6afecad794979cafd307e526d839aa998bc54915
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:24 +0000

    dress know ground professional uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def cube_number(x):
+        return x**3
