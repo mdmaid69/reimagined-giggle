7114420df86385274a7b56a467e0dce53439ef28
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:22 +0000

    shake sort nowhere row wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def extend_array(array, iterable):
+        array.extend(iterable)
