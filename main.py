f18946caaa127b3c47504005fbf3e6968b550d49
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:16 +0000

    whether sure keepout sure fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
