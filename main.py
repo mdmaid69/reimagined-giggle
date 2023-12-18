7ad14cda6eb73870e73dab4e370c4137296a15f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:46:48 +0000

    empty yesterday giveout wait similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
