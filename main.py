c0b7aeceed06b517063c3ca9f1a6026fdbf2ba73
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:46 +0000

    belief cruel lose open band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
