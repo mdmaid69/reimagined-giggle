53e4801a63990de78c92fc054d3116748df5ce55
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:06:57 +0000

    argument blame law pair rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
