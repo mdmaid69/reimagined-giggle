49a185f40f2590c5013c0b2272c97f0604e7b420
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:08 +0000

    rain board complain agreement getout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
