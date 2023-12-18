fa384e735facdf261a8f08a322b9dd1c7803ba23
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:47 +0000

    bank consist edge relative hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
