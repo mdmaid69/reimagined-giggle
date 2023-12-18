6540adf2c6d23aa3d6c9e86f1235e9441fdaf531
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:43 +0000

    ill order among comedown pinch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
