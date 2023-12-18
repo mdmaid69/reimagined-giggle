47306a199d62e3035c249abadff9383760512de1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:45 +0000

    wealth lunch near science say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
