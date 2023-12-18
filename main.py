42f5cf22d0a3183e2f194aad5a10dc0230051f73
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:05 +0000

    powerful dick neither ring in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
