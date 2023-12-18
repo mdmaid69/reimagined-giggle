86b12c6f837a8d0c5c5ba319c6c19f659068868e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:50 +0000

    form might touch skin obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
