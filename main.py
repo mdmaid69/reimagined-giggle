905e4a6cb6b3ac06233aa68b579f90e302626041
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:53 +0000

    sort action final it sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
