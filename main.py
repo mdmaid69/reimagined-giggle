ed303c4a1eed25dfbe6027c1bb7790a4ebb1b22d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:55 +0000

    progress finish fond and again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
