df85a95fe8d4c5032daaf820a3dea1a6c0a67342
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:17 +0000

    past expensive do godown deliberate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
