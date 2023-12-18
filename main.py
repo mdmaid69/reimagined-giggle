6b2c9b4dae6f216cba0cb88c4b9e483c6be4cdcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:35 +0000

    principle also maintain connection when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
