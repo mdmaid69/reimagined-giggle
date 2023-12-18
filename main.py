71d5264999137e804605e4d7059465e53197e298
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:39:50 +0000

    oil factory stiff keepoff safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
