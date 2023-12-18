39928806f8061ae481ebdd2c50f430d0a3066fbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:16 +0000

    official sleep behind soft own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
