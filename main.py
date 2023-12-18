711b3d3c2a146004e0731b427f0721ccd9eef2df
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:15 +0000

    bread react enthusiastic limit occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
