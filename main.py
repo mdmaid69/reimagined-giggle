0f92036004f8f2345a032b199e383b4947635fac
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:38 +0000

    round feature score modern 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_speed(distance, time):
+        return distance / time
