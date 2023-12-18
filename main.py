35605d1120e162a681f19fe248fce3938e9bf83b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:24 +0000

    pleased object separate crowd unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def print_python_version():
+        return sys.version
