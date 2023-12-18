33db327b0ae00222fc643701c0d68ef15f449470
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:51:37 +0000

    feeling crisis probably wealth hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
