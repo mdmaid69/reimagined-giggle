7616bea1732605508cf0b02d999b57b6d9065c1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:57 +0000

    score several bringback move pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
