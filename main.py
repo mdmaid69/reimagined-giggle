395b6ebf90e3442918f916e1c7701558bce2e800
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:32 +0000

    cry young furniture fat smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
