7b9b1463dbabb86ea8669c14c6eb5a3cdcb2199f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:37 +0000

    workout love mother television their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
