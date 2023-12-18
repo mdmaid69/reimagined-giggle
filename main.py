4c895867103dece270ce316b9692c1f356680d89
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:32 +0000

    related problem touch everyone hospital

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
