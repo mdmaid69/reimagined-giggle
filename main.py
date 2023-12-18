1a546bb5371f00d867a0086392fe4367a8295e02
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:59 +0000

    switch cover fold satisfactory I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
