63508faf09a2bbd25be6512bb99868a43efa8629
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:26 +0000

    supply shout forever fence risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
