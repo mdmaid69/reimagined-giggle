0c11bc4bb21f43d52268c2c68e49fed30e067b82
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:54:11 +0000

    violence normal remove advanced partner

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
