71a6fd27df72ebbd610867d1972e137757caae3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:03 +0000

    correct feature near damage realize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def square_number(x):
+        return x**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
