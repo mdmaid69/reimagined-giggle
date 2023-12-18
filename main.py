b5da0979bb718dd3e0a19eef63ad2b1c04c7fe55
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:49:48 +0000

    advise situation reduction factory heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
