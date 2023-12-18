73635e99390bddba3c48555379da2daee15b9fd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:38 +0000

    point after soon tune name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
