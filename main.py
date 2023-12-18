004eca95a5a7b05811dd0547da5e098f8dd4ff59
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:50:02 +0000

    connected take aim calm refuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_volume(length, width, height):
-        return length * width * height
+def get_array_typecode(array):
+        return array.typecode
