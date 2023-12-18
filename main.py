acdbb4f5fd45ed5a69612b1224cb8782240e8f25
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:55 +0000

    shallow Rr trap practical wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
