fbaf53686da5b083b0745bc3d989a59ca93ba4b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:29:30 +0000

    unpleasant study round ever settle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
