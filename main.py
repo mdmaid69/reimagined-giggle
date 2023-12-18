71f4796d1225138f9262fb7e5362c87704f94418
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:02:32 +0000

    top speak appeal refer rare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_set(array):
+        return set(array)
