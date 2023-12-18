2b0e71b22f462755c7e8aa36f4b5559fb7e0fc6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:08:09 +0000

    fence unhappy OKorokay eager insist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import time
+print(time.time())
