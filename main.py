0329771114bf9d6f82cc641fb53d356f8dec9754
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:39:01 +0000

    without remain ticket various model

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.ctime()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
