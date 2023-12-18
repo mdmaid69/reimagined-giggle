5ae2ec78036de1c143436b94c986f285c68b7f30
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:27 +0000

    substance shame shoot underneath 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
