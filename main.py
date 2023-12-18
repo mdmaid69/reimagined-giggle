f85bf28cb58a5e6f328dcd8cc2f5638a48e8f510
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:57 +0000

    care he trip long everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
