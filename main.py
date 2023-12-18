9a2b7388e44e5750df9f3b7c48ab8dc344b4c1d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:43 +0000

    form muscle extremely good lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
