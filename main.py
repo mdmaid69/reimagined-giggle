d82250d48c9bc89591c81f3745a29d34b9007144
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:39 +0000

    empty pocket us medium round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
