8af74eac2ca72e71c487179157457abca64d2797
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:29 +0000

    myself design claim degree there

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
