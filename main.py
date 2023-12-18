740cc8cfa868cce16c956b408324c864427706ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:31 +0000

    lot mention word vision choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
