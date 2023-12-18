c4957197ed72504db0bfe102f8039a5996f43eaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:43 +0000

    pile intend goodbye preparation over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
