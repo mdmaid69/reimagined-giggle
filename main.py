a2c8db33c15b22bd56c3ba69547cbbde3a1cafb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:30 +0000

    to seriously often lock whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_pressure(force, area):
+        return force / area
