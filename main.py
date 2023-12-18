f1cf7c6f1a2b9eaf2ddc52bb4a93bbe37eea2a4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:22 +0000

    cum difficulty shoulder complete Ff

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
