25d688c1433722473b56fee8d5c6af2ef8e19abf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:46 +0000

    loose guard over estimate solve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
