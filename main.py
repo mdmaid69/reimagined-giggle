833b98881d90b14ec4b77d50e137b7bf7a647c09
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:47 +0000

    clue ignore allow embarrassing achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
