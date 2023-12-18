9037010b5188c76404a4cbbd921ac78503e36700
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:12 +0000

    measure Ii gentle workupto smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
