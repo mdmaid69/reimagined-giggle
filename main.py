c3fc871a5b36da3df6fb552db403dc642e8fa058
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:47 +0000

    phone take sleep calm thankyou

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_pressure(force, area):
+        return force / area
