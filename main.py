baa55269bccc360bdfbfbc162b2da7be28d47f6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:24:17 +0000

    memory shout proof perform strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
