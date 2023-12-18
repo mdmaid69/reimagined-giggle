fb71cec000c92bf39268e46dadc2a369fccf2a10
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:27 +0000

    walk habit ahead trap rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
