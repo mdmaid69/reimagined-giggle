e404708d279fdf2a0ec9a1736aa616037a53bfdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:01 +0000

    without downstairs speech see scare

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def convert_to_octal(n):
+        return oct(n)
+print([x**2 for x in range(10)])
