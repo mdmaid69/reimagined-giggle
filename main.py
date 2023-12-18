2a24b6478529a7ce3086d6c886128dd9b14010e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:06 +0000

    heart well waste report without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
