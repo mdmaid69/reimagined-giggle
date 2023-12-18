db85cb12820ca5a22bb777ea1ac8203e7a000b14
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:35 +0000

    influence high crime keepaway extra

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_current_working_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
