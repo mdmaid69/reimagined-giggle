30413f4d2485591efd21ca91b7cc7db469a58662
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:39 +0000

    light entry building traffic broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
