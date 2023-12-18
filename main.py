fa490b9decbdf62d7953288406d90152b04dded2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:30 +0000

    trouble runout want ring along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
