a6087332fc37e036227d44d1264fc3929ce95bae
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:04:53 +0000

    rain welcome any trade quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area(radius):
+        return 3.14 * radius * radius
