eb7cd0d0def819c001679ecbdb3f2198045dbb19
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:59 +0000

    water scale perform very or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_pressure(force, area):
-        return force / area
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def reverse_list(lst):
+        return lst[::-1]
