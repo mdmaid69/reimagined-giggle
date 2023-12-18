bae63b68c95d8930a7bc11e496eeabffaf4d0b10
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:15 +0000

    live platform operation all platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
