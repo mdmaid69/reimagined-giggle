eb1455e417f8ef6488384f0d365c5b842b6e57c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:57 +0000

    brief operation square fast voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def square_number(x):
+        return x**2
