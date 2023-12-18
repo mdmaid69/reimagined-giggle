a0c90947ad6dfd9de5732d8ed061c3fa89f01337
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:36 +0000

    term car lock mean plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
