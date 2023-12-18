6a2baf90e799672798fa229de8b1e59aef8e0f0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:46:33 +0000

    exploration arrival father head repeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
