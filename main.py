2dfc65090ddf6c63d590c7e29758a8252a17bd49
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:26 +0000

    town border house turnback shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
