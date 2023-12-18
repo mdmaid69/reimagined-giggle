a2f66f2c2cafafabdc7131f3279e7d151343bb0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:36 +0000

    difficult disturb turnaway second pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
