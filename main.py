e57f33c97e8a87006b0a6707c94b4a99b4c13186
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:37 +0000

    second loan by consider guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
