e7b2498ec33fd69ef29fefb765fc6f42ea7aa7dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:43 +0000

    shout number place bury giveup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
