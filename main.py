654e7b11b5c067d910898e6158257881d17e1104
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:44 +0000

    information steal surface guide pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
