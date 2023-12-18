b7541afb2e4045d58187f72a3644df3589f57a0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:59:34 +0000

    reason watch importance worse sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
