afcc57ed0e95e95a23037b97112732409ce2df81
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:42 +0000

    myself matter fight immediate either

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
