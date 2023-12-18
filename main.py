66d30f7a32dad39d2236df4f4b9c91e7dfcfb35b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:49 +0000

    say into operate lost free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
