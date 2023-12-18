a709b96b9d7ca1e35215f604d49ae1ed36ac9d74
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:44:52 +0000

    bar attempt debt sight defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
