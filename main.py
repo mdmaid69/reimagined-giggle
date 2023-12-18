f227a7d377d95432cbcb9ec625b25adec4ac6356
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:11 +0000

    hollow shut go grow persuade

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def sort_numbers(numbers):
+        return sorted(numbers)
