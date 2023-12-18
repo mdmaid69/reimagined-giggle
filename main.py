b27fbfec64ba1fb8d4392f010c165c9e72ae3537
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:45 +0000

    nervous comein protest only seem

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_palindrome(s):
+        return s == s[::-1]
