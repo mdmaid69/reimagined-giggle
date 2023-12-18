cdb8685cbc7d20a07cf02266920fed9d509016dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:05 +0000

    curious interesting central mean dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def get_array_as_list(array):
+        return list(array)
