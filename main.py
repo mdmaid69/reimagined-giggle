81762ea8e7802e3d9e9a767b28e07517013d4903
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:50 +0000

    hold Oo hold know absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
