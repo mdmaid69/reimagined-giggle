9a4e7397d4bbd16114aa2aefc4271caa93270ac3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:42 +0000

    scare congratulations character lesson embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
