dc7ea9d05bc06f936cabf23745bdf1c38abbc5ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:57 +0000

    godown fierce getup nervous comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
