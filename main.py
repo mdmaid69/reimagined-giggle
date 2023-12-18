9d6de1dec0a3fcb20f8b05638d93cd737fdfeb93
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:59 +0000

    impossible government intend lucky influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
