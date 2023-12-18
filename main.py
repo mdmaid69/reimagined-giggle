42f2946f4aea3f0d2af5a4d506d39a53c016ab3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:10 +0000

    sweet never exploration provide conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
