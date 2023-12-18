c1fd6ffee482dc1d49a3d5e04b010d7def3e61c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:44 +0000

    ill onto cross pay edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
