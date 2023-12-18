e9d1df5eb5e0aa96c0fac9c5e75a2d8f53ba1289
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:36 +0000

    badly knock present share often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
