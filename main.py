2c5b1fb6d867edcf1bc984bb17c21cd1d7aca2e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:03 +0000

    fault stop drink point motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
