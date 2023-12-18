460e770b8af45c7bad7606ce05ba009664cc01af
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:04 +0000

    spin coat guess small save

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
