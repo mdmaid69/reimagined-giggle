2bc52a908c70a2425fec30dd734bc245a9b16009
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:38:46 +0000

    anybody block hard Jj middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
