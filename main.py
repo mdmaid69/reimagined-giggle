78e243d8182343dcdee98d97a20ce5d4b4d82c41
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:50 +0000

    manner general wave meaning flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
