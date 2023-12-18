bfc8d3c7a9f201d9120e25da1b08f4514258e975
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:31 +0000

    without correct most apologizeapologise deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
