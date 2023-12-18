1bab9c58a1acd29b1327bc2ddc005ac9f361a606
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:27 +0000

    winter mind production work act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_bool(array):
+        return bool(array)
