ef4d7c583da198ee12d20fb8bb70e98cb1701d61
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:43 +0000

    responsibility example shut confuse cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area_circle(r):
+        return 3.14 * r**2
