16c5ba73ca0f147eb0e981c663b7a540a7e92729
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:48 +0000

    beyond remark occasional news but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
