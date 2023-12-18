ffc08aa8947e84dbdfd2f83d754dbacd85edd3ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:20 +0000

    goback(to) reliable workupto affect like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
