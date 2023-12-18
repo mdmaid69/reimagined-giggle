e6346cf010d75695d95915c7b34200d88b2a3ce6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:48 +0000

    central runinto relative goon rate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def cube_number(x):
+        return x**3
