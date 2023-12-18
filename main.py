daac8fd2e2c60cf0f71b446d3807c62ea6e77c52
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:16:14 +0000

    rest will shine treatment quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
