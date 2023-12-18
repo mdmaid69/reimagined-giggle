f3490577e22b56aa549c92b54e513cb960bd60e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:45 +0000

    appear argue where skill sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
