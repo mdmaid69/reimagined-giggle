8af1b0ab3039f974dcef15eed7036a4abe5c9b96
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:59 +0000

    anyone hurry possible shade famous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
