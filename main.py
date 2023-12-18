bea016ef1856e66b65bfd24066af11ae336476d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:31 +0000

    swimming wide earth recover body

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
