fc566b65191139abcdae09bdef6be888a401d737
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:28 +0000

    close equal confuse history frightened

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
