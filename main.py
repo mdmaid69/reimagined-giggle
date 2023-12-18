c8f65414989ed8ae3cb0068168d8532830fda9e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:30 +0000

    formal suddenly entry thirsty dry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
