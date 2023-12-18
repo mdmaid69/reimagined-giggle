2ce2b4e9cde76d8044f0a7c9152064cbc8fef221
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:01 +0000

    keepout holiday slide delicate difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
