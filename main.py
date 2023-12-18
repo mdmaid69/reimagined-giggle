fd680b4dbccddc58f63f8509b06d00c239db46fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:16 +0000

    late bound direction wherever programme

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
