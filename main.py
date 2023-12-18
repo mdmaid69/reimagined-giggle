87876fb2453d4247fb7eefae85f81b4e770f4750
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:17 +0000

    refer succeed pour Hh flow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def is_even(n):
+        return n % 2 == 0
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
