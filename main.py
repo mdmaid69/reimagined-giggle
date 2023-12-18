497bdf4afcd4ac6f6397d580ae6b082c41153ee5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:04:15 +0000

    kind bother rubbish area sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
