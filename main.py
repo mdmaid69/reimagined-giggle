87050fd966cb818583f83d6ec864490ffe773e9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:19 +0000

    event discuss Itself hard left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
