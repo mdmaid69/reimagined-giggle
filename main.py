522ecd54e64c7be459bb5c08bc648a10ddad8266
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:07 +0000

    light contribute commit pattern human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_max(numbers):
-        return max(numbers)
+def get_array_as_complex(array):
+        return complex(array[0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
