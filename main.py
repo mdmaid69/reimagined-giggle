0970ce0c3a70ed505806c6b88966bab8c34cf258
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:36:02 +0000

    or pin gap dig one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
