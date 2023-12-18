7fc29130a505f7e025b4cb69f90117da9b83fedb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:26:28 +0000

    feeling person scream bring black

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
