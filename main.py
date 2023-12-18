0530b59dd20f88938815e8f9a089adb93d66caa3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:30 +0000

    shoulder until peace along want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
