8813e3bd4834c48fceb48ad71348e833b10724f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:34 +0000

    under walk spread spend pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
