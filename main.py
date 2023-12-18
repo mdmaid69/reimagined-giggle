b6b39e78f365019d02af75b557a5653479e4ce6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:16:20 +0000

    promise bar ring nature violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
