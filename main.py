c5ac0b98aee431840a287d376150efee451b073d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:24 +0000

    deserve weather indeed familiar gooff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
