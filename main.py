2bbc5dbfdacf2df144338cf50d1d87ef8ee98c74
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:55 +0000

    mass us reach run thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
