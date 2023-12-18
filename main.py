5d9b3fd76d7d119917d81a6dd9808269da511c5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:07 +0000

    noise left cheek through really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
