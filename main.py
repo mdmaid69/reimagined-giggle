55f9366a2f6dcc3fcc5f988e7f65a6e3e5ce69a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:01 +0000

    basically unlucky general introduce ourselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
