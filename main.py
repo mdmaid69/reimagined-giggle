b7e163d3bd2e589d2a9407cb720baa6e25534564
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:50 +0000

    miss people war inside confident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 def calculate_force(mass, acceleration):
         return mass * acceleration
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
