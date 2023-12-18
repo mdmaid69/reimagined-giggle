779a7a77dde3bc67f9c00c3924c53652aec70261
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:21 +0000

    from tape contents various harm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
