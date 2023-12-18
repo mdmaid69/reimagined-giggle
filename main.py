08a7f36e8a1d305b361cad4f313fc3bde80989b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:13 +0000

    swallow listenfor always past above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+print(os.getcwd())
