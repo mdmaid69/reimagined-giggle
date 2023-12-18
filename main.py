073e78d9962c519905176d4b9ea3e98e9255cce8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:11 +0000

    rough wash pause kick mine

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
