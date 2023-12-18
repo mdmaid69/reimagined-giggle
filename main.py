df6898553634806c049c75ef6d43efdfcb223d60
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:55 +0000

    sink when damp write listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
