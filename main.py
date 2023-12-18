bb78129569911ea4ba29fcc84b01da77f2e60086
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:04 +0000

    full bound direction constantly exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
