b9c77ce74c07f568efb79f9655763d56ebccad6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:26 +0000

    legal final scream order file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import os
-print(os.getcwd())
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
