f4d74e04ff2e920c103d463019b7065c9ec4437f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:50 +0000

    track imagination chance door freedom

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def count_elements(lst):
-        return len(lst)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
