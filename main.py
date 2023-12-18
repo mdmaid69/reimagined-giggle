216ade6e55564f1ec28824fff5b8561be8b37637
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:48 +0000

    you ear principle group underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
