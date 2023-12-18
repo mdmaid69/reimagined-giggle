cec0c82e29551f0195ed71c44d36e57078fd8293
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:27 +0000

    proof confused next confident breakdown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
