a78efe52c01da3d09c65075543c9fc993253c227
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:55 +0000

    hide evening move yourself pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
