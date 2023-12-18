f87df80e818be3c83afc19b0b9fdd3fbe57c7682
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:24:13 +0000

    build ashamed Vv art hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
