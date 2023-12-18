0f95d203deec8c5a3a3763289dc6df71db8e4543
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:34 +0000

    pity character give female fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def count_elements(lst):
+        return len(lst)
