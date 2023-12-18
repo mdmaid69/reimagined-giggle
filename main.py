c4956591e64e0232bca2439b2a223a2ddee3cee5
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:19:45 +0000

    second law set stamp belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
