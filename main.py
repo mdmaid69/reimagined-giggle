69811dd129ab720a2c20097fe2ccdc1602eda356
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:16 +0000

    scheme test file appeal sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+print("Hello, world!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
