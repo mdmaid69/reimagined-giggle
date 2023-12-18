a58f639b4ed806eaa64cc6e41610dc68f6ec1c00
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:06 +0000

    float basic squeeze cinema replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_power(work, time):
+        return work / time
