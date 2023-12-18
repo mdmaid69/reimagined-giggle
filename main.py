20d171ab8d667a113492a2d97a746a6903307129
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:04:17 +0000

    wind area raw Ss crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def exit_program():
-        sys.exit()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
