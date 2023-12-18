12caacf6be7c3590c311c70cd13700fdd60e79f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:45 +0000

    country border callat exchange properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_as_int(array):
+        return int(array[0])
