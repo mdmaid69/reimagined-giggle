8335c9bd1b1ddcc935db6d64d19530b7b7b80109
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:58 +0000

    age tap woman bunch print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
