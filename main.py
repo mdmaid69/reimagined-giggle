268fb6d77a9da60fad651d804b74694b5b9bf03f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:18 +0000

    centre sentence paper arrival truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
