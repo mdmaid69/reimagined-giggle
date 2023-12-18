1710f913613a29939ce98e500c6da3a11645e284
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:42 +0000

    reach conduct hurry contribute thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
