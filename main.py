e2534f7bb619fd7178aa12b45d43df87e4f2041f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:59 +0000

    comfortable cool cover speech 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
