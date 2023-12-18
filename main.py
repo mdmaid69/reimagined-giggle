0126f1ea91de5daf6cb70d9f7aa9fda7ea641905
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:38 +0000

    makeinto thanks inside one quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
+x = 10
+y = 20
+print("Sum:", x + y)
