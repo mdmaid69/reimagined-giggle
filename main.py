cff75d67f1e33bf342240ec2e1e61720d6d6c09d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:00 +0000

    pen failure her strict nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
