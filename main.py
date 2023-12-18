5ae2e244a56b06186a9ae46141e4c1ef92ed813f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:00 +0000

    list breakout Kk borrow dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
