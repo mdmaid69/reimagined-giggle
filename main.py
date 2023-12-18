ffa4bd6bb0fb3b4c7243569b5e82e31971ce53f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:01 +0000

    authority ill drawing honest complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
