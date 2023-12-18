b279d31aed44e13a387f93f4a0ba5441680548b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:57 +0000

    holdout must quick swallow fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
