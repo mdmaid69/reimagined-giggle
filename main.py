aaf9803f6c302fab3b8034a9b5b905fa76bc244f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:37 +0000

    remark frightened distant spring worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
