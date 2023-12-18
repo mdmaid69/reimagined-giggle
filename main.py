47addcb9ed71b2fecf0b77a104a6f776f9394090
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:23:38 +0000

    several peaceful unless space turnover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
