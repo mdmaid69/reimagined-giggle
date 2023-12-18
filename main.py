3b6f494897e8781d405afc9932412d4f4c1bae62
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:07 +0000

    condition flat prove persuade cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
