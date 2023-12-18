a0a54df31a4ba6ce058c122cbab5b9a1065b9e5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:51:33 +0000

    long chief bill sit change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
