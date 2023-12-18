1d8ccb4694f51018840aa95dea3f28813cdcb300
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:53 +0000

    disappointed wrong goback(to) radio getup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def reverse_string(s):
-        return s[::-1]
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
