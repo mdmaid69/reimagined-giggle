88e37d63ed4612340026fd3b0108e7c7715573c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:39:01 +0000

    will wipe into trousers splash

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
