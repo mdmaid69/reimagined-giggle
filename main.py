41321679686b077b5ce139c2aaeef3407afc3275
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:10:05 +0000

    argument ticket off sister rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
+import array
+def get_array_item(array, i):
+        return array[i]
