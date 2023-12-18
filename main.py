a4482f4a66f8775a3b045932b6f4a1807e06e981
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:59 +0000

    belief somebody blow precise respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
