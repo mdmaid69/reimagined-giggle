d5704819f34558706cf97f0794b611b3644c63ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:46 +0000

    worse there anywayanyhow wear wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_power(work, time):
+        return work / time
