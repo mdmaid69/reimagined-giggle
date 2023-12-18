ebd3329b263646c975c3900ffdbf100bdf336908
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:08 +0000

    Gg pretend fight sail sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
