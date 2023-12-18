29fb6de3212fbe59ff0fea2ec79d216f0f352177
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:19 +0000

    flood traffic fly language opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print(sum(range(10)))
