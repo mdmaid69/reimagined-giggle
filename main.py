75db9f95a79821d13de7a3dceef89156f323efeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:56 +0000

    actually uncertain car sit century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
