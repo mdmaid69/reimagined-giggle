9efc147cd0de72b090f15a15864743aaa79fc7b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:58 +0000

    goup programme gentle admit fire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
