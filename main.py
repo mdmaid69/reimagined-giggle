3959fada4f2e22500e72492e20cb3b7753c48639
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:46 +0000

    container guilty lay sticky shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
