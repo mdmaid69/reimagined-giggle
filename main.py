ec972c6dc3770f0be9920609b8c5cdcb884fd15b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:24:12 +0000

    natural edge hollow authority turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
