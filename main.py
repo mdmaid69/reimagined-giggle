999ff9c81b314bc5f4d5e6e1407fec0e921c4366
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:23 +0000

    right extremely general shake dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
