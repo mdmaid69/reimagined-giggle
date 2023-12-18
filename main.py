63d049f3e788159d2055d41b4fd0275f67d923b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:40:53 +0000

    offence past sorry corner hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+print(re.match("h.*o", "hello world"))
