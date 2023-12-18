0011659cd7b0ea1a4ab1eddabcf1fbce4f9def6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:43 +0000

    closely match spin quarter wrong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
