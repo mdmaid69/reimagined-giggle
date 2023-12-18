eded49a6e2ea0d00cd34b49d6dc18745be989560
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:04 +0000

    hour yourself avoid storm house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
