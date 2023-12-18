255094b963d393f9c7dbe8246f4433f89a1cbc5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:31 +0000

    open mother build clear passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
