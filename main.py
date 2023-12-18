532ff28fd8f4c47b5b7d896d224dd97b0ee0c399
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:11 +0000

    first report proof suit interrupt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+for i in range(5):
+        print(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
