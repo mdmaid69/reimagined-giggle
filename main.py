8680261c88cac4c0ba48bfc6b6fdc90fc68fae66
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:10 +0000

    slide chat breakin bringdown position

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_array_index(array, item):
+        return array.index(item)
