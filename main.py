503d02f06b2b9c54610641b41377c34b07d97227
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:04 +0000

    unable Ww afford capital undo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
