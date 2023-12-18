7fcf41418a0546297a4219c40c55dae6c69089f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:13 +0000

    sleep unlikely measure anybody raw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
