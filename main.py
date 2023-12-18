f1ec815f8d444fcf02e06514f8f3c3844375605f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:08 +0000

    show purpose slice prepare criticism

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_square_root(x):
+        return math.sqrt(x)
