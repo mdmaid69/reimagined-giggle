a486248baea5008898edec56421d7341be01bf7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:52 +0000

    after cope obvious keepfrom finish

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
