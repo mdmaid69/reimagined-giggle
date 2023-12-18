7fb1d0747efbfe8bd4cdb57c385eb7f008f5b6de
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:16 +0000

    walk breath immediate hello turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
