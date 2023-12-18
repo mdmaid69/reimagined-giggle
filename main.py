6f7bb9d1031ae8804fae3ce05f6898d338b4e449
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:38 +0000

    cover swing belong holdup scene

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
