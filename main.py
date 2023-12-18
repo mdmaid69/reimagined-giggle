9caf04aa31e4b82474086e8763cb384dcdded68b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:33 +0000

    murder hospital image regional accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
