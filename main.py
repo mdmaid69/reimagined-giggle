3f10c5e1b32a60c14253b6d58207f4562286195f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:31 +0000

    listen bad ago thinkof the

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_acceleration(speed, time):
+        return speed / time
