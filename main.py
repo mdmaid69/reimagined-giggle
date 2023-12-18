aa02ac11cc38658f027035fa0a7a8cb2cec922c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:09 +0000

    Itself single several ordinary land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
