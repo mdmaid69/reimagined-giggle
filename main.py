273f40aa4dd2885d9209c19afff0dbecf1a7ae34
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:03 +0000

    taste patient march demonstrate roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
