e76b234311c34273e129adbd1fb5724246333741
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:13 +0000

    frame cold rent state 566end

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def add_to_python_path(path):
+        sys.path.append(path)
