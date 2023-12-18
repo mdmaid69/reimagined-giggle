54cc94ae555a2931ae7718612fdf7eb21a3fea91
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:20 +0000

    put active so issue drive

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def find_min(numbers):
+        return min(numbers)
