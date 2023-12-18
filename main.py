c2faca83bcf1d57e2d484ed8f83d3e6209ef217e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:17 +0000

    agreement left 569enjoy stamp separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
