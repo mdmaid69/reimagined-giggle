f0ec9addd6240a038741eaa2a8ac402ba9f62b8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:35 +0000

    nice extremely especially sight like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
