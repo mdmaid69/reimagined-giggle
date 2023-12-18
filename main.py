2e1b5d5cd79f926c19d8cd5a8ef1fbeaa427aef8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:15:49 +0000

    cap giveup immediate danger equipment

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
