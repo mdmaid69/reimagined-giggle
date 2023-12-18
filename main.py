e467e69a24a68f0cf2bbe17396e567caeb29c93c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:58 +0000

    rich food warm unlikely turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
