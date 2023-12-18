876d399e3c40274c7d862a96006efd53eb4ac791
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:27 +0000

    bored music crazy settle Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
