d8d9968dec161686f5b2d339aa72fc45509065f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:40 +0000

    today challenge provide at match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
