c2d8f915a57f7975eded72f0a9c102d05c4144b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:11 +0000

    view cover imagine maximum broad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def check_if_array_contains_item(array, item):
+        return item in array
