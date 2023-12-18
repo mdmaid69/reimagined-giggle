5296503b6c3b587ed62313f1426db7c5412ac4f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:24:27 +0000

    sail by punishment border open

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
