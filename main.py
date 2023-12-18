9c5b26b6857ef07185220fec88c5b0a192a1c753
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:51:00 +0000

    strike tap stamp wet sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
