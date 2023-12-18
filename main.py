1593bf8a5c105ec8ede5aae3ee7801db95edda16
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:10 +0000

    die record surface process holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
