1c9c0f689a18e0c53d1c2b37558b9ff460413255
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:51 +0000

    distance march mean add turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_min(lst):
+        return min(lst)
