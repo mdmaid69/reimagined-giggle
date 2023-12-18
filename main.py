32fa679ce37ed4cfcd37580f2cd4b49b30012cc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:58 +0000

    direct transparent reply lend twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
