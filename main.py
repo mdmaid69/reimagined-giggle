2879839d1b463a366c2fcc13cae464d4688901f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:58 +0000

    play union accordingto complete connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
