7342c5822d0f7703000ba2ce882f059aa9819a68
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:27 +0000

    but frighten estimate base not

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
