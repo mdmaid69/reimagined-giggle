b646eb985ef3713cb0afbc27b0648d192c786c05
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:47 +0000

    impression prefer somewhere inside fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
