803c74b38e7c58b1c4be20b5cbabaf20b1c3675f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:26 +0000

    send translate feeling list emphasize

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
