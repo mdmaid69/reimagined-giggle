b95cf2ee23f247d56d52df49dfe0c469923a79a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:38:59 +0000

    approval children angry runaway pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
