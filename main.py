bc92886fe47d5a42395321d38e4d2ffcc806c4ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:41 +0000

    challenge afraid exhibition mysterious build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
