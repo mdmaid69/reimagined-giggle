c5f577079ae6fe2c97e71b5f7a0c357a9f4969be
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:29 +0000

    apply in artificial spring window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
