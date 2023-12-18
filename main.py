1db32b060a29e97817773f7aa55c4bc0d48024e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:30 +0000

    relax spring curious happen match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
