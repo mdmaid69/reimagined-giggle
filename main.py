d3918249e307b93b99c5b29aa5b506caf13a1974
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:52 +0000

    recommend region price fresh forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
