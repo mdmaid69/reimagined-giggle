f42bb071ce25b2484a7837c6bb70760b2327f9db
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:40 +0000

    ignore language why spring cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
