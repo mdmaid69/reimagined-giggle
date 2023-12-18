b0f1b402b8509fac98c7babcf107b43e1970f0e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:13 +0000

    every description meaning contain however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def is_even(n):
-        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
