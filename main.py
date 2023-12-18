376cf1c31a981b71c1b67c9160786f4a2fa6e933
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:18 +0000

    refuse disgusting scheme have lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
