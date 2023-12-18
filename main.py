a3a5d53c7fde0f6408765dc1911c8aa200db1d8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:42 +0000

    567enemy accident pronounce journey reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
