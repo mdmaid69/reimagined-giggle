479f89ed48acc49edbbecf7807251773fb53ce27
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:55 +0000

    minute low consider comeout small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
