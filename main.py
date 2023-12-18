06e5eae0c04ddeb6fe0cd8462a4a086f301a6b5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:16 +0000

    band spare exactly hurry any

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
+def find_min(numbers):
+        return min(numbers)
