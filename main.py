962b8a7e06d96af6d044cf35e913a77dc0a2ed7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:41 +0000

    relationship fetch measure less software

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
