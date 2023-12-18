b926166bce839b0003f42480313674169b5c2715
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:48 +0000

    rank Jj unless hope common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
