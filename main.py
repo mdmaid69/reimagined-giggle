be60015ff42cdf2ff7f695ca7bfec3ae73cef983
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:24 +0000

    introduction agreement tip transparent visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
