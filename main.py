0bcd63cfdf7876dbc629a6e98886302bb665e158
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:45 +0000

    pin bottom dear seat appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
