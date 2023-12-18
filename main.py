c9c4ac1e4bfe73525894c2487267967d79477e67
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:50 +0000

    declaration trip legal abroad attract

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
