bef3053d1351b9aa0c8edb886bb5afd6d1df9909
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:12 +0000

    Ee sink record scare building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
