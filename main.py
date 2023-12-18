a6e1ccb3275925a60d69e5e33caf6435116ed096
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:20 +0000

    rough major perfect rule over

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
