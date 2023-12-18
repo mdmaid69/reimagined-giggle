9a9aac6fe8dcf568b95d18f043a8c52ce8e85c82
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:48 +0000

    holiday neat correction my doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
