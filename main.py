c7cb73a1794bedfc837c75bfc0367ce7f42f9720
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:41:05 +0000

    reply people side just unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
