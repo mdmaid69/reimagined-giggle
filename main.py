70cad17dd8c5d67be7bbd9252a66c5c975fd64ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:59 +0000

    trouble ear country look side

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
