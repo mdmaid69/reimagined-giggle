8bdb1bde630c776c87a5316deea02456d1859d20
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:07 +0000

    same challenge square birth content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
