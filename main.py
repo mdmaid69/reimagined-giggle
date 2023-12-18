43428fa2769abbd1cff30abb838e85320f70592c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:40 +0000

    confidence clothes burn butter settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
