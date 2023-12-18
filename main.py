95c7c762b6cfef1967ba5c4689442716b068cbec
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:19 +0000

    interesting vanish wish favour week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def convert_to_hex(n):
+        return hex(n)
