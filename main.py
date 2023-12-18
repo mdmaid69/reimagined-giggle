116fc7427439e41f7e587748e317daf4106ae32f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:49 +0000

    pick nature eye environment us

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
