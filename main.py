1c463d192c567fa71979b80381a14b95055d9eb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:59:46 +0000

    bringdown steal average capital excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
