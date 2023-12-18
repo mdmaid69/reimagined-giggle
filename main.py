c114733cde36e222694baefd09e1aa9f7569e646
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:03:50 +0000

    minority wherever accident male otherwise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
+def find_max(numbers):
+        return max(numbers)
