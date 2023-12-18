98c7f4df4a47eecfd2e128454fc7dba38d036bfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:17 +0000

    belong contrast runaway regularly takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
