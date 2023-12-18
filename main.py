d926d01643d4be72ddc49ab5e5336fbdcb1fdbfc
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:21:55 +0000

    about move official cover healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
