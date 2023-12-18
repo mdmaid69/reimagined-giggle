f463d8bb59b77b9340cfccd68cd34683e5bb7f8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:00 +0000

    ask sense belong major whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
