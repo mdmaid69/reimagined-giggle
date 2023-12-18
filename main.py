970c301bed96e94fe72b029a16410bfa74a0589b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:08 +0000

    front approval steady knock like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
