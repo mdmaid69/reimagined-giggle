eb2708653f9dc1a2419f45cd2107ebb4a08be2b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:38 +0000

    similar insist step scale scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
