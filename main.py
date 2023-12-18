1981679e5ce8386745ec1c9101388f7bf7b2eaa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:37:51 +0000

    to invitation back sensitive object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def reverse_array(array):
-        array.reverse()
+def set_array_item(array, i, item):
+        array[i] = item
