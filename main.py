ef0bb5ac9f679b7d7e59ed8b5a3207467d2fad3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:14 +0000

    gather failure generally translate division

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
