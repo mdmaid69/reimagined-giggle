8ad3a74c2d44fe68dabe9c2d4f6bfb14e32f58ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:40 +0000

    grand cheap once practical expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
