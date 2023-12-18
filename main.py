dc5c36d0d479ef3c7b1e9ce9bd2ce850f48f9d75
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:24 +0000

    effort really lead message centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def convert_to_binary(n):
+        return bin(n)
