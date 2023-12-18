6068f702cab3cc24a90d99eb57b951ab98a399a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:36 +0000

    taste neat yours respect familiar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_max(lst):
-        return max(lst)
+def calculate_work(force, distance):
+        return force * distance
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
