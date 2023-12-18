f02723d8cc164e99b62ea06c24a4b796adb96a7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:55 +0000

    arrival as mess house pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
