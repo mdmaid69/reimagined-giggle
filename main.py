edc219fb423d51ac00fa53a49bec7082f5a81889
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:50 +0000

    age issue everywhere out wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
