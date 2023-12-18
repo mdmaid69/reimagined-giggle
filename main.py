e5f8c84d34cd21eccd76d3295f873eec45458fd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:09 +0000

    loan shoot scare wear win

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
