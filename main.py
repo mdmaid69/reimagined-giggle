1bb8f0662718dfc7fdafb9e4acf199895a39e0c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:47 +0000

    left approval thumb constantly squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_list(array):
-        return list(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
