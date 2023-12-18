fb0563c117b8b1e7bf8ad7425167bcd1fd56b96e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:55 +0000

    message present whisper long cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
