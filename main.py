339978be2cd4f02327b0b8dfacc849357489f1e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:01:11 +0000

    runaway stamp plenty giveaway should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
