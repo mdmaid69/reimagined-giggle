43224ce882b4769de6c63d736bed6adf229aa23d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:17 +0000

    half fix frightened earth few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
