2277e348905e2fecbbb23bc6d68d15c524c2b164
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:30 +0000

    inquire flat clean wonder giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_pressure(force, area):
-        return force / area
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
