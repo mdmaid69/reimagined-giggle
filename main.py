80f856f3c2ba71db7e675f4d4d69b31561c62a97
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:53 +0000

    film house sky trap hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
