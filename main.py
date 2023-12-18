fb3a4038b1fc0d44bfbc5363db4c82b2b3ca1b20
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:30 +0000

    treat brown unfair precise performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
