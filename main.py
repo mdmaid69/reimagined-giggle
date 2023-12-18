7c84efe7af17ad1e9d301be4b0b39d81bbf1c70c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:33 +0000

    creature protest steer respect region

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
