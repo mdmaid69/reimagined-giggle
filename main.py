089ebbcb8a48cd92f5989280882d08d3bde6278d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:01 +0000

    past throughout trap part split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_even(n):
+        return n % 2 == 0
