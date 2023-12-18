59256398f842606ee5553a5c45ac413f47c306b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:11 +0000

    slice face tool sign handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
