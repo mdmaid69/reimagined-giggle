a95cab557ae47220a5b84863477c731b8e5b18c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:00 +0000

    young under result bury thinkover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
