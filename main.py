df295782d95ee546277bdf885be7dcdc1c46d964
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:55 +0000

    always surface lot comment hope

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
+def add_numbers(a, b):
+        return a + b
