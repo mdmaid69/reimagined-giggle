11e718ff9920a5e426379a100670109d6f6f7b84
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:55 +0000

    official dark heavy responsible story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
