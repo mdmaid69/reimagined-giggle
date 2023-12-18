656c8c500ada76830a1f1ba1145f9e49a00ad2c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:00 +0000

    comeover steam extra home its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_palindrome(s):
+        return s == s[::-1]
