06eae7a604faab54d40b80d9a6beb368e4151d07
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:05:06 +0000

    attempt finish responsibility copy happy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
