746af30a05d9302f77e1839f60160ef77a97ff30
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:23 +0000

    use rub gather instant born

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+print(sum(range(10)))
