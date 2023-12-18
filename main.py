f5dd663019af6be4117b850ead639a784ee6eef9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:15:49 +0000

    place basis fall fine hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
