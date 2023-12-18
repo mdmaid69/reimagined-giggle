c47b2ddbadf12fe7fb4ba850886880853cc61dc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:34 +0000

    between light spread accept rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
