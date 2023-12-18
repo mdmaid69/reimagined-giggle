badb0ee4b20de4af36b515e3ee995a2153d633fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:04:11 +0000

    remove soft repair star rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
