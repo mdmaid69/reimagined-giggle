5cfd5f0856151132e82d908fdaaf2187325dc9df
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:49 +0000

    meeting stage game arrive backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
