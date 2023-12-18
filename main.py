550c610da1b764498d8d98c8e177ae19e2416c47
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:06:14 +0000

    we suppose track switch birthday

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def multiply_numbers(x, y):
-        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def reverse_array(array):
+        array.reverse()
