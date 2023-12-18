678f918780c356000c36f35629ee0cba9bd828ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:34:48 +0000

    talkoutof branch reach talk for

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
