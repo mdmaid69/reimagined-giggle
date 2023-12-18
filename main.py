4262ee8eeab2ff44690bb8b9ce8f0edcb69fea85
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:17 +0000

    expensive influence 565end else floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
