58a75e14b976464206bd588e0aa2566a287be7f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:45 +0000

    free meaning say several speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
