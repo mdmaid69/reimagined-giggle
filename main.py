9ea909209a1a85b4957a365b0ec89f2b088015fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:14 +0000

    left free honest message card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import collections
+def create_user_list():
+        return collections.UserList()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
