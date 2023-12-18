43b37bed61da4bc7f2fd38942d18b74085a68470
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:01 +0000

    either Yy wave shock day

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
