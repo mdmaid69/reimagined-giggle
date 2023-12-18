9eb35a8264d013bf80a06c8798961c42c199e075
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:21 +0000

    Ff regular warm bringback desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
