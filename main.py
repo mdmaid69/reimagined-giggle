3b01d68cbe7e18c03e04d9fdfb449da93c389faf
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:55 +0000

    standard mark like vision hope

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+print(sys.version)
