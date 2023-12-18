44573e079bea3f0ea46d04bdf9a33796a146bee1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:43 +0000

    partner cough emphasis offence present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def is_even(n):
-        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
