25d96421a1638ae4543e5e89386de3ff9a8bb8c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:04 +0000

    straight fight value polite paper

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import time
+def get_current_time():
+        return time.time()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
