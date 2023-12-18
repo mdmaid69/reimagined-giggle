b7c9f0b14247d8b58643c1dd5a469a406ca19c1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:50:14 +0000

    commit bet holdout hope ask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
