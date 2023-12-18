71ead95cde8b6217471acc33083c102352c1581c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:54 +0000

    keep his appeal eye able

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import platform
+def get_python_version():
+        return platform.python_version()
+print([x**2 for x in range(10)])
