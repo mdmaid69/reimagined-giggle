f94e1af932639ff7221abb6e8a6f3ad723aaf3dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:31 +0000

    her hour final basically coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
