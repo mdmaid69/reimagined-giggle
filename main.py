70c8f987d0965d165b4c103a28af905171f78c56
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:29 +0000

    community priority treatment goodbye careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
