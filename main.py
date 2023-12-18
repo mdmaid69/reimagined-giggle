620ad804ed067a74a31476ab90123dd764c4e87d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:47 +0000

    taste gap scream if university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
