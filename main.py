c97806f410312fb6ef59849eaf7252bed52d33c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:34 +0000

    comment cut string appearance add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
