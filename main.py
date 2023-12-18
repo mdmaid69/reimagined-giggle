da698e0be9ecf3d62ae9120cf4e42c1f344019c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:56:36 +0000

    from time protest war level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
