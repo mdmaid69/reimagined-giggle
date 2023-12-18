450c5e4fb70a9b027eea9ff7e48b47638cf156bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:48 +0000

    over exhibition fine desperate breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
