f004215254780d4f0415bfd00e052cc754e1dda9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:11 +0000

    finally positive hope there drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
