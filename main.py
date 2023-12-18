c6dfdfafa41279042beaf30a3999d054dbb196a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:06:07 +0000

    expect room business already over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
