2929839ab217dab510e61c581a9a653586d002d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:51 +0000

    whoever choose store typical large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
