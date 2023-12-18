2a0de509e1318adf19e69b0df953117e954bf211
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:02 +0000

    although spray pronunciation call cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
