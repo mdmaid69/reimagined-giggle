fe4632d11e805fc55432d4c647373654a561097f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:32 +0000

    will race person whenever class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
