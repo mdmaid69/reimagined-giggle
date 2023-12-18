e8c500d14ef382a8b0eff55bf8c88ad21600a215
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:00 +0000

    murder market rank proud defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
