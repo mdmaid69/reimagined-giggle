287ef1db07626f48af455d2499f8aee87b76f936
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:06:52 +0000

    close strong talkinto heat makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
