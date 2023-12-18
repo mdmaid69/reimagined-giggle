e74b568368bf0c87da9f1101723382c7130d2f23
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:14 +0000

    search die why could complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
