e03b9a07f6c98d536e9977ba6a0bc3e1321636cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:20 +0000

    watch charge period quantity friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
