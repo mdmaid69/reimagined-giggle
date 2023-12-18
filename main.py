ea7771019438316661e8efa0b50eab871db096c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:44:37 +0000

    draw once bringforward both again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
