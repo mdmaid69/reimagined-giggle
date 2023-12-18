6a797639e29ddb63c51ee0ce316c7a84b1ba50b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:12:00 +0000

    minor school strict taste similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
