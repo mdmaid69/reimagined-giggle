b09345db334214adc3af06d925b9e303a44f27cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:01 +0000

    complete sore many different pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
