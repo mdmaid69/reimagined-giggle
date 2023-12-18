86ae4587b95b4789f14d7f4bcac973e51672aca5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:55 +0000

    plan type direct talkinto language

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
