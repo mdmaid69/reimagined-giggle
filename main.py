42a94f90a91cc43946aea78d32fb40195a4eaeca
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:42 +0000

    hardly guess label surface exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
