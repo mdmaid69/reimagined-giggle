2206e74d8e70313c8ed79b7906b1ff60330d27eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:12:30 +0000

    attention spoil occur patience miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
