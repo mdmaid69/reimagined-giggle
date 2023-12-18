618aff3626f897ca78bfa661dcbc4a0fcd7f7d85
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:40 +0000

    general trade broken insist teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
