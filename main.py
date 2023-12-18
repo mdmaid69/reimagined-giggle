e23e51952c9644c71fb80f45be82e2f3ed414ff5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:10 +0000

    confidence because myself get now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
