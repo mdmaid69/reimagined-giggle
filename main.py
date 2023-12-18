3ccf49cbea040ca91fee623c7254d8a30cfc546a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:43 +0000

    Jj regional far quick rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
