b523ab9f62717de48781d192b298b66b5e002671
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:03 +0000

    piece shoulder favourite replace minute

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
