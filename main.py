59a2b84a9d4b394dda07b4be7ab7ad962a5f3d17
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:09 +0000

    disappointed waste Nn maintain clearly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
