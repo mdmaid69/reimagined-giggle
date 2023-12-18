c0b0b26cf1ea861c423d842ada587fe31596ea82
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:51 +0000

    clean collection murder callup burst

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
-def divide_numbers(x, y):
-        return x / y
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
