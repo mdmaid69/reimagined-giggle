e9c4ee921f232b13f2b30c23b766e51d00602c0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:03:15 +0000

    interview see harm port because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
