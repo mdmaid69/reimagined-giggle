be29e5873791d267de2afe3d7dc4b7c41523da1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:22 +0000

    search course tooth character wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
