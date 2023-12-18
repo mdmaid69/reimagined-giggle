d10d5b5d98ef20f9cfc8f90a206891f1a729341b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:24 +0000

    paper 571enough avoid how whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
