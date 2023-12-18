31e5bceacc0f801ae9be07820067c743022c7bea
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:57 +0000

    list sign world door operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
