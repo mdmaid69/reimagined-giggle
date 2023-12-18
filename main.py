57a6d23c43e2a801b8f8e904b817688fff544dd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:31:16 +0000

    whatever sound front damage stamp

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
