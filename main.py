5c7dfbbfe1bc97b1608a6e488309932e8cd640f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:18 +0000

    keep disturbing OKorokay accidentally pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
