1df10b2ea314609e2d7837f4ae16caf1681b231d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:42 +0000

    goodbye well yes chief spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
