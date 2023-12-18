5553d113370a4a749881b1a7c6965ba6812d0ea2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:08:22 +0000

    appear getoff ever complain rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
