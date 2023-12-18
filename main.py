b7c3138e83ca1de803acfe1537e7987a8c8e20a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:13 +0000

    division have store full poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
