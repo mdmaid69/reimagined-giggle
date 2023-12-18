b78998afd3787b74ca9c77c3075483d947bb9c29
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:23 +0000

    represent price hers range direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
