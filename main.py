87300b94c5a101c7d8411bae79c51460a3af0d5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:23 +0000

    before shoulder shout underneath ride

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
