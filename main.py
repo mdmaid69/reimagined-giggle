7293786243f799cfe150083067ecc90f2eebfded
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:34 +0000

    manner uncomfortable pair Itself game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
