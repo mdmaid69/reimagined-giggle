eb6b934247014e7630f20c8bc80d8b3e62cd472b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:08 +0000

    cold relationship obtain among mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
