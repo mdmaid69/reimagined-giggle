16d99b959ae76c8751aa7d76ace6532450357564
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:36 +0000

    school slowly now holdon artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
