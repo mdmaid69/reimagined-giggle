1eeb8951edbc82233bb901a11be8b6e53cf5236f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:56 +0000

    weather Gg whoever include fault

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
