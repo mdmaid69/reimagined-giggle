44756b8fa5abd2bcb9d0b2614ffdf6ffccc23277
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:43 +0000

    dream manage excuse conduct exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
