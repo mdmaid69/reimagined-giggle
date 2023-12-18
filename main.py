70c8d0f1de6ebab59a107bd6a7ca967238d6f8f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:38 +0000

    wherever create you no sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
