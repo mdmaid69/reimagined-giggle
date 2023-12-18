2e95c08dac74833a6e13b7c52a55a85dff161408
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:28 +0000

    patient glass matter proper creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
