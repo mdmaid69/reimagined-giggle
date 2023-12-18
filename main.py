9694c4d355fa5f78bcb7fd22cf52c7a9c4b67c2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:09 +0000

    impressed brush clearly how neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
