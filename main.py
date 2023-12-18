ae677cb2e5ec467ccb7a9a12aec24eedabe57b42
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:48 +0000

    uncertain calloff operate figure cry

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
