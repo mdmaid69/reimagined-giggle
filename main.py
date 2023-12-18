0a235a5c81505ccce197a638df27a698ad01ea94
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:40 +0000

    closed attempt personal prefer correction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
