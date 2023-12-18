2f74537a89c5cb84242705107b198ccff1fc773e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:14 +0000

    body concerned bridge open meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
