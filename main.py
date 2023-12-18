b602a6927f36062f17e542a28f03fa3f8ea17371
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:28 +0000

    my whether accordingto face month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_str(array):
+        return str(array)
+def calculate_density(mass, volume):
+        return mass / volume
