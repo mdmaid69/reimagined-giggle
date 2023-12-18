edc05b67a31f8ae4f9530e0e94071b3c99c44197
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:32:04 +0000

    sell proud debt appear it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
