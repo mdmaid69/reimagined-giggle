07360d3b829824727d73488fbc1300349949ac6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:47 +0000

    runaway meeting mean office 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
