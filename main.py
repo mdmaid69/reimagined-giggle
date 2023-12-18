3aeb25bddfe3b06ef14b42f92fe0a531d38d78ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:20:25 +0000

    spite push state believe line

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
