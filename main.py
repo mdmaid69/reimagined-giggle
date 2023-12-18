e96cde538ddae74bda2a2218b6e3a29ecb64818d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:25 +0000

    office ours book aware hurry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
