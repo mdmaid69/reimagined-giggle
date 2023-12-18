f43b4e5d2d63a6d39ca7e52d10d157c0db6bacbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:55 +0000

    repair gentle call my passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def subtract_numbers(x, y):
+        return x - y
