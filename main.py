1bc11de817325e3bbedf5ad6d8c17129ea555d1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:41 +0000

    meal marry deliver takeaway steam

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
