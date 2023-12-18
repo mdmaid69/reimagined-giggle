1e36647d461fdff8f6f22d012f9508aaf3a9ecca
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:11:40 +0000

    large problem find miss bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
