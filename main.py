f687afca397002a2dbdbe50eff6f2ecec0dd964d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:22 +0000

    forget trick school boring smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
