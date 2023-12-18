73d6d5af688f684b0bb3bf53ccb51701b2ee92f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:50 +0000

    spite what sit face stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_length(array):
+        return len(array)
