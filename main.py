4f4023857585d334951930d0374f0ca9343d1cbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:47 +0000

    recover squash pay during visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
