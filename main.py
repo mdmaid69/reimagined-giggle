1ae7bb784fdc5ccef10af07617cd970234dcc41c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:04 +0000

    gather Vv test treatment heat

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def count_words(sentence):
+        return len(sentence.split())
