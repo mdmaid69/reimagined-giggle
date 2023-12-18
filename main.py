735786b465f5d652ce250f5e49a3cf720806c3b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:52 +0000

    though create hard story compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.ctime()
