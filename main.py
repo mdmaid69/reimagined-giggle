b19f35717a3b60209a4d77bca32f778a8b7d320d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:34 +0000

    tragedy pause blow money he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
