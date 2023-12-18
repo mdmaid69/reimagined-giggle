87988dec0372fff42645a6e9506e31ebf10c6a7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:27 +0000

    possibly shop possibly height choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
