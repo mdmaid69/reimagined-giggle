3ecab13ad601b392130872646cb2a0719d94d9ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:06 +0000

    read back replace travel yours

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
