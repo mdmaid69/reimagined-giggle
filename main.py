e99295787737cbef491fdaba540641fe5883130c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:52 +0000

    vision ground lookafter new tone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-print(os.getcwd())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
