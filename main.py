95a4007563d4e15a18d9a2870cbdd97543f0d55b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:32 +0000

    music there break favourite throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
