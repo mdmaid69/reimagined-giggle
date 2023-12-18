e006bc787efd30120864bc02b9163b8e19828fd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:37:27 +0000

    date near standard advantage depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
