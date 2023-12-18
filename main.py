1e398cec36836c4b43f1c7e7b1d7e3b8ec77a85a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:48 +0000

    rubbish tomorrow guess this window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
