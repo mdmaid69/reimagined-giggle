eb0ebf6f00880fb03888b6245db1a1752a08b557
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:03 +0000

    total estimate tap continue thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
