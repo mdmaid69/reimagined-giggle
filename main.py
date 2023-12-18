54c6d6cfb544a739302e5ad4e3d675459b8587ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:24 +0000

    bang door back repeat Vv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
