266b1c40ee5edbc6b7d2f92df0ef97abe2306611
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:09 +0000

    fear habit suckme pretend nurse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def sort_list(lst):
+        return sorted(lst)
