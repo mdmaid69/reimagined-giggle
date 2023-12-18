d083dbce34a063eb31405d1d908f7d8db17a922f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:44 +0000

    separate across fact line all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
