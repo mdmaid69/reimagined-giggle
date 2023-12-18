6778df3e28435ce706ce55aaea0d23cd5e037d42
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:39 +0000

    ilovedicks development quit moment boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
 from collections import Counter
-print(Counter("hello world"))
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
