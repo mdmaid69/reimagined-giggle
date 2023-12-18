13b024c85b510483da4c3d35bf45b370cee23352
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:26 +0000

    godown stupid raw then good

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
