88d2ae5d7f115a6c3f5331632b1bcbbbb9c54159
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:28 +0000

    want music mystery roof passenger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
