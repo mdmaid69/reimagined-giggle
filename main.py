91455273375eca89e33710a8b7a75ad2831b704a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:34 +0000

    before outside never grand matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
