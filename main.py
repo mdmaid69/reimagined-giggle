595155b36311dcc490b31157d3f61a93fe827628
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:30 +0000

    form fly past bad in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
