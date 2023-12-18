bd48856691ac68edafdc5610c73152b8e2083700
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:57 +0000

    scrap upset cough only holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
