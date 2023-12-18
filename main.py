f070d218f4e1b57430e9d891d6921730b3a0b791
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:22 +0000

    low much amount recover all

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_max(numbers):
+        return max(numbers)
