4faae77f094abe3fdfc4c508f22257916c398bb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:50 +0000

    next second experiment our stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def subtract_numbers(x, y):
+        return x - y
