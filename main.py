d8b5120c90680eff2a745feb69974ff0831e5127
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:19 +0000

    mark support aim bridge now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_power(work, time):
+        return work / time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
