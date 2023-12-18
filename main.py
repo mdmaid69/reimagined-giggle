6e5932595677dc4f1d6404485b107665bd341e1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:59 +0000

    pretend OKorokay control answer yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
