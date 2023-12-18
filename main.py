39dd8764561d28276a795983925491b16c78cce2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:56 +0000

    punishment lot improvement strength spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
