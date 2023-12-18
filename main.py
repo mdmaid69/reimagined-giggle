cf74721e689164ddd8766b89e60ae5cc084e6314
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:08 +0000

    uniform please clear bush both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
