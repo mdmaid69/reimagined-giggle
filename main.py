2bd88863c16a1623b46519522abd9fb6e479f59a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:39 +0000

    abroad close centimetre turndown will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
