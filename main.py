f68a37bab43a078f268ea1a4c60976daf3e52e88
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:43 +0000

    cheat surprise possession week noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
