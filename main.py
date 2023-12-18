2386f0c6e3a0d92f52f119468787b02edfe79475
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:27 +0000

    both flood calm plan draw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
