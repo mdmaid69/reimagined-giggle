30d2bf72999c7fbb9b31e256c13df4678e68c472
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:24 +0000

    beat ourselves confusing influence spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
