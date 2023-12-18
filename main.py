8a43f9fecfdc7bbd90fc1a7d14b47fb11807bf3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:09 +0000

    convince break unusual respect scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
