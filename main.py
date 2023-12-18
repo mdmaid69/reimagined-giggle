78cb67ebbc637c6092d21809c20187535c4839e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:36 +0000

    dream chase fat past face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
