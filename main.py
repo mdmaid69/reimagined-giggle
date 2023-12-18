9155d35ca40236a1d26c90671563a75564e3777d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:33 +0000

    less careful habit knock trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
