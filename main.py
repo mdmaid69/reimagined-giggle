f712db9652bb2dc14c1d460bb5bd8716f5188516
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:59 +0000

    tip band him welcome ride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
