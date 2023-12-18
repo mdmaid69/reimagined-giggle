d6a9a9c10a15bcadb04fbecae5c9c0c02aaa763d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:52 +0000

    find criticize present problem hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
