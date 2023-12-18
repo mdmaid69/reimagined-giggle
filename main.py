9ca2b1b5a587773c4a11c28bb171576642ffefa1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:27 +0000

    hold first crash beside comeround

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 def count_characters(sentence):
         return len(sentence)
