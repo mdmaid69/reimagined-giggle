3620951656efa6bcf73af8c1c624dc998f92cf5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:34 +0000

    nice urgent Ww that wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
