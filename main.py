8c2c161a9a168a99c7d5e18f81eef0b2552f3229
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:46 +0000

    sometimes letter dance degree petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sys
+def exit_program():
+        sys.exit()
