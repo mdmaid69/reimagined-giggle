254b30b077b493131d2f67915c9bf983b1a449db
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:31 +0000

    scream eat learn tidy shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
