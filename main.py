9b61a76409705826a5340a087dd5f46a7b83a87d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:20 +0000

    achieve distance watch giveback ought

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
