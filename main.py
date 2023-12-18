c02cb10c2c18480b11d2d956b817440764adbe89
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:33 +0000

    beyond zero community grateful apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
