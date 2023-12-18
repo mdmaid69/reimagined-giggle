f8f6f65400b765bb35676589aac9f059d376bd2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:33 +0000

    trust then lesson runaway worried

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
