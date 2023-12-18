e1871deacdff1d826935de73452796e0885f751e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:13 +0000

    breakfast children brush few picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
