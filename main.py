b971192a1346af8bac660e0c779c9a700d314cbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:15 +0000

    to whatever study roll comeover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import time
+def get_current_time():
+        return time.time()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
