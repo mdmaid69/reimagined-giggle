8dc942e9eb51c503e433b93f276644d916022fb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:00 +0000

    similar chance worth benefit acceptable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
