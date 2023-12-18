9ecaba9fba9ff1890221f71d55c425d980b80148
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:30 +0000

    drop influence tonight meeting sleep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def add_numbers(x, y):
-        return x + y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def reverse_list(lst):
+        return lst[::-1]
