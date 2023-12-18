16f31b686ed238eb7452be23cc6ada81d25c68ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:00 +0000

    takeover measure same reject contain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
