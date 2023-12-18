b76da8a64109dcfbc94bdf33bd821bea8a85ab32
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:57 +0000

    workon house match surprising getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_string_from_array(array):
+        return array.tobytes()
