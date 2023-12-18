41124259ee5d59e792d5f1d86ebf8dc3833875ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:10 +0000

    unfair choice Ee loose choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
