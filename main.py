bb7d839ff0636cf70e93de8ed32f3e191082f924
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:17 +0000

    suppose brush brush healthy calloff

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+print([x**2 for x in range(10)])
