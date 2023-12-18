a259d7bca8be5053c273db509148d3bb07528310
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:23 +0000

    Pp field breakin from notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import numpy as np
+print(np.array([1, 2, 3]))
