dcaafab16961ccc9ea79cb62cca2ee010e6c2096
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:58 +0000

    touch closely regard obtain most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
