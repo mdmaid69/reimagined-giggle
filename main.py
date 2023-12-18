3735aa33ade2ad8e10feeedf915573e67644b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:05:36 +0000

    unkind Ee target embarrassed adjective

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
