be5e190c2234b8dda578d82f1d6d219424bc0e24
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:58 +0000

    escape geton rough confused law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
