bf9c23f57bee769fffb1c291b66e74e30963456c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:33 +0000

    comfortable pronunciation somewhere punishment object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
