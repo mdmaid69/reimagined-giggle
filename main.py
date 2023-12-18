3d8e527376e05c446bc52712ff743935458563c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:18 +0000

    poison claim share suffer public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
