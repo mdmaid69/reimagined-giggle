93747b1a94638ef8dfba004e79b2cfc2b00d2df7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:52 +0000

    discovery estimate leave suggest total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
