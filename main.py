c623aabc8a41914349744f19423f3f2cf6e668de
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:57 +0000

    cause add call favourite however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_index(array, item):
+        return array.index(item)
