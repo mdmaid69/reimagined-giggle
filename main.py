60b40fff03abbbd9d71b577548580c9eec4b802c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:02 +0000

    tend ignore nervous transport brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
