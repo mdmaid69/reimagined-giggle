3c2dbf05e99b937db6b83171d7b774f93b2b5190
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:26 +0000

    own inform spot cross pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
