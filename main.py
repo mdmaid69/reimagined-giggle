20abcc477491e298da3c51a335d8f1102adf347d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:09 +0000

    think pleased around else fold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
