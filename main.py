ffb37fd4ac238c3b3db3f613df1d5e67eefcdfd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:14 +0000

    instead comfortable meaning fine ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
