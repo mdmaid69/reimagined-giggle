8d57cea1da59f628580cf0fc52c924daddfeb6a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:49 +0000

    risk file forgive tall damage

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
