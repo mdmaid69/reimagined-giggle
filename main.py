0fd2d827de4b2d2c63cab83afc7c908c46772db9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:15:32 +0000

    lunch regret repair represent compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
