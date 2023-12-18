ce35aa5bcde037e93370b83157ff377c08d5149e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:07:27 +0000

    interview threaten sharp intelligent fire

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
