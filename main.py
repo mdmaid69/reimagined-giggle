7ec197db7b289e8ab6fb974ddb33bdf6f29cd3b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:51:44 +0000

    pronunciation similar concentrate afternoon community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
