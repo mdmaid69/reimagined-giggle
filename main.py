91182b5b2c6528ad2f567b0250bd9af46d15ce40
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:50 +0000

    dance quickly dull nigga control

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
