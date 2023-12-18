c89140bc047fd038f22a79b8be2526c27638fcf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:24:16 +0000

    class spring border wherever add

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
