ccbc3ea4225ca3c35f9a60f7ceac64dbea8b29bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:50:05 +0000

    turnup glad transparent like central

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_item(array, i):
+        return array[i]
