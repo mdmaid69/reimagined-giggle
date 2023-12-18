322c98864e0c6ff938e70ceb442e2bf6e3b45fad
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:03 +0000

    attach definite to attract used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
