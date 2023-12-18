4ae91dc191fa0a109022977a3a6d493628fee319
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:25:13 +0000

    scale bread interest straight ring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def count_words(sentence):
+        return len(sentence.split())
