017861be96e9d5abf5c11814cfd6b3bfb6c2d87e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:35 +0000

    their tap notice open charge

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
