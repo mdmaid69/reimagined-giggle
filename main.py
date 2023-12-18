2d5f03ca4cef019061952316359aca8d67b5f735
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:55 +0000

    smoke protect to accident plenty

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
