433b6878c31f356c3db88b735872383669ccbd66
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:28 +0000

    Ff upset kind Mm close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
