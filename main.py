16bcae0e0a71215a78e25e55e50d28d669113250
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:37 +0000

    exact stage situation bet immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
