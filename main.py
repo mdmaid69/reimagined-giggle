d0a0e8b99443f9c5e3b5047249e06f8727dee682
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:58 +0000

    tap top travel hospital radio

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
