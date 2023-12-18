420ccc543ef12aface9e981ec38ec8150adcdd6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:29 +0000

    worth treat period from responsible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
