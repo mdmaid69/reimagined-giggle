3dd6da52a4357357b61dd0c342c6fd7ce8355f92
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:26 +0000

    flavour expensive Ff callat agreement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+print(os.getcwd())
