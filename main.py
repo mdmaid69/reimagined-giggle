d40635a10159b98e6602db05189057e28aa75ddb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:03 +0000

    Ss group willing occasional bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
