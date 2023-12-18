738f717c9e50048829a13bf0e8fc212ed1e0d223
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:25 +0000

    quality prefer petrol difference talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
