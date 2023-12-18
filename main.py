e260eb4adc128abf14bec9d1355304438d1d1dec
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:40 +0000

    foot used underneath near if

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
