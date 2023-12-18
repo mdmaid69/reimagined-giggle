19caa6f699f563b4b850314d2d4a8fbe3a234a68
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:55 +0000

    debt turninto reach goout in

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
