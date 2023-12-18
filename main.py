613be4b789e483c9e45728f2e1bb520248adc1a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:40 +0000

    properly colour clue reliable company

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def cube_number(x):
-        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
