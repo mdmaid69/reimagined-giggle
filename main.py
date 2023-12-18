4cef90a1b73781cf3e63d50d3a28ef9f2fe30485
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:46 +0000

    expression invitation run operate pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
