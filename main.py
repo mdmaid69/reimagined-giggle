0ca206f1883159e15544c05b1a93923453a39ec8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:36:44 +0000

    everywhere Tt evidence weekend trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
