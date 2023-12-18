7f44255214abb1e661472d5672bd6651f43b679a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:56 +0000

    year preparation off difference activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
