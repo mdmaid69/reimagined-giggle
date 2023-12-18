fdf9f33fd0399f475c45364be87214ea98f19640
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:49:21 +0000

    welcome track aim sport appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
