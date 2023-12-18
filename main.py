c8b340ab6c172c615e47d4a435caf7f84ef104b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:40 +0000

    sale sound wind calm lower

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
