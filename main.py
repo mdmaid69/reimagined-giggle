b34e2c8eb147412c943ad5024d54b2caa4f8c343
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:59 +0000

    sad experience equipment word maintain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
