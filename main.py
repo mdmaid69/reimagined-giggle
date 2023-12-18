22860f97aaecaf141cc37ef5d12b25dd0f9d0f20
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:43:34 +0000

    live but so concern break

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_unique_words(sentence):
+        return set(sentence.split())
