4554597c26188ddfec1347ec2749cc1304fa9a39
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:17:13 +0000

    worth sick over quiet yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import re
+print(re.match("h.*o", "hello world"))
