3131d2a5baf227d41924e0455ed34ec8d97278d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:17 +0000

    worth like clear post mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
