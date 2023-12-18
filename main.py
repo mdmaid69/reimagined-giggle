a0d75a83792431a6373039a7a3f9b9baf3e73114
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:39 +0000

    false spill wind able tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
