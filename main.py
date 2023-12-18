716b7df5f9f7b51c024426e836c1a17bc937965a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:43 +0000

    permission listenfor nature mood already

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
