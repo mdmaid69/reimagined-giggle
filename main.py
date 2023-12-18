30f87d469ef3c26ff7895abf5cb3a95c73d0441f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:26 +0000

    fat friend do bite godown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
