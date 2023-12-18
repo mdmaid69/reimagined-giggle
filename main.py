9f8b49ee2743b0b4ca140c0ea546ebe135a61aeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:09 +0000

    pull closed sweet until taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
