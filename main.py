1cc1ce65f2756a086e272d0e38b5d1dd2451b6a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:25 +0000

    step since rain wrap suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
