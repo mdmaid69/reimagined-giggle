a0db91d18e52827f483956edcaef51b7de417ba9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:46 +0000

    noone marry length light beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_current_directory():
-        return os.getcwd()
-def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
