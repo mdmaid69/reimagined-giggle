a790baa700469a361901f6eee218d79a3011807c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:05:38 +0000

    star pinch preparation Nn stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
