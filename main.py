9336b1adbb7f8e8e65fccca1e94df55ecea7362d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:37 +0000

    contact dry improve carry me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
