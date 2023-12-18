7b491d26921fc65169f214b459d368aac8d35a3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:34 +0000

    appear pocket snow complete practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
