51e03ac148aaf364f8d824380b533855367be70e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:11 +0000

    accidental odd value comeup waste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
