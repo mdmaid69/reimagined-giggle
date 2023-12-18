2588f15487167788a5cbc8980e281ce2cbcb6831
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:09 +0000

    careful course load pour group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
