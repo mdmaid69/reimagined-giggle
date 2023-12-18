60af8c7c253782c8d188f3c014640e4ffd161cd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:35 +0000

    entry content positive car leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sys
+def exit_program():
+        sys.exit()
