c8eb1438e1df99ada67873e1c80a4b02d8e8c2f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:33:38 +0000

    church respect draw blame finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def clear_array(array):
-        array *= 0
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def cube_number(x):
+        return x**3
