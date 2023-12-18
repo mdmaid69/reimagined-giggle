56c20bd4120d91a96743a0206098ac05883ba6af
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:34 +0000

    rise follow later breakinto throughout

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
