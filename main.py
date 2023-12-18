26c76c498713e32673dc1df6269a995a5fe7313a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:25 +0000

    show pity gather load stranger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
