cb46f693f211bc82487bea6afd51b693c749dc8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:46 +0000

    nose ring practise punish wind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
