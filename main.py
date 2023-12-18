55056117e7d521123ff50314a285e60fce8d0dab
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:56 +0000

    home high deliberate nearly repair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
