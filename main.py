acebb294e38de0adc575f8e445259994f5065d64
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:03 +0000

    account cross software deep mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
