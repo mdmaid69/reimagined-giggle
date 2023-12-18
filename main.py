1d99756f004128e497708d7f160ce4f9d5e31619
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:32 +0000

    laugh somebody then deliberately sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
