1237416abb36b66a687406733043ea6b6ecf4ee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:27:10 +0000

    sick stupid translate rest steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def count_elements(lst):
-        return len(lst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
