76f78657c9b94f9536400bad5124e21e41538bc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:12:45 +0000

    science top round split forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
