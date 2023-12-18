c9fd67db5e6845dd26f3f7883863d1ef707e271f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:19 +0000

    remember respect tip able tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
