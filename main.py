d33ec4c5a849d2db2e8f918cad4a8e03240a6e45
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:09 +0000

    enthusiastic getup practice nervous show

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import re
+print(re.match("h.*o", "hello world"))
