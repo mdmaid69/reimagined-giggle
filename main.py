5d8c2beada4ba58f194b2f812c25766146db3e86
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:19 +0000

    town unhappy room warm snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
