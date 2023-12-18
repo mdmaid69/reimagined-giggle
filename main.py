a2524c212ec38631e28ecec02c46398dc9b5782c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:32 +0000

    pleasant trick moment creature tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
