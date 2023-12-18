2f08a09ad75393ce4a1af855e388c39d8743e89e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:53 +0000

    Pp occur hold exciting year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
