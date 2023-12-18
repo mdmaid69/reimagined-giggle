8eda49b6383214996ae48a31c628cd3aeb36ca92
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:04 +0000

    frightened rent powerful noise tip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
