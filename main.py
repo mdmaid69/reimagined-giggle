50786ff902cd45f149b383c86626c2376abeb6d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:43 +0000

    already since scene plenty Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
