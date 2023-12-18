b67d9d599ab9afc8756d0af54f9c10bf66dda2c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:31 +0000

    various pencil father near sky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_list(array):
+        return list(array)
