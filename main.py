9e50b809b9c25dcbceed47b8343b89d40f941145
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:40 +0000

    question square woman accident understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
