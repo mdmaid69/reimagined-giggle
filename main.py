ca656988bc734fb970354fa4164efa58d23dddf7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:34 +0000

    consider mainly catch challenge action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
