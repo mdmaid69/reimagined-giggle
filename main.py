d9a9639c7dbe68ac1edbd2abd02060e71395b138
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:26 +0000

    nonsense be absolutely fairly lock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
