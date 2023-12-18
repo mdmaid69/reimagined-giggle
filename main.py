5bf7b2fd64b6b81dbeec2737450055cd0c855557
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:56 +0000

    friendly now read instrument treat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
