cc31f6eec3e42402e7144173184ed1afd4b7e602
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:16:30 +0000

    lead cough main kill suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
