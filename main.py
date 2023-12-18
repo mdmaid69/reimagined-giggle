4790d85a040a6f114ccfa08ebff24dacdf990fee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:52 +0000

    fond violent advantage system production

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
