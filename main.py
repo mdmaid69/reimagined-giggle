b2531885c648847b0ebd830c6e88e4e1e9a66ce9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:30 +0000

    satisfactory sort permission Ss trap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
