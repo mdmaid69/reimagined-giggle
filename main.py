acebe9c251f9ff3d91aec6e7f409dd0104d4faf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:07 +0000

    bringdown row closed within why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
