7fd5eabe4439a4b6cba2617b91d2a38adf77ada4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:30 +0000

    opposite visit mad immediately police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
