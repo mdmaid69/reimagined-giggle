70f237ee0026b537669f1cc1e80b871f1ceea9be
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:53 +0000

    future regional something bus gentle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_power(work, time):
+        return work / time
