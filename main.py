c5ebf4c56ba17a4094778a14466fc0b801230eb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:31 +0000

    office cough moment suggestion exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
