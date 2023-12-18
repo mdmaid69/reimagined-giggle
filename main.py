830542d35e3e13e6cbae403d87f191192ab5776e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:06 +0000

    you risk there confused 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
