6d1f9e9e3ab7d279daea48c63b10c625a19de8e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:24 +0000

    hurt during last delight want

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
