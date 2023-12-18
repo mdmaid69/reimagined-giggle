4a06ed689f37576789b577d4c5efca1d496546f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:21 +0000

    drink knowledge material exactly extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
