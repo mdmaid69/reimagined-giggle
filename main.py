43053bf32e64aec16d4128cf259d70315c50e0f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:17 +0000

    plain so behind schedule for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
