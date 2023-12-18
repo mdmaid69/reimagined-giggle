72ac2dfe61b81189e340f36569c92febb6da42ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:26:54 +0000

    punishment command matter whom eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
