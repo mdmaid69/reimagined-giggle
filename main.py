72bf9497f3a7befb0d7df5230bc64ad160a9c3cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:08:17 +0000

    win patient live attract push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
