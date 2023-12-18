de7863db9a8124f73e73f79d63fe5c6a07789f85
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:38:34 +0000

    moment damage shallow earn doubt

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def multiply_numbers(x, y):
-        return x * y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
