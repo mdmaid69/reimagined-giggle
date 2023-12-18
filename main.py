e44bb7bb16140c77e80872700fc373bc587ca2ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:55:39 +0000

    design suckme sharp letter meaning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
