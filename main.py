82ef8bfe0a477b5532fd7f74b7268154c6584adf
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:15:41 +0000

    without sail great shrink giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_user_dict():
+        return collections.UserDict()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
