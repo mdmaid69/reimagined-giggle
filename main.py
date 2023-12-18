606480306518cbf55368775cd28ace11164b1e63
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:13 +0000

    rent hardly sting aim undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_list(array):
-        return list(array)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
