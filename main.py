f5aba6342e4ad671f2557b76d60215142b915183
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:56 +0000

    takeback famous mistake means tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
