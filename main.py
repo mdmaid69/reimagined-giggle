5ffce27abf844461b760750d699aa05e363f38ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:51:15 +0000

    record exaggerate repair welcome jump

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
