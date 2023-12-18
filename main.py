445490516e38f90c026df4d2fed2b6baca1c0ec7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:52 +0000

    hold fill mother getup down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
