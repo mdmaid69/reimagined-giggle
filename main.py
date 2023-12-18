425644a008f227a36150d32630f8c3007d61e2ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:10 +0000

    flow chat city separate really

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
