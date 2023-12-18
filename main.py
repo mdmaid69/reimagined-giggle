d8cf7df5b0277c99e4477cf0366ced1030cf6bd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:58 +0000

    schedule turnon effort geton unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
