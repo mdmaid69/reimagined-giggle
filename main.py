f0115dd6778c771512cfa1d3f69714b9957fdada
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:45 +0000

    term head bar suddenly female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
