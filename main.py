c146c9b3c387f67e561016d78a0388bad372386c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:40 +0000

    turnon experiment practice sore plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
