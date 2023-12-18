429c8cbcdec92e9781b9ae080abec5e6b7750d4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:29 +0000

    confidence there head as communication

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
