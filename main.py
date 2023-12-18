97ee03857e07df6242ad64cb13f8399db89504d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:51 +0000

    desperate carry plenty hard able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
