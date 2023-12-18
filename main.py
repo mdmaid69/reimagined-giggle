2a7e5bacb0c75d23c49d4689a59aca06502c819a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:10 +0000

    trousers result duty throughout area

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
