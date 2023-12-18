274720ad76d8a5296e3d8e0631121c50284ac6ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:29:08 +0000

    even file female pronounce keep

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def square_number(x):
-        return x**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
