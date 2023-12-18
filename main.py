a5909969f9ca6153b66f53da3ca6310bdba0f460
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:32 +0000

    art think combination among communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
