c9281fd2749d4f5335b74d0a67ca498fe332adf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:36 +0000

    pile introduction maximum black total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
