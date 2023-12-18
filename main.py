50e1dd707ecca2751860e3390cb0cbf7406c3b9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:44 +0000

    leg often squeeze stop but

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
