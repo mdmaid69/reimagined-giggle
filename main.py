760a284364e699301d44e56d3d64be0eb4bf8f88
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:14 +0000

    least example picture no contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pressure(force, area):
+        return force / area
