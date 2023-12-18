048af2e2fa71739b1ac90879888de81c85698be5
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:09 +0000

    contain they sea wrap its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def count_words(sentence):
-        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
