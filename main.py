d960a683fab66f2b59370b9b802a19a9445a6e35
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:14 +0000

    young flood payment instrument mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def add_numbers(x, y):
-        return x + y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
