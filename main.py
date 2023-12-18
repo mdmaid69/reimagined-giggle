6253e45c814cf1c081e05d4933dc36ca7f97dc6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:50 +0000

    worst operation convince community loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
