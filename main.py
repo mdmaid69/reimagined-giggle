f00e0aecc2b79139ee9143329dbcb19da461a76c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:56 +0000

    sting neither experiment born difference

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def find_max(lst):
-        return max(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
