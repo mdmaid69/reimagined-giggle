8693db26f0d75aa6d93878d9672d4b72716763db
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:12 +0000

    mail fall plate ever scene

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
