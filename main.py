95aa132bb7a4785b9c7d7a74edd8e5cce2cab4ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:40 +0000

    admit transport pocket difference quarter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_string():
+        return collections.UserString()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
