0ecd7a442ea5a9acc9d11ce9fea5e8f8ee33cd1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:44 +0000

    arrive healthy tired brush hot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
