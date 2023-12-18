36f420699b75d499d66d3d88f82f757b18bc21b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:59 +0000

    estimate mine Gg smile visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
