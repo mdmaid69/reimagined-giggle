1eb7d8cdae247c7fbd91db0b9fa2ea91cad3b5be
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:43 +0000

    distant record tear fold relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_item(array, i):
+        return array[i]
+def square_number(x):
+        return x**2
