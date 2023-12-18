c494308adb9ed30b81b7d3687182f5781b131f23
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:05 +0000

    spread because total fall introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
