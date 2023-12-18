bc1ebae9899fccefafcbe3d7ad7d2c47fd69fc7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:37 +0000

    excuse later field kind invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
