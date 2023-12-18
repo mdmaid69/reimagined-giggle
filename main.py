d22578897df4822ac43f4a934852e15e8a3a923d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:51 +0000

    buy drop child spite again

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
