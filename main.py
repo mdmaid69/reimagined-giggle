207c3670bd276f42bb16125ba102ea2621c7d7cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:39 +0000

    protection innocent tight visit basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
 def create_user_list():
         return collections.UserList()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
