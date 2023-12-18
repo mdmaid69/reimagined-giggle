05e8a23105d45baef581edd615e5cb5563b5c091
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:38 +0000

    attention what recognize stop damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
