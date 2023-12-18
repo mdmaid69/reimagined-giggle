05ce5f48e3d7204a97fc29614ff3915303478b63
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:08 +0000

    petrol model neither tense breathe

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def print_python_version():
+        return sys.version
