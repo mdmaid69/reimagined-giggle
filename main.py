dfaf4cb45dc2cac8815631d3a59f8ac4df55b2d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:03 +0000

    at surprise tap thought turndown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
