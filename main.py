fbf6b399d6c3653b65601bb2d679e1ae3d995f42
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:15 +0000

    special rely object deep which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
