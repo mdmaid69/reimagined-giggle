083da7564609b3437e2ad9a5b08174a6f1b4d6b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:57 +0000

    present yours tongue honest stick

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
