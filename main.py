1d4f6ed4bbc11ce104cce849faa1c875c6de6d7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:28 +0000

    may neat kind which finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def convert_to_octal(n):
+        return oct(n)
