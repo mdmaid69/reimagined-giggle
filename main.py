274e05f5eefc3dc354ea9f3aeafbb8587cb8e70b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:14 +0000

    bin Nn down problem wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
