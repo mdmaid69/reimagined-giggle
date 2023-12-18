00fe7a16f673b5fa86bf161ba25298d8d1412b29
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:05 +0000

    rule stick dress job pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
