3d6aabcd48e674fd1aee53056a6fe6b9c3d4551a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:10 +0000

    block somewhere seat find suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
