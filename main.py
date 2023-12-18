1d006d91950bac60e0a283701b4dcaaf1081e067
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:45 +0000

    always balance rare along oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
