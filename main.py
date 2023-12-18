dca73604b6ed60a72cdccae944fa67f33281a010
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:59 +0000

    name bottom believe murder Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
