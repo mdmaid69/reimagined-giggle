aca11d210b395684b410e266ee5b4fda97be2845
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:23 +0000

    century positive turndown scale bad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
