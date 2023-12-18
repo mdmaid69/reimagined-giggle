1d3d89b07971ec51b46f9b2eab00bf2c8f269608
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:48 +0000

    order distance attitude stop giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
