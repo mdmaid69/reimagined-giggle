4e0b7f5edb95c0410c05ae4b992fae55ffd2e335
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:15 +0000

    correction holdout star communicate cheque

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
