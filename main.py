62732559275ff61cb9bdb3bb945f00dce1243c25
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:52 +0000

    gas attract unpleasant whichever criticize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(x, y):
+        return x + y
