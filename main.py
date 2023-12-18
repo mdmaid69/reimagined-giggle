62deac6693ffc43161663aae5d81eaf11ddc3841
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:54 +0000

    ring pause unlikely priority disgusting

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
