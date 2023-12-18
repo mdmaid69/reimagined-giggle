0fdcce1272d582c81c3758a72e4433a18f4f8cb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:45 +0000

    true then left bet cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_unique_words(sentence):
+        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
