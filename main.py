8e2be7b0846c403b06295339b07a96fc080e5a80
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:14 +0000

    yet upstairs read section properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
