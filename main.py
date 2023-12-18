f3c1f0826c39bc587524cafe838e9eac5c7acf14
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:58 +0000

    both tell care 571enough miss

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_hex(n):
-        return hex(n)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
