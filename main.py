af186e2d49912ff3e8780936208287f46940e5c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:43 +0000

    middle claim such cut replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
