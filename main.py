b78b754fe05738dc3f83250b16312e42c177d0c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:12 +0000

    ride brown improve evening fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
