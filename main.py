0b213bffae52fb5ed646f3ae856b6b07541f401d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:00 +0000

    then sad period trial reduce

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_power(work, time):
+        return work / time
