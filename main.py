20a94220ec31e85dd7a38273af5bfd3e586428fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:07 +0000

    warn inside voice attempt guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
