f01687615f736ce5cfdd56d329d373ad934d8f21
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:10:22 +0000

    that seriously beginning outside giveback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
