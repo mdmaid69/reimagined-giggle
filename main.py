c01488acb83081ee789763c5c7dad83fd1de22fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:31:54 +0000

    stupid spare before straight spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
