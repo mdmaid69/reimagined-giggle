0bf00321fbaa99e062597687e2f41393784e28e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:33 +0000

    say steer cry thanks argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
