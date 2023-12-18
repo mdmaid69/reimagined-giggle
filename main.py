a1bc2666e1cbe878aee7174c7f3a8d2e1d44e06e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:17 +0000

    ashamed heat sight surprising lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
