3a272ddbb46b09e0fbf862a83a55e7de71c17351
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:20 +0000

    become high public anywayanyhow popular

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
