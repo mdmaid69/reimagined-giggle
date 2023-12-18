63c8bb4562cae50323a8f9cb7fe77e83ab2153d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:37 +0000

    fear prepare majority comeover collection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
