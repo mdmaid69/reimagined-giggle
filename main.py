ff5b26d09fc2e7bac2a465e55bbb6a17c62c4c03
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:44 +0000

    pile left label false wide

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_length(array):
-        return len(array)
+def is_odd(n):
+        return n % 2 != 0
+print(sum(range(10)))
