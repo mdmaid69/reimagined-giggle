a3e967c9db871151b4688160596e51d911b29b39
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:01 +0000

    act correct present stay owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
