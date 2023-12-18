82c8899f862a9ca9611d89024a4a2b84cc3f0cf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:04:28 +0000

    movement black touch miss number

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
