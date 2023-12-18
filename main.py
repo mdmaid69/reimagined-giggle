89aedf52479602fe3ec3dae032a391e5e1ff9891
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:43 +0000

    stupid date tune thick protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
