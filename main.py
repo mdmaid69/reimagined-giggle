a14a565cb89e611cfe861ca5e10d27b30697b5a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:47:39 +0000

    meanwhile prevent angry day today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
