62c7c343782f1867140c97c7ff2fa466a222f8a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:15:51 +0000

    whether Tt sad knowledge state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
