09805d07507a99c37a8669684c1ff61336262928
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:13 +0000

    last runinto never wide form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
