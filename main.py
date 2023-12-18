ee8ab57f1f2d6c4c325c16bfe409ad8b48febbeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:11 +0000

    colour sense tap light gather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
