7dd741843779cd4dc222305fb9418552e37b7bc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:58 +0000

    convincing possibly crisis affect never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-print(math.pi)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def exit_program():
+        sys.exit()
