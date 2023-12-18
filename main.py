bab5bf6ff9191e9b3e534ff6d6d2d860f36bbd27
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:22 +0000

    whoever talkinto ability greeting the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
