b541ede184a790d4d2b20636f171bf48327957bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:24 +0000

    downstairs bound pupil protest wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
