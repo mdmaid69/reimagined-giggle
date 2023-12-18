be542ee280abd2546a45cfff4bbd745971baf36e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:48:54 +0000

    second fly then pour wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
