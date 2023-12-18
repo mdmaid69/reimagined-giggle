dbc9114cc9087affb77b543adac2f4a30cf5a16e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:51:14 +0000

    tough promise part except spot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
