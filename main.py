2dada87a8c212d87a434b6beeb8769a525d08318
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:39 +0000

    right because correction let can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
