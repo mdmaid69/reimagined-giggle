9915eb5f4b8f9aa3f666f393c830139341e747d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:22:52 +0000

    exam hurt respect sensible light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
