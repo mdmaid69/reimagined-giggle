dddb2909e5efc050d6d439193576039558b0ad78
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:36 +0000

    design content repeat compete difficult

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
