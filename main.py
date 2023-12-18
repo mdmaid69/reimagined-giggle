77249a476f939ac2a807eea02cc9b9e1484e7197
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:15 +0000

    cause ask around justice themselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def subtract_numbers(x, y):
+        return x - y
