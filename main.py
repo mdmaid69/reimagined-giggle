b51d6aec8644fcb42fd3484a7bb20bc5bac5a673
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:00 +0000

    nigga 572enter beautiful worth advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
