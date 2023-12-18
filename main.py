8b51afa79e6cc68bcf2600176566868850303079
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:14:36 +0000

    poor thumb watch plastic fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+for i in range(5):
+        print(i)
