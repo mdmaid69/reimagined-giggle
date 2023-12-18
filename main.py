d5987cfae8de5c151c1f8f0d7ba1e7aaf7d786f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:04 +0000

    clear pinch Mm field about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
