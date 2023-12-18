d8b43a6e8a5f9e8a50d634a9259cbd7c8b2bad2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:12 +0000

    original trick workon tired miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
