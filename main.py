14d835bdb4c34ad7ed55ebfd87344862c8857d0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:14 +0000

    emphasize cinema there ought scrape

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
