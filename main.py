85379782e23f823c2617da994e22b8f6956fd3ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:46 +0000

    hope correction early disturbing practical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import logging
+def log_message(message):
+        logging.info(message)
