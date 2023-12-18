429c47254163d1ce737baf33abd4c09ee13c05f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:10 +0000

    forward complete back proper include

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
