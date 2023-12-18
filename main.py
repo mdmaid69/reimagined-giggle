d0ff774f1fd1bfb89c197cd6b6501a1335b22980
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:08 +0000

    sweep try wind nor arrive

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(5):
-        print(i)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
