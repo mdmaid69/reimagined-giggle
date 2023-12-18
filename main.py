6b1548638f2a8ea0329f55347b9d748f216d438e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:35 +0000

    unite legal splash responsible into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_current_directory():
+        return os.getcwd()
