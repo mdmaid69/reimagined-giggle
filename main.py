acc558a55d67b414bf3bc6f96451af31fb30a2e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:14 +0000

    see minimum me up thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
