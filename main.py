0c1697eda08b0791e758548ec433acc9893198d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:52 +0000

    today dry strike broad influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
