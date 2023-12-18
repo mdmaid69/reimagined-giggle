cd23f955f562ada4dfae55dfbd7ca4de49123f75
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:43 +0000

    copy match goon up stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
