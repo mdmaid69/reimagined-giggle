b5931b0e069622fc954692c982ceedc956cbe213
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:18 +0000

    part state sweet radio satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
