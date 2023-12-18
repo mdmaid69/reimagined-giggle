cd89dd3b82748a91ced324fa0e8306b63a37f77c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:48 +0000

    congratulations necessary even behind sleep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
