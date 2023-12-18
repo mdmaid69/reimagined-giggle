59c25196f57422e9f811bef697a3162ba7ea3611
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:43 +0000

    hand Kk interview stop yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import re
+print(re.match("h.*o", "hello world"))
