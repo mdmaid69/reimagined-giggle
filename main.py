46c890a57efe28993810fb390b5ff657826a86eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:13 +0000

    central draw issue reply process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
