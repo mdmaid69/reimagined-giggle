5521a631ca94ce7ecac518a176eacc6e079c3170
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:25 +0000

    callback discussion part whistle church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
