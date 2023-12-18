ddc657f639bc828a11b4ab2ff84996366f475ed7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:20:03 +0000

    arrival lesson switch through ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
