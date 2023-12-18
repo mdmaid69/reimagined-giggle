e4e986b4acb366494defcba27478d9f35db7101e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:59 +0000

    workon fear healthy light pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
