e40b63f5593a6e4e0bcc9bc07385c3a300232f61
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:46 +0000

    day aim transparent quickly please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
