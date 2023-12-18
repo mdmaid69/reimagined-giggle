c85cf901202c038f29eca057eeafe3e44e609edb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:11:37 +0000

    plenty club choice fall fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
