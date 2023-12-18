e649c19b62fcbbfa5c635cdfc3f852bb83b9a039
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:40 +0000

    plan cheque aim join march

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def sort_list(lst):
+        return sorted(lst)
