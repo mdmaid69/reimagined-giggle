d784fa1789f65fc53b02f30f77e4cd4d0ec427a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:10 +0000

    switch substance Nn major unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
