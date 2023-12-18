e28cabad87c602dab72d1b3e71c4673e7d1f1c2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:37 +0000

    belong combine secret air improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
