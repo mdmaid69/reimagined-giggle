7ad8531a14a8f6c55de24289ebff6db4425973a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:44 +0000

    drawing prize spill speech stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
