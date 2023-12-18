4571de93e85021ad0a94713bba876214afd0d5e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:24 +0000

    unhappy which greeting goup post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  def square_number(x):
+        return x**2
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+print(math.pi)
