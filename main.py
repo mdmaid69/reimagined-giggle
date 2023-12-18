12f18aa28f36a814bee0e6aaf91b4ecf2b327770
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:25:42 +0000

    worse fall here hurt must

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
