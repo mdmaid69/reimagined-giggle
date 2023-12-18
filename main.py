b2e0ee21cb02c31eecd5a2e839c66422f8f08620
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:24:49 +0000

    ordinary paper early anywhere unfair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
