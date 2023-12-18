4acede16d300d5939317bcb900a61badec95dd3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:16 +0000

    rough under repeat announce close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
