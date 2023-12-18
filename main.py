14528be85ecc9fe1ae469c7ba488378825c2b3c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:02 +0000

    centimetre continue once material here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
