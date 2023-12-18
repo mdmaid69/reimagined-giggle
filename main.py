cbd6d66ad6d4156816b731476f135a2b6527584c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:05 +0000

    sure most speech pretty accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
