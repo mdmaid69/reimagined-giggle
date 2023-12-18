4fd914ae0d2af95ad487156a89b667ca753d9477
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:36 +0000

    collection lonely buy pattern second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def exit_program():
+        sys.exit()
