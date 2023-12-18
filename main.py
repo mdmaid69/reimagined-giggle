723132efb84fb329249b6db061de15f01c2ccc31
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:06 +0000

    damage close 571enough statement tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
