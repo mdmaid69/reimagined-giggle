e257998ee5703f9a54040e3253e2bd985cd411b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:33 +0000

    dance find foot time pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
