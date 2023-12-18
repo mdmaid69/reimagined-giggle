5128a0337d41d933327259924c0824a1cc2fe4db
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:41 +0000

    problem goon influence practice 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_bytes(array):
+        return bytes(array)
