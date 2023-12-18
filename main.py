878c4939b80ce7be216e85c957a6b193ce164e69
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:45 +0000

    constantly part church own accommodation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
