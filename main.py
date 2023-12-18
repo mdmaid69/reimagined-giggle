02da9295361ba25b06fca86c2b5e513dd396e5cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:38 +0000

    smile bin physical each offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
