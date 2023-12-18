3df3f3db2615648453a01e8acad35c3979fa2c4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:37 +0000

    commit scheme basic impressive wind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
