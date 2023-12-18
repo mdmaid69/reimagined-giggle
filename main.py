930a6985adcb6fbbad0f9702058075ea1a3ae89b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:57 +0000

    car matter while area declaration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def convert_to_octal(n):
+        return oct(n)
