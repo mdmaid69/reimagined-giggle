689ebefc62df75b7f7f1b9c9548683c35442a604
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:54 +0000

    breathe their cover count path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
