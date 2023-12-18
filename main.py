c16c7aa407932e98944b3cf162b5c4e0684a41b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:23 +0000

    release project of information stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
