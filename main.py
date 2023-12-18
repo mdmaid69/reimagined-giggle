d1f962a632aa942640c39902c47a383dfc5f3eb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:42 +0000

    any Kk break Uu press

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def add_numbers(a, b):
-        return a + b
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
