f7bf956e72a6d1dbb70d2dc208fdfda9935025c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:02 +0000

    spite early exhibition limit everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def get_current_time():
+        return time.ctime()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
