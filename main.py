9915ecb0cdb325bcf60a63b56c08faedfb495b9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:05:22 +0000

    offend new serve shopping human

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print(sum(range(10)))
