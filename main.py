79cae0704681860af0d5fcbfb5f27ad4ff759b9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:27 +0000

    upwards lesson onto as stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
