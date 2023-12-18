1556b6f3ee8b5fc908d21403459969deeb14793c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:13 +0000

    either bang sit Rr benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
