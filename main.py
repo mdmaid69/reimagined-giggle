b5aa0d17f7bbc9b7a1315e7125b49acfcb9d4da7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:22:14 +0000

    possible cook switch workupto declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def count_elements(lst):
-        return len(lst)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
