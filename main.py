a11a2f9ba641a8d68e3a46776328f38405b0c60c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:37:18 +0000

    goodbye proud pay professional perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def square_number(x):
+        return x**2
