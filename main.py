d81c73000bde20377a99a011c4f311e9bb811092
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:08 +0000

    because total platform ashamed nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
