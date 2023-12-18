ac3b1681a9a6989ca993af3e7daf6c5de5bd23b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:40 +0000

    different regularly most lucky adjective

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
