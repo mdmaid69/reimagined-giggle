88e8f3783e9cf4d7b7925b5c8b456895fb5d521c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:03 +0000

    seem top comfort half squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
