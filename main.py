c1a4b46fd3951415fb01e7c6e95e03d961af2b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:40 +0000

    lot news scream 564encourage hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
