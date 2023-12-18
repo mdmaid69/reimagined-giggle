0313253310ef98c6f146558f9291da3c8648f71b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:37 +0000

    fork thinkover journey help age

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
