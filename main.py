9ef2dd051fbb990d24cdd5d47033b8a83f504051
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:14:20 +0000

    reaction lock upstairs range classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import sys
+def exit_program():
+        sys.exit()
