a929b4b42dc7fbcc034c8a40379fa3b03ae60937
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:38:39 +0000

    spring cut offend face skirt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
