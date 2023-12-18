4af356a48861fbfa2149f0b6aff30e2f32e0d3a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:01 +0000

    comfort shadow uniform smoke development

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+import tensorflow as tf
+print(tf.__version__)
