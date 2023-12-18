35e910695b4b40bf733c7b6116cf94114233b1ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:01 +0000

    battery office that sensible back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def reverse_array(array):
+        array.reverse()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
