fc9eecc0a727e9b9b830a692472344b26e102fd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:53:54 +0000

    style majority medium possible frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
