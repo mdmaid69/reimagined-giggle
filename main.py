ef5f76d07f521ed2e30295283fc10495b48471c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:19 +0000

    Ll show basis win callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
