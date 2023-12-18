dd14ad8fc9013cc3af10f132b2c6ad382859057c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:08 +0000

    content among less behave watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
