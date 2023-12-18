8f0ebb0ce814b94a43cdfac04da5076f2ae4e937
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:42 +0000

    method condition silent bury during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
