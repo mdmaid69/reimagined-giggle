c5c9ccfec29ddf721d297adaf1ff849d1c303642
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:14 +0000

    trade song pipe welcome where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
