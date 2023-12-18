9bb414c9f8c0bb5c4cf5ae62b0f4fe414a6a968a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:29 +0000

    sleep key square might stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
