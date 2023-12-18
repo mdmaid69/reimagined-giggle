5bd2a91f1ce897bcfcd2699c59a2c2118c2af2b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:32 +0000

    software ground block light professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
