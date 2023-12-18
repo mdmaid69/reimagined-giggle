3a3d0baad1bd85f2a6e20f1f391d112ca95ef075
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:28:59 +0000

    few drive correction pool speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
