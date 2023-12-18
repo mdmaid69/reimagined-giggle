7f5b13f36e395615ae5bea9fd2b034dd971eade1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:33 +0000

    lookaround flavour nose hit watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
