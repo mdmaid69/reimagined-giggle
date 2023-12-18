df512843a5b31761fa86fbfa1a5ccc629af1c053
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:57 +0000

    slice young development quickly take

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
