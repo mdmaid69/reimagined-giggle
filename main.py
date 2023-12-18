e2e816e48430c81a39e43270c16f0c3f902ec025
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:55:09 +0000

    Jj yes badly class tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
