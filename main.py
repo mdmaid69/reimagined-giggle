c9534e561f35d846c0686d15c955c74b01e5ddd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:14 +0000

    fold free apart sad kick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
