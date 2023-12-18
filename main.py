e76d955bd97aa1d4e403c8839ef2120729c1fdbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:46 +0000

    active fit wait translate wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
