e4b71aab87c7d8c0010016455a5c8666e8ee362c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:51 +0000

    letter getaway pass block damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
