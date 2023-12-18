9bba5552bf5e33235c51e0d632c0e19ced731fed
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:24 +0000

    go then injury object complaint

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
