b72001c62c40cd87f6f50394fa6de7fc522ad4df
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:44 +0000

    thing plant bread preparation moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
