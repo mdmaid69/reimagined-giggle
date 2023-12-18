5209d2dae6f60b621cfbe5fd82a87ea3f87037b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:10 +0000

    art past petrol oil window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
