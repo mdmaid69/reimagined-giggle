fe790b1a74b847057e00be8e9e9dbb6527475753
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:59:05 +0000

    rate talkoutof air custom pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
