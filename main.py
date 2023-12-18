b8a345ba237e0213957e947532cf90eebc6bf2a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:38:51 +0000

    error 572enter fear round wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_max(numbers):
+        return max(numbers)
