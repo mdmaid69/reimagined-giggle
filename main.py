2a2f061f5028d0b6f03735266411ce88ce843d80
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:54 +0000

    impatient die expensive valuable noone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
