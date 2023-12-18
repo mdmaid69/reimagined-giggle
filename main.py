3492f84b2fb63bfa6113d6f01d85342f5ae93eaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:16 +0000

    heat reject home thankyou flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
