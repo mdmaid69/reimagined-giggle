6886d0b72fd8527e9910624c0505df2711263023
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:01:30 +0000

    beside search ride substance occasional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
