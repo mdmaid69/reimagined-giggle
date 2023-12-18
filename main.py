0676355bf14b14b13939e6312d8fcd9f76904a83
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:22 +0000

    takeover smart behind walk tidy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def append_to_array(array, item):
-        array.append(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
