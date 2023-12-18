ec6fe9b221121b66e4ba5d08c6e2e53879417e4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:50 +0000

    fresh maintain style emphasis just

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
