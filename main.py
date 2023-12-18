c95d377dc9658267fb33b4d350b49238cba410a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:12 +0000

    we legal pay frighten legal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+print(math.pi)
