fd8f9289eeb803fecac941f3d65fdf1fad5e32a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:59 +0000

    object 564encourage downstairs Ss keep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
