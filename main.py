9c154e479bd3dbe21742d2093b207ec9b40a151c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:33 +0000

    vote friend disturbing sea strike

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
