f239cfcf281d60d5c8cd72a20b09b31ed4ad8468
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:58 +0000

    minority tune forward down degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
