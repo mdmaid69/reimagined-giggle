32b280bab6a06d10c072cfb3d69eca1e05ae0f89
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:35:27 +0000

    term tongue contents public brother

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
