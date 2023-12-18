ab5475322ff79497416c134082d20ab13e2d811d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:56 +0000

    extremely reply surprising major gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_density(mass, volume):
+        return mass / volume
