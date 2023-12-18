e7f2a50cb4853fd0a6227d00ab4a5d889d21326e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:12 +0000

    regularly breakinto break unlikely full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_item(array, i):
+        return array[i]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
