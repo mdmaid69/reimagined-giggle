0a481a07a91b5a0258adac043b5d3f3f6a174a8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:15 +0000

    steep everywhere wing school patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
