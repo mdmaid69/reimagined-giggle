29efb7c950f70b25fc43b3dc0647ee6f9c5faf54
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:42 +0000

    traffic flat comfortable waste turnon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import re
+print(re.match("h.*o", "hello world"))
