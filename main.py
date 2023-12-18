0ab5807996fe4d936787b2c5998496ed30b43bde
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:01 +0000

    mixture argue runover why discussion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
