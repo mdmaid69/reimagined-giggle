9575d379168a48c0f5da2cfca258429463e64c6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:25 +0000

    fierce grand cool pencil yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
