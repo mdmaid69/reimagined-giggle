b2c4810f1997b55c42e6d7ba5f6915be4f333707
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:21 +0000

    arm himself thankyou embarrassed error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
