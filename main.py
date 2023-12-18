61ef646c79bf20d79fbaec64f7ec890fdf964cb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:06:04 +0000

    murder area turnover walk them

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def count_elements(lst):
+def count_elements(lst):
         return len(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
