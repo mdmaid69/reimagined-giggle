e5968d72ba4a99cce37b5e47d8bd6c8597ee1074
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:03 +0000

    sort extra car Oo watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def append_to_array(array, item):
+        array.append(item)
