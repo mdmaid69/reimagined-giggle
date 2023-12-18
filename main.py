9e0d29b19765dacb764d3a69cde962616046907c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:02 +0000

    spoil successful open bored will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
