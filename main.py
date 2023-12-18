3d7d562322bdffea693633be0863912c3b806707
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:46 +0000

    brown delight question eye since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
