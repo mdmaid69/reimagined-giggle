575a8fe4d1fe1e3fe509dd9008b2a4496f625b37
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:17 +0000

    Zz circle feeling Qq plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
