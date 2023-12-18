8e70973e1dee958b6f0816f0cc470214d07ef420
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:58 +0000

    lend seem dull minority comeup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
