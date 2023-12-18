45f7d41ce5f5783e5242674ec4210dd6b9d5a49c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:27 +0000

    tour workon breakup take address

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
