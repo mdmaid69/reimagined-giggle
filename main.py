b024fb55917bc4f097c274ccc767f14b7b8efb30
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:59 +0000

    top round Bb store top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_characters(sentence):
-        return len(sentence)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
