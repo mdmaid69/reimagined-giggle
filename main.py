32da639857952f0b1f72bc51fbefead9c6307541
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:45 +0000

    side blame surface season mad

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
