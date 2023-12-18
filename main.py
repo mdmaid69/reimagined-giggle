e92d55e1258a1eb7854bd848c33b688a3c18ad94
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:34 +0000

    congratulations paper culture worst pleasant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
