653af45868c563ae6a321660dede25abd47fbb2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:35 +0000

    news might stay various weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
