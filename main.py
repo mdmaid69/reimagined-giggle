7ee670ee921d634dfad00fba533b38f22fde625d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:26 +0000

    after talkoutof art will slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_words(sentence):
+        return len(sentence.split())
