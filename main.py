6560778b7f82e505259a84281ad9902feee58e0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:19 +0000

    length progress injury round peace

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
