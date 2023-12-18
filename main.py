075c7890c5ed276454b2275f1eab25b2350f39f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:54 +0000

    suckme instrument then could unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
