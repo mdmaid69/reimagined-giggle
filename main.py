b00a414edd814ab0876e4f9fde0d2b3476db957b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:13 +0000

    fault tomorrow sad deal order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def cube_number(x):
+        return x**3
