95239c4dc2e8e37c0a218e8ba4fe1745254d93d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:21 +0000

    typical advice union market down

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_unique_words(sentence):
+        return set(sentence.split())
