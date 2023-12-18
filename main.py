571e97ee62a30e0b932995f3e5ce6dd681c2957b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:45:22 +0000

    makeinto into course accommodation long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-for i in range(5):
-        print(i)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
