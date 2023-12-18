8853061cbbf944205ca783c4d5d335693345abb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:40 +0000

    black find issue rain embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
