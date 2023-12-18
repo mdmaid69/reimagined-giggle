bc07af76dc689a95d75cbeba7c1dfb03444504a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:06 +0000

    blame otherwise hurry nowhere breakinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
