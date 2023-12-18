ccfb05c0c80a6a8912cf0c2a82b39f6c2c0e5a54
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:32 +0000

    sign write wealth advice tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
