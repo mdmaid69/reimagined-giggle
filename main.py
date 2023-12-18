e415dff8263e6572816b157ff4f7618283504e9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:01 +0000

    house event forever wind conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_min(lst):
+        return min(lst)
