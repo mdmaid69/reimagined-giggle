e4e7611e844b8139b433b924cb79eaccb34d28c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:11:40 +0000

    lookafter can result defend through

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
