bb0c0d95562fafcea60423e87045d85daaffd9ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:28 +0000

    wash far right too good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
