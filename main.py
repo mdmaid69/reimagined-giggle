9d52f6a4891e30f7ba43feee09969d2c85936da6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:38 +0000

    radio fold rank thorough concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def sort_list(lst):
+        return sorted(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
