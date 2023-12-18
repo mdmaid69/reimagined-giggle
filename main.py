64c91602f944acd1faaa427c61a72ecd9e18e051
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:29 +0000

    attractive interrupt song bet conduct

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def sort_list(lst):
+        return sorted(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
