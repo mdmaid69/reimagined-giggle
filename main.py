e22fa9ba20846b4bfa8764d154aafdc020d8a057
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:52 +0000

    load squash address less godown

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def split_path(path):
+        return os.path.split(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
