6fb8835fb857eafc080ec09cf869f9abcc754e43
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:10:19 +0000

    figure black drop either weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
