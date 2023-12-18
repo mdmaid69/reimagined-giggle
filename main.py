b4bb6a7241fe0a970d067edfe67ed0814f62e839
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:33 +0000

    alone difficulty hardly hand education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
