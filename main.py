541897e78558eacb83fa96f61b27431637778ba8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:30 +0000

    drop produce heat honest supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def convert_to_hex(n):
-        return hex(n)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
