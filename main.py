5b11d2ed52e474a5a5f19dee6cdf186f1a508ff2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:36 +0000

    ashamed Nn common ought pull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
