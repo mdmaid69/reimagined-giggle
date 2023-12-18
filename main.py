2c8ae244701ae0378abccf69814bbe40a330ad55
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:05 +0000

    pin rain popular once tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
+def multiply_numbers(x, y):
+        return x * y
