664472538e354203b46ca5d49bb245b752a8c0f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:16 +0000

    empty behind Cc worth takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
