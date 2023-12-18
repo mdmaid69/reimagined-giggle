33592768ca76324b5b6b4622f2a2b2376a287bfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:55 +0000

    content fill much car old

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
