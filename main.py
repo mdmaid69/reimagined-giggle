9a07cff57048368f822cfb8b7e583a3b5bc2a123
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:22 +0000

    skirt fear excuse pencil straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
