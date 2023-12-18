4d746abcf3ff8ca9d3c50fda8cec3727a51ed097
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:48 +0000

    paper list do store maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_pressure(force, area):
-        return force / area
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
