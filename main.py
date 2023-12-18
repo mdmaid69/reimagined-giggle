79dc162a76b35140bdeb18f9a90a7ac127e0f56f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:44 +0000

    powerful ring load art about

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
