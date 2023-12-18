345b3c236ce395fd52d518f79f1c9e23fdc1ab68
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:00 +0000

    wide characteristic agree swing Oo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
