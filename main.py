72c9b6d8c271d87fc806d89284d3139a9d830cae
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:51 +0000

    which nearly wrong home term

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
