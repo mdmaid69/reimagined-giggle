5191842a7bb6769c9869288ea31bbd694e42516d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:33 +0000

    pass succeed pen dress generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import os
-def get_current_working_directory():
-        return os.getcwd()
+print(os.getcwd())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
