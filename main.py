1bfe0b5413ebd3e6fed8654f8197bfa42ebb2642
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:30 +0000

    neck doctor rubbish pain public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
