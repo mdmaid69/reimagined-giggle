c157714b97940a78c72298ba76a803af430ebf9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:25 +0000

    connection sure direct door prize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
