cddd40ae4199141027d218e57364b44ebb6e4d99
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:45 +0000

    release will sound love hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
