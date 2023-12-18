330e6d67efa846f772e5fb6b8aa21e9df59a3f97
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:35 +0000

    cover listen rub black name

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
