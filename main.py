44fd27227d69f3c197cf479ed019e4c8e2b0f591
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:56 +0000

    prove bright another fill hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
