7ffc897565c2983de67a9d2fc6c5305279c8fbda
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:53 +0000

    handle equal charge comment respect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
