b1a55c31cd2b48fee7eb7132a887aeb1fe605bd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:41 +0000

    goon careless yours instruction tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
