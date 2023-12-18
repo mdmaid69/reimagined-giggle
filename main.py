758c34b5eb36e7c1d1481c2cfa3e14e49a5bffd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:18:13 +0000

    match radio joke trade future

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
