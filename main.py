4850f44e8c3e777fe9553330feddf38cc8eeebec
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:11:12 +0000

    invite track imagine side progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def add_numbers(x, y):
+        return x + y
