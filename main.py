baae82215dd3ae6c2dbfb24dbc051224f8741fad
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:27 +0000

    press send tape pair name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
