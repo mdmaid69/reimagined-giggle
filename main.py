c3a807b15e8761e76e60bd41ccb88502d7ccc018
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:28 +0000

    pretty computer slice door later

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
+import os
+print(os.getcwd())
