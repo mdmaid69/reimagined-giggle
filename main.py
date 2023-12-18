428c06f9d01592608b81eaa954f21681e6d5286a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:26 +0000

    shade trap organize cut bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
