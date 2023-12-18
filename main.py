d56ae34bd8020386a95f182eb044c3382f95a392
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:11 +0000

    past early dish holdon serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_current_directory():
+        return os.getcwd()
