dad87745776a393200b8523d5776732dc669ab71
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:46 +0000

    correct transport worse contrast go

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
