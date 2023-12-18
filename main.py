67464f75e78e5bad63fe85e9dba99b149f37de78
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:17:16 +0000

    oil idea bus embarrassed level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
