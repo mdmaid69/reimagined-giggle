fb449e253d727473888097e47e85016f9aaca324
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:16 +0000

    true bunch tradition print everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
