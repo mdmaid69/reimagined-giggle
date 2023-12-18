a0ec15a7c9b57ba37756f2a1fbb6d4e46f101e8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:27 +0000

    bridge familiar right form might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
