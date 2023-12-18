69a941aa79d83001fadf851440c08776cbf869d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:25:58 +0000

    win return school patience block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
