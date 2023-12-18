64a00c9d8884ac5c49c8f9182a6edebad99bc01e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:02:23 +0000

    solve clothes pretty hurt joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
