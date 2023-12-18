464597785d704a05880a7300b53f96d797f19247
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:12 +0000

    Rr seriously condition hit later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def get_current_working_directory():
+        return os.getcwd()
