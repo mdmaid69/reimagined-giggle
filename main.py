66ed93df8e88e554ea49b642036eda4d7fb5dd20
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:23 +0000

    announce this and door pattern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
