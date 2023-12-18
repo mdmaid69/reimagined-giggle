bae43ef9ef51f7459ba31b70ecb86e318281ac39
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:06 +0000

    damage claim concentration feed under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
