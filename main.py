5a0d098a9339667b76a389cde92b978c027f074b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:21 +0000

    very getoff slowly reject thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
