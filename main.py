65fbfddf56b00244b75f17b9dee7947a057df1f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:17 +0000

    proof question section unusual last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_odd(n):
-        return n % 2 != 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import numpy as np
+print(np.array([1, 2, 3]))
