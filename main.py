49b4a28d9bf28c976493d5f0e1c7f5ae04521a73
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:38:27 +0000

    Cc sweet past broken responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
