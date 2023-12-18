0e6940784b4d379af1957957beb7276d86fae845
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:44:19 +0000

    often by disappointed week freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
