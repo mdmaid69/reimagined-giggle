97f855b3488ac381b64b168478c43e3dceb3f05b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:26 +0000

    makeinto hit sink protection we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Characters:", len(text))
