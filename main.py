b5f9df943593db68daad33c30be3c82664741c9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:49 +0000

    temperature position language stone expect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
