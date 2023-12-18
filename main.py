bf717495a6b77aebff083fac1c01a109dfd81002
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:34 +0000

    downstairs goup hate afford power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
