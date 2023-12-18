095b653d5580b907d45fe1cfddb75ce1ec24c81a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:54 +0000

    satisfied practical dream control sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
