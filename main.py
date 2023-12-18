e44cb0b41146609b5571065ab5809258e6acd242
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:26 +0000

    better command doubt grow uncertain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+print([x**2 for x in range(10)])
