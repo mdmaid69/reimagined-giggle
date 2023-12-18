4754c137910bf4aa3fd4ab87decbc2f8ed6ebc9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:03 +0000

    learn quantity mistake towards react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
