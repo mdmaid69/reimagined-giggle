fd9ea220dd342f1422e845dab817c66ea747701a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:09 +0000

    start some my when break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
