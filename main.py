3c5f7f29ad7fea5332ec40e71606cef7618007d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:01 +0000

    contact guilty turnback may arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
