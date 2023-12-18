1bf835a4ab70f98406d33fcca4fb5dd757e88eb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:07:42 +0000

    nigga breakdown Vv debt tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
