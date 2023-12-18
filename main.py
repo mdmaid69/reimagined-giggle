740a64abd29ae076be9ca03980cc2713cbcae6bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:26 +0000

    runinto clearly actual better pain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
