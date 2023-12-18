ac810a7449d99ed9c63bda831cac84d1369bbe60
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:36 +0000

    ours generally debt turnout every

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
