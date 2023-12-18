3210f2169454cb749a232bd780e49d0c215f0010
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:00 +0000

    war excellent morning their breathe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
