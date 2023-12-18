68b9a5319b27a88378784b58d3d985aee70ed20e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:19 +0000

    after fire slightly slice within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
