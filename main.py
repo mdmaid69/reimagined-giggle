8b9641880ed706e59eff4e61064d2f6543e8ec95
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:32 +0000

    seriously trade holdon track bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
