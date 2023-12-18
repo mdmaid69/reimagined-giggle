9ccdaf56670e845b1f41f09c66a55ec195752dd5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:33:02 +0000

    by name its split happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
