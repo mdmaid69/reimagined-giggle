b739efbde87c8e9e6178abfdbe66bca61a1d3ea0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:26 +0000

    side breakinto bear trouble reliable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
