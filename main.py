9c43f3a4ce8c49fdda5785e8a9d39940d2f4885b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:09 +0000

    raw but cause convinced reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
