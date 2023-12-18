70a95e54ed2ad060f43499147026419b0c561165
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:55 +0000

    limit mean pool bored size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
