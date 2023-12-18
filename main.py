7ef7760f13e3df38da71e1b4d73bd5f49caae8a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:52 +0000

    taste whenever confusing long share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
