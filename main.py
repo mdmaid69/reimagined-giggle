db04998a4ad11f82cd243b6063e3f491c0a0ba11
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:27 +0000

    criticism already travel notice famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
