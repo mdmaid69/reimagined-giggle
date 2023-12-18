61ac9de767378268582a0419ae6c688ac38eef17
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:04:45 +0000

    minute complain head concerned separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
