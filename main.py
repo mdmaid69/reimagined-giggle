fb18246ea45c70de1da5923673dcbcc0291d5836
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:30:47 +0000

    precise division minimum settle instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
