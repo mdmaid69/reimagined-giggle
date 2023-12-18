17326867ac0b069b661cf661116480dcbc4f0c43
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:10 +0000

    office form protect possibly passage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
