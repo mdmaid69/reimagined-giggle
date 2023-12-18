ea818c73e61e02a184834cb900a629fafeeef6bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:12 +0000

    theirs narrow over repair branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
