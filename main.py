ae0644ba372d63202b0106e4d3066719918153dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:53 +0000

    travel buy copy approval secret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
