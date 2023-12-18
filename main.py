4f52a77ebd6194d7ed52cc669359460a21906e10
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:08 +0000

    mark 565end add card sweep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def greet(name):
+        print(f"Hello, {name}!")
