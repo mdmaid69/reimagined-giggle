262f7c75d61066a64a9fad6b83c15e6a5686c61f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:27 +0000

    food consider strike meanwhile desperate

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
