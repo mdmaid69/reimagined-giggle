ac9870518e4a94bb0b54904c5fe434969ca077ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:12 +0000

    high children general downstairs lie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
