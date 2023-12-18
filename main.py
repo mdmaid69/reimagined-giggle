5e38fa110259e8065868ddb742f89f753564a265
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:42 +0000

    star unlucky though against capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
