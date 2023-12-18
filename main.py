25fe5eac9bf4bd0cd3b3047beb7e0d1b1ac1e1dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:40 +0000

    rain basic love experiment search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
