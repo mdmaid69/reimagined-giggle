06ce7453e3f4e27ed48bc465bbbe93ba80ca512f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:09 +0000

    ago usually considering design owe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
