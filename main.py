1aab10eecf3a5f1eacc0ca01f9fe73c5d2d00919
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:18 +0000

    spin list work question hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
