249db40625b5fbd4b40b8b75fa04cdcbb403c661
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:02 +0000

    cover enthusiastic stir catch stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+x = 10
+y = 20
+print("Sum:", x + y)
