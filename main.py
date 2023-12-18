313643d1ff131f398eb5d911d428e59d8b7551ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:14:33 +0000

    notice round rain pick bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
