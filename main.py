5ffece1c1fcae836bf94430a5d4fc119e1637998
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:01 +0000

    rush cold available record somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
