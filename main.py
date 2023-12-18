14fd44b0d754ea41873d4d6bbb3496bc0adaa843
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:45 +0000

    needle chief moon train best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
