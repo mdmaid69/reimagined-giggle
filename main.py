5aa9dc19e5740dc0097c77654192e6e7bc060f6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:58 +0000

    sink splash food law push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
