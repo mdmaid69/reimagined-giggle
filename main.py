3723e13859a839e5c5cb5080bc3bba890729ded4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:22 +0000

    name sink offence comeround person

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
