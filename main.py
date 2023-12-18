211a16c3c333e1429ebb59b252e3c8f3be2e8f88
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:19 +0000

    shut most intend hardly desperate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
