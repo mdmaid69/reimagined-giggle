5f7dec5625a8d9675489172b514e80cd128a2a53
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:31 +0000

    combine explanation journey brave bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
