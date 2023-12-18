f7ce49d42b37b3f02fb57b78e196cc882176bb5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:47:36 +0000

    label fall face stranger will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
