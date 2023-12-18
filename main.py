9ca44beb4e26b36b72d805cc3664199c1ff7422a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:20 +0000

    dish Mm goodbye software study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_power(work, time):
+        return work / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
