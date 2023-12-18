243df5ec3c61db460d8c82cdb09eb6c45767ff24
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:43 +0000

    whoever visit minimum night computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
