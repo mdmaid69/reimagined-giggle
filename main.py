fa834174a7df55811012442f7d9ef48f260a30c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:39 +0000

    emergency space average joke person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
