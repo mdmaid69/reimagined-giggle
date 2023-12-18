e0d992bafb93fad8649422eca86850e57dd7325f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:32 +0000

    explain stick move somebody vote

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area(radius):
+        return 3.14 * radius * radius
