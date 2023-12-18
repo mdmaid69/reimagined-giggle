1cb824346662905d0870826b936ff395e4f16d5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:13 +0000

    corner sound mad me gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
