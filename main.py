8022f23338fbc21ea0a27012a3a98750b35c4d0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:51 +0000

    polite squeeze fill free share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
