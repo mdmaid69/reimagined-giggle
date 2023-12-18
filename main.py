183ba82f26cd546eb140639431d20e7232c0e6b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:01 +0000

    future ticket nice manner belief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
