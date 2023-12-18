5eb204008010ffc81f62db7d29e2def90ad4328d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:51 +0000

    hang game section dead OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
