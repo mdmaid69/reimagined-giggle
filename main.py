506a055ae2b640079e252d759dbba07e03739149
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:13 +0000

    most ask of blame repair

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
