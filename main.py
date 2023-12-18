ffe61eb1977cdef09a2e1c947f6720fe028df0bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:37 +0000

    form fire never noisy sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
