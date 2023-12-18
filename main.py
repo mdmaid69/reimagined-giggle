013fe3c1a733c778e9cdd9218d625764fe4ef67b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:43 +0000

    makeup occasion card eye colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
