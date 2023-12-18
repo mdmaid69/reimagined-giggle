c68b4d5a383e9053641e662cbb69a0a7cb3486c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:32 +0000

    forget relative police exam go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
