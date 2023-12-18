6af3dd89cc7d2b57003186a0ae23f142982a7fac
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:37:36 +0000

    poor hers something mine surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
