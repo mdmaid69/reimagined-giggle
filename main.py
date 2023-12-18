dfad6948045fce20da41c745589bd3c6f5882ca2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:52 +0000

    or however damage better but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
