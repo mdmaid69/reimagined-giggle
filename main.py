3dfb41608eebac3d14fbcc22b8bc60b76878bac4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:20:39 +0000

    allow someone little plastic behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def count_elements(lst):
+        return len(lst)
