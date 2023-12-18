e1eecd8fcbb3e43d5a1313f7936d46f9becdec54
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:41 +0000

    after share delicate keep trip

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import platform
+def get_python_version():
+        return platform.python_version()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
