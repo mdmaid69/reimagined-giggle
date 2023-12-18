539c40ece6d14106211108432495d7c1965ba745
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:28 +0000

    trade uncomfortable face bank fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
