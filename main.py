acaf35cc90ff9b03e6a58d39e594c82dd28ab0a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:38 +0000

    refer shout close even rank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_max(lst):
+        return max(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
