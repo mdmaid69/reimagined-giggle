525beb3a731718ed830f19e09e856350a3e36b91
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:13 +0000

    genuine up trade generally advanced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def sort_list(lst):
-        return sorted(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
