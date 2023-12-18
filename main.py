bd88734d7ecbcdbcf77868dfdc4a4541a8561f59
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:00 +0000

    strength some within return concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
