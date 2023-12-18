10c1efdc84950208c5607c6b02c883e750d344ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:59 +0000

    first fortunately change against squash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
