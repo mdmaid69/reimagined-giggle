073bc179cdf39537cca6de2a41f7334f019b53ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:35:37 +0000

    back makeup cover spill exam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
