e92ae3da0d834b5272ff38e9a468ff2f6456e908
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:48 +0000

    design sound priority weather extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
