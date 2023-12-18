97a4c29a9dc986c20c577195b4bf58177a584f67
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:08 +0000

    free platform force like law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
