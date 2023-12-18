9febf426ebe9f0e73ba5071ed2a242e63b3c76ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:16 +0000

    at pretend crowd report correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
