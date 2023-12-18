6e947d6926edcb7a84afbd3bb8ae3cfbfe2f38fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:23 +0000

    workupto choice record block after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
