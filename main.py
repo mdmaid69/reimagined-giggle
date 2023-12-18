6dd4cdba17dff081f024cc9fd02362fdde959371
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:18 +0000

    curious silent suit tragedy strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_directory_name(path):
+        return os.path.dirname(path)
