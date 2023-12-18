8fe4a5a195ad290cd142c49b05f5f84e0ac68448
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:11 +0000

    mad separate leg world cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_typecode(array):
+        return array.typecode
