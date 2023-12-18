7667da0c3d76844b4369e641586721bbb0d1540e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:10 +0000

    record view base connect age

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def sort_list(lst):
-        return sorted(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
