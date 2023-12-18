23875edc83135aaa82feda489833f5a0e94e2fa1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:44 +0000

    length bag obtain nonsense content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
