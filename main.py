151d2188429c15360479e1b9cd38d0536563ffb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:24:51 +0000

    dead frighten little offer late

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
