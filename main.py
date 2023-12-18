a05a7376606ddc00ced281b1391b7d1943b9ed49
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:05 +0000

    something patience confidence score raw

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-def print_python_version():
-        print(sys.version)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
