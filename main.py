fa0b1987ab74cb2c4bbcf07b9333e181f18544dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:53 +0000

    winter narrow affect doctor there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
