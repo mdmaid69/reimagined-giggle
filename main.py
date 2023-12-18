a9b86a1a2cd66815e8beff09d5994ceff56440a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:56 +0000

    spin sort match who welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
