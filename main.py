c046ff3b0d4e39695f077e41e4c1d3a6029621d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:11:27 +0000

    once serve drink label getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
