166436e87c92ec42daa1dfcf124385c92f9ec334
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:55 +0000

    space squash impression appearance sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
