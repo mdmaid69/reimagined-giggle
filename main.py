916c78a4183331af19245625a8ab1f074335ac4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:18 +0000

    towards cruelty deep underneath ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def cube_number(x):
+        return x**3
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
