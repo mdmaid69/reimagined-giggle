b2adc46152e97b39dee567246d69ddc52f46a37c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:01 +0000

    glass man letter rank passenger

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
