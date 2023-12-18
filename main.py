83e0f81487ad86aca039f8022c1e225f2ccf569c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:31 +0000

    hard whoever peace smell spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def is_odd(n):
+        return n % 2 != 0
