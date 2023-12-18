959978ea50ee683458a71b231e76f93c4f19621c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:26 +0000

    pleasure clear wide colour do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
