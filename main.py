f678f5feaa00f2b128162b3d4a17ff2c73c40250
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:03 +0000

    godown moment open everything stretch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
