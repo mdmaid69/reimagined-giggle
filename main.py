b5fad5fc6c1d14693ba20148be09107e0670f962
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:06 +0000

    recommend switch once action send

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
