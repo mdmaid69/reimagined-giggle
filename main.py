0d84a8f08f347143a428674b8adcf0fc98eef9de
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:20 +0000

    affect arrange there memory impossible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
