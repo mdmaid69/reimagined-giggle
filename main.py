649241670a332eaf8ba79310e33fc58ab2002537
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:21 +0000

    handle today regret Hh breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
