2e9a608c8d2ad4c26dcfc7ad8f2857e9ed54df3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:26 +0000

    necessary swim cheque taste wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
