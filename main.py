c6729ee1ff1ef8b27ef883c5ea183796f859a058
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:56:27 +0000

    unusual public meaning disturb plant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
