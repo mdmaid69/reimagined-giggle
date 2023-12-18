69c4918cf03aef55a1cd530e7f45d52f6d074478
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:07 +0000

    contain getback much improve comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
