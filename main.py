ddfe4bcd2fee33d5e8e26768019c1f7b055fb84d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:48 +0000

    across public evidence appeal rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
