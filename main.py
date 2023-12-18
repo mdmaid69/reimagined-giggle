797abef7661f5bb3ffd018d02f4fc9fb8405c939
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:48 +0000

    ride become unfortunate work break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
