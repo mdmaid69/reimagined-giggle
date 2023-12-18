f6e52ca8d4272aeaf7a0628ad6f9008350f5f239
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:37 +0000

    scene proof performance trap flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
