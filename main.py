e6e914ec8cf1606c2108c52a9b431b84d34e6c58
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:14:12 +0000

    thanks attitude eye risk save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
 import collections
 def create_counter():
         return collections.Counter()
