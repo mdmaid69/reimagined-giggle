3b691b26053f9885d5f600ea8248baeef4ddcf17
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:45 +0000

    afterwards coat workout evening accidentally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
