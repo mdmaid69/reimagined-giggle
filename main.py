61a7fb3ae1ace7fff366029a6d102a8d649349e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:54 +0000

    mysterious plate perfect quality purpose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def is_even(n):
-        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def is_odd(n):
+        return n % 2 != 0
