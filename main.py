bcfe301c565ee4fc7307750265422dc7bbf0fa78
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:03 +0000

    swimming make town funny quit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
