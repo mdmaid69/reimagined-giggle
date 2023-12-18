0a79a01d6602b9f58677caf460b7867a849fafaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:44 +0000

    play avoid near language but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
