8a2f5f5b9a57007a54413613cae94581d4fe36d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:13 +0000

    slip leave could meeting needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
