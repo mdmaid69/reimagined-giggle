c91206e40b1ae06d9ca6bbf6adf9e8e4f61b89d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:01 +0000

    cry tooth display trip far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
