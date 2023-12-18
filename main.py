781d6f13c13921170dbc1a352b6a7e7979e84df4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:04 +0000

    spring sort dance shade further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
