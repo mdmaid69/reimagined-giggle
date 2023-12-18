11c53acbc731c7f05291db02030a0506457e0d92
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:09 +0000

    plant polite deep hear flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
