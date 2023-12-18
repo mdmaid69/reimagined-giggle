996e46eb57fa9638b7cb8bc53cfdcfa0dc0b7b20
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:05 +0000

    satisfactory noone long skill truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def add_numbers(x, y):
+        return x + y
