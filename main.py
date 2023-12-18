f3eff705d6e16fd024f6284d0450af9ecf8e1ec1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:05 +0000

    earn she should view reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
