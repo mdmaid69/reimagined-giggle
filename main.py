62b705b61742a032adf68c1bfecbf6375a46b77f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:26 +0000

    act inform strike exit chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_array_as_set(array):
+        return set(array)
+  import sys
+  def get_python_version():
+        return sys.version
