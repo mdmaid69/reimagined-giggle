b017645cc848dd82566d764d96fc3b3fe26357d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:10 +0000

    upwards noise me bored success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
