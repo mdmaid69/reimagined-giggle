6d07da436846736508ec39af20c594709ec7a43c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:15:01 +0000

    degree light partner supply fall

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
