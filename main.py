c400a30b453ef231f5e566c32ca6999cb3f7a0ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:28 +0000

    content after the union perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_max(lst):
-        return max(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
