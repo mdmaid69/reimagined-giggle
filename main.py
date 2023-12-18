06ebc8ff0038e17032833ff458d35b52911e90a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:57 +0000

    centre fail certainly used beyond

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def reverse_string(s):
+        return s[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
