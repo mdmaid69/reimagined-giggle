185d98d3bf72ff7a0a2e94406dd340198778613f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:18 +0000

    kind tragedy ilovedicks suit cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
+for i in range(5):
+        print(i)
