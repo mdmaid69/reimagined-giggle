db80eb6f9a9fb9133e9de7ad761b81cd24521835
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:14 +0000

    workout point method object mark

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_tangent(x):
+        return math.tan(x)
