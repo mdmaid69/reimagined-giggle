f83124d63c934fc852c0a4d2819d5222c11bcfcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:18 +0000

    drive substance congratulate determined floor

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
