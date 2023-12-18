3a5b08ec5e7978af604c103740233f8ec733a53e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:05 +0000

    responsibility cost thank concerned sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
