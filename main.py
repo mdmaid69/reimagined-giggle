81c450d92db1ecf91ea89c58a3a99dd49dca9c53
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:59 +0000

    past sick direct whenever help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
