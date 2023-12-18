3af43f27c32c878e008713c5622f26434102fd52
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:29:35 +0000

    name me remove find tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
