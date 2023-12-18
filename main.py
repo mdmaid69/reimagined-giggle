359397f2eda916d6d74d691bccfa3bc4d2d112a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:15 +0000

    post feed score smart land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_max(numbers):
+        return max(numbers)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
