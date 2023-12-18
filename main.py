ce5f21f6f44e15c79dcaded2677a6da1545a1332
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:05 +0000

    crowded while soil genuine pause

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def multiply_numbers(x, y):
-        return x * y
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
