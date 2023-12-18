e6a088f77062f33ad073d439f9cbd215fe8d589a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:19 +0000

    foot meeting Hh insist fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def reverse_array(array):
+        array.reverse()
+  def add_numbers(x, y):
+        return x + y
