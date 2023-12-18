f213dfb545c8d6c7e0fdd46b036d033a679d77a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:26 +0000

    fun go recover thankyou team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
