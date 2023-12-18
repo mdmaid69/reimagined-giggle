21166dae0fa7c2e2546be9460015e078dd2d7a3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:53 +0000

    reply body tell old without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
