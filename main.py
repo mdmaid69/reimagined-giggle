b4b967868e17c1c586540f19ac971fd08231683d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:41 +0000

    that approve Nn Hh ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
