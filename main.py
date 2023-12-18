245d3d688838972dbb9db95547fef89e76cd3fd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:43 +0000

    public we themselves 179bite everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
