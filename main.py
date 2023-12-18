10a2e1ad3478eee1c3f52219436455c0537c77a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:14:53 +0000

    wherever rare reasonable dress touch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def add_numbers(a, b):
-        return a + b
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
