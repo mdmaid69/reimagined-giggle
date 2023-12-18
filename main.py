45609efc96d939ee0bff3686d9c38dcb30b4b099
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:36 +0000

    false surface scale lookaround second

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
