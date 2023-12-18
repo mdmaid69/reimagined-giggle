db354155f02db74d36ef61cde07cbbe007519dec
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:26 +0000

    proper demonstrate suckme blind story

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
