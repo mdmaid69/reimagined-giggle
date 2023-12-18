a516da33c56a9dff3052c7ebc1d0e63067e48037
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:24:55 +0000

    obvious moral moment miss unpleasant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
