5f168b1d8959e5a98c304a51fd7254fbbbe887df
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:49:39 +0000

    minority rate cough terrible tense

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
