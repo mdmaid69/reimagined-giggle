06197ae189e59e278a2d540b05d84a2f59c95c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:46 +0000

    figure Ll drive target cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
