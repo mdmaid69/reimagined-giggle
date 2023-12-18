59d95e26dfe980713774302b4d129c0df52be563
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:21 +0000

    song happy foreign meet gap

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
