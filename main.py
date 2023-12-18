591030c5cfb112be1b9aff8e084b2a1bada42885
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:01 +0000

    people purpose previous sore Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
