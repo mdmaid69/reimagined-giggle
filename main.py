5bdd7c9c574a7f3a62ddb36b6e117e29e2dd220f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:44 +0000

    hope tidy description deal their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
