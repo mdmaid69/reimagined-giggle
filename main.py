3ff20c7c23a77115c9dff9d12265007e185ba64d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:00 +0000

    proud own effect fresh OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
