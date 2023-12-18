40c83d126ea4edb09742ca2384089a208705e187
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:44 +0000

    giveup well finish admit talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
