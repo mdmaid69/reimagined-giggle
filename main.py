20cc1064c4b0aafb3dbe3a49f0e7880b85ef9bca
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:21:44 +0000

    calloff dinner future lookup quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
