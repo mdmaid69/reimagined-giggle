3b73ced61c3690a0ee9104ddd452ed4c893fe32c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:42 +0000

    the soak hard obvious ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
