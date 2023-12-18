5a7711fede87de5d5ab0a54bc4629e306ecbfe67
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:49 +0000

    train long let butter split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
