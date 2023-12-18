e0c225d9d21268f4f4f6837efe9de62bd2045bbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:03 +0000

    somewhere steal bit disturb properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
