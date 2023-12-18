7ffc3d990b9429f873e31a928f3e0e892758ac17
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:02 +0000

    stare understand figure fault almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
