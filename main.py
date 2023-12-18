bbf4042b8da8f4382f5316302af8b1dd8d8cb30e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:59:28 +0000

    mind consider thought shock bar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_area(radius):
+        return 3.14 * radius * radius
