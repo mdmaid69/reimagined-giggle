b7f95acafbe8cd98e58ce03b191369690676420e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:15 +0000

    rain pin place event equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
