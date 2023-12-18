1877215817358837718fdd01a6d097057319468f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:38 +0000

    protest doubt practice together pair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
