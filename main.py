fbe293f9474091d40bf7ba05491c0b06a6b8c38b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:14 +0000

    spill wind holdout beyond journey

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
