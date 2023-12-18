d55e82d7c123067b9daa5ab572ff0e8a240f4a8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:08 +0000

    why man disturbing firm forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-print(sum(range(10)))
+import getpass
+def get_username():
+        return getpass.getuser()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
