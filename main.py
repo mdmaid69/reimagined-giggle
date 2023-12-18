511cd3ec2809bca216fb13bcaa27487316733cb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:58 +0000

    mainly another dare wet 565end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import getpass
-def get_username():
-        return getpass.getuser()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
