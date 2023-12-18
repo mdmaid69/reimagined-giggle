4dc089da3592f25fbfcb0fec0ed484e1368b9534
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:25 +0000

    deep war mean on nerve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
