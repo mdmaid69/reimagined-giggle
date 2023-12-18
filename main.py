89cec3f634552d4238b9fd7a4337ed534bee176c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:17:42 +0000

    imagination inside settle teach handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
