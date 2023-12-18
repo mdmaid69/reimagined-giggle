19d97f2a2a18cc369fbc42ccf412c432c5b97fda
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:30 +0000

    smoke accept terrible error hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
