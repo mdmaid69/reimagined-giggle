1d13b397419d71f89d71b6715e606913de399eb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:30:17 +0000

    least agreement suffer after clean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
