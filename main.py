03d564ae852397b8c2ba20fe1fb3e22dffd2c2a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:45 +0000

    display lookback(on) whisper transparent into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
