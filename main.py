a8d6fff23fb9b57ccccd1e9acded130f48eaf174
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:55 +0000

    after in close talkinto murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bool(array):
+        return bool(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
