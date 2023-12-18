83bf3fbdce716d665ddd3252bf7de296186570e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:34 +0000

    quiet high quick later pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
