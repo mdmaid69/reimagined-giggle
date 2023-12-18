5b91dafb7ffbf98ec805f7fd34ab69aa64c9f1e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:17 +0000

    lie cry scared eye slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
