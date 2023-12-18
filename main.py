98b9c60977d98c1d5d3124e46f57245e86f40ebc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:27 +0000

    gentle check excited future neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
