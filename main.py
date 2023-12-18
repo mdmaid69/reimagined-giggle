9cfad955a77503d64ce679a68e74a3636f82a0c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:37:22 +0000

    sleep regard sensitive trick surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
