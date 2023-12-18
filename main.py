6d855f9f3d6c5f9ee87a661beddab9d791dee527
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:03 +0000

    satisfactory smell move solve furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
