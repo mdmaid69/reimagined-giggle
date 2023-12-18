f79c7e377f61341fa5bf5930485344caa680a67b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:50 +0000

    odd giveup let immediate ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
