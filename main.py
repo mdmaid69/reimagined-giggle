4dc7f23399e055ae49d0c8a106db598d39c109b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:57 +0000

    recognize calloff previous big fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
