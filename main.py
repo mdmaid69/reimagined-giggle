cd6c8b1cb4ac74b51142375977db523780b28e42
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:40 +0000

    favourite intelligent protest begin world

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
