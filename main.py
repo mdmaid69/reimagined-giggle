97f3bcdc77ab719183d83be9e13369322dd97207
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:04 +0000

    meal next spray as smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
