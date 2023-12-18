c428130ee51fdc589216af93f34e38c338929c83
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:11 +0000

    permission warm demand quit attach

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
