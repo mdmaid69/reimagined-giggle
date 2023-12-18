8814afcfa0c32409896f0b7fea82f67c7e17d370
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:49 +0000

    congratulate disappointed ground emotion shine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
