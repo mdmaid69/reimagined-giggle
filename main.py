5263ae98c7adf3a6bff3a691e506817e05651444
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:09 +0000

    display broken consist hold suit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-print(time.time())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
