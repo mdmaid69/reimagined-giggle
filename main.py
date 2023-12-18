c681ae2ea59febdc76504b71c99e44ab2f57ef20
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:19 +0000

    party city about wipe throughout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
