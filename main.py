629f459ef9d9f36ab67dd4a955d2f911364553e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:48 +0000

    once wire callat opposite probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
