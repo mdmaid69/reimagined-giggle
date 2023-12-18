4735dd8cac0d06f03722b829de28e3933ce24ff4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:51 +0000

    passage connect awake change string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
