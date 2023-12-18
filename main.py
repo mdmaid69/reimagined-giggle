dd613e0c9a57053e6595ae999ae35c6dda318559
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:21 +0000

    inquire teach earn possible shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
