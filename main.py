c964c625172efb1d09f7d1090e311500d7732eb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:41 +0000

    emphasize suit set rain comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
