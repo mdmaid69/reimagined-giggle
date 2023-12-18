329313dff72087e39f8db853935fde4dcde5a48e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:34 +0000

    rush continue unlikely light use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
