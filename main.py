e8c59ffb55d5c7930a5f1452f5ef0f82aa85ca6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:49 +0000

    smile beneath identify and zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_item(array, i):
+        return array[i]
