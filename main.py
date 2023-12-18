5a92382c2a7bbc007291dcf0a21033caee988bb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:37 +0000

    ability difficulty poison slip instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def square_number(x):
+        return x**2
