b3eb92434f4a608287c6343b69e88a52574d79bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:41 +0000

    zero way sticky decide her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
