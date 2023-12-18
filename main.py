ce66430d05d2508afdea2b89abdb198f1df2677b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:39 +0000

    thankyou tall big I stir

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
