5a34ba15f059076c85016092063eb8fe3d1eb0d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:56:30 +0000

    fly light age scene lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
