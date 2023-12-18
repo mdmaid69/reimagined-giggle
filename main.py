8ea4761e616ae6df6648ce725821f8f1e0b34deb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:30:40 +0000

    stay string mine shadow uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
