8caf1aaa361b759d3ce2811ebc87a019e24e0fa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:16 +0000

    rest human callat stare sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def remove_duplicates(lst):
+        return list(set(lst))
