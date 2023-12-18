56c521edf6799fc5b79fbf042a1b78b345d12c86
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:20:07 +0000

    hospital have television only hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
