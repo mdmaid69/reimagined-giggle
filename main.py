bf988f92f9489f23f80635a944fd46f3dc52e4e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:54 +0000

    unpleasant across learn succeed marry

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
