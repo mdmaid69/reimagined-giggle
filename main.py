f689cfdebe0f33c1894d3e7dc62a19d6493afbfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:41 +0000

    view practise influence pause set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
