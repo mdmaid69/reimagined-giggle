21d697b03221ebdfbe8a84a8f5d71c497f0a503a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:27:27 +0000

    often everyone before difference tall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
