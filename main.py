eefe206d39d8ea2779e7da973c540f2a47448f11
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:12 +0000

    settle angry child trust crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
