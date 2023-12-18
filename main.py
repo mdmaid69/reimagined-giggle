763905f02fa203624b9d6beb528a41711e1d8668
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:07 +0000

    takeoff close create possession television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
