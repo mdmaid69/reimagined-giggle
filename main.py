346f6b34dcc8f4d6712bdb972f09dd05b1f214bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:01:55 +0000

    sell transport under spin in

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
