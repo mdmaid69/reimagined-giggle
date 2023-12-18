bb7973938a046ff342fc2dd55a4331512dd1d563
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:02:51 +0000

    kind interrupt you middle attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
