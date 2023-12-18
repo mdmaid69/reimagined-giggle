9da98204dc3d490f1494f4029850086b3f6195d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:35:11 +0000

    mark couple pair smooth sudden

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
