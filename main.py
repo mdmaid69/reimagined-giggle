811640ec83b9a12f42783f1afe0a1ba3924aee92
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:01 +0000

    join very shop rich whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
