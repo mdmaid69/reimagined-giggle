27b8cc484c61fcb4d157123894b60985122b961b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:45:42 +0000

    ahead professional because refer middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
