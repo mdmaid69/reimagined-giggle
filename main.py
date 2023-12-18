16761a558fe431eae7828973da75901137b79bf5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:28 +0000

    lead freedom decide clean dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
