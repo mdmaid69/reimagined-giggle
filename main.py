2f11c047a1e3efed554ded27e41abd7d7c5fefba
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:26 +0000

    spend test extremely tune 568energy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
