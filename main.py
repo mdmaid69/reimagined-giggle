a7b3860da40dbbace41940ff162a582516511286
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:40 +0000

    under list tradition lookaround comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
