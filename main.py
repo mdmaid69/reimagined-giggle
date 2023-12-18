5b5bb8b7ef8b571aecf1f91d987fc4bc691c705c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:30:35 +0000

    probably consider maintain rate sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
