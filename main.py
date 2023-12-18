f133608d030aaa0d393711babe91ea72b8d0eee1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:42:14 +0000

    complain secret until normal eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
