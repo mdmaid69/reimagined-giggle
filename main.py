fe412ccdb587f572e53a4459c05969cec3b82bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:47 +0000

    face cruel modern movement scale

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
