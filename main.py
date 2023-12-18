808dabcb1f1b130d122a834a92b7b76a41a76239
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:49:29 +0000

    direct union not nor clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
