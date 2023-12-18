9259e38db1ad3ab5c892ed31a8cecd6dd968262d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:44 +0000

    develop play many period rely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
