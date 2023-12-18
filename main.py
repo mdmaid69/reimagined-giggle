2fd570b6417a99c97b06bce7a9ee6f7d0f94fb2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:47:40 +0000

    want seat miss Ee inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
