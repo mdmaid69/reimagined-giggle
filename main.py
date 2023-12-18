11ecee76255d8e48660e43b58c3598b6840b9217
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:11 +0000

    sound pressure detail there total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
