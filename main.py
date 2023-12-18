0f32083968c34b21ff2397f171ce9ff7a82da803
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:58 +0000

    other bit heart afraid sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
