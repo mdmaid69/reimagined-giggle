3786151adcc3262169dbd80fc8b4058b7769709b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:44:50 +0000

    worth the hour little breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
