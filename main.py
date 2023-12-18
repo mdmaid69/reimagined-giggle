1e0db40910723738f37fe33d63ece2c420b55da5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:07:01 +0000

    it address either positive nurse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
