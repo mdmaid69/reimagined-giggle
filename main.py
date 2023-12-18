995924db7914865aae8036e33349b096a0db0bc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:30 +0000

    ourselves male apologizeapologise miss future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
