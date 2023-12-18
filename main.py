9324d6078f0f8c0e9b62d7a3cf6e9d1ece6efec9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:03 +0000

    proud distant force sort bother

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
