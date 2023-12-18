e8d10d45f71523155586ea901595e07eb823aed9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:18 +0000

    he black friend split various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
