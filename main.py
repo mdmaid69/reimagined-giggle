f595497bf87a9c785fe7b8282a012bccfa14fdd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:16:27 +0000

    benefit mean discuss future couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
