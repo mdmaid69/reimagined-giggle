14f438c97fa26f85107ddd2cadf594721b99146b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:30 +0000

    mine makeinto idea dangerous stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
