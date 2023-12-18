0e808fe5220e2b082b30d20346b36c2008b157c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:44 +0000

    demand mysterious bunch lose swimming

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
