0d1126bfbaa0d3ddadd8516a5367d15026a1da23
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:57:58 +0000

    sort side agreement excellent settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_max(numbers):
+        return max(numbers)
