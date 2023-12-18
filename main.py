9079586a0e0239908a7ceaefb8ebf3273e8a7d91
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:46 +0000

    present play present make unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
