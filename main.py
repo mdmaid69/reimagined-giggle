bbffd48163e0757da50da1f154103616dd1e36e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:14:04 +0000

    open accident soon artificial case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
