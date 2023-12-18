2a5612ad97833d8ce2dcd24f320702ce7db1e67b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:04:55 +0000

    squeeze shut unlucky mark outside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def reverse_list(lst):
+        return lst[::-1]
