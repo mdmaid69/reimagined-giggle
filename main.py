14aef72b122628c84b1b50032445c2b1ba361325
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:44 +0000

    period drop cover early from

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import tensorflow as tf
+print(tf.__version__)
+import time
+print(time.time())
