5d097aa6c696172e943db91d71b074c7b57ccd2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:56 +0000

    answer congratulations within begin lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
