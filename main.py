02a14986ec8f9ea76d8667b01cf6d7f36196671d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:20:48 +0000

    single because record shock heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
