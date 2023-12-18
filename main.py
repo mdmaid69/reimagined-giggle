ec612558696b235f8e6a3b92511587aa7e21cb91
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:25 +0000

    film hard division support in

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def change_working_directory(path):
+        os.chdir(path)
