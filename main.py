3b1c56638cb6867f69d134cf0cb00647fe9841aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:25 +0000

    temperature degree Ww blue chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
