a9814b9d549dcbab81c76be3f805c738399287e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:25 +0000

    trip today breakout typical importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
