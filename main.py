8689b4cb76efb5db0164b2497b4b81bcb7019e68
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:53 +0000

    part pot hard whoever force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
