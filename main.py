d3e85395e3b2690ff47d8f2ea3a13e468ee81fff
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:11 +0000

    trip frequent sister giveback whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
