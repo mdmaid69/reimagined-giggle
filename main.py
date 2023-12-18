5a375b3021f7277536c1732f07e4b7acbe9d7117
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:16 +0000

    little idea speech gift help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
