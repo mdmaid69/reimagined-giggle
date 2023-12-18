b603a932f05d14890dab48435f9ce6e5b83d1171
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:36 +0000

    again oil in style arm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
