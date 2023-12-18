2fefb14579c1cd7921ea09cae1a6b7806a60b2cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:21:42 +0000

    exciting formal pure friend scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
