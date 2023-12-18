22f4639577cb20592e81ff35649dc74d0f9a6882
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:58:28 +0000

    rub safe mail talkoutof journey

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
