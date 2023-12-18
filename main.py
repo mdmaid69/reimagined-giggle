572b8bdf3045cc59b1beab1f41fc48ba545ae523
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:01:32 +0000

    pleasant blue 566end punishment central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
