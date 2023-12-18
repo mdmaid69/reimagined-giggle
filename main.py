b7efc431e516085955e7fb1ca8243d5cf6c0f50d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:56 +0000

    dinner try firm art possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
