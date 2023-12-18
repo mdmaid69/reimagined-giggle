ee31f86569a0c2c743ed1f761f36c061573776f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:32 +0000

    feed pay turnback spring behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
