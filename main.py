36d48fea81f6e46688a6345f51636d8972cc2ffd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:17 +0000

    dead equipment education fence experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
