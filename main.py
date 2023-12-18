7f1dfbcc5301b8973e4c8d9608ca3e6b91a7b067
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:32 +0000

    present method regret cool today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+def get_today_date():
+        return datetime.date.today()
