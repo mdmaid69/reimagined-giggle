8f56e6490f953a4c4e3d689ac06f168a77cf17d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:08:31 +0000

    radio goout leave borrow talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_str(array):
+        return str(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
