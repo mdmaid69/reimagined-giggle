f2bf60c3cc29d9396ac003681a7843546ca2e041
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:26 +0000

    spring pleased skin method opportunity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_work(force, distance):
-        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
