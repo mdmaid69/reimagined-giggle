7126cdbc6dca291aa4f8c231f309830daf778cfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:39 +0000

    comment talkinto case arrival trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
