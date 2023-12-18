020d78af86cfcde996b1bae7be2b1feed6b34e4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:13 +0000

    cruelty plastic would satisfied proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
