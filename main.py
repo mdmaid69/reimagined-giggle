f0f883a0510dcafaf1faa3176a094ec1de463e02
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:30:11 +0000

    march private bringback ticket medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
