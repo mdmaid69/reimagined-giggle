615cec1d6a52761ed1eaf52caa63cc98f266b74c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:35 +0000

    action plan whichever tired situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
