1c14dfb6e3cc2343d1471bafc37822b60c7eadce
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:30 +0000

    tip disapprove turnup miss intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
