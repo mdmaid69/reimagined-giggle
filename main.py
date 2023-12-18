2090d21f094e4158e04bd8eeead1a2ec4f9caab9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:55 +0000

    legal opportunity snow connect reject

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
