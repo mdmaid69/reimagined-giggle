1114695ecc50ed519f7b1ac82e1cce691a0a1e54
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:38 +0000

    hurt course but slice in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
