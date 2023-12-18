7cc2df8eb06fda51d89300f4a20c44cc67c88c76
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:21 +0000

    work hold fortunately rest nice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
