3d1fad1f7bb3cc1256066eab3ae91ecdcdb2e1d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:33:51 +0000

    bunch natural level beautiful into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
 def convert_array_to_bytes(array):
         return array.tobytes()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
