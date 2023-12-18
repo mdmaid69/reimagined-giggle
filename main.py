fc16ddaf9b547e3ff51066948597da6a3c346f8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:30 +0000

    hospital pen point share share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
