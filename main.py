de95d484f12ea8f7addcfd23224ab70ac9d8fedc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:15 +0000

    satisfied subject dress chance blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
