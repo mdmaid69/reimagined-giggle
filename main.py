99d7361c3782dbc34f9e29b8b47257aa01ae1b28
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:00 +0000

    terrible keepup his tip top

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
