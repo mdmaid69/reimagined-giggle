05592193d09b249ec78ed5e3e48b368282c901fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:39 +0000

    worth plain concerned sink rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
