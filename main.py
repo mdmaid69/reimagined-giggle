2b3431be596f912d85a73f43c4495727b4e51adb
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:22:51 +0000

    little supply minority run base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
