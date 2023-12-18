4740d92c75711d5c4a9f5d0e72d763f86677a690
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:32 +0000

    little illness close arrangement fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
