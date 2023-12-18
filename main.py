3a04f4b591dd444bc7753bffe35f4d597784c0f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:11 +0000

    gooff mix combination 179bite into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
