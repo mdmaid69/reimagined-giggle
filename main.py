c74da133226556c041a2f0dbfd9c552709e3e766
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:38 +0000

    earth permission declare prize production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
