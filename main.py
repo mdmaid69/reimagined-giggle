791b27ac51ca82eec35d495d5888cdfd008ca526
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:40 +0000

    with legal formal makeup holiday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
