9665c3e54957cd0644c0634e9610ced3164d7429
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:23:35 +0000

    crime 566end mind reduction dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-print([x**2 for x in range(10)])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
