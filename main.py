a3e7682ecce88f998b46bd944bfd08611fc1ff44
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:28:30 +0000

    out giveback connected nowhere feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
