58d13a4c4c10a5177e3dc3eb37bf4505e9dd0f13
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:14:18 +0000

    and judge female busy tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_speed(distance, time):
+        return distance / time
