7df7a21f867ec0cfa1eeb42283e5d4d4257dab50
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:50 +0000

    rate prepare protest work contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
