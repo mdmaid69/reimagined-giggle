6eee4c9392d1d082d6d23ee2e55754712faa9154
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:16 +0000

    butter sense please ilovedicks too

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
