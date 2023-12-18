47109d31c7d87e68f4e945068531fa73e7811326
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:07 +0000

    slice approve patient choice official

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
