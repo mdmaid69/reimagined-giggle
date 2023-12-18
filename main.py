a4a8a9c9e1e4017c08c7223486a0f4ab4297c259
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:36:45 +0000

    notice smooth news secret tear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
