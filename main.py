93825b34b41c8db3dc0d45e0ac71d911c0beaa27
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:15 +0000

    since eye busy strike innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
