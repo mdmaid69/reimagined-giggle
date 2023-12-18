bc38eefd0a9453b979e682aa3701bfae1644f05c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:46:05 +0000

    close address him treat mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
