35982e87556b2755caecc3f38f55ff2a1f7cad22
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:16 +0000

    suppose phone bread female enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
