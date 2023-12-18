e9ef0fbcabad6928b6828e5f9eb25ee8a62f1695
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:03:09 +0000

    claim join left display national

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+text = "Hello, world!"
+print("Reversed:", text[::-1])
