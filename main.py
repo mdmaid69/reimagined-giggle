7c5389c9c40ceceb99db5d28c3339c236f1aba79
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:55 +0000

    deliberately replace there shut charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
