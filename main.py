5d554c7b53b06ae20db306ad1b7c5f9554c44052
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:28 +0000

    round business otherwise chat nowhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_even(n):
+        return n % 2 == 0
