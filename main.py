4e2d6f3bf616c39b4ebb70ce86ca3af1f9afd34f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:35 +0000

    table sound like far pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
