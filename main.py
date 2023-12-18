120c3779c087516791bc9854da239544a4a6e813
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:58:55 +0000

    past breathe nowhere common abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def get_time_since_epoch():
+        return time.time()
