82fc8d7a3e721d7536c1f543a956fe139cb31cae
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:25 +0000

    forward keepoff glad onto escape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def convert_to_octal(n):
+        return oct(n)
