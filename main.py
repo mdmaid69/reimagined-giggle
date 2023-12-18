173d2152859492dc3fa9b0faeaa316d2f6c8d4f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:05 +0000

    lookaround mad extraordinary sharp touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
