e15cb1269c3d0cb056efea3d1e854834b79d59b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:25 +0000

    rain whichever couple drink union

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import sys
+def exit_program():
+        sys.exit()
