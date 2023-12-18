68ccf51d4d10cb4e2202b88a2631a873e8572396
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:31 +0000

    bill month shine vision represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
