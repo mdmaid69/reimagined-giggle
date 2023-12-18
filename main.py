d1a6cce21cda0f7d6c92df747580426c8b375f1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:14:33 +0000

    model respect burst calmly rate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
