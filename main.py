2dd19cbc4922b85f56b3e77f5d73569680d00e71
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:31 +0000

    tough personal juice takeback risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
