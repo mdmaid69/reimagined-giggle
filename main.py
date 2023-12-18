9433e36121cf9940047a46a8ffa309f09bd16caf
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:34 +0000

    service concentration classroom stamp squash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
