78f754332412455b3ed361bc9cbb5110d83254c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:49 +0000

    unite still odd appeal present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
