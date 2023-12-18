fbfd63e507fb00eadfcbde95c68dc44f402adb0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:59 +0000

    contact large dish keepaway grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
