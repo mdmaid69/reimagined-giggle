69c7dba3c18b3d183d39d464c70dbde4c4c8c998
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:50 +0000

    whoever dance border Pp original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
