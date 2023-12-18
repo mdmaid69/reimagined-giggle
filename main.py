384de89e23edf990336b41cdba06c14930311828
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:09:29 +0000

    no only concern tragedy any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
