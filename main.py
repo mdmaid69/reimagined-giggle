fced74b92e0ce35f9e2ebdb18c39c86eb0d98b30
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:02:07 +0000

    bringforward possible side confused invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-for i in range(5):
-        print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
