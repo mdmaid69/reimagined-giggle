f47ba26083c2506ee38694977f639486020670fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:26 +0000

    bread cruel dry freedom complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
