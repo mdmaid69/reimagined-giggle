47c51b7af2b15e6e2c3db6f4ea27de6f6bc77e8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:14 +0000

    about person workon properly this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
