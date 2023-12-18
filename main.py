794edf689bdddd3e1c07074a3b9af621f1818f5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:31 +0000

    short advice likely trust house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
