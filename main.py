0d55ba499cefacce3bc950b9a1ac2a952a40cf18
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:56 +0000

    different charge write brush avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
