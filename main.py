f1dae90c01ceecbc6e5c995791ccf2e0cc127dae
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:14 +0000

    yesterday properly before noone sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
