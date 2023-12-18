3f4ca0ab710e9a5bd36c70385e9e02186897c91a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:49 +0000

    main empty surprised contrast forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
