2d744867ecd05789d3fdad15409600d940adb7a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:49 +0000

    habit Ee centre dinner tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_current_directory():
+        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
