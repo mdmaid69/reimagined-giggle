d0433753bed13c816c8bd45721e738266f569ebd
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:28 +0000

    recover reaction attempt keep increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
