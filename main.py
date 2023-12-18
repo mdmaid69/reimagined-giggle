54a4a99147064da17e5d53950ec871928e875022
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:38 +0000

    confused theirs let beginning relation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
