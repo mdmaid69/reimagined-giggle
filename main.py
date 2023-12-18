86fbc22fc3e5bce710121604d7e37d1cffde5182
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:42:07 +0000

    prepare agreement past about joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
