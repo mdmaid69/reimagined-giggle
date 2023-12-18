cfaccb847a13fa79dd388e49ce1063bb337ffc6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:38 +0000

    fire press smile ahead reject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
