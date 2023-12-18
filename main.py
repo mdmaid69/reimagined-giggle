41c5a9ab04999254a6cd63da4c4bc54e8918a4f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:37 +0000

    copy care sea conclusion 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def square_number(x):
+        return x**2
