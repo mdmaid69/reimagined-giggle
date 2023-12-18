8a83f42818cedf054740e221cc18579c0700f891
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:52 +0000

    well back lookafter square health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
