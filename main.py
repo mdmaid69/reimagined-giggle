35e8815f06fe3c6e974f5ff878a5429f02d7de6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:15 +0000

    uncomfortable accidentally must about proper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
