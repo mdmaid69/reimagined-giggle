9214938e7cc9892d85b9b88402c226b3106910cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:10 +0000

    point hurry idea team edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+print(os.getcwd())
