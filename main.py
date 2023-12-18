66517a9eaba14c325fdf85a881706f80a9fdcb9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:15 +0000

    for decision thing positive replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
