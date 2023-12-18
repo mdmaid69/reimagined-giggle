877194fc7ea03cf57078c4cfc68a42237a8806ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:55 +0000

    profit apologizeapologise tall air 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+print(os.getcwd())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
