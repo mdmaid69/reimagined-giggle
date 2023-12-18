66b59b0ccdfc2142c20fe3f4c48d1a7cdfc9d3f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:23 +0000

    guess owe mix bend wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_current_directory():
+        return os.getcwd()
