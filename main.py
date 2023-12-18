0bda9762a936ad11b3f533c1f4073a11a3cc19de
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:38 +0000

    cut manner sense watch nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
