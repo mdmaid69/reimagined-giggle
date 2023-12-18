f978c0d43e7b468a7b4c5bcb7e2a2ff60b46dfa2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:15 +0000

    centimetre hole advanced concerning heavy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import random
+def roll_die():
+        return random.randint(1, 6)
