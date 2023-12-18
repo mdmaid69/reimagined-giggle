c83d2f9364d70655ba2f12d62b24a9c074440c98
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:18 +0000

    female interrupt throw noise actually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
