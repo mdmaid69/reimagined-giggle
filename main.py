562fcf5d70908dbd82a972ebc1be0c5a9510d44b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:49 +0000

    branch object drive general surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
