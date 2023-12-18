1c635299a567f90e3e832498da3616da9098a704
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:46 +0000

    further downstairs minority pile pleased

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
