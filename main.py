1e0dff92487b11d72a7fea459f87ff90c40c9498
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:45 +0000

    middle public Ff team final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
