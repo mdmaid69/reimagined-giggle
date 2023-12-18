4492e7519a85d2815d26595f6caac881bc87f112
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:43:01 +0000

    last drive surface change distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
