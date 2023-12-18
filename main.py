8f6b685c835f41693bca04830b339a8de0b087f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:29 +0000

    comein history different strong professional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
