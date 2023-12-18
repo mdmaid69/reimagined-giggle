7efc028d0d2e7b1d6d8f01b3167b6f46a18dab3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:40 +0000

    lock ahead either reasonable until

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
