ee5419de8b4d0eaf83665657646f3f230686ee0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:07 +0000

    history doubt man Zz circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
