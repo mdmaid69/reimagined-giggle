6ccf9d0d4da8c3791a44a8a49057ad83b84ce576
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:41 +0000

    round figure relief getoff order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
