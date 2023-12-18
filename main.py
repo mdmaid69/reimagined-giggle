8e53fcbf2fa40526c452262db805abd26713535f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:28 +0000

    lower while only back store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
