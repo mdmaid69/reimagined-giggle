ed4c08ebb5a010d370aed7f719b135bf5f210a6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:08 +0000

    live that rain worth pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def find_min(numbers):
-        return min(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
