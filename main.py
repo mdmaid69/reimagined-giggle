4234183d22271512946ae9f81a4b5d63b12eed07
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:46 +0000

    exchange container 567enemy expect wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
