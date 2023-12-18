afed3c125b918e8533f39a65183427853015259c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:49 +0000

    passage plant maximum physical get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def square_number(x):
-        return x**2
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
