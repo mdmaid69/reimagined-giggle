c2fb9affb793fbfe8ebea8a27e29b81c57f0c704
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:13 +0000

    shock tall suffer suffer later

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
