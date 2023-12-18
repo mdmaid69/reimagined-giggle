223c05197bb4aae041af9dba804f1e233d79c931
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:37 +0000

    split charge complete need worst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
