347c1eb7678d443fa41ed418809461096f287f11
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:50 +0000

    point gate upwards boil brief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
