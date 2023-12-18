17a7f514deb86b463e519b7b5feffdf5140a9171
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:58 +0000

    country various back difficult address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
