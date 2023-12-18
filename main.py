822ff62cef272fb03505d9c45b94ed8fc01045eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:09:17 +0000

    crisis answer address lift impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
