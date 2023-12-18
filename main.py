137fbd2ae8763cdf1752ce509ad12a659ef3a331
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:55 +0000

    runover read help between steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
