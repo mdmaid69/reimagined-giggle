d507f9707ed433fc70d013354e9909c43bbcaa1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:12 +0000

    rise noone on centre tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
