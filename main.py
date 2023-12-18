7eee63a1d87a63f96ad44128f642a68ca0863087
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:48 +0000

    press getup try force conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def count_elements(lst):
+        return len(lst)
