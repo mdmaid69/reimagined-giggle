26cb4c100deca74aecbf77fb44a999898a7bc6d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:36 +0000

    plenty favour buy might mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
