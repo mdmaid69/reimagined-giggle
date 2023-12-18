5a42969406f4f1e4a41b43d9cdad6cb334654156
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:36 +0000

    problem theirs operation behind usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
