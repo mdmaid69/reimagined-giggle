dbffb63196e9c0336959c437e74879583ba99f65
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:03:37 +0000

    arrangement do obvious moon surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def count_elements(lst):
+        return len(lst)
