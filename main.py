ae25e350b01df8d1b569fd5fce0cda476ac99716
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:31 +0000

    majority chase frequent later claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
