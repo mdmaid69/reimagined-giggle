35bcc014faf767760e482045e34307fe3aeb8251
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:13:57 +0000

    important doubt soil independent description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def reverse_list(lst):
+        return lst[::-1]
