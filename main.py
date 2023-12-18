1dc69e665cbbad761b009769660f03a386adb302
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:55 +0000

    lay bath arrangement scrap type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print(sum(range(10)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
