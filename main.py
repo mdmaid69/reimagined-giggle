daaa55cde80574c2ffb60d2270536d17daa8fede
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:15 +0000

    us crazy godown fortunately wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
