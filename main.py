12ee58dbf3bace1ebe6b92989f67824f5bbc44dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:05 +0000

    patience tap hear turn anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def convert_to_hex(n):
+        return hex(n)
