90bfe3b1199f20df07d98fd63492c565a5f1f7bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:06 +0000

    exaggerate base female male stand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
