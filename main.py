30628d9db05b2c05bff1e4e5950e2c0b12e0bff7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:32:52 +0000

    especially rubbish within shall pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_tangent(x):
+        return math.tan(x)
