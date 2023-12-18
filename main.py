2ef59d528fade7b4f61a8afb508153f326b49f16
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:01 +0000

    on slight attempt ordinary only

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
