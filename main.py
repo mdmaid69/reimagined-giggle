2b27cf4d9208ff373ab3967a708483047cd81282
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:36 +0000

    exit one trouble swimming substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
