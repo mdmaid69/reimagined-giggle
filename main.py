f91aa52b9f16d7ff51dcb69b42fe361b8eba0f36
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:11 +0000

    pleasure present season condition smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
