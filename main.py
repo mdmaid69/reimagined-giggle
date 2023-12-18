c2f36dd8adb730851b049ff04f260462cf1e2258
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:30 +0000

    related else cheque guilty track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
