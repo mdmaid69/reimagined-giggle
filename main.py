b1989d0734328917a41f67518d03c3dd2a8fed99
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:54:13 +0000

    wish busy spray autumn someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
