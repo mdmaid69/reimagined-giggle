3124949f3695f265e62c3f6274bdbb6384437e89
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:26:17 +0000

    whole never will refer nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
