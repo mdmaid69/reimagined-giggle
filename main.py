418a89797f6ee5c6b3afbd2828c403cba49d711a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:44 +0000

    main patient keepout gas unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
