04413d55f0df86608172755fbc724fe1359eaecb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:27 +0000

    classroom breakup keepaway ashamed hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
