7ceb3aa5acfc8ed74323ed21597f60100687da67
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:35 +0000

    how underneath comment shock face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-print("Hello, world!")
+def cube_number(x):
+        return x**3
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
