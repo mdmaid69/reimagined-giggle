a924144017c4f48afd0ac984b0f2137141ecde33
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:04:37 +0000

    miss container confused listenfor healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
