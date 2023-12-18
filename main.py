088efa3fd817bfee8cab6320bfc4497e31c7ae50
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:53 +0000

    likely cry bother deliberate stare

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+print(datetime.datetime.now())
