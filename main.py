dfc405b48fcdbd82532d8f81aecc48d3bad74d83
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:24 +0000

    teach juice fear trap winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
