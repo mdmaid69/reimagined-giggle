0e424167e25d2c281019eb15bc8a0bcb4f539873
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:36 +0000

    number fall choice worst achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
