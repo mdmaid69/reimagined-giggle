195c1043f103f590e8a8ba4b987fe788ddfdd908
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:33 +0000

    whistle question cry achieve top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
