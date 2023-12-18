c3df45ef4f01dac593c6a4074d67e357bd582b2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:54 +0000

    receive Pp again ever tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_time_since_epoch():
+        return time.time()
+import numpy as np
+print(np.array([1, 2, 3]))
