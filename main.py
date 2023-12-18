64ed16fc1e667a2a28c4d6473380b84cc0d6e80b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:33 +0000

    slide comment admit demand this

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
