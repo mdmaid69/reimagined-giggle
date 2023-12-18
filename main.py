f16b89f578bcca9fad063ed49e732347b663cc7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:27 +0000

    recent callback 572enter nigga prove

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_octal(n):
+        return oct(n)
