541dbb105b86a10af02a2c8dbf5bd6b64d61158a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:34 +0000

    Hh pity emotion sticky getout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
