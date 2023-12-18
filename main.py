eb6ecd9d3fd7b827528b2da66e5cf413aff9531e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:53 +0000

    type promise sentence work centre

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import socket
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import queue
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
