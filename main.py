ed4995e958ced29086c8c6aa92caf67272a8bfa8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:50 +0000

    open Ll apart experience Yy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
