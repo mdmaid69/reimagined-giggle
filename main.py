f1bda0967e527b88a5dc630cfc8489f35d0e71cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:15:00 +0000

    real fold breathe set slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
