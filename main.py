c884a4f4f42160d722215b024656f8269a46dc6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:11 +0000

    tool trick situation pile female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
