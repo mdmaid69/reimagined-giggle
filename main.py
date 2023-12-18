df2f72267168b5090525c3eb4e47d842ad638fc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:18:53 +0000

    boring doubt basis forward block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
