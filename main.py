323f8a82f8437e68c835dc692a8d518fc6ea92c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:34 +0000

    card flat into experiment 572enter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
