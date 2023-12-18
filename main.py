c2f8acf34bf1c612c9d900f2b0c9403802614c2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:53 +0000

    content though under goup further

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
