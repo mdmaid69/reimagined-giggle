cc1beeb7826108adfb6f187d535001ceac6e3c05
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:19 +0000

    skin translate active sleep pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
