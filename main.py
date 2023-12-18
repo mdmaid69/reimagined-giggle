817559425d2da3d0486a2e78627972e6624b5410
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:17 +0000

    prove unpleasant scream narrow satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
