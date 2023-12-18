060c58aa4531cfe3e7ac38ea83267760eff13f21
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:09 +0000

    single expression deaf excellent bank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
