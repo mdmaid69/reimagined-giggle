e7f8fc8436623e8220a0b4fbdaec91224953cad1
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:06:03 +0000

    always touch order under lay

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def reverse_list(lst):
+        return lst[::-1]
