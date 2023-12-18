a5aa80d2c084e06887a672e2e677990e94d286a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:03 +0000

    determined battery argue identify embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
