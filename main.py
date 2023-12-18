0c4dce52b88635e01c16d45d3a07d2bacc8ea214
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:49 +0000

    visit simple opposite away bank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
