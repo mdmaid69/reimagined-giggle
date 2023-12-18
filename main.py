60f221c05e9d3bd892a56d931410db254879000b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:28:12 +0000

    rise issue chin trial question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def square_number(x):
+        return x**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
