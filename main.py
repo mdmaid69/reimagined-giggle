50117fa43d62825f74160da597fc0aef493f1612
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:20 +0000

    over suit 567enemy door experiment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
