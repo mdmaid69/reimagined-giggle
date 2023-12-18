85298940eae382c7776ef4e5e7bcc7e88e1e3aca
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:11 +0000

    shout increase clothes charge back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+print(os.getcwd())
+  def is_odd(n):
+        return n % 2 != 0
