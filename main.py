7a491b33e35b7f8691d8ced08f3be978741a62cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:21 +0000

    stupid release juice official not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
