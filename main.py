b84f0a82bf4656b073c752e94d0cb7fa60cab934
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:05 +0000

    proper makeinto lift belong position

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
