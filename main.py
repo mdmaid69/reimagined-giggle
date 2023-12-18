c97753a5d373410cc84482350df170c808603bab
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:12 +0000

    away spread concerned listen schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
