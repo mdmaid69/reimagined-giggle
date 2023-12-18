e8c04f95a77b8a7d75409e6995bbaf51e8a99c49
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:19 +0000

    sweep obtain the kill wire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
