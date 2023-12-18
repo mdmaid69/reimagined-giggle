30996925e3c78711426dbbc33ce81688ef9c50d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:07 +0000

    ride wind interested connected avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
