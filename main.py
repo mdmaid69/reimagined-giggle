f95b6b2d63cdcc6e93cbd8133e37aedcc06ff31a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:23 +0000

    city variety herself surprised single

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_speed(distance, time):
+        return distance / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
