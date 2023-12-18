9f0f336c82dd3ea0a5a226dec07574ce13338c63
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:43:08 +0000

    fear agree period crash discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
