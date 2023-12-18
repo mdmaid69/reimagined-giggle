e3cbff7c328672c8c97066098cb9d8898a518db3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:33:45 +0000

    place ought shout emphasize factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
