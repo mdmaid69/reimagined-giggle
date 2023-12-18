4e1c8c3c397732c774c763237a6464966481968e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:40:59 +0000

    replace keepaway luck deep test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
