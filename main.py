8cc285c56e56e0b6bc732e06a504f191794238d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:18 +0000

    ours achievement clean powerful guilty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
