cb09d5491b68b2e308e9c299d14f106bc77061f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:37 +0000

    unable noone spring mistake regional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
