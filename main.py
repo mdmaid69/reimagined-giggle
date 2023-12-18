0910d073739c7524d9a7024e54fc0605a41ef886
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:03:35 +0000

    from claim autumn challenge valuable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
