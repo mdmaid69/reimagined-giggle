c8706d8da21dc375a25f3332b424e2a2ff020172
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:29:54 +0000

    undo fire major emergency equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
