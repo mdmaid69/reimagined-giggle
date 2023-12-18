2eed8becd000f610cbc2269529a14869cfce91a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:25 +0000

    hard street out old remember

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
