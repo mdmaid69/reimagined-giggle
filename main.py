ee17c0bd7eb876717e96b1c20aa06f6b190c0062
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:51 +0000

    should free judge free estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
