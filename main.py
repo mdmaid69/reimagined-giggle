c72bd2925d2a633aad57cbb4dc51b7ef41b6b79c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:33 +0000

    arrangement furniture act again pressure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
