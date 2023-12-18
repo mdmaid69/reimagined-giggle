695d00dd59f035added259a9a32cdafe08e76348
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:46 +0000

    run mystery ourselves couple firm

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
