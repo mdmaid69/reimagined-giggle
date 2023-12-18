d6c6a19649895580896629061f3e90a2c5b1a35d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:14 +0000

    criticism century fix port possibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
