d412c75e69130de672bf73ca76654b106fd8c7dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:43 +0000

    trousers anywayanyhow target hungry lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
