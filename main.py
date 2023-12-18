fef5491860e5a484b13aab4742ced5dc0dfb27ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:16 +0000

    tread possible march hurt offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
