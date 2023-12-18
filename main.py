974091834ff4daabab49b2d5d75301b7027feaea
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:12:09 +0000

    custom when allow willing powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
