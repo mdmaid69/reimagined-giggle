5f2b272c3a261d1f18d8c5c0aaaf785a01b51801
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:49 +0000

    lookaround little similar rain visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_power(work, time):
+        return work / time
