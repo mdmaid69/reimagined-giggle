2129d3dac465a90507c9966f9fadb8e316e4499c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:37 +0000

    shelter friend rain complaint strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
