9ce1e903abd7818997e62787903f99053f9e649f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:48 +0000

    lock I talk game rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def cube_number(x):
+        return x**3
