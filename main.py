e7c6906e73788c3a722d3fba120d1ec080da2763
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:37 +0000

    please jump yes old classroom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
