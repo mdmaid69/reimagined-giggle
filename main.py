79d289852ab7f37b1ef96ff1b8d0131555b9cfaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:39 +0000

    wipe fall operate deserve or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
