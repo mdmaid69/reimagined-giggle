7f41dc6e97a17cd87a5504398591ef6a39b05a38
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:45 +0000

    distance crowd reaction top earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
