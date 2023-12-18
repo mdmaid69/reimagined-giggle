89cf3f2ea51a695d16b470b3f3c573807d5afe4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:43 +0000

    scrap fit breath post science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
