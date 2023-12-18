fa7c48d64797c06bee2b2060d73692521ac689bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:05 +0000

    turn block raw flat cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
