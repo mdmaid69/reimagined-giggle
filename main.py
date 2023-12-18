4b04eef2680e0186141df99d9c64595f0c83e616
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:06 +0000

    leave sense table fairly excellent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
