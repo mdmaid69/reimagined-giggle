5953e56fa9a66ac01b514376be3d02eacced1e47
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:25 +0000

    empty trouble blame ourselves risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
