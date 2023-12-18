0931260f06b513d9f9d27facf16c9d5e7d3cebae
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:05 +0000

    steady accidentally scream unlucky Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
