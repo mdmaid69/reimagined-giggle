e433725c471dcdd71fae1203d4b4cc311e84743d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:47:21 +0000

    area autumn hungry method yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
