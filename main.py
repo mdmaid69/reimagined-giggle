2a0e3dcf0b9d23c60474b13396c7eb057c82c86f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:25 +0000

    pretty extraordinary destroy compete broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
