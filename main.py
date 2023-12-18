fbf49e8805d2bbd48eff8604526cfd522211dbe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:00 +0000

    neat power lookup wide cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def cube_number(x):
-        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+print(re.match("h.*o", "hello world"))
