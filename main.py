3d188d71f16f877d6afdafd6d782bf23e4505ec6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:10:56 +0000

    adjective float shine Ee stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
