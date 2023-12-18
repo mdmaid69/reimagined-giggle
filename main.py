ca31e11e888fe7d2fb1000e747855f0268472214
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:27 +0000

    finish but add improve congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
