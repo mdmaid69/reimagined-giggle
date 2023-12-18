c3a45cf6e62b0858114170e70bd5a6923bc967b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:33:23 +0000

    shame eat gentle luck exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
