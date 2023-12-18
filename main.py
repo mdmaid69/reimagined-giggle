ba8b7055cd94ee3f19d45d3f48ecb738016c6bcc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:00 +0000

    earth children only same wrong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
