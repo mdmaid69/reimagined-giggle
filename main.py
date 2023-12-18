b85fbca3ae3a020273bdb7f7af43a9ba1693843f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:21 +0000

    judge serve smart rush represent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
