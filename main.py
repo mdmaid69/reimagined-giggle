2761852a458b43d2213e04eacc6aa344896fc7ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:52 +0000

    ill file bang match join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
