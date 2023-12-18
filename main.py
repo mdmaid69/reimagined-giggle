f50980224656f207050b05b34316638c541eb002
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:33:38 +0000

    hit you head deaf free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
