c4a9d559a2e32ce63f9c6e1d6c9271ceae272214
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:37 +0000

    stupid follow measure goback(to) unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
