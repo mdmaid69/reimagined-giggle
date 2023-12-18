9bd01cdf29c04c8a92c4506c5b23bc14b7ff4454
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:58 +0000

    fire plan matter check whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
