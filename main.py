977122e18fee04609d46d3e30a77f92bd32ffbb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:46 +0000

    mad play she mine slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_max(lst):
+        return max(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
