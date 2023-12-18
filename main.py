f77e7e2fcde510bce1bef34623e746c29cffd596
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:48 +0000

    getup live repeat shop crowded

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
