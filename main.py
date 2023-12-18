b2bfc6152ac746cc5fde5a698c06f2c192fd6054
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:23 +0000

    pipe frame tidy yes border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
