a6d2ac4f9413a6169f943cd298e0623623deac73
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:21 +0000

    dream right mark platform immediately

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
