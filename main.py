c320524cb3b3c47337782df8f4a9f484b78e48b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:32 +0000

    wash live custom either ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
