338d8328147ad0358d57dbf2f0fed2adc2e718fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:57:52 +0000

    equipment trust basically city conversation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
