c0f62d14e32b6898b67aef0c9fac245fb06080bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:50 +0000

    clear criticism giveback run opportunity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
