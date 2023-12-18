6c5d1e061faa82b3339d30ab597cf48c28cef300
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:56 +0000

    fun minimum nurse where theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
