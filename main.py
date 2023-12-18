acd7b66b8df8ea0ea4d2c6c11fa33e454a013894
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:21 +0000

    most later work fresh fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def subtract_numbers(x, y):
+        return x - y
