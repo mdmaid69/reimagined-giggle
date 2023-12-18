9c3adb557ce17b32240c5038a2fdd03b34ec1a3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:20 +0000

    none used point warning turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
