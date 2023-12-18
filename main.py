f392049e9efe7e1d424bc8f262e76f0f06631be1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:26 +0000

    precise shake imagination meanwhile bet

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import unittest
 
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
