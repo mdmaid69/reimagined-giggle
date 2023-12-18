3197fa7fc86f56008ef90033374e71a112748752
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:18 +0000

    permission unfortunate opposite related possibly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
