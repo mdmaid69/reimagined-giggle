821f5f66f716c7c58ad07045452bcad5569c6cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:40 +0000

    lucky fit environment hole bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
