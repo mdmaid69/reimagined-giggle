a47cb5df5e41e4d134bbc0baa8028436f0153f30
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:53:15 +0000

    off partner only point copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def print_python_version():
+        print(sys.version)
