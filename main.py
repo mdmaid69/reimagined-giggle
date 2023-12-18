aa949dd54ec6aaca1cb09616a876c3d66af9b14b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:45 +0000

    sky appeal balance Rr anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
