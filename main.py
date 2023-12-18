e16ff5b89765caaf101c4d5632e5390a6c173f6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:00 +0000

    rain objection forget kick hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Max:", max(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
