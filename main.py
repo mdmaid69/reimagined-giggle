253edb24c050920f0760907e2d6c5c9bd27d14dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:20 +0000

    part search emotion all connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+print(math.pi)
