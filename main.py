adcd920581cab2deff32f0f1fdec7fe7701d4fd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:14 +0000

    drop programme round wrong human

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
