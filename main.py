2dd941442db670e3bc0faac0100b16c133edc356
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:35 +0000

    father field shine last slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
