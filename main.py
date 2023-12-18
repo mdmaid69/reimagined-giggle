8b694a09ed4f7db18b11142ceee6e1bc04592f85
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:23 +0000

    remark frightened frequent Ee without

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
