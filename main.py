0ac2d3ae4273dd068a86bd3f805521cf49658052
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:07 +0000

    branch question fail ball smooth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
