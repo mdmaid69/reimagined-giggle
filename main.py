273f2c44eefbb9a5ce182c819fbadac4794797ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:00 +0000

    main takeoff catch bury keepfrom

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+def exit_program():
+        sys.exit()
