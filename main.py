70edb738cc3a0d313630ff1b0643ab5d952093b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:57:02 +0000

    friendly process equal challenge pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
