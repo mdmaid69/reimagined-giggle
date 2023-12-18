35346915864d098e6b956f82267a64cf1c3443fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:23 +0000

    round deaf from food pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
