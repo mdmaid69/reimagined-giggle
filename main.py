ccc8536d126a315dd8c126f4055dfa36b7f92c8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:54 +0000

    fall organize fall question year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
