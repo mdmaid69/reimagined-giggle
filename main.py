fa32c6b013b7e949b00907fb87158e9221c1dec2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:10:18 +0000

    react perfect environment little comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
