ad073cceed533e61d0396a53f7b71716789720dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:39 +0000

    mess hit failure quiet pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
