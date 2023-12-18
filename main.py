b3425a9d34833859c1f557b02ccf2ce2af17a969
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:01 +0000

    argument entry nose share by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
