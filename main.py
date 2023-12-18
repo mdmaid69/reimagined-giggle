c9817a70cd6e63e088ee9b6bc0554e388e321063
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:05 +0000

    unit street print breakfast clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
