32a4f0a3ecedf3292aaa3e17815aa8568dfd88ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:27 +0000

    criticize tired visit flight peace

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def count_elements(lst):
-        return len(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_str(array):
+        return str(array)
