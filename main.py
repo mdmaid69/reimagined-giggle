ad260b7475a53ab5e870a71261ee1167b0319610
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:25 +0000

    complain mind anyone thumb top

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
