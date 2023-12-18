4e4c42e2be5be1dc626b432cb84dd1a7233092ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:29 +0000

    consider unfair bringback increase range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import time
+print(time.time())
