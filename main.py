50a76df204abcc19f232768f455930aa7512de0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:35 +0000

    criticize hour silent stage situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
