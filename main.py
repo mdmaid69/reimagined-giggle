1d53a6e6da7facbdcd6aba5fa24bb1c71999ab94
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:43 +0000

    mean up butter brave lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
