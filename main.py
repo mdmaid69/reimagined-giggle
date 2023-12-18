3f05533b5519ec6fb6b82d2c8bee40af87c05420
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:16 +0000

    several minimum mine operation open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
