b4eff9dd7f1c50b301aac2136f38ba17ccb15edc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:23 +0000

    temperature criticize kind giveup question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
