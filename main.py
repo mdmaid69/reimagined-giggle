970c7365165572b57ba7ab43ee321d663cb4f3e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:02:24 +0000

    flavour holiday blame thank beneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
