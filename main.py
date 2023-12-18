baf2623581def0cdaa7a7093ae2ddb1b1134b6b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:40 +0000

    bored around trust embarrassing walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
