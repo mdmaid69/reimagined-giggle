6a1513da9ef9ab0f3e5c6e012caa9b9a25308c8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:08 +0000

    post mix present yet exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
