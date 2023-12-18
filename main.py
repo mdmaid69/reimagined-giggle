4b86cb96fe3bc4be28ddc986c645d06efad08d04
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:02 +0000

    someone pipe organization ball name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import sys
-  def get_python_version():
-        return sys.version
+import time
+print(time.time())
+import array
+def get_array_as_repr(array):
+        return repr(array)
