61e88b5ab33bb62c2f9e9e974ffc8446bcd3aa7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:16 +0000

    tooth concentrate further official hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
