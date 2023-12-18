0b31dcb5cbbb52d4e88aa31bd8fb17f1005ad23e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:11 +0000

    either wealth among Cc ago

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
