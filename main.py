5a0f2ce492fc54350907c8b15c647ce807147695
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:10 +0000

    always thinkthrough crash mine goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def sort_list(lst):
+        return sorted(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
