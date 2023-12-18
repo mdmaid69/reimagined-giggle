6229c9d8eca53c56d4c42b33f52d3fe6481e1e9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:21 +0000

    funny interest cry skin forward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
