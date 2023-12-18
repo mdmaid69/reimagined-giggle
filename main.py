d41e27212753d2fb8788666a30806285e609c96a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:47 +0000

    interesting Ss late dangerous compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+x = 10
+y = 20
+print("Sum:", x + y)
