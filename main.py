5df9f12a27bdc974c59772b2acf9924de7b3ff47
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:43 +0000

    soak better goon knock turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def convert_to_json(data):
+        return json.dumps(data)
