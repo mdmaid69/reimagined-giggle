221901d80844b7a130c67e1a9d9975c431e6ca3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:03 +0000

    precise convinced spot choose confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
