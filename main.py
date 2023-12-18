32df3e164c39651331f9f314fbd659eb77ebd2eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:57 +0000

    separate thanks commit much central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
