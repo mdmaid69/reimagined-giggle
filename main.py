0e460234a7b2f5c3ba63ea153cab800394cdd5ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:12 +0000

    control short influence however fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
