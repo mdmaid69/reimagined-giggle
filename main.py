45f7e23fa57578f0ccae8cb7866672811cf594a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:05:07 +0000

    schedule or nothing fear responsibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def reverse_string(s):
-        return s[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
