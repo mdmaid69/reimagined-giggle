e258f783deca7830b2912f1b0b0e15ea363e1739
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:59 +0000

    getoff easy shut improve distant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def convert_to_hex(n):
+        return hex(n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
