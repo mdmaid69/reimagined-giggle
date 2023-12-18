b704a2a247119b4856e94931f1f279298affa201
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:08 +0000

    true start experiment boring possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
