9fb31bead820b8297d60e49f465a13f1f6b830e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:45 +0000

    way passenger kind from comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
