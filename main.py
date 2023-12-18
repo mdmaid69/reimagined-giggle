5201ace60d160e4a4a4a1c42ee21f53971199d86
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:23 +0000

    characteristic silent release mean already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
