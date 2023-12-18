e7ad8a7850d55afbef638ffc79e4f74a3cfdc7f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:18 +0000

    miss real opponent file slice

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
