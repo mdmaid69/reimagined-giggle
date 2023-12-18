26550f8b5e7cc72dd73dcaf04c3cf828275f455b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:55 +0000

    far spring protest cheek environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
