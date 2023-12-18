399972890b78f7948b68cf39acb99626f27920b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:18 +0000

    basic save wish happen usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
