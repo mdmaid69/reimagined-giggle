b1c198dcb5ba014e5e55dbde9825285b4765bbe6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:46 +0000

    throw range to short fat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
