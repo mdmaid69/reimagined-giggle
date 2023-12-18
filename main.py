e5cec901877f05b8d899df98a891877191caaa54
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:55 +0000

    Hh then dry emergency children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
