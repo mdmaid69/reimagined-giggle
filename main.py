5d6be283e245610b2729fcacc2aef26616467869
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:49 +0000

    file month fail reply average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
