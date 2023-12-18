b9469db1a08ae68c2127c087514971588b8c1a5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:54 +0000

    collection test charge suit flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
