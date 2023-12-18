a564515057ed14496d1f50f29ae362df485d5f53
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:47 +0000

    similar open people react down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
