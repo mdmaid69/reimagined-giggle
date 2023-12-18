b574714fc7deb1133011f891d6c14304f46eb578
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:40 +0000

    getup could angry guilty correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
