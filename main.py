a4be3162f3b15598f9ecb269eb36703f717b870e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:22 +0000

    fail move hole number base

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def greet(name):
+        print(f"Hello, {name}!")
