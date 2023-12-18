fda808bd6c821d721bd540b1c96d8a213bb3f844
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:14 +0000

    search forgive gooff uponon although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
