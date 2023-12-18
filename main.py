20dd9740a275f9007066f1ff6b27a25a122a1480
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:27 +0000

    cruelty precise importance government letter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
