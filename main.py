9d1390ca1bdb7a3259e46b94084fefeb8ad5900d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:00 +0000

    slow alone else view excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
