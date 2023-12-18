d71e4566ca85be4727b5151833a5c268e1164534
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:55 +0000

    mean sound swing gooff somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
