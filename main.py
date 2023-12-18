c4f785efa18f024cfa91c60e55334ca358938a1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:47 +0000

    collect protest spite man answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
