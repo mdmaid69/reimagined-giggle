8b3b7aefdc0f7751c5bd00ea243ba5016d889ea4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:20:35 +0000

    education black impression side accidental

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
