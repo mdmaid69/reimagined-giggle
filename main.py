d453885ae58d7aea5f918f3a479ba34c83c4fe6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:58:56 +0000

    forward lead against disapprove love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
