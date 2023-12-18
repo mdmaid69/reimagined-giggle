c4d7bab1508ca175468c8bc262a93e3312d2b213
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:53:53 +0000

    drop tragedy accident method disapprove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_list(lst):
+  def reverse_list(lst):
         return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
