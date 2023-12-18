b4c43d81ecac6b04674b40ad4fd3dce93f3d40e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:44 +0000

    cool hair wipe afterwards relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
