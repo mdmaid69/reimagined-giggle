4791206c68a1e8ebce7a87a102c3fee809cbe45f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:51 +0000

    cost ilovedicks ignore sentence organization

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
