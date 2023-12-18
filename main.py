1980c4ddde9f0a4b575bf9999aa129202b12d6ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:07:44 +0000

    tie line Cc visit benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def exit_program():
+        sys.exit()
+  def sort_list(lst):
+        return sorted(lst)
