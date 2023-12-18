1186ff289418572b411a9d7b48dce5d6b6f45864
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:33 +0000

    wealth evidence although car defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
