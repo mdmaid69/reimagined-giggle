ed5be1629525d27c5c8961176a465d75aa0e3de2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:10:02 +0000

    bear environment takeover Pp reject

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
