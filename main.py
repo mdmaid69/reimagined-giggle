c93d3e38f020b8f3b848f2f1ff8c9efaae1242ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:33 +0000

    upwards state kind shine confused

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
