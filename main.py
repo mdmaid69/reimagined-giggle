f2275c0b26637d08019c8d41d459532264dc1169
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:33 +0000

    similar combination loose evening quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_user_dict():
+        return collections.UserDict()
