87d054045772606123f8c7ca257ccb71151e8cc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:08 +0000

    sweet task male rob move

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-def divide_numbers(x, y):
-        return x / y
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
