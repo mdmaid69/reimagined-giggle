e6bbe255cdcbf2c41115d1fbd58987559f8ec093
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:55 +0000

    turnout disappointed getoff trust mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
