3df31a161365a22630df356a9320d6b12d4af6c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:56 +0000

    anything left shock to OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
