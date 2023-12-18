40c0e7aa1bd5399da55f69b4b1ffa976c5e67ffa
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:47 +0000

    improvement serve check sight lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
