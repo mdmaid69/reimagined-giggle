1b7514688abb9ce0967dca1b2fe4b3171b3c4cba
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:56:45 +0000

    Ff advice phone should explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
