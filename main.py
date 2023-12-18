b3244e3f2e8079eb1bf25fe51862458598749a73
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:26:52 +0000

    afford see better little rob

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+import numpy as np
+print(np.array([1, 2, 3]))
