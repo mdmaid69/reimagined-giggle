234a897e50bc08589fcf34000a05e484fd7c26ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:40:32 +0000

    level collect pronunciation sensible die

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
