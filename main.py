66a478316e903f8de0dc61a2005f388ce3bf9204
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:38 +0000

    used scared yes sudden lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
