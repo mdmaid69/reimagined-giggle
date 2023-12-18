e10568c20e0c18929efc8bd1d11628f25e41a2e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:48 +0000

    risk separate related autumn convincing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
