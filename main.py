ba542127057d375c8b0826da64e59b852e5a3f90
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:24 +0000

    rain 569enjoy contain past fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_hex(n):
+        return hex(n)
