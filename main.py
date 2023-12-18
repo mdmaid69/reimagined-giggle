c261c5d25075f291dbcb00f1bd485f30df04d8ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:37 +0000

    actually lookaround attractive well guess

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
