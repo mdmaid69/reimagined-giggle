87ffadcd932217cae413699c26ce44e625f6e685
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:01:11 +0000

    message wear unite point forgive

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def cube_number(x):
-        return x**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
