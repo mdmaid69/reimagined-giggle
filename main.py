52f5b7fe86c9607fe48266723b1854f38ed7fbf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:15 +0000

    wide among professional suppose drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
