0cf3e665b082c505a381171e527c7a12c180e3b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:26 +0000

    rarely pupil inside square likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
