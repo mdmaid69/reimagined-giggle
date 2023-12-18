74fc3475df19039fabcdc981c3b44fb5c08fdab9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:17 +0000

    affect promise side bringback task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
