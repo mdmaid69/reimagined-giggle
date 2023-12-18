54f81a30ab35bf4039651f59e4389bd76b23b6ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:57 +0000

    example violence within crazy shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
