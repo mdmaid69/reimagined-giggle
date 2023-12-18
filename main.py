ac0fdba62c78e1e9cb1a9e26852eb91efe1b9e1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:35 +0000

    settle sister smell length explanation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
