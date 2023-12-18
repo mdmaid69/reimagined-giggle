0c4e576daf623bd2df530a6b21568fe31ffbe02f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:49 +0000

    war contrast like contact far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
