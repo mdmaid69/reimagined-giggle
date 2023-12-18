60dc63900bc5a66a4d3a730ae237e7f40ec0caa1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:14:11 +0000

    skin start name comefrom pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
