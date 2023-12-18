cc389ac1552d7f11a01b5f2d63e5cb9b56364874
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:46 +0000

    brush skill neck getaway why

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
