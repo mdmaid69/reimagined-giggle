7ccf95e07902bd7ef1945b2d918895cd10cb254b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:27 +0000

    identify mouth habit window brain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
