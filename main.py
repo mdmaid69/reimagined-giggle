db6f942cd17222090a8e8b895f6f598843542ee7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:54 +0000

    certain breakdown claim dress whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
