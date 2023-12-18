4ea3d9bbd705533f7e846069f678811b259a7081
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:41 +0000

    suppose pause much method rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_current_working_directory():
+        return os.getcwd()
