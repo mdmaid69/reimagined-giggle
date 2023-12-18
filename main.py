fe6b26e65ba27f11df686104a0e9e16c12ef789d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:23:38 +0000

    typical spite his accidental picture

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
