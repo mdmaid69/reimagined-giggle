0bbcce715f40b484f9279290a7f8278484de3d7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:30 +0000

    weekend now choose hers content

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
