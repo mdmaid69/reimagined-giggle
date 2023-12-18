86a842704af17d4cc4a4d0f53a1324eceb81ffa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:56 +0000

    fault nice offence offer tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
