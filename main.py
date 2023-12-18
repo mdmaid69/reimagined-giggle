7bd1d526dbfcbbbb63bb052abb35df90f93e4e0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:47 +0000

    frightened press large forward connect

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
+for i in range(10): print(i)
