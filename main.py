b94fa6b6020e1b95c7b4eb03401bdd17be00c380
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:19 +0000

    why worth course emphasis nowhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import numpy as np
+print(np.array([1, 2, 3]))
