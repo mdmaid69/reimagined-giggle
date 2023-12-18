fd65162a8955dfee1b3b5653eea93541a8ac62ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:21 +0000

    fault deaf character information advantage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
