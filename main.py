6571f14342ffa8ecaaab592838ba8f481d297ccc
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:46 +0000

    lonely success environment almost harm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
