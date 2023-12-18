b31745c50845ccc6c6b33eee6dc0990a9f274741
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:44:47 +0000

    relax path Zz callup gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
