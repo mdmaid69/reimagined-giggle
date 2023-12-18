5ee73ad7a6f070bf2bfbfde34ec5606f473f7773
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:32 +0000

    congratulate cover advanced large admire

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
