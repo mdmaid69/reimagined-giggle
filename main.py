a7f2f07d4fe1df635c3490dec300ba5a2438dbfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:46 +0000

    rob wrong message certain frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
