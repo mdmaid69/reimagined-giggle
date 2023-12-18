0769aa9d127b7a2d02241ef9a37af2aeee995729
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:38 +0000

    challenge to where burn storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
