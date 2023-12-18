508c003a768014392399571917d1dd8a1f306232
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:55:09 +0000

    time petrol forward example simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import time
+print(time.time())
