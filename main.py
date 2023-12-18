ae8918afdf9877af11c15cd1e830e426d4044f31
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:08:56 +0000

    doubt content though wind takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
