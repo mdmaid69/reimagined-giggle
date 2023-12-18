a42dbaded0d764e5307542ea7700f38d4ee4a434
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:19 +0000

    sting muscle make absolutely place

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
