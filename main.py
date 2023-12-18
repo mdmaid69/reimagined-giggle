20f79347a47c863139b6ddae521f73e4735416cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:09 +0000

    several base although us used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
