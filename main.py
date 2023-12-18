9618f85fae6e083ff69f715c44eda1e09177d98d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:57 +0000

    funny add reliable weigh Ss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
