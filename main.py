1a0719cd50841ce68c8307c9b8f3fdba661bf126
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:21 +0000

    cum goout weekend base memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
