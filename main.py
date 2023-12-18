dbfbdf891e4c353cc272247d4c7be5cce4297f32
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:11:45 +0000

    complicated move contrast represent dinner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
