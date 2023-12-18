261a86d9b192bf46ccb3e3e718c8ee148247e4a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:05:15 +0000

    although design clear clean foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
