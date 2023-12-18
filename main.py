0e972d26c5a9c7cb4be6ba955fc7a14285bf69fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:03:29 +0000

    live snow news flavour pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def square_number(x):
+        return x**2
+x = 10
+y = 20
+print("Sum:", x + y)
