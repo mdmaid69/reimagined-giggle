225042bf5e349ba837dad7e469ee6d35f74177d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:46 +0000

    discover shine record else market

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
