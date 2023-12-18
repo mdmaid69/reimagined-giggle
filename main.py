46f7724ac7aa2124e514c7c5b92646f8315f6eda
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:56 +0000

    offence worried busy bringup snow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def convert_to_binary(n):
+        return bin(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
