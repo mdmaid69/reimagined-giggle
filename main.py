03424f533d5f272097bfbbee0b4770197be36540
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:31 +0000

    thanks sleep goodbye getup shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
