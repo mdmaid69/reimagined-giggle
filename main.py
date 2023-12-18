4ed3723a91c207055f158df730bb5335c5c677bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:07 +0000

    mother spite calloff bend wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
