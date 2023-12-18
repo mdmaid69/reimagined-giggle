4a0d3c144f2ed26a80c5084b7514db3fdb3a95a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:42 +0000

    person suddenly pool place brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
