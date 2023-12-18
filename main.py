7cd02aed545f8f95f266c08037e208501a7c5757
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:56:05 +0000

    manner flat come much ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
