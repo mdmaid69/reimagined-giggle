aee5c7698b1e2b0c83d4dabb375f34edfcebc285
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:29 +0000

    loose example separate fashion promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
