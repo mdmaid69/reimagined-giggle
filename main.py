5786a5fe6d7edbf5ddde32727b659d3a380b75e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:48 +0000

    mean operate official star rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def square_number(x):
+        return x**2
