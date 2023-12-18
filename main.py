684b73b5f35da7ac4e25c8267d2e56badfef7f49
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:05 +0000

    handle nowhere cheque law door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
