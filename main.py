2c2016c87e68a4e20ad3e744d0fdc94097977a9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:08 +0000

    train plastic shop getback his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def clear_array(array):
+        array *= 0
