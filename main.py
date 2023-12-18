b8c790d524b845ffcfeba0555f89d550231726ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:11 +0000

    might lose insist not aim

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-for i in range(10): print(i)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
