9a038b99300590595fbca7642910c8163a4c76f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:39 +0000

    which young wherever yours react

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
