61d4e3e96914b14cd63800641345ef5527448976
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:12 +0000

    underneath film low line lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def split_path(path):
+        return os.path.split(path)
+def convert_to_binary(n):
+        return bin(n)
