55fe1999508fdf57da3d2464af72bcb7ee571b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:41:26 +0000

    sharp breath large custom programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
