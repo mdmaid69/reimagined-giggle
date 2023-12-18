9b0510ad9cdf3aceef7b276a403bc5a751ae312b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:20 +0000

    steam confuse method fair plenty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
