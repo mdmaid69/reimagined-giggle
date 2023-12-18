709d06c75f23cf9aade703299ad5290e7bd0b7b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:28 +0000

    bringup blind during slow tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
