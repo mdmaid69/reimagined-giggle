e158f6320bf586810fbb1083e1ae8fa0166492e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:23 +0000

    whatever learn progress comment story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  def is_odd(n):
-        return n % 2 != 0
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
