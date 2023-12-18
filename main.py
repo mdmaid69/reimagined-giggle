b416c0cb39aab7a81eb32ce1ab0553a31396d329
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:33 +0000

    tall may go attract circle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
