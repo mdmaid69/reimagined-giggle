612cf10265d7aa7ae908ef7c38156b22995b8508
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:50:17 +0000

    drop genuine table frighten surprised

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
