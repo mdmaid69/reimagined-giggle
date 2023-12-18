17c9849d0805581e0a169fb99b7dce1881b873cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:15:38 +0000

    air thick opportunity dead active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
