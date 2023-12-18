5f87f0a3c3f6f1e971832036da1f3cb960ca9a0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:53:33 +0000

    excuse few behind throw strike

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
