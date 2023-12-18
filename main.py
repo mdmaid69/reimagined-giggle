7acbcaace63333061414a60b4d08d7657aa45bc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:36 +0000

    sign shoot warning science limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-print(sum(range(10)))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
