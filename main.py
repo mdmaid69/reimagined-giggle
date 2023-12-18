a1065169eca08ddafb13aa700a540f581b79f8c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:16 +0000

    capital really circumstance instead draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
