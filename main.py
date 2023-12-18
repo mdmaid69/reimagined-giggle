86348a0742785837a770f12bf6ca1b6d3206d44c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:12:47 +0000

    straight takeover communication rob tradition

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
