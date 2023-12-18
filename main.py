5e945edb7ac922d362b5691c35bff520ae6b7acf
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:21 +0000

    strict confused deliberate clean point

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def convert_to_binary(n):
+        return bin(n)
