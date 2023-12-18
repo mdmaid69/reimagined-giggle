543c18f7a6877d1ef72195c471d4effcf8d4f3de
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:44 +0000

    clothes tip bar unlucky nurse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
