de19999d7637f9014eacc6a3b8ba53db522f7a54
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:30 +0000

    gather towards somehow promise often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
