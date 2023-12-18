24c159f076b627ccc27b2d0702cabd80bf25eddf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:57 +0000

    wide usually attract ignore destroy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
