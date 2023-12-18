4037c122cd9317ac7534a4e7b5bc2a8a3e12a9d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:05 +0000

    afraid hello music move her

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_min(numbers):
-        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
