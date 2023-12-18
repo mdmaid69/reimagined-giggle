c483ef3051f6cfa14f85005aff6823a8c79593a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:06 +0000

    afterwards reduction mark meal passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
