844e3ca248499cd1023453a88557e05bd69d17cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:31:23 +0000

    not material nowhere limit sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
