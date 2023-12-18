c0305a7a87043eebede19240694e7a078998e2aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:05:48 +0000

    spring confuse aware pretty even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
