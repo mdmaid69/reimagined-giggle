7049a75e806ee6d159daee1c31dbefdbf8562ad1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:26 +0000

    perform congratulate perhaps original loud

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def convert_to_hex(n):
-        return hex(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
