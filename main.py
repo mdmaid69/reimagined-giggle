7887229b024bcafbb43c4d40dbfa34dc49219503
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:03 +0000

    regard own quiet limit slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
