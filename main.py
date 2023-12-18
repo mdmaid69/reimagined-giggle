4c8bb42e9bd7e6c01a56b3994271649ee7e2769f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:55:37 +0000

    destroy because by innocent 179bite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def cube_number(x):
+        return x**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
