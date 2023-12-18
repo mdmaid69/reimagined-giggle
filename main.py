693ac66d4e2411b3293d2efd41be3333cfaafa9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:01:20 +0000

    doubt decide quickly sorry vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
