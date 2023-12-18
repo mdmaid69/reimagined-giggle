83834c41566bed2a8a551a43d9e390cc3857d0e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:29 +0000

    swim switch wave match lesson

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
