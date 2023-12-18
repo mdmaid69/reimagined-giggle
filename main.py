f9d499701c5eeb0c2087ecd7b3c288d32f5527b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:27 +0000

    follow dear major measure service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
