f2a46bd995755dbae2a7b58fcff7d57e541a46cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:15 +0000

    frame talkoutof press bath beat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
