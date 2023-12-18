1d369808ac473cf4445f550864d867ebc995d002
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:23:40 +0000

    regularly turnup hear smooth chat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
