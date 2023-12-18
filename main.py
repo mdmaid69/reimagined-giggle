9ef0f7405fedb28d17d4c2c5cdf1ca9ad359da28
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:17 +0000

    crisis mix beautiful estimate other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
