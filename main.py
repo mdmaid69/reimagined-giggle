3214ef8ded3e1c5df0bdd8cb8013077c283036a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:16 +0000

    talkoutof escape rescue complicated compete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
