9e32ac1d26eb556538fcfac4ca0b41c03e6d30e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:29 +0000

    beat decision fat shall somewhere

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
