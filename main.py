cb7dff088d67ed4523a30b296d5b2c0ec4247b4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:45 +0000

    to dream search again change

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
