22137c920998df69f9855f208318447458863bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:54 +0000

    argument payment seem turnoff lunch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
