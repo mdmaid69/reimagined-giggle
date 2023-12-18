5c7f750ecb83b4d549754b08b4c7afb331b21c14
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:51 +0000

    objection politics shock listenfor fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_typecode(array):
+        return array.typecode
