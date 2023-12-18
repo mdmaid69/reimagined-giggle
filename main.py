3baf70c18d2af2c5c12081eb700c61bf408b105f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:29 +0000

    scrape upstairs book drop respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
