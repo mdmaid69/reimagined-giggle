aa38d1407ceecf58765e7db7f0ba375e70fee8b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:50 +0000

    criticism butter wonder matter artificial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
