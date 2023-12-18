b533edeeed76e3e6a3b26a5efbf49dae541149c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:01:29 +0000

    reserve naturally chin part storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
