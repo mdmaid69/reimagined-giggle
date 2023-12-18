edc239a7f9db7f5f835eae55fe3a071ff1baab57
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:30 +0000

    always upwards callat spring return

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
