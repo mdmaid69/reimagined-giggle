51f46626a25735316e58cff7b26808fd4d7ca7d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:30 +0000

    help comfort special unable juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
