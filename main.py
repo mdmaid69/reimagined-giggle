ba85146f23fa8041dfdfc9d31f320e50e9dbf0b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:07:06 +0000

    shut skirt quickly twist store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
