650e40821d8b0f03f65d12e945b1d959691692a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:34 +0000

    how we deal declaration home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
