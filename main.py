e3bd060fe3abecf67d46f67c9d21b366e2934195
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:50 +0000

    suit steam slide whistle minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+def change_working_directory(path):
+        os.chdir(path)
