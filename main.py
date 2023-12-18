923e6cdaffad4ebddd9c74847f4a8007b503cb35
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:32 +0000

    just reasonable equipment so realize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
