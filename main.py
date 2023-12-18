c2ee737346c0c8c90c17dcd8d045f3682bd831ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:14 +0000

    ourselves beside bin scream Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
