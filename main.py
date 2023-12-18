c560a0d7c3e15ec2c00ee20cac9b2ac542ce8a48
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:35 +0000

    holiday rescue account somewhere copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
