11c19f042561f5699d4dbfc6490524cf9983b4e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:50 +0000

    they watch grateful appear loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
