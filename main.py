f0da7a60087a0ab34f615e209bfe2b7e958fc2ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:29 +0000

    opposite cross smile relax carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
