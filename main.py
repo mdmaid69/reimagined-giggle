76b579f379c9d4be56f2a4bba69091bdbbb75ffa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:48 +0000

    phone goout walk sing callout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
