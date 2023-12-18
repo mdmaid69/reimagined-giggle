37aa81dad040436a6f008b43ddfc6c4ebdfb9daa
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:14 +0000

    special hollow birth careful recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
