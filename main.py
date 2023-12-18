6de2fe70cb535c404342d2c1dde6c68243309b5b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:50:50 +0000

    expression calloff 571enough move remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_pressure(force, area):
+        return force / area
