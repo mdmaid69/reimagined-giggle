0340ec381178d9505c9a6b1913c56104fd8075b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:48 +0000

    slight forever speed priority breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
