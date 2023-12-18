1de8aab0a00e25afab18c26faae91e4f4e46efe7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:30 +0000

    mixture choose object outside exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
