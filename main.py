24ab7aa3255d17a191be7e27b148b3f9cf8da1ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:10:43 +0000

    measure list art violent prize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
