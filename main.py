a037cbef8e73fa01e1e7cfa409a98d19fc3626eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:36:42 +0000

    affect manage centre cut guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
