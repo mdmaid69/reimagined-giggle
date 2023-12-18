b1cbe8ea4a854ff99d575da6e8227978991d9ea1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:16 +0000

    switch holiday appeal progress voice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
