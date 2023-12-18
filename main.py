556b883ac0e2866040cf4996d1b7f6a45aa658eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:47 +0000

    advanced goback(to) row push about

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
