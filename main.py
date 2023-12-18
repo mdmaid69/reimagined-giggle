8951d41cc2d1621bbd0b111bb6fac07186179537
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:37 +0000

    anywhere guess difference flow shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
