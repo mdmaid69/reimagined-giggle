5640949c6a3e65a6ee6213be6ee4ae104b4c7da0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:31 +0000

    no table anywayanyhow advise please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_itemsize(array):
+        return array.itemsize
