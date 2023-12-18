5fbedb3faf945d03554112b7fbf261b4ec187bb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:42 +0000

    behind interested use night range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
