d02971fe9dc51033c646efdb0cf40c9256c7a9d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:56:10 +0000

    silent tired still unit divide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
