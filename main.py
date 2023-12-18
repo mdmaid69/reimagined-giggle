397cee7e1d34e39812e06d047f47cc16fc81227d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:46 +0000

    hurt university suggest goup collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+for i in range(5):
+        print(i)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_set(array):
+        return set(array)
