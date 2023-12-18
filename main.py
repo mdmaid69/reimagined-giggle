1f3a5e84472d2192539bca5dc905832083d7bb4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:19:06 +0000

    shopping position yes 564encourage slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def convert_to_binary(n):
+        return bin(n)
