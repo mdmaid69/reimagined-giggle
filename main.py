5434d51d203076bf3914f11fe042d298721338fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:00 +0000

    godown sale punishment note sticky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import platform
+def get_python_version():
+        return platform.python_version()
