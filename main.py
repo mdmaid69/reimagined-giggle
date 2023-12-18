5327912ca6b8f2b0bfd7efd25a26539d69a07959
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:02 +0000

    trip fear embarrassing new matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
