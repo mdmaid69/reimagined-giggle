7927ea8f9779f65937fee589c26c78a8d18b8f1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:19 +0000

    event escape pity nose snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
