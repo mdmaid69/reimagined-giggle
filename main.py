708ef4061012cde1eda4250f47b33b9486333e0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:13 +0000

    because measure on home fasten

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
