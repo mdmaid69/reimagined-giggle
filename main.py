5742ee7319bdf7ffd283817889d1cce510bd59d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:04 +0000

    science cause safe giveout story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
