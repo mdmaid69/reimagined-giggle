16f6493e4000b4a79508a4c162b43f7f3d2e24d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:23 +0000

    usual capital detail such determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
