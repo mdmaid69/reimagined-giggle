34c0fac5f040c508d9ce5aa29b9b748d6b0158a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:35 +0000

    form personal grand instruction club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
