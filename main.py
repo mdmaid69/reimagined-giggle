555e35a00cca7036d663974a336ae36fa99c2378
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:21 +0000

    sense sore hang onto accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
