dcb59e0a5073c876e0be6f870d22ac2e1d8582c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:43 +0000

    offer watch dark swimming brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def square_number(x):
+        return x**2
+def sort_numbers(numbers):
+        return sorted(numbers)
