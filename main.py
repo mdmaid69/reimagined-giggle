8f148bb0a27cae1aae9c6c49fa972180db31128f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:02 +0000

    drive open arrangement pocket glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
