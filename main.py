52873e368abbba86b65f7aa960043810edd9bd32
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:52:50 +0000

    fence consult club prevent kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
