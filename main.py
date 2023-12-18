ce22daf23c965087ba9f519865a9b28583c15783
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:29 +0000

    giveout scared reserve decision calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_max(lst):
+        return max(lst)
