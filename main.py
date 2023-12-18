eb9e3c0b9628da9dd5fb29234ff70c6364513eaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:15 +0000

    pick way invitation giveaway innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
