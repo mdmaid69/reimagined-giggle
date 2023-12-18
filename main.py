e9ced75a3a194586d30f9e7d0ec72e0690c5e1d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:20 +0000

    to thorough generous somewhere persuade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
