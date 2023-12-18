4f725d2a192fabdc529523a8b3158b1838aca71f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:44 +0000

    day smile perhaps reduction fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def convert_to_binary(n):
-        return bin(n)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
