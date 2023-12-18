91a5881c6575cada9a6bc92828f479f01131e4c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:10 +0000

    distant car spray home afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
