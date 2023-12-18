5ca98231b41823de90cb0488990ae847b7a760c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:32:43 +0000

    some city nonsense programme pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
