e77d45a1e41f073762d6d3a23dd062840629c4df
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:26:31 +0000

    persuade schedule argue early quit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
