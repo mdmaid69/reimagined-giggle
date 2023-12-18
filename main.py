eba9140a850cb95ea48381d9e2fd24f8f9657cc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:26:11 +0000

    yet meaning eat breakin morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
