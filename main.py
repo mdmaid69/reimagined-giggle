9325886a44c57fba5962b25cfa0910e82c7ed090
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:39 +0000

    behaviour anywhere giveup doubt plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_min(lst):
+        return min(lst)
