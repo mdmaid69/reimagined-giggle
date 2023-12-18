35e55e0537b9d9d4944ce7ecc8e3e8343a6b211e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:36 +0000

    godown move mistake contain lead

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
