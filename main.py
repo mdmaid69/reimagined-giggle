68d21b3f220c5836ffc8fcc82a8671958dc12b85
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:41 +0000

    write anybody professional bang neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
