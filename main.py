10318595188aa9960dfb5208e50d9e2da955f725
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:45 +0000

    war slow bang clean punish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def count_characters(sentence):
+        return len(sentence)
