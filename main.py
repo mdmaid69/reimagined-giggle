08139dd8b21c6bee96105bf17adee662be44c4e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:26:46 +0000

    important wrap worry prize thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
