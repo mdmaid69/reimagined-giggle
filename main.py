95459af996a63cfa88ab18b017479404253f37da
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:27 +0000

    decision human tour imagine reaction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
