6504a7d74fad54d38802db6b0d278489a90eef97
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:19 +0000

    set sign get obtain quick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def delete_file(file_name):
+        os.remove(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
