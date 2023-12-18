ee2098d160d84489487c04d68835b178a7aa6b12
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:19 +0000

    worth complete if standard tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
