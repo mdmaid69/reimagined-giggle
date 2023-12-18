88566c2d19a04831692642db412185ec5180c7ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:47 +0000

    objection learn bath ahead arm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def cube_number(x):
+        return x**3
