5815f3f9994d081b072d78864c880962eb71523e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:52 +0000

    lend situation gentle press complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
