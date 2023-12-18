ddbe9ca0f35f719ccc4425ee6fa0b6f8b075ee84
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:21 +0000

    me idea certain nurse case

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
