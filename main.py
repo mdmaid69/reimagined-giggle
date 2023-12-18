1f6d87b286dc27f412df9a1e6cbf3119acbf6562
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:41 +0000

    behind definite collect objection face

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
