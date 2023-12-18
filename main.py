4e33753997614ca0f173304829af2b9e75bc7af3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:20 +0000

    law issue 565end unit beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
