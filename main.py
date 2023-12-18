e708ff677ad092bc43de62101d877775a70ebfe5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:28 +0000

    temper list interrupt row ourselves

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
