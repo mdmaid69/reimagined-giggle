fc3371769ef10a7e5244d041a5e11c8604c400f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:42 +0000

    succeed beginning callup catch accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
