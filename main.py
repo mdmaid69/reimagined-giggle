e9637909911e6f6320bb5f303123b088d1672a98
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:35:25 +0000

    pump 179bite what across original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
