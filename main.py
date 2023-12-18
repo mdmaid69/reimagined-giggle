9a70804d6525349bdfc2f1c632b25a1642228094
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:46 +0000

    should often introduction size occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
