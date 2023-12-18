8b2881bd3e2f642bac58fe20d9175fc35738f868
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:02 +0000

    dress squash experience between tight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
