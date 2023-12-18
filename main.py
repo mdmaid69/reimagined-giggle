c3580962a0e7653910f30b29f66b4b71a8211108
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:52:28 +0000

    or cheek 566end difference yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
