49e5c5a3dd899ed528a5b52737e3f9b1dda5ca5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:22 +0000

    decision late load wipe create

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
