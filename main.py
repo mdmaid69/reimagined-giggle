f04fe22452ce4ddfc145b6329b024fee1be8d9b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:59 +0000

    criticize invite port night goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
