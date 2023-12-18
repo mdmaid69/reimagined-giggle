845c3f182cf16194bf8bee292400f123460bb9db
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:47:53 +0000

    also impressive lie admit plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
