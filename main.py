3eb8b0f9673d7fe45e83d08aea9aa125dd10b6d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:50 +0000

    block continue occur fire nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
