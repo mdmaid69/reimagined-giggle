87e867b6b30dac5d68736840e6f670b24ec36dd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:45 +0000

    meaning exit gradually nature ignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
