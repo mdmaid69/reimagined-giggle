267bfb36a352c3247ce811d79bfcf155e1923314
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:23 +0000

    music opinion general people shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
