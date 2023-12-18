7f9b44ecfcf1ccce38f449fc53fad7b41f82f340
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:47:37 +0000

    space manage Ss genuine mother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
