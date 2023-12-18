9f82a3889e5dd9aed07553011e5b984ea75a4317
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:39:14 +0000

    show back general stamp introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
