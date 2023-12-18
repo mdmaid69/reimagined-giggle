b38150d621632225e845602c6c1722ecb1592ef3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:52 +0000

    noone serious far circumstance sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
