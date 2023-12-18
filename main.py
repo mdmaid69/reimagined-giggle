b85875a4295eeaa5aff27c003204cc6aaddc82e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:47 +0000

    pen future skill breathe exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def reverse_array(array):
-        array.reverse()
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
