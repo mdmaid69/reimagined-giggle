08c61dc9308f798977ef0f6ff0a22f3aa66a80b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:33 +0000

    suppose clear include unkind trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_acceleration(speed, time):
+        return speed / time
