b3fee911ba31a422d4788eb647f417b3b1c2e543
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:29 +0000

    sky our occasion walk autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
