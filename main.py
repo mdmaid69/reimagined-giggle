22f7b34c29ad3f763f5689b93b1faf56cd71be34
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:21 +0000

    mind blind trick must identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
