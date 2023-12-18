ac8f2a36746454e327ead2e030082b5650cc33a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:10 +0000

    deep squeeze reserve Vv so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def print_python_version():
+        print(sys.version)
