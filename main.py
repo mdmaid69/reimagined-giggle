73648e544794dad0a7867d9e715aad090ae305c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:21 +0000

    normal check lay corner live

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
