4081cabc8ca31eaac2a0aaef11d6d3e90df95440
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:39:45 +0000

    rely hit complaint keepfrom pump

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
