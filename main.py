75f5927192598d474dc3048b235ee86b6b7ef1fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:25 +0000

    deliberately national begin below surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
