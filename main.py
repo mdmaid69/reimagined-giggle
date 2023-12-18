0056e8c72850ab087ef560ff5da16ad01e1cbcdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:00 +0000

    aware why first town takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
