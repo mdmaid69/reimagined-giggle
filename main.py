76c839a2eafef0be4615a0f15d1ca9c72fe2cc4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:27 +0000

    prepare gate grade basically slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
