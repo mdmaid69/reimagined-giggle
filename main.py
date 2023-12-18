6360fd5765829fb6e2542d80cb43ae6509bd927b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:52 +0000

    temper imagination hello appeal generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def print_python_version():
+        return sys.version
