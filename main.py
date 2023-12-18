5812e7806a96e330891c23b55d7292c8781e4c5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:01 +0000

    Bb improve bored good clearly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
