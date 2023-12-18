4453528c84b9b920adb9a27a10073b892d19eddb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:28 +0000

    runaway land less disturb inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
