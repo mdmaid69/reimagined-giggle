feeddb7377044324b01e90054555c130c05e3069
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:57 +0000

    short coffee right and regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
