bb04af25daa78b8bd8d2dfdf965c6ca19af853a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:50 +0000

    embarrassed consult easy doubt official

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def count_elements(lst):
+        return len(lst)
