34a6dee8ad0cc4058705c435124ddba782759867
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:27 +0000

    hurt border ear visit feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
