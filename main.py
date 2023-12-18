0df4df1197a16eb7e749e047425457ac44461ba8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:09 +0000

    change hard colour admire vote

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_pressure(force, area):
+        return force / area
