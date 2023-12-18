147c4101a7434f164d1be980a391cecd32f1f755
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:44 +0000

    side government callback difference study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
