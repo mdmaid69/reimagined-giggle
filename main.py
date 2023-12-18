e4eccb928d38c89d418e434bf8b53c2750341dfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:01 +0000

    impression instead polite final behind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
