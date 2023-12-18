1c3ba2b5b47619ed7049226016147fdf266529c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:29 +0000

    previous old reduce eat kind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
