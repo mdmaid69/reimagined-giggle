b8479de86e41144df03e4183a54ad25a650dc068
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:56 +0000

    disgusting trip term dress emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
