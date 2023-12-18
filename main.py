ff754e44a5fb4cd36a4435e0c09cefa5fef61e82
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:17 +0000

    event sign pleasure certainly help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
