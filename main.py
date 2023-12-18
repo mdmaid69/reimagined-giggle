1ab3c7636494f06855e4545512c894ede8c30c58
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:17 +0000

    silent congratulations against of past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
