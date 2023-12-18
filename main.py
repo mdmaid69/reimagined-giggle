ee50fcfb617e01836d04ab619dfaad86bfca76f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:59:48 +0000

    stamp past near position result

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import numpy as np
+print(np.array([1, 2, 3]))
