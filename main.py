63c58c9675550dbcd506e5d127bffad8096f42c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:10 +0000

    unite any show deal depend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
