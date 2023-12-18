22828de4191e8580a97b1aa30adccdb613d167d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:10:21 +0000

    clear trade catch show knowledge

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
