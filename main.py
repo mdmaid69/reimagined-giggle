f6fb3a618ea8c54e30d09e9894a63b534fb86a18
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:15 +0000

    game broken top nor contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
