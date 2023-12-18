02aeb5d4bf1c806eab7561d173ae131067742f2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:33 +0000

    environment strength close couple stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
