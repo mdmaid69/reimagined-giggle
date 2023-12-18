5e0644cc1740ea990cf0b33b376d12d2a0a53fd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:34:04 +0000

    suit fairly squeeze disappointed scheme

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
