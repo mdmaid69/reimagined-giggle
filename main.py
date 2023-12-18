3d02d1e609fbcb5c8cc6aae4a7966bc943c76b38
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:06 +0000

    dangerous land party experience quiet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
