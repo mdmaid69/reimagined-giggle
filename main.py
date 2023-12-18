aa35d3fed3c1ffa5ed69f98c5adf95c6cde83afc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:29:12 +0000

    want except negative do excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
