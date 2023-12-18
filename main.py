7598ce22a61132d6a5dc107784e8944544b7fa63
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:59 +0000

    platform related best relation on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_unique_words(sentence):
+        return set(sentence.split())
