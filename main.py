f30915791a07ed395f22b60e5f36681dde34e24b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:09 +0000

    traffic comment each connected than

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import tensorflow as tf
+print(tf.__version__)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
