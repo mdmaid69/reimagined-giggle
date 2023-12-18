98a40c2fe03f29674dd148c143ab556c24e706a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:10:33 +0000

    attempt lay soft square sensible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
