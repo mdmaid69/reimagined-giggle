7842fdc3d2deecad10cd228e53c3c78a4fefa5ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:31:58 +0000

    know once pile pollution autumn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_pressure(force, area):
+        return force / area
