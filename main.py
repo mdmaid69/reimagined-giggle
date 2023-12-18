b56269ce0cb016758917e2f2c4b0e1a5296bf4ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:37 +0000

    limit unkind themselves ring its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
