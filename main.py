ceb10f894db5e140397bfe225b53a250b179af30
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:53 +0000

    order drink only normal truth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-print("Hello, world!")
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
