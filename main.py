fccf9f77e08328989760aec9a02685845e43746d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:30 +0000

    drop injury advice crazy fit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def add_numbers(x, y):
+        return x + y
