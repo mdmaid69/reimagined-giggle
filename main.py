1919f86794a7c2a61e4956124529624665207826
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:36 +0000

    hospital edge represent strong getback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
