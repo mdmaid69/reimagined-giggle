865b186b35d5f66a7a491b27d73b71e1df4ecc02
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:40:08 +0000

    makeup mother human their teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
