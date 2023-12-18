5380277c1fd76b9251765cf89b5ddec0ad5f62b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:57:45 +0000

    suggestion store punish consult short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def find_max(numbers):
+        return max(numbers)
