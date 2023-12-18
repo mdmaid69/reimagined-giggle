22a55198629bfd3a4a55510fa589a18ee8500959
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:54 +0000

    content invite unless lay pot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_min(numbers):
+        return min(numbers)
+def calculate_area_rectangle(l, w):
+        return l * w
