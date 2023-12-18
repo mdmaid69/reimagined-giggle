62311dd2892bca1446e6a2b83095ac30c808ee1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:05 +0000

    love total cough band throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
