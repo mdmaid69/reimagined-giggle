968e9095c8eed25eb9ca4ca81f4ea4cdada9eba4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:33 +0000

    steam goaway order recently event

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
