e94d7267d2815f4a88816f4bf6c35c3ea129afe6
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:24 +0000

    remove after container suck bit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_odd(n):
+        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
