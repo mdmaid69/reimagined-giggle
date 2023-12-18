933c7cfed4ae25f6fa4d7be2519839abd5967bb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:06 +0000

    deliberately proper history fence drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
