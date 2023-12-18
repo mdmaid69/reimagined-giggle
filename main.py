cc0b144aa0f419d687a4ab53dcce43b79589146d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:26:58 +0000

    right issue offer comedown rely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
