40cc61734f38046fb42d4aef2c1773da27f7781b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:57 +0000

    height movement deaf give leave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_list(array):
+        return list(array)
