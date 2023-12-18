26c18aeabf80f5dcbbb292cf02d82d44787002a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:14 +0000

    uncertain shall must worth motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import json
+print(json.dumps({"name": "John", "age": 30}))
