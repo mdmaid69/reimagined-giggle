76f28ade444c88acc7103b022f940ddc6f1e3587
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:14 +0000

    official heart whisper display exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
