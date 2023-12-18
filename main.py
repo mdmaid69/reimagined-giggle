565c0b00efb655f6fe044dc7c693c3a36686437d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:57 +0000

    strict right square characteristic country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
