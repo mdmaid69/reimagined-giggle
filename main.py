b2de2e48ad220aa0a0ad354e9263eb0aeb93bbb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:08 +0000

    important turnback cope swing Pp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
