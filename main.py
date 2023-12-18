1292f2b85e3c327d114d1599027858e3d7211e84
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:07 +0000

    computer fact unpleasant meet realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
