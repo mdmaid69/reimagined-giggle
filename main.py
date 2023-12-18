11e322f989048216eadc974e0cd88e6b8b7344d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:12 +0000

    cheap place float complain head

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
