954df35d64801804326ef31b08dcc172375b19ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:48 +0000

    radio Jj part thing label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
