4bec2e0236a7b451736fa871bc29207e4814bc19
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:15 +0000

    tone Dd frequent power survive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
