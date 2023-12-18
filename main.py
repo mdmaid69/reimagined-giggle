7a874882bc503de70d5cec16c21296824ae303b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:08 +0000

    disappointed ability naturally doubt none

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
