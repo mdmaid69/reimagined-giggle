5ce3b7ef629d6707a46ab2cc6aab8000ad57d4c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:06 +0000

    factory answer come we unkind

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
