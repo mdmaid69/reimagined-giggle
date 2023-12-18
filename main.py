4b5a037e7af167efe1908bd99f1ce35124d0e7be
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:25:09 +0000

    authority tip constantly greeting plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
