12b36bd5a44350832b20519be6bcf31b7e410213
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:03 +0000

    earth off attempt false rub

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-print([x**2 for x in range(10)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
