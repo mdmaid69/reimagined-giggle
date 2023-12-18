9e114c591a8dd4e42969a2adae208b6b963d361a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:34 +0000

    variety production face wait every

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
