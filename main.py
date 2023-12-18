4722cd5028291c30629727b49e326684239c357f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:19 +0000

    keepup act fear back chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-print(time.time())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
