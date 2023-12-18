ab75a756895bb144faf45061e91eaa3d4c9dceeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:39:55 +0000

    ask passage real most cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
