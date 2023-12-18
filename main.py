0eaa759704410d562c35982b3491c861cc7cd2a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:11 +0000

    should ahead tragedy tomorrow within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+print(math.pi)
+import array
+def get_array_as_bool(array):
+        return bool(array)
