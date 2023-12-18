2234aad5c75ba2485d39991606e4b386a216abba
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:46 +0000

    its anywhere cover face life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
