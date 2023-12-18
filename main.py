647691b47a834c4e8acd1b49050200c4c0445de9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:09 +0000

    mind accommodation book weigh please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import sys
+def exit_program():
+        sys.exit()
