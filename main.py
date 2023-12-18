d48d801a6d6ddbabbd1c957bd60e33b81d7052d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:45 +0000

    scream pen invite side treat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
