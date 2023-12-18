94811c03fd6ed7976b490a596f38013353fed10e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:00 +0000

    hard moment fairly doctor possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
