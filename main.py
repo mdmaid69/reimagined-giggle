adb7c7c325d55860ae73d2cf2cee3cf8e995f3b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:58 +0000

    difficulty work Dd skill advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
