54bfd37c96d3e8dc941712a9ba9abc739f7c586f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:28 +0000

    maybe influence change upset emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
