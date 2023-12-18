02fbd3af47a3a150ed8c9b414ba30a0f699aec43
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:47 +0000

    agree cold near mouth record

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
