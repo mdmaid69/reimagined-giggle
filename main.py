3ca9d52b1675396d92357b0ae964dfb0aed04e5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:04 +0000

    train history original male change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_cosine(x):
+        return math.cos(x)
