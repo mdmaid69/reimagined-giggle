936b6e73f3f67132d4c0f83da2cca116670c4b4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:50:53 +0000

    form through stop Yy company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
