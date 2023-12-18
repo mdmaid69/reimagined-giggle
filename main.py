777f30eaafd5213100dab21da8e3cc85b3e88830
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:00 +0000

    believe carry violent finally Ww

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
