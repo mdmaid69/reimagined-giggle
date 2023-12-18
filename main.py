8cdd1f1f8244ee16566720478a0460e3f584fca8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:51 +0000

    disturbing lunch public settle eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
