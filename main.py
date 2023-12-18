197ccf5cc0b993d8f323c42a398bc7ae99972d36
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:20 +0000

    steal pay earth comment either

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
