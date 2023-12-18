e5e2d501c725882833985a5db31550a2a70f3c38
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:53:50 +0000

    thank neat thick speed part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
