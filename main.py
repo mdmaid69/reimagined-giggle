f499d0b33d1cedd6d3f3e1740f1ba153387a809a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:04 +0000

    rise supply safe surprising travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
