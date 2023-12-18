856872404ea26f130744d8b5133f0f11b68d55dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:30 +0000

    ocean fall young convenient fork

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
