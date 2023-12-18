f5e1eca8a5c52ada4c319dcea25f0294d7bd8180
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:50:32 +0000

    born limit nervous permission thumb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
