ca19d120f05831dc427fbbe67822c2a15cc66f66
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:11 +0000

    translate why thick Qq wet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
