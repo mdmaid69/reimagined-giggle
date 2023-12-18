d428a47dd2324a6fd78ab8db284bcdb76c38486c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:02 +0000

    and risk murder almost form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
