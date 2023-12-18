dc87d5de6e0bf4b7ee44b809423e129f65c31664
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:37 +0000

    rubbish passenger age language fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def is_even(n):
-        return n % 2 == 0
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
