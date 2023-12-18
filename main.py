2ada3739931da582d7ec2c363c07ce2f24d5e06c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:46:15 +0000

    whatever law callout hit unfair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
