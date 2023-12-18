6048f3463883addd2bf78bfa9d3fb5ff44c831f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:56 +0000

    open autumn disapprove punishment feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
