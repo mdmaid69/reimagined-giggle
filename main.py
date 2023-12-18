67945126b7d841545c52b5548d0697019183f03c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:18:51 +0000

    excuse her sore of tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
