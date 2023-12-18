d55f59491bccbc140bd50d3b05374c3f6f88b87d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:10 +0000

    any knock catch mystery world

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
