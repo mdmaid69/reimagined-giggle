7d7eb0b5369f665f449e1ea67e10319b61ccb68c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:54 +0000

    build exact pin concern frightened

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_min(numbers):
+        return min(numbers)
