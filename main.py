f177486379b17b2b9d9e568de6f2f66f8b477ec1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:45 +0000

    scrape foreign 564encourage lift apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
