790e1f405e25c78258f2e653c843781303108688
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:58:46 +0000

    move hand notice scared plan

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
