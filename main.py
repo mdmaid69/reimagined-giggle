e1e97b3472300c09e06a5220ce5d834729bf0148
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:00 +0000

    ask variety store shape way

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
