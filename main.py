8624bb7be544d57aaaf23aead011323bd46a7783
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:09 +0000

    famous government this look official

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+print(sum(range(10)))
