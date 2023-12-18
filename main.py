fcb1fd4edb7fe80e030a069bd0291effef3d9a0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:06 +0000

    use payment section stage mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_stack():
+        return collections.deque()
+def subtract_numbers(x, y):
+        return x - y
