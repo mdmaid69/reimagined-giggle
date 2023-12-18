10bac853e463f98eb6c5d41686e9a3033e0c0db7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:25 +0000

    tooth preparation purpose press characteristic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def square_number(x):
+        return x**2
