e08aa0b9f0a120f1878d69f313baeccc429f7f97
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:52 +0000

    scream Yy your safe unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import platform
-def get_python_version():
-        return platform.python_version()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
