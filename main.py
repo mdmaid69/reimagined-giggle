256513511878d23ff329d3ed4eb2b56b7cc221c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:42 +0000

    suddenly vote surface trick appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
