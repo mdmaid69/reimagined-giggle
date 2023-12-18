ae2a51abf0ed2b4b953fbf0324e1c53b13392487
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:13 +0000

    apart laugh giveup may shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
