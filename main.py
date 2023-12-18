a8ce9cc01dfb45fb4df80514677986172fe7a2fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:09 +0000

    wander mark interesting name song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
