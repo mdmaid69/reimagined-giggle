b874a6df17db93285f5f6acd54a8645ac98a1143
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:44 +0000

    surprising count control easy control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
