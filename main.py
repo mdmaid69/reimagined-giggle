bb3082e8586b89cce181ee269647c029e63b8f66
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:28 +0000

    like friendly downstairs failure person

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
