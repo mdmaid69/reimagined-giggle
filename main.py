4b3ceff20f49f64b9d74e90f025ceb205f1481e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:51 +0000

    by drop talkoutof grand tend

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print(sum(range(10)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
