2d6e57aee22aa8a8a80bacd82796d53a88d29443
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:01 +0000

    wealth ours among scale extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
