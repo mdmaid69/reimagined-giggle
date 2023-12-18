837d2cbe8222e4622a6c7dfb5e01cecbf42b0b08
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:59 +0000

    precise frightened hurry wild sing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def create_priority_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
