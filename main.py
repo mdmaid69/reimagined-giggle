13a1937d6b2e8cd2cc1e0c9dab1f7dc9120eaba7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:29 +0000

    direct noise demand fast community

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
