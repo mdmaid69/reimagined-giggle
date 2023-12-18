d940b38cb42cf80399c8f253c261bd1ff1376136
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:43 +0000

    ever police understand long runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
