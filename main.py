6dcd79cb4802a51941193be34ae51b184bc5c49d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:44 +0000

    child attach deliberately able fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
