21a16b317a1142e3c5c4707c7abd5c2288897f11
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:48 +0000

    goback(to) entry stop left invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
