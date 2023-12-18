b61a2ed92965e3e5780594387612775e118ccc0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:09 +0000

    bother no reaction belong explain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
