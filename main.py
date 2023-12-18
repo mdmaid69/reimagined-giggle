99cf6a2a90c92b5e62e3054c10efae595121d17c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:25:54 +0000

    accept score draw side twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
