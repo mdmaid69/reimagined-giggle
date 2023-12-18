f1998d3690ad7a0cc82cb3539dc938c6551e8294
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:37 +0000

    her tear charge attract introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
