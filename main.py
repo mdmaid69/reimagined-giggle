fa49f435eaeadf1f546375f46977babf378a2be4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:09 +0000

    smoke rich makeinto under wipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+print(os.getcwd())
+def find_unique_words(sentence):
+        return set(sentence.split())
