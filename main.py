faffd8ce77b90f200fb37d17a9783ea3399ec945
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:47 +0000

    track quick pause long road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
