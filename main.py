27073952caafb23268a6b4cc1dd1ea7b7b14d8cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:16 +0000

    blind what hot increase road

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
