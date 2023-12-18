29262cc9a46a99e25e3a04abe2e39fbad78a2e36
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:57:52 +0000

    separate either difficult which exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
