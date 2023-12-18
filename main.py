b903e861cdc88cd9bda97e025492f8b58f998c4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:20 +0000

    bath aim watch fear recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
