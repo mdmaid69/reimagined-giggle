fdb02c61cc3f332dae867036b7370b9023d9776b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:16 +0000

    that company declare feel bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
