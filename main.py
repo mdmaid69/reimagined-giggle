b966403ae0d63e0c065dd48405705bef11cc3012
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:08:33 +0000

    goout mention write search method

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tensorflow as tf
-print(tf.__version__)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
