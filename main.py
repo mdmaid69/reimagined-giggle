210126396d80ff86d4577aa044d64443cdf67926
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:25 +0000

    condition 567enemy attractive stamp plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
