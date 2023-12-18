5641cc170d91cbda938b7ab0701ea7beed4163fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:44:56 +0000

    season pump fashion prove swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
