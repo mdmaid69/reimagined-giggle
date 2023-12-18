a54649ce3528810407641980970aaf0d0341d413
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:21 +0000

    giveback design slice takeoff translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_pressure(force, area):
+        return force / area
