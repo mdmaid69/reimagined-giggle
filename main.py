17361d7ad9f95d7afd7ca266cb46e94b40267eab
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:17 +0000

    serious usual total rate tread

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
