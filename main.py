297e875b71994cc44cec37d6d9a5eb30721c921b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:58 +0000

    best beat up underneath lookaround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
