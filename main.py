3eceebb6469182cb7adda509ef5bad796a999606
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:59 +0000

    smell never ask effort region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
