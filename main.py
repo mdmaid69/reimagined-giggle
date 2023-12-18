b5b6ee1edadc18088c5862956b7dafded9f17e4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:14 +0000

    design cross honest main conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
