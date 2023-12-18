8d063def4df588aa81f4e32715819df00dfde33e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:09 +0000

    human expect cool nothing maximum

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
