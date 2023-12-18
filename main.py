6b3eef071f030e1b3fb70db253e95b5a6b885968
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:19 +0000

    towards fault only tongue run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
