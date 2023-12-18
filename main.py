88e60543bda99d6d32b1ec7553104c43203cd028
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:39 +0000

    whatever develop like feeling borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
