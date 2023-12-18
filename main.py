66b98d8d989118b3fb57a7c1374ac0a88b5b9dd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:42 +0000

    autumn bus free crisis environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
