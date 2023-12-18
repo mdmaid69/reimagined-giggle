50e0be68dee308d911b2dd54941b8ca7a9d5314c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:03 +0000

    feed later like market understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
