ccd3b042c356f28bd1ed4a39b3dea1df7806e8a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:25 +0000

    fat refer search less sensitive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
