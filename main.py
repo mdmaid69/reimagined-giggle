0019abb4a7a9b17f2648f53a65700827dc1ab7b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:39:33 +0000

    head hurt callup Ww mess

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
