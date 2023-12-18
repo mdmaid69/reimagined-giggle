bedda8d8ba07ad333ca32f2274a7ad1812bfc752
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:16 +0000

    save second thick chin trip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
