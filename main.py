805579977c39019b562437df9dfde0e919967b03
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:39 +0000

    565end occasional wish priority over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_index(array, item):
+        return array.index(item)
