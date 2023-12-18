8a885c9757c57c4a7e29c910a285737f53fde1f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:12:47 +0000

    yourself stamp imagine dig bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def square_number(x):
+        return x**2
