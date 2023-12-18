44254d85e8f25c70b281dab5409548d5a9c4e2a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:43 +0000

    share challenge it protection giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
