f8c1cf0266700b238b247142427d49d9a7786773
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:07 +0000

    century shock command related narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
