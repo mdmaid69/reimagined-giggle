879aa9595a5b12da3ff3a08de9880fedaa5eaf08
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:51 +0000

    desperate reason right money situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
