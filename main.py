7325d00f4c95af29c0f475f14e1480c25a5bf980
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:44:40 +0000

    Ll constantly spare commit breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
