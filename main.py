6f9feb8710e1233c70f210f61fa0edc8fe1034bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:30:16 +0000

    contact word fashion mean lower

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
