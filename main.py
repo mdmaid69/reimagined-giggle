11348c03f02bb1f5b52018d2882fe098c8c52f18
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:33 +0000

    instead band later butter port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
