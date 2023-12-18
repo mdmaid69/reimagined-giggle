164d6b6213300765fdae570bdf5a693bb702dec2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:21 +0000

    cheat make threaten science too

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
