c2f32ef27c64ff97f8bda5acedfda88324554377
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:16 +0000

    difficult than subject paper accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
