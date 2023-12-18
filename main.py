94985fe029652eca67176255eed567c9c985feae
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:59:50 +0000

    hurry direction habit account check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
