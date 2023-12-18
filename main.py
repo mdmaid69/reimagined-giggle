3ac198b956e780a57e82057e767e0a7afe2e0228
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:19 +0000

    challenge season convinced happy shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def remove_duplicates(lst):
+        return list(set(lst))
