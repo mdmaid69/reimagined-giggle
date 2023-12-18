597bb633707244f91c36aa20db27702eda04dcd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:34 +0000

    later window bite sort plastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
