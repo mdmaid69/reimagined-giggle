a494210dde89055a2aa1a616059fb4509f385ad1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:13 +0000

    block promise push Nn whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
