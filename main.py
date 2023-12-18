d66a0aee70c32bfa83e10c5cd24aeb0f539382fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:54:56 +0000

    error curious range turnaway plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def find_max(lst):
+        return max(lst)
