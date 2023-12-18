dc8479ee4a6873ac5140c3d01251faf4181d8c37
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:53:52 +0000

    sink married holiday last disappear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
