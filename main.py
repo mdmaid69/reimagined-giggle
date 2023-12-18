50c3df56e9e7ddb99870c75feada8117a8729d79
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:49 +0000

    sure slide sure wind pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
