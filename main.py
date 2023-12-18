ac13bb61fb21b2c6b2e729f7f6423235c6b49e62
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:07 +0000

    floor environment weak mark best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
