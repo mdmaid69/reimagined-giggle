fa4830ebfcfb2e486dcff8c5e562b4e9b26ed750
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:05 +0000

    present 566end closed place thick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
