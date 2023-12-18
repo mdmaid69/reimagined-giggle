dc93c399cb9af247df908b57269e42048ab2fedb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:38:10 +0000

    taste communicate secret along shoot

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
