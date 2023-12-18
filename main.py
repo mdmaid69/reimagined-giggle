997009693dbbca9dfce66afa887bf455d200b809
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:15 +0000

    expect allow control compare recognize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
