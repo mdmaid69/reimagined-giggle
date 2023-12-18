7e90e8d5ab850de5b128a31dd3a2c00802491ef1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:54 +0000

    information smart sore pity transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
