ef1d50c668c74607bd7a0fb5dd5c7be9ce3c8472
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:04:19 +0000

    get actual interruption forward ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
