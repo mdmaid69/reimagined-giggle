a2ed4bae4f31a2a68754309385552021c516ad2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:13 +0000

    breathe person hard dare goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
