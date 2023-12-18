9265ee782a1ad04fd5e246ba57ab36bdedee0877
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:07 +0000

    arrive yet argument memory bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
