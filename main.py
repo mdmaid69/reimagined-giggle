2cd47fcd2a52b08cd44709e3a5c24275747a5ad9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:15:55 +0000

    wish land mistake famous nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
