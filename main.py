14c287d8ae95ef3ff5667010c2657cf1474ca8ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:51:17 +0000

    computer rate preparation advanced unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
