d5943b6cec0a1e385defcc9e9b65fba44860e8b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:41 +0000

    contribute burst pump like trade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
