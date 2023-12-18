a227d0afb665e8d5f8f7bc29e3b465e7d39317cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:25 +0000

    follow shop should wake legal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
