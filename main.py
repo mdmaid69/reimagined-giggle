e8c3e6e20411f9a5d5091e71075e77aa86e36491
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:13 +0000

    morning direction somehow push yesterday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
