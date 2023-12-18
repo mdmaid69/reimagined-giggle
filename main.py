24567a6e9a2a5e1fa20cd746e8e9d3907b63f256
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:00 +0000

    question but flat allow tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
