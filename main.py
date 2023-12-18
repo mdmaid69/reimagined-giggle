5d235c3560a92669831c922d5295588583614056
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:11 +0000

    advantage damp study dry tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
