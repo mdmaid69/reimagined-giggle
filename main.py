f8fe0b9422f98761825146f41823366c466f011b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:35 +0000

    appear information display room lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
