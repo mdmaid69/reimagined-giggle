62e1e124285bfbf53985a5ed82ede8e7de69aafd
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:52:58 +0000

    delicate conversation immediately noone ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
