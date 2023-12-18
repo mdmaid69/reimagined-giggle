5f22077588643f5a8d02796e983c6c9c002ddc42
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:55 +0000

    road wash downstairs getback attempt

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import datetime
+def get_current_date():
+        return datetime.date.today()
