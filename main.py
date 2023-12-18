1233c4ddab9e329a6d0894a45149c1b9dccd3966
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:52 +0000

    rid injury adjective difficulty late

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print("Hello, world!")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
