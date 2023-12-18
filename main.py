527a70e46787d57d9800990fa875f9f3d04520e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:03 +0000

    ear label pronunciation well sensible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
