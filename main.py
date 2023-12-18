e0ccdcc8945d9b863ce22b907230cd4a6a21b774
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:38 +0000

    equal description precise release bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def subtract_numbers(x, y):
+        return x - y
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
