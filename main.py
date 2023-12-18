9cd2bff6dd7d802495bf97df7c5f24d4225e697e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:50 +0000

    getaway Itself protection sale classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
