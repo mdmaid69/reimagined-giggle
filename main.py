9cc1a608cefb5c9ef94ebe6d4c304b934484dafd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:50 +0000

    getoff Rr stupid course as

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
