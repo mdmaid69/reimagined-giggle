214caf8a6d7bb3d59afe4bf9cff085ec576a0194
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:41 +0000

    safety basic rubbish explain flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import platform
-def get_python_version():
-        return platform.python_version()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import sys
+  def get_python_version():
+        return sys.version
