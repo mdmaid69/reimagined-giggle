3321a33445577de17ee2a56730ca4c2ebc846bd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:06 +0000

    hurt sit human backwards bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
