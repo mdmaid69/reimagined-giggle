640f2f415a0827114305058274b7838f085fb981
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:02 +0000

    rate fear commit exaggerate centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-print(os.getcwd())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
