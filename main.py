4f1deeec66df811563534438abd522333dd2eec8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:47 +0000

    nothing ball change their rest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_unique_words(sentence):
-        return set(sentence.split())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
