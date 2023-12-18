64889e13fec2b7a6f0efb2d674dac6de9c6c4d0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:48 +0000

    manage experiment broad shine cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
