3b42013fa9952b7d38552654b1f22a01c70716ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:37 +0000

    letter home within thumb slide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-name = "Python"
-print("Hello,", name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
