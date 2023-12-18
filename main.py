0c9504bc0a1c4f37884fe9b6b28d3b046c09dc08
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:39:37 +0000

    argument might trap prove order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
