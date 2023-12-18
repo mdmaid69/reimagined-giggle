aba9bb3af2faed1b5c8637b11b79855461ecf693
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:23 +0000

    567enemy serious control bin turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
