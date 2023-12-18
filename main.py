5616cf1f38318111984ff9ad7d62d5f2c0093465
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:11 +0000

    roof accidental drink harm attach

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_octal(n):
+        return oct(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
