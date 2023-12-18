97c8d4f6573e35f6ad8eeec8fca0a7968f5cf9ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:12 +0000

    after church Yy middle report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
