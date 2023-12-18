3a0135d2a475838cddd6a69939e187aa2e892e67
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:10:10 +0000

    child deliberately pin interesting themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
