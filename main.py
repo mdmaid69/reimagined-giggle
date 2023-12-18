90f75b8f21031cf2c707ce7c0cfe944d620a3146
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:19 +0000

    ticket shopping pencil check high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
