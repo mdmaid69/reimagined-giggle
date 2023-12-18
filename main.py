4e91e831e009dc7143f121b8026f1e70dea75b52
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:23 +0000

    chief port sting emphasis jump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
