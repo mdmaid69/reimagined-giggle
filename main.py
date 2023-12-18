1dab998fa5c518c5a0652dc81ef0e91ba37b5c40
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:44 +0000

    ride time furniture Ll trust

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def greet(name):
+        print(f"Hello, {name}!")
