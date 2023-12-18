24a817452aecad801bca1dec761f866e1b76b655
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:08 +0000

    whisper intend miss use wild

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
