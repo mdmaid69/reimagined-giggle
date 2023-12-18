29d96925aad427ac8cf6e5ceab46282f1f2cc658
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:44 +0000

    introduction become whisper change ignore

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
