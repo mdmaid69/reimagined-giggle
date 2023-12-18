80ea8c06193226e7fbcd9ea0c8a9e076444b34d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:38 +0000

    opposite belief scream bar tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
