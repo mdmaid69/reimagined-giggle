3e0c36472af5a53331be46f1ab151d143e3dfb4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:26 +0000

    furniture neat guilty measure use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
