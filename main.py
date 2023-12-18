1614d66db5a5f11c04346753d66c4fb6a002dc7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:11 +0000

    total result smart waste place

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import time
+print(time.time())
