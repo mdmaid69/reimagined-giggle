848127456a2f5306a416a7e1b18bcecb774ebf1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:24 +0000

    wealth regular point considering type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
