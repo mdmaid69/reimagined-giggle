a46d8ec9cc16be19bcbaf3f9930d518f1bb991bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:02 +0000

    holdup name leave throughout can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_current_directory():
+        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
