960a87b886b57c70790f7ba7f13c566d93890fdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:28 +0000

    remain dance reaction lucky uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def add_numbers(x, y):
+        return x + y
