7b0ee5e13a7bb424e899f1e93e972319e67d65d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:32 +0000

    feel welcome wind view still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
