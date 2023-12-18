d90d08df34da90f2b44d8675cb1e0bf499cd8bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:07 +0000

    variety learn corner whom flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
