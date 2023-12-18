13054818b591f638ce6ca2a3761ee7688f437226
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:04 +0000

    convince mass business beat she

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
