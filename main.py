2373690da0a12507d7d9fa15a4518266c2ba3105
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:58 +0000

    pain protect stage wealth meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_frozenset(array):
+        return frozenset(array)
