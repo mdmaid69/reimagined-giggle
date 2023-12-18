bf93e496f5087b188bebb40402215a07ab65f005
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:16 +0000

    school file list ill computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
