ce25d97cb79094f49c84ba8f0ac31a5a9590fc15
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:50 +0000

    floor total battery plan trade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_min(lst):
+        return min(lst)
