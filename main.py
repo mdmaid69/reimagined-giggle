00b9e74e017281d2fc657555a9f6af18f006b444
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:41 +0000

    edge figure us preparation return

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
