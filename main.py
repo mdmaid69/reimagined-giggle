77495aaeefe72bccdb066a1fce29e2be380e7285
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:20 +0000

    clever honest punishment before mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
