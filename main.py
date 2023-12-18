0cd7a719c7743c1bf9ee753526df58c5d03e0bab
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:50 +0000

    task crazy crowd attach exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
