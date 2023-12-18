92c3552187a7b4468284c571f47b0dbbad210507
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:05 +0000

    embarrassing explanation stay cheque shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_string_from_array(array):
+        return array.tobytes()
