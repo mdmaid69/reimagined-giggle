39d41fa21dfac69259f86ec7f42334d0f5a27c8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:00 +0000

    strike fat point hurry peace

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def sort_list(lst):
+        return sorted(lst)
