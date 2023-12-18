4f7e7d9dc0cbe1d513289e6854da2e8af7d1ccf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:11:50 +0000

    opportunity clean 565end manage beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
