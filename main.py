cd3d6ceea5efa6abe24f7bb9d39b4bbd5fe831bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:26 +0000

    this suppose floor union only

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
