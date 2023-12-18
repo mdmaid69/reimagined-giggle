241cfeab12f63701a352b46be339c5434b939cc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:28 +0000

    now breakinto advanced motion rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
