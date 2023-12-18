f00de34c7a25cf20e158f7bfb168634c394d186d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:14 +0000

    tight eat week fair compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
