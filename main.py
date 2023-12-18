7be0ce3de5a8a62c7753c22d505dfe99be60323a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:31:00 +0000

    hello miss rest present large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
