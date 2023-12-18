893b091b71c1b4d39ac8d2eee64481473606b643
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:41 +0000

    suckme exam strength improvement height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
