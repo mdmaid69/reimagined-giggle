0481c68694c4b063755c3ec625bc14cd7bb37548
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:54 +0000

    adjective pollution stamp within on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
