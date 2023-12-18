9df9c15a38d0536b5232677b61b4548238317f19
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:25 +0000

    serve fairly mean tradition cost

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
