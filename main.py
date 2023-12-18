51bad618dc13efe376087a7aa3df4518a483f717
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:32 +0000

    peace wish cover motion month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
