a106a0d4d4fc6e9061d51801f5fdcaea421558de
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:19:19 +0000

    ask belong would trap half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
