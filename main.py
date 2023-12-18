3cf369adc970be411001005ab0b1a72258185363
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:42:50 +0000

    ours quickly doctor Zz funny

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
