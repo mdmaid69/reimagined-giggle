d27719ee1d1ececd3d38b5c98572ab01cab3908b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:53 +0000

    equipment ourselves breakout either plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
