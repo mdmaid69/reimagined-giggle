eb95b49b7feef4271040014690c962eabaaa40b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:44 +0000

    tired decision tongue group runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
