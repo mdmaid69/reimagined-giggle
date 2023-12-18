6f0ce3b9f81971c56fb780a0dc601eab1acfc195
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:16:59 +0000

    welcome business just base knowledge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
