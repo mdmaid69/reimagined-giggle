b614133f0897fa06f02dfd2a71b72324ffced81c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:41 +0000

    rarely age not Itself mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
