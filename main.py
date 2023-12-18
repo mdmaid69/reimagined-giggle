12e983b4c6c2b98038663927dc389bd54517adc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:45 +0000

    direction fairly tape breakthrough hide

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
