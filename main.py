5770fecd4c8d2421fed67872b1ad00cd2bfb1bab
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:51 +0000

    straight steer geton whistle luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def add_numbers(a, b):
-        return a + b
+def reverse_array(array):
+        array.reverse()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
