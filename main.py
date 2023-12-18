85c503efeffb6a82ecbc312ce8f3a7ad460b98ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:18 +0000

    refer exaggerate plan dark apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
