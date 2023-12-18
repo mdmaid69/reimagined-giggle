43170088b38aa1a5a3a0461ced22b26ff9b159d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:55 +0000

    degree wall clean disgusting then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
