504f6000a0338faf79cc585fff3cd557c379b129
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:15:47 +0000

    exploration organization experience enthusiastic rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
