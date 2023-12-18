162ead846641c3f1bf958cfed32cfce474ff7cd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:11 +0000

    keepaway design equal interested for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
