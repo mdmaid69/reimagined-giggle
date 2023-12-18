ae478a2eb7b30ee75613d89ecd0d5426e1e5f251
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:13 +0000

    name typical common song division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
