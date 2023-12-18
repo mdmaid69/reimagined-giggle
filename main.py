0ac842c4eac1f1b4d47aa8c25a3aef9ec8e77dbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:48 +0000

    scheme admire sink turnoff breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
