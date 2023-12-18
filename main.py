3da69e2a5e7157886f6bc7125a7943ce20d9faf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:35 +0000

    clear scene pollution in warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
