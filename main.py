65b616a32c39c838c20e03d47c9715fb79f49ca1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:36 +0000

    something study medium occasion improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def is_odd(n):
+        return n % 2 != 0
