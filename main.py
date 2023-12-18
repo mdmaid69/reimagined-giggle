34846e174b0edf46910ef268fe8e8bd14c7c43e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:25 +0000

    570enjoyable life foreign low issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_area_rectangle(l, w):
+        return l * w
