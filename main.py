3c7c703cdcef41d32107aeadb37830bc722f5e9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:52 +0000

    fit nose another count success

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import numpy as np
+print(np.array([1, 2, 3]))
+def sort_numbers(numbers):
+        return sorted(numbers)
