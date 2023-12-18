f0df8ccfba4af143abf0bae495b99cdd4fac6293
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:34 +0000

    deep stage challenge takeback none

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
