0bcd651584eeabb6585994b0075def829185014b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:08:08 +0000

    strength blow paper by niggaacademy

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sys
-print(sys.version)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_acceleration(speed, time):
+        return speed / time
