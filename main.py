0883a195c48c2688fe1e75a0d931613a226ab3a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:10 +0000

    mainly container strange pot nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def add_numbers(x, y):
-        return x + y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
