f5e85b71a604d3f1c4c746f86180441e72e47ec4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:42 +0000

    ocean blind otherwise culture whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def get_array_length(array):
+        return len(array)
