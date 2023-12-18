94da7f6630dfce43459e59d2f840388e7537cbc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:03 +0000

    sea detail getup lead switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
