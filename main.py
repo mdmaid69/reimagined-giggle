014e0593be99c1a03ece438f9aa184d881140cab
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:51:09 +0000

    favourite Yy phone mean be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
