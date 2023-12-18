41e468f222875161b44c6b5e9af86fc59ef40469
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:53 +0000

    sad post brush pause frame

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def add_numbers(a, b):
+        return a + b
