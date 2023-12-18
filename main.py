d43cf0fb8f76dc53f39bb6ae376c0773413b1d69
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:15:46 +0000

    contact operation basic cut action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
