77248e79df462de0478ed6cab6505063b8af3250
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:52:56 +0000

    somewhere deserve range Vv near

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def count_elements(lst):
-        return len(lst)
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
