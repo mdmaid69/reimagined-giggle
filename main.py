657be2e28c2619baa09e56672195fa79e81c8e94
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:58 +0000

    collection convinced race confusing bang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
