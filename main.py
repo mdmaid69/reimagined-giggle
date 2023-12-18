e4bc9ae18dcc1cd94f86a97b683d2fd161614bff
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:50 +0000

    back surprised date stay anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
