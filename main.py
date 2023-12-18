e5da1cae55e5ca8f8912ba8ca9d53c4c482d5c70
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:33 +0000

    level mysterious situation base straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
