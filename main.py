5c75e5929c7e651735098f36352532d4ac855369
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:38 +0000

    Kk know impressed majority care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
