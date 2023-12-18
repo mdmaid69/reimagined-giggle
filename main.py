15bbe4906b80421daa9a7789557205db949ba48b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:14 +0000

    nigga hungry post thinkthrough side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_odd(n):
+        return n % 2 != 0
