c44bbe8f62941b736722a5e6f3c10e80a5969a47
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:46 +0000

    influence he chance winter sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
