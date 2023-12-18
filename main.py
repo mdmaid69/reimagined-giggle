b072b5027b184f3bb3d34b73a766bba57efad6c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:54:11 +0000

    set national command dig play

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
