cc18763e287cf208144ba46d76f1d83d86b27507
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:51:26 +0000

    skin such remove badly minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
