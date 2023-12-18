d8bf2748c991dff12863fc9640f00a0281f50ed4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:43 +0000

    mass comeoff office star across

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
