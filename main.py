e9646b1a57071a0a218c541a1b31af6f83d030e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:17 +0000

    hurt hollow possible best lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import platform
-def get_os_info():
-        return platform.uname()
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
