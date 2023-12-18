1691e19002ed980d02a19f7612f7591560a35da7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:06 +0000

    giveaway demand word weigh often

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_base_name(path):
+        return os.path.basename(path)
