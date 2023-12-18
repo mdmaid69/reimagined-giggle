a869c8e9d2b9a25df61fb97ebc2bb4036529e7ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:40 +0000

    pleased Yy drink store dull

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+print(sys.version)
