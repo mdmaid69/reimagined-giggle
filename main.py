c6a0dc8d8b41b2108934ff5b5a01cc9448c8440d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:40:13 +0000

    cause clue influence getup trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
