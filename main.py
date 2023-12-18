4c253c99410538c5149d7d81cb6b5318df92241b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:31 +0000

    successful lay translate flow fork

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def cube_number(x):
+        return x**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
