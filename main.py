fa9f48d601676c5201d89aeaf4c7c11246c66a20
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:01 +0000

    region improvement beginning interesting depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import numpy as np
+print(np.array([1, 2, 3]))
