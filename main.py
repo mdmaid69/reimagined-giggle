c5448f7571f3ee91e204bc4b5b1a60d54959fb93
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:42 +0000

    reduction either art flight clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
