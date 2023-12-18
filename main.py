3100925b9d2ad6ed19a7a1ca3454b41a8d7624fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:06 +0000

    urgent aim match wide fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
