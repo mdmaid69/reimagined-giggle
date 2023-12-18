8f06608555f100218b39d34c26fc549b0962139a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:31 +0000

    not basis suppose threaten crash

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
