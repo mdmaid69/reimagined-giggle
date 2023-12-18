50a96e6dd74d55480a62c11e017768c2e2dba0cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:35 +0000

    under weekend afternoon ground calloff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
