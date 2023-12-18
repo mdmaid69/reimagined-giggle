d5c2e8156a446e417c5e7742b17591925fdceb04
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:06:45 +0000

    tough prevent possibly grow reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
