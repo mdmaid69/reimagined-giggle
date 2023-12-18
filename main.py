cb05a2ede08e7cec587f1719333a4904b6d82964
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:06:09 +0000

    feel anybody cold bringback just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
