d9f5300ede6dbc0324d1954af867b577ae61d654
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:32 +0000

    pay face familiar bringdown increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
