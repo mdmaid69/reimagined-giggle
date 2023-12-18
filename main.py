a4fbab1e10d45a4d4a3fd01d1dda747478e8953f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:09:43 +0000

    absolutely firm guard miss black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def reverse_string(s):
+        return s[::-1]
