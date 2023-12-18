7ab1ffe5affc7314dd19491ba47df67ddae4c47a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:47 +0000

    be test ear typical press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+print(random.randint(0, 100))
