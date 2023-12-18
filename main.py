003dffeef9c071fd7476cc6f90df02d0bcb2a3d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:07 +0000

    race fond interruption side public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_max(lst):
+        return max(lst)
