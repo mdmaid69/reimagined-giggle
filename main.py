3a2d455183dbb7c70a749ffcb97542eb9085d4b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:25:54 +0000

    sense not float equal size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
