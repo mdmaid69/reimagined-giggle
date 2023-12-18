6ba992a65287826489f32a1e3bde6df6eda61759
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:48 +0000

    else equipment game artificial late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
