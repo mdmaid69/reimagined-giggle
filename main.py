7137a77bc5063d341f4134bd724ec2c1e145ca87
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:34 +0000

    bus excuse instead stupid Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_max(lst):
+        return max(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
