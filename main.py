3956c2878bdb8ef9ca6576df1fff136f3ff621a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:50 +0000

    nothing gentle worry modern able

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+def get_current_date():
+        return datetime.date.today()
