9b5d7e917084e1f0100172d3d58c6e3b0c235bf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:12 +0000

    travel distance comeout we heavy

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+print(sys.version)
