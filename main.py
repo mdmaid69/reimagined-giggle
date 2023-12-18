8cebf9756373e0fa9d4850e64fdc14b335f8c40c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:28 +0000

    clothes will for connection cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
