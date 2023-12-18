124c00edd0a7523cc6d18558aefb28f21268be44
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:54:21 +0000

    same comeout compete crazy collection

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
