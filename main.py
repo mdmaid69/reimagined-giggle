09ecc7ecffadeaba629fb49a32760c3f88e76b64
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:58:53 +0000

    approval nothing needle left sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
