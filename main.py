debe94191918ca88e4f77fa02da8f0680dc9a650
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:57:39 +0000

    second row listen bin shock

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def divide_numbers(x, y):
+        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
