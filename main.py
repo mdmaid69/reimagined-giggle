627f02c85a0ed433b89b4ff0e747e4a87b97f8c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:04 +0000

    practical strike already switch drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
