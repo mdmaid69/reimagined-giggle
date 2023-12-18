b4a36d6975cc0e1f76d4bbba187055fd75913955
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:47 +0000

    floor holdon mine demonstrate broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
