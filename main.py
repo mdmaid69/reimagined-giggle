70c86d8dc1633ee4d25ac3bff5fe69cabf8ece5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:01 +0000

    completely trade miss culture handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
