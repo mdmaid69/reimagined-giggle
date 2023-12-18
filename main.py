6cb59ff98ce93a810033eab72ec2d49209befcee
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:36 +0000

    along badly mistake hole education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
