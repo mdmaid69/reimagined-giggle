94906fccf618b27fa174454fb7ff186f48fa13e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:48:48 +0000

    turnup which stand major reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
