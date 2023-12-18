f5986ba6c58f1d848fac04aa602f54c86eef22f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:48 +0000

    news cut sport convincing point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
