63690b876abac43c4e9324b7a3b3757d87fc42c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:50:03 +0000

    breakfast workon suggestion split bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
