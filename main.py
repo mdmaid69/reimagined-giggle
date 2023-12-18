debbb57918c555155b6b4d2ab6e9ccfa1116cc7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:47 +0000

    slip successful ahead zero invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
