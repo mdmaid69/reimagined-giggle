ab9973b1ad6700aba786df8db31e778cbf428e8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:38 +0000

    excuse rub apart hard else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
