780687c2b7d5be61cd2a31129687c58b254d06d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:01 +0000

    common organize must whatever male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
