8f304df45603a67b5c4ad9eeb33bcd7a0835a6b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:10 +0000

    bin fair typical sink comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
