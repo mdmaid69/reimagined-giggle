2ce2f5b478a9b380c53047923cd6dfc1fe80686c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:06 +0000

    view perfect sign pleasant nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
