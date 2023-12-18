5bbcddfa71c771a8589ac032153d2f8ae04e31c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:22 +0000

    worry holdout identify tight hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
