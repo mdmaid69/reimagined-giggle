917ebfb95e2a6ae1966e6d768128dd314b456151
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:15 +0000

    work must cheque decide she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return math.factorial(n)
