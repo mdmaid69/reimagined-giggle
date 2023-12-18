6cd0af2379e3346672b22971b48976ed027cd853
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:41 +0000

    bunch glad pinch tap embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
