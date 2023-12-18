ff1023cf201cd72f7ecc7f9635bdb67ecd578ea2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:15 +0000

    land brush damp should dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
