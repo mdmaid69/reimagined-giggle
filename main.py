4c0d79ba8ba7f39e595a5e1f3ef6fa106878300a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:01:03 +0000

    rush worry conclusion weekend brother

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
