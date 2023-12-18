d0e007a6718f326213ee88d3401cec2b2ebba686
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:34:27 +0000

    swing Uu pollution cut cheque

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
