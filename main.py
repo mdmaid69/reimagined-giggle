ddcc7d01ea164c1f422f78498938beaaaae1668b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:11 +0000

    hole gift convinced male disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
