08e4e98ab90346657bf9385324289805353f1186
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:51 +0000

    late inform suit chin road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
