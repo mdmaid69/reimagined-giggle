f7ff9b422dccb3263db862e20fb33f8c8f2b459a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:23 +0000

    common ignore part protest scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
