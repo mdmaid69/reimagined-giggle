549445ceeb0587d16dc36c1595629d80c3b5010c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:41 +0000

    thanks flat necessary Gg throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
