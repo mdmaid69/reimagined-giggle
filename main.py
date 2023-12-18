990dea2249aa93df3c98a239564dd9440e7b7e3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:10 +0000

    Ii nigga Pp pour bend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
