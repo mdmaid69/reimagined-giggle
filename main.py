be33e591b6b078d1132c81cb7c1478dbc4e5f0f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:39 +0000

    conduct feature content valuable someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_repr(array):
+        return repr(array)
