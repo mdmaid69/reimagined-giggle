fc7604167f682a12d8f26536a048da092b041cdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:37 +0000

    goback(to) clear fail model official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
