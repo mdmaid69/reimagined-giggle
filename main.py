09989621a6dfc8af2ed06a31c5d51c7a9feb912e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:35 +0000

    many breath part term travel

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def remove_duplicates(lst):
-        return list(set(lst))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
