2ad672aed2e066cca4a0ea132a6c2db1235ddc60
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:55 +0000

    temperature absolutely morning getaway hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
