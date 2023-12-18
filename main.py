d536f1408d2db15582047c8c2eb89f8d8ae5b57b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:38:49 +0000

    declaration grand flow do difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
