b0ed4719715e9e7ca11a1c2561e52deb9b12dd56
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:44 +0000

    tour copy shape scream beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
