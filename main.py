b6189619b33b935f195af3ea3b99327af101b1a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:37 +0000

    key wide difficulty task introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
