e1304bf3f7b9c5cfc8fba70818aa82f6e6e2deab
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:13 +0000

    important advice pay ignore owe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_speed(distance, time):
+        return distance / time
