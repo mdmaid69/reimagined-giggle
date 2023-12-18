d411c4ceaa04829f6b5643564e20ac4ffa3e8424
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:07:57 +0000

    unfair goback(to) gentle angry present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_floor(x):
+        return math.floor(x)
