f4647a38f35a5e72e09b8794edadb45e827ba3b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:09 +0000

    confidence doubt silent steep completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
