32ecc996711795cd2bec67447e617f67bebff164
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:40 +0000

    divide sure concern judge ought

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
