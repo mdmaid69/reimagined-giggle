f3f2cbfbee9c349a76e7bd3d246e10c87fbc0809
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:01 +0000

    narrow method embarrassing alone my

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
