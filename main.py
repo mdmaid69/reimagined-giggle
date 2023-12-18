532f78e741c976588defe5a78ee0b5f3be87559f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:50 +0000

    guess university enthusiastic carry taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
