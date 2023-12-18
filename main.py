a55705d84067739478294f09598edc16a7f1fd5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:30 +0000

    allow along appear takeback birth

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_power(work, time):
+        return work / time
