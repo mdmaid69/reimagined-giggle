7ddc5b4f8d7276f3e05a14b50c99c514a25257e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:38 +0000

    sea 565end talkback run shopping

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
