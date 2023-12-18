bd42cf7f49511fa988e1cca047e96b080939fe5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:17:33 +0000

    wing run risk inquire leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import sys
-print(sys.version)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
