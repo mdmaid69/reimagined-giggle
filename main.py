abe140e39732c1a3cf439d63a525e7b7e3eeb842
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:25 +0000

    fun measure walk through ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
