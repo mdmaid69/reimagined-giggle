021675a3f32adc882cd8cbb04bbc6cb1129163e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:04 +0000

    injury easy declare acceptable occasion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
