a14edfa04ed13322538aa7d1a12a47b7bfad7cbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:37 +0000

    wild scrape state makeinto continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
