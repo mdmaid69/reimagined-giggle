e8de3787d9b3605295c238230f623ee0ee0615d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:18 +0000

    invite male drink argue runover

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
