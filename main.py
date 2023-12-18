cb3e87c40f81c5d43f48df3a282620a6fe99be20
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:05 +0000

    while correct recognize education and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
