6fe0367c2179d95ca12c4e74c2d338e6ca7587fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:39 +0000

    reason clear scheme together anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import platform
-def get_python_version():
-        return platform.python_version()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
