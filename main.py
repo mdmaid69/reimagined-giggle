70747351357b8eb5cf8c2d563a71e4b17e2a4ae0
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:37 +0000

    shop equal happy exploration keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
