b759e401edaaab686d09d9ef7ba687317883a956
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:59:34 +0000

    name him state muscle escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+def print_python_version():
+        print(sys.version)
