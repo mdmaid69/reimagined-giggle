b3b0b4453c56db18950019b77782ba56bac07220
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:10:59 +0000

    mistake hope care angry unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def sort_list(lst):
         return sorted(lst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def reverse_array(array):
+        array.reverse()
