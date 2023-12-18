9a13c26d8c6fb51686124fd946ba1105252c2a71
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:10 +0000

    welcome 566end present violent shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
