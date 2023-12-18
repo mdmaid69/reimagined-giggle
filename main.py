47f761c288e995088408cc10539f53fc059a2705
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:14 +0000

    laugh ground few tough narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
