2713ea498948955aa2f586d65edc25c5e1ddf1c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:32 +0000

    talk female control bottom rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
