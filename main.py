61c20906fd9fddf13d9d3f93d4e2d862f6a3d477
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:26:29 +0000

    square confident pen correction declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
