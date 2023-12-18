c3b1c5ab75e5e712947dc06b9c6a9066d60e1708
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:11:29 +0000

    generous sense future tell dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
