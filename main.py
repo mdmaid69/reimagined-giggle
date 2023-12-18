f8863d0f8b7a58d4142dfc700e36a745731f22e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:37 +0000

    glad sleep drink themselves turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-name = "Python"
-print("Hello,", name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
