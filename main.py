2ca5eb368f33b64b492b9e4d64c89bfb4c828230
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:28:41 +0000

    cause mouth square otherwise Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
