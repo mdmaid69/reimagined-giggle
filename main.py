04bee9794122dfe0305a40075f8c41271c4c93ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:16 +0000

    past concerning hope every bank

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
