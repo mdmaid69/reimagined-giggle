5891b2049faeda0cb26562f971d47b439e23692a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:55 +0000

    tired line 179bite especially avoid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
