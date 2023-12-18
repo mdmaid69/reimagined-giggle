39539f786aa4c2e43e1c99998bea885cd87d7cb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:29 +0000

    general interruption that only 571enough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
