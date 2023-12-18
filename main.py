3020f67f0c3a1a8719f6d0a6f288a2b57d5dd19b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:24 +0000

    wait free smell half swim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
