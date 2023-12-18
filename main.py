78e6e25df6098461959431c573dddcabc79634d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:40 +0000

    dream holdup limit bringup wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
