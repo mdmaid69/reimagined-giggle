e5c72604bfe0e1f9a02ac34a6a80f65d7028199f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:56:43 +0000

    anywhere centimetre catch character while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
