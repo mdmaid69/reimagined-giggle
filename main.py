353d0a0c912e8858a8b7ac36ca4fb24fa3b8e59a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:32 +0000

    cry responsibility regard protect tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_min(numbers):
+        return min(numbers)
+import os
+print(os.getcwd())
