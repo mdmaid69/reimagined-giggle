a4f6990575dda4a6ca9a0c563c15c03430a669dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:06 +0000

    judge tune study praise interview

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_max(lst):
-        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+print(sys.version)
