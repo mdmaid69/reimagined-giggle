945397ea20bc79b6d86dda7d3340893e2e6732c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:14:14 +0000

    smile want blame week shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
