f3b1624e895aa70a298c12926c88c98b37f9ed92
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:19 +0000

    concerning recovery quiet rule everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
