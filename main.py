cd0984c5095af845d5c74fc70398ab28d79baffd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:26 +0000

    contrast advice case convinced scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
