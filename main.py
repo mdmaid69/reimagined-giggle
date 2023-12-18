69cadf33036d528ae96f5251cfce407097432133
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:37 +0000

    loan manage any march warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
