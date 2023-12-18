8b512856de48ed8068734ec0fe7c1fd84ff0317c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:55 +0000

    keepup far survive provide talkinto

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
