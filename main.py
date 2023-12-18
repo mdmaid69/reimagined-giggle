2e2bebf3a37f211025ca3440850a883957a46d7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:22 +0000

    buy lock environment twist whom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
