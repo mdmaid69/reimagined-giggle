e384b92c47ec3e70abea84dbab7336fbcb696cda
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:38:58 +0000

    neat head Ee system bin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
