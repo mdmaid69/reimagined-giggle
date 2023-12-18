c9616a20e00002473dab9ae5064668c6b7e5fd72
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:56 +0000

    already cost contact lift file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
