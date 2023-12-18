0791b442c1e2e4346e8330823829547bd218daf3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:03 +0000

    so broad problem still excited

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
