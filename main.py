35a4c2bc7aceaf38572423a7f1b31b53fc54b5c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:34 +0000

    Ss knock whistle bored suffer

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
