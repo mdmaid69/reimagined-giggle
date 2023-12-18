756517361ef6dcfe2f3d4be10318ce81227feecd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:28 +0000

    react gradually complete will quarter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
