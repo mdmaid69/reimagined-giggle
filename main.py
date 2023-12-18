a2320e480c9dc19692c099fe087cb19b81fcd936
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:23 +0000

    zero turnup considering block long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
