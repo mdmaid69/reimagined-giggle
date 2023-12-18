0cbaae8172db77fd0a1abca435ee43aa10a9c8e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:44 +0000

    easy operation equal formal tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
