294cce47399be36e5636b2648068e708438f8963
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:59 +0000

    characteristic next small ahead used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
