3219afdcd9f457d4f487bd1ab426affc293013db
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:33 +0000

    564encourage check if getout confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
