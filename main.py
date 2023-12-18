cfc9a68ce58d3182158c9b75b014772d2fa870fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:06:34 +0000

    free downstairs side tape worst

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
