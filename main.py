2346f36da8dbe883a0b0b44abaece8c7ef2365af
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:46:02 +0000

    visit there risk sea except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
