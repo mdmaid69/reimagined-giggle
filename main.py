369671427a160e377fdd9221b3b5eb89baf16840
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:59 +0000

    my breath turndown smell satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def cube_number(x):
+        return x**3
