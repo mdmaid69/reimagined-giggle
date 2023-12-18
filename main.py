b98e3e50804822dbed578d1fa92cc0d7cc36133c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:31 +0000

    relation giveup hot involve call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
