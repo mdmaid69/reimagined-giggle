d93311ce1da5484ebbea4c692979e8af17f48af3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:10 +0000

    luck sound chase not spite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def square_number(x):
+        return x**2
