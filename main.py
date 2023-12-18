84aeeb5e2f780e132b8137df00a9df506896dff9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:54:01 +0000

    influence exit persuade hurry concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
