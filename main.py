047f96c9a7e1a3a9be91a14950a14a625ba64bfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:03 +0000

    round between accident distant equal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
