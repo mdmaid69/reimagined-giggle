c94a8a52cd4523720b0910a69487f1c2b9249423
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:45 +0000

    consult cold fresh everything test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_volume(length, width, height):
         return length * width * height
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
