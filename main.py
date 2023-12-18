eeba68abc788076f75665eacd15f0987fe92b1a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:24 +0000

    everyone nor interruption hope poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
