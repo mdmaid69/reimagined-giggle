e4a8a22c7af2cd5bd0da17d79b5bcdea58b00958
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:05 +0000

    pleasure meet morning complain join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
