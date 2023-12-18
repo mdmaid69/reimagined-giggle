335b9018af6ee089175e5906ddd221753bf4e4c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:57 +0000

    rough laugh somebody method reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
