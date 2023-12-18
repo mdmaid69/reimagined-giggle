be3db21bd0e98bd0e9da8dfc732901486e4052ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:44 +0000

    into approval nowhere same exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
