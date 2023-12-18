c7199b9b2ea3cf750fd3aec10646f9f8dc0881e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:31 +0000

    easy evening pour considering change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
