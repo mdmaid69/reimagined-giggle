0ea7ad19268e6b425f4326ad329182bb6f904cf5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:24 +0000

    may how definite frequent poison

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
