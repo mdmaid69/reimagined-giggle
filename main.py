9d3cdbcc7858584603447c426aa3431292b1a9d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:12 +0000

    scare subject bite shoot exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
