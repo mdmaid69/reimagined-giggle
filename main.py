1cbc9765eb4c0edeb765512862b276a59590f293
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:37 +0000

    spring murder wish bill drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
+def is_odd(n):
+        return n % 2 != 0
