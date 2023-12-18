6873475d8b5e080bad1fee7dfd61b7843bea24dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:43 +0000

    clean perhaps brown bringdown crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        return sys.version
+import tensorflow as tf
+print(tf.__version__)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
