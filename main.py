7217a97811ec8d30fc3d34e44217605188a4b5f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:55 +0000

    on introduction makeinto travel temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
