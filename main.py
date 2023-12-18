1bdf29f3e18d34c74e03f864abc494ffc887acbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:36:09 +0000

    snow favourite range voice goon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
