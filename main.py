becbd684bee63ad00f1bec0ff5c561d9975f3f03
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:58 +0000

    bit feel hers reduce extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def multiply_numbers(x, y):
+        return x * y
