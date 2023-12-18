9d315630969183788499b19d288ba33e228fb88a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:22:01 +0000

    inform public holdup else holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
