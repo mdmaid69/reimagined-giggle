3ffb27144ce2411160066c8a82d8e79f987b8f4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:27 +0000

    married afternoon should heavy bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_area_rectangle(l, w):
+        return l * w
