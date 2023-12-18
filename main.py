2f422a1189aa353d4bb394f1c3a92879f80ee621
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:07 +0000

    poor hungry thumb cold comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
