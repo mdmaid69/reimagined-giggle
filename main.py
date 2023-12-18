4fdc56a69a8fa334a86454801092a86f6e0890cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:25 +0000

    habit will card join mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
