ed8848426b2c9afd0e7148629aea66771d05e39f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:41 +0000

    air male accident urgent concerned

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
