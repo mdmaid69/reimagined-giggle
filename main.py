5e26223b7322f58ae37ef0849a450cd764dd11ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:26 +0000

    race together square tonight chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def get_array_as_set(array):
-        return set(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
