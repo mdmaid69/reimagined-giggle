0aa08a4bfd2150b601bcc0612de211a652eff140
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:44 +0000

    maybe weather Nn everyone weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+text = "Hello, world!"
+print("Characters:", len(text))
