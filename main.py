59a304048a93acc17e52702427c774164f378d4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:00:17 +0000

    needle turnout capital address thorough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
