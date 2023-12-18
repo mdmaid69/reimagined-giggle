bfa87537ef0cd1887339d91b448cf1b964f8a04b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:52 +0000

    that properly mind goup last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
