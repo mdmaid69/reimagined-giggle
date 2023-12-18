50e82288b8af5c8e75afbb55a33977755c1231b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:16 +0000

    stone maintain later crash habit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
