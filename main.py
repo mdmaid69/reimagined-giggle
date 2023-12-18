0637b1757cff5944aae23ca218709401b1308ea1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:10 +0000

    jump suckme everywhere trousers thumb

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
