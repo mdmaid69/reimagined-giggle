5da878722b19824753d3e8f4bd9ed5e1ceb99d56
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:50:29 +0000

    moment practical mainly top fit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
