d5607b214d08347992966f3164d207a798e8d512
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:57 +0000

    continue slice muscle brother reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
