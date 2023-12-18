1b2c4f2127b93410bd14a96a39659f35c559e59a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:43 +0000

    settle book fire behaviour plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
