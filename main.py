088436f5d975a4d4e224bdd1cc477f5f7d418ef9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:30 +0000

    become reject operation treat nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
