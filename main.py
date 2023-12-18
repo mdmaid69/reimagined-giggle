3d23806d324b51a0044cf1812f5181f472e903dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:37 +0000

    worst problem winter contrast dangerous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
