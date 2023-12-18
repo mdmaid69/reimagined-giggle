d3de057e855ad0f1ac291d3a9473b8980baf7273
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:34 +0000

    issue age understand disagree shake

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
