76f081c7eed3463ee215a2257191461500927013
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:48:33 +0000

    dance pot bank mainly tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
