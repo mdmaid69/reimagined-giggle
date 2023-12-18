49fd056554f3957ea50c483499dbe84bf4068664
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:36 +0000

    study meeting quiet party immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
