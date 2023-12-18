f633cd5c88db3c461381a4d700002cbb402a9d6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:57:34 +0000

    off limit press might young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
