15097fbb87aa3ebdd102698e0ebf5d597c23af75
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:36 +0000

    deliver connection feature general plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
