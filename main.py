414d1e222ae2637d60be63f8a3ebfa8b30e73255
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:27 +0000

    careless convinced attention uncomfortable string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
