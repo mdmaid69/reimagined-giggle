e7b359ae0b11eaff6f84f197d0aedcb1de0a61e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:33 +0000

    check raw not part pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_tuple(array):
+        return tuple(array)
