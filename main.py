900bb2c7a9b1d7658a21b1ae63828fc346dbb420
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:51 +0000

    tense organize talk battery 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
