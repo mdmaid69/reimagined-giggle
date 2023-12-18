22cf297a323f1c0273665a5114bd37d49dbfd1dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:32:37 +0000

    bite entry depend narrow letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
