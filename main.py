6bd31c565f5910f6febca0d6c6f782d5b530a674
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:29 +0000

    patient thanks judge division answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
