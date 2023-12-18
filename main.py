e53972ff2da882234d79021f1bd8e59ec880e61d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:16 +0000

    help more food care score

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
