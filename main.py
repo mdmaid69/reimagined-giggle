b90d39f01825fe675ccb18b69f402f41ffc6dd1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:42:47 +0000

    minority alone squeeze ground rare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
