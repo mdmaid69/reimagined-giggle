c5a0147f7a64f0297ea402eac484fa2b6df7ac13
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:51:31 +0000

    guard contain spray talkoutof ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
