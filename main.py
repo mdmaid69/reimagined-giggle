eaea7e55e4d7fb8d2734cb20fc970dced6cd9cd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:55 +0000

    passenger price everyone rid claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
