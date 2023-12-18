3595adfe0e66229b8b863f53668dd4450308c5cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:54 +0000

    still relax system story sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
