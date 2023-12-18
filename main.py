fb88342dff46f89e794110aadeadc95977290603
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:31 +0000

    road thank speed why bright

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
