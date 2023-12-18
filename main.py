d689ef75ba7e3a64ae80e4b74f88114cf90448a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:26:17 +0000

    anywayanyhow complete spring exam mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
