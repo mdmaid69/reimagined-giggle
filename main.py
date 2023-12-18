c79485133b3a94608c2c4fdfb7566d10c8207949
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:32:31 +0000

    cruelty Tt forward back he

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
