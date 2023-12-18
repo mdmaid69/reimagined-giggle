9893c9bef47f6a3878326b02e367d72aecc91460
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:18 +0000

    back sound takeout sometimes level

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
