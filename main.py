a263c31e195de69364eb3bb56f2731a8459954f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:59 +0000

    field exit front emphasize try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
