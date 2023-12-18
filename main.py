6c729a680f8917cdf43c34fe9b1b0c4c6070d22d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:32 +0000

    spend guess frightened justice regard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
