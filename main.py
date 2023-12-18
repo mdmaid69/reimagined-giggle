beaa22913f0f45b0b66b746f14f0146e48159525
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:21:31 +0000

    until Dd natural head guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import collections
+def create_stack():
+        return collections.deque()
