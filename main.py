776e092912274f4441b327b7cf74d4fd8191d9e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:34 +0000

    later neck fall 573entertainment hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
