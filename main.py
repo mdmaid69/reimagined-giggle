217d80ce2c2d2a4f9557fcebd4b4a40a859d9700
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:58 +0000

    disagree wipe public interested add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
