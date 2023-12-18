63278ee532f64be06f5f7d5eaa76fd91c915e772
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:36 +0000

    giveout this battery pocket belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
