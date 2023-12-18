10d59039860e2b991af0831f4f3b88cdf1be7b3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:34 +0000

    reserve rather side impression who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
