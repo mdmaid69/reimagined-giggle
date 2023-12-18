dab9e477ee1f2f722c9485807413628484d5aab7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:49 +0000

    grade straight either full complete

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
