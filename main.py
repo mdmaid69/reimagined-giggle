56b3d80aa8fb09273f795f19736a83aad20d031e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:26 +0000

    sure march limit getup wherever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
