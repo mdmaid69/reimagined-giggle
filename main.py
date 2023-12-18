8f9cada121ba033c036a85e0c6fcdb20ea0515b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:15 +0000

    close Ff pin ticket old

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def is_even(n):
+        return n % 2 == 0
