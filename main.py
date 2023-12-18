9746f9e6fa2055f4b4376f3b0db1bc4fd12e2246
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:10 +0000

    love storm nose corner freedom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
