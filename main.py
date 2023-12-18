acb746af49685c5dabec2653af59ff35dfb90538
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:00:15 +0000

    drive environment trip callout comeout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
