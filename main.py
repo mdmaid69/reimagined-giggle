91139af2487dfc254c1925447434cc669d7c3ac7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:35:46 +0000

    half tired catch bound breakout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
