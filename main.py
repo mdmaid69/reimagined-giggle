2b61ad0be7f9d5ec8e352633fc571ad89dab4415
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:26 +0000

    guard shopping trip profit study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_buffer_info(array):
+        return array.buffer_info()
