c7cf8b15b60595749546300bb3f7e162755b78f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:58:53 +0000

    sink occasional bank fire purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_str(array):
+        return str(array)
