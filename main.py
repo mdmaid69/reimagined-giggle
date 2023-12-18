fddf14b0a7b1b84c51a45391addf645a7c0fbf2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:11 +0000

    sink thinkover Ss interfere point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
