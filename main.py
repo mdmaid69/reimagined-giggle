32858ea5fce514097a3b1935c19b725cb83b092b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:19 +0000

    bringdown level who welcome genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
