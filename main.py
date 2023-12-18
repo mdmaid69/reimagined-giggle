5e7508609af8d5c7c940d0a87e50dcc34de1b6c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:46 +0000

    downstairs politics hear rain ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
