c897075db9d1b87c85b9ef915424dc68ba146772
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:22 +0000

    broad start street determined condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
