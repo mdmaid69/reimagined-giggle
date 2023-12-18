c11e1c547d38f53f8206bfb0735c5ee93505f408
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:41 +0000

    train team speak emphasize she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
