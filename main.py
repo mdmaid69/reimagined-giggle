6f5ea91416ed16a93e5073199207aee74471890a
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:44 +0000

    majority receive Ee snow object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
