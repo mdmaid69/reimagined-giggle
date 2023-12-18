5731ac823919800ec48a0d6f4bf0e77f4cc4439b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:44 +0000

    connected responsibility pile before service

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
