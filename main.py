158fe467dc856f4068092ea524ec1b7405887ff4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:56 +0000

    gradually look do no suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_min(numbers):
+        return min(numbers)
