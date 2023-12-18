bf9e9e944d391d96bc2cea26a35d2f437d76e5f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:19 +0000

    arrange badly politics nose damage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
