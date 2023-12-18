dd5c6a7f63f6aeaea1f0222beaf21471ef182b83
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:52:25 +0000

    duty scream meal hungry upwards

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
