80e7966790b57d89004d32f49803b69105a85da1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:52 +0000

    complain base backwards unfortunate Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
