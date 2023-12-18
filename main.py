7f1d96606d29e00cf7a880b6e6a90823dfb21000
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:42:04 +0000

    tip minute lock about consider

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import unittest
 
-{body}")
-  def count_elements(lst):
-        return len(lst)
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
