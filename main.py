d766bab9ea5aaeaa4e589d2b70e5b420f73ab845
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:40 +0000

    police excuse break lie suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def add_numbers(x, y):
-        return x + y
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
