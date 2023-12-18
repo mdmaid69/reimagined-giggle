79b871ed5bd0869585f73c343f6c23ed76360b76
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:16 +0000

    grade both handle getoff accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
