d06c8bd12bbb35362f816688675a72cd7856438d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:41 +0000

    sure wave point squash point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
