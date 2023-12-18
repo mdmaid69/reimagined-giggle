0d059f5847e44b8ccea46e79cf517af949408081
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:37:39 +0000

    scrap brother where calloff heart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
