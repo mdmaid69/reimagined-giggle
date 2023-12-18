d42b65f11c6cfde10d5640a3d5ccf09f96562024
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:08 +0000

    custom recovery switch instrument odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
