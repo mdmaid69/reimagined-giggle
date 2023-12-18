7dcc3d3d29d1ff8fe53468af6fe27a7fabc2514b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:33 +0000

    idea why schedule pleased frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
