d42d88f83d3ca39a052619a8c994935b65f7ceb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:02:18 +0000

    remain play on strict direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_area(radius):
+        return 3.14 * radius * radius
