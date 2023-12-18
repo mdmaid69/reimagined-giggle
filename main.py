61d0979735b34c7e03ca5ee02eeb7801909e1e1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:00 +0000

    organization performance politics want excuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
