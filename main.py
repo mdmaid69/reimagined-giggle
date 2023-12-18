fe7fa9d5fa4cbac3e355c48e589afe0cc5b360e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:01:51 +0000

    tip do splash suffer talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def subtract_numbers(x, y):
-        return x - y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
