01e625c1a6325731a6325fda3314a741447aae59
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:46 +0000

    boring experience mend thin Mm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+print(math.pi)
