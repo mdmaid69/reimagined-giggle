dcdd6d409db1f81967920d3421c872f76772c078
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:32:33 +0000

    discover long perform possible beginning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
