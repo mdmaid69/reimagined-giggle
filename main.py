31196c7aa68d62f6dca3dbdf35d2c91f3069a3ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:55:36 +0000

    favourite middle beside stand movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
