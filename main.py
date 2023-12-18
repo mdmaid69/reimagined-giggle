788cad7d8bc2887bbe072bcbce93fb11d91a3f6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:34:51 +0000

    unable correction fun nonsense mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def roll_die():
+        return random.randint(1, 6)
