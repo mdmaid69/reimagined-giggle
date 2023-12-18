538ed0b55e7d27372c52be0310d58f1d512b15ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:01:23 +0000

    middle still tell correction sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def append_to_array(array, item):
+        array.append(item)
