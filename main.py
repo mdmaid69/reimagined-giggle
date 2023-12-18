eea0923b16ccb6a99ae1420eee5ad778e3665850
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:05 +0000

    out aware unfortunate gentle invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
