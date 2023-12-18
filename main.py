5d27e2c69d118d7b968a7a5353bdd48cf759337e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:52 +0000

    pay survive name speed ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
