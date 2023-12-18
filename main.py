0089a8afb7f6ba43c2b0a5a0c6b46cf056882cff
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:53 +0000

    cost entrance top man beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
