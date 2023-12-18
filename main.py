2c7f013bee278b811b87f6216fadbe1a01510b61
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:11:23 +0000

    unless protest perfect difficult back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
