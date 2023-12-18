936204120206108d79d4139ebab7786a58fd512a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:14 +0000

    instant container and pick instruction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
