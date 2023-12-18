ee71a8e845b26b4336a03c3e22545c192838dfb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:49 +0000

    of pupil commit month involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
