cad949ce80def9f65931c3867772525befea86c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:51 +0000

    call dinner hope strike future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def convert_to_binary(n):
+        return bin(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
