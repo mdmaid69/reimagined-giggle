0374190009847f525f315f52636b3f6e53a6ba9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:51 +0000

    wet again variety reduction gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
