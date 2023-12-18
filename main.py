ad547c9fb8812ad13fd23c1dee3ba375c03ec63f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:43 +0000

    concentrate lost union cry raw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
