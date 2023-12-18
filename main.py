23fb2c3220a266158fb05db9d4545f08881c285c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:29 +0000

    team general nervous dish put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
