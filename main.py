b61f71eacd2e9ed9d426d0a078a746aeababf10f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:43 +0000

    answer care squeeze impression fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
