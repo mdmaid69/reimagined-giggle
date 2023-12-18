5007565d07f131e1f6d619ee9eaff39e29d9fbc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:16 +0000

    influence complain reply release sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
