1bd97ba6631224478bf1aa450e195dd040d1728d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:33 +0000

    principle often me blind oil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
