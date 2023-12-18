7ecfc94e1ff890299ead14fa3d6ca7d0b2a166d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:30 +0000

    embarrassed shade perform advantage 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
