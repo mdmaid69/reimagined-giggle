19e95f92c1c97181ea370576e60a1fcd1801233b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:44 +0000

    unable mine path good tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
