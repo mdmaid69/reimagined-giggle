829c3fa5aead5ad9d1043190e29e0036838937f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:34 +0000

    basic matter teach regular read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
