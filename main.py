a8e3fdfcc7c60aa99e19cf4812ed4c06a62ee863
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:56 +0000

    until back meaning indeed temper

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
