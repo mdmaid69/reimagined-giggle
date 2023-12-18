aa76a03fa6bc2e43bd4185ec950353909cf12555
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:31 +0000

    among switch safety image belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
