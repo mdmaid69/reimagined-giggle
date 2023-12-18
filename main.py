9ef4f2b9b39c95af891fa382e847af1152225aaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:30:40 +0000

    task complete company occur entry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
