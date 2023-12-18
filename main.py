3d3f7b4b8e3b3420bca72a550ca64e9ccd33a7e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:33 +0000

    Pp release increase land unkind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
