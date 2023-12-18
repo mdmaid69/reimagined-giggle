6e145001f1a88b78ccfd0ced170d3bd4b2c9f080
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:22 +0000

    pass dark undo zero light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
