8decb6d2c50736d5607b86a673f04bab839df983
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:57:31 +0000

    Dd repair squash mention few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
