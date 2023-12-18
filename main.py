eeb5dd5d3318261e09dbb51b3a45d7c440232ba3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:07 +0000

    interview shout promise bank own

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def exit_program():
+        sys.exit()
