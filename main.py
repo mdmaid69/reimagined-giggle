4dbaa3bdbdba292c86a0edb9c791cf67542661d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:41 +0000

    responsibility insist table control mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def remove_from_array(array, item):
         array.remove(item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
