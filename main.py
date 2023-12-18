7444a2f03f9444c3529d4d4625566655f6d2df9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:07 +0000

    above pronunciation precise nurse bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
