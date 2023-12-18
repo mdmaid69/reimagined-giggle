9f74904f5a9b40fe4589aca6f5b372de33d3f303
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:04 +0000

    ill more clever city persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
