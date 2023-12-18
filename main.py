12f2806e98832181a1204bff7b9ca1691dadcf98
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:22 +0000

    have normal choose middle Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
