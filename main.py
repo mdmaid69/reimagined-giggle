be48f2e9b8e630e3fcb7ca7924f3baf44d00742a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:49:52 +0000

    spring worst regret decision match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
