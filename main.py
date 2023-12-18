ad4f50cdce47dd3c1bf3e4b8baee6abb3514b044
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:43 +0000

    many pain mass date organize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
