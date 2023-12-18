42edd618e9467bd9b17a191a84a3fedba7f23178
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:48:39 +0000

    by Hh tidy deliver transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
