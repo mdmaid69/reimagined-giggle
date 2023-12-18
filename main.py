2b104e5383fc8638eacd642c24fc7aad95ca4d38
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:08 +0000

    reduction wander tax excuse comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def reverse_list(lst):
-        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
