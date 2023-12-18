fd550cb25306150db6a4375fa3f5fd9f463fcc05
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:49 +0000

    urgent minimum shall close certain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_floor(x):
+        return math.floor(x)
