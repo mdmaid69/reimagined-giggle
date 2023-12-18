c12eebcae2613692b4f59db59e342f75a36deea9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:44 +0000

    computer formal careless class Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
