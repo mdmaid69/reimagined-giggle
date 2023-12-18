ed772031ac25c851f812d4dda888d5e96e682684
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:02 +0000

    copy swim some rank far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
