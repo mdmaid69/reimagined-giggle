de6c1642cb393f63b6a921eef657e1e7d3b8a07e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:43 +0000

    who fine accommodation effort vision

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
