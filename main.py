49cdff25713a03baa3d65ad51895865735987ad5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:12 +0000

    train range still interrupt hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
