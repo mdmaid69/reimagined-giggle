7258d3cee57ff5c842468c0a96f2610e603ae424
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:39:34 +0000

    Hh action personal radio wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
