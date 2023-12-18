de90f669bab074e19e9c28b2ad5c8d3aba179a92
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:41 +0000

    object risk excuse hour similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_area_rectangle(l, w):
+        return l * w
