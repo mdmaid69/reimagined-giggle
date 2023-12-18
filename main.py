ae6a5ac73da86cd26a53f80cf9e78361ffc42a68
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:42 +0000

    waste direct suck able authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
