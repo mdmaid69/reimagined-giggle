3591e533ac7216ea631f5db95ce0031f784aa251
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:14 +0000

    company any period somehow at

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
