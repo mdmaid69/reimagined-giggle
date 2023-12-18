38234ddcd8e92939eb230c25a9d00fa87e1fe9b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:12 +0000

    often off zero stone radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
