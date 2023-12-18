030611f84713207ee1e01b7276baf5eaa85137cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:46 +0000

    community compete poor hurt ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
