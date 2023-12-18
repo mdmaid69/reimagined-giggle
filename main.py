42904b4e76f4892fa61ec7a1b513f4f54d190b2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:54 +0000

    project mind hurt war warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
