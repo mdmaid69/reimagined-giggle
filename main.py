c5cdc452611744106d9117f93377ddcac52affbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:30 +0000

    getback actually relax deliberately loose

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
