2ff32cd3b42c313ce12d912a583041456b3a93a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:09:45 +0000

    later extraordinary firm memory production

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
