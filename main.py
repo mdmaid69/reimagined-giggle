b230687f6dbf3ed1f6bfca1801c7165232853423
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:19 +0000

    number original wander hurt lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
