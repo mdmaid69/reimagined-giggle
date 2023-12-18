474fd7863fe4b0e86beeaeedf4704cf689b4bdc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:25 +0000

    wave house regret claim do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
