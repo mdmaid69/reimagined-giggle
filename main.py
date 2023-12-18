d40f280dbd04a4ab707af59648ba16664f429de0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:23 +0000

    though report declaration turnback shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def change_working_directory(path):
+        os.chdir(path)
