c9afc914331d66409db84abdfd2209392faf446e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:33 +0000

    trap office opportunity discover short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
