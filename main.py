52971d89ef8121e8deabef0aad038008053ea7ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:32 +0000

    left sink delicate moral holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
