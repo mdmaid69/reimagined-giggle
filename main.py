41552ce339d43780be6124b96c5cb55c10658428
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:44:52 +0000

    announce tell table tough close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-print(time.time())
+def find_unique_words(sentence):
+        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
