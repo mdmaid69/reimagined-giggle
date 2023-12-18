dd750dc95348433c862e81e9da140b589a9e4136
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:59:10 +0000

    me turnon lesson disappear variety

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
