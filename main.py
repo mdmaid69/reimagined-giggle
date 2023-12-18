36ad7ea29ba8cbcdddc5ef0f141f320d8120bb0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:50 +0000

    soil seem intend impression worry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
