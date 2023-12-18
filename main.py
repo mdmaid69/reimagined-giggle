3506d4d923291c2be305d7bc3a184a184c2bab2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:40:28 +0000

    brother probably damage low man

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
