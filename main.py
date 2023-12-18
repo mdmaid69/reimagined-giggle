aa7015194cb61216efa60c9c66e6ab57771b40d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:07:35 +0000

    priority will provide manage lead

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
