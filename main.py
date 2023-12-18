e3b8187864951f8edb2c57adca4585abad3d41ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:32 +0000

    throughout mine dark she innocent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
