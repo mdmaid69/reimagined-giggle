98fafb859bb345a9b468fb3443d85812d4a14455
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:03:09 +0000

    education sleep Qq angry position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
