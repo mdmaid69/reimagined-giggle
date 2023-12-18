0609051b51f6d97077f58065b596c74b404e2986
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:49 +0000

    disapprove connection yours destroy freeze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_density(mass, volume):
+        return mass / volume
