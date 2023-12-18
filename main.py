f4898f0d7594e302ffbff934eeb49181eb9637c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:49 +0000

    entry record love appeal usually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
