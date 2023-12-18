fa6a772e21248995749c93d35b04d06282344dad
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:18 +0000

    exhibition rise minority acceptable keepup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
