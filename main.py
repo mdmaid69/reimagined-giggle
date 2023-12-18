984440b6f70b1faf8f77ed2f108918358c3b0331
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:28:50 +0000

    once their house brush majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_current_directory():
+        return os.getcwd()
