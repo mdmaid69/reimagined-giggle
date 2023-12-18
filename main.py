b263a35bed29188791c2c64256ceb64e8ee5ab4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:55:24 +0000

    seem opportunity squeeze could field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def count_characters(sentence):
+        return len(sentence)
 import math
 def calculate_degrees_to_radians(degrees):
         return math.radians(degrees)
