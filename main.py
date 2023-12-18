0bb139bf7ca8292c954ad069ccbd5ea8d8b31ec9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:32 +0000

    wrap strength stretch smile familiar

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
