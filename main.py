9fc1b7730188abd5b578b2b31d0c454946e3aedf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:16 +0000

    moral fold time during defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
