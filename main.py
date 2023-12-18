ef033fc22b956f2c4ee7002ebbb9734d05549fd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:28 +0000

    dig mean comeback mouth direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
