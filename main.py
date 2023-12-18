fb95c1d6dfd4787c8adff5f8994c9f622c80bdd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:23 +0000

    patience mix best goaway content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
