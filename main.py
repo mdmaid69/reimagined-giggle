d43c7f0100c44d13251a128795d401f0cd3229ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:55 +0000

    game confuse grand clever hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
