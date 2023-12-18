58927c2f2000abe24acfa83c803acd918ccad6d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:50 +0000

    slightly scrap rent mine exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+print(sys.version)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
