2dd0faa138b15feb62b791173b22cf9e102489ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:28:27 +0000

    emergency chance comfortable change person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
