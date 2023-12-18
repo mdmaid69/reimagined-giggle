592c1da82e94a67538263d4c08c86c361467fbe9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:48 +0000

    stand towards imagination edge cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
