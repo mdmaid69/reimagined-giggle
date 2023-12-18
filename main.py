711b9f0cf020e0d7c4d6d54ba26e0ba370e93bfe
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:49 +0000

    fall opposite fairly agreement earth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def cube_number(x):
+        return x**3
