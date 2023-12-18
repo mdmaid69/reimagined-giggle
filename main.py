293ac1767e61a5b9b949d033082ecfef06c2a883
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:58:45 +0000

    me speak creature even comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
