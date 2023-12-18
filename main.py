8c0b36ed3f0503532c12af340722f08d1bc0cd37
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:15 +0000

    shrink discovery accidental breakout hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def square_number(x):
+        return x**2
