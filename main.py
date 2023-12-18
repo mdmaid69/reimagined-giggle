2926dce7b901e8905c2c1887588d8e1a21c96617
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:09 +0000

    exact trade line this born

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
