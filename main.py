35cd700ffe7c9431b34e44329d259839760d61a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:56 +0000

    argument practise lost break tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
