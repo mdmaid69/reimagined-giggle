07fc32cf840e2a163d785cc90df74f939eee6937
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:50 +0000

    adjective passenger niggaacademy such beginning

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
