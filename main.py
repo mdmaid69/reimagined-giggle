793986ea47e0dc93dd46ecb0c89e3045c7440096
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:05:17 +0000

    immediate be die phone change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
