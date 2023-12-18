5214ecdebb483570d39da607f82d3b09cce90070
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:02 +0000

    division state thinkthrough disturbing gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def subtract_numbers(x, y):
+        return x - y
