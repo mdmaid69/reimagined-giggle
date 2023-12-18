d2f0c510fcb42b553f8652f56864494850d573f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:41 +0000

    picture the divide shelter here

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
