f8c4f5c33620ae24c793d60e8a5c56a85f475d93
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:57:48 +0000

    strength deal strength though dress

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
