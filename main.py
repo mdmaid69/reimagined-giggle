6d531ca5411e6645d3e5a2d52de078af5458f295
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:42:23 +0000

    soon among manner everyone replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
