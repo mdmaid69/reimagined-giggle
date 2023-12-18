fe5df8784e068c67f3bc491738be84cec0a8ed08
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:41 +0000

    or cause region speech bored

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
