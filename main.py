b1c3e0bb9372cb2e020a3efe4cf0c46bcf099ffd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:26 +0000

    fire bang Ff charge press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
