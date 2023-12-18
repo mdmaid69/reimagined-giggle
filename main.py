7255d9b76125818ad7c8b5b9199b969cb610b858
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:54 +0000

    suggestion me see joke Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
