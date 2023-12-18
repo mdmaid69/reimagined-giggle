ae914581d3ee16b9db28fd02060e9f7b7532f8a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:38 +0000

    scene mouth rush computer gradually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
