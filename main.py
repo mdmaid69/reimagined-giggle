1a67a9417c261d4b8ecd03fe48ad1e4aa20edf18
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:08 +0000

    further ourselves wander crash satisfactory

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
