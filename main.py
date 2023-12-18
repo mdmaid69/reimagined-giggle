3462bf1db67024757bca015ef475f68cf841824d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:35 +0000

    crisis argue involve ring towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
