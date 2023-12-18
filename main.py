8477229bcfb36f130d72cb3374a487594391851c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:36:21 +0000

    delicate child remark company destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
