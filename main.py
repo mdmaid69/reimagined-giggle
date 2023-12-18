7a4b3aa9f406cb2cc029cd3043dcc8b3f4a2ab89
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:29 +0000

    unkind rarely comeout feature attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
