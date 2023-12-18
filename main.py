e49557c0238f6597be5720948165462d6f5673c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:47 +0000

    179bite ago born report report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
