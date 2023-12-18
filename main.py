7145a6a2f6556b2bab63dac49937a276aa8503de
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:34 +0000

    on brush back possession petrol

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
