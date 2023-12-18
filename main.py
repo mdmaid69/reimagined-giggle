4f913aa624f8422f360b8cfe78af5cc9cff58369
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:12:59 +0000

    increase difficulty alive regret air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
