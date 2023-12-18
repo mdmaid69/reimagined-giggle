5a4e5f107fa8d4e4377e3d5a694532a2b9fa3046
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:18:48 +0000

    fork prevent unable tense emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
