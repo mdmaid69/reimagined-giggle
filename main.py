17bf7204cc85562559eb1dba70bdabf57065d753
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:27:01 +0000

    drawing defeat major funny experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def convert_to_octal(n):
-        return oct(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
