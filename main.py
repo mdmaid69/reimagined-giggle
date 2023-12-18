03be33d6aef19c6452d904fffb7ae05f1403cbf5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:17:23 +0000

    phone home typical confused square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
