4e96afca18d12a16741a4224851d064b96335d02
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:36:29 +0000

    maybe run risk artificial mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_area_rectangle(l, w):
+        return l * w
