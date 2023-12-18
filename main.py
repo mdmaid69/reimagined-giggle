6d9ff530399e214d242d2c1281b87f3a55e8e6be
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:55 +0000

    zero brain thinkof gas single

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
