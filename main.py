4aa483448575f8bfef4c482093862176c64ec256
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:51 +0000

    strict paper than service party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
