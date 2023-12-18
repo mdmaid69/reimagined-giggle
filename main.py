98abff71624d97667e7ab050aaeaa712430c47cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:49 +0000

    warning catch angry somebody control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
