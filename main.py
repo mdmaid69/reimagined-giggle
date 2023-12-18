051651e9b03dd52e94a40c76a6d706054e362de3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:07:32 +0000

    introduction hollow hurt used cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
