44bfe558f8e09fd66955682e2b309133396eae29
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:20 +0000

    repair bath race task will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
