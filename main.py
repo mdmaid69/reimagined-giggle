dc0b2b4614077ffd02e1acbfdd7e00e92dc2d031
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:02 +0000

    pencil blow clean feature OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
