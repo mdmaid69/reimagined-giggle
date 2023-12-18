abd34247a163ca11875f4eaba692e5bd70dba51c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:20 +0000

    hold forward emotion sign neither

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
