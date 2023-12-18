0afb7f89213b033a5492afe5f39b5fe17a0131ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:31:20 +0000

    transparent bottom division swallow match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def is_odd(n):
+        return n % 2 != 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
