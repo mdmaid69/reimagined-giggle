24c1030eedc090f0336a1f04c902854d17f80a4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:29 +0000

    ilovedicks freedom clothes centimetre blue

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def is_even(n):
-        return n % 2 == 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
