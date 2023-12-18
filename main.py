075e41ec5a9c32df9949756906e0af729d1af61b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:05 +0000

    cruel able through science uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_min(lst):
+        return min(lst)
+def subtract_numbers(x, y):
+        return x - y
