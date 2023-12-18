bdec11fc16976d870f1c60141232b562e681d439
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:46 +0000

    unlikely fortunately likely notice secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
