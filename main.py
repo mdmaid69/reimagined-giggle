0e72bee2c6e257a32456d8da19a55dc09f2ae921
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:37:06 +0000

    cook language callout please sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-print(sum(range(10)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
