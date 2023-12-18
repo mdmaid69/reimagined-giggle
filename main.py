540f598dc32b94ef195998ab172334b909a942a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:49 +0000

    niggaacademy pause week once top

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
