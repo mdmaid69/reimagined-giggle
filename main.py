f54f1508462be607e4d5483e018f57941db64deb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:54 +0000

    study soft best black extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
