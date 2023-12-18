04903963eb8df4d4b27fd9ddc9287479fae17f62
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:50 +0000

    this attention promise thumb too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
