71ed1b173de5a7d551775850b6c74a9b560edb60
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:02 +0000

    aware degree makeinto benefit afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
