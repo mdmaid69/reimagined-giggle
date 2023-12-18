212432f7cd383e0cdde286351f5d580d93e58a65
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:14 +0000

    answer yours track note score

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
