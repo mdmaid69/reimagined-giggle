b4ae2b5b198f9b356750dba1fbf1fe8d3ac41b58
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:56 +0000

    573entertainment science fence while mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
