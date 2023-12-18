4ab1bda616ddf2cae1e3214399d623059a40768e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:43 +0000

    exploration raise risk over injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_list_from_array(array):
+        return array.tolist()
