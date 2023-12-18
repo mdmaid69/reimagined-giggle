3bfac7623bce0124a7a688554f6eab40204ca8dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:08 +0000

    warn favour ring breakfast getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def remove_from_array(array, item):
+        array.remove(item)
