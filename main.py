2c13a746b57d5c40e3e1ac6db32ef131c6bdf8be
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:43 +0000

    waste control strength mixture mean

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
