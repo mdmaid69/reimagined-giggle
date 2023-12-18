c123fcbad5054711f7bbbc2519b071e62eccbe45
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:45 +0000

    shout standard raise 569enjoy consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
