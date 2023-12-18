ebd4efb88c1dc43018151cfd20d22d5a6f000cda
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:57 +0000

    begin shop embarrassing moral Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
