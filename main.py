62114ae5b097ac8fb36dce2cea8536d3a219e58b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:02:35 +0000

    solve dangerous disapprove certainly themselves

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
