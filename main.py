edf27de7db69f061022c9e60ae0014e682fae379
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:00 +0000

    damage truth make science supply

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(lst):
-        return max(lst)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
