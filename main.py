3c46b8ae051af3a3c38f018865f28d5797573eae
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:38 +0000

    wind promise artificial tell inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_area_rectangle(l, w):
+        return l * w
