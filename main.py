6b18fbba23a42ba8d2f1566d6c67bc4a8dcfef08
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:49 +0000

    doubt phone introduce fear bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
