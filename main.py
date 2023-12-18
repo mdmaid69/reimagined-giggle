ba14e5540d2e6c77a5c6776727c5bbd458ab7aba
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:34 +0000

    dance nature nearly attempt clean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
