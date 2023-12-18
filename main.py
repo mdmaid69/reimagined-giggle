106a4926773105a4be9b9199e675bff304723b12
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:54 +0000

    estimate hope sorry concentration set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
