c2176ce649d8d934922e145a2cb7dc4f7b67e739
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:16 +0000

    muscle memory start better world

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_array_item(array, i):
+        return array[i]
