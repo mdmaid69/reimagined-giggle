2f4b9165262951e5a8f463e7466678df7d670bed
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:48 +0000

    introduction fix favour valuable correction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_rectangle(l, w):
+        return l * w
