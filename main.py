7d1858da4124bac32f3890a9793f046f038bb77b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:22 +0000

    post justice excuse care exaggerate

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
