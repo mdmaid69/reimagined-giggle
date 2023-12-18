f84f0aef093dafc3142e34e8f06f15a1de2b9e60
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:32 +0000

    shop open benefit anyone reliable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
