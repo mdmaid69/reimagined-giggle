4f557c959bf431ac2f62f242ce84282cacf600bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:25:20 +0000

    mend length surprising yours keepout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
