4ce86d5fff68baf87d7cb25a37b446fc30b5e19d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:47 +0000

    close compete cold kill before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
