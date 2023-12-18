1b32e8ba0049dff02ccc9d263d79328f26841dfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:40 +0000

    answer terrible away declaration thinkover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
