545f6d6063744895643536258869db1cc01cdc22
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:14 +0000

    makeinto lean shame sort pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
