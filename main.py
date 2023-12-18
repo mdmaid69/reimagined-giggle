46c38a2596260aba9e1feefe5a635c0822014c5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:33:50 +0000

    such believe sticky anyone consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
