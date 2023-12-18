1056cceeb6654923942d401a5b11a4337df9b44c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:56 +0000

    sense let Tt error shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
