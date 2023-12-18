fc8a3ef354520de0223d6ba5ea785f34c589f84b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:04:49 +0000

    television ought welcome certain display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
