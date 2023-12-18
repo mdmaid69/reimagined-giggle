030f5cdfb7ad81e1cf1ca30f0ddb225adb1858d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:50 +0000

    from please clue score job

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
