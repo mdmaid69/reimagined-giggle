7091e157012604c06a3ec29fb32800faf2bb28e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:54 +0000

    another disapprove odd sing family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_user_dict():
+        return collections.UserDict()
