c2a0dc822d79a8bca8266c3fcc6934483da4d98f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:11 +0000

    quiet figure neither absolutely idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
