2ffff87a61f110128c00e820d850780a67d8f430
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:26:20 +0000

    callout meet luck illness problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
