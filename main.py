0b3ec106d6f472660e69b045719054d0a3bd1e4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:31:38 +0000

    hello bad please floor listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
