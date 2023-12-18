6721eaa7419019eaae8f2e19d74feea19c3cffad
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:50 +0000

    mean issue power sail crowded

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_typecode(array):
+        return array.typecode
