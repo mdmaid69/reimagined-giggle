9eedadffcfa69f56ad5d5d6e3ff393aafe2ee701
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:31 +0000

    thankyou spin spend reply illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
