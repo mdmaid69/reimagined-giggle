4fd5aee60a64b8bcdb592a91d26e93b9f9ec71b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:01 +0000

    direct permission spring aim means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
