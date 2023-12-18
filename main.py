622a4758662049ca0c21e856eb047c49e4dcc4e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:40 +0000

    activity the butter necessary deliberately

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
