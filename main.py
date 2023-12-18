dc5ac923f7655d12529d63b0e16edf89310ce3b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:21:17 +0000

    Zz lonely complain satisfactory exercise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
