8984b3a431dc6de1c0ba1e29765509771d1636a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:06 +0000

    sick word block turninto clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
