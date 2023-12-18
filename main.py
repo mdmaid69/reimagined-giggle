c588ef473f3bbc6dd3e009ec8fa20cbc9084904a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:25 +0000

    kill prevent immediate forgive upset

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_acceleration(speed, time):
+        return speed / time
