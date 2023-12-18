0e8efc1429d23fdb9dcd8aa52223fd8427c38848
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:46:52 +0000

    floor afterwards and could trap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
