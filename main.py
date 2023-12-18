a0a43fcaa3c13d51dae23e99a878752e5c3fbaa8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:16 +0000

    about name adjective completely bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def cube_number(x):
-        return x**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
