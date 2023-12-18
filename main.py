757b022b69845c2d3e021c1cab6f3fa04cfdce47
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:11:02 +0000

    influence act discussion discover fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
