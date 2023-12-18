e37d34ece622ba9d32748f01ab187c2bc32ea886
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:07 +0000

    tense importance thumb disappointed vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
