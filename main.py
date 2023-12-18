2eb3bf0981b76517f4f653078c6986a89b94daa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:52 +0000

    violent increase neither line between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
+import time
+def get_current_time():
+        return time.time()
