7e80941433bd1de4a882f66d0b43dc738ea06922
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:23:43 +0000

    comment decision consist apparently round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
