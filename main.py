bfed5ce6de08c9f87ec35da0f5991d5897afad26
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:12 +0000

    movement bet order afraid accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
