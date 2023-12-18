888a8cc5cc982326abe8dc6150fdfd45cd04b7b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:22 +0000

    prove backwards side spite call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_area_circle(r):
+        return 3.14 * r**2
