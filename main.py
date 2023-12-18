b056e7cc0955ee24b2004e673db5e8a86edb9687
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:28 +0000

    high wave yes base calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
