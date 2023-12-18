ee2192792e2197e8eccb97156ca6ac46ce837632
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:11:41 +0000

    turndown cost key promise appeal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import numpy as np
-print(np.array([1, 2, 3]))
+print("Hello, world!")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
