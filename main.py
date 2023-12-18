513cbaa545ad133875ae7816bda98889b3412216
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:25:29 +0000

    creature fear normal report lot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import collections
+def create_stack():
+        return collections.deque()
