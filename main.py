76d7b080b384f17e23694ead63d2c70ecc2bc85c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:35 +0000

    night interview ordinary runaway ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
