bb2d48fa9312c0b2b2ba60997d8705e959b11f05
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:34 +0000

    turnoff want realize transparent finger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def print_python_version():
+        print(sys.version)
