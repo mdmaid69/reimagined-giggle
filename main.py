12f2a93358b7914dbb5e1cce89ee1f0cbe8271ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:08 +0000

    seem nigga Rr empty sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
