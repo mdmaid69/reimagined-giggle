4658898fc944e7a10da8484f06954ef01b257fbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:29 +0000

    accommodation shop goback(to) pencil whenever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
