c2272ef8a6db2cb5eddfed51be301e2dc0eaf380
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:13 +0000

    test hollow world mean treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+def count_characters(sentence):
+        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
