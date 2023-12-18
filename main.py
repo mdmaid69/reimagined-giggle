99bca63becc5bd46cb5ee22ef23c009dc5fdb6cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:08:19 +0000

    high listen benefit watch vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
