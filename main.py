8535150a3d303f3852f78fdccf27f2b6836d434c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:40 +0000

    must wonder live offence spread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_exponential(x):
+        return math.exp(x)
