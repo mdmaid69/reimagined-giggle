cbc377b287e6a33727d475aa69a32adbd4b9db57
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:25 +0000

    reaction turnover stay ear ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
