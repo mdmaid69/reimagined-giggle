72a4412d67475aca0363a592b88ed2425d52a7cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:20:02 +0000

    cut switch plenty grow whenever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
