e3f09660171f57c88545520634126810369b1bd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:32:37 +0000

    radio determined keepoff secret greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
