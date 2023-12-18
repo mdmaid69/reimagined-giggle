ed583e4e710078b030ceb964972fe878b85e299d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:57:33 +0000

    child throughout unfair blue chance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-from collections import Counter
-print(Counter("hello world"))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sys
+def print_python_version():
+        return sys.version
