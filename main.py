8fa96ab43774bb595cb119978e8faefd940b33b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:50:07 +0000

    weigh narrow pupil tragedy head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
