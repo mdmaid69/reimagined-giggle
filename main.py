9423aae22f6a4892562fd32668a87c1f70730568
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:09 +0000

    save disagree once mine square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
