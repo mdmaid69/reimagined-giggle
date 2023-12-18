fd3922954e6219397e7c2646637b5882bc22b8eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:48 +0000

    supply thirsty challenge better interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
