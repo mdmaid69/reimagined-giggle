5bad67a183685cef6a274203f4c1aaa459b0c743
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:39:57 +0000

    treatment same fail radio search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
