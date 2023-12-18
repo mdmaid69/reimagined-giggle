0149aacf3ff5ee76f58b1b322cb292c2d7935761
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:21 +0000

    prefer goon before disturb ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def convert_to_octal(n):
+        return oct(n)
