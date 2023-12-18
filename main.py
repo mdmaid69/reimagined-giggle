a2f175468fa79717f2dd7d26e1116a94d500c432
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:33 +0000

    loss community owe thumb put

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
