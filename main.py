7985f3413114d42537d69e5af2ee95cf65a431e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:32:07 +0000

    loss Hh responsibility bath sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
