c2c90d4ca0d9ab8219af0cf1d9b0fe2b4164d00f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:14 +0000

    contact hide distant sudden touch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
