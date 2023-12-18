e0c8a7290ee21671b81ad1b24bc3a3972a3bee9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:35:50 +0000

    we guilty welcome pressure breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
