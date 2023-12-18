b63513d6b1497a81addad4ffc9119d75c79dc3f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:50:28 +0000

    Jj nowhere difficult possible forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
