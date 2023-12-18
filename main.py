b7dceb232ca788abbab13cfe7c133009636ca20b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:17 +0000

    pencil will wheel emphasize medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
