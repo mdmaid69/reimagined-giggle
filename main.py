5bb115b3ba7ef37f9e7ae024c861b56a8c0d5068
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:59 +0000

    pupil serious but shame runover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
