d7e029459374abd92d7562b9518d04cd3c6ae170
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:35:19 +0000

    hungry hurry on die throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
