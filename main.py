18f7c0b3834ce6c9e49675c5d0afb875265f9fad
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:38 +0000

    snow love history hurt breakfast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
