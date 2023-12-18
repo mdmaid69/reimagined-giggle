2efb1fc747e4121582e6fe611949e0143aadd0f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:44 +0000

    suit or study congratulations office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
