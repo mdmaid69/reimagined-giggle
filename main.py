9920986fdc2e50e6974149eaa25cf57c11421d02
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:25 +0000

    path town game within thing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_max(numbers):
+        return max(numbers)
