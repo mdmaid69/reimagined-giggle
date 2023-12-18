cf7ac751fb676194c489e0e130d62f9139a85174
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:52 +0000

    collection bored fear realize wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
