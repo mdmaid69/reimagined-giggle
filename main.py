6a2dc759068f89a01d2b7466358ada530ca853ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:53:12 +0000

    smart include breakout project shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
