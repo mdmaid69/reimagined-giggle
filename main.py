ce8001011de00c536249afa661a97209950ab007
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:19 +0000

    fact introduction market hit nose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
