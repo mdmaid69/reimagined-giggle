3cfa0e7a860e6dcdc9a2f01f113ffb62d048883f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:40 +0000

    extraordinary frighten bringforward eat grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
