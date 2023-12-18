2c9119e863afb24550cc1699a1861c85a31b95fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:43 +0000

    breakup stupid comfort left active

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
