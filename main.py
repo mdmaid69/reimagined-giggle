e501c8912e409ca8596786eb0ce1b6369c7082e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:00 +0000

    ago cough cause callup centimetre

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
