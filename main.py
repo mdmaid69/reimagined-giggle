7bace9db9d99b12e33c0a5c620b6f894a32a8582
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:22 +0000

    experiment knock design increase rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
