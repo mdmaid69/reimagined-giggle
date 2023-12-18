dc41abc203d35f7b8efd52d7c91d8858f7cbaf53
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:59 +0000

    after favourite repeat unkind stamp

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
