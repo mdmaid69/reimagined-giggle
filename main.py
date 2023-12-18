6cbe6f7c2fea81474e7aa52204199cdd26524300
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:42 +0000

    burst cross run card attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
