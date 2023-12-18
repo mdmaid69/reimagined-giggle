19ed5ce1a4d87e7b853a27ddd9231e1649a9e577
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:11:36 +0000

    grow bright quit special belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def convert_to_octal(n):
-        return oct(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
