a9449594288935ead227b46d9a4088d18113b6e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:28 +0000

    concentrate state mail show rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
