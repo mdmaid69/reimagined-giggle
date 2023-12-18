daff821197b4fa72a18c8d4967013885b05bd625
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:33:27 +0000

    anywayanyhow disturb closed world anywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
