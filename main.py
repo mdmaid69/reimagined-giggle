6471c4def8fd895f928b9b37aedacac667041596
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:31 +0000

    bother mixture turnup main sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
