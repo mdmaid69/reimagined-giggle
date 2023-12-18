8b4d79bd248a4e9177c99085e92d3314547c442b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:06 +0000

    watch snow matter fit advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-print(os.getcwd())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
