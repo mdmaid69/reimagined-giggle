66786e88e44b12975b84413c87471c4354521bda
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:44 +0000

    unfair equal left her joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
