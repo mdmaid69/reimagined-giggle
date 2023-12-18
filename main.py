7c02e85bfb9794ea9859572a8bdc33571b2bf8d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:57 +0000

    purpose in business smile accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
