61928854df70bfed31ecbedc2e6a3daa57df68c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:59 +0000

    recovery lie process dead he

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
