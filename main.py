c6fb59c11e6f1558e825d72af6d4cc89ce0158f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:32 +0000

    powerful anything treatment shine refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
