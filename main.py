c5eef8a13705441f0375fa47865b037252528c77
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:03 +0000

    course mood chance get move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
