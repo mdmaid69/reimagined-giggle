0dacc3f88d926b87b3fe2f36025cfbbd4d566e5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:06 +0000

    attract snow with continue dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
