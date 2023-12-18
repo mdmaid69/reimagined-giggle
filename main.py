40411984b5ac521aab4447435ac6a4d7a8c3a13a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:02 +0000

    broad strange bang sink Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_item(array, i):
+        return array[i]
