ddfa4d389f0deac749cf0857413d6440afd26966
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:59 +0000

    on catch drive tidy dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
