ff718ce2b216ce6fbf4e09285fccb2cc4f973862
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:16:58 +0000

    pride around activity furniture estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
