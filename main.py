469f960ebca7f11c45af06bd2d83d9319932cb2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:12 +0000

    please then march flight comeover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
