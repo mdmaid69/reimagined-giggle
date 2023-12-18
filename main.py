c394fcd257f7e4dcb22b7ebbb2c43a3cefd82d53
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:43 +0000

    standard complete Nn cheek shadow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
