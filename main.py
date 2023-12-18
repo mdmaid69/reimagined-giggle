8162ed205669da293702c0d1ac92722c363bd00d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:48:23 +0000

    scene confused refer union task

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
