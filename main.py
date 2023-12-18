6fe90101c19a99b3240538a0eedb54f0eb82b0ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:44 +0000

    tight worry dress be day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_dict():
+        return collections.UserDict()
