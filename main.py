0ff395223c6eb215488722c69e93ea99c1359e09
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:59:17 +0000

    string not trap compare shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
