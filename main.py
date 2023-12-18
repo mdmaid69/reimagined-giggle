4417398259921163df1faed9e38018804654aa41
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:45 +0000

    height beginning year sail leave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
