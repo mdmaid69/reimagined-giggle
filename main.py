d5986e72f0cd81e8018f23ad85cc8a64b8abaaaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:50 +0000

    reasonable point connected event wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
