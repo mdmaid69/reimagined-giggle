0c65c823c63ed5c03919e5c66cb78a1b8c826b6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:14 +0000

    side float judge your house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def convert_to_binary(n):
+        return bin(n)
+import platform
+def get_os_info():
+        return platform.uname()
