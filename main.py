2c176f2d06679369a58f8f2caa06985e39a8a9a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:40:01 +0000

    cheque contents react post score

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
