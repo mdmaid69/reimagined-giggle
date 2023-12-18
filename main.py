d2b7996e524f968a9c6b5c24123c6b5497e9c629
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:11:32 +0000

    rain soon strength disapprove 571enough

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
