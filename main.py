0ff8a9e3385999b703de3e7c9e9cf5293562cc94
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:04:42 +0000

    meanwhile priority result near form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
