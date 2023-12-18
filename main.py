d9268cf832f1a2af3ba2df585e2aef5d29cb5a4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:15:43 +0000

    thorough bear wake order of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
