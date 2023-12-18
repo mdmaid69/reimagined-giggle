9f36d597aa06ee3efc54ed7403808bdc64da90c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:18:31 +0000

    temper takeback exhibition mention cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
