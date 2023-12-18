58b59babd0a6a556ac1a71e7f6a7e4bfe1b73030
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:47 +0000

    runover sort content strength our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
