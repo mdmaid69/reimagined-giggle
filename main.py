847d342ca9cdf1dd2e199f0cf65041043a8c17f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:58 +0000

    ours supply discussion hurry mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
