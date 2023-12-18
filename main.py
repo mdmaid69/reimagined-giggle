025f773b8ce28d36560e94813fba4afaf416bfdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:41 +0000

    failure study spray their roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
