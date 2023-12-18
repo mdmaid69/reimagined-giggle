146ef1c0e40db1cd688c4f8ed42c72f3e4f022e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:59 +0000

    moral practical fasten present above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
