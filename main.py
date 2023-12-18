aa783b3c81a722e55296931dd768b138e1a94cfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:51 +0000

    use kill really direct profit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+print([x**2 for x in range(10)])
