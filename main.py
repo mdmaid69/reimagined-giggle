0d550fa2d98c5ade32fa60a1a21a9617da89ad91
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:55 +0000

    pressure success frequent frame respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
