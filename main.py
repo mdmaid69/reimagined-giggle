c2524127ab378fa52fc53e269bec6bda0170ad6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:49 +0000

    turninto table holdout fly late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
