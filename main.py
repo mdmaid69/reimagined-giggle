3dce3e111487c15370d85f3f96b0c04164a1f28b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:04 +0000

    564encourage weigh keepup television blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
