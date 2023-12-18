33c8a7e1f39412e28ace48d32b3e0dd4ef3cd472
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:54:53 +0000

    square description drawing explanation spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
