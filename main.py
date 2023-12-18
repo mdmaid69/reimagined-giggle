3dc2e955114cadb0df43e91b863e83bd1be28d17
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:54:40 +0000

    rank life tidy check compare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_str(array):
+        return str(array)
