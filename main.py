5847083698fcec2e2e4be68bc70bf41d3c98b496
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:00 +0000

    by flood trip free us

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
