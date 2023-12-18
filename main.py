0a3348763ae805cc252745e2f0dab95156a87ba6
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:18:47 +0000

    control move butter first funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
