46f2d066a0892be78e6ecdcee5ec77fe2ef252a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:39 +0000

    appear instruction sensitive his birth

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
