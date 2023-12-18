d52eb7931e32e0bd73b346ea7ef1ef68562dce9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:04 +0000

    centimetre meanwhile cheek phone later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def get_array_as_bool(array):
+        return bool(array)
