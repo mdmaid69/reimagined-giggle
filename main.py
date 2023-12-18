890772443eca80162e7fe1b316366962c713ac1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:16:35 +0000

    snow glass insist in hate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
