105ccf186813c4c450dce29fac750d00c38e3bc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:31 +0000

    worth test same example yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
