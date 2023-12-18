a4b1bd58cffb910cc5587d97c15c21b7d172869b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:06:08 +0000

    software lie defend debt mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
