d8e9563a81d786a32b1911e276d92b0d98ac472b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:47:22 +0000

    spare male bringback basically cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
