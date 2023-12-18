905cc0b55b4ec2bbb5dd38ebc1de12c1e691cbfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:54 +0000

    operation Zz official ourselves move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+print(os.getcwd())
