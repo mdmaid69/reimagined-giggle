22ed4b0962d903da6b879238e9f6d02d7806c290
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:35 +0000

    like constantly bang claim emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
