af95f04aa4c856b40f1ab58b79c5c0aa3ed77c35
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:28 +0000

    shade stamp prove guide closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
