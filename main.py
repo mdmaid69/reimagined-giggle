05b4a1b3b482c80796631c85e9d4f2ee10422ad6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:15 +0000

    break precise let worst popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Characters:", len(text))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
