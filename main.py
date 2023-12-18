6aa4ed2974565ddb91cd957aaadb6b821ef12373
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:04 +0000

    keepfrom breakin glad offence journey

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
