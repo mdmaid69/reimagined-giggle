35e56e80209f31df498121f8bd661913aa8ef816
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:59 +0000

    spend big activity high machine

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
