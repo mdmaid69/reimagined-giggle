6710ecc02496e18b39cd2bc84b38cbe465bd4299
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:00 +0000

    scrape knock sell refuse sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
