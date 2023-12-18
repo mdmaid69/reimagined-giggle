4b47c35dcf30789bfcd9580bdd9658a0605a68bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:34:13 +0000

    read deliberately confusing uniform system

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
