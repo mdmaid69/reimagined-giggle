9606aca2644909269a46a9b9f5e43f1f85050db9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:42 +0000

    noisy whisper past temperature leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
