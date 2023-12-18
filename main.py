0b03a9581e2feea8ecfdaa3075b5aa45cf4c562b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:15 +0000

    guess quarter Tt exit notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def cube_number(x):
+        return x**3
