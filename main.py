f5a53b43d42222424c4bb1d25a3142b4e49f2a11
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:04 +0000

    earn stupid break destroy single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
