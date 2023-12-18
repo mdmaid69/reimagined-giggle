e6d37a3bf0d0b440dc755ac63f4d75e8d4d0b40e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:33 +0000

    also emphasis he hurry combine

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
