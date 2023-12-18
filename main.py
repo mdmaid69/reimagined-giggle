9d919335ade60a3d99be6a76d995d3ce7eb0d4b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:40:24 +0000

    tax reserve yours female story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
