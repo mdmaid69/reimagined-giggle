9c083f05b9f1ec083a5f4f373b1e1c4435dc1157
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:08:57 +0000

    care often education themselves before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
