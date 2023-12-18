b4efe5ce0968940026bd8dddfde8df125a83499f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:06:33 +0000

    shame else bang maintain warn

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def reverse_string(s):
+        return s[::-1]
