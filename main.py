ec00d09c04ef18852e985c188753de1f23ee7f91
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:07:17 +0000

    responsibility sentence large bet sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
