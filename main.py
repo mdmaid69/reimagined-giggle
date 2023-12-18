a48a781c05d4cc4c520550bf018ee3be334f0285
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:49 +0000

    giveup key push plate back

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
