b8a2d7babe3456ed1400c69c2b6410c4028a678e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:26 +0000

    fail clear unit would produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
