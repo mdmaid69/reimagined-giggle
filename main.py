98b8a61bcca1e0d8dd9db28b9435143185b499f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:58 +0000

    slowly report tune community work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
