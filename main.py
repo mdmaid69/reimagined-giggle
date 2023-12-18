5d5c375c01a8ba4e89896e4ecdd08f4a6d33bfe4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:49:28 +0000

    yourself than uncomfortable classroom protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
