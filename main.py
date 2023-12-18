cf929d30ad943000c6bfe59982f0887cadfe1813
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:45 +0000

    fast turnon shop concerning represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
