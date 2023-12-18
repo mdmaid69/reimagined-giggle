b6294dc4044457677c62c284bead25e0bc3aa493
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:39 +0000

    pass so bad lead partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
