1f20cb1667105fa753eeaa31c63df9ee38683a43
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:54 +0000

    display clean conversation recovery correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
