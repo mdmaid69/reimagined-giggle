d3439539b73b582680f5154d60e4eb80ca04d66e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:15 +0000

    instruction also wish Rr ignore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def greet(name):
-        print(f"Hello, {name}!")
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
