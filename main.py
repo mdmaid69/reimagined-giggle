a3020bfd7296d89aa485e9953c6d0e2436579d96
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:03:56 +0000

    off education crash programme wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
