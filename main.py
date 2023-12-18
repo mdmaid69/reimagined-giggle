4773f002b91ff11939d44337e58a010a798d5eae
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:34:35 +0000

    conduct between defeat healthy tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
