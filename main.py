84a9df3745e5870ca393c108c4ca1b84cabd0903
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:24 +0000

    petrol sweet law avoid burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
