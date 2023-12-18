1f4ed68108433af9c1c674a981126b00a40f46a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:10 +0000

    chin cause near variety blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
