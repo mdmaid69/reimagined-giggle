3de5c7301261279a4d6b1b4dd94ae6f5c14c92fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:46 +0000

    somewhere mysterious star flat dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
