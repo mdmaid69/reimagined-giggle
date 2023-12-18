978717ada35ea19d6eb0de740dac0267de7d5331
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:26 +0000

    drive often scare extraordinary turninto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
