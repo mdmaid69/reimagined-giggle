693de1b6251d666132c75a2b4e00ae0e303f4f21
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:19 +0000

    spring exercise practice please comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def greet(name):
-        print(f"Hello, {name}!")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
