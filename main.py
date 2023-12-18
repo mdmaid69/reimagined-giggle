ef633ade2f0b581105af846267fc77966d1a232f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:54 +0000

    hit platform workon kick perhaps

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
