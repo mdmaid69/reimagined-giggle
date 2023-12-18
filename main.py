96966fb6c02be6b2742a5ef21ca4dde7ad9232bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:46:28 +0000

    sticky excuse shut fat argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
