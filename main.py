ba0df757b29ba14c60e79c7880899acc4c49f92a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:14 +0000

    behaviour plenty goup it wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
