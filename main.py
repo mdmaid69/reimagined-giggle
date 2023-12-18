da8aa6e90ed175d596a14206c62b97a9cf3b4aac
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:20 +0000

    gate steam shock dream steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
