103a3fdf93b5d1934caa7de7eba690b4de345e53
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:54 +0000

    go detail wet beneath create

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+print(os.getcwd())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
