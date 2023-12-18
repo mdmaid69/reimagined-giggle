65ab3cfe4f7ba11948402d9dc200bf7e617a4a7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:20:00 +0000

    under Yy trouble dance wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import tensorflow as tf
+print(tf.__version__)
+import os
+def remove_directory(path):
+        os.rmdir(path)
