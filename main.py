719b955368d20c5ad4365786808a8a0da61d400b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:27 +0000

    level deal press string duty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
