6cac6a354c3bc708313579e3e24c958c01136dc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:40 +0000

    detail health name none professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
