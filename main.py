a31f4239381c5b6bfd4c99e31e28d3acac7f98dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:06:32 +0000

    lost alone swim top succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_permutations(n, k):
+        return math.perm(n, k)
