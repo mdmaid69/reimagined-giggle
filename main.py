c53429cc5aa047c8201a5c59d93116cb78f9466f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:33 +0000

    flat next thinkof comeoff 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
