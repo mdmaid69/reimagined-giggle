145167b3c7d23d6abe6f29a7f32b2efd66ff0ce6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:44 +0000

    knowledge instrument finger mention almost

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
