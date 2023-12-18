af2504729f562bc47abb1f68d0b2585e1246ea35
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:00 +0000

    fall habit some advise age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
