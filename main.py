3f8bef069e10b8e215fe0c4fe95bd127ed4690b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:07 +0000

    abroad excited old pen accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
