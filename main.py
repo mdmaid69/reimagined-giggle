fadbbbb5b5845be6cf04363d0184103ef19c7a60
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:37 +0000

    agreement typical his active stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
