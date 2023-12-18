41de1724a9a131daf5444b495ff02b39fa8bfc15
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:13:46 +0000

    should about clue area surface

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def subtract_numbers(x, y):
+        return x - y
