e022a38ac8bbe054e7691282419e353fc77adacf
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:53 +0000

    slice drop hold hand face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
