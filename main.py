2d30c321f4a08e593d2d024af16b6c14ba2a7669
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:23 +0000

    hair laugh main drop Qq

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
