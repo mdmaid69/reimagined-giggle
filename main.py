f90314082b55107fbac1189364954099709616e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:27:58 +0000

    work born cook black beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
