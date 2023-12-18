aea4a9fb160550eac1249e1f521791c226db049a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:47 +0000

    equal the visit rule feeling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
