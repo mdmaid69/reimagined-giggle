d3191bca9b7095d9106fc11fbc5ba777b1a418e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:40 +0000

    real score willing corner nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
