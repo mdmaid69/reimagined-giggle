015fa3731de5f5a7c5e2b4832778f37c56a2fdea
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:43 +0000

    flavour simple backwards tip computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
