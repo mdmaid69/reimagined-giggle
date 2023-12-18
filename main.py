740ca0966492c024a72d7aa87a91d806318393aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:40 +0000

    become could side equal Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
