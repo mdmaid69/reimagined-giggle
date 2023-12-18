fe06c050d7e77bb4718c0cedb8dbaef8a549c406
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:03 +0000

    congratulations comeup explain somehow measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
