2f0174ebdd9e2f549b2510d59938461e205953e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:14 +0000

    carry 564encourage anyone gradually along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
