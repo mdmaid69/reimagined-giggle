4af4cae9baef73d612e0cab61135e1f3e4a28765
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:12 +0000

    regret post least dig issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
