af3bb1fcfefc02b79509053014ddd230e0e9c613
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:16 +0000

    if tune rather downstairs persuade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
