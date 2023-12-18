bf2802b9080c08a2d2f6564a30b29bb4fae9390c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:24 +0000

    once name war himself draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
