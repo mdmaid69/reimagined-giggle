87ad64b5e3601f91f104a68822a19787f3f41037
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:26:16 +0000

    breath healthy thought mean difference

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
