c870aaf415e9d7b36f3e6c65592ae828b51cde76
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:19:43 +0000

    relationship demonstrate whistle free basic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
