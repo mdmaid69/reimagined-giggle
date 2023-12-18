5e7b72621a316e7b416603df9a6583e2653db339
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:46 +0000

    warm previous Qq strong spin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
