9758cc00030589e1ab43e2977fe81f8d5cf1720f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:41 +0000

    forward among difficult she cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
