d34fd98c9afdef6d2bc7214b74af88eb05d451e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:06:21 +0000

    ignore intelligent prepare perform suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
