d4c8e3ede775e37a4832f439084296addb0c6c34
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:54 +0000

    loose advantage fight thumb common

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_return_on_equity(net_income, total_equity):
         return net_income / total_equity
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
