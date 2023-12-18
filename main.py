7027856690f1c1f946007a92360e5e9d42752d83
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:00 +0000

    look not equal cough letter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_characters(sentence):
-        return len(sentence)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
