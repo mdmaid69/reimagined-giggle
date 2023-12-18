92b09463759253b4ee04545db7b9ae9b8cf75fce
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:07:18 +0000

    book official under begin flavour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def count_elements(lst):
+        return len(lst)
