b7618502a17b50c9cd70c3cad03019f292bf029c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:54:40 +0000

    petrol crazy kill equal freedom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
