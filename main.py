0f3d8cb8c659e38af39b085ed984c921368290c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:45 +0000

    566end future try dull appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
