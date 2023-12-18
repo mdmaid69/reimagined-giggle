fc8d85aef3ac8f4521a71a8986597dcea2512adc
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:28 +0000

    ring back appearance grateful mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
