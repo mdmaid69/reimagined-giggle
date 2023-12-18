e6cf064336ed328348816fe54c013b3d773e8bc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:52 +0000

    frighten correct but bottom suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
