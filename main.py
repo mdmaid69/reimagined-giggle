17aa70e2f1c8085180fd8fa8abb176f051a42097
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:41:47 +0000

    nervous suffer nerve remember disappear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import tensorflow as tf
-print(tf.__version__)
+def remove_from_array(array, item):
+        array.remove(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
