1c82167cfd7c189c1f2557bf3d501165436135d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:03 +0000

    band mind really print organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
