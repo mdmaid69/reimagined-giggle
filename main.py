79f0ef11d3446856f73ebb314d8c3d257151cc7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:13:54 +0000

    involve lay tough disagree last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
