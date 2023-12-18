d0226c7a2deb9addf8caa3ad35029e82e2be2796
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:09 +0000

    usually approval strike treat desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
