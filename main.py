8f29341b96fccff3ff58593c6af92dd25446fa17
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:48 +0000

    unless anywayanyhow almost confused switch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def is_odd(n):
+        return n % 2 != 0
