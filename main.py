be88ae4c5a045207566af8758ea1716d16f9e5a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:11 +0000

    flow except necessary wild throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
