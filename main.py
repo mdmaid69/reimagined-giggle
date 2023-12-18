a45b769940cb22560071c66779de5ea372e748c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:53 +0000

    meanwhile same brain welcome approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
