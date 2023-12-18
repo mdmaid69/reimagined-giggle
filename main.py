cb11d76c9e1a46984f07c8c1483c630797cc2664
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:38 +0000

    soon appearance bottom wave taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_string(array):
+        return array.tostring()
