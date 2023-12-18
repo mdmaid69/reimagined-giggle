5d618ec6261f6f90eb1e52b9da24063eb81101a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:00:06 +0000

    moral increase tune to careless

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
