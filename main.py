15edea7f945aa0449b1d7a1a44ba4a73fb3cddb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:18:26 +0000

    workout lift judge family principle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
