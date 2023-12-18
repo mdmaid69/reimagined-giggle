b3138fd76b9bea8d8d061b0b5afef61fdc77b865
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:04 +0000

    principle ring press alone confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
