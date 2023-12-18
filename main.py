493e8828deb5b8593a63e2e23c4fb8126843d05b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:35 +0000

    hang upstairs single mend talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
