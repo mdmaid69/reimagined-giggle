85f22d1e915f5f02f86ed305b227cc4e66ec50de
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:59 +0000

    under refuse mix drink example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_octal(n):
+        return oct(n)
