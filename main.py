ff59e49375a020ee14174df3748a80e144c321db
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:55:49 +0000

    tidy healthy punishment name during

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
