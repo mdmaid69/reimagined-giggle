7e3df8f6ecc46ffc29033a4dba98abd40b494516
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:28 +0000

    question corner tall bet near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
