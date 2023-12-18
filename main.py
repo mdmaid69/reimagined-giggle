17cf5a60fdb8c72c5ddc129108f5d57092df4f28
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:06 +0000

    dress Vv suckme set thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
