c0fad1de12914f6ee0169886802879dd0455a739
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:27 +0000

    giveaway large ring bottom rare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
