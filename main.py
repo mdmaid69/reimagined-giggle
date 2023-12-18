e1de6bb7c752092b5d4346ecaf7003251f1c0ac5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:21 +0000

    opposite inform evidence across mistake

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
