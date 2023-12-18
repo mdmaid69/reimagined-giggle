3af932ae27a04382c6a7c6d37eda8725c1ed4650
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:59 +0000

    firm importance rob summer recover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
