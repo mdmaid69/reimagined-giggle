129f7c42596b86388ab8df89104934656bb61ae8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:44 +0000

    enthusiastic base before expression oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
