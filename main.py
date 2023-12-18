d49b3b2718a30d77d67cca15fc1601740be414f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:18:32 +0000

    unite Qq dinner support react

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
