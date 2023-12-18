cc8671fc1b16c941974418c4d50d086f3752e93c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:52 +0000

    picture together jump laugh partner

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-print(random.randint(0, 100))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
