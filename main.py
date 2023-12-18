1522e71e6dc5b4f7b988c34b0d04c93ac591140e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:48 +0000

    everywhere least scheme rarely completely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
