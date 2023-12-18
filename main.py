70eaba1f4242e04e9b75427a2f1f02a046c9a383
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:45:47 +0000

    uncertain opponent hardly spring frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
