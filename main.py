4f60b94e137d425eed5ce297ba1d6fe4f3119ae0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:48 +0000

    emphasize beside shoulder keepfrom nor

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
