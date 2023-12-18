9b958b541d51d8450b28ba6f9b4b015dd14e383e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:46 +0000

    shine brown often brother cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
