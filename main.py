732de6584e10d69df0d12a393ab393355e84f83e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:13 +0000

    holdon repeat herself light smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
