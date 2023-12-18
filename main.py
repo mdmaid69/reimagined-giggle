56a0f5514b214818344bec3378de76b18e8dbb9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:09:25 +0000

    heavy rare beautiful test drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
