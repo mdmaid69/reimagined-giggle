b1ee164629f5c918227be13a4ebed32fc500d936
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:01:53 +0000

    Jj temperature people along anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
