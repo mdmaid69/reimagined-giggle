f7e5ad6c5f7899684aa558225c9bb2627d13c7c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:17 +0000

    bag breath people between meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
