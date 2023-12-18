61a8ff1309227b3937d99b5c2eb986279f3ff7c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:57 +0000

    patience wait such another treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
