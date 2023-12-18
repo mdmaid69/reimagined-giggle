2f5be2d515e7cd7c32671407c4cd49de44944ae2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:33 +0000

    bin depend centimetre exaggerate nearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+for i in range(5):
+        print(i)
+def count_characters(sentence):
+        return len(sentence)
