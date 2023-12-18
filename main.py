5b3c6aa8bfc3a6629a7d7cc074bfb3078e14073e
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:40 +0000

    touch stage pleasant possible possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
