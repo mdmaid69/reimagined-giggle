42719381439ba2d2a71d6aa0413bc90e48ac7218
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:05 +0000

    still write hot fall cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+name = "Python"
+print("Hello,", name)
