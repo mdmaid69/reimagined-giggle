c5c6ff5ebec4f1a70632e5a6f9f1629013bc9ef7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:54:51 +0000

    consider communication quantity guard breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
