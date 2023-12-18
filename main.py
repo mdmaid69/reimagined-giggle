bb70dc60bc330768e11eb81255ee2c76d6096580
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:36:09 +0000

    bored wild left show runinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
