51f5d0653bede1ad9e6ffa2dcc8e3fc0112e4fd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:38 +0000

    understand custom nigga commit dangerous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_str(array):
+        return str(array)
