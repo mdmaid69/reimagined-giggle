97a1aac66e4619c9e3bb8a2a7f4a72b1c048bca3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:49 +0000

    float inquire ticket do second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 def calculate_power(work, time):
         return work / time
+import collections
+def create_user_list():
+        return collections.UserList()
