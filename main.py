570e2eba1297febbbff89fad3b2a06987fa24936
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:01 +0000

    actually manner clean copy though

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
