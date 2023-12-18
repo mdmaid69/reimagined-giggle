aebe58fff0004655247241f4202b1bd876a76536
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:21 +0000

    visit concerning every contact release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
