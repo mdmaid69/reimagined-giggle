000cc51f6eb40b2a0c5ddebe05b2804b05a83347
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:55 +0000

    emergency medium culture make comeround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_power(work, time):
+        return work / time
+def find_max(lst):
+        return max(lst)
