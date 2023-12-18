d6b6db34860d08f2969e60a6a6c7f8e0f6790892
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:36 +0000

    interview behind murder give prefer

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sys
+def exit_program():
+        sys.exit()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
