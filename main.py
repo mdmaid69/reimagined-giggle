c33ac51c6236d9a77d4d3eb04ce722930caa85b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:44 +0000

    represent neck just provide bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
