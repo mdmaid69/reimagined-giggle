ebaa2c52717c1009ba059f0ad2de07058ce2dac9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:32:29 +0000

    violent familiar hurry purpose dare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+for i in range(10): print(i)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
