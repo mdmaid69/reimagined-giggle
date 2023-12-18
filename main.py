bf08fc886d40d675dce314f0fe946da5c53899f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:41 +0000

    thank active average contents world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
