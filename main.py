9748f65a2561c8924b125509a47f1419007fda97
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:17 +0000

    settle bear would understand look

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
