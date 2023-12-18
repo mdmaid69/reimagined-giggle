7aabff6a13a68561560985479f7c76f0c25607b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:06 +0000

    only spin face feature pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
