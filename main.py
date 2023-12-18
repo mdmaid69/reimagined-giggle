6004db6a0f3c7c0cf4694754b96363782fd13a94
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:43 +0000

    genuine degree much record probably

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
