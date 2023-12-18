45af2366ca7c60d8b35401bb96a97d3501d88a6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:29 +0000

    marry past kind bye approval

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import numpy as np
+print(np.array([1, 2, 3]))
