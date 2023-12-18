0c2a7f32eb38da054ef7a4d15072626c91557ce6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:21 +0000

    age win she off burst

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area(radius):
+        return 3.14 * radius * radius
