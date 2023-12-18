982fcd0f838ad4e64f8e8b2d7ebda7f915eaae45
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:37:39 +0000

    workon general clean basic prepare

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
