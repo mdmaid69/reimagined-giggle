95e82a765a7984ecadc5d41a60e15780e412da8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:42 +0000

    suggestion violent chin round sea

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def cube_number(x):
-        return x**3
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
