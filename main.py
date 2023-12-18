c324db9c923d7f67a019d48a6c4afb09dfe04dbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:17 +0000

    roof rate represent them trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
