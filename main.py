50c1ca88e5328e334416045e46571e3842694b55
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:47 +0000

    behind joke completely regional total

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
