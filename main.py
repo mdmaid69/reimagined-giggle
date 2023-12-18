21390dce5e245d28e15b853d967db4fe7dde4f0e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:54:34 +0000

    morning inside debt treat extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
