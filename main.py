9792c5dfc20115e6146e47b0dd5edefb93d17305
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:10 +0000

    under worst measure opinion successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
