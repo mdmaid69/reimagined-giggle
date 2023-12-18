a4333b2905441d98da9ced89d6c44dee8830bbd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:53 +0000

    pinch ticket remark unhappy never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
