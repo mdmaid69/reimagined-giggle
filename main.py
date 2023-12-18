65f27d0001210ec4b8cc5047ceb913009f8f2728
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:23 +0000

    word challenge mistake table raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
