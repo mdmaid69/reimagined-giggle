b9245adff20af4a5de7ebe5823728e571e29ab57
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:15:12 +0000

    protest arrange sport live 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
