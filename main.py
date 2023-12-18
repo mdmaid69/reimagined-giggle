9bef0d4f4231cb845c164955ba79a0f325ad8e38
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:38 +0000

    different where scale cum spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
