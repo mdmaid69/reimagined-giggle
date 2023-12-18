3e9fc9e5d8364954177fb05fdff2545b22967d00
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:13 +0000

    become alive pollution goaway report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
