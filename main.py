4e6a3ca934687c1e55a795863e80e07d0d6e7e9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:33:26 +0000

    excuse own jump force occasional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
