b157de27973ae7a8bc27316efe37d2c0519a1597
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:39 +0000

    whoever communication agree suit pause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
