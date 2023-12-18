48149f495b66198dc0633e70126f83a96e56631b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:44 +0000

    ilovedicks crime suit point view

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
