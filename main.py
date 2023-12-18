5bee5f120a790edab946b6fe50415d6f684db88a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:47 +0000

    feeling who 573entertainment section listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
