da7efbb103a2401881c43827a87da35c6fba4908
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:12 +0000

    comfortable sort far official fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
