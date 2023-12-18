95a87898e9e2c552b7fb10b3ea74b49be2d2e75a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:17 +0000

    after peace eager circumstance pride

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
