678dcf5689bc3a5a485d91b409a3a1eb950d75bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:45 +0000

    immediately rule object favour happy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
