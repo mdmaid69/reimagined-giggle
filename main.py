9cbb82c3f92609460ff57fd88ba167588bd02c62
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:55 +0000

    wide curious interest build phone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-print(math.pi)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
