a029913e619860aa23fd43f12273cada171b2386
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:23 +0000

    exercise black purpose ordinary deal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
