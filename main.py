469123ec1d4b1ebab660461057efe8c0a97cca1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:41 +0000

    of fold bad open character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_set(array):
+        return set(array)
