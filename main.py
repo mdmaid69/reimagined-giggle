340cecd17414cf8e02f891b42f0b3b1a7ca95488
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:27 +0000

    avoid for remove public gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
