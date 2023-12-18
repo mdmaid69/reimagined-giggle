e0bc5aa25082cf0f559d98cce883fea1cc6f4029
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:10 +0000

    point later between support wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
