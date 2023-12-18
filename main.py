cc2513dbad9bfedc8816ec8e27033803071f9604
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:31 +0000

    rough breath correction turnon break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import time
+print(time.time())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
