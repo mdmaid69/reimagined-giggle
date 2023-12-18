3e94d5f57f8c446a1c1701976593ffe3962eccac
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:04:34 +0000

    admire touch live freedom father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
