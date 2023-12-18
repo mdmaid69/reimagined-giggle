307f111069f437945f18a3facd484a436f3def17
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:09 +0000

    mix successful closed part book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_queue():
+        return collections.deque()
