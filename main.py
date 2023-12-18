fc956fba69f95ad193917e3a79876a131ce2f2db
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:16 +0000

    when ride unable tough remember

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-print("Hello, world!")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def subtract_numbers(x, y):
+        return x - y
