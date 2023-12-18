cedafd1049cb24f6be2913c28b39a20d93a3a1cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:04 +0000

    care connection country suit independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
