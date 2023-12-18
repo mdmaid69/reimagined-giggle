534c566f6f6446ceded7de0e19090979e62ff939
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:31 +0000

    butter squash giveaway after hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
