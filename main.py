930a6019ed681be6ee04bb9af38ea6a605db8732
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:08 +0000

    or uncomfortable clue friend classroom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_pressure(force, area):
+        return force / area
