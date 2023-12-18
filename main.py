5a2f1c3ee8ef2122307722c3f858189bda9795ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:05 +0000

    square people pretty passenger curious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
