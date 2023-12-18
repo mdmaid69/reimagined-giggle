8f70858cb89d34781b8fb7e845de0b5d0a650ad3
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:15 +0000

    might project flat difference survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
