1669bb4776afd009fd9b26c3eca56d9e2313b3bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:50 +0000

    error unlikely union town genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
