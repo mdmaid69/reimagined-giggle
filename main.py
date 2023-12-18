49577d2883487bedb4161e1b8115650678fa394e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:47 +0000

    first me development everything material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
