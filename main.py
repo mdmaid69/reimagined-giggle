7f3fce4832d7732e21c2e1f41730d4759620e998
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:15 +0000

    clever regard great heart comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
+def find_max(lst):
+        return max(lst)
