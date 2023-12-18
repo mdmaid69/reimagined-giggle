d1e8d9de98c70313e3a8c50a9f737c9141513ca7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:49:30 +0000

    this stiff price write coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
