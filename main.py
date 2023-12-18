b8b4f2043358ebc99a64d8f7d54c8875df4180c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:08 +0000

    suckme best it out perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
