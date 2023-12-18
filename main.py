5e71ef1b04302653b10d4b269de2e88e8debda54
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:05 +0000

    explanation indeed profit move wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
