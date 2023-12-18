2661d114ee27b4b1f5b900e28a98af137cfde0eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:27 +0000

    attitude contact fair passenger decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def remove_from_array(array, item):
+        array.remove(item)
