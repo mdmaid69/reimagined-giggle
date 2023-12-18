3098dde0d996391a41addf9231d73719dd5ec666
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:36:42 +0000

    fly street sweep goon protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
