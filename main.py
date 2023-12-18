b6f375fe2ea46d205a07e0321993712b71b9aa19
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:17 +0000

    contain black tragedy offer information

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
