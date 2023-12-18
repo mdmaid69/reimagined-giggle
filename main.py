e58d968686e29e44fae767e2007bcd5432201c54
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:09 +0000

    lunch Ff phone connected getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
