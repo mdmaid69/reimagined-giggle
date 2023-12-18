3efcd0a96c4bc2cd84f0e71a4bafd8ba3f8b8668
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:08 +0000

    whichever reply day fat alive

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
