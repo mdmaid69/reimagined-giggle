5c3d2c15f18f03bf467fa54a4177223ed3d77aab
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:23 +0000

    half less cope neither treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_repr(array):
+        return repr(array)
