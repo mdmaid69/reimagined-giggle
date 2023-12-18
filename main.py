ccbd840ee35fb1e1bdf4d2d3229765e64b83563a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:15 +0000

    attract mother early float lookafter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
