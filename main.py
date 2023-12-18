07a4c8c6429ce8c38b8161a2289883d810e1d45f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:37 +0000

    tall bringdown sorry naturally scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def reverse_array(array):
+        array.reverse()
+  def convert_to_octal(n):
+        return oct(n)
