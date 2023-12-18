afb084e875e696fc295fb82821b9ad2b84615283
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:11 +0000

    accidentally thirsty under recommend confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
