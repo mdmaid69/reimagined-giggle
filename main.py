2ee912e30030e37d84f5b531b70dcb937cfb9851
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:51:13 +0000

    air nearly report past touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
