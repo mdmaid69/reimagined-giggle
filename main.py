0f73dbc8bb7c33b4bf581d1f15106be5724b4ae9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:38 +0000

    right survive theirs contact care

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
