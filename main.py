563209ea3031b7e33955bb061d369b685cbc33b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:05:02 +0000

    funny please gather drink hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
