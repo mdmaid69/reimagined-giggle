fb6785d07a56777f68f8ee0db4cc0e9d048a2225
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:15 +0000

    hello range attempt treat trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
