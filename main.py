23ec6e56dc1168d90ca5f00cb25310c716053b59
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:14 +0000

    lucky warn should vision flow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
