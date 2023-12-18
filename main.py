757763301c28a296ce47ae6a632781e4908ac038
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:30 +0000

    congratulate imagine skin extraordinary ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
