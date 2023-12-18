173049632e67441c6bad555c9364b33566b1afe9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:14 +0000

    book note question agreement broken

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
