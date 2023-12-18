6f8b461bbee810aee3dea08882efb5faaf80311a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:59 +0000

    succeed somewhere private judge uponon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
