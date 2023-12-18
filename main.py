78b950e3b2b8ffd5cc4e96eb0d681a9f31bfa503
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:21 +0000

    opportunity turndown down at better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
