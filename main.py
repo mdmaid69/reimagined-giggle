cff8a8678625215ea36157cb9817ea1665c3e355
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:49 +0000

    nose dare chance scale pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_volume(length, width, height):
+        return length * width * height
