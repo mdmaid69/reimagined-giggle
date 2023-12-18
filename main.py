55a75042f71cf19c2a87c361477c458c8052d2f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:52 +0000

    post proper attach broken game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
