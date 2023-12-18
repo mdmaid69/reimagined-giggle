429bafa1cb1f8cdd4416a083af9453b5e6c2e073
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:07 +0000

    interested future anybody sign raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
