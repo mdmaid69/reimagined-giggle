b317da0c09109dc7b2869c6fa72aeb7a528c4142
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:47 +0000

    house nerve match coat unkind

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
