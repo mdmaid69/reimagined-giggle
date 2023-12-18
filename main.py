d76da9f385456709ec11de6d3a6cfa7c98581ad3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:49:46 +0000

    phone apart general sensible morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
