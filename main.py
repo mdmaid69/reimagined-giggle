51726c168f98b4fcd70e7ebff5936f81395832a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:41 +0000

    believe similar hide hurry mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
