a96e254f7cd25bde18628de77816852a9d09fba3
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:16:29 +0000

    study communicate 565end 566end show

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
