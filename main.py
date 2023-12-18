038dae49d28ba052207a10caba16c67b64f6527c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:17 +0000

    pot hour whom into partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
