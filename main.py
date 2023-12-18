204f5641d9be89098155f670b76b7a299f04f0fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:20 +0000

    between his expensive careless miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_acceleration(speed, time):
+        return speed / time
