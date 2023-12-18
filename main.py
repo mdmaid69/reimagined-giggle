c977dfb5be44cead33e3f55e4702ea4fa75ebd4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:42 +0000

    turnon imagine dream mean live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from collections import Counter
+print(Counter("hello world"))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
