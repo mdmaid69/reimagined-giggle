6c804eeaea6b415fad811ca3655808654ed5f701
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:54:46 +0000

    early describe surprised figure spin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
