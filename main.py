d0d9e7b65f8ff3df05046bf5c337652e8ed3dec1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:51:55 +0000

    blame appeal total without straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
