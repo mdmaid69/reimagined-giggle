129823fef4f9d1369f2ed95b44a84a5336b9b207
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:44:12 +0000

    emergency complain clean true strict

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
