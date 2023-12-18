7c4fd0a67a30ee0f7f489e3b3a2d4878593ac7c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:50:17 +0000

    club manage takeaway kick however

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
