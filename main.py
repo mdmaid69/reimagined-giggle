016c5aaa1a735f5ff52bf07d59be32eda52c3b0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:14 +0000

    principle represent worse contents spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
