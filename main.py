981ae1be28d9ec266bb455bf0d17260b7ca35f3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:53 +0000

    goup reach sink naturally harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
