d4e25db045b2de8487743636cc0f8a94c0cfb4c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:36 +0000

    cry face comeround concerned coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
