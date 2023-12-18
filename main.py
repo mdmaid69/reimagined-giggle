046c99b4b42a282798e8fdabfd5eb4e9dd0da933
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:31:01 +0000

    noone track inside simple offence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
