9720c36f17505cb2c21efab9e4bb05260d212fc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:48 +0000

    breakout gooff organization believe collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
