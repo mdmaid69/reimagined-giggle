59b9cdce449132a3ac16ed9083c25e80a57ae74c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:17 +0000

    pretty disagree mark black better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
