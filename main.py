00885ccfd254533541842e300dd7bf9f379b61f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:57 +0000

    present record excited complete born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
