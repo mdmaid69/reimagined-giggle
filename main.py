fb5b8a93af2fe37942c5a6241f888e4b3a7d7184
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:00 +0000

    lead rush ask disapprove discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
