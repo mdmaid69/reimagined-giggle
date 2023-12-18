7ba9875367f4b7f071189569f6e192581ad39457
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:57 +0000

    567enemy lean Yy backwards fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
