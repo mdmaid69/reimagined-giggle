cfb23292f8c175b74d3c3d7d9657dd7914a1368a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:26:40 +0000

    march mood turnback any somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
