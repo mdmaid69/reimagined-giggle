4cedc46a0dbdce15cfe3f02307943560171efebc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:28 +0000

    regional right laugh else couple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+print("Hello, world!")
