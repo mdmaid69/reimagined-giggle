da594f0e5813b41b981312e92b09a5367c5e7e41
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:09 +0000

    thinkthrough play warm brave deep

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
