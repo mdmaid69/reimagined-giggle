92e73af4e3fa63564709464a593568820651c93b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:23:14 +0000

    flow sign free by scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
