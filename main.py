45fad359a1aa93743da075c21bde34a89f26fe97
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:10 +0000

    secret tonight congratulate 571enough takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_max(lst):
+        return max(lst)
