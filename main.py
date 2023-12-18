497ab6113d8779cef2656ebd14e98eb967d7a94c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:41 +0000

    none shall style explanation himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
