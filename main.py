2ebbe28cad72cf144ac860d2eb39645baa80c89e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:46 +0000

    wide smile between future person

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_factorial(n):
+        return math.factorial(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
