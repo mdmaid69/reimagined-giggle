e1c4054c3a49b48b376a973d627a756a2553f878
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:21:15 +0000

    look attitude everywhere string arm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def clear_array(array):
+        array *= 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
