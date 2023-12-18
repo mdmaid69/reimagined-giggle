bad7f84983b0c2a14295036e5bc3bb300616942b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:29 +0000

    disturbing branch disgusting lose cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
