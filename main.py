1d926247577c08f70dbc3313bb565a0e0920e444
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:48 +0000

    whisper our pick example squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
