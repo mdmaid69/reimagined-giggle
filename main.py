2e0e46d0377a584d18a600cdaff1baca48f1c33a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:35:59 +0000

    time touch serve afraid nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
