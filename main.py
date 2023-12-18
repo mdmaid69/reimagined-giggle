5466e81aef5ec2973afb887dc3ad83fa05fb121f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:15 +0000

    pocket by luck black bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
