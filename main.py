1dcf4f39eb098ce6d75b0e5777eb18fdd7e9a3d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:03:29 +0000

    probably hard correct exactly interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
