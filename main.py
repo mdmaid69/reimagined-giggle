238b35273e72cf43c5fa25cdf262217637b6a729
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:39 +0000

    steep giveout scene proof tour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
