0029e487e7da890fdb5315a5e98dd47023ea932d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:35 +0000

    Ff Tt song key slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def convert_to_hex(n):
+        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
