bf076026d3c8f88dfa531ef8d0f6a85989ccdb75
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:00:34 +0000

    pipe suckme undo mark accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_list_from_array(array):
+        return array.tolist()
