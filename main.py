fb3cb868fdbac0be64827c5671f994408cea5ee7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:00 +0000

    forward beautiful better matter accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
