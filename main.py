ec514b23b560802dfb97c78855c7c6cedd395f14
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:29 +0000

    find satisfied area off sometimes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
