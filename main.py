594565e925173ce584f39f375208d0e56805fad0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:44 +0000

    deep interruption attach surface wing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
