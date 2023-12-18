6be9a3fb64a71e6affdd8c9467ed47a3c2fa3a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:15 +0000

    make week connected claim provide

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
