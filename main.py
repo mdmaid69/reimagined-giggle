cd54a2944b5974d1d934c10b2aef9e1709aa984b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:05 +0000

    pleased otherwise comefrom typical problem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
