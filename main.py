92d0df34788b125521366b06eefea41e2ff8790e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:14:05 +0000

    congratulate claim priority shop spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
