c4917d420300ce1463405ae0b1f2d7af7137f2ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:16:05 +0000

    thinkthrough variety skill later claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
