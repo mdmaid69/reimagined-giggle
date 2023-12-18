abf73bad4695c8cc512c8aa3838ee0287f54cd15
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:19 +0000

    back better lend sudden guess

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
