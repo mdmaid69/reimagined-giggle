d92c974107c2b6171e2d2e6a6d2008456323a226
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:19:46 +0000

    increase theirs available round advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
