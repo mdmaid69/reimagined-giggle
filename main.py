7c8bac1f96e71336a8df5b672fa77bf38643ba96
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:19 +0000

    lookback(on) towards comefrom soil point

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
