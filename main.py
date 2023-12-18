5b7c5cccffbee06f22cc8e83745791ab7cd98a92
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:42 +0000

    mistake save trick grand hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
