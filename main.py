29f026867dcffa07233cf0c452a5d823eb179a7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:39 +0000

    view impossible insist upset accident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
