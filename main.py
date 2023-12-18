8c3fdc329667c0ea0a8ff1d54189b0b1f1c3828c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:46:02 +0000

    confusing unusual go doubt payment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
