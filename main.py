3a620ea62ad9fb4200d1785d3afe4d255e2bce01
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:15 +0000

    taste old him advantage tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
