1e516ae9277a4e9394a984b86ab9f117b5118bf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:52:27 +0000

    objection project makeinto pull exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
