c0929f90663c86845ce1853b4868aa71db5f668c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:28 +0000

    city yourself fall hide tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
