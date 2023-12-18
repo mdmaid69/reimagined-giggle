59f9c2c2d08f80b768632ad323f1e32a557e8af0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:11 +0000

    lower guard window along dry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
