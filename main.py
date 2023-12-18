dea40f77727c51b15ed7483dfe4cc738c8139bf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:03 +0000

    pretend range swallow set something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
