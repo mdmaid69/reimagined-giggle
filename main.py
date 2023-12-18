b801ba9db455c967980390073491edbdbe7fe077
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:52:36 +0000

    relation past separate bound appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
