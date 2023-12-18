0fd96e3dee61f7404de28dd2b894239dc860648a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:34:53 +0000

    description answer scrape nurse deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def is_odd(n):
+        return n % 2 != 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
