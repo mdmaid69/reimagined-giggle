5dc4dfb8a29f7efe6d91ba9788cd0c5ce3c21170
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:02:47 +0000

    bush right by tear our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
