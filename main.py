ebfa4f2fa9d12d351cd1ece07b871c03cc18f0bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:57:56 +0000

    walk least of port famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_speed(distance, time):
+        return distance / time
