7fdac566b9f5cae7484a9bee35dc1877f32d239b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:02 +0000

    bank deliberately dress climb side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
