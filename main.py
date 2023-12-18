6ba5371bee0a9aca9f4c4d25e15a82776f1b8fb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:01:14 +0000

    check want breakup least broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
