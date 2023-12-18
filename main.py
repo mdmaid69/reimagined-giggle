28bfa763ac8b1057a468156604fe9eb29764e109
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:10 +0000

    scene other discussion shopping through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
