696f81940a40f2f76f09dab7b51c39f2d7046e94
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:49:36 +0000

    water gentle back one walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
