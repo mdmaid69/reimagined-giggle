b4604cf594e4800987354eb4a9666e59c388425f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:58 +0000

    commit display bright earth claim

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
