29ec8bcdf4f165aa72cb5a3f76e6875ece2929bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:06:58 +0000

    translate share breathe Tt talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
