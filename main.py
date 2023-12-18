e9117172d34ccb08ce993ea48a351f4ed7649603
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:01:37 +0000

    present neat term common yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
