acd61ab73ac64811066bcbc0e8623c9f44645f06
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:25 +0000

    side down breath birthday coat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
