d299b99f0b4a6a2759cc8157064bd4d2215b5118
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:48 +0000

    rest through mark before mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
