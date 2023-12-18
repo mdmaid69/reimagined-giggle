ab7f043002a980381c7a92708476c25ea392b43b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:03 +0000

    food turn include people finish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
