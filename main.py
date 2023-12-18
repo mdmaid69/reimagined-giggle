acdfbac9e97b421f22a296986d95ff7a56ef197e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:41 +0000

    570enjoyable spring team run basic

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+print(re.match("h.*o", "hello world"))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
