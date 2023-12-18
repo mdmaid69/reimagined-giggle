c0bf4f40b15b48d3bb5beae6040d49cc7387baeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:44 +0000

    pause pencil firm hard stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
