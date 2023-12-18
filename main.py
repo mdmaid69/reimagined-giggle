b37163f3e407811b75f14163973efda0b6358c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:22:12 +0000

    cruel fast payment lookback(on) guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
