7e8c39d1fd9d605573534280e96daddec34d3081
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:37:56 +0000

    cool string such room will

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+import random
+print(random.randint(0, 100))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
