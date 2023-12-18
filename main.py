0d6274fbd48796176f13d88c784ad69e4f3f1b93
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:30 +0000

    Ff line neither show error

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
