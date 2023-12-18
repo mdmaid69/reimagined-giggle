df3084ef7e61c6224adcaa1c598292c06ca78a79
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:34:46 +0000

    father intend affect model also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
