14c52822dfea95f648e1a6694e42bed87b4d7799
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:56:14 +0000

    hear considering exactly interest shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
