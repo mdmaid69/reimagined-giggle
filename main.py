758b6acd84306e5e79dd506431528fbf08774c73
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:21:55 +0000

    coffee fence slowly suit her

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   def is_odd(n):
         return n % 2 != 0
