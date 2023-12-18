afb30786f6b3752ee5991c96ceb9a798b58350ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:22 +0000

    Ee supply software touch journey

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
