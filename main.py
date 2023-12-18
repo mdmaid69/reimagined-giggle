8f30f2c25a03d290f94101f83aefc0210982bb54
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:18 +0000

    comeout cost affect creature freeze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
