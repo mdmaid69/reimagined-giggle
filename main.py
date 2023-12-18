64a2d617c590280b525099f3b7ac34eca224ff73
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:00:00 +0000

    kick sharp separate even please

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
