9ebb90d623f13977e3697f5488ac7fc623b167a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:19:44 +0000

    produce squeeze better lookafter nothing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
