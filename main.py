b8e11e552ad5bcd163ed47eb4de40bcceb3246db
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:19 +0000

    in important doubt frightened heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
