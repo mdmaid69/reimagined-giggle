ef3475cac7e4e7163344e3d9187ad62ff3ee3531
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:28 +0000

    suffer this occur wrong time

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
