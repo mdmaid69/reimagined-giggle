03dad3629de1de1cc5cb26def0e1aa1863f7c375
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:33:02 +0000

    but vote spin suggestion print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
