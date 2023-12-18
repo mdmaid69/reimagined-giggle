2da31f33c53bea447065b442665e81ec6b5d5f43
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:38 +0000

    top swing danger hardly business

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
