86b781cb061582f961d617af16e323ae4cb4deef
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:53 +0000

    row crash gooff search fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import os
-print(os.getcwd())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
