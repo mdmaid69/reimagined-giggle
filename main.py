ada4b9e6fb720e6e01e69561a52fbd8a713778c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:33 +0000

    friendly spot common under building

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
