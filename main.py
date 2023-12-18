6d92355eb89ffb8be8f5e63e1d5256264a95f763
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:23:06 +0000

    after around shoulder reach what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
