eb30a4b0fda5c03e53272324366465319c567631
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:59 +0000

    only various around fair university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
