412ec8eb4410951b81537c6f190585f17b9ace45
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:28 +0000

    twist bush bread pay mouth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
