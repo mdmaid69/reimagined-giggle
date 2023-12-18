61cfd74ae1213e7a2941e1958092c1b07641dbb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:24 +0000

    high age accidentally usual extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
