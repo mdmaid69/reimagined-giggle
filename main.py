a0961a3faafaaa3c0a1ddd20eaf2ec2cd9fff142
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:12:48 +0000

    there central by makeup impressed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_list_from_array(array):
+        return array.tolist()
