cccc15910361ab6845c31c82aed0504df36ced03
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:58 +0000

    zero success arrange quickly software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
