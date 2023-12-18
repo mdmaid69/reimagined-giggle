978183ff2c120db7a5a23c69b81120318a7c8516
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:55 +0000

    miss excuse equipment culture today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def subtract_numbers(x, y):
+        return x - y
