ba4c4ddc385f4bff024480488bb55f27a59c36a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:41 +0000

    serious operate impression think left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
