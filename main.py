c2d567c07963ab12345439bfecc6a62e5165ba35
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:10 +0000

    spoil hear smile up slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
