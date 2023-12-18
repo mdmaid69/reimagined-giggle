356358944e398b12b3901bdd58953b1e38cd2fb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:52 +0000

    water new criticism getout except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
