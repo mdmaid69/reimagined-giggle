0d4ce7e087eab5a9d9ce0f9f8caf9ad2f0ee4758
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:15:16 +0000

    Bb keepup polite the distance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
