2ad5aa161f830274746d0cb7fdae168e71e5b12e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:55 +0000

    used lookaround there set popular

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
