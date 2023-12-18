e647ef7df2ccc7c7b6b1fe4318750d8498f24727
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:03 +0000

    star perform makeinto refuse address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
