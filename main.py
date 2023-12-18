b413699ca8a9f26aa559f12f434f299d43dcf7bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:44 +0000

    goodbye spot only freeze side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def greet(name):
+        print(f"Hello, {name}!")
