99993dd1ae3c2167ac981c191f84cccb38a33130
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:36 +0000

    meanwhile reduction size answer beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
