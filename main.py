a123623ec1eea8259cec7ed4863d66973b6c6e9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:21 +0000

    shallow deaf expression somewhere land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
