dd65746b9e9c9dd5309266711fee7683b26d50dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:22:52 +0000

    against sensible lookup Ii whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
