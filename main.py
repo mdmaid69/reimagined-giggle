505e433a9a3e16a73154167d30c827f980f07d99
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:03 +0000

    gentle know Gg life lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
