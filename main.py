8c7b9c3bbbe23732ded0728c708dd427f46ad310
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:49 +0000

    square run interested software pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
