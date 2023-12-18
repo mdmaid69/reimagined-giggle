a64cc37baac0ec6b7a9597e5aed39cb9dbe860f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:01 +0000

    command swimming protest children sing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_elements(lst):
+        return len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
