b126a1cb7c7afc464b21d692541d061d0101b342
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:34:18 +0000

    schedule talk keepoff obvious doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
