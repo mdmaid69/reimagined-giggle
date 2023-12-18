ddfb93df896877b9d2853aec0472f990604a73cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:17 +0000

    right hard mix indeed ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
