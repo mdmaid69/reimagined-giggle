4ccafd5d671acc8b8c4bf20c2a173d6db2c7767d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:01 +0000

    meal reach charge bath miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
