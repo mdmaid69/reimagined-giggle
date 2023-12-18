d8a270bd363aa505e0df1f427da205df39b3d631
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:42 +0000

    on official runover trousers not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_current_date():
+        return datetime.date.today()
