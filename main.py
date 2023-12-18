ef73db1f324e6c1b4b6f2acf14457afd057939a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:32 +0000

    everybody rich yesterday short answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def square_number(x):
-        return x**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
