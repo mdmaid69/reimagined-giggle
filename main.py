e076f98e10904b2db1753510bb9c599f33f15326
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:59 +0000

    Kk suppose harm stamp close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
