e65fffa237ba123924db1a7c5b2ed3141abdd4e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:01 +0000

    pleasant software height form square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def reverse_array(array):
+        array.reverse()
