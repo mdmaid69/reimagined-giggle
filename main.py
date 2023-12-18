c94fc935b8e80940fca9b53f133ff82866536f30
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:39:01 +0000

    giveback breakfast yours block worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
