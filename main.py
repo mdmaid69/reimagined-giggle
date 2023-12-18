5ab921a1a0b78ae1432820b2636db69d53767eed
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:22 +0000

    success view over attractive nice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
