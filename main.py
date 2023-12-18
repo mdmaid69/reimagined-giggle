f72f6fd24f286599e4ca6a91ebd0ac3c798b9550
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:41 +0000

    later lock neat dangerous excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
