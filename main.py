07c6a6261cda4eae1fad8c6ba8b764530ecb0a9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:55 +0000

    everywhere pocket account giveup shadow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
