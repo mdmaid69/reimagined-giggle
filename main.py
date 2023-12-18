020cce09bf7aa7abb5312d08b737bce47aa1691c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:10 +0000

    responsibility trousers reach soon failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import sys
-  def get_python_version():
-        return sys.version
