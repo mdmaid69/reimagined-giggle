b17270ec87eae6b7a97170bec38acd52e3a3e8e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:32:57 +0000

    convenient objection spoil catch bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
