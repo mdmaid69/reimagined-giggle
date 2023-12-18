03bf64ab8f82755173da81d62be5db1c88aafee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:28 +0000

    weigh rare wise seat comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
