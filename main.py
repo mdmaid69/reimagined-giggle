bac949bf21252e03ae81ef65b82fa6317e6f7561
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:40 +0000

    evening cold success apparently born

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
