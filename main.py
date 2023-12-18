473e9dededb4f42dc3ab7f8adc5189a22b918c81
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:29 +0000

    much safe food within steer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
