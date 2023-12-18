ce10aab47e84a29863bc45dd73d43dcc1c9c0098
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:22 +0000

    pay goout turninto successful shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
