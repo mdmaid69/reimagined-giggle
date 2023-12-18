de8666b8308292ac2120346021053529e4eb020b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:07 +0000

    afternoon over unable watch again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
+import sys
+def print_python_version():
+        return sys.version
