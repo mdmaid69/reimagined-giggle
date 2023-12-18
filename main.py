3df4fa43ad9c256ee60ad3695d99700a6e1bdc6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:58 +0000

    interruption top over suit them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
