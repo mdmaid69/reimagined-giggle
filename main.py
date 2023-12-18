b0e6eab0da5da4a53cae67d82d73b9eef29d8417
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:49 +0000

    fork one note 565end light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
