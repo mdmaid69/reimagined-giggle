609e087df8537fb15c77fc090c6a84aa5edc165f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:03 +0000

    down lucky cum pleasure ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
