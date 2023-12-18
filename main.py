20e9a1fa028de00f390bca612c934ab796d62434
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:12 +0000

    bear block key maximum moral

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
