798c70ea6b4f218ec2705326a697cb87373e6058
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:14:27 +0000

    about world switch ours face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def roll_die():
-        return random.randint(1, 6)
+print(sum(range(10)))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
