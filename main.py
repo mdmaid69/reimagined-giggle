e8b9241d498f91a10b2986d1e4adf451065b0177
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:04:34 +0000

    mix dance Jj effort juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
