531c8bf8c1eb9d2336c91f60c2433dc3329dcc9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:54 +0000

    total undo wild joke immediately

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_even(n):
-        return n % 2 == 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
