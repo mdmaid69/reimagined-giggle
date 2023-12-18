6bd0a20e75bcd6cee02871da6b9190f261d28791
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:26 +0000

    prize must immediately less exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
