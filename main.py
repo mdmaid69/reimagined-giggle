7c403f604ec805a63e225fa3ae45c49b72662718
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:47 +0000

    recover exit bright shut natural

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
