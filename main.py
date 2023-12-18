3dde76f72326d8bbeaf5d0ae39d8f999f6daec1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:55 +0000

    skirt crowd snow equipment bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
