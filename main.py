51f58399a1d115aef7362eb23bf61f020d429fe9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:44 +0000

    achievement tongue slice body right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
