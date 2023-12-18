2657bb88c6cd69f88584638a18eedc124c4385b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:04:47 +0000

    doctor seem love name excuse

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def cube_number(x):
-        return x**3
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_to_hex(n):
+        return hex(n)
