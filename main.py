d3679bdaeef64443b4cd11288671a37101ac3224
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:24 +0000

    mark score basic theirs collection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
