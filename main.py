72288ece3baee4dd0ed6485fef3de98360d5566d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:51 +0000

    disagree spend fall contrast cost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
