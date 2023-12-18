44ce90a77567299735182c830e0ae35a37d34d30
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:03 +0000

    computer square command heat happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_directory():
+        return os.getcwd()
