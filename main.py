c1715cc2c4516694faae1570b2841c8aa36c2e55
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:17:55 +0000

    fashion spill confident maintain bear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def remove_directory(path):
-        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
