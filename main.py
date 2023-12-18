8380d212936c7ae562fae7848c6c21af5f286b32
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:07 +0000

    central pass surprising offence ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
