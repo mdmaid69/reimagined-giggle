f2e324abd9f91e87b3738aea481dd4e6d5393ded
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:39 +0000

    please front comfortable uncomfortable last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
