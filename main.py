5abf2e25fc13426eff7de331f5c06dcbee3dce3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:39 +0000

    mend except still willing quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
