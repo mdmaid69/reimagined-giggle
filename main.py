982738fc954f51feee748847f34d4adf5ed9189f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:45:08 +0000

    last breathe goup mainly top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
