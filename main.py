807cc4c24323a8ce19ab7c8f7752e6fe29bea211
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:48 +0000

    aim expect soil artificial neither

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
