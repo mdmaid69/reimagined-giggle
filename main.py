46961430e500efd65491bbd971afae4babe29257
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:04 +0000

    direct hollow future straight approve

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_work(force, distance):
+        return force * distance
