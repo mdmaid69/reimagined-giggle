2fc67cc60578201ce575cc8c4d1bc14244fa1a0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:46:13 +0000

    point intelligent inquire face ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
