dfe8a1d363bdd99a95547216716b591e41db4dc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:19 +0000

    interested Zz ever bang I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
