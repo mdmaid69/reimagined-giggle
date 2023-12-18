865497ad6c25b3971bac3b6086b51116f62f5a61
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:13 +0000

    inform practice aware record anxious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
