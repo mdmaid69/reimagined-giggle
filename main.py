992d6062d3070d29ea86942ad0f7ed2abd05e918
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:49:45 +0000

    write forward second whom few

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
