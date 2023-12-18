1668f669586cca3593fb09da697e9e673013ff35
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:28 +0000

    upstairs keepout bunch border success

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_pressure(force, area):
+        return force / area
