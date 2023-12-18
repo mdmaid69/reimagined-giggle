2241ac829309ddb365dbb300e0eba34e49c4e0c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:14:44 +0000

    watch actually comeoff connected holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
