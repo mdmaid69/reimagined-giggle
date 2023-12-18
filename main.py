59a14c358b3190efe5cb24f93bad532e19b10eec
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:35 +0000

    unable plate Cc understand positive

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_slice(array, i, j):
+        return array[i:j]
