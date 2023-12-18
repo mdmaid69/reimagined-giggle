297f2e3f97a731d3c070e47944ce0dc517162937
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:10 +0000

    mystery range through oil though

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def convert_to_hex(n):
-        return hex(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
