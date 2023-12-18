c7eb0a391db20954debf990a141e61237afafd61
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:35 +0000

    glass belong through change listen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_priority_queue():
+        return collections.deque()
