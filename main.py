4290d6fc5fe327306d76519a285f3e186bd4732f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:10 +0000

    conclusion valuable object attractive instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
