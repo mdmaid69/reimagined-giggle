7de1e51daa88bb80d2325c7d6ade89ef612062ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:38 +0000

    there moon build crash contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
