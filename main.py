c97ee9bf4a2ba23cdfccde48ee523c6bbac325bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:15:17 +0000

    whistle approve period narrow afford

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
