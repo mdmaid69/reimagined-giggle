4e8c054c1cc162511f84b9dbcdcc4a5661c6fc0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:03 +0000

    over control comeup first crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
