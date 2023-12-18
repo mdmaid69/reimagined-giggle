c20a48e35c42212b39dd3213eab1ad9199d7a124
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:39 +0000

    show different demonstrate Hh right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_repr(array):
+        return repr(array)
