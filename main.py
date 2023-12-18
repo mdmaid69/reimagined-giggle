03fa6f2ad6a8cf9f0c0c52c04cf39a3bc8a7dfd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:58 +0000

    behind physical already scale shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def sort_list(lst):
+        return sorted(lst)
