17a29fcc8437a88ea7dffd4b70193d1437b04653
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:48 +0000

    decide interested around learn world

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
