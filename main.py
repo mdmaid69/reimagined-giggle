7373cffbd99786b9a9da2c65f5034a63d513d769
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:16 +0000

    past leave Oo protection help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
