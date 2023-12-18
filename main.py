4727871ad2d11df089d51c7ffb2b215dc6422c63
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:01 +0000

    miss pretty path round estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
