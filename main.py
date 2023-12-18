b3975d03c3200e687d6f615ce39d320a34042dc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:34 +0000

    dull suggest 570enjoyable shop off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+print(os.getcwd())
