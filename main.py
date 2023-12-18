1efd4cbd703e53e8254d0493029a11c12e9a1d3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:55 +0000

    early store stupid so brief

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
