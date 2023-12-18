7b0370469e06ba6790b2fd349fb56b4eb3f7dd63
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:41 +0000

    part week government sorry 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
