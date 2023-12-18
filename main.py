986d0ee0eb7107cda41b7e304b54890fdea7abcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:06:01 +0000

    plastic better face fast tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
