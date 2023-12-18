a319aa761c4ee12024d60d9e1f47bda9f6526272
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:01 +0000

    leg like judge tone sport

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_array_to_string(array):
+        return array.tostring()
