43d866c253f12c70f9c8911ae6aef4c80ef2222e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:21 +0000

    mystery Zz second trick top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
