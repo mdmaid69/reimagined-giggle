9d8b13f5053c5b3e7a2fb3c9a05d7497d1c19bde
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:20:46 +0000

    female leave holdup fasten listenfor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
+import logging
+def log_message(message):
+        logging.info(message)
