419cc1378fefd0fa433affb88de312e9d4fbd421
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:01 +0000

    country road interest crash finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
