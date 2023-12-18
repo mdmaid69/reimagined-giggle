fe82fcaef546e0606c88cb44eee981becf64ac6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:08 +0000

    afterwards stupid responsibility available opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
