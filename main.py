c3d3dff8d3723e432fb494e266ae18af5d42348b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:16:05 +0000

    exit clever everyone teach complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def exit_program():
+        sys.exit()
