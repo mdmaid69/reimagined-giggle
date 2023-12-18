74e0ffe0b67041c4b4382f51c586058b96affb09
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:28 +0000

    among indeed spend material dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
