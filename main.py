4cdb3fb08961ba899e41f22d3a5312b2ab3ec6c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:42:07 +0000

    wrong brush flat conversation steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
