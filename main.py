241577895c6e4dcc3dd797d5e8a94ea1518fcf66
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:35 +0000

    problem few let respect tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
