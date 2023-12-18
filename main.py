44304b976a6b24c7b067e788701c5aa29453b31f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:49 +0000

    road exact though because arm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
