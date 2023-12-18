f743eeb116f0c623975552a6e53544333e9bfeb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:14 +0000

    last last you ask balance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def count_characters(sentence):
-        return len(sentence)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
