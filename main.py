62d479ac1957718e40d27fe5ed183c37f0e6d50c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:46 +0000

    uponon disappointed ring stupid protect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_int(array):
+        return int(array[0])
