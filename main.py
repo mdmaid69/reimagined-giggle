f13c069d1585a7d730f82315e871edadee9245c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:38 +0000

    thinkover main announce eat satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
