3861230c9138f1099d1ac0e0bb23251145a9d8ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:18:13 +0000

    fair arm congratulate little relation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
