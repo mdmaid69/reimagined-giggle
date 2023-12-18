d5c660d7d101028ff4563aad79d37cf428714f0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:21 +0000

    count matter tread them case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
