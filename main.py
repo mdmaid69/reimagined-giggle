99e43e0d8b30cb677b9ba99d66e30ef830eebbaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:07 +0000

    light free takeover respect Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
