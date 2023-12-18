275fde3ddc9d7b6c67a2af2500122fbed8db1cf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:35 +0000

    understand flavour minority break quit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
