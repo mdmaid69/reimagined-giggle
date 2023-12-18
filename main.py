d426bdbd8431ca640ca3d42bf109527ece9216f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:41 +0000

    base sensitive shake compare pattern

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
