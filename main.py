d30d969a5b9f050253218e12173b20434f741b4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:20 +0000

    follow rate near classroom record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
