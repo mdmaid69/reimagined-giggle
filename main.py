c4d9acc941ad631311db957dddf5d8d19507455a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:29:28 +0000

    work exactly with minimum along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def subtract_numbers(x, y):
+        return x - y
