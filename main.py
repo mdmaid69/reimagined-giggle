4dab88b11d64938d88a591ac5077d97e79df3ca5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:07 +0000

    weigh offer cheap simple advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
