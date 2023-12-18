06d177ff4c9955639d848c9f643fba37a2ba6834
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:18:05 +0000

    characteristic else far married expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
