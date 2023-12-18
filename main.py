11e84e6e53ac5fbb83b58ad7017967b583c000f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:54 +0000

    sure sore regular suppose match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
