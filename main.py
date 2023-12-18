b028ac491c137c49bfd6ac6fdb93040ecea111f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:49 +0000

    group name downstairs waste lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
