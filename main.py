f10387591a91b974c671ed8dbece00889cfe33aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:49 +0000

    feature wish shade moment vanish

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import tensorflow as tf
-print(tf.__version__)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
