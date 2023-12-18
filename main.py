5415cb91ea470bd3be0d3b20f1f30f61d2f6839e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:54 +0000

    share maximum report dull surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
