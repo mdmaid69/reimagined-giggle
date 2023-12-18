49bd375127899a9f6d95a46ebdf93302349d4aaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:02 +0000

    somewhere genuine exactly present persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
