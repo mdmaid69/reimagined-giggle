afc487576f85a451e716d05e592baaaedcfe9a02
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:07 +0000

    practise burst workout progress comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
