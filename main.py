f0f8982ee91a589900ebdcb832bd0ee72eb7236c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:09 +0000

    tread project crash mess arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
