722740a52dc6666f20e2dbc4bbde75053304f388
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:26 +0000

    hard without reserve sentence range

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
