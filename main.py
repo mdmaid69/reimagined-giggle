435bb1e11308b2050f7cf0448534c841015bc6cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:14 +0000

    question skirt past flow identify

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
