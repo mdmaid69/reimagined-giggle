905782da2b2553851b3102491431e48085cd4dd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:03 +0000

    shelter risk thought correct importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
