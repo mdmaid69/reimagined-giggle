6e9d4c041c3dead1a265247e941a40191133a190
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:49 +0000

    like injury mouth breakup serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
