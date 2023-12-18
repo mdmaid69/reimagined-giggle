2aefd50a2bde5c7e0c949538c97e906ce62499cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:13 +0000

    fact temperature protest godown temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
