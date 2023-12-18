82db5f30d7fe7028652885b806ed0caf110ea4bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:48:24 +0000

    freedom stone or Nn model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
