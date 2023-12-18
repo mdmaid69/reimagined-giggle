a9b2714bbfd312cf3049928215105d157cf6f50d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:18 +0000

    together against wonder whether nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
