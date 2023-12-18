752b75f747d1912385aada93c03d05e74eaae5f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:04 +0000

    position face strange immediate fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
