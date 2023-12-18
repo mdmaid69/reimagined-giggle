5a27a9dc0e6ec103a133c4be676098c0301a266f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:41:54 +0000

    worry gap air foot contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
