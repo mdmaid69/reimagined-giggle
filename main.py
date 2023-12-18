931124544bb904bdf4a67cb4c632cbbca54f5fec
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:29 +0000

    maybe in reaction expect weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
