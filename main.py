3883ccae8cbffc9d7fd36e9d39c9af5e7300ae40
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:38:22 +0000

    impressive record difference culture only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
