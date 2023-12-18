4d4eec1978cf6dc1b029ae7b99b8cd4831edd465
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:13 +0000

    impressed exhibition sign voice match

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
