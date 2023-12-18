19b4c2760ed077cb4d703cb8338f9effbf5da78c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:31 +0000

    system direct church appearance whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tensorflow as tf
+print(tf.__version__)
