84a2e053748f343c61acb7feeb1537899798d8de
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:19 +0000

    moment level runinto demand nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_float(array):
+        return float(array[0])
