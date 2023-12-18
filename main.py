bc74206053e32a49ef7a90d1f72f26546b58b17f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:06:21 +0000

    commit thumb danger activity suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
