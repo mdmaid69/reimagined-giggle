c6eeb8304de91cf000589639aa2c48835496fa04
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:34 +0000

    dress letter bad wild obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def is_palindrome(s):
+        return s == s[::-1]
