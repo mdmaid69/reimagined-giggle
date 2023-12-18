edad6417daeaaafdd7649a1d432fb572c0cc9d3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:38:20 +0000

    I contrast obvious pile turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
