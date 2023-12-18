7e84a56fd46e1c23b9b26e6559dad817a65126f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:24:53 +0000

    deaf quick wrong spread minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
