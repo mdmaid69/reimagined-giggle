264057aacac82d36f6271e5947980343c431bdd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:19 +0000

    throw thank foreign recently inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
