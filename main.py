d33b41ebc1a66202e72a2bde280919145a039147
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:20:15 +0000

    that down rest task loss

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def subtract_numbers(x, y):
-        return x - y
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
