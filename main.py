e69ad5e98bec452a401d82c75098d5a11bb4812c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:17 +0000

    over success nothing 573entertainment arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
