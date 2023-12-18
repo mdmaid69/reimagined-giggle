9c48b5c675b9a129124e703d8c428bad3a269960
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:43 +0000

    tone disagree 564encourage understand table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
-print(math.pi)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
