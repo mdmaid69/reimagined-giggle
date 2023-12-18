5ca331239604050200f7ec043f02d6ecf32909f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:34:57 +0000

    terrible claim ground field broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def count_elements(lst):
-        return len(lst)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
