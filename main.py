d8d2239ff9666585ec56ee581e2c65ec30e4fa23
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:32 +0000

    night steady throughout boring test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
