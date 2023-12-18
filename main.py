db884874261695a52cb142a9fdcec425568937a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:33 +0000

    characteristic female behind floor sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
