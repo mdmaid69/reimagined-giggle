2ae0564c00bcd273bf4dcc73caf21f5fe3420521
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:54:59 +0000

    see able name quick matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
