c72af40f7ef34a43076c0ef304c221ac8cb961d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:35 +0000

    naturally entrance mad roof turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item(array, i):
+        return array[i]
