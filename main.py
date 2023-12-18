ab019439ec130e3c5d41b6195263e2319c08f38f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:53 +0000

    basic discover possible force frame

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+print(os.getcwd())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
