b53f84dc263ad9c5e2fae0af6d7afbcc3bb03939
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:35 +0000

    phone divide crime design treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
