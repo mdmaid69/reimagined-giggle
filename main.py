441d824dcc06a582ffc1ea82098fd00e0c1cfdb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:31 +0000

    manner warm copy excited no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
