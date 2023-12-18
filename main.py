43da1a0046509fef444eda4cd6c61df607863c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:45 +0000

    possibly might do talkback thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
