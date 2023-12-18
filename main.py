1c6a5028bfb151f2df9512a6a54e45b6b683528d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:48 +0000

    rain worth callat interview clever

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
