afa878d77f536ec10ed56d8f76b94703af9582f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:13 +0000

    entry match interrupt powerful information

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
