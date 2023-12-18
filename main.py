49700a138f6c79b17a61252d47f09713dc5ea591
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:07:12 +0000

    plastic survive estimate change experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
