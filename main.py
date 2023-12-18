f657ac46207f1c6470123b6e2e18e43f949fccec
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:38 +0000

    spring noone impressed company expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_str(array):
+        return str(array)
