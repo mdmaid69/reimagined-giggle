18f672212da488bf3778627a89011a82f978eddc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:13:11 +0000

    shoulder urgent odd wall interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
