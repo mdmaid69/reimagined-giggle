93dd43c96bf6c86ddf6e13fa5af419e786c671f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:54 +0000

    watch drawing event task hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
