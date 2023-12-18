c87fbb098284c81924ef0c8b0340582c285d4354
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:16 +0000

    lead slowly sure land sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
