e275151674fbdee0a534a7e939eb652f49f98a4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:43 +0000

    wake notice meet what speed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def reverse_array(array):
+        array.reverse()
