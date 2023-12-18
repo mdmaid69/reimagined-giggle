5c07b4a67aac2e80fa100dea100feadc259b0621
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:46:22 +0000

    doubt standard want doubt sister

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
