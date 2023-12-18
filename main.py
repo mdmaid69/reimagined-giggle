f3d4ac31b09dd0027bfc84befa994bccf4d181fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:32 +0000

    independent classroom wall fair create

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_max(numbers):
-        return max(numbers)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def convert_to_octal(n):
+        return oct(n)
