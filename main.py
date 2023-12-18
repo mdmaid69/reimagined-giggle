e19bf3556c3738ae2e351cfa1c2cd078163b8c1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:43:44 +0000

    few performance dress level tell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
