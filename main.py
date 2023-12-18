1e36dcdd00b4672b3bcf6022b7cf8f10e49b02c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:20:17 +0000

    entrance comfortable strike comeover grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sys
+print(sys.version)
+  import os
+  def get_current_directory():
+        return os.getcwd()
