f4dfa8ffc7af7a1884499d238408a08242ee4b1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:16 +0000

    think night world convince snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
