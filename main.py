370a3329337d8734d106a533c851edc42036c2eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:23 +0000

    food bound train miss write

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
