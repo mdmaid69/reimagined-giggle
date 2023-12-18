6ecc59f8664a9bff30bf33db0e93977968508f32
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:55:11 +0000

    cheek season call part theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
