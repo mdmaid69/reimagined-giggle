f4232beb654b478953e1e6db0f08130b626647e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:15 +0000

    short raise sale probably off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
