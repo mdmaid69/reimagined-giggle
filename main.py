8d3357dab329dad00e3926d0e421e28587dae1e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:28 +0000

    mystery probably coat entry relation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_power(work, time):
-        return work / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
