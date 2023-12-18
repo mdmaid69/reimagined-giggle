4785ac922e6957bee15038934b424e80cdc898fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:19:27 +0000

    home careless workout night crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
