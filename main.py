b89dfde93c4a1f73983a124e6e088aebee2c0431
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:01 +0000

    rob degree Mm steer motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_density(mass, volume):
+        return mass / volume
