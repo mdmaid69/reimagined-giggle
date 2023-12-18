1202b1adb39ae38fd44bab070db3ae22af5f48b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:49:01 +0000

    persuade healthy software material present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
