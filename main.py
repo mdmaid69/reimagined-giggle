6fad40dca0eb24e72becc44fc39a6b9cec556c38
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:20 +0000

    push manner everywhere ahead judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
