2009ae783cb5dd8562d38e8c9ecf96d02764e6f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:38:19 +0000

    well worst callout shut aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def convert_array_to_string(array):
+        return array.tostring()
