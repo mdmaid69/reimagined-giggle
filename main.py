9fc4569456b096e5e3f470070546657b6fa9b7d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:30 +0000

    definite near pool year company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        print(sys.version)
