513c14e84af7c215e13829865a40d3b56af3d5e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:34 +0000

    plenty eat unless breath tell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def subtract_numbers(x, y):
+        return x - y
