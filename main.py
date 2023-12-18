f8216aafad2f0eb7bf89fe70b7cbd09bf74d0d7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:41 +0000

    message organization careless though concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
