a070d27828ab8ecc9a004388a90957cdf0224a21
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:33 +0000

    down new better run support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
