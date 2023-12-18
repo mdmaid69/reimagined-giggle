6053ee98d437a46b0163e3c6818148509d7ef0b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:38:09 +0000

    slip distance software report recover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_string_from_array(array):
+        return array.tobytes()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
