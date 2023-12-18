9ed306d09843e0d5f79f3e3577e79474840ea1bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:14:21 +0000

    colour among indeed actually comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def remove_duplicates(lst):
+        return list(set(lst))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
