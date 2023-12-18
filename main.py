b955aab046ab7f0d90ce9f3a0017b945bb1469e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:53 +0000

    connected way rich uniform phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
