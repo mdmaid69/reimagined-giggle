56b5386a7bfb1dd7078acb0f3da9cb48068b51da
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:10 +0000

    be patience see call this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
