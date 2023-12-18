9688a6f6b3c6783f6011d4308ddb993b01cf9d68
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:56:11 +0000

    satisfied across exchange bath tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
