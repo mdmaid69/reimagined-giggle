f6da43a60be0b407b9c86f4d513f3307c6601a25
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:59 +0000

    sink me proof young shoot

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
