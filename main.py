f333de568b039d5e0e921c962736e7e54154c7ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:42 +0000

    invitation shopping develop hour worry

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
