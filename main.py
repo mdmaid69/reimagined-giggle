4a82507b7a2f6f4c46cbc04066ac279003dd4d1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:51:24 +0000

    soak night somehow cry act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
