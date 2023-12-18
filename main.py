03ba2e1462b6fd8acb46c0cd23970b2d77272a65
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:14 +0000

    nerve meanwhile in copy wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
