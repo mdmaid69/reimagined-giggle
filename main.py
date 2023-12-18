d560d0dc35f9918318035f0961ba74b99df56f02
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:23 +0000

    exist feed argument steer fortunate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
