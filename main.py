a9c33915c1324fc02cc2e2eba81c98958b3b1ae3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:50 +0000

    phone flavour service practical turnoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
