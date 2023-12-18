745bb332139267aabd0c9a4dd141622746555b8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:09:21 +0000

    rarely project near suck up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
