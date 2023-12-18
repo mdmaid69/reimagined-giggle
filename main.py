0cfff71d91474d544fb761e494efc92c4fac3ec8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:35 +0000

    ignore wind inside favour burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_int(array):
+        return int(array[0])
