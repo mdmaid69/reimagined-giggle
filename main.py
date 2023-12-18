e30b23892492d7f34b81babd79193a7f2f4163c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:57 +0000

    compare activity basically chase display

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
