8d803fa151f8529b8f6d8c1ee232a346a1e2d748
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:58 +0000

    partner slice achievement rubbish temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_density(mass, volume):
-        return mass / volume
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
