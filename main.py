70db2927d4769ddbda036fc658dcbe27650e2e1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:23 +0000

    forward shoulder book false tape

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
