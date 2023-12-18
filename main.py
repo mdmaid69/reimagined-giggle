1f4b3f85e4472ce1a52456b3b250ff0adddbee10
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:32 +0000

    spoil sensitive cost stupid increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
