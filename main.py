c604f4c487d9e8977012684dd5fa46dd486a2009
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:34:38 +0000

    comein left hurry shoulder answer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import time
-def get_time_since_epoch():
-        return time.time()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
