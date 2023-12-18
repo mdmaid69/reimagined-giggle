847f007c1c43ca51a38bddb5f4692d6943270387
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:05 +0000

    in grow special once dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
