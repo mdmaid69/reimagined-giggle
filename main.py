4ab34b5502be58c9d8b90f44dc37e29c35d65d8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:15 +0000

    against feel hide test plenty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
