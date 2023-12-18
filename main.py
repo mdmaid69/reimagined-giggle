d5c8ba390d5279f5475eabc43e7f24a821f71c0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:05 +0000

    depend lock soil image forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
