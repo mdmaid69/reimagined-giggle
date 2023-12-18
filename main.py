e9dc9c3cae0d10a09dc38094b392964a64c11f79
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:41:16 +0000

    relationship room mistake chance sudden

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
