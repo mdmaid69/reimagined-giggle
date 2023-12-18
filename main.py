daa2e9d7f11f411f25adc6216ec3cdc3d3d30a9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:08:03 +0000

    slight mark disgusting dance keepout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
