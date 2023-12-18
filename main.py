1687df53e9b2440fb81a1444868e4463255f3081
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:02 +0000

    cry goout Nn matter person

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
