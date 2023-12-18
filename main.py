11e3ac2e20316c5f718a1096f52afe4007a5cd2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:13:01 +0000

    performance apparently inform sentence squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
