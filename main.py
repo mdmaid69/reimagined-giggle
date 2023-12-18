88761a97a2f01caff577d033cc02adbe64dbc582
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:48 +0000

    hard square visit cheque reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
