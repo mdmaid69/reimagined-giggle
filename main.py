f74a7d181f7467293bb7dd819368b152b9d168a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:35:52 +0000

    slice mistake and mess address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
