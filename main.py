d6cd41a75ef0d8517721029f2751fcbcd51f9ad0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:09 +0000

    least frame neck thorough doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
