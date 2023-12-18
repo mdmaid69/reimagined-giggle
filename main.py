a6ebe7a5e9e7c8e03a9244616f51e79e3172c6ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:24 +0000

    happen letter share ring great

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
