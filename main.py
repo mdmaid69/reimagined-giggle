d2778a1fb63d125d11c9f80893e73944907cbca8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:49:54 +0000

    meal relationship breakthrough future congratulations

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
