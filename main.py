5315b77a8baa7bf8cd97bd38a299cb32eb1df25c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:43 +0000

    closed hide solve they steal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-print(sys.version)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_min(numbers):
+        return min(numbers)
