958383da0a147e96c2b2b9e5fbd5cfd8f7e7ad29
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:38 +0000

    legal ride book behaviour afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
