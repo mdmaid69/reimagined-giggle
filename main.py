8992f6acdf02f651ef60c8d7c8ce022373e212ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:23 +0000

    unite talkback raw them correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
