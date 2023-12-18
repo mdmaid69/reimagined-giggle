0a50f0a9456f1f08cb44a065dda7f962dce9d04e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:39 +0000

    project fond black Qq free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
