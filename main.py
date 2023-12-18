d37d25f21a97e47787390edcb990cedc8e7cde8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:36 +0000

    frighten mean cross replace slight

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
