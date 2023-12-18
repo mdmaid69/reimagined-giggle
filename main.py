59c684bd9074aa1a91564d4a52549c8c55328cce
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:08 +0000

    ground dare quick life model

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
