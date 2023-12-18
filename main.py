6979a7a9763d23187a13448b64383a7f25c616ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:35 +0000

    possession maintain pause crime foot

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
