2a812ffafe2c3de9d9ad41f8fb83f4faecc031f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:34 +0000

    talkback trip otherwise moon still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
