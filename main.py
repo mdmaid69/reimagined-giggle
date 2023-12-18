e1e43320aa4e3d411a69f1e3c6a20ac6d80b5eeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:50:41 +0000

    shoulder nervous platform film hungry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
