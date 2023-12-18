907549040295e5a9b17aaba6cc96b0715d08ffe1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:27 +0000

    recover shout correct skirt full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
