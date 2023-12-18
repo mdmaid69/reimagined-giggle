dc46b320ebe49f721b0fdb5c0b389d58a64026de
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:57 +0000

    space little dress bang bit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
