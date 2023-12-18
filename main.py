79ac47598a42af859acbf0245341dec3e5948c7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:29 +0000

    acceptable limit Vv appeal formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
