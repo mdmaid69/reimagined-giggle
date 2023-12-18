353c22b09511c572a9ea12feb4abe6ed36886de6
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:22 +0000

    roof holiday not fierce tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
