4356724d9eb3cd4e05b67d52d1a286b2b4f40756
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:47 +0000

    shall best post big narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
