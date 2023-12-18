bc0549bd365ce75246200b58123f5e0ca4b51cee
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:16 +0000

    production path long everything OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
