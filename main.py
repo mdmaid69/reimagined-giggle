48b359c9816528e37ccaa0f1566871d4eeea7522
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:43 +0000

    smoke all journey Yy complain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def multiply_numbers(x, y):
+        return x * y
