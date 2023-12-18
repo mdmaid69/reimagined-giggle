227a6ca2c8209edc44fb1418acb4a5724ac36775
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:50 +0000

    possible answer noone steal ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
