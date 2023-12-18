4e9898d3086f587948a05e010e2b83d7bc2173d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:31 +0000

    fence storm duty minute world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
