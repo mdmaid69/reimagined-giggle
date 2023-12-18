b66209b5f6aa2fb9849b57d4de916099500efac4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:21 +0000

    considering absolutely remember loud care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import re
+print(re.match("h.*o", "hello world"))
