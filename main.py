ba32f9884dd1b20dd5961b15a5ed842d26a08a55
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:04 +0000

    object discovery plate water care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
