a8e49e75b68a186ccd10cbff411d44153e994d7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:48 +0000

    address so calm reply rough

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_max(numbers):
-        return max(numbers)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
