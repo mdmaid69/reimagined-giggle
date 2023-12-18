a6baf0d39a8685af1998df901bc35895a21d24be
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:52:15 +0000

    whistle try star petrol age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_exponential(x):
+        return math.exp(x)
