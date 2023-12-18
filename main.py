c23703ea6bdf1093d73f555340d2f5c8a6be0fa2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:11 +0000

    organization bite cough steam all

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
