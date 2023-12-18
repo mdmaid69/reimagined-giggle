383f52f8f5322a11945c22f2352f17d44bad939b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:29 +0000

    lead pretty strength minimum concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
