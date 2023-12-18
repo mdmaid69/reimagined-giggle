19e4c8e1a5c158bd21397cd446fc94230477b200
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:03 +0000

    judge light principle along runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
