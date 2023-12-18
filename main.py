30cbff7fe011e8deaa49efeaa03a8fdaf2281298
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:53 +0000

    fix honest comeover shop weigh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_queue():
+        return collections.deque()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
