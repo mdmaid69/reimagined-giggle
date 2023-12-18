2218c45da99ced06ce98e114b5de384feee59726
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:30 +0000

    shall indeed foreign knock direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
