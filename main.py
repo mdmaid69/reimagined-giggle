26b41c4372767d26588bb8e9f3c3e5fb98f280f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:13 +0000

    forever trust violent hurry soft

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_array_to_list(array):
+        return array.tolist()
