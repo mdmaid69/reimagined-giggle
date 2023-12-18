3340a8f736b1230f200389a180817dcfabfbe89f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:56:49 +0000

    already transparent lesson call influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-print(math.pi)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
