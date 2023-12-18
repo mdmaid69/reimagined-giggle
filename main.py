4c453845192298e360e82f8433b719f3beb7b5d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:12 +0000

    behind letter again fork neat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+def log_message(message):
+        logging.info(message)
