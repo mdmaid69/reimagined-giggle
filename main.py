b706e39749cf9624477b50dc5010ddef702fcf91
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:03 +0000

    hour bet contrast square name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def subtract_numbers(x, y):
+        return x - y
