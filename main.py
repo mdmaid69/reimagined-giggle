457ff77b04973618070d739e0fe2fc25e466a601
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:57 +0000

    attitude ball laugh advantage sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
