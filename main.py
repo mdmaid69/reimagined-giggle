b70e0ec86d3b717d1a69fa9828a9b7e284e98eaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:32 +0000

    direct excited independent neck sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
