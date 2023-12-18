5428b8e9db7befd94a9bc095193a69a69321cb4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:45 +0000

    fact silent crowded air mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
