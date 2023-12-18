e0e9948b4b9f46afe041c924ec8f488ba1d415d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:41 +0000

    give wire water black group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
