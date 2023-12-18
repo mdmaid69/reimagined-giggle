4d7c8d06543d0ca23b2a2cc0aeb1d251df9cd746
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:08:15 +0000

    rich breathe gentle sink pocket

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-print([x**2 for x in range(10)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+print(os.getcwd())
