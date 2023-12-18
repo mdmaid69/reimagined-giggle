0fb5efb3fcf6fe2dd11e75367d03ac9c0b0118f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:58 +0000

    experiment 565end either motion trust

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 import urllib.request
 def download_file(url, filename):
         urllib.request.urlretrieve(url, filename)
