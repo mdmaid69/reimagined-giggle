45a24adf414f8facb29d07eaabe619f259405447
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:41 +0000

    dinner cry beat confuse lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
