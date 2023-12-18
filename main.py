6ad1db0a1a9a028e9233975c649bc6251cf6441d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:22:17 +0000

    fear contain cruelty Rr voice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
