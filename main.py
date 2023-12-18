d0a583b0d3f6dcdf11dd5a5f566baafe3b1fd90e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:51 +0000

    university chat treatment spread group

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
