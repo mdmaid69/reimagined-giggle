a5e9330e948250caea731858533bcc4d6edc2faa
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:59 +0000

    guess process favourite neither television

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
