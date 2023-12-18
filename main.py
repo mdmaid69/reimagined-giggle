4edfabcd4eb196f038cd4d53c7e02d0430e08fb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:43 +0000

    price fat letter pause increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
