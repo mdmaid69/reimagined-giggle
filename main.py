ef7c6cdf519382c786ac754d1f6fb75567d60ab4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:37:26 +0000

    cause in present always compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
