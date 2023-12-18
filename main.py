7c3721810c10c51a737fddabaad9e6721e962cb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:56 +0000

    pass keep modern repeat angry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def reverse_string(s):
+        return s[::-1]
