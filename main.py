c8bd36d2fa3c834df7ad0c978580ed4db50daaa1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:49 +0000

    suggest today wide action dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
