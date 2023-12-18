2f6e17fdcd9a34e1641d72ba3f7e72e22b8468a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:43 +0000

    comeoff occasional bear basic upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
