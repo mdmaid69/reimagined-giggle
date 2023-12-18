b4beb1058411fdf2a089fb080124d5af825b99c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:12 +0000

    telephone foot disagree design unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
