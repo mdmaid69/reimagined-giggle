9690fc11199112007becb6e5fefb0b47c2836ff2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:16:40 +0000

    recently maybe raise mark after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
