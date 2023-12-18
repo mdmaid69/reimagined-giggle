6cf1d25cfa8e8e314430176fba4e304cffe2e5fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:05 +0000

    usual fortunate unit disappointed fetch

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
