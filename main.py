cda25afd432e18a5b345d894bc6cb85f5abfae7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:29 +0000

    butter indeed used refer getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
