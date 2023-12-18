a062c6bab41de5068668bdbd7c1f53850586f5a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:36 +0000

    goon classroom violence left everyone

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
