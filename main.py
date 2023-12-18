d0a261db80d430844cc105c1537dc562d36443d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:39:47 +0000

    sweep touch paper Oo deal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_max(numbers):
-        return max(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def roll_die():
+        return random.randint(1, 6)
