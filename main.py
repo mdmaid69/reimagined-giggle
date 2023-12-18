59c3231867ae20f389f40a08ee3af337378cef5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:30 +0000

    bring language miss release share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_max(numbers):
+        return max(numbers)
