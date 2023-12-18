0cd6abf9524bb64a0ef4067c47bd2771942760c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:27 +0000

    tragedy suddenly understand gift use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
