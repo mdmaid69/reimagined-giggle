632a41189e1f1543c52b6071d6b3bc6a52e1cb46
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:23 +0000

    scream environment turnon flow memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
