0b440ed2bc9772256f81ed296149c48fba85d9f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:34:51 +0000

    fill visit characteristic pencil immediate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
