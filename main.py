6436902a0425a67a278d9e941f3f8890cacb25a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:15 +0000

    unpleasant move concern win Qq

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def square_number(x):
+        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
