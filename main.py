de430461486e9eeb809d13fbebdabe45f07bf130
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:32 +0000

    light hair weekend peaceful exist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
