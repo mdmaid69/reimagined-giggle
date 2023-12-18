410e9f8bd57050e749dbf39d111cbf60a7d6e9c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:35:19 +0000

    container survive like relax wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import platform
-def get_python_version():
-        return platform.python_version()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
