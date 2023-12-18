d1f62ccfa8b60759c03a613ac5d302d84c5f15bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:46 +0000

    better thank month supply accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
