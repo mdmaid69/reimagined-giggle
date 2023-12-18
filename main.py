fb3f5c555ff45262a9a8da184bc49dd3711e6e2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:01:30 +0000

    reply nearly comment permission yet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
