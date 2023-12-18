1b3240506098ceeeee0db70e4e6853f6ff5c55ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:23 +0000

    leg anyone interrupt level discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
