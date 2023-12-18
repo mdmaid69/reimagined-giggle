ec4d088320d71fbb8354db30383ab48fae18eae7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:02:45 +0000

    call work drop own tone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
