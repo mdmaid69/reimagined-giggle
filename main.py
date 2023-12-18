0299ff1a22f32eccfb5d764f287796c6cdb21bb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:17:04 +0000

    last call mother will them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
