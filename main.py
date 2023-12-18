cb31796aeaa1faf3ff20054a41002fb7342c0857
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:57 +0000

    turn except normal jump favourite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
