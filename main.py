03b6e3568a92fc0c0e6eaa7e8318cfd6c52e665f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:24 +0000

    wander crazy back remember off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_logarithm(base, x):
+        return math.log(x, base)
