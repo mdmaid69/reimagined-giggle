b6890072e9e3a07e61b107b79c916aacc58c152c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:40 +0000

    appearance central correct soon comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+for i in range(5):
+        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
