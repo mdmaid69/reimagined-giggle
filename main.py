83d6b304dafaa2b3ebdbd91e61ff7a125ab5d478
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:00 +0000

    describe perfect wide flat shut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
