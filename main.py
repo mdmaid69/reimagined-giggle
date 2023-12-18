52bf1f4345574044530779f220af8c4b2d5e640e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:35 +0000

    seat appearance rob sale separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
