11022c0e33deb626ceab34103d81a766d8c5a40e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:07:07 +0000

    contain today go sure mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_item(array, i):
+        return array[i]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
