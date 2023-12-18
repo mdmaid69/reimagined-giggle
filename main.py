8d719eeb52d26d0d089c51c78e7dfc0f4425d037
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:15:02 +0000

    harm dare lock agreement attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_odd(n):
+        return n % 2 != 0
