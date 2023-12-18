4fc5d96550514b1ec6cf0abd9487fd5bf72cdb42
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:02 +0000

    question fetch show then present

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
