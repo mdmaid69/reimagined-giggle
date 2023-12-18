28f5a381301b5a58e4a48fd3efeebbe86f2429f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:17:34 +0000

    effect improve left law cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sign(x):
+        return math.copysign(1, x)
