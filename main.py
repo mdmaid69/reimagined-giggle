a1784e4aac95abd9f5fcaf8e5fc536fa2f4e4a74
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:28 +0000

    steep noise process behaviour scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
