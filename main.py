1bc82fe0c8accd76a75e27da86f743189aa73423
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:51 +0000

    telephone fine lookback(on) recommend complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
