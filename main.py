4df6e5cf516bd2d7b89a37f5c88df634911acf44
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:37 +0000

    season recently disturb everything down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def is_odd(n):
+        return n % 2 != 0
