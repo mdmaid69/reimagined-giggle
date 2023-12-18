710fe37362a580dfecf8e0737b8c0db50bed8009
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:45 +0000

    memory risk next advice firm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
