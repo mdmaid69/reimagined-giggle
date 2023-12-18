7261e7e6ba3f276a5d9e01ba98a8ea3856bae18e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:19 +0000

    straight than wait swim food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
