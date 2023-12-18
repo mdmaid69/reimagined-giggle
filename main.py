78096619288f43a7a6f2ec75298481abc1ccbf26
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:17:44 +0000

    succeed thick probably spread wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_min(numbers):
+        return min(numbers)
