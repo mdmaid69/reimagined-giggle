836feb9d681c14cf145405721e4ce7485f8d400d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:26 +0000

    speak year word awake base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
