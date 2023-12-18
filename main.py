380ecd0884e5e6d9c8fddad7e44a2d747c0dd0cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:18:24 +0000

    turnoff thanks rescue term love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
