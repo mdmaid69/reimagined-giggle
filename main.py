c6b560154c40a84926192a69ac402a1772f3c339
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:40 +0000

    piece Ss danger close gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
