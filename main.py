15344d2231a9c7fb04e6b665a581a3c35f50da02
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:26:53 +0000

    tax concentration since project content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
