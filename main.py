66298d7bfd17e237345b6ed0fc4a8430455c16c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:52 +0000

    body command suit prefer sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
