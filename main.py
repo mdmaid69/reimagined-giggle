6918bb5fa9eaa6aced5aa654425aeaea5685affb
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:31:57 +0000

    mad prepare manner crowded carry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
