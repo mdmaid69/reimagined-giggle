fcccc91e696d0c9c35ba27535c17ce35379016c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:38 +0000

    weak passage brief final friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
