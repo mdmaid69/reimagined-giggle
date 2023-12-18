7f881bbcd028901f802dffc9c29d16a6bfdbea10
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:17:13 +0000

    flow open cover slip smile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_priority_queue():
+        return collections.deque()
