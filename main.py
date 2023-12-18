ab02c0ad2673e95144844e950e40f7d3acfd77d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:03 +0000

    teach wing responsible kind for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
