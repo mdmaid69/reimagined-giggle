f4ccadb09c65551b393fe5ba76436c4b565f772e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:06 +0000

    wealth dry but burst spare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+print(sum(range(10)))
