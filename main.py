9c872321cd55d7fadb8260c6c7aa4dca1c426a90
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:07:46 +0000

    properly wide admit conduct something

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+def exit_program():
+        sys.exit()
+text = "Hello, world!"
+print("Characters:", len(text))
