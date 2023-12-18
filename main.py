b6073d47a41e19eeda51d3f957b0d6977fbf21f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:45 +0000

    strong heart below spare contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
