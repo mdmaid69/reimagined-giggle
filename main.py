e709ae5a0305df7ead3a36b0e047c77c7ee41bc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:19 +0000

    spill glass takeaway attempt priority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
