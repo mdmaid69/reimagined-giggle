37db19365fc26a326c7ab4dd62505437e2e0f423
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:27 +0000

    cheque sister contribute plain blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
