a38e14fc7fa3db0586afbd9e6ca21349680c2764
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:38:04 +0000

    break reason lend pause fierce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
