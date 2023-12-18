476fe7d7ad353380605633ee56abbb5f30b78021
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:57 +0000

    day taste limit bringback unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(x, y):
+        return x + y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
