2fa0016c28252fb3071195a5999e5c7453491b0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:45 +0000

    roof lock official callback dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
