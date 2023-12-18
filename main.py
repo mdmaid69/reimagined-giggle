736dba17e2fd873ba09116cad10e56a939cdbae4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:07:58 +0000

    patience cost tap then apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_acceleration(speed, time):
+        return speed / time
