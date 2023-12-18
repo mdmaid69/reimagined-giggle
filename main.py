722a3ad0caec39bb5c593b70fa416959e116c2b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:15 +0000

    anybody hard shut backwards cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
