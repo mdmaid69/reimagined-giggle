e5b1090ce6d5bfa642a73a6f3293598ca6307698
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:31 +0000

    rarely often rob centre bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
