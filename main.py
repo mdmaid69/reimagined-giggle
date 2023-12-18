d1c750a1ce8791959da8aa0ca8c9d4c702f34235
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:25 +0000

    file show unit display good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
