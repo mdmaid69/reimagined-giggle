34f3e3b4efe5052f796efb6515e58fdb1d76f7b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:42:27 +0000

    just mine change lift ask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def convert_to_binary(n):
-        return bin(n)
+import math
+print(math.pi)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
