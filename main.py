5faba6bfd4c94d2e8d1d0a6c9fdd7b31ec6ad456
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:13 +0000

    past that age drive eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_counter():
-        return collections.Counter()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
