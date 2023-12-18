6f4a9449ba8ad4310fe332e4593ab20b11bd0970
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:23:33 +0000

    stretch earth coffee unite because

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import csv
-with open("some.csv", "w", newline="") as f:
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+        writer.writerows(data)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
