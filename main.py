4555c77a17cac4f7b29d6f4a2352e38317e436d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:26:18 +0000

    mind later comment game cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
