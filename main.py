e2e300655cf43232d86bfb74e91d0bd73855508b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:21 +0000

    getout worth 569enjoy depend common

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
