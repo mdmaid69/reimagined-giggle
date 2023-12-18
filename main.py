5c0e18179bcea6b714da04bfe3010ec6915c87d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:04 +0000

    finish improvement hardly Ww central

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def multiply_numbers(x, y):
+        return x * y
