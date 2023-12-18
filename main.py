9a37e8b99b7805488923a3aafbe2e7f415f95782
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:07 +0000

    conclusion shout hour children behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
