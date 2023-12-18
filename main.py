b821ac663da3ca97230123d2591c19d69974b490
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:45 +0000

    568energy even lookback(on) near dangerous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
