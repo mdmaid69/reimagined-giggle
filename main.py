14bb92122ec52c265648571dc196116f2b11344f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:26:10 +0000

    include attractive dull shock lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
