2fdd5d4f618c0ba9db37af6821a3891cf2b39c2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:21 +0000

    night slice accommodation sort charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
