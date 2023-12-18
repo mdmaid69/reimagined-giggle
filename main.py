cfae2bd27cc51a7ae90882fd1457899f6c0628ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:17 +0000

    suit extremely sport pair culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
