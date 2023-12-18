5918a2db05ab7736b0fa84365c04987c82ed9eb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:30:50 +0000

    different comfortable check murder on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_error_function(x):
+        return math.erf(x)
