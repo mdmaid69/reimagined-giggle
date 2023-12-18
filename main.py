b16245749581549443fbc89c648dc0858f44cadc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:53 +0000

    post something city equal tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
