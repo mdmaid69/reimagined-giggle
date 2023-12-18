a9bb198f66f9fc792385519b63a6504ad0839ec0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:48:28 +0000

    shopping reduce holdout their slightly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
