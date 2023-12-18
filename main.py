2a252871bd7c55910336493170792cd3e360fbde
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:25:03 +0000

    force nurse production justice position

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
