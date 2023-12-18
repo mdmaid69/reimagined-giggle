a7a60dd6a8fb3cd40e8fb90e96ced5638ca75339
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:18 +0000

    warn interfere race cool plain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
