8a2ca21b24376bde6bb6e0cdb56f5e15ced6ed61
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:46 +0000

    protection me transparent use protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
