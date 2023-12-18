c226de3d7a70dec2553b42bbf372ecbf810429bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:14 +0000

    now sharp takeover friend traffic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
