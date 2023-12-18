8b2a63ce1598f9d253c42711e27168e3054debae
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:47 +0000

    choice seem climb owe before

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
