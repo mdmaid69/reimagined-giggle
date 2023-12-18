bdedf05c1130988cef2d4bd7b37d7d7ef8a4da8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:56 +0000

    car luck generous late concentration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
