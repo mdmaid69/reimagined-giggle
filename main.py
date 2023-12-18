98d45a19d438bd2be10bbb70ed4d45498e8be684
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:50:16 +0000

    pass minimum spill level concerned

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
