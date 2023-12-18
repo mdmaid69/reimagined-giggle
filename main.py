f71afed5059325b3a4bcd3fd1aeb94d597a2667e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:36 +0000

    communicate unkind once murder hope

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
