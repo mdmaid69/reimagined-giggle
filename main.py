652342a03fd2915717ee36f70a8ad2a0b23b0e05
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:27 +0000

    measure storm clue hurry convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
