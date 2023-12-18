786ac7cf23836e892544b3aea9f057f486bb1bea
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:21 +0000

    smart breakinto splash scheme workout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
