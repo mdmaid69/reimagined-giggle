4bb734a2ebba450a368292d1e038bbf4b842dd47
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:13 +0000

    season thin responsible search juice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
