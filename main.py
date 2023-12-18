658fbcf7605f43e6bde11fce433e619d5980c489
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:32 +0000

    see hardly dance exact instant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+name = "Python"
+print("Hello,", name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
