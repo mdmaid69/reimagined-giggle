b0f9c74438b153be27dcb8ee23c21678c8dec121
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:38 +0000

    brave thumb table solve concentration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
