6b7c4b1bba8527ab177987f5ab22a527b61861a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:13 +0000

    than mistake case comeback afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import time
+print(time.time())
