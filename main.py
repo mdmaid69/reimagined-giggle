b46317451c3b600c2ca8dc5f97f49304c963b636
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:20:38 +0000

    meeting but deal along earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
