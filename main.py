6b6a92e9040f2a07423ab090ba37fca65c1cb7c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:09 +0000

    set to will hand murder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
