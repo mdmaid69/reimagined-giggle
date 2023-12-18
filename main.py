a96c7c247e190a0f154dcc1f2d4e78daa4e5e0b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:57 +0000

    naturally spot bury discovery flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
