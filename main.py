b59c5ab4c85f58a962416927cb10ffc1928b1c22
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:39:37 +0000

    minor tough rich talkoutof pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
