10700add5a702885655bca24a4c2cc8eb9b7fb73
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:21 +0000

    mistake suck like range behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
