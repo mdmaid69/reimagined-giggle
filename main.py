0d410948e1554232a1ac340fc6ef2cdd664aedc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:25 +0000

    use fortunate keepaway now somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
