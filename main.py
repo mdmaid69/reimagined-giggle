9f3b3d4f791ea3836c632919c141da2b2f05a7ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:18 +0000

    figure action return something plant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
