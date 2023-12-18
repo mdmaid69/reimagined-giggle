9faa4e7b348b6dc89e740432d25063aaea20be40
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:03 +0000

    Pp activity content disappear beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
