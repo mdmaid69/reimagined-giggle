ffe6d42fc55a313d0b152824daf5a00f255468c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:03:15 +0000

    supply cover later course accept

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
