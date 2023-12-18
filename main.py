66e1817487014811f4c48a16e24ad9fec33951c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:59 +0000

    live absolutely unlucky me gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
