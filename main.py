a61325fb3a57c7e263ee57ad9013984f345896ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:36 +0000

    thought smell 565end coffee grow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
