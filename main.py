b4a2ee5c938605f72e72a1da6922e1f036940676
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:12 +0000

    deal false this we niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def cube_number(x):
+        return x**3
