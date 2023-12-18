98a6531532913d0fec5b0e05a239d1e430164d09
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:44 +0000

    easy middle confused giveup complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
