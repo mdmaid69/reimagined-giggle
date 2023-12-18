26ce34fff90adfbb441b9fd1e49ae84ff8687eca
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:02:37 +0000

    oil shake moment complete to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
