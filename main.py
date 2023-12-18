e5a6f58133eadd0bbabebbdfc5411da63e1e6e29
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:57:23 +0000

    backwards worst use bunch Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
