a5f636928f5bfcac298a6f107b94c62790a0741c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:40 +0000

    vote ahead wonder deliberate organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def divide_numbers(x, y):
-        return x / y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
