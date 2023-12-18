48f55cbf38acf8747f32c9e16a16f4d69d1f504b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:12 +0000

    support as refuse low talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
