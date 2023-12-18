a165e5ac1c932efef139d5b22758936da20688a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:40 +0000

    nurse pen measure change into

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_power(work, time):
+        return work / time
