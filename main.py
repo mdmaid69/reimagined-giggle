bd5cfdf52c2965c37d0db3fb01269202bb98ebf6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:32 +0000

    thinkthrough shade suckme free excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
