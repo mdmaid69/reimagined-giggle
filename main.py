8483e67406404c5d7fe28103607daac860e33a58
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:28:00 +0000

    awake congratulate brief water protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
