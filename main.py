33285ef7635f2904cdb929e3ad5a6da0bdb1cec8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:36 +0000

    cope split popular responsibility cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
