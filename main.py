192d8e795992d1285242558a1d5d6f7f57215bfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:55 +0000

    contents send everywhere fight contrast

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
