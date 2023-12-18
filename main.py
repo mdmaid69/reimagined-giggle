bc51a43c363226d0a039a38887076aebe4d4edda
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:11 +0000

    correction hardly Zz rush benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def greet(name):
-        print(f"Hello, {name}!")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
