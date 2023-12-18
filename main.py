f081b9c69fefee34bc889a9fd082e43e0049ec9b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:08 +0000

    branch shake image after shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def subtract_numbers(x, y):
+        return x - y
