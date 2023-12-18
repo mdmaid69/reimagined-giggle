5109a40e1e12a1581bfeeb9f5b44e64a22f3cb42
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:14 +0000

    inform too against protest example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def is_palindrome(s):
+        return s == s[::-1]
