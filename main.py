282d07a124f779468c7f8af217c2d48db3071aaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:27 +0000

    pressure organize attach almost battery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
