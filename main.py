14684a330cc3c64498db93d6589315b63bbc9aeb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:08 +0000

    calmly excellent reach land emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
