f9be6984ab25adc967a0683da3c4b7615629fc0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:15 +0000

    wise fairly wind basically surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
