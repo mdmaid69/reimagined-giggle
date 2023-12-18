0b3b1ee9e12cea4b329523f0de92e93fdc7639fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:58 +0000

    stretch measure freedom well when

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_max(lst):
-        return max(lst)
+def add_numbers(a, b):
+        return a + b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
