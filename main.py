7eaa7f7f5d79bb3033fe47f8525ac0249217808b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:32 +0000

    crowd bath inside allow artificial

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def cube_number(x):
-        return x**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
