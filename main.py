78c5e9cb412a7ade503137eaa835b16fd28fbe13
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:53 +0000

    swim comeround fresh yes spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_typecode(array):
-        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
