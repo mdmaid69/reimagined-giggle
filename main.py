08aa9eb90bee953f3d99caa6a9f359aafa350676
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:29 +0000

    568energy chat worth drive base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
