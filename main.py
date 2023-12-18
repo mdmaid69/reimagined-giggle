3b480535f13277655144f7aadde25f4b7cf2742e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:10 +0000

    Dd grade blue limit there

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
