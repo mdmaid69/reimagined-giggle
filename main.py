fd15ae6b73056832b1711c3d66cfa34cc5f1fa38
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:00 +0000

    relief Uu almost way anybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
