7f1bc6eff7e7bdeefbde47f0cb3e26faf1704ab2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:17:38 +0000

    absolutely operation win impressed so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def divide_numbers(x, y):
+        return x / y
