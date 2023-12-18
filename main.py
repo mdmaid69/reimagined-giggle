fec4520cf015c4856ec5e1de61b01decada75d62
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:49 +0000

    repeat she upset description sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def sort_list(lst):
+        return sorted(lst)
