aae14c20378ab4d059ec0d8b84e8a6dbdd55fd62
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:56:46 +0000

    smoke accordingto up spill better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
