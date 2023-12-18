a4379e13da903002aeb1dff01a32e023004595f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:33 +0000

    cover convincing show personal then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
