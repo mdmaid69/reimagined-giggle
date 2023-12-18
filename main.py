eae9e8d15ac3af48513b607534129c845b2d0f78
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:11 +0000

    brother no almost damp nurse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
