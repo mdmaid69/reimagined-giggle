fc21422ce6af531830a64529cf92971b1623a7a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:08 +0000

    future before rest patient second

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_volume(length, width, height):
+        return length * width * height
