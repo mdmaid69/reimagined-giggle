2923ec8ae0e510f99ddb6ea20f559304592c09b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:51 +0000

    danger do exam Gg match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
