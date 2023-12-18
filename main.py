722defff22c458612d6bf3139c81a8adb5201b88
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:10:02 +0000

    reply pipe sudden scrape niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def count_elements(lst):
-        return len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
