49c92953d65f6c45a8485414c888e72e63ff3ac2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:24 +0000

    aim more wander justice slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_floor(x):
+        return math.floor(x)
