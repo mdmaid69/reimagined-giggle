367db947f120eb678ad571f2ac1bd04f37539f7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:26:09 +0000

    carry opponent typical food habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
