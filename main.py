9c1f8dd05225c9d71d188c7eef0f948b85a433d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:37 +0000

    dance nature region well leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
