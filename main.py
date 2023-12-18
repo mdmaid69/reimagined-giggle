ab587ad5697a64819f95f808b019794a4e1c2010
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:54:21 +0000

    than practice correction period let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def greet(name):
+        print(f"Hello, {name}!")
