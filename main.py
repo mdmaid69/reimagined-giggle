5cbd7280c8557b2a1b5efe2995d4d0feb4a2d0c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:05 +0000

    medium touch relax upwards view

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
