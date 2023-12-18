1bb1bc41b14f8bbab084a3c8b1c4ece713fe7bbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:41 +0000

    bin trap lead by throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
