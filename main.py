f32f996ef62e5eeff4f97f74fbf868eb2f54d94d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:32 +0000

    limit patient advanced vote disgusting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
