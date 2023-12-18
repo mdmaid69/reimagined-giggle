1536f8d4704a65961b3715e31032858fb48f719e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:29 +0000

    unfair manage involve generally medium

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def add_numbers(x, y):
-        return x + y
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
