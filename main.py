a98ceb636dc00b13ecd9589215666afe13b04128
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:22 +0000

    average quantity guess plan wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
