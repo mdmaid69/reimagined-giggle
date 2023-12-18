1af0871d88acbdc38f1168690db479f7be114efd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:58:29 +0000

    goup surface shadow mistake sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
