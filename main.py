539559c287060af4f3512e60733e82f7760ae260
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:43:25 +0000

    float close foot surprising skill

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
