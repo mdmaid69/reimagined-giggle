114176abb388b85ffff64aae2008656d1bc5a5d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:26 +0000

    time worried doubt goodbye whichever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+from collections import Counter
+print(Counter("hello world"))
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
