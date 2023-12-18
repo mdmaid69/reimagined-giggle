b52fa3b51d68f9dbf0ec195dfb8a3b1b82e9f139
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:46 +0000

    lead whom favour reduce range

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
