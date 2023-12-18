0d2c5604e1c1fb9bd175299ac4e32a672aa95af8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:34 +0000

    always soil deaf used confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
