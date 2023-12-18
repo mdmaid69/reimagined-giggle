0d5bcfeb5f2ea523afb8311cc0ca2d720e90d8e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:58:18 +0000

    arrangement post have shopping bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
