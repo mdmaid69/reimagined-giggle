4da047f6bc768168a6c15375016d0425358aa98e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:00:49 +0000

    near serious beside will reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
