5c8de8531f55d9d28f6eac37e11617c9bd2f5d1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:02 +0000

    ask earn recovery before run

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tensorflow as tf
-print(tf.__version__)
+import datetime
+print(datetime.datetime.now())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
