115225646fb8df7105ff91e69663a907e7e3ba66
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:51:47 +0000

    wise satisfied star march lonely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_priority_queue():
+        return collections.deque()
