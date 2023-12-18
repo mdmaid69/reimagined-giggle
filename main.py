f95d5f5150e9583dd43269f1b68a29f1c4f08002
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:12 +0000

    disappear emotion doubt her board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def square_number(x):
-        return x**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
