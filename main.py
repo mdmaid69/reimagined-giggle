281b372aa604e67f432851bebb5f86dae01d335e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:38 +0000

    popular bin anywayanyhow successful comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
