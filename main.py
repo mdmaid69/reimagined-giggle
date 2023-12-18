0b0237f037c37079548b79eef3ed60bd526f95df
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:17 +0000

    opposite hollow fit 566end prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
