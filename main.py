e7839fc5170861e7a85d578379c7e2625bec752e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:39 +0000

    so match always stick joke

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
