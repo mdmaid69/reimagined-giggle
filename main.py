cf61f49c57b2839a97d716d1d2d04c3d3f4e242c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:07 +0000

    workupto respect makeinto grand swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
