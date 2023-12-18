f22976805e2dcbd18f1efec1b7a1b088eba4623f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:02:27 +0000

    provide tip many fast software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
