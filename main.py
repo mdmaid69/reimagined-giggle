a917f92c3563ccd11b0f0ecda27234dbb7c2ff76
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:42:00 +0000

    pay cost burst risk on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
