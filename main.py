151dcda8187c9c4b47cde2253ced0cced86ff9a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:58 +0000

    move makeinto question turnout combination

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def square_number(x):
+        return x**2
