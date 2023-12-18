f8fe8cf9b90efe58c9c155722df798b638f72f82
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:06 +0000

    think anyone nowhere mark freedom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
+  def convert_to_hex(n):
+        return hex(n)
