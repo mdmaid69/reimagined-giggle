4d320d553051513a07e8907de98426971c6896d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:39 +0000

    here well oil table unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
