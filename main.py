fa4a073e60b041d5cec545fab7ce6b3c8b96fe16
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:39 +0000

    goodbye spot famous against half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
