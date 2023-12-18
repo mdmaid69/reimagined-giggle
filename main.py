3c67699a651a478ed4dbfe7e113434f45bc38fb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:57 +0000

    train refer board ignore fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
