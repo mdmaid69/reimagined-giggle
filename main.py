5dca47517199931e70561b4a59e369c50fc31488
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:12 +0000

    limit confusing clearly wall whichever

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
