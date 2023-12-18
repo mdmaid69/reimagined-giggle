ff13a1690ee83f7ba16582ba211c96a62cb0d69b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:25 +0000

    welcome single abroad sometimes kind

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
