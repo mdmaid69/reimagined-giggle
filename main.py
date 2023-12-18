a3eca84bf18664f63b99122a220262db737dc13a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:50 +0000

    nurse patient manage lesson identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+print(math.pi)
