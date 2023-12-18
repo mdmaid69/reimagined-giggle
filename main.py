24f56cfdb6e8a7ca6e37ae4370a8104d0cad3558
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:19 +0000

    activity swim wherever hard payment

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_work(force, distance):
+        return force * distance
