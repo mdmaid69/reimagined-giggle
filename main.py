3e9569c246bf2c1fef1d500f2b6856e38260acea
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:26:38 +0000

    tap exciting repair rest weather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
