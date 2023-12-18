0a670dc6fa38e2de53a7aa4d74cdf8d90d5bdc34
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:36 +0000

    look after increase quarter loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
