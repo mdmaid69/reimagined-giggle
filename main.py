b9e531b8fce558491af04aca9dfd8a7fbaf35b11
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:24 +0000

    pick town radio our brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_memoryview(array):
+        return memoryview(array)
