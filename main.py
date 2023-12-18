c6a71a7d625cd041c98227f0e33264ec71c585e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:47 +0000

    hurry any path suddenly blind

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
