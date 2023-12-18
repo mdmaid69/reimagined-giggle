ff8e2ae1ce7b3077410c4f4cdf24f91a38cf4540
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:11:11 +0000

    lucky water recovery small suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
