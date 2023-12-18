e6fe5011ea498f03609b59dba659dcea2d512d2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:24:46 +0000

    off rob beyond flat actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_item(array, i):
-        return array[i]
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
