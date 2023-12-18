4e4aeddb579e619124614908c730b9da3379d69b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:02 +0000

    future afterwards business muscle drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def greet(name):
+        print(f"Hello, {name}!")
