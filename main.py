49fe6fa8138cc6e6e92eef10973b6e65113f076b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:53 +0000

    collect read movement team issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
