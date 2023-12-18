ddca622eb4d169cb5c98cfda3e613e96b36da1ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:14 +0000

    wet passenger design represent most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
