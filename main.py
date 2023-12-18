0809fed891fa32285bc39bfed85ca8b175849814
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:53 +0000

    while arrive no over extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
