6e55847547eb8d5722e16e0f8f1e0a6f8ec2a3cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:02 +0000

    pretty brush giveout weekend of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def is_even(n):
+        return n % 2 == 0
