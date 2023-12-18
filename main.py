42d6f1ebd9f1ed089b8921a6c303eaaa2c431ec9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:35 +0000

    whistle with relative strength relative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
