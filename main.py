7a22d2b2658f614f0405e23063216121f30de334
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:03 +0000

    improvement our punish train waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
