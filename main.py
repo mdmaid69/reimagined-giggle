64b0d802effe567f573a73459ab7f9cb91e08eda
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:33 +0000

    dish frighten free all average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
