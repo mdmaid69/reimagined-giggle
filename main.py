b9653e7c1b9c0ef3b85a0d0df874201de7d2a0f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:14:49 +0000

    near attempt make chase convenient

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def reverse_list(lst):
+        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
