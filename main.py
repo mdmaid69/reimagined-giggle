5eafb3d09971255b89c2c4aecc125c3b34807ed9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:03 +0000

    refuse both breakdown reduce culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
