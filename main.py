f49bdcdff66ede716cc2a5323ecab41b5a8bda41
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:36 +0000

    convinced image throughout could Mm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
