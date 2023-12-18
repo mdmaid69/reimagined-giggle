acc2918a19389c98f3ff211fcf4b351c35dd0c3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:45 +0000

    plant spite 569enjoy better storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def create_directory(dir_name):
         os.makedirs(dir_name, exist_ok=True)
