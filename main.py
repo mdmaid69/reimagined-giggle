0fad95e55efb821eb0219c1bd0acad484b94bac5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:27 +0000

    special summer whisper that crisis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+print(os.getcwd())
+import math
+def calculate_sine(x):
+        return math.sin(x)
