8c1fc17c71b5926929bc0778c9f6ea82085d5754
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:41:27 +0000

    dig takeoff already previous speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
