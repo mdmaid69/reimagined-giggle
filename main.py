5a7befceec7eeba531588bb5041394ef26c79c0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:51 +0000

    from Gg demonstrate rest accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
