106a7ac926cbb92fec474f2c75243961f732d1b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:35 +0000

    damage national water copy colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
