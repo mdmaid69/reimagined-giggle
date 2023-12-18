7c0ee26d375f24fc265b5ab448932da97505c05d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:05 +0000

    hear boil motion suckme cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
