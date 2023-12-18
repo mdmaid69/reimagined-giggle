afe6e329e6a6c57f0af0c0b0361360d16487783e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:51:05 +0000

    win impatient skin sensible rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
