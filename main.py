25cd5d2a6201b12527c45468613fe568f19d8708
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:59 +0000

    answer read proper getoff unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
