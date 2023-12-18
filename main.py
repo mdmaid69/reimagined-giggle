30aab2aca9d7c310bbeaac3405a1d54b7006b3e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:16 +0000

    loose flat total weekend roll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
