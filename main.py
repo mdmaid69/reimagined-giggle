0bfe8c9b91fefd9c8181f331cac26f0e35fd98d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:09 +0000

    ours talkinto pride worth stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
