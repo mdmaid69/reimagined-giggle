76b05c908133dfda0f2f194db59124f232d005d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:57:50 +0000

    least standard steal own close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
