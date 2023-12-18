d281f2af5080796f8de2a9412702e13292849138
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:15 +0000

    suck across amount danger colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
