69a28e3c1501f26a0d7037503a6819b42babf4a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:37 +0000

    congratulations foot Dd consult outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
