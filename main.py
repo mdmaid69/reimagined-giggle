51073dcba69d3507d45190df6e858480277e298a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:37 +0000

    summer tie breakthrough principle trick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print(sum(range(10)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
