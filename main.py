c9f58b980ebe5f4e0284cd254c11b89414ce4258
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:31 +0000

    far create embarrassed answer in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
