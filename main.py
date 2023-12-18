8b13bcba669f503141cfa88acd37102ebb21e206
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:18 +0000

    store fire sorry reach likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
