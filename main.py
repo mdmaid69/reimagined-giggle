cf670736aba788654f484b5592627b129f50fbd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:46 +0000

    allow generally wave mine nonsense

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
