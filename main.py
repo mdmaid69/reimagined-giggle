dbd4746649efed25b222350bce40daed50b01948
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:15:34 +0000

    recently soak recently share surprised

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
