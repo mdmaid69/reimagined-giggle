3f0574023598ca9c505de23841d9718da56ec0df
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:01 +0000

    up notice vanish slight frequent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
