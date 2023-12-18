e7ecffac1e0b15427bbee1d5a9cfdb6ee309b30f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:29 +0000

    objection lift contrast standard cry

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
