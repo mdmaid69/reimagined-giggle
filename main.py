254a805d7f943d43a9d44e697d3624594d12afc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:59 +0000

    tidy take side oil whole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
