8e98e8f5a4caa36ac954f5240a2780acd457dedf
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:18:58 +0000

    criticize join whisper list waste

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
