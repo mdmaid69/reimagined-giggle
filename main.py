1a749481988048af35218a0ffbff7c34c7cae9fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:06:52 +0000

    store port kill plastic clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
