b98d0773338e5b03a7950fa8823694014a96351c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:06 +0000

    who task capital nurse type

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_acceleration(speed, time):
-        return speed / time
+  def count_elements(lst):
+        return len(lst)
+def add_numbers(a, b):
+        return a + b
