dace7b9f503966c8e3dcd0f5b71c9036c4deb5df
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:52 +0000

    contribute joke truth progress steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
