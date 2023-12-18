1070bf84815f20c41a8e195acd2ce87d911f9e1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:29 +0000

    fashion present lose model hers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
