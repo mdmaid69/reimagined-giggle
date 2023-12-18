b3fad030f9c41e87f6b2c752bdf2c18309441780
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:27:41 +0000

    female far failure distance ashamed

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
