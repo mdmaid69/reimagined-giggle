a2203b10e43dc5178f5bcbd4f18b663257568b84
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:07 +0000

    wrap accidentally meaning bend plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
