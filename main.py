f18823b7b40b7b1f5793a60979dcb4c773f13710
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:28 +0000

    earn lift phone talk unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
