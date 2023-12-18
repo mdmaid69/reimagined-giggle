7bb6cc86c22e4929cab5151ae7f91058ee67de4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:11 +0000

    work clean complete weekend small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
