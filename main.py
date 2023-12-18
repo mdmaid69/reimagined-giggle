103530f6aee9fd9c8b9968ee0fae66f4c97ba8ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:26:54 +0000

    neither damage say block unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
