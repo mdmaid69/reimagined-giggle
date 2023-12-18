8a3088d1e70431fbcd72f75ea9c7e1dad98196ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:23 +0000

    new depend turn love conduct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
