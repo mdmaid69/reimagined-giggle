bc1e46ecfda9d32ee57f97a7a9cde66327330cf5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:48 +0000

    dear this feature complicated burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
