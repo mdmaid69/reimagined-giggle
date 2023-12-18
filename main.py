a1cc64b0840f209e119c1b9deb2fbe74cb6af201
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:41 +0000

    surprised hide matter band hospital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+print(json.dumps({"name": "John", "age": 30}))
