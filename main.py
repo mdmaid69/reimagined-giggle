8ab6d336e1a7a5d2f402f7626cf7ae26c2c88ae8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:02 +0000

    somehow zero produce loss hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def reverse_array(array):
+        array.reverse()
