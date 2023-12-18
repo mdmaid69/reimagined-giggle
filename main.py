f81573400730670be4f41abd8dee423f840e542f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:30 +0000

    mind file aim sing raw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
