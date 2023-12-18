febc77769a64db4f5cb3027770a6664a0232c97e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:26:34 +0000

    surprise present invite check near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_list_from_array(array):
+        return array.tolist()
