b9c2231d394c67c6503c0cb20c1cf19a1d13efdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:00 +0000

    achievement straight manner sorry claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
