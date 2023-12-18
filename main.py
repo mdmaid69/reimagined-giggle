b17d1004a25e05998deed2fedb44687432a05d96
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:33 +0000

    square from radio see explain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
