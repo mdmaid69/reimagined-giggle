5d3223f20706fe03daf3f04b4c7a01596fbdc764
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:09 +0000

    meet practical takeaway street further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_odd(n):
+        return n % 2 != 0
