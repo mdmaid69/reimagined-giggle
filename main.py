669d231443032427f0fe0b7611a7146ddfd6a820
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:05 +0000

    raise situation central season fault

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_speed(distance, time):
+        return distance / time
