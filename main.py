6c98084e02db53131a4d490e0b3afb117ccee794
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:08:01 +0000

    home anyone after comment seriously

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
