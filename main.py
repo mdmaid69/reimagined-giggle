963bdd6f06cc7a06f8fd4e1e4286b07486444cc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:17:08 +0000

    blame print thumb strike adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
