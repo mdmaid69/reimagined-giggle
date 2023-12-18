4685543b3d7ea6cedf59ad739065b524493a3f03
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:45 +0000

    sharp any pour equal arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
