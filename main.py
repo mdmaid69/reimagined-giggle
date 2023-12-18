4f0142277bfff33094abf7a3a642e3e7f78a0a15
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:05 +0000

    person risk laugh last effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
