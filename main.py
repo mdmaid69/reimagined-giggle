39c63976905f9bc3021a2ae0d6d9e80151efe34e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:40 +0000

    comeout trap contact gap issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+print([x**2 for x in range(10)])
