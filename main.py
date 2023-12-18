073b94a2d304de9fbb4b2a974c763068e2223f77
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:22 +0000

    unlikely trick distant message treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
