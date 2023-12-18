fc7b23261c7bd1ea2ed6a62811e0c040a575bd71
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:23 +0000

    delight past basically agreement giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
