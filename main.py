d86cdb9b36f4a9a8101622dd21323b146f131dd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:22 +0000

    build low set direct cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
