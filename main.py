c9d9afbcc53877fc8e808cab3f75cadd1e407516
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:42 +0000

    face act occasional open would

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
