82d4211ead90ce9e69ff8ac9d1c95690e5ab6419
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:15 +0000

    out explain takeoff greeting keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
