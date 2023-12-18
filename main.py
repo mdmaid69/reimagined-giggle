9bc499f4af7af810e4c606bdf34d018a998d4bbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:50 +0000

    tip call today danger stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
