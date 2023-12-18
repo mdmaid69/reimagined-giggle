f3060532e485cfbbd7fe845918606ff6df57f9a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:17 +0000

    bound object wake harm mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
