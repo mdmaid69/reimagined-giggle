1c484d29e19b7944f57eebc37c03e10bb5ae6fb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:49 +0000

    manage practise eat place clean

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
