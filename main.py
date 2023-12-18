82f73c638e0bb83e34c4b2d569fc5003a1d0f047
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:41:26 +0000

    go represent mixture offer music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_max(lst):
+        return max(lst)
