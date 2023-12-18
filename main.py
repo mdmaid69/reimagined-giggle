d07e5c8b73cd0b9a1bc0aedb1a55cca68e5fc465
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:14:07 +0000

    number powerful hear rare worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
