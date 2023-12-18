e1b39acea1389d699d456410b40e33a05b7af6a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:23 +0000

    pleasure embarrassed makeinto getup scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_force(mass, acceleration):
+        return mass * acceleration
