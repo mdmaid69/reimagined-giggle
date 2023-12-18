b321f83a66568d506273b61aa4d405d1554b624f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:06 +0000

    relax community mean bury cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
