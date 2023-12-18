1206b677e663f14254bc9d4e8a40cd936b6a08d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:29:19 +0000

    mine stamp broken refuse band

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
