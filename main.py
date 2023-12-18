b1ac50a70d09abd3a2fb31077a41b73790504f96
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:45 +0000

    compare gooff whenever scare door

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def convert_to_octal(n):
-        return oct(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
