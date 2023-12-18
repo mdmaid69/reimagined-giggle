e8d82e8077ef6ff949bd079edc195f8d05e38243
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:29 +0000

    step anyone clean across himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+x = 10
+y = 20
+print("Sum:", x + y)
+  def convert_to_binary(n):
+        return bin(n)
