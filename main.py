3ceb9ed7ed0900ff9e0c83b2520b0815fd25b158
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:14 +0000

    by pause during pencil improvement

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+print(sys.version)
