38a6c86d9d9556c2fc94aebadba253c2ad19b923
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:07 +0000

    relax crazy call apply unusual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
