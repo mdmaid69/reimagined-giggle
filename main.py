0382a6d0eb6328a81d14dc50ca85924b6e373a5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:41 +0000

    or entrance pot news attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
