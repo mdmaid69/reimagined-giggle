c2eac44d97bae5961dcd5e36c85f4d7eb61dc61e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:06 +0000

    preparation runover national base goodbye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
