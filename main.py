3f4a4df34b3a816c8a26d15062e2ecd9ff624e5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:25 +0000

    call so challenge soft difficulty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
