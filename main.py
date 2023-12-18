5f5479908527ebc39b82067ba13196425d8a2d87
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:28:37 +0000

    rest distant before for strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-print(time.time())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
