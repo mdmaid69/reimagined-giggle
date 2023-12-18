116b54ff0a00c3555ca2ab36bdd54a66f0c69b7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:12 +0000

    anyone no charge pattern hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
