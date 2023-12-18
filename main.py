cbffb3c881a9a12ebdf96810c65dabb57a128f01
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:03:35 +0000

    over slide notice sort here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
