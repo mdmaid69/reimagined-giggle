6503cf98ea9d8f1ba631d4188dacf9efb014bf7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:44:23 +0000

    shake anyone especially direct if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_floor(x):
+        return math.floor(x)
+import collections
+def create_priority_queue():
+        return collections.deque()
