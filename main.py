a80d90c0703760fdeb6506db62eb41f30291c390
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:49 +0000

    twist danger drive announce hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
