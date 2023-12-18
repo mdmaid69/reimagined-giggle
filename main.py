24a2953cf64bc3a2b6604230c7fae283eaf26fde
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:01 +0000

    certain immediately mind comfort style

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
