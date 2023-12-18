8c8d65099b0c378c0ce4c51305f21f025d3a75f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:12:17 +0000

    Ii pleased cut balance judge

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-print(math.pi)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
