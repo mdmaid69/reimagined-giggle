9c63895a571ce1178d628db300c7c6891e73f9d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:53:59 +0000

    oil practice suckme emphasis wait

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
