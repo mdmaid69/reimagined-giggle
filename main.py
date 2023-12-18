74ddcd3d93b0607146b808a9624b64f9e2101bce
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:22:41 +0000

    bye yourself dangerous unlucky seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def square_number(x):
-        return x**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
