dbe082e89b042b2d01c99bac4d31881593b988ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:15 +0000

    reject mail breakup side translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
