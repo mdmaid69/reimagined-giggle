31966162784281048a1d4645c229bebe680af3b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:10 +0000

    float study length town exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
