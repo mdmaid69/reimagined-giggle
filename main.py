d895285d5a1777a63909605c166a40388c2bd48e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:54 +0000

    coffee introduce at move acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
