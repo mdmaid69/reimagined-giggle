63dd872ff366817093b032a2919ff520fef8352d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:15 +0000

    catch after least noise month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
