e8cf56dfbb9f1aede5b7af9f4c542a297cf3fa80
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:25 +0000

    score sign me already eat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
