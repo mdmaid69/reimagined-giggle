82c0f80f77bca00a8daecc38aed91cbae54150f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:37 +0000

    interrupt bye odd pleasure giveout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
