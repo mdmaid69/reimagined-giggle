458e60800c7a0928faf119bd1d6f40316f612c41
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:01:46 +0000

    challenge use complaint plain crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
