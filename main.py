c1a3c7d241d6d979ed5f9c3aad7e0a6ff2ac08ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:32:04 +0000

    whisper wall joke majority tooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
