dbbbeb3d268fe5b02182908d19443dda571af5c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:31:26 +0000

    somebody march anxious alone downstairs

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
