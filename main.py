0f9e9e843bae04a47a30a8dd5a910e7358d55871
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:04 +0000

    remove complaint tear various need

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
