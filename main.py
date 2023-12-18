a425705c49d67fc9fcf583bff91cc9601fe81501
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:14 +0000

    bound swallow sweep car backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
