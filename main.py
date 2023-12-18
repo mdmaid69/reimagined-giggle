9c2a8a55744a4ce7d4434488c41ef92668c21cc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:01 +0000

    somewhere settle story roof none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
