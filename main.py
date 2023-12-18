624b1c3421f9f26893d0066dd9f1eefc99caaaf8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:44 +0000

    juice success our organization nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
