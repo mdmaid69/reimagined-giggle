c30fcd211560fa355902e52daafb3d51dea62346
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:02 +0000

    short nervous branch pull workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
