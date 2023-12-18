5793195a6eccca730d8ad8afc188b8e636e79c42
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:32 +0000

    might herself clue important wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
