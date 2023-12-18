ff18b1dff979c720f18edbf31fa17d19f85a8421
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:32:58 +0000

    common period OKorokay round chin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
