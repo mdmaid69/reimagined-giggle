8593f50ddc34b785f119c8adc05cb3935031aee8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:34 +0000

    minimum evening capital refuse used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
