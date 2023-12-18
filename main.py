c6ade8c8e4a86a70a91e49d9850fcb7f9ad59c07
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:37 +0000

    wave beneath decide determined distant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+def find_min(lst):
+        return min(lst)
