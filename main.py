131c278019b1baec231e1fc3d0fe6e7635e7e988
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:07 +0000

    record steam power lookafter under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_bool(array):
+        return bool(array)
