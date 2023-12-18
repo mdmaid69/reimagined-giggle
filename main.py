7b6fa61d3e1ae2d936eba67bf3b7e274726b67ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:16 +0000

    narrow read argument considering indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
