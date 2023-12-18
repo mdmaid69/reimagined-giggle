f3c5d42953334f6814dec2be02c9400085280666
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:34 +0000

    one world smell want cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
