c23e11181d33d3824f20ce2240f1a018ea9d7fef
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:56 +0000

    prove fear high left thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
