e056e4ec2ea26b50d379f4f3ce9bd04cf282eb46
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:07 +0000

    show although murder town hardly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
