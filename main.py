6f2fc688e8b72c5f0c7b8535613f65d26478ba4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:50:50 +0000

    somebody study opportunity plant freedom

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+print(json.dumps({"name": "John", "age": 30}))
