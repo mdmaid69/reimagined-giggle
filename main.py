d7ad5da225f3c96b7cbf740b9b324ec3ec1cd042
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:14 +0000

    prefer cost low delight people

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
