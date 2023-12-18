139d129abcfe7ae097bc2d9be9a43fd08ffd2fa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:55 +0000

    description permission visit but more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
