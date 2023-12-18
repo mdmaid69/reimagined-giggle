76193f5e31ae025b9d1c8228f7de084a8ad8c491
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:48:00 +0000

    extremely breakinto call prevent shout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
