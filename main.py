69a96ef1ba088786db5ddac551012b639561bec7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:02 +0000

    chance study child young top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
