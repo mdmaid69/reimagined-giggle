cd6af9a5eb58cf5c3c6e6dea0e32f797cf07366c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:16 +0000

    once attach spin doctor search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
