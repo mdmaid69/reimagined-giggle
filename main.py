5df5fef536c83ab65310104f58b35a5d3d3fe2c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:05:53 +0000

    learn week brain before leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import numpy as np
+print(np.array([1, 2, 3]))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
