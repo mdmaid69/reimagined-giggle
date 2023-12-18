5ab3391e536b7a9848eb36968d6409dbdb9e335b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:56:56 +0000

    admire drop shoulder scale computer

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
