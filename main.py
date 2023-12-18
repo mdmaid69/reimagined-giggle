441f318fe4a2189b0f6d97504819748abf682257
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:06 +0000

    immediately same gooff rescue identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
