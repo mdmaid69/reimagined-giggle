8e162fc95451380b3aef9db9b4d9cba34ea95497
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:39:10 +0000

    consult involve means change invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
