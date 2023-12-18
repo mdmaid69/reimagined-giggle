2b554a45119ce2b912844b99748cbb90707fc104
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:50 +0000

    community Rr go post sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_string(s):
+        return s[::-1]
