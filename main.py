1ff377044755fe134114490be33d7b720c848a8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:00 +0000

    move her harm foot sting

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+def get_today_date():
+        return datetime.date.today()
