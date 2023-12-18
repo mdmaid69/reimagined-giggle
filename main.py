6db2e32eb5d9df93173893860c591edd5bd17a40
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:56 +0000

    getoff receive ever upset communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
