025f145dddf91195649a59c6a30fcc7f1fc93c76
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:47 +0000

    except press callback basic sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
