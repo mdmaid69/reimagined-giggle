a12f00186d7c0d9152230fcb530af2ac01883a67
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:03:55 +0000

    choose tap convinced clever protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def is_even(n):
-        return n % 2 == 0
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
