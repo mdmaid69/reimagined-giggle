337116f3708113fc8158eca2ef322835cec8949d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:22 +0000

    cry point load money yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
