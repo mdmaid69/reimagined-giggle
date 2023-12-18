b892cdf0435462ccd2fa1376418a6c673af489f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:29 +0000

    trust desperate block figure chief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_power(work, time):
-        return work / time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
