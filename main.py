0a99101564fd575a2ee4ce9c1a129f67347972f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:10 +0000

    calloff press language ear feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
