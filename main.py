03c2f7f08ad532fb4ad3fe4e83eb10a54e72a630
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:39:43 +0000

    most impatient stamp holdon land

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
