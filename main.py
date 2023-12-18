331b18e6de07b5958897810faa1d9a78cc27c9c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:50 +0000

    cruel mainly tradition push spray

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
