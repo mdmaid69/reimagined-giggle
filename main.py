58a9973b34de460ffe1e20b406c8235be24822c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:40 +0000

    whom go aware thorough claim

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
