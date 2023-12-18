e5195b1a53282b428a73c0ac0db58d3725b5ad33
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:23 +0000

    suddenly gradually brave account line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
