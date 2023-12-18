1a20269ae05e97b597724f270c41bee9aaef023b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:16 +0000

    runout pinch defend once breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
