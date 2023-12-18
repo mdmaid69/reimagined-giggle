654859ec32ef433711fb4a5cb236138c0d337734
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:27 +0000

    maintain forgive cut so on

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_length(array):
+        return len(array)
