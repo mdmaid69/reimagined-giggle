a4679ab46255f18fe711659aab85015a08370cdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:15:14 +0000

    man shoot reply lost main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
