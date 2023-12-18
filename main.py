c1f1ad01cf7ad7d33b7c985b72cf5729bae9568f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:11:33 +0000

    deliberately open bury spare fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
