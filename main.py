d4a76e2d647fc989439330f5a14f525a5d1c0c13
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:44:40 +0000

    stretch correct bringforward sky comment

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
