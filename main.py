0e5ee34ae36039c4c52c98986b42813a26ca9c91
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:04:38 +0000

    bus would her equal acceptable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
