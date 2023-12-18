518f80d84497571c979fe98947e7e236830640d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:31 +0000

    hand reliable full understand belong

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def exit_program():
+        sys.exit()
