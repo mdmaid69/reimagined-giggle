f4b24402ad306a4f3631df671dbc6d28ae53fb87
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:37 +0000

    traffic instant approval away godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
