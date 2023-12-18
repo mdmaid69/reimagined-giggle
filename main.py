80868fbe8bc40c4e831fa29d2e5938339c34823f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:01 +0000

    grow along joke disappointed Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
