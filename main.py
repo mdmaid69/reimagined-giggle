7b9729053d825a380f0f8d827518e475eca480c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:38:39 +0000

    towards free leg various Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
