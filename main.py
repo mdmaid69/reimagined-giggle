a8b3e2fc39bee4eecb2fbbbebef602fb831e3b85
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:59 +0000

    serve Cc least keen legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
