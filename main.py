d63f04454b51e7aa8f85e4f747202e1ace1d3061
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:29 +0000

    man mass cry put juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
