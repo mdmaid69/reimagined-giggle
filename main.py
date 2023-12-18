9a235a67e51c28d2de0fc14a40d0c58d9607cfc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:23 +0000

    hand fairly split big thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
