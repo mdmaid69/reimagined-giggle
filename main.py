ff511fe4dda591268cbe0bf75943ad63e3e1dce9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:48 +0000

    vision instant curious basic answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
