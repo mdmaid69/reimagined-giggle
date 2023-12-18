0caa4c2eaa9aa2538b04003c9deed58c9ad1316d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:03 +0000

    Kk world bus refer explain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+def calculate_power(work, time):
+        return work / time
