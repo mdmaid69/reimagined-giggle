c92353d12ac130e3433051e2708c6a769abce6ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:33 +0000

    generally that complaint more whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
