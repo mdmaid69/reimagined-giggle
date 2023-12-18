a94a4767cd62f43f6501c09672fbb7bf17b990f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:28:25 +0000

    together travel trick play wrong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
