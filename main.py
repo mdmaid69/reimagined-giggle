326e647b78f88f9a948a2f6b80aa8c73551c6265
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:37 +0000

    distance fresh flavour increase cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import array
-def get_bytes_from_array(array):
+def get_string_from_array(array):
         return array.tobytes()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
