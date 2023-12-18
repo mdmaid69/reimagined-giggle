7d68a0818d64f68f8cbd2f28542a0bb0c518b515
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:05:32 +0000

    fly wide only should characteristic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
