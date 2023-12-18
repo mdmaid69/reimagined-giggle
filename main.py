b700e3a9113a6845b0b59485ab9915cce0fa5de5
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:52 +0000

    unable help anywhere last impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
