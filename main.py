1eb3df57d372d3674053a7e4c8d9f1cd335b1376
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:11 +0000

    holdout rush demonstrate welcome practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
