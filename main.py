120b6c9da40792712430e3467f40093592803816
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:00:21 +0000

    real that comeback promise century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
