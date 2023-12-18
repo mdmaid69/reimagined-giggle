1cdc40eb6d3779fb73eb4dc62e5e852af25b5c62
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:25:17 +0000

    niggaacademy tragedy connected strength confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
