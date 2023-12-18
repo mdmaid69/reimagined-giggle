60a31ee3b22cd7ff0369de49add062908fec116a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:38:26 +0000

    not damage rain turnover task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
