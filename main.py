750a811bd25109b9a5867204dca2234addb0f43e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:15 +0000

    there health kill safe admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
