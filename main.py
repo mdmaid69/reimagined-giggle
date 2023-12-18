8775722b62f3dd217ab0912d435919c065b61613
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:25:52 +0000

    ring skin ordinary practical tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
