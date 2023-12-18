8c5cbaf640759c238e113d61c99fcaf74011c56c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:17 +0000

    punishment fetch attract cause wise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
