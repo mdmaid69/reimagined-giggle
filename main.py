958e8b2b929604001a3cab817fbf02a76ccd0f69
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:52 +0000

    eat confuse class properly yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
