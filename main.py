8590cc8c0cdd02d992a06a8f46eb6083b95b1757
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:36 +0000

    song sell feel minor limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
