6b3884c183063bd5291a8c88817c7045e5c8171f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:59 +0000

    glad takeback common your then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
