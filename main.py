92095f187b4ac303322d26779bcef68389e083c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:17:19 +0000

    whether empty somewhere catch head

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
