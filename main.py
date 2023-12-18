e638f6f2cbe8bfef7dbc6d6c856f6a2e4fbd4709
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:51 +0000

    feeling loud continue finally runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
