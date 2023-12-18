e924c0e2566fbe81afa03aa8bf87882b2bf4052e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:24 +0000

    undo most climb upwards constantly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
