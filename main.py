70db2c3f2862c2555d2fa142e6bc77174b5ccb24
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:29 +0000

    may paper pot modern ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
