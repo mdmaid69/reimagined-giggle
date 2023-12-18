ae4ef31de51e6860a4334287d6630b8e4abe97cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:55 +0000

    breath defeat draw shout recently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_even(n):
+        return n % 2 == 0
