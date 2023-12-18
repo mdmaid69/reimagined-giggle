9d0a6d5b37d0db0bbd650704bd668eac1b173828
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:02 +0000

    report plenty suggestion free bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
