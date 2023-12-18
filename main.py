c8a22e011e6f4618f239bb7b62fe283a8174c9cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:07 +0000

    damage twist wipe brief Mm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def add_numbers(a, b):
-        return a + b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
