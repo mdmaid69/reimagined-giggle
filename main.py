617d0c94b1314d587c7d38e28b22d33f70975700
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:31 +0000

    group whistle stupid Qq before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
