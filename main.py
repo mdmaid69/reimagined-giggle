44c2daaa897197dff67ee0ec194c87beedb291bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:34:11 +0000

    which accordingto wind try over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
