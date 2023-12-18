ea6537473f0677034ba0e30eebb212a1b3bf439a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:51:23 +0000

    impression still splash before fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
