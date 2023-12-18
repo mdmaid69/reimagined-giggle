ece43dc952f48cabcb94617d5a6e4e186158879c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:22 +0000

    prove depend 179bite unless as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
