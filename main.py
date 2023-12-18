87dbfb70bded26b83e4f2be71a92cbf8a387584f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:38 +0000

    exam satisfied fast skirt Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_bool(array):
+        return bool(array)
