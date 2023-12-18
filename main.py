c331ac6aec5e218d2bcc55f043968cb62d0f865b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:53:25 +0000

    measure until case road usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
