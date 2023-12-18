829ae3214478916631b8195526448cb525379fff
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:38 +0000

    cry relief touch my hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
