2474e69c72d014dbe53d5c25231a03b2125950ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:15 +0000

    hair hungry immediate guilty substance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import re
+print(re.match("h.*o", "hello world"))
+def convert_to_hex(n):
+        return hex(n)
