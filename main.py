d62dee4153336a9cf19efc3de1e2230de01472e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:02 +0000

    food offence belief disappointed chance

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
