8e04977b0cd614a6d00fe10334440cc58b71aad5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:51:51 +0000

    excellent level heat shoot laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
