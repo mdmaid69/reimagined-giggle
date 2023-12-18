50948fe35b430f3998a228af1c6a6b6dbaf291c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:34:56 +0000

    desperate bye suddenly culture curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
