d50e72792d31f183ec358e2622bb92f0595ae51e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:03 +0000

    defend avoid stick education back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
