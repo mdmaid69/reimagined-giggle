9d57aee6f04e29d518064294fd4d1eff5122f7df
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:47 +0000

    usually meeting party welcome responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_density(mass, volume):
+        return mass / volume
