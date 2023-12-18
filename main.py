dabf93e762c7aef6f31b61ae9bcf2790a2133906
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:16 +0000

    shade giveback ticket careless side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
