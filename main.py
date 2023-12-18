243f6fa333ba7c2017fb756852b6a54e0891c4b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:43 +0000

    clothes fold Pp police excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
