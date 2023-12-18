37a5baea7d15a00e8bbd0433db44ea8a32f8babd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:50:22 +0000

    negative cope answer plenty dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
