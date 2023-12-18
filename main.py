394f6e5ada605438d4fa9d5def2258260423d80e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:35 +0000

    land cruel after interruption soon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
