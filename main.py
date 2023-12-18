06f5def7822af127bf443e4578176008d06bb282
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:21:57 +0000

    there how release practice dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
