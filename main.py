3681f2b971cc2410c64a540019033c1d5356f2da
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:56 +0000

    vanish pour forward near beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
