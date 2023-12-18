a91eabcd7bcb6c75c84528c19997ea6a5eb02e27
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:09 +0000

    pair history dress joke fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
