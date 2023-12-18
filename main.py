66ef75bad43d59228f53f7c0af56b0c19a073818
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:51:18 +0000

    steal only food list fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
