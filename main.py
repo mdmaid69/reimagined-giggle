78d3cf3272e8a8e791d25ce11dd9b265650736e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:27 +0000

    rule especially than natural slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
