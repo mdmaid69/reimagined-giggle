6e2bdd4482975dcc0e06ffb738564ac1b34ecf5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:02:20 +0000

    storm feeling break so behind

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+print([x**2 for x in range(10)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
