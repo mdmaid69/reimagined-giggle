503ad0ba83ec691275fe76357f647b44ab9e554e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:34 +0000

    cry pump turnoff accidental emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
