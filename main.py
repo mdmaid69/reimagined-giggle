d9f1df0b9ae76ef85eb0464ca164909ef5723b8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:18 +0000

    top moment calmly concerning judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
