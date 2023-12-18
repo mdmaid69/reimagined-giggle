1c817d13eba9994de81ced7e8205ed7cb601b111
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:04 +0000

    quantity ability mood notice far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
