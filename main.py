3db1610a9597bc66542d4c66c1265e3288d4711d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:06 +0000

    throw cold stay mess active

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def greet(name):
+        print(f"Hello, {name}!")
