f954e41fd7533785b41adee36bd63da9aa822d6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:18 +0000

    tooth sleep murder thick greeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
