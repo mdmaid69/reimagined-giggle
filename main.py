e240f1b2cf5edade99d2621f1cbc97479f404b23
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:12 +0000

    dream kick list relation capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
