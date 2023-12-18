20f493a3be2de0275b03b2df9a7f8ac88bc2b06e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:19:54 +0000

    single behind share but press

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
