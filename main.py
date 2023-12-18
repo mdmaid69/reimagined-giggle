5fdf8fad3244a797ce23f99b0e9fe3e2bc257933
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:43 +0000

    connection live head rather slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_octal(n):
+        return oct(n)
