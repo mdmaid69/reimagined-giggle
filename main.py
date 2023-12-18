46c11ac7ab64aad046fc606c843b48c842871561
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:22 +0000

    present perfect smoke no tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
