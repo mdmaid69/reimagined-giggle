13661a5d1d8a9d6b8fb6826b665df5ea06e42ac8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:18 +0000

    conversation conduct responsible thought food

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
