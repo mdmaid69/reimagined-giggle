c839c36cbdab7c76d37e4039256cce0157869058
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:55 +0000

    stay treatment mark powerful but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
