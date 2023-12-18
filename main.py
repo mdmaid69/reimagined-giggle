111085475e4004fbeb3e2ac9a6f3583e4839d702
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:36 +0000

    before close Dd slight type

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
