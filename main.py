fc6506a6a1e411039f6c8718b24552e175cd2c76
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:07 +0000

    glass doubt tough guess smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
