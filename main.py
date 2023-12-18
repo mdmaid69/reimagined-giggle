c159883f2ec91119c84541e79c1b5c514a8246e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:39 +0000

    mean fork fat holdout majority

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+sentence = "Hello, world!"
 from collections import Counter
-print(Counter("hello world"))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+print("Word frequencies:", Counter(sentence.split()))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
