744214f3c84a582b1e1a1ff6c133d194662faf1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:53 +0000

    get niggaacademy furniture wealth fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
