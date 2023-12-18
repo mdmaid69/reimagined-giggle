6ee601d0a056d1b320bdf434dc7c5dcf5a55c73c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:47 +0000

    lookaround they obtain loud row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item_count(array, item):
+        return array.count(item)
