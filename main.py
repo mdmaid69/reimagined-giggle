dd5e3d696a943a2c2f25bc5ed0add13494349da9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:37 +0000

    by breakin address which city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def find_max(numbers):
+        return max(numbers)
