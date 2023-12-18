47231e83e6d6782e40619885e4fa803537ae9d12
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:57 +0000

    sit congratulations relationship skirt thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
