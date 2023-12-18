62f6ea3848a3b76ff0643fb3e4e19e3635f2b46f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:03 +0000

    fold but successful record charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_index(array, item):
+        return array.index(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
