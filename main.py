415b54aa5e8f5fdb0dd5caaed5eaa87dc6a6437e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:05:09 +0000

    refer sort keepaway can edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
