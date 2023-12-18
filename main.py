516cfa03089ddb05ec49da5a15074011ba8605b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:09 +0000

    lie tape press fashion plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
