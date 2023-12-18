e4e5ca7c90bb010c7f304f83f1b121c25759860d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:46 +0000

    you base sometimes clean hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def subtract_numbers(x, y):
+        return x - y
