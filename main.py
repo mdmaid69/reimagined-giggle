8d72120409475acc9209182a78db29d03fcc6dbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:51 +0000

    advice trap cold check bath

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import sys
+  def get_python_version():
+        return sys.version
