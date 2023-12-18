5a22ff258e3f903c849fbc577537b6dc63712a8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:35 +0000

    why couple face spring minority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import collections
-def create_priority_queue():
+def create_queue():
         return collections.deque()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
