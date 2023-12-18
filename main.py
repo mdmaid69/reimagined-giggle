c29d5a3026c13ccabef8286348110268e43df130
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:54 +0000

    spring official community contact path

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
