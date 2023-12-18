bdd2a72bd6a6684d5638c48e34bcc88496d7709e
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:32 +0000

    bother vote attitude luck 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
