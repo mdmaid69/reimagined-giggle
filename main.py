9789d67647fd63c0e5b60a8bfdf9ae6edeecd3ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:50 +0000

    formal feature perfect prevent pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
