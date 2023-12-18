2ae94e11f1cca94654a5153a7fec50e26fd0e221
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:08 +0000

    pocket slice sort height skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
