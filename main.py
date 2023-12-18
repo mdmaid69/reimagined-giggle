1b90113a4e7170e33d4fc7ee98789aaf417185fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:08 +0000

    night disappear chief Zz university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cosine(x):
+        return math.cos(x)
