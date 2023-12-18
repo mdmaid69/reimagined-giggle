2b5b4a6dc05d49b8070407b5c6166d9b3d302613
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:53:29 +0000

    light error contact callup sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
