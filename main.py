cf55210e8a1c495eab5d0553912cee96f953ee5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:06:27 +0000

    wipe twist window notice situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
