56f74ecf0a8c96eb0180d7a7fc817fe0e46c4db9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:15 +0000

    beyond finish land crazy hate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+print(sum(range(10)))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
