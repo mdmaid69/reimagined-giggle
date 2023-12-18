74161f01a7ed4e77dbdec1a06806c70372d9b058
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:03 +0000

    trip into fire dress usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
