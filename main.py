c222ca6b9bcd88dfcdc3e94bf54d01cfba94b4e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:18 +0000

    centre aware delight steady makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
