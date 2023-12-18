1a220c70ac0c4e32a7cdd3e24e91c515edd7437a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:01 +0000

    bored building water ignore welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
