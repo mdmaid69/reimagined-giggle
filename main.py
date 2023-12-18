e40846f6442d50f3265f73351be1add1f668d4b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:35:13 +0000

    form less hold narrow law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
