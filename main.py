346d7f61a101fb1a6ae8ff28c705d4998a6d9d69
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:33:10 +0000

    trouble agreement understand punishment dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_acceleration(speed, time):
+        return speed / time
