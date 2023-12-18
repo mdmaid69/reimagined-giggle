3ef3c4ec97ce8538427ffb8a57a5b8732478c6aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:43 +0000

    left hers question case giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
