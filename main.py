3defbfa77deca3278afa3669628b7a72f6a072c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:50 +0000

    strong though crisis strict entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
