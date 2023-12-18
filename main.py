5720ef84e3b6f2480c1f9d28df2c2c535c5e0afb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:08 +0000

    improve form succeed wrong without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_tangent(x):
+        return math.tan(x)
