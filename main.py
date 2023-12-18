91027865de172d0dfb210b48717606e0ee7b6524
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:32 +0000

    dish near absolutely Yy story

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
