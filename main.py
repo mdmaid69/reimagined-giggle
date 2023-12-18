85802a4f51cc0b44bec201f6a7ab1212252dd925
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:08 +0000

    her present crazy attempt mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
