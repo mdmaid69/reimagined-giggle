05d8e57ca723b1ff73ad0e3d3d1b2365989600f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:51:09 +0000

    explain scared trip afford under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
