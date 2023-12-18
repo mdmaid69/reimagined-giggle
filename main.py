02c8ca8a1561db9e68de14608df88c1926715c3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:17 +0000

    send nice cut mark along

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
