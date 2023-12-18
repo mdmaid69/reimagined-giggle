fdc19e30a74ce07b3c32ef7d01d5909dbe744639
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:36 +0000

    distance avoid could whisper escape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
