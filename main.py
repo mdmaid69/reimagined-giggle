5007afc1b5bb166fab215a729a2728187c766786
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:06:12 +0000

    young half important cheap more

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
