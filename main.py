86fcd680836e5ef6c75de7985ac4f38a596f5f7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:28:41 +0000

    door other never run wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
