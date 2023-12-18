86f969ef71dba73b561ffe9aa4a5ea58b167362f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:16:41 +0000

    nervous side rescue drive delicate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import re
+print(re.match("h.*o", "hello world"))
