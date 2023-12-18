cacd070fc7eebfe44cb74415a6f8e038342c2a1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:41 +0000

    government luck rest shopping waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def cube_number(x):
-        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
