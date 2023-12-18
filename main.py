1715fba1e8ffae9878dfc4689de2523e65302955
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:40 +0000

    prize weigh fact comefrom suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
