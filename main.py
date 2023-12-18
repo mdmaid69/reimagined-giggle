7d5783e3ff8fc02990a00262e8625b1d87462b04
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:12 +0000

    572enter answer community Oo terrible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
