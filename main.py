f63a885207eaf1a89d2ff66d1ee0d7125d48b7c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:52 +0000

    order basically anybody good light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
