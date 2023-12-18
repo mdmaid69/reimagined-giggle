f1bc4e1571acc9e48a47d543e3c7c69f5c588dc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:10:21 +0000

    butter chin beginning stupid any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
