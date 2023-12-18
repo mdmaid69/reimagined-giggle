e69637621e7d732a38ec53fcc319626a77857fad
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:45 +0000

    unite silent fork choose tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
