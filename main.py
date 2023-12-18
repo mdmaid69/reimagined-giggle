1d9d25b68fa971d4842af59b85f370521a287266
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:02:02 +0000

    storm bush flat rule bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
