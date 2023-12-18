3664af53c887d42c570a944fd8f31b71b02c6c0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:46:31 +0000

    perhaps string condition sleep mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.ctime()
