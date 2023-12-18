f9438af5e9412cf03ba908957dd0bc73be8bd81b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:25:50 +0000

    unite achievement turnon pause be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
