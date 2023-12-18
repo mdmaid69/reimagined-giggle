6cc0b489c4d6a59940a3bc11185b79d0021804a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:26 +0000

    offer proud height goup hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
