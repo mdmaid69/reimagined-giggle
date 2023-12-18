aad174e550c510ba489ca5d24b1fe22ac95790b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:54 +0000

    never example idea him drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
