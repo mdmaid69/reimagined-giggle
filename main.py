b645b26bbaaa2f2e1ed514fe8b9262603f37e4ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:01:43 +0000

    strike interfere confidence onto stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
