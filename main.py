ab2a513f431856ccd8c8e33829a6eecc03e20f57
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:29 +0000

    inquire treatment geton share shine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
