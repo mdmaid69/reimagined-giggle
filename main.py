adb5b2e0541402d3345dc3cfc7e58eb7773c9790
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:17:33 +0000

    subject build hole find unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
