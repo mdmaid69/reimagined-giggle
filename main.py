870fa892a9a256eba5449122256da3e498a863e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:07 +0000

    jump ill always deserve professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
