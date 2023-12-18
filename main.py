acbedb8d5637c510235696be30d8ed37ba2a4f59
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:58:41 +0000

    quickly look of list foreign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
