47aa541a3936cce82bbe5e014fcd60dc2ca27503
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:25:50 +0000

    noone proof guess down quiet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
