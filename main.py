91967b6193e8f313f95ce7b9b6e4b72bd5408960
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:13:54 +0000

    total crash house coat place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
