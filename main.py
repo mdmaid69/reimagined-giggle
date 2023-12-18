dc7cd8ff9180d2591e66e914e9ff28f07a4cd7f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:16:17 +0000

    hard paper table free store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
