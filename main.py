e347c38740a99d03e10e2f17ede40059d4b4a60c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:17 +0000

    emotion left sorry backwards unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
