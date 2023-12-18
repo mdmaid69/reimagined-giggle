e362c79fbc88e2f95d69542af8586c835c2a57c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:32 +0000

    manage interested politics afternoon small

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
