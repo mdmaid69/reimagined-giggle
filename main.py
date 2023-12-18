5b70296fb4a516b3afe0f6fc5d3673f53597db03
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:59 +0000

    debt pour mixture giveaway keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
