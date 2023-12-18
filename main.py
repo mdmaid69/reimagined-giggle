2411ed62fb23a4b9229208efc52aa34242547f6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:16 +0000

    uncomfortable remember wind excellent eye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
