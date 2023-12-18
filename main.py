2208be351997b1655e6d5e2b50ff1f1495a80afa
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:07 +0000

    vision exactly shall next project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
