8bc7864d243ea22d8678d98f98ae034bfed7f0f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:27:35 +0000

    clearly confident turn quarter somewhere

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+print(sum(range(10)))
