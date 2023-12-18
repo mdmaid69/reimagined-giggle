0ed2c8a54921805ad813fdc7c2de3bddb6548443
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:17 +0000

    breakfast performance himself speech ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
