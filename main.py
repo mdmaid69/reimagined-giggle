c3a6c97d63a97f8f8b4ec240508d375f6fffef58
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:39:08 +0000

    equal pot clothes shout loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
