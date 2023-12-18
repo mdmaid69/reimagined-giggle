85f19953901f3cc473f0ff17ea695b2af4c0539d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:34 +0000

    patient talk recommend female produce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
