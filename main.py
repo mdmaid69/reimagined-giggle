acbb6a672ce4fb89bfe7df95e305fbbe6aa47306
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:25 +0000

    story murder fairly practise emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
