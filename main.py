8d24c5f9ceb53a2eabc8bfce8b4f066e076e2558
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:55:31 +0000

    shut bunch Kk effort niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
