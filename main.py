18fe604ff6c4bf86ceeb59f7c7b614fb52aed9f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:37:57 +0000

    tradition doubt suffer turnback meeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
