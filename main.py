4433803cb78780d03a596441deb0b4c7d2d971ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:23 +0000

    test increase practice long proper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
