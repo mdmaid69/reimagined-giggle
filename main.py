36a4d4f34b13bab04b1fc61356c3ae65ec470fef
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:57:01 +0000

    wing win work must her

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def count_elements(lst):
-        return len(lst)
+def multiply_numbers(x, y):
+        return x * y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
