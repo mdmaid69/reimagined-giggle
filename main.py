ab63b1d1409bd1c950a2f5e24e3310e6d7144dba
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:57 +0000

    opinion succeed careful approval in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_power(work, time):
+        return work / time
