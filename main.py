0e721d1e7913dbc139d048f4fb8cc6f3034e75b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:26 +0000

    pronounce equal wind black prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item(array, i):
+        return array[i]
