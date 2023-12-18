c4dca5c023f30b155dde24afd3543d5dc1af4a4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:52 +0000

    nature report influence battery change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
