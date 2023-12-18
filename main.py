8326c7659644e1c34580c7273c4e4a62357b64a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:16:50 +0000

    watch doubt butter mind suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
