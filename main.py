a3330824c966c2a77ff3ca3e43dffbdc09c3921c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:02 +0000

    claim start piece refer look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
