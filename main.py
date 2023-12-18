dd94b8de9ff87a94aef8fc72cdd81c3142140e53
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:43:56 +0000

    then direct keep that square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_counter():
+        return collections.Counter()
