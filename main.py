5582f2cdf14cc3da905d05947c714f9db141417f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:19 +0000

    post gather doctor turnback calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
