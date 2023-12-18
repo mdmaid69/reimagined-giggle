968c3c0b7578433e9048e3bf8e20b7ddfd76d780
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:07:34 +0000

    question guilty remark recently precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
