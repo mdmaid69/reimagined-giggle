2b5f1d52a4bcea8a3cc8fe68e0901cf07e3ee0dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:40 +0000

    except description radio bread practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
