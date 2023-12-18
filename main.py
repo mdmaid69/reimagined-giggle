23dc89ebc6bf8010152162ae6e8dda9e8312053e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:18 +0000

    she become inform store wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def count_elements(lst):
-        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
