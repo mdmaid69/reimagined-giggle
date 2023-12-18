07d4fd054d750e5d43bb19a11a0567f2385e9873
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:24 +0000

    Ee uncertain organization produce add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
