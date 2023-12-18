de25b060adcf4e3a1ccb358d0dfd179a8c3df671
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:28 +0000

    unhappy car spoil centre collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
