b5f3f728dda8f1b3718c75901874871e3a35eefe
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:01:25 +0000

    almost platform breath Dd Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
