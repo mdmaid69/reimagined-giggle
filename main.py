5860ae2202610f9c4f7f729c576eb9e67b903b5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:33 +0000

    maybe tune miss plan pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
