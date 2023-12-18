152f4e63ba9ff743e19e05c34292eb49dcb214fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:01:31 +0000

    place failure blue flight properly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def square_number(x):
-        return x**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
