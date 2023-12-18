df336f86aa64069bb56a46ea1f233c56dc585ff8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:08 +0000

    cross regional train lock lookaround

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
