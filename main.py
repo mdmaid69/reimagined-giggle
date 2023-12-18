05dc121370f8313afef161560ba7c71faa7a6523
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:42:24 +0000

    surprising classroom left suit produce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
