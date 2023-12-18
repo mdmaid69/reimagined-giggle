84b9a13fc0540e4e16b54ce6f0fd80c0e3a4c813
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:23 +0000

    skin somewhere push above drop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Characters:", len(text))
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
