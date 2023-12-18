a4a477614ddd43e1c515455003c78e186df48d15
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:11 +0000

    Ss attempt past hard sticky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
