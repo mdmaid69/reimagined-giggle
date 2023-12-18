89d38c36942d67895bf054e18fd13c14f19f0fe8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:06 +0000

    centre mine bringup new calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
