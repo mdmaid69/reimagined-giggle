2d74621d23e94071cfe3feb26e10033f6354fb05
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:18:05 +0000

    read giveout ready effort legal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def subtract_numbers(x, y):
+        return x - y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
