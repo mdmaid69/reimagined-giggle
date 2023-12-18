684defb3f6502983a6cc01a1573089bd5a5c7bc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:20 +0000

    priority vision clean keepfrom march

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
