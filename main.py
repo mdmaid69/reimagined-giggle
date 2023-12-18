718cbc3f19d4e0ecdd0b0598e4bc0895241889d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:21 +0000

    uponon refer classroom careful sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-x = 10
-y = 20
-print("Sum:", x + y)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
