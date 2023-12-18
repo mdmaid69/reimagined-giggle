da23bb25aeb79b6ae9bf74cd3e6b218aca406558
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:04 +0000

    married onto clear seriously damp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
