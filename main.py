70ca72f1c6564a39fab5aa76008d1023d09158d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:47:21 +0000

    blue either drop almost offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
