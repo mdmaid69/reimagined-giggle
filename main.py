490ddcd44862acdc6cb8c8589b5c8ccd03b2aa9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:58 +0000

    shine concentrate clear uncertain teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
