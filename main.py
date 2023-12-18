49c5cc5837ab1abc82639ab799ee678ab6b5f126
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:24:03 +0000

    fall square always decide funny

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def find_max(numbers):
+        return max(numbers)
