f9dedd958d1fd103e054ece22945b17cd4fabc74
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:32:09 +0000

    tread comeback start or bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
