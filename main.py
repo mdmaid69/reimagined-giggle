b87002d7f3ad84f03ae55ce38ad58d612fc31833
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:15 +0000

    height love wash chief angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_list(array):
+        return list(array)
