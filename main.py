b9bcb0c51912cfcb9034d4dc9da091c9e4f4a4f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:11 +0000

    pinch physical snow accident wake

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import time
-print(time.time())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def reverse_list(lst):
+        return lst[::-1]
