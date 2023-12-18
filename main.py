faab63ef10b05e2b6afb0a4f3c8f595496018a36
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:28:29 +0000

    wrong move need tear dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
