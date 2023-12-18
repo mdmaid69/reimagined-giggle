5783f1f7660740eb11f74674582fbe862e4afa06
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:13 +0000

    sport until typical stick unlucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
