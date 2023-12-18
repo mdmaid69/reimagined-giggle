b68c7763928b96586add44a42bb7d5b54fb22966
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:53:28 +0000

    cross cool round think approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+print(time.time())
