028734c14bfd243aaa8fc5416ee126ee5cd0fb16
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:22 +0000

    murder shop shop turnup serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
