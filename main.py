b50848ec46dca02b62bb66f5f2b4f9bf826544de
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:57 +0000

    people each bye sound before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
