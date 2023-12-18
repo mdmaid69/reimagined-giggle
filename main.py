a960e742d35d8fcffb15a9824b7c4fe4d36e5f23
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:33 +0000

    maximum reach even continue complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
