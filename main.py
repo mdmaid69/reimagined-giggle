52622b7b4c765011df69d4c4ce9cddfb75e4c8cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:45 +0000

    juice report doubt acceptable teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
