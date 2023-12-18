b679f6358cd42ca32cda30f5aba6b24a6c2326f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:07 +0000

    head intelligent name go human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
