a9cbba3c51d340103a7575657e3e5adbf5f6eb8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:45:40 +0000

    uniform pure hope fault smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
