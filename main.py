db6a8b4bc80eb56443e2ed88644e79bd25cbf6ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:40 +0000

    heavy pollution makeup cry play

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
