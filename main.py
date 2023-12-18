e19e2a76ed2c6281aadef4a931b6061a8c8598e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:30:37 +0000

    backwards let vote confusing turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_even(n):
-        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
