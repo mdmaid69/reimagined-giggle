5d83d6c9a64a7b38fb523d5016aa5a92495efd2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:45:51 +0000

    for sound petrol explanation description

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def add_numbers(x, y):
+        return x + y
