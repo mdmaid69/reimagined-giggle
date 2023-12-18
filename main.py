a5feb5beddb37a4cf18729d61c0d34baaa6f92fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:15 +0000

    serve confidence murder gentle stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
