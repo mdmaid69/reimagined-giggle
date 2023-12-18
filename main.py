7a2c20c91c06896d55aadd6becf7bcf543240c92
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:53 +0000

    callback drive control attitude whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
 def create_named_tuple(name, fields):
         return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
