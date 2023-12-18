1bc1a8648b78ecfee547fd86573d0f543bc8b1cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:27 +0000

    each bag hear last mass

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_tangent(x):
+        return math.tan(x)
