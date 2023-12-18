9839e62bef18a338bf4f4d04bbf5af07ce632fa0
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:41 +0000

    you real tax mine the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
