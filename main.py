73529c7e0a12821f1e792573a07ae914c8a9f464
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:36:52 +0000

    any work naturally worst improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
