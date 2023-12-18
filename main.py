4ed4d424a1c73da6b4bf081b12f88b66ab7642bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:14:24 +0000

    equal uponon lookup correct clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
