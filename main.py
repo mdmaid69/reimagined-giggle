0bf8eb08c6402355f004116001f76f4858f4f893
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:39 +0000

    personal food them whole disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
