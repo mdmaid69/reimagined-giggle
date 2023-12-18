ec2fe62d891ef2b3e5ced27d286878cc33f05e1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:27 +0000

    feature realize even ashamed tax

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
