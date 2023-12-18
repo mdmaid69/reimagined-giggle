37e1234834dde30419b4cbd814bffde948f39946
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:44 +0000

    travel positive accommodation introduce command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
