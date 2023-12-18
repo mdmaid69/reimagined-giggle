1d89aa3898c70bd6c1af028296d3039384916f47
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:36:31 +0000

    sticky Ee sale skill quality

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
