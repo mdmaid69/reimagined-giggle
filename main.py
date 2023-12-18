2f375cba442b24f16d5ace061f97df9f65b34094
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:24 +0000

    upwards respect open hour front

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def insert_into_array(array, i, item):
+        array.insert(i, item)
