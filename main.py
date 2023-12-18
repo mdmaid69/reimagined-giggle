f9f322bf967a4951ce69d03b658edda8ec41294b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:27:27 +0000

    thirsty name fork demonstrate sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
