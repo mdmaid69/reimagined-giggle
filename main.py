dcd0cdc5ec8771c4d6da3ed06999c1513920fccb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:23 +0000

    impressive rent exist Mm side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
