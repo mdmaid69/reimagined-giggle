77a073fde7c2d11f75c5846d3a46b33a92a99196
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:21 +0000

    trip whoever him worry flow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
