47e3183ed7674eb22f1ddbdb1f66fa66847a70bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:50:54 +0000

    nerve limit born why evidence

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
