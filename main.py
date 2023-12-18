2501651b5afcbdf82ff567f15e046fcf215d4e48
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:34 +0000

    talkoutof black whoever official low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
