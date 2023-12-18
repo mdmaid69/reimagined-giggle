5e62489055000924d2187a11f88db7e0543023b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:07 +0000

    cross sorry intend raise reaction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
