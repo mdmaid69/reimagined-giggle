633eea95c33638b58c9c22a99736160da331baac
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:08:20 +0000

    recommend house crowded information speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
