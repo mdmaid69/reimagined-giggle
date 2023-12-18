d3ef25e5c131be6d6ad2fb72ca9ee11bb6665a21
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:41 +0000

    exercise holdup violence ring talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
