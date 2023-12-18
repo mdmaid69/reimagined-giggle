238fd1ed322bf71b7594a3ea219e0dbbfd82cc3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:06 +0000

    usual fast number object steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
