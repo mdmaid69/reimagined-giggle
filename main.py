41f6a8b9b9fff8f3d77d9bd1f09ad69781217d1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:49 +0000

    above less aware judge dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_str(array):
+        return str(array)
