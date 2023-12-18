d25401ee041ce667743e8f99bc046e9ee842572b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:57 +0000

    Qq right organize interested supply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def cube_number(x):
+        return x**3
