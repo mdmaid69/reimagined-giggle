8654f242717831265d5f67f9feb87a3df0f8ae13
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:16 +0000

    exam minority proper not minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
