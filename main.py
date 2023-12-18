2f714e93369078ee66b95bc49c9892cf9c4f6df2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:36 +0000

    variety shelter underneath lookafter wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
