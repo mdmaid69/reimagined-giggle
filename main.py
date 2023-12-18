db3f799331a3cb869573daefb193d9fbdf6441a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:11:23 +0000

    danger one step suckme trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+import array
+def append_to_array(array, item):
+        array.append(item)
