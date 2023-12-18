6f405c2b88bce244350f41df53e6f76b654ab013
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:52 +0000

    stamp interested able dull goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
