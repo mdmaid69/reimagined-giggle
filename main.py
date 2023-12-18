621de09751e5c035bbc6f7bf75fabbfaf0d33ac2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:56 +0000

    interview call partner pay children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
