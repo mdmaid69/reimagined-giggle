a5de75be25c73b36163d0471433d292bee29866a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:04:14 +0000

    label wake trap evening scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
