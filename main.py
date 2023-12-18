9b34c4d41bc51d62d740568463e6d8f3f0eed9af
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:17 +0000

    die organization plain rich turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
