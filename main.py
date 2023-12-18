5f67d65dda20dcb1abff521d8d51b18a97c71c20
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:11 +0000

    attempt until conversation your statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import sys
+def exit_program():
+        sys.exit()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_list_from_array(array):
+        return array.tolist()
