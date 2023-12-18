3cf5d31b8dd76125aa47200e5ad770c4de1281df
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:41 +0000

    run mouth once steal getaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
