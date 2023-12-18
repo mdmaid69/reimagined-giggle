f32bf5da10794d163ce5992b8a2cdd28dbbdc941
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:47:12 +0000

    hand similar grateful fond butter

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
