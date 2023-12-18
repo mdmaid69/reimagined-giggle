5eaef4dee314707f828311abeadaf0dd7770cd40
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:58 +0000

    tired contain complaint respect movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
