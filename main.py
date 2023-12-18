a1b624e8e42166cece272a83758bf3aaa019a818
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:49 +0000

    set fast knowledge Vv drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
