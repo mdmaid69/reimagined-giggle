0334daf7fdf3e1cc61c896e3f3cd3a1f60b89261
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:52 +0000

    serve recovery understand also sudden

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
