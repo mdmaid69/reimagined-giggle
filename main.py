1bed4e8f23546b679dcd25280bb43d4e4a27b55c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:10 +0000

    port mean phone hungry with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
