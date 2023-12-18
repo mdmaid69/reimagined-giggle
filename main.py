965f5617599ed4ada93a5a72878ceb76392d4511
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:49 +0000

    child completely payment mix number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
