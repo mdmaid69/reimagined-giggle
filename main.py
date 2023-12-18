aa42b522c1d0ecfe9353feeea00b190311c1dcc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:56:37 +0000

    within relationship range fit term

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
