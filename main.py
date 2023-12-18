b2ae26aac42ece180149e11a4bbbfe5ba2e78bf9
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:19 +0000

    bored believe come stop damp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
