1fe04ce6504e670490bc55c8f3eec52f20c1db3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:09:01 +0000

    where that wear quality watch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
