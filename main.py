b34b501de670998a2e4cf60b3bc42e4ff3162a31
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:53:08 +0000

    cheque plain sound turninto runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
