9eec57928d76962a633348bb30787589fd9e5c02
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:10:53 +0000

    warning sort word plan under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
