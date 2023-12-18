1fc9ebd6e02e68d1dc1da4211890a3b1a092b73d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:35 +0000

    trap command before play little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_area_rectangle(l, w):
+        return l * w
