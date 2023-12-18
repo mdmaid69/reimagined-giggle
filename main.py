86d8fe2b0a2d69584206a65b9c10b392e0315d3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:43:38 +0000

    means wash maybe read card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
