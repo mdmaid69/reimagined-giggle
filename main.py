0a74543a0afb6b43a7196a34f0b0de9b69114706
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:18:28 +0000

    whichever apparently sting nerve suit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
