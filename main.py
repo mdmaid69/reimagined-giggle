fcfc37a83ce6eadaae7b566e768ffa3bc73f7680
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:04 +0000

    excuse bin anything let jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_sine(x):
+        return math.sin(x)
