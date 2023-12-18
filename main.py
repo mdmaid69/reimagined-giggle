be5c0e7bf2ed0cd2c01e94db2e5d284f69a336b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:21:52 +0000

    whistle blame now variety happen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
