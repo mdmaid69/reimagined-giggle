1d051da4e33dd762fef38fcbe95258f42080f924
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:23 +0000

    operation not correction goout waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
