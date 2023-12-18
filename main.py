310bb11b71dd12343a04550015fe00b516937db7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:31 +0000

    inform secret information force mess

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(lst):
-        return max(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
