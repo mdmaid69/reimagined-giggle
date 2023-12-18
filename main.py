bef12f1495b80bc9df10e6150a9bd0c0c077ca0c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:32 +0000

    turnon justice unless base later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def reverse_list(lst):
+        return lst[::-1]
