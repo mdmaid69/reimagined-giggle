182d4301ccb0f021e30ca70ff920d7bfbf82f266
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:26 +0000

    form giveback early late message

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
