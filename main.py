38808cd22ec578a52e8bdee40df4b2ec43af0b9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:18 +0000

    ask top warn nurse tear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
