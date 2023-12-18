04d2092657c1417162e53d25c9316a8f7f003bbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:42 +0000

    challenge software tool table comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_current_datetime():
+        return datetime.datetime.now()
