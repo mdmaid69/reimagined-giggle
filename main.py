36b6794843f097af75bd23a5b74e02afe0abf671
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:32 +0000

    damage pass tidy trick runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
