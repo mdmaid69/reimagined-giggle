d01dec8311d884c95af70a5e15bda0f316c49257
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:13 +0000

    opposite trust we switch scale

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def sort_list(lst):
+        return sorted(lst)
