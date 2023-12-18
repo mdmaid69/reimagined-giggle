b6f287203d0212ae38eae8605df5966f73eef506
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:02:56 +0000

    bang computer tragedy throw recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
