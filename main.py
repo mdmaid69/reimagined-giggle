0461030f0f54381e8a40472756517fd4040fb4c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:18 +0000

    better wave somewhere check fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
