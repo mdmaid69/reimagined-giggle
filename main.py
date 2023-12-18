8a278f70d4661f1ca06dd8ca3cd93d644029d6d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:37 +0000

    shrink switch behind them bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import collections
+def create_user_list():
+        return collections.UserList()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
