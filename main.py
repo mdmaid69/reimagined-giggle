78cd18eef95eb07a7006e367b5cd605da6c0d027
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:43:34 +0000

    job work list empty beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
