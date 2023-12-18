ec9ed545505865df379b7c099ffdeb8d50f0663c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:05:06 +0000

    that angry appearance trip year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
