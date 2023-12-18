95e6cb5b13f285638e2f0e8f9cf9d535484f28a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:19:07 +0000

    gentle switch entrance interesting flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
