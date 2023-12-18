036547ca583d3be33c6d98d57e8a24ee252a234a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:19 +0000

    apparently pinch deserve wash safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
