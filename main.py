93ccbde505ecaa5b94d52d44ade0e5c63d0b5b74
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:33:11 +0000

    excellent measure through cry pretty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
