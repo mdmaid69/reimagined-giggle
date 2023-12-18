e1c2518e6aeb17cfbae3d14de321d0db4f6efe0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:44:05 +0000

    regularly memory shock unkind everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
