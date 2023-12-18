a64f4f883347e7be8de0fb55d14f46c24a0e5b21
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:36 +0000

    ear breakout each neat flow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
