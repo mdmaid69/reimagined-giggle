5fa308c9a3f4c3335c96a7bd5dff1b5ac0e4c1bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:23 +0000

    influence vision young me himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
