d76f0d7d814041d5dbd85786b70534c80109445c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:25 +0000

    mystery Cc Ee inside drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
