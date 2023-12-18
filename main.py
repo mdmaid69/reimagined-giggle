2b84188c2693b556b94ba8979809ee9c44210144
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:11:40 +0000

    interview cap set traffic insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
