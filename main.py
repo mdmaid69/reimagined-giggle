ceed76bfce0a07267a8af0b63a17c85929b5f64e
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:08:07 +0000

    either promise popular because period

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
