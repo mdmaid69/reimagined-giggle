f6cc7611f943e0edf86aeb1f2525a4494d7ad29f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:55 +0000

    makeinto number urgent face middle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
