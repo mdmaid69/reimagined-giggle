4bff9d02bfe9283f9bccef5ee064bba6b997fc7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:10 +0000

    cope still invitation play dry

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
