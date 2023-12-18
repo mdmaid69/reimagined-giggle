dc2676e99a02acc7ea7b5e2b4b3260762cc24cf0
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:33 +0000

    importance squash attract bath legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_tangent(x):
+        return math.tan(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
