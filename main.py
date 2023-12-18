a84fabfb98c90268f8897d3221bba4515976cbd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:27 +0000

    standard increase regret increase nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
