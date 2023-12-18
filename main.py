39ef43e2b4efea0d116b57ccfb6eaac0ef2afb97
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:04:54 +0000

    service listen guard comeover especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
