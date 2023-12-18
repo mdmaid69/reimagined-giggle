baf3d084a562f073cffd74f561d6e3c31dbe0329
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:26 +0000

    order name foot switch march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
