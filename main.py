fcc33ddad814700ef5efc5d41798429f9ec2bedc
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:29:56 +0000

    sign ought service trick boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_current_working_directory():
+        return os.getcwd()
