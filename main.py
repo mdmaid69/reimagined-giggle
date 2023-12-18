f8732ad69f486098c82429126245d05712ccc8a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:34:09 +0000

    question careful furniture comeout light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
