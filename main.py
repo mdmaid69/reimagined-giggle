ec90ef2cb02aa00d05fda73fc708dcd489e6ac53
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:49 +0000

    part throughout correction rain smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
