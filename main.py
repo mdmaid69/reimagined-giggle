6094f013a55afc2176f378d7e5b95cf244be959a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:46 +0000

    original guide such impressed town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
