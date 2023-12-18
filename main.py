67e87d4b91fb6050faf8b05717a9da65730c0f26
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:40 +0000

    necessary survive worst persuade home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
