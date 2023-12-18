7d8565cb685c463612136c2ad9af1fb8e66a11d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:27 +0000

    some big fun feed search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
