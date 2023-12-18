d6493b0b2a3cfe12ef34119f650a27a810ba182b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:20 +0000

    snow call instead talk war

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
