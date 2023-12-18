0f8971da7d2bed83696d6e3d3d1b22d97ff2c802
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:31:37 +0000

    she life strong law pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
