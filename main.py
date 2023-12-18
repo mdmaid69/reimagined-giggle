20e4d2c7b11147d067fc015006925e70ac599121
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:44 +0000

    unfortunate convenient certainly own mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
