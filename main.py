cdeb0428bff258cc4c8d1d4543767d9b70373023
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:07 +0000

    science add space mine new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
