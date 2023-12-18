4cc0d139dc014b282b3413723c71297eeaaa4396
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:35 +0000

    no gentle sensible discuss crowd

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def pop_from_array(array, i=-1):
+        return array.pop(i)
