ae5380cf6af9cabe0ea63d0b67b1a5e87de96583
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:07:22 +0000

    boil frequent choice active 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
