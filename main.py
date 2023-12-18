3184c89b16d154ebe4ba5d8e3211785c3a4d5c08
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:33 +0000

    represent judge low mood this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
