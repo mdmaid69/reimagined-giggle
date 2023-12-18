4f63042d7057f67ce6e6a16c78ca303e57fa7e68
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:08 +0000

    office thick thick everybody trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
