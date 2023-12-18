3b3d64c4ebd8ae012bcc863bc354c53e45706205
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:57:47 +0000

    laugh whom mean party petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
