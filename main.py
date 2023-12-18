8c12861566bc5a774d5195e5714634376e878528
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:19 +0000

    use never influence operate trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_error_function(x):
+        return math.erf(x)
