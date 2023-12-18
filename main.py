7f1ce89da2cfa3ddc958fb18e099ee85b5481864
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:17 +0000

    trouble public pay about whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
