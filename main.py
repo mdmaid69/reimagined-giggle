69ec7f0e15ac031c59a1e3fe64de5878db6cdd5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:09 +0000

    support lesson whole awake share

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_int(array):
+        return int(array[0])
