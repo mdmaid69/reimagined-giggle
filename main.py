fe0237f0efca2ca323a8ba8ce6316e34af0e5358
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:49 +0000

    stretch start roof opposite urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_speed(distance, time):
+        return distance / time
