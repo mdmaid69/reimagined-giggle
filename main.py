eb6deb9b05ff5fc7e7bb6ee86f880d9303e8b22f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:02 +0000

    give proof figure minority steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
