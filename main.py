6863db97d8479487135aec5e7dd25eeb3538371f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:07:36 +0000

    rather must pass order middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
