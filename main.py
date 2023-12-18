56a3d32b14c34c7a2e4491de88f809e4fd3323d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:44:30 +0000

    movement shadow square autumn pupil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
