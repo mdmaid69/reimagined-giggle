7bb506f57b3bf72ad5aef67a67af40d84f9710fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:37:33 +0000

    post jump choice statement surprising

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+  def delete_file(file_name):
+        os.remove(file_name)
+print("Hello, world!")
