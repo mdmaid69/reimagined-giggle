392931529a51ad059a1c4f6cce8d8f2182b87a90
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:00:33 +0000

    full drink loose container pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
