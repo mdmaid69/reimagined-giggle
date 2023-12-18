4ab7c718ad7ed1e8169f0b479c038d7a7ab7b2c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:46:29 +0000

    period period achievement pass lead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def convert_to_binary(n):
+        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
