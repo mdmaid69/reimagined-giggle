0085b2d856da2a92e00c74bd94ab6d21eec8d591
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:55 +0000

    makeinto loud intend platform complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
