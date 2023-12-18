c74d7f61d4c8010866bb073f65022c0fc58cc021
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:25 +0000

    foot on state almost inside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
