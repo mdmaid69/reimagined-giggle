abdd748f6d34bdde06b41a569a31731568735c89
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:16:44 +0000

    then street size maintain admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
+def subtract_numbers(x, y):
+        return x - y
