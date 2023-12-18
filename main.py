8622b3f92bbcd61b8143a827c81a7ebac6ba83f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:09:19 +0000

    sort hold sound convenient callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
