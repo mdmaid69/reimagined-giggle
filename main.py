3dfde7f5bbbc713db540e27847d5272535bdb3be
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:53:23 +0000

    shock better near conduct communication

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
