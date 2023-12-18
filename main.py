578a0f7dc629b40a67f7b3a46547e89e86356600
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:11:18 +0000

    loud everywhere frightened doubt lot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
