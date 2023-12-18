65b8d815b18a6f05dd3be3a91a7b1b9b9ab2a9a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:16 +0000

    satisfactory turnaway pollution stop belief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
