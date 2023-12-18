5ed14505e92a80c9d0a1f00530b72fa8428933ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:36 +0000

    argument tend hair limit impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
