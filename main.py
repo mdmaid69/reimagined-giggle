25a00b4e61ee3b1af25b1d421bc1f612d2410d35
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:10 +0000

    curious crazy likely bang computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
