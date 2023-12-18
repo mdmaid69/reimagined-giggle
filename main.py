f76e0b2c2bf9abccc607674cee6fde7fa9d31454
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:56:19 +0000

    apply tune gradually refuse stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
