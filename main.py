87e6c3d23e34870c3e4da5247d6a5e04987da286
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:56:03 +0000

    neither tell choose near wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
