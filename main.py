f377b6b18efb30dbb5edef0d97aa0a586c3946a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:32 +0000

    rarely crime normal collection aim

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
