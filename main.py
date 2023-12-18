f69c3417501bd77b2a2f1eb8de2d947fae6f8b8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:25:50 +0000

    permission hold journey proud straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
