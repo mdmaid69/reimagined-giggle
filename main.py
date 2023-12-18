1407f4dcbd8bd5b0bf103382247c7e5e8b9d975f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:50 +0000

    takeout or content anxious open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
