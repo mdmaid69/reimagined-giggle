958d9bd59751e67895b42827c2b6784319092c6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:26 +0000

    example sail flat uncomfortable store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
