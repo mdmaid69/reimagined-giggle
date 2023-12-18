0ff5cd26a8e9fa6853406c103467e650e9cf78df
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:28 +0000

    exciting injury bear improvement fair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
