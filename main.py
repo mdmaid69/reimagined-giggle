41d8e19d8348284deba73dd718c4def342440d1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:14 +0000

    describe Rr teach recent shine

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
