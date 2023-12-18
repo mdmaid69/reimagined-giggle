bee0ac62350f1485c3706a7f002c0dfab0bb10b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:23 +0000

    transport finally forever wealth record

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
