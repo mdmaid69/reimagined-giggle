5941c680f359a988e9a244da5e1bbd975a387dd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:50 +0000

    art everywhere new expression journey

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
