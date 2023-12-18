e15ed0775234b2afd1a63e2c56556cc551330840
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:38 +0000

    turnback interview letter possibility worth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
