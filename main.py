db6c7ad3747398c86edc1abad32fd0de384db42c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:48 +0000

    opinion yourself cost content rarely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
