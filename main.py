857500dc7d9b1982841ea4d95e6cfd6d660e905a
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:03:46 +0000

    passenger base intend from run

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
