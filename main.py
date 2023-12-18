03d22c4c1a522028b278293150ed63c64a6479db
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:52 +0000

    glad smooth method bush gas

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
