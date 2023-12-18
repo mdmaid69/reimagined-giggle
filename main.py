aa33ca0f965a4f8347e393925fe620831298f45b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:50 +0000

    wash nurse view store wide

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
