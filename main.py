e098690db0276d6fc5296187cdebef8779d5aaa2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:46:53 +0000

    Mm exciting wise lead not

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
