1fccef432003eccef3448bca68ff5a73b543d29e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:36 +0000

    watch justice any time classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
