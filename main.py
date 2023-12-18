78c8fbbad54506a33950e5e29eb0ab04c5227cd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:50 +0000

    bridge exit declaration us sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
