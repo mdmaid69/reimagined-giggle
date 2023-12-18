70f96f4469d8b2a0e3a74273ccb0e830b5481537
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:38:08 +0000

    fall hollow every correct life

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import time
+def get_current_time():
+        return time.time()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
