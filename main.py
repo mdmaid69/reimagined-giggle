984e88253b12adf05d5fbe2bc108c83f236db3ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:18:30 +0000

    talkoutof their clear possession drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
