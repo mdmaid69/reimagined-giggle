01f8672130bb2ada1acc926952a45e421cd26fbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:44 +0000

    nowhere shoulder freedom bang stupid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def square_number(x):
+        return x**2
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
