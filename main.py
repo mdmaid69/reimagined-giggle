668f7b3bab5def7e5382ac5caaa71db77753e5b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:30 +0000

    them taste beautiful meeting apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
