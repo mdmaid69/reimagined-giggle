096c07d3c5a24a588d0ba389373f9add99645eb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:45 +0000

    combination clear whenever unpleasant dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
