2fec104402276609cb9ef176bbf46485812e9a47
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:15 +0000

    whistle deaf average wonder many

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def count_words(sentence):
+        return len(sentence.split())
