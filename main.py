2c5f5568409c023b9024f2e016e4c7058f71a843
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:49 +0000

    bar mood expression blind 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
