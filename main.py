387856fbcca8cf0fdc13c7dd36b29f5dcd8d1111
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:27 +0000

    move deaf Yy step large

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
