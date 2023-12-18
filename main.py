5f85f84af4677a26c8757ec3b087568902955c46
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:50 +0000

    build exam behind upset blow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def divide_numbers(x, y):
+        return x / y
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
