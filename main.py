de6635944e29fed598915869b2b1ae82f99eb91c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:30:42 +0000

    influence him support fence best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
