bc761e3f9a04841633813c38cedc08f0917a3cb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:43:18 +0000

    cost passenger so form stop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
