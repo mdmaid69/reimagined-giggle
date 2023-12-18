29a2b68f33c7831b27a5948d0da51d8f0a3f262d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:30:34 +0000

    dish crowded spill environment fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def convert_to_binary(n):
-        return bin(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
