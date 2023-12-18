c0f351b18670835c8b4e7b9da62bc00c1d69ce46
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:10:03 +0000

    block among somewhere task lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
