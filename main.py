cccc7b1fcc3c7261bfb4fd227c1eed7dfbef0538
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:27 +0000

    dry yet whenever pronounce pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
