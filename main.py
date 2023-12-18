63ae9e04d2b1e0f8c0aa02abcd1a8e7dab64ae0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:43 +0000

    Qq prevent for telephone please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
