f4588af2c503b7f29e50b8e2e7f67d8f10afa91f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:00 +0000

    butter grow confusing wherever climb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+x = 10
+y = 20
+print("Sum:", x + y)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
