d62fa858ab783e423ac0771857b168ee62ecd751
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:11 +0000

    clean they butter principle surprising

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
