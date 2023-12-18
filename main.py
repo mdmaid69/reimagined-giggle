fc2c3be084bfd1e0256dc11fa4fadadb113861d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:12 +0000

    study height afternoon fork entry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def multiply_numbers(x, y):
+        return x * y
