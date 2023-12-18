e459ed93d4621fe6eec9653f1e3886e36eeb428c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:35 +0000

    mention offer married lose himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
