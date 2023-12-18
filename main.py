0371b8a5affa57cd070400e9818b5534e5aca3dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:05 +0000

    copy worth piece clue cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
