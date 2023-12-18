d8cea50c999f5ef5e8516afe97c8427bc109b774
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:30 +0000

    several where callout worth perhaps

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_density(mass, volume):
-        return mass / volume
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_current_directory():
+        return os.getcwd()
