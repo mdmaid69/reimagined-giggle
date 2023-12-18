4ff7b1a8be9c3f27fb5eb97e7acc273671d0c010
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:41:16 +0000

    them us exhibition concentrate slice

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
