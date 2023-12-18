227e47a9f86f82b657e53cc5e5070f9f1dc449c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:48 +0000

    interested short yours brain visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
