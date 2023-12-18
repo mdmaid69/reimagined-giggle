38cb55d01e5acb5e0a2857464fca5c95f45a4f06
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:23:25 +0000

    division like language shake business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_min(numbers):
+        return min(numbers)
