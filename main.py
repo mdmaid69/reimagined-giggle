54fb2b18afac305566a147e220ef01f1f15df504
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:11 +0000

    impossible different convincing too risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
