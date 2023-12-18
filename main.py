ac1c7d9bb60b6f716593cb6e0fe7e7e24db81614
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:45 +0000

    form contrast bath circle content

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
