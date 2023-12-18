2754454026b63b8e7ea3e4428330b4e3b8e9e0fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:30:37 +0000

    address meanwhile announce science report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
