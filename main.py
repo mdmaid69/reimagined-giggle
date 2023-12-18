78fa99637cb2ca225399a97b2d506e281a35eee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:00 +0000

    few care even hand muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_tuple(array):
+        return tuple(array)
