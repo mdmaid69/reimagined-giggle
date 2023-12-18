11eed1a887cbf58d2ce6c6b3a90b1cdaaf4c15cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:48 +0000

    prefer that near work show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
