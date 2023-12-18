1144edb33e153b5427d3c34579089b069354b822
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:33:01 +0000

    nice autumn spend relief never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
