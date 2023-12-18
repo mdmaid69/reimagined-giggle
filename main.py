93d257b580a1e5a2f7479b9f3594f859093a5c2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:00 +0000

    sore exaggerate affect point somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
