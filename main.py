965c95d5b07f75574215de78cfa13bf4cb5cabfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:09 +0000

    spoil sticky allow objection region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
