fec98d3744a56e9793591ad9012073b1a2e41a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:54 +0000

    receive to confused produce act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import time
+def get_current_time():
+        return time.time()
