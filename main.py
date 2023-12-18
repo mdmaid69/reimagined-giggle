2e8d43d13c2e6ec806d58fbc422950fbb1316be5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:34 +0000

    juice correction clean bill coat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
