415f5709a41df85436beeef82816682903de29b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:10 +0000

    reason flight reserve breakinto shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
