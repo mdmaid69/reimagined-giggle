5ac5cb49a93e2a7d0472baf2ad1664d3f28e6b61
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:35:52 +0000

    escape change thank principle level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
