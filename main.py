782fccfb4d27eebd7d22409cf94de6bf55a85886
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:51 +0000

    sea them brush break low

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
