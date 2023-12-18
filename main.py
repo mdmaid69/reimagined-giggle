6210100fab2ec2c3bc28d5d2942fff262cddbb0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:47:51 +0000

    morning continue possession general clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def log_message(message):
+        logging.info(message)
