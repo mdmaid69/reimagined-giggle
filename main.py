caa27673aa3965a4944dccfa5b2db0d6bf044c7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:55 +0000

    fortunate common guess correction compare

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
