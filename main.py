b41003a0aca5b8621e803d14cd90466863ebb822
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:36 +0000

    exact decide mind move feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_repr(array):
+        return repr(array)
