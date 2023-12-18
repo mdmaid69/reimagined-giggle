d18aefb3f0b569d73f98f32844b9301052397cbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:09 +0000

    right swimming original warning cheque

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
