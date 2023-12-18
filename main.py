f3ed81d6004e89dbb9263555665f846f67d0f49e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:54 +0000

    inquire rank cruelty choose whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_ceiling(x):
+        return math.ceil(x)
