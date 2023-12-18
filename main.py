4b56fdab0eb3c622568f44140a118d91b8ec4c5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:52 +0000

    compete fat piece balance though

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def cube_number(x):
-        return x**3
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
