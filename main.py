ae5dff28836cad620220c1230b6a17b51b94fef2
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:11 +0000

    taste trick maybe appearance forward

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
