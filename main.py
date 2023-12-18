4cf3dbc914afcc23ad1bcbb5330c1df9ec13d1c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:51 +0000

    square but roll close contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
