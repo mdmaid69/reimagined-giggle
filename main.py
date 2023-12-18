50d81abcbabaf240601cd51ca650062f83901584
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:23 +0000

    dress my and corner handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
