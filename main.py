4cdfd9494dd5127bdbf96c5b3c3b10e40ee38b1e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:24 +0000

    both object good fasten spring

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-print(random.randint(0, 100))
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def convert_to_hex(n):
+        return hex(n)
