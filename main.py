bfca943dd7d85261b1b4db0f6401424067539276
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:29 +0000

    suggestion hate material prepare pollution

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
