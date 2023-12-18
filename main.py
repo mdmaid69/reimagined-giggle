0c0718388703256f5d801e9f0f04a06ec0e831fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:05:07 +0000

    colour worst stand reaction listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
