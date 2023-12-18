9fd593c7f8dbfeb48c8387ad38a8b5001a7b826d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:19:06 +0000

    lead appeal amount sort gradually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
