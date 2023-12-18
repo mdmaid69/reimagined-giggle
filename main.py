5466ae1d85df26f1e1be07a6ad8ac349a85b7e73
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:27 +0000

    cross smart instrument 571enough plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_tuple(array):
+        return tuple(array)
