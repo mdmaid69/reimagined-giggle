7a5de8d85a789a57a5027dedf12beb73da67f888
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:33 +0000

    instrument comefrom hard convenient emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
