2cb49cb96247b9cd03429ccbe679a4b0efa2339c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:26 +0000

    strong noise obtain agreement born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
