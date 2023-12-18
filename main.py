ea65a94dc903d6a79e539209ebb130c0566f0a91
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:30 +0000

    sell exit comedown squash thing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
