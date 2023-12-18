604b63b272e9bc1e6afe4747e4ba33c5b1d7c9bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:47:59 +0000

    supply hair collection past obtain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+def greet(name):
+        print(f"Hello, {name}!")
