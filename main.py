881c2b201ce20019ca8ef090f4712abf4569def9
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:00 +0000

    pump city story chin feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
