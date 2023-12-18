7550d2e015ab93ff8a0f3f26ce0a6f33e3a98d0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:52 +0000

    fashion manage say quantity comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
