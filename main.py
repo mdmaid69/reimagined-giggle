b4927a7fbb860d7eb3ce4b7e1461cd97447294cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:39 +0000

    little cut progress comeout practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def subtract_numbers(x, y):
-        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
