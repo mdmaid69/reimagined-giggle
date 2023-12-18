c41db2cc42e82315a1055567a8b27a8a52b0be37
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:57:42 +0000

    production yet study smell label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
