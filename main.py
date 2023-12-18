84aaa8ad2d39004bbd440b1a994c5b52637ef1c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:58:09 +0000

    goon shadow comeover tired heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_int(array):
+        return int(array[0])
