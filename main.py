b1403333bf2c12202897267576d4b3035bf815e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:33 +0000

    bush score someone interruption hollow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
