3c8b4ade8159d710930408f697b0c5460ab2bf70
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:37 +0000

    post manage awake land beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
