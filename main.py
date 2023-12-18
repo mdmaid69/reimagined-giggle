ee300910108958ecd26fd753f589fd182b48ae83
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:55 +0000

    level myself autumn slowly fairly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def subtract_numbers(x, y):
-        return x - y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
