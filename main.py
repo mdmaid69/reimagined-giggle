e8b02a3f091026ef6e9468bc84fd0316e1fc3602
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:36 +0000

    close emphasize instead grand half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
