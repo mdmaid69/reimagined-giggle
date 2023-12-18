730510972cb1e4b9f7a8cfd884f539a62630ffe3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:56:57 +0000

    image sort runaway comedown position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
