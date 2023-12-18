8ba92ae2bb4d79a940605f77c629d9143a60d38a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:07:25 +0000

    disgusting means noise catch certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
