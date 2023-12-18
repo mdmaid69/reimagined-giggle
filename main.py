351fd541c11d0f7bb9fe24cc8027401420ac6a2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:16 +0000

    section vanish opposite scare connection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
