ff9010dddfd7770f4ecf68755ae0920362a15d93
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:57:43 +0000

    discussion else attract activity stiff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
