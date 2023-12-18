417fdcf8d44e34cb876fde7400939d5922dd17a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:38 +0000

    protest on ability furniture gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(lst):
-        return min(lst)
+  def split_path(path):
+        return os.path.split(path)
