4405531c34a66bca4a630deefe92cb13b462229b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:22 +0000

    race range money to law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
