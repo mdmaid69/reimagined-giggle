1b8c9f9233d4330ed90743a554a3f62f016fe2fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:28:32 +0000

    ahead murder sad direction trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
