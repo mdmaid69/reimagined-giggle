9fb2517111baa982279a31e90c49db8096151707
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:05 +0000

    crowd platform brave interruption although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def convert_to_binary(n):
-        return bin(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
