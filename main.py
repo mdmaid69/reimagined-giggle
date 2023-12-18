304aa9d5361c3fbb48830527a7a8feb54848a52c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:37 +0000

    time belief miss comment direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
