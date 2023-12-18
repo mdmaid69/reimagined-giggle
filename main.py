97dceaa89e581f59c388b366f737544b1da357dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:43:06 +0000

    noise spot ring there display

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
