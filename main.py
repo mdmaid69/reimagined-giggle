79507bc3d3c1f7587c3b62f13d3a80399f59d148
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:11:30 +0000

    teach interruption fresh task both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
