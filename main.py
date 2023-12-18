b11579f546dcc47d6e6eeff223a2b60526bc26fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:25 +0000

    contain Ww trade sight afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
