232955f8c175823f7d98906ad4cedf0f735f66d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:48 +0000

    agree event common education afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
