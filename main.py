bb8df44e51a7d6a2f6fff7e90961dee085cf4244
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:00 +0000

    the feeling arrival raw spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
