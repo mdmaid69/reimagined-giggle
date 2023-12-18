45f8469d1f251c9c8a6a046f12279bc57e5eed8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:32 +0000

    I join fasten refer get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def remove_from_array(array, item):
+        array.remove(item)
