0042672238d07ff55a75e5b09dcec8e1dc2977d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:35 +0000

    opinion turnover go impression comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
