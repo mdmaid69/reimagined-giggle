32647fde68503982e25e3f0d3f20525c4736e3c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:50:51 +0000

    impressed dinner nerve clear miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
