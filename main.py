91c58a866465553a187ae01908f09518460f0bf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:01 +0000

    agreement cover sensible them uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
