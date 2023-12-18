ba19d535221b4cec4162c59086e42eab3e3afe87
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:27 +0000

    bread confused hard negative equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def add_numbers(x, y):
+        return x + y
