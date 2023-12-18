21480f4f1d0e0bda62b9d950436625cc23ba795f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:07 +0000

    slice somebody add myself calloff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_item(array, i):
-        return array[i]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
