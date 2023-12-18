ee92b5cb41f5e5b3c58b4fff8ce3bf4cf0a41be2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:46:30 +0000

    it there scrap street inside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
