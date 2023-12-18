110cdf33b56507854942ad626450d253ea779244
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:03 +0000

    music after feed glass target

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
