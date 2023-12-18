daf27c383039d486d1793e4ff8361a770f3d6104
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:01 +0000

    position book clearly always meeting

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+print(sum(range(10)))
