ebfc707d08b26adadd9cac4abadb33069137f991
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:09 +0000

    near use cost objection test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
