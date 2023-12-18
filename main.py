1dbb4088e3dbfffd4aeaceea63b501128de855b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:46 +0000

    sticky cost try original negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
