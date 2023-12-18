58fa2a4bf840d1da6e085eed6902f0be23d1f9b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:26 +0000

    travel put song claim love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
