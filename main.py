4c07cbbab18c31df8e2f7e0266503c677a9de497
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:33 +0000

    love hard where brain Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
