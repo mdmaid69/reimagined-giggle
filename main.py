a75ab6bc5f8bc270e6ab008710b3d521a0fdbaa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:11 +0000

    workon compete issue clever respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import shutil
 def copy_file(src, dst):
         shutil.copy(src, dst)
