7c0714a6e60094aaf4c6f8f9f2e6035773f21dbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:15:36 +0000

    wing judge pretty kill maintain

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+name = "Python"
+print("Hello,", name)
