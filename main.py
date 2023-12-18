ffd66cebd1b1e66c208101ab7c2ffccda9957a6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:26 +0000

    string open rough niggaacademy congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
