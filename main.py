a275d051e293793b021b008d7943b4f900404ccb
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:11:49 +0000

    remark walk extra share phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
