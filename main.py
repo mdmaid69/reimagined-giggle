e39dae97656167f8f7fa2fc23f18495a2fb34458
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:32:31 +0000

    thing arrange previous somehow complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
