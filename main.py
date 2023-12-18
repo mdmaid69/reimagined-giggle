93baa8338620cf4d58c1fef96d281d758190570d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:51:00 +0000

    better soft telephone thing play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
