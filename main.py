ed2387c605043ca9f96b9231dc9d867e3c3404d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:05 +0000

    sign throw steal attach building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
