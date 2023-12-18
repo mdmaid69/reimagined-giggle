895a3bb4e8a77a6bd7a5f4bb0fae7b8c791cbd97
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:05:18 +0000

    knowledge sensible extraordinary turnup shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
