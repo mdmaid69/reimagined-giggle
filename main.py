a672d690e602f7da0437a89fbb50aec4c5d78d17
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:20:40 +0000

    fact popular total concerning freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
