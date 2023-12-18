3c7358dfd1c9eaae7fb93b80d6d2d337605bfa9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:03 +0000

    far interest general will drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
