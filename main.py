eb2fe5b1abea13f83a1579a996a2b46afd906831
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:52:32 +0000

    reduce team dead information block

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  def subtract_numbers(x, y):
-        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def add_numbers(x, y):
+        return x + y
