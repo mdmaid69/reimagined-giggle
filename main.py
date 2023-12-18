58322d3d80aa8ea8a6e88c5aecb64eea9f2c330e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:30 +0000

    activity lost brown keepoff yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
