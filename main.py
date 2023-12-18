70a4a5e93657e44a0173fa2594a0533871f9a8f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:06:43 +0000

    before steam Vv draw add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
