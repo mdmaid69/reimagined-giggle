5775e1316745248ad883afc8ba89d82be1c58beb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:47:23 +0000

    bread call shoulder settle coat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
