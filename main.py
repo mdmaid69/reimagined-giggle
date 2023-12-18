2fbf7f298405cc30f59b1d6cfce5f91deb4f6566
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:33 +0000

    score demonstrate pencil top kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
