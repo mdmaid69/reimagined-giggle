ef6d06938d18ed72bf4120a6654ca4c26f65c0c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:07 +0000

    afford fairly study loose interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
