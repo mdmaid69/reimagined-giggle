8a51fee335d6a21ada4760b6270c5a775324a659
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:30 +0000

    shake strong surprise left quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import numpy as np
+print(np.array([1, 2, 3]))
