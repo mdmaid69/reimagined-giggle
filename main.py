48b0b2801921b98c406431b37657c99a8f4bdf42
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:51:11 +0000

    quantity sea past sudden style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_array_as_int(array):
+        return int(array[0])
