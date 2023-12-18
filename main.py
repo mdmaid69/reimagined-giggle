90965ab159dfee104fc99f555de642d6a7097b50
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:52 +0000

    activity left flow rely once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def reverse_list(lst):
+        return lst[::-1]
