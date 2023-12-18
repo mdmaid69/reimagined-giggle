dcae5a626e471b491f1f4ac76931092565431f81
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:20 +0000

    tape travel consider claim runaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
