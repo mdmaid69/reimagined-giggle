9bc9192cbddbfee46341be884a4b30c72ea0276e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:07:31 +0000

    pot set Itself physical punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
