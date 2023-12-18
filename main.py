c458fa8dd21c80f8d2da73a364a55da4d413e0e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:37 +0000

    film interest book hard male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
