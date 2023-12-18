25b570fd8eddf4985393058269357d93dbb5a825
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:31 +0000

    blow change play off think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
