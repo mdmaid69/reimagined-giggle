32ee7ac608492d6a80ca53cd8fb56b2b65cac9ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:12 +0000

    thinkthrough somehow otherwise slowly travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
