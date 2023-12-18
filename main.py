5ebbd3e8e043f6238e236524d10cf931c5b6662a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:17:56 +0000

    quiet live equal nonsense accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
