733ca9d5042ff7efceb95cd5f347f6d22135e021
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:07 +0000

    steam fetch mean force bin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_power(work, time):
-        return work / time
+def find_max(numbers):
+        return max(numbers)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
