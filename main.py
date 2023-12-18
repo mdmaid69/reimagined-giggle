b3a56e41610dbcb3aa4e2382a82a3c38543f5e00
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:10 +0000

    official suppose address manner computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
