744e8a6e15ca05be81281cb85d77a3e0162aa2a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:10:16 +0000

    now compete port sound Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
