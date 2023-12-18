5dd0a1e27314854a7d37c11ffdc523518e849e0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:54:25 +0000

    talkoutof welcome equal lookup who

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
