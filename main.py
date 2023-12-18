89538c4254d5982ae10ac32ed5a9293ca4ae610f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:38 +0000

    exaggerate fire week book present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
