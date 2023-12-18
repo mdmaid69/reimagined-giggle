5360f1c2c64581da7d766417fc9f0a1374f2b4db
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:24 +0000

    but task hope for send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_acceleration(speed, time):
+        return speed / time
