62ed11369d6a01dd5684d23cab3f4d0b4d722ce7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:35 +0000

    common command violent male figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
