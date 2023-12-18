4ca0f9c10e482955f3a4450a78ae10abff872e61
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:17 +0000

    base rub dick deep careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
