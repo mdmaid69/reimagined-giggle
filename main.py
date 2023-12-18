1bfbe3677b27d6b22b529bc62e6787f13a9f3f47
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:44:36 +0000

    Cc that home unfortunate become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
