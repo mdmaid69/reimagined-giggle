477761815cad2feea21ab7c8e37f04603b595ac0
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:06:01 +0000

    goup whether tense right happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def count_characters(sentence):
+        return len(sentence)
