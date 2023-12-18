928b4f6e59f971e39b346fe9bbb163ad69c8e397
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:10:58 +0000

    that related complete usual shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
