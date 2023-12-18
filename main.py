72dc540eaeedd721cd71af4357c266ea36e36757
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:52 +0000

    top justice somehow Ss violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
