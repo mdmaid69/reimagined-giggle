850b502b4958f6c873c4cbbc801b9c82da8ff2d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:56 +0000

    mass thorough 573entertainment necessary which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
