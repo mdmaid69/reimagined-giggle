5a7f1c0c658cbfcfeeebc2508c6895a1515e31cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:31:49 +0000

    crowd whenever succeed shoulder heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
