4b99ae86dea44b28fb943c5146848901d8c64a58
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:05 +0000

    tidy almost takeaway example under

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
