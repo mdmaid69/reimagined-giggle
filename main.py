240f6b77b5608ac02d4ac94af368faa82e6da923
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:47 +0000

    settle similar her condition sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-print(time.time())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
