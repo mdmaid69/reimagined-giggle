7c36a56566cd92ee692560254300720aa5361c4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:25 +0000

    blind zero bear floor wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
