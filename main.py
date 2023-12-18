d9f0015ec0b924047a3b5f8ee6c6077f96d90832
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:11 +0000

    trick strength programme pour tune

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
