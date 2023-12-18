11b150a8753983a6dbb7a5e3ebb7915c9912faab
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:35:31 +0000

    tradition stand trick aware beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
