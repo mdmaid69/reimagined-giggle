7daa490de625846512074253b3ba687bc86949f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:30 +0000

    trick clear measure once touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def is_even(n):
+        return n % 2 == 0
