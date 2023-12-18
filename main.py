1a8fc32c89f3eaacff586a4b77cf13636be1cc2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:03 +0000

    feeling style dress own section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
