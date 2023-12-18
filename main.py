59f883595c4774d7dc4fa52267f8d471bb60093a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:58:57 +0000

    order tall about typical plain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def convert_to_binary(n):
+        return bin(n)
