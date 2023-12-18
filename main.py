10f5f5a4650a4aa654cccbea90aec8d1b78043a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:51 +0000

    perhaps dry holdout light happy

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+{body}")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
