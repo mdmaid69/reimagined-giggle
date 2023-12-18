7febd906338bdb72bede1f472d052674d9f0a141
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:01 +0000

    apply rid lower suit permission

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
