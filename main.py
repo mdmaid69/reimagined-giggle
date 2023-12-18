e5a05cbc41a39c8aa1b65dfbf2bd7a7cd09f0cb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:12 +0000

    clue twist artificial indeed point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def square_number(x):
+        return x**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
