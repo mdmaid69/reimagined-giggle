ecb60fbbb1e569b3d0054a148870828a98737fb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:12 +0000

    protest patience holdup mend drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
