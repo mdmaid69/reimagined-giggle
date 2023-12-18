9dac71861b69f45ef108b5e2742fc95a237e878e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:07 +0000

    contribute surprised jump always whom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
