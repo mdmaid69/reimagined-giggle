cfb48a8821eab161d5f01609ff371df0085a5fb0
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:37 +0000

    comedown push carry fond block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
