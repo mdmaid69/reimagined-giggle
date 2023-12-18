e4e37521128e1221978fe6e98416ed52676f0676
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:03:16 +0000

    wash course zero professional badly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
