0d9d9e5ddee83abcb4768542faa49999f61cbe79
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:01:21 +0000

    confusing press score mind example

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
