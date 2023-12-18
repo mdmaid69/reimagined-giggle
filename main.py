68e8056cb3ff3c25a618d0acf92ca33ce9a185db
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:43 +0000

    objection minor compete distance them

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
