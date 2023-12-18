1a3a6422b2049873c1fb3d65c507be94e1de6486
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:29:47 +0000

    steer comeup unite not flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
