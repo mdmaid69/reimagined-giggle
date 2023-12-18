bf63f350c603f7cf970f3cdbfa050cf7a98b5b0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:14 +0000

    trouble spring mind impressive finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
