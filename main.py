9c44497e375ace20a18e6365da1a8bb8f4c12c94
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:29:51 +0000

    the before evening noone Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
