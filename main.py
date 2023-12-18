ca92e19849f8eff0daa88384264d36b6816bc80b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:43:50 +0000

    then play ear benefit badly

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_length(array):
+        return len(array)
