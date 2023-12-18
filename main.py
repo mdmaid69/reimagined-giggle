b11952b8a3e4ebe6fe6f3d7105b04915b57da642
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:24:47 +0000

    breakdown curious claim phone connected

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
