8954a058e9f63b1997fed942ecaba07f6173bc70
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:40:22 +0000

    thinkof bush under transport wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
