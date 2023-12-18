824687b14a92eb544830b48c975171e1823be003
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:23:23 +0000

    yes mistake great his score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+print(sys.version)
