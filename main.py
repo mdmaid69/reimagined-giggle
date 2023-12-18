81c1fa3d752d724baf2f08e3b404217e3e178ebe
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:07:33 +0000

    community degree centimetre soak base

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+print(sum(range(10)))
