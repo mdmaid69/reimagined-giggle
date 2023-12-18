e2d8d6eb8fcffc2243a36f3724c2c5a25e571e2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:46 +0000

    excellent then vote month passage

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
