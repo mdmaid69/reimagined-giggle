92d93af8b46d02679d7878101e48375d0d0b20b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:52 +0000

    deal form bag mainly here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
