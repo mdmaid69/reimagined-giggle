ad2f1f643be843cc3a97771b250d40cd8bb61371
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:55 +0000

    discuss song blue embarrassing attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
