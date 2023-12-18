0c6f44b8eab2b6b08bfbe1c9968c0883265233a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:27 +0000

    measure common rough complete even

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
