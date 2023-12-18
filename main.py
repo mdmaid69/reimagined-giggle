e6787d3a41c64fbaa68cc09b1e6bd43a0fb14140
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:45 +0000

    past fat open chance underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
