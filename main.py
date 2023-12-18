5f3baf46fa1305101650f0152251babc81d988f0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:31:48 +0000

    bottom wear find blind drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_current_date():
+        return datetime.date.today()
