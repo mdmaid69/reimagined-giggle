e5f9d6c90fbf404e401d96006266566088285dc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:27 +0000

    record control clean present tread

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print(sum(range(10)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
