c2a0808a8a5cd3bc45ce0f1cbaf3bb2b1fd02ae5
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:21 +0000

    thin improve intend back exist

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 i = 0
 while i < 5:
         print(i)
         i += 1
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
