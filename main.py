a0035fc6b79bd772a4bcb1d4e5909fa8d15b78e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:45 +0000

    easy wherever Nn period equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
