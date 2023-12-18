b59c51f7143743fb08412a859b666559ba9beaa0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:00:38 +0000

    breakup spread dark solve business

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-for i in range(10): print(i)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
