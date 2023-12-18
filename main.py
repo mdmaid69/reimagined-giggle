7c1af7d040c13e3573ec794b944991d5d0d410dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:31:52 +0000

    whichever fast art usual order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
