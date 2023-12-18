a7c03234c7a656456b7f3d185afdc91d6dc0666e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:56 +0000

    Zz nature test certain plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
