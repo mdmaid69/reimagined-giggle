fea5fb00458bc0c9b6a639ed0c5eadc33fcb4336
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:02 +0000

    interrupt mainly welcome yours suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def divide_numbers(x, y):
+        return x / y
