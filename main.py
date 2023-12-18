9e95cde0fee4d3edbeef64731d7170d039d06d96
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:46 +0000

    pure exact exploration market shine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
