7bf7af7bcbcdbfd5d4768aeb2dfb06d7f9cad739
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:02 +0000

    term unlikely race manage beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
