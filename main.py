1e3eecad80c3bec588d60926b7b8bf50f6904ff9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:14:48 +0000

    perfect pour shock public nowhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
