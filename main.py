4cda6a6cb000e31a45f75ac05d7a6f8d5c1bec55
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:00 +0000

    nowhere tragedy goaway mess invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
