bf5b204c25b0c09d502af9a2cf106f4187c0bd1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:12 +0000

    concerned wide perhaps slight plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
