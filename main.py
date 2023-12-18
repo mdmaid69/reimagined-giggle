97926573b5b6e984057df5fdbd0e188cd3801573
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:34:50 +0000

    bend workupto excellent opinion vote

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
