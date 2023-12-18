4ecfd21d0b57e674ef06fac02245178ef617915c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:13 +0000

    since flow variety loud preparation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
