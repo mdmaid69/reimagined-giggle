81a5951a1208e24835b4351c75634f7b95115bb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:11:36 +0000

    fierce disappear discussion matter from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
