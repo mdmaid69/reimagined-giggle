6171352c3a6343b691d4e4f4338ca85e07765893
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:37 +0000

    congratulations sail load winter means

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
