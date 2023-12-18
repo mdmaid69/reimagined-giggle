9c35f98b3b84f1ff0ed1328f9ac8b243aca23bbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:02 +0000

    no private fly people play

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
