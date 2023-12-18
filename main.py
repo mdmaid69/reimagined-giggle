3acbb44f31eea469d1f34f425719cb4d4f19bb5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:24 +0000

    too fun prove matter artificial

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
