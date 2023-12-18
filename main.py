b2dc85aa49a736222db406a3f61453e9118eb1d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:34 +0000

    effort dead part unless agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
