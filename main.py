25caba73fe7beaed255e5b0ca54fedafe5e4d690
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:16:47 +0000

    not example cost responsibility disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
