81ff37719ba6b02049d00e7a7716b20d695b5301
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:21 +0000

    company perhaps public free sting

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
