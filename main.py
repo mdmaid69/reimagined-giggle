d4e8a050c4918291b52c47068dcbd60cf6deb8ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:59 +0000

    tidy rub speed like disappear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
