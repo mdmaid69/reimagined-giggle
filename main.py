3c2807e67d90f5a876a03f4739bd35fd6bb7add9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:26 +0000

    declaration experiment belief firm traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
