cec94493f2171a31c3d9b6adbaf20ba5e77194a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:23 +0000

    wall talk will autumn dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
