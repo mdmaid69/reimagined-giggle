40c050e4d02d60c6fbc231c576f031602b4189a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:08 +0000

    whole safe bang please downstairs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
