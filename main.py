6233509618721706ce45b6fb61731adad60ee114
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:21:32 +0000

    tell laugh breakfast die company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
