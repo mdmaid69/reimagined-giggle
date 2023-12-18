a774919d18ccf32beb0eefa2ae3ec574719f5077
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:34:19 +0000

    ocean juice many activity lot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def convert_to_octal(n):
+        return oct(n)
