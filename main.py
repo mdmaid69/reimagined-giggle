e3c891f4681522be90107a3694e230e62b8b8110
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:24:43 +0000

    distance closely separate themselves holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
