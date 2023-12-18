c9518f3ff95e6a033cf69c7e97872ab2cea4e1c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:28:37 +0000

    introduction relationship free would score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
