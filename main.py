fc96d97d6ae4158d0d3a41b58cc6930b3fa11486
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:16:28 +0000

    touch freedom pure full lie

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
