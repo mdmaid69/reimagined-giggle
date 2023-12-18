5a67ca69f78d6429051b42b19855dd10ef7e75da
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:38 +0000

    vision play awake rise own

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
+  def convert_to_octal(n):
+        return oct(n)
