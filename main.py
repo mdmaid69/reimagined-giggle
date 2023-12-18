95e3a8a624913964d752d6e2a490ad71d6e3dd81
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:54 +0000

    introduce number interesting when fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def convert_to_octal(n):
+        return oct(n)
