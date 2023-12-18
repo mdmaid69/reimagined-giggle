b61c6391c34dfed423f331da6725bfc775a0f90f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:11:46 +0000

    event original bread breakinto never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
