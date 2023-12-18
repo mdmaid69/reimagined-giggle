63430922146e6b35608547a0b0bc3e1eb1da7106
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:53:16 +0000

    to wear lot scare male

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_power(work, time):
+        return work / time
+def calculate_area_rectangle(l, w):
+        return l * w
