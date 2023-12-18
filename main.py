7f6cd52d70a43cdf20b3ff3ea66f8e357a1a1020
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:59 +0000

    platform teach every table rush

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
