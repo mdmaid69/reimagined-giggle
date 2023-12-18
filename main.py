6d251ff9dcddf9d69fbd35dc43ddcebf6ccb9a21
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:12 +0000

    precise immediate Kk workon win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
