36ece8f2119841dee7b9a356a6ca0d908d563854
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:19:27 +0000

    valuable one afternoon favourite meaning

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
