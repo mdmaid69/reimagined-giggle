d7b69270f5e71d66e7490c3a66844f167d6620a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:39 +0000

    rid lift include callup sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
