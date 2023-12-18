f6d016961406db77dda562ecf6a43fdc6a33d05d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:26:29 +0000

    tongue cap team Cc extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
