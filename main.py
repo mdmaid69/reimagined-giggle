6cc8b29ff3a02a0d4769a9760a549f25ba88d96b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:19:28 +0000

    even message sail unlucky love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
