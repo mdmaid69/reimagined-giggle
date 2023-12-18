f7136ca3322ba162096eee424b5aed3fc2d3b2af
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:18 +0000

    passenger equal ticket protest contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
