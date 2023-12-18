b0d9a2409015a9dfc71a91c5534fd28ac9d15c9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:09 +0000

    block sink justice capital record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
