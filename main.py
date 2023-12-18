04b986a0c6165b64ee4c67b063476fe682a9dd68
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:03:11 +0000

    before fail fond wherever ticket

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
