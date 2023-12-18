f9463aa45c6b38518e2ea68ad4019591f6eec8d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:53:18 +0000

    accordingto smile how doctor waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
