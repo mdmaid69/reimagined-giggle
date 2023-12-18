03dc44de21b4275ba39543127f403958aaf24d42
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:59:27 +0000

    die to art ride strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
