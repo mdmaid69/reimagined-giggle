e562a3bb587bbb23d87d8c6acab563deeda51d9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:31:44 +0000

    account strike strange finally behind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
