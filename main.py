023a55c9d241c7f4320c17d469b1edba11d284ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:59:52 +0000

    shock chance force exercise pronounce

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
