ce17d4e737da5366e6721dd6092d4c067564a4f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:16 +0000

    but available minority pocket right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
