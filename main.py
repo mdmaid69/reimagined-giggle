86fc6b3d704bcf69fda4fef6bc1f344b142df230
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:58 +0000

    still Ll with fly spread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
