924f6eb452aa1ffa3d9aeaded3dd4a33266131c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:12:40 +0000

    away exciting wrong window considering

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
