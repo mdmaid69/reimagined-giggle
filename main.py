eaa06e371f988de611cf2cb74b99ffcb045b260e
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:04 +0000

    talkinto upwards answer hold develop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
