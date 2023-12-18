5ae7786c5d3a8d2b2d96ce0dff64decff28f48f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:57:03 +0000

    refuse dance under soil apart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
