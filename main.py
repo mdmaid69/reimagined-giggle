38c4e93c2597ea180160955204b8857128b1f4e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:01 +0000

    for trust system have lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_as_str(array):
+        return str(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
