8c3f04aa04e82748913bf790fc0aa007ca141750
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:33 +0000

    satisfactory collection belong discuss letter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def extend_array(array, iterable):
+        array.extend(iterable)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
