cb88e3275451c86017ab4630f7205aa58f1945e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:18 +0000

    behind rub appeal film include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def print_python_version():
+        return sys.version
