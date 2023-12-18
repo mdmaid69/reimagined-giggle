5896172527df44136ff683a235484137094d1461
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:11 +0000

    feed country exaggerate light lay

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def count_words(sentence):
+        return len(sentence.split())
