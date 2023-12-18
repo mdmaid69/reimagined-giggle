10a1cfad1323f4ed27a52bc7782942070e9ea693
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:15 +0000

    cap previous shake raw call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
