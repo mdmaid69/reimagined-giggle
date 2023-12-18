9d62e1057121050376c3cd641e8a71bc90c460e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:53:09 +0000

    feed sound evening proof maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
