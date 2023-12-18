6ca19711cf77b10b5f81e4fb221080e646317975
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:17 +0000

    drop street study general shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import sys
+def exit_program():
+        sys.exit()
