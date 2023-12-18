4d0a370d7ce7906512191a2a1c26f10ee4231379
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:44:38 +0000

    translate modern nonsense exciting reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
