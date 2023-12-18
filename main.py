ea3c8f8410e3e6d9f701e28df83d7fbc829a6112
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:36 +0000

    door children coffee system depend

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
