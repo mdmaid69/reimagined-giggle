84340c7787a8178ae10785b856822d75e8b72e3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:15:00 +0000

    she they total consider mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
