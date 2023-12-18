0399af6035a12e5ab5df8e2d8b6638e49572b7d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:58 +0000

    smoke concerning steep order 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
