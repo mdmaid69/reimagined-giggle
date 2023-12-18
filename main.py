17ef25707dc37fd076cd59342059d5ee0bf08a92
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:36:02 +0000

    war unfair whenever journey sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
