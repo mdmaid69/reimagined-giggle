971dc7ee5f1c8d9faff4bf625dbd0316a6f7bea1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:39 +0000

    cry cold world above scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
