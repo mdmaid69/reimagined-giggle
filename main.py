cb6a5be7166dc4a08d3175b488d3bff3b60bd6cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:38 +0000

    practice then breakdown crowded sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
