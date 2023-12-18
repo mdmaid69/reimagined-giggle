4b7a01fb233dac915fc06db5cbbd529ba88db5da
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:16:14 +0000

    move handle where search since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
