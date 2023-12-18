983e8ef462089b43be558d035b9549c1226c1ca9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:33 +0000

    long beside practical leave weather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
