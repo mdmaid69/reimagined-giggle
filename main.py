2ee3670a017dd105823b470a6ee752fc9aad2c03
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:23:49 +0000

    bear refuse message choose fit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
