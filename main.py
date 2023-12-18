fbf7b5675ef050b20c00401fffdbe15f2eb6b2a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:31:01 +0000

    push soil uponon snow replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
