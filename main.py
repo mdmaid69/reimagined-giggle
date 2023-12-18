de272119ab67f4f7ab85c3a7a2a28c71f53654bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:47 +0000

    meet temperature foreign speed wild

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(numbers):
+        return max(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
