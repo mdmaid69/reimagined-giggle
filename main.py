94bf3312b689d5b75eb4bfac604023798b1653e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:17 +0000

    suppose forget clothes hide generous

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
