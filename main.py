108d52393ea3c3cc2268f8f560451550181cf4e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:40 +0000

    suffer help power seem always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import collections
+def create_user_list():
+        return collections.UserList()
+def count_elements(lst):
+        return len(lst)
