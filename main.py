b4e809265c97ea3f048d88cd1d3584b0f1ec31be
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:55 +0000

    classroom listen give that pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
