cb4dc075e10012d453d40f44f658a245f8e61f14
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:55:11 +0000

    sorry perform survive plastic tragedy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
