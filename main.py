de003c4aff803ac0b3344a6d3b22a1f296e4b58c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:36 +0000

    he greeting straight aim about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
