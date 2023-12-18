9974bd55de63c38d85888824ade30ff40f30a2bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:25 +0000

    bar can waste side contrast

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
