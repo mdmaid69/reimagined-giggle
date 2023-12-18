e20551f7ba0f7b253c0ceb5d30ead6c0bda30554
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:38 +0000

    sight somewhere soft imagine piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
