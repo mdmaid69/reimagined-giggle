7268df305cf35314bea48895a358a3aeec5ed02b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:10 +0000

    land match after mistake large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
