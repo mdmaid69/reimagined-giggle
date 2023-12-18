9b2218ac4229d009669278b967e767395626da7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:31 +0000

    charge handle arrive skirt young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
