5e0fc7579de3438386866be318a2e2cbd6818b20
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:25:17 +0000

    watch comedown wish base authority

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
