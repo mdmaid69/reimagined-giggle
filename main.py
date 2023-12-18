07e5fdbcb8b9ffef67fa6222b2968eaca9a64aef
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:57 +0000

    attract share flood frequent tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
