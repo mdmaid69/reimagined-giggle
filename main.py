0e527ec2430b1987168a28b32246e212cc479242
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:04 +0000

    principle freedom this minor form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
 def is_even(n):
         return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
