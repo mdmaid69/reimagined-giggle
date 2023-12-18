8c8cf5f547ab243b9af8c62bb43b28e06c517774
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:55:22 +0000

    passenger bringback judge be tonight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def reverse_list(lst):
+        return lst[::-1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
