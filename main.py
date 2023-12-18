7426fbec6c10429e0dc256ff4ca0e85c51f3a5e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:50 +0000

    battery contact slip keepup powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
