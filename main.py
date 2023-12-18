e8a05185f701305762d2e57e81bad09759a11975
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:20:25 +0000

    set distant sound convenient attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
