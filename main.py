9abe07683a513821e7fa087c32cfd3403102ae42
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:14:05 +0000

    most noise dry ticket limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
