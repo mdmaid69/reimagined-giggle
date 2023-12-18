a4382bddf195ee9a8a34c3d9ba7a4fcc3b7f6f59
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:11 +0000

    some set soil until complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
