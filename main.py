f300814ac74e44ebcb06b6f13c65fb99935c4094
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:01:23 +0000

    post vote react bet platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
