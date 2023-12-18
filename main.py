9839b2e73362134fd8d2cbd648ce33d516960bbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:00:31 +0000

    reserve description suck dangerous tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
