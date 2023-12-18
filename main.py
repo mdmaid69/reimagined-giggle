ed984f48cd9eaf29d5f6bedf01fb2faa1c232fbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:38 +0000

    concerning achieve trousers mouth black

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
