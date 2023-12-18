eee52f1977e92eeb3a395d0d89b12ff7b425cb9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:35:03 +0000

    opinion extraordinary fierce complaint after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def clear_array(array):
+        array *= 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
