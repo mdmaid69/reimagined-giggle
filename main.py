fedde70607918f8387af9813165900ed1db2b034
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:33 +0000

    flow remove sale Nn cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import numpy as np
   def calculate_variance(arr):
         return np.var(arr)
-import time
-def get_current_time():
-        return time.time()
