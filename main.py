b3d3b89d03cd190d8596cde0b65f12c5d4e5f831
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:17 +0000

    disturbing spoil sign this anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
