f926ec5285a5384548e889c04aabfcef4b763042
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:28:13 +0000

    crowded test unable oil whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
