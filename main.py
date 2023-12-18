46adf68be9e5a6dddba4863181a06b073248b700
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:03:18 +0000

    adjective task breakdown tongue everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
