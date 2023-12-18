9df08995c26cfa714a5e8d1bbf99e10c5a91d506
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:46:27 +0000

    phone nonsense distance goaway mail

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
