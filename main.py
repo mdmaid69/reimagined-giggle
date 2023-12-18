3697e7659784b43d66948283106ef50a07755109
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:01 +0000

    walk suppose steer attach 565end

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def cube_number(x):
-        return x**3
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
