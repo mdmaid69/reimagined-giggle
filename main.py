ebcf99733b855a6c88bb3051ca3a6b93cc1f37e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:59:44 +0000

    promise centre connected occasion bank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
