00a89e69c55a7218c63daf39ac43a80997d8c508
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:48:15 +0000

    rank order certainly advise connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
