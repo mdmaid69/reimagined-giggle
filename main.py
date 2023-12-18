3f4200e8604fa1747bde432aaea0d76d9e98dde1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:47 +0000

    please contrast charge equal get

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
