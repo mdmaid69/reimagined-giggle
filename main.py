90ec092c76fd71757ee2cf2b94a1796d62881d4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:14 +0000

    safety getout shop but pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
