532cd3d5412101fe286bbedacda34595cfa17e4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:53 +0000

    little evidence lookaround along start

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
