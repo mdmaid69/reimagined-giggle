ec669cd06b0db739bd3907dc67eba326efd0b983
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:29:45 +0000

    565end better last go keepup

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_typecode(array):
+        return array.typecode
