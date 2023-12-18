624e9e860caa96e98d1a5660112f427983be6d33
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:34:56 +0000

    clothes terrible part share tape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
