bdf3ca08d3425a5a3e23afc304d6070d0c724dea
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:06:37 +0000

    appear criticize hope stare excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
