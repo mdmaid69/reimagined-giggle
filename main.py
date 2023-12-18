c48d7a37ec54e01fe68719f1d5b04311e3af64b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:04:39 +0000

    computer lay quiet step underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
