ce84f73aef314861f8b9ed1e03383024f4210ec2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:45 +0000

    better firm possible definite fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
