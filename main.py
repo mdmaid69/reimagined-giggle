0717dff188cb23feffde8aa87365b9c937667975
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:28 +0000

    fat relief Pp combine everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
