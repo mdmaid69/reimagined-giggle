421179e1a2f69869de56fa2d4a27f872d4f84d51
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:47:48 +0000

    fact name doctor finger shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
