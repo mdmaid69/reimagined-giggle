74d061ceaf22a85118997d1f372da2a5e93a9612
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:02 +0000

    knock argue wipe never stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+x = 10
+y = 20
+print("Sum:", x + y)
