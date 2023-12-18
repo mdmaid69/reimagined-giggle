6e50b32bd92660daaa93af90659ee4516d251cce
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:53 +0000

    fault fight none tight breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
