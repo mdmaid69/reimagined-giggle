67dc4a955c236073dd91439530c1536d0be0815c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:22 +0000

    over put bar live home

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
