5ea84f19840c2502702f84825e5354fa4831b253
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:09:44 +0000

    yourself lunch praise basis improvement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_acceleration(speed, time):
+        return speed / time
