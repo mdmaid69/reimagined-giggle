0f7bb375e0276fafa5a94ae09a60d5300b099289
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:29 +0000

    move formal strict claim represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
