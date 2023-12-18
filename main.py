d133e4732caedef6adeafcc33bb97b9d7321eb93
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:24 +0000

    dream he trick wake confusing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
