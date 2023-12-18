b6178b9d697e9188fe0af9c02ad2786f6d2b7450
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:29 +0000

    anyone flight spite OKorokay blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
