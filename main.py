9649c80adfd8ed946bfbaed28e6afff2e25366d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:15:38 +0000

    on ask file accidentally each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
+import sys
+def exit_program():
+        sys.exit()
