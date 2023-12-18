6d07a6ddf94b850816b0aabd47f2e0faa3522d00
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:21:41 +0000

    healthy dangerous today notice hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def multiply_numbers(x, y):
+        return x * y
