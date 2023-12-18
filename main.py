04afadd7653d6b3aacf81febe541fdc93f902b24
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:59:30 +0000

    search far reasonable and pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
