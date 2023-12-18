cb7f3baed5d07a3bd510df8d38e7d143332409a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:15:19 +0000

    mine than scream ahead weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_unique_words(sentence):
-        return set(sentence.split())
+name = "Python"
+print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
