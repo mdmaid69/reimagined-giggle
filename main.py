f3cc10d5fe314ecf4d5fc8657a2d088d4884ce7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:50 +0000

    answer transport well fine sort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
