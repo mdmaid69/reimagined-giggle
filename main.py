1e6ad54ed9fc5b43b2b8122145cb6b89c126a95f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:44:22 +0000

    comeround getout beyond characteristic block

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
