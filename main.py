7b37b3c764a17a120c25d549106f8f256664960b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:03:14 +0000

    once fast careful loss fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def reverse_string(s):
-        return s[::-1]
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
