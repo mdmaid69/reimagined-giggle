f4a4875e436e9253be34804ef2ecf62dbe1c871a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:55 +0000

    both top eat pencil pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
