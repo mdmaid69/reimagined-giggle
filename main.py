a3955a5b67720e57c23013cfe0e50b99574a1cb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:35 +0000

    same what quality bottom obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
