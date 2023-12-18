b538a3bb5f0044fd4a8a5007736b572baf3a8349
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:12 +0000

    steady left like breathe first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
