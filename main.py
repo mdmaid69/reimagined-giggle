4212d94efebc33467db6ebf5457fbdc4053a495f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:33 +0000

    night speak opportunity age contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def count_characters(sentence):
+        return len(sentence)
