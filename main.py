3fb60735b72ea37930ed96080a08912c5b4244f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:57 +0000

    splash regional scream anything suck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
