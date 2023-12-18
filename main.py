3cf6fa294f6ab10674f451b0d748b3061e88ccc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:21 +0000

    comment add song get recover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
