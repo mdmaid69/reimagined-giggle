255c43fcebebd4671524f3509d6d3bd55443b07c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:05 +0000

    getback wing quality passage wire

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_pressure(force, area):
+        return force / area
+def calculate_speed(distance, time):
+        return distance / time
