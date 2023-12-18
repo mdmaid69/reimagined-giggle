45d08354fbec146d491a810bce74766b880d8097
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:50 +0000

    innocent rob exchange behaviour different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
