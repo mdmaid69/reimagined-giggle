40e1bdeb42c72872c79595f4fce02bf4bb2c2a1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:12 +0000

    measure kick wipe buy allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def set_array_item(array, i, item):
+        array[i] = item
