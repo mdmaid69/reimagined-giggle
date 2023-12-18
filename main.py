dc41d23c7ab686eea07455b08412315ab7da9a6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:28:38 +0000

    neither storm flavour hate sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
