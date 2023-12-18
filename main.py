e6c098879f4d81a99d94e4608cede2184bbbbcfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:06:12 +0000

    human responsibility ability male face

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
