1da092e7985f0efee09ed3ef0fcbf6041e3c7bcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:20 +0000

    ought definite food slow push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
