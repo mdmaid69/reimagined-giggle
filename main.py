12d1e6994871afc2d7a12d08569dfc954d8e409c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:40 +0000

    deliberate cost advice each write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
