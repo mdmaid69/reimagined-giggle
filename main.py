8e68afb25406ea8cc92c731b309962702e119474
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:54 +0000

    prize colour centre model type

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
