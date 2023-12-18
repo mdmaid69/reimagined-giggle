3d1877f4e3441d23fd361b5cb96e58edc942385b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:40:20 +0000

    noone repeat often badly gift

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
