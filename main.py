49926bbcff20637a28aa2266018b3e70e18c5041
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:30:00 +0000

    rid abroad pinch sit design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def add_numbers(x, y):
+        return x + y
