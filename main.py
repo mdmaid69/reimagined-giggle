db05a42d4dba0328f84081421c9ceace12de6f76
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:15 +0000

    direct custom seem pure slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
