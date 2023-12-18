0019211a4597ef99b25b2c5ae019ba258f6fe57c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:43 +0000

    see pile slight city combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
