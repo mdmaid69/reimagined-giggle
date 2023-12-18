b29f477787dbbfb0751c4ea1ca5bc348f3941096
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:16:48 +0000

    tour prevent everywhere opposite ask

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_to_binary(n):
+        return bin(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
