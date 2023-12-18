5edc10ae4298e1bb7c313f88202d1509338a2e7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:57 +0000

    excuse acceptable first classroom trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
