eb4bfb82246d7c3e178af06ace42ac28d42c3929
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:27 +0000

    suppose Rr call subject love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
