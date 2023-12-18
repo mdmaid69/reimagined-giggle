75159b5e8ab78ea3882c1cce41e17f98598d7a39
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:58 +0000

    not pot tread owe married

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
