3b9a31860caf1d2d7b34c99956c40423027b8cf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:55:20 +0000

    dark question light physical size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def multiply_numbers(x, y):
+        return x * y
