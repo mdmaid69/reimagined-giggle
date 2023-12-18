fc4bc7b60504999ecbdaa0c42e3404264b0477e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:05 +0000

    mind trick mixture check identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
