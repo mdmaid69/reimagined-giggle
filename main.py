322b7d05596935c09235e049c14d70ce8fa8d6b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:42 +0000

    content they high crime reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import time
+print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
