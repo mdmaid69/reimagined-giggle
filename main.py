3243545ff10ff0ab2f3d69ec13aaffeb6ac8c273
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:05:13 +0000

    information can lead office boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
