4632174e1c07e3e3854c6442ef602cbcc73e14bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:13:33 +0000

    before band size criticism workon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
