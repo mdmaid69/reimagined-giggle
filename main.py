c8f37e991a2becbe565ff261c2f8edb209b42e46
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:17 +0000

    standard mind trade somehow seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
