dba582360a4c14ae24e63d1e8d9b3ca2e767091c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:01 +0000

    exit believe loss over need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import time
+def get_current_time():
+        return time.ctime()
+def remove_duplicates(lst):
+        return list(set(lst))
