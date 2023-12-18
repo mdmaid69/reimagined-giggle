c0c7931f2cff0ec488dfcc7d2fe4c6760fa1976c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:06 +0000

    separate traffic late final ours

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
