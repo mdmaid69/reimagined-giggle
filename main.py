97153414fb5c1fcc00623140317ebee57d994e70
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:53:46 +0000

    union act house declare fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sys
-  def get_python_version():
-        return sys.version
+import collections
+def create_queue():
+        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
