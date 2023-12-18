82fd1db0bb9eae0afa056f3a4d37cb199354c860
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:37 +0000

    post over whistle matter down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_string_from_array(array):
+        return array.tobytes()
