e148247e92d8b1b1297819afb42577d47600aa29
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:39 +0000

    practise smell receive home guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
