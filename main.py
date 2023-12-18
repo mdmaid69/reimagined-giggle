a90b86401abb0ea5b553cec5f5bc54b1245232e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:38:36 +0000

    fat minute nearly legal there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
