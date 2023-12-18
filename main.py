ee519b60f058945fc527ea02defe037ed0136ee0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:42:54 +0000

    force means test stamp Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
