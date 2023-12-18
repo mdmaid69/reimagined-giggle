5dc71b5dfe6b794016dfbc2c102c2b6e51a580bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:45 +0000

    bank mine protect spot list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print(math.pi)
