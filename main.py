07155ce8cb063199cbc0c317bd3c71e96ecf1e4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:50:47 +0000

    catch protection belong skirt pen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
