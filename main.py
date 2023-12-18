67e580bceedd96c5eccad3b594b59eaa39497c69
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:35:53 +0000

    sharp shoulder needle lay battery

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
