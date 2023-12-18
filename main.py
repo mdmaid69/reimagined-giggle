9b21d9cc95f695bf6ac9877cc8e41803ad01724e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:10 +0000

    rent insist sting commit march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
