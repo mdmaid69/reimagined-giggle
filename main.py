8854df0e55bbdb2b4f9669564c0820809f1ab849
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:37 +0000

    computer destroy fear advantage along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
