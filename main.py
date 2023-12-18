7370eb799845ac6d1ca9c38c1d7edbc90de7b4dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:38 +0000

    to wire commit he team

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
