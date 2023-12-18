e86188e925fe0e4aef3907599e36ecef9547a490
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:14 +0000

    beginning 564encourage curious detail guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
