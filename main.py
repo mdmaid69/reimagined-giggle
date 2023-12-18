6dc604f44d13508aec87732dd01661128f9f73d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:37 +0000

    now opposite price rubbish uniform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
