d81fe9e1cc8052f5144afa9bbdacd2ee3d822995
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:50 +0000

    angry Yy forget workon emphasis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
