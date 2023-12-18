515b0f65ce0fc5bd2c5a8220d4b36156c5e7a777
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:37 +0000

    save since patience speak letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
