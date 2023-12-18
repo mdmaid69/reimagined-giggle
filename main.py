2c9d5252a1794bdedc6e9f1c6222bd6af69c5c92
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:08:57 +0000

    children in type free for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
