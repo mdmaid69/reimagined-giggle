b27d7feeadd3b21847fa01fef67697a40c7351b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:08:42 +0000

    wheel drawing either right furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
