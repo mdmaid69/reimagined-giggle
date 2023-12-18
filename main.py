c58cb214e9535939c404160f90a3772a595aef3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:05 +0000

    brave combine crowded protest getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
