e9c6021a3f77b29294c5cb3c3840a27a57b459dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:53 +0000

    opinion dead improve old right

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def clear_array(array):
-        array *= 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
