15771a82a0b0c5f698d97b6028a2e5a29872f0c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:25:34 +0000

    ride holdon accommodation family state

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
