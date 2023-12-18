2f1cc60784263248ca3c04f843eb2885a6a588b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:06 +0000

    yesterday Gg later reply who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
