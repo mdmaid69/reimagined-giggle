e9385df014bcd3047de19db8c83f693e993b2b84
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:57:44 +0000

    fairly beyond contents actual thorough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-print(re.match("h.*o", "hello world"))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
