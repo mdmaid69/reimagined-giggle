87be0f917ee39b67a48a16da4fe410bb8c836e31
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:30:50 +0000

    port suit first there cope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
