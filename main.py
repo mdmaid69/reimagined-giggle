23ae72997fd2ee21755ac6f0f2b5f3efa0d8b843
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:19 +0000

    drop question boat pause news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
