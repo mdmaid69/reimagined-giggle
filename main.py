392906a85a1d79af54b080474f4621be7e0a0168
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:34 +0000

    communication regularly spread basis lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
         return os.getcwd()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
