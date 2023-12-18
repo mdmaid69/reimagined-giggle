ca80cd7f1331ab6589c566e2a3e37fdd31110dcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:00 +0000

    sink own order legal justice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
