bafafdf05d8c1e87c6a3a766ea1e1d15a6062cc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:30 +0000

    stamp what board what wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
