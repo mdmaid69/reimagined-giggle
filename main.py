582e129b338ff5e872470bd94e2a7012918142d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:38 +0000

    Ff importance past fierce over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
