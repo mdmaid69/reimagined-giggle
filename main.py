acff5ffadcc8d78f85d8d47863888ab09566c749
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:28:59 +0000

    brother task else breakup talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
