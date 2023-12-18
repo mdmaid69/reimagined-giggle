06bd93f150bfda25f1cf8359c554248d5b452344
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:53 +0000

    butter transport laugh switch sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
