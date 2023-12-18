9e9ae0417a946490d4cccf9a1cfef90022b66935
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:27 +0000

    he that distant whom available

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+print(time.time())
