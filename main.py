0c61db453a1c8e645be71e77e83591fbd8be9ebb
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:35 +0000

    fresh exhibition flavour manner workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
