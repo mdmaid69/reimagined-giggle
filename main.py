125e753e0dd0be549c503d041543f4e025fde1cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:54 +0000

    judge gap mistake ready force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
