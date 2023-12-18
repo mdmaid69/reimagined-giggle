8b749cfedeec49dd88e3b70f0fa05dd00593d52e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:47 +0000

    produce check upstairs letter circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
