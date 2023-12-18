f47ca6d6b23d05bb402091fdde8573bd91d9ced2
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:04 +0000

    last often now young balance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
