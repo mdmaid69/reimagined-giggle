302a4b4e57ecee50c7037502b1a78c6317ba5c9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:28 +0000

    goout present pool hardly there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
