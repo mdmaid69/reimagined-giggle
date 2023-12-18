725632a5fbd91c0507501f0c7661c7b63f42830b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:58:40 +0000

    recent nigga cut weigh university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
