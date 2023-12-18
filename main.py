cc50c610c54b988fd4a0840919671e71d65302f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:10:39 +0000

    own left search better cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
