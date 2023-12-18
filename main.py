557e6eb8c6eec5e0304a9182fb23cb0dd7fb69df
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:25 +0000

    favourite concerned advice warning badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
