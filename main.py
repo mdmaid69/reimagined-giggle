5e64392cf715acdf0aaf7f9aa938242439046be5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:36 +0000

    she advice repair nearly argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
