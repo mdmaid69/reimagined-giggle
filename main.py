8e112a414a0eecad5c31c17ed16496632d2fcba8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:47:59 +0000

    seriously themselves dick 570enjoyable shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def count_characters(sentence):
+        return len(sentence)
