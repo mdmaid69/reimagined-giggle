65d4e5437bed4a70b7788e9bb5a5651b42f30c03
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:22:13 +0000

    stay difficulty also pay equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
