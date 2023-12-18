0c4e475cd9d7cda06a08dfe0c877965eb4f1e912
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:42 +0000

    heat boring soil invitation them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
