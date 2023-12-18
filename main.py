6bdc54cf1747c93d698464f3893c940d82525168
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:13 +0000

    drink extremely scheme copy soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
