299e3fa8a41b3d708d93bd3375d84a6c75ab1e51
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:07 +0000

    amount Kk praise speech phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_volume(length, width, height):
+        return length * width * height
