aa39b30035040f3b2ab68e4691172ca5a921377f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:08 +0000

    her really holdout fix pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
