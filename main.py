3edab23b89a49b6674ec328359f89ac249678418
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:39 +0000

    trial house across light curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
