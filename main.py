b8863ad54d2f05ca19c1a1b79ceca7cc818d4704
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:48 +0000

    this break company system spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
