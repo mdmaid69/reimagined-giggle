b701e2599ad266e34f7ae4b307d1fd4ef038ede7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:20:23 +0000

    wish amount trust section seat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_cosine(x):
+        return math.cos(x)
