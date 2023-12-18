f4346bdc386dcdb3234f19c68050dc7fc9bdce6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:19 +0000

    comeback brain next popular print

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
