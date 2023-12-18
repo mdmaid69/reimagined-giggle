b4472fabd2c108a48d74a97c7a0e0de57ce4806b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:42:26 +0000

    love exam even well platform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
