8a606048dab0b1c4bdf07177b62dc6cc0e531989
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:51 +0000

    carry great boring perfect breathe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
