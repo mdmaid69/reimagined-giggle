492e491335c867d0d73a65092cc3e968d9e729e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:47:07 +0000

    once then variety achieve face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
