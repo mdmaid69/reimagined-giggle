d629eae652624c3189ac8197d67d79fde2218ff7
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:47:42 +0000

    strict real supply reduce loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_exponential(x):
+        return math.exp(x)
