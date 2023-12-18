66d3c06824c1beaffbe5f85b1e1e0f92fc36f658
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:19 +0000

    thorough mind makeinto fetch radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
