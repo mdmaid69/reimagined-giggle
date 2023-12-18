1cccfb82feace72c6037c053ad439d2c20bbeea6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:03:55 +0000

    giveout like illness person crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
