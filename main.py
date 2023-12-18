0443d55c1af3b46168e97ac75b23f130566b6c66
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:51:03 +0000

    shame later before fast passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
