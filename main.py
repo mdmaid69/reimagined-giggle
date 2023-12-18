9666312cb7d6f31e8fa3bb51b8bfe6223859d3f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:15:57 +0000

    Gg father prepare roll unit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def convert_to_binary(n):
-        return bin(n)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
