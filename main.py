091a4030fb3a6cc0a6e7bc8261311621ce78062f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:36 +0000

    moral hollow appeal attempt adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
