f17cfda543899803353a046818f3f9f1a9354a38
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:18 +0000

    new interest miss match luck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
