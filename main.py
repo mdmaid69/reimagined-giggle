1c13a1e19a93a33e089d9f4046669c0ed8becdb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:40 +0000

    degree taste pull list hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
