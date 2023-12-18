37f9f00dd21dab4332eede2866efc01a074994e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:35 +0000

    speed hold pretty regard married

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def divide_numbers(x, y):
+        return x / y
