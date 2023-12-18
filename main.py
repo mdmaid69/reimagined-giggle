eda4cd4c0527957a8f4de5fde50d9a793be552ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:10 +0000

    kind runaway serious pleased breath

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
