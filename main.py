3591f77175e6bf8a5d3a1638457070ae923a5cec
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:20 +0000

    imagine somehow shall Bb upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(5):
+        print(i)
