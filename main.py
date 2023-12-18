dcbd671da5b2d630a937e3acbac4c4f001365acf
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:24 +0000

    patient flood consult themselves listen

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
