764c39db3ec62cb61786f4b35fd31a8270cfbf87
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:40 +0000

    sleep above ready near suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
