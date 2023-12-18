58a1c577070d52077eaa66125f4306f882015f58
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:19 +0000

    gift cheek guilty interview steer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
