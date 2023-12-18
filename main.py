c701bfe09b77b44e1186a68e8a65a4779c8d6485
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:43:35 +0000

    flavour approval scream connected explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
