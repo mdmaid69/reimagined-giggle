054e55015e7b4e878a2fabe7a96eb74a2a1ace3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:10 +0000

    content grateful university my up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
