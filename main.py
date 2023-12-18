a4b8bc6f4ae523d578d976012a5d1468611ed38b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:27:50 +0000

    especially description perhaps weak breathe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
