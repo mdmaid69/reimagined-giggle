92467b7f58af983cf2dff02baf524f1efc8053d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:03:08 +0000

    treat refer smoke glass strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
