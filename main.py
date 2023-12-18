abeecf3def601619c22bca264dfb87f5b8e6f771
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:09 +0000

    cope too leave earth cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+print([x**2 for x in range(10)])
