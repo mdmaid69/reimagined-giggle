83a075418c143d279a5a7eb899711e735ba99072
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:34 +0000

    issue several lost classroom machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
