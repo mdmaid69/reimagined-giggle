94fbe40842e5f7bbb3254e5dac3563090b02ab98
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:34:33 +0000

    demand loud gooff everything strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
