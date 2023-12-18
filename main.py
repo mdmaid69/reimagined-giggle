3a4b323c42b1efdf6390e0d6246dc0deacca89a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:34:22 +0000

    mistake organization it second insist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
