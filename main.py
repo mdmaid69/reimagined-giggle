9eec6d67262e30d070b2e9187ae451ed324e14ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:20:58 +0000

    difficulty project most slight petrol

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
