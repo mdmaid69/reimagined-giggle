a53c1b1bb2810db152e936269cce04dc679ad890
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:43 +0000

    giveup spin mark team along

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
