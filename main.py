1b84a3c1fa84288f047db8c9b03d4514aca8273f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:57 +0000

    keen division familiar temperature either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
