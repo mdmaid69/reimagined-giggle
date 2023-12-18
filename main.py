1915199e53619c1592e51bb5af4db3802c6e7064
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:14 +0000

    clever lock otherwise short scream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
