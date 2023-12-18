c30517156ed1a17c0c11189b8f1e80c45fe0d670
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:49 +0000

    basis wherever learn upwards hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
