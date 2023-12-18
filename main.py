cfa26c2b31bc2501768247040be8ab05de268aa2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:58:19 +0000

    since concentrate whole hurt lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
