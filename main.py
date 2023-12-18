8a4ff505484c5a9f4b3b1b6f50d5916ae5f65fc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:22:54 +0000

    comment confused yes back regular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
