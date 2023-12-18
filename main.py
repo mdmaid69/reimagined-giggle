d6f3ee5ed70e09968f9c145bfd63084057b28da3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:01 +0000

    yourself lost freeze callup black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
