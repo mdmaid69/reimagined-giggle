ee7cc2b40a57e6656b40a5e1ca22dbf5fed5a131
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:13:36 +0000

    happen moment store confident exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
