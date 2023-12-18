d58aaf4f577a87f4b0c558fc3653fc6c0120f24c
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:36:42 +0000

    reply regard coat record warn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
