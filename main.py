3790d2ea71f5c8c2b00cedaf6729cc9f8b5f18bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:46 +0000

    nearly recognize inside which begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_counter():
-        return collections.Counter()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
