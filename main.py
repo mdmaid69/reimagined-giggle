20c200ab100cca864fd98a070f546c2064370c32
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:54:49 +0000

    they shoulder train rain unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
