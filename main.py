6f9c4522d9b68dc85fa483c82ba764f79a46b29f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:05:49 +0000

    door narrow software rid badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def cube_number(x):
-        return x**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def get_current_time():
+        return time.ctime()
