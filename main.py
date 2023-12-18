ccd2234f6e8dc5bfc41b82a3851ce5316952491a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:14:58 +0000

    whenever distance business tidy swimming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
