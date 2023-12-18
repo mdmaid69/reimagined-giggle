2b18c72183063687c58a9e6d4427f90aaa1d0e52
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:05:49 +0000

    his sensitive temper safety but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+print([x**2 for x in range(10)])
