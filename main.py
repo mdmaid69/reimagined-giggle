5803eff2cb274b5be31e9d276319171951e60cdc
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:48 +0000

    after party land before call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
