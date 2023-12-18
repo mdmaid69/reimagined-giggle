4fffec56b4ec54ac1f0c5988bc46e8d202bd3d34
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:18 +0000

    operation impatient scream careful regularly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
