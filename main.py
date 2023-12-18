ece66c292663e40f058b070d5912cac99fa1fec4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:53 +0000

    fire friend in prize wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
