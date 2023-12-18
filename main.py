3bbb3f03702036425a706f399e62a826bc5dcc50
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:05:59 +0000

    view ear through ticket clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
