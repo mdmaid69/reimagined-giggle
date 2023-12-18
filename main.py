ebdc5553449c7a57be5e57d17d2167bc57dcd4f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:45:57 +0000

    afternoon closely identify use nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
