f547ea595e2f965c9ba232781798b335edc6b1e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:31 +0000

    me comedown win your look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
