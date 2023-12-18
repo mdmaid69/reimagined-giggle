4087e263519cbc153e577a4ce245564dcb5c8a11
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:43 +0000

    reduce deliberately strange escape forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
