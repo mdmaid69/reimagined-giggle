b12db4d771df2e9eed9de8067909309b603fb04b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:16 +0000

    seat authority there majority order

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
