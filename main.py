5b833f5683bbab662eeca3c7cfa7f40d5b3650c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:15:24 +0000

    complete class load radio equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
