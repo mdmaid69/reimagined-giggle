2834e8ee57d9480797f27bc52dbd718398e662bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:54:52 +0000

    advise thanks scream become talkinto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def square_number(x):
-        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
