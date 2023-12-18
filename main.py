6ff55293f8a18e6929b6563a42e0ea15a0159c37
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:18:19 +0000

    grateful century amount soil stone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def sort_numbers(numbers):
+        return sorted(numbers)
