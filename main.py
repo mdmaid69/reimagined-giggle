a03060c955e5fc0844faa1d4d544b547178a6c99
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:20:29 +0000

    runover tool complain rough refer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+name = "Python"
+print("Hello,", name)
