bb3435aa8bb4b8f70fad21790cfb022f403fd386
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:07:07 +0000

    dance pronunciation better national strike

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def cube_number(x):
-        return x**3
+for i in range(10): print(i)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
