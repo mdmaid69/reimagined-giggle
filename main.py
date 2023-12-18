777cce502bdb0fdedc055c331631570119abfbd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:15:02 +0000

    line century pot type winter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
