95167c526a0cd303ec0961b55aafe10d05c1f604
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:09:31 +0000

    me big available bury arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
