ab112ea2461cb35951850de1ed2898f0539a4489
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:17:08 +0000

    mark accident Itself basic same

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
