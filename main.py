c4072f6b2a6dff2bde905c381d6a9c4dd9f4ffd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:36 +0000

    century fact mess mind note

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
