69e2000a76d91f5c49637c75cee303fb31c32650
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:58:48 +0000

    game university mistake pure real

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
