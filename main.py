e0349e7e0e0d906e90fcfa6d5c36f31a801f54a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:26 +0000

    call unlikely pipe science coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
