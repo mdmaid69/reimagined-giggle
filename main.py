f25f23081f9cfab51bfe85c8512379d6d2acb38f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:10 +0000

    change nice solve however inquire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
