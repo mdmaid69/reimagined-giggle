a8d4b89029f7e914ee3d70fbdd09aab7bff2cf51
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:51 +0000

    thick relative refer healthy me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
