ddbc8348e2554fc331d1679cbbd5039b89164d0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:42:13 +0000

    priority exhibition after this approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
