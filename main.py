63c5e2127e7bd183cdd94cec5fd2a45abb139549
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:45 +0000

    future already meet block indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
