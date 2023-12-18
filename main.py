0cf96d039b257f033af52d5b47a43f4d7e534213
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:33 +0000

    sharp naturally Tt artificial secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
