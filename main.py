fa64bdc2687857e50591717d370608215fbd4ac8
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:13 +0000

    couple bit language ignore slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
