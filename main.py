e27bdd931bb44a2cf9dce37394df6882eee1ad29
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:04 +0000

    can turnup lookup so turnoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
