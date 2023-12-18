ad2d64f9e8d642274940f1af3d31904bb4cc20b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:01 +0000

    brain genuine shall also address

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+from collections import Counter
+print(Counter("hello world"))
