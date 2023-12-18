7e1cd78b76272864f8f1d3053f73328793a3fc50
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:45 +0000

    turn normal smart serious nigga

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
