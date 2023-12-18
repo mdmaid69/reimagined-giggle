6f2279b50a75a3d2924e2faf9aa00582c242cd53
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:27 +0000

    agree skill floor lonely concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
