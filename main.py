98b715a2d7dc60cb4880e60f52d010a22f010523
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:02:51 +0000

    bringdown sentence formal excellent though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
