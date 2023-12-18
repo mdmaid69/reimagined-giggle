ebfa78c6aa59f6ff99c4aea6bea20e0f15ba47aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:50 +0000

    full fold towards film whisper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
