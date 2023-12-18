e12be1bbf64e06cea004169c62f89442b32d4093
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:05 +0000

    dress you later general almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_power(work, time):
+        return work / time
