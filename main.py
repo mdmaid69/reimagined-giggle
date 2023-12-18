ef46483f7bb4f11898b96e47acdf39dcb70bb257
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:00:22 +0000

    notice production sign health expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
