c49b88e5d9a1e2ece7762ad9478d5c6f852002e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:29:24 +0000

    contact whatever risk seat hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
