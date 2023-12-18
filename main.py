2cc1f435ea9eb257d2efe1023ac283bb6f324f42
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:49 +0000

    sharp hungry accidentally ordinary weekend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+name = "Python"
+print("Hello,", name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
