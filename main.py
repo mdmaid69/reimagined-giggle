4cc272b7fbe3025015a52ef8ad324369615b9e7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:15 +0000

    honest image famous creature certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
