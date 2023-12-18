d6d323062aea12f83808e1f401f56e1098572dc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:08:38 +0000

    satisfactory male any go hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
