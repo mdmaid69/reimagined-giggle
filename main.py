deded7affc77a9b09c5b7821d9a9af2342c6a5f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:56 +0000

    admire sharp contact fence bread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
