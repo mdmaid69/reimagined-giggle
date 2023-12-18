af03eb6f5217017c5c9e21313214140d80c3e0ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:28 +0000

    wake suggest report foot pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
