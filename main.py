783150c8d203e5b573246a11b54cca7ba1622073
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:48 +0000

    close computer guard ready noisy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_sine(x):
+        return math.sin(x)
