9f7e4c34c622932905caeeac117e2d50251c132d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:08:05 +0000

    admit shadow past breakthrough after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
