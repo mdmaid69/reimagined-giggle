ad4b2422848d06b9d5d216ee881dbb433294f784
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:34 +0000

    unable since thinkof talkoutof anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
