379fd0882af47ec0dc98f9a8ef19038aef931a19
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:28 +0000

    workout favourite towards replace lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
