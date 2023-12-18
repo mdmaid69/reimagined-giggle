7e4187fc76095151a9f3e8741837f9b8865d5788
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:44:42 +0000

    notice fond round runinto react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
