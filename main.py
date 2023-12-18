417aa291b311fe338ebe8439aeafe9abe67ebf06
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:16 +0000

    fit bottom famous reliable 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
