d581855dc72b267388f3b555f93d9f93c0b225ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:22 +0000

    apply remark seat plan lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
