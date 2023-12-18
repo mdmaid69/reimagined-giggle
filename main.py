77af5c00c5ce5cc9c6661dce39b347f8cc9ef45c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:24:21 +0000

    weigh certainly base Ff case

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def subtract_numbers(x, y):
+        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
