310bf4060b6ec05b7915864e21c51ee2d8085aae
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:51:01 +0000

    scrap prepare manner piece borrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
