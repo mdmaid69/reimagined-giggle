82d42f640bd18b80178cfb05145b18ec5f03af8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:13 +0000

    smile name drop awake trap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def cube_number(x):
+        return x**3
