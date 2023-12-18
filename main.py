c121c5061dcb8e5783b77a0ed7a8b428c7e400e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:58 +0000

    joke supply behave peace centre

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
