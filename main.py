c22f175ea93ca42b7a4828ca056fbed602fff599
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:51:22 +0000

    push me motion whistle Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
