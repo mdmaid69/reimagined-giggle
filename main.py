a34a05002420ac1f79f90b22c57ec55161830933
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:11 +0000

    recognize interruption sting squash begin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
