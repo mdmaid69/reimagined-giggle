873a1ff50dd77ac71fa73361610d1552f3ee2d4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:19 +0000

    hot section debt spring again

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
