4b3e62b90c3d1465c4d5c0dc3d9a8405ffb83ffb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:53 +0000

    pollution still face consult house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
