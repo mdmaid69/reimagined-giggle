2581be4affd793da560825daca91ac405d6527f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:45:23 +0000

    behaviour base seriously cold secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
