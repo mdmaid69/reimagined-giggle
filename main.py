353a6d18db3caf0c24f89982c883625df45709e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:48:37 +0000

    funny way sudden belief public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
