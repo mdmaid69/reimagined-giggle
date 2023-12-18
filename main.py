002d2dcc467a645cadf7a3075a1bdf8d13e2ca56
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:41:36 +0000

    could ground should hollow exchange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
