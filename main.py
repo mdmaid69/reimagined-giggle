45f0f1767cdc923aac5a9984914c13c9942aa8c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:49 +0000

    disappear progress dead somebody speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
