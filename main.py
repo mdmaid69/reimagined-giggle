1da1f049d0f2d7543933739dfc4179f52d3aa7a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:15 +0000

    exhibition slight plate discovery behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
