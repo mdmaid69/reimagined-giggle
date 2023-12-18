1d217eae3ee9e04c2de69484f58ba130f94454c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:27 +0000

    fair hers slightly future start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
