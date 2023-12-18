7be8dd37baf0fbe3227acd326a32ca3a65bcd139
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:50 +0000

    hard smell comment base love

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
