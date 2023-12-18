6e00e0616d58fc526dff421227e91b7c81211334
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:34 +0000

    slide meal awake callup smoke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_odd(n):
-        return n % 2 != 0
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
