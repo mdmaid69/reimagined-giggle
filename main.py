8dc22673e7a76b621438285f081b290ce73ddd4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:42 +0000

    challenge uniform dance file brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
