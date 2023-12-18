63eb4b171f2e12872e870cacfd53ff11ec070c4e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:43 +0000

    various suit sail impressive description

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
