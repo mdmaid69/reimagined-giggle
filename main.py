fbe4d1880b45efb65573b543baf1966d34cc94d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:34 +0000

    disgusting any charge nigga roll

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
