d84fc9d1501d881bfcb821fd016fe39e2fe16df6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:50 +0000

    brother knock sing mystery company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
