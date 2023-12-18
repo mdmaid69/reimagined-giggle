feaee0939521f63a7ce1d5e5b9540740a6928b8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:55 +0000

    suppose walk even argue nonsense

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_as_set(array):
-        return set(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
