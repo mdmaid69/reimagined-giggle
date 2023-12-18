353343199fbfd853cf6a2c32325646950650c2f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:39 +0000

    across Cc success ought pass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
