4ef023cb9a905fca22a92731ac4aca45ed75d5bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:43 +0000

    movement round broad doubt sometimes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_current_directory():
+        return os.getcwd()
