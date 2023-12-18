c8258f000865c38efb13eadde6f4cc609ec54ffd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:04 +0000

    breakup try path interest try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
