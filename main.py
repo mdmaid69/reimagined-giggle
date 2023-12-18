921c2689a7952f8aa15158826f617433a1c6fbd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:30 +0000

    relation smooth miss future compete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def is_even(n):
-        return n % 2 == 0
+  def multiply_numbers(x, y):
+        return x * y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
