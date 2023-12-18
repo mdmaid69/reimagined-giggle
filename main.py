85ebe954e56a27f8a36c0041dc2ed9f2419fa6ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:07 +0000

    relation pressure process familiar neither

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
