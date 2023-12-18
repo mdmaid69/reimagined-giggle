6fef81cfb591da65049a13d83c7a82d5ccaa5f02
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:57:25 +0000

    free road soon general getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
