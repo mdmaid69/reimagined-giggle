7e9fa64c2263e4ddafcdb68cae3dafe1033b7f65
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:50:43 +0000

    hollow comment back build picture

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
