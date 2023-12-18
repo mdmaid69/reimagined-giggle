4f916229fc0f667315858ad6b3e51dfe28b6f30a
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:14:48 +0000

    congratulate advise upset report breakin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_acceleration(speed, time):
+        return speed / time
