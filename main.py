8b8ae1464e2010ad916add0b7186a821ef4feeb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:02 +0000

    top violent search they rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
