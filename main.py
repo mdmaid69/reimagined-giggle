76fb2c4fc810c664fffef1a9bb591205983886a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:11:42 +0000

    steal walk drop kick either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
