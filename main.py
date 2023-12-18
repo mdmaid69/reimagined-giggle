909e7308c825923f89ca691f68dd5a12be8be58a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:50:10 +0000

    ready upwards gate confuse thorough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
