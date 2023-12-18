6ec9ad60229e7465899cd15f59c5dbec4391ea93
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:14:10 +0000

    snow nurse form measure wrap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
