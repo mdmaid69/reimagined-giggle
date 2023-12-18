44ce0d29dbc312ab2946b71d0d182d3c391ac795
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:22:15 +0000

    through match affect walk pool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
