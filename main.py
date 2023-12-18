4f04307a184efd2c09085b8fe7e42859836d9e9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:42 +0000

    dig but off pronunciation about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
