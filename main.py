9db0b5c14b2e11ffbe0a2ed791a6e77b2cae396c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:35:50 +0000

    thankyou transparent disturb accident stranger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
