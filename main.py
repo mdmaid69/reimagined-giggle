1c54bcc686227bfc1683cd754443ae9f2b834c57
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:07 +0000

    light pocket equal trip dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
