5ad42546f6da33c0fdc902068a05a0b91a426346
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:54:28 +0000

    train when dead past strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
