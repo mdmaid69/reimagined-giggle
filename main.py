a0e7833386d35e2fab60533bd0bbe5604f5908e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:08 +0000

    he cool happy share remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
