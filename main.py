616b85093a779164b54a6b93b7b485de09ad24f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:07 +0000

    foot thinkof 568energy every Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
