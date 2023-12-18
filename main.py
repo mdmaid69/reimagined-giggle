4c300b824b7607d9f9a1d7b566ce381ebf3bc004
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:56 +0000

    shock guide refer personal equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
