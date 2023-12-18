7a387f0bfc410469b0bb76844ab8bf7c5588a016
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:06 +0000

    heart fortunate position pay score

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
