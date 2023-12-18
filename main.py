aa21203d3cf13b37f6940a325893acc44e4312fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:22:40 +0000

    beautiful low introduce level tend

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
