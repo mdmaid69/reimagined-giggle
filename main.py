04d0de7ba058f5707b1852f0dc09329f333e0290
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:10 +0000

    move delicate unit grow cruel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
