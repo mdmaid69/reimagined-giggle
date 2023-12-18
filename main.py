41e8cf05be5b00ee118ffa3bcc909251905f9d9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:47 +0000

    fear exist proud design cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
