9f5c2bb468f1dcb3ab7c7c3d57a7d55d05088bbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:48 +0000

    570enjoyable maximum chance sky shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
