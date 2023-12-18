3fcc0073bf85ed5277c58e53d2e03594b5ea2b19
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:43:03 +0000

    connect run rank platform danger

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+name = "Python"
+print("Hello,", name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
