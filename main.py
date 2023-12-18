ac35e7d4df5be90659951a1cc1f04bd143223ecf
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:53 +0000

    importance lonely swing keepout little

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
