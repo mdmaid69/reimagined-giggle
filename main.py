7c8c20f3c2d86b9d9911a581bd69887d528adfa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:24:48 +0000

    advantage wind joke vote rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
