cee1764e3893e3ff2fa30c2259e854d49808f0a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:53 +0000

    acceptable answer finger care owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
