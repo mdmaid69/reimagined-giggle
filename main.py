4fa305236c52a98d9e303dcdf06beec40d3c13a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:52 +0000

    mark smile attractive previous tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
