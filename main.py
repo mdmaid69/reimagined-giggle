163b3c4a18d4eaab6af7e55af9f1f7d22069a090
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:22 +0000

    smooth contribute foot instruction comeround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
