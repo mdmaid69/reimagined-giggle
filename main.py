8820334ef0973d3ad5d8b471d5c39f3d2dc863db
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:59:42 +0000

    everyone near drop harm room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+x = 10
+y = 20
+print("Sum:", x + y)
+for i in range(10): print(i)
