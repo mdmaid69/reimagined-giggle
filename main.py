9f4dcbd3816c4623ea5c999535bce5f45ee0d8ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:40 +0000

    pupil clue side secret alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_area(radius):
+        return 3.14 * radius * radius
