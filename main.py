9ee0511414a9161f080ba6397c031ffae00ed666
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:48 +0000

    wake receive fierce fly performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
