5f58a62073f7990be7351ed821c222fa67e14374
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:29:05 +0000

    cook consider argue task fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
