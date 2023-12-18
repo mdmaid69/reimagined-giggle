961cc0c9bde9ddf309f8ce578248c96a0d1eaffd
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:33:46 +0000

    sense medium butter fresh breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
