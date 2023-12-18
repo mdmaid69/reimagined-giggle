3ac25483df031b5e4bf56e439dcafc2127f4da66
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:35:48 +0000

    seem concentrate ashamed base house

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
