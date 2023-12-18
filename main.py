75722ee9bb2b4b457462f65dc1c72ff1c35d1e87
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:41:48 +0000

    edge crazy butter obvious stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
