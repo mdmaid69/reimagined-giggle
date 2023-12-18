79047099882823dcde8fbbf399e2fcb408acd605
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:29 +0000

    lookback(on) order gas check sea

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
