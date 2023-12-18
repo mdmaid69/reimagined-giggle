dbb478a1631de856565d2cf6791527d665bd3ebc
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:02 +0000

    spill convinced declaration smart drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
