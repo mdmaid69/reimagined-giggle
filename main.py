7c2784d29e465262b474e3851f028257b7e16787
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:33 +0000

    purpose check safe principle break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
