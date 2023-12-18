0ed6236dcce4ba533a5a88fe3336cc5215813d67
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:41 +0000

    paper unkind crime build nor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
