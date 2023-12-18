7302c4e8dec7bc991046839624e9a74ab216f6f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:27:59 +0000

    closely fair dare pretty underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_area(radius):
+        return 3.14 * radius * radius
