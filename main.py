7da071e1fb60abc795bd72da23f228b982017b2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:33 +0000

    shrink scrap comfort lead cope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def greet(name):
+        print(f"Hello, {name}!")
