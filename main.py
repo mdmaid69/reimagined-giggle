2c57c1d57e49ae2d571fd116a0f32bce39e1c223
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:41:26 +0000

    fold spite need common condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def reverse_list(lst):
+        return lst[::-1]
