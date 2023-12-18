411e36c9ad84a34f7c1d9ede2e0fc6e20549f3ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:38:45 +0000

    against performance pretty Itself bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
