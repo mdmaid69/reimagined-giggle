9af6cf08eb143b0feca0388d27ef2e7bcd9f8eef
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:53 +0000

    back seem past summer broken

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
