60ce93bd79c9b896b5ef10fcf91a0d1e7bbce374
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:43:51 +0000

    priority play string vision space

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
