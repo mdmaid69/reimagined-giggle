8dc0a847dcfcced68df14d93bd2975989f6a9f9d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:01:24 +0000

    disappointed expect nor set travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
