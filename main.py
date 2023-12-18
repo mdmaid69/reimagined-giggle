b2430325b98e741f5855593ec914b19c8a2dc587
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:37:11 +0000

    worst become figure suggestion break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
