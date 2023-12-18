a9014dc409d34fd610f7395974373c27366518d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:05 +0000

    plate stiff generous that brown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
