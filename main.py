bc988576f2c378d00d94f98a490d8431beba6a45
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:47 +0000

    extra slip feeling touch church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
