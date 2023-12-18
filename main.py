e6f00481e2fd1a34da8b97a4dc48dfe81753ea23
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:07 +0000

    little train surprise classroom except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
