176d80e67f444f0a97793e0c4a2fd60e3886a8b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:29:32 +0000

    body number project any worry

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def count_elements(lst):
+        return len(lst)
