b55e6a57a08be367b8416e30dd8bf67fa8e3c0df
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:55 +0000

    nerve fence extraordinary major create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
