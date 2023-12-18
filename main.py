320f8b972ad71797d4f9dd7af34aa1a6230ce4b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:24:28 +0000

    consider load imagination pollution solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
