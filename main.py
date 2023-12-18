78c2bdca50caab5e159fcb0ba4902327d1270d26
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:21 +0000

    balance yours instrument water eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
