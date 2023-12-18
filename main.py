8cd30e5775f89de151015bccae5631b4b7aaee71
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:08:59 +0000

    second judge fortunately this tell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def cube_number(x):
+        return x**3
