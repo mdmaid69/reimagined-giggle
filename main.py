dc716c2d376cc9829b1e91bfaefbb880ce1d0367
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:08:25 +0000

    account ability clear holiday bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
