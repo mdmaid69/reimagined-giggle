e072bc70b9bb8294f6029f397a11f4c70acae254
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:55 +0000

    lost gate lead close tend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
