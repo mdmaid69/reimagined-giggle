870e8c8773345398ba852e386f9e1432ffd654c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:45:49 +0000

    abroad runout add towards last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
