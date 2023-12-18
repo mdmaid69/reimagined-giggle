0afcfe0f22c47ec4d3dcc5b78c6a52fc02dabfad
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:10:29 +0000

    combine left male error busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
