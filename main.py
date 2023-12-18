2ad49cee6c016632838fe0fc93bc4acc6803d065
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:49 +0000

    since to tap Ww necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
