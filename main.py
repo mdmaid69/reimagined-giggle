b5328bead3c5fb44ec700724e0a8c3b76367b921
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:39:43 +0000

    secret judge manage himself failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_density(mass, volume):
+        return mass / volume
