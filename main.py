d8f049c8c662d6dc251f69319c8fbc06287b63a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:19 +0000

    connection fix they suddenly please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
