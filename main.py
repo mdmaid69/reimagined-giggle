37d080df1bd58ef6f4613eb9da1a1555d643efea
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:11:54 +0000

    pride total content far convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_list_from_array(array):
+        return array.tolist()
