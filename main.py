a9c0e89d8265d36ddea06c2d0fe9f75e8a8f611b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:02 +0000

    run snow freeze doubt involve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
