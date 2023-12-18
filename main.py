41c173b292f613f42687e012e70cc4f3839823ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:11:58 +0000

    neither unless swim weather except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
