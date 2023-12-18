25aeeb30933562454cac098f7826df05dc64a93a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:58:54 +0000

    touch win forgive back type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
