1a6e9ab33fbf4a672b3450ec0af52361d2a73100
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:19 +0000

    send height correct handle crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
