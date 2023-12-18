f883b24265650bfbe8d039cbd7e18ec3b46582eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:24:22 +0000

    special yesterday recently operate I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-print(math.pi)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
