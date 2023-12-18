a62b1a341ddd482defaa51c656064f401dca7bff
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:30 +0000

    exciting string dream improvement serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
