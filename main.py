6be0bd34fbea16087444fecae3bb8d5a09438ae6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:12 +0000

    rank while work undo heart

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
