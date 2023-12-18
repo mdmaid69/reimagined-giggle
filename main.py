2d08595865b388d2fd0d4e44f87b22e43fd7d101
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:01:38 +0000

    573entertainment inquire hate close turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
