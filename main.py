af55f38944c124c8c69f77eaf6f91f126f739d4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:15:28 +0000

    one difficult unpleasant short least

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
