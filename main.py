e21b9dd3027f31115be8c7912b60c1c23d291324
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:35:48 +0000

    trouble hand share beginning characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
