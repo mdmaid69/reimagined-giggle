9f762f6f3b9ca60c2d540c63ac6397bc352f9ac9
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:14 +0000

    compete goodbye whenever flat drop

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+x = 10
+y = 20
+print("Sum:", x + y)
+print("Hello, world!")
