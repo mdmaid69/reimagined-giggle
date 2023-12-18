856bf980d0d0ad20b0ab1870fb43f1840132593b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:59 +0000

    snow sharp whether waste secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
