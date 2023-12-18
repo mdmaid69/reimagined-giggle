0d5b096be835d2d93cc85acb3f9e723b741d86e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:14:33 +0000

    refer cut expression especially happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
