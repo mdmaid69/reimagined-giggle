fef955eeb35a4038dc709b395c091c1f249170f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:28 +0000

    reduction Rr value nigga computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
