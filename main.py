0874275fcca8cbf0a85d4468d9140554baacaa10
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:39:32 +0000

    payment Mm tend rule confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def square_number(x):
+        return x**2
