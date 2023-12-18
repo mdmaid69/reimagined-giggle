423db168f75be4ee2286fc5d7be1f55787726b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:06:55 +0000

    health breakup preparation fast continue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
