f51d2e807ee2e2181d51667306ae606be8300097
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:46:05 +0000

    culture breakin train practical gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
