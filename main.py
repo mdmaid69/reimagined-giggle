fb162d97149d69345659716bced9f732f99b56f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:25 +0000

    contribute record yes fasten scheme

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
