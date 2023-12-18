10141e9c191e55a2042e05c25b5f8850b8679145
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:31 +0000

    soak thin new flood combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
