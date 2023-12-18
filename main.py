ed608d17d597ad3f31b4bf6fef21f2b067efd7d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:07 +0000

    physical bread hear only tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def count_elements(lst):
-        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
