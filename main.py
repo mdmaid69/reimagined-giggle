24c44f6f6013252628f0c1be6e03c49be3dfe2dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:34:37 +0000

    before height march drop interesting

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
