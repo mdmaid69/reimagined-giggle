4a76abe5df04f7bd290dd6d95736dfcc290eb20d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:08:04 +0000

    badly makeup accidental post treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
