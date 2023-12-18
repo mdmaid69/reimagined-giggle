904c10cd59be83cba08b40f7570f83df059c4d23
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:52 +0000

    order soon precise phone disappointed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
