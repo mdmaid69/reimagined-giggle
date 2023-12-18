aad40f40e44e15a339a0f8e0bc3b6327b88cac40
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:52:28 +0000

    slowly order supply much drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
