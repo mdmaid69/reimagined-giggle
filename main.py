67536e2dfba1ada520c86d26f21d72d489501837
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:06:06 +0000

    indeed downstairs habit ready equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
