372801ab560e4d5bdecb1c67af16523d1f7dc4cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:08:17 +0000

    free exist test popular shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
