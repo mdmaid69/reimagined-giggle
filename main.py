fb79729a09456562235abcff6f7bf2609bfd2830
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:42 +0000

    pass backwards lead vanish close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
