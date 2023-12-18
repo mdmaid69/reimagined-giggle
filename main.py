d14af3a898ab8e67e9a0594b875167ecffef4adc
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:14 +0000

    error deaf fasten empty beautiful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
