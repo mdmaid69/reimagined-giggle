92a4f209fcd45bd35b367d2f195f994ea3a72a98
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:45 +0000

    traffic wherever copy loose juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
