84d054fee77a8ef4a6ef71f89f08d8f4fc065680
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:08 +0000

    compare obtain project calm children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
