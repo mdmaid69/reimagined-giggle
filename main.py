7f05a2e8a7cd8dc741be47a33c7ad780b6d4ab0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:14 +0000

    go that join miss comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
