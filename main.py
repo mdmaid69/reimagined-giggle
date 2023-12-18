3df93c0d539431ac7cfbd5b00a3a607842252224
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:07:20 +0000

    famous instruction order dig attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
