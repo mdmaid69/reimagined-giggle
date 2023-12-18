09cd1ed8c73519d813908a65306be69b91de22b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:04:41 +0000

    present regret everybody Nn trouble

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
