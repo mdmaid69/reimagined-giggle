1a9a8461fe861ee5b9e6fa8eb99df22d94466de3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:46 +0000

    zero strength fun lookback(on) attach

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
