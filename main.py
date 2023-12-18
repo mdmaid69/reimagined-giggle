2e8eda1b7dc7b94cc3fdb87cebe4d7f17a0f9eea
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:47 +0000

    poor bringforward thorough holdout boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
