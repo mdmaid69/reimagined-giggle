1ca171e4877c1f9ccd341933899172a0ded43130
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:50 +0000

    bound repeat gap affect getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
