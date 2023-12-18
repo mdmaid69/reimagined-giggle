1051b18e769b13da0232b8c54de5038671914df7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:46:40 +0000

    back speech temper more concern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
