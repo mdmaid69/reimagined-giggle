c1e9c9db8cae7587d577f09d0d1e0c23efa3125b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:14 +0000

    remain breakout separate either command

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
