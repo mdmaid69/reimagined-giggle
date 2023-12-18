fc55da463014142972a951db4683ffaf2b003669
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:37 +0000

    development estimate instant success glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
