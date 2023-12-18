071618d45baeec5000d91471f30ce036d6a747e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:44:04 +0000

    apologizeapologise bunch middle boring trust

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
