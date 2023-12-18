eedcaea16e428583bc4b6e494eff7d92416e52af
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:23:42 +0000

    team judge tear first over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
