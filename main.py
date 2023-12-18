879537d9868156f16a98488f7ba63cf271cac98b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:41 +0000

    roll news crime record care

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
