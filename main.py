f1ec31e30f2c97326fd190c72316769e9aa92b10
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:06:20 +0000

    influence report obvious answer abroad

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_list_from_array(array):
+        return array.tolist()
