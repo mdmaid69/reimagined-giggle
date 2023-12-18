4c7e9087ff12bcd9217ce121c4ff92746cad0fcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:52 +0000

    than business form nice influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_power(work, time):
+        return work / time
