1920853ddee9d601553269e0d20e010ed5c844c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:24 +0000

    criticism play shoot fetch perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import platform
+def get_os_info():
+        return platform.uname()
