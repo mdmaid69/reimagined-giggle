6275b126ee90f1faa244e16047db72e14d51f7d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:41:35 +0000

    perform experience juice sweet thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
