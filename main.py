0704cae34529a11185ddd8edf324c80ca1b7e904
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:51:03 +0000

    bang set like machine fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
