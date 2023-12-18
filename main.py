f1eb9cb24f1ef0df65f8fa503a3f6b6045b006fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:23 +0000

    afternoon great something address touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
