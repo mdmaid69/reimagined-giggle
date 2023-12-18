d693de1fccebd38ffbc6ac7fe8ae3b3054c14244
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:21:00 +0000

    reason friendly skirt group sight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
