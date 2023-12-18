2d3c9de4340f3de26a0bbe489cf0e851eaee032b
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:31:57 +0000

    feel unkind it available silent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
