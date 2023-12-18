f805a1ad1e447f1f8714dcd8179f3726d892e114
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:43 +0000

    impression pass situation operation correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
