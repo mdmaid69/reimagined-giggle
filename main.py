563b1fa56512b9fc36c61dc42a7713b9e0277ad3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:19:04 +0000

    more train 569enjoy yet how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
