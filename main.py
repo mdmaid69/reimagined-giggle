af8d7682476b36dc640e3ba3c81ae24bd2b8ca36
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:09:56 +0000

    equal curious possession deal limit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
