ab52bd1ecf802b3512e92d6abbd66236cc588a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:10 +0000

    burst cool past afraid rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
