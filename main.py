4505e3a7266380c3bcc69fc416f7ca58d381ed49
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:11:08 +0000

    minority scene joke window national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import getpass
+def get_username():
+        return getpass.getuser()
