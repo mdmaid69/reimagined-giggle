d4a92333218d576431daef2a82cc2807974167bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:23:37 +0000

    squeeze nerve laugh community journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
