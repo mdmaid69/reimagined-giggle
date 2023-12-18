8c38a0c7c483f7fa9c6ecde76928d75d33307fb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:46 +0000

    film disgusting ours train follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import time
+def get_current_time():
+        return time.time()
