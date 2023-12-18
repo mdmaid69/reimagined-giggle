edd47a80181a960c3d47a898693c420dca2d1fc8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:53 +0000

    either justice breath gift call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
