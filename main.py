5760d3a5cc2d1b5a91e68073aa705761b24368b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:30 +0000

    vote spot slice communicate guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
