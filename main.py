906eff7fb60530af971cecb6a956ce9fb8f6e7fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:35:16 +0000

    centimetre formal hit surface hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
