2ee5f7814f12a1d0fe94a4871f24a7a266bbc704
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:21:11 +0000

    interfere lookback(on) may change hair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
