c208fb026cfcab60281ebf84a4e84c7e4b71f26b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:18:42 +0000

    squash description shut wild load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
