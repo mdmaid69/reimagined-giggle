064bce6845704035ca95135e6e6ad48cd090b560
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:43:54 +0000

    organize branch board sign pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
