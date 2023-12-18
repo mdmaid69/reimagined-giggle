e48aa92ca06ce13e08b8a6259eab5b0f19f5a819
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:54:38 +0000

    sensitive believe strike sell private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
