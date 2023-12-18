b9a850da65d07b09aca974da667f731b1100f165
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:00:50 +0000

    thumb somewhere train mine chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
