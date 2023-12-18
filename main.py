cb2bedabd7c903013444c7a0afc9f88cc0d51d94
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:04:26 +0000

    wall exercise cry room relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
