c176519ab7d0c64f4212a1d6872206aab5748e0b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:56:25 +0000

    delight pay demonstrate somewhere switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
