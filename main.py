0a9c2a430ac98edd131773717e194335ba528d94
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:39:55 +0000

    noise again knock best reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-from collections import Counter
-print(Counter("hello world"))
+text = "Hello, world!"
+print("Characters:", len(text))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
