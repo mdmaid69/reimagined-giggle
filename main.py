cc45679667b5b8fe3c9678d8a609227d9889355f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:59 +0000

    lot travel band error sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
