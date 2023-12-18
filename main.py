009837cc72daa54c5bbd79afb2af9fa3e85a6e6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:44:37 +0000

    soft minor often spread polite

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
-def greet(name):
-        print(f"Hello, {name}!")
