3fc5da80993657640b3db502baf2d20fb4f61ea3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:30 +0000

    violent sign noise very some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
