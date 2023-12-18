78f42902c703382124fe36dd2bd76e4828af9f30
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:55 +0000

    bang luck under common 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
