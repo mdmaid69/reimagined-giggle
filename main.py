93bafab90fc76051ca1d93fd47c1ecb96e3e4481
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:28 +0000

    interested central ours Hh stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
