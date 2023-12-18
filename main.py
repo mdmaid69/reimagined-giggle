bdb733bb24b2e769130e099020c54e2ab20aed4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:02 +0000

    false path right wheel congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
