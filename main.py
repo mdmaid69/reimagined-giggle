1bd741d3a7ad145c8cb922c4ee0364645f6de015
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:49 +0000

    term attention mistake that discuss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_octal(n):
-        return oct(n)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
