06f25eaba1a7fd7da87f98eced103e56e9b8a9fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:01:31 +0000

    involve on art part bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
