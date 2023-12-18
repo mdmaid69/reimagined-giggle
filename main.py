fb0d57d419b593db0774b96d03f6b506b7d989f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:56 +0000

    provide nurse job Tt sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
