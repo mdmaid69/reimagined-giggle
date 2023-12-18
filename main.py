34353c956dde86567c1d93a3d8b7e089bc56aa72
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:17 +0000

    rest uponon your pleasant thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
