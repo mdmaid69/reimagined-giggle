8170909eea477b48e520b3eabe04720d8e832f4a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:29 +0000

    sport exist solve twist sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
