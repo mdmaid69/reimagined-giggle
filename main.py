5d098c2e921c5e8b34be370c3094d1e57ce997ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:30:49 +0000

    influence over tie become edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
