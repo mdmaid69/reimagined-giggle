a2cab963185596e6cd232908d4fa190160e99884
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:50 +0000

    borrow paper morning than practice

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
