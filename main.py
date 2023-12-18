d7ca5f4c7772646ccdc864681ae268e4f7d9009c
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:19 +0000

    night everything shop chief off

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
