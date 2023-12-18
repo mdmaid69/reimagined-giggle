21bf1d72ad6e9077750d06f2ff3ef4b1dc50d1ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:37:24 +0000

    mess practise journey give complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
