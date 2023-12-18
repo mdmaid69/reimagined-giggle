63ac5771925905ba435f8867d555d9a6f94bd3c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:45:48 +0000

    fetch rarely you public legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
