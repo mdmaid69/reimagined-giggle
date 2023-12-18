b46dcf3c756197c1765d1a7e94ed92087bf22dc0
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:57 +0000

    welcome about one strike rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
