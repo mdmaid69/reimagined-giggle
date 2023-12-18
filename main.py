2f1af79d85466d0509517f741dde792d2d8e6184
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:27 +0000

    terrible everyone difficult throw father

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
