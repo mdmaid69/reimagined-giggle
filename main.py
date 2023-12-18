2496fd60b9afcce15d2ab1e2335827aca928cb67
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:48 +0000

    rely interfere sea present information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
