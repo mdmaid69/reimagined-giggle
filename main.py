97b92fb7cde4b46f85c6c2979a4d50f6968c76de
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:16 +0000

    lookup regular 570enjoyable win if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
