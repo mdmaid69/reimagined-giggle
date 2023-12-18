135689b1ec6ff98b9e42083ca6ffebc9cb448266
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:45:48 +0000

    test close period then Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
