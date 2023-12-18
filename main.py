a4726758e1687a1a1f4c01617445a4be248b8a90
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:50 +0000

    alone pressure find advise waste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
