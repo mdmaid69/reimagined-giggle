b9ec0ef116630c2aedd6341677c746e9fc83abd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:00 +0000

    male breakdown organize themselves dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
