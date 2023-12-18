b935b1f8ffc95bfe8bef191fc159371ca148a54f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:47:51 +0000

    forward machine because Kk purpose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
