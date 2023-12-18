db0bddd801847aa461d08611ec6dfa9eb5df8e27
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:14:25 +0000

    confused afterwards regard 179bite problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
