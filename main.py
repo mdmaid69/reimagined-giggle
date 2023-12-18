7471635ce20df6aa3def53a93d00662de49cd26f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:02 +0000

    float freeze on adjective treatment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
