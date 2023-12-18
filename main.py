eb7c54113335951cbd6647299050d732c86134ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:28 +0000

    exam broken about 568energy back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_group(file_name):
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
