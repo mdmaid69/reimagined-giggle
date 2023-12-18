be114d0b889f7e2404d4d3f05e340af748eea956
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:55 +0000

    praise careless brush tomorrow guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
