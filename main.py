10e92070012fdcc11504311899460efdac8a6fb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:09 +0000

    trousers unusual information taste unkind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
