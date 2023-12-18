7f1eda2eed09083fea44fe69e4f0007d74c3f718
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:12 +0000

    some search dig drive takeover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
