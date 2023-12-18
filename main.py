f983cefd3065fce31798a8d2b55ed9d794d16db4
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:34 +0000

    conversation pronounce comedown lost hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
