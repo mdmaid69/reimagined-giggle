5d7b2f2e21933b41b52591c15a21368eb6fd70fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:49 +0000

    short dark near discovery lock

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-for i in range(10): print(i)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
