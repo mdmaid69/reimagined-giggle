e1ed056ed8301bbd66bd4dccea555501a1852c8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:49 +0000

    ball danger situation into beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
