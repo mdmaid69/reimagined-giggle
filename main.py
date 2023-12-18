7e196182e12ba43fdf2a4f580dd9ccf3e46ef63e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:43:28 +0000

    of sale quantity complete except

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
