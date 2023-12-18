f2ad208c2f152419ddebbef008b6b2931a7378ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:36:02 +0000

    model move dare this loose

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
