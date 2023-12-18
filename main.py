8485f1f4aa05a9766cf85de834b3aa8265633a12
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:00 +0000

    neither fasten ahead school key

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
