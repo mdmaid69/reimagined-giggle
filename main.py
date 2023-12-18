51d8642edbf704f88c0f0c5d05024280c76a89c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:27 +0000

    approval cheap succeed probably spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
