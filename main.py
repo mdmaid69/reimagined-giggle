94f68b202f9123d3aa0bb5e34a011d11d95eb2fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:45 +0000

    pronounce belief frame thick attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
