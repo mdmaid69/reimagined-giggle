d7d49a4b89aef43c6628267ace91f4863a7992c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:36 +0000

    room term arrive under all

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
