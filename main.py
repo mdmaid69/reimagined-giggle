aea3932010b86248e6cbc8e3f26ed6397f1c62bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:31:22 +0000

    tear zero under phone friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
