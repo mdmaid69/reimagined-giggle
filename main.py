25f450045be955ff0a77ad39f65098598a9242e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:24 +0000

    depend scheme makeinto breakup tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_octal(n):
+        return oct(n)
