7d0351962b9a1cd751395bff7d190fd27424850d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:28 +0000

    left knowledge thanks responsible sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
