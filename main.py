86ade384ff2ae46e8dea5ddca3656ea367687d60
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:56:04 +0000

    be crazy car think prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
