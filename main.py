e141fc3f725d2e3b683f5dbd4a3a9f767575b40e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:08 +0000

    face wash only hers stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
