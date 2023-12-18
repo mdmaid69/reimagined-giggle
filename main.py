ad6516fa7dfa59628b47b360ddd0a668cd1a35a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:23 +0000

    then able principle future yours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
