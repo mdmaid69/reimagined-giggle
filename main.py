d4c2cba9c551d7376eb83692e3b579f42d868577
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:56 +0000

    necessary takeover come possibility apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
