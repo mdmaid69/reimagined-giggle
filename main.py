62654872473d1543be564e94d5924aa874766ebb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:14 +0000

    listen Bb act really cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sys
-def exit_program():
-        sys.exit()
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
