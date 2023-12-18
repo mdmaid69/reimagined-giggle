b9ca173084dfe9bd9b2a5c470beea67b27f34584
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:00 +0000

    finish ticket complete age reply

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
