199171c1a1ccb309b17fb9937d7935d67898a7e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:39:43 +0000

    feed your steady belong lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def convert_to_octal(n):
+        return oct(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
