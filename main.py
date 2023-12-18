f87cc99b7fcb2983fcdd466425fe11a18dc08380
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:21:07 +0000

    exam dress nothing nor underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
