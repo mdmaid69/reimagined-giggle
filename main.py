a6692bdb8b36548c23dd0e943862546aa0db3300
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:49:02 +0000

    mean split admit invitation power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
