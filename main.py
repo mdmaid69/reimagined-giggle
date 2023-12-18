dfd70f4adbb858eff5f076bf6cbbcd151fe8668f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:26:32 +0000

    too forgive 565end or squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
