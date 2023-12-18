f8f40685a0e72e0efa45591357cf9b2ac87db8eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:54:44 +0000

    introduction replace foot could recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
