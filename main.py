0d1ae50a4d039a56941df0b5ea1c36798b432655
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:34 +0000

    callback whether shop previous read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
