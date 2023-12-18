0fcaed48a34d30df4d0c57938e0cdc673ddc6520
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:12 +0000

    depend law match partner share

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(numbers):
-        return max(numbers)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
