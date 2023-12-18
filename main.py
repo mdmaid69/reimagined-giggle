7a15337e36b17837ad66a0f3e7e61d23d9047bbb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:30 +0000

    trousers friend use music jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+def exit_program():
+        sys.exit()
