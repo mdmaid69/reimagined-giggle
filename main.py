22788e9f2d86aaf3f6c64a90a9f7eef872b2de14
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:20 +0000

    inside temperature convenient Pp machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def subtract_numbers(x, y):
-        return x - y
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
