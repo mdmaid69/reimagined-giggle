592e39d761b5726d969df77b83a5fb8d6f84a7bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:03 +0000

    partner entrance cross on arrange

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def subtract_numbers(x, y):
+        return x - y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
