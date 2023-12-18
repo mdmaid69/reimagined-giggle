7af0d3b3e73afb753bd910478d0d1b04d1fd557c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:54:26 +0000

    close benefit relief passenger niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_today_date():
+        return datetime.date.today()
