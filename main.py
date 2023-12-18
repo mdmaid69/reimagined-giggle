5e8d0b1804167a0dc3bf95585fc25958a11e491a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:54 +0000

    survive rent would naturally little

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def convert_to_octal(n):
+        return oct(n)
+print("Hello, world!")
