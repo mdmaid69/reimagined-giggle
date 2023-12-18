409f7d20df0f21ecc8522ee444e21d1d81f5d147
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:00:19 +0000

    basis risk bear error spread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
