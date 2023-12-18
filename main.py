82c2e85ac81d97bdb68913943e614237a4a1e1b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:59:07 +0000

    importance tape behind limit shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def add_numbers(x, y):
+        return x + y
