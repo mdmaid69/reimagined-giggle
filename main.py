bec17adeca872dcdd888847122194a44a06edc37
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:21 +0000

    bush regularly bound bit have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
