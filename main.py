f43ecda524d5a34255fe3d1da2b1f5f94e4a89e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:05:24 +0000

    broad stand invitation regional damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
