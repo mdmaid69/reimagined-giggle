87dd57bdae039c722a77c1d7d9ab93cf4c12ac68
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:00:40 +0000

    pupil as firm movement colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
