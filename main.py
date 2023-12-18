606aedac7ffe85c554c9034286169642a074f1e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:47 +0000

    comefrom minor drive independent shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
