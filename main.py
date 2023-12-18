fe3744bd7d807467a852d31211532a9ce7064703
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:35 +0000

    entrance delight ago glass thought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
