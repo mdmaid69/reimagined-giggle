1242f9958ce686ca326c6ac605dc11e5b5039e64
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:32 +0000

    scrap home search organize actual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
