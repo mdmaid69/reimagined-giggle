f451ffa5be8060117d430325cf8c49a1d3629775
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:48 +0000

    fairly winter stone tonight towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
