9b155c9ecd8d59b151c15b18acb4fa9dbc098eab
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:02:55 +0000

    pinch watch fresh water smart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def square_number(x):
-        return x**2
+def reverse_array(array):
+        array.reverse()
