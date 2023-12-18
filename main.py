cf979a7c9b350f5d946a43d0f63dfc0ad9afa8e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:51:12 +0000

    sort flight mistake meet than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
