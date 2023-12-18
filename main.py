c18976e291f15ad2b146a8127d6bceb75b80a69c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:19 +0000

    rent fierce Gg upstairs objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
