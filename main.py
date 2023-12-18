175c962e7e80e7950130920726c0beda1a4d3634
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:06 +0000

    break boil defeat whether harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
