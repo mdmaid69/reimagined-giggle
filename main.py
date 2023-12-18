fb3dd7ffe96d728cdfd47a8dbc4b89274f6d3802
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:13 +0000

    invitation test centimetre right punishment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+print(time.time())
