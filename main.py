752eb235e90146673b4e8b5474a4d08de40c9bda
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:11:09 +0000

    say pupil building sink test

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def square_number(x):
-        return x**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
