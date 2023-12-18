46fc00ed2a4614c3fd500289d8d96fa6dcdd779c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:21 +0000

    loss frighten advise giveout expect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import collections
-def create_queue():
-        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
