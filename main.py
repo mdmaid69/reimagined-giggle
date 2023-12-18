8e2cccdbc669b88acaeacc438ab45f8e93d2f78c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:41 +0000

    along straight else report prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
