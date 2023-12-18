7d48b212c7b20e2e6568a7aa8f27cc412e4c2838
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:03:50 +0000

    right worse correct bus correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
