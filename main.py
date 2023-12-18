54add9311edb96149f78d380907f7c40750b6377
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:56 +0000

    attention offence occasion pay permission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
