50a99287d55ca500a3462e302cd1a2cc09034a1c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:35 +0000

    must trap play sail watch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
