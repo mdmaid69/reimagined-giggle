6849a2186105971570bd6fffe3098672e15984bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:20:22 +0000

    aim ilovedicks fashion scale strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
