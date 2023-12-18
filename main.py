c2e5bd514d54aa8e383edb8d13f093133cc8ba31
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:54 +0000

    break drink bunch goout flight

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
