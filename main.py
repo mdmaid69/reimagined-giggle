864639cea7e45a5bfc970a31c2931b9fb994d898
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:37:16 +0000

    precise produce doubt patience turninto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
