9327261a775bb006c36740535d1e023e95ebc945
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:30 +0000

    tool amount except must properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def get_current_working_directory():
+        return os.getcwd()
