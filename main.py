bfa5c38f1185a39928a1479eb2c26f6b0b6d5a76
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:50:24 +0000

    why steal memory break shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Average:", sum(numbers) / len(numbers))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
