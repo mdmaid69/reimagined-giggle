5fb1b12dc5a70b9989ca6a2dc33d87a9327f1ffd
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:29 +0000

    uncomfortable correct morning fast course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
