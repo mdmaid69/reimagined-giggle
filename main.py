9ae48ac166431888d22bcae852646d819cebbf19
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:41 +0000

    listenfor date star while hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
