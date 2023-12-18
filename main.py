4c5c392d8da56387191ff358e2451b928ab17d65
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:25 +0000

    sink prove alone strike pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
