5762fa581e0125ee1ccbe43e41d927cb54b20884
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:03:16 +0000

    care start tonight tough lookup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
