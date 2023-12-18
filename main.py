402c92a61768c4603cc8d8e7f06fbebb4cb3c62e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:13:05 +0000

    lookaround average character bridge until

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
