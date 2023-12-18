ba4fb02a304fc7d4a90394eddad7f5da2e2fd207
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:53:42 +0000

    voice occasion slow hollow fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
