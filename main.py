5307f8438e86bbcc32234755f6e0cc9f57e6bf69
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:47 +0000

    stay remark detail reserve soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_volume(length, width, height):
+        return length * width * height
