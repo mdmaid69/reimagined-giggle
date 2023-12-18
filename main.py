7688e767470ad68359900ef99713302f7108f38d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:25:11 +0000

    pinch pause relation sink head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
