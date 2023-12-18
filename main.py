fc3ea59102ca949055c17d23cd6682bfdfdd976c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:14 +0000

    healthy exam evening scare expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
