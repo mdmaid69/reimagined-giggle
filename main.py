09305cf897fc8bbab6141840fa152ecfdcece329
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:00 +0000

    wait treat tour fact snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def cube_number(x):
+        return x**3
