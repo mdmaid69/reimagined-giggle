67562d9f8796624cd508b3dd30454931a8a83ad6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:22 +0000

    gentle modern prove surface bite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
