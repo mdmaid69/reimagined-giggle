83e57b584cb786c30657e1a2dd438d5533aa08cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:31 +0000

    duty can though confused open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def convert_to_binary(n):
+        return bin(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
