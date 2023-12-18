538ce2d895a6729fb5caff7afb46b52bcf148bd5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:02:37 +0000

    Ss autumn start that bad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+print("Hello, world!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
