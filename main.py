4d0eafc6c981f120b43db5838b75c830ab5ed4e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:53:31 +0000

    substance treatment important also away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
