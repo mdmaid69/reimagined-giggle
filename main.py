f0bfa4608d7d377cf651f0ff1fe1a607099cdcc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:49:33 +0000

    rob Hh fresh delicate later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
