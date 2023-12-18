48213782e2dce65807f0d8ae6c0d7d387d6ad37d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:12:07 +0000

    medium compare force scene entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
