077ec5a724ce1fcc29e9d53e9157f075ff8354b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:05 +0000

    trap narrow damage offence react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
