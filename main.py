ae0374b8f2baf2553ea532506e39030b5fdd05aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:30:23 +0000

    bottom false study yours anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
