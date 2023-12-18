c91cc0d58297fb046f5e27339a337e1611fd01c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:58:27 +0000

    please comeround rough favour holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
