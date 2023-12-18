5a2bd9204389e97fdf3c4429cc4f73dc0216c4bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:27 +0000

    related bad rarely on takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
