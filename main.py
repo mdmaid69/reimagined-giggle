b4fce479bd048cc1fb759e3ef975541e8a7d1e17
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:01 +0000

    arrange behaviour friendly blame petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
