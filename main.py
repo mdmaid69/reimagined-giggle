805445e7cdfdc14f3ca1087066478333ed1b6998
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:01:05 +0000

    work wise dare smooth such

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
