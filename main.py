fab33ee833db88976a0d0b22f444995712707842
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:04 +0000

    why neat underneath land extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def remove_from_array(array, item):
+        array.remove(item)
