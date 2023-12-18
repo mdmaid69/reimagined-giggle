1059391b890a5b405d83c40a1980aa4cf9a7fe10
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:08:21 +0000

    breathe surface heart gift raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
