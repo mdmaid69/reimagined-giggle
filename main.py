e63c906977bf98b3109f10c58ea1236b637ec041
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:19 +0000

    thinkof wave how card regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
+def convert_to_octal(n):
         return oct(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
