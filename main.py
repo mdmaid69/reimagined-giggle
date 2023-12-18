b39e7d0803d23f0d389b7ea2e847f6a6966c5ba2
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:48 +0000

    change about tonight plate battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
