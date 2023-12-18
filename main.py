ef18f542892956b2f16daf1947c561c797fb08f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:20 +0000

    report near put within story

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
