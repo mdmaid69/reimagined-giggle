ac909850a592948fd370cd7267d3ac5c6574aec7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:07:23 +0000

    study season aim stick reason

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
