557435e21bf72a978fbeb927c8908f668b4dfafb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:09:53 +0000

    snow disagree close whoever shut

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
