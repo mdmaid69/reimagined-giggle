7a85f926530d6ef4bb4654b268eb038dabf88d90
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:26 +0000

    subject belief shut fashion interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def sort_list(lst):
+        return sorted(lst)
