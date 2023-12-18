0652e0e18ca7809e5f2cc016ba2758047bf3ea00
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:41:44 +0000

    want there my recommend arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
