c25c125637e72c9999b5e96427ae0ccef30d7fef
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:40 +0000

    well possible pressure match distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 def get_today_date():
         return datetime.date.today()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
