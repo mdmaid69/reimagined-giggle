2be37da48e26e979c367d5b7e03f86f4a1bc7dc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:06:59 +0000

    circumstance exchange interested oil question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
