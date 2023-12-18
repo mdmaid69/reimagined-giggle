367da2f7000ff5c3410aab3fde5506fb3c0cacfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:09:10 +0000

    coffee cook well tired usually

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def delete_directory(dir_name):
         os.rmdir(dir_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
