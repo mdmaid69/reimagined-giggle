a18943d16476d8a7cfb09ff19c9d40a1bd1c9241
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:12:17 +0000

    sticky service argument against yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
