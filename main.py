5b26c23892d442873d68cf73cf2de9e89b128dad
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:37 +0000

    negative follow especially face tax

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
