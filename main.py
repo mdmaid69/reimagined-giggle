66e1e49a8c8b2aec74a7950bb85d48f57d8a0242
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:30:07 +0000

    shut bored fetch comefrom feeling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def sort_list(lst):
+        return sorted(lst)
