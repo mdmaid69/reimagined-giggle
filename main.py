1fa6fcdbb5488c6875a863e731592457214a348f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:07 +0000

    any mixture 568energy authority original

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-for i in range(5):
-        print(i)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
