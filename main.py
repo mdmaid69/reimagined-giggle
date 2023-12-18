166b22c9c091182528107c5e5c9ca849d9c32265
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:20:56 +0000

    dance pot grateful congratulate getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
