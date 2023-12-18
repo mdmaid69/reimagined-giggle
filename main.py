ff10d5312379c3b865692c20751aea513f15e7b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:57:06 +0000

    all so bet write without

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
