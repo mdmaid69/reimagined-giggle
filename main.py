0abe048501bb7594ba183f7234da0bf29a09b0f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:39 +0000

    tip mention design interest radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
