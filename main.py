7562742325a9160b0b8599a6dec274125bcd78a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:52:03 +0000

    put occasional present damage open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
