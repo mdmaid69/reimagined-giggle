32a9f8901fd59c900185388a5df83f53b4ddb1e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:32:21 +0000

    across as grow break born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def calculate_perimeter_rectangle(l, w):
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
