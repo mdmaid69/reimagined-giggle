3dfb650c43c67c0edae0f2e0fce72bcbb31de56a
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:22:03 +0000

    lookafter cheap estimate relative sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  def reverse_list(lst):
-        return lst[::-1]
