eb7e8f5f4d3e91662697872bbdd2e3959449a60f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:58 +0000

    pupil when decision present broken

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
