40e6f44c68a6eb22f36d58cc1a6e090be62b7001
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:33 +0000

    state Yy car than through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
