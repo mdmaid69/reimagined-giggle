1dc95cd375bc35428c43bf84ed082f626f9cf0ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:29:21 +0000

    measure suddenly appear sink alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
