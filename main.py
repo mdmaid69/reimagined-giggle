9cb029b116b051dd36764b308399cab00a2f07eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:39 +0000

    path attach mean comeover thorough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
