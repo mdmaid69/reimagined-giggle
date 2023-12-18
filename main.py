7a874661cb6eb98e791f46d2e33e19ba9a92a152
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:26 +0000

    rob measure principle stay real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
