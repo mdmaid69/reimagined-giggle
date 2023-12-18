77921bb3e9ff8534b03ffe333c818aea92045a35
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:14:22 +0000

    record pull warm what coffee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def is_palindrome(s):
+        return s == s[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
