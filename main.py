430208115f86c06cbb8d3c7a53e142b529e974dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:22 +0000

    spot anywhere check thought pleased

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
