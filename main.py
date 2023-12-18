3e8cfafc4b60095a70fea75e551bf9f6b29ac1b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:55 +0000

    something long successful memory dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def greet(name):
-        print(f"Hello, {name}!")
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
