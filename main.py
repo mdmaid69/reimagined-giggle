566dcfbddf1bec33ee2d18fc12d3e8efc292b4b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:04 +0000

    Hh rarely love steal section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def convert_to_octal(n):
+        return oct(n)
