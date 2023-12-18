af41085f502c8dbc1ccc8637810eb9fb0e23a629
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:46 +0000

    frightened they watch danger factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
