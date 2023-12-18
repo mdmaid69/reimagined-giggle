45ff9ffda9d1a8f79daca62c052ea54a72acfaa8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:38:59 +0000

    fence firm describe level collect

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
