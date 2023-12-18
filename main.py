9e1c94175e1a9f8d050fff210834e9649fca0a85
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:17:24 +0000

    square wake interesting comedown when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
