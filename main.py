dae91855d3e63278fb4c7b93f17e0ba4d32a5449
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:44:21 +0000

    mind violence door middle maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
