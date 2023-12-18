4057ec495ec77e741c79d9df8896c5bb813b9550
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:45 +0000

    fix neither hear clearly prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
