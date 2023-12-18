c37ba9f0881bf335d7cfce776acbefed5ad9544e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:19:10 +0000

    pain flow trust keepoff imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
