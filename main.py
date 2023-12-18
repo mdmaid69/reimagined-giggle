51378b985e1121e6aa77d5f270c666f7f58cc45f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:19 +0000

    block Oo apart maximum meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
