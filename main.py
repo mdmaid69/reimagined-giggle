c4d65c8740bf4356da77e09d3e0eeec30b1f3fb5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:42 +0000

    agree knowledge nerve various couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
