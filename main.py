bff4d4606a914629cea1d5a20187173e269676d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:01 +0000

    fly could too public pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
