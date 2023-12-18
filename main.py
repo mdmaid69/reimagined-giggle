d6e228e8f3de12c683612adf41488227d2be84dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:09:46 +0000

    arm machine space respect separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
