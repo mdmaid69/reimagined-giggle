3e72a7b58df6c84df661a7c5bc1615bc4fa93804
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:26:07 +0000

    slight name suggest beginning wet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import sys
-def exit_program():
-        sys.exit()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
