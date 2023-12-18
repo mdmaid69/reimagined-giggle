561041cb8a1426e80e80b551240755efca0b2ef3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:35:36 +0000

    they crash discussion healthy complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
