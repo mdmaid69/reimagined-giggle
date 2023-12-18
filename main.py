9b1e713518abacd3c06b49f61cdbf6aeffaac3c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:20 +0000

    shut temperature pupil experience bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_odd(n):
-        return n % 2 != 0
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
