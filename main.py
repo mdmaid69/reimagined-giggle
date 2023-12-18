7494f17f8b182f8f1b90ae05e3519bba3c8a4b84
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:22:14 +0000

    up avoid sea exact comein

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import collections
+def create_queue():
+        return collections.deque()
