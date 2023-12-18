4e435e048a2632b40ae6d72fd9a47f6ab5ec4eb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:32 +0000

    safety roll convincing hospital Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
