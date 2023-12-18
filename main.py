b430119aabd34f5fc95b387e9031635a64ac2622
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:31 +0000

    raise most reduce stick together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def subtract_numbers(x, y):
-        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
