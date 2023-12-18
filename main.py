fb982c9f672885900a1d3cc04f6570fc499de9be
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:35:41 +0000

    grade lift trick store myself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
