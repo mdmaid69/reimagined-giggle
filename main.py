6e0ed1c7ecc29699ed02ff821827ab0a9a183e66
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:51:30 +0000

    suggestion tip hurry sure this

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
