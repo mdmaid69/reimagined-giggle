b647efd1db6bf04cdc4c6f167e7174e57b726b14
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:53:43 +0000

    tool 571enough point story point

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
