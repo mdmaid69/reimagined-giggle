3ae4e4473e33532492968bfeb65d9a32e9b37dbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:44 +0000

    talkoutof maintain practice funny someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def multiply_numbers(x, y):
+        return x * y
