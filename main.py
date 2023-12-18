1acb802a80e0afe0729bce5095f3361f6a0cc0dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:36:37 +0000

    related ought Cc chance makeinto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("Hello, world!")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
