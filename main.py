06a6b252c1ec0e6bc8256d145bafb14121f3a96c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:28 +0000

    remark machine trip heat contact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sys
-print(sys.version)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
