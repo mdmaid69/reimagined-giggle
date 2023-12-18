5344ccd4ba93c6b1165da299c487f7d652033f96
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:40 +0000

    worry weather train soak how

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
