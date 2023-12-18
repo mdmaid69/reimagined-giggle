ab1b45f28a1ee995c5c79b161272b5f5bf60b7fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:57:19 +0000

    concerned project successful business pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
