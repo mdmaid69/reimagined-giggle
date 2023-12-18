30836f37743249d263c9c0ae8e5d8ce5ea791021
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:46 +0000

    wave respect hard present insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
