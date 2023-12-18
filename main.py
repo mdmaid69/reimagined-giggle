da15586f8de232b5ba2d007f930d0fd4d3dae354
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:17 +0000

    pick everywhere close married university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
