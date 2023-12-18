3cd70a6ae71030f539ea5f8437df1707173d5f2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:23 +0000

    turnup reasonable character piece match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def convert_to_octal(n):
-        return oct(n)
+def find_min(lst):
+        return min(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
