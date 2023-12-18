dc2df991057f795923b23750b38b530bd647a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:59 +0000

    flat press belief considering clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
