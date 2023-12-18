922da6fa68da937f171ea99da306b840c007511b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:04:36 +0000

    produce straight tidy related quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sine(x):
+        return math.sin(x)
