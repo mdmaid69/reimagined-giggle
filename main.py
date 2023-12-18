a14eafaf0b189c73469ddbc78d21c52c9d141b89
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:25:26 +0000

    desperate law flow hurry night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
