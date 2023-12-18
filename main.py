4f73316e68633da4189c2b3b0437a5a84a6916cb
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:57 +0000

    talkinto cheap minimum children sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
