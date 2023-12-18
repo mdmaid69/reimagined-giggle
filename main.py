4df5af2952d50fd994c43018b0a44c19cf9035fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:40 +0000

    both goback(to) any drop picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
