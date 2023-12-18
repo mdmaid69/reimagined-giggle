dfe4f0cccc61be8729e05a9e857204e76d230b02
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:58:15 +0000

    protest free gooff concentration tone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
