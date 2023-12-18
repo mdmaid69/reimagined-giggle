541dce3908c4ec375dd2686e1439498325bb4099
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:43:27 +0000

    immediate short compete form defend

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-print(sum(range(10)))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
