1613efaa3d4c0aeb247ba5de63be75c4fc4b8f03
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:03 +0000

    complain rent delicate split crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
