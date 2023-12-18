bca7b46c5d9f72288c62b5c805264212b469ca20
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:41:53 +0000

    lesson could healthy head live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
