15950caa5f525f423f9276d2b8f0095ef77c776a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:11 +0000

    present Qq change brush usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_index(array, item):
+        return array.index(item)
