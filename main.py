a83015d5ef55d1df5f175228dbefb34bd9f59c87
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:15:55 +0000

    invitation medium rush machine gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
