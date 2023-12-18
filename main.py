c7c11d90ea04f3e87fb53e56ce033fa8dcebda82
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:46 +0000

    issue have patient accidentally means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
