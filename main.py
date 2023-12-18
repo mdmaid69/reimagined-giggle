66bce39bba47c2a3bb7ba1d1a42ded3ec3ee10ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:24 +0000

    plate sorry wrong advantage minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def convert_to_hex(n):
+        return hex(n)
