cee85c3e73b3aa594c8ba60827c7bca5ff82567f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:20 +0000

    Jj period live loose keepaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
