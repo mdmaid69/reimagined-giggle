b669c2f7ff09daae826198388082b90ae5df3022
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:31 +0000

    dead know Ss help whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
