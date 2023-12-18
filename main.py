4bc20ac0dcea18062b22421d5a896d18098aab1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:10 +0000

    worth almost advanced man cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
