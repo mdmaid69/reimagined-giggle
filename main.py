2e03d7b09c3124eae1dc4a3ad60176f804549cc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:15:18 +0000

    course operation politics below runinto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
