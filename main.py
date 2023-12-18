a39c9290c03d52d34b4fb373d555f7546c8e07bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:16:54 +0000

    morning error idea fly insist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
