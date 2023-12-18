746e34ce09f344c9ee8a635e28a0c3eb54f66df2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:58:23 +0000

    female pin must keepoff holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
