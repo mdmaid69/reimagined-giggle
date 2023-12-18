a3cc6abb69e51429552faba961943b58df23e82c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:23 +0000

    press familiar sink nigga address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
