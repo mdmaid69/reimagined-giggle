66670f6458213c4229b74b1f98ae2ff436848098
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:37 +0000

    pain unless shock accept deliberately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def reverse_list(lst):
+        return lst[::-1]
+import random
+print(random.randint(0, 100))
