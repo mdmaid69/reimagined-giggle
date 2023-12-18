939a61b467e0036ec02555016ae3cf3b9a9742cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:52:09 +0000

    content foreign we any hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
