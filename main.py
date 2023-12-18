b657193ae16629c7ec126adfd4b4fa8c9285a826
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:44 +0000

    handle mend ring needle belief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
