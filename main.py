6a1c3ffa0bf328167cdd54e9eb51ad20b32d884d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:08 +0000

    apparently official hard seem opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
