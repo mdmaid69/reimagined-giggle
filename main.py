ae91756f7b3f79e2b3916aaecb4f985915a9b4ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:31:19 +0000

    swim suddenly imagination ring stick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
