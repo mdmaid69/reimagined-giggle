8587dc828d8bc5499bb2b085c028ab2489429ae7
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:10 +0000

    behaviour plain press hold absolutely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
