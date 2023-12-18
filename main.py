33305496548a6b7fe2b80c9ad473fe4b6252b304
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:12 +0000

    shame out ordinary trial brown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
+for i in range(10): print(i)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
