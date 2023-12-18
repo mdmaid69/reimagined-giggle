5d7aab679e84cdc88c7f07e9221212b8aca4a970
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:58 +0000

    breakdown interview trust straight address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
