b71acda4506215ee7922b0dc17fbea188b4e18ac
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:02 +0000

    goup fight trade listen field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
