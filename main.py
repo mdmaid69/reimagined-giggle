4f8f901ced6436061c07f4662c0b9117ce73ee9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:57 +0000

    base their with activity splash

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
