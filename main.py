e3caa755872d5c366ff1f13cd347dac0ae934da0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:52 +0000

    rescue inform date stupid left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
