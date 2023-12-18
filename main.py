105abff21e7f480675db20495dc01f45204fad71
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:38 +0000

    persuade wonder serve once freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
