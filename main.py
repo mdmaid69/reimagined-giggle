3c69fa92a8967e0265218f8a32b290ac2c62c8d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:32:46 +0000

    seem lookup promise gentle attempt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
