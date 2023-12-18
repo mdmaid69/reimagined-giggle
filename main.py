95b6f7ecb131f89e30bdb9df8efcde22c2f65158
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:20 +0000

    guess indeed display be grateful

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
