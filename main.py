34b3f469d49db29850a709fb593d89ae9892b067
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:37 +0000

    target begin holdon advise stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
