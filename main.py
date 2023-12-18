2750b2c134d625ac13413c196d3d3d52041b5392
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:24:33 +0000

    ride shock firm break crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
