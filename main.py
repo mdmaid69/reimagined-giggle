e512342fcd44144c455e4432af2727c4a82878a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:57 +0000

    often large hit terrible top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
