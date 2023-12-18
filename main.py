6261b0ebd2d2ed9979b8581a0e57753d8a3a4462
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:03 +0000

    callback broad hold run bright

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def reverse_list(lst):
+        return lst[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
