aa23633ebc6b992c5f9e09ecd970e77bc6236db4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:29:03 +0000

    heat free mystery both whatever

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def square_number(x):
+        return x**2
