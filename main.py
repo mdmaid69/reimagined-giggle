63c4cfa1ad56dcaac382bf1cd90bde60922570af
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:04 +0000

    fortunate new surprised special hang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
