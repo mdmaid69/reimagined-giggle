51657dc28132558bb0b3cb6240356b954ee5889c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:38 +0000

    first treat neat later fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def square_number(x):
+        return x**2
