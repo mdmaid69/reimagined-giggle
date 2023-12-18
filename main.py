d3ea7580142e1d418b4deaab142722d0c7870752
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:38 +0000

    border getup successful immediately phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
