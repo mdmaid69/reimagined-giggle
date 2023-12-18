54e447618127a88f78b6c0826ed5c0e814750082
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:22 +0000

    mean bear average objection exciting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
