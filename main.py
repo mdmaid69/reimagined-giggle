0ab1ceed65bdbbf09fe96e60c55e390b00b4e6eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:00:33 +0000

    holdout count excuse affect close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
