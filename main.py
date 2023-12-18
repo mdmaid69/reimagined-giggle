a279a3d21bfb2e549333aada9b35e1be001476a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:27 +0000

    popular remember getback clever crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
