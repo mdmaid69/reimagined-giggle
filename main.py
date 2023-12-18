3f49013b6fbf108664d04e801b04708f7c88a4e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:48:11 +0000

    backwards further mail hurt definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
