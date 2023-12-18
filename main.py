5f710532e1bd15f2b35f5165767aef6b77a8b2ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:01 +0000

    thinkof turnaway bet together woman

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
