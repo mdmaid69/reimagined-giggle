ea9a487705d98d41a8a3ba5d51ad9f9ac9836b16
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:11 +0000

    already fast Dd keepout crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
