9ec5e997bb1df293f063d6e0966660a3a00f1693
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:54:37 +0000

    exchange worst clean familiar shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
