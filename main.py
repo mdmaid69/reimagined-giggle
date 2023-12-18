b1ff74eb0c74d25a7889c4af84772b9cea2adaab
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:16 +0000

    somewhere butter fault attach tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(a, b):
-        return a + b
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
