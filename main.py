443b09491d937c7b754d1c1394850faf98707f4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:20:19 +0000

    certain write less most funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
