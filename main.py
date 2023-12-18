882688cf58a484a1ce5f0cb1afc1addb08389c0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:40:47 +0000

    journey noisy man please fold

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def add_numbers(a, b):
-        return a + b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
