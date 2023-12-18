75d16d5270357ed369ec931b827bb4c0efbe1690
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:33 +0000

    important education performance though thankyou

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
