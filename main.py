d9ba72081398a84221c085f9d5a7080749302937
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:28:29 +0000

    intend call section happen lookafter

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_odd(n):
+        return n % 2 != 0
+  def remove_duplicates(lst):
+        return list(set(lst))
