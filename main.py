7865e5c61e62e117aa628a674cc5124cbd419d89
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:39 +0000

    hour character conduct way common

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+for i in range(5):
+        print(i)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
