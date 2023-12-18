8cf098b62e9510a9f0d5e0b1c5f059dfedd589f5
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:32 +0000

    calloff march grand direct report

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
