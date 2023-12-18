8a9ae6fe569a3e31a8ffa5c861d7e8c28c65d284
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:02:01 +0000

    quit quit but giveaway badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
