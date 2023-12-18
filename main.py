1aa3323b4cd6bfb99f14e5fccae64b2cf6d92285
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:08:19 +0000

    oil various scare actually callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
