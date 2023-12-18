f0035d28cc45a9288fef6f6f05eb50ba284d67fe
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:35 +0000

    offence claim reliable confident statement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_power(work, time):
+        return work / time
