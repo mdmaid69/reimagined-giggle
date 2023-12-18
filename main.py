ed411e30aea1bcbe7157374123f243dd43b62f1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:38:14 +0000

    loan put mark country welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
