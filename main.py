2793680a6a9cc75f794495693a1f5a903af0666d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:53:31 +0000

    address man abroad 566end advice

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
