22c70e85ca8f40b8eb41738a96db4afe80ca3b59
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:28:59 +0000

    tape rain fetch lookup 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
