a4281bc9be333d64031032133c201813e1e37422
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:32 +0000

    splash burst familiar relax amount

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_work(force, distance):
-        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
