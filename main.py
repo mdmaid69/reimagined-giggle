60b6b28d4b81c46bd71d27a9c6176c3384633020
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:33:03 +0000

    transparent light instead sense insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
