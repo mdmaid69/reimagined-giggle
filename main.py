86335c7600759afd03d682a3faa0bc2b7cf66b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:37:15 +0000

    about confident successful gate modern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
