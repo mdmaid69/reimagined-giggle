5dc4d39f2c496481bd6e3602108ffd73030cd602
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:28 +0000

    describe our Ff cry concerning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
