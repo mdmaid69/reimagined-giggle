b65ac53732bedaf353e228d2b7d257d691430ebc
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:00:13 +0000

    brown mark clean boring save

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
