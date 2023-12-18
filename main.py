c897da12539c2b711a8e4b22c7d3fde5bea37763
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:41 +0000

    quick patience include cope against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
