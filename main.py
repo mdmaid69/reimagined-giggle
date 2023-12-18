1485c6644bc732ebf30ba32ec65b5aba8e62c6dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:43 +0000

    protest same sensitive reduction laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
