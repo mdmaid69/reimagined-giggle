2febd6c8ca193538392a04d6466605cd1bf9e9bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:35:43 +0000

    sky roof mail peace wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
