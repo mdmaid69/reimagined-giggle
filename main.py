51a7729de3110a1a997687ff952ac21e1b6c5817
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:51:28 +0000

    any Ff middle year identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
