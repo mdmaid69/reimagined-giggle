72e880692c39fe29eb110409a1911e10b3b54779
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:13 +0000

    defend ride slice deaf typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bytearray(array):
+        return bytearray(array)
