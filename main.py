66a68829f9618f3a5525e5e7ace0cabb57cac56f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:22 +0000

    keep drive load heart shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
