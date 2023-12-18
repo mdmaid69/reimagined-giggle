e539ab4c5adb55927ca229972353898bdd854920
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:51 +0000

    bringback surprising hit different dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
