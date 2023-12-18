e29b7dffb2df8bfce05d353f85b576f7cd63a8e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:37 +0000

    wind 568energy pot tread priority

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
