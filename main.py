d0eb9b5546861c78ba9621997ad5bd57f1b7f12b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:27:22 +0000

    strike cap must sister law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
