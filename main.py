a26b855ffb8d10a73929ac5a948eaf26fc95d892
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:02:33 +0000

    apart public this touch suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+x = 10
+y = 20
+print("Sum:", x + y)
