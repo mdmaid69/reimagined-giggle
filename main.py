ad048a0d9cfab042cabf8dd4554832615abf78e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:16:57 +0000

    them shop needle disgusting holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
