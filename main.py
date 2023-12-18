3ce1eed441944f4a199e80e25c9de19316b1057c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:44:16 +0000

    much understand above drop break

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_array_to_list(array):
+        return array.tolist()
