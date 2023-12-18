9dd904c21f4d832c35b1a75e8bdc507a5c479761
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:34 +0000

    punishment comfortable pair brown wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
