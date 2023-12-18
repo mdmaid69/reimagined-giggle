9a44330a1abd1c4e5b32d177268638bae1b975a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:16 +0000

    appearance among character as shine

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
