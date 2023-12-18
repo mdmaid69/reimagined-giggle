b8c7d577089191d2879987d3b50af2d5aa0b8587
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:05 +0000

    open odd turnup walk settle

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_speed(distance, time):
+        return distance / time
