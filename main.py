03e9a90649274307d9008dca8bd16a4150f9555c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:52:46 +0000

    race delicate tragedy lay actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
