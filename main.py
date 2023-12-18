a5d48ea491ea3f5a8ba403f34640c0d74685cb40
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:24:58 +0000

    broken fun correct flow broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
