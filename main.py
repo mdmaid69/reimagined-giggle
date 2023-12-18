1633e668676a9d023c154cd506118313f6d42de6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:38:47 +0000

    swing activity official flood race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
