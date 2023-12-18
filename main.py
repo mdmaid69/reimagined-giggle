0e585ed92b54fe3519e62bfbffb8dff3c94ca9e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:05:35 +0000

    base punishment disappointed flood bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
