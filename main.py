d8df2f3d969a2094688ea625c04cfd1552c135c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:08 +0000

    pen male shop spray fresh

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
