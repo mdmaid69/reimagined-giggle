4ffe34b9a7b2837b769437931738d409d2fe8a41
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:07 +0000

    example crazy wave market somehow

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_min(lst):
+        return min(lst)
+for i in range(10): print(i)
