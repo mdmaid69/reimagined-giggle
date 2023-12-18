feb9b2c5930aa1c915df6312ea10b2084240e2f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:23:05 +0000

    inside wise address person boring

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
