c6b75b51a0c03f6a09a0deddabb921283bdbdc7b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:55:21 +0000

    takeover artificial own hand lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
