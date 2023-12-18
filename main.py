5033e4e28b0847b5c6c2c5e281129e5559645d23
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:47 +0000

    distance further quick classroom recommend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
