058f2e330146b6d47fede5d6a0d8168a3a1fce2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:57:31 +0000

    wealth edge 179bite tip negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
