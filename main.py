e9758c2a6174779c6d4ece7765b0e36ffd2bc690
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:51:57 +0000

    move pause lookup design complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
