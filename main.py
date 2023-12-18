183783d637c6e68d6d708fd7744a2ad5d412e5c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:28:00 +0000

    refuse shape it valuable fall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
