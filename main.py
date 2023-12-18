c2ba638e92d0697f36d74e5984d63deb71933f7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:45:43 +0000

    report bang conversation takeoff most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
