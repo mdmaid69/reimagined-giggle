56fc7eb7cb88be2202b3e9355e135d9d1ddaed2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:32:08 +0000

    change detail disapprove silent march

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
