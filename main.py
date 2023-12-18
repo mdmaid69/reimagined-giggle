3ebc4b35d9b105765c2d104514a8f2aa4a1f457c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:10:24 +0000

    culture plan some along count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
