c2d8535a8447cb62c3fb96d73424a07b316fa053
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:19 +0000

    computer them giveout heat quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
