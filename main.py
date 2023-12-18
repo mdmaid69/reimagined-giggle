cfec8512e884985d258eec5d329b4b661dfc47ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:23 +0000

    test confuse mean debt sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-print(time.time())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
