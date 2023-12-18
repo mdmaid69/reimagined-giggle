be5e893304cbcc63737df1ebaefbca0369093107
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:42:49 +0000

    false win season takeover normal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
