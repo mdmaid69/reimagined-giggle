8e975b18687ef34138ae82e5db2160b38a0e3db9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:53 +0000

    sail next breakup oil suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
+import sys
+print(sys.version)
