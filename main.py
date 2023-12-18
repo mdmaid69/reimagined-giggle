2439176df922dcafd9fcdf1e17102c7be95c7010
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:28:18 +0000

    track main expect want grade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
