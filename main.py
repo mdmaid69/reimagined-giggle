ba5292d1a47aa55d48de22606a008b08239283bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:16 +0000

    mention steep giveback spread autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
