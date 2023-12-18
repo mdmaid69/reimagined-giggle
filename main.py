3c033f8e94816ceb3a5eb2a063c5703ff4ecc04c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:45 +0000

    false some challenge ago interruption

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
