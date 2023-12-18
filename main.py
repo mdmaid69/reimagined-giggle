35b2e62de27bb62c53ef60d2733ff67becc3cfd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:30 +0000

    be beginning unite them stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
