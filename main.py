c35a7abf424da1a98e9133959342475753cda50e
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:24 +0000

    correction cause hardly movement unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
