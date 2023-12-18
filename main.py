0daec9ada5b1902b89da9d24212a82ed346c5cb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:26 +0000

    hear boil mark first backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
