f01bbf197cacb84da371b589b4c441652a5abc6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:10 +0000

    unite now everybody sleep basis

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
