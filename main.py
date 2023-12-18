6f90f20de239b2783a1ab86c03b88255bc480595
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:18:46 +0000

    think play today pay time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def check_if_array_contains_item(array, item):
+        return item in array
