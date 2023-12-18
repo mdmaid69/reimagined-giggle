67b0997ddcc80677cdca7eb4e44e00b73841f13b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:52:07 +0000

    disappointed free gather since wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-print(os.getcwd())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
