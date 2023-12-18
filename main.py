bd1d3b693d93a403bc2fb322580198de4a5804db
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:44 +0000

    preparation conclusion eager care drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def is_even(n):
+        return n % 2 == 0
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
