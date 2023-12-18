a171ed20940c56bc7234f22b051ed6f7ea515526
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:34:18 +0000

    separate test drive watch ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
