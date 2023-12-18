2c9b7bbb32bc262619c6203c29215f9b92f2d6e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:20 +0000

    phone guilty offence force soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
