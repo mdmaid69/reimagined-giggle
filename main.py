78758dcb16b12ff06a16e5f05fe3dfe30fd30f93
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:15 +0000

    dream play slide worry generally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def multiply_numbers(x, y):
-        return x * y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
