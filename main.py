e45c2a656ba9cc51233b6b4ec14ed234c3b99e8f
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:32:26 +0000

    justice dear justice nervous colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
