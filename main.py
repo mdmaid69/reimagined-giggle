f312965a10cccf93cf77b613f908c9a9f6e486bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:48:09 +0000

    cost various agreement suit blue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import platform
-def get_python_version():
-        return platform.python_version()
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
