657fb2208f3a23eef37f9fa3075cf9228b8b9161
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:20 +0000

    shock report touch run positive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
