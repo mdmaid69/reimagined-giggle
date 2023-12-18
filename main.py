87c40ef532cc64d375ed913c211e9d365d32fa28
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:08 +0000

    race vanish pressure spread none

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
