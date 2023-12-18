4bafbc21cc4857e39f226e6ef65fdec0d5ad2d09
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:41:39 +0000

    dark upstairs spin government question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
