f8d63745a31a1dff84150acc20514a8faba99048
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:48:23 +0000

    command Zz mind knowledge hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
