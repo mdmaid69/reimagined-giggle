28aeece15f48d96b59794e59d18f4f9a24a242e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:32:24 +0000

    correction quantity lock bring branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_typecode(array):
+        return array.typecode
