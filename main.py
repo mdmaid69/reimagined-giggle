d374ffd40d36d589055b31495fbe98e6f361d1a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:54 +0000

    crash no figure well convince

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
