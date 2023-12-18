a7dbc0f0c234f384436751d3576ba93f2dd71541
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:53:45 +0000

    sound doubt worried well Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
