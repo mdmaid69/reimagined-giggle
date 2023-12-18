02d471b647bb8606e60f997d0a286b8880386136
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:55 +0000

    sail bringup explain glass picture

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
