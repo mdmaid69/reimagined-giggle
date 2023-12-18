7c97def70260f8fc29dcb71175284b42f9ab17ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:59:27 +0000

    hear kind moon straight fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def find_min(numbers):
+        return min(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
