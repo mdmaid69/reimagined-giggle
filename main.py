7cdf80f085a74b19aff4f7cace1ac9916d09b36b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:41 +0000

    takeback ever squeeze stretch because

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
