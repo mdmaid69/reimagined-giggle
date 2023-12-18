7acd7e0df5fb318acabdf68c393f70014f5dd6c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:45:07 +0000

    feel 564encourage straight in instead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
