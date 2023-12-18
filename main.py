15bfebea2ba405c2df4874e12e4f61f03d58bfdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:54:55 +0000

    wire even public trouble excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
