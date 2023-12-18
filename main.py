07ab57cc2024a448c0510fd3d4d7fdf1df97cc38
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:12:00 +0000

    fetch crazy near makeup give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print(sum(range(10)))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
