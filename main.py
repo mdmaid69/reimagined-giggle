f9b0ba87570a444b2e9aa3aba68a7210cee623a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:01:34 +0000

    go butter help cheque taste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
