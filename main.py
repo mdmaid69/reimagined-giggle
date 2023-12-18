9c8cc757a0902fc4d95d3a1fa997f147c748bdda
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:19 +0000

    different marry talk fairly tear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
