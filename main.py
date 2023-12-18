ce6206543a26ec3b658658a456a805d857725dc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:55 +0000

    takeout see familiar prevent move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import platform
 def get_python_version():
         return platform.python_version()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
