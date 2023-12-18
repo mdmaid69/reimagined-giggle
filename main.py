d2bdc3fe1f317db51b28adfc6e5387354e6c4604
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:31:59 +0000

    interesting further comeoff straight change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_typecode(array):
-        return array.typecode
-name = "Python"
-print("Hello,", name)
+def get_string_from_array(array):
+        return array.tobytes()
