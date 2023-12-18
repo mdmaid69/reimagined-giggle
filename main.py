9e26048427c188c9417a16fb01aff783381b1871
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:12 +0000

    blue lean work claim exact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
