2e2b28516e191cd172770953332fd585fcfb706d
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:30 +0000

    alive Ff emotion bother miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
