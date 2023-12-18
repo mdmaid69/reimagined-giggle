c49e864afec8babc8895979b31275ecd783aa849
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:53 +0000

    letter ashamed confident accident advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
