e2ae24cd847787a6306b54e29a9b7911dd1eefa7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:27:54 +0000

    bad slice become real couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
