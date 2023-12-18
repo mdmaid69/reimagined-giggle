e943f2608adcf69935db60c44455c11ba17a14bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:31 +0000

    fast loan organize letter stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_current_directory():
+        return os.getcwd()
