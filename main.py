205ffb21ee037d023bae754c49537e4d2fa4da2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:21:14 +0000

    window suppose Gg claim loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
