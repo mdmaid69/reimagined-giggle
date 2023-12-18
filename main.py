0d2b05c052ea84dcb8db8ba5e9bf5cfa3ef577c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:12 +0000

    city record colour clothes jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def multiply_numbers(x, y):
-        return x * y
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_current_directory():
+        return os.getcwd()
