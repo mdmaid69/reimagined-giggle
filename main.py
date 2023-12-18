c4cdcb470557a508bf8a072d41b22a8f586d50ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:46 +0000

    morning process remark over mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import time
-print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
