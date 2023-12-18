ddd30ea0d98f12995c39749d53e39dfc40e30942
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:26 +0000

    mess Dd who concentrate main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
