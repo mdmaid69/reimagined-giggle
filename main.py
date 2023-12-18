1deec80e3b035fac5e3a3a9e43078476895ce46c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:40 +0000

    race opinion relation complaint room

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
