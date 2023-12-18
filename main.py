439c1b80eae6e8b26937dc45d52c2664a360a485
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:16 +0000

    bridge piece impressive centre pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
