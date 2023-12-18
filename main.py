f7ffbebb30403e6f4ddece4939f67f9f33305377
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:40:23 +0000

    tradition difficulty breakdown minority nice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def is_even(n):
-        return n % 2 == 0
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def clear_array(array):
+        array *= 0
