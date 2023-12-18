ba1755794aa6a1eed1bad909c9f74eded3562251
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:03:04 +0000

    afraid measure loud point dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
