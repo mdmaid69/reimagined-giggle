f2116a6bc44c710f69ab8eee5b0bf18b7ae1a4e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:01 +0000

    ring she explanation university sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
