ca6dc23b0db4e46c42878eba704beb6b2a60ea91
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:56 +0000

    rub test lift tap fact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def find_max(numbers):
-        return max(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
