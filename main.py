2798eaefb13b62a165fff4c233464b541cff16d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:08:07 +0000

    extraordinary set argument definite hair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
