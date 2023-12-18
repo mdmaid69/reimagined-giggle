2266db59a41886deb2d24abca3d3c2d25942a0e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:55:01 +0000

    black slowly generous man eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
