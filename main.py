245b8bbf044178bfa0c76dd1e0d9a57deb622885
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:29:22 +0000

    value workout language we case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
