977bead507f87b7a1ee7afe78109af0edbb483f8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:52 +0000

    broken distant ask success minority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
