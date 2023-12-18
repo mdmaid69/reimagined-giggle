5d12b2780c1c896ab719244d7390bfcdef250ce3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:47 +0000

    thumb transparent Ww free speak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
