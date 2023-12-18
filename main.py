82c1c232913d61bb96cbe66305cce8455546cfd7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:19:40 +0000

    serious expect burst habit shut

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
