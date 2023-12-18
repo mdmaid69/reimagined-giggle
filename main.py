33e174aad6f7addfabfda34dfa909a0c7edef85b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:04 +0000

    expression comeover difficult face period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
