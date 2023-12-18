111b9dd2731867f51f0e656aedefef32c12e6a50
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:45:14 +0000

    free live fit gate scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
