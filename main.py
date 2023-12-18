02b93cf8896e51382119ab6dd50ecc5f7fbad7d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:56 +0000

    fire error flow relation consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
