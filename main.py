8c8457c260c41ce19f6f9c83e99d5a8afed193ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:03:49 +0000

    unlikely nice brave letter plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_length(array):
+        return len(array)
