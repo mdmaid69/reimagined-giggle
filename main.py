3dc14f87822d0e33172b0ac14f17a993918bc648
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:58 +0000

    forget during comedown nearly short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
