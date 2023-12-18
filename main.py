06e0f0ebea186993fd602f6b32f8b6cb884fdb5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:48 +0000

    busy string Yy still stamp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_typecode(array):
+        return array.typecode
