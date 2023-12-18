6cd1de4a450d7309e1bfbe6f7f9240fcf659050d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:06 +0000

    belong sink interest brother before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
