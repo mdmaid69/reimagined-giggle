3c36f52e8a28adea042b856730b8ce15404f0ee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:32 +0000

    habit radio move brush dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sys
+def exit_program():
+        sys.exit()
