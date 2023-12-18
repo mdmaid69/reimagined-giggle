90642c381143fc4247bdba669dc82c969aa156d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:32 +0000

    case cause awake open steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+def multiply_numbers(x, y):
+        return x * y
