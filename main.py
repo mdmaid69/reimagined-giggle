f7044685afcd8c195fd2f52dbaf34cbaa93cf037
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:13:11 +0000

    concerned house profit train time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
