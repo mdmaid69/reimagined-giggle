530c1e6e1687ae6973d09e5a8b50016caf152d94
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:13:37 +0000

    owe rest contrast pause bunch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
