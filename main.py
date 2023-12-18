d49e4e4b13b7b45f6daa7d6eb043c4b23547d497
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:56:53 +0000

    close regard character protect there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
