53d5f8bced54178e3d0730517dd81882979a4ef5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:10:08 +0000

    greeting quantity gate innocent practice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_min(numbers):
+        return min(numbers)
