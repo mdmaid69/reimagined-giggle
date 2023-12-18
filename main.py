dd7827f1bb7d641b35eb6c67db422b205e4eb6f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:28:42 +0000

    doctor practical match turninto market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_set(array):
+        return set(array)
