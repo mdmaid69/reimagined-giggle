9e332835dbc531573000971ed48e91c3150b02f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:15 +0000

    touch gift capital ill pure

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def reverse_list(lst):
+        return lst[::-1]
+print(sum(range(10)))
