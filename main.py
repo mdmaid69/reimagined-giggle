5eea657df48a3df736e799be1c0cb5c79fba315a
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:57:51 +0000

    listenfor cross month secret feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
