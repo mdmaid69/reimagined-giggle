bb9366c12751d42abc4591c1a037b62441d1f25a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:48 +0000

    hold Ww name ought instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
