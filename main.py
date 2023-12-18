a6aab8d60cffbe10b51b67c24beb6ea1bc6f979f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:17 +0000

    thought callat reason stretch breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
