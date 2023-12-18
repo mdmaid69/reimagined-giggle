9a5620f284f1753651051fcc25b37ecf55e3aa84
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:40:51 +0000

    also straight equipment group naturally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
