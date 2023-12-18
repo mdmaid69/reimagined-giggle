829238c21bf50503e1db5c5cdd4ed6355ed5a447
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:37 +0000

    form excellent splash reduction punishment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
