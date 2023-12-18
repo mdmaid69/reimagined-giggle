303102d955c80442f98e1c19127ae6323ccf369f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:04 +0000

    still free into card busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def sort_list(lst):
+        return sorted(lst)
