839c0884172fc0fb3893ce23a7aafe5cd7752514
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:21 +0000

    lot shock talkback quantity book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def reverse_array(array):
-        array.reverse()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_current_directory():
+        return os.getcwd()
