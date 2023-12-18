5a3ec0cd967a7de744836f71de2f7342e04c05ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:22:19 +0000

    forward sea charge Pp why

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
