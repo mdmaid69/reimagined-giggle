96eb18797c8e06a4f42f75fae8215cdb7acd8a96
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:52 +0000

    die bill word poor sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
