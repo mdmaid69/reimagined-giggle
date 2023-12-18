6dc9a15e9a27433d090232c505ecf9be76d43635
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:09 +0000

    long smile embarrassing regional spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def count_elements(lst):
+        return len(lst)
