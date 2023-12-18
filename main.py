989a6ce812712dd189d640ab322beb79639adfda
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:58:08 +0000

    refer once damage mixture depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
