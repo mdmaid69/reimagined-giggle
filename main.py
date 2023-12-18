bd23724a28295502dc4b92b82721eca5454625ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:00:48 +0000

    tend shock deaf pencil round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def cube_number(x):
+        return x**3
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
