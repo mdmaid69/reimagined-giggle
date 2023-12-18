4103d427d2aaf7a1158312c4fadb47d080251557
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:46:44 +0000

    downstairs swim human advise private

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
