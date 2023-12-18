bf69bdeebe114a0d2e55100e8f379a53ac1babbd
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:36 +0000

    rest team far spite giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
