fffbfec90a06366ecaa291008cb5229c0398da74
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:12 +0000

    whisper letter father practise hold

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_area_rectangle(l, w):
+        return l * w
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
