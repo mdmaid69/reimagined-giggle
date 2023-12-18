970fbb7dc8329e22eba1b699269d648a8278dc33
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:00:51 +0000

    size experiment under judge difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def roll_die():
+        return random.randint(1, 6)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
