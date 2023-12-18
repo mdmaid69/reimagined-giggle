9013635ca946163ecf13f5ecb2fd49794956ef7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:35:44 +0000

    speed away trouble demand apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
