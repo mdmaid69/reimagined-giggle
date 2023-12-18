9a5fa455bccd81e5b68de598c2d411753c8ba5d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:16:24 +0000

    unhappy waste skill sort place

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sys
+  def get_python_version():
+        return sys.version
