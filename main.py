f9f38a2d6ddaebcda9e31fbaf20fffe62cb70f25
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:33:17 +0000

    post sure before leave match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def square_number(x):
+        return x**2
