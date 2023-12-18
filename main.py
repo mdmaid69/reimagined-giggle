d1b22874d68170be8ff25480b7ef922ec5a4cce4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:13 +0000

    pause tread culture talkback exit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
