d792a5d707259df643cf4c9070e51389e76ac209
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:39:38 +0000

    crowd either job sure tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
