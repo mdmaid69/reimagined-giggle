8fd2e9980c7148c75c80a78ea184c413951bd280
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:01 +0000

    draw about absolutely thinkof feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
