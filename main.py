99fa058120a7f215ae8f69f1515f0f7bc91e2ff5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:02 +0000

    564encourage support shopping hang closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
