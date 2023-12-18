ec272c77c3d4d3a05f34f34d71e94281d92f1bc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:40 +0000

    179bite lock risk bang politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def square_number(x):
+        return x**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
