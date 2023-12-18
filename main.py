ed32d92512f0161bb979fb187b772f75a2b34862
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:48:26 +0000

    whichever by laugh possible bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def cube_number(x):
+        return x**3
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
