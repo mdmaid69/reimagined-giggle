ba921c5e2f8e700ab3f08d17bdfd1b81845e8093
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:36 +0000

    government for yes up charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
