0f4b8e7211ac734c286b59bd032c91067429a9b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:49 +0000

    before compete excuse activity home

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
