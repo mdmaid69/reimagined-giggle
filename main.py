9125c03a17aa834e5d5012799de4ecb13b630929
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:23:14 +0000

    excuse everyone male tip strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
