15e92882d54cf4ad4f4baf26042822f57463359a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:10:31 +0000

    ago famous ball catch least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
