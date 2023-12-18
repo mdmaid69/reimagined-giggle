85edc9709b78df0349048acce978abf44d71dd65
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:51:39 +0000

    rarely fortunate movement interview react

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
