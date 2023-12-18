248a471948d714c6359b91b8933f6d9cc2fdfa98
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:32 +0000

    he attractive like find respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
