6beb00cb39c7cfcc43d8ca0b8d98cc16717a57c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:39 +0000

    miss education difficult its estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
