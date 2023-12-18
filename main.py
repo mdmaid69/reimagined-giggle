0432314b5bd6a045a1e2356911420fb8f22c6755
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:34:32 +0000

    born breakthrough service community thought

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
