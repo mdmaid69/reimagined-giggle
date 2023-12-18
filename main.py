9c34f532c0ffa163c366f51f528c09b8023af64a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:54 +0000

    admire perhaps belong trip part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_list_from_array(array):
+        return array.tolist()
