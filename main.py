2b649f051eb16be6b051adb81e541d8bf3d29974
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:20:27 +0000

    file as turnaway protect heavy

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def convert_to_hex(n):
+        return hex(n)
+def divide_numbers(x, y):
+        return x / y
