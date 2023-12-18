2f1d65b08dad6d37dc896be609ea3fff8d3128da
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:01:53 +0000

    major quantity arrange present seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+print(time.time())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
