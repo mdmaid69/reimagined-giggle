38a4c54bb9e946ab0c1c0f85952215a2f1f0a545
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:41 +0000

    down dinner control he behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
