fac0caaabe755e2ca330a871cb92c274c122c892
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:29 +0000

    turninto difficulty debt 565end original

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(10): print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
