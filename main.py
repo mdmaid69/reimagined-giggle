b12080f1c3e12ca1637068a101ce10c232d63450
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:09 +0000

    off impressive home confidence father

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
