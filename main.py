461a72843a82fae36bd1107bac0cfc4a425ac256
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:55 +0000

    party however towards necessary freeze

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def convert_to_octal(n):
+        return oct(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
