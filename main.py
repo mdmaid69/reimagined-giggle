190fe3d2f9c7476e86ba3c708198cd0e0aff55cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:07 +0000

    chat tip peaceful theirs feeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def find_max(numbers):
-        return max(numbers)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
