9ccaa12e0ad37abd527641480ba709373a1b2097
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:08:19 +0000

    drink wet chance basically bother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
