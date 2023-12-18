869715a093c4e5f48eb2b6e43f334e93a26e1bd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:56 +0000

    advantage coffee clothes anyone blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
