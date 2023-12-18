bcf45eeb96f5f87cafa60198c1bf64239f7146a2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:06 +0000

    standard figure the rough both

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
