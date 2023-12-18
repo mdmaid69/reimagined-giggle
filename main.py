202693b1eec6fcce85ecd8d65585b286bdc793fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:59:37 +0000

    fear freeze collection reason accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def subtract_numbers(x, y):
+        return x - y
