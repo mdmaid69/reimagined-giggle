10f596716c777b26c9f61cad531812c539a4fcf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:45:43 +0000

    and study Oo enthusiastic trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
