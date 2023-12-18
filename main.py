0efa8c0971f3b4f5902db9e2d4bc4e38128bd5fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:48:31 +0000

    close upstairs either noone shame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
+def find_unique_words(sentence):
+        return set(sentence.split())
