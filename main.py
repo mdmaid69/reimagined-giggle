5466ef27a7c05629592b9e793f6212442536f634
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:22 +0000

    real crash think careful collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
