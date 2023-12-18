80f3ef234d8e9aeda19a9b84a20419459cb86ddf
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:23 +0000

    talk vanish pronunciation guess male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
