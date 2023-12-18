83b4299e81870275d222545f576d62e10fdff441
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:36:34 +0000

    affect tradition spare film hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
