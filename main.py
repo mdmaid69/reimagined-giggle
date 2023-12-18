515e720bb8c562c5e91573682e5f8e02e374a2cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:59:21 +0000

    definite himself drive account public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def change_working_directory(path):
+        os.chdir(path)
