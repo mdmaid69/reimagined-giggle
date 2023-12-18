e19ed6688309275b62cd94726cd664e6064d6d97
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:26:35 +0000

    slip tragedy fly protest excited

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def convert_to_octal(n):
-        return oct(n)
+def count_characters(sentence):
+        return len(sentence)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
