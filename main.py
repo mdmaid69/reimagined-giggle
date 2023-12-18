e19e784dc0862155400500cde320e242e0422f83
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:15:46 +0000

    present angry cruel nature like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
