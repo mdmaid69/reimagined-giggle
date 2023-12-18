621c495781b7f901c27cd220a09a839d1444abf4
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:27:44 +0000

    correct example fix difference cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
