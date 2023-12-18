547771c53a6526d62ca693eb8ae83988793e82d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:11:32 +0000

    think bush before thing usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
