9060e3bad586d0942bef145052cb5ed1bb319ff0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:05:47 +0000

    best discussion search like whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
