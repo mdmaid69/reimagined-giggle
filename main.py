8b6598e7a07ff2045e701f3f661740935ed8228f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:39 +0000

    cut live next game various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
