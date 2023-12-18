cbad62f86d14e3f0964f5f2af0e3c2983cd29422
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:20:05 +0000

    stiff giveback soak public run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
