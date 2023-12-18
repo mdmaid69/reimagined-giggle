70e4a9f4c2579ce41baf3af52b1e257e51bcc161
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:08:06 +0000

    suppose allow afternoon smile range

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
