d089c65054a6916230c8dca53bdd096f7ea1b684
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:19 +0000

    walk uncomfortable trust plenty plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
