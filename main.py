a90a5454cffeaccb38d3afafadafe115ab9b2f6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:10 +0000

    legal afford close cause patient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
