fb29a2ec1262407a0a3836ca15459502cf559cf7
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:50:53 +0000

    loss nearly breakinto situation partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
