4a84368929ce00f795f95c2502e1d25f45ea8cd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:46 +0000

    major serve exactly neither depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
