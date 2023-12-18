35288a04e81e48a946d86aecb0d6a162c29c8b24
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:21:03 +0000

    expect spend juice shoot good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
