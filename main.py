db35684926f37a3c61d56eaca3652f2b0c84cb56
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:50 +0000

    quickly harm duty test event

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
