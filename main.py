95b1ccd779090f819e76608f5026a2fbcffc87d4
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:54 +0000

    damage only level control any

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def add_numbers(x, y):
+        return x + y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
