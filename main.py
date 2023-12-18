7ac29a92f9486238fb81f99dd1eb20800f5a39d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:36:06 +0000

    takeoff cost accident provide bang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
