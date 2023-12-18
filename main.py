32cda19ced0363abeabea5d0745fa0663a8fee26
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:57 +0000

    common behave row church secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
