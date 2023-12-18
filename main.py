15e9a96216e3cad3d8df0ea9f4642f4aa990e326
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:05 +0000

    mark poor contact gate belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
