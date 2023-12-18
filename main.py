53cc16a63f18e7e46d3445083eda491a33de97e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:48:24 +0000

    sharp accordingto question hardly 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
