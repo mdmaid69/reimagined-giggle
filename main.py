23944c52513c4ad83f3059b1848a47758d8f9f66
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:26 +0000

    uniform 567enemy pretty heat correct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
