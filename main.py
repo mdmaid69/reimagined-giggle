d3d8223b7109aa4ca7abc23b71d22a64aa8f1e3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:43:23 +0000

    balance over explain opposite whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
