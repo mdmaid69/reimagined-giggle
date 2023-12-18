0ccf3cd7a89074b0ac975636311e398e5df6ceb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:58:52 +0000

    rescue impressive else base with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
