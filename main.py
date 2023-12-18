febf703c2001bae4535902dcdae4e4b5fe6bbb43
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:19:56 +0000

    team later upstairs guilty mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_itemsize(array):
+        return array.itemsize
