de3609d5270f2ebb035ad02959127f331a1590c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:05 +0000

    crisis community wet turnout he

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def square_number(x):
+        return x**2
