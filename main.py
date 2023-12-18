91ca768d9989b0bfb1fced21b8758ec444059f12
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:36:57 +0000

    accordingto settle injury regular definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
