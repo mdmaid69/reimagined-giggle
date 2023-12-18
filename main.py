07746c73b61f0879bd8d5d3ebd8fbfb1d6387a80
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:03 +0000

    straight sleep satisfied draw present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
