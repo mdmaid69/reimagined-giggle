2af6b2a75783c6b751ddcfc6b218a5ce2b845965
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:25:01 +0000

    considering common why anywayanyhow tap

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
