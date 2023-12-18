033b99277e0fd65f29c831eb0196921248135df9
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:00:40 +0000

    unpleasant dull everything quick agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
