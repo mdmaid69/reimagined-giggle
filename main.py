3c5c152f64f59885f175aa763b494811221dc3f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:05:51 +0000

    behind put grateful date thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_speed(distance, time):
-        return distance / time
+import time
+print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
