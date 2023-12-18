b8346693bd713d1b3d08e8120e039ee195de70ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:49 +0000

    communication file grateful actual idea

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_density(mass, volume):
-        return mass / volume
+  def square_number(x):
+        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
