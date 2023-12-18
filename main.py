f25132000093d03ba8b1969f692c39237ef83e29
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:02 +0000

    male pattern bet steer water

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+print(time.time())
