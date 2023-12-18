b4603de54ac8224219b3393bcea9e3d6b201e894
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:32 +0000

    scream bottom gather personal shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
