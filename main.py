4c148bc58457f7d6b35101b2ca11eff04079a656
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:02:13 +0000

    bus moment early interrupt please

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
