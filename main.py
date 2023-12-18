16a72fc25bb51bde9bc23735b08b14ea41f5d6e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:27 +0000

    this what half before criticism

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
