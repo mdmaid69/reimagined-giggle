d8679c978da8d9d7b9a8a2f2a16bd4757a9501e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:01:44 +0000

    collect organize allow right test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def cube_number(x):
-        return x**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
