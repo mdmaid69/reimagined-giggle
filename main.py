0e7fd971d9f95dc17233571d5a64584a1f0f1676
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:53 +0000

    post create behaviour giveout shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+name = "Python"
+print("Hello,", name)
