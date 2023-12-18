87c2a8b95ef8bcb66d78296e7166b379d3103f9c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:38 +0000

    part cap drink expect confuse

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
