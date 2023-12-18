ba9653ded20bd2cb86eff0b1dd56ad6f829c4ba1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:49:05 +0000

    preparation waste time swimming less

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
