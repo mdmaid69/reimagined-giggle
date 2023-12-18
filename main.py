c9f15de2346fe04c6ad7b94b31da54b6f39a69ba
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:34 +0000

    spin part back ordinary hard

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_min(lst):
+        return min(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
