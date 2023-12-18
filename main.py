4f2d252b228babfed2cb14b423fb567bc5f28dc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:29:04 +0000

    among claim press instrument soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
