08ef274f4b8967926b61de490aed3a60c7971f7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:52:35 +0000

    opposite very television detail future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
