42990d600a610d3bb4dbc42bf39acc14e7bbab6a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:58:10 +0000

    fact roll anything pollution inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
