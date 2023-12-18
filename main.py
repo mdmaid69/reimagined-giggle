7bed63bcbd0485d7485f326963cbd77d9b598d08
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:06:59 +0000

    business loose criticism somehow warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
