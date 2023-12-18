e5ce73e6b8e29c78961a378d1fb01381bc0c1e9a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:04 +0000

    extra for tragedy anywayanyhow cheque

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
