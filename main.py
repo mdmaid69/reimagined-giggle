44b0bd42a85d278cb7f94afe2e0630a65fc58bb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:10 +0000

    surface touch butter afternoon variety

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
