d7bb4afae629fc7134a6af39a72d595d2b0324a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:55 +0000

    trust motion turnover cross check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_min(lst):
+        return min(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
