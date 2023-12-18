ff98e22553a4e422c2532cec0ac6042e3c60def1
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:50:02 +0000

    under content likely there among

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
