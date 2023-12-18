784c7de429f2ff47c9e5a3e8e60ef3887baabcc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:20 +0000

    mood yourself proof glad recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
