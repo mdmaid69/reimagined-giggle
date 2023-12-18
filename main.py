663af067535996138f7313c79222e2db0d470069
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:18:11 +0000

    ought flight danger arm understand

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
