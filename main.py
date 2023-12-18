c1e34f77a2e1906ac8d4804be56ed1a221cb5bc4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:49 +0000

    suit possession convincing he least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
