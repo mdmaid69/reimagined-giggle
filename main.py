0583d0137cb24bfb82e35754f4ef310ed662c75b
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:56 +0000

    should content reach colour scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
