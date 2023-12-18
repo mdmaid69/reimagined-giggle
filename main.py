872e0e29be2ac0c02fef5d6dabc7b50e8513668b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:50:16 +0000

    fire discuss talk eye test

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
