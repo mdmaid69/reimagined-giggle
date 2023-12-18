bb6cf701679a1f75dcddaf1deb9938878250e96e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:39 +0000

    young whole guide style regular

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
