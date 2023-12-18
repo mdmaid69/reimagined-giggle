0892f88301cc9c7fe007339dd7a1d8706590a334
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:54:19 +0000

    declaration stretch nose type identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
