41ebc5666be7c953b8feb470750fbc505cbe9eee
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:40 +0000

    ticket method bar whistle around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
