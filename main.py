e9368650bacd3a148b8d228fcc77bbe13d52ced8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:09 +0000

    until relationship tip thing finger

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+        return os.path.getsize(file_name)
