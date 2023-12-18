9ec0db3438bcbffc7c9c6461ca288fc9510afbca
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:24:44 +0000

    work damp steer distance declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
+def find_min(lst):
+        return min(lst)
