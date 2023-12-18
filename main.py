5cc66079afab90c9985189f6988397128dc22d12
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:39:12 +0000

    delicate only excited relief appear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
