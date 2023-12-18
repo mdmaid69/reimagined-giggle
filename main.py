1211a7bd51b14e05161ff76ccf9074f159cf4cc2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:08 +0000

    sore stranger cause natural sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
