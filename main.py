15ec46f5ae2bce8fb04926b3b7479b8ce4e6568a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:55 +0000

    giveback design loan part recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
