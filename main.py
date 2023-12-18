215b9594a840d5ab959dd88a234200db7aea3d86
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:52:52 +0000

    far 572enter runinto settle nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
