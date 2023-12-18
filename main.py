73d9b91df37d5e11efdd0bbe9f3570e6f77cd6fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:33 +0000

    surprised destroy bush offence responsible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
