95094778a582378b391cc0f33a17a8eb4138ff68
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:48:20 +0000

    hope save careless shelter judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
