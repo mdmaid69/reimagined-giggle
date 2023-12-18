882ec6ce51ae9fbd1ca15ad172233efe6320ae60
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:37:31 +0000

    exciting disapprove know colour himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
