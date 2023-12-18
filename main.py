5c4f27fda724e8b7974be5fb2774c36b936948bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:14:58 +0000

    there special float pronunciation risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def is_odd(n):
+        return n % 2 != 0
