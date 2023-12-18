39f09093c0dec65eb0a833aa3b2a749a69733eff
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:50 +0000

    exam damage careless snow own

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
