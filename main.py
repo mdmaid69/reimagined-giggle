fa9ff992cd4ee8be8053c7444652e058e4b3898c
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:52 +0000

    like range deliberate mother declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
