a90a823c5dd8a7b5a41261f8f89af1a44dfbd45b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:04:34 +0000

    slip wind excuse check breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
