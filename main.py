19eb51a34d1fbe662a3cf56609e49df3308f99e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:22 +0000

    long general polite swing sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
