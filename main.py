1e8a19994aa0047d18e14e2c45398b96df409d08
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:48:14 +0000

    oil entrance scheme aim appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
