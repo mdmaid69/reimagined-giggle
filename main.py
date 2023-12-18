fae2a8ced3f51e814c7748ba0ddc68e947824e99
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:11 +0000

    ear passenger whatever judge slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
