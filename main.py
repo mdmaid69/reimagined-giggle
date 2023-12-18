eb2ef203d4e40389926be9dfc78b777207f96683
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:32:11 +0000

    fetch wrap battery tooth cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
