d61f2cd25d911231576629b816034cb8f203de30
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:07 +0000

    safety ground tap straight communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
