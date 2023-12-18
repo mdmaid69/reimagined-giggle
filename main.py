3f715b6da5e688ee762af76f72caacd02ded1da7
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:15:16 +0000

    seat centre needle independent road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
