58526defe4e8d5b4d2a7d17f294cdac20e48eee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:34:35 +0000

    keepup suit coat disagree quick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
