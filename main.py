a85a7f2253aae278da6da31c09a6c27868995f14
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:23:56 +0000

    everywhere eager himself forget run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
