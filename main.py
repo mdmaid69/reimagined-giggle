409ae37622a5c8549cc4a0903681d94572b6596d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:28 +0000

    cover tone but state geton

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
