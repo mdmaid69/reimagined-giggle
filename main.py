5bc107f70095bc723209c30480a6e56bc475e669
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:03:33 +0000

    mystery dry week interesting Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
