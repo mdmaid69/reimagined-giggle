c994b0bff9ab8468ec06a09c6f1948ad9052194a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:47:11 +0000

    mix week his dance suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_to_binary(n):
+        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
