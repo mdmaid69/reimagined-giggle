14f07c3461978c458f2edd8cc012ac40c3cb6057
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:42 +0000

    hole though tax comeoff control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def is_even(n):
+        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
