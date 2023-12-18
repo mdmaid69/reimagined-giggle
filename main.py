421d4d272ed386343a85324d2d9af0d448e4ce91
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:54:19 +0000

    loan familiar smile slice stare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
