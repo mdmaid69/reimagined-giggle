3cc95e7f731160711d20e2aef0b09a9f799d1a82
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:12 +0000

    determined mysterious away anywayanyhow possibly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
