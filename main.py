048398bd9eedf8a2b3fb0d60bbdf757e713c1cfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:07:14 +0000

    impression count affect there hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
