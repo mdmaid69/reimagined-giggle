ccf70102dff8654d20b5417b6bfc2b59b2a024dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:38 +0000

    blow test giveout talk suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
