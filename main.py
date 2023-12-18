2be89645d9b6790816b5c034ccb5e59892cc6ff6
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:30:40 +0000

    everything ability emphasize measure claim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
