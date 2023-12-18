3808ee0b8e1c47bb95d2bacb4c5d945ecc138ac2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:48:07 +0000

    come give will deserve side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def add_numbers(x, y):
+        return x + y
