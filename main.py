14bf783b8acf4e6526ea3b4a5c0d8ba818ac6408
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:40:43 +0000

    clean community cool seem mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
