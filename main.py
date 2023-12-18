672f70c36203f927318f79e05bfd5904c5bd715d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:19:23 +0000

    rush bath fall accept upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
