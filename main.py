16f049ba6013af8ae2ae226696a45a6ef945e622
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:07:42 +0000

    left strong shoulder understand information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
