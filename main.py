17d95bb26b7a6146e25b83b7ed319a077fe3c665
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:49 +0000

    sharp uncomfortable skin second nonsense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
