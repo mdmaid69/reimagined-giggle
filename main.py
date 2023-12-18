7489d0c2fb14909bfe7cd7041e3f51e52f5373c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:44 +0000

    centimetre instruction character before middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
