0c9e19f472b1fc1ea89afd82aaa86fccbcde2333
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:53:56 +0000

    ought fix callout smell fear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
