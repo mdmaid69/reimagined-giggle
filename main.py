b0e641aa8554fbb56812231656dde5d56d8c6b1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:23:09 +0000

    impatient compare control protect keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
