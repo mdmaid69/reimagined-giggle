90ca8f25241fa052ba42e171bf5ab52de0d45709
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:12 +0000

    wave stamp callup worth 565end

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
