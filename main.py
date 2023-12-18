98cac30748785efd6637856364bdc8d7c3108eaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:51 +0000

    active apologizeapologise contents plant later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
