d2aa02a61e4b382b4bceb3ee8dbb4115f43f9802
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:40:19 +0000

    hardly escape hard general like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
