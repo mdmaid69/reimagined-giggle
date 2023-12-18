18f11edc334ef34b04aae6328548ba41dacac5bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:30 +0000

    risk space exciting not congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
