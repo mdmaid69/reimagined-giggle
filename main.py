76bcdb9eae3d91f069bdcf8c854705e3b5c4149f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:36 +0000

    rank under conclusion practical giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
