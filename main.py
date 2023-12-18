6de9d2c57db6d6c54d89601697a0cffc7d0f6f65
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:11 +0000

    wipe from wait original estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
