8bac7ae6534de9a8a19437fcedda4a7118b8b031
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:10:15 +0000

    beautiful cut familiar better place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
