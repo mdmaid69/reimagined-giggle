089ec5767d7a79e97a5733dec866a01610bb8596
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:02:04 +0000

    comeround bring friend list Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
