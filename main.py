fbb9edff2fe897675b03929479335bdfeb990480
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:30:35 +0000

    break unpleasant survive at unkind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Square numbers:", [x**2 for x in range(n)])
