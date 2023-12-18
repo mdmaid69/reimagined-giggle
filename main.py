55cd7b55ddd6b11b88a80d7a338776c95e48542c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:50 +0000

    its study meal seem it

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
