236eccc1c9d2936b46bfb00ebdad446f94693b36
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:51:38 +0000

    evening central thumb row use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
