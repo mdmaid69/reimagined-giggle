cdc3a65cff6f88adc4949686127b4c65edce33de
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:58 +0000

    up dream pair violence square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
