117b30976ecd75ea93d01d7af2ff769bbb2b2646
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:40:28 +0000

    open think breakin club fresh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def append_to_array(array, item):
+        array.append(item)
