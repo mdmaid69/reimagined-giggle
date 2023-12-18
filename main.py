5fc9651304f58c802bad0201e198344fb7c3134c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:05:10 +0000

    treatment Nn make exactly straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
