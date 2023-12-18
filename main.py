0a67f0d4bce9a35150cdcc53007e8d37f6c397bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:47:01 +0000

    part importance difficulty remain without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
