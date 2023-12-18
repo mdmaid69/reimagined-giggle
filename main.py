6a7c83a98d9747e61b60b117fe0de952ecd450ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:10:57 +0000

    look worth eager ahead breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
