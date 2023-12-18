ed8b1be99673d2cc2077c00ce851b25d914725a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:26:38 +0000

    ask work regret difference fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
