21f8a82b8e551fb35da9bd2b978b7978d4dfb175
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:42 +0000

    flat straight over pause shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
