344998f77dbd9b44620fe2938b1f6eee7a0703a7
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:25:41 +0000

    arrangement church move fork become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_user_list():
+        return collections.UserList()
