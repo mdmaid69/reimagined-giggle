dc785d9b5b2838a32b45b9f0e51b760db09177d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:50 +0000

    like rest tape stamp everyone

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
