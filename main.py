dbb54cf93e23972039135076cf540deb4590a5a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:55:35 +0000

    declare now before excellent effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
