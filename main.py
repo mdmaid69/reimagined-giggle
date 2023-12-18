0219f4e0e07c1a37fb4a5e7117dd94d782738900
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:19:59 +0000

    possible frame yours our shut

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
