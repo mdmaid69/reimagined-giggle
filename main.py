3b313c61632a644673fa90313f966815fc0a2a24
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:54 +0000

    near board runout pity progress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
