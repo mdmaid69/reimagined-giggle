29ac9b0df045cbd62c6577e0b33879897416ec35
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:18 +0000

    down old violent crash would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
