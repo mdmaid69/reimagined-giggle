b73a1edd203abbe702b902f28f7343faab8acc5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:17:25 +0000

    person morning again or hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
