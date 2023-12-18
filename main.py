6d65c248660e52bdbeb07cfbd5ddf6ddfb3244ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:08:34 +0000

    protest bang shape sign exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
