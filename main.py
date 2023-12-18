2afe0041e9fa651c9ce1f07fe6e81fb78fde9701
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:14:32 +0000

    everyone compare solve movement greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
