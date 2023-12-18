de52272c787f86d50ffa0973c179707f30646400
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:01:33 +0000

    intend scare by neat road

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
