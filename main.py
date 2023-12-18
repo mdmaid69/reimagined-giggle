47ef7138e599953aff4d45fb66daaa71e850bca0
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:59:22 +0000

    autumn confused ring rush scrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
