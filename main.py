7ee9d91ff832524765c65e2c680a14aa13969b94
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:55 +0000

    secret label emphasize post blue

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def count_characters(sentence):
-        return len(sentence)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
