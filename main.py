33873b4bb6405f7c8deefba25820b696a5a052a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:11:39 +0000

    class definite plate represent aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import numpy as np
+print(np.array([1, 2, 3]))
