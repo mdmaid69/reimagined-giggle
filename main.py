3483b83e02b5af8ae4dbb2094425f3d962ddf6e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:25:29 +0000

    treat over oil whenever takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
