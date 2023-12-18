a02e42464b99228b033cbc6a4fef829f0dbe6dbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:50 +0000

    present tomorrow comment state underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
