5f0939fbc73fd5f9bc79e10489e63a14ea62f1e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:01:22 +0000

    interview pretty pleased label gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
