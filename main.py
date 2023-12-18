0b5e854d663d90642ca5859e63302037227620dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:40:26 +0000

    tax comment drink sail breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
