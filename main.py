7bcae65fc54a781becd9a27bc0db51c832191e4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:49 +0000

    bringback flat thanks clear fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
