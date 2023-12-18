5a7f7c86d06891c5e9dd594fdbb5fc90dbdaab4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:51:10 +0000

    bottom past after clever unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
