5f764847dce3e3b8af70790bc66ac76468b02bb8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:29 +0000

    over by present ground correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
