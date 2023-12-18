c5688fa8fe003cd9158310ce51fb19309f7d49da
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:25:16 +0000

    advise arm something notice should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
