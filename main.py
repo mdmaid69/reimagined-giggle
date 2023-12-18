34725891f809f23ca340d8731267e82a38af0ed9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:40 +0000

    complain so quickly seriously unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
