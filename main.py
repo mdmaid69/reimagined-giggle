4c430ab0c5bb5be462353f2021afb33fb17fa75e
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:21:32 +0000

    laugh interview whole line reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
