5636116acb0dd80ac916f196b4b93053ae345760
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:21 +0000

    second trust add somehow past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
