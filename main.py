51eb00682f52a07357b01ff94c146b36e6e24338
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:57:29 +0000

    anxious ought succeed head doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+print(os.getcwd())
