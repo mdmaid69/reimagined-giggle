697ab534355fea35653e5bebe846cb1d3b3586aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:01:34 +0000

    estimate rather new warning wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
