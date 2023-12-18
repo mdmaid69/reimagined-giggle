6db6d1b67a53d50a0e45e835fb8fd382da07e655
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:28 +0000

    exchange bye classroom model uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def get_base_name(path):
+        return os.path.basename(path)
+  def subtract_numbers(x, y):
+        return x - y
