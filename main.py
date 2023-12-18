a5c91cde6792e7fe59b78a3b803996e69218ae55
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:59:51 +0000

    trousers cheap expect Ii invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
