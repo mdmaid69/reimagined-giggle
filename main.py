6a2786943ab2a94c677a109adfe9f099177bdc4c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:17:35 +0000

    government should catch worth size

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
