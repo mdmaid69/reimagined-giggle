806bd5f3ecaeb5f27aa549aa23e9c84584ca8271
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:59 +0000

    set release wear concerning underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
