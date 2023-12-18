f2dc0251ed9a8c448ad18c7a64bf9a402ef09daa
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:02 +0000

    gate able except road active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def count_elements(lst):
+        return len(lst)
