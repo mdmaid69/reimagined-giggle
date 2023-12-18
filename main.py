ddc3fe84d6fe757beb9ea0c6c2a91432d27453df
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:33 +0000

    watch tap sing shout waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
