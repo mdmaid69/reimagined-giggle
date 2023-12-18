fc871f6284dff160254edcfc66f5f7d726677591
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:43:35 +0000

    lower tall alive central morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
