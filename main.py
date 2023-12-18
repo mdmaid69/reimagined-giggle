6776366950977a7ad2f4c1680235cb7732cb6b02
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:26:48 +0000

    different his complaint now to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
