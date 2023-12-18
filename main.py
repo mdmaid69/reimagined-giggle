476a2ccd8a8d15fbd7d9b735cf22e6c447ae73b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:56 +0000

    central label process gift keep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
