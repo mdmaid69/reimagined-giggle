2fcbae7ae854e9b654fbd44e27c7a5aa5767495c
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:56:37 +0000

    everything only whether wait fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print([x**2 for x in range(10)])
