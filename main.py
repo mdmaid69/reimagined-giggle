88e344cf3aa1f79324d817bff59f8980267f4df9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:46 +0000

    pride cause crash address indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
