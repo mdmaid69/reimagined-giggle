b92ca049438b15f5143e464e27033749f0fe50ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:05 +0000

    direct divide fair mind unlucky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
