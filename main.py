befc0579859614d55762b64e944b0d4a5118ad5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:21:13 +0000

    life set left correct important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
