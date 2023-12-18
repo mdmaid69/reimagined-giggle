34b83d8beca8b8e3b62ddbc9a8e23bd29763500a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:22 +0000

    instrument objection doubt behind pleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
