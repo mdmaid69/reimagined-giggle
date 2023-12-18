994850de52d2a69b1d604bced8d8c13c814db53d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:07:22 +0000

    arrange material payment contents straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
