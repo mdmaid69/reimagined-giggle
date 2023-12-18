756bb15089f83115efb20745a1fbb2dc6bc3e665
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:30:30 +0000

    Ee star surface difficulty that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
