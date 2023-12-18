ac8f725a1f927898396515e70f04a709d0951b30
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:12:45 +0000

    factory any scrape round similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
