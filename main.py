97559076bc775f5f7ad910477056b89967f8f42b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:56 +0000

    close advise swallow goon slide

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
