1e6620c81db0b05100a841f48add38f45e4d80ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:06:39 +0000

    this hello body before couple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
