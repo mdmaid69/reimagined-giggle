1c2ef7f80a267f1ec78b2ff2d26fe52c539c3cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:43:16 +0000

    unusual everything favourite somebody involve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
