a14b9841e44a9d7cbdc52e5437841b2437ee8e2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:18:15 +0000

    pity measure meet post work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
