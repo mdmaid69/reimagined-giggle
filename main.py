812222c496f4a310577db87e22d7086017b912b3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:16:59 +0000

    direct clear central fail shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
