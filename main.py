a337a092ac8a10b1256c56c114aaaeca07f353d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:24:28 +0000

    club smile suit off claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
