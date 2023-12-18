adf668f01b83a2eb9be57b789f4e271ff962fa7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:40:51 +0000

    sleep straight print mark agreement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
