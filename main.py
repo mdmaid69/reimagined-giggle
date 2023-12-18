a1de1ad851db97bce9c18e8c9498b6887f3bc622
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:54:24 +0000

    everywhere dish finger admit length

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
