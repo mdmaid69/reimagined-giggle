8428fa99105541027bab077e5efa7090b38706cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:07:26 +0000

    nor absolutely guilty size treat

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_power(work, time):
+        return work / time
+print([x**2 for x in range(10)])
