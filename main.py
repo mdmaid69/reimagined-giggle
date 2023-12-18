2417cb29e4747852cd9279ba45bba030acc51210
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:53:45 +0000

    disappear over dry collect offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
