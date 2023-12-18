7d36158cdde270497ca4e52ab297f2f4b110072e
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:24 +0000

    pot combine please blind the

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
