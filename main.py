5f10d514add3455fa9a7865fc25fc8f13287295b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:00:12 +0000

    spring emphasize good cold and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
