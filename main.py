284a163a7f0820b2380344fd4f744b7eb9283ee6
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:15 +0000

    question maybe protest air head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import re
+print(re.match("h.*o", "hello world"))
