da6612c6d228f5c8f4b00fa111cce15fbe906dee
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:14 +0000

    blind idea excuse lookup uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
