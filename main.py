636454ca085eaa2c09268c7a5bcbfde79c215df0
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:22:35 +0000

    funny degree dry twist bush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
