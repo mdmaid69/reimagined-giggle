799825f0b0619a8712c558b6f748c0d7f5376190
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:35:28 +0000

    sit steam far disapprove unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_dict():
+        return collections.UserDict()
