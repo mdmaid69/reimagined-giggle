67346dd09f4888cfe9c9b5bbfcf610b31a297cd8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:58:10 +0000

    agreement apologizeapologise price scare frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
