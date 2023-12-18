bcb76ce974f496b6c60fe4f97cecfef65f788f68
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:53:16 +0000

    sleep whistle behave wish sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-name = "Python"
-print("Hello,", name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
