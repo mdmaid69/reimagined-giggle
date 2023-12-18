0942633f3cdbc862fd62055779f365d1fb2a1d0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:03:11 +0000

    convenient along file related hair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_elements(lst):
+        return len(lst)
