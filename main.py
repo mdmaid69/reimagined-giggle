c900153fa0f3adeb8c0ba15068ad88979bcb4a34
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:36:02 +0000

    tall attractive front 179bite form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import collections
-def create_counter():
-        return collections.Counter()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_list(array):
+        return list(array)
