db3ae725bbc503227de1f55be96e4de92631975d
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:06:25 +0000

    fix send mouth brave window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
