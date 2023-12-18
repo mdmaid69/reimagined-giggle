89404765a02bc9235c7133c68edb4d1b8ba663b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:26:35 +0000

    arrive correct country container design

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
