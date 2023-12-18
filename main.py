57039a3d5451e0c18626f8ee88a6d9d02f7f1397
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:58 +0000

    right successful mean sad bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
