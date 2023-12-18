db2e31b592e95f7b209ebb514938beae7e99afaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:36 +0000

    furniture holdup eat loss body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
