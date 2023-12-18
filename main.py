5b7c2e76e3edf5b3924a2758fab4919355aa7590
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:43 +0000

    join yourself run win along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
