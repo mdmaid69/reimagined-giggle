7dc323f0c806e37ac396cfa0e6202131d9265099
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:14:06 +0000

    regret callat nurse declaration should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_list_to_array(list, typecode):
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
