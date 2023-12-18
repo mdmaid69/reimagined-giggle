3c4c6a75dafaf3c95c41860956c3ca6636b31960
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:44:20 +0000

    pay kill punish peaceful flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
