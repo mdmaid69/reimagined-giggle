345c9e9511d5a26983103f2cdb0b4163aaa1f9f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:41:19 +0000

    getout ought translate thought whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
