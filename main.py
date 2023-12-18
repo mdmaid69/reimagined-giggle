f1f1c231adae0248390adf312f9c548706d0969c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:25 +0000

    Hh temper somehow sure material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
