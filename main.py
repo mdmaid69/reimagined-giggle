dbf0fd5724d0fdbcf1edd26277cfa43c100e71b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:41 +0000

    flavour scale do soft expression

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
