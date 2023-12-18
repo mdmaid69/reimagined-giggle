ae8ed5e10d017018d1d5c79d82c5c649269e21a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:32 +0000

    swimming mark tooth upwards equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
