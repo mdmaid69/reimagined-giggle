f19047e5518f7dff151ac225999900c4f109b6ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:22:47 +0000

    ashamed blind wide poison broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
