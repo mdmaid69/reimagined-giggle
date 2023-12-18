c9154b2860590f3d92f0602d5a5e49d5abc1d0f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:40 +0000

    Cc my just issue rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
