23eff5bb29b4418878e03598d436bd7743a8c25a
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:10:23 +0000

    trick actually bother change mail

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
