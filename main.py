ce1a37829aa4ae964371541a0f4dfe51fa908807
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:14 +0000

    Ll equipment payment dream breakin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
