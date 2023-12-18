3dbdead6d82233e1c60e4d87606a12500d388ab3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:54 +0000

    answer bringforward power fall hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
