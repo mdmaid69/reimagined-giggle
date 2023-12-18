36d7c0b39475e92f75c799e69dec49c9c1c5f23d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:36:14 +0000

    tool blame determined yours praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
