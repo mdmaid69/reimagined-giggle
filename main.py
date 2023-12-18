2cba113ab97bcc581e2f723f9a296963823ab4d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:26:10 +0000

    know pretty address scrap among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
