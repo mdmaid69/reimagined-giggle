73baf4296601c94e4e13ac7ecce61e0915f6e8b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:09 +0000

    Uu drive attempt well yesterday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
