6c6242d129232fe1cc9f816da27a22303c1027c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:02:11 +0000

    consider except dangerous expression calmly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
