79dcae3e812934d03c5d4a96683bcdec8f87e95b
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:39 +0000

    arrange repair would pencil crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
