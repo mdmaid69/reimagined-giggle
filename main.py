eb321b4d268c1d12fbfbd17842c6f6636f49caa7
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:52:12 +0000

    wander minute change remark nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
