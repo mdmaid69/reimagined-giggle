f75e0c41ff48f4fd371a3a180b500dd19fd2f04b
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:36:44 +0000

    turn temperature stretch if advice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
