304652f93b9126499c62cf0e354d127dc674cfc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:09 +0000

    age discussion spring operation other

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
