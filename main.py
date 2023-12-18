4e2eb634cf83caf984ab3e6777161bef22a24c31
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:26:43 +0000

    so pleased pause along off

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
