efb8b759286b89cfb65bdcfb4596c737db46b2b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:48 +0000

    power fault such imagine preparation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import time
-def get_current_time():
-        return time.ctime()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
