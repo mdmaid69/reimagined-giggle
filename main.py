a64726baf983423746d8b30007ade0c5a4b0a0b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:54:32 +0000

    active lost challenge rush attractive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
