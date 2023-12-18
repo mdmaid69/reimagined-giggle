578abd1abc5f7c1de90dbf49fbe74a790a4f4d92
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:47:03 +0000

    unpleasant tip early want organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
