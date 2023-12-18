5077dd90a13ceb6b14bda4cad1fe5993f59ecf72
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:09 +0000

    summer claim patience attempt comfort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
