9635bbba2e2df82dbb99efc026b0d0d5f1f4e0d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:06 +0000

    account plate abroad obvious refuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
