d3b6ac78a38bf768d325e7941cb0bcec46ecadfb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:27 +0000

    letter bye pay meet natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
