f9ca62c2e9aaf83723ce83f56036a615cadad30e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:15 +0000

    wire escape familiar complicated abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_complex(array):
+        return complex(array[0])
