2dc8610ea593e2d9eeb27cc57819e9dbf9e08391
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:50 +0000

    Zz contribute bag final bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
