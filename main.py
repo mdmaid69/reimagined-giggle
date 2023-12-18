2653862fce9417940aca1c5738375ef21161064a
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:41 +0000

    basically intelligent dress phone borrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def subtract_numbers(x, y):
+        return x - y
