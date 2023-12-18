7f34c58b11a90223f694410435536eed36b53826
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:20:00 +0000

    equal mystery talkoutof pronunciation minimum

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
