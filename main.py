593e38ba514b6ec40b194139bc9c2fcf7ffaa87c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:09:08 +0000

    argument dark nothing wish about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
