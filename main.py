c35531e56439670e09d56463dc27ec398eef9b41
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:19:57 +0000

    forever own environment place form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_speed(distance, time):
+        return distance / time
