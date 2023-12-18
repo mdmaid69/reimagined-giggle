98c73fcb406185cbaf12b4938b81e24fdc1af4e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:28:50 +0000

    leave truth phone wipe none

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
