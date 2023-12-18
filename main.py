9cfcba1c6b9c6c1c24eea517205bf75bbabd18a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:09:32 +0000

    bringdown fear both afford turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def is_palindrome(s):
+        return s == s[::-1]
