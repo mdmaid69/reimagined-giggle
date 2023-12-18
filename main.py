60dbc1cf47f2e4244583c85c5ec1d0f45495ad2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:48:20 +0000

    list appeal finally original accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
