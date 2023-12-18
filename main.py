26a648d52d9d2c804a71661f38c74c9644be7f79
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:18 +0000

    recover public develop right famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_power(work, time):
+        return work / time
