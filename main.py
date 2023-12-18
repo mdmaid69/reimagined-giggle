377f4147048ea604bee2b2289a7407fe91d9cbc3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:41:17 +0000

    repeat heavy before how sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
