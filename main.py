a05a018fb9d644703e24b534fc4fb8420e855c63
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:18 +0000

    spoil home bet method buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_bool(array):
+        return bool(array)
