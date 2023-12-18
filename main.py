bcbfb410e200deb27ca99e5c82a19de7519b656d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:35:03 +0000

    combine left find later sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
