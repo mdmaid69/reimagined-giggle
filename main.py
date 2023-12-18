38fceb7e5608ba6d45b8715c316f640b4e8a589f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:07:54 +0000

    thanks challenge hurry bring word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
