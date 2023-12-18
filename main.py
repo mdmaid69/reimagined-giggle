3f631a2445c0d11de10b6b6f36bebb0980c5c027
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:05 +0000

    never heavy area lean man

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
