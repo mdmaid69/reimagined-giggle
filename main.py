797e2e53666025375582602e1b2c33a1dc7fa9d2
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:44 +0000

    section feature range afraid chin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
