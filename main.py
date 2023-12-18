289432fd0c30f0f5bb26009a3081c51fda09b0a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:02:16 +0000

    bored collect relief variety environment

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
