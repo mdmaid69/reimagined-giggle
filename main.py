007ee8969f4a4d683aeb30b50341f387cc532257
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:49 +0000

    cruel anywhere secret pride contact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
