1f7e4beb9e0264af86f2df419bfacf5da20387da
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:59 +0000

    hear work tongue ought left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
