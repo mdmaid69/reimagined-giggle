1afd0a8159df0954cd29b138fe7879b006a6f788
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:10:56 +0000

    exact concentrate prize our accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_list(array):
+        return list(array)
