becb4e8e27d981846c5772dd89824a1e0ea1c877
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:36 +0000

    complain arrange sale film intend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import time
-print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
