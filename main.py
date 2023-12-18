9c6f29179a03cc5437f3f03a54770acf72725be2
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:35:21 +0000

    carry crowded collect hot apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import collections
+def create_counter():
+        return collections.Counter()
