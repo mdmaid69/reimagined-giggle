6bee6129964aaea3db18d6ae9bd05a54aed52d96
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:13:01 +0000

    next school wake trial I

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
