55104f909c394543babd3ed1babb3c757ff1716b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:56:29 +0000

    neat whatever Hh break lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
