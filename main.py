3dcfc6ab2430735e23530f226fa837a0c4baadbf
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:35 +0000

    watch emergency Ii list same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import collections
+def create_user_list():
+        return collections.UserList()
