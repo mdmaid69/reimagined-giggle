98a48d2dec7bd696319fe13a0c2960b7c9ece0b6
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:09:54 +0000

    Pp history smell improve relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
