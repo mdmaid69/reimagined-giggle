878192ef9a5cbde78cdaff5255b2b83df180462f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:24:28 +0000

    ring hard approval embarrassing powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
