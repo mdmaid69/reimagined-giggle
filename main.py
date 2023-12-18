eb6e77c739e62e68163a607c518fbe2fb32d65c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:01 +0000

    pour various correct lower otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
