ab57c6f3faab4ecb1547c4396916dfd983073e4b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:20:42 +0000

    everything neat each decide hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-for i in range(5):
-        print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+text = "Hello, world!"
+print("Reversed:", text[::-1])
