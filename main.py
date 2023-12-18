4a5ea31719a8b6d4046f9ccadc0c7eb92f398af0
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:34:22 +0000

    awake beat trick minimum open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def add_numbers(x, y):
+        return x + y
 import array
-def get_array_as_set(array):
-        return set(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
