ccdd8052d69fbdf9089316186e5d5f7ae2f099de
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:00:13 +0000

    needle bread absolutely schedule report

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
