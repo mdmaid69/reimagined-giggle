3a3ec25941c6131ac1ae76b9298b29a79d8c8ee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:36 +0000

    petrol protection claim lift used

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def cube_number(x):
-        return x**3
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
