88099a25240b3bb9fe9c4096f771fda9d87a3a66
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:05:40 +0000

    remain angry giveout weak cheque

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
