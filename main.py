bc89bb3bb352983dc02a4fc4a62adfc51318326f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:25:24 +0000

    spring empty along tax cut

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
