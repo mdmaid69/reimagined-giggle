264b457a29a6971d894345985344b268c3143ced
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:13 +0000

    emphasize custom aim clear interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
