a455421a9794f969263b52af43a7ee597f17771e
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:35:43 +0000

    tired food government emphasize rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
