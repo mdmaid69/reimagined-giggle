04e2a994a1f3614a9be924b0007814b348fa91bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:39:08 +0000

    similar trial anyone reduce perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
