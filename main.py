62ad67bdaf10215011624544c7b887ea3e36635f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:49:20 +0000

    control time dance still later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
