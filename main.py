e9f86b736dd779620409a5b835e4286a2800317b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:15:28 +0000

    sorry name recently do shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
