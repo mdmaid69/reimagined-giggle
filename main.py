611298ee8cfe87a90f7cdeaa586888c15b8e2267
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:33 +0000

    tape honest father welcome die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
