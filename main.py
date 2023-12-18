34a195f557a95eb2b33442f3668199fbcac90f52
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:00:21 +0000

    century relationship hers form string

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print(sum(range(10)))
+def calculate_acceleration(speed, time):
+        return speed / time
