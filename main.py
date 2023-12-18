b60d9079c1183d93cd135b50bb6af0d84d4a3b63
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:34:54 +0000

    phone close pleasant among badly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
