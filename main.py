629e08d10caf7056acfd27938a1d061a522704a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:22:02 +0000

    across cut across brush born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
