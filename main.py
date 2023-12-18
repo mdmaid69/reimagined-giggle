6aba88769c9c3e5160e654f167dfb29bf3b87b92
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:16 +0000

    previous what off worry radio

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
