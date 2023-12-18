466a03960b820494718989d047b2b6e913cf9a3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:07:18 +0000

    wind worst alone now behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
