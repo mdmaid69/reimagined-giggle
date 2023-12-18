e7528c6f3816f5c3972675f9b66879cd3200492c
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:47:25 +0000

    influence conduct close afford breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
