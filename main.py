f34ed21b33c055419763bebac198773a293baf22
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:49 +0000

    upwards mine middle turnout keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
