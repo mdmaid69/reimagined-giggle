f65be5786203d511c2fe2ad97658cfaf11476a71
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:07 +0000

    expensive lookafter art city black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
