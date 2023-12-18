9bf0a2c19e4a95612b2162af9180c10b57374524
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:16:57 +0000

    Itself neat undo exercise problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
