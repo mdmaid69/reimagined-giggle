efe233cca1578bc80f807f465a339a73b448bfdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:13:59 +0000

    pupil throw weigh bus exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
