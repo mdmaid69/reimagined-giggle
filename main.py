24f90fe5f8059333e61b7f52f3d2630d46134e1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:45:43 +0000

    poison keep flavour opponent occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(numbers):
-        return min(numbers)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
