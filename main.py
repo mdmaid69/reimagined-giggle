e2918a67d04704f37b2ec3a984557330a45c746c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:50:46 +0000

    before feed warm well stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
