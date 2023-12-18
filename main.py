206452ef149a5a1452c53464e4f1d3a446bd06ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:28 +0000

    game ask belief you accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_even(n):
+        return n % 2 == 0
