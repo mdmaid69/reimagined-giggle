4d1eac0a5074ea2e846e5b556226da0604019d75
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:29:11 +0000

    success little general people steep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
