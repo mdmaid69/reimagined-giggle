abb166b85697e136487b23940ef4bbdf980dddbe
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:27:21 +0000

    wrap for another time after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_work(force, distance):
+        return force * distance
