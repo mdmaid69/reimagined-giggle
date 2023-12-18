d9d767964d99a776c5d7f62713c43e7e4927a19d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:14 +0000

    single strange ring telephone convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-print(os.getcwd())
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_as_float(array):
+        return float(array[0])
