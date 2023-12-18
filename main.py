f7a71f1dfa34d407f14795a241207f4630d43658
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:22 +0000

    forward then normal instruction yours

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
