c97ff4bb715ea872f454a289ca2fd34790785a61
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:33 +0000

    equal means high else hold

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
