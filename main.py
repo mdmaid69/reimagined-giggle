3db3bff88b6ec5fa9804141ec7a06e54e255ed50
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:55 +0000

    each dry off no suffer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
