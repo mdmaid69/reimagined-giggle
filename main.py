f5893923a804314e0929d7e10cdbe76e887f54e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:34 +0000

    deserve boat slice past march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
