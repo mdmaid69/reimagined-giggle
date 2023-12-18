fc9dd47b5bbe4de51635c384708f1f93cebaa740
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:02:11 +0000

    takeout cause sound instrument cry

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
