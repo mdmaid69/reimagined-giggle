23975e5b27eace7c77ed0b3447d2699e632aba3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:01 +0000

    shrink everybody somebody pause which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
