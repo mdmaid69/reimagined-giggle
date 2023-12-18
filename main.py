705711c9ed0b9af29ff0d32ed43143356a02ac1b
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:42 +0000

    receive summer lookafter suit swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
