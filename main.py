34b02fa4a56a49867521e6a2b50a369948034244
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:34:31 +0000

    new study wheel so present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_volume(length, width, height):
+        return length * width * height
