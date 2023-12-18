aa5046d6e942b01144922ee92407f9b61587dff5
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:15:35 +0000

    kick ear radio sign yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
