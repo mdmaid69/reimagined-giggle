67996c1671ff5890b7f3f8969aa6d5c83bd552d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:34 +0000

    jump know urgent table interfere

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+x = 10
+y = 20
+print("Sum:", x + y)
