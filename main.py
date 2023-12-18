e4c409b757c2fa84de300c5db76e922808aa3a5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:10 +0000

    wild trousers blame brain university

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
