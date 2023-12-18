c04a91a08b6066293323074acfc61ce244464f71
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:59:44 +0000

    gooff talk perform ready begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
