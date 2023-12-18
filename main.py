ac8ca74a9d0bbfdd88c286a394202096fbf4d4bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:35 +0000

    well giveback sign second fine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def is_even(n):
+        return n % 2 == 0
