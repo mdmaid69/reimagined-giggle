c654dd87ca65f8723beecdb81cdc50ad5c279967
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:49:42 +0000

    transparent bus night computer mystery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
