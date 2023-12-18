00f0e1e77db616cbcd481e926970ab0f7be29271
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:47 +0000

    task mine entrance tomorrow journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
