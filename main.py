6f20744aff7792ffd02bc6e02d671ef71979ed08
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:42:17 +0000

    tooth who will detail pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
