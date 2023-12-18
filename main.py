b653cef3aa4ded26a81c4ba9cd710610bd01d051
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:59:58 +0000

    complaint food recovery warning style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import random
-print(random.randint(0, 100))
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
