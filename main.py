9e1a10ff298e2120f2142e5a1538fb13eeeb14e2
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:26:44 +0000

    sense action comfortable correct abroad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
