d938c0705b3becdf2fed24a6a99c02a58222b42b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:11 +0000

    form sorry centre comment whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
