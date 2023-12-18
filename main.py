0aa3a8c552c9fc43500ce6487e49c780bf286821
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:55 +0000

    dance uniform climb guilty appeal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
