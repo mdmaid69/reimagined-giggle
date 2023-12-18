fced2ec72d4ad6e4a91020e605fd5dd6e5962d49
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:33:28 +0000

    accident soft read appeal regional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
