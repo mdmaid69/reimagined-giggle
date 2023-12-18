452c7a5956bbf8832899bc9f9ee4f1a7feb5d5db
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:27:35 +0000

    comment press keen result there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
