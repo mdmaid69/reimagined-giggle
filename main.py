aeaa8ff90231d752d9efea101d5370306257ad4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:50 +0000

    expression reasonable transparent one strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
