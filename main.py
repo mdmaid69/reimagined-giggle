eea306bbe9aae38d01599d0e0092ef00bf17aa8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:40 +0000

    transparent shape tip work circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
