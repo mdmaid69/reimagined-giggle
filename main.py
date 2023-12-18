f3386de4b925a466c8e635c41a1c070a202ce6e4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:47 +0000

    accommodation low see wish serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
