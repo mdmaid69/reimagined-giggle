7c6d85f9fac9668b44ac061d722487cb0a132e6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:28:26 +0000

    match question clearly yours convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
