792122149ef610ff39fcf21073fb03222e439436
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:15:57 +0000

    wipe how present crash towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
