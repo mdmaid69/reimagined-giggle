8715632c8942f4a9fb2e51261c511afeb0b926ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:56:50 +0000

    person trust late free spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
