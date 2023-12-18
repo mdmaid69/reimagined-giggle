0e5c3c5d8eeab2fab84de824d7387c141f825cf2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:43:07 +0000

    stranger win hurt Vv later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
