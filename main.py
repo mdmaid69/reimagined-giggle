974d4015a3968bbb999cce95f20e1913b3948b7a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:40 +0000

    black talkoutof giveback serve temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
