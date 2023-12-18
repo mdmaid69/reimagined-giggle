1ea9e2c7191e85f4ebacddf51a89b7a16cc5dba3
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:36:52 +0000

    stretch regional uncertain tear comeback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def subtract_numbers(x, y):
+        return x - y
+  def convert_to_octal(n):
+        return oct(n)
