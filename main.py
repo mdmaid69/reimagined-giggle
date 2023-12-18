586f947289397ed89a46bd76380766d97a69cbc1
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:09 +0000

    finish balance street communicate ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_current_directory():
+        return os.getcwd()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
