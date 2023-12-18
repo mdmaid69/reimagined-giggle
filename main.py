49240b8407f98cfd9f9e517c556c48bdf866ac16
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:34:06 +0000

    throughout fair regard direct comfortable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
