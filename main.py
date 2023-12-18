ec139da3771cf45bd8be80b36cc66ac299630d62
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:11 +0000

    food life ocean cost suddenly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
