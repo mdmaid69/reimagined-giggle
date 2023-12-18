8d884cec9b6ac55a3d0dfbc9720b54791a6405ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:20:50 +0000

    sharp culture protest married occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
