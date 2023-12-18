fdd4885fb5fab98e13e51d56207b2ab4a690146d
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:33:23 +0000

    personal fall fail bottom reduce

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def count_elements(iterable):
+        return collections.Counter(iterable)
