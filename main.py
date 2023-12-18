db9d738525e6de5272c1888b73f6d1fafc631898
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:24 +0000

    transport appear soak pause ready

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
