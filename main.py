673dc6885c8d5d0d770f0844fbcfea6ad305f5bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:19 +0000

    weekend slice steer square firm

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
