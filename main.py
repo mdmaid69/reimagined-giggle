84acc50c32b383dbdbe47cc8025ef682edcade3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:49:03 +0000

    pretty advice regularly comeback connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_python_version():
-        return platform.python_version()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
