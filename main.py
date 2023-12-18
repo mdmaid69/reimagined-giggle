78871d74df6a3a1e6097db765f62347a78afde99
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:42:15 +0000

    people cheap accordingto tooth lend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
