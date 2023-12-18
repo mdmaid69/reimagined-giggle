6f993ffdfdb41bac1f18b62f092fdbf1e8c68f3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:39:21 +0000

    level spring regular spread thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
