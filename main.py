e6770bbd79d9faca51fe494e7144d87762a06f9e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:12 +0000

    game pile design question available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
