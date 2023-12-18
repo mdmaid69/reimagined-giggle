0fef729170709f5a3ef94bdc354677e5b4186ca0
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:39:21 +0000

    clearly frame half yes drink

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
