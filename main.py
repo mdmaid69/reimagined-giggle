1b93bff9fc8061ca3272b4ef26e57aff7c34be07
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:31:25 +0000

    step talkinto Ii problem only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
