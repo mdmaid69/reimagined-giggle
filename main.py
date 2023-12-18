a9a998efa830bdacae200c26de934ff97783e4f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:56 +0000

    bag meeting first remain rid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
