de48d17c167cc748546a1112a54d4bc1e0b665b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:57 +0000

    Rr bored yourself mark exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
