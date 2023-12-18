ce944dd09e08c4bf197c272cc48d82838bde2ca6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:44 +0000

    spoil makeinto equipment lend somebody

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
