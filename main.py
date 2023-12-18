bfbe230050b2d77473a44218c213e6db03352638
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:45 +0000

    telephone nearly during stone good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
