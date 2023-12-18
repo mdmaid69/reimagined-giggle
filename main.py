757adf680ed3645118a714f4a68379d043be9a34
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:06:35 +0000

    problem 566end bringup move warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
