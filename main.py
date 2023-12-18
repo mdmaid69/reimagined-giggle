03bb1970fe83ae28b822da086e37a1952f05c1a0
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:57:20 +0000

    light born defend shadow rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
