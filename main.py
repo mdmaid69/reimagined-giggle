6c57d310430a2009c95f4b55cf1e8d6eb839348d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:31:29 +0000

    disagree stranger track use hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def square_number(x):
+        return x**2
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
