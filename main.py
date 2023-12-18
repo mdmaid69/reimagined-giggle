5347074cafdadae601e1f8a9f55ee18c91f34fba
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:08:58 +0000

    wealth mood count manner pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
