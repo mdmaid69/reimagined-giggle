d7d10e1c81ef5625610540aa9df070fde38b5a99
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:52:03 +0000

    unkind entry perfect pause demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
