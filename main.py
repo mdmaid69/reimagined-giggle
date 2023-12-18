f05ea670a377e83fed0ea31a59c6530bd5e19969
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:45:12 +0000

    machine shopping clean separate repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_hex(n):
+        return hex(n)
