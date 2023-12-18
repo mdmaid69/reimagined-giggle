9ea776219274c81dca2b3557f275704a94078dcb
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:32:17 +0000

    length limit tone extraordinary lucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
