2d81fb8dfa0d4bd544a6f855d4d34731dbbbb467
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:05:44 +0000

    expression software gather public stranger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
