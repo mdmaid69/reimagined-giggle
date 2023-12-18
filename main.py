628f777e539cba649c67d18b1e2c7e7fa58b31a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:25 +0000

    majority fortunate police side 179bite

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
