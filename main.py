4a5be798b6d2d0c3adf3afab429ba7611039c1b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:05:05 +0000

    crowd town clothes rid ground

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
