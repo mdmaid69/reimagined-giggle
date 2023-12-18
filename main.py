068891fa1b0e562c97c16a44f5f2e145f5e03e0d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:57:27 +0000

    value hers party forget niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
