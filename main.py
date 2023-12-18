cfe118d0fc0f503ccf146665feb1c8a36edcd81f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:04 +0000

    brief leg cry worried bridge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
