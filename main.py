6d44454d77fa741feb462abead9eb3dff314b565
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:27 +0000

    turn last wherever best modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
