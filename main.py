c74c0c93071f7e25cf9c7db07051fba6e5dfb09d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:23 +0000

    surprise from probably thinkthrough interesting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
