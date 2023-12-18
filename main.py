37dd50bce1e754292515c22206f56b38d01cb0cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:57:49 +0000

    fire so want car street

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
