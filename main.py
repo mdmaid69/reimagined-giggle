bb9577ecbf987c53f0f0cadfab2cf631d3a1d5b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:00:48 +0000

    second brush keepout steer once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
