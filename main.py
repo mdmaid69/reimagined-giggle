830de562dcfe06ff43ca9ecf0cb693009bd9c734
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:37:53 +0000

    clearly hide mainly butter by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
