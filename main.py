aa4d0398ea6f844aa4da31803a4a415ccfdc960a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:59 +0000

    572enter sharp possible recover wall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_length(array):
-        return len(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
