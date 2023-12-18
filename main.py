dc21bc6d3966bc75e00325301b1e857d58692baa
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:53 +0000

    godown lonely die dress rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
