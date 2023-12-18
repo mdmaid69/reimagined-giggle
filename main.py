22e9c97790c4995d378d447a92b447419c891529
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:52 +0000

    rob deep object mention safe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
