a621c2158ef8377ab2d0d5a09361390b07edf5ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:33:18 +0000

    bury pause relax law callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
