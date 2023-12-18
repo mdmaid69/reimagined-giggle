639e1fd58b8138957ab015b3d95abdfa87c02b19
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:43 +0000

    autumn bottom interesting once phone

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def add_numbers(x, y):
-        return x + y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
