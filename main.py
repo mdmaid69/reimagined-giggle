77c67322f810238cc82e72a96ee99cb834f0fb6e
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:40 +0000

    main trap arrangement section thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
