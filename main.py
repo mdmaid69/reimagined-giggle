af5f666e6c61246d913120457082fcdef350f293
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:23:40 +0000

    call busy suit heart race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
