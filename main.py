7eae872724852ca5b05cb50197c5f67882fbaeb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:56:58 +0000

    difference turnover pass eat crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
