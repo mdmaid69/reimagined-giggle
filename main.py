ff9934823b4d9d3dd338211fd8c668507628e9e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:00:03 +0000

    final mistake ear national breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
