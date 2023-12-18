64d320cbffe1b1fc601e6aed136187b0f262a6d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:15 +0000

    treat runinto only show lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def reverse_list(lst):
+        return lst[::-1]
