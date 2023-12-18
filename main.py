4c0ea413e780def4d038468b07c038e87c30e152
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:31:20 +0000

    part warm more fortunately coffee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
   def plot_graph(x, y):
         plt.plot(x, y)
         plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
