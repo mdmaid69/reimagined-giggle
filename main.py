308beb846c283643b783949485798f55a89c6d79
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:54 +0000

    carry goaway world correction recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
