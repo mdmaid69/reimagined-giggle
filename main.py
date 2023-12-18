3f97175000a122a8432addadace6e94bb0b423fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:24 +0000

    scrape place interesting kick contents

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_current_directory():
+        return os.getcwd()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
