85a3b203af04507a4e051a1a6e1e5aae2f428970
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:50 +0000

    real no holdon suck giveaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
