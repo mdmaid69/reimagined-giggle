7f88fdf2b327efc5a0e86ad8c35415af9d4b9def
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:34:37 +0000

    block consider smoke makeup season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
