3fb419ad0e7aa630b2729e2a4415e41264efceb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:32:20 +0000

    tonight next smile put bad

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+print(time.time())
+def calculate_power(work, time):
+        return work / time
