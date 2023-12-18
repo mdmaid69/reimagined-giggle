d733f7ab695c2d98bb4081c3ff4a2c5e5befbc69
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:15 +0000

    claim capital seriously steady should

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
