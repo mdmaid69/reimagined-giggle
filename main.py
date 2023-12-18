5bbfb0b045745e52a773ea83b8637573bd0d4867
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:35:10 +0000

    somehow either view keep onto

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_power(work, time):
+        return work / time
