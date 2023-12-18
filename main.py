2247f23a79c0d445c5fb04dadc2fbae9a5775fad
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:58:57 +0000

    without common suckme equal sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_stack():
+        return collections.deque()
+import time
+def get_current_time():
+        return time.time()
