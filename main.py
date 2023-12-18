a09d01c3c7a5b150448b4b0ca61508470e9608e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:56:21 +0000

    left approval themselves embarrassed say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_current_time():
-        return time.time()
+import array
+def reverse_array(array):
+        array.reverse()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
