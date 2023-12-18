b3ef33803f45ae3ccd758eb6c9580617409fd251
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:37:56 +0000

    suggestion before live can shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
