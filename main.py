c5542a8f6b3447dd899d73251b7f4a8158c068e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:32:43 +0000

    Cc sale union community drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
