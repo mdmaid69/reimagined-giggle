c984f95f93c3e0f24b2f98441d47b59ba04d2bea
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:04 +0000

    support shade him so deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+for i in range(5):
+        print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
