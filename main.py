eef7e8439ac854f578d5cbeaa857d4ef31a9ec56
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:16:34 +0000

    service feed check keepfrom ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
