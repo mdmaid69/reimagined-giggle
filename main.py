97ac26e5a3873bac37e9a44bcd220cf5adf7a9f3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:04:32 +0000

    frightened fault grateful crowded show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def insert_into_array(array, i, item):
+        array.insert(i, item)
