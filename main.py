7c4077ddfa25ffd1e2eeae09102717d4956c1a08
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:32:33 +0000

    plan offend although eager contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
