49bf2350f46958e438ead79089cf53e235e86788
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:52:40 +0000

    much light anywhere radio wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
