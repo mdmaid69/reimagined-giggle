b8ed6c9361aad51436f6936c52103879789135e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:13 +0000

    scared example contain people scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
