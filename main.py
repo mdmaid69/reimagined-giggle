88dd344f557215582b4279d0def5e2fbd036b54d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:58:53 +0000

    turnover capital plenty head rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
