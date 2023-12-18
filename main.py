b4df96bb2554df2338c7c2245330def5a6c4ab2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:54 +0000

    equal available something handle band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
