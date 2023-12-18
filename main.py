35a3b684d4e0e178bbfd87a1025e57a7163b52ed
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:44:10 +0000

    failure strike seem build occasion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
