f62d866c1f7372a6af1cc6ac963a65a663928320
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:59:30 +0000

    keep speak system whole height

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
