98df7896ef6b53f4a2a4d6af5b4fc8f906be2fc7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:33 +0000

    intend piece condition extraordinary ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
