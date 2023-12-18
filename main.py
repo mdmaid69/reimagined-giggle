20cc257a2ca73d1fac1b1de0bead5da9f0955a87
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:24 +0000

    team instruction reject advanced street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_string_from_array(array):
+        return array.tobytes()
