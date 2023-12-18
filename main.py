1a05db54f96220d37ef49a96430270b77fb242fd
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:39:10 +0000

    afternoon Cc about brother pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
