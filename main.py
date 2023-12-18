b0e266245031af0d13b394ad65ccc4686c1e8b2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:21 +0000

    little scrap declare geton quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
