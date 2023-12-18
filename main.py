e2f1ab4ffea50644541af49daf3b14c215f026cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:14:03 +0000

    church regard maybe theirs track

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def convert_to_binary(n):
+        return bin(n)
