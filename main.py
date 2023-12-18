5c6cc55d046fd3e986dc3e11b6d7b41ec7714c75
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:30:21 +0000

    suit our shade dry freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+name = "Python"
+print("Hello,", name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
