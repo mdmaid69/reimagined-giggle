4953f5bd24efd87993d6da58f3a72539a4427b90
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:43 +0000

    goup effort respect 572enter ashamed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
