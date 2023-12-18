809fc64cf74c5e57e5e419d3d42387e8a90895d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:52:53 +0000

    road beneath whether soil recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
