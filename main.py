792a386c1640da2b22109fc37c1e936aa94de5a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:36:36 +0000

    summer private expensive emphasize wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
