69d5c5c3944d30bcfdc3fa1899bdda5d8c73c2a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:44 +0000

    always mind street event take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
