dda77e9f2333c63d0ddb0b841ff941b60843297d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:53:37 +0000

    hide operate although piece later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import getpass
+def get_username():
+        return getpass.getuser()
