09bc2104735d5ee2f4624f4f6f1b120f3210288d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:33:41 +0000

    Nn yourself ordinary spring rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
