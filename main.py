dfd123748e471b4f77b1118cb0fe6099110547b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:01 +0000

    problem generally reduction Ss risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
