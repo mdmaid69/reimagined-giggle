664a2f55446c22b79d5b6e4e52ed8ba739ab3326
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:15:42 +0000

    stupid drop arrive tired programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def reverse_array(array):
+        array.reverse()
