5bf6a44f097eaeab6954990076d48182d7d4d036
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:39:58 +0000

    fix favourite authority impatient waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
