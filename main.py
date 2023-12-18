5d04df3f2b98ffea1046c49c39229c804fbf96c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:25:13 +0000

    recovery match cold offence software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
