810902082cd0787e03c1a7b8431af163f25b6da3
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:02 +0000

    event injury considering interest getback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
