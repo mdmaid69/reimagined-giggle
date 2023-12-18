4351ea4d53f0c5ae726544720144aaa6443985f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:18:10 +0000

    connect illness rare hardly content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
