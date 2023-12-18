f2b9bf36538677d2f4aa47fb9a447b9fd0599fce
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:10:37 +0000

    expensive charge takeover tread only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_max(numbers):
+        return max(numbers)
