df25ad1dde4459139627a7630e53a06b5c881f84
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:33:10 +0000

    ahead market accept quick arrangement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
