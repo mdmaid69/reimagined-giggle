59ead8dc679cfbc21a9ab79da1f2ba85e7c5fd81
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:58:28 +0000

    value stick term ordinary fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
