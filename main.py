c691c0d084aca50845c5fdd33407c041f14fac5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:32:54 +0000

    direct scale rarely accidentally swing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(lst):
+        return min(lst)
