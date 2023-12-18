c8752d3d6b2111edfc750ee50d823ca10ce75fe1
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:27:08 +0000

    medium apologizeapologise clean priority roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
