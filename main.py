d3d8e3e93de4ea41919f64b6828a72a84ab81918
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:42:12 +0000

    people match cut loan mark

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def subtract_numbers(x, y):
+        return x - y
+def add_numbers(x, y):
+        return x + y
