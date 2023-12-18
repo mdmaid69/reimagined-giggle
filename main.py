bb5f7b9d032bbfdaaee4183cd4c7acdfb1bcb2a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:36 +0000

    confused name turnon callup hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
