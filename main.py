69393d0262c8dbba2a5965f45ce3b3cce3a217bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:45 +0000

    activity workout unlucky wind square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
