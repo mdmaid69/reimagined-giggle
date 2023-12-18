dced2b939eb911b463e3c3b92f566e2c2b37c6aa
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:49:59 +0000

    shine goout best not year

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
