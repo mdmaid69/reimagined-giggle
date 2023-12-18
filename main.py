2d59bdcc0c882ae2f9e5ebd8f798627fd4b9bfd0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:24:56 +0000

    collect challenge bright ought circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
