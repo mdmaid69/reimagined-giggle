057a676feb9ee7a226ed6a88cbb4f4f4cca10651
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:53:05 +0000

    takeout off surprising any idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_volume(length, width, height):
-        return length * width * height
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
