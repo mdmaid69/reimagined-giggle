d017b4ae0d401197ac74dd1bc2f69ebcd0d70dd1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:05 +0000

    impatient until front deliver Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def append_to_array(array, item):
-        array.append(item)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
