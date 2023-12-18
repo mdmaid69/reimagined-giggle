08f07ce79166658e91c95acff242979751cd5565
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:10:52 +0000

    abroad refer ours well about

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
