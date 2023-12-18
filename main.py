a8262af1116a38bb6f5be4a01597fd933fe559a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:40:10 +0000

    mouth loud load various characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
