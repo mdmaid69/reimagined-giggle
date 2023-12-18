1a886206932d5dc9992200465f6127ddc821a8bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:41:45 +0000

    life the between against rise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
