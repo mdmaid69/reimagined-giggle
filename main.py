b995dcc2e5d0007b6443004a2b5efcb2c4c639df
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:33 +0000

    payment float trade accident burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
