3b5eff06d98a57d86a5a12a3d657b2c673753aaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:08 +0000

    let miss confident neither snow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
