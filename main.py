301e8806e077280839256f37450ac0fc89b93f74
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:44:01 +0000

    minority figure needle interrupt nature

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import collections
 def create_priority_queue():
         return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
