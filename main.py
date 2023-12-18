1bc481067780f70b067a1fa10ce61e38817e279b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:07:43 +0000

    storm draw detail decide bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
