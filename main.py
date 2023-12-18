701f35fc417d570e53208b7171a57d3267f14c0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:18:44 +0000

    comfortable move even old nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
