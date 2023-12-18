6182b66114959d5c093f3236868af6612b80959b
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:16:25 +0000

    argue only should shop stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
+import sys
+print(sys.version)
