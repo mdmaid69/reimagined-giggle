ee519e7dba2984f899180bd8888d9fc95ff41781
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:22:39 +0000

    part avoid announce regular earth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
