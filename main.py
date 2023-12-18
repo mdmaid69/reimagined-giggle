da53091fe2a6f513770192881387ba3f5bda1797
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:57 +0000

    taste exhibition anxious brain report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
