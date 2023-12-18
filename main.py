f89a56a177b02d65f91ccf52415680adf0672260
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:22 +0000

    pipe against sport contrast reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
