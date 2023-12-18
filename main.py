92ac7509611f8226f4615787e45b27e7de878057
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:03:38 +0000

    character quickly creature power fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
