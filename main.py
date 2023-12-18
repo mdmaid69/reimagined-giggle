5657f090140cacbc3f62911a078dabc55d0b4e16
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:20:14 +0000

    narrow we estimate shine exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
