53247b556d36933688c48dfbb8f1bd3ec0e9a8b2
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:56:39 +0000

    everyone pretty though damage could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
