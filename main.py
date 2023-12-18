c3525b33b5c0edf4bdec2c1fa4a7a5e151da3aaf
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:02:08 +0000

    stop mine tip then heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
