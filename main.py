9b074c4433ad8e1c87c8ade75648642d170a13d7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:32 +0000

    nigga nature system relief considering

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
