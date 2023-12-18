0479cee9006d10ac48f3989ed7192f5b0fd6420f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:07 +0000

    Pp arrange squeeze accident beneath

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
