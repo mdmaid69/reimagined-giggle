76b3fd88509ffae443f6cc0cacef4f22f8bf1f00
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:23 +0000

    wear communication ride attract usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
