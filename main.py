de5c5fa44b95f8b53174ba8e76faabd3120df44e
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:36:39 +0000

    while loose other interest always

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
