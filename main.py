6560c12645cdbe394171cbeaf52b1280c70c436c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:28:38 +0000

    meaning moon influence store turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
