ed6545aa71bd2596b797c7564eb17a2df93e4afa
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:23:19 +0000

    centre attractive proper manage tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
