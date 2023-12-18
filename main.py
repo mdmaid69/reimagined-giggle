72d9a16a1706bbbc77ce6ec999ae3f5394344050
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:00 +0000

    stop illness cut fall variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_str(array):
+        return str(array)
