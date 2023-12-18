d62d38e91d7c47fa0a14028f18f019f482644eea
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:54:14 +0000

    exactly figure computer bin scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
