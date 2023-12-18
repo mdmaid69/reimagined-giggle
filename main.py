5806a66b64c9de096188dd97318ede49e96a0062
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:46:49 +0000

    part waste about prepare bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
