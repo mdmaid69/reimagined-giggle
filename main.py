00162fb4ee541aed7ee353d7a4a41cf4d6ce307b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:37:02 +0000

    movement side bottom instead consult

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
