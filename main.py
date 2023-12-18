c65ee8a97b51f03980eeef4a4a2dcb4eaecb1744
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:00:54 +0000

    side immediate fierce collection declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
