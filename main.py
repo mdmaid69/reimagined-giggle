3e13b38697c45bcd650cc3e72c1a7ada61218c3e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:38:17 +0000

    professional explanation disturb matter sensible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
