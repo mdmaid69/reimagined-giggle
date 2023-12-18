80ee09d8718c7092ff76242c27ef106f6e8f961e
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:43:13 +0000

    trap party breakup nerve great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
