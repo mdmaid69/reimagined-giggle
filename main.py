8f40bd6c3124a4833b04d6e6cab1dc441de9df1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:24:11 +0000

    along dick mark disappointed display

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+print(os.getcwd())
+def sort_list(lst):
+        return sorted(lst)
