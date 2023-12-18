3f1fe7c86f11329e89b3553f3fcebba4a7849bcf
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:14 +0000

    ocean tough sort mean line

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
