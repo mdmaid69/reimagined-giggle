ebeaa28bd1df109da0b985ea29044093259d4bcd
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:22:38 +0000

    take noone valuable fine switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
