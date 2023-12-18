45e738267e9f18d3023f77c6f8e1453d69912cf1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:50:05 +0000

    now damage anything grand match

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
