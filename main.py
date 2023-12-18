57dea79776a01b0c8227fbf4f6f8e890c895f3cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:07 +0000

    number warn rain now peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
