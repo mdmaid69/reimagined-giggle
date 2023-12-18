cef9152c2ce018452e2e7493e7d0d05f5b3de494
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:03:56 +0000

    doubt think juice factory stage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
