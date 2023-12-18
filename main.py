a31bccafa3d06240e887b27a22d31db551fb54ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:37 +0000

    doubt rush measure sea reject

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-print(time.time())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
