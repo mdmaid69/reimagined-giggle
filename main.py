1424d517a9602b50ec29908e403b90c04251a32b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:56:12 +0000

    scale simple cheek lose limit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
