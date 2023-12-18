7b9a820c55528ae507da19c2582fbb7a55d3aba6
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:56 +0000

    sleep familiar ours compete bill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
