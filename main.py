dd3a94fdc5eb823483f93a47f7772e216bc72c6b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:00:06 +0000

    expect you behind late pause

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
