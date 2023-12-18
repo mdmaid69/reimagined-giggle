1374898097833573ed2e207a98c58a02176f5582
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:23 +0000

    opportunity again moon pity lay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
