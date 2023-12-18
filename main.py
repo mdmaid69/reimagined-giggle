68dac2cb4ec73d8a4a2abdcd6e59e7d6bd5eab3a
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:50:19 +0000

    Ll proof store backwards understand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
