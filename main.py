e17eac90d6dc44e2832292020f8808cfd88fe559
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:51:01 +0000

    suggest talk memory I fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
