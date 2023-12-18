25cf842f9f9e9f243fc28d585a468f30bb867d45
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:42:34 +0000

    help instrument cause temper abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import time
+print(time.time())
