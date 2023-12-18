4bd6736b083ceed03c384dd5bcaedf272333a51d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:26:24 +0000

    prize far have square few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
