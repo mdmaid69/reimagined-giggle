cdcba89f6d1c489417ba8005c6076bd08cdf300f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:11:11 +0000

    probably workout available commit suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
