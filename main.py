80ebaf28f736e919275203f4278f9124b3393214
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:18:46 +0000

    connection print transparent wealth treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
