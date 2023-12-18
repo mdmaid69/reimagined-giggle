ddabed9bdd0fb9fe2945e08adaeefc6f221daf81
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:36 +0000

    nearly plenty which heart clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
