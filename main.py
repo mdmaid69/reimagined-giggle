c843f0a11cafbb7fee077bdb4aa87cb29f7c33e0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:43 +0000

    degree 564encourage Ff material flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
