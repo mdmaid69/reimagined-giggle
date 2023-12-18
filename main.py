1129b0b88228e808b4694e592c5521bbcbc49253
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:31:32 +0000

    act sound exciting trap reserve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-print(time.time())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
