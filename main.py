cedb1ea510ea456057c5e0699dcf16b1cce7f0ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:39:52 +0000

    coffee impression meaning nerve responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+x = 10
+y = 20
+print("Sum:", x + y)
