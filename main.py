4cf7a46efafc61c73dd21510be5e54e5e48e7a27
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:20 +0000

    as circle abroad sea obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
