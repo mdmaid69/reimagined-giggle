f9b8b69de61fa0cd9e1084ef951341680834c54e
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:21 +0000

    near pay front shopping other

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
