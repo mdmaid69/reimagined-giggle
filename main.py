b86bdff3cb87372f9c44ba4fcb8451760ef9636f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:38:49 +0000

    frighten leg smile eat extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
