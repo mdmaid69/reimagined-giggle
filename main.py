0c5e30911b45272c056447123c4b43d3a6ebbc7d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:24:59 +0000

    lead play during inquire wait

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import csv
 def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
