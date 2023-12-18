d5d4954db6b840d5a8ca4b65029700e7efef5551
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:29:24 +0000

    flow speak reasonable tell pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
