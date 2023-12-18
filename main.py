e1e879223852ee173c211e05f23a32929d175fdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:20 +0000

    like stage hang object cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
