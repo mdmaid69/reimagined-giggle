3ee49f9c99a3bf4b5610bd5630c396f77cd2c84b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:27:08 +0000

    smart shade rest pump apart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def is_palindrome(s):
+        return s == s[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
