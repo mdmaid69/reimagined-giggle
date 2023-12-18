b0c8c35918b96c31508f7f864ad4dbb0f1dc72c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:11:22 +0000

    search away declaration fit arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
