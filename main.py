a6614c091d800ef2a2b70a1178f113974202f6a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:38:26 +0000

    kill reduce mean chin none

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
