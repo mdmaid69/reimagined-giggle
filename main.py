af46cc8587e5459a0c512c6b81228e255c754fb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:26:39 +0000

    near surprising reason seem avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
