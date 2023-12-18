cdc2c5adfc80dcf095557d48ba63182bc6cd5d8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:43:10 +0000

    fear exhibition loud pleasure crime

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
