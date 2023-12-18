0c4eb934c8175029799ade9429a9ec81631bd9b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:42 +0000

    broken proper wet tune common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def clear_array(array):
-        array *= 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
