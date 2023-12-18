feaf1ec342a13940a7f0d9cf885be29741f04f48
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:01 +0000

    complaint value ordinary bringup profit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
