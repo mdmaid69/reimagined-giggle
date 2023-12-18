7da84b3034f1516fa7928f99d7f435f0748016ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:40:28 +0000

    smile best holdout mention distance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
