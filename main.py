ca32a6dfa92e164684f9a49bc280aa3c7fbcd976
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:30 +0000

    calm track rank suggest bread

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def reverse_string(s):
-        return s[::-1]
+def convert_to_hex(n):
+        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
