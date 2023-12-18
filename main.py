9325ee84549d9a7801c3054d7f2339473ead03dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:10 +0000

    card play action shallow back

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
