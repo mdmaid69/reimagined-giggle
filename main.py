0429a314b3b7b7778b8fcea7d80522ecc1fa0419
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:56:49 +0000

    slip damage ashamed such main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
