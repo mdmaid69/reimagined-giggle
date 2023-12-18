560468d848ad42e92f470aea22713a09fb74cb77
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:03 +0000

    turninto fear complete plenty present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
