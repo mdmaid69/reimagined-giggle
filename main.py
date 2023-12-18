d9653ea3759f86fb87fb54814f8548af8b345d5a
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:27:55 +0000

    thankyou often behind serve run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
