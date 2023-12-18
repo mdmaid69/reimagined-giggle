dc7fa822a2cd8a763debc9922ea30e32a58cab89
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:03 +0000

    blue drop bottom unit soak

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
