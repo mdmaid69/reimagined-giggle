e9899837dd8fdf3a55d1467cca3cb8f749da0ac0
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:36:43 +0000

    neither note long break arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
