05fd722f3e1c36c87140f350a69b14fd87949d2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:38:36 +0000

    collect contact event live seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
