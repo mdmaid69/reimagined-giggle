9da0987d10e006d6cb13f441698eede29b8019c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:25 +0000

    touch wear nose mention furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
