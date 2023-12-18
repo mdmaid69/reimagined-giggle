3f310dbee12132a686a65f0961878c609b68966b
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:39:00 +0000

    high fit pause term loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
