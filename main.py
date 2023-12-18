3ef51c0e56e7c5d0035fc4b4c054314cbb3232f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:48 +0000

    so care getout corner lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
