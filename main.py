07d31ff20bb0aa9f76765775728ad293290ac300
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:09:27 +0000

    success respect relationship tough hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
