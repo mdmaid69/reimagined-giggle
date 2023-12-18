92f91caf8b97881a07861c3cd83598543148ff5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:39 +0000

    decision the free show who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
