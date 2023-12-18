df563e167c7f731ac5400f33b51f8103d4915858
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:09 +0000

    cinema feature run address problem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_queue():
-        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
