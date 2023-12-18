72d3ab2f7e261891f9b85edbc07d66bc37d99502
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:37:23 +0000

    cry level sore wander dinner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def convert_to_hex(n):
-        return hex(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_min(numbers):
+        return min(numbers)
