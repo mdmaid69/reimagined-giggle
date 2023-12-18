17bf784613356ec280e76f2012fe91d8c4a26044
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:53:07 +0000

    various fortunate mess several thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
