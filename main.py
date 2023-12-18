0af7f7c05684ae249780a07fb3ef91340c9ae91c
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:21 +0000

    swing just friendly fix accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
