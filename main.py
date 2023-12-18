96ac079ebce4fb867ba1893e4588bfe8d60347e6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:07 +0000

    Itself which lead disappear runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+print(os.getcwd())
