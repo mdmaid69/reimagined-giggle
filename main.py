f02eca09bd297bc22cdc76af1cd78455a43a5075
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:00 +0000

    bath deaf before over write

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
