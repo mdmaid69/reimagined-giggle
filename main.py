d312fc1c14a91e0d729034921034becb4aef9a28
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:46:54 +0000

    bear human commit rid interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_tangent(x):
+        return math.tan(x)
