53a7204d9595be9768068f659269fab8c82db438
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:09:11 +0000

    unit no comedown deliver effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
