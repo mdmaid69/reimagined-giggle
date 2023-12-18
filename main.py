9a494da0bc75ae33e858e0c9b0649d493ac8cb3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:59:05 +0000

    regret principle blind onto bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
