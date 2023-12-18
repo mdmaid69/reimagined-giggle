ec86798e15ff8fd2a15a0cc0d68cfe3b723e0e14
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:42:16 +0000

    hope bar soft crowded exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
