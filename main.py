a4d8409a1896dae323fa7ee4f88657e299e92131
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:56:43 +0000

    easy callat wire unpleasant goout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
