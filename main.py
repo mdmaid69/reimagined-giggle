1876b54a214cf71e51b0921ed05e7be2bc2f89b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:24:08 +0000

    takeback own trade drink yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
