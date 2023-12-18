629a6a4b6962b6c3870f740b425417bd8955608c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:42:03 +0000

    freeze little team turndown size

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
