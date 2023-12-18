52c21785671854b91372bd5d58b7e0d2bb0fd6f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:55:01 +0000

    bang immediate travel complaint separate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
