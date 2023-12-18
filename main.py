506204a9651e6341b803394727c1d604dddbdb42
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:42:42 +0000

    urgent bang action nowhere first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
