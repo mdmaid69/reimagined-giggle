6857edcf146003ae44fcb106c9a4f7c15364302b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:28:53 +0000

    average sound finger union culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
