2f6c45ca345e486b5b4a86f9923e46b8a1308199
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:31:12 +0000

    grade punish point scream exercise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
