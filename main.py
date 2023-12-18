9b7e791d9ffafe8d9563242f36dd9f56408558a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:19:55 +0000

    wonder speak top range plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
