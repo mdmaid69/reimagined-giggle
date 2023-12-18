3e93ad29db5b7036481e0d5443324a856a58bfb7
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:33 +0000

    what taste ear pride cruelty

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
