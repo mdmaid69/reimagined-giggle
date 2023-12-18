fd3f55c962e4fe83f5c74d3dbd1f9e2040365239
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:35:26 +0000

    start tired makeup section course

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
