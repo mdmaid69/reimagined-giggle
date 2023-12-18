799a5fdcc8a0edd3ec692b30f229b247eb940fac
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:01 +0000

    hurt discovery common destroy within

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
