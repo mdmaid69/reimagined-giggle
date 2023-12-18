33669a9843eccecee3289084576311526f1446f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:12 +0000

    talkoutof various mix heat getup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def square_number(x):
-        return x**2
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
