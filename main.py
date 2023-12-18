cfd02d4555f49f74adafd8f0a688d021eb28de2b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:29:39 +0000

    organize direct no central trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
