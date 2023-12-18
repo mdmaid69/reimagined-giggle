bdd4563e649394321f4b79b8f79291852433da7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:38:53 +0000

    damage term drive separate wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
