b287f2c63bdd9dc322e2f18330077d97086cf70c
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:55:47 +0000

    invitation copy gift advice pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
