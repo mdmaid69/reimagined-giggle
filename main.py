286a0ab37f36bc66a26ef18eaecd0cdfc714d342
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:00:22 +0000

    consult yourself shut live about

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
