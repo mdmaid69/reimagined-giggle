4f13e6da511990fc2ae536d44067ead260e6c8d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:58:08 +0000

    breakdown tight land catch poor

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
