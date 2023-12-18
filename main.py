0b196bbd9cfb33d330e5f606c87c44ffb2247dab
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:31:09 +0000

    command pity behind reliable immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
