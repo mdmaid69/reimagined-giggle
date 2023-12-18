cbe83d3b0c6f3f9fcbaa7441c60122ec038f2c5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:47:21 +0000

    it repeat claim fortunately comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
