c535d73b55df4277478ecf96f412661acf68ae2e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:33:30 +0000

    few cinema especially label wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Uppercase:", text.upper())
