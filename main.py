6786156078676b4559aa2060ba84ff774a834f44
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:04:14 +0000

    front force thankyou quick compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def get_current_time():
+        return time.ctime()
+import time
+print(time.time())
