9f7da92992d2071b779707e99bbfea5f08670fdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:59:05 +0000

    apart kick only thinkover Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
