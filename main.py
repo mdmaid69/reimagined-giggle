0abd550d66f963322d90c2eb29b61de77fd7e503
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:27:01 +0000

    spot turninto journey wipe brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import os
+print(os.getcwd())
