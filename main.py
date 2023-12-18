699d31cea5c24398f93d271e02d3812ac65f6c5e
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:59:09 +0000

    reach chin proper ever raw

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import json
+print(json.dumps({"name": "John", "age": 30}))
