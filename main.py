c2ac519cb0a55fb469f331ea4c8d1311db61d4d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:37 +0000

    crazy cheque violence wear dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
