018e688fe4120a5364edd4e5be8dbc7361be89f2
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:37:01 +0000

    total he crime control degree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
