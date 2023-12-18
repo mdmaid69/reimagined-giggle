f2d2833c099d6cb303332f19d9cf492d14b587b7
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:17:04 +0000

    true group dance avoid offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import collections
+def create_stack():
+        return collections.deque()
