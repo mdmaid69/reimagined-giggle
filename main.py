a73892e62c2ead66152a97f63b389c012e4db2dc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:41 +0000

    unit love evening train genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
