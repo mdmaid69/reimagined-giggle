30a713f7d67a84063583f141c932fe7e51a3a3a1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:11 +0000

    wear fault tap treatment past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
