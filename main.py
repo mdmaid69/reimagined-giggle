d243e11e455fe2aa2b372bd841fbf0e031bbf963
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:43:18 +0000

    build undo hole when high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
