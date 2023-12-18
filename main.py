84fb99a51ca33eb7d33e86b071b8d46fa57ff3ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:38 +0000

    guide university ours table distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
