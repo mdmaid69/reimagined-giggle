1a7500ed4d67986a91d6fcd3f315a41b6eaae331
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:12:16 +0000

    male answer break enthusiastic catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
