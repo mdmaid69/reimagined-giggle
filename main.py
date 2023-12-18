13b0c4303d851dbece8a7e7938d17e96ea18da78
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:16:54 +0000

    battery scheme refer jump sport

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_power(work, time):
-        return work / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
