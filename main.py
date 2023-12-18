53f92dfac82970d5b47b5571792f3567f1fad4b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:20 +0000

    cheque bright lookafter get reliable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
