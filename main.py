60074f8bb04c37471acdb2b8bd16de0d74175d34
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:47:38 +0000

    cook copy base contrast commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
