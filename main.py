92fed56042974f0f741aa7beb75feb888412fd47
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:12:40 +0000

    goaway hers turn violence passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 def remove_duplicates(lst):
         return list(set(lst))
