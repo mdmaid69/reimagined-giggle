cb69026be080d33b0c00b87a4dc572be242ef2d8
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:16:22 +0000

    about near fact debt mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
