0e779ca6450ab1ccc03600f38fd3d9819db19f2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:34:57 +0000

    path sensible development juice any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
