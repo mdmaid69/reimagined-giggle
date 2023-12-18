5b8c998d0a443f92cf4d53d99263e5b06948d527
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:19:08 +0000

    contact used meanwhile hurry question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
