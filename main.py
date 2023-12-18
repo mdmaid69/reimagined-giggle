5a50bdab52e59f0ba7a87965faf86debe4d98488
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:33:52 +0000

    there ready whether tape short

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
