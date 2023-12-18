f67866fd4b62543d7da607be86cdd1d0c573b900
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:06:46 +0000

    label smooth class question sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
