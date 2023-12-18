0675d4dd4a14edcb070e50199a346a396f85d35b
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:45 +0000

    mark match pain by 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
