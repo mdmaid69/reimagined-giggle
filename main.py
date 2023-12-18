8699af1a39beb19f9d42b8278b62159cdf114226
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:26:49 +0000

    immediate friend confidence brush bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
