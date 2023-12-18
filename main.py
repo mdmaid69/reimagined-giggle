a99bf27db72bb9f37aeabf5aa51785b98365ef69
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:13:02 +0000

    clear Cc male pollution fact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
