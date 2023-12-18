b03c8450984aaedc4815dfa6f29c34b32c497c55
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:31:05 +0000

    plan lock uniform disagree sleep

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
