7da60fcc02abe30a3180f5a6acf796acc18b1c50
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:13:13 +0000

    admit extra form bringback worry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import time
+def get_current_time():
+        return time.time()
