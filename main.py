4e7c70307e92047693e52472e9128dfc0b7cac49
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:57:35 +0000

    over nice unfair bin surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
