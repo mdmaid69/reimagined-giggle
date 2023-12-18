ceef9682102bdd82a32be1ff389b0460984857ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:37:44 +0000

    either art touch introduce provide

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def remove_duplicates(lst):
+        return list(set(lst))
