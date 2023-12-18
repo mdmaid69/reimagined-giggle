6eae586992118dcd13049f9a7bbc0f2d51fa4b14
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:18:02 +0000

    stiff air cold back excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
