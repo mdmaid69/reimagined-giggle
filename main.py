b04d11e68271c0a505d6100cc56375e14efa1949
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:31:13 +0000

    lookaround survive equal hardly forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
