2e17ee464a84d19163f21489f9b62ebdd138570d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:35 +0000

    various Kk popular zero takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
