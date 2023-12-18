f88a01e929c78b597ca018c9a1ee7c3238095536
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:21:46 +0000

    word waste narrow direct crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
