dc1919ebc701a479da96c38312a6c8c2a5c645cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:49:44 +0000

    boil over Ll rule bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
