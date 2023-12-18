1255b755dbbcf74f87662f46a61f4b84dae6884a
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:18:16 +0000

    fire law warning habit rescue

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
