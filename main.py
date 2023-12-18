c71dbc34e02a60293744778f7e4651ee9dd4f61d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:05:57 +0000

    owe degree control loose Cc

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
