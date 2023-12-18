83951af799a295a02e1005b89a2ae50045769275
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:01 +0000

    payment 569enjoy tour cum distant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_work(force, distance):
-        return force * distance
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_list(lst):
+        return sorted(lst)
