03eb903e7ab1828f5ad15206a3b477b03abad9c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:13:26 +0000

    culture react takeaway little fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def sort_list(lst):
+        return sorted(lst)
