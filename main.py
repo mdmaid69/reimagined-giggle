f7c79603b4380fea27f496b9e0cbac6edb747211
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:56:25 +0000

    might pride drink woman block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
