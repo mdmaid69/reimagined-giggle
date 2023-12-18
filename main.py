05b4fbf9b159308b91828028f8febc7513d3b649
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:57 +0000

    effect intend rub something even

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def convert_to_binary(n):
+        return bin(n)
