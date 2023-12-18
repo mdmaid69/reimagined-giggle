b5662afecfc3482ca13be74febf9c38d628c565d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:22:08 +0000

    bet successful interview anywayanyhow bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
