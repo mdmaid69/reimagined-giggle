10981224afcd3cbdb9bcb466988f12d4ada64c03
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:00 +0000

    fire anywhere lesson first general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
 import sys
-def exit_program():
-        sys.exit()
+print(sys.version)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
