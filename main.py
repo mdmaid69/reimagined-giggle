ba9058f2a8bdcaae1a563f427e84d7cb80fffbaa
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:55:11 +0000

    disgusting trap ever getaway cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
