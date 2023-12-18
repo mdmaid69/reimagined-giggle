16ea63b2c119784aceffff074b1500335083e4ff
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:11 +0000

    thinkthrough safety hope pour ring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
