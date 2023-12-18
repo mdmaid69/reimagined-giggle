138bfb5eddb3daab9b907a06110c484e7b5481ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:37 +0000

    wire because stretch collection part

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def reverse_list(lst):
+        return lst[::-1]
