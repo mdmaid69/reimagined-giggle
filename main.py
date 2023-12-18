22ee9d74c778d0ebbfc9e31b5ea97feed92f081b
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:19:01 +0000

    demonstrate Nn explain uniform pocket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def cube_number(x):
+        return x**3
