822126cf1eeb27807356bb089d638ba7dc689c76
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:00 +0000

    entrance black forgive mail advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
