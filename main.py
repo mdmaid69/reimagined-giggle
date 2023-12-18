e1b7b57f799fa9ed986e6ae9704dc71e9ea81f27
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:18:46 +0000

    start indeed formal finish polite

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def append_to_array(array, item):
+        array.append(item)
