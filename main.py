180478b14ba3500f3ebb8586586702599d813bb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:30:38 +0000

    hate pipe capital make thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
