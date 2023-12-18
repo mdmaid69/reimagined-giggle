a486a2edaa6b98184d0c51190c9375b6973ddbdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:30:15 +0000

    sit unite finger suggest prize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
