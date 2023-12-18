7481fe8a3c7c80656d62112491f73cc2d4de138b
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:18:06 +0000

    decide wet hole pleasure permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
