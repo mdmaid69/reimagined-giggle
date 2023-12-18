2a21ca28478ab330f83ae6a09512217ee30d1ac1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:24:26 +0000

    waste direct loud prize rich

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
