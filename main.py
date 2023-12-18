762de9d70864fa44f3e46ca409fe2a2ba856e6c0
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:00 +0000

    place right arrange cold keepoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
