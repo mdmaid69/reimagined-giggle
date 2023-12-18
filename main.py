321569a759b710d4d072fb1a67a7ea62b0e833ec
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:47:35 +0000

    mend even dress quick runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
