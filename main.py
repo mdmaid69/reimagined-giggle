efb9d2d70a573b135200d084608e353b9f767281
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:15:08 +0000

    arrangement window underneath buy kill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
