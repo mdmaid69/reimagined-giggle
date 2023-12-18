223ea8eb7e74b18dcdabc648a8ab48fa124689e8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:41 +0000

    favour entry open soak direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
