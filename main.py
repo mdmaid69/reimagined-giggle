d796525a9d51208c53919e59201cc80aea97119d
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:33:55 +0000

    frequent stand want intelligent pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
