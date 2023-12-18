adb1447615606795bca206134f6b33b87ca9309c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:32 +0000

    on design as takeoff tough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_base_name(path):
+        return os.path.basename(path)
