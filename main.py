baf9d3bd3d9fd4fc27ed559371afcb64e5644f72
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:25:54 +0000

    workupto exactly gate majority tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
