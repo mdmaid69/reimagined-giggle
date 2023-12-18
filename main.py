c9211768647bce129c431c0321d6e3f24a5627d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:09:53 +0000

    plain file slip miss act

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
