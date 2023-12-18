9af7cd88d3be3bbdfb7747fb8cbc08a252e5aa8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:41:35 +0000

    at positive ordinary damage post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-print([x**2 for x in range(10)])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
