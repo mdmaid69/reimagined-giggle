2775972c27da928dfef2e26286bc1708b65ce3dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:11 +0000

    fierce himself powerful cheque contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
