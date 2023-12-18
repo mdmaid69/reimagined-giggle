62ac3c8a4d710e9783b7f94ec070639edd46273f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:33 +0000

    storm school do wind chance

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
