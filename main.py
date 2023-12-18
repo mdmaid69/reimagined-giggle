9c4d4a69ba600001dafc31d76f81fe0b65baa7e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:15 +0000

    use Bb interview track zero

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
