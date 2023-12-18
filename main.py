ea85e94979e793c26baa9a2e14944b6c8a54963d
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:44:58 +0000

    smoke borrow grow persuade scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
