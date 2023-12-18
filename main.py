4292b02f963786821dd2723fb7a428fabb2d4c1a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:40:07 +0000

    dish relax share if well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
