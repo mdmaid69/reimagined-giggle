0ec92e9fbfb1ed4a8d17a99c86e533017f1af9a6
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:52 +0000

    whistle force near bill kind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
