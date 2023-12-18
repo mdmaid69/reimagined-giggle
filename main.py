c52b7f467bf9bdc40de05c98a0e3aee9731229d0
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:11:14 +0000

    until preparation vanish range choose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
