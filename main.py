be5f52a8626ccff6f0323b71fb0161769fcb73a3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:40 +0000

    getaway feed freeze dead way

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_work(force, distance):
+        return force * distance
