d7ecff03a4838c926d063ec19222cf7d44a05159
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:51 +0000

    spray receive splash compete tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
