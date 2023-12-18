c36110928a0a998b290b73cf880684decd798269
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:28 +0000

    failure environment deserve there imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
