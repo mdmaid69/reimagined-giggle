e1ecb971a3fa9f0fa76206be72c8ea05f3286d38
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:54:23 +0000

    forward vanish sick urgent important

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
