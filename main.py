fe08e50811d2c75d17da6d30b767e12a01fcbc44
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:23:43 +0000

    the everywhere press and freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def reverse_array(array):
+        array.reverse()
