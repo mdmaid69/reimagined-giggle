2a407b66e077078e89de8863de4d6156cfad6520
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:22:31 +0000

    either Tt top total once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def append_to_array(array, item):
+        array.append(item)
