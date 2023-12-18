758b13b22e93860bd20bc51c52326e164393d2a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:17:38 +0000

    keep seem knowledge Kk thirsty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
