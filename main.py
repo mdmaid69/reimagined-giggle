6125c2b0af0c57615f33aa88e1eaabaea0926472
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:43:22 +0000

    smell prevent control judge calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
