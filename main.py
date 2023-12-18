be94c8956e1ad4648c8fb40bbdd641f9aea79391
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:46 +0000

    danger but quality direct apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
