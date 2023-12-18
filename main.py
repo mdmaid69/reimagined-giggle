dfdff54230a5d10ba73307fdbd0f3ff365fe51c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:40:22 +0000

    explain lay music neat properly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
