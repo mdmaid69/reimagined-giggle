7af0948bf371a9517b35c0689bb89d8a5b8f3454
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:52:39 +0000

    sail achievement loose getoff patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def remove_duplicates(lst):
+        return list(set(lst))
