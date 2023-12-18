861e54003dbfe91e280e684fe20f41d586b11ed3
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:03:54 +0000

    Bb worse date whom disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
