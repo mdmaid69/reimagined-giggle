18dbd2e3dbe7500012cb7a325cca76b75a6170fc
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:08:20 +0000

    violence accordingto board stretch up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
