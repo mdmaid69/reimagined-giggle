7b0b1d6fc83d0f360ed04b8c8e47ce095c6888cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:27:38 +0000

    steep television press trial fact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
