46e150c66e626f9176b37cd1c22faacabb0b3bb3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:13:24 +0000

    fond advanced middle Gg line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
