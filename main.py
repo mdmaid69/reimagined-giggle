fe7a758584ac64ff73cfb1470447c3f3f11aa43d
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:43:07 +0000

    disappointed sink other note lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_sine(x):
+        return math.sin(x)
