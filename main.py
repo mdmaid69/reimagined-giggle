cc588243dbd09699984bf37038e408aa5cd4fd26
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:24:27 +0000

    blind environment kind search post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
