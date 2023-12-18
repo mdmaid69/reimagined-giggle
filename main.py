2188937264f37dc0769dfce4ca786c98037bdc4d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:27:32 +0000

    pleased grateful object gate tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
