ebbdf2e3f35219fdb96f747039a1f8d829d069dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:29:34 +0000

    practice die stick disapprove opportunity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
