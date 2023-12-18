4a5695606e9a9253aa1f4884ccc8dc293543ea1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:17:19 +0000

    shop close trust entry can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
