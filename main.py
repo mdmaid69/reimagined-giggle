6155f842ef5bd9515ca1c5fa882db12f6a755632
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:29:53 +0000

    continue sort trap scream expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
