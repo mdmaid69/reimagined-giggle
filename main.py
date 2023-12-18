b1c41a1bd5e7621900821d7f4368f3b7d5bde955
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:28 +0000

    scared injury majority almost hardly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
