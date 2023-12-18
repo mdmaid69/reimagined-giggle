4025fffaa7211609c6d47349dcf6ede78ab2a299
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:13:59 +0000

    beat similar cheek wherever wide

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
