b9fd96c3852f5474316b58bfbd8f4d607a811554
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:39:47 +0000

    that rough country weigh week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
