b929e317d86b81c9ce64debf0c66af264954b6fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:46:52 +0000

    government dare generally word point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def get_array_as_list(array):
+        return list(array)
