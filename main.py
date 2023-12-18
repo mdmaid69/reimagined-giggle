c5e927f3a67df3486ce2ce3b7b0ab6eaf7afdc14
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:41:44 +0000

    train gate take show successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
