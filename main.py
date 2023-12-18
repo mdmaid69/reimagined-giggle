3abf20d6b7262cfbe9c7bbee646365abe2038115
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:41:17 +0000

    exchange little adjective turnover chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
