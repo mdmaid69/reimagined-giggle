4c321a41a25b7c496e3c270e423b60c286d1da67
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:35 +0000

    music display land politics suggest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
