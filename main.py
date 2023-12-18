62703f64a02bcb492b938c81358173d93856b54a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:25:29 +0000

    both remark contribute turn father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
