2c6fb4ded925b5cec96005a641dd40ebef589ee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:52 +0000

    use unkind show show freeze

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def reverse_string(s):
-        return s[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
