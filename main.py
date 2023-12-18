7506a146bfbcf47fd0f5afd3a72fc59c8c03885d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:35 +0000

    great hard hand comment shout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
