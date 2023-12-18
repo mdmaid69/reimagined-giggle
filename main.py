7c389dc6ccc7c5ba88d853674cf88a558107ec34
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:19:09 +0000

    please without idea wander formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
