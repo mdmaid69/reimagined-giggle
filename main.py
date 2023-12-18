24bbbe6fcd558dc2afacb83a63c5a5d99a56794e
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:18 +0000

    judge coat around worth their

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-for i in range(10): print(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+print(os.getcwd())
