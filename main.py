e551bb8b7d17e11194b93178a9f856b93150212f
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:53:01 +0000

    yesterday leave maximum own gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
