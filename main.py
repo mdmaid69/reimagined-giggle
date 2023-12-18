d292b5de04b5ee35317034aa3c844c6c6c4515b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:38:33 +0000

    ilovedicks depend story hate statement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
