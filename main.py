2ee3427ab18aa623406e87e3126cf3482c143401
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:17:45 +0000

    hit free call post work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
