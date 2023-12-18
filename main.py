5412d8c44e8c8d8cd5673b84de4ad6734f66c378
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:56:18 +0000

    ability comeround while succeed choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
