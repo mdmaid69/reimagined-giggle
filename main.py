1a13234ef483e65a339d10968f359a743112947e
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:37:21 +0000

    risk sister each rely window

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
