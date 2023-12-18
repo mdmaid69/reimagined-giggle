1f21b7dbe48ec008b56ac466017a90a073f93117
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:24:37 +0000

    film majority tax squash doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
