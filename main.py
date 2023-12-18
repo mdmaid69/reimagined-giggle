6a253bad09cf33852425f17eb44752fc9c81c662
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:59:25 +0000

    professional false loose high fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
