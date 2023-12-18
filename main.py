3962145654f02f6b29ba88c6fbeb4ba934bf1073
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:50:51 +0000

    left physical computer tend step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
