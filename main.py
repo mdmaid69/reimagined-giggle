7f89c415248a42b4374d2779ae3dc08836db55c4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:10 +0000

    real eye choice new holdup

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
