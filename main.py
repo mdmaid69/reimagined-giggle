6065c5190c6310034841ccdfc398e2ae357223db
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:58:51 +0000

    Tt deep practice turnback design

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
