c720d3807da836212b420733bb2de6dc3aaa9118
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:50:07 +0000

    put urgent their sharp listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
