b62f7ad3f48a3cfbe85b8191ec623ef06e3afed3
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:13:19 +0000

    wrong set true turndown compete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
