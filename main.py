5312b6615e1b58554809ffa4befb1292941191ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:25:41 +0000

    suit lot fork distance exhibition

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
