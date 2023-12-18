0edd8067aa36e088d4954dc1363ab144e91fe1dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:18 +0000

    Mm promise present market seat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
