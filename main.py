a4911151c3d3755a9ab586175fe8ff0dc28e4e05
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:37 +0000

    deep yours rough bag above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_power(work, time):
-        return work / time
+import random
+print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
