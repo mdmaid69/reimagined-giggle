e56dbd6989a95c8ea43df2b38e6fb7380d792743
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:12:52 +0000

    by worry float once deep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
