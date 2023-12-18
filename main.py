dfaa601f4cc3005fbf903519ad2aa18a71574860
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:54 +0000

    before your only demand tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def subtract_numbers(x, y):
+        return x - y
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
