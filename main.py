244524074f460d465e7ca44e8686757fa8d0598f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:46 +0000

    active 566end immediately remain trial

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import getpass
+def get_username():
+        return getpass.getuser()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
