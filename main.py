ba187d3f4688df317f17db2431351b17290095db
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:36:10 +0000

    behave none discussion upstairs case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
