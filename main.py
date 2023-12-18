7d9d90b4431efeb37b940a4dca902fcd9ee081e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:01:08 +0000

    sting usual commit nonsense better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
