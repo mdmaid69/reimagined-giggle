72f934d69c6cca0f7eecd23e9d6261e2f6cf4766
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:18:03 +0000

    culture cross method level sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
