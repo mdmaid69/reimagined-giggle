d24ea5c154a892a6ae4457c966b4d1d2e3fda295
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:12:54 +0000

    wake moment rob fault sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
