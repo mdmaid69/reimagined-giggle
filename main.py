cb9aa7162a4a455c82d6ef0d56244e1b9991d102
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:48:18 +0000

    whoever quit debt dinner destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
