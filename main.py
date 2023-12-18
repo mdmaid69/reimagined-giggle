ece2af9b0c2f6fc799dd2b4850780fe48a184efe
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:44 +0000

    complain degree total amount proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
