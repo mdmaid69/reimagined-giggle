4c4edc3a5858186613146ba287a04eb76f691ad6
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:23:50 +0000

    wherever thick speak fresh nurse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
