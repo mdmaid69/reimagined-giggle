e11f716affdd865b91fdf779905418b530063480
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:11:08 +0000

    finish air contact near quiet

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
