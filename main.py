fd343e41e6a5ae3b27680af0897d4e57fa09769e
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:23 +0000

    port money welcome church present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_float(array):
+        return float(array[0])
