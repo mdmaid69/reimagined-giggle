a96533cf769f5062aee981f1c6f1ea8ea50662f6
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:30 +0000

    but copy vote smell rent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
