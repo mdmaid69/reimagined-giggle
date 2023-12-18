3e67627dc5bbf8fbdf49a4fbc926e0794c97be78
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:06:21 +0000

    promise final criticize walk mouth

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+print(sum(range(10)))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
