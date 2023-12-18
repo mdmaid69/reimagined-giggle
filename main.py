f68e40ff938c507b6c97c338cc8b1d3bcc192694
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:51:16 +0000

    pity stranger claim present Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
