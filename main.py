f2e90ba4fc32a1f558b766cda28fb79d6cf0d25d
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:21:46 +0000

    accept method agreement wire shoulder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
