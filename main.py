06a82988df9fcb6decc2856f158cfa2cdac8232f
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:19:01 +0000

    murder manage murder favourite guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
