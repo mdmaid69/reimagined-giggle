0d7736fbeac61737af3d50275e21e24fecd2e32f
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:28:00 +0000

    shopping top blame might classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
