e3617fc319a762dedbd67207d00d9bea5edc8749
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:01:19 +0000

    conduct issue sleep cold thanks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
