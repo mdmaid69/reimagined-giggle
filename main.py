156c86e9b66815e80cd6f338ce8632d96e8cb0b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:27:22 +0000

    low advantage that necessary bunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
