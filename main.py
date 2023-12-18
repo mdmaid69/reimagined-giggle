94b710d1a414f661bdf17cda5073f4abf3f0184c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:25 +0000

    peace satisfactory chase quickly ordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
