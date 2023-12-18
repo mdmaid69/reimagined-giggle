ed6f7d30dda894cfade99ce8220d9a18b86725fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:13:36 +0000

    breakin prefer perform skill cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def clear_array(array):
+        array *= 0
