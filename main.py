876f21741d6c901ec3da585c686fd7da9e1dbc1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:23:24 +0000

    getoff friend why description suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
