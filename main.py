987e73a58c439d0b504ded56203f6283510c68a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:04:28 +0000

    confused table trip rarely inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
