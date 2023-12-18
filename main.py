260f904dfb3409a6f50276a4a8795a16999922da
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:45 +0000

    centimetre instruction holiday teach clear

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
