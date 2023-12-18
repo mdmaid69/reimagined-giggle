92a205b670d135505b3e2d1227d19455edbd0ac7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:48:45 +0000

    region base full fashion behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
