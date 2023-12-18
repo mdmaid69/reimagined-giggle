43d0ebe1b5b851ef5c4e37913e978b4c27d61b0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:38:12 +0000

    dish wide born mine shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
