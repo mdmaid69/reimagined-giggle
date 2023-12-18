8209141a2a2423eeec4cec0a33be70e6c29d73b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:43:36 +0000

    address brother before greeting serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
