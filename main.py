89725cad8416097de969a430bb76ff92e29bb26b
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:12 +0000

    pile wash match cost hardly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
