ffff588789a6c50c82b8e74392d60871bd7e9e8d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:04:23 +0000

    priority stretch hit food bottom

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
