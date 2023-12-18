1977515238c1f8db3dc8f505be864c4430550b2d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:46 +0000

    change turnover fly against conversation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
