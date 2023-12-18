a000040c624788ca7e5c55784ec43bf19f87658d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:57:22 +0000

    fortunately lunch thank frighten heart

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_area_rectangle(l, w):
+        return l * w
