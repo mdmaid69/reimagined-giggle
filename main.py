dd4e70aeb942cd796d428c3a6acf3b61995fa2ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:10:02 +0000

    takeoff commit occasion complete ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
