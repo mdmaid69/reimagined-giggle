f678466f7e5ee1d57e3fe38b27660953d401e1ad
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:53:13 +0000

    talk shoulder birth ability wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
