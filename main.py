80fc4eb4583a95ba6b1acc12239857623eb1d1cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:30 +0000

    in song explain right light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
