47605642ffdeb151feca8c1356f1aa940aeb6835
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:36:53 +0000

    have question ability weak powerful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
