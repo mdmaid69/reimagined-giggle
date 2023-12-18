ca7886c5b3e2d1d57fc9a57c046258421a09cee8
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:22:22 +0000

    pin unfair wake sit suppose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def greet(name):
-        print(f"Hello, {name}!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_odd(n):
+        return n % 2 != 0
