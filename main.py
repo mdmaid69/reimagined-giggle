7f503d07b99a9653218daead79ca0df0cfd69e44
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:25:40 +0000

    interview pleasure game black cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
