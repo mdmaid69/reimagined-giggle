d4a4d35ce0a41205ba06f7f5101b3b3a86ab9d5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:35:59 +0000

    ball extra hair interview give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
