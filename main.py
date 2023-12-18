2edaabc9d491db1ec112211d14b260a68e900bad
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:25:09 +0000

    shock smell town formal entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
