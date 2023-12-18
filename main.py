ceb85e0db51177c9e4291b1dd5fcb586391e4dda
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:42:05 +0000

    study except hard where close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
