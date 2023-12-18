2ccc91ac851a812a16975570fc4694d27339d7eb
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:39:39 +0000

    general include middle noisy load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
