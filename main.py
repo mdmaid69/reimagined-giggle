541657a05baa30c1823c4adcdeb242139332124f
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:09 +0000

    knock lead guilty answer sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
