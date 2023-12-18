fe6b0a8c2537e1c28dd13a25d0fa9007bf173fb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:03:06 +0000

    even then neither delicate fire

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
