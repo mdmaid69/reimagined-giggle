2e25e5fd010531478066731799076e9b9d5bf671
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:46:27 +0000

    risk arrive union vanish tongue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
