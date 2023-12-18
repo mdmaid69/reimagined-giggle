bd255eb5a6c0102f1200ac45de4830bb348881cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:48:56 +0000

    colour field deserve police record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
