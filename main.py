844166cb345be4d27438e9063e79d5ce6f7defc5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:58:21 +0000

    force hard holiday second quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-print(sum(range(10)))
+def get_array_slice(array, i, j):
+        return array[i:j]
