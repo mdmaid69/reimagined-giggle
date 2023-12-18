38591bb55403efc6c6c77f72bd91416af28dd963
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:57 +0000

    scrap beside feeling about address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
