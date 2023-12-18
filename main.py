08bfdfb1ab3bed5031c77727497ee573985f019e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:16:39 +0000

    suckme occur violence grade ourselves

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+name = "Python"
+print("Hello,", name)
