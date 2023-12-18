35b9570923ea95a0b7d9c03a0856a8b69079259c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:40 +0000

    dig towards burst free drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
