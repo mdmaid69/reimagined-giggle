b2ef72b2dde49025124512d6376384ec0e7c6934
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:37:31 +0000

    dress survive mysterious conduct better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
