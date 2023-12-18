31ed70919b49ba862a108715eece8d10aae5bf35
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:47 +0000

    community large play string owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
