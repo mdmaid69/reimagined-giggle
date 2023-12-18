3627662ceb19918fdd6c4f9987d8d71804ebbb35
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:06:46 +0000

    before base meet pause happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
