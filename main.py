91a112d693c6a0c8866f419121d96c394cc2c336
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:44:51 +0000

    trust callup comein morning beneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
 import array
 def pop_from_array(array, i=-1):
         return array.pop(i)
