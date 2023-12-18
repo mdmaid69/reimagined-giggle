4e8fba3a6007e816d333acf6b61dee596c8f6a11
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:03 +0000

    season tense far either boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
