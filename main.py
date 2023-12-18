5f29f3c34d46b5bb7695ab8d26ed9be2fc06ba44
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:59 +0000

    worth expect relative create write

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
