1fa08a03ca31c289a065078b03fd7ecb8c7ab03d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:24:00 +0000

    set on blame clean wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
