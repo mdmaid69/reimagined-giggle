a0466589cb744344450a1157836d807dbaf0103c
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:02:49 +0000

    shut note boil precise risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_list(array):
+        return list(array)
