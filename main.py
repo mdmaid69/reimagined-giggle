4148e3bd7fdb72e44e712e88006a78373cba0dff
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:03:06 +0000

    smell even entry run wrong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
