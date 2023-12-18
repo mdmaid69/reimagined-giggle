0c3be89d197025697ebc8f0160f231c434a3cfa9
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:47:25 +0000

    book Tt message emotion beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
