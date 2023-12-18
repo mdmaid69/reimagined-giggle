68966759b3a717fd6da0afb0781dfe3fbbecc509
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:34 +0000

    fly quiet trade entrance far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def clear_array(array):
+        array *= 0
