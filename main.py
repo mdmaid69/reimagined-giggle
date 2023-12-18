ce69bbe935d7058e12715f6bcceff762b2f57431
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:09:57 +0000

    against clothes report life broad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def divide_numbers(x, y):
-        return x / y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
