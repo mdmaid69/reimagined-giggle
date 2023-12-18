6ec21c874814d002d8283f4e39ba7180af1d450c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:42:26 +0000

    Rr claim rain underneath environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
