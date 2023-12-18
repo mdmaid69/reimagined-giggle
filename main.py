5d70840d9e5163190709e5bac3e70716a456099a
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:55:48 +0000

    fair spite news impossible persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
