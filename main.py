b7364a6c26d58beb31f2c444148abf736a0696a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:27:27 +0000

    borrow float congratulations mine check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_array_to_string(array):
+        return array.tostring()
