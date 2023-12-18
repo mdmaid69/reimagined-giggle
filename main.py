33b4443800d4ae28eebb4eddc6b4e40c3de5006d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:49:33 +0000

    absolutely either thumb polite hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
