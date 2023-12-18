6ab0676c604e0f4e7e945f28d95010b8274650e1
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:01:28 +0000

    fast week live result handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
