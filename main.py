f96179f43a62a254702742be910a5e34e5347fd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:22:40 +0000

    bear ilovedicks building outside 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
