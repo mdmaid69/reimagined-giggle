fd306b09730bc3ad7474d9dc1adaebb5bdc7a89b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:50 +0000

    race accident hospital share lookaround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
