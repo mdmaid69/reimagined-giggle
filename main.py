1bf7d0e8cfaa5e0d4b4700e9bcc5172a9bb30f8b
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:54:00 +0000

    back plain post nature 565end

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
