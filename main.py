56907f7cc1c087b9e509ef022979e24082cae8bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:55:54 +0000

    themselves father bridge month prize

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
