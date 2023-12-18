eb34da06dd6cc9a23e455ce0125d19cb983698b0
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:30 +0000

    accidental apart must really hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
