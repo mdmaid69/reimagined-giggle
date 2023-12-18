1dc2bc8f89cfade32d889693b2ad5391a8c284e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:16:10 +0000

    central wise container chin sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
