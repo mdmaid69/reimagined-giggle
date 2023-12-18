80ed17b831fefeedab13259bc005fc3bbaea1ee3
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:13:03 +0000

    closed conduct spring either run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
