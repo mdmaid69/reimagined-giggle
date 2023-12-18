1aaf53c5ec61192cd5513925e8567f444bea6961
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:47 +0000

    arrival popular lookback(on) inform respect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
