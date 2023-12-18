5e8da5f7259c8685e8835fc2871a6ec5b574808d
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:01:13 +0000

    big choose could 570enjoyable club

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_hex(n):
+        return hex(n)
