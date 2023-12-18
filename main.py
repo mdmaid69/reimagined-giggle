acda105517a0be36191748777de98134745e1abc
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:07 +0000

    object job hole nonsense maybe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
