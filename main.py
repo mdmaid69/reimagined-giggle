8ca161b828f56046dac111e8f3010d6e1c2c644c
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:04:48 +0000

    steal detail serious personal shine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
