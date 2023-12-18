7f26fa301c9568363417830085f2babecd8d6c38
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:41:33 +0000

    silent intelligent slight container promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def clear_array(array):
+        array *= 0
