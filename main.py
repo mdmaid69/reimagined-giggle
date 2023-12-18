b3897456a044d4bb5b5b6fba2ebe88860aacc080
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:14:02 +0000

    coffee seat Ii apply match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def reverse_string(s):
+        return s[::-1]
