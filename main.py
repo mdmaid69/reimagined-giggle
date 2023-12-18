6f85666a318cc369719fce023f2af16f1d3b655c
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:34 +0000

    spin married pay pronunciation fear

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
