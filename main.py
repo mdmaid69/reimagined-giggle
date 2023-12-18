0c3d1a19c500891f69e37ab854deb47995c06882
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:06:07 +0000

    accidentally kind far pause physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
