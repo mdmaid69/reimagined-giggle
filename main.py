d555618453cd7991a464a0b87cad46f61545f577
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:31:46 +0000

    national any splash record whoever

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def reverse_list(lst):
+        return lst[::-1]
