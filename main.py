17f5abf59738760cc3d27cc8f72547016b474eb2
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:09:12 +0000

    ever that late convinced system

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
