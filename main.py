72aa2022a890a44f8939b36774c9b72fdb61b3ab
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:10:13 +0000

    earth heavy any increase tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
