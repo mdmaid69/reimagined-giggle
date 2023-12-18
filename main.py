600c5581769cf2a9a245273551190b8615e7050f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:33:11 +0000

    anxious impatient study anybody store

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def pop_from_array(array, i=-1):
+        return array.pop(i)
