4508d2a2be4ad963057b962c1398124f691647d9
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:33 +0000

    miss there smoke discover happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
