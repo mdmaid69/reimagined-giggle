5d4e1edcf33d2c46cca56c22c0d8b143ed086d40
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:32:41 +0000

    arrangement mistake cheque appeal urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
