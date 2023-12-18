728bb41f08ac71e53dff1db726ed95d906175ee1
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:15 +0000

    upwards key bad stop offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
