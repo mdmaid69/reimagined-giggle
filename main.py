267324b72615387fa94dbb638867ebf12e067dd4
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:05:00 +0000

    tend tired near full person

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
