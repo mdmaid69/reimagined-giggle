6b32b89c92cf21efea37dab58305eb3010063064
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:14 +0000

    print cinema final Nn fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
