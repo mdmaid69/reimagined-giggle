0460424ff090fba9b79d63051367b1521f7b4bc6
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:56:36 +0000

    nearly explanation weigh spot pressure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def count_words(sentence):
-        return len(sentence.split())
+def greet(name):
+        print(f"Hello, {name}!")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
