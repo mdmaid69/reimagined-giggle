91eb74b9b7bf85eb3c99f5565d30756d86a6411b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:23:37 +0000

    minimum ago often black regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
