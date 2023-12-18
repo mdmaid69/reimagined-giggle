057a7b3c59868f71a57e0831c8a5ff3b3d0a393d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:19 +0000

    section car badly married smooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import json
+def convert_to_json(data):
+        return json.dumps(data)
