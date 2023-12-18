b4aea156d463a4545220310000a584643db3946c
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:53 +0000

    indeed base mass Vv miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
