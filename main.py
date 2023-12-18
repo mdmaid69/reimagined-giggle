95daaa128a40eec1d7b6b9bb86397bb6c2520917
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:04:26 +0000

    soil accept major every young

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
