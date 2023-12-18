2fe721c666003b9a1a0ba5e35d5d68745de0ae47
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:47:59 +0000

    proud otherwise official sorry public

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import datetime
+print(datetime.datetime.now())
