a5d2e95b06eed763e3c333910aea3b44ced74af3
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:53:47 +0000

    experience murder instead cost knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def convert_to_hex(n):
+        return hex(n)
