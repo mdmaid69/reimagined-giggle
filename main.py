7be6d09edcbb005efdb2807fece50c7f40fa60a4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:48:05 +0000

    breakout try practice callup centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
