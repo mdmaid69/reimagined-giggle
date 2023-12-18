9caa0a2fb997778b032bad50dfb4dba6c3f161ee
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:28:19 +0000

    seriously either accidental correct majority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(lst):
+        return max(lst)
