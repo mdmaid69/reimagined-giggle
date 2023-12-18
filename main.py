af68475436fc2531faff36903473fb794a8b267f
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:24:39 +0000

    hold need case month afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
