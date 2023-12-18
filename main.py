c25baf716fa1294baa3b9b99bac3f55d831fca10
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:06:45 +0000

    perfect remember always wing price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def square_number(x):
-        return x**2
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
