0383928c744d2d7b14568fe05e1b73cc86245b1d
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:49:37 +0000

    turnup disapprove concentration answer commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
