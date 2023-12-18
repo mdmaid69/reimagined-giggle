982af7d3a7d42f7b5fe39d9d31652e3fa3f540bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:29 +0000

    suggestion radio one false out

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
