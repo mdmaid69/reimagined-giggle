2454e2125b1c30be338167fe418ce6239e470b2a
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:30:42 +0000

    knock unpleasant punish area cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
