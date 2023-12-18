8d2edbf0aaac26254969777e281ba5201d07e518
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:12:45 +0000

    Hh necessary sing eye disturb

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
