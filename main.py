8919d46c6091dbe34c54e0c08fcde9094f5ed651
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:41 +0000

    onto load share continue fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
