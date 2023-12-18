91e9091a752e303d871cf9ae9f0b7a1e63de75c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:03:52 +0000

    free shout several shoot pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
+  import sys
+  def get_python_version():
+        return sys.version
