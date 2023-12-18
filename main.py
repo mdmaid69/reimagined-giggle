620a5cd238298af2072046a19ba77283597f1bef
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:46:29 +0000

    satisfied advanced go godown damp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
