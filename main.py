8d09b1452c52bde32a200726db4627ed598a8bdb
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:40:26 +0000

    open bottom number brown professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
