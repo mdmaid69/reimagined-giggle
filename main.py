87385e54fa2c52b57db45d4a05264f94808125c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:11:32 +0000

    help between equal clean represent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
