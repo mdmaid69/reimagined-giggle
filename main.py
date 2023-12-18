44adab8bf368a5588a852f7137496f9aa89e0f80
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:34:27 +0000

    second confuse fear pull advanced

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
