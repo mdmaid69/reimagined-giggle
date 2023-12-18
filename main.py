5ffb724228e5dd9a934fd41a1e957c8900bf4361
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:12:09 +0000

    have responsibility rely chief total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
