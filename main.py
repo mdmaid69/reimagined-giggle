e3196e71259ab93ff638b04b16bf7fc5cbed9ae2
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:12:09 +0000

    crowd advice silent connected recognize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
