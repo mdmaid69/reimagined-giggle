bc410d016e986d036de2b916543671e50a823768
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:11:45 +0000

    somehow importance close takeaway far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
