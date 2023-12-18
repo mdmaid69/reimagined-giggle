e681859e82d25a93b450c782b8c8050a857dba7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:38:14 +0000

    bar visit walk best fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
