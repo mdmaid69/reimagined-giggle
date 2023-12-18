e194673f2024aaccc994a0820483b9be08c18959
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:32:16 +0000

    price pretty nature dress understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
