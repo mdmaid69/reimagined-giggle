3070acd00b9b062f0f0c2e89c5cd9f415a01bc3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:51:49 +0000

    upwards let awake such calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
