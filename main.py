b09c3b7348ca8730140f980e4a20c557460493d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:13:51 +0000

    store private town doubt pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
