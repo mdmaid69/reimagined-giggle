d43c28b3bf35be3c96e8ea9536fd0c33a65c002d
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:46:20 +0000

    actually sharp naturally environment threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
