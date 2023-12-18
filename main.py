32a0ecfd28a1f248a7445840d6f30eeace4bfd36
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:41 +0000

    holdup freeze Jj so dry

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
