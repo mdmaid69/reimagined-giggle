8e8dcd256c259035335fd92d648f198679270938
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:21:07 +0000

    art job taste road will

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+print([x**2 for x in range(10)])
