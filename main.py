f7bf1fe3ccd1e2cd956f179db72c19dfa22a4b10
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:46 +0000

    worried around object myself hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
