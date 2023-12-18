1a2dcefbc8baf386fd327e27d82a13d66d1c7b66
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:29 +0000

    break fear shame whistle appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
