c77ce07e0b960cf9c3486dbe417097c562592a92
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:19:26 +0000

    physical persuade young fresh dry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
