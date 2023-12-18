d5f246aed06de8c9dd4dbf080f1121240da0893d
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:03:34 +0000

    peaceful clever whole afford reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
