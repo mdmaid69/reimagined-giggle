d7aacc8ac1d52e95f8263ea9177c00d84a3fe257
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:57:18 +0000

    school protest disappointed happy disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
