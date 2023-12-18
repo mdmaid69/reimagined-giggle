b7da35f72d488b036ee61f88ba99a21bc12e7b84
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:17:42 +0000

    impressed vote quarter decision lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
