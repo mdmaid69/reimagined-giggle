703818fe69b2be3196a3734503ff95fb0a589bca
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:34:09 +0000

    stay this kick laugh cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
