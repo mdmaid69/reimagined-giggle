55f67df2ec1710f61497b7129ba59318f0cdbf38
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:28:42 +0000

    impatient model forgive near dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
