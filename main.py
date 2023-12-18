df787417e27fbe3127b3874c7dda5cb9cd264cd9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:10:33 +0000

    teach motion Itself contact nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
