e7e0babec4943604936b27961d3911ae840648e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:55:51 +0000

    pot concerning cut moral patience

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
