28393c09257154b1f36a06882d94faf140e5978a
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:33:02 +0000

    disturbing tip patient call deserve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def square_number(x):
+        return x**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
