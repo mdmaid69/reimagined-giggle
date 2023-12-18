a0b129d4633e671649b885f8d72b67eaceb7aeb9
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:01:28 +0000

    careless constantly say healthy design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
