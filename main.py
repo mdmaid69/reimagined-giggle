d10bd3df973ba4deda62cca2c210141f8ba46042
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:36:04 +0000

    union claim turnback dull stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
