166e26e2eea7bd8b23c28867c0d03f83e6e05662
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:48:22 +0000

    contact exactly sick sure there

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
