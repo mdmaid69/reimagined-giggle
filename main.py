b39d8eee5ae8de86595cc38c9de1a7a8eaea20a5
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:02:02 +0000

    pause major test answer shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
