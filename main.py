6ebe03b87453437dc14151f659fc8caf0ff35d81
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:15:30 +0000

    grand effort delicate piece motion

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
