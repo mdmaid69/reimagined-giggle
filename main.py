7bca181e42b8ea7d38d00a8dccfc993ed929aaa8
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:51:39 +0000

    paper second doubt condition 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
