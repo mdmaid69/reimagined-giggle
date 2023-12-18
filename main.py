bf6bb7178003eff7a7b640eda941ec07aae407b9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:54:55 +0000

    mystery build impossible rush stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def greet(name):
+        print(f"Hello, {name}!")
