8a27518fc65c192e6994a79d18e989684cd192f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:19:36 +0000

    no close because runover dare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
