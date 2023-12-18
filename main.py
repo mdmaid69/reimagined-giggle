aa99822fc7420bae7c2dc2ddd767066e855bae2f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:54:09 +0000

    previous public suffer anxious maybe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
