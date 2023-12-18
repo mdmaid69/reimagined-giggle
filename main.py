aef7e5bdabfe8cd610de163c939850c2a5ef7947
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:12:47 +0000

    broad Uu right journey describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
