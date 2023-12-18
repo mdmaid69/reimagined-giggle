f3547e15a91d6c6011a71ed8302aa0b7eab7a472
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:10:55 +0000

    announce silent discuss room dream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
