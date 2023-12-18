31dfc76b9b4ec0633b4817da8d55d04b7ebf9191
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:28:49 +0000

    upwards lift line achieve favourite

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
