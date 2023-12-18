346115bf2ee9ef44c2c7ca59e6bab97fe14b8651
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:40:53 +0000

    study careful where rain tax

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def convert_to_binary(n):
+        return bin(n)
