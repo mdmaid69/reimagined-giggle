b462ec7ec4e024ccbe1956d0a67c3620d1e6abde
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:48:33 +0000

    able important pause pronounce square

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def reverse_array(array):
-        array.reverse()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
