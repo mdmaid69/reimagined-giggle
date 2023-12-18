14b7bdad45f910a3b35ae5c58bc445c5c6bd7e2c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:49:47 +0000

    upwards female callat war autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_density(mass, volume):
+        return mass / volume
