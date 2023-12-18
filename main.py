3e425f0cefad0a773ae44e57eae7e49c8e84286c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:29 +0000

    year side introduce smooth rid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
