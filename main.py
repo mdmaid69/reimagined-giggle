402fbf50685e3ad4cdc2ddd5b699923a938a2674
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:12:04 +0000

    lunch lot Nn though workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
