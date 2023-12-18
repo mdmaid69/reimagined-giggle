fdad324ea232a1f249acd7b82eb4ba8f4971c2ce
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:53:30 +0000

    order regret name half business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
