945387f392bc3e2f61e27e42de70b033b7112f8e
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:00:05 +0000

    regular dish course naturally match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
