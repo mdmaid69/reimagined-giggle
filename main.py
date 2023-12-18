6233f99c1323f00740a3644c6ed564d705716a5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:04:25 +0000

    language whistle nonsense buy turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
