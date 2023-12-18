1da7d4aed7bd04431f37e231a4140dfe7c8c67c7
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:42:34 +0000

    soak turnup knowledge back around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
