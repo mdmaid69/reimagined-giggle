3cbebf477940f814b947e6295f2054c04bb46b08
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:08 +0000

    intend arrive realize pick manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
