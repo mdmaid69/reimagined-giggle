de2429dc8fc1bac07eac3138d192eb36786c282d
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:36:17 +0000

    treatment above centimetre fast machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
