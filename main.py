e05157a45572bc71900fdff135f9e49972b2bc58
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:02:15 +0000

    effect concern describe pretend patient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def cube_number(x):
-        return x**3
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
