bb12042e637c7a4961dcefa4e0c555c83ece8ffa
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:07:29 +0000

    success joke Jj extra roll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
