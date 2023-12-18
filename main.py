3432a9970e993e394b5a4fd0e096f78863495d94
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:06:14 +0000

    people middle live close disturbing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
