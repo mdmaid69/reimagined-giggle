c1da5b7a50f79109e265a65f96f48fb460ebede4
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:25:23 +0000

    involve prefer comeout study crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
