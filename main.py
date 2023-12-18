2db554d4144f28bac41ca63e28e849b3fac0dfed
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:31 +0000

    dry borrow proud choice experience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import time
+def get_current_time():
+        return time.time()
