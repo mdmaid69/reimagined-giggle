a3da3e29d541453528eb638f31e268f1d07979d1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:10 +0000

    key feel comedown inquire present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
