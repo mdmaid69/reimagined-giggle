51a554c8be8cd5cc566d88a41995173ebe786266
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:34 +0000

    anxious instruction switch week rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
