bdab7d962ec0fbc4effdb1b18793eb98a92c59fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:27:36 +0000

    lock him taste Ss tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
