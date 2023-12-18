b0d1f8f31851a871118d90696d674c7902ddc0bc
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:20:53 +0000

    exchange friendly event 568energy coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
