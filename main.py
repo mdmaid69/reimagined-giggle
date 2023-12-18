a1633026477515d82e857f81609c7b886f18b77e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:32:29 +0000

    pot wave you cover callout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
