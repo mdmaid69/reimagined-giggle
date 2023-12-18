5ed224e1cce9154bf1c7fbd76606488ad9e22fd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:03:15 +0000

    seriously demand all union tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
