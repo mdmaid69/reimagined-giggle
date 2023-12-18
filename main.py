c76dc339b1c37a026e986562bc7e8fa5e8369b86
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:48:23 +0000

    second mind mend road related

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
