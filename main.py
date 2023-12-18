1e5d4b4cb6d3139ffbce6ca937df1107c3112a13
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:36:13 +0000

    punishment difficulty create bear last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
