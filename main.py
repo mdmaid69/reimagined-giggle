b776ba920289271d200fa0b2f7753f73888e8479
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:08:45 +0000

    emotion normal start turnover sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
