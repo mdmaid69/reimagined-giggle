8034bf980ea0e25950090145679b2e1fc0f71cd2
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:05:20 +0000

    danger human along interview beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_unique_words(sentence):
+        return set(sentence.split())
