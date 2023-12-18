fd976a9ac58fc9c97156d5fc8c797c6f387bc684
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:17 +0000

    complete maybe thing join handle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
