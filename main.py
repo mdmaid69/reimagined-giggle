df3b200cabde504f90f72432f4e7ef579c3024a8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:44:03 +0000

    impossible family on loud approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
