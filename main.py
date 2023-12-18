5dc1dbca49096c628dc0ad4906672d273105da42
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:36:05 +0000

    point hard dance spend performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
