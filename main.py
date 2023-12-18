7f0e8c214612c62c909d2996f3ddea765168dd6c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:29:02 +0000

    worried follow near pretty do

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
