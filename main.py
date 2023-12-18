231649a2e58c614538973e14827290fd72a9a13d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:37:41 +0000

    act someone sing gap unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
