6643afd96132d5b57519a4150ba4e8a511c8eff0
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:43 +0000

    history left should away watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
