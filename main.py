978c9a4d33eef29d802ead75e06ff7212ea6094f
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:00:26 +0000

    aim range shoot left hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
