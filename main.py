c2e0c6c0f6f2b008b18cef8bf4320b48b4eb58fa
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:28:16 +0000

    prevent contrast narrow border lock

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
