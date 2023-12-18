6c7f322f4d4cd8f390479bef86b81c0086683d6d
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:17:03 +0000

    swimming grand discuss unless rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
