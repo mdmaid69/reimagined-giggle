d9fce41ea486f5304461ff114d95a79cc7fd8f6f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:22 +0000

    seat chat content behaviour term

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
