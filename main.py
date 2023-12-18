f0ad413cbcae2acbf62ab81ee9b4a320b500923b
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:45:49 +0000

    work height introduce with light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
