a05da07df51eea3b6ed1ba3374453395702d7c7f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:39 +0000

    only also gentle opposite bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
