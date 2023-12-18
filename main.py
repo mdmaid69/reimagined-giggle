48d92ac6b9daeb4a7954c6cfbeb01c06c0e407b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:24:18 +0000

    cap handle serve holiday loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
