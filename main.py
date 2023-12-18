3a6018dc912c36a244bd5b0c71e45b76c363936d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:32:05 +0000

    turn pause loose please earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
