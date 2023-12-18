57c5159a91529eedeb5f084c9f3a6eb617c969ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:52:19 +0000

    flight illness block both flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
