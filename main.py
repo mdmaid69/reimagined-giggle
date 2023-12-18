bfdc43b1ba365fd8d52ab7fd554366e662dc693f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:22:10 +0000

    judge like this bottom clear

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
