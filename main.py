fe35f965f3fb78291ff83ffeb4e455d22d8ec8ae
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:55:37 +0000

    pretty keepup Kk fit genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
