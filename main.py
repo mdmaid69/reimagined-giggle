1b8addeaed5b8111c039f9a84a0b644299610c0f
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:11:33 +0000

    train damp tone flat pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
