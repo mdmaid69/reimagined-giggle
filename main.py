7cdd17d536f39b2a9992a90da76c052016649226
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:53:37 +0000

    beside warning nearly shop complain

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
