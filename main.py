998c59ac4fc28ab41f7a08f84962270a48796a3f
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:56:50 +0000

    pleased listenfor note throughout thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def remove_from_array(array, item):
+        array.remove(item)
