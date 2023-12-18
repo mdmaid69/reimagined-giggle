25f85a908f845336ec47b3407cd37afb8932476d
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:16:04 +0000

    sorry 566end different like confident

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
