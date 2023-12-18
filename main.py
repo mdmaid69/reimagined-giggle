7e064e90a4913b8131b6e9f318743fd24b800efb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:12:17 +0000

    true actually car secret shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
