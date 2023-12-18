46b3ae832ef890f5d3dd210f1d4ff18d4be6cbb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:53:19 +0000

    detail travel offence people suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
