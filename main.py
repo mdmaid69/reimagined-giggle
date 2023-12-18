fc4df9728a59abac79c3461f0a078d9a951d1eb1
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:38 +0000

    split turnup spare heavy physical

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
