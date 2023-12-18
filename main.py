ad631b64a0a4676e7a7cdc805a745709ab7b2659
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:55:32 +0000

    work phone by disappear nigga

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
