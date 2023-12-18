73df8ab3f813079c34e02084a21238c6479f020c
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:35:20 +0000

    influence man imagination press compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def calculate_circumference_circle(r):
         return 2 * 3.14 * r
-import array
-def get_array_as_bool(array):
-        return bool(array)
