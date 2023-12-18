3810577b5d9ce5992a1f003d52047e8a8552f512
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:57:02 +0000

    type birth treatment juice table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
