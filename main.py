bb8c4334a5bf33f9f3a6d7d012d59f1273bcdd12
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:45:45 +0000

    favourite cope breathe recently can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def append_to_array(array, item):
+        array.append(item)
