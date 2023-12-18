7391810405569e8ca185d7820f1d738567e4bd49
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:52 +0000

    call pair pile round strike

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
