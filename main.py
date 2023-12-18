45bf3187cdbe62c1c089ca60935f4fe08cb82b61
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:39:57 +0000

    hole boring air marry let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
