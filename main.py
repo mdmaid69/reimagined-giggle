8887e125d4164675282e75927b3809aa8ae4fa64
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:44:21 +0000

    niggaacademy hide forget another treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import sys
+  def get_python_version():
+        return sys.version
