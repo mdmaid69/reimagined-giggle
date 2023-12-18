9881f0c4a4b0b73f2541a14e14c1144a265619c1
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:58:55 +0000

    gas people in all draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
