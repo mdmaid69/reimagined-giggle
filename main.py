b16e0d96e3576b4c9594851316c54bceb2774a8c
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:04:23 +0000

    classroom petrol subject match bag

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
