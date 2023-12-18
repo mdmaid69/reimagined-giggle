965b5c9fff61d355ef42a681af50062d836926f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:56 +0000

    slow neither turninto previous tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
