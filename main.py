c1aa37993700a3e54682a682d91f42099d359c02
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:54:20 +0000

    further clean not strike road

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def divide_numbers(x, y):
-        return x / y
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
