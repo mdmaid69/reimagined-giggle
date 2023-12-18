04f89fc6a0acb557058b527c16fb52bdcb4781c9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:22:28 +0000

    mood concerning into event vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
