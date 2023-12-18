cef8bfc68cd197e9475bd5462689dbc433544da9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:03:52 +0000

    business rest sticky transparent welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
