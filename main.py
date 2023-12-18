cb8224a8fa0037bb18d09989c807239588c826cd
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:59:50 +0000

    thick proud anybody breakdown eye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
