ef202d02fab79270cc3eaa25b3884a0f00fc5d31
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:40:14 +0000

    acceptable allow live exercise side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
