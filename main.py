8afc507a3243d28b01a68f83ac717eb41f372b1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:39:17 +0000

    down backwards pass practice minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
