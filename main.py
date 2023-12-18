b672812da068d77b7b049b0660178f1d1778fd3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:55:52 +0000

    burn squash ordinary past convincing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
