59a28eb299cde6a978abcf9dfdba0478f6cb2203
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:52:50 +0000

    than limit standard deaf in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def sort_list(lst):
+        return sorted(lst)
