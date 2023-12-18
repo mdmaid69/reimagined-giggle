a17fdfde728993e66bffeeb2d09e886c4cd9281f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:31:05 +0000

    discuss official succeed result here

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
