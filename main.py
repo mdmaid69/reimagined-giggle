276cb645058fae5cf44770cb0c78438d28a7120a
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:18:55 +0000

    exchange read nice hit declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
