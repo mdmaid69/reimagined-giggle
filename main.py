7a8196d3cd37f15416f6100751691e99738f41f7
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:16:35 +0000

    sign thing within Ll deep

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
