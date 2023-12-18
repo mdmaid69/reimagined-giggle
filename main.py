b570a60093130b290cae919bd6b72e06df8e6c35
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:40:52 +0000

    pen degree plate steer yet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
