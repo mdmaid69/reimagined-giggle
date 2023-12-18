732db4ea8ff4cfc6d19a198b0804ca4c86afc9d5
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:35:06 +0000

    downstairs safety steady measure improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
