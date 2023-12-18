caca58a112ee9c22421bad5d6996c4df14a2cf1f
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:29:10 +0000

    sad world confident break such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
