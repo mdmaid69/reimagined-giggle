581d7132e6e82a96d7028094aa0a58347bc7346b
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:18:57 +0000

    class secret further honest interview

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import tensorflow as tf
+print(tf.__version__)
