e3e1996ca3853a4658b0fb0896acbb915dd4d326
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:19:07 +0000

    appear must of relative responsibility

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import platform
-def get_os_info():
-        return platform.uname()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
