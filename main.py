3433a82686bae89e7dcf4579c4bef854fac47c33
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:48:59 +0000

    getout immediately instant greeting relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
