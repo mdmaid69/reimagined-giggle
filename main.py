ec20e66c80e4f12cf62e0a83a625c3b8c0d7df98
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:09:25 +0000

    priority object anyone interesting switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
