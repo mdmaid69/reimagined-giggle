21995fa62ccc7cafcbedc64a980b88332a5bd823
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:47:27 +0000

    release damage upwards tip maybe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
