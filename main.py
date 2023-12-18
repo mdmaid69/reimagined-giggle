a50b355019b715b4797fde63855442034dd2cf5f
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:38:51 +0000

    mistake quickly surface mess lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import tensorflow as tf
+print(tf.__version__)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
