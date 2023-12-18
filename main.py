4f5137e87039c84bf450bd1a21622b2cafab6580
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:41:21 +0000

    government Ll pride ground maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
