f9482590599afa656c9f6ff9515acc81cfa1b027
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:27:46 +0000

    bringdown rubbish stamp acceptable replace

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
