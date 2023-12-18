e24c0f701af83c9c52fe50d515b10299c021be27
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:45:25 +0000

    disturb flow friend soil invite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
