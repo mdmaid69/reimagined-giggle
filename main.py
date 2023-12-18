9303c194315e760d14beec0841e93a209e4b67dd
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:18:24 +0000

    represent pump fork empty reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
