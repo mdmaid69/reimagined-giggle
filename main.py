6159ac2d2afc51798c4a375db62f02ad62b3ebe8
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:52:30 +0000

    software able communicate some swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
