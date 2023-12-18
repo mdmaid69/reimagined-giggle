8b69965bb73a846f3ace71b8e9489c322994efd6
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:30:02 +0000

    least open month discuss order

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def cube_number(x):
-        return x**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+print(sum(range(10)))
