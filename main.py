a453fa8c37efb20606c6b8b69403f288bfdbc756
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:24:11 +0000

    usual delicate face comefrom marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
