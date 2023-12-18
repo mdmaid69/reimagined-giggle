2731f0b40c2e70104f158923644b9c8c62bf34d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:07:01 +0000

    together birthday determined comedown affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
