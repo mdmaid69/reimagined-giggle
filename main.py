7a29fbaaff70da5a1424954e4419ec0656bddecb
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:17 +0000

    damp below lend fold kill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
+import time
+print(time.time())
