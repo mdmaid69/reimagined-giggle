4a27ff82ab632faebd666e7a601df51df94b4e64
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:10:06 +0000

    creature early wish conversation go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
