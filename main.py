3ee856abc5843ec29bad541d1999c046f1f2007f
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:44:27 +0000

    clear but music win constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
