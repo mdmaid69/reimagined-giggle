15f06ee0b66bb4b2d321fc3f52b33f4b668396ca
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:18 +0000

    block stir recently community join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
