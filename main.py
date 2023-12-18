93e3ced1b2b311ae4a43a3f31049fb69245e1a37
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:46:48 +0000

    base beautiful warn table reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
