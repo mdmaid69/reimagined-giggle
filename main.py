7e882877927206828e80764c0dfa2b811058d042
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:03:48 +0000

    represent Ff before sticky attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
