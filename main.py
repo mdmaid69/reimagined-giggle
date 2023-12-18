55c708111893b595c8f69c1d819f48155b5ff48b
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:58:12 +0000

    comeup though activity bad autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
