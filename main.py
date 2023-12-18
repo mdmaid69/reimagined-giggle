33b8aa35664c18dd7720fee19270da39581dc893
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:15:10 +0000

    sing care swimming curious crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
