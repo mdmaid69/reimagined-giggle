b96c8fe5133b8a9e1d77d31893428d5b67ac321e
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:36 +0000

    ocean whatever already mean confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
