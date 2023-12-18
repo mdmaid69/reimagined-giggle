e64a73e0472ceacd44784f3ce764cc02029ade45
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:21 +0000

    hard score possession practise blind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
