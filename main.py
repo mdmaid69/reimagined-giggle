6fcbac333ab690cd84dc9350fe6a9bb50fd262e5
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:44:29 +0000

    your disturb art base morning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
