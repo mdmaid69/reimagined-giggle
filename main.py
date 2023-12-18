b456489e5b4871847ade4196980b1047d64bd461
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:30:53 +0000

    practical club insist union defend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
