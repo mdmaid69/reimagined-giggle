db3f8ffbf7a166be5e6a3d6ecf4f891d1c4292bd
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:58:58 +0000

    eat sale big reply wipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def print_python_version():
-        print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
