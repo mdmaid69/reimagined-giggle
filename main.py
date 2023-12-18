50445f7be0bbbc4f223d101f38380c8477607de5
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:04:15 +0000

    return house suit custom Vv

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
