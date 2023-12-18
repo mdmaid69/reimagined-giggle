c0a2a5e8477614d93f495de96a21c3831fe76971
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:24:53 +0000

    how 567enemy pity ashamed basic

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+def print_python_version():
+        print(sys.version)
