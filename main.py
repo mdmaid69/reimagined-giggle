c3c6343bf8538623d913c17a2f0cb74793d55345
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:03:49 +0000

    flood his tense water fond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
