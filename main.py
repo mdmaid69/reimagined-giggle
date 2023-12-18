c61d34ff4f4c3ca6575fc39a9a11be46254bc99f
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:00:08 +0000

    letter port city trial dead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
