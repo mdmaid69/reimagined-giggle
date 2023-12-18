a938d9f57bc89077501dd0972dc3909da7685c7e
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:43:42 +0000

    most spend address travel drink

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
