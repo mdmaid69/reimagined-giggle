d06900a6e8f0add90d8faa5801fd80927be42d09
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:17:20 +0000

    sing second slide genuine reply

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
