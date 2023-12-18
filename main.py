3c8cd70d443e4c1d193f2502528c4cac38bb7875
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:23:30 +0000

    disagree sometimes post 565end thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
