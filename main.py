dca8910c6bcce15bbcda71b675080d69a985d8ef
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:16:24 +0000

    complaint bored right book naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
