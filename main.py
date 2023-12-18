26618b7ab9fd161cc7d7ed7a237a7d15d730e08c
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:13:25 +0000

    but sort damage development calloff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
