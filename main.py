d7b35f461fdace179ad3ba9da7fa7b50a6ba690f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:47:01 +0000

    duty Ss steal medium substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
