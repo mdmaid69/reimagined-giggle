3fc450941f4750d79e7079395b862e9a2d0110f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:11:24 +0000

    ring contact carry sign fit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_str(array):
-        return str(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
