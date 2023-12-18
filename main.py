05f725183e164e1a612feaba44fc801c4f9c05ea
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:27 +0000

    fit bush his tip contact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
