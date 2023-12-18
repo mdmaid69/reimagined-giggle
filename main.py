5700cbe7e1330837698163ebaf914e66cc0e95c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:25:57 +0000

    condition music switch sign her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
