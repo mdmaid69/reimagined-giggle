9c381a2e7f2a30a8a3465b4973b48daa644e8763
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:04:48 +0000

    any all with off noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
