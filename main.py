615f99277910b0bee15edc36034eb7a3e6f7d9d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:27:16 +0000

    means suffer secret comment pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
