6988fe31ae5a23cfafba2edf8e52cd9fdb32c1c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:03 +0000

    dull letter relation protection but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
