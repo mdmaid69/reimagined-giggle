d1490645b7ee6f53dbec0cb9c6673339e63dbfdf
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:19:08 +0000

    boil holiday rarely interrupt ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
