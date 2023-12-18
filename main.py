525572a10a3668548a95d0926248107d719cab55
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:34:13 +0000

    style her goodbye final whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
