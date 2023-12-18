21ef2c43668fae1ce83379edad8370117af1552d
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:34:53 +0000

    extremely injury breakfast recognize 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
