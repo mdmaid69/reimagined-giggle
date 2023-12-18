4f137461dfcbd25aa8c232e03b4f620e5ba8d32d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:07:55 +0000

    moral tidy choose further open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
