82e3457ea21962ef7952996d2d77d233d69d9e3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:25:33 +0000

    exploration no 568energy city weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
