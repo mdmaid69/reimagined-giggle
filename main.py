6609d5ffbaa4e98c8d7f466840382c9c0da64ab5
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:49:47 +0000

    after total believe whistle his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
