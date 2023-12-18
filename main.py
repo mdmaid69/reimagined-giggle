59a3069ae86d3246abf9e6a87dc72703d1fe55da
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:53:51 +0000

    boring shame exactly culture correction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import random
+print(random.randint(0, 100))
