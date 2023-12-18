2e86bea2ab4bee6da6cdec5f8c73a16e7a7c072a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:33:30 +0000

    accordingto through finish communicate Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
