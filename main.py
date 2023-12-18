566186d4a7e028dfacd640fb180336925ca57c95
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:19:08 +0000

    save ear when hurry thought

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
