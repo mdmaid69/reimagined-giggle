efd600c3d53665ae296824bc45a97d636099d8e9
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:17:48 +0000

    giveup family uncomfortable smile amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
