9fe8413bd24880ded63ac88e550efac3132ec6f1
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:45:03 +0000

    share detail exist approve 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
