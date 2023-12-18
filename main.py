54b02f3b8f96c538d89616f952a00e96aebe9c68
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:18:59 +0000

    switch criticize club sting physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_current_directory():
-        return os.getcwd()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
