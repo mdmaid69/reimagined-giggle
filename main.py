53fe8790bb886e69b55808059fdedd2c7ea24e3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:26:19 +0000

    rarely hardly eye hear smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
