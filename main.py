f85776be55a8bb4851d055d426f3ffab422e8b3d
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:12 +0000

    rank thank autumn dance getoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-print(sys.version)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
