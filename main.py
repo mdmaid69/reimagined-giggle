8fddb2bbb6d280ed42456e3eb17e5cfd7ee6ad77
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:27:49 +0000

    see answer account event discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
