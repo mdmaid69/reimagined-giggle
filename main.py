207b1897563fd29c5cc2907dfd37302287a4888f
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:18:43 +0000

    hold whistle real doctor almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
