dc0d8910876ad2ac20e0609e996d36c8fe9605a9
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:57:02 +0000

    seem suggest responsibility above play

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
