8870ee3141bd443734b5ef91fddbe274133918c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:12:58 +0000

    care unlikely declare sound runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
