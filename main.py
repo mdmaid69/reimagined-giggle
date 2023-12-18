d64a4bfc35d6da15d326c706bfd2f6ddc99f4845
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:38:24 +0000

    science floor violence hope lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
