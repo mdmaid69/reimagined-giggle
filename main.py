7c14ce5a3823f39b710b6f77766c6d0852304ce2
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:04:02 +0000

    when bang favour record change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def reverse_list(lst):
+        return lst[::-1]
