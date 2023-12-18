2c52c71c66ea3732845961d41fd279f69edafa7c
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:46 +0000

    sing improvement always wealth division

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
