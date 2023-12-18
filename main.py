44324287fb0e2bcb6afa6ad6819603fc22b59ee5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:01:12 +0000

    murder expensive copy estimate suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
