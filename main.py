587b93b1d6b5deff4c04f475497f1f0dbd9f3b5d
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:04:15 +0000

    regular support upstairs suggestion responsible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_min(numbers):
+        return min(numbers)
+import math
+print(math.pi)
