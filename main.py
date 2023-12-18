54ccb4fb7df03025c0fc9b6ae4cd50ea54dab96a
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:39:44 +0000

    area cross keepaway branch stupid

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
