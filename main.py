5760fcbdc95d16401ecf3eec1c3ad2b36d3f2625
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:45:02 +0000

    famous time progress tune bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
