4d9de37cd41699a22c41cea16cf0708ca718ce70
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:05 +0000

    central name help makeup report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_exponential(x):
+        return math.exp(x)
