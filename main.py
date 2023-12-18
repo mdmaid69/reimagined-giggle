482a94e4f47b0006d8a15379560e0c537d4a03bb
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:08:43 +0000

    ought squeeze feature forget ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def is_odd(n):
+        return n % 2 != 0
+import platform
+def get_os_info():
+        return platform.uname()
