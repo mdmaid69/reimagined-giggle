abc0863c1f84aaf9bc6d397c06d308729a57dbfd
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:56:12 +0000

    century usually obvious black about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_density(mass, volume):
+        return mass / volume
