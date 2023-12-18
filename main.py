cbf37e51105597b1d3e5d32755b3edbf694104d6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:55:55 +0000

    complaint oil material I slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
