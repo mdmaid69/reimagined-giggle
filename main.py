1bc0e669c3c1274cbebcc173cefcf40b7a10113a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:06:32 +0000

    change store because intelligent certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_arc_tangent(x):
+        return math.atan(x)
