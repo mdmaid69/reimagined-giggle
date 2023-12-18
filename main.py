c4f8f0b38614fc68efd73f3b83f86e6e03fa1a3c
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:45:21 +0000

    meet permission friend previous like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
