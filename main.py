03bfacdb6db4c0ff5224ec187eb99e95aee8cbd3
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:43:18 +0000

    difficult difficult join strong factory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
