210e665e528f9b8e6e14a5a38c78b6da0b9abf3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 04:44:36 +0000

    activity turn act trade thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import re
 def replace_all_occurrences(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
