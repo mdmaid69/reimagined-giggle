95f1960f4430a47ec7a158f75b86640bf1290c4f
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:40:24 +0000

    lesson over over comein Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
