d53e2a3042c59f2bf9f589e04c1c3411625bedce
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:52:51 +0000

    door Oo talkback importance possession

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
