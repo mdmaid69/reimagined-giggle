b29b5405993632b1188c8b07587a79d5d3cad9bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:54:30 +0000

    collection among delicate praise flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
