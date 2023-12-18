f057c57afe77f59152ce31b67439cad40eefc3b8
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:02 +0000

    silent position getout border badly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
