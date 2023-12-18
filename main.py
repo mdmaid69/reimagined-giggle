5794ab4a19e455524fb8e7e12c7ff82d2dd03eea
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:09:54 +0000

    recent like town statement duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
