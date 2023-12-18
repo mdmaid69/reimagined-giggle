a319b067c18a370e9ff18646204ed1f0e975c3b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:13:35 +0000

    objection plate evening debt thirsty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
