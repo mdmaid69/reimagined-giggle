8701305acd213775d8ad82b9ed398ce83813fb5c
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:13:12 +0000

    kind uniform left education dream

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
