841e1f6d00a2055813c4add66a11316287e99b98
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:05:52 +0000

    stamp eager approve relative mystery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
