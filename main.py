85340ffed751c81c944ae2ee679824d3aae6fee4
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:46:15 +0000

    treat remark match hide rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
