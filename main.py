3729e3623512b401d4304da5c99980741870714a
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:36:49 +0000

    mark marry smell giveup plastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
