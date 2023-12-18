e1d5613f414b72025fa61c3c3adebb92ae47aa41
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:31:20 +0000

    shout slide Ww responsibility know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
