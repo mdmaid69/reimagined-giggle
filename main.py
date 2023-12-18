66008fbbfc5b6cb11770be47d40e70105a3cadbc
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:59 +0000

    drink problem trip popular drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
