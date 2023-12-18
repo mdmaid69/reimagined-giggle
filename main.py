321e12cd4f71e784b73df5ca35dd864e9f4fadec
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:43:48 +0000

    dangerous steam number sticky lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
