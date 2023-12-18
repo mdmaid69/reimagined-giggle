ebd1902691dc6b41bd88c32338959d5f3c3226e7
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:52:06 +0000

    male fact Rr wind frequent

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
