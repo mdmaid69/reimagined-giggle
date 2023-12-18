f10871e530a612df785a79dcd82f69ab660fe279
mdmaid69 <robot@alexsyw.me>
2023-12-18 17:02:20 +0000

    suggestion nowhere meanwhile run fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
