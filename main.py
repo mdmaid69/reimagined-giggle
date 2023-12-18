d2cd13765d5e62ae8c00d7e7ccdb6f0e90874f36
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:21:19 +0000

    lay frequent to ocean clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
