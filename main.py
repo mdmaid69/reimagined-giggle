e616e88156837cd1a2c40855f405089664d7e1fb
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:13:51 +0000

    sign thinkthrough Gg maybe attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+print(os.getcwd())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
