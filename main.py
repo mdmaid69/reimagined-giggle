405d604a8a2aa943f40f37abd4df787adee97b50
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:26:23 +0000

    sort university find blame shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def divide_numbers(x, y):
-        return x / y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
