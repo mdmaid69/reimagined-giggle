e4b002fdbb1ab41aad3f7bb222802fd4e507a43a
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:32:28 +0000

    thinkthrough bringback estimate above free

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
