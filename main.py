7750f5cfca33c44d6b70ae3f4f9fe5a5ce9e982d
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:49:21 +0000

    price whisper pinch familiar plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
