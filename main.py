e0dba917e6e367d8e04d67ac913f96876423836e
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:54:22 +0000

    healthy goout form out part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-name = "Python"
-print("Hello,", name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
