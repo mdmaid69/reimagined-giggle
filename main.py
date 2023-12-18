ba1db5ab7734c96457519abc6e9cdff4fea52cfa
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:42:03 +0000

    upset little be sleep ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def get_array_typecode(array):
+        return array.typecode
