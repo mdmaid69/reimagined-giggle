bad39c2f8572d5d721e800d16c4980d17e77855d
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:51:13 +0000

    talkinto food breakout closed shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
