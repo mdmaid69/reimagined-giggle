aa209d428d3c0d3acf58bdd7ffbdaac5dbc1a716
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:50:25 +0000

    blame interesting autumn that poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_current_date():
+        return datetime.date.today()
