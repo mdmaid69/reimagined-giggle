8a15592b25d0118373ad9814f36f33972899bff2
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:43:19 +0000

    butter beginning tell hardly study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
