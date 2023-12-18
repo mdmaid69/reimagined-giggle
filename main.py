18aae5d2a3415a505645b622ad89ce1e588476c3
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:58:36 +0000

    church relax steer weekend remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
