7987f3756ce19253ca68a7c12dfdf3378e60b328
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:14:06 +0000

    distant open difference out serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
