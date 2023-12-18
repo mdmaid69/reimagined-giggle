bdb42631eb0cd1d9130aa8631aee3c17ef9a54bf
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:58:20 +0000

    minority reply accommodation tense instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
