907521cac69c04693e27676c118d2b4d237f9218
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:35:30 +0000

    turnaway mysterious underneath study strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import tensorflow as tf
+print(tf.__version__)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
