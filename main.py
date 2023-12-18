1a47690e0266db0fb3686dcbaef2d85be334ad23
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:22:48 +0000

    brush summer in zero among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def count_characters(sentence):
+        return len(sentence)
