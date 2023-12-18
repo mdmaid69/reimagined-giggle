9d32d35abb5dbb75e9ea557942ed4e77f206cdb4
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:42:26 +0000

    around tip scale still club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
