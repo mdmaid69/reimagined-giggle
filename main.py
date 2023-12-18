e94b1d7ae40081ce1efc224ce2646faf7d45f5c6
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:42:53 +0000

    praise report unless otherwise conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
