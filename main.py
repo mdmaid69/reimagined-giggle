55f6c991e8731e5aef06f4c707809b3c7f4c8b30
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:26:38 +0000

    where office command pour wish

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
