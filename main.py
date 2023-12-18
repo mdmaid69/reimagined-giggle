677b0ba2c197d76c741c9bb91ea6930fdbeb28c2
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:26:44 +0000

    sport yesterday pause excuse black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
