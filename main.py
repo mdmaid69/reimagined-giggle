f0eaf1fcd67960841ced957da8db604e3b4e0eb6
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:24:06 +0000

    drop turnup previous like public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def square_number(x):
+        return x**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
