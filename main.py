cc5104ba29f1e0e9e0c8f43d3812cb1c7458e8b5
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:30:42 +0000

    shoot opposite tough spray hot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
