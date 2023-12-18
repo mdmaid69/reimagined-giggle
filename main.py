cc37477da882dc904b2d3a823e58dd25fcbe2d33
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:21:31 +0000

    obtain family car display ticket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+print([x**2 for x in range(10)])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
