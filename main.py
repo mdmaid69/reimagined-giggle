08762eec19298450ba8de74bca395c235038c164
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:14:28 +0000

    question production slice busy divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
