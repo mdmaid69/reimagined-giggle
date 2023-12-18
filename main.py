31157cb911ec83df51e8d12d783fadc6c2329ca7
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:38:44 +0000

    type dry feeling provide sea

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
