06dcb5404a45a014fc198e2a4c169357cfcbb8cf
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:41:52 +0000

    explanation final see nothing advise

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
