6c0e47d8186b5a7c08bfdd383d84e4d80b9d1294
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:45:58 +0000

    precise here scrap shape track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
