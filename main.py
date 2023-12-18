a226fe98dc3a6c21f5b4402b833b5c18aba69a80
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:28:46 +0000

    crowd pile confidence love light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def count_characters(sentence):
-        return len(sentence)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+print(sys.version)
