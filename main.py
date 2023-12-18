3f2e82bcc934b421e55761f2c81e68484ee8cec0
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:00:24 +0000

    report clear him will crowded

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
