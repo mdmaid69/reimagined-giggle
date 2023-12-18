812acadf9044f6c13fa42837fd39cbc7e450e983
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:17:31 +0000

    like turn statement Pp willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import time
+def get_current_time():
+        return time.ctime()
+import time
+print(time.time())
