89871e5aa36a84625693d6b1c2ef2317f1d08003
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:21:59 +0000

    short of evidence any card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
