e60de05567a4e79412f16336360fe8b6b4a8eee2
mdmaid69 <robot@alexsyw.me>
2023-12-18 21:56:46 +0000

    friend polite confused excited tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+def get_current_time():
+        return time.ctime()
