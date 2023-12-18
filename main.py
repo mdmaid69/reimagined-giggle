ac6ee88860879b0f7f9cb75fd96d7b4748979c9f
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:52:36 +0000

    the takeover Nn print apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
