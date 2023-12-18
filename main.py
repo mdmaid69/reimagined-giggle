9f243a98ecccf377717ac481336c81a686ec31d3
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:33:18 +0000

    relative flat speak pride smooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import collections
-def create_queue():
-        return collections.deque()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
