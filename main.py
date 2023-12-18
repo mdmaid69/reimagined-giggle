e4e42a01c19feaf78afdebe4245eb69263a9990b
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:49 +0000

    rely circle shock claim welcome

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
