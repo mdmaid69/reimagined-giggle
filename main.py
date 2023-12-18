113ef68fdc825e60a00ba1a4240984a8951b77c8
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:49:22 +0000

    through unpleasant read joke injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area(radius):
+        return 3.14 * radius * radius
