90ee60d58e4a7acf2e756826855b827e9007884c
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:04:15 +0000

    own neither minority how soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
