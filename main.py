5706792317ca29c5a365236a0dcceed9b6989c3b
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:24:55 +0000

    priority provide congratulate maximum lay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def sort_list(lst):
+        return sorted(lst)
