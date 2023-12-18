91c71ebad64d36fe60e6cb3f56e2d88be4dc75c5
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:57:02 +0000

    community create 571enough watch spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
