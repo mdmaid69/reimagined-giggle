2358011c07d16d4e2b7f10d95c0f67d46e379aa0
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:55:44 +0000

    by sure increase joke support

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
