026293f23379fc234d08ea1bcd85b027d921e350
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:39:53 +0000

    better consider reasonable develop detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
