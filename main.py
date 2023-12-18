f1d51f03bef3f0da09266a991b2dd94fe0a08b8a
mdmaid69 <robot@alexsyw.me>
2023-12-18 14:02:08 +0000

    yesterday rely exchange immediate match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
