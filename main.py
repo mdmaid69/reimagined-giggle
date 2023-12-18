f6c1af9879466644a9d4514192a7b9d4f244776a
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:16:11 +0000

    agree pleased increase type mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
