8053e9d3e5d45dbb65e2424e32a4c68dfb2b832f
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:01:35 +0000

    brush move accordingto keepoff hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
