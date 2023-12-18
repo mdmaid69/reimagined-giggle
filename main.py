bd42dafc5c00d5fe436aadb4148d652f15dde916
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:30:38 +0000

    birthday port behave break theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
