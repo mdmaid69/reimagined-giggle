781d14923a71cf69ee344c5d3784aacecf1c3b95
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:29:14 +0000

    either breakthrough lead by rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
