6dfcb7268cf0ecc1b7dd933ba42d41d066377179
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:37:30 +0000

    tax pile pattern basic brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def cube_number(x):
-        return x**3
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
