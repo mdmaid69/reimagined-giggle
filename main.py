7ddbf7f429367ac9718c55132b467d53cb0489f4
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:28:51 +0000

    think pick quality turn near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
