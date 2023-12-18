e294d93eaa1eed2f5c6df39e37f637b9922afea6
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:04:32 +0000

    trial murder violence class Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def divide_numbers(x, y):
+        return x / y
