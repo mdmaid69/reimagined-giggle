bc3c8063f1052816ba73582aaf972b33db50060b
mdmaid69 <robot@alexsyw.me>
2023-12-18 08:46:03 +0000

    plant tool makeup face top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
