f06800f1015acdd45dc6a0eb157e56db9043fea9
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:32:41 +0000

    appear complete question ill sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
