1596b9a2e325a820260d9812e0ae2545742066e3
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:51:44 +0000

    thing anyone before depend train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
