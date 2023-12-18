20489e05d0dd3a15b7864569dd10dbe31fc7b234
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:04:47 +0000

    attempt use cause backwards sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
