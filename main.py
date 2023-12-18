39c8b178369d970c59c9338cc8afe79d46ba3faf
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:55:25 +0000

    pump size act holiday accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
