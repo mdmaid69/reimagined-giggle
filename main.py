e97bdc6c3f258873c8878ba5f2b836da0ef40e01
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:41:20 +0000

    follow pump art hide new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
