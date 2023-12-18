12d50e17bc89d7dc1a4d50773d81dbf2d9b35572
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:36:30 +0000

    fit spill intend crisis ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
