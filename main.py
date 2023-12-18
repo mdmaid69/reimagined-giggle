3c4661948194688910f6c23c7da96105fcc8fcc9
mdmaid69 <robot@alexsyw.me>
2023-12-18 12:29:46 +0000

    violent unkind hard forgive prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def convert_to_octal(n):
+        return oct(n)
