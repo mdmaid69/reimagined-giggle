6f61a9d177d5a7b2240ec29a3a0ca83e45dbd733
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:20:12 +0000

    Mm past sink film load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
