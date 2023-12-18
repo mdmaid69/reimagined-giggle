5ee3a3a357e7e442ab39259096d18ca9aa0123f9
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:12:11 +0000

    since little tape swallow violence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
