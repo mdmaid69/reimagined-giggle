886f1d0aa99497cd11e0bbd2b6dbc7d6c8c3a221
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:26:32 +0000

    spray question little exaggerate meanwhile

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def is_prime(n):
         if n % i == 0:
                 return False
         return True
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
