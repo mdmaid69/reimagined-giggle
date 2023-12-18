8292f3d638a97532a4f2e736d585abe25a245333
mdmaid69 <robot@alexsyw.me>
2023-12-18 03:25:31 +0000

    sting delicate store our anyone

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def create_stack():
-        return collections.deque()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def add_numbers(a, b):
+        return a + b
