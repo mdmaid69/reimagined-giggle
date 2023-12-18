8bda9905126564fbc5d6af3d5b2859ad4feef89f
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:15:46 +0000

    age arm national shallow stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
