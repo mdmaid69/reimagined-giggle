a37e6a8d2490a2daa9d9cac0dd40e08d1896c6b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 05:01:12 +0000

    reduce spray shine aware well

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   def is_odd(n):
         return n % 2 != 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
