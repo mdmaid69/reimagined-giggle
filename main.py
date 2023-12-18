47edbb01beaa8f92cab812b242d792145bf421b4
mdmaid69 <robot@alexsyw.me>
2023-12-18 06:23:47 +0000

    expect Yy taste furniture free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
