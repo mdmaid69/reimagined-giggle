4a32b4ec38dc2ba950ddfaacba9c5a928c01117b
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:00:08 +0000

    offence common bar pattern save

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-name = "Python"
-print("Hello,", name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
