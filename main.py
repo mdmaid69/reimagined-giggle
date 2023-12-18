6423841254f01a4d9a9bb117b7d42c2556b78921
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:55:37 +0000

    unfortunate come children professional move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
