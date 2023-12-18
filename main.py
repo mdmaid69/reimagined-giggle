8d21a45f772b0319f31efba8f38037613cb99be1
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:27:54 +0000

    letter phone wet gate slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
