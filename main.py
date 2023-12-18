bd204f8df8d4afe45ea93be47e73e594dcbdb13d
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:44:18 +0000

    nowhere bath plain trade illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
+import array
+def convert_array_to_list(array):
+        return array.tolist()
