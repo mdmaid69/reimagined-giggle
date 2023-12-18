2eb7dccf74c684013330559dceec315b5c2f008f
mdmaid69 <robot@alexsyw.me>
2023-12-18 13:45:21 +0000

    exercise reply cap natural path

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_user_dict():
+        return collections.UserDict()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
