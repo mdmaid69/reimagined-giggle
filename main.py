9bd515715d246988fbf6052557faa0ab923bc119
mdmaid69 <robot@alexsyw.me>
2023-12-18 22:40:25 +0000

    charge obvious over somewhere error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
