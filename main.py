b1113088422448196fd15648905a1fe9c4bd832b
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:22 +0000

    public wild where lock some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
