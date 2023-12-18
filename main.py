bd8c049f96071425329696ef0d881794f7af56b1
mdmaid69 <robot@alexsyw.me>
2023-12-18 10:10:20 +0000

    exact best willing fail look

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
