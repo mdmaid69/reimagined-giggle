5bde9192444212937293730cb12a579004cdc645
mdmaid69 <robot@alexsyw.me>
2023-12-18 00:49:49 +0000

    Ll sky whisper between slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
