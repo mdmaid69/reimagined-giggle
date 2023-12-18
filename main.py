d6f6ee8c5f9753939422377bf65f6019bea86a00
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:06:43 +0000

    noise passage your guess whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_item(array, i):
+        return array[i]
