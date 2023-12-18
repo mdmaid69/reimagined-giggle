4a7ec5a998a4397522e12bd6017c47722bfefd55
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:01:43 +0000

    giveaway bit base properly properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
