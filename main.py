80e90da225f86a62ed75e40da269e7b6b244dd60
mdmaid69 <robot@alexsyw.me>
2023-12-18 11:42:34 +0000

    whether serious actually unless fun

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
