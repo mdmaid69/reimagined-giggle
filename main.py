2bb559e7ed3c686b7e4703f866d5ef8abf472b89
mdmaid69 <robot@alexsyw.me>
2023-12-18 20:07:07 +0000

    lay might murder medium record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import sys
+  def get_python_version():
+        return sys.version
