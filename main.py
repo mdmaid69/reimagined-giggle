e3a165380e820d02b38be0834f9b922a5f040b57
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:35:40 +0000

    pot punish extremely somehow height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def count_elements(lst):
+        return len(lst)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
