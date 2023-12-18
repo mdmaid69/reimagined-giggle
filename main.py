06cbe8e2963362e5f4bd1da424da2462e53405da
mdmaid69 <robot@alexsyw.me>
2023-12-18 23:25:51 +0000

    succeed of likely disapprove estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
