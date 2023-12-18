7b13af4773efca74876f8ddd11b69efac06ce444
mdmaid69 <robot@alexsyw.me>
2023-12-18 19:14:28 +0000

    point unlucky straight properly order

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def reverse_list(lst):
+        return lst[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
