34720bc55d322bdd644b21b8b47897bc9534ea66
mdmaid69 <robot@alexsyw.me>
2023-12-18 02:27:07 +0000

    different complicated loss foot whenever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
