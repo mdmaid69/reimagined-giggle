5823bac54d68eb797e5e351bef59171d31eb2a21
mdmaid69 <robot@alexsyw.me>
2023-12-18 16:48:36 +0000

    secret schedule this string party

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
