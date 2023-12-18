26084efd2a327c911a82f21cad075cf83bbf9d87
mdmaid69 <robot@alexsyw.me>
2023-12-18 18:33:19 +0000

    smart check drop brown upwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_work(force, distance):
+        return force * distance
