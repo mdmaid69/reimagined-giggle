edae558363114724326db5ba2e198a843a7fe4cc
mdmaid69 <robot@alexsyw.me>
2023-12-18 07:45:30 +0000

    health think share excellent once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
