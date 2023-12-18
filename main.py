978d99f5184248e4c7dac7b52d8547d8e4dc7ff3
mdmaid69 <robot@alexsyw.me>
2023-12-18 09:41:37 +0000

    except walk runout error man

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area_circle(r):
+        return 3.14 * r**2
