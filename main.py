97b2e557caac3d7b89108a178b2180f093ee8a0a
mdmaid69 <robot@alexsyw.me>
2023-12-18 01:50:16 +0000

    increase forget operation aim state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
