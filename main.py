f65f1db553609cb7a43527bfb9ea707d6cc29728
mdmaid69 <robot@alexsyw.me>
2023-12-18 15:45:21 +0000

    bringback class afternoon see around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
