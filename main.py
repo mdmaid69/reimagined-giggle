848765e5a20f9a53fbc34b60f019571fa710b970
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:50 +0000

    serious wing communicate wonder gentle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
