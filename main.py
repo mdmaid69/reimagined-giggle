561a557bb20c4cbd02c06c3bbeab34372f0d174f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:48 +0000

    train quarter success lucky square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
