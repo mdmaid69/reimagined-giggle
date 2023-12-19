90cbee040e76ffe25ca81283b9c71e327ff330e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:45 +0000

    close violent come gentle interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
