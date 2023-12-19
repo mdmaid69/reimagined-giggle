78bfa966f3abe42f8457e5ce54551506d0b0d885
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:05 +0000

    admit crime belief claim kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
