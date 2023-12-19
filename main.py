902819325982bea28e194336329bd857612de06e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:23 +0000

    spring welcome sense statement demand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-print(sum(range(10)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_volume(length, width, height):
+        return length * width * height
