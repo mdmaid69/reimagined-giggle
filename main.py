373a7ee6c8b0fbbfe6381dcdc2c43e62c4902d1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:36 +0000

    statement ocean fly down dream

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
