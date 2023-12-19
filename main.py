e3835f94f9ee27a2aa79244cea624ef737473a87
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:46 +0000

    walk account destroy there because

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
