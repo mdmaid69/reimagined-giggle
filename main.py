3d63b3bfe7a1bb24d2aaa935b23d46071b9dc6b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:26:59 +0000

    problem damage whole street love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
