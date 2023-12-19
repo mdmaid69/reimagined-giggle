d9ad8cadbbebcb523e8b08b16eadb5b453490a64
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:32:24 +0000

    willing shrink agree similar steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
