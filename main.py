936aca335bfd0b5e075f2614ce3df5228c420c1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:45 +0000

    priority urgent tie trust public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
