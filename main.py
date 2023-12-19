a087401a55bae6be8417a24f936c2176a9a84041
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:16 +0000

    he meet keepoff quickly level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
