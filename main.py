b836dbc555db6609f7bd5304f05cf6d9df35aa70
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:04 +0000

    label our boring car finish

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
