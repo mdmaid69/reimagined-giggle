ec438943f40a6cea125a4aa3abe9aa99684b26ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:53 +0000

    ever send afraid number backwards

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
