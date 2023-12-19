0323cc13a34a20b3494ab70164f5d18d1f3919ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:56 +0000

    each little tough brief excellent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
