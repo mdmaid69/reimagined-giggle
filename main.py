a81350d8955d0f2a3a65d99e5600ce2dbb3df47a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:47 +0000

    rise quantity yesterday conduct recover

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
