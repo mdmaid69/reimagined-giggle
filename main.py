2348f96818c74679826604d359ffd3e85ca72d57
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:01 +0000

    manage strange hers full along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def greet(name):
+        print(f"Hello, {name}!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
