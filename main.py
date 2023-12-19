198a5f6e0400818aae821841c4ea3455149cd94e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:42 +0000

    Bb grade sight like perhaps

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
