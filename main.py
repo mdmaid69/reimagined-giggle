3b3c6693c382926d7141161a54f3adff23e107c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:51 +0000

    recovery disappear error cut insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import sys
+print(sys.version)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
