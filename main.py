b07359f1ccfcf12b4e31ff1e8f87b3e8d7213dd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:06 +0000

    welcome late pass crowded service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
