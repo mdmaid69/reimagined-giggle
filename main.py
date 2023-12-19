a189f8737527261611421af598463f04d0e770e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:11:07 +0000

    see spread activity accidentally swim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
