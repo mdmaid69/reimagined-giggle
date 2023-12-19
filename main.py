fc964dfe70f2440494ba5084e62c9d0e99f55afe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:50 +0000

    violent difficult warm quantity exercise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+print("Union:", set(list1) | set(list2))
