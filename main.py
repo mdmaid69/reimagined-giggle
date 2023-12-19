b90b8999cde06b3606a84cbe73f84d2ad07319e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:28 +0000

    regard visit absolutely final hungry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
