6ebe9c222153c73d9f8c83ea48d7ebfaa56dffe8
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:51 +0000

    female collect plastic although record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
