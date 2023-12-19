a34a1d85936f362078c1f6563bbeccf66ef9225f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:07 +0000

    mend lean doubt protect mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
