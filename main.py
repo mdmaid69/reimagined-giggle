d255956ff3ef53323eeecd3a98f003a06071faa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:40 +0000

    Pp 566end health rid makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def add_numbers(x, y):
+        return x + y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
