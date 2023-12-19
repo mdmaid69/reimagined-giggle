0ffe4bf9a6638c7cffd91efbf687eec127e5aa53
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:11 +0000

    name impressed talk 572enter win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
