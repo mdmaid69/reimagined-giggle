897952f23edcf0ff6b46aaf8a453d95924b8b0aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:55 +0000

    period trick cry cry church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
