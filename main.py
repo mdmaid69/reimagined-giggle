3b66aae6cb6587c50e65532b2daae706b630de5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:14:46 +0000

    wide cheque anywhere Mm professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
