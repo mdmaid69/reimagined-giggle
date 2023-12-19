b99595d8aa849120099abd7e6f6de68cb0372a73
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:19 +0000

    vote excellent hurt spin side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
