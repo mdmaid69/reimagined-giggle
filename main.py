172e9b142372e76e5bf53bf70aa3399ca099f27b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:24:24 +0000

    label wall whenever save knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
