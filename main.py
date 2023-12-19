f70fd2622fe52bc37c4c5ef0c7b4f18098e5eda7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:54 +0000

    authority last branch ill copy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
