056c96cba094363184dd08d0d2ce83fb1c2e05b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:57:14 +0000

    safety takeaway reach listen married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
