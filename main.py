4652111f37a4b168ab13a2a9d6c529e8cfe47bc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:08 +0000

    container 179bite least ready telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
