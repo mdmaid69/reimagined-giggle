cc51a9a8f0e41a76cc6479d5607d2cfc5ec60992
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:10 +0000

    load tax day delight clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
