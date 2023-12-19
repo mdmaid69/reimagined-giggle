268efe62437ae3cc654598097c55ef08e8719bd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:37 +0000

    butter medium build fear confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
