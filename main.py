e014c4140cc8811b010f19b259389aacfc59b874
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:29:50 +0000

    cap trick tooth breakdown frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
