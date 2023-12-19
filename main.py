cc9f38909ee0f11e36e63cf35f7b557dffba3dce
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:21 +0000

    scream visit legal life wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+print(math.pi)
