5c39da3890b79e2e67d49cc06e8b957566fa62f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:59 +0000

    thing basically day average favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
