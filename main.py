4ac9c0e430cf275a5a28820ec29a8e2994c7ade3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:13 +0000

    both discuss traffic busy environment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
