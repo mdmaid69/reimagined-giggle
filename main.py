2e4d8e5c56974f54e23bfabbace88b49cd7eca34
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:51 +0000

    convincing wide suit move Jj

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_power(work, time):
-        return work / time
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
