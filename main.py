5bb6784a6d01e504085cc431b9b858df353fffe7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:35 +0000

    see stamp concerned freeze common

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def add_numbers(x, y):
-        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
