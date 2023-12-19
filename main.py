a7b268613945499fbebb20d263d4cc76dbb7e75a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:47 +0000

    both great once female backwards

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
