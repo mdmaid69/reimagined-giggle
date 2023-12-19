890c994151f477a7cd0ee5311b7801df9daa4e42
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:02 +0000

    satisfactory chance switch distant innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
