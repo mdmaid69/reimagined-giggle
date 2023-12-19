237553ea843e9ac23dd07aaf4808c584ad77d627
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:32 +0000

    how takeout justice take report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
