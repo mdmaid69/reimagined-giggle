555b98a6b929dfb76eda4bdc0562acdade9fed27
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:25 +0000

    step might several damage communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
