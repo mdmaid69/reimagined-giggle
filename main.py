3e87f1dee68b62d0b5588e742eb4258016aa7477
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:35 +0000

    or someone aware original getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
