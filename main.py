eac393a6c666ec1635bff83547480e5fb55996b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:20:04 +0000

    mix clear suggest they tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
