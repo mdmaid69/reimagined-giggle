46dc22c5ad37d77e73213149ea6e703e4ee90fb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:30 +0000

    yes now turnoff worst that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
