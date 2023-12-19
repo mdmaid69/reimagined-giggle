e70b918f31a46e3f43e0f11005e5d8e37f41ef3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:18:33 +0000

    prepare stay exaggerate book that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
