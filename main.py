392cccf46e152dd12da5d3a7afb6b6e15687f685
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:25 +0000

    justice high count tip true

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
