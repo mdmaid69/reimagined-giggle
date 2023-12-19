4e480651e2d6aad930444448fc5fabe07b6de1c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:30:57 +0000

    back imagination deserve spray calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
