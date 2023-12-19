2f13f21aed5ee047bc31cb5d068cd6bd8c0f66a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:07 +0000

    late bored impatient brown proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
