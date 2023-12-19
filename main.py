4fbc1270b9fc7a3030f360b395dd770e3746f763
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:44 +0000

    fail region tour interview then

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_acceleration(speed, time):
+        return speed / time
