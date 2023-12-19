4ff2f434241aaaa71ecac3495a2e15f4d09c1db3
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:53:37 +0000

    undo mouth head room wise

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
