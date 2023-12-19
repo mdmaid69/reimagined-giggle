3b3429be94cd6dc5b3cebc8b6fb9e05f402e05ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:38 +0000

    earn compare jump runinto great

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
