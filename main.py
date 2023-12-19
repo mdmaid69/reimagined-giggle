cd248f584a3980da98d9c48e4b5eea4e72eb2e6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:31:46 +0000

    force prove record offence talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
