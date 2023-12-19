f77a75a37c9cce40b1ef83bc2046271bcf47070c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:43 +0000

    proud hope lot kind joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def convert_to_binary(n):
-        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_list_from_array(array):
+        return array.tolist()
