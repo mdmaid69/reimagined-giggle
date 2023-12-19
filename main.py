d46d8e20d46f3a5bb316b05185bde6e8d28c741b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:17 +0000

    valuable innocent physical promise achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_array_to_string(array):
+        return array.tostring()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
