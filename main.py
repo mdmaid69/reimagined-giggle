1279498d87ddf6fe05602798fbd03558a8853775
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:33 +0000

    event think fix himself correction

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
