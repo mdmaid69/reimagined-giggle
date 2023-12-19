4087f9a0fdac592bca93e795ac88fa40b3cc3864
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:26 +0000

    bush active swim reliable earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def convert_to_binary(n):
-        return bin(n)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
