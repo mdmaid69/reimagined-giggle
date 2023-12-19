2a395496baf311160c7f0b6e12d7209858629999
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:26:51 +0000

    importance pull either education please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
