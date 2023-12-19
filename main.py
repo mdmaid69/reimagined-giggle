8bbac02ba26b7ab6771620f238a3329bff3e1314
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:55:39 +0000

    Bb refer stop wash union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
