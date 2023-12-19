040e035c4064b8dac4af11a6b58e03975cb30550
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:19:22 +0000

    government call pattern destroy age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
