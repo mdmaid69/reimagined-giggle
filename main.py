2977112d007af7dceb1c7844a50f463d177a6aac
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:22 +0000

    building complain accept advise service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
