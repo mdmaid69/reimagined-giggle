0f9c5b7f32919a51fb9b25ab932d64cc2d6e3349
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:32 +0000

    neat contents capital fork perfect

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
