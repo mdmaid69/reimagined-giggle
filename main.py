d29a07df7d1774bb2e0bb0aceb396d39122b4c92
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:42:20 +0000

    related there search pupil next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
