5a5b61c9b9510d9a1f9ba254648f51553432f346
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:42 +0000

    skill order recognize nervous convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
