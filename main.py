fd4320eebb9967ecebfe8b113ac729412693b8cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:45 +0000

    hot clearly whether uncertain learn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-x = 10
-y = 20
-print("Sum:", x + y)
+def add_numbers(a, b):
+        return a + b
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
