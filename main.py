dea149bd4544410aa2295977bfe0e7e947add766
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:08 +0000

    confidence most climb murder hello

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
