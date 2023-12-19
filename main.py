64a6fc6e8505d40690e5cf69726782efa709fd50
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:01 +0000

    floor view off finger appeal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
