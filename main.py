34e7ddd4116a5c1241f51c1ac04c8b9b55cd07e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:10 +0000

    creature wonder objection know broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def is_even(n):
+        return n % 2 == 0
+def count_elements(lst):
+        return len(lst)
