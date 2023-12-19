76765e4acc708fa888104f07a86c3275832c6f9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:08 +0000

    reject grand Mm teach fortunately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
