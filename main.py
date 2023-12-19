88a91d92b1fa2892bebe9b1333c36011de942538
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:56 +0000

    Rr beat ticket sign left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
