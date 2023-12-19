7a6f2c86b40128a2601ef2768141f6443ea7bdf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:41 +0000

    lock hit behind base worst

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
