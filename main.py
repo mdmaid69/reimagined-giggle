5d18c3c520f30c2f51fab4e7c87493dc248a7cd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:18 +0000

    mess power fair voice afraid

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
