d982310df88d9b641787bd85666281083546e694
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:16 +0000

    sensible turndown period brief exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
