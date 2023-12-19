8dfd78387c45a5f901396b826a05cae7d9966875
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:49:34 +0000

    content complicated character Gg brush

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import numpy as np
-print(np.array([1, 2, 3]))
+  def subtract_numbers(x, y):
+        return x - y
+def divide_numbers(x, y):
+        return x / y
