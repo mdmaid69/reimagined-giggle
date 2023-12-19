1a28c5e7e2d6fb0e2f14fd7c5faa04544ebc8d83
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:58:03 +0000

    Hh sound Tt general disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def convert_to_binary(n):
-        return bin(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
