32e570b29daf961a37dcc1b527005524cb240251
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:38:17 +0000

    curious sleep concern somebody go

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
