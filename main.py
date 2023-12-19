94d16aeb84d944b3ec95fe6d9d2e7cb86f98617c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:21 +0000

    watch lie address negative along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
