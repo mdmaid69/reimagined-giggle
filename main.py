b4b5ea607189b37d4e01ce93414112323b9e3f32
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:28 +0000

    knowledge deliberate square bush team

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
