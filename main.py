91b0762317bccb91c5d78207f45edef176fb8e92
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:40 +0000

    schedule concerning admit phone embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
