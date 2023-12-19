5f00864e30e5345e3ae995a84c18f648e2c28170
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:56 +0000

    thin what yes soft where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
