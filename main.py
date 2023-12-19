54ae4c08afc8479f751b7f81619bc04a0bf1cf32
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:19:54 +0000

    soft air official hard sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
