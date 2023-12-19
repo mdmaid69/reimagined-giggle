f2b600ff3a5ee38f59034c89d42977b6ddbcb903
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:45:49 +0000

    keepfrom mistake appeal few treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
