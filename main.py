7b5711bfd739ccd6031a68e59e9a0fe47bd549a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:38 +0000

    transparent way check silent lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
