584bcd14999281ba204e9f844877966a5412509e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:11 +0000

    knowledge Jj corner frightened embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
