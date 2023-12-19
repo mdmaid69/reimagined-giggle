8dbade0fc4304cf32a9f174e4bd26fdc0cd4ec3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:53 +0000

    content dare authority form command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
