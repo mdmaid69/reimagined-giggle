023adb8f844471771c860ae5d5f69a8ec51fc248
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:52:27 +0000

    stupid offence more experience in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
