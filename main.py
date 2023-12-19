2d0c4c2d7c028f7a872c68f2c19d67c76e275829
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:42 +0000

    record computer rather smile crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
