90a47a3105a62c644569c1bfee8df8811f15a79b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:44:44 +0000

    unite compare winter thing switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-text = "Hello, world!"
-print("Characters:", len(text))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
