d1df30a2b230f627c9edfea1425c0b7fbeb689f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:59:39 +0000

    empty file talk mention sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
