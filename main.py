39261cd6b88f960df79ef97bd7177aa09386b0ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:25 +0000

    ride often brush scrap contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
