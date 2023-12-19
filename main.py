800503fdd041fe527fe629aa40680e24cfdbfff0
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:51 +0000

    after decision thank ride crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def square_number(x):
+        return x**2
