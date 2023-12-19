bf978d84ec5493fab5dd38b2f48a839cf8440ff8
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:59 +0000

    agree turnoff something consist like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
