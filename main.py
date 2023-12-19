9f49fe6be63d978c59c98971658cc506932d760c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:45:36 +0000

    key blame impatient date there

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
