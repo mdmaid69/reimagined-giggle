96007863ee6fb9ff4aa2b958d6dfafbee8b12de0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:05:57 +0000

    game instruction discuss heat sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
