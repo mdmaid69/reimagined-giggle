c80172be7afc9ffa53f30344490289722277a07d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:21 +0000

    pleased sweep bite vision supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
