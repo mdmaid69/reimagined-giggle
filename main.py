628ed786947330e7314c8e4a88193047f25440c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:38:27 +0000

    kind bit brain lend separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
