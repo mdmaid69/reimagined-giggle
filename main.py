f8b51f261a66ed612c16b86aa278b05bd747ce02
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:56 +0000

    exercise recent he put try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
