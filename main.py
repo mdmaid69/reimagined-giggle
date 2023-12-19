52dc4b095a7794696dba79bf3bf17518e8c24bb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:21:03 +0000

    snow list instrument period further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
