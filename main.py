183af8a24faf03fb98121fab57d4dfb57763228c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:11 +0000

    around happy hurt ear priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
