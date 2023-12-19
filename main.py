716c90b523ee1a629b621239b7d1e468081b36c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:12:21 +0000

    birth anybody sensible consult march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
