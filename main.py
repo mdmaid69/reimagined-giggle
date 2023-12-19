7e03025c4e7f21aeb7d51a1752887fcb8b3f127f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:12:49 +0000

    cover split tone careful acceptable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
