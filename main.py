0e628428f4f8cd06f806231cd52a08a316548010
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:07 +0000

    lead contribute test together lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
