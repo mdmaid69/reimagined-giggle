1fe1fbf25c70341cc15e832e30e39dadf10a443c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:18:24 +0000

    general comment damage suck good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
