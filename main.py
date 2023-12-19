31e3c78fada59fb3747847ddb5006b5fa18fc9b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:49 +0000

    opposite frightened couple repair even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def count_words(sentence):
+        return len(sentence.split())
