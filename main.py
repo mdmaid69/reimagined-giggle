a021e64bb4f55aa75aaf745553c85bf58e0c4a49
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:13:57 +0000

    out promise correct rain swing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
