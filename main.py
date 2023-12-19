5ce5c73f495f96febe6e1a08d2330ebd612d221b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:13 +0000

    breathe burst surprising most fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
