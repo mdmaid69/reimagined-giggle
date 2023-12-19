d0f4395782fa7dd28c426435dcfd66595a48226a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:57 +0000

    paper remark stamp flat important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
