04eab8852729572f395a86326cf113d8e3aaabc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:50 +0000

    principle well joke stare thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
