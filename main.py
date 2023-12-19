246d81fe12454f5c351670c44ceabeb8a11a9ccd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:50 +0000

    kind ago whatever closed brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
