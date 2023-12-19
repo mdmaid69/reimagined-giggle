bca1cff69341d09978e2a193619260e08fe01026
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:54 +0000

    entry seem since sail flow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
