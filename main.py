cfe95a91855aab135889634e6f00b18cc1ece945
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:49:14 +0000

    length grand settle chase claim

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_item(array, i):
-        return array[i]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
