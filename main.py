5f97a1bcc9030987e81ca0e96dbc72a54621343a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:54 +0000

    content we church question personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def add_numbers(a, b):
-        return a + b
+  def split_path(path):
+        return os.path.split(path)
