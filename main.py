c3c1ef3e9dbaa4f4dbd859e446419f274c8582d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:35 +0000

    key natural reason term tell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def cube_number(x):
+        return x**3
