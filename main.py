4321187fe776c52a57f185e3ec1809bc385c6cb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:04 +0000

    but press improvement against flow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
