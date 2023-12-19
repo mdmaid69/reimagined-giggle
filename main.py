e71e53cd4e8ddef05ef31dd5da5d1f972eb4e57c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:45:43 +0000

    content shallow petrol majority freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
