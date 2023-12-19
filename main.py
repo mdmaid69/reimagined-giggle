10504d092d0640d993df4f72a4b6f965b16b60ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:57 +0000

    road cross expensive Qq left

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
