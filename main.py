0dcdc3a1cea5ac360b3e78765ac2476f0da554b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:22 +0000

    empty exist rare word straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def exit_program():
+        sys.exit()
