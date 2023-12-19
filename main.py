2c0a5ac62171710b1b301c09ee7b6a11528f4af1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:19 +0000

    change mine snow along bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
