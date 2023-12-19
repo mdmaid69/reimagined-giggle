f9b7f2c4339548ace11e54acd3e840209b357fae
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:23:10 +0000

    city weigh day above lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
