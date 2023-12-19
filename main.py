bca572c1d51e200b7a7cfbc2a4eaa4500f382d2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:58 +0000

    marry proof steer cruel spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
