f89b02502a0782ff8ce89903f1c7cfdf345758c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:15:31 +0000

    refuse together sister keepfrom mail

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
