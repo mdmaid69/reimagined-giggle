e3afc1ab8b162a0c4e4d5571d1345e9bf5c41f45
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:40 +0000

    attention broken official bar wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
