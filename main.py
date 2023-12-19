cb0dff1086e0a2667231707077bafb0999129d8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:21 +0000

    left power mess by callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import time
+def get_current_time():
+        return time.ctime()
+import datetime
+def get_current_date():
+        return datetime.date.today()
