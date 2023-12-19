2af99f8e542288a5fde7bb2ae729af3b386c6b82
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:56 +0000

    stone put theirs pot bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_base_name(path):
+        return os.path.basename(path)
