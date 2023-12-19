a8644314d60dcfd68d78abc19241fb9fb434c8f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:09 +0000

    moment holdon gradually load refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
