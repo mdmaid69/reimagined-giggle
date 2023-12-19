9d52c9288f9b76c9a2283e97f8275bfeaaeb5dfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:46 +0000

    behind imagine brush left shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
