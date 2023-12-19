a0a774866850adf1ddbed719e31706458883bf04
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:18 +0000

    town all remove pollution independent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+print(os.getcwd())
