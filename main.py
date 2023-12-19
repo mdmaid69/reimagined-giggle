23d421731574a9c5bbec9201fffb968b3ff52e0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:49:31 +0000

    murder strike face spill moment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import datetime
-print(datetime.datetime.now())
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
