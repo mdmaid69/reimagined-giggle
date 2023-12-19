6582c2ad97e31cea9da264bcb760f585098e6b79
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:46:39 +0000

    character Kk explanation simple splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
