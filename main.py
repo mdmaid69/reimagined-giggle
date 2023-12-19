311727df8d9dcfffc6942b9cb28ea20aea50efc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:02 +0000

    time show people thanks basically

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+text = "Hello, world!"
+print("Words:", len(text.split()))
