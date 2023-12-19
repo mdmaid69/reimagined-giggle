1b83b633544a88281ff379acb76042eb47a2b43c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:15 +0000

    fire team absolutely pity perform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def cube_number(x):
+        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
