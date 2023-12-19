f31c1c69287049b9c8008970aa288fe285f70b1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:59:30 +0000

    everybody listenfor worth mind small

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
