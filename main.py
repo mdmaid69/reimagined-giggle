057d40c4c50d7358b71314426951b046590a05aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:56:35 +0000

    pressure peace subject steady sense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
