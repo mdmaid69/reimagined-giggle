b32ca5bdbc1e94d4a7a7dbe45665aeff7734cb81
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:22 +0000

    care careful fence quality bit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
