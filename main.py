22fe015cbdab9c1f63b26a2b492c1ada31c71626
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:13 +0000

    unlikely no police spend muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
