74d184921ab2e39bb8a6a4de3f6515bce0133d33
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:18:12 +0000

    heart practise start exaggerate flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
