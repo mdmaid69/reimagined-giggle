87a27c871eda5e726f63d98271fa53c0e6c12c48
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:16 +0000

    sorry confuse clue past happen

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
