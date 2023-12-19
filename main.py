b8ba9927ad808f2dd08a789f2beb8c5c482cdbe4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:17:41 +0000

    judge bath true image force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def split_path(path):
+        return os.path.split(path)
