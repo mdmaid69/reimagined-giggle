e2eaaf462d29d6a5bda027b2ec46c7e408a365a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:17 +0000

    present impressed yours post keep

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
