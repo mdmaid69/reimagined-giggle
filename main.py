c5fbb86c192c479d4a04eaf3c9ed891ec8cc21b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:07:48 +0000

    bag afternoon lie maybe accident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
