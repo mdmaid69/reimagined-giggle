8059d2319819848ccace40546b7cc65e15a6494c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:33 +0000

    expression difficult order recommend same

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_pressure(force, area):
+        return force / area
