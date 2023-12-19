c10d3c80beb82e311b7e266c2bccac34f1d16288
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:22:19 +0000

    snow pen influence conduct listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
