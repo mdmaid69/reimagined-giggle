142fed493d93ed38289be46ecc0ede97e4cdafef
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:35 +0000

    area blow breakthrough amount receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
