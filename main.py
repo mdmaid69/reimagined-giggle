3ccd8ffd04d19880105aa58bd871080f3f9d5462
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:41 +0000

    least accidentally improve determined material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
