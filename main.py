c15a0ed2564f8e48aaf7836a6333932c03aedd3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:20:59 +0000

    in record holiday pencil 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
