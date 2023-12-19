09bd7ee547f13d304c92138873f94129d337fb0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:12 +0000

    reliable wise praise powerful creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(lst):
+        return max(lst)
