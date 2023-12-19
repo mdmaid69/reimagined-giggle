c6342001b3c505ae049d1fad2d6e701d0efa672f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:53 +0000

    load clue film die reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
