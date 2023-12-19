40180450d64dfe732d7f5f7572bc051eca1b2f3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:24 +0000

    talkinto lesson money pretend find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_exponential(x):
+        return math.exp(x)
