00249cb4465820176ff89ef68769439607fd4c5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:04 +0000

    seem steep yes fail stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
