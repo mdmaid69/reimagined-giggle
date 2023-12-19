f4123f32c0f12f0ac358ca96b03634b6713374a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:33:09 +0000

    check remember convenient why drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
