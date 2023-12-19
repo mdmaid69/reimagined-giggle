3452505053dd593cf319045165f22237fc6ca9d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:12 +0000

    lead young slowly sick exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
