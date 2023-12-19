4a1e29992722a1cb97611090aa0f2f08d4c373bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:08:47 +0000

    journey expression brown past my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
