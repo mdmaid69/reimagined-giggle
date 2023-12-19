645a4f981ef52838389562f9be67a58cfa1be9b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:38 +0000

    reliable seriously belong owe pleasant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
