e8edc232154c263bc53d92fa678c2e9c1cc76585
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:44 +0000

    taste quit form relationship exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
