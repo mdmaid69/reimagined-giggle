aeda4ce70c9d4b6e453c5f1e30168afbbd42b249
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:08 +0000

    concentration wipe ours welcome song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
