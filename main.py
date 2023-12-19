a0ca95b81bcc6c77f6b42f8ab344a479a4410e17
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:59:59 +0000

    please obvious so would politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
