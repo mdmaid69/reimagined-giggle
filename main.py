bce975d752931585973e4f625eedb498624d7c4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:19 +0000

    dinner receive visit forget idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
