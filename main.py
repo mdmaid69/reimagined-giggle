178fd9e24e75f798c6306ccb093c06e4f5ed452e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:34 +0000

    or sleep complete memory office

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
