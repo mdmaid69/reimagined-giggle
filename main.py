6595e2012c252c1f6258f718178ebdf3684ed0d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:58 +0000

    branch tap instant takeaway myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_item(array, i):
+        return array[i]
