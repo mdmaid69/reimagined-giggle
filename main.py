93ba361513556b1f1ad5e0236d71e27974f7c87f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:29 +0000

    why previous Ww hurt long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_user_dict():
+        return collections.UserDict()
