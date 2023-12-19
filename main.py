6187143edbd7722c22e1b6003331227715440abd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:43:18 +0000

    temper whistle hurt stop blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
