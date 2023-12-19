852366b9e526116fb47e31f155bf60602ca24568
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:07:42 +0000

    cinema clean university mind tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
