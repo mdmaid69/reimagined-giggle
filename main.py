d22f4559d492d46e0f6ab6cd77f1f6a2c0173928
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:08:55 +0000

    appearance within beat better hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
