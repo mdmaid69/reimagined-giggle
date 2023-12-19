2552281c97c1a1d623ce7b187206b41fc4d16a21
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:58 +0000

    fairly principle increase on another

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
