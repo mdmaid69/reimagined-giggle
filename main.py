4417bfd905a9f13f4083bf2b6480f3d7ff20e5e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:34 +0000

    suddenly sweep talk regular without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
