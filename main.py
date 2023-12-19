151e10961cc243a042d6ad4cc718dcf9126699c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:41 +0000

    conversation date turndown punish throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
