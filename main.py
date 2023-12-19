cf0570819774169fc9457f08abf20809c2374ece
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:06 +0000

    pride event commit offer report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
