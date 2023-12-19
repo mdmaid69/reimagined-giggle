fa8c4ac82e5e222ba40ca2f72e79e99876002d81
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:49 +0000

    search show command judge wise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
