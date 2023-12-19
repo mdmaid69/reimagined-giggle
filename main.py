912fb3328feaa921939bca1df6f2ac010bd1a6be
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:32:12 +0000

    tooth on me little comeup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
