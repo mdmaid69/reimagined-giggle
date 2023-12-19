3e57abf197a29e4fda5cd8db2cdcde9e5aa00a1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:10:36 +0000

    fit Dd human book correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def convert_to_binary(n):
+        return bin(n)
