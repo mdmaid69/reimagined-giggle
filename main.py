707b5c34813c4fc3317a58c3721260bc7ae98f89
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:38 +0000

    side excellent talk warning however

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
