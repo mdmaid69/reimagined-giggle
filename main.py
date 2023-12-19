d9df51f79466c290d30865001c6e0843354a3e87
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:01:34 +0000

    score usual shape effort level

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
