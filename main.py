43d9af5cf441755c349e567d5f6e043985fb7876
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:45:36 +0000

    treat holdout touch turnout bush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
