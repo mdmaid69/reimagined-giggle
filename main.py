68c28c1b51bd6d3eb27b411acaab8ed871daa043
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:04 +0000

    angry pressure mind downstairs undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
