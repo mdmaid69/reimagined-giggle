4e03b551cbdbf8990d8d703a8ab9e3521b9f3ac4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:54 +0000

    Mm yes sink sensible conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_min(lst):
-        return min(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
