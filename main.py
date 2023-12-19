63b8dd518537ff77155d20b5465f1d4e29248228
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:22 +0000

    die brush hair should immediately

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
