bd389a531e4feaa9bc69087788d92697f40e0965
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:08 +0000

    rid health lie me upstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+print(sum(range(10)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
