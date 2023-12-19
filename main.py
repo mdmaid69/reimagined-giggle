6df7de6096fc7987a3267bf32d2ab9aa00708ea7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:27 +0000

    runout nonsense exaggerate birthday cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
