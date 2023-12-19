91eba7e25e749ba7c5653d963568780740c560e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:32 +0000

    drink boring material lock minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
