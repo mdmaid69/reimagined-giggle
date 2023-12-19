3f00f044b58c185e751422515cb5b010c18d4a37
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:43:49 +0000

    say for precise honest rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_from_array(array, item):
+        array.remove(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
