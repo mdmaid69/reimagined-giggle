f3348d9fdb47d10b982789dec47abafe35050e05
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:59:01 +0000

    form upset nurse cope please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
