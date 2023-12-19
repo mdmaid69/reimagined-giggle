2c81c5dd7049d4f2e748cb4b9e24eaf1767a63bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:07 +0000

    shame again destroy circumstance bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
