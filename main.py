a44eb5f4628a067e9ae8ae4d86019a28d2320bc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:35:46 +0000

    doctor uncertain ready pen move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
