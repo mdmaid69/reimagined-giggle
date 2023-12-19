a31dc6a43f6ae8118dcece0039dd5956cb41fd66
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:04 +0000

    difference throw inside shut happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
