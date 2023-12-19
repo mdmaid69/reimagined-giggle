030999b62be4ec353782d912aa8e9ae31236fb43
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:29:45 +0000

    like deep someone deep post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
