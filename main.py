98857153ca17ae9ff97405616928f0c6eb05f56e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:11:27 +0000

    fine teach bus opponent unhappy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_exponential(x):
+        return math.exp(x)
