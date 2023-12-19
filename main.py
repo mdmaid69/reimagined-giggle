6485a87a5974eeb349605809c6a443bbbc3f3873
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:57 +0000

    eager stare one scream the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_pressure(force, area):
+        return force / area
+import os
+def remove_directory(path):
+        os.rmdir(path)
