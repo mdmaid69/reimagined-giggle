c279d4bbd308bf8f6131a1770732c95cce988fca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:44:34 +0000

    mouth apologizeapologise change large file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-for i in range(5):
-        print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
