28a9795fd8be52f6f65a014b8f9d78feb4f17fd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:25:57 +0000

    only region related throughout hardly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
