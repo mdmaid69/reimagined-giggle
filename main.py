d1deb960196c539332ad95e2b0f92cffd30bc569
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:11 +0000

    usually hardly much detail unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
