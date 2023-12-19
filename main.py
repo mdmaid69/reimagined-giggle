ebba6764897309ad6bc8d232164292cf483f7064
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:22 +0000

    inside used determined cost block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_work(force, distance):
+        return force * distance
