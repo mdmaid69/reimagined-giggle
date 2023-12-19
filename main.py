8b9ba083d8d1c4f8e4d6ab3a61e4feaa2c3605b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:48 +0000

    mood suppose storm scream wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
