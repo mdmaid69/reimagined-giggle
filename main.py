6d605fa6ab97198a8c2f2c3d80deff82a15670f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:21:23 +0000

    dull sad instant protest person

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
