dd8195ba3d4f205d1808f500f938cafedc1630d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:28 +0000

    total fact anywayanyhow keepfrom big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
