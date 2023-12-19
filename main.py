c69e862a4fe704e846be92a1732313a71aa6b951
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:56:59 +0000

    foreign step wrong share more

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
