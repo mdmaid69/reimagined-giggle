79a9d18306b0d3225df80f72b64ca40033734c61
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:37:17 +0000

    ring conduct memory reach used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
