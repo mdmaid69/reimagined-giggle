6dd16bda9b99f27b8faaff128264c93cf68b7ef7
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:15:20 +0000

    later after arrangement dress lock

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-print(math.pi)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
