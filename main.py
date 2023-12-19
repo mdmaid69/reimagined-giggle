273442fd60b8b7c9c1f5380fe86d8e8e16a3fb6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:06 +0000

    closely proud whether deaf suggestion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
