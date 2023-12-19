acde5a1622029ad5684c7192643a08cc6735ac6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:02 +0000

    tomorrow reduce information common computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
