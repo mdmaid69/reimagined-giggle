c2f44fa031078f634c2e3ebcd2ab2c4e13911db1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:29 +0000

    leg which trap time talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+print(math.pi)
