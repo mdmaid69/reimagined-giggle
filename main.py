418d1a6055c9c1a21c1e553b722252229f197aa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:22 +0000

    estimate ilovedicks room confuse temperature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
