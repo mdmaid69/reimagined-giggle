2ecc3e33d552034937670271bd1ec66e5ca55579
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:23 +0000

    record next creature politics block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
