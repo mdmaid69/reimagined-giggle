956d2a9197f5152f1f77b8078ff90c195aa08478
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:43 +0000

    callup me practical whom much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
