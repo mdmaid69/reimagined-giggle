40e8ad8304055fa4b50fb2c73f34d0b54cc347d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:41 +0000

    rain backwards official where pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
