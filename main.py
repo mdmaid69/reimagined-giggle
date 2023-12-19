dda3cde8db5aed6e4a8afb71e4c20e76aad51b38
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:37 +0000

    close pot fit list live

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
