9bf72c9c0d97d11abc36554911ccbb1e7d072d4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:06 +0000

    workupto manage workon measure drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
