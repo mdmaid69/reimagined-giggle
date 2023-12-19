44189626f7e9781d585ccfa6abab129976b7e99a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:45 +0000

    hold straight represent memory improve

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
