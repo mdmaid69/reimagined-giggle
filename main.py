1f7e864fe58dfcb84c64d4aaa32792c8328ab406
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:20 +0000

    water takeback noisy please window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_str(array):
+        return str(array)
