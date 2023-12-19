7f5339977f7ccb59a1faad963a82c9a629a4f134
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:57 +0000

    simple plant weak full free

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
