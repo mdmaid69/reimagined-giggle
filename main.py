9378347743382248ba2045b7c4775487d1a59735
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:23 +0000

    fun cook pay then hair

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_current_time():
-        return time.ctime()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
