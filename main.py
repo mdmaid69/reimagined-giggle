ace79b97e5f595c52790da0409f4365485b65310
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:28 +0000

    rob little record neat month

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def is_even(n):
+        return n % 2 == 0
