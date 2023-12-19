b414f529f33c6e2afb0dbc99c7556e1c3a568705
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:07 +0000

    unhappy easy rain hurry opinion

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
