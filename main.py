536de661ea6de64dba8a67ef5a653ae47706afcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:48 +0000

    certainly culture otherwise dance rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
