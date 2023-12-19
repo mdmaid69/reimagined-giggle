0a0d75136ceb76dac689e795d124a9e0dc366759
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:49:50 +0000

    lunch violent seriously view complete

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
