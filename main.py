ca1fbcf3a451c045ed7a9fdaafbaf1c91c09efdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:32 +0000

    argument worth obvious female through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
