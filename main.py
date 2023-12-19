282d341fe122b70484e07dffa41d7119832f272d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:21 +0000

    keepout card ball sensible class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
