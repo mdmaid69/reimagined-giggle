73b2fccae721b2d1fad4b064be7b8e87b5c5b44e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:05 +0000

    considering wake my practical large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
