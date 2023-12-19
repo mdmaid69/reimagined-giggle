bd9e6c4abdaa13dee2098da6a4de532d07d13ff1
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:37 +0000

    tone equal side experience brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
