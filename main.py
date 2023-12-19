35e011529cab8df85917069e351753fcf69c004f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:27 +0000

    substance answer callup event serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
