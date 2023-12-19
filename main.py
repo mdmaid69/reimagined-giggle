81b92c0a144a6ebc66f2cfd994fa61eb40d49bac
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:52:31 +0000

    excited mad alone within nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def sort_list(lst):
+        return sorted(lst)
