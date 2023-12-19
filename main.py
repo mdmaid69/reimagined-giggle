1d40c588535fdce66165283dec7167ef9bc1e3ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:30:53 +0000

    can takeoff university afford travel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
