db8f22bfe38668ac761510f8e650a03189af52d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:16 +0000

    offend clean stupid Nn hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
