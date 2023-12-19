78442f3eab25cc4fe07749010a5b2a5ad214b52c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:41 +0000

    guess where storm none trial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
