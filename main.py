1f32261cd964e4073a70b659a26faf05116fa341
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:21:49 +0000

    soft image difficulty wait equipment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import sys
-  def get_python_version():
-        return sys.version
+def find_min(numbers):
+        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
