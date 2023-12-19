ec450ae69c8e2331a493e475871dde12c89a3bf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:52 +0000

    table floor ground recently shake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
