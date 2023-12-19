61fdf9d22d4679ded2b8a32dcb478fbc16e3c9df
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:44 +0000

    intend group chief load peaceful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
