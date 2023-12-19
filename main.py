85903b0bd2baae05c1b3c00104adc91329584847
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:04 +0000

    pipe divide story level up

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
