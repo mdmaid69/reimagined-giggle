4b1fa41c5a6bad6829dc18cff53c830ad1906661
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:20 +0000

    range workon promise reserve preparation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
