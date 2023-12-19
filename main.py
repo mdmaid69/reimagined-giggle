1920de82c7b6ff4a326d56aa4c1c36039bad3ed4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:59:00 +0000

    word test desperate full history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
