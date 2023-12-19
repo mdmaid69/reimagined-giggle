8c118f26585dac7c321dea806585e5443a50a828
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:24:55 +0000

    achievement show subject still soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
