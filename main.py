ad4b1ab426ae539e85718be8e11c3b723b2ff816
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:54 +0000

    appeal contain trust practise condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
