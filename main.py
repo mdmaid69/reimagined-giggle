d572cbb85e9bc36bd2cf584c167f42365fbe61a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:57 +0000

    loss help thank moral away

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
