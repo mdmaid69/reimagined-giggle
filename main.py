0e13e55a4af54fded335f3124d1f4521f5bbd9f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:17 +0000

    weather strange foot mother unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
