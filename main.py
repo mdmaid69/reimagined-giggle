3330d5e635046895125b90f69580b2dd0fedf454
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:09 +0000

    file light central search child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
