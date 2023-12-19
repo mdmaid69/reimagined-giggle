b7d720bad7ce80e72c640b8f884995eb4dff695f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:22 +0000

    effect rub mistake tour sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
