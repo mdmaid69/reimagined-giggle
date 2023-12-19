e9791ca098c258e53f586a70290007bd29319099
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:51:12 +0000

    part best violent hurry bye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
