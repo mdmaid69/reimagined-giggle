2a4c6882c0f59999c3032c8122cf51e49533899d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:22 +0000

    prevent towards object report young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
