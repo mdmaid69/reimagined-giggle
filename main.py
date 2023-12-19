781128ff4389d9ed6d1e03ac5bb6e0895d9abda0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:57:31 +0000

    meal invite sweep community callout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
