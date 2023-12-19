e523ab9a382a2c2d128440baf1579d37ca3ea523
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:19 +0000

    meanwhile force unpleasant traffic loan

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def multiply_numbers(x, y):
+        return x * y
