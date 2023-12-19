2444aac5d9a529eb3240664065840d47b51ad464
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:27 +0000

    turnup fairly accident trap impressed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
