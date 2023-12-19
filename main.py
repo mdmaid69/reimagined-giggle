7c7595c5f362cd8447201ea6e22abead71638be7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:59 +0000

    lend doctor in minute pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
