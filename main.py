7266bf878c9115fa58d0f1f8e9a8645edaf98751
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:52 +0000

    criticize belong train join opinion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def convert_to_binary(n):
+        return bin(n)
+from collections import Counter
+print(Counter("hello world"))
