23150572c2404234e440ab58ddfd2a3d86c70d7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:10 +0000

    raw sometimes friend whistle otherwise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
