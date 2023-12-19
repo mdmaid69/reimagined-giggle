c4cea136752dc70e293d3f59d55911a8792f2167
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:02 +0000

    past moral false shock score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
