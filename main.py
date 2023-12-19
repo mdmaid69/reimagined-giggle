1c03a48b8c00fea13a19ef2f5fb9e4e3415c5cbe
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:43 +0000

    past break visit beautiful holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_directory(path):
+        os.rmdir(path)
