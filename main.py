645e9831a01a4c8e60de36c878bed7e36e174346
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:13:45 +0000

    recover reduction door takeout pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
