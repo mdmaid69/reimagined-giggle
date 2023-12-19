0ca80e937efc573f3c9db8724b9d19f5494378db
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:41:38 +0000

    heart deep thorough part wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
