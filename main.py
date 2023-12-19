d5e4b77f20bbc45223a3739328c8630abef61d91
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:33 +0000

    shout opposite move weigh furniture

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
