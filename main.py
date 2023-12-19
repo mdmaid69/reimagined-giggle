a444d78faa5c34dc82fef5adb331160a86b582e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:52:59 +0000

    centre crowd uniform rubbish Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
