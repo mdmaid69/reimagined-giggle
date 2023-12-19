3f3221f7b829b07907bc21e9b33ab3b7987e2b1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:41 +0000

    lock grade float criticize evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_density(mass, volume):
-        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
