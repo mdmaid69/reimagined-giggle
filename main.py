f241d6df132de912159e56b4f34a04e625eff7e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:42:13 +0000

    certain more join fear university

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
