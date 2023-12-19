f97cee0c203a7c61fa0064d190298157e7d8f489
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:58 +0000

    stand talkinto shallow possible prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
