44639785a4302c9ef9ae97e8865c90463576257c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:55 +0000

    bound Gg grade string beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_odd(n):
+        return n % 2 != 0
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
