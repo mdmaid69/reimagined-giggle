6096dbb1f2bb588db1b98f773970d6083d7f913c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:10 +0000

    either unit quick plan tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
