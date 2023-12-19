ce2bd9f3d3beb57d43879825590fb59b728588f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:58:22 +0000

    produce price refer boil connect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
