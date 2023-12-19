e864f84c94de56642349a7e8f16fd3a9cff7e6b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:34 +0000

    breakdown deaf pass ready discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
