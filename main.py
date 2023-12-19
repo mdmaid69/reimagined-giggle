c03114795a0d1edde850ec544b13688ff6e326cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:06:17 +0000

    care neck crash comment inform

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
