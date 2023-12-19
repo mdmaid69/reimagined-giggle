b36b8b9abca6d4c38d8cccd2507cd2364136e06b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:16 +0000

    patience money she influence wet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
