57c19eafa4d8aaeb46a582616c560df7d78a6db8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:32 +0000

    fall ocean line strike show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
