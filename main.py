cf96216f4b6dcfba1f43809263d47253a4402b3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:03 +0000

    rely turninto off arrive by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
