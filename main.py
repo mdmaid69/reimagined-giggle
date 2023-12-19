a222f5ec3ef6bc4a9724809cd21850d1b09f32df
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:24:42 +0000

    kill pronounce single strict time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
