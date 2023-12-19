bb7a16e988750ca5062825658abd61b4e6f378fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:38 +0000

    deliver properly each answer flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
