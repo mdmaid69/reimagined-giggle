254acd505cfea92f4eeb870f57ecf97c909171dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:07 +0000

    high low solve comeoff persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
