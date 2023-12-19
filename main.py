cef38cc7cc5f70ccbb7882f6c440fb67e893503e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:06 +0000

    injury holdout study sensible schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
