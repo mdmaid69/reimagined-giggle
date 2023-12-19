92ddf6a0320f44af56bb17baa639a0132577f4b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:15 +0000

    so cry couple demonstrate earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
