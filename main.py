3a4b75c9d1177f223a137e26fef432e8fa01f4c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:25:33 +0000

    comeup along mine slide teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def remove_duplicates(lst):
+        return list(set(lst))
