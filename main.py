f5017463dbeef982473cd20b06ae4ee735b78036
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:03 +0000

    sign shopping show land team

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Characters:", len(text))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
