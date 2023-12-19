aab81f930531de107305912140619dcfa56b9b29
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:46 +0000

    turn whether gooff drop thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+print(time.time())
