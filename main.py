b80269db4061611287a4a11530b8c60e8218d08d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:50 +0000

    keepoff language sorry force sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print(sys.version)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
