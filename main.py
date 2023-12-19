b1a2e9287ff1fba4ea199e4d5a723de7db813302
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:08 +0000

    apparently crash when punish safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import random
+print(random.randint(0, 100))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
