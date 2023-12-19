a2bb32a3b46931019115935fe4423fdf3faa7977
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:25:14 +0000

    patience so town memory manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
