b60732652ab8de7d66fd34ce5a52135d73c8367e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:52 +0000

    reliable principle steal point supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
