8bdace9f491cda7571080c90f7123b5f70183b2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:02 +0000

    supply underneath give friend agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print("Hello, world!")
