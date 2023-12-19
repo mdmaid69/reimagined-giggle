21af35a69aa643d5c6b0edb85587594e628d93ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:21:08 +0000

    convince lookafter second runinto really

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
