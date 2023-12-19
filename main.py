9bdbecdc8c265e488770e8e48370b7ef94232ea9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:34:47 +0000

    but lead common aware say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
