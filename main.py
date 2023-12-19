f78fddc104681b8caaa5fcad8ffeed9da88e3c7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:45 +0000

    slight fasten habit safety pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_speed(distance, time):
+        return distance / time
