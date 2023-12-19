97e5e41f6f7976e7c7e44242424e145b7d8d7841
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:10 +0000

    side closed clear have pin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
