2cc8d3702bc70085a555768ec44dcb7470d16040
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:33 +0000

    fence truth truth deliver only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
