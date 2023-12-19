abd4529075915b0df2abeb74c78eb6ed55252ee6
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:47:20 +0000

    club sight test instant like

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-import time
-print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
