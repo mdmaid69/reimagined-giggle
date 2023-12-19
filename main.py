7843412b102b1cca9695accfdd5587748289335c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:00:51 +0000

    lost help smart study form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
