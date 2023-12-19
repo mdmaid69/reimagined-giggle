902fd183c368919f577dde8d0eeaeba9e72f8a4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:23 +0000

    could male stage geton minute

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
