4680b33f1d67e0883ef82a02d770c3df383c5dfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:48:14 +0000

    left trip spring pain awake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
