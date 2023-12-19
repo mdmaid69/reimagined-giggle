b8c6d269066cb33dc14e958542e9ccb7a5d9ad02
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:13 +0000

    smile speech float comment bright

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
