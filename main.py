6b77951f608ef186949347666176e838141ebcdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:05 +0000

    willing town ourselves consider stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
