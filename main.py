ed4405e410325676ef756daf376f98d3c0355695
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:45:35 +0000

    shall serve rather sleep while

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
