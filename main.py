393d6ea534acbd95fadfdaf56ab3c6b06a2c65de
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:17:12 +0000

    crash right will sure share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+print(time.time())
