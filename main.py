daf9acefb6e9c8fb2a0d8560f141ef6f6e975c0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:47 +0000

    spite watch when argue boil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
