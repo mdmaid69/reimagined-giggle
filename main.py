9a1b846563e22a4d7ae2054915f7d2ed9e3cfc47
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:07 +0000

    transparent instruction strike gooff concentration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def square_number(x):
+        return x**2
