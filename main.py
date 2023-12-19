13f3e811d4c6698637c486507cdca348f854b45c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:59 +0000

    practice union 567enemy pocket accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
