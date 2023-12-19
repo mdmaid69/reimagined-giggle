6453ee5226d97f39e88fcf78f4ba1eded49dda97
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:43 +0000

    exchange mark least aim burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
