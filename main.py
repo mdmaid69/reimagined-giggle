48bce1316d52204916d9b7309e17e20cf13b7895
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:40 +0000

    long favourite constantly runinto familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
