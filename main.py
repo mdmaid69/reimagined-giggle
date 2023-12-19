ab5ecf18d5a9fcff27db0e848200f8236d3d7356
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:54 +0000

    which bet that cheap present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
