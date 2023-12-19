4cba733ecd653d74a4d76a2bcb2c9d9539b8bdcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:18 +0000

    offer cross future unite loan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
