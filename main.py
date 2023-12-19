7a901aa661ca77118f128badabd5510a27501cbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:26 +0000

    fall bath still second breakfast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def reverse_list(lst):
+        return lst[::-1]
