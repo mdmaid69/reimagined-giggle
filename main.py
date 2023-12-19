cbbbda8b5dad6d99a64c073cdd699b511ad37c0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:50 +0000

    combination except obtain strike side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_odd(n):
+        return n % 2 != 0
