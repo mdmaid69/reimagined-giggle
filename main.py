c1005c2c4d1eebfe3fe7748bfa76694f728a5cb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:54 +0000

    common reasonable reduce priority judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
