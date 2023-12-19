be6ea7e3b6e6ba1387f17c21c7a5996af7d78cf7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:11:12 +0000

    abroad important balance injury scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
