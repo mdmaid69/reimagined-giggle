a184f5141d0ea3cfd78ad9b0c5d7f455421dbc89
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:13 +0000

    poor every forget frighten road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
