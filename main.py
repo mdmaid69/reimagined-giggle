d1191037216a53e783db5c7365ec0deeee06c995
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:16 +0000

    centre shop television major jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
