954d16718704ffed44f2a118a4e80216383e5a23
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:56:55 +0000

    brave its crash discovery cinema

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_max(lst):
+        return max(lst)
