bd8dcb65b1667d412b6998782e1b7d5ab3019d19
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:47 +0000

    plenty write month left unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_min(numbers):
+        return min(numbers)
