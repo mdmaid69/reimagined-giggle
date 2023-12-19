47ea9f4611d2e84e1dc263cce3c99f8fd4c6fdd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:33 +0000

    trade cook naturally comeout mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
