a88b4e0dedc06a5abb3a0ffb8ee83cf11d2635b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:58:07 +0000

    job difficulty turn central look

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
