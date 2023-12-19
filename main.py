13fc05774b982edb63cd2af5c4e6e839d5d7b8aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:20 +0000

    attention once peace steal shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
