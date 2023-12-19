edaf8ea9bf315ec924c590dc809fa828970fc646
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:38 +0000

    gap store leave stop pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_as_list(array):
+        return list(array)
