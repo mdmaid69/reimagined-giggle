f660da458a4f16c510ede31f5328c01ba8e5c60e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:36 +0000

    question explain different afford sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
