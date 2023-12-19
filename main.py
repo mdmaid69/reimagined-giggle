2c99807be3b2ebe846a3f4fc005681ebb5295287
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:16 +0000

    use indeed further hold become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
