5c514d675e5a0a801615686ce3aa3b034b5f3c00
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:28 +0000

    just position mail error lose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
