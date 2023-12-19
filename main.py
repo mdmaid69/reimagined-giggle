a8e6787e1c483b29c0909395182f20442f6bff18
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:24 +0000

    transport mess apologizeapologise rain profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
