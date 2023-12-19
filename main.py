ea0587aab1d98c85c7a1414afa67d9c2683c438a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:45 +0000

    somebody complain sound design sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
