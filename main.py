96de6b2ca976d5edcb03c8789801f9b2b76a3b71
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:57 +0000

    experience with travel petrol getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
