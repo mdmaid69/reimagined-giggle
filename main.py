c6888043dc22851f312d09cadc69a34a03f399e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:10 +0000

    shut by thinkover study mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
