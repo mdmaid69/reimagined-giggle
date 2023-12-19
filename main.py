43e000befb5f7726c63451b135e9de96e8f023ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:37 +0000

    straight opinion loose flight sometimes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
