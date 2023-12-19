280c238be0b2a2f8422d2e1f05bfb374ff855215
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:11 +0000

    sick drop public pride loan

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-print("Hello, world!")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def convert_to_binary(n):
+        return bin(n)
