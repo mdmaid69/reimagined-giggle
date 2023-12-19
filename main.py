8381865b0b5e8a0fc81a93a51055d53e8da0d782
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:03 +0000

    whether calloff water too wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
