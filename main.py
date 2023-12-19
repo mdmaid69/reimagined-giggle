96d5560c606bda62ef8593a6370df4791847f993
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:33 +0000

    whom software crowd refer upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sine(x):
+        return math.sin(x)
