f8332d9396d1a780162f0d8aa529d9923c364882
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:05 +0000

    form friendly final arrange ring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
