589061f311c288418e05a8c00a8123820a9b422e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:59 +0000

    light market have above happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def multiply_numbers(x, y):
-        return x * y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
