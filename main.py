0d6ad82791b1b381856b1e108bd432da55c5f42e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:41 +0000

    person occasion finish rather Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
