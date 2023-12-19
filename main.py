3440f6558a06be17711fb0ed4b31cd577ef9ae56
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:15 +0000

    think front print into could

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
