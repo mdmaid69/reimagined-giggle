7116d260863a6c6fa4821dcf5a45a81cb463fb92
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:31 +0000

    include prefer hungry Bb left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
