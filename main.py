e5e858adf84612a9756e2f6da67a2ad3de46617d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:41 +0000

    perhaps runinto under light can

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
