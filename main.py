97b9fb7424a89600de5894716512cf863221a34e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:22 +0000

    motion length switch raw lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
