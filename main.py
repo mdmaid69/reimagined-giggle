3a1aaeb0cd994fbcfd07c3caeb64562e7be7be43
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:49 +0000

    age calm recommend rule doctor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
