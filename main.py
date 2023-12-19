7bfaf4a5af22f88f84fcbf71b0db93b5004a24a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:15 +0000

    pity but same trial stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def cube_number(x):
+        return x**3
