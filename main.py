e0c575e92c22bbcce87ec3e94cb5e349d01cab04
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:50:59 +0000

    fast accordingto tired sign crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
