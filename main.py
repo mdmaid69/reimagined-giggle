26462d9a21de3fc8a07d6e8735aadc65b2923334
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:52 +0000

    fit wish wire taste comment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello, world!")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
