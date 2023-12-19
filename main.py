1546a93e96fa235271fcb82987d2494e95c75742
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:04 +0000

    love apparently confuse path 571enough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
