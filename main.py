fe8f59d1e30784b9bdb90e2e64f293760eb3df57
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:07:12 +0000

    bank friendly Nn maybe friend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_exponential(x):
+        return math.exp(x)
