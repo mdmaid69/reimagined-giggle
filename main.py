7497a70198c0698e9c55773b6f84420932491b7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:31 +0000

    left among aware tap fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
