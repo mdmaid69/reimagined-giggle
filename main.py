5d3d4c24efad12c68b34b1f254b36ee08f6d7766
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:52 +0000

    appeal tap pronunciation indeed extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
