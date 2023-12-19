e6d7f8da28fd5a3d0952edc84b0e25d58d68f104
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:57 +0000

    range help tune support press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bool(array):
+        return bool(array)
