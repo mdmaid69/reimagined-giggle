d866d734286a9da8e077121267276f7fdcd42091
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:19 +0000

    reach discover used fix complaint

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def calculate_area_rectangle(l, w):
+        return l * w
