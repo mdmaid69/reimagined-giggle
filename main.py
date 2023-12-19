2633f6b7fbfa4405aefa3d5942f52de2f69de59d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:26 +0000

    shock immediately else work Jj

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
