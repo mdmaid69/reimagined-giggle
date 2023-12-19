2877662ebfcd42bc65246bcf541a61e00da6c918
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:39 +0000

    sure lookaround with month threaten

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-print("Hello, world!")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
