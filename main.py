9bd815960ee79d17a0b13ec466857e20de70f5e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:30:28 +0000

    valuable apparently sensible undo so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
