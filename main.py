87897ad144303b797dba0493ffe616911778d259
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:53 +0000

    save tooth recovery deaf against

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
