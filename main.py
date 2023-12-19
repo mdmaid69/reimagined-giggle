b86892125293066ed7d4a05528b5b700918ee63b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:21 +0000

    question reject part effort come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
