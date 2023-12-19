71e2b9fc5414f87fd84933a01efc99f379976a8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:55 +0000

    OKorokay circumstance slip far manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
