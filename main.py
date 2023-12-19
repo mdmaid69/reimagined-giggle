15889e043bfad8ea2269cd55c27a2459b5346237
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:28 +0000

    arrange deaf exciting expression agree

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def reverse_list(lst):
+        return lst[::-1]
