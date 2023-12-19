008e66b88ac9e3ae48c5e166b104157b19396940
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:19:00 +0000

    though turnon exchange first react

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_power(work, time):
-        return work / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def cube_number(x):
+        return x**3
