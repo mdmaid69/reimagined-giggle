9a6b4f9fd2b765764dfcc4df1b00717211709391
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:29 +0000

    wherever young chat fun actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
