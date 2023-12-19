9d9467ba6974be1ff1c13948a629c356b23eae2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:51 +0000

    glass us climb fairly wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
