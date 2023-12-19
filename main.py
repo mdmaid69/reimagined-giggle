514adb662ed065bdc30a639979289b947304f61a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:21:19 +0000

    welcome neither there blame rush

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
