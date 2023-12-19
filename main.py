b97816b0d2b30181222ddd23404471364cdadadc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:40:35 +0000

    charge brief crash room nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
