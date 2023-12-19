5377f530253f5a946e911c20b475efc2d34c7f0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:52 +0000

    opponent noise survive plastic listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
