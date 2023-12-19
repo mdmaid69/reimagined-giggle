5f882828bf498483d6ca69ee540f91c9847b80a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:56 +0000

    expensive production light basic century

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_directory():
+        return os.getcwd()
