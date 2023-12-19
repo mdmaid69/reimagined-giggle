907a450b1a906c76865ac004755c4f6404e42186
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:37:41 +0000

    money fear hot cause greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_density(mass, volume):
-        return mass / volume
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
