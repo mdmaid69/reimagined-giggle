f762da03bcbd414c848564a2ed67a470e1eef5ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:17 +0000

    threaten wander suppose tie abroad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
