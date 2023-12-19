2e835a04cc3d4c9ccf7ea0af2bb9deb31fab94b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:46 +0000

    attempt makeinto effort perform replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
