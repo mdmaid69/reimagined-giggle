f9ee852a221d0c8bb3f1a2d877e06f672c2fba87
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:34 +0000

    pleasure declare flow like pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
