db7d265cdb6c5fddec17356efaebc66eebfb13a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:29 +0000

    try success sometimes tape other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
