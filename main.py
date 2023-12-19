e20da6bc31e08541ad6f66c2591c70c93c1523ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:54:52 +0000

    cold complicated few comment your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
