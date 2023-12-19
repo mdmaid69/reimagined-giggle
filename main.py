946c6830236835d686ee33e5d0fcc1179348c050
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:04 +0000

    priority sit boat congratulate exist

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sys
-def print_python_version():
-        return sys.version
+print("Hello, world!")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
