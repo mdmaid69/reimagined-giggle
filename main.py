3fe8413138c54da33df72173654bd219f2911436
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:41:52 +0000

    grand the little decision mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
