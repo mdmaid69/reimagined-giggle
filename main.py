f3c2df4e359106dc4b6826c22785cba9ddcb75bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:21:34 +0000

    accommodation news butter private appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
