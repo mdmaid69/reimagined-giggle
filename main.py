1cbe5f8bec7c6f1eca6bcbc4ff7ef1380f3904f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:13:22 +0000

    pain translate there limit there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
