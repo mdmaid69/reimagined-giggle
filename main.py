0985f57ad120fa56c5730e8bd26790f2a2ce1dde
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:29 +0000

    edge pleasant crash history gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        return sys.version
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
