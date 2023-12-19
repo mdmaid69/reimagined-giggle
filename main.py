08ad23c5c05181befddd336dffaa3073c8496618
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:19 +0000

    Pp ear win excellent honest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
