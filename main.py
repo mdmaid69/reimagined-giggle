c2a86a96d62a2d5309324cbe18bc193cb20a4ad9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:20 +0000

    new theirs book debt consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
