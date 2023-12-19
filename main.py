8a93c1e4d960a7b58521f35f61619daf074d1097
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:34:39 +0000

    finally nerve off back beat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def convert_to_octal(n):
+        return oct(n)
