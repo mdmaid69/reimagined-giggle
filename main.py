ae431f48cb58a9fb627c12417b30e4e5db374f00
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:26 +0000

    anything brother bringback wake concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
