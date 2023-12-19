a78759be75277da72e47dd22228002ff3d53ce5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:35 +0000

    characteristic ashamed motion violence neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
