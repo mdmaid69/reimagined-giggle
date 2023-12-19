c2e539a7f4d0427a4eb9fc69c08453c14bba39a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:34 +0000

    manage Vv exhibition bury cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def print_python_version():
+        return sys.version
+import re
+print(re.match("h.*o", "hello world"))
