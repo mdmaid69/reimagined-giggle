5500e939ce22d1bc290d068557a53a9ad4f9274e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:48 +0000

    prevent worse estimate report goup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+print(os.getcwd())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
