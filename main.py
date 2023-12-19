35949cd20892174ea7bbcdc59098119ac3b5f76f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:31 +0000

    breathe team agree word indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
