ef5f917ffc81fe04d88e7b822fe5e9233d81eaca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:06 +0000

    if book matter read name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
