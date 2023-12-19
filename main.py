943c4a0a121bfa9c962699d46106baa9cb3bc3d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:02 +0000

    you feed guilty each furniture

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def square_number(x):
-        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
