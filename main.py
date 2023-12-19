f9d243069e0cc74776f413f12fd77f9da0338bc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:14 +0000

    pause guess unhappy taste hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
