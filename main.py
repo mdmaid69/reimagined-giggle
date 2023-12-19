0d9cb5171a8a9da67f0ac9fc3ffffaa74821e30e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:19 +0000

    exercise matter please little punish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
