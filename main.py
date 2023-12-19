66516ef7bfe43e3e561d1d70e301f7080ce0edab
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:35 +0000

    introduction practise bush before clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def find_max(numbers):
+        return max(numbers)
