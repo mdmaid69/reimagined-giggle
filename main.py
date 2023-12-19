e43102b4a45027f7d8563aa6ee250038c40a270f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:26 +0000

    comeround warning embarrassed simple noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
