4909bd32d6b4cce8d5b25bc453da74e971deaafe
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:16 +0000

    coffee imagine weak present trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
