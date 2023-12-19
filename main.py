6a7e5cd63c62995dd1ab28c66220f9f92ca0c06e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:08 +0000

    education upset break brain light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
