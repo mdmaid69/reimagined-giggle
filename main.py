c85543fa443e21eea2424f24a3cf05a5b7dbd516
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:50:43 +0000

    switch thick rate mark top

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_pressure(force, area):
-        return force / area
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
