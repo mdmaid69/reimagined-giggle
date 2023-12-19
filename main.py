49345f2e3b7240d6143887ee44fbfc7c40fc7f18
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:56 +0000

    heat advice weigh middle hole

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(lst):
-        return min(lst)
-def calculate_power(work, time):
-        return work / time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
