bc45d9506c5b6488d487b0d3378534df0effb441
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:36 +0000

    contrast equipment conclusion tap occur

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_unique_words(sentence):
+        return set(sentence.split())
