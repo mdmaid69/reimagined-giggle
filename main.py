2d27cdb37c6c8037005859df644fc3ef51931757
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:01 +0000

    correct throw keepout get high

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def square_number(x):
+        return x**2
