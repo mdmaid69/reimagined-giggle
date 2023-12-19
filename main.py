2490dec88ee213c5912cfa183a817b501e5731ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:11 +0000

    sweep picture obvious answer many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
