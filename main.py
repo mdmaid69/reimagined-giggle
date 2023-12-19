28bb745f447cb213aaec69bf563da446f104e43d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:47 +0000

    sail plenty die contact yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
