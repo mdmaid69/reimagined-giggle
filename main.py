21a12d75432dfccca88617f79cf4d23f952c8630
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:52 +0000

    visit plenty hour genuine afterwards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
