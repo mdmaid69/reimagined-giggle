40b7f27440a6c6bf41bd0639f95cf2710a0c9d67
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:39 +0000

    amount breakinto means demonstrate fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytes(array):
+        return bytes(array)
