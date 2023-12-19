edb45a96b2ebdddd77dacab726dd277b8c137203
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:34 +0000

    law scene close cheat while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
