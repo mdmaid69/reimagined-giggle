0d30579bea73f91db89cfc051570970ff58d5e27
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:11 +0000

    getoff sight become guess concerned

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
