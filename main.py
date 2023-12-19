b7f5c24f672edd6f04b030a4a37aa26e9522c967
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:57 +0000

    store workout sleep this unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
