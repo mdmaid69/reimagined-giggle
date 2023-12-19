fa71b2a513e1a24eea8c79aa49b6ba408b0b3dbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:06 +0000

    clean escape explanation remain invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
