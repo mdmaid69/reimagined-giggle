6a8b8c821d56ce5221c7fa8a0b55ce921d3e1464
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:19:29 +0000

    less defeat morning normal nose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
