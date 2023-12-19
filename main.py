463f0819429c7d7f30d87cafd87ac97aed237317
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:19:50 +0000

    attempt legal error burst bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_min(numbers):
+        return min(numbers)
