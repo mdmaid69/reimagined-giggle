89c81aa4c05403e2bc886d9d33743bc80409a05c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:32 +0000

    join couple laugh then loud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
