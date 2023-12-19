2f5c8bed568957be47cf49644893925f5cba5fbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:38 +0000

    Pp hope occasional apart rather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
