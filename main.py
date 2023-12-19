fff818eaeb4ad7951cafff6afb1366a3b4112f48
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:54 +0000

    walk sort correct performance callat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
