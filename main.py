ff612021ee17b3ee65932eb6c39cd137497ff29d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:41:02 +0000

    scrape money even use Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
