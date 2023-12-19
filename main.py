c76d9d29630037a5faf20bca4359910b2076cd7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:08 +0000

    measure spray none attempt plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
