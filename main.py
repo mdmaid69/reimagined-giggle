f3f0f07a7d47d418b90e6669b04d73d142c0e5d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:42:31 +0000

    shut board heavy meanwhile morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
