0dff51e00815fd105a93dc8093dfd22b0a2a7325
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:37 +0000

    authority lot field middle across

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
