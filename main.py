14389111a5ad4dcc50a42d12640bc47b0ec0ca87
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:56 +0000

    Bb height slightly complete time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
