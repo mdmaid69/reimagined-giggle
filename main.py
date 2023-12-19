b8d19fb8179558d7eeca94b5f129cdc06b133284
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:13:16 +0000

    taste small detail crash mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
