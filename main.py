876662db322b6939beb11e2b68b2f707cfc4e71c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:09 +0000

    rich inside behind upwards service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
