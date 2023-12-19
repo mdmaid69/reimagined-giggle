142afe8dc07e5b1c0fae5034c963ceaff23a852d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:47 +0000

    age seriously like group train

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
