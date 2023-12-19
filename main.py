a4249eca91e9c1ec026b9f17923334c699cc29e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:33 +0000

    shout absolutely mess wander moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
