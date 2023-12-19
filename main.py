3fbd6449fb73515c089fce42ba3851fc90b45a57
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:31 +0000

    mail slide few lean truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
