25b69412f7e89b91bbe33ad8f083baa4208641bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:50:17 +0000

    broken slightly drink near hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+print(re.match("h.*o", "hello world"))
