8ab4bd967275fc21b04b94d4e6378d828c3aa823
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:35 +0000

    round wake reply feed push

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import csv
 def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-def find_min(lst):
-        return min(lst)
