a782d0728691b8419062ee657e3564e4f99b2f78
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:55 +0000

    allow danger completely perhaps drink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
