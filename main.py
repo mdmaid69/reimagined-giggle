6c2e1a18100ca27aa745c5d9896d254eebf167da
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:58 +0000

    mouth thinkof move speed begin

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_acceleration(speed, time):
+        return speed / time
