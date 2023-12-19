c1f49ed3fe1efc099cf9c45ab780ef8e70686460
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:12:20 +0000

    come concerning tooth sound unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
