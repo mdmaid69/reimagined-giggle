56a7a09ac8a55e0c02d698e137a4fdb654cbba1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:29 +0000

    subject university treat project disturbing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def add_numbers(a, b):
+        return a + b
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
