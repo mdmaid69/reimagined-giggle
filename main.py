b8ba22e6456051e0eab7b2190b5982c7c22f7f9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:22 +0000

    blow family contact fill afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
