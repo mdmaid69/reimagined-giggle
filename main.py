3e682a58928545062f0ec8db60253d3f2d674f75
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:31:29 +0000

    contain rain proud worst getback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
