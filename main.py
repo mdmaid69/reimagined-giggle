514e52a268b2df441305a4919cc5a97160087e89
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:44 +0000

    agree delicate hurry mention arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_length(array):
+        return len(array)
