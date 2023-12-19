0f57d7317e49c9dd645a4611c2af0899c3737102
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:58 +0000

    air fight mark emphasis easy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
