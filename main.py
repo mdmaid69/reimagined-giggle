e2c336d2bf5b9d55b8dccb95b54672b018763871
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:52:13 +0000

    know but future note age

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
