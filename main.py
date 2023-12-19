84bdc5f807e35ea34bf3657ede440b7944cbb4db
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:53:27 +0000

    unfortunate tax mess scared send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
