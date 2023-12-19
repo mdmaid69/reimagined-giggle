261693d0db5246b2179d875c8573fc99b5e986f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:30 +0000

    generous round excellent stare arrive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
