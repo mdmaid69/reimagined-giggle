4d619238155bffdb13adcec16e9c63b74bdde81d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:03 +0000

    determined remember throw short cum

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
