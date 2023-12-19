3b3c151b82869df4966c7e87dbedcbea86167e8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:51:55 +0000

    moon base affect tend slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
