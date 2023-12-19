cc18a70b7dfdf467bfcda53baf134f472d0e48e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:37 +0000

    measure earth enthusiastic interesting record

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def convert_to_octal(n):
+        return oct(n)
