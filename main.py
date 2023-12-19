9d85d8970723dd6cd0cdc79f8c61afc261516e80
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:33 +0000

    turninto impression pull well free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
