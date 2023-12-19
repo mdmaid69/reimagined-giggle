6e4ce952f82fa5cf991342a1d7c0b00eefdf467a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:56 +0000

    prevent hurt read afterwards already

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
