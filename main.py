762bb842dc486cf5b119d8219b2a8852f1504fe1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:40 +0000

    politics her call mood discover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
