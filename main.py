569c6314dbccd4bd8fae97208974a6c6f27adb88
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:50:33 +0000

    desperate genuine mixture remark benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
