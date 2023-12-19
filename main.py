1cb0b4596d513cfd69cb89e3cb95220d5b2064d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:24 +0000

    somewhere rough world thankyou acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import datetime
-def get_today_date():
-        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
