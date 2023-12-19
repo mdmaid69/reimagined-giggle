8af78d0ad9dba98e16a291ae3167aadc047ee753
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:16 +0000

    leg reply herself uponon runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
