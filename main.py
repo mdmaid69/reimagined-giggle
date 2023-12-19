ed7276829de38be05dcadeacbef0af5d9e7758b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:41 +0000

    petrol takeout spring programme minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.ctime()
+def find_max(numbers):
+        return max(numbers)
