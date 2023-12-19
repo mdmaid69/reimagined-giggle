6394161aac1fd19f18c8004bb937a63d79c65134
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:03 +0000

    567enemy mouth flow favourite switch

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def is_even(n):
-        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
