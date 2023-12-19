d5c8b16889f205207cf8797b44ac0b719ec2fd36
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:45:07 +0000

    glad terrible lend peaceful used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
