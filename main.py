0a61f9c27b367565057f877fa75fd1c4a7c12c53
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:05 +0000

    familiar not around swallow funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
 import array
 def get_array_buffer_info(array):
         return array.buffer_info()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
