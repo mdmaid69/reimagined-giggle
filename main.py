236e47ceb1c907a8e8a34310ba3eef82965daaa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:51 +0000

    me address rule use level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
