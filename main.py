6fdc0c98eb6a840381ba9b435c97749fcd7a8c3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:11:35 +0000

    after remember fast add grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
