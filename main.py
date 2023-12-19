2fab41170eee5867b6396e433c8c83c8ba92ff43
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:08 +0000

    change progress complain getup hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_string(array):
+        return array.tostring()
