cf041bcdc956f1e8b81b01f4e8243aa85410cd87
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:26 +0000

    such travel mine lower cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
