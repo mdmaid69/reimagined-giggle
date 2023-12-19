4f3c4afa038118bb322d6a47c406d9f1e910afc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:39 +0000

    consult treat entry run fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
