0e95594498e444eda8d79ec4a15a97d750a7ed7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:08 +0000

    only couple mean concern top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
