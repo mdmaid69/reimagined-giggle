c282e16bf200cd61685067155c209cabd489ed62
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:59 +0000

    fear possession Uu murder full

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
