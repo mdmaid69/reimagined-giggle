1b6a3d616b8ca7c768f96522319e930457f96046
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:03 +0000

    greeting warm consult exaggerate time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
