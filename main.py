5f07fd26902cb81935baac4bd48d7f4bbe0261d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:23 +0000

    debt impatient lift demand general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
