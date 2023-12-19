777455a64fb81c6915d27c86c901514e135c43dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:14 +0000

    matter risk reply fight where

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
