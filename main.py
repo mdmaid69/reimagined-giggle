4c4e23203491f647797b5c3e85a76f726a454b55
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:02 +0000

    precise carry return gate properly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
