3c359ab96e195f7f77ef34f475474dbaf8961cd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:28:39 +0000

    left eat cause cross deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
