ed0c217884eb7884adb7b66781923b982d26562d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:25:07 +0000

    section year history off suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+print(os.getcwd())
