d94313122f1c9f22eacec4cb01b3e4b3c532be3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:52 +0000

    interview murder general exchange image

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def convert_to_octal(n):
+        return oct(n)
