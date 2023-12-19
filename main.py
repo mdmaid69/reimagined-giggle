008380273840db33b54362008c9374c44a938e34
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:49 +0000

    as getup happen later address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
