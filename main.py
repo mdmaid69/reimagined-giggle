188b0bc628574a3926d66aa6adeb8faf5a4a4529
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:19 +0000

    mine come cope emotion will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
