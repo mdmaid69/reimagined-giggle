f66f9aefcf5b2fe1e5070af4a9767d7a8b66e348
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:56:28 +0000

    drive outside tour much help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
