879a3b58792ff83a461b083b29daf74b9309a8af
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:23 +0000

    quiet nose art happen my

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
