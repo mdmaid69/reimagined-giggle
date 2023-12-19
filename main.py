1a8dc00ca08e0277dd0abfb4d64beeeedad16c8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:02 +0000

    definite television feature blind inquire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
