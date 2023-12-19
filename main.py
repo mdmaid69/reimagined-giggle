bd5668711312ff090888db564b19010dbd29c47f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:52 +0000

    switch cut condition software stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
