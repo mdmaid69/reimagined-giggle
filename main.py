25f3c608ab53367f82d158e2fc79bd583629447a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:26 +0000

    behind suck only state past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
