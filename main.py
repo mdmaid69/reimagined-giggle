e7a89373fce7a625316918e4e72b5fa60a70d145
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:13 +0000

    lead group thinkthrough shadow idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import collections
-def create_stack():
-        return collections.deque()
+def create_ordered_dict():
+        return collections.OrderedDict()
