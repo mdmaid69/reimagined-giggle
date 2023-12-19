7d6b42102af5e04339f7b834e64a287c77056d52
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:22 +0000

    treatment course gap fair behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
