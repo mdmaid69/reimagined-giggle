72bcae534397dc75a2268cfa4a452452b6dfaaf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:17:41 +0000

    pool trip leg market fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
