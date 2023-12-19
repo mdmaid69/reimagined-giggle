19282dd05b62875b1c64d50575d373f0fbac388e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:14 +0000

    again raise trouble proper improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
