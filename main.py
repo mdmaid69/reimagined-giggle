642fbb3ac6490a4e555be356f340a339ef9f5a53
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:41 +0000

    switch expect as fashion 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
