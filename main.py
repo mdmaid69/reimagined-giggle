464d480b39a5c4bd80e02bcd902302c97dafe652
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:02 +0000

    stage serious neat hot floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
