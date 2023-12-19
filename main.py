32b82d5c11001b61def56ecd6dd9ff2780c6866d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:54 +0000

    sail comment average spill Ee

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def count_elements(lst):
-        return len(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
