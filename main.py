145e7b20c47c225e78b9016876b9cda8d838dfea
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:09 +0000

    back run sore basic story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
