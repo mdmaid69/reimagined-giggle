30534bbbdcd4028d222617b62576ba3767998c73
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:18:58 +0000

    job allow stay path naturally

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
