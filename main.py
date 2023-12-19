cf20d7cf2b638528d9613aee22114544786de9e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:35 +0000

    guess go limit discover so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
