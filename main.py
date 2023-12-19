91e391e4ed2a3ea65b4b4c778f7b6f150c291b84
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:14 +0000

    different talkback fall remain sore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
