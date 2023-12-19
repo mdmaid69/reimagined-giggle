438f8d30142fadc32b64c289a77b9064a57f50ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:29 +0000

    callat morning crowd finger swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
