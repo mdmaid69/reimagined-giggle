ef81c74daa179afb69d05ade15ba7c0cfa548095
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:22 +0000

    calm dance trust danger food

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
