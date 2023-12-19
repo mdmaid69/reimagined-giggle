7032675de5a91ea78096fb9f5d28ef61fd01b101
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:03:06 +0000

    body poison at talk let

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-for i in range(5):
-        print(i)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
