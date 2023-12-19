db8e210b2304fe639d1b9172793d292598b777ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:36:51 +0000

    middle makeup effect occasion space

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
