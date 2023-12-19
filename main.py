adbb4c5490cecba137ff42117e5830d333266ae1
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:36:30 +0000

    glass fortunately love flood control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
