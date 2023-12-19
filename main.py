d057965c7af35604c76f53a4699aba12231c4be1
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:01 +0000

    actually Tt ring shape OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
