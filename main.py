170427e010e20f0b01a61ff03eb51416b021446e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:01 +0000

    possibly trial bet study test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
