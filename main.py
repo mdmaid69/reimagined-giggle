b40f26f7b6cb2f192848a6c01f939025082a8337
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:26:44 +0000

    score Rr right discussion intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
