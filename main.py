9d22c4a031a7d133f6c1f15566e535c48e85aaf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:51 +0000

    spend impressed draw false plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
